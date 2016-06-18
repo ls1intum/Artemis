209bd1163d6447e7b2e8aa79869818c14259d06b
Dominik Muench <dominikmuench@gmail.com>
2016-06-18 17:04:20 +0200

    Add possibility to export results for exercise as CSV file

--- src/main/webapp/app/instructor-dashboard/instructor-dashboard.component.js
+++ src/main/webapp/app/instructor-dashboard/instructor-dashboard.component.js
@@ -12,16 +12,34 @@
             templateUrl: 'app/instructor-dashboard/instructor-dashboard.html'
         });
 
-    InstructorDashboardController.$inject = ['ExerciseResults'];
+    InstructorDashboardController.$inject = ['$window', 'ExerciseResults'];
 
-    function InstructorDashboardController (ExerciseResults) {
+    function InstructorDashboardController ($window, ExerciseResults) {
         var vm = this;
 
         vm.$onInit = init;
+        vm.export = exportData;
         vm.refresh = getResults;
 
         function init() {
             getResults();
+        }
+
+        function exportData() {
+            if (vm.results.length > 0) {
+                var rows = [];
+                vm.results.forEach(function (result, index) {
+                    var studentName = result.participation.student.firstName;
+                    rows.push(index === 0 ? 'data:text/csv;charset=utf-8,' + studentName : studentName);
+                });
+                var csvContent = rows.join('\n');
+                var encodedUri = encodeURI(csvContent);
+                var link = document.createElement('a');
+                link.setAttribute('href', encodedUri);
+                link.setAttribute('download', 'results.csv');
+                document.body.appendChild(link); // Required for FF
+                link.click();
+            }
         }
 
         function getResults() {
