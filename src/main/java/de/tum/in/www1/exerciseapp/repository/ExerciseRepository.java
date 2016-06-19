17460f5e82a3e9f4a75edd56fa3cf9cacc062569
Dominik Muench <dominikmuench@gmail.com>
2016-06-19 21:20:29 +0200

    Fix bug where every course would display all exercises instead of just its own in course view

--- src/main/java/de/tum/in/www1/exerciseapp/repository/ExerciseRepository.java
+++ src/main/java/de/tum/in/www1/exerciseapp/repository/ExerciseRepository.java
@@ -4,16 +4,14 @@ import de.tum.in.www1.exerciseapp.domain.Exercise;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 /**
  * Spring Data JPA repository for the Exercise entity.
  */
 @SuppressWarnings("unused")
-public interface ExerciseRepository extends JpaRepository<Exercise,Long> {
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    @Query(value = "select e from Exercise e where e.course.id = :courseId and e.releaseDate > CURRENT_TIMESTAMP")
     Page<Exercise> findByCourseId(@Param("courseId") Long courseId, Pageable pageable);
 
 }
