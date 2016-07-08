4db9e4dcd6353c3bf422b9b5a0d872e8708d543f
Dominik Muench <dominikmuench@gmail.com>
2016-07-08 14:28:19 +0200

    Add lodash library

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/jquery/dist/jquery.js',
             'src/main/webapp/bower_components/messageformat/messageformat.js',
             'src/main/webapp/bower_components/json3/lib/json3.js',
+            'src/main/webapp/bower_components/lodash/lodash.js',
             'src/main/webapp/bower_components/sockjs-client/dist/sockjs.js',
             'src/main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
             'src/main/webapp/bower_components/moment/moment.js',
