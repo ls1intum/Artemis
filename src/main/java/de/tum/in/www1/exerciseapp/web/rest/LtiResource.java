7993a1f151ad955a3fb4326a6c8f156d961e7132
Josias Montag <josias@montag.info>
2016-09-26 14:57:27 +0200

    LTI configuration dialog for instructors

--- src/main/java/de/tum/in/www1/exerciseapp/web/rest/LtiResource.java
+++ src/main/java/de/tum/in/www1/exerciseapp/web/rest/LtiResource.java
@@ -3,14 +3,17 @@ package de.tum.in.www1.exerciseapp.web.rest;
 import de.tum.in.www1.exerciseapp.domain.Exercise;
 import de.tum.in.www1.exerciseapp.repository.ExerciseRepository;
 import de.tum.in.www1.exerciseapp.service.LtiService;
+import de.tum.in.www1.exerciseapp.web.rest.dto.ExerciseLtiConfigurationDTO;
 import de.tum.in.www1.exerciseapp.web.rest.dto.LtiLaunchRequestDTO;
 import de.tum.in.www1.exerciseapp.web.rest.util.HeaderUtil;
 import org.imsglobal.lti.launch.LtiVerificationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,6 +35,16 @@ public class LtiResource {
 
     private final Logger log = LoggerFactory.getLogger(LtiResource.class);
 
+
+    @Value("${exerciseapp.lti.id}")
+    private String LTI_ID;
+
+    @Value("${exerciseapp.lti.oauth-key}")
+    private String LTI_OAUTH_KEY;
+
+    @Value("${exerciseapp.lti.oauth-secret}")
+    private String LTI_OAUTH_SECRET;
+
     @Inject
     private LtiService ltiService;
 
@@ -40,11 +53,12 @@ public class LtiResource {
 
 
     /**
-     * POST  /lti/launch/:exerciseId : Launch the exercise app using request by an LTI consumer
+     * POST  /lti/launch/:exerciseId : Launch the exercise app using request by an LTI consumer. Redirects the user to the exercise on success.
      *
-     * @param exerciseId the id of the exercise the user wants to open
-     * @return the ResponseEntity with status 200 (OK), or with status 400 (Bad Request) if the result has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
+     * @param launchRequest the LTI laucnh request (ExerciseLtiConfigurationDTO)
+     * @param exerciseId    the id of the exercise the user wants to open
+     * @param request       HTTP request
+     * @param response      HTTP response
      */
     @RequestMapping(value = "/lti/launch/{exerciseId}",
         method = RequestMethod.POST,
@@ -83,6 +97,38 @@ public class LtiResource {
         response.sendRedirect(redirectUrl);
 
 
+    }
+
+
+    /**
+     * GET  /lti/configuration/:exerciseId : Generates LTI configuration parameters for an exercise.
+     *
+     * @param exerciseId the id of the exercise for the wanted LTI configuration
+     * @param request    HTTP request
+     * @return the ResponseEntity with status 200 (OK) and with body the LTI configuration, or with status 404 (Not Found)
+     */
+    @RequestMapping(value = "/lti/configuration/{exerciseId}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ExerciseLtiConfigurationDTO> exerciseLtiConfiguration(@PathVariable("exerciseId") Long exerciseId, HttpServletRequest request) {
+        Exercise exercise = exerciseRepository.findOne(exerciseId);
+
+
+        return Optional.ofNullable(exercise)
+            .map(result -> {
+                String launchUrl = request.getScheme() + // "http"
+                    "://" +                                // "://"
+                    request.getServerName() +              // "myhost"
+                    ":" +                                  // ":"
+                    request.getServerPort() +
+                    "/api/lti/launch/" + exercise.getId();
+
+                String ltiId = LTI_ID;
+                String ltiPassport = LTI_ID + ":" + LTI_OAUTH_KEY + ":" + LTI_OAUTH_SECRET;
+                return new ResponseEntity<>(new ExerciseLtiConfigurationDTO(launchUrl, ltiId, ltiPassport), HttpStatus.OK);
+            })
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 
