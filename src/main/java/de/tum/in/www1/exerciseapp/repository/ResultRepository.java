218b015b34a8dd4becce0e8098c77fbdec643094
Josias Montag <josias@montag.info>
2016-10-05 18:22:01 +0200

    report exact score to LTI consumer
    depending on number of failed/successful tests

--- src/main/java/de/tum/in/www1/exerciseapp/repository/ResultRepository.java
+++ src/main/java/de/tum/in/www1/exerciseapp/repository/ResultRepository.java
@@ -25,8 +25,8 @@ public interface ResultRepository extends JpaRepository<Result, Long> {
     @Query("select r from Result r where r.buildCompletionDate = (select min(rr.buildCompletionDate) from Result rr where rr.participation.exercise.id = :exerciseId and rr.participation.student.id = r.participation.student.id and rr.buildSuccessful = true) and r.participation.exercise.id = :exerciseId and r.buildSuccessful = true order by r.buildCompletionDate asc")
     List<Result> findEarliestSuccessfulResultsForExercise(@Param("exerciseId") Long exerciseId);
 
-    @Query("select r from Result r where r.participation.id = :participationId and r.buildSuccessful = true order by r.buildCompletionDate asc")
-    List<Result> findEarliestSuccessfulResultsForParticipationId(@Param("participationId") Long participationId);
-
     List<Result> findByParticipationExerciseIdAndBuildSuccessfulOrderByBuildCompletionDateAsc(Long exerciseId, boolean buildSuccessful);
+
+    Optional<Result> findFirstByParticipationIdOrderByBuildCompletionDateDesc(Long participationId);
+
 }
