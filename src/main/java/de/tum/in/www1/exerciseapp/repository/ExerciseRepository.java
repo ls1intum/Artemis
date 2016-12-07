2e5c479fc06afee53a2b7e516624da626cf836c0
Josias Montag <josias@montag.info>
2016-12-07 15:12:48 +0100

    withLtiOutcomeUrlExisting as optional parameter for API endpoint

--- src/main/java/de/tum/in/www1/exerciseapp/repository/ExerciseRepository.java
+++ src/main/java/de/tum/in/www1/exerciseapp/repository/ExerciseRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.security.Principal;
+
 /**
  * Spring Data JPA repository for the Exercise entity.
  */
@@ -24,7 +26,7 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
      * there does exist an LtiOutcomeUrl for the current user (-> user has started exercise once using LTI)
      *
      */
-    @Query("SELECT e FROM Exercise e WHERE e.course.id =  :#{#courseId} AND ((NOT EXISTS(SELECT l from LtiOutcomeUrl l WHERE e = l.exercise)) OR EXISTS (SELECT l2 from LtiOutcomeUrl l2 WHERE e = l2.exercise AND l2.user.login = ?#{principal})) ")
-    Page<Exercise> findByCourseIdWhereLtiOutcomeUrlExists(@Param("courseId") Long courseId, Pageable pageable);
+    @Query("SELECT e FROM Exercise e WHERE e.course.id =  :#{#courseId} AND ((NOT EXISTS(SELECT l from LtiOutcomeUrl l WHERE e = l.exercise)) OR EXISTS (SELECT l2 from LtiOutcomeUrl l2 WHERE e = l2.exercise AND l2.user.login = :#{#principal.name})) ")
+    Page<Exercise> findByCourseIdWhereLtiOutcomeUrlExists(@Param("courseId") Long courseId, @Param("principal") Principal principal, Pageable pageable);
 
 }
