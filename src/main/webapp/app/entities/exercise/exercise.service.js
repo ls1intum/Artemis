1d20bb9125a9519f88aca1a0eeedee2c082741c1
Josias Montag <josias@montag.info>
2016-12-27 11:50:22 +0100

    Immediately filter Course and Participation objects #1

--- src/main/webapp/app/entities/exercise/exercise.service.js
+++ src/main/webapp/app/entities/exercise/exercise.service.js
@@ -1,54 +1,66 @@
-(function() {
-    'use strict';
-    angular
-        .module('exerciseApplicationApp')
-        .factory('Exercise', Exercise)
-        .factory('ExerciseResults', ExerciseResults)
-        .factory('ExerciseLtiConfiguration', ExerciseLtiConfiguration);
+(function () {
+        'use strict';
+        angular
+            .module('exerciseApplicationApp')
+            .factory('Exercise', Exercise)
+            .factory('ExerciseResults', ExerciseResults)
+            .factory('ExerciseLtiConfiguration', ExerciseLtiConfiguration)
+            .factory('ExerciseParticipations', ExerciseParticipations);
 
-    Exercise.$inject = ['$resource', 'DateUtils'];
+        Exercise.$inject = ['$resource', 'DateUtils'];
 
-    function Exercise ($resource, DateUtils) {
-        var resourceUrl =  'api/exercises/:id';
+        function Exercise($resource, DateUtils) {
+            var resourceUrl = 'api/exercises/:id';
 
-        return $resource(resourceUrl, {}, {
-            'query': { method: 'GET', isArray: true},
-            'reset': { method: 'DELETE' , url: 'api/exercises/:id/participations'},
-            'get': {
-                method: 'GET',
-                transformResponse: function (data) {
-                    if (data) {
-                        data = angular.fromJson(data);
-                        data.releaseDate = DateUtils.convertDateTimeFromServer(data.releaseDate);
-                        data.dueDate = DateUtils.convertDateTimeFromServer(data.dueDate);
+            return $resource(resourceUrl, {}, {
+                'query': {method: 'GET', isArray: true},
+                'reset': {method: 'DELETE', url: 'api/exercises/:id/participations'},
+                'get': {
+                    method: 'GET',
+                    transformResponse: function (data) {
+                        if (data) {
+                            data = angular.fromJson(data);
+                            data.releaseDate = DateUtils.convertDateTimeFromServer(data.releaseDate);
+                            data.dueDate = DateUtils.convertDateTimeFromServer(data.dueDate);
+                        }
+                        return data;
                     }
-                    return data;
-                }
-            },
-            'update': { method:'PUT' }
-        });
-    }
+                },
+                'update': {method: 'PUT'}
+            });
+        }
 
-    ExerciseResults.$inject = ['$resource'];
+        ExerciseResults.$inject = ['$resource'];
 
-    function ExerciseResults ($resource) {
-        var resourceUrl =  'api/courses/:courseId/exercises/:exerciseId/results';
+        function ExerciseResults($resource) {
+            var resourceUrl = 'api/courses/:courseId/exercises/:exerciseId/results';
 
-        return $resource(resourceUrl, {}, {
-            'query': { method: 'GET', isArray: true},
-        });
-    }
+            return $resource(resourceUrl, {}, {
+                'query': {method: 'GET', isArray: true}
+            });
+        }
 
 
-    ExerciseLtiConfiguration.$inject = ['$resource'];
+        ExerciseLtiConfiguration.$inject = ['$resource'];
 
-    function ExerciseLtiConfiguration ($resource) {
-        var resourceUrl =  'api/lti/configuration/:exerciseId';
+        function ExerciseLtiConfiguration($resource) {
+            var resourceUrl = 'api/lti/configuration/:exerciseId';
 
-        return $resource(resourceUrl, {}, {
-            'query': { method: 'GET', isArray: true},
-        });
-    }
+            return $resource(resourceUrl, {}, {
+                'query': {method: 'GET', isArray: true}
+            });
+        }
 
 
-})();
+        ExerciseParticipations.$inject = ['$resource'];
+
+        function ExerciseParticipations($resource) {
+            var resourceUrl = 'api/exercise/:exerciseId/participations';
+
+            return $resource(resourceUrl, {}, {
+                'query': {method: 'GET', isArray: true}
+            });
+
+
+        }
+    })();
