94443ebfa572d20d57c3eff53d5feee255f9f956
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-06-15 17:09:24 +0200

    Creating UmlExcercises works now

--- src/main/java/de/tum/in/www1/exerciseapp/domain/enumeration/ExerciseType.java
+++ src/main/java/de/tum/in/www1/exerciseapp/domain/enumeration/ExerciseType.java
@@ -4,5 +4,5 @@ package de.tum.in.www1.exerciseapp.domain.enumeration;
  * The ExerciseType enumeration.
  */
 public enum ExerciseType {
-    CODING,UML_CLASS_DIAGRAMM
+    CODING,UML_CLASS_DIAGRAM
 }
