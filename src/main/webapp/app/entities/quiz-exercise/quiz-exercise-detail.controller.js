fd77f4a648b92fed9124358e752dc1ec61730ebe
Valentin Schlattinger <v.schlattinger@gmail.com>
2017-09-26 00:02:50 +0200

    added page for creating/editing quiz exercises (WORK IN PROGRESS)

--- src/main/webapp/app/entities/quiz-exercise/quiz-exercise-detail.controller.js
+++ src/main/webapp/app/entities/quiz-exercise/quiz-exercise-detail.controller.js
@@ -5,13 +5,16 @@
         .module('artemisApp')
         .controller('QuizExerciseDetailController', QuizExerciseDetailController);
 
-    QuizExerciseDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'QuizExercise', 'Question'];
+    QuizExerciseDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'QuizExercise', 'Question', 'courseEntity'];
 
-    function QuizExerciseDetailController($scope, $rootScope, $stateParams, previousState, entity, QuizExercise, Question) {
+    function QuizExerciseDetailController($scope, $rootScope, $stateParams, previousState, entity, QuizExercise, Question, courseEntity) {
         var vm = this;
 
         vm.quizExercise = entity;
         vm.previousState = previousState.name;
+        vm.quizExercise.course = courseEntity;
+
+        console.log(vm.quizExercise);
 
         var unsubscribe = $rootScope.$on('artemisApp:quizExerciseUpdate', function(event, result) {
             vm.quizExercise = result;
