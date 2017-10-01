b803356d7ce561fde2e4396f8ccf570224e3df1e
Florian Sprang <florian.sprang@tum.de>
2017-10-01 15:03:17 +0200

    Feature/instructor dashboard scores (#30)
    * instrucotr dashboard added 3 new displaying types
    
    types:
    - participation in exerciseses in percent
    - succesfull participated in exerciseses in percent
    - total score out of all participations (not calculated yet)
    
    * instrucotr dashboard added 3 new displaying types
    
    types:
    - participation in exerciseses in percent
    - succesfull participated in exerciseses in percent
    - total score out of all participations (not calculated yet)
    
    * instructor dashboard fixed wrong percentage showing
    
    fixed error where no participations were found for percentage calculation
    
    * ordering now working
    
    Apllied changes so that list is ordered correctly for 100%
    
    * overAllScore not working beginning edition
    
    * overAllScore displaying but apparently not mapped correctly
    
    * fixed error where overallScore was misscalculated, fixed displaying
    
    * cleanup of Code
    
    * added comments and more cleanup
    
    * some renaming to make code more readable
    
    * removing unneeded imports
    
    * setting authorization in resource
    
    * changed rest reply to send participation with user, further commenting
    
    * improved code understanding
    
    * comments and formatting
    
    * updated comments, moved division to server code so functionality is at the same place
    
    * changed clientside to not divide any longer
    
    * corrected naming of functions
    
    * added requested change to calculate the taken result differently
    
    Case 1: there is a due date:
    first determine the latest result within the due date (i.e. the one that is closest to the due date, but still before it), and simply use this result
    Case 2: there is no due date:
    simply take the result with the latest completion date
    
    * changed implementation of best Result
    
    * changed naming slightly since not the best result is taken any longer

--- src/main/java/de/tum/in/www1/exerciseapp/service/CourseService.java
+++ src/main/java/de/tum/in/www1/exerciseapp/service/CourseService.java
@@ -1,8 +1,6 @@
 package de.tum.in.www1.exerciseapp.service;
 
-import de.tum.in.www1.exerciseapp.domain.Authority;
-import de.tum.in.www1.exerciseapp.domain.Course;
-import de.tum.in.www1.exerciseapp.domain.User;
+import de.tum.in.www1.exerciseapp.domain.*;
 import de.tum.in.www1.exerciseapp.repository.CourseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -45,9 +43,9 @@ public class CourseService {
     }
 
     /**
-     *  Get all the courses.
+     * Get all the courses.
      *
-     *  @return the list of entities
+     * @return the list of entities
      */
     @Transactional(readOnly = true)
     public List<Course> findAll() {
@@ -68,10 +66,10 @@ public class CourseService {
     }
 
     /**
-     *  Get all the courses.
+     * Get all the courses.
      *
-     *  @param pageable the pagination information
-     *  @return the list of entities
+     * @param pageable the pagination information
+     * @return the list of entities
      */
     @Transactional(readOnly = true)
     public Page<Course> findAll(Pageable pageable) {
@@ -80,10 +78,10 @@ public class CourseService {
     }
 
     /**
-     *  Get one course by id.
+     * Get one course by id.
      *
-     *  @param id the id of the entity
-     *  @return the entity
+     * @param id the id of the entity
+     * @return the entity
      */
     @Transactional(readOnly = true)
     public Course findOne(Long id) {
@@ -92,9 +90,9 @@ public class CourseService {
     }
 
     /**
-     *  Delete the  course by id.
+     * Delete the  course by id.
      *
-     *  @param id the id of the entity
+     * @param id the id of the entity
      */
     public void delete(Long id) {
         log.debug("Request to delete Course : {}", id);
@@ -104,5 +102,91 @@ public class CourseService {
     public List<String> getAllTeachingAssistantGroupNames() {
         List<Course> courses = courseRepository.findAll();
         return courses.stream().map(c -> c.getTeachingAssistantGroupName()).collect(Collectors.toList());
+    }
+
+    /**
+     * Getting a Collection of Results in which the average Score of a course is returned as a result
+     *
+     * @param courseId the courseId
+     * @return the collection of results in the result score the average score is saved, which contains the participation and the user
+     */
+    @Transactional(readOnly = true)
+    public Collection<Result> getAllOverallScoresOfCourse(Long courseId) {
+        Course course = findOne(courseId);
+        Set<Exercise> exercisesOfCourse = course.getExercises();
+        //key stores the userId to identify if he already got a score, value contains the Result itself with the score of the user
+        HashMap<Long, Result> allOverallSummedScoresOfCourse = new HashMap<>();
+
+        for (Exercise exercise : exercisesOfCourse) {
+            Set<Participation> participations = exercise.getParticipations();
+            boolean exerciseHasDueDate = exercise.getDueDate() != null;
+
+            for (Participation participation : participations) {
+
+                //id of user in the datbase to reference to the user
+                long userID = participation.getStudent().getId();
+                Result bestResult = choseResultInParticipation(participation, exerciseHasDueDate);
+
+                //if student already appeared, once add the new score to the old one
+                if (allOverallSummedScoresOfCourse.containsKey(userID)) {
+                    long oldScore = allOverallSummedScoresOfCourse.get(userID).getScore();
+                    bestResult.setScore(oldScore + bestResult.getScore());
+                    allOverallSummedScoresOfCourse.remove(userID);
+                }
+                allOverallSummedScoresOfCourse.put(userID, bestResult);
+            }
+        }
+
+        //divide the scores by the amount of exercises to get the average Score of all Exercises
+        Collection<Result> allOverallScores = allOverallSummedScoresOfCourse.values();
+        int numberOfExercises = exercisesOfCourse.size();
+        for (Result result : allOverallScores) {
+            result.setScore(result.getScore() / (long) numberOfExercises);
+        }
+
+        return allOverallScores;
+    }
+
+    /**
+     * Find the best Result in a Participation
+     *
+     * @param participation the participation you want the best result from
+     * @param hasDueDate if the participation has a duedate take last result before the due date if not take the overall last result
+     * @return the best result a student had within the time of the exercise
+     */
+    @Transactional(readOnly = true)
+    public Result choseResultInParticipation(Participation participation, boolean hasDueDate) {
+        List<Result> results = new ArrayList<Result>(participation.getResults());
+
+        Result chosenResult;
+        //edge case of no result submitted to a participation
+        if(results.size() <= 0){
+            chosenResult = new Result();
+            chosenResult.setScore((long) 0);
+            chosenResult.setParticipation(participation);
+            return chosenResult;
+        }
+
+        //sorting in descending order to have the last result at the beginning
+        results.sort(Comparator.comparing(Result::getCompletionDate).reversed());
+
+        if(hasDueDate){
+            //find the first result that is before the due date otherwise handles the case where all results were submitted after the due date,
+            chosenResult = results.stream()
+                .filter(x -> x.getCompletionDate().isBefore(participation.getExercise().getDueDate()))
+                .findFirst()
+                .orElse(new Result());
+        }else{
+            chosenResult = results.remove(0); //no due date use last result
+        }
+
+        //edge case where the db has stored null for score
+        if (chosenResult.getScore() == null) {
+            chosenResult.setScore((long) 0);
+        }
+        //setting participation in result to have student id later
+        chosenResult.setParticipation(participation);
+
+        return chosenResult;
     }
 }
