c6d2cb0451072a55d956b9889998a2fa076a733f
Valentin Schlattinger <v.schlattinger@gmail.com>
2017-10-04 23:37:37 +0200

    save new quiz exercises

--- src/main/webapp/app/entities/quiz-exercise/quiz-exercise-detail.controller.js
+++ src/main/webapp/app/entities/quiz-exercise/quiz-exercise-detail.controller.js
@@ -15,38 +15,42 @@
         vm.previousState = previousState.name;
         vm.quizExercise.course = courseEntity;
         vm.datePickerOpenStatus = {
-            plannedStartDateTime: false
+            releaseDate: false
         };
-        vm.statusOptions = [
+        vm.isSaving = false;
+        vm.true = true;
+        vm.statusOptionsVisible = [
             {
-                key: 1,
+                key: false,
                 label: "Hidden"
             },
             {
-                key: 2,
+                key: true,
                 label: "Visible"
-            },
+            }
+        ];
+        vm.statusOptionsPractice = [
             {
-                key: 3,
-                label: "Active"
-            },
-            {
-                key: 4,
+                key: false,
                 label: "Closed"
             },
             {
-                key: 5,
+                key: true,
                 label: "Open for Practice"
             }
         ];
+        vm.statusOptionsActive = [
+            {
+                key: true,
+                label: "Active"
+            }
+        ];
+        vm.showDropdown = showDropdown;
         vm.pendingChanges = pendingChanges;
         vm.validQuiz = validQuiz;
         vm.openCalendar = openCalendar;
         vm.addQuestion = addQuestion;
-        vm.goBack = goBack;
         vm.save = save;
-
-        console.log(vm.quizExercise);
 
         var unsubscribe = $rootScope.$on('artemisApp:quizExerciseUpdate', function (event, result) {
             vm.quizExercise = result;
@@ -55,6 +59,18 @@
 
         function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
+        }
+
+        function showDropdown() {
+            if (vm.quizExercise.isPlannedToStart) {
+                var plannedEndMoment = moment(vm.quizExercise.releaseDate).add(vm.quizExercise.duration, "minutes");
+                if (plannedEndMoment.isBefore(moment())) {
+                    return "isOpenForPractice";
+                } else if(moment(vm.quizExercise.releaseDate).isBefore(moment())) {
+                    return "active";
+                }
+            }
+            return "isVisibleBeforeStart";
         }
 
         function addQuestion() {
@@ -66,8 +82,8 @@
             return [
                 "title",
                 "duration",
-                "plannedStart",
-                "plannedStartDateTime",
+                "isPlannedToStart",
+                "releaseDate",
                 "status",
                 "questions"
             ].some(function (key) {
@@ -76,40 +92,25 @@
         }
 
         function validQuiz() {
-            if (!vm.quizExercise.title || vm.quizExercise.title === "") {
-                return false;
-            }
-            if (!vm.quizExercise.duration) {
-                return false;
-            }
-            if (vm.quizExercise.plannedStart) {
-                switch (vm.quizExercise.status) {
-                    case 1:
-                    case 2:
-                        if (moment(vm.quizExercise.plannedStartDateTime).isBefore(moment())) {
-                            return false;
-                        }
-                        break;
-                    case 3:
-                    case 4:
-                    case 5:
-                        if (moment(vm.quizExercise.plannedStartDateTime).isAfter(moment())) {
-                            return false;
-                        }
-                        break;
-                }
-            }
-            return true;
-        }
-
-        function goBack() {
-            // TODO
-            alert("TODO");
+            return vm.quizExercise.title && vm.quizExercise.title !== "" && vm.quizExercise.duration;
         }
 
         function save() {
-            // TODO
-            alert("TODO");
+            vm.isSaving = true;
+            if (vm.quizExercise.id) {
+                QuizExercise.update(vm.quizExercise, onSaveSuccess, onSaveError);
+            } else {
+                QuizExercise.save(vm.quizExercise, onSaveSuccess, onSaveError);
+            }
+        }
+        function onSaveSuccess(result) {
+            vm.isSaveing = false;
+            savedEntity = Object.assign({}, result);
+            vm.quizExercise = result;
+        }
+        function onSaveError() {
+            alert("Saving Quiz Failed! Please try again later.");
+            vm.isSaveing = false;
         }
     }
 })();
