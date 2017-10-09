0ec1caa18c843bb80c37ab05c9a5fade7e6f5b0e
Stephan Krusche <s.krusche@tum.de>
2017-10-09 02:42:31 +0200

    code improvements after IntelliJ Code Inspect
    Signed-off-by: Stephan Krusche <s.krusche@tum.de>

--- src/main/java/de/tum/in/www1/exerciseapp/domain/enumeration/ParticipationState.java
+++ src/main/java/de/tum/in/www1/exerciseapp/domain/enumeration/ParticipationState.java
@@ -6,7 +6,7 @@ package de.tum.in.www1.exerciseapp.domain.enumeration;
 public enum ParticipationState {
     UNINITIALIZED(0), REPO_COPIED(1), REPO_CONFIGURED(2), INACTIVE(3), BUILD_PLAN_COPIED(4), BUILD_PLAN_CONFIGURED(5), INITIALIZED(6);
 
-    private Integer stateNumber;
+    private final Integer stateNumber;
 
     ParticipationState(int stateNumber) {
         this.stateNumber = stateNumber;
