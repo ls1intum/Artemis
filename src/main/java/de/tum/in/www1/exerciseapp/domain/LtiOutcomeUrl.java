30e25946d4d7489089dc4e3026e1bbd332a3558f
Stephan Krusche <s.krusche@tum.de>
2017-10-17 15:50:17 +0200

    fix wrong annotation in LtiOutcomeUrl

--- src/main/java/de/tum/in/www1/exerciseapp/domain/LtiOutcomeUrl.java
+++ src/main/java/de/tum/in/www1/exerciseapp/domain/LtiOutcomeUrl.java
@@ -18,7 +18,7 @@ public class LtiOutcomeUrl implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "url")
