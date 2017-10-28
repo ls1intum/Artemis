d94f43c5ef68642205f9f5f3e5ec500595c4c9f3
Stephan Krusche <s.krusche@tum.de>
2017-10-28 16:45:38 +0200

    Generated with JHipster 4.10.2

--- src/test/java/de/tum/in/www1/exerciseapp/web/rest/CourseResourceIntTest.java
+++ src/test/java/de/tum/in/www1/exerciseapp/web/rest/CourseResourceIntTest.java
@@ -23,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static de.tum.in.www1.exerciseapp.web.rest.TestUtil.createFormattingConversionService;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -69,6 +70,7 @@ public class CourseResourceIntTest {
         this.restCourseMockMvc = MockMvcBuilders.standaloneSetup(courseResource)
             .setCustomArgumentResolvers(pageableArgumentResolver)
             .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
             .setMessageConverters(jacksonMessageConverter).build();
     }
 
