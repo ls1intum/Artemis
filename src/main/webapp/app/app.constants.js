04f6a5d631bb1637274d3c993d55ca6f60380633
VitaNuova <vitanuova0512@gmail.com>
2017-11-29 16:17:04 +0100

    reverted app constants

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('artemisApp')
         .constant('VERSION', "1.3.2")
-        .constant('DEBUG_INFO_ENABLED', true)
-        .constant('BUILD_TIMESTAMP', "")
+        .constant('DEBUG_INFO_ENABLED', false)
+        .constant('BUILD_TIMESTAMP', 1509203409638)
 ;
 })();
