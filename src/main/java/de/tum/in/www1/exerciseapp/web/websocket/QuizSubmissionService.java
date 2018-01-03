4f0e63912866db768f8cc84413d2f3cc37b02e9b
Valentin Schlattinger <v.schlattinger@gmail.com>
2018-01-03 16:04:23 +0100

    Removed Database Access from QuizSubmissionService to improve performance. Submissions are now saved in a ConcurrentHashMap until they get submitted.

--- src/main/java/de/tum/in/www1/exerciseapp/web/websocket/QuizSubmissionService.java
+++ src/main/java/de/tum/in/www1/exerciseapp/web/websocket/QuizSubmissionService.java
@@ -1,9 +1,6 @@
 package de.tum.in.www1.exerciseapp.web.websocket;
 
 import de.tum.in.www1.exerciseapp.domain.*;
-import de.tum.in.www1.exerciseapp.domain.enumeration.ParticipationState;
-import de.tum.in.www1.exerciseapp.repository.QuizSubmissionRepository;
-import de.tum.in.www1.exerciseapp.repository.ResultRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -13,65 +10,71 @@ import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 import java.time.ZonedDateTime;
-import java.util.Optional;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class QuizSubmissionService {
     private static final Logger log = LoggerFactory.getLogger(QuizSubmissionService.class);
 
-    private final SimpMessageSendingOperations messagingTemplate;
-    private final QuizSubmissionRepository quizSubmissionRepository;
-    private final ResultRepository resultRepository;
+    private static final Map<String, QuizSubmission> cachedSubmissions = new ConcurrentHashMap<>();
 
-    public QuizSubmissionService(SimpMessageSendingOperations messagingTemplate, QuizSubmissionRepository quizSubmissionRepository, ResultRepository resultRepository) {
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public QuizSubmissionService(SimpMessageSendingOperations messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
-        this.quizSubmissionRepository = quizSubmissionRepository;
-        this.resultRepository = resultRepository;
+    }
+
+    /**
+     * generate the key for cached submissions using the username (for security reasons) and the submission id
+     * @param username the username of the user who sent the submission
+     * @param submissionId the id of the submission
+     * @return the key used in the cachedSubmissions Map
+     */
+    private static String keyForUsernameAndSubmissionId(String username, Long submissionId) {
+        return username + "_&_" + submissionId;
+    }
+
+    /**
+     * Get the cached submission for the given username and submission id
+     * @param username the username of the user who sent the submission
+     * @param submissionId the id of the submission
+     * @return The QuizSubmission object (answers saved by user through websocket)
+     */
+    public static QuizSubmission getCachedSubmission(String username, Long submissionId) {
+        return cachedSubmissions.get(keyForUsernameAndSubmissionId(username, submissionId));
+    }
+
+    /**
+     * Removes an entry from the cached submissions (should be used at the end of a quiz to free up memory)
+     * @param username the username of the user who sent the submission
+     * @param submissionId the id of the submission
+     */
+    public static void removeCachedSubmission(String username, Long submissionId) {
+        cachedSubmissions.remove(keyForUsernameAndSubmissionId(username, submissionId));
     }
 
     @SubscribeMapping("/topic/quizSubmissions/{submissionId}/save")
     public void sendSubmission(@Payload QuizSubmission quizSubmission, Principal principal) {
         log.debug("Received Submission over Websocket: {}", quizSubmission);
 
+        // do nothing if id is missing
+        if (quizSubmission.getId() == null) {
+            return;
+        }
+
         // recreate pointers back to submission in each submitted answer
         for (SubmittedAnswer submittedAnswer : quizSubmission.getSubmittedAnswers()) {
             submittedAnswer.setSubmission(quizSubmission);
         }
 
-        if (quizSubmission.getId() == null) {
-            return;
-        }
+        // set submission date for response
+        quizSubmission.setSubmissionDate(ZonedDateTime.now());
 
-        // update corresponding result
-        Optional<Result> resultOptional = resultRepository.findDistinctBySubmissionId(quizSubmission.getId());
-        if (resultOptional.isPresent()) {
-            Result result = resultOptional.get();
-            Participation participation = result.getParticipation();
-            QuizExercise quizExercise = (QuizExercise) participation.getExercise();
-            User user = participation.getStudent();
-            // check if participation (and thus submission) actually belongs to the user who sent this message
-            if (principal.getName().equals(user.getLogin())) {
-                // only update if exercise hasn't ended already
-                if (quizExercise.isSubmissionAllowed() && participation.getInitializationState() == ParticipationState.INITIALIZED) {
-                    // save changes to submission
-                    quizSubmission = quizSubmissionRepository.save(quizSubmission);
+        // save submission in cache
+        cachedSubmissions.put(keyForUsernameAndSubmissionId(principal.getName(), quizSubmission.getId()), quizSubmission);
 
-                    // update completion date (which also functions as submission date for now)
-                    result.setCompletionDate(ZonedDateTime.now());
-                    resultRepository.save(result);
-                } else {
-                    // overwrite with existing submission to send back unchanged submission
-                    quizSubmission = (QuizSubmission) result.getSubmission();
-                    // remove scores from submission if quiz hasn't ended yet
-                    if (quizSubmission.isSubmitted() && quizExercise.shouldFilterForStudents()) {
-                        quizSubmission.removeScores();
-                    }
-                }
-                // set submission date for response
-                quizSubmission.setSubmissionDate(result.getCompletionDate());
-                // send response (to all subscribers)
-                messagingTemplate.convertAndSend("/topic/quizSubmissions/" + quizSubmission.getId(), quizSubmission);
-            }
-        }
+        // send response (to all subscribers)
+        messagingTemplate.convertAndSend("/topic/quizSubmissions/" + quizSubmission.getId(), quizSubmission);
     }
 }
