c73b72a4cb504d5212a5fa9d07041d8af454e123
Valentin Schlattinger <v.schlattinger@gmail.com>
2018-01-22 22:42:51 +0100

    automatically adjust text-size in drag items

---
+++ src/main/webapp/app/components/util/textfill.directive.js
@@ -0,0 +1,56 @@
+/**
+ * Copied and adapted from https://github.com/HenningM/angular-textfill/blob/master/angular-textfill.js
+ * (accessed: 2018-01-22 22:14)
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('artemisApp')
+        .directive('textfill', textfill);
+
+    textfill.$inject = ['$window'];
+
+    function textfill($window) {
+        return {
+            restrict: 'A',
+            scope: {
+                textfill: '@'
+            },
+            template: '<span>{{textfill}}</span>',
+            link: function (scope, element, attr) {
+                var options = {
+                    innerTag: attr.innerTag || "span",
+                    debug: attr.debug || false,
+                    minFontPixels: parseInt(attr.minFontPixels) || 6,
+                    maxFontPixels: parseInt(attr.maxFontPixels) || 30,
+                    widthOnly: attr.widthOnly || false,
+                    explicitHeight: attr.explicitHeight || null,
+                    explicitWidth: attr.explicitWidth || null
+                };
+
+                function runTextFill() {
+                    element.textfill(options);
+                }
+
+                runTextFill();
+
+                // react to changes in model
+                scope.$watch('textfill', runTextFill);
+
+                // react to resize event
+                function onResize() {
+                    runTextFill();
+                    scope.$digest();
+                }
+                angular.element($window).bind('resize', onResize);
+
+                // clean up when element is removed
+                scope.$on('$destroy', function () {
+                    angular.element($window).unbind('resize', onResize);
+                });
+            }
+        };
+    }
+})();
