58a375177f6e316cce92bb910284b4a15ad18b31
Valentin Schlattinger <v.schlattinger@gmail.com>
2018-02-06 13:13:50 +0100

    - only automatically reconnect when participating in a quiz and before the quiz has ended - show quiz-footer with connection status when opening quiz before start

--- src/main/webapp/app/admin/websocket/websocket.service.js
+++ src/main/webapp/app/admin/websocket/websocket.service.js
@@ -18,6 +18,7 @@
         var alreadyConnectedOnce = false;
         var isConnected = false;
         var consecutiveFailedAttempts = 0;
+        var shouldReconnect = false;
 
         var service = {
             connect: connect,
@@ -28,40 +29,47 @@
             subscribe: subscribe,
             unsubscribe: unsubscribe,
             bind: bind,
-            unbind: unbind
+            unbind: unbind,
+            enableReconnect: enableReconnect,
+            disableReconnect: disableReconnect
         };
 
         return service;
 
         //adapted from https://stackoverflow.com/questions/22361917/automatic-reconnect-with-stomp-js-in-node-js-application
         function stompFailureCallback(error) {
-            console.error("Websocket disconnect due to: " + error)
+            // console.error("Websocket disconnect due to: " + error); // this console.error is not needed, because error is already logged by stomp-websocket
             isConnected = false;
             consecutiveFailedAttempts++;
             disconnectListener.forEach(function (listener) {
                 listener();
             });
-            // NOTE: after 5 failed attempts in row, increase the timeout to 5 seconds,
-            // after 10 failed attempts in row, increase the timeout to 10 seconds
-            // after 20 failed attempts in row, increase the timeout to 20 seconds
-            // after 30 failed attempts in row, increase the timeout to 60 seconds
-            var waitUntilReconnectAttempt;
-            if (consecutiveFailedAttempts > 30) {
-                waitUntilReconnectAttempt = 60
-            } else if (consecutiveFailedAttempts > 20) {
-                waitUntilReconnectAttempt = 20
-            } else if (consecutiveFailedAttempts > 10) {
-                waitUntilReconnectAttempt = 10;
-            } else if (consecutiveFailedAttempts > 5) {
-                waitUntilReconnectAttempt = 5;
-            } else {
-                waitUntilReconnectAttempt = 1;
+            if (shouldReconnect) {
+                // NOTE: after 5 failed attempts in row, increase the timeout to 5 seconds,
+                // after 10 failed attempts in row, increase the timeout to 10 seconds
+                // after 20 failed attempts in row, increase the timeout to 20 seconds
+                // after 30 failed attempts in row, increase the timeout to 60 seconds
+                var waitUntilReconnectAttempt;
+                if (consecutiveFailedAttempts > 30) {
+                    waitUntilReconnectAttempt = 60
+                } else if (consecutiveFailedAttempts > 20) {
+                    waitUntilReconnectAttempt = 20
+                } else if (consecutiveFailedAttempts > 10) {
+                    waitUntilReconnectAttempt = 10;
+                } else if (consecutiveFailedAttempts > 5) {
+                    waitUntilReconnectAttempt = 5;
+                } else {
+                    waitUntilReconnectAttempt = 1;
+                }
+                setTimeout(connect, waitUntilReconnectAttempt * 1000);
+                console.log("Websocket: Try to reconnect in " + waitUntilReconnectAttempt + " seconds...");
             }
-            setTimeout(connect, waitUntilReconnectAttempt * 1000);
-            console.log("Websocket: Try to reconnect in " + waitUntilReconnectAttempt + " seconds...");
         }
 
         function connect () {
+            if (isConnected) {
+                return; // don't connect, if already connected
+            }
             //building absolute path so that websocket doesn't fail when deploying with a context path
             var loc = $window.location;
             var url = '//' + loc.host + loc.pathname + 'websocket/tracker';
@@ -201,6 +209,23 @@
                     });
                     break;
             }
+        }
+
+        /**
+         * enable automatic reconnect
+         */
+        function enableReconnect() {
+            shouldReconnect = true;
+            if(!isConnected) {
+                connect();
+            }
+        }
+
+        /**
+         * disable automatic reconnect
+         */
+        function disableReconnect() {
+            shouldReconnect = false;
         }
     }
 })();
