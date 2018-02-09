c9e9865ab94e85a375114c1cce89da6b0731c649
Stephan Krusche <krusche@in.tum.de>
2018-02-09 16:35:20 +0100

    Bugfix/websockets (#66)
    * add logging to better understand websocket issues on server
    * allow * origins for websocket connections to prevent error messages

---
+++ src/main/java/de/tum/in/www1/exerciseapp/config/websocket/CustomWebsocketSessionHolder.java
@@ -0,0 +1,32 @@
+package de.tum.in.www1.exerciseapp.config.websocket;
+
+import org.springframework.web.socket.WebSocketSession;
+
+public class CustomWebsocketSessionHolder {
+
+    private WebSocketSession session;
+    private final long createTime;
+    private long lastMessageTime;
+
+    public CustomWebsocketSessionHolder(WebSocketSession session) {
+        this.session = session;
+        this.createTime = System.currentTimeMillis();
+        this.lastMessageTime = this.createTime;
+    }
+
+    public WebSocketSession getSession() {
+        return session;
+    }
+
+    public void setSession(WebSocketSession session) {
+        this.session = session;
+    }
+
+    public long getLastMessageTime() {
+        return lastMessageTime;
+    }
+
+    public void setLastMessageTime(long lastMessageTime) {
+        this.lastMessageTime = lastMessageTime;
+    }
+}
