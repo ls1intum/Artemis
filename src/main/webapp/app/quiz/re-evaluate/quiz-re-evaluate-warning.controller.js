48091a1eb7f73825274776d305ff654696023f92
Moritz Issig <m.issig@tum.de>
2018-02-16 18:32:07 +0100

    finished drag-and-drop-re-evaluation editor on frontend

--- src/main/webapp/app/quiz/re-evaluate/quiz-re-evaluate-warning.controller.js
+++ src/main/webapp/app/quiz/re-evaluate/quiz-re-evaluate-warning.controller.js
@@ -14,9 +14,9 @@
         vm.failed = false;
         vm.busy = false;
 
-        vm.answerDeleted = false;
-        vm.answerInvalid = false;
-        vm.answerCorrectess = false;
+        vm.questionElementDeleted = false;
+        vm.questionElementInvalid = false;
+        vm.questionCorrectness = false;
         vm.questionDeleted = false;
         vm.questionInvalid = false;
         vm.scoringChanged = false;
@@ -38,7 +38,7 @@
          *          - it is set invalid
          *          - it has another scoringType
          *          - an answer was deleted
-         *  3. check for each answer if:
+         *  3. check for each question-element if:
          *          - it is set invalid
          *          - the correctness was changed
          *
@@ -61,41 +61,96 @@
                         backUpQuestion = backUpQuiz.questions[i];
                     }
                 }
-                if (backUpQuestion != null) {
+                if (backUpQuestion !== null) {
                     // question set invalid?
                     if (question.invalid !== backUpQuestion.invalid) {
                         vm.questionInvalid = true;
-                    }
-                    // answer deleted?
-                    if (question.answerOptions.length !== backUpQuestion.answerOptions.length) {
-                        vm.answerDeleted = true;
                     }
                     // question scoring changed?
                     if (question.scoringType !== backUpQuestion.scoringType) {
                         vm.scoringChanged = true;
                     }
-                    //check each answer
-                    question.answerOptions.forEach(function (answer) {
-                        // only check if there are no changes on the answers yet
-                        if (!vm.answerCorrectess || !vm.answerInvalid) {
-                            var backUpAnswer = null;
-                            for (var j = 0; backUpAnswer === null && j < backUpQuestion.answerOptions.length; j++) {
-                                if (backUpQuestion.answerOptions[j].id === answer.id) {
-                                    backUpAnswer = backUpQuestion.answerOptions[j];
-                                }
-                            }
-                            if (backUpAnswer != null) {
-                                //answer set invalid?
-                                if (answer.invalid !== backUpAnswer.invalid) {
-                                    vm.answerInvalid = true;
-                                }
-                                //answer correctness changed?
-                                if (answer.isCorrect !== backUpAnswer.isCorrect) {
-                                    vm.answerCorrectess = true;
-                                }
-                            }
+                    //check MultipleChoiceQuestions
+                    if (question.type === "multiple-choice") {
+                        // question-Element deleted?
+                        if (question.answerOptions.length !== backUpQuestion.answerOptions.length) {
+                            vm.questionElementDeleted = true;
                         }
-                    });
+
+                        //check each answer
+                        question.answerOptions.forEach(function (answer) {
+                            // only check if there are no changes on the question-elements yet
+                            if (!vm.questionCorrectness || !vm.questionElementInvalid) {
+                                var backUpAnswer = null;
+                                for (var j = 0; backUpAnswer === null && j < backUpQuestion.answerOptions.length; j++) {
+                                    if (backUpQuestion.answerOptions[j].id === answer.id) {
+                                        backUpAnswer = backUpQuestion.answerOptions[j];
+                                    }
+                                }
+                                if (backUpAnswer !== null) {
+                                    //answer set invalid?
+                                    if (answer.invalid !== backUpAnswer.invalid) {
+                                        vm.questionElementInvalid = true;
+                                    }
+                                    //answer correctness changed?
+                                    if (answer.isCorrect !== backUpAnswer.isCorrect) {
+                                        vm.questionCorrectness = true;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    //check DragAndDropQuestions
+                    if (question.type === "drag-and-drop") {
+                        if (question.dragItems.length !== backUpQuestion.dragItems.length
+                            || question.dropLocations.length !== backUpQuestion.dropLocations.length) {
+                            vm.questionElementDeleted = true;
+                        }
+                        if (!angular.equals(question.correctMappings, backUpQuestion.correctMappings)) {
+                            vm.questionCorrectess = true;
+                        }
+
+                        //check each dragItem
+                        question.dragItems.forEach(function (dragItem) {
+                        // only check if there are no changes on the question-elements yet
+                            if (!vm.questionElementInvalid) {
+                                var backUpDragItem = null;
+                                for (var j = 0; backUpDragItem === null && j < backUpQuestion.dragItems.length; j++) {
+                                    if (backUpQuestion.dragItems[j].id === dragItem.id) {
+                                        backUpDragItem = backUpQuestion.dragItems[j];
+                                    }
+                                }
+                                if (backUpDragItem !== null) {
+                                    //dragItem set invalid?
+                                    if (dragItem.invalid !== backUpDragItem.invalid) {
+                                        vm.questionElementInvalid = true;
+                                    }
+                                }
+                            }
+                        });
+
+                        //check each dropLocation
+                        question.dropLocations.forEach(function (dropLocation) {
+                            // only check if there are no changes on the question-elements yet
+                            if (!vm.questionElementInvalid) {
+                                var backUpDropLocation = null;
+                                for (var j = 0; backUpDropLocation === null && j < backUpQuestion.dropLocations.length; j++) {
+                                    if (backUpQuestion.dropLocations[j].id === dropLocation.id) {
+                                        backUpDropLocation = backUpQuestion.dropLocations[j];
+                                    }
+                                }
+                                if (backUpDropLocation !== null) {
+                                    //dropLocation set invalid?
+                                    if (dropLocation.invalid !== backUpDropLocation.invalid) {
+                                        vm.questionElementInvalid = true;
+                                    }
+                                }
+                            }
+                        });
+                    }
+
+
+
                 }
             });
         }
