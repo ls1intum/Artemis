5f9a2b0c4f934600f0ccdc61ffee7cd48f0bb05c
Moritz Issig <m.issig@tum.de>
2018-02-20 14:45:56 +0100

    refactored edit and re-evaluate DragAndDrop-Question Controllers

--- src/main/webapp/app/components/util/fileUpload.service.js
+++ src/main/webapp/app/components/util/fileUpload.service.js
@@ -10,12 +10,30 @@
         .module('artemisApp')
         .factory('FileUpload', FileUpload);
 
-    FileUpload.$inject = ['$http'];
+    FileUpload.$inject = ['$http', 'MAX_FILE_SIZE', '$q'];
 
     var uploadUrl = "api/fileUpload";
 
-    function FileUpload($http) {
+    function FileUpload($http, MAX_FILE_SIZE, $q) {
         return function (file) {
+
+            var rejectedPromise = $q(function(resolve, reject) {reject()});
+
+            var fileExtension = file.name.split('.').pop().toLocaleLowerCase();
+            if (fileExtension !== "png" && fileExtension !== "jpg" && fileExtension !== "jpeg" && fileExtension !== "svg") {
+                alert('Unsupported file-type! Only files of type ".png", ".jpg" or ".svg" allowed.');
+                return rejectedPromise;
+            }
+            if (file.size > MAX_FILE_SIZE) {
+                alert('File is too big! Maximum allowed file size: ' + MAX_FILE_SIZE / (1024 * 1024) + ' MB.');
+                return rejectedPromise;
+            }
+
+            if (!file) {
+                alert("Please select a file to upload first.");
+                return rejectedPromise;
+            }
+
             var fd = new FormData();
             fd.append('file', file);
             return $http.post(uploadUrl, fd, {
