c3ecc34fb0b005eecfdc1e78b12269623f21377e
Stefan Kreuzer (ga73rog) <stefan.kreuzer@tum.de>
2018-08-01 16:36:47 +0200

    Add missing exports

--- src/main/webapp/app/editor/index.ts
+++ src/main/webapp/app/editor/index.ts
@@ -2,6 +2,10 @@ export * from './editor.component';
 export * from './editor.module';
 export * from './editor.route';
 export * from './ace/editor-ace.component';
-export * from './file-browser/editor-file-browser.component'
-export * from './build-output/editor-build-output.component'
+export * from './file-browser/editor-file-browser.component';
+export * from './file-browser/editor-file-browser-create';
+export * from './file-browser/editor-file-browser-delete';
+export * from './build-output/editor-build-output.component';
+export * from './instructions/editor-instructions.component';
+export * from './instructions/editor-instructions-result-detail';
 export * from './editor.directive';
