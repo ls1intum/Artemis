bbdd52d482f33675d7035071dfe0d1c610d7b5bf
Mai Ton Nu Cam <maitonnucam@googlemail.com>
2018-08-18 12:34:34 +0200

    refactored functions to service

--- src/main/java/de/tum/in/www1/artemis/web/rest/ModelingAssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ModelingAssessmentResource.java
@@ -2,17 +2,12 @@ package de.tum.in.www1.artemis.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import de.tum.in.www1.artemis.domain.*;
-import de.tum.in.www1.artemis.domain.enumeration.AssessmentType;
 import de.tum.in.www1.artemis.repository.JsonAssessmentRepository;
 import de.tum.in.www1.artemis.repository.ParticipationRepository;
 import de.tum.in.www1.artemis.repository.ResultRepository;
-import de.tum.in.www1.artemis.service.AuthorizationCheckService;
-import de.tum.in.www1.artemis.service.CourseService;
-import de.tum.in.www1.artemis.service.ModelingExerciseService;
-import de.tum.in.www1.artemis.service.UserService;
+import de.tum.in.www1.artemis.service.*;
 import de.tum.in.www1.artemis.service.compass.CompassService;
 import de.tum.in.www1.artemis.web.rest.util.HeaderUtil;
 import org.slf4j.Logger;
@@ -22,8 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DecimalFormat;
-import java.time.ZonedDateTime;
 import java.util.Set;
 
 /**
@@ -44,8 +37,9 @@ public class ModelingAssessmentResource {
     private final UserService userService;
     private final AuthorizationCheckService authCheckService;
     private final CourseService courseService;
+    private final ModelingAssessmentService modelingAssessmentService;
 
-    public ModelingAssessmentResource(JsonAssessmentRepository jsonAssessmentRepository, ResultRepository resultRepository, ParticipationRepository participationRepository, CompassService compassService, ModelingExerciseService modelingExerciseService, UserService userService, AuthorizationCheckService authCheckService, CourseService courseService) {
+    public ModelingAssessmentResource(JsonAssessmentRepository jsonAssessmentRepository, ResultRepository resultRepository, ParticipationRepository participationRepository, CompassService compassService, ModelingExerciseService modelingExerciseService, UserService userService, AuthorizationCheckService authCheckService, CourseService courseService, ModelingAssessmentService modelingAssessmentService) {
         this.jsonAssessmentRepository = jsonAssessmentRepository;
         this.resultRepository = resultRepository;
         this.participationRepository = participationRepository;
@@ -54,6 +48,7 @@ public class ModelingAssessmentResource {
         this.userService = userService;
         this.authCheckService = authCheckService;
         this.courseService = courseService;
+        this.modelingAssessmentService = modelingAssessmentService;
     }
 
     @DeleteMapping("/modeling-assessments/exercise/{exerciseId}/optimal-models")
@@ -188,7 +183,7 @@ public class ModelingAssessmentResource {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
-        Result result = updateManualResult(resultId, exerciseId, modelingAssessment, false);
+        Result result = modelingAssessmentService.updateManualResult(resultId, exerciseId, modelingAssessment, false);
 
         return ResponseEntity.ok(result);
     }
@@ -218,71 +213,11 @@ public class ModelingAssessmentResource {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
-        Result result = updateManualResult(resultId, exerciseId, modelingAssessment, true);
+        Result result = modelingAssessmentService.updateManualResult(resultId, exerciseId, modelingAssessment, true);
         Long submissionId = result.getSubmission().getId();
         // add assessment to compass to include it in the automatic grading process
         compassService.addAssessment(exerciseId, submissionId, modelingAssessment);
 
         return ResponseEntity.ok(result);
-    }
-
-    //TODO: move more logic into a new ModelingAssessmentService
-
-    /**
-     * Updates result and set attributes according to rated state.
-     *
-     * @param resultId              the resultId the assessment belongs to
-     * @param exerciseId            the exerciseId the assessment belongs to
-     * @param modelingAssessment    the assessments as string
-     * @param rated                 if the result is rated or not
-     * @return the ResponseEntity with result as body
-     */
-    public Result updateManualResult(Long resultId, Long exerciseId, String modelingAssessment, Boolean rated) {
-        Result result = resultRepository.findOne(resultId);
-        result.setRated(rated);
-        result.setCompletionDate(ZonedDateTime.now());
-        result.setAssessmentType(AssessmentType.MANUAL);
-
-        User user = userService.getUser();
-        result.setAssessor(user);
-
-        Long studentId = result.getParticipation().getStudent().getId();
-        Long submissionId = result.getSubmission().getId();
-
-        ModelingExercise modelingExercise = modelingExerciseService.findOne(exerciseId);
-
-        // write assessment to file system
-        jsonAssessmentRepository.writeAssessment(exerciseId, studentId, submissionId, true, modelingAssessment);
-
-        if (rated) {
-            // set score, result string and successful if rated
-            JsonObject assessmentJson = jsonAssessmentRepository.readAssessment(exerciseId, studentId, submissionId, true);
-            Double maxScore = modelingExercise.getMaxScore();
-            Double totalScore = Math.min(Math.max(0, calculateTotalScore(assessmentJson)), maxScore);
-            Double percentageScore = totalScore/maxScore*100;
-            result.setScore(percentageScore.longValue());
-            DecimalFormat formatter = new DecimalFormat("#.##"); // limit decimal places to 2
-            result.setResultString(formatter.format(totalScore) + " of " + formatter.format(modelingExercise.getMaxScore()) + " points");
-            result.setSuccessful(percentageScore.longValue() == 100L);
-        }
-
-        resultRepository.save(result);
-
-        return result;
-    }
-
-    /**
-     * Helper function to calculate the total score of an assessment.
-     *
-     * @param assessmentJson    the assessments as JsonObject
-     * @return the total score
-     */
-    public Double calculateTotalScore(JsonObject assessmentJson) {
-        Double totalScore = 0.0;
-        JsonArray assessments = assessmentJson.get("assessments").getAsJsonArray();
-        for (JsonElement assessment : assessments) {
-            totalScore += assessment.getAsJsonObject().getAsJsonPrimitive("credits").getAsDouble();
-        }
-        return totalScore;
     }
 }
