a6556795dbb62d965d709bfa07a01ea3c70214a5
Johann Rottenfusser <johann.rottenfusser@googlemail.com>
2018-08-31 21:39:53 +0200

    fix typescript errors

--- src/main/webapp/app/components/pipes/format-date.pipe.ts
+++ src/main/webapp/app/components/pipes/format-date.pipe.ts
@@ -10,7 +10,7 @@ export class DatePipe implements PipeTransform {
        return 'YYYY-MM-dd,HH:mm:ss';
      }
      const d = new Date(date);
-     if (d && !isNaN(d)) {
+     if (d && !isNaN(d.getTime())) {
        return moment(d).seconds(0).format('YYYY-MM-DD,HH:mm:ss');
      } else {
         return 'YYYY-MM-dd,HH:mm:ss';
