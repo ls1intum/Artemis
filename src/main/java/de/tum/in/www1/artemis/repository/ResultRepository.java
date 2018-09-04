cee39f0e88eaee73ab65d474a8734f4dff4e44b3
Tom <tomwenzel@hotmail.de>
2018-09-04 19:07:49 +0200

    Added modified result call in native query
    Added SQL call in resultRepository to call less data from backend to reduce load on server and load on network. 
    //commented out to avoid conflicts with current testing.

--- src/main/java/de/tum/in/www1/artemis/repository/ResultRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ResultRepository.java
@@ -26,12 +26,16 @@ public interface ResultRepository extends JpaRepository<Result, Long> {
     @Query("select r from Result r where r.completionDate = (select max(rr.completionDate) from Result rr where rr.participation.exercise.id = :exerciseId and rr.participation.student.id = r.participation.student.id) and r.participation.exercise.id = :exerciseId order by r.completionDate asc")
     List<Result> findLatestResultsForExercise(@Param("exerciseId") Long exerciseId);
 
-//    @Query("select r from Result r where r.completionDate = (select min(rr.completionDate) from Result rr where rr.participation.exercise.id = :exerciseId and rr.participation.student.id = r.participation.student.id and rr.successful = true) and r.participation.exercise.id = :exerciseId and r.successful = true order by r.completionDate asc")
-//    List<Result> findEarliestSuccessfulResultsForExercise(@Param("exerciseId") Long exerciseId);
+    //native query to get data from backend. where only most recent result gets called to reduce load on server
+    //@Query q = em.createNativeQuery("select e.due_date, r.completion_date, e.discriminator, r.rated from Result r, Exercise e, Participation p where r.participation_id = p.id and p.exercise_id = e.id and e.course_id = 13 and e.due_date is not null  and (r.rated = 1 or (r.rated is null && r.completion_date <= e.due_date)) group by p.id order by r.completion_date desc");
+    //List<Object[]> authors = q.getResultList();
+
+//@Query("select r from Result r where r.completionDate = (select min(rr.completionDate) from Result rr where rr.participation.exercise.id = :exerciseId and rr.participation.student.id = r.participation.student.id and rr.successful = true) and r.participation.exercise.id = :exerciseId and r.successful = true order by r.completionDate asc")
+//List<Result> findEarliestSuccessfulResultsForExercise(@Param("exerciseId") Long exerciseId);
 
     @Query("select r from Result r where r.participation.exercise.course.id = :courseId order by r.completionDate asc")
     List<Result> findAllResultsForCourse(@Param("courseId") Long courseId);
-    
+
     /*
      * Custom query that counts the number of results for each participation of a particular exercise
      * @param exerciseId id of exercise for which the number of results in participations is aggregated
