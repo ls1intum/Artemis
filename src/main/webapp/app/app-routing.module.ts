42cd4ab9621dd154a79c32882a4257244d0382d8
Stephan Krusche <krusche@in.tum.de>
2018-09-05 13:11:48 +0200

    remove unnecessary code

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -1,51 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, UrlHandlingStrategy, UrlTree } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { UserRouteAccessService } from './shared';
 import { errorRoute, navbarRoute } from './layouts';
 import { EditorComponent } from './editor';
 
-const LAYOUT_ROUTES = [
+const LAYOUT_ROUTES: Routes = [
     navbarRoute,
-    ...errorRoute,
-    /**
-     * @description Routing entry for the upgraded editor component:
-     * Defines the path for the editor, the participation id is provided as only argument
-     */
-    {
-        path: 'editor/:participationId',
-        component: EditorComponent,
-        data: {
-            authorities: ['ROLE_USER'],
-            pageTitle: 'arTeMiSApp.editor.title'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export class CustomHandlingStrategy implements UrlHandlingStrategy {
-    /**
-     * @class CustomHandlingStrategy
-     * @implements UrlHandlingStrategy
-     * @desc Can be used to tell Angular whether to handle a given URL or not.
-     * Useful when in need to handle a dual-router setup. Currently, the Angular router handles
-     * every URL change, so this just return 'true'
-     */
-    shouldProcessUrl(url: UrlTree) {
-        return true;
-    }
-    extract(url: UrlTree) { return url; }
-    merge(url: UrlTree, whole: UrlTree) { return url; }
-}
+    ...errorRoute
+]; // TODO add future feature routes here, e.g. quiz, modeling, apollon, programming editor
 
 @NgModule({
     imports: [
         RouterModule.forRoot(LAYOUT_ROUTES, { useHash: true , enableTracing: false })
     ],
-    exports: [
-        RouterModule
-    ],
-    providers: [
-        { provide: UrlHandlingStrategy, useClass: CustomHandlingStrategy }
-    ],
+    exports: [RouterModule]
 })
 export class ArTEMiSAppRoutingModule {}
