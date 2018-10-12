06f74b9de5a3e4225cf017318f04790d9e56d720
kreuzersteve <31563850+kreuzersteve@users.noreply.github.com>
2018-10-12 20:29:14 +0200

    Enhance online editor (#108)
    * Avoid participation REST call in Online Editor when navigating form Courses Dashboard
    * Patch interact.js to support 'enabled'-option
    * Rearrange the whole structure for the FileBrowser-Component; 
    * Migrate file-browser resizable object to file-browser-component; 
    * Fix scrolling behavior for file-browser

--- src/main/webapp/app/courses/index.ts
+++ src/main/webapp/app/courses/index.ts
@@ -2,3 +2,4 @@ export * from './courses.component';
 export * from './courses.module';
 export * from './courses.route';
 export * from './exercises/exercise-list.component';
+export * from './exercises/participation-data-provider';
