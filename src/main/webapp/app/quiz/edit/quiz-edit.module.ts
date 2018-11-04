035a5238513d7a04bbe955ff39062aaae5037266
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2018-11-04 23:01:09 +0100

    added SA quiz type folder and empty files into edit folder and set references in index.ts and quiz-edit.module.ts

--- src/main/webapp/app/quiz/edit/quiz-edit.module.ts
+++ src/main/webapp/app/quiz/edit/quiz-edit.module.ts
@@ -9,6 +9,7 @@ import { QuizComponent } from '../participate/quiz.component';
 import { QuizExerciseComponent } from '../../entities/quiz-exercise';
 import { EditMultipleChoiceQuestionComponent } from './multiple-choice-question/edit-multiple-choice-question.component';
 import { EditDragAndDropQuestionComponent } from './drag-and-drop-question/edit-drag-and-drop-question.component';
+import { EditShortAnswerQuestionComponent } from './short-answer-question/edit-short-answer-question.component';
 import { AngularFittextModule } from 'angular-fittext';
 import { AceEditorModule } from 'ng2-ace-editor';
 import { DndModule } from 'ng2-dnd';
@@ -16,17 +17,18 @@ import { ArTEMiSQuizModule } from '../participate';
 
 @NgModule({
     imports: [ArTEMiSSharedModule, DndModule.forRoot(), AngularFittextModule, AceEditorModule, ArTEMiSQuizModule],
-    declarations: [EditMultipleChoiceQuestionComponent, EditDragAndDropQuestionComponent],
+    declarations: [EditMultipleChoiceQuestionComponent, EditDragAndDropQuestionComponent, EditShortAnswerQuestionComponent],
     entryComponents: [
         HomeComponent,
         QuizComponent,
         QuizExerciseComponent,
         JhiMainComponent,
         EditMultipleChoiceQuestionComponent,
-        EditDragAndDropQuestionComponent
+        EditDragAndDropQuestionComponent,
+        EditShortAnswerQuestionComponent
     ],
     providers: [RepositoryService, JhiAlertService],
-    exports: [EditMultipleChoiceQuestionComponent, EditDragAndDropQuestionComponent],
+    exports: [EditMultipleChoiceQuestionComponent, EditDragAndDropQuestionComponent, EditShortAnswerQuestionComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ArTEMiSQuizEditModule {}
