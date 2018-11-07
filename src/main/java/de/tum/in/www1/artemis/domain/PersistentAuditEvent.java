7c719446e76ccfd723acc396b7987b4f5523293e
Stephan Krusche <krusche@in.tum.de>
2018-11-07 09:19:57 +0100

    update to jhipster 5.6.1, update client dependencies
    Signed-off-by: Stephan Krusche <krusche@in.tum.de>

--- src/main/java/de/tum/in/www1/artemis/domain/PersistentAuditEvent.java
+++ src/main/java/de/tum/in/www1/artemis/domain/PersistentAuditEvent.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Persist AuditEvent managed by the Spring Boot actuator.
@@ -75,5 +76,32 @@ public class PersistentAuditEvent implements Serializable {
 
     public void setData(Map<String, String> data) {
         this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PersistentAuditEvent persistentAuditEvent = (PersistentAuditEvent) o;
+        return !(persistentAuditEvent.getId() == null || getId() == null) && Objects.equals(getId(), persistentAuditEvent.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "PersistentAuditEvent{" +
+            "principal='" + principal + '\'' +
+            ", auditEventDate=" + auditEventDate +
+            ", auditEventType='" + auditEventType + '\'' +
+            '}';
     }
 }
