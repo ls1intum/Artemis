15111b0afcaafe86f5c4050fe3f1e568016dd19d
Stephan Krusche <s.krusche@tum.de>
2018-12-13 00:19:30 +0100

    add documentation

--- src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
@@ -427,31 +427,35 @@ public class ParticipationService {
         //filter all irrelevant results, i.e. rated = false or before exercise due date
         for (Participation participation : participations) {
             List<Result> relevantResults = new ArrayList<Result>();
+
+            //search for the relevant result by filtering out irrelevant results using the continue keyword
+            //this for loop is optimized for performance and thus not very easy to understand ;)
             for (Result result : participation.getResults()) {
                 if (result.isRated() == Boolean.FALSE) {
+                    // we are only interested in results with rated == null (for compatibility) and rated == Boolean.TRUE
                     //TODO: for compatibility reasons, we include rated == null, in the future we can remove this
                     continue;
                 }
-                if (result.getCompletionDate() == null) {
-                    continue;
-                }
-                if (result.getScore() == null) {
+                if (result.getCompletionDate() == null || result.getScore() == null) {
+                    // we are only interested in results with completion date and with score
                     continue;
                 }
                 if (participation.getExercise() instanceof QuizExercise) {
-                    //in quizzes we take all rated results
+                    //in quizzes we take all rated results, because we only have one! (independent of later checks)
                 }
                 else if (participation.getExercise().getDueDate() != null) {
                     if (participation.getExercise() instanceof ModelingExercise || participation.getExercise() instanceof TextExercise) {
                         if (result.getSubmission() != null && result.getSubmission().getSubmissionDate() != null
                             && result.getSubmission().getSubmissionDate().isAfter(participation.getExercise().getDueDate())) {
-                            //Filter out late submissions
+                            //Filter out late results using the submission date, because in this exercise types, the
+                            //difference between submissionDate and result.completionDate can be significant due to manual assessment
                             continue;
                         }
                     }
                     else {
                         //For all other exercises the result completion date is the same as the submission date
                         if (result.getCompletionDate().isAfter(participation.getExercise().getDueDate())) {
+                            //and we continue (i.e. dismiss the result) if the result completion date is after the exercise due date
                             continue;
                         }
                     }
