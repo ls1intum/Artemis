c221db75bda5886a17ac88a9ecca02688ade16eb
Katie Sebina <katie.sebina@tum.de>
2018-12-20 13:12:33 +0100

    Checkout from develop

---
+++ src/test/javascript/spec/app/entities/lti-outcome-url/lti-outcome-url.service.spec.ts
@@ -0,0 +1,106 @@
+/* tslint:disable max-line-length */
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { LtiOutcomeUrlService } from 'app/entities/lti-outcome-url/lti-outcome-url.service';
+import { ILtiOutcomeUrl, LtiOutcomeUrl } from 'app/shared/model/lti-outcome-url.model';
+
+describe('Service Tests', () => {
+    describe('LtiOutcomeUrl Service', () => {
+        let injector: TestBed;
+        let service: LtiOutcomeUrlService;
+        let httpMock: HttpTestingController;
+        let elemDefault: ILtiOutcomeUrl;
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [HttpClientTestingModule]
+            });
+            injector = getTestBed();
+            service = injector.get(LtiOutcomeUrlService);
+            httpMock = injector.get(HttpTestingController);
+
+            elemDefault = new LtiOutcomeUrl(0, 'AAAAAAA', 'AAAAAAA');
+        });
+
+        describe('Service methods', async () => {
+            it('should find an element', async () => {
+                const returnedFromService = Object.assign({}, elemDefault);
+                service
+                    .find(123)
+                    .pipe(take(1))
+                    .subscribe(resp => expect(resp).toMatchObject({ body: elemDefault }));
+
+                const req = httpMock.expectOne({ method: 'GET' });
+                req.flush(JSON.stringify(returnedFromService));
+            });
+
+            it('should create a LtiOutcomeUrl', async () => {
+                const returnedFromService = Object.assign(
+                    {
+                        id: 0
+                    },
+                    elemDefault
+                );
+                const expected = Object.assign({}, returnedFromService);
+                service
+                    .create(new LtiOutcomeUrl(null))
+                    .pipe(take(1))
+                    .subscribe(resp => expect(resp).toMatchObject({ body: expected }));
+                const req = httpMock.expectOne({ method: 'POST' });
+                req.flush(JSON.stringify(returnedFromService));
+            });
+
+            it('should update a LtiOutcomeUrl', async () => {
+                const returnedFromService = Object.assign(
+                    {
+                        url: 'BBBBBB',
+                        sourcedId: 'BBBBBB'
+                    },
+                    elemDefault
+                );
+
+                const expected = Object.assign({}, returnedFromService);
+                service
+                    .update(expected)
+                    .pipe(take(1))
+                    .subscribe(resp => expect(resp).toMatchObject({ body: expected }));
+                const req = httpMock.expectOne({ method: 'PUT' });
+                req.flush(JSON.stringify(returnedFromService));
+            });
+
+            it('should return a list of LtiOutcomeUrl', async () => {
+                const returnedFromService = Object.assign(
+                    {
+                        url: 'BBBBBB',
+                        sourcedId: 'BBBBBB'
+                    },
+                    elemDefault
+                );
+                const expected = Object.assign({}, returnedFromService);
+                service
+                    .query(expected)
+                    .pipe(
+                        take(1),
+                        map(resp => resp.body)
+                    )
+                    .subscribe(body => expect(body).toContainEqual(expected));
+                const req = httpMock.expectOne({ method: 'GET' });
+                req.flush(JSON.stringify([returnedFromService]));
+                httpMock.verify();
+            });
+
+            it('should delete a LtiOutcomeUrl', async () => {
+                const rxPromise = service.delete(123).subscribe(resp => expect(resp.ok));
+
+                const req = httpMock.expectOne({ method: 'DELETE' });
+                req.flush({ status: 200 });
+            });
+        });
+
+        afterEach(() => {
+            httpMock.verify();
+        });
+    });
+});
