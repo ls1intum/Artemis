d308ee1863905fd30b238293276d7f81a460a948
Katie Sebina <katie.sebina@tum.de>
2018-12-28 09:44:48 +0100

    Frist edition of the pop-up for the exercises points

--- src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-infostudent-modal.component.ts
+++ src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-infostudent-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,6 +8,11 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class QuizScoringInfostudentModalComponent implements OnInit {
     constructor(private modalService: NgbModal) {}
+
+    @Input() score: number;
+    @Input() question: number;
+    @Input() scoringType: string;
+    @Input() selectedOption: boolean;
 
     ngOnInit() {}
 
