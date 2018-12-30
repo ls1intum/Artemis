e0280484f5b6c476304cc45670e4864c98a8f0dd
Katie Sebina <katie.sebina@tum.de>
2018-12-30 20:59:04 +0100

    Changed the Ace Editor style and added underline as command

--- src/main/webapp/app/markdown-editor/commands/underline.command.ts
+++ src/main/webapp/app/markdown-editor/commands/underline.command.ts
@@ -2,13 +2,13 @@ import { Command } from './command';
 
 export class UnderlineCommand extends Command {
     buttonTitle = 'Underline';
-    buttonIcon = 'fa fa-bold';
+    buttonIcon = 'fa fa-underline';
 
     execute(editor: any): void {
         let chosenText = editor.getSelectedText();
         editor.remove('left');
         editor.clearSelection();
-        const textToAdd = `**${chosenText}**`;
+        const textToAdd = `<ins>${chosenText}</ins>`;
         editor.moveCursorTo(editor.getCursorPosition().row, Number.POSITIVE_INFINITY);
         //const search = new Search();
         //search.set({needle: chosenText});
--- src/main/webapp/app/markdown-editor/markdown-editor.component.ts
+++ src/main/webapp/app/markdown-editor/markdown-editor.component.ts
@@ -5,7 +5,9 @@ import 'brace/mode/markdown';
 import { Command } from 'app/markdown-editor/commands/command';
 import { BoldCommand } from 'app/markdown-editor/commands/bold.command';
 import { ItalicCommand } from 'app/markdown-editor/commands/italic.command';
-import { UnderlineCommand } from 'app/markdown-editor/commands/underline';
+import { UnderlineCommand } from 'app/markdown-editor/commands/underline.command';
+import { AnswerOption } from 'app/entities/answer-option';
+import { ArtemisMarkdown } from 'app/components/util/markdown.service';
 
 @Component({
     selector: 'jhi-markdown-editor',
@@ -16,12 +18,70 @@ export class MarkdownEditorComponent implements AfterViewInit {
     @ViewChild('aceEditor')
     aceEditorContainer: AceEditorComponent;
 
-    height: string = '300px';
     text: string = 'hallo';
+
+    questionEditorText = '';
+    questionEditorMode = 'markdown';
+    questionEditorAutoUpdate = true;
+
+    constructor(private artemisMarkdown: ArtemisMarkdown) {}
+
+    showPreview: boolean;
 
     commands: Command[] = [new BoldCommand(), new ItalicCommand(), new UnderlineCommand()];
 
+    ngOnInit(): void {
+        this.showPreview = false;
+    }
+
     ngAfterViewInit(): void {
+        requestAnimationFrame(this.setupQuestionMarkdownEditor.bind(this));
+    }
+
+    /**
+     * @function setupQuestionEditor
+     * @desc Initializes the ace editor for the mc question
+     */
+
+    setupQuestionMarkdownEditor(): void {
         this.aceEditorContainer.setTheme('chrome');
+        this.aceEditorContainer.getEditor().renderer.setShowGutter(false);
+        this.aceEditorContainer.getEditor().renderer.setPadding(10);
+        this.aceEditorContainer.getEditor().renderer.setScrollMargin(8, 8);
+        this.aceEditorContainer.getEditor().setHighlightActiveLine(false);
+        this.aceEditorContainer.getEditor().setShowPrintMargin(false);
+        this.aceEditorContainer.getEditor().clearSelection();
+        this.aceEditorContainer.getEditor().on(
+            'blur',
+            () => {
+                this.parseMarkdown(this.questionEditorText);
+            },
+            this
+        );
+    }
+
+    parseMarkdown(text: string): void {
+        // First split by [], [ ], [x] and [X]
+        const questionParts = text.split(/\[\]|\[ \]|\[x\]|\[X\]/g);
+        const questionText = questionParts[0];
+
+        let endOfPreviousPart = text.indexOf(questionText) + questionText.length;
+        /**
+         * Work on answer options
+         * We slice the first questionPart since that's our question text and no real answer option
+         */
+        for (const answerOptionText of questionParts.slice(1)) {
+            // Find the box (text in-between the parts)
+            const answerOption = new AnswerOption();
+            const startOfThisPart = text.indexOf(answerOptionText, endOfPreviousPart);
+            const box = text.substring(endOfPreviousPart, startOfThisPart);
+            // Check if box says this answer option is correct or not
+            answerOption.isCorrect = box === '[x]' || box === '[X]';
+            // Update endOfPreviousPart for next loop
+            endOfPreviousPart = startOfThisPart + answerOptionText.length;
+
+            // Parse this answerOption
+            this.artemisMarkdown.parseTextHintExplanation(answerOptionText, answerOption);
+        }
     }
 }
