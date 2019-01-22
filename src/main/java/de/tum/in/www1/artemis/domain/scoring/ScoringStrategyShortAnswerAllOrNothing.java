b7ec9ec409e23ead965664a0ac153537b8ce7c06
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2019-01-22 05:11:15 +0100

    added functionality to check if duplicate submittedTexts are valid or not and how to handle the correction

--- src/main/java/de/tum/in/www1/artemis/domain/scoring/ScoringStrategyShortAnswerAllOrNothing.java
+++ src/main/java/de/tum/in/www1/artemis/domain/scoring/ScoringStrategyShortAnswerAllOrNothing.java
@@ -2,6 +2,7 @@ package de.tum.in.www1.artemis.domain.scoring;
 
 import de.tum.in.www1.artemis.domain.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -17,25 +18,46 @@ public class ScoringStrategyShortAnswerAllOrNothing implements ScoringStrategy {
         }
         if (submittedAnswer instanceof ShortAnswerSubmittedAnswer && question instanceof ShortAnswerQuestion) {
             ShortAnswerSubmittedAnswer saAnswer = (ShortAnswerSubmittedAnswer) submittedAnswer;
+            /*
+            for(ShortAnswerSubmittedText submittedText : saAnswer.getSubmittedTexts()){
+                submittedText.setSubmittedAnswer(saAnswer);
+            } */
+
             ShortAnswerQuestion saQuestion = (ShortAnswerQuestion) question;
             int correctSolutions = 0;
+            Set<ShortAnswerSolution> solutionsAlreadyUsed = new HashSet<>();
 
             // iterate through each spot and compare its correct solutions with the submitted texts
             for (ShortAnswerSpot spot : saQuestion.getSpots()) {
                 Set<ShortAnswerSolution> solutionsForSpot = saQuestion.getCorrectSolutionForSpot(spot);
-
                 for(ShortAnswerSubmittedText text : saAnswer.getSubmittedTexts()){
                     if(text.getSpot().equals(spot)){
                         for(ShortAnswerSolution solution : solutionsForSpot){
-                            //needs to be changed with better algorithm
-                            if(solution.getText().equalsIgnoreCase(text.getText())){
+                            /*submittedText answers can be the same (more than once)
+                            when spots do not exist more than once */
+                            if(text.isSubmittedTextCorrect(text.getText(), solution.getText())
+                                && (!saAnswer.submittedTextMoreThanOnceInSubmittedAnswer(text) || !saQuestion.spotMoreThanOnceInMapping(spot))
+                                ){
+                                text.setIsCorrect(true);
                                 correctSolutions++;
+                                break;
+                            }
+                            /*submittedText answers cannot be the same (more than once) */
+                            if(text.isSubmittedTextCorrect(text.getText(), solution.getText())
+                                && saAnswer.submittedTextMoreThanOnceInSubmittedAnswer(text)
+                                && saQuestion.spotMoreThanOnceInMapping(spot)
+                                && saQuestion.solutionMoreThanOnceInMapping(solution)
+                                && !solutionsAlreadyUsed.contains(solution)
+                                ){
+                                solutionsAlreadyUsed.add(solution);
+                                text.setIsCorrect(true);
+                                correctSolutions++;
+                                break;
                             }
                         }
                     }
                 }
             }
-
             if(correctSolutions == saQuestion.getSpots().size()){
                 return saQuestion.getScore();
             } else {
--- src/main/java/de/tum/in/www1/artemis/domain/scoring/ScoringStrategyShortAnswerProportionalWithPenalty.java
+++ src/main/java/de/tum/in/www1/artemis/domain/scoring/ScoringStrategyShortAnswerProportionalWithPenalty.java
@@ -2,6 +2,7 @@ package de.tum.in.www1.artemis.domain.scoring;
 
 import de.tum.in.www1.artemis.domain.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -27,18 +28,35 @@ public class ScoringStrategyShortAnswerProportionalWithPenalty implements Scorin
             double totalSolutions = saQuestion.getSpots().size();
             double correctSolutions = 0;
             double incorrectSolutions = 0;
+            Set<ShortAnswerSolution> solutionsAlreadyUsed = new HashSet<>();
 
 
             // iterate through each spot and compare its correct solutions with the submitted texts
             for (ShortAnswerSpot spot : saQuestion.getSpots()) {
                 Set<ShortAnswerSolution> solutionsForSpot = saQuestion.getCorrectSolutionForSpot(spot);
-
                 for(ShortAnswerSubmittedText text : saAnswer.getSubmittedTexts()){
+                    foundCorrectSolution = false;
                     if(text.getSpot().equals(spot)){
-
                         for(ShortAnswerSolution solution : solutionsForSpot){
-                            //needs to be changed with better algorithm
-                            if(solution.getText().equalsIgnoreCase(text.getText())){
+                            /*submittedText answers can be the same (more than once)
+                            when spots do not exist more than once*/
+                            if(text.isSubmittedTextCorrect(text.getText(), solution.getText())
+                                && (!saAnswer.submittedTextMoreThanOnceInSubmittedAnswer(text) || !saQuestion.spotMoreThanOnceInMapping(spot))
+                                ){
+                                text.setIsCorrect(true);
+                                correctSolutions++;
+                                foundCorrectSolution = true;
+                                break;
+                            }
+                            /*submittedText answers cannot be the same (more than once) */
+                            if(text.isSubmittedTextCorrect(text.getText(), solution.getText())
+                                && saAnswer.submittedTextMoreThanOnceInSubmittedAnswer(text)
+                                && saQuestion.spotMoreThanOnceInMapping(spot)
+                                && saQuestion.solutionMoreThanOnceInMapping(solution)
+                                && !solutionsAlreadyUsed.contains(solution)
+                                ){
+                                solutionsAlreadyUsed.add(solution);
+                                text.setIsCorrect(true);
                                 correctSolutions++;
                                 foundCorrectSolution = true;
                                 break;
@@ -46,6 +64,7 @@ public class ScoringStrategyShortAnswerProportionalWithPenalty implements Scorin
                         }
                         if(!foundCorrectSolution){
                             incorrectSolutions++;
+
                         }
                     }
                 }
