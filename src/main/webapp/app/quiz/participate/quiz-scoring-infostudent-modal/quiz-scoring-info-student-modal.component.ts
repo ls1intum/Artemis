84fe034b0cd26ed17f6ecfdf2dd831c52432e241
Katie Sebina <katie.sebina@tum.de>
2019-02-01 15:32:06 +0100

    Right counting only for the MCQ

--- src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-info-student-modal.component.ts
+++ src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-info-student-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Question, QuestionType, ScoringType } from 'app/entities/question';
+import { AnswerOption } from 'app/entities/answer-option';
 
 @Component({
     selector: 'jhi-quiz-scoring-infostudent-modal',
@@ -17,20 +18,30 @@ export class QuizScoringInfoStudentModalComponent implements OnInit {
     @Input() score: number;
     @Input() selectedOption: boolean;
 
-    @Input() correctAnswers: number;
-    @Input() wrongAnswers: number;
+    @Input() correctlyChosenAnswers: number;
+    @Input() correctAnswer: number;
+    @Input() wronglyChosenAnswers: number;
 
     @Input() amountOfAnswerOptions: number;
 
     @Input() question: Question;
 
+    forgottenRightAnswers: number;
+
     constructor(private modalService: NgbModal) {
     }
 
     ngOnInit() {
+        this.count()
     }
 
     open(content: any) {
         this.modalService.open(content, { size: 'lg' });
     }
+
+    count() {
+        this.forgottenRightAnswers = this.correctAnswer - this.correctlyChosenAnswers;
+    }
+
+
 }
