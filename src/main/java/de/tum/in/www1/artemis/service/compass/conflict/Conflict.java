d8a494fe1e8fff54c1f98ecbd74dbae7efa8753d
Maximilian Jelinek <jelinek@in.tum.de>
2019-02-04 15:58:32 +0100

    feat: added mapping from jsonElemenetID to lementId in Conflict; added Conflict in Client

--- src/main/java/de/tum/in/www1/artemis/service/compass/conflict/Conflict.java
+++ src/main/java/de/tum/in/www1/artemis/service/compass/conflict/Conflict.java
@@ -3,23 +3,24 @@ package de.tum.in.www1.artemis.service.compass.conflict;
 import de.tum.in.www1.artemis.domain.User;
 import de.tum.in.www1.artemis.service.compass.assessment.Assessment;
 
+import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 
 public class Conflict {
-    public Set<Integer> elementIdsInConflict;
+    public HashMap<String, Integer> elementIdsInConflict;
 
     private String id;
     private User initiator;
 
 
-    public Conflict(Set<Integer> elementIdsInConflict) {
+    public Conflict(HashMap<String, Integer> elementIdsInConflict) {
         this.elementIdsInConflict = elementIdsInConflict;
         id = UUID.randomUUID().toString();
     }
 
-    public void addConflictingElement(int elementID) {
-        elementIdsInConflict.add(elementID);
+    public void addConflictingElement(String jsonElementId, int elementID) {
+        elementIdsInConflict.put(jsonElementId, elementID);
     }
 
     public User getInitiator() {
---
+++ src/main/webapp/app/entities/modeling-assessment/Conflict.ts
@@ -0,0 +1,7 @@
+export class Conflict {
+    elementIdsInConflict: {[jsonElementId:string]:number};
+    initiator: any;//TODO set type of attributes
+    id: string;
+
+
+}
