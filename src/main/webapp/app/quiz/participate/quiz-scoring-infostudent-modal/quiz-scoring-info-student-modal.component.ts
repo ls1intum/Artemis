b5de54867bc817a56019644b91dab6be10d83313
Katie Sebina <katie.sebina@tum.de>
2019-02-05 17:06:23 +0100

    fixing mistakes of the text

--- src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-info-student-modal.component.ts
+++ src/main/webapp/app/quiz/participate/quiz-scoring-infostudent-modal/quiz-scoring-info-student-modal.component.ts
@@ -18,22 +18,22 @@ export class QuizScoringInfoStudentModalComponent implements OnInit {
     readonly ALL_OR_NOTHING = ScoringType.ALL_OR_NOTHING;
     readonly PROPORTIONAL_WITH_PENALTY = ScoringType.PROPORTIONAL_WITH_PENALTY;
 
-    @Input() score: number;
-
+    @Input() score: number; // Score of the student that has been achieved
     @Input() question: Question;
     @Input() DragAndDropMapping = new Array<DragAndDropMapping>();
     @Input() MultipleChoiceMapping = new Array<AnswerOption>();
 
-    @Input() correctAnswer: number;
-
     /* Multiple Choice Counting Variables*/
     multipleChoiceCorrectAnswerCorrectlyChosen: number;
     multipleChoiceWrongAnswerChosen: number;
+    amountofCorrectMultipleChoiceAnswers:number;
     forgottenRightAnswers: number;
     amountOfAnswerOptions: number;
 
     /* Drag and Drop Counting Variables*/
     dragAndDropElementsCount: number;
+    wrongMappedItems: number;
+    @Input() correctAnswer: number; //Amount of correctly mapped drag and drop items
 
     constructor(private modalService: NgbModal) {
     }
@@ -57,15 +57,17 @@ export class QuizScoringInfoStudentModalComponent implements OnInit {
 
     private countMultipleChoice() {
         const mcmQuestion = this.question as MultipleChoiceQuestion;
-            this.amountOfAnswerOptions = mcmQuestion.answerOptions.length;
+            this.amountOfAnswerOptions = mcmQuestion.answerOptions.length; // Amount of answer options
+            this.amountofCorrectMultipleChoiceAnswers = mcmQuestion.answerOptions.filter(option => option.isCorrect).length;
             this.multipleChoiceCorrectAnswerCorrectlyChosen = this.MultipleChoiceMapping.filter(option => option.isCorrect).length; // how many right answers chosen correctly
             this.multipleChoiceWrongAnswerChosen = this.MultipleChoiceMapping.filter(option => !option.isCorrect).length; // how many wrong answers have been selected
-            this.forgottenRightAnswers = this.MultipleChoiceMapping.filter(option => option.isCorrect).length - this.multipleChoiceCorrectAnswerCorrectlyChosen; // how many right options have been forgotten to be chosen
+            this.forgottenRightAnswers = this.amountofCorrectMultipleChoiceAnswers - this.multipleChoiceCorrectAnswerCorrectlyChosen; // how many right options have been forgotten to be chosen
     }
 
     private countDragandDrop() {
         const dndQuestion = this.question as DragAndDropQuestion;
-        this.dragAndDropElementsCount = dndQuestion.dropLocations.length;
+        this.dragAndDropElementsCount = dndQuestion.dropLocations.length; //Amount of drag and drop zones that should be matched
+        this.wrongMappedItems = this.dragAndDropElementsCount - this.correctAnswer; //Amount of wrong matched items or not matched at all
 
     }
 
