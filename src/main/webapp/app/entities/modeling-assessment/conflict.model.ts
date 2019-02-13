c9915fea2f1cc0af7ccc3a3925234b17fa0ef281
Maximilian Jelinek <jelinek@in.tum.de>
2019-02-13 21:03:35 +0100

    ref: did TODO

--- src/main/webapp/app/entities/modeling-assessment/conflict.model.ts
+++ src/main/webapp/app/entities/modeling-assessment/conflict.model.ts
@@ -1,11 +1,12 @@
 import {Score} from "app/entities/modeling-assessment/score.model";
 import {UMLElement} from "app/entities/modeling-assessment/uml-element.model";
 import {ModelingAssessment} from "app/entities/modeling-assessment/modeling-assessment.model";
+import {User} from "app/core";
 
 export class Conflict {
     elementInConflict: UMLElement;
     conflictingAssessment: ModelingAssessment;
     scoresInConflict: Score[];
-    initiator: any;//TODO set type of attributes
+    initiator:User;
     id: string;
 }
