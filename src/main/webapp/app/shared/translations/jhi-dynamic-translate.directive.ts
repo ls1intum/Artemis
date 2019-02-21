6fc5b5baac797e57592b9dec417173aa966c3898
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-02-21 20:51:06 +0100

    Update Course: Form refactoring, including datetime-picker (#168)
    * refactored form into reactive version, including simple validation
    * added more form validation to shortname field
    * refactored date-picker component to inline
    * refactored usages of date-picker in other areas of the application
    * moved validation texts form course update comp to i18n file

---
+++ src/main/webapp/app/shared/translations/jhi-dynamic-translate.directive.ts
@@ -0,0 +1,18 @@
+import { Input, Directive, OnChanges } from '@angular/core';
+import { JhiTranslateDirective } from 'ng-jhipster/';
+
+/**
+ * A wrapper directive on top of the translate pipe as the inbuilt translate directive from ngx-translate is too verbose and buggy
+ */
+@Directive({
+    selector: '[jhiDynamicTranslate]'
+})
+export class JhiDynamicTranslateDirective extends JhiTranslateDirective implements OnChanges {
+    @Input() jhiDynamicTranslate: string;
+    @Input() dynamicLookup: string;
+
+    ngOnChanges() {
+        this.jhiTranslate = [this.jhiDynamicTranslate, this.dynamicLookup || ''].join('.');
+        super.ngOnChanges();
+    }
+}
