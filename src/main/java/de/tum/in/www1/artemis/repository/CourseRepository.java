cad54bd65416b9dc13e04c8bd56d50aaa0fe46e9
Maximilian Meier <maxi.meier92@gmail.com>
2019-03-08 10:10:21 +0100

    Feature/student can now register for a new course (#190)

--- src/main/java/de/tum/in/www1/artemis/repository/CourseRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/CourseRepository.java
@@ -21,4 +21,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     @Query("select distinct course from Course course left join fetch course.exercises where course.id = :#{#courseId}")
     Course findOneWithEagerExercises(@Param("courseId") Long courseId);
+
+    @Query("select distinct course from Course course where course.startDate <= current_timestamp and course.endDate >= current_timestamp and course.onlineCourse = 0")
+    List<Course> findAllCurrentlyActiveAndNotOnline();
 }
--- src/main/webapp/app/overview/overview.component.ts
+++ src/main/webapp/app/overview/overview.component.ts
@@ -12,12 +12,30 @@ import { Exercise, ExerciseService } from 'app/entities/exercise';
 export class OverviewComponent {
     public courses: Course[];
     public nextRelevantCourse: Course;
+    public coursesToSelect: Course[];
+    public courseToRegister: Course;
+    showCourseSelection = false;
+    addedSuccessful = false;
+    loading = false;
 
-    constructor(private courseService: CourseService, private exerciseService: ExerciseService, private jhiAlertService: JhiAlertService, private  courseScoreCalculationService: CourseScoreCalculationService) {
+    constructor(private courseService: CourseService, private exerciseService: ExerciseService, private jhiAlertService: JhiAlertService, private courseScoreCalculationService: CourseScoreCalculationService) {
+        this.loadAndFilterCourses();
+
+    }
+
+    loadAndFilterCourses() {
         this.courseService.findAll().subscribe(
             (res: HttpResponse<Course[]>) => {
                 this.courses = res.body;
                 this.courseScoreCalculationService.setCourses(this.courses);
+                this.courseService.findAllToRegister().subscribe(
+                    (registerRes: HttpResponse<Course[]>) => {
+                        this.coursesToSelect = registerRes.body.filter(course => {
+                            return !this.courses.find(el => el.id === course.id);
+                        });
+                    },
+                    (response: string) => this.onError(response)
+                );
             },
             (response: string) => this.onError(response)
         );
@@ -25,6 +43,51 @@ export class OverviewComponent {
 
     private onError(error: string) {
         this.jhiAlertService.error(error, null, null);
+    }
+
+    startRegistration() {
+        this.showCourseSelection = true;
+        if (this.coursesToSelect.length === 0) {
+            setTimeout(() => {
+                this.courseToRegister = null;
+                this.showCourseSelection = false;
+
+            }, 3000);
+        }
+    }
+
+    cancelRegistration() {
+        this.courseToRegister = null;
+        this.showCourseSelection = false;
+    }
+
+    registerForCourse() {
+        if (this.courseToRegister) {
+            this.showCourseSelection = false;
+            this.loading = true;
+            this.courseService.registerForCourse(this.courseToRegister.id).subscribe(
+                () => {
+                    this.addedSuccessful = true;
+                    this.loading = false;
+                    setTimeout(() => {
+                        this.courseToRegister = null;
+                        this.addedSuccessful = false;
+
+                    }, 3000);
+                    this.loadAndFilterCourses();
+
+                },
+                error => {
+                    console.log(error);
+                    this.loading = false;
+                    this.courseToRegister = null;
+                }
+            );
+        }
+    }
+
+    trackCourseById(index: number, item: Course) {
+        return item.id;
     }
 
     get nextRelevantExercise(): Exercise {
