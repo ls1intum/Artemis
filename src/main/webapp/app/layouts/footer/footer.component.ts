f922987b53517ecc77ff8c6f70553ab467ad12a9
Jan Philip Bernius, M.Sc <janphilip@bernius.net>
2019-03-20 14:37:22 +0100

    Add Privacy Statement and Imprint Link (#203)
    Fixes ARTEMIS-213

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { VERSION } from 'app/app.constants';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+    readonly email = 'artemis.ase@in.tum.de';
+    readonly imprintUrl = 'https://ase.in.tum.de/lehrstuhl_1/component/content/article/179-imprint';
+    get releaseNotesUrl(): string {
+        return `https://github.com/ls1intum/ArTEMiS/releases/tag/${VERSION}`;
+    }
+}
