10fcdfff1dc5f3de606694bf78405abc975ea1c8
Stephan Krusche <s.krusche@tum.de>
2019-03-20 13:27:32 +0100

    update client dependencies
    Signed-off-by: Stephan Krusche <s.krusche@tum.de>

--- src/main/webapp/app/polyfills.ts
+++ src/main/webapp/app/polyfills.ts
@@ -19,24 +19,22 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es7/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
+import 'core-js/features/symbol';
+import 'core-js/features/object';
+import 'core-js/features/function';
+import 'core-js/features/parse-int';
+import 'core-js/features/parse-float';
+import 'core-js/features/number';
+import 'core-js/features/math';
+import 'core-js/features/string';
+import 'core-js/features/date';
+import 'core-js/features/array';
+import 'core-js/features/regexp';
+import 'core-js/features/map';
+import 'core-js/features/weak-map';
+import 'core-js/features/set';
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/features/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
