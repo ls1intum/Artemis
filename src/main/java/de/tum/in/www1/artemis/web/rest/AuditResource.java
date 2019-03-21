28b79578d0bf5bbf50416787fea10274cd73a303
Stephan Krusche <s.krusche@tum.de>
2019-03-21 17:37:35 +0100

    use preauthorize in all new resource REST calls
    Signed-off-by: Stephan Krusche <s.krusche@tum.de>

--- src/main/java/de/tum/in/www1/artemis/web/rest/AuditResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/AuditResource.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -21,6 +22,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/management/audits")
+@PreAuthorize("hasAnyRole('ADMIN')")
 public class AuditResource {
 
     private final AuditEventService auditEventService;
