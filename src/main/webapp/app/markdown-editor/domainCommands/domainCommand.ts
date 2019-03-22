6ed9ac96823a0a894b20396c3b0254575197fcfe
Katie Sebina <katie.sebina@tum.de>
2019-03-22 17:32:49 +0100

    add on

--- src/main/webapp/app/markdown-editor/domainCommands/domainCommand.ts
+++ src/main/webapp/app/markdown-editor/domainCommands/domainCommand.ts
@@ -1,7 +1,8 @@
 import { Command } from 'app/markdown-editor/commands/command';
 
 /** abstract class for all domainCommands - customized commands for ArTEMiS specific use cases
- * e.g multiple choice questons, drag an drop questions **/
+ * e.g multiple choice questons, drag an drop questions
+ * Each domain command has its own logic and an unique identifier**/
 export abstract class DomainCommand extends Command {
 
     abstract getIdentifier(): string; // e.g. [-e]
