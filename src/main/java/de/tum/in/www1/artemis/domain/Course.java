864a669da823b1fea854642279d2a71b12ab0531
Ceany <maxi-meier@web.de>
2019-03-22 16:16:57 +0100

    fixed wordings

--- src/main/java/de/tum/in/www1/artemis/domain/Course.java
+++ src/main/java/de/tum/in/www1/artemis/domain/Course.java
@@ -362,26 +362,26 @@ public class Course implements Serializable {
      * NOTE:
      *
      * The file management is necessary to differentiate between temporary and used files
-     * and to delete used files when the corresponding question is deleted or it is replaced by
+     * and to delete used files when the corresponding course is deleted or it is replaced by
      * another file.
      *
      * The workflow is as follows
      *
      * 1. user uploads a file -> this is a temporary file,
-     *           because at this point the corresponding question
+     *           because at this point the corresponding course
      *           might not exist yet.
-     * 2. user saves the question -> now we move the temporary file
-     *           which is addressed in backgroundFilePath to a permanent
-     *           location and update the value in backgroundFilePath accordingly.
+     * 2. user saves the course -> now we move the temporary file
+     *           which is addressed in courseIcon to a permanent
+     *           location and update the value in courseIcon accordingly.
      *           => This happens in @PrePersist and @PostPersist
      * 3. user might upload another file to replace the existing file
      *           -> this new file is a temporary file at first
-     * 4. user saves changes (with the new backgroundFilePath pointing to the new temporary file)
+     * 4. user saves changes (with the new courseIcon pointing to the new temporary file)
      *           -> now we delete the old file in the permanent location
      *              and move the new file to a permanent location and update
-     *              the value in backgroundFilePath accordingly.
+     *              the value in courseIcon accordingly.
      *           => This happens in @PreUpdate and uses @PostLoad to know the old path
-     * 5. When question is deleted, the file in the permanent location is deleted
+     * 5. When course is deleted, the file in the permanent location is deleted
      *           => This happens in @PostRemove
      */
     @PostLoad
