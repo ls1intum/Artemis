b225863071fbe3d02bc351ca18bb08392c8289a7
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2019-03-29 13:54:35 +0100

    feat: added retry strategy

--- src/main/webapp/app/modeling-assessment/modeling-assessment.module.ts
+++ src/main/webapp/app/modeling-assessment/modeling-assessment.module.ts
@@ -6,30 +6,23 @@ import { ArTEMiSSharedModule } from 'app/shared';
 import { ModelingAssessmentService } from './modeling-assessment.service';
 import { RouterModule } from '@angular/router';
 import { modelingAssessmentRoutes } from 'app/modeling-assessment/modeling-assessment.route';
-import { ModelingAssessmentComponent } from 'app/modeling-assessment/modeling-assessment.component';
+import { ModelingAssessmentEditorComponent } from 'app/modeling-assessment/modeling-assessment-editor.component';
 import { HomeComponent } from 'app/home';
 import { ArTEMiSResultModule, ResultComponent, ResultDetailComponent } from 'app/entities/result';
 import { ModelingAssessmentDashboardComponent } from 'app/modeling-assessment/modeling-assessment-dashboard.component';
 import { JhiMainComponent } from 'app/layouts';
 import { SortByModule } from 'app/components/pipes';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModelingAssessmentComponent } from './modeling-assessment/modeling-assessment.component';
 
 const ENTITY_STATES = [...modelingAssessmentRoutes];
 
 @NgModule({
-    imports: [
-        ArTEMiSSharedModule,
-        RouterModule.forChild(ENTITY_STATES),
-        NgbModule, SortByModule, ArTEMiSResultModule,
-    ],
-    declarations: [
-        ModelingAssessmentDashboardComponent, ModelingAssessmentComponent
-    ],
-    entryComponents: [
-        HomeComponent, ResultComponent, ResultDetailComponent, ModelingAssessmentDashboardComponent, JhiMainComponent, ModelingAssessmentComponent
-    ],
+    imports: [ArTEMiSSharedModule, RouterModule.forChild(ENTITY_STATES), NgbModule, SortByModule, ArTEMiSResultModule],
+    declarations: [ModelingAssessmentDashboardComponent, ModelingAssessmentEditorComponent, ModelingAssessmentComponent],
+    entryComponents: [HomeComponent, ResultComponent, ResultDetailComponent, ModelingAssessmentDashboardComponent, JhiMainComponent, ModelingAssessmentEditorComponent],
     providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ArTEMiSModelingAssessmentModule {
     constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
