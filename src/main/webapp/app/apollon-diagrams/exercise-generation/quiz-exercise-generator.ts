95163678608438d4e05d03cf829034a2a29fa6af
Julian Willand <willand@in.tum.de>
2019-03-31 21:16:39 +0200

    Optimised creation of drag and drop quiz exercises. Margins and sizing of relationships is now more accurate. Drop locations are now smaller to prevent overlaps.

--- src/main/webapp/app/apollon-diagrams/exercise-generation/quiz-exercise-generator.ts
+++ src/main/webapp/app/apollon-diagrams/exercise-generation/quiz-exercise-generator.ts
@@ -1,15 +1,18 @@
-import { convertRenderedSVGToPNG } from './svg-renderer';
-import * as TempID from '../../quiz/edit/temp-id';
-import { Course } from '../../entities/course';
-import { QuizExercise, QuizExerciseService } from '../../entities/quiz-exercise';
-import { FileUploaderService } from '../../shared/http/file-uploader.service';
-import { DragAndDropQuestion } from '../../entities/drag-and-drop-question';
-import { DropLocation } from '../../entities/drop-location';
-import { DragAndDropMapping } from '../../entities/drag-and-drop-mapping';
-import { DragItem } from '../../entities/drag-item';
-import { ScoringType } from '../../entities/quiz-question';
+import { ApollonEditor, Element, SVG, UMLElementType, UMLModel, UMLRelationshipType } from '@ls1intum/apollon';
 import * as moment from 'moment';
-import { ApollonEditor, Element, UMLModel, SVG, ElementType, UMLRelationshipType, UMLElementType } from '@ls1intum/apollon';
+import { Course } from '../../entities/course';
+import { DragAndDropMapping } from '../../entities/drag-and-drop-mapping';
+import { DragAndDropQuestion } from '../../entities/drag-and-drop-question';
+import { DragItem } from '../../entities/drag-item';
+import { DropLocation } from '../../entities/drop-location';
+import { QuizExercise, QuizExerciseService } from '../../entities/quiz-exercise';
+import { ScoringType } from '../../entities/quiz-question';
+import { FileUploaderService } from '../../shared/http/file-uploader.service';
+import { convertRenderedSVGToPNG } from './svg-renderer';
+
+type Position = { x: number; y: number };
+type Size = { width: number; height: number };
+type Boundary = Position & Size;
 
 // Drop locations in quiz exercises are relatively positioned and sized
 // using integers in the interval [0,200]
@@ -20,13 +23,12 @@ export async function generateDragAndDropQuizExercise(
     model: UMLModel,
     course: Course,
     fileUploaderService: FileUploaderService,
-    quizExerciseService: QuizExerciseService
+    quizExerciseService: QuizExerciseService,
 ) {
     // Render the layouted diagram as SVG
     const renderedDiagram = ApollonEditor.exportModelAsSvg(model, {
-        margin: 20,
         keepOriginalSize: true,
-        exclude: [...model.interactive.elements, ...model.interactive.relationships]
+        exclude: [...model.interactive.elements, ...model.interactive.relationships],
     });
 
     // Create a PNG diagram background image from the given diagram SVG
@@ -40,29 +42,19 @@ export async function generateDragAndDropQuizExercise(
     const dropLocations: DropLocation[] = [];
     const correctMappings: DragAndDropMapping[] = [];
 
-    const elements = [ ...model.elements, ...model.relationships ];
+    const elements = [...model.elements, ...model.relationships];
     // Create Drag Items, Drop Locations and their mappings for each interactive element
     for (const elementId of [...model.interactive.elements, ...model.interactive.relationships]) {
         const element = elements.find(elem => elem.id === elementId);
-        const { dragItem, dropLocation, correctMapping } = await generateDragAndDropItem(
-            element,
-            model,
-            renderedDiagram.clip,
-            fileUploaderService
-        );
+        const { dragItem, dropLocation } = await generateDragAndDropItem(element, model, fileUploaderService);
+        const correctMapping = new DragAndDropMapping(dragItem, dropLocation);
         dragItems.push(dragItem);
         dropLocations.push(dropLocation);
         correctMappings.push(correctMapping);
     }
 
     // Generate a drag-and-drop question object
-    const dragAndDropQuestion: DragAndDropQuestion = generateDragAndDropQuestion(
-        diagramTitle,
-        dragItems,
-        dropLocations,
-        correctMappings,
-        backgroundFilePath
-    );
+    const dragAndDropQuestion: DragAndDropQuestion = generateDragAndDropQuestion(diagramTitle, dragItems, dropLocations, correctMappings, backgroundFilePath);
 
     // Generate a quiz exercise object
     const quizExercise: QuizExercise = generateQuizExercise(course, diagramTitle, dragAndDropQuestion);
@@ -89,13 +81,12 @@ function generateDragAndDropQuestion(
     dragItems: DragItem[],
     dropLocations: DropLocation[],
     correctMappings: DragAndDropMapping[],
-    backgroundFilePath: string
+    backgroundFilePath: string,
 ): DragAndDropQuestion {
     const dragAndDropQuestion = new DragAndDropQuestion();
     dragAndDropQuestion.title = diagramTitle;
-    dragAndDropQuestion.text =
-        'Fill the empty spaces in the UML diagram by dragging and dropping the elements below the diagram into the correct places.';
-    dragAndDropQuestion.scoringType = ScoringType.PROPORTIONAL_WITH_PENALTY; // default value
+    dragAndDropQuestion.text = 'Fill the empty spaces in the UML diagram by dragging and dropping the elements below the diagram into the correct places.';
+    dragAndDropQuestion.scoringType = ScoringType.PROPORTIONAL_WITH_PENALTY;
     dragAndDropQuestion.randomizeOrder = true;
     dragAndDropQuestion.score = 1;
     dragAndDropQuestion.dropLocations = dropLocations;
@@ -105,57 +96,68 @@ function generateDragAndDropQuestion(
     return dragAndDropQuestion;
 }
 
-async function generateDragAndDropItem(
+async function generateDragAndDropItem(element: Element, model: UMLModel, fileUploaderService: FileUploaderService): Promise<{ dragItem: DragItem; dropLocation: DropLocation }> {
+    if (element.type in UMLRelationshipType) {
+        return generateDragAndDropItemForRelationship(element, model, fileUploaderService);
+    } else {
+        return generateDragAndDropItemForElement(element, model, fileUploaderService);
+    }
+}
+
+async function generateDragAndDropItemForElement(
     element: Element,
     model: UMLModel,
-    clip: { x: number; y: number; width: number; height: number },
-    fileUploaderService: FileUploaderService
-): Promise<{ dragItem: DragItem; dropLocation: DropLocation; correctMapping: DragAndDropMapping }> {
-    const isRelationship = element.type in UMLRelationshipType;
-
-    const margin = isRelationship ? 30 : 0;
-
-    const renderedEntity: SVG = ApollonEditor.exportModelAsSvg(model, {
-        margin,
-        include: [element.id]
-    });
+    fileUploaderService: FileUploaderService,
+): Promise<{ dragItem: DragItem; dropLocation: DropLocation }> {
+    const renderedEntity: SVG = ApollonEditor.exportModelAsSvg(model, { include: [element.id] });
     const image = await convertRenderedSVGToPNG(renderedEntity);
+    const imageUploadResponse = await fileUploaderService.uploadFile(image, `element-${element.id}.png`);
 
-    const imageUploadResponse = await fileUploaderService.uploadFile(image, `entity-${element.id}.png`);
     const dragItem = new DragItem();
-    dragItem.tempID = TempID.generate();
     dragItem.pictureFilePath = imageUploadResponse.path;
+    const dropLocation = computeDropLocation(element.bounds, model.size);
 
-    renderedEntity.clip.x += margin;
-    renderedEntity.clip.y += margin;
-    renderedEntity.clip.width -= margin * 2;
-    renderedEntity.clip.height -= margin * 2;
+    return { dragItem, dropLocation };
+}
 
-    if (isRelationship) {
-        const MIN_SIDE_LENGTH = 30;
+async function generateDragAndDropItemForRelationship(
+    element: Element,
+    model: UMLModel,
+    fileUploaderService: FileUploaderService,
+): Promise<{ dragItem: DragItem; dropLocation: DropLocation }> {
+    const MIN_SIZE = 30;
 
-        if (renderedEntity.clip.width < MIN_SIDE_LENGTH) {
-            const delta = MIN_SIDE_LENGTH - renderedEntity.clip.width;
-            renderedEntity.clip.width = MIN_SIDE_LENGTH;
-            renderedEntity.clip.x -= delta / 2;
-        }
-
-        if (renderedEntity.clip.height < MIN_SIDE_LENGTH) {
-            const delta = MIN_SIDE_LENGTH - renderedEntity.clip.height;
-            renderedEntity.clip.height = MIN_SIDE_LENGTH;
-            renderedEntity.clip.y -= delta / 2;
-        }
+    let margin = {};
+    const bounds = { ...element.bounds };
+    if (bounds.width < MIN_SIZE) {
+        const delta = MIN_SIZE - element.bounds.width;
+        margin = { ...margin, right: delta / 2, left: delta / 2 };
+        bounds.x -= delta / 2;
+        bounds.width = MIN_SIZE;
+    }
+    if (bounds.height < MIN_SIZE) {
+        const delta = MIN_SIZE - element.bounds.height;
+        margin = { ...margin, top: delta / 2, bottom: delta / 2 };
+        bounds.y -= delta / 2;
+        bounds.height = MIN_SIZE;
     }
 
-    const dropLocation = new DropLocation();
-    dropLocation.tempID = TempID.generate();
-    dropLocation.posX = Math.round((MAX_SIZE_UNIT * (renderedEntity.clip.x - clip.x)) / clip.width);
-    dropLocation.posY = Math.round((MAX_SIZE_UNIT * (renderedEntity.clip.y - clip.y)) / clip.height);
-    dropLocation.width = Math.round((MAX_SIZE_UNIT * renderedEntity.clip.width) / clip.width);
-    dropLocation.height = Math.round((MAX_SIZE_UNIT * renderedEntity.clip.height) / clip.height);
+    const renderedEntity: SVG = ApollonEditor.exportModelAsSvg(model, { margin, include: [element.id] });
+    const image = await convertRenderedSVGToPNG(renderedEntity);
+    const imageUploadResponse = await fileUploaderService.uploadFile(image, `relationship-${element.id}.png`);
 
-    const correctMapping = new DragAndDropMapping();
-    correctMapping.dragItem = dragItem;
-    correctMapping.dropLocation = dropLocation;
-    return { dragItem, dropLocation, correctMapping };
+    const dragItem = new DragItem();
+    dragItem.pictureFilePath = imageUploadResponse.path;
+    const dropLocation = computeDropLocation(bounds, model.size);
+
+    return { dragItem, dropLocation };
+}
+
+function computeDropLocation(elementLocation: Boundary, totalSize: Size): DropLocation {
+    const dropLocation = new DropLocation();
+    dropLocation.posX = Math.round((MAX_SIZE_UNIT * elementLocation.x) / totalSize.width);
+    dropLocation.posY = Math.round((MAX_SIZE_UNIT * elementLocation.y) / totalSize.height);
+    dropLocation.width = Math.round((MAX_SIZE_UNIT * elementLocation.width) / totalSize.width) - 1;
+    dropLocation.height = Math.round((MAX_SIZE_UNIT * elementLocation.height) / totalSize.height) - 1;
+    return dropLocation;
 }
