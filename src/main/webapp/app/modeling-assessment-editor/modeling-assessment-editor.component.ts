1bd9166699efaaeb3b140640b97a7a4dac09426f
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2019-03-31 12:42:00 +0200

    fix: removed moved method

--- src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
+++ src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
@@ -143,7 +143,6 @@ export class ModelingAssessmentEditorComponent implements OnInit, OnDestroy {
 
     onFeedbackChanged(feedback: Feedback[]) {
         this.result.feedbacks = feedback;
-        this.calculateTotalScore();
     }
 
     /**
