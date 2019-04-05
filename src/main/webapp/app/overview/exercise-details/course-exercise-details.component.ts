499f16445aed865578e0b76f53a5db5a80ab0bdb
Ceany <maxi-meier@web.de>
2019-04-05 17:20:12 +0200

    updated comment

--- src/main/webapp/app/overview/exercise-details/course-exercise-details.component.ts
+++ src/main/webapp/app/overview/exercise-details/course-exercise-details.component.ts
@@ -85,8 +85,8 @@ export class CourseExerciseDetailsComponent implements OnInit, OnDestroy {
                 this.websocketChannelResults = `/topic/participation/${participation.id}/newResults`;
                 this.jhiWebsocketService.subscribe(this.websocketChannelResults);
                 this.jhiWebsocketService.receive(this.websocketChannelResults).subscribe((newResult: Result) => {
-                    //     // convert json string to moment
                     console.log('Received new result ' + newResult.id + ': ' + newResult.resultString);
+                    // convert json string to moment
                     newResult.completionDate = newResult.completionDate != null ? moment(newResult.completionDate) : null;
                     this.handleNewResult(newResult);
                 });
