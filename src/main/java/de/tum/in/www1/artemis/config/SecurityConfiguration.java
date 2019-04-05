aa9c1b55ecdf8d96ef906140be806e723ae8c5fd
Stephan Krusche <s.krusche@tum.de>
2019-04-05 14:02:46 +0200

    revert changes in SecurityConfig
    Signed-off-by: Stephan Krusche <s.krusche@tum.de>

--- src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
@@ -41,34 +41,33 @@ import de.tum.in.www1.artemis.security.jwt.TokenProvider;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
     private final UserDetailsService userDetailsService;
-
     private final TokenProvider tokenProvider;
-
     private final CorsFilter corsFilter;
-
     private final SecurityProblemSupport problemSupport;
-
     private final Optional<AuthenticationProvider> remoteUserAuthenticationProvider;
+    private final JHipsterProperties jHipsterProperties;
 
-    public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService, TokenProvider tokenProvider,
-            CorsFilter corsFilter, SecurityProblemSupport problemSupport, Optional<AuthenticationProvider> remoteUserAuthenticationProvider) {
+    public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService, TokenProvider tokenProvider, CorsFilter corsFilter, SecurityProblemSupport problemSupport, Optional<AuthenticationProvider> remoteUserAuthenticationProvider, JHipsterProperties jHipsterProperties) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.userDetailsService = userDetailsService;
         this.tokenProvider = tokenProvider;
         this.corsFilter = corsFilter;
         this.problemSupport = problemSupport;
         this.remoteUserAuthenticationProvider = remoteUserAuthenticationProvider;
+        this.jHipsterProperties = jHipsterProperties;
     }
 
     @PostConstruct
     public void init() {
         try {
-            authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-            remoteUserAuthenticationProvider.ifPresent(authenticationManagerBuilder::authenticationProvider);
-        }
-        catch (Exception e) {
+            authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+            if(remoteUserAuthenticationProvider.isPresent()) {
+                authenticationManagerBuilder.authenticationProvider(remoteUserAuthenticationProvider.get());
+            }
+        } catch (Exception e) {
             throw new BeanInitializationException("Security configuration failed", e);
         }
     }
@@ -99,24 +98,53 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**").antMatchers("/app/**/*.{js,html}").antMatchers("/i18n/**").antMatchers("/content/**").antMatchers("/test/**");
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers("/app/**/*.{js,html}")
+            .antMatchers("/i18n/**")
+            .antMatchers("/content/**")
+            .antMatchers("/test/**");
 
         web.ignoring().antMatchers(HttpMethod.POST, RESULT_RESOURCE_API_PATH + "*-*");
-        web.ignoring().antMatchers(HttpMethod.POST, NEW_RESULT_RESOURCE_API_PATH);
         web.ignoring().antMatchers(HttpMethod.POST, PROGRAMMING_SUBMISSION_RESOURCE_API_PATH + "*");
         web.ignoring().antMatchers(HttpMethod.POST, TEST_CASE_CHANGED_API_PATH + "*");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringAntMatchers("/websocket/**").ignoringAntMatchers("/api/lti/launch/*").and()
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class).exceptionHandling().authenticationEntryPoint(problemSupport)
-                .accessDeniedHandler(problemSupport).and().headers().frameOptions().disable().and().headers().httpStrictTransportSecurity().disable().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().antMatchers("/api/register").permitAll().antMatchers("/api/activate").permitAll()
-                .antMatchers("/api/authenticate").permitAll().antMatchers("/api/account/reset-password/init").permitAll().antMatchers("/api/account/reset-password/finish")
-                .permitAll().antMatchers("/api/lti/launch/*").permitAll().antMatchers("/api/**").authenticated().antMatchers("/websocket/tracker")
-                .hasAuthority(AuthoritiesConstants.ADMIN).antMatchers("/websocket/**").permitAll().antMatchers("/management/health").permitAll().antMatchers("/management/info")
-                .permitAll().antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN).and().apply(securityConfigurerAdapter());
+        http
+            .csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .ignoringAntMatchers("/websocket/**")
+            .ignoringAntMatchers("/api/lti/launch/*")
+        .and()
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint(problemSupport)
+            .accessDeniedHandler(problemSupport)
+        .and()
+            .headers()
+            .frameOptions()
+            .disable()
+        .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeRequests()
+            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/activate").permitAll()
+            .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/account/reset-password/init").permitAll()
+            .antMatchers("/api/account/reset-password/finish").permitAll()
+            .antMatchers("/api/lti/launch/*").permitAll()
+            .antMatchers("/api/**").authenticated()
+            .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("/websocket/**").permitAll()
+            .antMatchers("/management/health").permitAll()
+            .antMatchers("/management/info").permitAll()
+            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+        .and()
+            .apply(securityConfigurerAdapter());
 
     }
 
