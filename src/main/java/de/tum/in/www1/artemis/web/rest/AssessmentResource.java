0da4ed0b3d0b68e2296844e4cebf5586ab4147e7
Jan Philip Bernius, M.Sc <janphilip@bernius.net>
2019-04-05 20:54:40 +0200

    Allow automatic selection of text blocks, if no text blocks are defined so far. (#245)

--- src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
@@ -1,41 +1,21 @@
 package de.tum.in.www1.artemis.web.rest;
 
-import javax.annotation.Nullable;
-import org.springframework.http.ResponseEntity;
 import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.service.*;
 import de.tum.in.www1.artemis.web.rest.errors.*;
-import de.tum.in.www1.artemis.web.rest.util.HeaderUtil;
-import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.forbidden;
 
 public abstract class AssessmentResource {
-    protected final AuthorizationCheckService authCheckService;
-    protected final UserService userService;
 
+    protected final AuthorizationCheckService authCheckService;
+
+    protected final UserService userService;
 
     public AssessmentResource(AuthorizationCheckService authCheckService, UserService userService) {
         this.authCheckService = authCheckService;
         this.userService = userService;
     }
 
-
     abstract String getEntityName();
-
-
-    @Nullable
-    @Deprecated
-    <X> ResponseEntity<X> checkExercise(Exercise exercise) {
-        Course course = exercise.getCourse();
-        if (course == null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(getEntityName(), "courseNotFound", "The course belonging to this modeling exercise does not exist")).body(null);
-        }
-        User user = userService.getUserWithGroupsAndAuthorities();
-        if (!authCheckService.isAtLeastTeachingAssistantForExercise(exercise, user)) {
-            return forbidden();
-        }
-        return null;
-    }
-
 
     /**
      * @param exercise exercise to check privileges for
@@ -50,11 +30,10 @@ public abstract class AssessmentResource {
         }
     }
 
-
-    private void validateExercise(Exercise exercise) throws BadRequestAlertException {
+    void validateExercise(Exercise exercise) throws BadRequestAlertException {
         Course course = exercise.getCourse();
         if (course == null) {
-            throw new BadRequestAlertException("The course belonging to this modeling exercise does not exist", getEntityName(), "courseNotFound");
+            throw new BadRequestAlertException("The course belonging to this exercise does not exist", getEntityName(), "courseNotFound");
         }
     }
 }
