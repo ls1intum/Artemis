43eacee34c49ae46f40052eae3da642a62d08e3d
Maximilian Jelinek <jelinek@in.tum.de>
2019-04-05 16:24:28 +0200

    feat: added navigation button to conflict resolution view; added local conflict storage to modeling assessment service

--- src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
+++ src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
@@ -269,6 +269,11 @@ export class ModelingAssessmentEditorComponent implements OnInit, OnDestroy {
         }
     }
 
+    onShowConflictResolution() {
+        this.modelingAssessmentService.addLocalConflicts(this.submission.id, this.conflicts);
+        this.router.navigate(['modeling-exercise', this.modelingExercise.id, 'submissions', this.submission.id, 'assessment', 'conflict']);
+    }
+
     onFeedbackChanged(feedback: Feedback[]) {
         this.referencedFeedback = feedback;
         this.validateFeedback();
