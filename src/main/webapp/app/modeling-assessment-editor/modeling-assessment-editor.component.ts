7b82d8c089c10cac29ad3204e45a55d6ad4ce9a2
Maximilian Jelinek <jelinek@in.tum.de>
2019-04-07 16:59:13 +0200

    ref: styling improvements; feat: added translations, added more generic title to modeling-assessment.component, implemted apply changes logic;

--- src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
+++ src/main/webapp/app/modeling-assessment-editor/modeling-assessment-editor.component.ts
@@ -151,6 +151,7 @@ export class ModelingAssessmentEditorComponent implements OnInit, OnDestroy {
 
     onShowConflictResolution() {
         this.modelingAssessmentService.addLocalConflicts(this.submission.id, this.conflicts);
+        this.jhiAlertService.clear();
         this.router.navigate(['modeling-exercise', this.modelingExercise.id, 'submissions', this.submission.id, 'assessment', 'conflict']);
     }
 
