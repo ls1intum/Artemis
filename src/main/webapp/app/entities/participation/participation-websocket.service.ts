f2740409ddf62bfd1a0027b3f2b81f7edd197b81
Ceany <maxi-meier@web.de>
2019-04-14 19:35:58 +0200

    added docs, added web socket connection for new submissions

--- src/main/webapp/app/entities/participation/participation-websocket.service.ts
+++ src/main/webapp/app/entities/participation/participation-websocket.service.ts
@@ -6,12 +6,15 @@ import { JhiWebsocketService } from 'app/core';
 import { Result } from 'app/entities/result';
 import { Exercise } from 'app/entities/exercise';
 
+const RESULTS_WEBSOCKET = 'results_';
+const PARTICIPATION_WEBSOCKET = 'participation_';
+
 @Injectable({ providedIn: 'root' })
 export class ParticipationWebsocketService {
     cachedParticipations: Map<number, Participation> = new Map<number, Participation>();
-    openWebsocketConnections: Map<number, string> = new Map<number, string>();
+    openWebsocketConnections: Map<string, string> = new Map<string, string>();
     resultObservables: Map<number, BehaviorSubject<Result>> = new Map<number, BehaviorSubject<Result>>();
-    participationObserver: BehaviorSubject<Participation>;
+    participationObservable: BehaviorSubject<Participation>;
 
     constructor(private jhiWebsocketService: JhiWebsocketService) {}
 
@@ -29,7 +32,15 @@ export class ParticipationWebsocketService {
         this.addParticipationToList(participation, exercise);
     }
 
-    addParticipationToList(participation: Participation, exercise?: Exercise) {
+    /**
+     * This adds a participation to the cached data maps. The exercise information is required to find the correct
+     * participations for a given exercise.
+     *
+     * @param participation The new participation for the cached data maps
+     * @param exercise (optional) The exercise that the participation belongs to. Only needed if exercise is missing in participation.
+     * @private
+     */
+    private addParticipationToList(participation: Participation, exercise?: Exercise) {
         if (!participation.exercise && !exercise) {
             throw new Error('a link from the participation to the exercise is required. Please attach it manually or add exercise as function input');
         }
@@ -38,32 +49,92 @@ export class ParticipationWebsocketService {
         this.checkWebsocketConnection(participation);
     }
 
+    /**
+     * Returns all participations for all exercises. The participation objects include the exercise data and all results.
+     * @return array of Participations
+     */
     getAllParticipations(): Participation[] {
         return [...this.cachedParticipations.values()];
     }
 
-    getAllParticipationsForExercise(exerciseId: number): Participation[] {
+    /**
+     * Returns all participation for the given exercise. The participation objects include the exercise data and all results.
+     *
+     * @param exerciseId ID of the exercise that the participations belong to.
+     * @return array of Participations
+     * @private
+     */
+    private getAllParticipationsForExercise(exerciseId: number): Participation[] {
         return [...this.cachedParticipations.values()].filter(participation => {
             return participation.exercise.id === exerciseId;
         });
     }
 
+    /**
+     * Removes all participation information locally from all cached data maps.
+     *
+     * @param id ID of the participation that should not be tracked anymore
+     */
     removeParticipation(id: number) {
         this.cachedParticipations.delete(id);
-        const participationResultTopic = this.openWebsocketConnections.get(id);
+        // removing results observable
+        const participationResultTopic = this.openWebsocketConnections.get(`${RESULTS_WEBSOCKET}${id}`);
         this.jhiWebsocketService.unsubscribe(participationResultTopic);
-        this.openWebsocketConnections.delete(id);
+        this.openWebsocketConnections.delete(`${RESULTS_WEBSOCKET}${id}`);
+        // removing participation observable
+        const participationTopic = this.openWebsocketConnections.get(`${PARTICIPATION_WEBSOCKET}${id}`);
+        this.jhiWebsocketService.unsubscribe(participationTopic);
+        this.openWebsocketConnections.delete(`${PARTICIPATION_WEBSOCKET}${id}`);
     }
 
-    checkWebsocketConnection(participation: Participation) {
-        if (!this.openWebsocketConnections.get(participation.id)) {
+    /**
+     * Checks for the given participation all necessary websocket connections to the server already exists.
+     *
+     * @param participation Participation object that has to be checked
+     * @private
+     */
+    private checkWebsocketConnection(participation: Participation) {
+        this.checkWebsocketConnectionForNewResults(participation);
+        this.checkWebsocketConnectionForNewParticipations(participation);
+    }
+
+    /**
+     * Checks for the given participation if a websocket connection for new results to the server already exists.
+     * If not a new one will be opened.
+     *
+     * @param participation Participation object that has to be checked
+     * @private
+     */
+    private checkWebsocketConnectionForNewResults(participation: Participation) {
+        if (!this.openWebsocketConnections.get(`${RESULTS_WEBSOCKET}${participation.id}`)) {
             const participationResultTopic = `/topic/participation/${participation.id}/newResults`;
             this.jhiWebsocketService.subscribe(participationResultTopic);
             const participationResultObservable = this.jhiWebsocketService.receive(participationResultTopic);
             participationResultObservable.subscribe((result: Result) => {
                 this.addResultToParticipation(result);
             });
-            this.openWebsocketConnections.set(participation.id, participationResultTopic);
+            this.openWebsocketConnections.set(`${RESULTS_WEBSOCKET}${participation.id}`, participationResultTopic);
+        }
+    }
+
+    /**
+     * Checks for the given participation if a websocket connection for new participations to the server already exists.
+     * If not a new one will be opened.
+     *
+     * @param participation Participation object that has to be checked
+     * @private
+     */
+    private checkWebsocketConnectionForNewParticipations(participation: Participation) {
+        if (!this.openWebsocketConnections.get(`${PARTICIPATION_WEBSOCKET}${participation.id}`)) {
+            const participationResultTopic = `/user/topic/quizExercise/${participation.exercise.id}/participation`;
+            this.jhiWebsocketService.subscribe(participationResultTopic);
+            const participationObservable = this.jhiWebsocketService.receive(participationResultTopic);
+            participationObservable.subscribe((participationMessage: Participation) => {
+                debugger;
+                this.cachedParticipations.set(participationMessage.id, participationMessage);
+                this.participationObservable.next(participationMessage);
+            });
+            this.openWebsocketConnections.set(`${PARTICIPATION_WEBSOCKET}${participation.id}`, participationResultTopic);
         }
     }
 
@@ -71,20 +142,41 @@ export class ParticipationWebsocketService {
         return this.cachedParticipations.get(id);
     }
 
-    addResultToParticipation(result: Result) {
+    /**
+     * This adds newly received results to the corresponding participation. Then all listeners for the
+     * participationObservable will be notified with the complete participation object.
+     *
+     * @param result Newly received result object from the websocket message
+     * @private
+     */
+    private addResultToParticipation(result: Result) {
         const correspondingParticipation = this.cachedParticipations.get(result.participation.id);
         correspondingParticipation.results.push(result);
         this.cachedParticipations.set(correspondingParticipation.id, correspondingParticipation);
-        this.participationObserver.next(correspondingParticipation);
+        this.participationObservable.next(correspondingParticipation);
     }
 
+    /**
+     * Subscribing for general changes in a participation object. This will triggered if a new result is received by the service.
+     * A received object will be the full participation object including all results and the exercise.
+     *
+     * If no observable exists a new one will be created.
+     */
     subscribeForParticipationChanges(): BehaviorSubject<Participation> {
-        if (!this.participationObserver) {
-            this.participationObserver = new BehaviorSubject<Participation>(null);
+        if (!this.participationObservable) {
+            this.participationObservable = new BehaviorSubject<Participation>(null);
         }
-        return this.participationObserver;
+        return this.participationObservable;
     }
 
+    /**
+     * Subscribing to new results of a certain participation. This will be triggered if a new result is received by the service.
+     * A received Object will be a result object.
+     *
+     * If there is no observable for the participation a new one will be created.
+     *
+     * @param participationId ID of the participation that the new results should belong to
+     */
     subscribeForLatestResultOfParticipation(participationId: number): BehaviorSubject<Result> {
         let resultObservable = this.resultObservables.get(participationId);
         if (!resultObservable) {
