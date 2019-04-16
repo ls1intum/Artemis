4ace695b04c94eea96b97ea47e37e28968b4d3c8
Maximilian Meier <maxi.meier92@gmail.com>
2019-04-16 23:58:18 +0200

    added user check that prevented external users access to artemis (#292)

--- src/main/webapp/app/layouts/notification-container/notification-container.component.ts
+++ src/main/webapp/app/layouts/notification-container/notification-container.component.ts
@@ -18,20 +18,27 @@ export class NotificationContainerComponent implements OnInit {
     constructor(private notificationService: NotificationService, private userService: UserService, private accountService: AccountService) {}
 
     ngOnInit() {
-        this.notificationService.getRecentNotificationsForUser().subscribe((res: HttpResponse<Notification[]>) => {
-            this.notifications = res.body;
-            this.updateNotificationCount();
-        });
-        setTimeout(() => {
-            this.notificationService.subscribeUserNotifications();
-        }, 500);
-        this.notificationService.subscribeToSocketMessages().subscribe((notification: Notification) => {
-            if (notification) {
-                notification.notificationDate = notification.notificationDate ? moment(notification.notificationDate) : null;
-                this.notifications.push(notification);
-                this.updateNotificationCount();
-            }
-        });
+        this.accountService
+            .identity()
+            .then((user: User) => {
+                if (user) {
+                    this.notificationService.getRecentNotificationsForUser().subscribe((res: HttpResponse<Notification[]>) => {
+                        this.notifications = res.body;
+                        this.updateNotificationCount();
+                    });
+                    setTimeout(() => {
+                        this.notificationService.subscribeUserNotifications();
+                    }, 500);
+                    this.notificationService.subscribeToSocketMessages().subscribe((notification: Notification) => {
+                        if (notification) {
+                            notification.notificationDate = notification.notificationDate ? moment(notification.notificationDate) : null;
+                            this.notifications.push(notification);
+                            this.updateNotificationCount();
+                        }
+                    });
+                }
+            })
+            .catch(error => console.log(error));
     }
 
     startNotification(notification: Notification) {
