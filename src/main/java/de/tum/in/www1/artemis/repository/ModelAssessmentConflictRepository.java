ed24f62386ab892c7a4d6fd10363e66042d6f341
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2019-04-17 09:44:47 +0200

    feat: added Query to find Conflict by Result; added CausingResultService; added update helper functions to sevices

--- src/main/java/de/tum/in/www1/artemis/repository/ModelAssessmentConflictRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ModelAssessmentConflictRepository.java
@@ -1,10 +1,17 @@
 package de.tum.in.www1.artemis.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import de.tum.in.www1.artemis.domain.Result;
 import de.tum.in.www1.artemis.domain.modeling.ModelAssessmentConflict;
 
 @Repository
 public interface ModelAssessmentConflictRepository extends JpaRepository<ModelAssessmentConflict, Long> {
+
+    @Query("select c from ModelAssessmentConflict c where c.causingConflictingResult.result.id = :#{#result.id}")
+    List<ModelAssessmentConflict> findAllConflictsByCausingResult(@Param("result") Result result);
 }
