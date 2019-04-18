3606945c487e7adfa1f4d7cda32ffa33671f6262
Stephan Krusche <krusche@in.tum.de>
2019-04-18 15:40:34 +0200

    Apply suggestions from code review

--- src/main/java/de/tum/in/www1/artemis/web/rest/StudentQuestionAnswerResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/StudentQuestionAnswerResource.java
@@ -59,7 +59,7 @@ public class StudentQuestionAnswerResource {
         }
         StudentQuestionAnswer result = studentQuestionAnswerRepository.save(studentQuestionAnswer);
         if (result.getQuestion().getExercise() != null) { //TODO: what happens if the questions belongs to a Lecture?
-            groupNotificationService.notifyGroupAboutNewAnswer(result);
+            groupNotificationService.notifyGroupAboutNewAnswer(result); //TODO Which group do we notify about the new answer here? This should be specified in the method name
             singleUserNotificationService.notifyUserAboutNewAnswer(result);
         }
         return ResponseEntity.created(new URI("/api/question-answers/" + result.getId())).headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
