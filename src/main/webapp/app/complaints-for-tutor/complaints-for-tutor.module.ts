1df4e747399bed578e874577ad7a01e7a7022cc2
Riccardo Padovani <riccardo@rpadovani.com>
2019-04-19 08:37:52 +0200

    Move complaints for tutor to its own component (#302)

---
+++ src/main/webapp/app/complaints-for-tutor/complaints-for-tutor.module.ts
@@ -0,0 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { JhiLanguageService } from 'ng-jhipster';
+import { JhiLanguageHelper } from 'app/core';
+
+import { ArTEMiSSharedModule } from '../shared';
+import { JhiAlertService } from 'ng-jhipster';
+import { ComplaintsForTutorComponent } from './complaints-for-tutor.component';
+import { MomentModule } from 'angular2-moment';
+import { ClipboardModule } from 'ngx-clipboard';
+import { ComplaintService } from 'app/entities/complaint/complaint.service';
+import { ComplaintResponseService } from 'app/entities/complaint-response/complaint-response.service';
+
+@NgModule({
+    imports: [BrowserModule, ArTEMiSSharedModule, MomentModule, ClipboardModule],
+    declarations: [ComplaintsForTutorComponent],
+    exports: [ComplaintsForTutorComponent],
+    providers: [
+        JhiAlertService,
+        ComplaintService,
+        ComplaintResponseService,
+        {
+            provide: JhiLanguageService,
+            useClass: JhiLanguageService,
+        },
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class ArTEMiSComplaintsForTutorModule {
+    constructor(private languageService: JhiLanguageService, private languageHelper: JhiLanguageHelper) {
+        this.languageHelper.language.subscribe((languageKey: string) => {
+            if (languageKey !== undefined) {
+                this.languageService.changeLanguage(languageKey);
+            }
+        });
+    }
+}
