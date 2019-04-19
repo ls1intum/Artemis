56425e8051a27c80789931acd7c6000139cddfc9
Riccardo Padovani <riccardo@rpadovani.com>
2019-04-19 09:01:31 +0200

    Make text assessment components more resilient (#303)

--- src/main/webapp/app/text-assessment/text-assessment.component.ts
+++ src/main/webapp/app/text-assessment/text-assessment.component.ts
@@ -2,6 +2,7 @@ import * as $ from 'jquery';
 
 import { ChangeDetectorRef, Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { TextExercise } from 'app/entities/text-exercise';
 import { TextSubmission } from 'app/entities/text-submission';
 import { HighlightColors } from '../text-shared/highlight-colors';
@@ -39,6 +40,7 @@ export class TextAssessmentComponent implements OnInit, OnDestroy, AfterViewInit
     showResult = true;
     includeComplaint = false;
     complaint: Complaint;
+    notFound = false;
 
     formattedProblemStatement: string;
     formattedSampleSolution: string;
@@ -76,16 +78,28 @@ export class TextAssessmentComponent implements OnInit, OnDestroy, AfterViewInit
         this.includeComplaint = !!this.route.snapshot.queryParamMap.get('includeComplaint');
 
         if (submissionValue === 'new') {
-            this.assessmentsService.getParticipationForSubmissionWithoutAssessment(exerciseId).subscribe(participation => {
-                this.receiveParticipation(participation);
+            this.assessmentsService.getParticipationForSubmissionWithoutAssessment(exerciseId).subscribe(
+                participation => {
+                    this.receiveParticipation(participation);
 
-                // Update the url with the new id, without reloading the page, to make the history consistent
-                const newUrl = window.location.hash.replace('#', '').replace('new', `${this.submission.id}`);
-                this.location.go(newUrl);
-            });
+                    // Update the url with the new id, without reloading the page, to make the history consistent
+                    const newUrl = window.location.hash.replace('#', '').replace('new', `${this.submission.id}`);
+                    this.location.go(newUrl);
+                },
+                (error: HttpErrorResponse) => {
+                    if (error.status === 404) {
+                        this.notFound = true;
+                    } else {
+                        this.onError(error.message);
+                    }
+                    this.busy = false;
+                },
+            );
         } else {
             const submissionId = Number(submissionValue);
-            this.assessmentsService.getFeedbackDataForExerciseSubmission(exerciseId, submissionId).subscribe(participation => this.receiveParticipation(participation));
+            this.assessmentsService
+                .getFeedbackDataForExerciseSubmission(exerciseId, submissionId)
+                .subscribe(participation => this.receiveParticipation(participation), (error: HttpErrorResponse) => this.onError(error.message));
         }
     }
 
@@ -171,11 +185,14 @@ export class TextAssessmentComponent implements OnInit, OnDestroy, AfterViewInit
             return;
         }
 
-        this.assessmentsService.save(this.assessments, this.exercise.id, this.result.id).subscribe(response => {
-            this.result = response.body;
-            this.updateParticipationWithResult();
-            this.jhiAlertService.success('arTeMiSApp.textAssessment.saveSuccessful');
-        });
+        this.assessmentsService.save(this.assessments, this.exercise.id, this.result.id).subscribe(
+            response => {
+                this.result = response.body;
+                this.updateParticipationWithResult();
+                this.jhiAlertService.success('arTeMiSApp.textAssessment.saveSuccessful');
+            },
+            (error: HttpErrorResponse) => this.onError(error.message),
+        );
     }
 
     public submit(): void {
@@ -189,17 +206,23 @@ export class TextAssessmentComponent implements OnInit, OnDestroy, AfterViewInit
             return;
         }
 
-        this.assessmentsService.submit(this.assessments, this.exercise.id, this.result.id).subscribe(response => {
-            this.result = response.body;
-            this.updateParticipationWithResult();
-            this.jhiAlertService.success('arTeMiSApp.textAssessment.submitSuccessful');
-        });
+        this.assessmentsService.submit(this.assessments, this.exercise.id, this.result.id).subscribe(
+            response => {
+                this.result = response.body;
+                this.updateParticipationWithResult();
+                this.jhiAlertService.success('arTeMiSApp.textAssessment.submitSuccessful');
+            },
+            (error: HttpErrorResponse) => this.onError(error.message),
+        );
     }
 
     public predefineTextBlocks(): void {
-        this.assessmentsService.getResultWithPredefinedTextblocks(this.result.id).subscribe(response => {
-            this.assessments = response.body.feedbacks || [];
-        });
+        this.assessmentsService.getResultWithPredefinedTextblocks(this.result.id).subscribe(
+            response => {
+                this.assessments = response.body.feedbacks || [];
+            },
+            (error: HttpErrorResponse) => this.onError(error.message),
+        );
     }
 
     private updateParticipationWithResult(): void {
@@ -279,4 +302,10 @@ export class TextAssessmentComponent implements OnInit, OnDestroy, AfterViewInit
         }
         return baseKey + 'assessment';
     }
+
+    private onError(error: string) {
+        console.error(error);
+        this.jhiAlertService.error(error, null, null);
+    }
+
 }
