01005ac125df9e0b2082cfaa7415b7fe5d51f352
Stephan Krusche <s.krusche@tum.de>
2019-04-23 18:46:59 +0200

    fix error in test case changed path
    Signed-off-by: Stephan Krusche <s.krusche@tum.de>

--- src/main/java/de/tum/in/www1/artemis/config/Constants.java
+++ src/main/java/de/tum/in/www1/artemis/config/Constants.java
@@ -40,7 +40,7 @@ public final class Constants {
 
     public static final String NEW_RESULT_RESOURCE_API_PATH = "/api" + NEW_RESULT_RESOURCE_PATH;
 
-    public static final String TEST_CASE_CHANGED_PATH = "/programming-exercises/test-cases-changed";
+    public static final String TEST_CASE_CHANGED_PATH = "/programming-exercises/test-cases-changed/";
 
     public static final String TEST_CASE_CHANGED_API_PATH = "/api" + TEST_CASE_CHANGED_PATH;
 
