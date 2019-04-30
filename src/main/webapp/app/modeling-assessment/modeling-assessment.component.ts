1493869020a7714308b4b899bac44a8656c31732
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2019-04-30 14:51:58 +0200

    ref: deactivate Compass, remove username and passwords from repo; fix: no model error message, back button not showed in assessment, lint error

--- src/main/webapp/app/modeling-assessment/modeling-assessment.component.ts
+++ src/main/webapp/app/modeling-assessment/modeling-assessment.component.ts
@@ -40,8 +40,6 @@ export class ModelingAssessmentComponent implements AfterViewInit, OnDestroy, On
         this.initializeApollonEditor();
         if (this.highlightedElementIds) {
             this.updateHighlightedElements(this.highlightedElementIds);
-        } else {
-            this.jhiAlertService.error('modelingAssessment.noModel');
         }
         this.applyStateConfiguration();
         if (this.resizeOptions) {
