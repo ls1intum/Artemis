5c43865267bf66b68169913891badd6ce201f0ad
Jan Philip Bernius <janphilip.bernius@tum.de>
2019-05-03 17:44:23 +0200

    Reduce changes in account.model.ts

--- src/main/webapp/app/core/user/account.model.ts
+++ src/main/webapp/app/core/user/account.model.ts
@@ -1,12 +1,21 @@
 export class Account {
-    constructor(
-        public activated: boolean | null,
-        public authorities: string[] | null,
-        public email: string | null,
-        public firstName: string | null,
-        public langKey: string | null,
-        public lastName: string | null,
-        public login: string | null,
-        public imageUrl: string | null,
-    ) {}
+    public activated: boolean | null;
+    public authorities: string[] | null;
+    public email: string | null;
+    public firstName: string | null;
+    public langKey: string | null;
+    public lastName: string | null;
+    public login: string | null;
+    public imageUrl: string | null;
+
+    constructor(activated?: boolean, authorities?: string[], email?: string, firstName?: string, langKey?: string, lastName?: string, login?: string, imageUrl?: string) {
+        this.login = login || null;
+        this.firstName = firstName || null;
+        this.lastName = lastName || null;
+        this.email = email || null;
+        this.activated = activated || null;
+        this.langKey = langKey || null;
+        this.authorities = authorities || null;
+        this.imageUrl = imageUrl || null;
+    }
 }
