548c8173aed6cd9c3e80c72353b77ff6408a271a
Maximilian Jelinek <jelinek@in.tum.de>
2019-05-07 17:28:57 +0200

    feat: added conflict notification method to notification service

--- src/main/java/de/tum/in/www1/artemis/service/SingleUserNotificationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/SingleUserNotificationService.java
@@ -6,10 +6,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import de.tum.in.www1.artemis.domain.Notification;
-import de.tum.in.www1.artemis.domain.SingleUserNotification;
-import de.tum.in.www1.artemis.domain.StudentQuestionAnswer;
-import de.tum.in.www1.artemis.domain.User;
+import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.repository.SingleUserNotificationRepository;
 
 @Service
@@ -33,6 +30,14 @@ public class SingleUserNotificationService {
         return new SingleUserNotification(recipient, author, title, text);
     }
 
+    public SingleUserNotification createTutorNotificationForConflictingResult(Result conflictingResult) {
+        User recipient = conflictingResult.getAssessor();
+        User author = conflictingResult.getAssessor();
+        String title = "New Conflict";
+        String text = "A coflict with one of your assessments has been escalated!";
+        return new SingleUserNotification(recipient, author, title, text);
+    }
+
     public void notifyUserAboutNewAnswerForExercise(StudentQuestionAnswer studentQuestionAnswer) {
         SingleUserNotification userNotification = createUserNotificationForNewAnswer(studentQuestionAnswer);
         userNotification.setTarget(userNotification.studentQuestionAnswerTargetForExercise(studentQuestionAnswer));
@@ -42,6 +47,11 @@ public class SingleUserNotificationService {
     public void notifyUserAboutNewAnswerForLecture(StudentQuestionAnswer studentQuestionAnswer) {
         SingleUserNotification userNotification = createUserNotificationForNewAnswer(studentQuestionAnswer);
         userNotification.setTarget(userNotification.studentQuestionAnswerTargetForLecture(studentQuestionAnswer));
+        saveAndSendSingleUserNotification(userNotification);
+    }
+
+    public void notifyTutorAboutNewConflictForResult(Result conflictingResult) {
+        SingleUserNotification userNotification = createTutorNotificationForConflictingResult(conflictingResult);
         saveAndSendSingleUserNotification(userNotification);
     }
 
