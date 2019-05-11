29a3ace0ff6b747902215c22746bdee4fed29ddb
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-05-11 21:20:42 +0200

    Feature/programming exercise instructor status (#418)
    * implementation of instructor status icon
    * added unit tests
    * added result join to solution participation

---
+++ src/main/webapp/app/entities/programming-exercise/status/programming-exercise-instructor-status.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Result } from 'app/entities/result';
+import { ParticipationType } from '../programming-exercise-participation.model';
+
+@Component({
+    selector: 'jhi-programming-exercise-instructor-status',
+    templateUrl: './programming-exercise-instructor-status.component.html',
+})
+export class ProgrammingExerciseInstructorStatusComponent {
+    ParticipationType = ParticipationType;
+
+    @Input()
+    participationType: ParticipationType;
+    @Input()
+    result: Result;
+}
