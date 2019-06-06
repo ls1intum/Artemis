29071d1fc453999ef640ac7b7ec5ba7dff38a4e6
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-06-06 17:29:30 +0200

    Bugfix/instructor status/use participation websocket for results (#547)
    * implement websocket subscription for instructor status
    * fixing & updating test cases
    * unsubscribe result subscription on destroy

---
+++ src/test/javascript/spec/mocks/mock-sync.storage.ts
@@ -0,0 +1,18 @@
+import { StorageService } from 'ngx-webstorage/lib/core/interfaces/storageService';
+import { Observable } from 'rxjs';
+
+export class MockSyncStorage implements StorageService {
+    clear(key?: string): any {}
+
+    getStrategyName(): string {
+        return '';
+    }
+
+    observe(key: string): Observable<any> {
+        return undefined;
+    }
+
+    retrieve(key: string): any {}
+
+    store(key: string, value: any): any {}
+}
