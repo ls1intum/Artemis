82f067a01b8d15c882c7a0659f8f2b6a2e46302f
Stephan Krusche <s.krusche@tum.de>
2019-06-08 10:27:56 +0200

    coding conventions

--- src/main/webapp/app/shared/alert/alert.component.ts
+++ src/main/webapp/app/shared/alert/alert.component.ts
@@ -5,7 +5,7 @@ import { JhiAlertService } from 'ng-jhipster';
     selector: 'jhi-alert',
     template: `
         <div class="alerts" role="alert">
-            <div *ngFor="let alert of alerts" [ngClass]="{\'alert.position\': true, \'toast\': alert.toast}">
+            <div *ngFor="let alert of alerts" [ngClass]="{ 'alert.position': true, toast: alert.toast }">
                 <ngb-alert *ngIf="alert && alert.type && alert.msg" [type]="alert.type" (close)="alert.close(alerts)">
                     <pre [innerHTML]="alert.msg"></pre>
                 </ngb-alert>
