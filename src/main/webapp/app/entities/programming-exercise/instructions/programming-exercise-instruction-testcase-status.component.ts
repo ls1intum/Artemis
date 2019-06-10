25bacf1c6af73e556c78feb6a028f46eb9b5d847
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-06-10 20:15:34 +0200

    Enhancement/Domain command improvements for programming-exercises (#570)
    * sort test cases
    * implementation of naive content replace
    * implement list handling of test cases
    * added auto complete for test cases
    * linting fixes
    * refactored string segmentation into util
    * refactored test case command behavior
    * add auto completion for task command
    * refactoring for programming-exercise domain command enhancements
    * implementation of test case analysis
    * added translations for test case analysis

---
+++ src/main/webapp/app/entities/programming-exercise/instructions/programming-exercise-instruction-testcase-status.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { compose, difference, filter, flatten, intersection, map, uniq } from 'lodash/fp';
+
+@Component({
+    selector: 'jhi-programming-exercise-instruction-testcase-status',
+    templateUrl: './programming-exercise-instruction-testcase-status.component.html',
+})
+export class ProgrammingExerciseInstructionTestcaseStatusComponent implements OnChanges {
+    @Input()
+    exerciseTestCases: string[];
+    @Input()
+    problemStatement: string;
+    @Input()
+    taskRegex: RegExp;
+
+    missingTestCases: string[] = [];
+    invalidTestCases: string[] = [];
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ((changes.problemStatement || changes.exerciseTestCases) && this.exerciseTestCases && this.problemStatement && this.taskRegex) {
+            this.analyseTestCases();
+        }
+    }
+
+    /**
+     * Checks if test cases are used in the right way in the problem statement.
+     * This includes two possible errors:
+     * - having invalid test cases (that are not part of the test files)
+     * - not using existing test cases in the markup
+     * The method makes sure to filter out duplicates in the test case list.
+     */
+    analyseTestCases() {
+        const tasksFromProblemStatement = this.problemStatement.match(this.taskRegex) || [];
+        const testCasesInMarkdown = compose(
+            uniq,
+            flatten,
+            filter(test => !!test),
+            map((tests: string) => tests.split(',').map(string => string.trim())),
+            flatten,
+            filter(match => !!match),
+            map((taskMatch: string) => {
+                const testCase = taskMatch.match(/.*\((.*)\)/);
+                return testCase && testCase.length > 1 ? testCase[1] : null;
+            }),
+            filter((match: string) => !!match),
+        )(tasksFromProblemStatement) as string[];
+
+        this.invalidTestCases = compose(
+            difference(testCasesInMarkdown),
+            intersection(this.exerciseTestCases),
+        )(testCasesInMarkdown);
+        this.missingTestCases = difference(this.exerciseTestCases, testCasesInMarkdown);
+    }
+}
