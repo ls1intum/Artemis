47b92eced6cf88695133d61d9d17135d8d95cf14
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-06-14 09:40:54 +0200

    further refactoring of result component

--- src/main/webapp/app/entities/result/updating-result.component.ts
+++ src/main/webapp/app/entities/result/updating-result.component.ts
@@ -1,18 +1,14 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { orderBy as _orderBy } from 'lodash';
 import { Subscription } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
-import { Participation, ParticipationService } from 'app/entities/participation';
+import { hasParticipationChanged, Participation } from 'app/entities/participation';
 import { ParticipationWebsocketService } from 'app/entities/participation/participation-websocket.service';
 import { Result, ResultService } from '.';
-import { AccountService, JhiWebsocketService } from '../../core';
+import { AccountService } from '../../core';
 import { RepositoryService } from 'app/entities/repository/repository.service';
-import { HttpClient } from '@angular/common/http';
-import { Exercise, ExerciseType } from 'app/entities/exercise';
 
 import * as moment from 'moment';
-import { TranslateService } from '@ngx-translate/core';
-import { ProgrammingExercise } from 'app/entities/programming-exercise';
 
 @Component({
     selector: 'jhi-updating-result',
@@ -24,46 +20,20 @@ import { ProgrammingExercise } from 'app/entities/programming-exercise';
  * When using the result component make sure that the reference to the participation input is changed if the result changes
  * e.g. by using Object.assign to trigger ngOnChanges which makes sure that the result is updated
  */
-export class UpdatingResultComponent implements OnInit, OnChanges, OnDestroy {
-    // make constants available to html for comparison
-    readonly QUIZ = ExerciseType.QUIZ;
-    readonly PROGRAMMING = ExerciseType.PROGRAMMING;
-    readonly MODELING = ExerciseType.MODELING;
-
+export class UpdatingResultComponent implements OnChanges, OnDestroy {
     @Input() participation: Participation;
     @Input() isBuilding: boolean;
     @Input() short = false;
     @Input() result: Result;
     @Input() showUngradedResults: boolean;
     @Input() showGradedBadge: boolean;
-    @Output() newResultReceived = new EventEmitter<boolean>();
 
     private resultUpdateListener: Subscription;
 
-    constructor(
-        private jhiWebsocketService: JhiWebsocketService,
-        private resultService: ResultService,
-        private participationService: ParticipationService,
-        private repositoryService: RepositoryService,
-        private accountService: AccountService,
-        private translate: TranslateService,
-        private http: HttpClient,
-        private participationWebsocketService: ParticipationWebsocketService,
-    ) {}
+    constructor(private accountService: AccountService, private participationWebsocketService: ParticipationWebsocketService) {}
 
-    ngOnInit(): void {
-        if (!this.participation || !this.participation.id) {
-            return;
-        }
-
-        if (this.result) {
-            const exercise = this.participation.exercise;
-            if (exercise && exercise.type === ExerciseType.PROGRAMMING) {
-                this.subscribeForNewResults(exercise as ProgrammingExercise);
-            }
-        } else {
-            const exercise = this.participation.exercise;
-
+    ngOnChanges(changes: SimpleChanges) {
+        if (hasParticipationChanged(changes)) {
             // Sort participation results by completionDate desc.
             this.participation.results = this.participation.results && _orderBy(this.participation.results, 'completionDate', 'desc');
             // The latest result is the first rated result in the sorted array (=newest) or any result if the option is active to show ungraded results.
@@ -71,17 +41,30 @@ export class UpdatingResultComponent implements OnInit, OnChanges, OnDestroy {
             // Make sure that the participation result is connected to the newest result.
             this.result = this.result && { ...this.result, participation: this.participation };
 
-            this.subscribeForNewResults(exercise);
+            this.subscribeForNewResults();
         }
     }
 
-    subscribeForNewResults(exercise: Exercise) {
+    ngOnDestroy() {
+        if (this.resultUpdateListener) {
+            this.resultUpdateListener.unsubscribe();
+        }
+    }
+
+    subscribeForNewResults() {
+        // TODO: I don't think we need to check the users identity here.
+        // If it could be the case that the result passed into the component would not belong to the user,
+        // it would still be shown in html. Only the result subscription would not be instantiated.
         this.accountService.identity().then(user => {
+            const { exercise, student } = this.participation;
             // only subscribe for the currently logged in user or if the participation is a template/solution participation and the student is at least instructor
-            const isInstructorInCourse = this.participation.student == null && exercise.course && this.accountService.isAtLeastInstructorInCourse(exercise.course);
-            const isSameUser = this.participation.student && user.id === this.participation.student.id;
+            const isInstructorInCourse = student == null && exercise.course && this.accountService.isAtLeastInstructorInCourse(exercise.course);
+            const isSameUser = student && user.id === student.id;
 
             if (isSameUser || isInstructorInCourse) {
+                if (this.resultUpdateListener) {
+                    this.resultUpdateListener.unsubscribe();
+                }
                 this.resultUpdateListener = this.participationWebsocketService
                     .subscribeForLatestResultOfParticipation(this.participation.id)
                     .pipe(
@@ -91,22 +74,9 @@ export class UpdatingResultComponent implements OnInit, OnChanges, OnDestroy {
                         filter(result => this.showUngradedResults || result.rated === true),
                         map(result => ({ ...result, completionDate: result.completionDate != null ? moment(result.completionDate) : null, participation: this.participation })),
                         tap(result => (this.result = result)),
-                        tap(() => this.newResultReceived.emit(true)),
                     )
                     .subscribe();
             }
         });
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes.participation) {
-            this.ngOnInit();
-        }
-    }
-
-    ngOnDestroy() {
-        if (this.resultUpdateListener) {
-            this.resultUpdateListener.unsubscribe();
-        }
     }
 }
