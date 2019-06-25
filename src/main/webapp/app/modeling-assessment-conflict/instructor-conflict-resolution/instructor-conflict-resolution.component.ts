2022394952f884eb851510feafdbed2ee8b04961
Maximilian Jelinek <maximilian.jelinek@maibornwolff.de>
2019-06-25 10:53:25 +0200

    feat: added instructor conflict resolution component

---
+++ src/main/webapp/app/modeling-assessment-conflict/instructor-conflict-resolution/instructor-conflict-resolution.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-instructor-conflict-resolution',
+    templateUrl: './instructor-conflict-resolution.component.html',
+    styles: [],
+})
+export class InstructorConflictResolutionComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
