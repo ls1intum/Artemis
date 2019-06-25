f6cc7aea1c441de6cb685c52010fba36a0ebf971
Anh Montag <anh.montag@tum.de>
2019-06-25 02:38:27 +0200

    Add jhi-quiz-statistics-footer component to mc quiz statistics, quiz point statistics and drag and drop question statistics

--- src/main/webapp/app/quiz-statistics/quiz-statistics-footer.component.ts
+++ src/main/webapp/app/quiz-statistics/quiz-statistics-footer.component.ts
@@ -11,6 +11,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs/Subscription';
 import { QuizPointStatistic } from 'app/entities/quiz-point-statistic';
 import { HttpResponse } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
     selector: 'jhi-quiz-statistics-footer',
@@ -18,6 +19,7 @@ import { HttpResponse } from '@angular/common/http';
     providers: [QuizStatisticUtil, ShortAnswerQuestionUtil, ArtemisMarkdown],
 })
 export class QuizStatisticsFooterComponent implements OnInit, OnDestroy {
+    @Input() isQuizPointStatistic: boolean;
     @Input() isQuizStatistic: boolean;
 
     readonly DRAG_AND_DROP = QuizQuestionType.DRAG_AND_DROP;
@@ -31,6 +33,14 @@ export class QuizStatisticsFooterComponent implements OnInit, OnDestroy {
     questionIdParam: number;
     private sub: Subscription;
     private websocketChannelForData: string;
+    // timer
+    waitingForQuizStart = false;
+    remainingTimeText = '?';
+    remainingTimeSeconds = 0;
+    interval: any;
+    disconnected = true;
+    onConnected: () => void;
+    onDisconnected: () => void;
 
     constructor(
         private route: ActivatedRoute,
@@ -39,7 +49,6 @@ export class QuizStatisticsFooterComponent implements OnInit, OnDestroy {
         private translateService: TranslateService,
         private quizExerciseService: QuizExerciseService,
         private quizStatisticUtil: QuizStatisticUtil,
-        private artemisMarkdown: ArtemisMarkdown,
         private jhiWebsocketService: JhiWebsocketService,
     ) {}
 
@@ -51,11 +60,77 @@ export class QuizStatisticsFooterComponent implements OnInit, OnDestroy {
                     this.loadQuiz(res.body!);
                 });
             }
+
+            // listen to connect / disconnect events
+            this.onConnected = () => {
+                this.disconnected = false;
+            };
+            this.jhiWebsocketService.bind('connect', () => {
+                this.onConnected();
+            });
+            this.onDisconnected = () => {
+                this.disconnected = true;
+            };
+            this.jhiWebsocketService.bind('disconnect', () => {
+                this.onDisconnected();
+            });
         });
+
+        // update displayed times in UI regularly
+        this.interval = setInterval(() => {
+            this.updateDisplayedTimes();
+        }, 200);
+    }
+
+    /**
+     * updates all displayed (relative) times in the UI
+     */
+    updateDisplayedTimes() {
+        const translationBasePath = 'showStatistic.';
+        // update remaining time
+        if (this.quizExercise && this.quizExercise.adjustedDueDate) {
+            const endDate = this.quizExercise.adjustedDueDate;
+            if (endDate.isAfter(moment())) {
+                // quiz is still running => calculate remaining seconds and generate text based on that
+                this.remainingTimeSeconds = endDate.diff(moment(), 'seconds');
+                this.remainingTimeText = this.relativeTimeText(this.remainingTimeSeconds);
+            } else {
+                // quiz is over => set remaining seconds to negative, to deactivate 'Submit' button
+                this.remainingTimeSeconds = -1;
+                this.remainingTimeText = this.translateService.instant(translationBasePath + 'quizhasEnded');
+            }
+        } else {
+            // remaining time is unknown => Set remaining seconds to 0, to keep 'Submit' button enabled
+            this.remainingTimeSeconds = 0;
+            this.remainingTimeText = '?';
+        }
+    }
+
+    /**
+     * Express the given timespan as humanized text
+     *
+     * @param remainingTimeSeconds {number} the amount of seconds to display
+     * @return {string} humanized text for the given amount of seconds
+     */
+    relativeTimeText(remainingTimeSeconds: number) {
+        if (remainingTimeSeconds > 210) {
+            return Math.ceil(remainingTimeSeconds / 60) + ' min';
+        } else if (remainingTimeSeconds > 59) {
+            return Math.floor(remainingTimeSeconds / 60) + ' min ' + (remainingTimeSeconds % 60) + ' s';
+        } else {
+            return remainingTimeSeconds + ' s';
+        }
     }
 
     ngOnDestroy() {
+        clearInterval(this.interval);
         this.jhiWebsocketService.unsubscribe(this.websocketChannelForData);
+        if (this.onConnected) {
+            this.jhiWebsocketService.unbind('connect', this.onConnected);
+        }
+        if (this.onDisconnected) {
+            this.jhiWebsocketService.unbind('disconnect', this.onDisconnected);
+        }
     }
 
     /**
@@ -72,22 +147,43 @@ export class QuizStatisticsFooterComponent implements OnInit, OnDestroy {
         this.quizExercise = quiz;
         const updatedQuestion = this.quizExercise.quizQuestions.filter(question => this.questionIdParam === question.id)[0];
         this.question = updatedQuestion as QuizQuestion;
+        this.quizExercise.adjustedDueDate = moment().add(this.quizExercise.remainingTime, 'seconds');
+        this.waitingForQuizStart = !this.quizExercise.started;
     }
 
     /**
-     * got to the template with the previous QuizStatistic
-     * if first QuizQuestionStatistic -> go to the quiz-statistic
+     * This function navigates to the previous quiz question statistic.
+     * If the current page shows the first quiz question statistic then it will navigate to the quiz statistic
      */
     previousStatistic() {
-        this.quizStatisticUtil.previousStatistic(this.quizExercise, this.question);
+        if (this.isQuizStatistic) {
+            this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/quiz-point-statistic');
+        } else if (this.isQuizPointStatistic) {
+            if (this.quizExercise.quizQuestions === null || this.quizExercise.quizQuestions.length === 0) {
+                this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/quiz-statistic');
+            } else {
+                const previousQuestion = this.quizExercise.quizQuestions[this.quizExercise.quizQuestions.length - 1];
+                if (previousQuestion.type === this.MULTIPLE_CHOICE) {
+                    this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/multiple-choice-question-statistic/' + previousQuestion.id);
+                } else if (previousQuestion.type === this.DRAG_AND_DROP) {
+                    this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/drag-and-drop-question-statistic/' + previousQuestion.id);
+                } else if (previousQuestion.type === this.SHORT_ANSWER) {
+                    this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/short-answer-question-statistic/' + previousQuestion.id);
+                }
+            }
+        } else {
+            this.quizStatisticUtil.previousStatistic(this.quizExercise, this.question);
+        }
     }
 
     /**
-     * got to the Template with the next QuizStatistic
-     * if last QuizQuestionStatistic -> go to the Quiz-point-statistic
+     * This function navigates to the next quiz question statistic.
+     * If the current page shows the last quiz question statistic then it will navigate to the quiz point statistic
      */
     nextStatistic() {
-        if (this.isQuizStatistic) {
+        if (this.isQuizPointStatistic) {
+            this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/quiz-statistic');
+        } else if (this.isQuizStatistic) {
             // go to quiz-statistic if the position = last position
             if (this.quizExercise.quizQuestions === null || this.quizExercise.quizQuestions.length === 0) {
                 this.router.navigateByUrl('/quiz/' + this.quizExercise.id + '/quiz-point-statistic');
--- src/main/webapp/app/shared/pipes/truncate.pipe.ts
+++ src/main/webapp/app/shared/pipes/truncate.pipe.ts
@@ -6,6 +6,9 @@ export class TruncatePipe implements PipeTransform {
         if (completeWords) {
             limit = value.substr(0, limit).lastIndexOf(' ');
         }
+        if (value.length <= limit) {
+            ellipsis = '';
+        }
         return `${value.substr(0, limit)}${ellipsis}`;
     }
 }
