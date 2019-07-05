469078f0079d091843d5d63ced152d22022549e8
Jan Philip Bernius <janphilip.bernius@tum.de>
2019-07-05 19:14:17 +0200

    Schedule Event Lifecycle Tasks

---
+++ src/main/java/de/tum/in/www1/artemis/service/ExerciseLifecycleService.java
@@ -0,0 +1,49 @@
+package de.tum.in.www1.artemis.service;
+
+import java.time.ZonedDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.artemis.domain.Exercise;
+import de.tum.in.www1.artemis.domain.enumeration.ExerciseLifecycle;
+
+@Service
+public class ExerciseLifecycleService {
+
+    private final Logger log = LoggerFactory.getLogger(ExerciseLifecycleService.class);
+
+    private TaskScheduler scheduler;
+
+    ExerciseLifecycleService(@Qualifier("taskScheduler") TaskScheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    public void scheduleTask(Exercise exercise, ExerciseLifecycle lifecycle, Runnable task) {
+        final ZonedDateTime lifecycleDate;
+
+        switch (lifecycle) {
+        case RELEASE:
+            lifecycleDate = exercise.getReleaseDate();
+            break;
+
+        case DUE:
+            lifecycleDate = exercise.getDueDate();
+            break;
+
+        case ASSESSMENT_DUE:
+            lifecycleDate = exercise.getAssessmentDueDate();
+            break;
+
+        default:
+            return;
+        }
+
+        scheduler.schedule(task, lifecycleDate.toInstant());
+        log.debug("Scheduled Task for Exercise \"" + exercise.getTitle() + "\" (#" + exercise.getId() + ") to trigger on " + lifecycle.toString() + ".");
+    }
+
+}
