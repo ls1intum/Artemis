71bfc20f57ecf6dc1dd04bade325eeb618d5d436
Christian Ziegner <ziegner.christian@gmail.com>
2019-07-07 11:37:26 +0200

    add comment to every constant of the compass configuration

--- src/main/java/de/tum/in/www1/artemis/service/compass/utils/CompassConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/service/compass/utils/CompassConfiguration.java
@@ -5,45 +5,70 @@ package de.tum.in.www1.artemis.service.compass.utils;
  */
 public class CompassConfiguration {
 
-    // Similarity related parameters
+    /* Similarity related parameters */
+
+    // Amount that is subtracted from the similarity of two UML elements/diagrams for every missing element
     public final static double MISSING_ELEMENT_PENALTY = 0.05;
 
+    // Weight of the relationship type when calculating the similarity of two UML relationships
     public final static double RELATION_TYPE_WEIGHT = 0.3;
 
+    // Weight of the source and destination elements when calculating the similarity of two UML relationships
     public final static double RELATION_ELEMENT_WEIGHT = 0.35;
 
+    // Weight of the multiplicity when calculating the similarity of two UML relationships
     public final static double RELATION_MULTIPLICITY_OPTIONAL_WEIGHT = 0.05;
 
+    // Weight of the roles when calculating the similarity of two UML relationships
     public final static double RELATION_ROLE_OPTIONAL_WEIGHT = 0.05;
 
+    // Weight of the class type when calculating the similarity of two UML classes
     public final static double CLASS_TYPE_WEIGHT = 0.3;
 
+    // Weight of the class name when calculating the similarity of two UML classes
     public final static double CLASS_NAME_WEIGHT = 0.7;
 
+    // Weight of the name similarity of their attributes when calculating the similarity of two UML classes
     public final static double ATTRIBUTE_NAME_WEIGHT = 0.7;
 
+    // Weight of the type similarity of their attributes when calculating the similarity of two UML classes
     public final static double ATTRIBUTE_TYPE_WEIGHT = 0.3;
 
+    // Threshold for the similarity of child elements (attributes/methods in UML classes, classes and relationships in UML diagrams) when calculation the similarity of two UML
+    // classes/diagrams. If the similarity for specific child elements is smaller than the threshold, the elements will not be considered similar at all.
     public final static double NO_MATCH_THRESHOLD = 0.1;
 
+    // Threshold used for building similarity sets. If the similarity for two UML elements is smaller than this threshold, they will not be put into the same similarity set.
     // TODO CZ: decrease equality threshold again in the future
     public final static double EQUALITY_THRESHOLD = 0.95;
 
+    // Threshold used for re-assessing poorly assessed models. If the confidence for a model is smaller than this threshold the model is send to the client for re-assessment.
+    /* CURRENTLY DISABLED */
     public final static double POORLY_ASSESSED_MODEL_THRESHOLD = 0.8;
 
+    // Weight of the partial name similarity of two UML elements. The partial name similarity compares the first three characters of the names.
     public final static double PARTIALLY_NAME_WEIGHT = 0.8;
 
-    // Confidence and coverage parameters
+    /* Confidence and coverage parameters */
+
+    // Threshold of the coverage of a UML diagram. If the coverage for a specific diagram is smaller than this threshold, no automatic result will be created for the diagram.
+    /* CURRENTLY DISABLED */
     public static final double COVERAGE_THRESHOLD = 0.8;
 
+    // Threshold of the confidence of a UML diagram. If the confidence for a specific diagram is smaller than this threshold, no automatic result will be created for the diagram.
+    /* CURRENTLY DISABLED */
     public static final double CONFIDENCE_THRESHOLD = 0.75;
 
+    // Threshold of the confidence of an element in a UML diagram. If the confidence for a specific model element is smaller than this threshold, no automatic feedback will be
+    // created for this element.
     public static final double ELEMENT_CONFIDENCE_THRESHOLD = 0.8;
 
-    // Calculation engine cleanup parameters
+    /* Calculation engine cleanup parameters */
+
+    // Number of days to keep unused calculation engines in memory. If an engine is unused for a longer time, it will be removed in the cleanup job running every night.
     public static final int DAYS_TO_KEEP_UNUSED_ENGINE = 1;
 
-    public static final int TIME_TO_CHECK_FOR_UNUSED_ENGINES = 3600000;
+    /* Optimal model parameters */
 
     // Number of optimal models to keep in cache
     public static final int NUMBER_OF_OPTIMAL_MODELS = 10;
