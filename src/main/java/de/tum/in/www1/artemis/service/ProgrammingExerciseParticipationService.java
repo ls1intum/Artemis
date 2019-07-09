3c8d85954c20d38784055a400db4e90e5935c444
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-07-09 23:00:36 +0200

    refactored programming exercise participation resources into one

--- src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseParticipationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseParticipationService.java
@@ -1,4 +1,50 @@
 package de.tum.in.www1.artemis.service;
 
+import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.artemis.domain.ProgrammingExerciseParticipation;
+import de.tum.in.www1.artemis.domain.SolutionProgrammingExerciseParticipation;
+import de.tum.in.www1.artemis.domain.TemplateProgrammingExerciseParticipation;
+import de.tum.in.www1.artemis.domain.User;
+import de.tum.in.www1.artemis.repository.SolutionProgrammingExerciseParticipationRepository;
+import de.tum.in.www1.artemis.repository.TemplateProgrammingExerciseParticipationRepository;
+
+@Service
 public class ProgrammingExerciseParticipationService {
+
+    private SolutionProgrammingExerciseParticipationRepository solutionParticipationRepository;
+
+    private TemplateProgrammingExerciseParticipationRepository templateParticipationRepository;
+
+    private AuthorizationCheckService authCheckService;
+
+    private UserService userService;
+
+    public ProgrammingExerciseParticipationService(SolutionProgrammingExerciseParticipationRepository solutionParticipationRepository,
+            TemplateProgrammingExerciseParticipationRepository templateParticipationRepository, UserService userService, AuthorizationCheckService authCheckService) {
+        this.solutionParticipationRepository = solutionParticipationRepository;
+        this.templateParticipationRepository = templateParticipationRepository;
+        this.authCheckService = authCheckService;
+        this.userService = userService;
+    }
+
+    public boolean canAccessParticipation(ProgrammingExerciseParticipation participation) {
+        if (participation instanceof SolutionProgrammingExerciseParticipation) {
+            return canAccessParticipation((SolutionProgrammingExerciseParticipation) participation);
+        }
+        else if (participation instanceof TemplateProgrammingExerciseParticipation) {
+            return canAccessParticipation((TemplateProgrammingExerciseParticipation) participation);
+        }
+        return false;
+    }
+
+    public boolean canAccessParticipation(SolutionProgrammingExerciseParticipation participation) {
+        User user = userService.getUserWithGroupsAndAuthorities();
+        return authCheckService.isAtLeastTeachingAssistantForExercise(participation.getExercise(), user);
+    }
+
+    public boolean canAccessParticipation(TemplateProgrammingExerciseParticipation participation) {
+        User user = userService.getUserWithGroupsAndAuthorities();
+        return authCheckService.isAtLeastTeachingAssistantForExercise(participation.getExercise(), user);
+    }
 }
---
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ProgrammingExerciseParticipationResource.java
@@ -0,0 +1,89 @@
+package de.tum.in.www1.artemis.web.rest;
+
+import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.forbidden;
+import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.notFound;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tum.in.www1.artemis.domain.*;
+import de.tum.in.www1.artemis.repository.ResultRepository;
+import de.tum.in.www1.artemis.repository.SolutionProgrammingExerciseParticipationRepository;
+import de.tum.in.www1.artemis.repository.TemplateProgrammingExerciseParticipationRepository;
+import de.tum.in.www1.artemis.service.ProgrammingExerciseParticipationService;
+
+@RestController
+@RequestMapping("/api")
+public class ProgrammingExerciseParticipationResource {
+
+    private final Logger log = LoggerFactory.getLogger(ProgrammingExerciseParticipationResource.class);
+
+    private ProgrammingExerciseParticipationService programmingExerciseParticipationService;
+
+    private SolutionProgrammingExerciseParticipationRepository solutionParticipationRepository;
+
+    private TemplateProgrammingExerciseParticipationRepository templateParticipationRepository;
+
+    private ResultRepository resultRepository;
+
+    public ProgrammingExerciseParticipationResource(ProgrammingExerciseParticipationService programmingExerciseParticipationService,
+            SolutionProgrammingExerciseParticipationRepository solutionParticipationRepository, TemplateProgrammingExerciseParticipationRepository templateParticipationRepository,
+            ResultRepository resultRepository) {
+        this.programmingExerciseParticipationService = programmingExerciseParticipationService;
+        this.solutionParticipationRepository = solutionParticipationRepository;
+        this.templateParticipationRepository = templateParticipationRepository;
+        this.resultRepository = resultRepository;
+    }
+
+    /**
+     * GET /courses/:courseId/exercises : get all the exercises.
+     *
+     * @return the ResponseEntity with status 200 (OK) and the list of programmingExercises in body
+     */
+    @GetMapping(value = "/programming-exercises-solution-participation/{participationId}/latest-result-with-feedbacks")
+    @PreAuthorize("hasAnyRole('TA', 'INSTRUCTOR', 'ADMIN')")
+    public ResponseEntity<Result> getLatestResultWithFeedbacksForSolutionParticipation(@PathVariable Long participationId) {
+        Optional<SolutionProgrammingExerciseParticipation> participation = solutionParticipationRepository.findById(participationId);
+        if (!participation.isPresent()) {
+            return notFound();
+        }
+        return getLatestResultWithFeedbacks(participation.get());
+    }
+
+    /**
+     * GET /courses/:courseId/exercises : get all the exercises.
+     *
+     * @return the ResponseEntity with status 200 (OK) and the list of programmingExercises in body
+     */
+    @GetMapping(value = "/programming-exercises-template-participation/{participationId}/latest-result-with-feedbacks")
+    @PreAuthorize("hasAnyRole('TA', 'INSTRUCTOR', 'ADMIN')")
+    public ResponseEntity<Result> getLatestResultWithFeedbacksForTemplateParticipation(@PathVariable Long participationId) {
+        Optional<TemplateProgrammingExerciseParticipation> participation = templateParticipationRepository.findById(participationId);
+        if (!participation.isPresent()) {
+            return notFound();
+        }
+        return getLatestResultWithFeedbacks(participation.get());
+    }
+
+    private ResponseEntity<Result> getLatestResultWithFeedbacks(ProgrammingExerciseParticipation participation) {
+        if (!programmingExerciseParticipationService.canAccessParticipation(participation)) {
+            return forbidden();
+        }
+        Optional<Result> result = resultRepository.findFirstWithFeedbacksByParticipationIdOrderByCompletionDateDesc(participation.getId());
+        if (!result.isPresent()) {
+            return notFound();
+        }
+        // avoid circular serialization issue.
+        result.get().setParticipation(null);
+        return ResponseEntity.ok(result.get());
+    }
+
+}
