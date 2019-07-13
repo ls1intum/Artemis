bab5a3f9180d667255a2ac85966c08751b08d550
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-07-13 10:27:56 +0200

    Tests/Fix or remove faulty tests for next assessment (#628)

---
+++ src/test/javascript/spec/component/text-exercise-assessment/text-exercise-assessement.spec.ts
@@ -0,0 +1,136 @@
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AccountService, JhiLanguageHelper } from 'app/core';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as moment from 'moment';
+import { SinonStub, stub } from 'sinon';
+import { ArTEMiSTestModule } from '../../test.module';
+import { MockSyncStorage } from '../../mocks';
+import { UpdatingResultComponent } from 'app/entities/result';
+import { MockComponent } from 'ng-mocks';
+import { ArTEMiSSharedModule } from 'app/shared';
+import { ExerciseType } from 'app/entities/exercise';
+import { MockAlertService } from '../../helpers/mock-alert.service';
+import { JhiAlertService } from 'ng-jhipster';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { TextAssessmentComponent } from 'app/text-assessment/text-assessment.component';
+import { TextSubmission, TextSubmissionService } from 'app/entities/text-submission';
+import { TextExercise } from 'app/entities/text-exercise';
+import { TextAssessmentEditorComponent } from 'app/text-assessment/text-assessment-editor/text-assessment-editor.component';
+import { ResizableInstructionsComponent } from 'app/text-assessment/resizable-instructions/resizable-instructions.component';
+import { TextAssessmentDetailComponent } from 'app/text-assessment/text-assessment-detail/text-assessment-detail.component';
+import { ComplaintsForTutorComponent } from 'app/complaints-for-tutor';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MockAccountService } from '../../mocks/mock-account.service';
+import { Location } from '@angular/common';
+import { textAssessmentRoutes } from 'app/text-assessment/text-assessment.route';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { SubmissionExerciseType, SubmissionType } from 'app/entities/submission';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('TextAssessmentComponent', () => {
+    let comp: TextAssessmentComponent;
+    let fixture: ComponentFixture<TextAssessmentComponent>;
+    let textSubmissionService: TextSubmissionService;
+    let getTextSubmissionForExerciseWithoutAssessmentStub: SinonStub;
+    let debugElement: DebugElement;
+    let router: Router;
+    let location: Location;
+
+    const exercise = { id: 20, type: ExerciseType.TEXT } as TextExercise;
+
+    beforeEach(async () => {
+        return TestBed.configureTestingModule({
+            imports: [ArTEMiSTestModule, ArTEMiSSharedModule, RouterTestingModule.withRoutes([textAssessmentRoutes[0]])],
+            declarations: [
+                TextAssessmentComponent,
+                MockComponent(UpdatingResultComponent),
+                MockComponent(TextAssessmentEditorComponent),
+                MockComponent(ResizableInstructionsComponent),
+                MockComponent(TextAssessmentDetailComponent),
+                MockComponent(ComplaintsForTutorComponent),
+            ],
+            providers: [
+                JhiLanguageHelper,
+                { provide: JhiAlertService, useClass: MockAlertService },
+                { provide: AccountService, useClass: MockAccountService },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+            ],
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(TextAssessmentComponent);
+                comp = fixture.componentInstance;
+                comp.exercise = exercise;
+                debugElement = fixture.debugElement;
+                router = debugElement.injector.get(Router);
+                location = debugElement.injector.get(Location);
+                textSubmissionService = TestBed.get(TextSubmissionService);
+                getTextSubmissionForExerciseWithoutAssessmentStub = stub(textSubmissionService, 'getTextSubmissionForExerciseWithoutAssessment');
+
+                router.initialNavigation();
+            });
+    });
+
+    afterEach(() => {
+        getTextSubmissionForExerciseWithoutAssessmentStub.restore();
+    });
+
+    it(
+        'AssessNextButton should be visible, the method assessNextOptimal should be invoked ' + 'and the url should change after clicking on the button',
+        fakeAsync(() => {
+            // set all attributes for comp
+            comp.ngOnInit();
+            tick();
+
+            comp.userId = 99;
+            comp.submission = {
+                submissionExerciseType: SubmissionExerciseType.TEXT,
+                id: 2278,
+                submitted: true,
+                type: SubmissionType.MANUAL,
+                submissionDate: moment('2019-07-09T10:47:33.244Z'),
+                text: 'asdfasdfasdfasdf',
+            } as TextSubmission;
+            comp.result = {
+                id: 2374,
+                resultString: '1 of 12 points',
+                completionDate: moment('2019-07-09T11:51:23.251Z'),
+                successful: false,
+                score: 8,
+                rated: true,
+                hasFeedback: false,
+                submission: comp.submission,
+            };
+            comp.isAuthorized = true;
+            comp.isAtLeastInstructor = true;
+            comp.assessmentsAreValid = true;
+            const unassessedSubmission = { submissionExerciseType: 'text', id: 2279, submitted: true, type: 'MANUAL' };
+
+            fixture.detectChanges();
+
+            // check if assessNextButton is available
+            const assessNextButton = debugElement.query(By.css('#assessNextButton'));
+            expect(assessNextButton).to.exist;
+
+            // check if getTextSubmissionForExerciseWithoutAssessment() is called and works
+            getTextSubmissionForExerciseWithoutAssessmentStub.returns(of(unassessedSubmission));
+            assessNextButton.nativeElement.click();
+            expect(getTextSubmissionForExerciseWithoutAssessmentStub).to.have.been.called;
+            expect(comp.unassessedSubmission).to.be.deep.equal(unassessedSubmission);
+
+            // check if the url changes when you clicked on assessNextAssessmentButton
+            tick();
+            expect(location.path()).to.be.equal('/text/' + comp.exercise.id + '/assessment/' + comp.unassessedSubmission.id);
+
+            fixture.destroy();
+            flush();
+        }),
+    );
+});
--- src/test/javascript/spec/mocks/mock-account.service.ts
+++ src/test/javascript/spec/mocks/mock-account.service.ts
@@ -3,6 +3,7 @@ import { Course } from 'app/entities/course';
 export class MockAccountService {
     identity = () => Promise.resolve({ id: 99 });
     hasAnyAuthority = (authorities: any[]) => Promise.resolve(true);
+    hasAnyAuthorityDirect = (authorities: any[]) => Promise.resolve(true);
     getAuthenticationState = () => of();
     isAtLeastInstructorInCourse = (course: Course) => true;
 }
