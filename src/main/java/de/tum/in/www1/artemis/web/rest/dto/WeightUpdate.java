d83f1acbe0ea3dcdedb8f3a64c5a73d19ee0f9ad
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-07-14 14:12:23 +0200

    add doc to dto

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/WeightUpdate.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/WeightUpdate.java
@@ -1,5 +1,8 @@
 package de.tum.in.www1.artemis.web.rest.dto;
 
+/**
+ * This is a dto for updating the weight of an entity. Currently only used for updating programming exercise test case weights.
+ */
 public class WeightUpdate {
 
     private Integer weight;
