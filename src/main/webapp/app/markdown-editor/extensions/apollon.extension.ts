490050825b39333dca4f012f10dc1808346b47ab
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-07-16 16:47:33 +0200

    parse all apollon diagrams from the editor (not only the first)

--- src/main/webapp/app/markdown-editor/extensions/apollon.extension.ts
+++ src/main/webapp/app/markdown-editor/extensions/apollon.extension.ts
@@ -7,29 +7,36 @@ export const ApollonExtension = (componentFactoryResolver: ComponentFactoryResol
     const extension: showdown.ShowdownExtension = {
         type: 'lang',
         filter: (text: string, converter: showdown.Converter, options: showdown.ConverterOptions) => {
+            const idPlaceholder = '%idPlaceholder%';
             const regex = /(?!\[apollon\])(\d+?)(?=\[\/apollon\])/g;
-            const match = regex.exec(text);
-            if (match) {
-                const diagramId: number = Number(JSON.parse(match[1]));
-                apollonService
-                    .find(diagramId)
-                    .map(({ body }) => body)
-                    .toPromise()
-                    .then((diagram: ApollonDiagram) => {
-                        const componentRef = componentFactoryResolver.resolveComponentFactory(ModelingEditorComponent).create(injector);
-                        componentRef.instance.readOnly = true;
-                        componentRef.instance.umlModel = JSON.parse(diagram.jsonRepresentation);
-                        appRef.attachView(componentRef.hostView);
-                        const domElem = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
-                        const apollonContainer = document.getElementById(`apollon-${diagramId}`);
-                        if (apollonContainer) {
-                            apollonContainer.innerHTML = '';
-                            apollonContainer.append(domElem);
-                        }
-                    });
-                return `<div id="apollon-${diagramId}"></div>`;
-            }
-            return text;
+            const regexTemplate = `\\[apollon\\]${idPlaceholder}\\[\\/apollon\\]`;
+            const apollonHtmlContainer = `<div id="apollon-${idPlaceholder}"></div>`;
+            const apollonDiagrams = (text.match(regex) || []).map(Number);
+            const replacedText = apollonDiagrams.reduce(
+                (acc: string, id: string) => acc.replace(new RegExp(regexTemplate.replace(idPlaceholder, id), 'g'), apollonHtmlContainer.replace(idPlaceholder, id)),
+                text,
+            );
+            apollonDiagrams
+                .reduce((acc: number[], x: number) => (acc.includes(x) ? acc : [...acc, x]), [])
+                .forEach((diagramId: number) => {
+                    apollonService
+                        .find(diagramId)
+                        .map(({ body }) => body)
+                        .toPromise()
+                        .then((diagram: ApollonDiagram) => {
+                            const componentRef = componentFactoryResolver.resolveComponentFactory(ModelingEditorComponent).create(injector);
+                            componentRef.instance.readOnly = true;
+                            componentRef.instance.umlModel = JSON.parse(diagram.jsonRepresentation);
+                            appRef.attachView(componentRef.hostView);
+                            const domElem = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+                            const apollonContainer = document.getElementById(`apollon-${diagramId}`);
+                            if (apollonContainer) {
+                                apollonContainer.innerHTML = '';
+                                apollonContainer.append(domElem);
+                            }
+                        });
+                });
+            return replacedText;
         },
     };
     return extension;
