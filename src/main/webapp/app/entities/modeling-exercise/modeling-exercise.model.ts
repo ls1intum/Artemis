be009b4d929455a54ff06daed7255697baf230ce
jwilland-tum <38191490+jwilland-tum@users.noreply.github.com>
2019-07-16 22:52:55 +0200

    Apollon 2.0 (#647)
    * Updated to Apollon 2.0
    * Update help text for apollon 2.0

--- src/main/webapp/app/entities/modeling-exercise/modeling-exercise.model.ts
+++ src/main/webapp/app/entities/modeling-exercise/modeling-exercise.model.ts
@@ -1,9 +1,9 @@
 import { Exercise, ExerciseType } from '../exercise';
 import { Course } from '../course';
-import { DiagramType } from '@ls1intum/apollon';
+import { UMLDiagramType } from '@ls1intum/apollon';
 
 export class ModelingExercise extends Exercise {
-    public diagramType: DiagramType;
+    public diagramType: UMLDiagramType;
     public sampleSolutionModel: string;
     public sampleSolutionExplanation: string;
 
@@ -12,11 +12,11 @@ export class ModelingExercise extends Exercise {
     public automaticAssessmentSupported = false; // TODO: in the future, we will set this value to true when loading the modeling exercise
     // from the server when the corresponding diagram type supports automatic assessment
 
-    constructor(diagramType: DiagramType, course?: Course) {
+    constructor(diagramType: UMLDiagramType, course?: Course) {
         super(ExerciseType.MODELING);
         this.course = course || null;
         this.diagramType = diagramType;
     }
 }
 
-export { DiagramType };
+export { UMLDiagramType as DiagramType };
