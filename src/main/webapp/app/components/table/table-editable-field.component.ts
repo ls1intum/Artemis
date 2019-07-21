b9025ee3d4f27703149ebd2a00cef072c3048183
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-07-21 15:02:44 +0200

    fix bug with autofocus of table edit field

--- src/main/webapp/app/components/table/table-editable-field.component.ts
+++ src/main/webapp/app/components/table/table-editable-field.component.ts
@@ -14,6 +14,7 @@ import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges, ViewC
                 {{ value }}
             </span>
             <input
+                #editingInput
                 *ngIf="isEditing"
                 class="table-editable-field__input form-control mr-2"
                 autofocus
@@ -44,7 +45,7 @@ export class TableEditableFieldComponent<T> implements OnChanges {
                 if (this.editingInput) {
                     this.editingInput.nativeElement.focus();
                 }
-            });
+            }, 0);
         }
     }
 
