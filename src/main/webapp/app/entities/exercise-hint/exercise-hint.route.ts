8108695b80563d5d76636e4ad6e37b739d3addca
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-07-21 14:54:42 +0200

    fix exercise hint route permissions

--- src/main/webapp/app/entities/exercise-hint/exercise-hint.route.ts
+++ src/main/webapp/app/entities/exercise-hint/exercise-hint.route.ts
@@ -33,7 +33,7 @@ export const exerciseHintRoute: Routes = [
         path: 'exercise/:exerciseId/hints',
         component: ExerciseHintComponent,
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'artemisApp.exerciseHint.home.title',
         },
         canActivate: [UserRouteAccessService],
@@ -45,7 +45,7 @@ export const exerciseHintRoute: Routes = [
             exerciseHint: ExerciseHintResolve,
         },
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'artemisApp.exerciseHint.home.title',
         },
         canActivate: [UserRouteAccessService],
@@ -57,7 +57,7 @@ export const exerciseHintRoute: Routes = [
             exerciseHint: ExerciseHintResolve,
         },
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'artemisApp.exerciseHint.home.title',
         },
         canActivate: [UserRouteAccessService],
@@ -69,7 +69,7 @@ export const exerciseHintRoute: Routes = [
             exerciseHint: ExerciseHintResolve,
         },
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'artemisApp.exerciseHint.home.title',
         },
         canActivate: [UserRouteAccessService],
@@ -84,7 +84,7 @@ export const exerciseHintPopupRoute: Routes = [
             exerciseHint: ExerciseHintResolve,
         },
         data: {
-            authorities: ['ROLE_USER'],
+            authorities: ['ROLE_ADMIN'],
             pageTitle: 'artemisApp.exerciseHint.home.title',
         },
         canActivate: [UserRouteAccessService],
