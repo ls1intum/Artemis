b273ec8b3b2d0254f86329d9c347c567087a7ab4
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2019-07-24 11:11:46 +0200

    simplified for loop

--- src/main/java/de/tum/in/www1/artemis/service/ExerciseService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ExerciseService.java
@@ -217,9 +217,7 @@ public class ExerciseService {
         log.debug("Request reset Exercise : {}", exercise.getId());
 
         // delete all participations for this exercise
-        for (Participation participation : exercise.getParticipations()) {
-            participationService.delete(participation.getId(), true, true);
-        }
+        participationService.deleteAllByExerciseId(exercise.getId(), true, true);
 
         if (exercise instanceof QuizExercise) {
 
