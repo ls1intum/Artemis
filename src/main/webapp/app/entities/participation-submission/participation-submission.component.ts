1693df2e77479d0c63dc1b59f3209feb180add8e
ge68his <ge68his@mytum.de>
2019-07-24 14:40:23 +0200

    new component added to view the participation submission

---
+++ src/main/webapp/app/entities/participation-submission/participation-submission.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-participation-submission',
+    templateUrl: './participation-submission.component.html',
+    styles: [],
+})
+export class ParticipationSubmissionComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
