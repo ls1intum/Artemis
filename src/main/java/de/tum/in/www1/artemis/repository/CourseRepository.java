695e2765d4d35193a48f00a23cdfc097ddeb0dac
Simon Leiß <git@simon-leiss.de>
2019-07-25 12:22:43 +0200

    Use date instead of timestamp.

--- src/main/java/de/tum/in/www1/artemis/repository/CourseRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/CourseRepository.java
@@ -23,12 +23,12 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             @QueryHint(name = "org.hibernate.cacheRegion", value = "query_de.tum.in.www1.artemis.domain.Course") })
     List<Course> findAll();
 
-    @Query("select distinct course from Course course where (course.startDate <= current_timestamp or course.startDate is null) and (course.endDate >= current_timestamp or course.endDate is null)")
+    @Query("select distinct course from Course course where (course.startDate <= current_date or course.startDate is null) and (course.endDate >= current_date or course.endDate is null)")
     @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true"),
             @QueryHint(name = "org.hibernate.cacheRegion", value = "query_de.tum.in.www1.artemis.domain.Course") })
     List<Course> findAllActive();
 
-    @Query("select distinct course from Course course left join fetch course.exercises where (course.startDate <= current_timestamp or course.startDate is null) and (course.endDate >= current_timestamp or course.endDate is null)")
+    @Query("select distinct course from Course course left join fetch course.exercises where (course.startDate <= current_date or course.startDate is null) and (course.endDate >= current_date or course.endDate is null)")
     @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true"),
             @QueryHint(name = "org.hibernate.cacheRegion", value = "query_de.tum.in.www1.artemis.domain.Course") })
     List<Course> findAllActiveWithEagerExercises();
@@ -38,7 +38,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
             @QueryHint(name = "org.hibernate.cacheRegion", value = "query_de.tum.in.www1.artemis.domain.Course") })
     Course findOneWithEagerExercises(@Param("courseId") Long courseId);
 
-    @Query("select distinct course from Course course where course.startDate <= current_timestamp and course.endDate >= current_timestamp and course.onlineCourse = 0 and course.registrationEnabled = 1")
+    @Query("select distinct course from Course course where course.startDate <= current_date and course.endDate >= current_date and course.onlineCourse = 0 and course.registrationEnabled = 1")
     @QueryHints(value = { @QueryHint(name = "org.hibernate.cacheable", value = "true"),
             @QueryHint(name = "org.hibernate.cacheRegion", value = "query_de.tum.in.www1.artemis.domain.Course") })
     List<Course> findAllCurrentlyActiveAndNotOnlineAndEnabled();
