24861c88c3515ebf1d6d5e07de6cd1ffa764f5fb
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-07-26 06:43:25 +0200

    Bugfix/Account service: Check if userIdentity is null to avoid null pointer (#685)

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -90,7 +90,7 @@ export class NavbarComponent implements OnInit {
         this.isNavbarCollapsed = !this.isNavbarCollapsed;
     }
 
-    getImageUrl() {
-        return this.isAuthenticated() ? this.accountService.getImageUrl() : null;
+    getImageUrl(): string | null {
+        return this.accountService.getImageUrl();
     }
 }
