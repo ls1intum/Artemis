fcfc639cfe09c39fe90968346617cc3c994ead4b
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-02 10:12:08 +0200

    streamline exercise hint ts model

--- src/main/webapp/app/entities/exercise-hint/exercise-hint.model.ts
+++ src/main/webapp/app/entities/exercise-hint/exercise-hint.model.ts
@@ -1,5 +1,9 @@
 import { Exercise } from 'app/entities/exercise';
+import { BaseEntity } from 'app/shared';
 
-export class ExerciseHint {
-    constructor(public id?: number, public title?: string, public content?: string, public exercise?: Exercise) {}
+export class ExerciseHint implements BaseEntity {
+    id: number;
+    public title: string;
+    public content: string;
+    public exercise: Exercise;
 }
