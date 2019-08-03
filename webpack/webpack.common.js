a076916937961720471c11400949466d997b5fd0
Stephan Krusche <s.krusche@tum.de>
2019-08-03 16:52:18 +0200

    fix unspecified version in production build
    improve README

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -58,7 +58,7 @@ plugins: [
             NODE_ENV: `'${options.env}'`,
             BUILD_TIMESTAMP: `'${new Date().getTime()}'`,
             // APP_VERSION is passed as an environment variable from the Gradle / Maven build tasks.
-            VERSION: `'${process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : utils.parseVersion()}'`,
+            VERSION: `'${process.env.hasOwnProperty('APP_VERSION') && process.env.APP_VERSION !== "unspecified" ? process.env.APP_VERSION : utils.parseVersion()}'`,
             DEBUG_INFO_ENABLED: options.env === 'development',
             // The root URL for API calls, ending with a '/' - for example: `"https://www.jhipster.tech:8081/myservice/"`.
             // If this URL is left empty (""), then it will be relative to the current context.
