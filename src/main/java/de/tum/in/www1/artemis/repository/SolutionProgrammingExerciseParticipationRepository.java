0db7c9ac6f6d1fb1589229114301c329098a9584
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-08 17:35:13 +0200

    remove unnecessary changes

--- src/main/java/de/tum/in/www1/artemis/repository/SolutionProgrammingExerciseParticipationRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/SolutionProgrammingExerciseParticipationRepository.java
@@ -2,10 +2,7 @@ package de.tum.in.www1.artemis.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.SolutionProgrammingExerciseParticipation;
@@ -17,9 +14,5 @@ import de.tum.in.www1.artemis.domain.SolutionProgrammingExerciseParticipation;
 @Repository
 public interface SolutionProgrammingExerciseParticipationRepository extends JpaRepository<SolutionProgrammingExerciseParticipation, Long> {
 
-    @EntityGraph(attributePaths = { "results" })
-    @Query("select p from SolutionProgrammingExerciseParticipation p where p.buildPlanId = :#{#buildPlanId}")
-    Optional<SolutionProgrammingExerciseParticipation> findByBuildPlanIdWithResults(@Param("buildPlanId") String buildPlanId);
-
-    SolutionProgrammingExerciseParticipation findByProgrammingExerciseId(Long exerciseId);
+    Optional<SolutionProgrammingExerciseParticipation> findByBuildPlanId(String buildPlanId);
 }
