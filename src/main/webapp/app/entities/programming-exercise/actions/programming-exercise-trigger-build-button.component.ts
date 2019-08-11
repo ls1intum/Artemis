26f586c09e44ce0be0ce2ae0edba20fd7ad14f4a
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-11 15:01:30 +0200

    disable trigger button if a build is running

--- src/main/webapp/app/entities/programming-exercise/actions/programming-exercise-trigger-build-button.component.ts
+++ src/main/webapp/app/entities/programming-exercise/actions/programming-exercise-trigger-build-button.component.ts
@@ -1,15 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 import { ProgrammingExerciseParticipationService } from 'app/entities/programming-exercise/services';
 import { ProgrammingSubmissionWebsocketService } from 'app/submission/programming-submission-websocket.service';
+import { hasParticipationChanged } from 'app/entities/participation';
 
 @Component({
     selector: 'jhi-programming-exercise-trigger-build-button',
     templateUrl: './programming-exercise-trigger-build-button.component.html',
 })
-export class ProgrammingExerciseTriggerBuildButtonComponent {
+export class ProgrammingExerciseTriggerBuildButtonComponent implements OnChanges {
     @Input() participationId: number;
 
+    isBuilding: boolean;
+
+    private submissionSubscription: Subscription;
+
     constructor(private participationService: ProgrammingExerciseParticipationService, private submissionService: ProgrammingSubmissionWebsocketService) {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.participationId && this.participationId !== changes.participationId.previousValue) {
+            this.setupSubmissionSubscription();
+        }
+    }
+
+    setupSubmissionSubscription() {
+        if (this.submissionSubscription) {
+            this.submissionSubscription.unsubscribe();
+        }
+
+        this.submissionSubscription = this.submissionService
+            .getLatestPendingSubmission(this.participationId)
+            .pipe(tap(submission => (this.isBuilding = !!submission)))
+            .subscribe();
+    }
 
     triggerBuild() {
         this.participationService.triggerBuild(this.participationId).subscribe();
