788a954d2740b91a0e1f3a9dbca462b4b81bc7d5
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-12 11:23:24 +0200

    finish implementation: - add size of button - check for first result of participation - integrate into exercise row component

--- src/main/webapp/app/entities/programming-exercise/actions/programming-exercise-trigger-build-button.component.ts
+++ src/main/webapp/app/entities/programming-exercise/actions/programming-exercise-trigger-build-button.component.ts
@@ -1,26 +1,62 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { filter, take, tap } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { ProgrammingExerciseParticipationService } from 'app/entities/programming-exercise/services';
 import { ProgrammingSubmissionWebsocketService } from 'app/submission/programming-submission-websocket.service';
-import { hasParticipationChanged } from 'app/entities/participation';
+import { hasParticipationChanged, Participation, ParticipationWebsocketService } from 'app/entities/participation';
 
+export enum ButtonSize {
+    SMALL = 'btn-sm',
+    MEDIUM = 'btn-md',
+    LARGE = 'btn-lg',
+}
+
+/**
+ * Component for triggering a build for the CURRENT submission of the student (does not create a new commit!).
+ * The participation given as input needs to have the results attached as this component checks if there is at least one result.
+ * If there is no result, the button is disabled because this would mean that the student has not made a commit yet.
+ */
 @Component({
     selector: 'jhi-programming-exercise-trigger-build-button',
     templateUrl: './programming-exercise-trigger-build-button.component.html',
 })
-export class ProgrammingExerciseTriggerBuildButtonComponent implements OnChanges {
-    @Input() participationId: number;
+export class ProgrammingExerciseTriggerBuildButtonComponent implements OnChanges, OnDestroy {
+    @Input() participation: Participation;
+    @Input() btnSize = ButtonSize.SMALL;
 
+    participationHasResult: boolean;
     isBuilding: boolean;
 
     private submissionSubscription: Subscription;
+    private resultSubscription: Subscription;
 
-    constructor(private participationService: ProgrammingExerciseParticipationService, private submissionService: ProgrammingSubmissionWebsocketService) {}
+    constructor(
+        private participationService: ProgrammingExerciseParticipationService,
+        private participationWebsocketService: ParticipationWebsocketService,
+        private submissionService: ProgrammingSubmissionWebsocketService,
+    ) {}
 
+    /**
+     * Check if the participation has changed, if so set up the websocket connections.
+     *
+     * @param changes
+     */
     ngOnChanges(changes: SimpleChanges): void {
-        if (this.participationId && this.participationId !== changes.participationId.previousValue) {
+        if (hasParticipationChanged(changes)) {
+            this.participationHasResult = !!(this.participation.results && this.participation.results.length);
+            if (!this.participationHasResult) {
+                this.setupResultSubscription();
+            }
             this.setupSubmissionSubscription();
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.submissionSubscription) {
+            this.submissionSubscription.unsubscribe();
+        }
+        if (this.resultSubscription) {
+            this.resultSubscription.unsubscribe();
         }
     }
 
@@ -32,14 +68,32 @@ export class ProgrammingExerciseTriggerBuildButtonComponent implements OnChanges
         if (this.submissionSubscription) {
             this.submissionSubscription.unsubscribe();
         }
-
         this.submissionSubscription = this.submissionService
-            .getLatestPendingSubmission(this.participationId)
+            .getLatestPendingSubmission(this.participation.id)
             .pipe(tap(submission => (this.isBuilding = !!submission)))
             .subscribe();
     }
 
-    triggerBuild() {
-        this.participationService.triggerBuild(this.participationId).subscribe();
+    /**
+     * Wait for the first result to come in, when it comes in set the boolean flag participationHasResult to true.
+     */
+    setupResultSubscription() {
+        if (this.resultSubscription) {
+            this.resultSubscription.unsubscribe();
+        }
+        this.resultSubscription = this.participationWebsocketService
+            .subscribeForLatestResultOfParticipation(this.participation.id)
+            .pipe(
+                filter(result => !!result),
+                take(1),
+                tap(() => (this.participationHasResult = true)),
+            )
+            .subscribe();
+    }
+
+    triggerBuild(event: any) {
+        // The button might be placed in other elements that have a click listener, so catch the click here.
+        event.stopPropagation();
+        this.participationService.triggerBuild(this.participation.id).subscribe();
     }
 }
