4e6b6920bf32fa724d7dd5e901fd4cd9eb2e64c3
Alexander Ungar <alexander.ungar@tum.de>
2019-08-12 14:07:23 +0200

    Add IntelliJ button component

--- src/main/webapp/app/core/core.module.ts
+++ src/main/webapp/app/core/core.module.ts
@@ -1,15 +1,16 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { DatePipe, registerLocaleData } from '@angular/common';
+import { CommonModule, DatePipe, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Title } from '@angular/platform-browser';
 import locale from '@angular/common/locales/en';
 import { ArTEMiSNotificationModule } from 'app/entities/notification/notification.module';
-import { IntellijDirective } from './intellij/intellij.directive';
+import { IntellijButtonComponent } from './intellij/intellij-button/intellij-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    imports: [HttpClientModule, ArTEMiSNotificationModule],
-    exports: [IntellijDirective],
-    declarations: [IntellijDirective],
+    imports: [HttpClientModule, ArTEMiSNotificationModule, FontAwesomeModule, CommonModule],
+    exports: [IntellijButtonComponent],
+    declarations: [IntellijButtonComponent],
     providers: [
         Title,
         {
---
+++ src/main/webapp/app/core/intellij/intellij-button/intellij-button.component.ts
@@ -0,0 +1,27 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { JavaBridgeService } from 'app/core/intellij/java-bridge.service';
+
+@Component({
+    /* tslint:disable-next-line component-selector */
+    selector: 'button[jhi-intellij-button]',
+    templateUrl: './intellij-button.component.html',
+    styleUrls: ['./intellij-button.component.scss'],
+})
+export class IntellijButtonComponent {
+    @Input() buttonLabel: string;
+    @Input() buttonLoading = false;
+    @HostBinding('class.btn-outline-primary') @Input() outlined = false;
+    @HostBinding('class.btn-sm') @Input() smallButton = false;
+    @HostBinding('class.btn') isButton = true;
+
+    javaBridge: JavaBridgeService;
+
+    constructor(javaBridge: JavaBridgeService) {
+        this.javaBridge = javaBridge;
+    }
+
+    @HostBinding('class.btn-primary')
+    public get btnPrimary(): boolean {
+        return !this.outlined;
+    }
+}
