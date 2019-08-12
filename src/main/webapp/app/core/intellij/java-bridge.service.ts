ce590e18a82f0536cc87cf9000fecc4017d4df1c
Alexander Ungar <alexander.ungar@tum.de>
2019-08-12 11:31:06 +0200

    Fix window calls to Java bridge

--- src/main/webapp/app/core/intellij/java-bridge.service.ts
+++ src/main/webapp/app/core/intellij/java-bridge.service.ts
@@ -7,19 +7,16 @@ export class JavaBridgeService {
     constructor() {}
 
     login(username: String, password: String) {
-        this.intellij().login(username, password);
+        // @ts-ignore
+        window.intellij.login(username, password);
     }
 
     clone(repository: String, exercise: String) {
-        this.intellij().clone(repository, exercise);
+        // @ts-ignore
+        window.intellij.clone(repository, exercise);
     }
 
     isIntelliJ(): boolean {
         return window.navigator.userAgent.includes('IntelliJ');
-    }
-
-    private intellij() {
-        // @ts-ignore
-        return window.intellij;
     }
 }
