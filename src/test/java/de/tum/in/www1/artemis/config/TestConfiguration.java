f58548a8b2658576ae3a15c55e7ceb2dfb083ed0
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-13 20:20:04 +0200

    move cache configuration to test repository

--- src/test/java/de/tum/in/www1/artemis/config/TestConfiguration.java
+++ src/test/java/de/tum/in/www1/artemis/config/TestConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @EnableCaching
 @Profile("test")
-public class TestCacheConfiguration {
+public class TestConfiguration {
 
     @Bean
     public CacheManager getCache() {
