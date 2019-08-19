279decb11ca698e43a4ff38fadfe7ddaab4b7b30
Anh Montag <anh.montag@tum.de>
2019-08-20 00:58:22 +0200

    Implement feedback

--- src/main/webapp/app/shared/icons/icons.module.ts
+++ src/main/webapp/app/shared/icons/icons.module.ts
@@ -1,10 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgJhipsterModule } from 'ng-jhipster';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { CookieModule } from 'ngx-cookie';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import {
