a878eb0605cd5cf4f09fa89a92913752a156577f
Stephan Krusche <s.krusche@tum.de>
2019-08-19 11:47:27 +0200

    reactivate user management
    it works again without changes ;-)

--- src/main/webapp/app/admin/admin.route.ts
+++ src/main/webapp/app/admin/admin.route.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core';
-import { auditsRoute, configurationRoute, healthRoute, logsRoute, metricsRoute, trackerRoute, notificationMgmtRoute } from './';
+import { auditsRoute, configurationRoute, healthRoute, logsRoute, metricsRoute, trackerRoute, userMgmtRoute, notificationMgmtRoute } from './';
 
-const ADMIN_ROUTES = [auditsRoute, configurationRoute, healthRoute, logsRoute, trackerRoute, ...notificationMgmtRoute, metricsRoute];
+const ADMIN_ROUTES = [auditsRoute, configurationRoute, healthRoute, logsRoute, trackerRoute, ...notificationMgmtRoute, ...userMgmtRoute, metricsRoute];
 
 export const adminState: Routes = [
     {
