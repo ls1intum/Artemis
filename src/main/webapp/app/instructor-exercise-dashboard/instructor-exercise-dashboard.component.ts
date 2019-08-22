772f13a7c54193964d958fe810ba5301a099e1e3
Gregor <gregor@trum.eu>
2019-08-22 13:41:22 +0200

    Added Client Tests

--- src/main/webapp/app/instructor-exercise-dashboard/instructor-exercise-dashboard.component.ts
+++ src/main/webapp/app/instructor-exercise-dashboard/instructor-exercise-dashboard.component.ts
@@ -38,7 +38,20 @@ export class InstructorExerciseDashboardComponent implements OnInit {
     back() {
         this.router.navigate([`/course/${this.courseId}/instructor-dashboard`]);
     }
+    public setStatistics() {
+        if (this.stats.numberOfSubmissions > 0) {
+            this.totalManualAssessmentPercentage = Math.round(
+                ((this.stats.numberOfAssessments - this.stats.numberOfAutomaticAssistedAssessments) / this.stats.numberOfSubmissions) * 100,
+            );
+            this.totalAutomaticAssessmentPercentage = Math.round((this.stats.numberOfAutomaticAssistedAssessments / this.stats.numberOfSubmissions) * 100);
+        }
 
+        this.dataForAssessmentPieChart = [
+            this.stats.numberOfSubmissions - this.stats.numberOfAssessments,
+            this.stats.numberOfAssessments - this.stats.numberOfAutomaticAssistedAssessments,
+            this.stats.numberOfAutomaticAssistedAssessments,
+        ];
+    }
     private loadExercise(exerciseId: number) {
         this.exerciseService
             .find(exerciseId)
@@ -47,19 +60,7 @@ export class InstructorExerciseDashboardComponent implements OnInit {
         this.exerciseService.getStatsForInstructors(exerciseId).subscribe(
             (res: HttpResponse<StatsForDashboard>) => {
                 this.stats = Object.assign({}, this.stats, res.body);
-
-                if (this.stats.numberOfSubmissions > 0) {
-                    this.totalManualAssessmentPercentage = Math.round(
-                        ((this.stats.numberOfAssessments - this.stats.numberOfAutomaticAssistedAssessments) / this.stats.numberOfSubmissions) * 100,
-                    );
-                    this.totalAutomaticAssessmentPercentage = Math.round((this.stats.numberOfAutomaticAssistedAssessments / this.stats.numberOfSubmissions) * 100);
-                }
-
-                this.dataForAssessmentPieChart = [
-                    this.stats.numberOfSubmissions - this.stats.numberOfAssessments,
-                    this.stats.numberOfAssessments - this.stats.numberOfAutomaticAssistedAssessments,
-                    this.stats.numberOfAutomaticAssistedAssessments,
-                ];
+                this.setStatistics();
             },
             (response: string) => this.onError(response),
         );
---
+++ src/test/javascript/spec/component/instructor-dashboard/instructor-exercise-dashboard.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { ArtemisTestModule } from '../../test.module';
+import { MockComponent } from 'ng-mocks';
+import { ArtemisSharedModule } from 'app/shared';
+import { RouterModule } from '@angular/router';
+import { HeaderExercisePageWithDetailsComponent } from 'app/exercise-headers';
+import { SidePanelComponent } from 'app/components/side-panel/side-panel.component';
+import { TutorLeaderboardComponent } from 'app/instructor-course-dashboard/tutor-leaderboard/tutor-leaderboard.component';
+import { InstructorExerciseDashboardComponent } from 'app/instructor-exercise-dashboard';
+import { StatsForDashboard } from 'app/instructor-course-dashboard/stats-for-dashboard.model';
+import { ChartsModule } from 'ng2-charts';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('InstructorExerciseDashboardComponent', () => {
+    let comp: InstructorExerciseDashboardComponent;
+    let fixture: ComponentFixture<InstructorExerciseDashboardComponent>;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule, ArtemisSharedModule, RouterModule, ChartsModule],
+            declarations: [
+                InstructorExerciseDashboardComponent,
+                MockComponent(HeaderExercisePageWithDetailsComponent),
+                MockComponent(SidePanelComponent),
+                MockComponent(TutorLeaderboardComponent),
+            ],
+            providers: [],
+        });
+        fixture = TestBed.createComponent(InstructorExerciseDashboardComponent);
+        comp = fixture.componentInstance;
+    });
+
+    it('Statistics are calculated correctly', () => {
+        const stats = new StatsForDashboard();
+        stats.numberOfSubmissions = 420;
+        stats.numberOfAssessments = 333;
+        stats.numberOfAutomaticAssistedAssessments = 42;
+        comp.stats = stats;
+        comp.setStatistics();
+        expect(comp.totalManualAssessmentPercentage).equal(69);
+        expect(comp.totalAutomaticAssessmentPercentage).equal(10);
+        expect(comp.dataForAssessmentPieChart[0]).equal(87);
+        expect(comp.dataForAssessmentPieChart[1]).equal(291);
+        expect(comp.dataForAssessmentPieChart[2]).equal(42);
+    });
+});
