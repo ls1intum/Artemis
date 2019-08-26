0e0aeda848fa419ca586df86eac20e106403c9b1
Alexander Ungar <alexander.ungar@tum.de>
2019-08-26 17:17:39 +0200

    Fix intellij module imports

--- src/main/webapp/app/intellij/intellij.module.ts
+++ src/main/webapp/app/intellij/intellij.module.ts
@@ -2,7 +2,6 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntellijButtonComponent } from 'app/intellij/intellij-button/intellij-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IdeProgrammingExerciseDetailsComponent } from './ide-programming-exercise-details/ide-programming-exercise-details.component';
 import { ArtemisProgrammingExerciseModule } from 'app/entities/programming-exercise/programming-exercise.module';
 import { ArtemisHeaderExercisePageWithDetailsModule } from 'app/exercise-headers';
 import { ArtemisSharedModule } from 'app/shared';
@@ -15,7 +14,7 @@ export function initJavaBridge(bridge: JavaBridgeService) {
 }
 
 @NgModule({
-    declarations: [IntellijButtonComponent, IdeProgrammingExerciseDetailsComponent],
+    declarations: [IntellijButtonComponent],
     imports: [
         CommonModule,
         FontAwesomeModule,
