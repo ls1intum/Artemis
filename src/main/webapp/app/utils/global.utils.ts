5aa1b6c45bc88c6e4f91f3eec1dba135a7f27b5f
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-08-26 13:02:40 +0200

    write util tests and fix bugs in util

--- src/main/webapp/app/utils/global.utils.ts
+++ src/main/webapp/app/utils/global.utils.ts
@@ -49,11 +49,17 @@ export const getStringSegmentPositions = (stringToSegment: string, delimiter: st
 export type RegExpLineNumberMatchArray = Array<[number, string]>;
 /**
  * Executes a regex on a multi line text ("text \n more text") and returns the [lineNumber of the match, matched object] in a tuple.
+ * The given regex must have the global flag, otherwise the multiline match will cause an out of memory exception.
  *
  * @param multiLineText in which to search for matches of the regex.
  * @param regex RegExp object.
+ * @return the matches found in the multiline string.
+ * @throws Error if a regex is provided without a global flag.
  */
 export const matchRegexWithLineNumbers = (multiLineText: string, regex: RegExp): RegExpLineNumberMatchArray => {
+    if (!regex.flags.includes('g')) {
+        throw new Error('Regex must contain global flag, otherwise this function will run out of memory.');
+    }
     const result: RegExpLineNumberMatchArray = [];
     let match = regex.exec(multiLineText);
     while (match) {
