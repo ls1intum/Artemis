4a9a592887ec22b801872e64a9fa6ce6608e29c6
Jan Philip Bernius <janphilip.bernius@tum.de>
2019-08-28 17:54:49 +0200

    Add Documentation with purpose of the component.

--- src/main/webapp/app/assessment-shared/assessment-header/assessment-header.component.ts
+++ src/main/webapp/app/assessment-shared/assessment-header/assessment-header.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Result } from 'app/entities/result';
 
+/**
+ * The <jhi-assessment-header> component is shared between the modeling and text assessment interfaces.
+ * It displays a header bar above the assessment editor with information of locking, as well as offering save/submit/etc buttons.
+ * This guarantees a unified look and feel for both interfaces.
+ * Depending Components need to perform actions based on the save/submit/cancel/resolveConflict/nextSubmission/goBack outputs.
+ */
 @Component({
     selector: 'jhi-assessment-header',
     templateUrl: './assessment-header.component.html',
