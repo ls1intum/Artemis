ce205899820cbeb6507e6f936e212b0d82ac8ff6
ge68his <ge68his@mytum.de>
2019-08-29 22:50:57 +0200

    removed unused constants

--- src/main/webapp/app/entities/participation-submission/participation-submission-delete-dialog.component.ts
+++ src/main/webapp/app/entities/participation-submission/participation-submission-delete-dialog.component.ts
@@ -4,10 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
-import { ExerciseType } from '../exercise';
-
 import { Subscription } from 'rxjs/Subscription';
-import { Submission } from 'app/entities/submission';
 import { ParticipationSubmissionPopupService } from 'app/entities/participation-submission/participation-submission-popup.service';
 import { SubmissionService } from 'app/entities/submission/submission.service';
 
@@ -16,10 +13,6 @@ import { SubmissionService } from 'app/entities/submission/submission.service';
     templateUrl: './participation-submission-delete-dialog.component.html',
 })
 export class ParticipationSubmissionDeleteDialogComponent implements OnInit {
-    // make constants available to html for comparison
-    readonly QUIZ = ExerciseType.QUIZ;
-    readonly PROGRAMMING = ExerciseType.PROGRAMMING;
-    readonly MODELING = ExerciseType.MODELING;
     submissionId: number;
 
     constructor(private submissionService: SubmissionService, public activeModal: NgbActiveModal, private eventManager: JhiEventManager) {}
