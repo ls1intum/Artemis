076534005e68118fa2dbe3488daea23ff31c781b
maxr96 <S1a5l6i3v2a>
2019-08-30 22:28:10 +0200

    Rolled back resolver changes

--- src/main/webapp/app/entities/file-upload-exercise/file-upload-exercise.route.ts
+++ src/main/webapp/app/entities/file-upload-exercise/file-upload-exercise.route.ts
@@ -1,9 +1,29 @@
-import { Routes } from '@angular/router';
+import { Routes, Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 
 import { UserRouteAccessService } from '../../core';
 import { FileUploadExerciseComponent } from './file-upload-exercise.component';
 import { FileUploadExerciseDetailComponent } from './file-upload-exercise-detail.component';
+
+import { FileUploadExercise } from 'app/entities/file-upload-exercise/file-upload-exercise.model';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { FileUploadExerciseService } from 'app/entities/file-upload-exercise/file-upload-exercise.service';
 import { FileUploadExerciseUpdateComponent } from 'app/entities/file-upload-exercise/file-upload-exercise-update.component';
+
+@Injectable({ providedIn: 'root' })
+export class FileUploadExerciseResolve implements Resolve<FileUploadExercise> {
+    constructor(private service: FileUploadExerciseService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const id = route.params['id'] ? route.params['id'] : null;
+        if (id) {
+            return this.service.find(id).pipe(map((fileUploadExercise: HttpResponse<FileUploadExercise>) => fileUploadExercise.body!));
+        }
+        return Observable.of(new FileUploadExercise());
+    }
+}
 
 export const fileUploadExerciseRoute: Routes = [
     {
@@ -27,6 +47,9 @@ export const fileUploadExerciseRoute: Routes = [
     {
         path: 'course/:courseId/file-upload-exercise/new',
         component: FileUploadExerciseUpdateComponent,
+        resolve: {
+            fileUploadExercise: FileUploadExerciseResolve,
+        },
         data: {
             authorities: ['ROLE_TA', 'ROLE_INSTRUCTOR', 'ROLE_ADMIN'],
             pageTitle: 'artemisApp.fileUploadExercise.home.title',
@@ -36,6 +59,9 @@ export const fileUploadExerciseRoute: Routes = [
     {
         path: 'file-upload-exercise/:exerciseId/edit',
         component: FileUploadExerciseUpdateComponent,
+        resolve: {
+            fileUploadExercise: FileUploadExerciseResolve,
+        },
         data: {
             authorities: ['ROLE_TA', 'ROLE_INSTRUCTOR', 'ROLE_ADMIN'],
             pageTitle: 'artemisApp.fileUploadExercise.home.title',
