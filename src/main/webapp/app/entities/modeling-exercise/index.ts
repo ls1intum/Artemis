1a69106fef9f319f2526bf156f320c1e5bde65f8
maxr96 <S1a5l6i3v2a>
2019-09-03 16:00:51 +0200

    Completely removed ModelingExercisePopupService

--- src/main/webapp/app/entities/modeling-exercise/index.ts
+++ src/main/webapp/app/entities/modeling-exercise/index.ts
@@ -1,5 +1,4 @@
 export * from './modeling-exercise.model';
-export * from './modeling-exercise-popup.service';
 export * from './modeling-exercise.service';
 export * from './modeling-exercise-detail.component';
 export * from './modeling-exercise.component';
--- src/main/webapp/app/entities/modeling-exercise/modeling-exercise.module.ts
+++ src/main/webapp/app/entities/modeling-exercise/modeling-exercise.module.ts
@@ -4,14 +4,7 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { JhiLanguageHelper } from 'app/core';
 
 import { ArtemisSharedModule } from 'app/shared';
-import {
-    ModelingExerciseComponent,
-    ModelingExerciseDetailComponent,
-    ModelingExerciseUpdateComponent,
-    ModelingExercisePopupService,
-    modelingExerciseRoute,
-    ModelingExerciseService,
-} from './';
+import { ModelingExerciseComponent, ModelingExerciseDetailComponent, ModelingExerciseUpdateComponent, modelingExerciseRoute, ModelingExerciseService } from './';
 import { SortByModule } from 'app/components/pipes';
 import { FormDateTimePickerModule } from 'app/shared/date-time-picker/date-time-picker.module';
 import { ArtemisMarkdownEditorModule } from 'app/markdown-editor';
@@ -35,7 +28,7 @@ const ENTITY_STATES = [...modelingExerciseRoute];
     ],
     declarations: [ModelingExerciseComponent, ModelingExerciseDetailComponent, ModelingExerciseUpdateComponent],
     entryComponents: [ModelingExerciseComponent, DeleteDialogComponent],
-    providers: [ModelingExerciseService, ModelingExercisePopupService, { provide: JhiLanguageService, useClass: JhiLanguageService }],
+    providers: [ModelingExerciseService, { provide: JhiLanguageService, useClass: JhiLanguageService }],
     exports: [ModelingExerciseComponent],
 })
 export class ArtemisModelingExerciseModule {
