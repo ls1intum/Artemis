9fb3854da04435a45440f068632f6ca1b5a6ebc4
maxr96 <maxr96@gmail.com>
2019-09-03 14:25:51 +0200

    Create and Manage File Upload Exercise (#774)

--- src/main/webapp/app/entities/file-upload-exercise/file-upload-exercise-detail.component.ts
+++ src/main/webapp/app/entities/file-upload-exercise/file-upload-exercise-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpResponse } from '@angular/common/http';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager } from 'ng-jhipster';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 import { FileUploadExercise } from './file-upload-exercise.model';
 import { FileUploadExerciseService } from './file-upload-exercise.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-file-upload-exercise-detail',
@@ -16,30 +17,62 @@ export class FileUploadExerciseDetailComponent implements OnInit, OnDestroy {
     private subscription: Subscription;
     private eventSubscriber: Subscription;
 
-    constructor(private eventManager: JhiEventManager, private fileUploadExerciseService: FileUploadExerciseService, private route: ActivatedRoute) {}
+    constructor(
+        private eventManager: JhiEventManager,
+        private fileUploadExerciseService: FileUploadExerciseService,
+        private route: ActivatedRoute,
+        private jhiAlertService: JhiAlertService,
+    ) {}
 
+    /**
+     * Initializes subscription for file upload exercise
+     */
     ngOnInit() {
         this.subscription = this.route.params.subscribe(params => {
-            this.load(params['id']);
+            this.load(params['exerciseId']);
         });
         this.registerChangeInFileUploadExercises();
     }
 
-    load(id: number) {
-        this.fileUploadExerciseService.find(id).subscribe((fileUploadExerciseResponse: HttpResponse<FileUploadExercise>) => {
-            this.fileUploadExercise = fileUploadExerciseResponse.body!;
-        });
+    /**
+     * Loads file upload exercise from the server
+     * @param exerciseId the id of the file upload exercise
+     */
+    load(exerciseId: number) {
+        this.fileUploadExerciseService
+            .find(exerciseId)
+            .pipe(filter(res => !!res.body))
+            .subscribe(
+                (fileUploadExerciseResponse: HttpResponse<FileUploadExercise>) => {
+                    this.fileUploadExercise = fileUploadExerciseResponse.body!;
+                },
+                (res: HttpErrorResponse) => this.onError(res),
+            );
     }
+
+    /**
+     * Returns to previous state (same behaviour as back button in the browser)
+     */
     previousState() {
         window.history.back();
     }
 
+    /**
+     * Unsubscribes on component destruction
+     */
     ngOnDestroy() {
         this.subscription.unsubscribe();
         this.eventManager.destroy(this.eventSubscriber);
     }
 
+    /**
+     * Listens to file upload exercise list modifications
+     */
     registerChangeInFileUploadExercises() {
         this.eventSubscriber = this.eventManager.subscribe('fileUploadExerciseListModification', () => this.load(this.fileUploadExercise.id));
+    }
+
+    private onError(error: HttpErrorResponse) {
+        this.jhiAlertService.error(error.message);
     }
 }
