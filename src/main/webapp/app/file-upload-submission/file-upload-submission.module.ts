d66601377e8d44b414ed9ed2f5b845f929f7a30f
maxr96 <Simpsons2>
2019-09-06 19:50:42 +0200

    Removed FileUploadResult, as it will be introduced only with Assessment PR

--- src/main/webapp/app/file-upload-submission/file-upload-submission.module.ts
+++ src/main/webapp/app/file-upload-submission/file-upload-submission.module.ts
@@ -1,24 +1,17 @@
-/* angular */
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-/* application */
 import { JhiLanguageHelper } from 'app/core';
 import { ArtemisSharedModule } from 'app/shared';
 import { ArtemisResultModule } from 'app/entities/result';
 import { fileUploadSubmissionRoute } from './file-upload-submission.route';
 import { FileUploadSubmissionComponent } from './file-upload-submission.component';
 import { ArtemisComplaintsModule } from 'app/complaints';
-import { FileUploadResultComponent } from 'app/file-upload-submission/file-upload-result/file-upload-result.component';
-
-/* 3rd party */
 import { JhiLanguageService } from 'ng-jhipster';
 
 const ENTITY_STATES = [...fileUploadSubmissionRoute];
 
 @NgModule({
-    declarations: [FileUploadSubmissionComponent, FileUploadResultComponent],
-    entryComponents: [FileUploadSubmissionComponent],
+    declarations: [FileUploadSubmissionComponent],
     imports: [ArtemisSharedModule, RouterModule.forChild(ENTITY_STATES), ArtemisResultModule, ArtemisComplaintsModule],
     providers: [{ provide: JhiLanguageService, useClass: JhiLanguageService }],
 })
