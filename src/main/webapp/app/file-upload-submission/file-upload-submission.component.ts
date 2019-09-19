a23ba7ff0b2b036449aacd37e1194e295bc3a005
maxr96 <maxr96@gmail.com>
2019-09-19 22:57:57 +0200

    Proper error if file size is too big

--- src/main/webapp/app/file-upload-submission/file-upload-submission.component.ts
+++ src/main/webapp/app/file-upload-submission/file-upload-submission.component.ts
@@ -148,7 +148,7 @@ export class FileUploadSubmissionComponent implements OnInit, ComponentCanDeacti
             if (!allowedFileExtensions.some(extension => submissionFile.name.toLowerCase().endsWith(extension))) {
                 this.jhiAlertService.error('artemisApp.fileUploadSubmission.fileExtensionError', null, undefined);
             } else if (submissionFile.size > MAX_SUBMISSION_FILE_SIZE) {
-                this.jhiAlertService.error('artemisApp.fileUpload.fileError', { fileName: this.submissionFile!['name'] });
+                this.jhiAlertService.error('artemisApp.fileUploadSubmission.fileError', { fileName: submissionFile['name'] });
             } else {
                 this.submissionFile = submissionFile;
             }
