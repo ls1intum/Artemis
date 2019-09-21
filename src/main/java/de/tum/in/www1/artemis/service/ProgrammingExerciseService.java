d530dff88cbefef4eb9fe905229e9d4aa7c34b77
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-09-21 17:06:02 +0200

    Revert "fix broken update endpoint"
    This reverts commit c8b7d30b65a97c2473164799e8160661b974699a.

--- src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseService.java
@@ -602,6 +602,19 @@ public class ProgrammingExerciseService {
     }
 
     /**
+     * This method saves the template and solution participations of the programming exercise
+     *
+     * @param programmingExercise The programming exercise for which the participations should get saved
+     */
+    public void saveParticipations(ProgrammingExercise programmingExercise) {
+        SolutionProgrammingExerciseParticipation solutionParticipation = programmingExercise.getSolutionParticipation();
+        TemplateProgrammingExerciseParticipation templateParticipation = programmingExercise.getTemplateParticipation();
+
+        solutionProgrammingExerciseParticipationRepository.save(solutionParticipation);
+        templateProgrammingExerciseParticipationRepository.save(templateParticipation);
+    }
+
+    /**
      * Squash all commits of the given repository into one.
      * 
      * @param repoUrl of the repository to squash.
