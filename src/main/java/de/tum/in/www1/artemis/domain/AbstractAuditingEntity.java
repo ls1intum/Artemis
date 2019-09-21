acc6d3033434c9b972499b3901d18b539eaaa5a0
Stephan Krusche <s.krusche@tum.de>
2019-09-21 08:55:36 +0200

    update to jhipster 6.3.1

--- src/main/java/de/tum/in/www1/artemis/domain/AbstractAuditingEntity.java
+++ src/main/java/de/tum/in/www1/artemis/domain/AbstractAuditingEntity.java
@@ -7,7 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
-import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -20,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * Base abstract class for entities which will hold definitions for created, last modified by and created, last modified by date.
  */
 @MappedSuperclass
-@Audited
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractAuditingEntity implements Serializable {
 
