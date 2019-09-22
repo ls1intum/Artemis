60eb5f66e61d629c7db7a0d8b70b74de4024c120
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-09-22 14:39:34 +0200

    restore dep update

--- src/main/java/de/tum/in/www1/artemis/service/AuditEventService.java
+++ src/main/java/de/tum/in/www1/artemis/service/AuditEventService.java
@@ -1,6 +1,7 @@
 package de.tum.in.www1.artemis.service;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,6 +39,20 @@ public class AuditEventService {
         this.persistenceAuditEventRepository = persistenceAuditEventRepository;
         this.auditEventConverter = auditEventConverter;
         this.jHipsterProperties = jhipsterProperties;
+    }
+
+    /**
+     * Old audit events should be automatically deleted after 30 days.
+     *
+     * This is scheduled to get fired at 12:00 (am).
+     */
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void removeOldAuditEvents() {
+        persistenceAuditEventRepository.findByAuditEventDateBefore(Instant.now().minus(jHipsterProperties.getAuditEvents().getRetentionPeriod(), ChronoUnit.DAYS))
+                .forEach(auditEvent -> {
+                    log.debug("Deleting audit data {}", auditEvent);
+                    persistenceAuditEventRepository.delete(auditEvent);
+                });
     }
 
     public Page<AuditEvent> findAll(Pageable pageable) {
