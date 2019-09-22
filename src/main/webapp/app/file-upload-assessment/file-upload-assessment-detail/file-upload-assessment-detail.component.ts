ea8a9c0bc0aa66d8d78d96eaad985a5ad61b4d8f
maxr96 <maxr96@gmail.com>
2019-09-22 17:32:33 +0200

    Added File Upload Assessment

---
+++ src/main/webapp/app/file-upload-assessment/file-upload-assessment-detail/file-upload-assessment-detail.component.ts
@@ -0,0 +1,33 @@
+/* angular */
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+/* application */
+import { Feedback } from 'app/entities/feedback';
+
+/* 3rd party */
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+    selector: 'jhi-file-upload-assessment-detail',
+    templateUrl: './file-upload-assessment-detail.component.html',
+    styleUrls: [],
+})
+export class FileUploadAssessmentDetailComponent {
+    @Input() public index: number;
+    @Input() public assessment: Feedback;
+    @Output() public assessmentChange = new EventEmitter<Feedback>();
+    @Output() public deleteAssessment = new EventEmitter<Feedback>();
+    @Input() public disabled = false;
+
+    constructor(private translateService: TranslateService) {}
+    public emitChanges(): void {
+        this.assessmentChange.emit(this.assessment);
+    }
+
+    public delete() {
+        const confirmation = confirm(this.translateService.instant('artemisApp.fileUploadAssessment.deleteAssessment', { index: this.index }));
+        if (confirmation) {
+            this.deleteAssessment.emit(this.assessment);
+        }
+    }
+}
