b38f11ccc79631c0684c76c844ce56428288002a
Stephan Krusche <krusche@in.tum.de>
2019-09-26 14:48:37 +0200

    Change landing page for LTI users to new exercise details (#857)
    * change landing page for LTI users to exercise details
    - remove old courses list
    - remove old exercise list
    - remove old course score calculation component
    - remove course list module and routes
    - move course score calculation service into overview module and fix some compilation errors in tests

--- src/main/webapp/app/course-list/exercise-list/participation-data-provider.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Participation } from '../../entities/participation';
-
-@Injectable({ providedIn: 'root' })
-export class ParticipationDataProvider {
-    public participationStorage: Participation;
-
-    public constructor() {}
-}
