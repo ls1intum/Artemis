ba952146fbb862b6005dcdb88c5633597fa0eba4
Anh Montag <anh.montag@tum.de>
2019-09-27 21:52:14 +0200

    Add comment

--- src/main/webapp/app/guided-tour/guided-tour.component.ts
+++ src/main/webapp/app/guided-tour/guided-tour.component.ts
@@ -380,6 +380,8 @@ export class GuidedTourComponent implements AfterViewInit, OnDestroy {
             return null;
         }
         const selectedElement = document.querySelector(this.currentTourStep.highlightSelector) as HTMLElement;
+
+        // Workaround for instruction elements in the code-editor view, since the element can be in the viewport but hidden by the build output div
         const instructions = selectedElement.closest('.instructions__content__markdown');
         if (instructions && instructions.scrollHeight > window.innerHeight && instructions.querySelector(this.currentTourStep.highlightSelector)) {
             selectedElement.scrollIntoView({ block: 'center' });
