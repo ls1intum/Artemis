99ef8301ca36bf05e5bc884ba361d56b6ee27bc2
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-09-27 11:49:37 +0200

    fix broken client tests

--- src/test/javascript/spec/mocks/mock-programming-exercise.service.ts
+++ src/test/javascript/spec/mocks/mock-programming-exercise.service.ts
@@ -3,5 +3,5 @@ import { of } from 'rxjs';
 export class MockProgrammingExerciseService {
     updateProblemStatement = (exerciseId: number, problemStatement: string) => of();
     findWithTemplateAndSolutionParticipation = (exerciseId: number) => of();
-    getReleaseState = (exerciseId: number) => of({ body: { released: true, hasStudentResult: true, testCasesChanged: false } });
+    getProgrammingExerciseTestCaseState = (exerciseId: number) => of({ body: { released: true, hasStudentResult: true, testCasesChanged: false } });
 }
