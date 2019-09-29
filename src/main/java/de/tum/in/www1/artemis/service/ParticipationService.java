d8cf78e7e8ed1fd48b6362eeb4f800e80292f49d
maxr96 <maxr96@gmail.com>
2019-09-29 17:47:52 +0200

    Fixed fileUploadExercise unable to delete

--- src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
@@ -842,8 +842,8 @@ public class ParticipationService {
             conflictService.deleteAllConflictsForParticipation(participation);
         }
 
-        if (participation.getExercise() instanceof ModelingExercise || participation.getExercise() instanceof TextExercise) {
-            // For modeling and text exercises students can send complaints about their assessments and we need to remove
+        if (participation.getExercise() instanceof ModelingExercise || participation.getExercise() instanceof TextExercise || participation.getExercise() instanceof FileUploadExercise) {
+            // For modeling, text and file upload exercises students can send complaints about their assessments and we need to remove
             // the complaints and the according responses belonging to a participation before deleting the participation itself.
             complaintResponseRepository.deleteByComplaint_Result_Participation_Id(participationId);
             complaintRepository.deleteByResult_Participation_Id(participationId);
