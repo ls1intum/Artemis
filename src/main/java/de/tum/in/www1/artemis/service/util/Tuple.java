4f23248c898c5825a56353a78d2fb2444d98c03f
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-09-29 13:06:34 +0200

    bugfixing for tuple equals and test update

--- src/main/java/de/tum/in/www1/artemis/service/util/Tuple.java
+++ src/main/java/de/tum/in/www1/artemis/service/util/Tuple.java
@@ -1,5 +1,7 @@
 package de.tum.in.www1.artemis.service.util;
 
+import java.util.Objects;
+
 /**
  * Immutable tuple object.
  * @param <X> first param.
@@ -14,5 +16,26 @@ public class Tuple<X, Y> {
     public Tuple(X x, Y y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * Two Tuple objects are considered equal if x1 and x2, y1 and y2 are equal.
+     *
+     * @param o Tuple
+     * @return true if the Tuple objects are equal.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Tuple<?, ?> tuple = (Tuple<?, ?>) o;
+        return Objects.equals(x, tuple.x) && Objects.equals(y, tuple.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
