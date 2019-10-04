b0cf0cf12c6f9d8c104d36c67ecf627472240c90
maxr96 <maxr96@gmail.com>
2019-10-04 19:23:37 +0200

    File Upload Exercise Assessment (#841)

--- src/main/java/de/tum/in/www1/artemis/web/rest/CourseResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/CourseResource.java
@@ -525,6 +525,7 @@ public class CourseResource {
 
         long numberOfSubmissions = textSubmissionService.countSubmissionsToAssessByCourseId(courseId);
         numberOfSubmissions += modelingSubmissionService.countSubmissionsToAssessByCourseId(courseId);
+        numberOfSubmissions += fileUploadSubmissionService.countSubmissionsToAssessByCourseId(courseId);
 
         stats.setNumberOfSubmissions(numberOfSubmissions);
         stats.setNumberOfAssessments(resultService.countNumberOfAssessments(courseId));
