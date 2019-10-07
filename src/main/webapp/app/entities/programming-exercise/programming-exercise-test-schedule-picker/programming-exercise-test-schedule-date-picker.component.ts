43610b2a202e2ff7eb1e51a0817fe909d56ffdcf
Alexander Ungar <alexander.ungar@tum.de>
2019-10-07 19:17:25 +0200

    Implement PR suggestions
    Fix translations
    Add press ENTER hint to categories
    Move preview html to separate file
    Move date picker html to separate file
    Use speaking variable name in date picker

--- src/main/webapp/app/entities/programming-exercise/programming-exercise-test-schedule-picker/programming-exercise-test-schedule-date-picker.component.ts
+++ src/main/webapp/app/entities/programming-exercise/programming-exercise-test-schedule-picker/programming-exercise-test-schedule-date-picker.component.ts
@@ -4,32 +4,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'jhi-programming-exercise-test-schedule-date-picker',
-    template: `
-        <div>
-            <div>
-                <span class="font-weight-bold" [jhiTranslate]="label"></span>
-                <jhi-help-icon *ngIf="tooltipText" placement="top" [text]="tooltipText"></jhi-help-icon>
-            </div>
-            <div class="invisible-date-time-picker">
-                <input class="form-control" [ngModel]="val" [min]="min" [max]="max" (ngModelChange)="updateField($event)" [owlDateTime]="dt" />
-            </div>
-            <button *ngIf="!val" [owlDateTimeTrigger]="dt" type="button" class="btn btn-light btn-lifecycle" [class.btn-lifecycle--deactivated]="!val">
-                <fa-icon class="icon-calendar-plus" icon="calendar-plus" size="2x"></fa-icon>
-            </button>
-            <button *ngIf="val" (click)="resetDate()" type="button" class="btn btn-light btn-lifecycle calendar-event-toggle">
-                <fa-icon class="icon-static" icon="calendar-check" size="2x"></fa-icon>
-                <fa-icon class="icon-remove" icon="calendar-minus" size="2x"></fa-icon>
-            </button>
-            <div *ngIf="val">
-                {{ val | date: 'MMM, dd' }}<br />
-                {{ val | date: 'HH:mm' }}
-            </div>
-            <div *ngIf="!val" class="text-danger" jhiTranslate="artemisApp.programmingExercise.timeline.notSet">
-                not set
-            </div>
-            <owl-date-time [startAt]="startAt" #dt></owl-date-time>
-        </div>
-    `,
+    templateUrl: './programming-exercise-test-schedule-date-picker.component.html',
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
@@ -41,7 +16,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 })
 export class ProgrammingExerciseTestScheduleDatePickerComponent implements ControlValueAccessor {
     @ViewChild('dateInput', { static: false }) dateInput: ElementRef;
-    @Input() val: Date | null;
+    @Input() selectedDate: Date | null;
     @Input() startAt: Moment | null;
     @Input() min: Moment;
     @Input() max: Moment;
@@ -50,13 +25,6 @@ export class ProgrammingExerciseTestScheduleDatePickerComponent implements Contr
     @Output() onDateReset = new EventEmitter();
 
     _onChange: any = () => {};
-
-    set value(val: Moment | null) {
-        if (val !== undefined && this.val !== val) {
-            this.val = !val ? null : isDate(val) ? val : val.toDate();
-            this._onChange(val);
-        }
-    }
 
     registerOnChange(fn: any): void {
         this._onChange = fn;
@@ -67,16 +35,10 @@ export class ProgrammingExerciseTestScheduleDatePickerComponent implements Contr
     setDisabledState(isDisabled: boolean): void {}
 
     writeValue(obj: any): void {
-        this.value = obj;
-    }
-
-    /**
-     * Updates the current modal using the selected value from the date picker
-     *
-     * @param newValue The new value selected by the user
-     */
-    updateField(newValue: Moment) {
-        this.value = newValue;
+        if (obj !== undefined && this.selectedDate !== obj) {
+            this.selectedDate = !obj ? null : isDate(obj) ? obj : obj.toDate();
+            this._onChange(obj);
+        }
     }
 
     /**
@@ -84,7 +46,7 @@ export class ProgrammingExerciseTestScheduleDatePickerComponent implements Contr
      * This makes it easier to also reset date, that can only be selected if the current date is not null
      */
     resetDate() {
-        this.value = null;
+        this.writeValue(null);
         this.onDateReset.emit();
     }
 }
