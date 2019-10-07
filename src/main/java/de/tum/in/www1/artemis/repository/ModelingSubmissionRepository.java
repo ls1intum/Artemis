5be68fdf57fd3a88c5fa05c29c363dc7df632df6
Maksym <maxr96@gmail.com>
2019-10-07 16:13:15 +0200

    Moved ModelingSubmissionRepo and TextSubmissionRepo logic to GenericSubmissionRepo

--- src/main/java/de/tum/in/www1/artemis/repository/ModelingSubmissionRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ModelingSubmissionRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,25 +14,8 @@ import de.tum.in.www1.artemis.domain.modeling.ModelingSubmission;
 /**
  * Spring Data JPA repository for the ModelingSubmission entity.
  */
-@SuppressWarnings("unused")
 @Repository
-public interface ModelingSubmissionRepository extends JpaRepository<ModelingSubmission, Long> {
-
-    @Query("select distinct submission from ModelingSubmission submission left join fetch submission.result r left join fetch r.assessor where submission.id = :#{#submissionId}")
-    Optional<ModelingSubmission> findByIdWithEagerResult(@Param("submissionId") Long submissionId);
-
-    @Query("select distinct submission from ModelingSubmission submission left join fetch submission.result r left join fetch r.feedbacks left join fetch r.assessor where submission.id = :#{#submissionId}")
-    Optional<ModelingSubmission> findByIdWithEagerResultAndFeedback(@Param("submissionId") Long submissionId);
-
-    /**
-     * Load the modeling submission with the given id together with its result, the feedback list of the result, the assessor of the result, its participation and all results of
-     * the participation.
-     *
-     * @param submissionId the id of the modeling submission that should be loaded from the database
-     * @return the modeling submission with its result, the feedback list of the result, the assessor of the result, its participation and all results of the participation
-     */
-    @EntityGraph(attributePaths = { "result", "result.feedbacks", "result.assessor", "participation", "participation.results" })
-    Optional<ModelingSubmission> findWithEagerResultAndFeedbackAndAssessorAndParticipationResultsById(Long submissionId);
+public interface ModelingSubmissionRepository extends GenericSubmissionRepository<ModelingSubmission> {
 
     /**
      * Load all modeling submissions with the given ids. Load every submission together with its result, the feedback list of the result, the assessor of the result, its
@@ -51,27 +33,4 @@ public interface ModelingSubmissionRepository extends JpaRepository<ModelingSubm
 
     @Query("select distinct submission from ModelingSubmission submission left join fetch submission.result r left join fetch r.feedbacks where submission.exampleSubmission = true and submission.id = :#{#submissionId}")
     Optional<ModelingSubmission> findExampleSubmissionByIdWithEagerResult(@Param("submissionId") Long submissionId);
-
-    /**
-     * @param courseId  the course we are interested in
-     * @param submitted boolean to check if an exercise has been submitted or not
-     * @return number of submissions belonging to courseId with submitted status
-     */
-    long countByParticipation_Exercise_Course_IdAndSubmitted(Long courseId, boolean submitted);
-
-    /**
-     * @param courseId the course id we are interested in
-     * @return the number of submissions belonging to the course id, which have the submitted flag set to true and the submission date before the exercise due date, or no exercise
-     *         due date at all
-     */
-    @Query("SELECT COUNT (DISTINCT submission) FROM ModelingSubmission submission WHERE submission.participation.exercise.course.id = :#{#courseId} AND submission.submitted = TRUE AND (submission.submissionDate < submission.participation.exercise.dueDate OR submission.participation.exercise.dueDate IS NULL)")
-    long countByCourseIdSubmittedBeforeDueDate(@Param("courseId") Long courseId);
-
-    /**
-     * @param exerciseId the exercise id we are interested in
-     * @return the number of submissions belonging to the exercise id, which have the submitted flag set to true and the submission date before the exercise due date, or no
-     *         exercise due date at all
-     */
-    @Query("SELECT COUNT (DISTINCT submission) FROM ModelingSubmission submission WHERE submission.participation.exercise.id = :#{#exerciseId} AND submission.submitted = TRUE AND (submission.submissionDate < submission.participation.exercise.dueDate OR submission.participation.exercise.dueDate IS NULL)")
-    long countByExerciseIdSubmittedBeforeDueDate(@Param("exerciseId") Long exerciseId);
 }
--- src/main/java/de/tum/in/www1/artemis/repository/TextSubmissionRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/TextSubmissionRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,30 +13,11 @@ import de.tum.in.www1.artemis.domain.TextSubmission;
 /**
  * Spring Data JPA repository for the TextSubmission entity.
  */
-@SuppressWarnings("unused")
 @Repository
-public interface TextSubmissionRepository extends JpaRepository<TextSubmission, Long> {
-
-    List<TextSubmission> findByIdIn(List<Long> textSubmissionsId);
+public interface TextSubmissionRepository extends GenericSubmissionRepository<TextSubmission> {
 
     @Query("select distinct submission from TextSubmission submission left join fetch submission.result r left join fetch r.assessor where submission.id = :#{#submissionId}")
     Optional<TextSubmission> findByIdWithEagerResultAndAssessor(@Param("submissionId") Long submissionId);
-
-    /**
-     * @param courseId the course id we are interested in
-     * @return the number of submissions belonging to the course id, which have the submitted flag set to true and the submission date before the exercise due date or no exercise
-     *         due date at all
-     */
-    @Query("SELECT COUNT (DISTINCT textSubmission) FROM TextSubmission textSubmission WHERE textSubmission.participation.exercise.course.id = :#{#courseId} AND textSubmission.submitted = TRUE AND (textSubmission.submissionDate < textSubmission.participation.exercise.dueDate OR textSubmission.participation.exercise.dueDate IS NULL)")
-    long countByCourseIdSubmittedBeforeDueDate(@Param("courseId") Long courseId);
-
-    /**
-     * @param exerciseId the exercise id we are interested in
-     * @return the number of submissions belonging to the exercise id, which have the submitted flag set to true and the submission date before the exercise due date or no exercise
-     *         due date at all
-     */
-    @Query("SELECT COUNT (DISTINCT textSubmission) FROM TextSubmission textSubmission WHERE textSubmission.participation.exercise.id = :#{#exerciseId} AND textSubmission.submitted = TRUE AND (textSubmission.submissionDate < textSubmission.participation.exercise.dueDate OR textSubmission.participation.exercise.dueDate IS NULL)")
-    long countByExerciseIdSubmittedBeforeDueDate(@Param("exerciseId") Long exerciseId);
 
     /**
      * Gets all open (without a result) TextSubmissions which are submitted and loads all blocks, results, and participation
