8fbec8f0320ad9284bbf3618851a4897533ce94a
thilo-behnke <jan-thilo.behnke@gmx.de>
2019-10-10 16:28:19 +0200

    the build status takes priority over the normal result status - if it changes we ALWAYS have to change the result status (#902)

--- src/main/webapp/app/entities/result/result.component.ts
+++ src/main/webapp/app/entities/result/result.component.ts
@@ -94,6 +94,12 @@ export class ResultComponent implements OnInit, OnChanges {
     ngOnChanges(changes: SimpleChanges) {
         if (changes.participation || changes.result) {
             this.ngOnInit();
+            // If is building, we change the templateStatus to building regardless of any other settings.
+        } else if (changes.isBuilding && changes.isBuilding.currentValue) {
+            this.templateStatus = ResultTemplateStatus.IS_BUILDING;
+            // When the result was building and is not building anymore, we evaluate the result status.
+        } else if (changes.isBuilding && changes.isBuilding.previousValue && !changes.isBuilding.currentValue) {
+            this.evaluate();
         }
     }
 
