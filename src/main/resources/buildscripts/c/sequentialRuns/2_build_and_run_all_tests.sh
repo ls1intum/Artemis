053b2a9f43a38172d9126e9350e127269b03b641
Alexander Ungar <alexander.ungar@tum.de>
2019-10-12 18:37:52 +0200

    Fix sequential test build scripts for Python and C tests

--- src/main/resources/buildscripts/c/sequentialRuns/2_build_and_run_all_tests.sh
+++ src/main/resources/buildscripts/c/sequentialRuns/2_build_and_run_all_tests.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Actual build process:
 cd tests
 
@@ -7,7 +9,7 @@ cp -r structural/tests ./tests
 python3 Tests.py structural
 
 # Only continue if structural tests have been successful
-if [[ ! grep -P '(errors|failures)="[1-9]+' ../test-reports/structural-results.xml ]]; then
+if ! grep -P '(errors|failures)="[1-9]+' ../test-reports/structural-results.xml ; then
   # Cleanup structural tests
   rm Tests.py
   rm -rf ./tests
