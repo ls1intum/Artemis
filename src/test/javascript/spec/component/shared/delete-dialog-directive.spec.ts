58a269eb07498d74a226014ed7674f270e1f342c
maxr96 <S1a5l6i3v2a>
2019-10-14 10:01:10 +0200

    Additional tests for newly cleanup and reset dialog logic

--- src/test/javascript/spec/component/shared/delete-dialog-directive.spec.ts
+++ src/test/javascript/spec/component/shared/delete-dialog-directive.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { DebugElement, Component } from '@angular/core';
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
@@ -14,21 +14,25 @@ import * as sinon from 'sinon';
 import { DeleteButtonDirective } from 'app/shared/delete-dialog/delete-button.directive';
 import { DeleteDialogService } from 'app/shared/delete-dialog/delete-dialog.service';
 import { DeleteDialogComponent } from 'app/shared/delete-dialog/delete-dialog.component';
+import { ActionType } from 'app/shared/delete-dialog/delete-dialog.model';
 
 chai.use(sinonChai);
 const expect = chai.expect;
 
 @Component({
     selector: 'jhi-test-component',
-    template: '<button jhiDeleteButton entityTitle="title" deleteQuestion="question" deleteConfirmationText="text"></button>',
+    template: '<button jhiDeleteButton [actionType]="actionType" entityTitle="title" deleteQuestion="question" deleteConfirmationText="text"></button>',
 })
-class TestComponent {}
+class TestComponent {
+    actionType = ActionType.Delete;
+}
 
 describe('DeleteDialogDirective', () => {
     let comp: TestComponent;
     let fixture: ComponentFixture<TestComponent>;
     let debugElement: DebugElement;
     let deleteDialogService: DeleteDialogService;
+    let translateService: TranslateService;
 
     beforeEach(async () => {
         return TestBed.configureTestingModule({
@@ -42,11 +46,16 @@ describe('DeleteDialogDirective', () => {
                 comp = fixture.componentInstance;
                 debugElement = fixture.debugElement;
                 deleteDialogService = TestBed.get(DeleteDialogService);
+                translateService = TestBed.get(TranslateService);
             });
     });
 
     it('directive should be correctly initialized', fakeAsync(() => {
+        const translateSpy = sinon.spy(translateService, 'instant');
+
         fixture.detectChanges();
+        expect(translateSpy.callCount).to.be.equal(1);
+        expect(translateSpy.lastCall.lastArg).to.be.equal('entity.action.delete');
 
         // Check that button was assigned with proper classes and type.
         const deleteButton = debugElement.query(By.css('.btn.btn-danger.btn-sm.mr-1'));
@@ -75,5 +84,21 @@ describe('DeleteDialogDirective', () => {
         directiveEl.nativeElement.click();
         fixture.detectChanges();
         expect(deleteDialogSpy.callCount).to.equal(1);
+    }));
+
+    it('action type cleanup should change button title', fakeAsync(() => {
+        const translateSpy = sinon.spy(translateService, 'instant');
+        comp.actionType = ActionType.Cleanup;
+        fixture.detectChanges();
+        expect(translateSpy.callCount).to.be.equal(1);
+        expect(translateSpy.lastCall.lastArg).to.be.equal('entity.action.cleanup');
+    }));
+
+    it('action type reset should change button title', fakeAsync(() => {
+        const translateSpy = sinon.spy(translateService, 'instant');
+        comp.actionType = ActionType.Reset;
+        fixture.detectChanges();
+        expect(translateSpy.callCount).to.be.equal(1);
+        expect(translateSpy.lastCall.lastArg).to.be.equal('entity.action.reset');
     }));
 });
