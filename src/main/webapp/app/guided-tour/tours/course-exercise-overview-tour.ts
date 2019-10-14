1ccac912db700cc913c34e7829c42f0b5f350450
Anh Montag <anh.montag@tum.de>
2019-10-15 00:57:38 +0200

    Adjust short name for testing purpose

--- src/main/webapp/app/guided-tour/tours/course-exercise-overview-tour.ts
+++ src/main/webapp/app/guided-tour/tours/course-exercise-overview-tour.ts
@@ -4,7 +4,7 @@ import { TextTourStep, VideoTourStep } from 'app/guided-tour/guided-tour-step.mo
 
 export const courseExerciseOverviewTour: GuidedTour = {
     courseShortName: 'artemistutorial',
-    exerciseShortName: 'tutorial',
+    exerciseShortName: 'test',
     settingsKey: 'course_exercise_overview_tour',
     steps: [
         new TextTourStep({
