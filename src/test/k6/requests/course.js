514c5bae9e61096f075256acb13b39c2aadb9d49
Alexander Ungar <alexander.ungar@tum.de>
2019-10-22 15:28:49 +0200

    Feature/api tests (#960)

---
+++ src/test/k6/requests/course.js
@@ -0,0 +1,38 @@
+import { COURSE, COURSES } from './endpoints.js';
+import { nextAlphanumeric } from '../util/utils.js';
+import { fail } from 'k6';
+
+export function newCourseShortName(artemis, courseId) {
+    const course = JSON.parse(artemis.get(COURSE(courseId), null)[0].body);
+    course.shortName = 'TEST' + nextAlphanumeric(5);
+    artemis.put(COURSES, course);
+}
+
+export function newCourse(artemis) {
+    const course = {
+        title: 'K6 Test Course',
+        description: 'K6 performance tests generated course',
+        shortName: nextAlphanumeric(5),
+        studentGroupName: 'artemis-test',
+        teachingAssistantGroupName: 'artemis-test',
+        instructorGroupName: 'artemis-test',
+        registrationEnabled: true
+    };
+
+    const res = artemis.post(COURSES, course);
+    if (res[0].status !== 201) {
+        fail('ERROR: Unable to generate new course');
+    }
+    console.log('SUCCESS: Generated new course');
+
+    return JSON.parse(res[0].body).id;
+}
+
+export function deleteCourse(artemis, courseId) {
+    const res = artemis.delete(COURSE(courseId));
+
+    if (res[0].status !== 200) {
+        fail('ERROR: Unable to delete course ' + courseId);
+    }
+    console.log('SUCCESS: Deleted course ' + courseId);
+}
