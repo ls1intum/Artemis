4a8d13ac4213f37f35963aefbfd00c7dae0ce321
Thilo Behnke <jan-thilo.behnke@gmx.de>
2019-10-25 13:14:28 +0200

    fix issue with missing submission in websocket client

--- src/main/java/de/tum/in/www1/artemis/service/ResultService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ResultService.java
@@ -323,7 +323,13 @@ public class ResultService {
         });
 
         // this call should cascade all feedback relevant changed and save them accordingly
-        Result savedResult = resultRepository.save(result);
+        resultRepository.save(result);
+        // The websocket client expects the submission and feedbacks, so we retrieve the result again instead of using the save result.
+        Optional<Result> savedResultOpt = resultRepository.findWithEagerSubmissionAndFeedbackById(result.getId());
+        if (savedResultOpt.isEmpty()) {
+            throw new EntityNotFoundException("Could not retrieve result with id " + result.getId() + " after save.");
+        }
+        Result savedResult = savedResultOpt.get();
 
         // if it is an example result we do not have any participation (isExampleResult can be also null)
         if (result.isExampleResult() == Boolean.FALSE || result.isExampleResult() == null) {
