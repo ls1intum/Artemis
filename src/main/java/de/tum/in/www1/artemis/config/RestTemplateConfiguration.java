56a9d8070a81bedc369ed6535bd53a8a00c48f7e
Alexander Ungar <alexander.ungar@tum.de>
2019-10-31 01:22:03 +0100

    Add basic GitLab service and authentication injector

--- src/main/java/de/tum/in/www1/artemis/config/RestTemplateConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/RestTemplateConfiguration.java
@@ -1,8 +1,14 @@
 package de.tum.in.www1.artemis.config;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
+
+import de.tum.in.www1.artemis.service.connectors.gitlab.GitLabHeaderAuthorizationInterceptor;
 
 /**
  * For now only provides a basic {@link org.springframework.web.client.RestTemplate RestTemplate} bean. Can be extended
@@ -12,6 +18,22 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfiguration {
 
     @Bean
+    @Profile("gitlab")
+    @Autowired
+    public RestTemplate restTemplate(GitLabHeaderAuthorizationInterceptor gitlabInterceptor) {
+        final var restTemplate = new RestTemplate();
+        var interceptors = restTemplate.getInterceptors();
+        if (interceptors.isEmpty()) {
+            interceptors = new ArrayList<>();
+        }
+        interceptors.add(gitlabInterceptor);
+        restTemplate.setInterceptors(interceptors);
+
+        return restTemplate;
+    }
+
+    @Bean
+    @Profile("!gitlab")
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
