c355cf614deeef1941c9dba633b5aa66cb7f782a
Stephan Krusche <s.krusche@tum.de>
2019-11-03 02:16:31 +0100

    resume programming exercises when builds are triggered in case they are inactive
    also improve automatic build plan cleanup service

--- src/main/java/de/tum/in/www1/artemis/service/ProgrammingSubmissionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ProgrammingSubmissionService.java
@@ -95,18 +95,18 @@ public class ProgrammingSubmissionService {
             throw new EntityNotFoundException("ProgrammingExerciseParticipation with id " + participationId + " could not be found!");
         }
 
-        ProgrammingExerciseParticipation peParticipation = (ProgrammingExerciseParticipation) participation;
+        ProgrammingExerciseParticipation programmingExerciseParticipation = (ProgrammingExerciseParticipation) participation;
 
         if (participation instanceof ProgrammingExerciseStudentParticipation
-                && ((ProgrammingExerciseStudentParticipation) peParticipation).getInitializationState() == InitializationState.INACTIVE) {
+                && (programmingExerciseParticipation.getBuildPlanId() == null || programmingExerciseParticipation.getInitializationState() == InitializationState.INACTIVE)) {
             // the build plan was deleted before, e.g. due to cleanup, therefore we need to reactivate the build plan by resuming the participation
             // This is needed as a request using a custom query is made using the ProgrammingExerciseRepository, but the user is not authenticated
             // as the VCS-server performs the request
             SecurityUtils.setAuthorizationObject();
-            participationService.resumeExercise(peParticipation.getProgrammingExercise(), (ProgrammingExerciseStudentParticipation) peParticipation);
+            participationService.resumeExercise((ProgrammingExerciseStudentParticipation) programmingExerciseParticipation);
 
             try {
-                continuousIntegrationService.get().triggerBuild(peParticipation);
+                continuousIntegrationService.get().triggerBuild(programmingExerciseParticipation);
             }
             catch (HttpException ex) {
                 // TODO: This case is currently not handled. The correct handling would be creating the submission and informing the user that the build trigger failed.
@@ -350,7 +350,13 @@ public class ProgrammingSubmissionService {
      */
     public void triggerBuildAndNotifyUser(ProgrammingSubmission submission) {
         try {
-            continuousIntegrationService.get().triggerBuild((ProgrammingExerciseParticipation) submission.getParticipation());
+            var programmingExerciseParticipation = (ProgrammingExerciseParticipation) submission.getParticipation();
+            if (programmingExerciseParticipation instanceof ProgrammingExerciseStudentParticipation
+                    && (programmingExerciseParticipation.getBuildPlanId() == null || programmingExerciseParticipation.getInitializationState() == InitializationState.INACTIVE)) {
+                // in this case, we first have to resume the exercise: this includes that we again setup the build plan properly before we trigger it
+                participationService.resumeExercise((ProgrammingExerciseStudentParticipation) programmingExerciseParticipation);
+            }
+            continuousIntegrationService.get().triggerBuild(programmingExerciseParticipation);
             notifyUserAboutSubmission(submission);
         }
         catch (HttpException e) {
