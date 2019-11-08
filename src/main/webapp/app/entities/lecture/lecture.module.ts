8e4255f0c7e844510492ffbd4c0ca9b78280ed73
Beatris Burdeva <beatris.burdeva@tum.de>
2019-11-08 12:11:03 +0100

    Removed unnecessary change

--- src/main/webapp/app/entities/lecture/lecture.module.ts
+++ src/main/webapp/app/entities/lecture/lecture.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ArtemisSharedModule } from 'app/shared';
-import { LectureAttachmentsComponent, LectureComponent, LectureDetailComponent, lectureRoute, LectureService, LectureUpdateComponent } from './';
+import { LectureAttachmentsComponent, LectureComponent, LectureDetailComponent, lectureRoute, LectureUpdateComponent } from './';
 import { FormDateTimePickerModule } from 'app/shared/date-time-picker/date-time-picker.module';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { ArtemisMarkdownEditorModule } from 'app/markdown-editor';
@@ -13,6 +13,5 @@ const ENTITY_STATES = [...lectureRoute];
     imports: [ArtemisSharedModule, RouterModule.forChild(ENTITY_STATES), FormDateTimePickerModule, ArtemisSharedComponentModule, ArtemisMarkdownEditorModule],
     declarations: [LectureComponent, LectureDetailComponent, LectureUpdateComponent, LectureAttachmentsComponent],
     entryComponents: [LectureComponent, LectureUpdateComponent, LectureAttachmentsComponent],
-    providers: [LectureService],
 })
 export class ArtemisLectureModule {}
