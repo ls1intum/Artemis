28cd04d029c9523cd673155ef3450c6dc76e18da
Alexander Ungar <alexander.ungar@tum.de>
2019-11-12 11:52:50 +0100

    Add resource for feature toggle

---
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ManagementResource.java
@@ -0,0 +1,36 @@
+package de.tum.in.www1.artemis.web.rest;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tum.in.www1.artemis.service.feature.Feature;
+
+@RestController
+@RequestMapping(ManagementResource.ROOT_MANAGEMENT)
+public class ManagementResource {
+
+    static final String ROOT_MANAGEMENT = "/api/management";
+
+    private static final String SUB_FEATURE_TOGGLE = "/feature-toggle";
+
+    @PutMapping(SUB_FEATURE_TOGGLE)
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<List<Feature>> toggleFeatures(@RequestBody Map<Feature, Boolean> features) {
+        features.forEach((feature, setEnabled) -> {
+            if (setEnabled)
+                feature.enable();
+            else
+                feature.disable();
+        });
+
+        return new ResponseEntity<>(Feature.enabledFeatures(), HttpStatus.OK);
+    }
+}
