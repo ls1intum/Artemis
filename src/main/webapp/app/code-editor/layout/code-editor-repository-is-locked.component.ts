79b2f3b8168a2b6992423dfe4df939fdac3471a3
Alexander Ungar <alexander.ungar@tum.de>
2019-11-18 18:33:23 +0100

    Fix icon names

--- src/main/webapp/app/code-editor/layout/code-editor-repository-is-locked.component.ts
+++ src/main/webapp/app/code-editor/layout/code-editor-repository-is-locked.component.ts
@@ -4,7 +4,7 @@ import { Component, HostBinding } from '@angular/core';
     selector: 'jhi-code-editor-repository-is-locked',
     template: `
         <span class="badge badge-warning d-flex align-items-center locked-container">
-            <fa-icon [icon]="'info - circle'" class="text-white" size="2x"></fa-icon>
+            <fa-icon [icon]="'info-circle'" class="text-white" size="2x"></fa-icon>
             <span
                 class="ml-2 locked-lable"
                 jhiTranslate="artemisApp.programmingExercise.repositoryIsLocked.title"
