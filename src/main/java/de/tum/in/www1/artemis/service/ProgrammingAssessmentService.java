bd89296e81a30aaa8aadf3871704d0bbb40bfe4d
Maksym Radchuk <maxr96@gmail.com>
2019-11-19 00:11:29 +0100

    Applied feedback from code review

--- src/main/java/de/tum/in/www1/artemis/service/ProgrammingAssessmentService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ProgrammingAssessmentService.java
@@ -1,9 +1,6 @@
 package de.tum.in.www1.artemis.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.repository.ComplaintRepository;
@@ -12,8 +9,6 @@ import de.tum.in.www1.artemis.repository.StudentParticipationRepository;
 
 @Service
 public class ProgrammingAssessmentService extends AssessmentService {
-
-    private final Logger log = LoggerFactory.getLogger(ProgrammingAssessmentService.class);
 
     public ProgrammingAssessmentService(ComplaintResponseService complaintResponseService, ComplaintRepository complaintRepository, ResultRepository resultRepository,
             StudentParticipationRepository studentParticipationRepository, ResultService resultService, AuthorizationCheckService authCheckService) {
@@ -30,7 +25,6 @@ public class ProgrammingAssessmentService extends AssessmentService {
      * @param assessmentUpdate the assessment update
      * @return the updated Result
      */
-    @Transactional
     public Result updateAssessmentAfterComplaint(Result originalResult, Exercise exercise, ProgrammingAssessmentUpdate assessmentUpdate) {
         super.updateAssessmentAfterComplaint(originalResult, exercise, assessmentUpdate);
         originalResult.setResultString(assessmentUpdate.getResultString());
