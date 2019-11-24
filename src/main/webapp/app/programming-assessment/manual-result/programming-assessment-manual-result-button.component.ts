78fe9c0dbaf997c3f57ba198c794529cc583076f
maxr96 <S1a5l6i3v2a>
2019-11-24 21:16:14 +0100

    New button and dialog title: "View Manual Result"

--- src/main/webapp/app/programming-assessment/manual-result/programming-assessment-manual-result-button.component.ts
+++ src/main/webapp/app/programming-assessment/manual-result/programming-assessment-manual-result-button.component.ts
@@ -16,7 +16,7 @@ import { filter } from 'rxjs/operators';
             [btnType]="ButtonType.WARNING"
             [btnSize]="ButtonSize.SMALL"
             [icon]="'asterisk'"
-            [title]="latestResult ? 'entity.action.updateResult' : 'entity.action.newResult'"
+            [title]="latestResult ? (latestResult.hasComplaint ? 'entity.action.viewResult' : 'entity.action.updateResult') : 'entity.action.newResult'"
             (onClick)="openManualResultDialog($event)"
         ></jhi-button>
     `,
