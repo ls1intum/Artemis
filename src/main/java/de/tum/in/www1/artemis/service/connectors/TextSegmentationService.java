2a88d07f2cf7553c78890dbf4c3beb558f99b6d6
Jan Philip Bernius <janphilip.bernius@tum.de>
2019-11-28 14:40:34 +0100

    Use new Text Submission object as DTO for segmentation network call.
    - Reduce Size of Request
    - Prevent JSON Infinite recursion Stack Overflow
    
    Signed-off-by: Jan Philip Bernius <janphilip.bernius@tum.de>

--- src/main/java/de/tum/in/www1/artemis/service/connectors/TextSegmentationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/TextSegmentationService.java
@@ -1,10 +1,13 @@
 package de.tum.in.www1.artemis.service.connectors;
 
 import static de.tum.in.www1.artemis.service.connectors.RemoteArtemisServiceConnector.authenticationHeaderForSecret;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,8 +29,21 @@ public class TextSegmentationService {
 
         public List<TextSubmission> submissions;
 
-        Request(List<TextSubmission> submissions) {
-            this.submissions = submissions;
+        Request(@NotNull List<TextSubmission> submissions) {
+            this.submissions = submissionDTOs(submissions);
+        }
+
+        /**
+         * Create new TextSubmission as DTO.
+         */
+        @NotNull
+        private List<TextSubmission> submissionDTOs(@NotNull List<TextSubmission> submissions) {
+            return submissions.stream().map(textSubmission -> {
+                final TextSubmission submission = new TextSubmission();
+                submission.setText(textSubmission.getText());
+                submission.setId(textSubmission.getId());
+                return submission;
+            }).collect(toList());
         }
     }
 
@@ -36,6 +52,8 @@ public class TextSegmentationService {
         // keywords can be used in future to calculate text blocks for a single submission
         public List<String> keywords;
 
+        // TODO: textBlock.id is used to reference the submission id in the response of the segmentation service.
+        // This should be refactored to be called submissionId, to not confuse this with the text block id, computed with textBlock.computeId().
         public List<TextBlock> textBlocks;
 
     }
@@ -49,6 +67,12 @@ public class TextSegmentationService {
 
     private RemoteArtemisServiceConnector<Request, Response> connector = new RemoteArtemisServiceConnector<>(log, Response.class);
 
+    /**
+     * Calls the remote text segmentation service to segment a List of Submissions into TextBlocks
+     * @param submissions a List of Submissions which should be segmented into TextBlocks
+     * @return a List of textBlocks corresponding the submissions
+     * @throws NetworkingError if the request isn't successful
+     */
     public List<TextBlock> segmentSubmissions(List<TextSubmission> submissions) throws NetworkingError {
         return segmentSubmissions(submissions, 1);
     }
@@ -62,19 +86,24 @@ public class TextSegmentationService {
      */
     public List<TextBlock> segmentSubmissions(List<TextSubmission> submissions, int maxRetries) throws NetworkingError {
         log.info("Calling Remote Service to segment " + submissions.size() + " submissions into text blocks.");
+
         final Request request = new Request(submissions);
         Response response = connector.invokeWithRetry(API_ENDPOINT, request, authenticationHeaderForSecret(API_SECRET), maxRetries);
 
-        Map<String, TextSubmission> submissionsMap = submissions.stream().collect(Collectors.toMap(submission -> submission.getId().toString(), item -> item));
+        Map<String, TextSubmission> submissionsMap = submissions.stream()
+                .collect(toMap(/* Key: */ (submission -> submission.getId().toString()), /* Value: */ (submission -> submission)));
 
-        // for each textBlock, take the corresponding TextSubmission and add the text blocks.
-        // The addBlocks method also sets the submission in the textBlock
-        response.textBlocks.forEach(textBlock -> submissionsMap.get(textBlock.getId()).addBlocks(textBlock));
+        for (TextBlock textBlock : response.textBlocks) {
+            // take the corresponding TextSubmission and add the text blocks.
+            // The addBlocks method also sets the submission in the textBlock
+            submissionsMap.get(textBlock.getId()).addBlock(textBlock);
 
-        // compute correct ID for each textBlock
-        response.textBlocks.forEach(textBlock -> textBlock.computeId());
+            // compute correct ID for each textBlock
+            textBlock.computeId();
 
-        response.textBlocks.forEach(textBlock -> textBlock.setText(textBlock.getSubmission().getText().substring(textBlock.getStartIndex(), textBlock.getEndIndex())));
+            // Use start/end index to compute text of segment for easy access.
+            textBlock.setText(textBlock.getSubmission().getText().substring(textBlock.getStartIndex(), textBlock.getEndIndex()));
+        }
 
         log.info("Segmentation finished. " + response.textBlocks.size() + " TextBlocks calculated.");
 
