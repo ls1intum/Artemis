fe7a01de863743adb98067db346c81e87ffd11f8
Hanya Elhashemy <ga87saf@mytum.de>
2019-12-03 12:08:52 +0100

    resolve merge conflict

--- src/main/webapp/app/assessment-instructions/assessment-instructions.module.ts
+++ src/main/webapp/app/assessment-instructions/assessment-instructions.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { AssessmentInstructionsComponent } from './assessment-instructions.component';
 import { ExpandableParagraphComponent } from './expandable-paragraph/expandable-paragraph.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ExpandableSampleSolutionComponent } from './expandable-sample-solution/expandable-sample-solution.component';
@@ -9,8 +8,8 @@ import { ExpandableProblemStatementComponent } from './expandable-problem-statem
 import { ArtemisProgrammingExerciseInstructionsRenderModule } from 'app/entities/programming-exercise/instructions/instructions-render';
 
 @NgModule({
-    declarations: [AssessmentInstructionsComponent, ExpandableParagraphComponent, ExpandableSampleSolutionComponent, ExpandableProblemStatementComponent],
-    exports: [AssessmentInstructionsComponent, ExpandableSampleSolutionComponent],
+    declarations: [ExpandableParagraphComponent, ExpandableSampleSolutionComponent, ExpandableProblemStatementComponent],
+    exports: [ExpandableSampleSolutionComponent],
     imports: [NgbModule, ArtemisSharedModule, ArtemisModelingEditorModule, ArtemisProgrammingExerciseInstructionsRenderModule],
 })
 export class AssessmentInstructionsModule {}
