49d27599f2c9e8055424827a0d99c389584af5c8
Maksym Radchuk <maxr96@gmail.com>
2019-12-09 15:51:24 +0100

    Apply feedback from code review

--- src/test/java/de/tum/in/www1/artemis/FileUploadSubmissionIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/FileUploadSubmissionIntegrationTest.java
@@ -2,6 +2,8 @@ package de.tum.in.www1.artemis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -101,11 +103,15 @@ public class FileUploadSubmissionIntegrationTest {
     @WithMockUser(value = "student3")
     public void submitFileUploadSubmission() throws Exception {
         FileUploadSubmission submission = ModelFactory.generateFileUploadSubmission(false);
+        submission.setId(1L);
         FileUploadSubmission returnedSubmission = performInitialSubmission(fileUploadExercise.getId(), submission);
         String actualFilePath = FileUploadSubmission.buildFilePath(fileUploadExercise.getId(), returnedSubmission.getId()).concat("file.png");
         String publicFilePath = fileService.publicPathForActualPath(actualFilePath, returnedSubmission.getId());
         assertThat(returnedSubmission).as("submission correctly posted").isNotNull();
         assertThat(returnedSubmission.getFilePath()).isEqualTo(publicFilePath);
+        var localPath = fileService.actualPathForPublicPath(publicFilePath);
+        var fileBytes = Files.readAllBytes(Path.of(localPath));
+        assertThat(fileBytes.length > 0).as("Stored file has content").isTrue();
         checkDetailsHidden(returnedSubmission, true);
     }
 
