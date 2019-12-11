9860e54f3ceb5ada0a0d8434540f9c815ac09018
Hanya Elhashemy <ga87saf@mytum.de>
2019-12-11 20:03:03 +0100

    add domain objects (entities)

--- src/main/java/de/tum/in/www1/artemis/domain/GradingInstruction.java
+++ src/main/java/de/tum/in/www1/artemis/domain/GradingInstruction.java
@@ -5,7 +5,11 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
+/**
+ * A Structured Grading Instruction.
+ */
 @Entity
 @Table(name = "grading_instruction")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
@@ -36,17 +40,103 @@ public class GradingInstruction implements Serializable {
     @Column(name = "usage_count")
     private int usageCount;
 
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getCredits() {
+        return credits;
+    }
+    public GradingInstruction credits(Double credits) {
+        this.credits = credits;
+        return this;
+    }
+    public void setCredits(Double credits) {
+        this.credits = credits;
+    }
+
+    public void setUsageCount(int usageCount) {
+        this.usageCount = usageCount;
+    }
+    public int getUsageCount() {
+        return usageCount;
+    }
+    public GradingInstruction usageCount(int usageCount) {
+        this.usageCount = usageCount;
+        return this;
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+    public GradingInstruction result(Exercise exercise) {
+        this.exercise = exercise;
+        return this;
+    }
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
     public String getInstructionDescription() {
         return instructionDescription;
     }
-
     public GradingInstruction instructionDescription(String instructionDescription) {
         this.instructionDescription = instructionDescription;
         return this;
     }
-
     public void setInstructionDescription(String instructionDescription) {
         this.instructionDescription = instructionDescription;
+    }
+    public String getLevel() {
+        return level;
+    }
+    public GradingInstruction level(String level) {
+        this.level = level;
+        return this;
+    }
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+    public GradingInstruction feedback(String feedback) {
+        this.instructionDescription = feedback;
+        return this;
+    }
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GradingInstruction gradingInstruction = (GradingInstruction) o;
+        if (gradingInstruction.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), gradingInstruction.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "GradingInstruction{" + "id=" + getId() + ", credits='" + getCredits() + "'" + ", level='" + getLevel() + "'" + ", instructionDescription='"
+            + getInstructionDescription() + "'" + ", feedback='" + getFeedback() +  ", usageCount='" + getUsageCount() + "'" + "}";
     }
 
 }
