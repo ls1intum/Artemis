4b4c69a677abeab09fc21b38fd11318e8ba50908
maxr96 <maxr96@gmail.com>
2019-12-12 15:00:50 +0100

    Bugfix: Ensure that the stored file is the same as the one passed from client (#1114)
    Fixes #1097.

--- src/main/java/de/tum/in/www1/artemis/web/rest/FileUploadSubmissionResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/FileUploadSubmissionResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import de.tum.in.www1.artemis.config.Constants;
 import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.participation.StudentParticipation;
+import de.tum.in.www1.artemis.exception.EmptyFileException;
 import de.tum.in.www1.artemis.service.*;
 import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException;
 import de.tum.in.www1.artemis.web.rest.util.HeaderUtil;
@@ -111,8 +112,14 @@ public class FileUploadSubmissionResource {
             submission = fileUploadSubmissionService.handleFileUploadSubmission(fileUploadSubmission, file, exercise, principal);
         }
         catch (IOException e) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(applicationName, true, "fileUploadSubmission", "fileUploadSubmissionCantStore",
-                    "The uploaded file could not be saved on the server")).build();
+            return ResponseEntity.badRequest()
+                    .headers(HeaderUtil.createFailureAlert(applicationName, true, "fileUploadSubmission", "cantSaveFile", "The uploaded file could not be saved on the server"))
+                    .build();
+        }
+
+        catch (EmptyFileException e) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(applicationName, true, "fileUploadSubmission", "emptyFile", "The uploaded file is empty"))
+                    .build();
         }
 
         this.fileUploadSubmissionService.hideDetails(submission, user);
