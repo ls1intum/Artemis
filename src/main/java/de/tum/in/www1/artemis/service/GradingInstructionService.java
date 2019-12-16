43e56aa750135b673ca1933d05bff731af14897e
Hanya Elhashemy <ga87saf@mytum.de>
2019-12-16 23:01:26 +0100

    fix java code style

--- src/main/java/de/tum/in/www1/artemis/service/GradingInstructionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/GradingInstructionService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.Exercise;
 import de.tum.in.www1.artemis.domain.GradingInstruction;
