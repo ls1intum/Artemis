e0ec1816866904524f9b0c54a823391bad6b7661
Stephan Krusche <s.krusche@tum.de>
2019-12-29 22:49:29 +0100

    add more tests for quiz resource
    improve tempId handling for quizzes on client side

--- src/test/java/de/tum/in/www1/artemis/QuizExerciseIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/QuizExerciseIntegrationTest.java
@@ -2,7 +2,9 @@ package de.tum.in.www1.artemis;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -63,37 +66,168 @@ public class QuizExerciseIntegrationTest {
     @Test
     @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
     public void testCreateQuizExercise() throws Exception {
+        QuizExercise quizExerciseServer = createQuizOnServer();
+
+        // TODO: add some checks
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testEditQuizExercise() throws Exception {
+        QuizExercise quizExerciseServer = createQuizOnServer();
+        MultipleChoiceQuestion mc = (MultipleChoiceQuestion) quizExerciseServer.getQuizQuestions().get(0);
+        mc.getAnswerOptions().remove(0);
+        mc.getAnswerOptions().add(new AnswerOption().text("C").hint("H1").explanation("E1").isCorrect(true));
+        mc.getAnswerOptions().add(new AnswerOption().text("D").hint("H1").explanation("E1").isCorrect(true));
+
+        DragAndDropQuestion dnd = (DragAndDropQuestion) quizExerciseServer.getQuizQuestions().get(1);
+        dnd.getDropLocations().remove(0);
+        dnd.getCorrectMappings().remove(0);
+        dnd.getDragItems().remove(0);
+        ShortAnswerQuestion sa = (ShortAnswerQuestion) quizExerciseServer.getQuizQuestions().get(2);
+        sa.getSpots().remove(0);
+        sa.getSolutions().remove(0);
+        sa.getCorrectMappings().remove(0);
+
+        quizExerciseServer.addQuestions(createMultipleChoiceQuestion());
+        quizExerciseServer.addQuestions(createDragAndDropQuestion());
+        quizExerciseServer.addQuestions(createShortAnswerQuestion());
+
+        quizExerciseServer = request.putWithResponseBody("/api/quiz-exercises", quizExerciseServer, QuizExercise.class, HttpStatus.OK);
+        // TODO: add some checks
+    }
+
+    private QuizExercise createQuizOnServer() throws Exception {
         List<Course> courses = database.createCoursesWithExercises();
         Course course = courses.get(0);
 
-        QuizExercise quizExercise = ModelFactory.generateQuizExercise(ZonedDateTime.now().plusSeconds(5), null, course);
+        QuizExercise quizExercise = createQuiz(course);
+        return request.postWithResponseBody("/api/quiz-exercises", quizExercise, QuizExercise.class, HttpStatus.CREATED);
+        // TODO: add some checks
+    }
 
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testDeleteQuizExercise() throws Exception {
+        QuizExercise quizExerciseServer = createQuizOnServer();
+        request.delete("/api/quiz-exercises/" + quizExerciseServer.getId(), HttpStatus.OK);
+        // TODO: check that the quiz was actually deleted from the database
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testGetQuizExercise() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        QuizExercise quizExerciseGet = request.get("/api/quiz-exercises/" + quizExercise.getId(), HttpStatus.OK, QuizExercise.class);
+        QuizExercise quizExerciseForStudent = request.get("/api/quiz-exercises/" + quizExercise.getId() + "/for-student", HttpStatus.OK, QuizExercise.class);
+        List<QuizExercise> allQuizExercisesForCourse = request.getList("/api/courses/" + quizExercise.getCourse().getId() + "/quiz-exercises", HttpStatus.OK, QuizExercise.class);
+        // TODO: add some additional checks for the retrieved data
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testRecalculateStatistics() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        QuizExercise quizExerciseWithRecalculatedStatistics = request.get("/api/quiz-exercises/" + quizExercise.getId() + "/recalculate-statistics", HttpStatus.OK,
+                QuizExercise.class);
+        // TODO: add some additional checks for the retrieved data
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testReevaluateStatistics() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        // we expect a bad request because the quiz has not ended yet
+        request.putWithResponseBody("/api/quiz-exercises-re-evaluate/", quizExercise, QuizExercise.class, HttpStatus.BAD_REQUEST);
+        // TODO: also write one test that expects HttpStatus.OK
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testPerformStartNow() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        QuizExercise updatedQuizExercise = request.putWithResponseBody("/api/quiz-exercises/" + quizExercise.getId() + "/start-now", quizExercise, QuizExercise.class,
+                HttpStatus.OK);
+        // TODO: add some additional checks for the retrieved data
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testPerformSetVisible() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        // we expect a bad request because the quiz is already visible
+        request.putWithResponseBody("/api/quiz-exercises/" + quizExercise.getId() + "/set-visible", quizExercise, QuizExercise.class, HttpStatus.BAD_REQUEST);
+        // TODO: also write one test that expects HttpStatus.OK
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void testPerformOpenForPractice() throws Exception {
+        QuizExercise quizExercise = createQuizOnServer();
+        // we expect a bad request because the quiz has not ended yet
+        request.putWithResponseBody("/api/quiz-exercises/" + quizExercise.getId() + "/open-for-practice", quizExercise, QuizExercise.class, HttpStatus.BAD_REQUEST);
+        // TODO: also write one test that expects HttpStatus.OK
+    }
+
+    @NotNull
+    private QuizExercise createQuiz(Course course) {
+        QuizExercise quizExercise = ModelFactory.generateQuizExercise(ZonedDateTime.now().plusSeconds(5), null, course);
+        quizExercise.addQuestions(createMultipleChoiceQuestion());
+        quizExercise.addQuestions(createDragAndDropQuestion());
+        quizExercise.addQuestions(createShortAnswerQuestion());
+        return quizExercise;
+    }
+
+    @NotNull
+    private ShortAnswerQuestion createShortAnswerQuestion() {
+        ShortAnswerQuestion sa = (ShortAnswerQuestion) new ShortAnswerQuestion().title("SA").score(2).text("This is a long answer text");
+        var shortAnswerSpot1 = new ShortAnswerSpot().spotNr(0).width(1);
+        shortAnswerSpot1.setTempID(generateTempId());
+        var shortAnswerSpot2 = new ShortAnswerSpot().spotNr(2).width(2);
+        shortAnswerSpot2.setTempID(generateTempId());
+        sa.getSpots().add(shortAnswerSpot1);
+        sa.getSpots().add(shortAnswerSpot2);
+        var shortAnswerSolution1 = new ShortAnswerSolution().text("is");
+        shortAnswerSolution1.setTempID(generateTempId());
+        var shortAnswerSolution2 = new ShortAnswerSolution().text("long");
+        shortAnswerSolution2.setTempID(generateTempId());
+        sa.getSolutions().add(shortAnswerSolution1);
+        sa.getSolutions().add(shortAnswerSolution2);
+        sa.getCorrectMappings().add(new ShortAnswerMapping().spot(sa.getSpots().get(0)).solution(sa.getSolutions().get(0)));
+        sa.getCorrectMappings().add(new ShortAnswerMapping().spot(sa.getSpots().get(1)).solution(sa.getSolutions().get(1)));
+        return sa;
+    }
+
+    @NotNull
+    private DragAndDropQuestion createDragAndDropQuestion() {
+        DragAndDropQuestion dnd = (DragAndDropQuestion) new DragAndDropQuestion().title("DnD").score(1).text("Q2");
+        var dropLocation1 = new DropLocation().posX(10).posY(10).height(10).width(10);
+        dropLocation1.setTempID(generateTempId());
+        var dropLocation2 = new DropLocation().posX(20).posY(20).height(10).width(10);
+        dropLocation2.setTempID(generateTempId());
+        dnd.getDropLocations().add(dropLocation1);
+        dnd.getDropLocations().add(dropLocation2);
+        var dragItem1 = new DragItem().text("D1");
+        dragItem1.setTempID(generateTempId());
+        var dragItem2 = new DragItem().text("D2");
+        dragItem2.setTempID(generateTempId());
+        dnd.getDragItems().add(dragItem1);
+        dnd.getDragItems().add(dragItem2);
+        dnd.getCorrectMappings().add(new DragAndDropMapping().dragItem(dragItem1).dropLocation(dropLocation1));
+        dnd.getCorrectMappings().add(new DragAndDropMapping().dragItem(dragItem2).dropLocation(dropLocation2));
+        return dnd;
+    }
+
+    private Long generateTempId() {
+        return ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
+    }
+
+    @NotNull
+    private MultipleChoiceQuestion createMultipleChoiceQuestion() {
         MultipleChoiceQuestion mc = (MultipleChoiceQuestion) new MultipleChoiceQuestion().title("MC").score(1).text("Q1");
         mc.getAnswerOptions().add(new AnswerOption().text("A").hint("H1").explanation("E1").isCorrect(true));
         mc.getAnswerOptions().add(new AnswerOption().text("B").hint("H2").explanation("E2").isCorrect(false));
-
-        DragAndDropQuestion dnd = (DragAndDropQuestion) new DragAndDropQuestion().title("DnD").score(1).text("Q2");
-        dnd.getDropLocations().add(new DropLocation().posX(10).posY(10).height(10).width(10));
-        dnd.getDropLocations().add(new DropLocation().posX(20).posY(20).height(10).width(10));
-        dnd.getDragItems().add(new DragItem().text("D1"));
-        dnd.getDragItems().add(new DragItem().text("D2"));
-        dnd.getCorrectMappings().add(new DragAndDropMapping().dragItem(dnd.getDragItems().get(0)).dropLocation(dnd.getDropLocations().get(0)));
-        dnd.getCorrectMappings().add(new DragAndDropMapping().dragItem(dnd.getDragItems().get(1)).dropLocation(dnd.getDropLocations().get(1)));
-
-        ShortAnswerQuestion sa = (ShortAnswerQuestion) new ShortAnswerQuestion().title("SA").score(2).text("This is a long answer text");
-        sa.getSpots().add(new ShortAnswerSpot().spotNr(0).width(1));
-        sa.getSpots().add(new ShortAnswerSpot().spotNr(2).width(2));
-        sa.getSolutions().add(new ShortAnswerSolution().text("is"));
-        sa.getSolutions().add(new ShortAnswerSolution().text("long"));
-        sa.getCorrectMappings().add(new ShortAnswerMapping().spot(sa.getSpots().get(0)).solution(sa.getSolutions().get(0)));
-        sa.getCorrectMappings().add(new ShortAnswerMapping().spot(sa.getSpots().get(1)).solution(sa.getSolutions().get(1)));
-
-        quizExercise.addQuestions(mc);
-        quizExercise.addQuestions(dnd);
-        quizExercise.addQuestions(sa);
-        QuizExercise quizExerciseServer = request.postWithResponseBody("/api/quiz-exercises", quizExercise, QuizExercise.class);
-
-        // TODO: add some checks
+        return mc;
     }
 
 }
