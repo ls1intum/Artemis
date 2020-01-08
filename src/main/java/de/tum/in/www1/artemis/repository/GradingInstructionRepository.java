22b4b75f3c747bbea9b677bc464ae2fdc8ae913b
Hanya Elhashemy <ga87saf@mytum.de>
2020-01-08 15:33:20 +0100

    fix java code style

--- src/main/java/de/tum/in/www1/artemis/repository/GradingInstructionRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/GradingInstructionRepository.java
@@ -3,7 +3,6 @@ package de.tum.in.www1.artemis.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.GradingInstruction;
