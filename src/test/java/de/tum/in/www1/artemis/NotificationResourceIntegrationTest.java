f9b398bf526e7ab61556a5d9949043809d6e8c64
= <minh.tran@tum.de>
2020-01-11 21:25:33 +0100

    chore(notification-resource): update test case

--- src/test/java/de/tum/in/www1/artemis/NotificationResourceIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/NotificationResourceIntegrationTest.java
@@ -1,5 +1,9 @@
 package de.tum.in.www1.artemis;
 
+import de.tum.in.www1.artemis.domain.User;
+import de.tum.in.www1.artemis.repository.NotificationRepository;
+import de.tum.in.www1.artemis.util.DatabaseUtilService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +13,28 @@ import org.springframework.security.test.context.support.WithMockUser;
 import de.tum.in.www1.artemis.domain.Notification;
 import de.tum.in.www1.artemis.domain.SingleUserNotification;
 import de.tum.in.www1.artemis.util.RequestUtilService;
+import de.tum.in.www1.artemis.util.ModelFactory;
 
 public class NotificationResourceIntegrationTest extends AbstractSpringIntegrationTest {
+    User user;
 
-    private Notification notification;
+    Notification notification;
 
     @Autowired
     RequestUtilService request;
 
+
+    @Autowired
+    NotificationRepository notificationRepository;
+
     @BeforeEach
-    public void init() throws Exception {
-        notification = new SingleUserNotification();
+    public void initTestCase() throws Exception {
+        user = ModelFactory.generateActivatedUser("");
+        notification = new SingleUserNotification().recipient(user).title("test title").text("test test");
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
     }
 
     @Test
