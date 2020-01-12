6ef724c632c573ae14f36c70c6c5c2d99f7f7366
Martin Wauligmann <martin@wauligmann.com>
2020-01-12 13:59:23 +0100

    Add comment

--- src/main/webapp/app/submission-result-status/submission-result-status.component.ts
+++ src/main/webapp/app/submission-result-status/submission-result-status.component.ts
@@ -9,6 +9,13 @@ import { StudentParticipation } from 'app/entities/participation';
 export class SubmissionResultStatusComponent {
     readonly ExerciseType = ExerciseType;
 
+    /**
+     * @property exercise Exercise to which the submission's participation belongs
+     * @property studentParticipation Participation to which the submission belongs (optional, used for updating-result)
+     * @property updatingResultClass Class(es) that will be applied to the updating-result component
+     * @property showGradedBadge Flag whether a colored badge (saying e.g. "Graded") should be shown
+     * @property short Flag whether the short version of the result text should be used
+     */
     @Input() exercise: Exercise;
     @Input() studentParticipation: StudentParticipation | null;
     @Input() updatingResultClass: string;
