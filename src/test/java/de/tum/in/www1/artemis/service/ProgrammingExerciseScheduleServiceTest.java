2f6e70b231f4b6752efeb963cb759dcb3891e5a1
Stephan Krusche <s.krusche@tum.de>
2020-01-12 17:19:02 +0100

    change mockbean to spybean

--- src/test/java/de/tum/in/www1/artemis/service/ProgrammingExerciseScheduleServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/ProgrammingExerciseScheduleServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationTest;
 import de.tum.in.www1.artemis.domain.ProgrammingExercise;
@@ -22,6 +21,7 @@ import de.tum.in.www1.artemis.repository.ProgrammingExerciseRepository;
 import de.tum.in.www1.artemis.service.scheduled.ProgrammingExerciseScheduleService;
 import de.tum.in.www1.artemis.util.DatabaseUtilService;
 import de.tum.in.www1.artemis.util.TimeService;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 class ProgrammingExerciseScheduleServiceTest extends AbstractSpringIntegrationTest {
 
@@ -31,7 +31,7 @@ class ProgrammingExerciseScheduleServiceTest extends AbstractSpringIntegrationTe
     @Autowired
     ProgrammingExerciseRepository programmingExerciseRepository;
 
-    @MockBean
+    @SpyBean
     ProgrammingSubmissionService programmingSubmissionService;
 
     @Autowired
