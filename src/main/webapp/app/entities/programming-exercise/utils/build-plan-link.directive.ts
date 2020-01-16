1b7cbe9790e561423df437629a6ebd5416d51f88
Alexander Ungar <alexander.ungar@tum.de>
2020-01-16 18:34:40 +0100

    Add JSDoc

--- src/main/webapp/app/entities/programming-exercise/utils/build-plan-link.directive.ts
+++ src/main/webapp/app/entities/programming-exercise/utils/build-plan-link.directive.ts
@@ -3,6 +3,13 @@ import { take, tap } from 'rxjs/operators';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
 import { ProfileInfo } from 'app/layouts';
 
+/**
+ * Creates the build pan URL for the given template URL.
+ *
+ * @param template The URL to the CI with placeholders for the plan ID and project key
+ * @param projectKey The project key of the programming exercise
+ * @param buildPlanId The ID of the build plan for which to construct the URL
+ */
 export const createBuildPlanUrl = (template: string, projectKey: string, buildPlanId: string) => {
     return template.replace('{buildPlanId}', buildPlanId).replace('{projectKey}', projectKey);
 };
