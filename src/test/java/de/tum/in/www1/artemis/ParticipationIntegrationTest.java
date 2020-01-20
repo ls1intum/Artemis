d71f2f2f0bdd5ddec95e658b35f4058fca658f9f
Stephan Krusche <s.krusche@tum.de>
2020-01-21 00:06:04 +0100

    fix java code style

--- src/test/java/de/tum/in/www1/artemis/ParticipationIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/ParticipationIntegrationTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.net.URI;
 import java.util.Optional;
 
-import de.tum.in.www1.artemis.domain.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.modeling.ModelingExercise;
 import de.tum.in.www1.artemis.domain.modeling.ModelingSubmission;
 import de.tum.in.www1.artemis.domain.participation.Participation;
