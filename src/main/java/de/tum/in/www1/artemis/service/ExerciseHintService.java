245361db4e58d5de455e8df98ad0402fdda231f8
Stephan Krusche <s.krusche@tum.de>
2020-01-20 12:23:42 +0100

    update dependencies and test configuration, fix some spring integration test problems
    we should prefer @EntityGraph instead of @Query if possible in JPA repositories, because this does not cause problems with the security context
    remove @Transactional in many places where it was not necessary

--- src/main/java/de/tum/in/www1/artemis/service/ExerciseHintService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ExerciseHintService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.Exercise;
 import de.tum.in.www1.artemis.domain.ExerciseHint;
@@ -16,7 +15,6 @@ import de.tum.in.www1.artemis.repository.ExerciseHintRepository;
  * Service Implementation for managing {@link ExerciseHint}.
  */
 @Service
-@Transactional
 public class ExerciseHintService {
 
     private final Logger log = LoggerFactory.getLogger(ExerciseHintService.class);
@@ -43,7 +41,6 @@ public class ExerciseHintService {
      *
      * @return the list of entities.
      */
-    @Transactional(readOnly = true)
     public List<ExerciseHint> findAll() {
         log.debug("Request to get all ExerciseHints");
         return exerciseHintRepository.findAll();
@@ -55,7 +52,6 @@ public class ExerciseHintService {
      * @param id the id of the entity.
      * @return the entity.
      */
-    @Transactional(readOnly = true)
     public Optional<ExerciseHint> findOne(Long id) {
         log.debug("Request to get ExerciseHint : {}", id);
         return exerciseHintRepository.findById(id);
@@ -66,7 +62,6 @@ public class ExerciseHintService {
      * @param exerciseId of exercise.
      * @return hints of provided exercise.
      */
-    @Transactional(readOnly = true)
     public Set<ExerciseHint> findByExerciseId(Long exerciseId) {
         log.debug("Request to get ExerciseHints by Exercise id : {}", exerciseId);
         return exerciseHintRepository.findByExerciseId(exerciseId);
--- src/main/java/de/tum/in/www1/artemis/service/StudentQuestionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/StudentQuestionService.java
@@ -3,13 +3,11 @@ package de.tum.in.www1.artemis.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.StudentQuestion;
 import de.tum.in.www1.artemis.repository.StudentQuestionRepository;
 
 @Service
-@Transactional
 public class StudentQuestionService {
 
     StudentQuestionRepository studentQuestionRepository;
