721ba5b05970ad68eee3a303fdd8effb65230509
Hanya Elhashemy <ga87saf@mytum.de>
2020-01-20 20:20:15 +0100

    add warning to jhi-assessment-dashboard

--- src/main/webapp/app/assessment-shared/assessment-warning/assessment-warning.component.ts
+++ src/main/webapp/app/assessment-shared/assessment-warning/assessment-warning.component.ts
@@ -1,7 +1,10 @@
 import { Component, SimpleChanges, Input, OnChanges } from '@angular/core';
 import * as moment from 'moment';
 import { Exercise } from 'app/entities/exercise';
-
+/*
+Warn instructors in the assessment and submission page
+if the due date is not over yet as they are allowed to assess submissions there
+ */
 @Component({
     selector: 'jhi-assessment-warning',
     templateUrl: './assessment-warning.component.html',
