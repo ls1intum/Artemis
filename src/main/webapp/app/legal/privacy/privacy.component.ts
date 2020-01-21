87620e8f08df2364f32faee8e5456a293269f90f
Alexander Ungar <alexander.ungar@tum.de>
2020-01-21 20:29:05 +0100

    Show static content in client
    Show static company logo
    show static privacy statement

--- src/main/webapp/app/legal/privacy/privacy.component.ts
+++ src/main/webapp/app/legal/privacy/privacy.component.ts
@@ -1,13 +1,26 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SafeHtml } from '@angular/platform-browser';
+import { StaticHtmlContentService } from 'app/shared/service/static-html-content.service';
+
+const privacyStatementFile = 'privacy_statement.html';
 
 @Component({
     selector: 'jhi-privacy',
-    templateUrl: './privacy.component.html',
+    template: `
+        <h3 jhiTranslate="legal.privacy.title">Datenschutzerklärung</h3>
+        <div [innerHTML]="privacyStatement"></div>
+    `,
     styles: [],
 })
-export class PrivacyComponent implements AfterViewInit {
-    constructor(private route: ActivatedRoute) {}
+export class PrivacyComponent implements AfterViewInit, OnInit {
+    privacyStatement: SafeHtml;
+
+    constructor(private route: ActivatedRoute, private staticContentService: StaticHtmlContentService) {}
+
+    ngOnInit(): void {
+        this.staticContentService.getStaticHtmlFromArtemisServer(privacyStatementFile).subscribe(statement => (this.privacyStatement = statement));
+    }
 
     ngAfterViewInit(): void {
         this.route.params.subscribe(params => {
