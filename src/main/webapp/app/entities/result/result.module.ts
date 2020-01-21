e1c7bbb0461fce5d84f0961b89d891f6a7338194
Martin Wauligmann <martin@wauligmann.com>
2020-01-21 21:05:28 +0100

    New component submission result status (#1181)

--- src/main/webapp/app/entities/result/result.module.ts
+++ src/main/webapp/app/entities/result/result.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { ArtemisSharedModule } from 'app/shared';
-import { ResultComponent, ResultDetailComponent, ResultService, UpdatingResultComponent } from './';
+import { ResultComponent, ResultDetailComponent, ResultService, UpdatingResultComponent, SubmissionResultStatusComponent } from './';
 import { MomentModule } from 'ngx-moment';
 import { ResultHistoryComponent } from 'app/entities/result/result-history.component';
 import { ArtemisProgrammingSubmissionModule } from 'app/programming-submission/programming-submission.module';
+import { ArtemisProgrammingExerciseActionsModule } from 'app/entities/programming-exercise/actions/programming-exercise-actions.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingSubmissionModule],
-    declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent],
-    exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent],
-    entryComponents: [ResultComponent, UpdatingResultComponent, ResultDetailComponent],
+    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingSubmissionModule, ArtemisProgrammingExerciseActionsModule],
+    declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
+    exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
+    entryComponents: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, SubmissionResultStatusComponent],
     providers: [ResultService],
 })
 export class ArtemisResultModule {}
