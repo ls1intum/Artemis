f6d6d6d2057a704a94eb1e53c9e9ce60f73439c2
Stephan Krusche <s.krusche@tum.de>
2020-01-22 13:59:55 +0100

    fix java code style

--- src/main/java/de/tum/in/www1/artemis/repository/ComplaintResponseRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ComplaintResponseRepository.java
@@ -3,7 +3,6 @@ package de.tum.in.www1.artemis.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.ComplaintResponse;
