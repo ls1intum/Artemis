585974a27782dd3315f3888e058fd51f40779d8f
Alexander Ungar <alexander.ungar@tum.de>
2020-01-23 18:26:23 +0100

    Fix Mocked Jira Requests and Test Application Properties (#1205)

--- src/test/java/de/tum/in/www1/artemis/connector/jira/JiraRequestMockProvider.java
+++ src/test/java/de/tum/in/www1/artemis/connector/jira/JiraRequestMockProvider.java
@@ -51,7 +51,8 @@ public class JiraRequestMockProvider {
         final var regexGroups = String.join("|", groups);
         final var uriPattern = Pattern.compile(JIRA_URL + "/rest/api/2/group/member\\?groupname=(" + regexGroups + ")");
 
-        mockServer.expect(ExpectedCount.twice(), requestTo(MatchesPattern.matchesPattern(uriPattern))).andExpect(method(HttpMethod.GET)).andRespond(withStatus(HttpStatus.OK));
+        mockServer.expect(ExpectedCount.times(groups.size()), requestTo(MatchesPattern.matchesPattern(uriPattern))).andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK));
     }
 
     public void mockAddUserToGroup(Set<String> groups) throws URISyntaxException {
@@ -59,7 +60,8 @@ public class JiraRequestMockProvider {
         final var regexGroups = String.join("|", groups);
         final var uriPattern = Pattern.compile(JIRA_URL + "/rest/api/2/group/user\\?groupname=(" + regexGroups + ")");
 
-        mockServer.expect(ExpectedCount.twice(), requestTo(MatchesPattern.matchesPattern(uriPattern))).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+        mockServer.expect(ExpectedCount.times(groups.size()), requestTo(MatchesPattern.matchesPattern(uriPattern))).andExpect(method(HttpMethod.POST))
+                .andRespond(withStatus(HttpStatus.OK));
     }
 
     public void mockRemoveUserFromGroup(Set<String> groups, String username) {
