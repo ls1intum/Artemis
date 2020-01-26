0fe5e478f14281c6ae8e6a93d737f61e715bd5c3
Alexander Ungar <alexander.ungar@tum.de>
2020-01-26 20:29:26 +0100

    Move default static content to resources

--- src/main/java/de/tum/in/www1/artemis/config/PublicResourcesConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/PublicResourcesConfiguration.java
@@ -11,8 +11,10 @@ public class PublicResourcesConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Static assets, accessible without authorization.
+        // Allowed locations are under $artemisRunDir/public/** and resource/public/**
         final var userDir = System.getProperty("user.dir");
         final var publicFiles = Path.of(userDir, "public");
-        registry.addResourceHandler("/public/**").addResourceLocations("file:" + publicFiles.toString() + "/");
+        registry.addResourceHandler("/public/**").addResourceLocations("file:" + publicFiles.toString() + "/", "classpath:public/");
     }
 }
