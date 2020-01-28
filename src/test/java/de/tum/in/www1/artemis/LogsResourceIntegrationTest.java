a8e27c1797e2d91e65bdd29e31e69ea0358fa8d2
= <minh.tran@tum.de>
2020-01-28 01:30:54 +0100

    test(logs-resource): add integration test for logs resource

---
+++ src/test/java/de/tum/in/www1/artemis/LogsResourceIntegrationTest.java
@@ -0,0 +1,31 @@
+package de.tum.in.www1.artemis;
+
+import java.util.List;
+import de.tum.in.www1.artemis.AbstractSpringIntegrationTest;
+import de.tum.in.www1.artemis.util.RequestUtilService;
+import de.tum.in.www1.artemis.web.rest.vm.LoggerVM;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+
+public class LogsResourceIntegrationTest extends AbstractSpringIntegrationTest {
+
+    @Autowired
+    RequestUtilService request;
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void testGetList() throws Exception {
+        request.get("/management/logs", HttpStatus.OK, List.class);
+    }
+
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void testChangeLevel() throws Exception {
+        LoggerVM logger = new LoggerVM();
+        logger.setLevel("1");
+        logger.setName("logger");
+        request.put("/management/logs", logger, HttpStatus.NO_CONTENT);
+    }
+}
