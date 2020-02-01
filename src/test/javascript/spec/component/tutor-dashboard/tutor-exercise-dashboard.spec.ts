119035b26d4397971a9527cba1e8ff0835a1e112
Anh Montag <anh.montag@tum.de>
2020-02-01 03:30:42 +0100

    Fix client tests

--- src/test/javascript/spec/component/tutor-dashboard/tutor-exercise-dashboard.spec.ts
+++ src/test/javascript/spec/component/tutor-dashboard/tutor-exercise-dashboard.spec.ts
@@ -32,6 +32,9 @@ import { ArtemisProgrammingAssessmentModule } from 'app/programming-assessment/p
 import { ArtemisProgrammingExerciseInstructionsRenderModule } from 'app/entities/programming-exercise/instructions/instructions-render';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { ArtemisAssessmentSharedModule } from 'app/assessment-shared/assessment-shared.module';
+import { GuidedTourMapping } from 'app/guided-tour/guided-tour-setting.model';
+import { GuidedTourService } from 'app/guided-tour/guided-tour.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -40,8 +43,8 @@ describe('TutorExerciseDashboardComponent', () => {
     let comp: TutorExerciseDashboardComponent;
     let fixture: ComponentFixture<TutorExerciseDashboardComponent>;
     let modelingSubmissionService: ModelingSubmissionService;
-
     let modelingSubmissionStub: SinonStub;
+    let guidedTourService: GuidedTourService;
 
     const exercise = { id: 20, type: ExerciseType.MODELING, tutorParticipations: [{ status: TutorParticipationStatus.TRAINED }] } as ModelingExercise;
     const submission = { id: 30 } as ModelingSubmission;
@@ -72,6 +75,7 @@ describe('TutorExerciseDashboardComponent', () => {
             ],
             providers: [
                 JhiLanguageHelper,
+                DeviceDetectorService,
                 { provide: JhiAlertService, useClass: MockAlertService },
                 { provide: ActivatedRoute, useClass: MockActivatedRoute },
                 { provide: Router, useClass: MockRouter },
@@ -101,6 +105,7 @@ describe('TutorExerciseDashboardComponent', () => {
                 fixture = TestBed.createComponent(TutorExerciseDashboardComponent);
                 comp = fixture.componentInstance;
                 modelingSubmissionService = TestBed.get(ModelingSubmissionService);
+                guidedTourService = TestBed.get(GuidedTourService);
 
                 comp.exerciseId = exercise.id;
 
@@ -113,6 +118,9 @@ describe('TutorExerciseDashboardComponent', () => {
     });
 
     it('should set unassessedSubmission if lock limit is not reached', () => {
+        const guidedTourMapping = {} as GuidedTourMapping;
+        spyOn<any>(guidedTourService, 'checkTourState').and.returnValue(true);
+        guidedTourService.guidedTourMapping = guidedTourMapping;
         modelingSubmissionStub.returns(of(submission));
 
         comp.loadAll();
