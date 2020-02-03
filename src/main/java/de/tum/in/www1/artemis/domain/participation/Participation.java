f68493761d0c9b9b0295288db9d8cfb67b981c9e
Stephan Krusche <s.krusche@tum.de>
2020-02-03 15:05:47 +0100

    add @DiscriminatorOptions(force = true) to all abstract entities

--- src/main/java/de/tum/in/www1/artemis/domain/participation/Participation.java
+++ src/main/java/de/tum/in/www1/artemis/domain/participation/Participation.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.DiscriminatorOptions;
 
 import com.fasterxml.jackson.annotation.*;
 
@@ -23,6 +24,7 @@ import de.tum.in.www1.artemis.domain.view.QuizView;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "discriminator", discriminatorType = DiscriminatorType.STRING)
 @DiscriminatorValue(value = "P")
+@DiscriminatorOptions(force = true)
 // NOTE: Use strict cache to prevent lost updates when updating statistics in semaphore (see StatisticService.java)
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
