e9cb357434e724a96d67c54c3517ae7702537683
Maksym Radchuk <maxr96@gmail.com>
2020-02-15 23:23:47 +0100

    Removed unused code

--- src/test/java/de/tum/in/www1/artemis/ParticipationSubmissionIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/ParticipationSubmissionIntegrationTest.java
@@ -2,7 +2,6 @@ package de.tum.in.www1.artemis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
@@ -15,7 +14,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import de.tum.in.www1.artemis.domain.Submission;
 import de.tum.in.www1.artemis.domain.TextExercise;
 import de.tum.in.www1.artemis.domain.TextSubmission;
-import de.tum.in.www1.artemis.domain.modeling.ModelingSubmission;
 import de.tum.in.www1.artemis.repository.ExerciseRepository;
 import de.tum.in.www1.artemis.repository.ResultRepository;
 import de.tum.in.www1.artemis.repository.SubmissionRepository;
@@ -93,50 +91,5 @@ public class ParticipationSubmissionIntegrationTest extends AbstractSpringIntegr
     @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
     public void deleteParticipation_notFound() throws Exception {
         request.delete("/api/submissions/" + 1, HttpStatus.NOT_FOUND);
-    }
-
-    @Test
-    @WithMockUser(username = "student1", roles = "USER")
-    public void createSubmission() throws Exception {
-        request.postWithResponseBody("/api/submissions", new TextSubmission(), Submission.class, HttpStatus.CREATED);
-    }
-
-    @Test
-    @WithMockUser(username = "student1", roles = "USER")
-    public void createSubmission_withId() throws Exception {
-        var submission = new TextSubmission();
-        submission.setId(1L);
-        request.postWithResponseBody("/api/submissions", submission, Submission.class, HttpStatus.BAD_REQUEST);
-    }
-
-    @Test
-    @WithMockUser(username = "student1", roles = "USER")
-    public void updateSubmission() throws Exception {
-        var submission = new TextSubmission();
-        submissionRepository.save(submission);
-        request.putWithResponseBody("/api/submissions", submission, Submission.class, HttpStatus.OK);
-    }
-
-    @Test
-    @WithMockUser(username = "student1", roles = "USER")
-    public void updateSubmission_noId() throws Exception {
-        var submission = new TextSubmission();
-        request.putWithResponseBody("/api/submissions", submission, Submission.class, HttpStatus.CREATED);
-    }
-
-    @Test
-    @WithMockUser(username = "tutor1", roles = "TA")
-    public void getAllSubmissions() throws Exception {
-        submissionRepository.save(new TextSubmission());
-        submissionRepository.save(new ModelingSubmission());
-        List<Submission> submissions = request.getList("/api/submissions", HttpStatus.OK, Submission.class);
-        assertThat(submissions.size()).isEqualTo(2);
-    }
-
-    @Test
-    @WithMockUser(username = "tutor1", roles = "TA")
-    public void getSubmission() throws Exception {
-        var submission = submissionRepository.save(new TextSubmission());
-        request.get("/api/submissions/" + submission.getId(), HttpStatus.OK, Submission.class);
     }
 }
