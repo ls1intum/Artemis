a60b9cab3182c03eed0def4d6ef4e5cd49e90da7
Stephan Krusche <s.krusche@tum.de>
2020-02-17 13:44:32 +0100

    update alert with newest jhipster version and use a custom alert subclass as workaround

--- src/main/webapp/app/blocks/interceptor/notification.interceptor.ts
+++ src/main/webapp/app/blocks/interceptor/notification.interceptor.ts
@@ -1,12 +1,12 @@
-import { JhiAlertService } from 'ng-jhipster';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { AlertService } from 'app/core/alert/alert.service';
 
 @Injectable()
 export class NotificationInterceptor implements HttpInterceptor {
-    constructor(private alertService: JhiAlertService) {}
+    constructor(private alertService: AlertService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
@@ -23,10 +23,9 @@ export class NotificationInterceptor implements HttpInterceptor {
                         }
                     });
 
-                    // TODO: deactivate due to problems in ng-jhipster with the new Angular 9 configuration
-                    // if (alert) {
-                    // this.alertService.success(alert, { param: alertParams });
-                    // }
+                    if (alert) {
+                        this.alertService.success(alert, { param: alertParams });
+                    }
                 }
             }),
         );
