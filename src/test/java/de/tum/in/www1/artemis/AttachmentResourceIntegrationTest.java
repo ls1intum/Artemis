7c0faf4f32e86f2e662270cbabddfce75a8c9851
Maksym Radchuk <maxr96@gmail.com>
2020-02-18 08:09:24 +0100

    Add tests for getting attachment

--- src/test/java/de/tum/in/www1/artemis/AttachmentResourceIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/AttachmentResourceIntegrationTest.java
@@ -10,11 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import de.tum.in.www1.artemis.domain.Attachment;
+import de.tum.in.www1.artemis.domain.Lecture;
 import de.tum.in.www1.artemis.domain.enumeration.AttachmentType;
-import de.tum.in.www1.artemis.repository.AttachmentRepository;
-import de.tum.in.www1.artemis.repository.CourseRepository;
-import de.tum.in.www1.artemis.repository.CustomAuditEventRepository;
-import de.tum.in.www1.artemis.repository.UserRepository;
+import de.tum.in.www1.artemis.repository.*;
 import de.tum.in.www1.artemis.util.DatabaseUtilService;
 import de.tum.in.www1.artemis.util.RequestUtilService;
 
@@ -38,13 +36,20 @@ public class AttachmentResourceIntegrationTest extends AbstractSpringIntegration
     @Autowired
     UserRepository userRepo;
 
+    @Autowired
+    LectureRepository lectureRepository;
+
     private Attachment attachment;
+
+    private Lecture lecture;
 
     @BeforeEach
     public void initTestCase() {
         database.addUsers(1, 1, 1);
 
         attachment = new Attachment().attachmentType(AttachmentType.FILE).link("files/temp/example.txt").name("example");
+
+        lecture = new Lecture().title("test").description("test");
     }
 
     @AfterEach
@@ -76,5 +81,32 @@ public class AttachmentResourceIntegrationTest extends AbstractSpringIntegration
         var expectedAttachment = attachmentRepository.findById(actualAttachment.getId()).get();
         assertThat(actualAttachment.getName()).isEqualTo("new name");
         assertThat(actualAttachment).isEqualToIgnoringGivenFields(expectedAttachment, "name");
+    }
+
+    @Test
+    @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
+    public void updateAttachment_noId() throws Exception {
+        attachment.setName("new name");
+        request.putWithResponseBody("/api/attachments", attachment, Attachment.class, HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
+    public void getAttachment() throws Exception {
+        attachment = attachmentRepository.save(attachment);
+        attachment.setName("new name");
+        var actualAttachment = request.get("/api/attachments/" + attachment.getId(), HttpStatus.OK, Attachment.class);
+        assertThat(actualAttachment).isEqualTo(attachment);
+    }
+
+    @Test
+    @WithMockUser(username = "tutor1", roles = "TA")
+    public void getAttachmentsForLecture() throws Exception {
+        lecture = lectureRepository.save(lecture);
+        attachment.setLecture(lecture);
+        attachment = attachmentRepository.save(attachment);
+        var actualAttachments = request.getList("/api/lectures/" + lecture.getId() + "/attachments", HttpStatus.OK, Attachment.class);
+        assertThat(actualAttachments.size()).isEqualTo(1);
+        assertThat(actualAttachments.stream().findFirst().get()).isEqualTo(attachment);
     }
 }
