02b1562b49ff1581b578491aa0798c5c15e4c6b2
Hanya Elhashemy <ga87saf@mytum.de>
2020-02-18 20:23:35 +0100

    add new switch toggle component to allow switching between SGI and GI

---
+++ src/main/webapp/app/components/slide-toggle/slide-toggle.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'jhi-slide-toggle',
+    template: `
+        <!-- Default switch -->
+        <div class="custom-control custom-switch">
+            <input type="checkbox" class="custom-control-input" id="customSwitches" [(ngModel)]="checked" (change)="toggleAll(checked)" />
+            <label class="custom-control-label" for="customSwitches"></label>
+        </div>
+        <label *ngIf="checked === false" jhiTranslate="artemisApp.exercise.gradingInstructions"> Grading Instructions</label>
+        <label *ngIf="checked === true" jhiTranslate="artemisApp.exercise.structuredGradingInstructions"> Structured Grading Instructions</label>
+    `,
+    styleUrls: ['./slide-toggle.scss'],
+})
+export class SlideToggleComponent implements OnInit {
+    checked: boolean;
+
+    constructor() {}
+
+    ngOnInit() {
+        this.checked = false;
+    }
+    toggleAll(checked: boolean) {}
+}
