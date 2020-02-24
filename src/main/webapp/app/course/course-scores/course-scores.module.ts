7061139930700bf75bb59df6e86fdd8e403837c7
Alexander Ungar <alexander.ungar@tum.de>
2020-02-24 14:54:53 +0100

    Move scores component to new management route

--- src/main/webapp/app/course/course-scores/course-scores.module.ts
+++ src/main/webapp/app/course/course-scores/course-scores.module.ts
@@ -1,25 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 import { MomentModule } from 'ngx-moment';
 import { CourseScoresComponent } from './course-scores.component';
 import { SortByModule } from 'app/shared/pipes/sort-by.module';
-
-const ENTITY_STATES = [
-    {
-        path: 'course/:courseId/dashboard',
-        component: CourseScoresComponent,
-        data: {
-            authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR', 'ROLE_TA'],
-            pageTitle: 'instructorDashboard.title',
-        },
-        canActivate: [UserRouteAccessService],
-    },
-];
+import { ArtemisCourseScoresRoutingModule } from 'app/course/course-scores/course-scores-routing.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, MomentModule, RouterModule.forChild(ENTITY_STATES), SortByModule],
+    imports: [ArtemisSharedModule, MomentModule, ArtemisCourseScoresRoutingModule, SortByModule],
     declarations: [CourseScoresComponent],
 })
 export class ArtemisCourseScoresModule {}
