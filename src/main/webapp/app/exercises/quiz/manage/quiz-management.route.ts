1dfe6cfecee6dba6950547b38e42817c73101748
Alexander Ungar <alexander.ungar@tum.de>
2020-02-25 12:06:49 +0100

    Remove unused resolver of quiz-management.route.ts

--- src/main/webapp/app/exercises/quiz/manage/quiz-management.route.ts
+++ src/main/webapp/app/exercises/quiz/manage/quiz-management.route.ts
@@ -1,46 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
-import { QuizExerciseService } from './quiz-exercise.service';
 import { QuizExerciseComponent } from './quiz-exercise.component';
 import { QuizExerciseDetailComponent } from './quiz-exercise-detail.component';
 import { QuizExerciseExportComponent } from './quiz-exercise-export.component';
 import { PendingChangesGuard } from 'app/shared/guard/pending-changes.guard';
 import { QuizReEvaluateComponent } from 'app/exercises/quiz/manage/re-evaluate/quiz-re-evaluate.component';
-import { CourseManagementService } from '../../../course/manage/course-management.service';
-import { Course } from 'app/entities/course.model';
-
-@Injectable({ providedIn: 'root' })
-export class QuizExerciseResolve implements Resolve<QuizExercise> {
-    constructor(private quizExerciseService: QuizExerciseService, private courseService: CourseManagementService) {}
-
-    /**
-     * Resolves the route and initializes quiz exercise either from exerciseId (existing exercise) or
-     * from course id (new exercise)
-     * @param route
-     * @param state
-     */
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (route.params['exerciseId']) {
-            return this.quizExerciseService.find(route.params['exerciseId']).pipe(
-                filter(res => !!res.body),
-                map((quizExercise: HttpResponse<QuizExercise>) => quizExercise.body!),
-            );
-        } else if (route.params['courseId']) {
-            return this.courseService.find(route.params['courseId']).pipe(
-                filter(res => !!res.body),
-                map((course: HttpResponse<Course>) => {
-                    return new QuizExercise(course.body!);
-                }),
-            );
-        }
-        return Observable.of(new QuizExercise());
-    }
-}
 
 export const quizManagementRoute: Routes = [
     {
