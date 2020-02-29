6664c732ed75748edf37f4449b0d7d5d2ae19efa
Stephan Krusche <s.krusche@tum.de>
2020-02-29 17:02:25 +0100

    add updates for Jhipster 6.7.1

--- src/main/java/de/tum/in/www1/artemis/ApplicationWebXml.java
+++ src/main/java/de/tum/in/www1/artemis/ApplicationWebXml.java
@@ -6,16 +6,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import io.github.jhipster.config.DefaultProfileUtil;
 
 /**
- * This is a helper Java class that provides an alternative to creating a web.xml. This will be invoked only when the application is deployed to a Servlet container like Tomcat,
- * JBoss etc.
+ * This is a helper Java class that provides an alternative to creating a {@code web.xml}.
+ * This will be invoked only when the application is deployed to a Servlet container like Tomcat, JBoss etc.
  */
 public class ApplicationWebXml extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        /**
-         * set a default to use when no profile is configured.
-         */
+        // set a default to use when no profile is configured.
         DefaultProfileUtil.addDefaultProfile(application.application());
         return application.sources(ArtemisApp.class);
     }
