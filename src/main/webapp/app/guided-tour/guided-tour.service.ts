ac7aee51c0d539e6976f70c7c0efc49f3b21cbe5
Anh Montag <anh.montag@tum.de>
2020-03-02 21:29:04 +0100

    Enhancement Guided Tour: Add tests and prefixes for tutorial selectors (#1259)

--- src/main/webapp/app/guided-tour/guided-tour.service.ts
+++ src/main/webapp/app/guided-tour/guided-tour.service.ts
@@ -390,15 +390,19 @@ export class GuidedTourService {
      * Skip current guided tour after updating the guided tour settings in the database and calling the reset tour method to remove current tour elements.
      */
     public skipTour(): void {
-        if (this.currentTour) {
-            if (this.currentTour.skipCallback) {
-                this.currentTour.skipCallback(this.currentTourStepIndex);
-            }
+        if (!this.currentTour) {
+            return;
         }
+
+        if (this.currentTour.skipCallback) {
+            this.currentTour.skipCallback(this.currentTourStepIndex);
+        }
+
         if (this.currentTour === cancelTour || this.currentTour === completedTour) {
             this.resetTour();
             return;
         }
+
         if (this.currentTourStepIndex + 1 === this.getFilteredTourSteps().length) {
             this.finishGuidedTour();
         } else {
@@ -480,7 +484,7 @@ export class GuidedTourService {
      * Get the last step that the user visited during the given tour
      */
     public getLastSeenTourStepIndex(): number {
-        if (!this.availableTourForComponent) {
+        if (!this.availableTourForComponent || !this.guidedTourSettings) {
             return 0;
         }
         const tourSettings = this.guidedTourSettings.filter(setting => setting.guidedTourKey === this.availableTourForComponent!.settingsKey);
