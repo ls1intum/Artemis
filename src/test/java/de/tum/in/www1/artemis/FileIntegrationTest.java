1ca3e32fd859c3605ad8c37afd1ed10c338615ba
Clemens Zuck <clemens@zuck-online.de>
2020-03-09 19:06:08 +0100

    created FileIntegrationTest.java

--- src/test/java/de/tum/in/www1/artemis/FileIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/FileIntegrationTest.java
@@ -1,4 +1,79 @@
 package de.tum.in.www1.artemis;
 
-public class FileIntegrationTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.util.LinkedMultiValueMap;
+
+import de.tum.in.www1.artemis.domain.*;
+import de.tum.in.www1.artemis.domain.participation.StudentParticipation;
+import de.tum.in.www1.artemis.repository.*;
+import de.tum.in.www1.artemis.service.FileUploadSubmissionService;
+import de.tum.in.www1.artemis.service.ParticipationService;
+import de.tum.in.www1.artemis.util.DatabaseUtilService;
+import de.tum.in.www1.artemis.util.ModelFactory;
+import de.tum.in.www1.artemis.util.RequestUtilService;
+
+public class FileIntegrationTest extends AbstractSpringIntegrationTest {
+
+    public static final String API_FILE_UPLOAD_SUBMISSIONS = "/api/file-upload-submissions/";
+
+    @Autowired
+    CourseRepository courseRepo;
+
+    @Autowired
+    ExerciseRepository exerciseRepo;
+
+    @Autowired
+    ModelAssessmentConflictRepository conflictRepo;
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    RequestUtilService request;
+
+    @Autowired
+    DatabaseUtilService database;
+
+    @Autowired
+    FileUploadSubmissionService fileUploadSubmissionService;
+
+    @Autowired
+    FileUploadSubmissionRepository fileUploadSubmissionRepository;
+
+    @Autowired
+    ResultRepository resultRepo;
+
+    @Autowired
+    ParticipationService participationService;
+
+    @Autowired
+    ComplaintRepository complaintRepo;
+
+    private FileUploadExercise fileUploadExercise;
+
+    @BeforeEach
+    public void initTestCase() {
+        database.addUsers(2, 2, 1);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        database.resetDatabase();
+    }
+
+    @Test
+    @WithMockUser(value = "instructor1", roles = "INSTRUCTOR")
+    public void saveFile() throws Exception {
+        var file = new MockMultipartFile("file", "file.png", "application/json", "some data".getBytes());
+        String filepath = request.postWithMultipartFile("/api/fileUpload", true,"keepFileName",  file, String.class, HttpStatus.OK);
+    }
 }
