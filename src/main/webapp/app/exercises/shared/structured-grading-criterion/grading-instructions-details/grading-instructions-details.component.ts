5519af5be09bf789a7532776c8e303e80679d264
Hanya Elhashemy <ga87saf@mytum.de>
2020-03-13 15:06:52 +0100

    small fix

--- src/main/webapp/app/exercises/shared/structured-grading-criterion/grading-instructions-details/grading-instructions-details.component.ts
+++ src/main/webapp/app/exercises/shared/structured-grading-criterion/grading-instructions-details/grading-instructions-details.component.ts
@@ -83,7 +83,7 @@ export class GradingInstructionsDetailsComponent implements OnInit {
         }
         for (const instruction of criterion.structuredGradingInstructions) {
             markdownText +=
-                GradingCriterionCommand.identifier +
+                GradingInstructionCommand.identifier +
                 '\n' +
                 '\t' +
                 this.generateCreditsText(instruction) +
