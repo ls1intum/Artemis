ca00480ad798d6bf391792655d560cbe62d64cb6
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-03-16 12:47:42 +0100

    Remove changes from unchanges files

--- src/main/java/de/tum/in/www1/artemis/domain/TextCluster.java
+++ src/main/java/de/tum/in/www1/artemis/domain/TextCluster.java
@@ -10,7 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
