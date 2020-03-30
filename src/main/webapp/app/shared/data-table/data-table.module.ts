13dde93719a97086b485f131f184949407b52e75
Martin Wauligmann <martin@wauligmann.com>
2020-03-30 17:20:23 +0200

    Move SortByPipe provider directly in data-table.module.ts

--- src/main/webapp/app/shared/data-table/data-table.module.ts
+++ src/main/webapp/app/shared/data-table/data-table.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { DataTableComponent } from './data-table.component';
+import { SortByPipe } from 'app/shared/pipes/sort-by.pipe';
 
 @NgModule({
     imports: [ArtemisSharedModule, NgxDatatableModule],
     declarations: [DataTableComponent],
     exports: [DataTableComponent],
+    providers: [SortByPipe],
 })
 export class ArtemisDataTableModule {}
