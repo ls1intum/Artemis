279facd13d9571a7b5c5f8521bce54b8f658868e
Filip Gregurevic <filipgregurevic@gmail.com>
2020-04-03 16:34:25 +0200

    added test for student questions component

--- src/test/javascript/spec/component/student-questions/student-question-row.component.spec.ts
+++ src/test/javascript/spec/component/student-questions/student-question-row.component.spec.ts
@@ -2,12 +2,11 @@ import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { By } from '@angular/platform-browser';
 import { StudentQuestionRowComponent } from "app/overview/student-questions/student-question-row.component";
 import { StudentQuestionAnswer } from "app/entities/student-question-answer.model";
 import { StudentQuestion } from "app/entities/student-question.model";
-import {ArtemisTestModule} from "../../test.module";
-import {ArtemisSharedModule} from "app/shared/shared.module";
+import { ArtemisTestModule } from "../../test.module";
+import { ArtemisSharedModule } from "app/shared/shared.module";
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -21,20 +20,20 @@ describe('StudentQuestionRowComponent', () => {
         answerDate: null,
         answerText: 'not approved',
         tutorApproved: false
-    } as StudentQuestionAnswer
+    } as StudentQuestionAnswer;
 
     const approvedStudentQuestionAnswer = {
         id: 2,
         answerDate: null,
         answerText: 'approved',
         tutorApproved: true
-    } as StudentQuestionAnswer
+    } as StudentQuestionAnswer;
 
     const studentQuestion = {
         id: 1,
         creationDate: null,
         answers: [unApprovedStudentQuestionAnswer, approvedStudentQuestionAnswer]
-    } as StudentQuestion
+    } as StudentQuestion;
 
     beforeEach(async () => {
         return TestBed.configureTestingModule({
@@ -65,5 +64,13 @@ describe('StudentQuestionRowComponent', () => {
         componentFixture.detectChanges();
         expect(component.isAnswerMode).to.be.true;
         expect(component.selectedQuestionAnswer).to.deep.equal(approvedStudentQuestionAnswer);
+    });
+
+    it('should toggle question edit mode', () => {
+        component.studentQuestion = studentQuestion;
+        componentFixture.detectChanges();
+        component.toggleQuestionEditMode();
+        componentFixture.detectChanges();
+        expect(component.isEditMode).to.be.true;
     });
 });
