ba892027d3e09ac6e45f477c772f7bd91875f6b5
F4lka <AdrianLoher@gmx.de>
2020-04-05 18:43:29 +0200

    fixed the bug that the name of the checkbox was not translated

--- src/main/webapp/app/entities/programming-exercise.model.ts
+++ src/main/webapp/app/entities/programming-exercise.model.ts
@@ -17,7 +17,7 @@ export class ProgrammingExercise extends Exercise {
     public testRepositoryUrl: string;
     public publishBuildPlanUrl = false; // default value
     public allowOnlineEditor = false; // default value
-    public noLocalSetup = false;
+    public noLocalSetup = false; // default value
     public programmingLanguage = ProgrammingLanguage.JAVA; // default value
     public packageName: string;
     public problemStatement: string;
