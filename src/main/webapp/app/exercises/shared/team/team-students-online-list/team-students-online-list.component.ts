9ada5f6c0442fc908c4ca27c04731babbc4f2a8e
Martin Wauligmann <martin@wauligmann.com>
2020-04-05 15:41:50 +0200

    Basic online status switch

--- src/main/webapp/app/exercises/shared/team/team-students-online-list/team-students-online-list.component.ts
+++ src/main/webapp/app/exercises/shared/team/team-students-online-list/team-students-online-list.component.ts
@@ -1,25 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Team } from 'app/entities/team.model';
 import { AccountService } from 'app/core/auth/account.service';
 import { User } from 'app/core/user/user.model';
 import { orderBy } from 'lodash';
+import { JhiWebsocketService } from 'app/core/websocket/websocket.service';
+import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+
+class UserOnlineStatus {
+    userId: number;
+    online: boolean;
+}
 
 @Component({
     selector: 'jhi-team-students-online-list',
     templateUrl: './team-students-online-list.component.html',
     styleUrls: ['./team-students-online-list.component.scss'],
 })
-export class TeamStudentsOnlineListComponent implements OnInit {
-    @Input() team: Team;
+export class TeamStudentsOnlineListComponent implements OnInit, OnDestroy {
+    @Input() participation: StudentParticipation;
 
     currentUser: User;
+    onlineUserIds = new Set<number>();
+    websocketChannel: string;
 
-    constructor(private accountService: AccountService) {}
+    constructor(private accountService: AccountService, private jhiWebsocketService: JhiWebsocketService) {}
 
     ngOnInit(): void {
         this.accountService.identity().then((user: User) => {
             this.currentUser = user;
+            this.onlineUserIds.add(user.id!);
+
+            this.websocketChannel = `/topic/participation/${this.participation.id}/team`;
+            this.jhiWebsocketService.subscribe(this.websocketChannel);
+            this.jhiWebsocketService.receive(this.websocketChannel).subscribe(({ userId, online }: UserOnlineStatus) => {
+                return online ? this.onlineUserIds.add(userId) : this.onlineUserIds.delete(userId);
+            });
+            setTimeout(() => this.jhiWebsocketService.send(this.websocketChannel, this.userOnlineStatus(true)));
         });
+    }
+
+    ngOnDestroy(): void {
+        this.jhiWebsocketService.send(this.websocketChannel, this.userOnlineStatus(false));
+        this.jhiWebsocketService.unsubscribe(this.websocketChannel);
+    }
+
+    get team(): Team {
+        return this.participation.team;
     }
 
     get studentList(): User[] {
@@ -41,4 +67,15 @@ export class TeamStudentsOnlineListComponent implements OnInit {
     isOther = (user: User): boolean => {
         return !this.isSelf(user);
     };
+
+    isOnline = (user: User): boolean => {
+        return this.onlineUserIds.has(user.id!);
+    };
+
+    userOnlineStatus(online: boolean): UserOnlineStatus {
+        return {
+            userId: this.currentUser.id!,
+            online,
+        };
+    }
 }
