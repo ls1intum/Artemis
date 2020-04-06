5b00a5b023d0af72a1e8998db2c0bedb0cb44672
Martin Wauligmann <martin@wauligmann.com>
2020-04-06 18:10:11 +0200

    Bounded sticky

--- src/main/webapp/app/exercises/shared/team/team-participate-info-box/team-participate-info-box.component.ts
+++ src/main/webapp/app/exercises/shared/team/team-participate-info-box/team-participate-info-box.component.ts
@@ -3,8 +3,6 @@ import { Exercise } from 'app/entities/exercise.model';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
 import { StickyStatus } from '@w11k/angular-sticky-things';
 
-type Position = 'top' | 'bottom';
-
 @Component({
     selector: 'jhi-team-participate-info-box',
     templateUrl: './team-participate-info-box.component.html',
@@ -13,7 +11,8 @@ type Position = 'top' | 'bottom';
 export class TeamParticipateInfoBoxComponent {
     @Input() exercise: Exercise;
     @Input() participation: StudentParticipation;
-    @Input() stickyEnabled = false;
+    @Input() stickyEnabled = true;
+    @Input() bounded = true;
 
     stickyStatus: StickyStatus;
 
