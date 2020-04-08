f97a8c5123040f086d26d6055ed59edcba2d689b
Filip Gregurevic <51987021+filip-gregurevic@users.noreply.github.com>
2020-04-08 13:10:53 +0200

    Feature/questions answers/tutor approval (#1285)

--- src/main/webapp/app/overview/student-questions/student-question-row.component.ts
+++ src/main/webapp/app/overview/student-questions/student-question-row.component.ts
@@ -31,10 +31,12 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
     isAnswerMode: boolean;
     isEditMode: boolean;
     isQuestionAuthor: boolean;
+    showOtherAnswers = false;
     selectedQuestionAnswer: StudentQuestionAnswer | null;
     questionAnswerText: string | null;
     studentQuestionText: string | null;
     sortedQuestionAnswers: StudentQuestionAnswer[];
+    approvedQuestionAnswers: StudentQuestionAnswer[];
 
     constructor(private studentQuestionAnswerService: StudentQuestionAnswerService, private studentQuestionService: StudentQuestionService) {}
 
@@ -45,17 +47,31 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         this.sortQuestionAnswers();
     }
 
+    /**
+     * sorts the answers of a question into approved and not approved and then by date
+     */
     sortQuestionAnswers(): void {
         if (!this.studentQuestion.answers) {
             this.sortedQuestionAnswers = [];
+            this.approvedQuestionAnswers = [];
             return;
         }
-        this.sortedQuestionAnswers = this.studentQuestion.answers.sort((a, b) => {
-            const aValue = moment(a.answerDate!).valueOf();
-            const bValue = moment(b.answerDate!).valueOf();
+        this.approvedQuestionAnswers = this.studentQuestion.answers
+            .filter((ans) => ans.tutorApproved)
+            .sort((a, b) => {
+                const aValue = moment(a.answerDate!).valueOf();
+                const bValue = moment(b.answerDate!).valueOf();
 
-            return aValue - bValue;
-        });
+                return aValue - bValue;
+            });
+        this.sortedQuestionAnswers = this.studentQuestion.answers
+            .filter((ans) => !ans.tutorApproved)
+            .sort((a, b) => {
+                const aValue = moment(a.answerDate!).valueOf();
+                const bValue = moment(b.answerDate!).valueOf();
+
+                return aValue - bValue;
+            });
     }
 
     ngOnDestroy(): void {}
@@ -71,6 +87,9 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         this.selectedQuestionAnswer = questionAnswer;
     }
 
+    /**
+     * Changes the question text
+     */
     saveQuestion(): void {
         this.studentQuestion.questionText = this.studentQuestionText;
         this.studentQuestionService.update(this.studentQuestion).subscribe((studentQuestionResponse: HttpResponse<StudentQuestion>) => {
@@ -79,6 +98,9 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * deletes the studentQuestion
+     */
     deleteQuestion(): void {
         this.studentQuestionService.delete(this.studentQuestion.id).subscribe((res: HttpResponse<any>) => {
             this.interactQuestion.emit({
@@ -88,12 +110,16 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * Creates a new studentAnswer
+     */
     addAnswer(): void {
         const studentQuestionAnswer = new StudentQuestionAnswer();
         studentQuestionAnswer.answerText = this.questionAnswerText;
         studentQuestionAnswer.author = this.user;
         studentQuestionAnswer.verified = true;
         studentQuestionAnswer.question = this.studentQuestion;
+        studentQuestionAnswer.tutorApproved = false;
         studentQuestionAnswer.answerDate = moment();
         this.studentQuestionAnswerService.create(studentQuestionAnswer).subscribe((studentQuestionResponse: HttpResponse<StudentQuestionAnswer>) => {
             if (!this.studentQuestion.answers) {
@@ -106,6 +132,9 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * Updates the text of the selected studentAnswer
+     */
     saveAnswer(): void {
         this.selectedQuestionAnswer!.answerText = this.questionAnswerText;
         this.studentQuestionAnswerService.update(this.selectedQuestionAnswer!).subscribe((studentAnswerResponse: HttpResponse<StudentQuestionAnswer>) => {
@@ -115,9 +144,24 @@ export class StudentQuestionRowComponent implements OnInit, OnDestroy {
         });
     }
 
+    /**
+     * Takes a studentAnswer and deletes it
+     * @param   {studentQuestionAnswer} studentAnswer
+     */
     deleteAnswer(studentAnswer: StudentQuestionAnswer): void {
         this.studentQuestionAnswerService.delete(studentAnswer.id).subscribe((res: HttpResponse<any>) => {
             this.studentQuestion.answers = this.studentQuestion.answers.filter((el) => el.id !== studentAnswer.id);
+            this.sortQuestionAnswers();
+        });
+    }
+
+    /**
+     * Takes a studentAnswer and toggles the tutorApproved field
+     * @param   {studentQuestionAnswer} studentAnswer
+     */
+    toggleAnswerTutorApproved(studentAnswer: StudentQuestionAnswer): void {
+        studentAnswer.tutorApproved = !studentAnswer.tutorApproved;
+        this.studentQuestionAnswerService.update(studentAnswer).subscribe((studentAnswerResponse: HttpResponse<StudentQuestionAnswer>) => {
             this.sortQuestionAnswers();
         });
     }
---
+++ src/test/javascript/spec/component/student-questions/student-questions.component.spec.ts
@@ -0,0 +1,107 @@
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { AccountService } from 'app/core/auth/account.service';
+import { MockAccountService } from '../../mocks/mock-account.service';
+import { StudentQuestionsComponent } from 'app/overview/student-questions/student-questions.component';
+import { Lecture } from 'app/entities/lecture.model';
+import { StudentQuestionAnswer } from 'app/entities/student-question-answer.model';
+import { StudentQuestion } from 'app/entities/student-question.model';
+import { ArtemisTestModule } from '../../test.module';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('StudentQuestionRowComponent', () => {
+    let component: StudentQuestionsComponent;
+    let componentFixture: ComponentFixture<StudentQuestionsComponent>;
+
+    const unApprovedStudentQuestionAnswer =
+        {
+            id: 1,
+            answerDate: null,
+            answerText: 'not approved',
+            tutorApproved: false,
+        } as StudentQuestionAnswer;
+
+    const approvedStudentQuestionAnswer =
+        {
+            id: 2,
+            answerDate: null,
+            answerText: 'approved',
+            tutorApproved: true,
+        } as StudentQuestionAnswer;
+
+    const studentQuestion1 =
+        {
+            id: 1,
+            creationDate: null,
+            answers: [unApprovedStudentQuestionAnswer, approvedStudentQuestionAnswer],
+        } as StudentQuestion;
+
+    const studentQuestion2 =
+        {
+            id: 2,
+            creationDate: null,
+            answers: [unApprovedStudentQuestionAnswer, approvedStudentQuestionAnswer],
+        } as StudentQuestion;
+
+    const lectureDefault =
+        {
+            id: 1,
+            title: 'test',
+            description: 'test',
+            startDate: null,
+            endDate: null,
+            studentQuestions: [studentQuestion1, studentQuestion2],
+            isAtLeastInstructor: true,
+        } as Lecture;
+
+    beforeEach(async () => {
+        return TestBed.configureTestingModule({
+            imports: [TranslateModule.forRoot(), ArtemisTestModule, ArtemisSharedModule],
+            providers: [{ provide: AccountService, useClass: MockAccountService }],
+            declarations: [StudentQuestionsComponent],
+        })
+            .overrideTemplate(StudentQuestionsComponent, '')
+            .compileComponents()
+            .then(() => {
+                componentFixture = TestBed.createComponent(StudentQuestionsComponent);
+                component = componentFixture.componentInstance;
+            });
+    });
+
+    it('should set student questions correctly', () => {
+        component.lecture = lectureDefault;
+        componentFixture.detectChanges();
+        component.ngOnInit();
+        componentFixture.detectChanges();
+        expect(component.studentQuestions).to.deep.equal([studentQuestion1, studentQuestion2]);
+    });
+
+    it('should toggle edit mode', () => {
+        component.lecture = lectureDefault;
+        componentFixture.detectChanges();
+        component.toggleEditMode();
+        componentFixture.detectChanges();
+        expect(component.isEditMode).to.be.true;
+    });
+
+    it('should delete studentQuestion from list', () => {
+        component.lecture = lectureDefault;
+        componentFixture.detectChanges();
+        component.ngOnInit();
+        componentFixture.detectChanges();
+        component.deleteQuestionFromList(studentQuestion1);
+        componentFixture.detectChanges();
+        expect(component.studentQuestions).to.deep.equal([studentQuestion2]);
+    });
+
+    it('should at least be instructor', () => {
+        component.lecture = lectureDefault;
+        componentFixture.detectChanges();
+        expect(component.isAtLeastTutorInCourse).to.be.true;
+    });
+});
