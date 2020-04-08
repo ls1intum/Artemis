0368716b0080ed7c63a6347e3f6d89450c820e35
Hanya Elhashemy <ga87saf@mytum.de>
2020-04-08 17:59:25 +0200

    -add table header to markdown editor preview -change count to maxCountInScore

--- src/main/webapp/app/shared/markdown-editor/domainCommands/usageCount.command.ts
+++ src/main/webapp/app/shared/markdown-editor/domainCommands/usageCount.command.ts
@@ -2,7 +2,7 @@ import { DomainTagCommand } from 'app/shared/markdown-editor/domainCommands/doma
 import { addTextAtCursor } from 'app/shared/util/markdown-util';
 
 export class UsageCountCommand extends DomainTagCommand {
-    public static readonly identifier = '[count]';
+    public static readonly identifier = '[maxCountInScore]';
     public static readonly text = ' 0';
     // 'Add how often the credits should be taken into consideration for this instruction: 0 -> the credits should be added as often as the instruction occurs' +
     // ' x of type int -> credits will be added x times only, if instruction occurs more than x times it will not be counted and instead marked as subsequent fault';
