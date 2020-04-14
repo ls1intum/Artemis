9b1ddf17709bea1575de097281adae0157ba7797
Sascha Beele <sascha@beele.io>
2020-04-14 16:08:41 +0200

    Fix typescript code formatting

--- src/test/k6/resource/constants_c.js
+++ src/test/k6/resource/constants_c.js
@@ -1,4 +1,5 @@
-export const programmingExerciseProblemStatementC = '### Tests\n' +
+export const programmingExerciseProblemStatementC =
+    '### Tests\n' +
     '\n' +
     '#### General Tasks\n' +
     '1. [task][0 as Input](TestInput_0)\n' +
@@ -22,20 +23,22 @@ export const programmingExerciseProblemStatementC = '### Tests\n' +
 
 export const buildErrorContentC = {
     newFiles: [],
-    content: [{
-        fileName: 'main.c',
-        fileContent: 'a'
-    }]
+    content: [
+        {
+            fileName: 'main.c',
+            fileContent: 'a',
+        },
+    ],
 };
 
-export const someSuccessfulErrorContentC= {
+export const someSuccessfulErrorContentC = {
     newFiles: [],
-    content: [{
-        fileName: 'main.c',
-        fileContent: 'int main(void) {\n' +
-        '\treturn 0; // Success\n'+
-        '}\n'
-    }]
+    content: [
+        {
+            fileName: 'main.c',
+            fileContent: 'int main(void) {\n' + '\treturn 0; // Success\n' + '}\n',
+        },
+    ],
 };
 
 export const allSuccessfulContentC = {
@@ -43,83 +46,84 @@ export const allSuccessfulContentC = {
     content: [
         {
             fileName: 'main.c',
-            fileContent: '#include <stdio.h> // For printf(...)\n' +
-            '#include <unistd.h> // For fork()\n' +
-            '#include <sys/wait.h> // For wait(...)\n' +
-            '#include <string.h> // strcmp(...)\n' +
-            '\n' +
-            'int readProcessCount();\n' +
-            'int createForks(int count);\n' +
-            'void waitForEnd();\n' +
-            'void waitPidChildren(int count);\n' +
-            '\n' +
-            'int main(void) {\n' +
-            '\tint count = readProcessCount();\n' +
-            '\n' +
-            '\tint pid = createForks(count);\n' +
-            '\tif(pid != 0) {\n' +
-            '\t\twaitPidChildren(count);\n' +
-            '\t\tprintf("Parent with PID %i terminated.\\n", getpid());\n' +
-            '\t}\n' +
-            '\treturn 0; // Success\n' +
-            '}\n' +
-            '\n' +
-            'int readProcessCount() {\n' +
-            '\tint count = -1;\n' +
-            '\tdo\n' +
-            '\t{\n' +
-            '\t\tprintf("Enter process count:\\n");\n' +
-            '\t\tfflush(stdout);\n' +
-            '\t\tif(!scanf("%i", &count)) {\n' +
-            '\t\t\t// Clear input if user did not enter a valid int:\n' +
-            '\t\t\tint c;\n' +
-            '\t\t\twhile ((c = getchar()) != \'\\n\' && c != EOF);\n' +
-            '\t\t}\n' +
-            '\t} while (count < 0);\n' +
-            '\treturn count;\n' +
-            '}\n' +
-            '\n' +
-            '// Recursively create new processes:\n' +
-            'int createForks(int count) {\n' +
-            '\tif(count <= 0) { return 1;}\n' +
-            '\n' +
-            '\tint pid = fork();\n' +
-            '\tswitch(pid) {\n' +
-            '\t\tcase -1: // Error\n' +
-            '\t\t\tperror ("fork() failed");\n' +
-            '\t\t\tbreak;\n' +
-            '\n' +
-            '\t\tcase 0: // Child\n' +
-            '\t\t\tpid =  getpid();\n' +
-            '\t\t\tprintf("I\'m your child! PID: %i, PPID: %i\\n", pid, getppid());\n' +
-            '\t\t\twaitForEnd();\n' +
-            '\t\t\tprintf("Child with PID %i terminated.\\n", pid);\n' +
-            '\t\t\treturn 0;\n' +
-            '\n' +
-            '\t\tdefault: // Parent\n' +
-            '\t\t\treturn createForks(--count);\n' +
-            '\t}\n' +
-            '\treturn pid;\n' +
-            '}\n' +
-            '\n' +
-            'void waitForEnd() {\n' +
-            '\tchar buffer[1024];\n' +
-            '\tdo\n' +
-            '\t{\n' +
-            '\t\tif(!scanf("%s", buffer)) {\n' +
-            '\t\t\t// Clear input if user did not enter a valid int:\n' +
-            '\t\t\tint c;\n' +
-            '\t\t\twhile ((c = getchar()) != \'\\n\' && c != EOF);\n' +
-            '\t\t}\n' +
-            '\t\t// printf("Read: %s\\n", buffer); // Debug output\n' +
-            '\t} while (strcmp(buffer, "END"));\n' +
-            '}\n' +
-            '\n' +
-            'void waitPidChildren(int count) {\n' +
-            '\tfor(int i = 0; i < count; i++) {\n' +
-            '\t\twaitpid(-1, NULL, 0); // Make sure we wait on the child process to prevent it from getting a Zombie process\n' +
-            '\t}\n' +
-            '}'
-        }
-    ]
+            fileContent:
+                '#include <stdio.h> // For printf(...)\n' +
+                '#include <unistd.h> // For fork()\n' +
+                '#include <sys/wait.h> // For wait(...)\n' +
+                '#include <string.h> // strcmp(...)\n' +
+                '\n' +
+                'int readProcessCount();\n' +
+                'int createForks(int count);\n' +
+                'void waitForEnd();\n' +
+                'void waitPidChildren(int count);\n' +
+                '\n' +
+                'int main(void) {\n' +
+                '\tint count = readProcessCount();\n' +
+                '\n' +
+                '\tint pid = createForks(count);\n' +
+                '\tif(pid != 0) {\n' +
+                '\t\twaitPidChildren(count);\n' +
+                '\t\tprintf("Parent with PID %i terminated.\\n", getpid());\n' +
+                '\t}\n' +
+                '\treturn 0; // Success\n' +
+                '}\n' +
+                '\n' +
+                'int readProcessCount() {\n' +
+                '\tint count = -1;\n' +
+                '\tdo\n' +
+                '\t{\n' +
+                '\t\tprintf("Enter process count:\\n");\n' +
+                '\t\tfflush(stdout);\n' +
+                '\t\tif(!scanf("%i", &count)) {\n' +
+                '\t\t\t// Clear input if user did not enter a valid int:\n' +
+                '\t\t\tint c;\n' +
+                "\t\t\twhile ((c = getchar()) != '\\n' && c != EOF);\n" +
+                '\t\t}\n' +
+                '\t} while (count < 0);\n' +
+                '\treturn count;\n' +
+                '}\n' +
+                '\n' +
+                '// Recursively create new processes:\n' +
+                'int createForks(int count) {\n' +
+                '\tif(count <= 0) { return 1;}\n' +
+                '\n' +
+                '\tint pid = fork();\n' +
+                '\tswitch(pid) {\n' +
+                '\t\tcase -1: // Error\n' +
+                '\t\t\tperror ("fork() failed");\n' +
+                '\t\t\tbreak;\n' +
+                '\n' +
+                '\t\tcase 0: // Child\n' +
+                '\t\t\tpid =  getpid();\n' +
+                '\t\t\tprintf("I\'m your child! PID: %i, PPID: %i\\n", pid, getppid());\n' +
+                '\t\t\twaitForEnd();\n' +
+                '\t\t\tprintf("Child with PID %i terminated.\\n", pid);\n' +
+                '\t\t\treturn 0;\n' +
+                '\n' +
+                '\t\tdefault: // Parent\n' +
+                '\t\t\treturn createForks(--count);\n' +
+                '\t}\n' +
+                '\treturn pid;\n' +
+                '}\n' +
+                '\n' +
+                'void waitForEnd() {\n' +
+                '\tchar buffer[1024];\n' +
+                '\tdo\n' +
+                '\t{\n' +
+                '\t\tif(!scanf("%s", buffer)) {\n' +
+                '\t\t\t// Clear input if user did not enter a valid int:\n' +
+                '\t\t\tint c;\n' +
+                "\t\t\twhile ((c = getchar()) != '\\n' && c != EOF);\n" +
+                '\t\t}\n' +
+                '\t\t// printf("Read: %s\\n", buffer); // Debug output\n' +
+                '\t} while (strcmp(buffer, "END"));\n' +
+                '}\n' +
+                '\n' +
+                'void waitPidChildren(int count) {\n' +
+                '\tfor(int i = 0; i < count; i++) {\n' +
+                '\t\twaitpid(-1, NULL, 0); // Make sure we wait on the child process to prevent it from getting a Zombie process\n' +
+                '\t}\n' +
+                '}',
+        },
+    ],
 };
