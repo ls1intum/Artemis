9da4f735ac36460c9513a1b0bd4e9ead8a1a184e
Tobias Priesching <tobias.priesching@gmail.com>
2020-04-14 08:57:10 +0200

    restore JSDoc description

--- src/main/webapp/app/exercises/quiz/manage/apollon-diagrams/exercise-generation/quiz-exercise-generator.ts
+++ src/main/webapp/app/exercises/quiz/manage/apollon-diagrams/exercise-generation/quiz-exercise-generator.ts
@@ -233,7 +233,7 @@ async function generateDragAndDropItemForRelationship(
  * Create a Drag and Drop Quiz Exercise `DropLocation` for an `Element`.
  *
  * Based on the total size of the complete UML model and the boundaries of an element a drop location is computed. Instead of abolute values
- * for position and size, `DropLocation`s use precentage values.
+ * for position and size, `DropLocation`s use precentage values to the base of `MAX_SIZE_UNIT`.
  *
  * @param {Boundary} elementLocation The position and size of an element.
  * @param {Size} totalSize The total size of the UML model.
