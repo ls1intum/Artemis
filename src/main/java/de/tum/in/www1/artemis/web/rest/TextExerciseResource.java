b0dfadfc0f77b7b95ac85648eac149f7108d552b
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-04-16 14:38:03 +0200

    Add Testcase
    Signed-off-by: Jan Philip Bernius <janphilip.bernius@tum.de>

--- src/main/java/de/tum/in/www1/artemis/web/rest/TextExerciseResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/TextExerciseResource.java
@@ -308,6 +308,7 @@ public class TextExerciseResource {
 
             Result result = textSubmission.getResult();
             if (result != null) {
+                // Load TextBlocks for the Submission. They are needed to display the Feedback in the client.
                 final List<TextBlock> textBlocks = textBlockRepository.findAllBySubmissionId(textSubmission.getId());
                 textSubmission.setBlocks(textBlocks);
 
