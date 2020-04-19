911e379f07314e2ac6e959e1286f84aa6b1bc7ac
Martin Wauligmann <martin@wauligmann.com>
2020-04-19 19:00:39 +0200

    Header participation page

---
+++ src/main/webapp/app/exercises/shared/exercise-headers/header-participation-page.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, Output, OnChanges, OnInit, SimpleChanges, EventEmitter, ViewEncapsulation } from '@angular/core';
+import * as moment from 'moment';
+import { Exercise, ExerciseCategory, getIcon } from 'app/entities/exercise.model';
+import { ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
+import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+import { ButtonType } from 'app/shared/components/button.component';
+
+@Component({
+    selector: 'jhi-header-participation-page',
+    templateUrl: './header-participation-page.component.html',
+    styleUrls: ['./header-participation-page.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class HeaderParticipationPageComponent implements OnInit, OnChanges {
+    readonly ButtonType = ButtonType;
+
+    @Input() title: string;
+    @Input() exercise: Exercise;
+    @Input() participation: StudentParticipation;
+    @Input() onBackClick: () => void;
+
+    @Output() submit = new EventEmitter<void>();
+
+    public exerciseStatusBadge = 'badge-success';
+    public exerciseCategories: ExerciseCategory[];
+
+    getIcon = getIcon;
+
+    constructor(private exerciseService: ExerciseService) {}
+
+    ngOnInit(): void {
+        this.setExerciseStatusBadge();
+        this.exerciseCategories = this.exerciseService.convertExerciseCategoriesFromServer(this.exercise);
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.setExerciseStatusBadge();
+        this.exerciseCategories = this.exerciseService.convertExerciseCategoriesFromServer(this.exercise);
+    }
+
+    private setExerciseStatusBadge(): void {
+        if (this.exercise) {
+            this.exerciseStatusBadge = moment(this.exercise.dueDate!).isBefore(moment()) ? 'badge-danger' : 'badge-success';
+        }
+    }
+}
