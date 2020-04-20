70951d74ee7dc6420d9852c64505290428471f70
Sascha Beele <sascha@beele.io>
2020-04-20 11:47:22 +0200

    Add findAllRecentNotificationsForRecipientWithLogin() to NotificationRespository

--- src/main/java/de/tum/in/www1/artemis/repository/NotificationRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/NotificationRepository.java
@@ -1,5 +1,7 @@
 package de.tum.in.www1.artemis.repository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.domain.Page;
@@ -24,4 +26,13 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             + "or (notification.course.studentGroupName in :#{#currentGroups} AND notification.type = 'STUDENT')))"
             + "or notification.course is null and notification.recipient.login = :#{#login}")
     Page<Notification> findAllNotificationsForRecipientWithLogin(@Param("currentGroups") Set<String> currentUserGroups, Pageable pageable, @Param("login") String login);
+
+    @Query("select notification from Notification notification left join notification.course left join notification.recipient "
+            + "where (:#{#lastNotificationRead} is null or notification.notificationDate > :#{#lastNotificationRead}) AND "
+            + "((notification.recipient is null and ((notification.course.instructorGroupName in :#{#currentGroups} AND notification.type = 'INSTRUCTOR') "
+            + "or (notification.course.teachingAssistantGroupName in :#{#currentGroups} AND notification.type = 'TA') "
+            + "or (notification.course.studentGroupName in :#{#currentGroups} AND notification.type = 'STUDENT')))"
+            + "or notification.course is null and notification.recipient.login = :#{#login})")
+    List<Notification> findAllRecentNotificationsForRecipientWithLogin(@Param("currentGroups") Set<String> currentUserGroups, @Param("login") String login,
+            @Param("lastNotificationRead") ZonedDateTime lastNotificationRead);
 }
