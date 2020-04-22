1c04a10113cfa8afd546b49ba72cee07f2f60d49
Martin Wauligmann <martin@wauligmann.com>
2020-04-22 21:01:49 +0200

    Fix test

--- src/test/javascript/spec/app/entities/programming-exercise/programming-exercise.component.spec.ts
+++ src/test/javascript/spec/app/entities/programming-exercise/programming-exercise.component.spec.ts
@@ -1,51 +1,75 @@
-/* tslint:disable max-line-length */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { ArtemisTestModule } from '../../../test.module';
 import { ProgrammingExerciseComponent } from 'app/exercises/programming/manage/programming-exercise.component';
 import { ProgrammingExerciseService } from 'app/exercises/programming/manage/services/programming-exercise.service';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { MockSyncStorage } from '../../../mocks/mock-sync.storage';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { MockTranslateService } from '../../../mocks/mock-translate.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { Course } from 'app/entities/course.model';
+import { OrionConnectorService } from 'app/shared/orion/orion-connector.service';
+import { MockOrionConnectorService } from '../../../mocks/mock-orion-connector.service';
+import { CourseExerciseService } from 'app/course/manage/course-management.service';
+import { MockCourseExerciseService } from '../../../mocks/mock-course-exercise.service';
 
 describe('Component Tests', () => {
     describe('ProgrammingExercise Management Component', () => {
+        const course = { id: 123 } as Course;
+        const programmingExercise = new ProgrammingExercise(course);
+        programmingExercise.id = 456;
+
         let comp: ProgrammingExerciseComponent;
         let fixture: ComponentFixture<ProgrammingExerciseComponent>;
-        let service: ProgrammingExerciseService;
+        let service: CourseExerciseService;
+
+        const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id }) } } as any) as ActivatedRoute;
 
         beforeEach(() => {
             TestBed.configureTestingModule({
                 imports: [ArtemisTestModule],
                 declarations: [ProgrammingExerciseComponent],
-                providers: [],
+                providers: [
+                    { provide: SessionStorageService, useClass: MockSyncStorage },
+                    { provide: LocalStorageService, useClass: MockSyncStorage },
+                    { provide: TranslateService, useClass: MockTranslateService },
+                    { provide: ActivatedRoute, useValue: route },
+                    { provide: OrionConnectorService, useClass: MockOrionConnectorService },
+                    { provide: CourseExerciseService, useClass: MockCourseExerciseService },
+                ],
             })
                 .overrideTemplate(ProgrammingExerciseComponent, '')
                 .compileComponents();
 
             fixture = TestBed.createComponent(ProgrammingExerciseComponent);
             comp = fixture.componentInstance;
-            service = fixture.debugElement.injector.get(ProgrammingExerciseService);
+            service = fixture.debugElement.injector.get(CourseExerciseService);
         });
 
         it('Should call load all on init', () => {
             // GIVEN
             const headers = new HttpHeaders().append('link', 'link;link');
-            spyOn(service, 'query').and.returnValue(
+            spyOn(service, 'findAllProgrammingExercisesForCourse').and.returnValue(
                 of(
                     new HttpResponse({
-                        body: [new ProgrammingExercise(123)],
+                        body: [programmingExercise],
                         headers,
                     }),
                 ),
             );
 
             // WHEN
+            comp.course = course;
+            fixture.detectChanges();
             comp.ngOnInit();
 
             // THEN
-            expect(service.query).toHaveBeenCalled();
-            expect(comp.programmingExercises[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+            expect(service.findAllProgrammingExercisesForCourse).toHaveBeenCalled();
+            expect(comp.programmingExercises[0]).toEqual(jasmine.objectContaining({ id: programmingExercise.id }));
         });
     });
 });
