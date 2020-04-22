ed4b9b4c74750ddb5311160ff605d0e3a3825edd
Martin Wauligmann <martin@wauligmann.com>
2020-04-22 19:19:57 +0200

    Get rid of AngularStickyThings in favor of css position sticky (much smoother)

--- src/main/webapp/app/exercises/shared/team/team-participate-info-box/team-participate-info-box.component.ts
+++ src/main/webapp/app/exercises/shared/team/team-participate-info-box/team-participate-info-box.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Exercise } from 'app/entities/exercise.model';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
-import { StickyStatus } from '@w11k/angular-sticky-things';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,18 +12,7 @@ export class TeamParticipateInfoBoxComponent {
     @Input() exercise: Exercise;
     @Input() participation: StudentParticipation;
     @Input() stickyEnabled = true;
-    @Input() bounded = true;
     @Input() dockedToLeftSide = false;
     @Input() dockedToRightSide = false;
     @Input() typing$: Observable<any> | null;
-
-    stickyStatus: StickyStatus;
-
-    onStickyStatusChange(stickyStatus: StickyStatus) {
-        this.stickyStatus = stickyStatus;
-    }
-
-    get isSticky(): boolean {
-        return this.stickyStatus?.isSticky;
-    }
 }
