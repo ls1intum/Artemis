3d56efb0ca85d1e5f3034c200b613da8102d08c5
Balazs Czopf <balazs.czopf@tum.de>
2020-04-25 21:16:48 +0200

    Fix test case in text-submission.service.spec.ts

--- src/test/javascript/spec/service/text-submission.service.spec.ts
+++ src/test/javascript/spec/service/text-submission.service.spec.ts
@@ -21,17 +21,6 @@ describe('TextSubmission Service', () => {
     });
 
     describe('Service methods', async () => {
-        it('should find an element', async () => {
-            const returnedFromService = Object.assign({}, elemDefault);
-            service
-                .find(123)
-                .pipe(take(1))
-                .subscribe((resp) => expect(resp).toMatchObject({ body: elemDefault }));
-
-            const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify(returnedFromService));
-        });
-
         it('should create a TextSubmission', async () => {
             const returnedFromService = Object.assign(
                 {
@@ -43,7 +32,7 @@ describe('TextSubmission Service', () => {
             service
                 .create(new TextSubmission(null))
                 .pipe(take(1))
-                .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+                .subscribe((resp: any) => expect(resp).toMatchObject({ body: expected }));
             const req = httpMock.expectOne({ method: 'POST' });
             req.flush(JSON.stringify(returnedFromService));
         });
@@ -60,36 +49,9 @@ describe('TextSubmission Service', () => {
             service
                 .update(expected)
                 .pipe(take(1))
-                .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+                .subscribe((resp: any) => expect(resp).toMatchObject({ body: expected }));
             const req = httpMock.expectOne({ method: 'PUT' });
             req.flush(JSON.stringify(returnedFromService));
-        });
-
-        it('should return a list of TextSubmission', async () => {
-            const returnedFromService = Object.assign(
-                {
-                    text: 'BBBBBB',
-                },
-                elemDefault,
-            );
-            const expected = Object.assign({}, returnedFromService);
-            service
-                .query(expected)
-                .pipe(
-                    take(1),
-                    map((resp) => resp.body),
-                )
-                .subscribe((body) => expect(body).toContainEqual(expected));
-            const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify([returnedFromService]));
-            httpMock.verify();
-        });
-
-        it('should delete a TextSubmission', async () => {
-            service.delete(123).subscribe((resp) => expect(resp.ok));
-
-            const req = httpMock.expectOne({ method: 'DELETE' });
-            req.flush({ status: 200 });
         });
     });
 
