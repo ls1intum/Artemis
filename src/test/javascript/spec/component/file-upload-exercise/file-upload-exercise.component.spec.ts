326f39b7f830dbc2e47b1299a742f51c5ce08fe0
Sascha Beele <sascha@beele.io>
2020-04-25 11:29:04 +0200

    Fix test case in file-upload-exercise.component.spec.ts

--- src/test/javascript/spec/component/file-upload-exercise/file-upload-exercise.component.spec.ts
+++ src/test/javascript/spec/component/file-upload-exercise/file-upload-exercise.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 import { ArtemisTestModule } from '../../test.module';
 import { FileUploadExerciseComponent } from 'app/exercises/file-upload/manage/file-upload-exercise.component';
@@ -11,18 +12,24 @@ import { MockTranslateService } from '../../helpers/mocks/service/mock-translate
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { TranslateService } from '@ngx-translate/core';
 import { Course } from 'app/entities/course.model';
+import { CourseExerciseService } from 'app/course/manage/course-management.service';
 
 describe('FileUploadExercise Management Component', () => {
     let comp: FileUploadExerciseComponent;
     let fixture: ComponentFixture<FileUploadExerciseComponent>;
-    let service: FileUploadExerciseService;
+    let service: CourseExerciseService;
+
     const course: Course = { id: 123 } as Course;
+    const fileUploadExercise = new FileUploadExercise(course);
+    fileUploadExercise.id = 456;
+    const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id }) } } as any) as ActivatedRoute;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
             declarations: [FileUploadExerciseComponent],
             providers: [
+                { provide: ActivatedRoute, useValue: route },
                 { provide: LocalStorageService, useClass: MockSyncStorage },
                 { provide: SessionStorageService, useClass: MockSyncStorage },
                 { provide: TranslateService, useClass: MockTranslateService },
@@ -33,26 +40,27 @@ describe('FileUploadExercise Management Component', () => {
 
         fixture = TestBed.createComponent(FileUploadExerciseComponent);
         comp = fixture.componentInstance;
-        service = fixture.debugElement.injector.get(FileUploadExerciseService);
+        service = fixture.debugElement.injector.get(CourseExerciseService);
     });
 
-    it('Should call load all on init', () => {
+    it('Should call loadExercises on init', () => {
         // GIVEN
         const headers = new HttpHeaders().append('link', 'link;link');
-        spyOn(service, 'query').and.returnValue(
+        spyOn(service, 'findAllFileUploadExercisesForCourse').and.returnValue(
             of(
                 new HttpResponse({
-                    body: [new FileUploadExercise(course)],
+                    body: [fileUploadExercise],
                     headers,
                 }),
             ),
         );
 
         // WHEN
+        comp.course = course;
         comp.ngOnInit();
 
         // THEN
-        expect(service.query).toHaveBeenCalled();
-        expect(comp.fileUploadExercises[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        expect(service.findAllFileUploadExercisesForCourse).toHaveBeenCalled();
+        expect(comp.fileUploadExercises[0]).toEqual(fileUploadExercise);
     });
 });
