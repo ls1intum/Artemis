8084ab1df5895c6f5df84ccd3ff9f225105302ef
F4lka <AdrianLoher@gmx.de>
2020-04-26 21:05:06 +0200

    fixed text-exercise-detail.component.spec.ts

--- src/test/javascript/spec/component/text-exercise/text-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/text-exercise/text-exercise-detail.component.spec.ts
@@ -1,15 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
-
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ArtemisTestModule } from '../../test.module';
 import { TextExerciseDetailComponent } from 'app/exercises/text/manage/text-exercise/text-exercise-detail.component';
-import { TextExercise } from 'app/entities/text-exercise.model';
+import { Course } from 'app/entities/course.model';
+import { TextExerciseService } from 'app/exercises/text/manage/text-exercise/text-exercise.service';
 
 describe('TextExercise Management Detail Component', () => {
     let comp: TextExerciseDetailComponent;
     let fixture: ComponentFixture<TextExerciseDetailComponent>;
-    const route = ({ data: of({ textExercise: new TextExercise(123) }) } as any) as ActivatedRoute;
+    let service: TextExerciseService;
+    const course: Course = { id: 123 } as Course;
+    const route = ({ params: of({ courseId: course.id }) } as any) as ActivatedRoute;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -21,17 +24,28 @@ describe('TextExercise Management Detail Component', () => {
             .compileComponents();
         fixture = TestBed.createComponent(TextExerciseDetailComponent);
         comp = fixture.componentInstance;
+        service = fixture.debugElement.injector.get(TextExerciseService);
     });
 
     describe('OnInit', () => {
         it('Should call load all on init', () => {
             // GIVEN
-
+            const headers = new HttpHeaders().append('link', 'link;link');
+            spyOn(service, 'find').and.returnValue(
+                of(
+                    new HttpResponse({
+                        body: course,
+                        headers,
+                    }),
+                ),
+            );
             // WHEN
+            fixture.detectChanges();
             comp.ngOnInit();
 
             // THEN
-            expect(comp.textExercise).toEqual(jasmine.objectContaining({ id: 123 }));
+            expect(service.find).toHaveBeenCalled();
+            expect(comp.textExercise).toEqual(jasmine.objectContaining({ id: course.id }));
         });
     });
 });
