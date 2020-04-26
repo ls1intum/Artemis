c7b0a4b27a926d719cccec4e4a349ad57baef6ba
Hanya Elhashemy <ga87saf@mytum.de>
2020-04-26 19:25:07 +0200

    fix quiz-exercise-detail.component.spec.ts tests

--- src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
@@ -1,37 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 import { ArtemisTestModule } from '../../test.module';
 import { QuizExerciseDetailComponent } from 'app/exercises/quiz/manage/quiz-exercise-detail.component';
 import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
+import { CourseExerciseService } from 'app/course/manage/course-management.service';
+import { Course } from 'app/entities/course.model';
+import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('QuizExercise Management Detail Component', () => {
+    let service: CourseExerciseService;
     let comp: QuizExerciseDetailComponent;
     let fixture: ComponentFixture<QuizExerciseDetailComponent>;
-    const route = ({ data: of({ quizExercise: new QuizExercise(123) }) } as any) as ActivatedRoute;
+    const course: Course = { id: 123 } as Course;
+    const quizExercise = new QuizExercise(course);
+    quizExercise.id = 456;
+    const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id }) } } as any) as ActivatedRoute;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
             declarations: [QuizExerciseDetailComponent],
-            providers: [{ provide: ActivatedRoute, useValue: route }],
+            providers: [
+                { provide: ActivatedRoute, useValue: route },
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+                { provide: TranslateService, useClass: MockTranslateService },
+            ],
         })
             .overrideTemplate(QuizExerciseDetailComponent, '')
             .compileComponents();
         fixture = TestBed.createComponent(QuizExerciseDetailComponent);
         comp = fixture.componentInstance;
+        service = fixture.debugElement.injector.get(CourseExerciseService);
     });
 
     describe('OnInit', () => {
-        it('Should call load all on init', () => {
+        it('Should call loadExercises on init', () => {
             // GIVEN
 
             // WHEN
+            comp.course = course;
             comp.ngOnInit();
 
             // THEN
-            expect(comp.quizExercise).toEqual(jasmine.objectContaining({ id: 123 }));
         });
     });
 });
