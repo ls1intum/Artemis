559a8882fd98eb3549bbe63dc5598ab009caa95c
Clemens Zuck <clemens@zuck-online.de>
2020-04-27 15:19:44 +0200

    fix testcases in modeling-assessment-editor.component.spec.ts

--- src/test/javascript/spec/helpers/mocks/service/mock-account.service.ts
+++ src/test/javascript/spec/helpers/mocks/service/mock-account.service.ts
@@ -2,14 +2,13 @@ import { of } from 'rxjs';
 import { Course } from 'app/entities/course.model';
 import { IAccountService } from 'app/core/auth/account.service';
 import { User } from 'app/core/user/user.model';
-import { Account } from 'app/core/user/account.model';
 
 export class MockAccountService implements IAccountService {
     identity = () => Promise.resolve({ id: 99 } as User);
     hasAnyAuthority = (authorities: any[]) => Promise.resolve(true);
-    hasAnyAuthorityDirect = (authorities: any[]) => true;
+    hasAnyAuthorityDirect = (authorities: any[]) => authorities.length != 0;
     getAuthenticationState = () => of({ id: 99 } as User);
-    isAtLeastInstructorInCourse = (course: Course) => true;
+    isAtLeastInstructorInCourse = (course: Course) => course.isAtLeastInstructor;
     authenticate = (identity: User | null) => {};
     fetch = () => of({ body: { id: 99 } as User } as any);
     getImageUrl = () => 'blob';
