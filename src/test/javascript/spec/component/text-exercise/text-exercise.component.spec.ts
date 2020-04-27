66e73d5026c00c830d4aa6b84941c849bf7a0740
Sebastian Jagla <sebastianjagla@gmx.de>
2020-04-27 11:56:46 +0200

    Fix test case in text-exercise.component.spec.ts

--- src/test/javascript/spec/component/text-exercise/text-exercise.component.spec.ts
+++ src/test/javascript/spec/component/text-exercise/text-exercise.component.spec.ts
@@ -1,48 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { TranslateService } from '@ngx-translate/core';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 import { ArtemisTestModule } from '../../test.module';
 import { TextExerciseComponent } from 'app/exercises/text/manage/text-exercise/text-exercise.component';
-import { TextExerciseService } from 'app/exercises/text/manage/text-exercise/text-exercise.service';
 import { TextExercise } from 'app/entities/text-exercise.model';
+import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { CourseExerciseService } from 'app/course/manage/course-management.service';
+import { Course } from 'app/entities/course.model';
 
 describe('TextExercise Management Component', () => {
     let comp: TextExerciseComponent;
     let fixture: ComponentFixture<TextExerciseComponent>;
-    let service: TextExerciseService;
+    let courseExerciseService: CourseExerciseService;
+
+    const course = { id: 123 } as Course;
+    const textExercise: TextExercise = { id: 456 } as TextExercise;
+    const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id }) } } as any) as ActivatedRoute;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
             declarations: [TextExerciseComponent],
-            providers: [],
+            providers: [
+                { provide: ActivatedRoute, useValue: route },
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+                { provide: TranslateService, useClass: MockTranslateService },
+            ],
         })
             .overrideTemplate(TextExerciseComponent, '')
             .compileComponents();
 
         fixture = TestBed.createComponent(TextExerciseComponent);
         comp = fixture.componentInstance;
-        service = fixture.debugElement.injector.get(TextExerciseService);
+        courseExerciseService = fixture.debugElement.injector.get(CourseExerciseService);
     });
 
-    it('Should call load all on init', () => {
+    it('Should call loadExercises on init', () => {
         // GIVEN
         const headers = new HttpHeaders().append('link', 'link;link');
-        spyOn(service, 'query').and.returnValue(
+        spyOn(courseExerciseService, 'findAllTextExercisesForCourse').and.returnValue(
             of(
                 new HttpResponse({
-                    body: [new TextExercise(123)],
+                    body: [textExercise],
                     headers,
                 }),
             ),
         );
 
         // WHEN
+        comp.course = course;
         comp.ngOnInit();
 
         // THEN
-        expect(service.query).toHaveBeenCalled();
-        expect(comp.textExercises[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        expect(courseExerciseService.findAllTextExercisesForCourse).toHaveBeenCalled();
     });
 });
