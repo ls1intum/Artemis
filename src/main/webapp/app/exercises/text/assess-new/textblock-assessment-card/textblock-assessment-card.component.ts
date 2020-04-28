80d784fd1ffce1aa9383904ae8fc83b8fc2bb419
Hanya Elhashemy <50635005+HanyaElhashemy@users.noreply.github.com>
2020-04-28 19:27:23 +0200

    adapt the new text assessment version to the SGI feature (#1356)

--- src/main/webapp/app/exercises/text/assess-new/textblock-assessment-card/textblock-assessment-card.component.ts
+++ src/main/webapp/app/exercises/text/assess-new/textblock-assessment-card/textblock-assessment-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { TextBlockRef } from 'app/entities/text-block-ref.model';
 import { TextblockFeedbackEditorComponent } from 'app/exercises/text/assess-new/textblock-feedback-editor/textblock-feedback-editor.component';
+import { StructuredGradingCriterionService } from 'app/exercises/shared/structured-grading-criterion/structured-grading-criterion.service';
 
 type OptionalTextBlockRef = TextBlockRef | null;
 
@@ -15,7 +16,7 @@ export class TextblockAssessmentCardComponent {
     @Output() didSelect = new EventEmitter<OptionalTextBlockRef>();
     @Output() didChange = new EventEmitter<TextBlockRef>();
     @ViewChild(TextblockFeedbackEditorComponent) feedbackEditor: TextblockFeedbackEditorComponent;
-
+    constructor(public structuredGradingCriterionService: StructuredGradingCriterionService) {}
     select(autofocus = true): void {
         this.didSelect.emit(this.textBlockRef);
         this.textBlockRef.initFeedback();
@@ -33,5 +34,11 @@ export class TextblockAssessmentCardComponent {
 
     feedbackDidChange(): void {
         this.didChange.emit(this.textBlockRef);
+    }
+    connectStructuredGradingInstructionsWithTextBlock(event: Event) {
+        this.select();
+        if (this.textBlockRef.feedback) {
+            this.structuredGradingCriterionService.updateFeedbackWithStructuredGradingInstructionEvent(this.textBlockRef.feedback, event);
+        }
     }
 }
