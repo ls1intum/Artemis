b937e0e64c961d2bed32726b3a8c2c6b8bd22300
Andi Turdiu <andi.turdiu@tum.de>
2020-04-28 12:04:32 +0200

    Add sessionStorage mock

--- src/test/javascript/spec/component/course/course-update.component.spec.ts
+++ src/test/javascript/spec/component/course/course-update.component.spec.ts
@@ -8,6 +8,7 @@ import { CourseManagementService } from 'app/course/manage/course-management.ser
 import { Course } from 'app/entities/course.model';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
 import { LocalStorageService } from 'ngx-webstorage';
+import { SessionStorageService } from 'ngx-webstorage';
 
 describe('Course Management Update Component', () => {
     let comp: CourseUpdateComponent;
@@ -17,7 +18,10 @@ describe('Course Management Update Component', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
-            providers: [{ provide: LocalStorageService, useClass: MockSyncStorage }],
+            providers: [
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+            ],
             declarations: [CourseUpdateComponent],
         })
             .overrideTemplate(CourseUpdateComponent, '')
