add26c9904aa35896c2635f55f91746b8d204f7e
Hanya Elhashemy <50635005+HanyaElhashemy@users.noreply.github.com>
2020-04-28 19:27:23 +0200

    adapt the new text assessment version to the SGI feature (#1356)

---
+++ src/main/webapp/app/exercises/shared/structured-grading-criterion/structured-grading-criterion.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Feedback } from 'app/entities/feedback.model';
+
+@Injectable({ providedIn: 'root' })
+export class StructuredGradingCriterionService {
+    /**
+     * Connects the structured grading instructions with the feedback of a submission element
+     * @param {Event} event - The drop event
+     * @param {Feedback} feedback - The feedback of the assessment to be updated
+     * the SGI element sent on drag in processed in this method
+     * the corresponding drag method is in StructuredGradingInstructionsAssessmentLayoutComponent
+     */
+    updateFeedbackWithStructuredGradingInstructionEvent(feedback: Feedback, event: any) {
+        event.preventDefault();
+        const data = event.dataTransfer.getData('artemis/sgi');
+        const instruction = JSON.parse(data);
+        feedback.credits = instruction.credits;
+        feedback.detailText = instruction.feedback;
+    }
+}
