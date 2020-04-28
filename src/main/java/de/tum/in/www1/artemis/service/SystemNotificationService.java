49d3559f96fc8bf88f5a1ce8e2bd3f4021b605a4
Stephan Krusche <s.krusche@tum.de>
2020-04-29 01:44:54 +0200

    fix java.lang.IllegalArgumentException: Authentication object cannot be null

--- src/main/java/de/tum/in/www1/artemis/service/SystemNotificationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/SystemNotificationService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import de.tum.in.www1.artemis.domain.SystemNotification;
 import de.tum.in.www1.artemis.repository.SystemNotificationRepository;
+import de.tum.in.www1.artemis.security.SecurityUtils;
 
 @Service
 public class SystemNotificationService {
@@ -23,6 +24,8 @@ public class SystemNotificationService {
     }
 
     public SystemNotification findActiveSystemNotification() {
+        // The 'user' does not need to be logged into Artemis, this leads to an issue when accessing custom repository methods. Therefore a mock auth object has to be created.
+        SecurityUtils.setAuthorizationObject();
         List<SystemNotification> allActiveSystemNotification = systemNotificationRepository.findAllActiveSystemNotification(ZonedDateTime.now());
         return allActiveSystemNotification.size() > 0 ? allActiveSystemNotification.get(0) : null;
     }
