01a11f1b849d0b56df223593bbb0292f2c0eb700
Sascha Beele <sascha@beele.io>
2020-04-30 01:50:43 +0200

    Fix test case in modeling-exercise.component.spec.ts

--- src/test/javascript/spec/component/modeling-exercise/modeling-exercise.component.spec.ts
+++ src/test/javascript/spec/component/modeling-exercise/modeling-exercise.component.spec.ts
@@ -1,48 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 import { ArtemisTestModule } from '../../test.module';
 import { ModelingExerciseComponent } from 'app/exercises/modeling/manage/modeling-exercise.component';
-import { ModelingExerciseService } from 'app/exercises/modeling/manage/modeling-exercise.service';
-import { ModelingExercise } from 'app/entities/modeling-exercise.model';
+import { ModelingExercise, UMLDiagramType } from 'app/entities/modeling-exercise.model';
+import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { CourseExerciseService } from 'app/course/manage/course-management.service';
+import { Course } from 'app/entities/course.model';
 
 describe('ModelingExercise Management Component', () => {
     let comp: ModelingExerciseComponent;
     let fixture: ComponentFixture<ModelingExerciseComponent>;
-    let service: ModelingExerciseService;
+    let courseExerciseService: CourseExerciseService;
+
+    const course: Course = { id: 123 } as Course;
+    const modelingExercise = new ModelingExercise(UMLDiagramType.ClassDiagram, course);
+    modelingExercise.id = 456;
+    const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id }) } } as any) as ActivatedRoute;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
             declarations: [ModelingExerciseComponent],
-            providers: [],
+            providers: [
+                { provide: ActivatedRoute, useValue: route },
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+                { provide: TranslateService, useClass: MockTranslateService },
+            ],
         })
             .overrideTemplate(ModelingExerciseComponent, '')
             .compileComponents();
 
         fixture = TestBed.createComponent(ModelingExerciseComponent);
         comp = fixture.componentInstance;
-        service = fixture.debugElement.injector.get(ModelingExerciseService);
+        courseExerciseService = fixture.debugElement.injector.get(CourseExerciseService);
     });
 
-    it('Should call load all on init', () => {
+    it('Should call loadExercises on init', () => {
         // GIVEN
         const headers = new HttpHeaders().append('link', 'link;link');
-        spyOn(service, 'query').and.returnValue(
+        spyOn(courseExerciseService, 'findAllModelingExercisesForCourse').and.returnValue(
             of(
                 new HttpResponse({
-                    body: [new ModelingExercise(123)],
+                    body: [modelingExercise],
                     headers,
                 }),
             ),
         );
 
         // WHEN
+        comp.course = course;
         comp.ngOnInit();
 
         // THEN
-        expect(service.query).toHaveBeenCalled();
-        expect(comp.modelingExercises[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        expect(courseExerciseService.findAllModelingExercisesForCourse).toHaveBeenCalled();
+        expect(comp.modelingExercises[0]).toEqual(modelingExercise);
     });
 });
