6c0e7aadf1632812b54208f4686763569936f7bf
sjagla27 <sebastianjagla@gmx.de>
2020-05-02 18:48:12 +0200

    test fix

--- src/main/java/de/tum/in/www1/artemis/repository/ApollonDiagramRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ApollonDiagramRepository.java
@@ -1,10 +1,10 @@
 package de.tum.in.www1.artemis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 import de.tum.in.www1.artemis.domain.modeling.ApollonDiagram;
 
