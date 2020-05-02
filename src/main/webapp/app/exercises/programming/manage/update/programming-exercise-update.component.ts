b7c30efaf10ec09757b8d2699c003989998f8afa
F4lka <AdrianLoher@gmx.de>
2020-05-02 15:13:11 +0200

    added documentation for programming-exercise-update.component.ts

--- src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.component.ts
@@ -88,6 +88,9 @@ export class ProgrammingExerciseUpdateComponent implements OnInit {
         return this.selectedProgrammingLanguageValue;
     }
 
+    /**
+     * Sets the values for the creation/update of a programming exercise
+     */
     ngOnInit() {
         this.isSaving = false;
         this.notificationText = null;
@@ -155,14 +158,24 @@ export class ProgrammingExerciseUpdateComponent implements OnInit {
         });
     }
 
+    /**
+     * If an user clicks on the back button the previous page should be loaded
+     */
     previousState() {
         window.history.back();
     }
 
+    /**
+     * Updates the categories
+     * @param categories which should be set
+     */
     updateCategories(categories: ExerciseCategory[]) {
         this.programmingExercise.categories = categories.map((el) => JSON.stringify(el));
     }
 
+    /**
+     * Saves the programming exercise with the provided input
+     */
     save() {
         // If no release date is set, we warn the user.
         if (!this.programmingExercise.releaseDate) {
