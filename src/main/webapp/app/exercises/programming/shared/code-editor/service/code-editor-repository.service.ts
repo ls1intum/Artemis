e26e7672338748582a796fb57a103d336802b947
F4lka <AdrianLoher@gmx.de>
2020-05-07 19:43:21 +0200

    removed the unused rest call

--- src/main/webapp/app/exercises/programming/shared/code-editor/service/code-editor-repository.service.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/service/code-editor-repository.service.ts
@@ -106,8 +106,6 @@ export class CodeEditorBuildLogService extends DomainDependentEndpointService {
         const [domainType, domainValue] = this.domain;
         if (domainType === DomainType.PARTICIPATION) {
             return this.buildLogService.getBuildLogs(domainValue.id);
-        } else {
-            return this.buildLogService.getTestRepositoryBuildLogs(domainValue.id);
         }
     };
 }
