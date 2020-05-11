33466631db6da59ad0aad027e7881d66e549ab8b
F4lka <AdrianLoher@gmx.de>
2020-05-11 19:39:22 +0200

    added Documentation code-editor-file-browser-on-file-delete-delegate.ts

--- src/main/webapp/app/exercises/programming/shared/code-editor/file-browser/code-editor-file-browser-on-file-delete-delegate.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/file-browser/code-editor-file-browser-on-file-delete-delegate.ts
@@ -1,5 +1,10 @@
 import { FileChange } from 'app/exercises/programming/shared/code-editor/model/code-editor.model';
 
 export interface IFileDeleteDelegate {
+    /**
+     * Method signature of the on File Deleted method
+     * Classes which implement this interface have to comply with this method signature
+     * @param fileChange
+     */
     onFileDeleted(fileChange: FileChange): void;
 }
