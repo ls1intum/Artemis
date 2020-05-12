0ebd6daf14f83908feade309031a16d42e80d18e
Sascha Beele <sascha@beele.io>
2020-05-12 11:29:24 +0200

    Fix jsdoc style in attachment, bold and code command component

--- src/main/webapp/app/shared/markdown-editor/commands/bold.command.ts
+++ src/main/webapp/app/shared/markdown-editor/commands/bold.command.ts
@@ -5,13 +5,12 @@ export class BoldCommand extends Command {
     buttonTranslationString = 'artemisApp.multipleChoiceQuestion.editor.bold';
 
     /**
+     * Make/Remove bold text
+     * 1. Check if the selected text includes (**)
+     * 2. If included reduce the selected text by these elements and replace the selected text by textToAdd
+     * 3. If not included, add (**) before and after the selected text and insert them into the editor
+     * 4. Bold markdown appears
      * @function execute
-     *
-     * @desc Make/Remove bold text
-     *       1. Check if the selected text includes (**)
-     *       2. If included reduce the selected text by these elements and replace the selected text by textToAdd
-     *       3. If not included, add (**) before and after the selected text and insert them into the editor
-     *       4. Bold markdown appears
      */
     execute(): void {
         const selectedText = this.getSelectedText();
