77b707e4f04bffa4523d597de663cf3c36c15890
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2020-05-19 17:32:03 +0200

    update component

--- src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.component.ts
+++ src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 import { Exercise } from 'app/entities/exercise.model';
+import { Router } from '@angular/router';
 
 import { ArtemisMarkdownService } from 'app/shared/markdown.service';
+import { AssessmentType } from 'app/entities/assessment-type.model';
 
 @Component({
     selector: 'jhi-exercise-details',
@@ -12,11 +14,13 @@ import { ArtemisMarkdownService } from 'app/shared/markdown.service';
 export class ExerciseDetailsComponent implements OnInit {
     @Input() exerciseDetails: Exercise;
 
+    AssessmentType = AssessmentType;
+
     superExerciseDetails: Exercise;
     formattedProblemStatement: SafeHtml | null;
     formattedGradingInstructions: SafeHtml | null;
 
-    constructor(private artemisMarkdown: ArtemisMarkdownService) {}
+    constructor(private artemisMarkdown: ArtemisMarkdownService, private route: Router) {}
     /**
      * Life cycle hook to indicate component creation is done
      */
