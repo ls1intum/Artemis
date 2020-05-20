013f4d68171928cd1ee95f0514353d89099bfc10
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2020-05-20 13:29:34 +0200

    fix module imports

--- src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.module.ts
+++ src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { ArtemisAppRoutingModule } from 'app/app-routing.module';
+import { RouterModule } from '@angular/router';
 import { ExerciseDetailsComponent } from 'app/exercises/shared/exercise/exercise-details/exercise-details.component';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { ArtemisModePickerModule } from 'app/exercises/shared/mode-picker/mode-picker.module';
 import { AssessmentInstructionsModule } from 'app/assessment/assessment-instructions/assessment-instructions.module';
+import { exercisePopupRoute } from 'app/exercises/shared/exercise/exercise.route';
+
+const ENTITY_STATES = [...exercisePopupRoute];
 
 @NgModule({
-    imports: [ArtemisSharedModule, ArtemisSharedComponentModule, ArtemisModePickerModule, AssessmentInstructionsModule, ArtemisAppRoutingModule],
+    imports: [ArtemisSharedModule, ArtemisSharedComponentModule, ArtemisModePickerModule, AssessmentInstructionsModule, RouterModule.forChild(ENTITY_STATES)],
     declarations: [ExerciseDetailsComponent],
     exports: [ExerciseDetailsComponent],
 })
