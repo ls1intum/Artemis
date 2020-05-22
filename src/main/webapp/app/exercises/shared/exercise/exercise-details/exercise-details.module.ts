c95e61cde6bae4c8e01f651f17fa59673699c239
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2020-05-22 21:12:17 +0200

    fix failing tests

--- src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.module.ts
+++ src/main/webapp/app/exercises/shared/exercise/exercise-details/exercise-details.module.ts
@@ -6,12 +6,13 @@ import { ArtemisSharedComponentModule } from 'app/shared/components/shared-compo
 import { ArtemisModePickerModule } from 'app/exercises/shared/mode-picker/mode-picker.module';
 import { AssessmentInstructionsModule } from 'app/assessment/assessment-instructions/assessment-instructions.module';
 import { exercisePopupRoute } from 'app/exercises/shared/exercise/exercise.route';
+import { ExerciseLtiConfigurationDialogComponent, ExerciseLtiConfigurationPopupComponent } from 'app/exercises/shared/exercise/exercise-lti-configuration-dialog.component';
 
 const ENTITY_STATES = [...exercisePopupRoute];
 
 @NgModule({
     imports: [ArtemisSharedModule, ArtemisSharedComponentModule, ArtemisModePickerModule, AssessmentInstructionsModule, RouterModule.forChild(ENTITY_STATES)],
-    declarations: [ExerciseDetailsComponent],
-    exports: [ExerciseDetailsComponent],
+    declarations: [ExerciseDetailsComponent, ExerciseLtiConfigurationDialogComponent, ExerciseLtiConfigurationPopupComponent],
+    exports: [ExerciseDetailsComponent, ExerciseLtiConfigurationDialogComponent, ExerciseLtiConfigurationPopupComponent],
 })
 export class ExerciseDetailsModule {}
