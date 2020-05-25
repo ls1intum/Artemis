49d9c12b3af940665a2524103bd3f34e1493b1d8
Sascha Beele <sascha@beele.io>
2020-05-25 03:32:42 +0200

    Add all notification types to NotificationType enum

--- src/main/webapp/app/entities/notification.model.ts
+++ src/main/webapp/app/entities/notification.model.ts
@@ -5,8 +5,19 @@ import { User } from 'app/core/user/user.model';
 export enum NotificationType {
     SYSTEM = 'system',
     CONNECTION = 'connection',
-    GROUP = 'group',
     SINGLE = 'single',
+    SINGLE_NEW_ANSWER_FOR_EXERCISE = 'single-newAnswerForExercise',
+    SINGLE_NEW_ANSWER_FOR_LECTURE = 'single-newAnswerForLecture',
+    GROUP = 'group',
+    GROUP_ATTACHMENT_UPDATED = 'group-attachmentUpdated',
+    GROUP_EXERCISE_CREATED = 'group-exerciseCreated',
+    GROUP_EXERCISE_PRACTICE = 'group-exercisePractice',
+    GROUP_EXERCISE_STARTED = 'group-exerciseStarted',
+    GROUP_EXERCISE_UPDATED = 'group-exerciseUpdated',
+    GROUP_NEW_ANSWER_FOR_EXERCISE = 'group-newAnswerForExercise',
+    GROUP_NEW_ANSWER_FOR_LECTURE = 'group-newAnswerForLecture',
+    GROUP_NEW_QUESTION_FOR_EXERCISE = 'group-newQuestionForExercise',
+    GROUP_NEW_QUESTION_FOR_LECTURE = 'group-newQuestionForLecture',
 }
 
 export class Notification implements BaseEntity {
