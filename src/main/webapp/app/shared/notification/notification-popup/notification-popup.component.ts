325d431d450ab6c35fcb31af0b46285baf966046
Sascha Beele <sascha@beele.io>
2020-05-27 02:45:48 +0200

    Remove notification after clicked or closed

--- src/main/webapp/app/shared/notification/notification-popup/notification-popup.component.ts
+++ src/main/webapp/app/shared/notification/notification-popup/notification-popup.component.ts
@@ -6,28 +6,20 @@ import { User } from 'app/core/user/user.model';
 import { AccountService } from 'app/core/auth/account.service';
 import { Notification } from 'app/entities/notification.model';
 
-// TODO: visible not necessary --> simple remove notification from array
-type NotificationUIState = {
-    notification: Notification;
-    icon: string;
-    visible: boolean;
-};
-
 @Component({
     selector: 'jhi-notification-popup',
     templateUrl: './notification-popup.component.html',
     styleUrls: ['./notification-popup.scss'],
 })
 export class NotificationPopupComponent implements OnInit {
-    notifications: NotificationUIState[] = [
-        {
-            notification: { id: 1, title: 'Quiz just started', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing.' } as Notification,
-            icon: 'check-double',
-            visible: true,
-        },
-    ];
+    notifications: Notification[] = [];
 
-    constructor(private accountService: AccountService, private notificationService: NotificationService, private router: Router) {}
+    constructor(private accountService: AccountService, private notificationService: NotificationService, private router: Router) {
+        // TODO: remove
+        this.notifications.push({ id: 1, title: 'Quiz just started', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing.' } as Notification);
+        this.notifications.push({ id: 2, title: 'Quiz just started', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing.' } as Notification);
+        this.notifications.push({ id: 3, title: 'Quiz just started', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing.' } as Notification);
+    }
 
     /**
      * Subscribe to notification updates that are received via websocket if the user is logged in.
@@ -41,15 +33,24 @@ export class NotificationPopupComponent implements OnInit {
     }
 
     /**
-     * TODO
-     * @param index
+     * Returns a string that can be interpreted at fontawesome icon based on the notification type of the given notification.
+     * @param notification {Notification}
      */
-    close(index: number): void {
-        this.notifications[index].visible = false;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    notificationIcon(notification: Notification): string {
+        return 'check-double';
     }
 
     /**
-     * Navigate to the target (view) of the notification the user clicked on.
+     * Removes the notification at the specified index from the notifications array.
+     * @param index {number}
+     */
+    removeNotification(index: number): void {
+        this.notifications.splice(index, 1);
+    }
+
+    /**
+     * Navigate to the target (view) of the notification that the user clicked.
      * @param notification {Notification}
      */
     navigateToTarget(notification: Notification): void {
@@ -61,18 +62,18 @@ export class NotificationPopupComponent implements OnInit {
         setTimeout(() => {
             this.notificationService.subscribeUserNotifications(); // TODO: listen here too? Maybe remove for now
         }, 500);
-        this.notificationService.subscribeToSocketMessages().subscribe((receivedNotification: Notification) => {
-            if (receivedNotification && receivedNotification.notificationDate) {
-                this.addNotification(receivedNotification);
+        this.notificationService.subscribeToSocketMessages().subscribe((notification: Notification) => {
+            if (notification && notification.notificationDate) {
+                this.addNotification(notification);
             }
         });
     }
 
-    private addNotification(receivedNotification: Notification): void {
-        receivedNotification.notificationDate = moment(receivedNotification.notificationDate);
-        if (!this.notifications.some(({ notification }) => notification.id === receivedNotification.id)) {
+    private addNotification(notification: Notification): void {
+        notification.notificationDate = moment(notification.notificationDate);
+        if (!this.notifications.some(({ id }) => id === notification.id)) {
             // TODO: only add if it is a notification about quiz exercise start
-            this.notifications.unshift({ notification: receivedNotification, icon: 'check-double', visible: false });
+            this.notifications.unshift(notification);
         }
     }
 }
