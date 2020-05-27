3f6bc11a73fbe7fc84fee0e05953ef50f9345ed2
Jonas Petry <ga27der@mytum.de>
2020-05-27 22:41:15 +0200

    Add JsonIgnore to Rating.java

--- src/main/java/de/tum/in/www1/artemis/domain/Rating.java
+++ src/main/java/de/tum/in/www1/artemis/domain/Rating.java
@@ -13,6 +13,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -33,6 +34,7 @@ public class Rating implements Serializable {
     @MapsId
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "id")
+    @JsonIgnore
     private Result result;
 
     public Long getId() {
