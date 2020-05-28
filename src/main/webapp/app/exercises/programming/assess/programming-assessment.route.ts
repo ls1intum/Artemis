2114174c111469126f01ae2f1071a0191572bb45
F4lka <AdrianLoher@gmx.de>
2020-05-28 23:05:24 +0200

    removed unused import

--- src/main/webapp/app/exercises/programming/assess/programming-assessment.route.ts
+++ src/main/webapp/app/exercises/programming/assess/programming-assessment.route.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
-import { CodeEditorStudentContainerComponent } from 'app/exercises/programming/participate/code-editor-student-container.component';
 import { CodeEditorTutorAssessmentContainerComponent } from 'app/exercises/programming/assess/code-editor-tutor-assessment-container.component';
 
 export const routes: Routes = [
     {
         path: ':courseId/programming-exercises/:exerciseId/code-editor/:participationId/assessment',
-        //component: CodeEditorStudentContainerComponent,
         component: CodeEditorTutorAssessmentContainerComponent,
         data: {
             authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR', 'ROLE_TA'],
