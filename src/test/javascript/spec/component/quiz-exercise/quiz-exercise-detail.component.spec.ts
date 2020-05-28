145760d9b2155742e633e8cf6e838c5fc9193b70
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-05-28 03:05:46 +0200

    added test case

--- src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
@@ -13,6 +13,9 @@ import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
 import { QuizExerciseService } from 'app/exercises/quiz/manage/quiz-exercise.service';
+import * as moment from 'moment';
+import { MultipleChoiceQuestion } from 'app/entities/quiz/multiple-choice-question.model';
+import { AnswerOption } from 'app/entities/quiz/answer-option.model';
 
 describe('QuizExercise Management Detail Component', () => {
     let comp: QuizExerciseDetailComponent;
@@ -22,7 +25,14 @@ describe('QuizExercise Management Detail Component', () => {
 
     const course: Course = { id: 123 } as Course;
     const quizExercise = new QuizExercise(course);
+    const mcQuestion = new MultipleChoiceQuestion();
+
     quizExercise.id = 456;
+    quizExercise.title = 'test';
+    quizExercise.duration = 600;
+    mcQuestion.title = 'test';
+    mcQuestion.answerOptions = [new AnswerOption()];
+    quizExercise.quizQuestions = [mcQuestion];
 
     const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id, exerciseId: quizExercise.id }) } } as any) as ActivatedRoute;
 
@@ -70,6 +80,20 @@ describe('QuizExercise Management Detail Component', () => {
             // THEN
             expect(courseManagementService.find).toHaveBeenCalled();
             expect(quizExerciseService.find).toHaveBeenCalled();
+        });
+    });
+
+    describe('onDurationChange', () => {
+        it('Should update this.duration and this.quizExercise.duration with same values', () => {
+            // setup
+            comp.quizExercise = quizExercise;
+            comp.ngOnInit();
+            comp.duration = { minutes: 15, seconds: 30 };
+            comp.onDurationChange();
+
+            // compare this.duration with quizExercise.duration
+            const durationAsSeconds = moment.duration(comp.duration).asSeconds();
+            expect(durationAsSeconds).toEqual(comp.quizExercise.duration);
         });
     });
 });
