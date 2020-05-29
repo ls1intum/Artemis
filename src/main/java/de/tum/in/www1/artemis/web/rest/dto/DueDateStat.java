5e8d4871b1571eeeaf662effad4537abb0aa65d8
Kilian Schulte <kilian@4xschulte.de>
2020-05-29 14:31:17 +0200

    Added JavaDoc for DueDateStat

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/DueDateStat.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/DueDateStat.java
@@ -1,9 +1,15 @@
 package de.tum.in.www1.artemis.web.rest.dto;
 
+/**
+ * Wrapper class for a two-component statistic
+ * depending on the due-date of an exercise.
+ */
 public class DueDateStat {
 
+    // The statistic component before the due-date
     private Long inTime;
 
+    // The statistic component after the due-date
     private Long late;
 
     public DueDateStat(Long inTime, Long late) {
