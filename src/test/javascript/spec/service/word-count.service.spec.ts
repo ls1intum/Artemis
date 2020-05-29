96618c2c8353a720d7d258bd9c0f88a1f02d20db
Stefan Waldhauser <stefan.waldhauser@tum.de>
2020-05-29 17:13:50 +0200

    Created Unit Test for Word Count Service

---
+++ src/test/javascript/spec/service/word-count.service.spec.ts
@@ -0,0 +1,29 @@
+import { WordCountService } from 'app/exercises/text/participate/word-count.service';
+
+describe('WordCountService', () => {
+    let service: WordCountService;
+
+    const TEXT_WITH_63_WORDS =
+        'Deutsches Ipsum Dolor quo Grimms Märchen posidonium Kaftfahrzeug-Haftpflichtversicherung adhuc Schnaps sadipscing Krankenschwester at, ' +
+        'Sprechen Sie deutsch mei Handschuh gloriatur. Freude schöner Götterfunken inermis Apfelstrudel accommodare Berlin Id Angela Merkel assum Fernweh te ' +
+        'Fernweh erroribus bitte Nec Bildung amet Guten Tag iriure, Joachim Löw gloriatur Aperol Spritz ut. Brezel virtute Goethe per Schweinsteiger ' +
+        'At 99 Luftballons sßaevola Weltanschauung An Hochzeit malorum Prost ius';
+
+    const EMPTY_TEXT = '';
+
+    beforeEach(() => {
+        service = new WordCountService();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('german text should contain 63 words', () => {
+        expect(service.countWords(TEXT_WITH_63_WORDS)).toBe(63);
+    });
+
+    it('Empty Text should contain 0 words', () => {
+        expect(service.countWords(EMPTY_TEXT)).toBe(0);
+    });
+});
