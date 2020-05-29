04c5442c354d69d94118edbfdbf7b46d163e9da0
Stefan Waldhauser <stefan.waldhauser@tum.de>
2020-05-29 17:45:12 +0200

    Added word count to text-editor.component

--- src/main/webapp/app/exercises/text/participate/text-editor.component.ts
+++ src/main/webapp/app/exercises/text/participate/text-editor.component.ts
@@ -23,6 +23,7 @@ import { TextExercise } from 'app/entities/text-exercise.model';
 import { ButtonType } from 'app/shared/components/button.component';
 import { Result } from 'app/entities/result.model';
 import { TextSubmission } from 'app/entities/text-submission.model';
+import { WordCountService } from 'app/exercises/text/participate/word-count.service';
 
 @Component({
     templateUrl: './text-editor.component.html',
@@ -59,6 +60,7 @@ export class TextEditorComponent implements OnInit, OnDestroy, ComponentCanDeact
         private location: Location,
         private translateService: TranslateService,
         private participationWebsocketService: ParticipationWebsocketService,
+        private wordCountService: WordCountService,
     ) {
         this.isSaving = false;
     }
@@ -171,6 +173,10 @@ export class TextEditorComponent implements OnInit, OnDestroy, ComponentCanDeact
         }
 
         return null;
+    }
+
+    get wordCount() {
+        return this.wordCountService.countWords(this.answer);
     }
 
     // Displays the alert for confirming refreshing or closing the page if there are unsaved changes
