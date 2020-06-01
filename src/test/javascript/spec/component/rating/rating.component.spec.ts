5af8e704f3d8fd9f6aa6c7e2f165b79dd73790b1
Jonas Petry <ga27der@mytum.de>
2020-06-01 14:09:20 +0200

    Improve clients tests for rating.component to achieve 100% test coverage

--- src/test/javascript/spec/component/rating/rating.component.spec.ts
+++ src/test/javascript/spec/component/rating/rating.component.spec.ts
@@ -18,6 +18,7 @@ import { MockRatingService } from '../../helpers/mocks/service/mock-rating.servi
 import { Result } from 'app/entities/result.model';
 import { Submission } from 'app/entities/submission.model';
 import { Rating } from 'app/entities/rating.model';
+import { of } from 'rxjs';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -57,6 +58,27 @@ describe('RatingComponent', () => {
         expect(ratingComponent.result.id).to.equal(89);
     });
 
+    it('should return', () => {
+        sinon.spy(ratingService, 'getRating');
+        ratingComponent.result.submission = null;
+        ratingComponent.ngOnInit();
+        expect(ratingService.getRating).to.not.have.been.called;
+    });
+
+    it('should create new local rating', () => {
+        ratingComponent.ngOnInit();
+        expect(ratingComponent.rating.result.id).to.be.equal(89);
+        expect(ratingComponent.rating.rating).to.be.equal(0);
+    });
+
+    it('should set rating received from server', () => {
+        const fake = sinon.fake.returns(of(new Rating({ id: 90 } as Result, 1)));
+        sinon.replace(ratingService, 'getRating', fake);
+        ratingComponent.ngOnInit();
+        expect(ratingComponent.rating.result.id).to.be.equal(90);
+        expect(ratingComponent.rating.rating).to.be.equal(1);
+    });
+
     describe('OnRate', () => {
         beforeEach(() => {
             ratingComponent.rating = new Rating({ id: 89 } as Result, 0);
@@ -72,6 +94,8 @@ describe('RatingComponent', () => {
             });
             expect(ratingService.createRating).to.have.been.calledOnce;
             expect(ratingService.updateRating).to.not.have.been.called;
+            expect(ratingComponent.rating.result.id).to.be.equal(89);
+            expect(ratingComponent.rating.rating).to.be.equal(2);
         });
 
         it('should update rating', () => {
@@ -83,6 +107,8 @@ describe('RatingComponent', () => {
             });
             expect(ratingService.updateRating).to.have.been.calledOnce;
             expect(ratingService.createRating).to.not.have.been.called;
+            expect(ratingComponent.rating.result.id).to.be.equal(89);
+            expect(ratingComponent.rating.rating).to.be.equal(2);
         });
     });
 });
