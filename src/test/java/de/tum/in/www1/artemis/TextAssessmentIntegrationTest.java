463cfa34e5a97f4fce2679d33862b76e36f2f945
Stefan Waldhauser <stefan.waldhauser@tum.de>
2020-06-01 15:34:53 +0200

    Fixed failing test by adding the necessary lock parameter

--- src/test/java/de/tum/in/www1/artemis/TextAssessmentIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/TextAssessmentIntegrationTest.java
@@ -543,8 +543,10 @@ public class TextAssessmentIntegrationTest extends AbstractSpringIntegrationBamb
         database.addTextSubmissionWithResultAndAssessorAndFeedbacks(textExercise, textSubmission2, "student2", "tutor1", asList(feedback));
         feedbackRepository.save(feedback);
 
+        LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("lock", "true");
         TextSubmission textSubmissionWithoutAssessment = request.get("/api/exercises/" + textExercise.getId() + "/text-submission-without-assessment", HttpStatus.OK,
-                TextSubmission.class);
+                TextSubmission.class, parameters);
 
         request.put("/api/text-assessments/exercise/" + textExercise.getId() + "/submission/" + textSubmission1.getId() + "/cancel-assessment", null, HttpStatus.OK);
 
