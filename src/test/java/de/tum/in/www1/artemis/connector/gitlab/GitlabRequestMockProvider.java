e68385600ffbf8064911f9f7197695a43c7bf084
Andi Turdiu <andi.turdiu@tum.de>
2020-06-02 20:58:22 +0200

    Implement Request Mock Providers GitlabRequestMockProvider & JenkinsRequestMockProvider

--- src/test/java/de/tum/in/www1/artemis/connector/gitlab/GitlabRequestMockProvider.java
+++ src/test/java/de/tum/in/www1/artemis/connector/gitlab/GitlabRequestMockProvider.java
@@ -1,14 +1,19 @@
 package de.tum.in.www1.artemis.connector.gitlab;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tum.in.www1.artemis.domain.ProgrammingExercise;
-import org.gitlab4j.api.models.Group;
-import org.gitlab4j.api.models.Visibility;
+import de.tum.in.www1.artemis.domain.User;
+import de.tum.in.www1.artemis.domain.enumeration.RepositoryType;
+import de.tum.in.www1.artemis.exception.VersionControlException;
+import de.tum.in.www1.artemis.service.connectors.gitlab.GitLabException;
+import de.tum.in.www1.artemis.service.connectors.gitlab.dto.GitLabProjectDTO;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -19,6 +24,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Set;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
@@ -38,7 +44,7 @@ public class GitlabRequestMockProvider {
 
     private final RestTemplate restTemplate;
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();;
 
     private MockRestServiceServer mockServer;
 
@@ -57,17 +63,174 @@ public class GitlabRequestMockProvider {
     public void mockCreateProjectForExercise(ProgrammingExercise exercise) throws IOException, URISyntaxException {
         final var projectKey = exercise.getProjectKey();
         final var projectName = exercise.getProjectName();
-        final var group = new Group().withPath(projectKey).withName(projectName).withVisibility(Visibility.PRIVATE);
-        final var body = Map.of("key", projectKey, "name", projectName);
+        final var body = Map.of("path", projectKey, "name", projectName);
 
-        mockServer.expect(ExpectedCount.once(), requestTo(BITBUCKET_SERVER_URL + "/rest/api/1.0/projects")).andExpect(method(HttpMethod.POST))
-            .andExpect(content().json(mapper.writeValueAsString(body))).andRespond(withStatus(HttpStatus.OK));
+        mockServer.expect(ExpectedCount.once(), requestTo(GITLAB_SERVER_URL + "/api/v4/groups")).andExpect(method(HttpMethod.POST))
+            .andExpect(content().json(mapper.writeValueAsString(body))).andRespond(withStatus(HttpStatus.CREATED));
+        mockAddUserToGroup(exercise, "1");
+    }
 
-        mockGrantGroupPermissionToProject(exercise, ADMIN_GROUP_NAME, "PROJECT_ADMIN");
-        mockGrantGroupPermissionToProject(exercise, exercise.getCourse().getInstructorGroupName(), "PROJECT_ADMIN");
-        if (exercise.getCourse().getTeachingAssistantGroupName() != null) {
-            mockGrantGroupPermissionToProject(exercise, exercise.getCourse().getTeachingAssistantGroupName(), "PROJECT_WRITE");
+    private void mockAddUserToGroup(ProgrammingExercise exercise, String userID) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/4/projects").pathSegment(exercise.getProjectKey()).pathSegment("members")
+            .queryParam("user_id", "1", "access_level", "50").build().toUri();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockCreateRepository(ProgrammingExercise exercise, String repositoryName) throws IOException, URISyntaxException {
+        final var projectKey = exercise.getProjectKey();
+        final var body = Map.of("name", repositoryName, "namespace_id", projectKey);
+        final var createRepoPath = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/project");
+
+        mockServer.expect(requestTo(createRepoPath.build().toUri())).andExpect(method(HttpMethod.POST)).andExpect(content().json(mapper.writeValueAsString(body)))
+            .andRespond(withStatus(HttpStatus.CREATED));
+    }
+
+    public void mockCopyRepositoryForParticipation(ProgrammingExercise exercise, String username) throws URISyntaxException, IOException {
+        final var projectKey = exercise.getProjectKey();
+        final var templateRepoName = exercise.getProjectKey().toLowerCase() + "-" + RepositoryType.TEMPLATE.getName();
+        final var clonedRepoName = projectKey.toLowerCase() + "-" + username.toLowerCase();
+
+        mockCopyRepository(projectKey, projectKey, templateRepoName, clonedRepoName);
+    }
+
+    public void mockCopyRepository(String sourceProjectKey, String targetProjectKey, String sourceRepoName, String targetRepoName)
+        throws JsonProcessingException, URISyntaxException {
+        final var originPath = sourceProjectKey + "%2F" + sourceRepoName.toLowerCase();
+        final var targetRepoSlug = (targetProjectKey + "-" + targetRepoName).toLowerCase();
+
+        final var copyRepoPath = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects")
+            .pathSegment(originPath).pathSegment("fork").build().toUri();
+        final var body = Map.of("namespace", targetProjectKey, "path", targetRepoSlug, "name", targetRepoSlug);
+
+        mockServer.expect(requestTo(copyRepoPath)).andExpect(method(HttpMethod.POST)).andExpect(content().json(mapper.writeValueAsString(body)))
+            .andRespond(withStatus(HttpStatus.CREATED));
+    }
+
+    public void mockConfigureRepository(ProgrammingExercise exercise, String username, Set<User> users) throws URISyntaxException, IOException {
+        final var projectKey = exercise.getProjectKey();
+        final var repoName = projectKey.toLowerCase() + "-" + username.toLowerCase();
+        for (User user : users) {
+            mockGiveWritePermission(exercise, repoName, user.getLogin());
         }
+        mockProtectBranches(exercise, repoName);
+    }
+
+    public void mockGiveWritePermission(ProgrammingExercise exercise, String repositoryName, String username) throws URISyntaxException {
+        final var projectKey = exercise.getProjectKey();
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var permissionPath = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects")
+            .pathSegment(projectPath).pathSegment("members")
+            .pathSegment(username).queryParam("access_level", "30").build().toUri();
+
+        mockServer.expect(requestTo(permissionPath)).andExpect(method(HttpMethod.PUT)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockProtectBranches(ProgrammingExercise exercise, String repositoryName) throws URISyntaxException {
+        final var projectPath = exercise.getProjectKey() + "%2F" + repositoryName.toLowerCase();
+        final var protectBranchPath = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects")
+            .pathSegment(projectPath).pathSegment("protected_branches")
+            .queryParam("name", "*", "push_access_level", "30", "merge_access_level", "30").build().toUri();
+        mockServer.expect(requestTo(protectBranchPath)).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockRepositoryUrlIsValid(final URL repositoryUrl, final String projectKey, final boolean isValid) throws URISyntaxException {
+        final var repositoryName = getRepositorySlugFromUrl(repositoryUrl);
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment(projectPath).build().toUri();
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)).andRespond(withStatus(isValid ? HttpStatus.OK : HttpStatus.BAD_REQUEST));
+    }
+
+    /**
+     * TODO: this method is currently copied from GitlabService for testing purposes. Think about how to properly reuse this method while allowing it to be mocked during testing
+     *
+     * Gets the repository slug from the given URL
+     *
+     * @param repositoryUrl The complete repository-url (including protocol, host and the complete path)
+     * @return The repository slug
+     */
+    public String getRepositorySlugFromUrl(URL repositoryUrl) throws VersionControlException {
+        final var splittedUrl = repositoryUrl.toString().split("/");
+        if (splittedUrl[splittedUrl.length - 1].matches(".*\\.git")) {
+            return splittedUrl[splittedUrl.length - 1].replace(".git", "");
+        }
+
+        throw new GitLabException("Repository URL is not a git URL! Can't get slug for " + repositoryUrl.toString());
+    }
+
+    public void mockCheckIfProjectExists(final ProgrammingExercise exercise, final boolean exists) throws JsonProcessingException, URISyntaxException {
+        final var existsUri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment(exercise.getProjectKey()).build().toUri();
+        exercise.getProjectName();
+        final var uniqueUri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").queryParam("name", exercise.getProjectName()).build()
+            .toUri();
+        final var foundProject = new GitLabProjectDTO();
+        foundProject.setName(exercise.getProjectName() + (exists ? "" : "abc"));
+
+        mockServer.expect(requestTo(existsUri)).andExpect(method(HttpMethod.GET)).andRespond(withStatus(HttpStatus.NOT_FOUND));
+        mockServer.expect(requestTo(uniqueUri)).andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsString(foundProject)));
+    }
+
+    public void mockGetExistingWebhooks(String projectKey, String repositoryName) throws URISyntaxException {
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment(projectPath).pathSegment("hooks").build().toUri();
+        final var existingHooks = "{\"values\": []}";
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)).andRespond(withStatus(HttpStatus.OK).body(existingHooks).contentType(MediaType.APPLICATION_JSON));
+    }
+
+    public void mockAddWebhook(String projectKey, String repositoryName, String url) throws JsonProcessingException, URISyntaxException {
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment(projectPath).pathSegment("hooks").build().toUri();
+        final var body = Map.of("id", "1", "url", url, "push_events", true);
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.POST)).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json(mapper.writeValueAsString(body))).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockAddWebHooks(ProgrammingExercise exercise) throws IOException, URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment("hooks").build().toUri();
+        mockServer.expect(ExpectedCount.once(), requestTo(uri)).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockDeleteProject(String projectKey) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/groups").pathSegment(String.valueOf(projectKey)).build().toUri();
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.DELETE)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockDeleteRepository(String projectKey, String repositoryName) throws URISyntaxException {
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/v4/projects").pathSegment(projectPath).build().toUri();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.DELETE)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockSetRepositoryPermissionsToReadOnly(String repositorySlug, String projectKey, Set<User> users) throws URISyntaxException {
+        Map<String, String> body = new java.util.HashMap<>();
+        User[] userArray = new User[users.size()];
+        users.toArray(userArray);
+        for (int i = 0; i < users.size(); i++) {
+            body.put(userArray[i].getName(), String.valueOf(i+1));
+        }
+        for (User user : users) {
+            mockSetStudentRepositoryPermission(repositorySlug, projectKey, body.get(user.getName()));
+        }
+    }
+
+    private void mockSetStudentRepositoryPermission(String repositoryName, String projectKey, String userID) throws URISyntaxException {
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/4/projects").pathSegment(projectPath).pathSegment("members")
+            .queryParam("user_id", "1", "access_level", "10").build().toUri();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockRemoveStudentRepositoryAccess(final String repositoryName, final String projectKey, final User user) throws URISyntaxException {
+        final var projectPath = projectKey + "%2F" + repositoryName.toLowerCase();
+        final var body = Map.of("id", 1, "name", user.getName());
+        final var userID = body.get("id");
+        final var uri = UriComponentsBuilder.fromUri(GITLAB_SERVER_URL.toURI()).path("/api/4/projects").pathSegment(projectPath)
+            .pathSegment("members").pathSegment(String.valueOf(userID)).build().toUri();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.DELETE)).andRespond(withStatus(HttpStatus.OK));
     }
 
 }
--- src/test/java/de/tum/in/www1/artemis/connector/jenkins/JenkinsRequestMockProvider.java
+++ src/test/java/de/tum/in/www1/artemis/connector/jenkins/JenkinsRequestMockProvider.java
@@ -1,4 +1,190 @@
 package de.tum.in.www1.artemis.connector.jenkins;
 
+import com.appfire.common.cli.CliClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tum.in.www1.artemis.config.Constants;
+import de.tum.in.www1.artemis.domain.ProgrammingExercise;
+import de.tum.in.www1.artemis.domain.enumeration.BuildPlanType;
+import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseParticipation;
+import de.tum.in.www1.artemis.service.connectors.CIPermission;
+import de.tum.in.www1.artemis.service.connectors.jenkins.dto.ErrorDTO;
+import de.tum.in.www1.artemis.service.connectors.jenkins.dto.TestCaseDTO;
+import de.tum.in.www1.artemis.service.connectors.jenkins.dto.TestResultsDTO;
+import de.tum.in.www1.artemis.service.connectors.jenkins.dto.TestsuiteDTO;
+import de.tum.in.www1.artemis.util.Verifiable;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+@Component
+@Profile("jenkins")
 public class JenkinsRequestMockProvider {
+
+    @Value("${artemis.continuous-integration.url}")
+    private URL JENKINS_SERVER_URL;
+
+    private final RestTemplate restTemplate;
+
+    private MockRestServiceServer mockServer;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    public JenkinsRequestMockProvider(@Qualifier("jenkinsRestTemplate") RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void enableMockingOfRequests() {
+        mockServer = MockRestServiceServer.createServer(restTemplate);
+        MockitoAnnotations.initMocks(this);
+    }
+
+    public void reset() {
+        mockServer.reset();
+    }
+
+    public void mockCheckIfProjectExists(ProgrammingExercise exercise, final boolean exists) throws IOException, URISyntaxException {
+        final var projectKey = exercise.getProjectKey();
+        //final var projectName = exercise.getProjectName();
+        if (!exists) {
+            mockServer.expect(ExpectedCount.once(), requestTo(JENKINS_SERVER_URL + "/job/" + projectKey + "/api/json")).andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND).body("Folder " + projectKey + " does not exist!"));
+        } else {
+            mockServer.expect(ExpectedCount.once(), requestTo(JENKINS_SERVER_URL + "/job/" + projectKey + "/api/json")).andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK));
+        }
+    }
+
+    public void mockRemoveAllDefaultProjectPermissions(ProgrammingExercise exercise) {
+        //TODO after decision on how to handle users on Jenkins has been made
+    }
+
+    public void mockGiveProjectPermissions(String projectKey, List<String> groups, List<CIPermission> permissions) {
+        // TODO after decision on how to handle users on Jenkins has been made
+    }
+
+    public void mockCopyBuildPlanForParticipation(ProgrammingExercise exercise, String username) throws CliClient.RemoteRestException, CliClient.ClientException {
+        final var verifications = new LinkedList<Verifiable>();
+        final var projectKey = exercise.getProjectKey();
+        final var targetPlanName = username.toUpperCase();
+        final var targetPlanKey = projectKey + "-" + targetPlanName;
+        final var sourcePlanKey = projectKey + "-" + BuildPlanType.TEMPLATE.getName();
+
+        mockCopyBuildPlan(projectKey, sourcePlanKey, projectKey, targetPlanKey);
+    }
+
+    public void mockCopyBuildPlan(String sourceProjectKey, String sourcePlanName, String targetProjectKey, String targetPlanName)
+        throws CliClient.RemoteRestException, CliClient.ClientException {
+        mockServer.expect(ExpectedCount.once(), requestTo(JENKINS_SERVER_URL + "/job/" + sourceProjectKey + "/job/" + sourcePlanName + "config.xml"))
+            .andExpect(method(HttpMethod.GET)).andRespond(withStatus(HttpStatus.OK));
+        mockServer.expect(ExpectedCount.once(), requestTo(JENKINS_SERVER_URL + "/job/" + targetProjectKey + "/createItem?name=" + targetPlanName))
+            .andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockUpdatePlanRepositoryForParticipation(ProgrammingExercise exercise, String username) {
+        final var projectKey = exercise.getProjectKey();
+        final var planName = exercise.getProjectKey() + "-" +  username.toUpperCase();
+        final var repoNameInCI = Constants.ASSIGNMENT_REPO_NAME;
+        final var vcsRepositoryUrl = projectKey.toLowerCase() + "-" + username;
+
+        mockUpdatePlanRepository(exercise, projectKey, planName, repoNameInCI, vcsRepositoryUrl, List.of());
+    }
+
+    public void mockUpdatePlanRepository(ProgrammingExercise exercise, String projectKey, String planName, String repoNameInCI, String vcsRepositoryUrl, List<String> triggeredBy) {
+        //TODO
+    }
+
+    public void mockTriggerBuild(ProgrammingExerciseParticipation participation) throws URISyntaxException {
+        final var projectKey = participation.getProgrammingExercise().getProjectKey();
+        final var planKey = participation.getBuildPlanId();
+        mockTriggerBuild(projectKey, planKey);
+    }
+
+    public void mockTriggerBuild(String projectKey, String planKey) throws URISyntaxException {
+        final var triggerBuildPath = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/" + projectKey + "/job/" + planKey + "/build").toUriString();
+        mockServer.expect(requestTo(triggerBuildPath)).andExpect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockQueryLatestBuildResultFromBambooServer(final String projectKey,final String planKey) throws JsonProcessingException, URISyntaxException {
+        final var response = createBuildResult(projectKey + " - "+ planKey);
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/" + projectKey + "/job/" + planKey + "/lastBuild/testResults/api/json").toUriString();
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsString(response)));
+    }
+
+    private TestResultsDTO createBuildResult(final String planKey) {
+        final var result = new TestResultsDTO();
+        final var suite = new TestsuiteDTO();
+        suite.setTests(3);
+        suite.setTestCases(List.of(createFailedTest("test1"), createFailedTest("test2"), createFailedTest("test3")));
+        suite.setErrors(3);
+        suite.setFailures(0);
+        suite.setSkipped(0);
+        result.setFullName(planKey);
+        return result;
+    }
+
+    private TestCaseDTO createFailedTest(final String testName) {
+        final var test = new TestCaseDTO();
+        final var error = new ErrorDTO();
+
+        error.setMessage("java.lang.AssertionError: Some assertion failed");
+        test.setErrors(List.of(error));
+        test.setName(testName);
+        test.setTime(120);
+        return test;
+    }
+
+    public void mockRetrieveBuildStatus(final String projectKey, final String planKey) throws URISyntaxException, JsonProcessingException {
+        final var response = Map.of("building", false);
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/"+ projectKey+ "/job/" + planKey + "/lastBuild/api/json").toUriString();
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET))
+            .andRespond(withStatus(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(mapper.writeValueAsString(response)));
+    }
+
+    public void mockBuildPlanIsValid(final String projectKey, final String planKey, final boolean isValid) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/"+ projectKey+ "/job/" + planKey + "/api/json").toUriString();
+
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)).andRespond(withStatus(isValid ? HttpStatus.OK : HttpStatus.BAD_REQUEST));
+    }
+
+    public void mockEnablePlan(final String projectKey, final String planKey) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/"+ projectKey + "/job/" + planKey + "/enable").toUriString();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)) .andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void  mockDeleteProject(final String projectKey) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/"+ projectKey + "/doDelete").toUriString();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)) .andRespond(withStatus(HttpStatus.OK));
+    }
+
+    public void mockDeletePlan(final String projectKey, final String planKey) throws URISyntaxException {
+        final var uri = UriComponentsBuilder.fromUri(JENKINS_SERVER_URL.toURI()).path("/job/"+ projectKey + "/job/" + planKey + "/doDelete").toUriString();
+        mockServer.expect(requestTo(uri)).andExpect(method(HttpMethod.GET)) .andRespond(withStatus(HttpStatus.OK));
+    }
+
 }
