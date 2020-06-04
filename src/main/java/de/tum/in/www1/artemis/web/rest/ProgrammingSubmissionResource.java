292e1a3fcd48d01e48e87810e7ef4347ec20f6a9
Simon Leiß <git@simon-leiss.de>
2020-06-04 15:27:05 +0200

    Revert unwanted code change.

--- src/main/java/de/tum/in/www1/artemis/web/rest/ProgrammingSubmissionResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ProgrammingSubmissionResource.java
@@ -296,7 +296,7 @@ public class ProgrammingSubmissionResource {
      * We have removed this trigger for newly created exercises, but can't remove it from legacy ones.
      * This means that legacy exercises will trigger the repositories to be built, but we won't create submissions here anymore.
      * Therefore incoming build results will have to create new submissions with SubmissionType.OTHER.
-     *
+     * 
      * @param exerciseId the id of the programmingExercise where the test cases got changed
      * @param requestBody the body of the post request by the VCS.
      * @return the ResponseEntity with status 200 (OK)
