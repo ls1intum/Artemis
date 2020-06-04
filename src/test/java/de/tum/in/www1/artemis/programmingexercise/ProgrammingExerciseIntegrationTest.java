2788aa2445f5f58533f0096872c373b9e1ed0c05
F4lka <AdrianLoher@gmx.de>
2020-06-04 18:23:11 +0200

    adjusted testcase name

--- src/test/java/de/tum/in/www1/artemis/programmingexercise/ProgrammingExerciseIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/programmingexercise/ProgrammingExerciseIntegrationTest.java
@@ -208,7 +208,7 @@ class ProgrammingExerciseIntegrationTest extends AbstractSpringIntegrationBamboo
 
     @Test
     @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
-    void textExportSubmissionsByParticipationIds_invalidParticipation_badRequest() throws Exception {
+    void textExportSubmissionsByParticipationIds_invalidParticipationId_badRequest() throws Exception {
         final var path = Endpoints.ROOT
                 + Endpoints.EXPORT_SUBMISSIONS_BY_PARTICIPATIONS.replace("{exerciseId}", "" + programmingExercise.getId()).replace("{participationIds}", "" + 10L);
         request.postWithResponseBodyFile(path, getOptions(), HttpStatus.BAD_REQUEST);
