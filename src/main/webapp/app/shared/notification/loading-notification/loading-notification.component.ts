ccc8889d7d953b5ae34f2f3a1f746d7fd0752c5f
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-06-05 15:13:15 +0200

    run prettier

--- src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
+++ src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
@@ -5,53 +5,64 @@ import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-loading-notification',
-    template: `
-        <div *ngIf="isLoading" class="slider">
-            <div class="line"></div>
-            <div class="subline inc"></div>
-            <div class="subline dec"></div>
-        </div>`,
+    template: ` <div *ngIf="isLoading" class="slider">
+        <div class="line"></div>
+        <div class="subline inc"></div>
+        <div class="subline dec"></div>
+    </div>`,
     styles: [
         `
-            body{
-                background:#ffffff;
-                margin:50px 300px;
+            body {
+                background: #ffffff;
+                margin: 50px 300px;
             }
 
-            .slider{
-                position:absolute;
-                width:100%;
-                height:5px;
+            .slider {
+                position: absolute;
+                width: 100%;
+                height: 5px;
                 overflow-x: hidden;
             }
 
-            .line{
-                position:absolute;
+            .line {
+                position: absolute;
                 opacity: 0.4;
-                background:#4a8df8;
-                width:150%;
-                height:5px;
+                background: #4a8df8;
+                width: 150%;
+                height: 5px;
             }
 
-            .subline{
-                position:absolute;
-                background:#4a8df8;
-                height:5px;
+            .subline {
+                position: absolute;
+                background: #4a8df8;
+                height: 5px;
             }
-            .inc{
+            .inc {
                 animation: increase 2s infinite;
             }
-            .dec{
+            .dec {
                 animation: decrease 2s 0.5s infinite;
             }
 
             @keyframes increase {
-                from { left: -5%; width: 5%; }
-                to { left: 130%; width: 100%;}
+                from {
+                    left: -5%;
+                    width: 5%;
+                }
+                to {
+                    left: 130%;
+                    width: 100%;
+                }
             }
             @keyframes decrease {
-                from { left: -80%; width: 80%; }
-                to { left: 110%; width: 10%;}
+                from {
+                    left: -80%;
+                    width: 80%;
+                }
+                to {
+                    left: 110%;
+                    width: 10%;
+                }
             }
         `,
     ],
