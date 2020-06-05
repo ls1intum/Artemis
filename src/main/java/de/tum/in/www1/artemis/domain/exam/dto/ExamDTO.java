4362dacf9f109d8970739d1daf4cdbfe40c2cd5a
Sascha Beele <sascha@beele.io>
2020-06-05 19:41:13 +0200

    Add DTO for Exam entity

---
+++ src/main/java/de/tum/in/www1/artemis/domain/exam/dto/ExamDTO.java
@@ -0,0 +1,42 @@
+package de.tum.in.www1.artemis.domain.exam.dto;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+import de.tum.in.www1.artemis.domain.Course;
+import de.tum.in.www1.artemis.domain.ExerciseGroup;
+
+public class ExamDTO implements Serializable {
+
+    public ExamDTO(Long id, Course course, Set<ExerciseGroup> exerciseGroups, ZonedDateTime releaseDate, ZonedDateTime dueDate, String startText, String endText,
+            String confirmationStartText, String confirmationEndText) {
+        this.id = id;
+        this.course = course;
+        this.exerciseGroups = exerciseGroups;
+        this.releaseDate = releaseDate;
+        this.dueDate = dueDate;
+        this.startText = startText;
+        this.endText = endText;
+        this.confirmationStartText = confirmationStartText;
+        this.confirmationEndText = confirmationEndText;
+    }
+
+    public Long id;
+
+    public Course course;
+
+    public Set<ExerciseGroup> exerciseGroups;
+
+    public ZonedDateTime releaseDate;
+
+    public ZonedDateTime dueDate;
+
+    public String startText;
+
+    public String endText;
+
+    public String confirmationStartText;
+
+    public String confirmationEndText;
+}
---
+++ src/main/java/de/tum/in/www1/artemis/domain/exam/dto/ExamInstructorDTO.java
@@ -0,0 +1,45 @@
+package de.tum.in.www1.artemis.domain.exam.dto;
+
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+import de.tum.in.www1.artemis.domain.Course;
+import de.tum.in.www1.artemis.domain.ExerciseGroup;
+import de.tum.in.www1.artemis.domain.exam.StudentExam;
+
+public class ExamInstructorDTO {
+
+    public ExamInstructorDTO(Long id, Course course, Set<StudentExam> studentExams, Set<ExerciseGroup> exerciseGroups, ZonedDateTime releaseDate, ZonedDateTime dueDate,
+            String startText, String endText, String confirmationStartText, String confirmationEndText) {
+        this.id = id;
+        this.course = course;
+        this.studentExams = studentExams;
+        this.exerciseGroups = exerciseGroups;
+        this.releaseDate = releaseDate;
+        this.dueDate = dueDate;
+        this.startText = startText;
+        this.endText = endText;
+        this.confirmationStartText = confirmationStartText;
+        this.confirmationEndText = confirmationEndText;
+    }
+
+    public Long id;
+
+    public Course course;
+
+    public Set<StudentExam> studentExams;
+
+    public Set<ExerciseGroup> exerciseGroups;
+
+    public ZonedDateTime releaseDate;
+
+    public ZonedDateTime dueDate;
+
+    public String startText;
+
+    public String endText;
+
+    public String confirmationStartText;
+
+    public String confirmationEndText;
+}
