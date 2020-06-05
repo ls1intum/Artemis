5251eaa9f00615b8fd0938521fc6243b7fd10335
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-06-05 14:53:56 +0200

    try other loading bar

--- src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
+++ src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
@@ -5,53 +5,53 @@ import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-loading-notification',
-    template: ` <div *ngIf="isLoading" class="progress-line"></div> `,
+    template: `
+        <div *ngIf="isLoading" class="slider">
+            <div class="line"></div>
+            <div class="subline inc"></div>
+            <div class="subline dec"></div>
+        </div>`,
     styles: [
         `
-            .progress-line,
-            .progress-line:before {
-                height: 7px;
-                width: 100%;
-                margin: 0;
+            body{
+                background:#ffffff;
+                margin:50px 300px;
             }
-            .progress-line {
-                background-color: #b3d4fc;
-                display: -webkit-flex;
-                display: flex;
+
+            .slider{
+                position:absolute;
+                width:100%;
+                height:5px;
+                overflow-x: hidden;
             }
-            .progress-line:before {
-                background-color: #3f51b5;
-                content: '';
-                -webkit-animation: running-progress 1.3s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-                animation: running-progress 1.3s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+
+            .line{
+                position:absolute;
+                opacity: 0.4;
+                background:#4a8df8;
+                width:150%;
+                height:5px;
             }
-            @-webkit-keyframes running-progress {
-                0% {
-                    margin-left: 0px;
-                    margin-right: 100%;
-                }
-                50% {
-                    margin-left: 25%;
-                    margin-right: 0%;
-                }
-                100% {
-                    margin-left: 100%;
-                    margin-right: 0;
-                }
+
+            .subline{
+                position:absolute;
+                background:#4a8df8;
+                height:5px;
             }
-            @keyframes running-progress {
-                0% {
-                    margin-left: 0px;
-                    margin-right: 100%;
-                }
-                50% {
-                    margin-left: 25%;
-                    margin-right: 0%;
-                }
-                100% {
-                    margin-left: 100%;
-                    margin-right: 0;
-                }
+            .inc{
+                animation: increase 2s infinite;
+            }
+            .dec{
+                animation: decrease 2s 0.5s infinite;
+            }
+
+            @keyframes increase {
+                from { left: -5%; width: 5%; }
+                to { left: 130%; width: 100%;}
+            }
+            @keyframes decrease {
+                from { left: -80%; width: 80%; }
+                to { left: 110%; width: 10%;}
             }
         `,
     ],
@@ -66,12 +66,8 @@ export class LoadingNotificationComponent implements OnInit, OnDestroy {
         /**
          * wait 200 ms before updating isLoading value to ensure the loading screen will not be visible for fast HttpRequests
          * */
-        console.log('loading status');
-        console.log(this.loadingNotificationService.loadingStatus);
         this.loadingSubscription = this.loadingNotificationService.loadingStatus.pipe(debounceTime(200)).subscribe((value) => {
-            console.log('before: ' + this.isLoading);
             this.isLoading = value;
-            console.log('after: ' + this.isLoading);
         });
     }
 
