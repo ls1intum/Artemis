78f1117e106af537e1020b4f060273be775fe800
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-06-08 22:41:39 +0200

    changed icon to loading spinner and increased debounce time

--- src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
+++ src/main/webapp/app/shared/notification/loading-notification/loading-notification.component.ts
@@ -5,67 +5,7 @@ import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'jhi-loading-notification',
-    template: ` <div *ngIf="isLoading" class="slider">
-        <div class="line"></div>
-        <div class="subline inc"></div>
-        <div class="subline dec"></div>
-    </div>`,
-    styles: [
-        `
-            body {
-                background: #ffffff;
-                margin: 50px 300px;
-            }
-
-            .slider {
-                position: absolute;
-                width: 100%;
-                height: 5px;
-                overflow-x: hidden;
-            }
-
-            .line {
-                position: absolute;
-                opacity: 0.4;
-                background: #4a8df8;
-                width: 150%;
-                height: 5px;
-            }
-
-            .subline {
-                position: absolute;
-                background: #4a8df8;
-                height: 5px;
-            }
-            .inc {
-                animation: increase 2s infinite;
-            }
-            .dec {
-                animation: decrease 2s 0.5s infinite;
-            }
-
-            @keyframes increase {
-                from {
-                    left: -5%;
-                    width: 5%;
-                }
-                to {
-                    left: 130%;
-                    width: 100%;
-                }
-            }
-            @keyframes decrease {
-                from {
-                    left: -80%;
-                    width: 80%;
-                }
-                to {
-                    left: 110%;
-                    width: 10%;
-                }
-            }
-        `,
-    ],
+    template: ` <div *ngIf="isLoading" class="spinner-border text-light ml-2" role="status" style="width: 1.5rem; height: 1.5rem;"></div> `,
 })
 export class LoadingNotificationComponent implements OnInit, OnDestroy {
     isLoading = false;
@@ -75,9 +15,9 @@ export class LoadingNotificationComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         /**
-         * wait 200 ms before updating isLoading value to ensure the loading screen will not be visible for fast HttpRequests
+         * wait 1000 ms before updating isLoading value to ensure the loading screen will not be visible for fast HttpRequests
          * */
-        this.loadingSubscription = this.loadingNotificationService.loadingStatus.pipe(debounceTime(200)).subscribe((value) => {
+        this.loadingSubscription = this.loadingNotificationService.loadingStatus.pipe(debounceTime(1000)).subscribe((value) => {
             this.isLoading = value;
         });
     }
