78d6b27c0419154146e6893aea81b7027fedff25
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-06-09 09:14:53 +0200

    Add Tests
    Signed-off-by: Jan Philip Bernius <janphilip.bernius@tum.de>

--- src/main/java/de/tum/in/www1/artemis/config/ApiVersionFilter.java
+++ src/main/java/de/tum/in/www1/artemis/config/ApiVersionFilter.java
@@ -1,11 +1,6 @@
 package de.tum.in.www1.artemis.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,12 +9,20 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ApiVersionFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiVersionFilter.class);
+
+    public static final String CONTENT_VERSION_HEADER = "Content-Version";
 
     @Value("${artemis.version}")
     private String VERSION;
@@ -31,17 +34,20 @@ public class ApiVersionFilter implements Filter {
 
         logger.debug("Adding Version to Response to {}", httpRequest.getRequestURI());
 
-        httpResponse.addHeader("Content-Version", VERSION);
+        httpResponse.addHeader(CONTENT_VERSION_HEADER, VERSION);
 
         chain.doFilter(httpRequest, httpResponse);
     }
 
     @Bean
     public FilterRegistrationBean<ApiVersionFilter> registerFilter() {
+        logger.debug("Register API Version Filter.");
+
         final FilterRegistrationBean<ApiVersionFilter> bean = new FilterRegistrationBean<>();
 
         bean.setFilter(new ApiVersionFilter());
-        bean.addUrlPatterns("/api/*");
+        bean.addUrlPatterns("/api/**");
+        bean.addUrlPatterns("/management/**");
 
         return bean;
     }
