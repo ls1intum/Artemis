5d864f409a843dd432e24b1545a71052cedf2353
Stefan Waldhauser <stefan.waldhauser@tum.de>
2020-06-09 09:44:06 +0200

    Fixed typo

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
@@ -11,7 +11,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.User;
@@ -25,7 +30,7 @@ import de.tum.in.www1.artemis.web.rest.errors.BadRequestAlertException;
 import de.tum.in.www1.artemis.web.rest.util.HeaderUtil;
 
 /**
- * REST controller for managing Course.
+ * REST controller for managing Exam.
  */
 @RestController
 @RequestMapping("/api")
