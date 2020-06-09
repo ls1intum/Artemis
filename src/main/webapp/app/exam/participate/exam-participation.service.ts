3d1a0c237eb5ec2cd6549366f698f4ce3fe5d1e5
Filip Gregurevic <filipgregurevic@gmail.com>
2020-06-09 07:45:44 +0200

    remove unused vars

--- src/main/webapp/app/exam/participate/exam-participation.service.ts
+++ src/main/webapp/app/exam/participate/exam-participation.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
 
 import { SERVER_API_URL } from 'app/app.constants';
 
 @Injectable({ providedIn: 'root' })
 export class ExamParticipationService {
     public resourceUrl = SERVER_API_URL + 'api/exams';
-
-    constructor(http: HttpClient) {}
 }
