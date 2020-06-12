5550487f94495a140d447ee1463550947a4e7d3e
Sascha Beele <sascha@beele.io>
2020-06-12 15:01:54 +0200

    Improve ExamResource

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
@@ -1,7 +1,5 @@
 package de.tum.in.www1.artemis.web.rest;
 
-import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.notFound;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -20,8 +18,6 @@ import de.tum.in.www1.artemis.config.Constants;
 import de.tum.in.www1.artemis.domain.User;
 import de.tum.in.www1.artemis.domain.exam.Exam;
 import de.tum.in.www1.artemis.repository.ExamRepository;
-import de.tum.in.www1.artemis.service.AuthorizationCheckService;
-import de.tum.in.www1.artemis.service.CourseService;
 import de.tum.in.www1.artemis.service.ExamService;
 import de.tum.in.www1.artemis.service.UserService;
 import de.tum.in.www1.artemis.web.rest.errors.BadRequestAlertException;
@@ -43,23 +39,16 @@ public class ExamResource {
 
     private final UserService userService;
 
-    private final CourseService courseService;
-
     private final ExamService examService;
 
     private final ExamRepository examRepository;
 
-    private final AuthorizationCheckService authCheckService;
-
     private final AuditEventRepository auditEventRepository;
 
-    public ExamResource(UserService userService, CourseService courseService, ExamService examService, ExamRepository examRepository, AuthorizationCheckService authCheckService,
-            AuditEventRepository auditEventRepository) {
+    public ExamResource(UserService userService, ExamService examService, ExamRepository examRepository, AuditEventRepository auditEventRepository) {
         this.userService = userService;
-        this.courseService = courseService;
         this.examService = examService;
         this.examRepository = examRepository;
-        this.authCheckService = authCheckService;
         this.auditEventRepository = auditEventRepository;
     }
 
@@ -105,14 +94,9 @@ public class ExamResource {
             return createExam(courseId, updatedExam);
         }
 
-        Optional<ResponseEntity<Exam>> courseAccessFailure = examService.checkCourseAccess(courseId);
-        if (courseAccessFailure.isPresent()) {
-            return courseAccessFailure.get();
-        }
-
-        Optional<Exam> existingExam = examRepository.findById(updatedExam.getId());
-        if (existingExam.isEmpty()) {
-            return notFound();
+        Optional<ResponseEntity<Exam>> courseAndExamAccessFailure = examService.checkCourseAndExamAccess(courseId, updatedExam.getId());
+        if (courseAndExamAccessFailure.isPresent()) {
+            return courseAndExamAccessFailure.get();
         }
 
         Exam result = examService.save(updatedExam);
@@ -125,11 +109,10 @@ public class ExamResource {
      * @param courseId  the course to which the exam belongs
      * @param examId    the exam to find
      * @return the ResponseEntity with status 200 (OK) and with the found exam as body
-     * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @GetMapping("/courses/{courseId}/exams/{examId}")
     @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
-    public ResponseEntity<Exam> getExam(@PathVariable Long courseId, @PathVariable Long examId) throws URISyntaxException {
+    public ResponseEntity<Exam> getExam(@PathVariable Long courseId, @PathVariable Long examId) {
         log.debug("REST request to get Exam : {}", examId);
         Optional<ResponseEntity<Exam>> courseAndExamAccessFailure = examService.checkCourseAndExamAccess(courseId, examId);
         Exam exam = examService.findOne(examId);
@@ -141,11 +124,10 @@ public class ExamResource {
      *
      * @param courseId  the course to which the exam belongs
      * @return the ResponseEntity with status 200 (OK) and a list of exams. The list can be empty
-     * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @GetMapping("/courses/{courseId}/exams")
     @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
-    public ResponseEntity<List<Exam>> getExamsForCourse(@PathVariable Long courseId) throws URISyntaxException {
+    public ResponseEntity<List<Exam>> getExamsForCourse(@PathVariable Long courseId) {
         log.debug("REST request to get all exams for Course : {}", courseId);
         Optional<ResponseEntity<List<Exam>>> courseAccessFailure = examService.checkCourseAccess(courseId);
         return courseAccessFailure.orElseGet(() -> ResponseEntity.ok(examService.findAllByCourseId(courseId)));
@@ -156,11 +138,10 @@ public class ExamResource {
      *
      * @param courseId the course to which the exam belongs
      * @return the ResponseEntity with status 200 (OK)
-     * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @DeleteMapping("/courses/{courseId}/exams/{examId}")
     @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
-    public ResponseEntity<Void> deleteExam(@PathVariable Long courseId, @PathVariable Long examId) throws URISyntaxException {
+    public ResponseEntity<Void> deleteExam(@PathVariable Long courseId, @PathVariable Long examId) {
         log.info("REST request to delete Exam : {}", examId);
 
         Optional<ResponseEntity<Void>> courseAndExamAccessFailure = examService.checkCourseAndExamAccess(courseId, examId);
