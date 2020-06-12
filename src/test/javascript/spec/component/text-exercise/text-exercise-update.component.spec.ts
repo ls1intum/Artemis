2ce9c722816e55685d5b3c91cd75a856b4adba36
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-06-12 20:58:34 +0200

    Add client tests

--- src/test/javascript/spec/component/text-exercise/text-exercise-update.component.spec.ts
+++ src/test/javascript/spec/component/text-exercise/text-exercise-update.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpResponse } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
 import { ArtemisTestModule } from '../../test.module';
@@ -8,6 +9,9 @@ import { TextExerciseService } from 'app/exercises/text/manage/text-exercise/tex
 import { TextExercise } from 'app/entities/text-exercise.model';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
+import { ExerciseGroup } from 'app/entities/exercise-group.model';
+import { MockActivatedRoute } from '../../helpers/mocks/activated-route/mock-activated-route';
+import { Course } from 'app/entities/course.model';
 
 describe('TextExercise Management Update Component', () => {
     let comp: TextExerciseUpdateComponent;
@@ -20,6 +24,7 @@ describe('TextExercise Management Update Component', () => {
             providers: [
                 { provide: LocalStorageService, useClass: MockSyncStorage },
                 { provide: SessionStorageService, useClass: MockSyncStorage },
+                { provide: ActivatedRoute, useValue: new MockActivatedRoute() },
             ],
             declarations: [TextExerciseUpdateComponent],
         })
@@ -59,6 +64,42 @@ describe('TextExercise Management Update Component', () => {
             // THEN
             expect(service.create).toHaveBeenCalledWith(entity);
             expect(comp.isSaving).toEqual(false);
+        }));
+    });
+
+    describe('ngOnInit with given exerciseGroup', () => {
+        let textExerciseForExam = new TextExercise(null, new ExerciseGroup());
+
+        beforeEach(() => {
+            let route = TestBed.get(ActivatedRoute);
+            route.data = of({ textExercise: textExerciseForExam });
+        });
+
+        it('Should be in exam mode', fakeAsync(() => {
+            // WHEN
+            comp.ngOnInit();
+            tick(); // simulate async
+            // THEN
+            expect(comp.isExamMode).toEqual(true);
+            expect(comp.textExercise).toEqual(textExerciseForExam);
+        }));
+    });
+
+    describe('ngOnInit without given exerciseGroup', () => {
+        let textExercise = new TextExercise(new Course(), null);
+
+        beforeEach(() => {
+            let route = TestBed.get(ActivatedRoute);
+            route.data = of({ textExercise: textExercise });
+        });
+
+        it('Should not be in exam mode', fakeAsync(() => {
+            // WHEN
+            comp.ngOnInit();
+            tick(); // simulate async
+            // THEN
+            expect(comp.isExamMode).toEqual(false);
+            expect(comp.textExercise).toEqual(textExercise);
         }));
     });
 });
