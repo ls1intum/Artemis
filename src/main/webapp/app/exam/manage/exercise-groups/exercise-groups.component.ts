9d7d4a7e1f9ff73268d9e407486a176996de7a54
Sascha Beele <sascha@beele.io>
2020-06-13 01:33:41 +0200

    Get all exercise groups for the exam on initialization

--- src/main/webapp/app/exam/manage/exercise-groups/exercise-groups.component.ts
+++ src/main/webapp/app/exam/manage/exercise-groups/exercise-groups.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ExerciseGroupService } from 'app/exam/manage/exercise-groups/exercise-group.service';
+import { ExerciseGroup } from 'app/entities/exercise-group.model';
 
 @Component({
     selector: 'jhi-exercise-groups',
@@ -9,14 +10,16 @@ import { ExerciseGroupService } from 'app/exam/manage/exercise-groups/exercise-g
 export class ExerciseGroupsComponent implements OnInit {
     courseId: number;
     examId: number;
+    exerciseGroups: ExerciseGroup[] | null;
 
     constructor(private route: ActivatedRoute, private exerciseGroupService: ExerciseGroupService) {}
 
     /**
-     * Initialize the courseId and examId
+     * Initialize the courseId and examId. Get all exercise groups for the exam.
      */
     ngOnInit(): void {
         this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
         this.examId = Number(this.route.snapshot.paramMap.get('examId'));
+        this.exerciseGroupService.findAllForExam(this.courseId, this.examId).subscribe((res) => (this.exerciseGroups = res.body));
     }
 }
