eca9fee1cbd0b2754647a44e181ae743945f4b5d
tessaRuckstuhl <tessa.ruckstuhl@web.de>
2020-06-14 19:52:20 +0200

    remove mocks + adjust component logic

--- src/main/webapp/app/exam/participate/exam-cover/exam-cover.component.ts
+++ src/main/webapp/app/exam/participate/exam-cover/exam-cover.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import * as moment from 'moment';
 import { SafeHtml } from '@angular/platform-browser';
-import { Exam } from 'app/entities/exam.model';
 
 import { ArtemisMarkdownService } from 'app/shared/markdown.service';
-
 import { CourseManagementService } from 'app/course/manage/course-management.service';
-import { ActivatedRoute } from '@angular/router';
+
+import { Exam } from 'app/entities/exam.model';
 import { Course } from 'app/entities/course.model';
 
 @Component({
@@ -15,36 +14,32 @@ import { Course } from 'app/entities/course.model';
     styles: [],
 })
 export class ExamCoverComponent implements OnInit, OnDestroy {
-    // boolean true - use start text, false - use end text
     /**
-     *
+     * if startView is set to true: startText and confirmationStartText will be displayed
+     * if startView is set to false: endText and confirmationEndText will be displayed
      */
     @Input() startView: boolean;
     @Input() exam: Exam;
-    course: Course;
+    course: Course | null;
     courseId = 0;
-
     title: string;
     submitEnabled: boolean;
     confirmed: boolean;
+    examId: number;
 
     formattedGeneralInformation: SafeHtml | null;
     formattedConfirmationText: SafeHtml | null;
 
     interval: any;
 
-    constructor(private courseService: CourseManagementService, private route: ActivatedRoute, private artemisMarkdown: ArtemisMarkdownService) {}
+    constructor(private courseService: CourseManagementService, private artemisMarkdown: ArtemisMarkdownService) {}
 
     /**
      * initializes the component
      */
     ngOnInit(): void {
-        // mocks values
-        // this.mock();
-
         this.confirmed = false;
         this.submitEnabled = false;
-
         if (this.startView) {
             this.formattedGeneralInformation = this.artemisMarkdown.safeHtmlForMarkdown(this.exam.startText);
             this.formattedConfirmationText = this.artemisMarkdown.safeHtmlForMarkdown(this.exam.confirmationStartText);
@@ -93,36 +88,4 @@ export class ExamCoverComponent implements OnInit, OnDestroy {
      * TODO: add session management, this function is bound to the start exam button
      */
     startExam() {}
-
-    // TODO remove
-    mock() {
-        this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
-        this.courseService.find(this.courseId).subscribe((courseResponse) => (this.course = courseResponse.body!));
-
-        this.title = 'ARTEMIS 101 ENDTERM';
-        this.startView = true;
-        this.exam = {
-            id: 1,
-            startText:
-                '- *You can answer exam questions either in German or English. Once you have selected a language, you cannot change it.*\n' +
-                '- Physically turn off all electronic devices, put them into your bag and close the bag.\n' +
-                '- ```No aids are permitted except one handwritten DIN A4 sheet of paper and a language dictionary without any notes.``` We reserve the right to check this.\n' +
-                '- No questions related to the exam content will be answered.\n' +
-                '- The exam consists of questions worth 60 points and is 90 minutes long. Use the amount of points and the text of the question to determine the appropriate length of your answer. Please dont give too long answers...\n' +
-                '- Explain concepts and differences in your own words. Answering an exam question only with a\n' +
-                'definition from the slides **will not** give any points.',
-            endText: 'You can answer exam questions either in G o E.',
-            confirmationStartText: 'Hereby I confirm that I will obey the stated rules and will write this exam alone with no outside help.',
-            confirmationEndText: 'Hereby I confirm that I will obey the stated rules',
-            startDate: moment().add(20, 'seconds'),
-            endDate: moment(),
-            visibleDate: null,
-            maxPoints: null,
-            numberOfExercisesInExam: null,
-            randomizeExerciseOrder: true,
-            course: this.course,
-            exerciseGroups: null,
-            studentExams: null,
-        };
-    }
 }
