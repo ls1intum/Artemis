9abff16d1c5318ed95cd189858cfc1e5e5fa8d74
Sascha Beele <sascha@beele.io>
2020-06-14 13:54:46 +0200

    Refactor StudentExamIntegrationTest

--- src/test/java/de/tum/in/www1/artemis/StudentExamIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/StudentExamIntegrationTest.java
@@ -18,7 +18,6 @@ import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.exam.Exam;
 import de.tum.in.www1.artemis.domain.exam.StudentExam;
 import de.tum.in.www1.artemis.service.ExamAccessService;
-import de.tum.in.www1.artemis.service.StudentExamService;
 import de.tum.in.www1.artemis.util.DatabaseUtilService;
 import de.tum.in.www1.artemis.util.RequestUtilService;
 
@@ -33,9 +32,6 @@ public class StudentExamIntegrationTest extends AbstractSpringIntegrationBambooB
     @SpyBean
     ExamAccessService examAccessService;
 
-    @Autowired
-    StudentExamService studentExamService;
-
     private Course course1;
 
     private Exam exam1;
@@ -49,7 +45,7 @@ public class StudentExamIntegrationTest extends AbstractSpringIntegrationBambooB
         exam1 = database.addExam(course1);
         Exam exam2 = database.addExam(course1);
         studentExam1 = database.addStudentExam(exam1);
-        StudentExam studentExam2 = database.addStudentExam(exam2);
+        database.addStudentExam(exam2);
     }
 
     @AfterEach
@@ -59,14 +55,20 @@ public class StudentExamIntegrationTest extends AbstractSpringIntegrationBambooB
 
     @Test
     @WithMockUser(username = "student1", roles = "USER")
-    public void testGetStudentExam_asStudent() throws Exception {
-        request.get("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams/" + studentExam1.getId(), HttpStatus.FORBIDDEN, StudentExam.class);
+    public void testAll_asStudent() throws Exception {
+        this.testAllPreAuthorize();
     }
 
     @Test
     @WithMockUser(username = "tutor1", roles = "TA")
-    public void testGetStudentExam_asTutor() throws Exception {
+    public void testAll_asTutor() throws Exception {
+        this.testAllPreAuthorize();
+    }
+
+    private void testAllPreAuthorize() throws Exception {
         request.get("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams/" + studentExam1.getId(), HttpStatus.FORBIDDEN, StudentExam.class);
+        request.getList("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams", HttpStatus.FORBIDDEN, StudentExam.class);
+
     }
 
     @Test
@@ -74,18 +76,6 @@ public class StudentExamIntegrationTest extends AbstractSpringIntegrationBambooB
     public void testGetStudentExam_asInstructor() throws Exception {
         request.get("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams/" + studentExam1.getId(), HttpStatus.OK, StudentExam.class);
         verify(examAccessService, times(1)).checkCourseAndExamAndStudentExamAccess(course1.getId(), exam1.getId(), studentExam1.getId());
-    }
-
-    @Test
-    @WithMockUser(username = "student1", roles = "USER")
-    public void testGetStudentExamsForExam_asStudent() throws Exception {
-        request.getList("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams", HttpStatus.FORBIDDEN, StudentExam.class);
-    }
-
-    @Test
-    @WithMockUser(username = "tutor1", roles = "TA")
-    public void testGetStudentExamsForExam_asTutor() throws Exception {
-        request.getList("/api/courses/" + course1.getId() + "/exams/" + exam1.getId() + "/studentExams", HttpStatus.FORBIDDEN, StudentExam.class);
     }
 
     @Test
