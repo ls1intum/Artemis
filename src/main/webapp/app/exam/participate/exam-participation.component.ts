b87320f936ea69c5c8afe57b54b19f1010851bc5
Hanya Elhashemy <ga87saf@mytum.de>
2020-06-15 15:03:23 +0200

    add connection status footer

--- src/main/webapp/app/exam/participate/exam-participation.component.ts
+++ src/main/webapp/app/exam/participate/exam-participation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from 'app/entities/course.model';
+import { JhiWebsocketService } from 'app/core/websocket/websocket.service';
 
 @Component({
     selector: 'jhi-exam-participation',
@@ -11,8 +12,16 @@ import { Course } from 'app/entities/course.model';
 export class ExamParticipationComponent implements OnInit {
     course: Course;
     courseId = 0;
+    unsavedChanges = false;
+    disconnected = false;
 
-    constructor(private courseService: CourseManagementService, private route: ActivatedRoute) {}
+    /**
+     * Websocket channels
+     */
+    onConnected: () => void;
+    onDisconnected: () => void;
+
+    constructor(private courseService: CourseManagementService, private jhiWebsocketService: JhiWebsocketService, private route: ActivatedRoute) {}
 
     /**
      * initializes courseId and course
@@ -20,5 +29,28 @@ export class ExamParticipationComponent implements OnInit {
     ngOnInit(): void {
         this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
         this.courseService.find(this.courseId).subscribe((courseResponse) => (this.course = courseResponse.body!));
+        this.initLiveMode();
+    }
+
+    initLiveMode() {
+        // listen to connect / disconnect events
+        this.onConnected = () => {
+            if (this.disconnected) {
+                // if the disconnect happened during the live exam and there are unsaved changes, we trigger a selection changed event to save the submission on the server
+                if (this.unsavedChanges) {
+                    // ToDo: save submission on server
+                }
+            }
+            this.disconnected = false;
+        };
+        this.jhiWebsocketService.bind('connect', () => {
+            this.onConnected();
+        });
+        this.onDisconnected = () => {
+            this.disconnected = true;
+        };
+        this.jhiWebsocketService.bind('disconnect', () => {
+            this.onDisconnected();
+        });
     }
 }
--- src/main/webapp/app/exam/participate/exam-participation.module.ts
+++ src/main/webapp/app/exam/participate/exam-participation.module.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 
 import { ExamParticipationComponent } from 'app/exam/participate/exam-participation.component';
 import { examParticipationState } from 'app/exam/participate/exam-participation.route';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
 
 const ENTITY_STATES = [...examParticipationState];
 
 @NgModule({
-    imports: [RouterModule.forChild(ENTITY_STATES)],
+    imports: [RouterModule.forChild(ENTITY_STATES), ArtemisSharedModule],
     declarations: [ExamParticipationComponent],
 })
 export class ArtemisExamParticipationModule {}
