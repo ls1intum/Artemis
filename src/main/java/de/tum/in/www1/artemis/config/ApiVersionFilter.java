3fee6b86613d8c3284f712bf1ad689912ab9c378
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-06-16 22:28:08 +0200

    Remove @Profile("artemis")
    Signed-off-by: Jan Philip Bernius <janphilip.bernius@tum.de>

--- src/main/java/de/tum/in/www1/artemis/config/ApiVersionFilter.java
+++ src/main/java/de/tum/in/www1/artemis/config/ApiVersionFilter.java
@@ -13,11 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("artemis")
 public class ApiVersionFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiVersionFilter.class);
