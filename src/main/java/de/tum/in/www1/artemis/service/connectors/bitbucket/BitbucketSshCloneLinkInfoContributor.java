aa6dfb40ba32ec9ee186068510152af666386cac
Kilian Schulte <schultek@in.tum.de>
2020-06-16 16:00:57 +0200

    make config values optional

--- src/main/java/de/tum/in/www1/artemis/service/connectors/bitbucket/BitbucketSshCloneLinkInfoContributor.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/bitbucket/BitbucketSshCloneLinkInfoContributor.java
@@ -1,6 +1,7 @@
 package de.tum.in.www1.artemis.service.connectors.bitbucket;
 
 import java.net.URL;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
@@ -17,18 +18,22 @@ public class BitbucketSshCloneLinkInfoContributor implements InfoContributor {
     @Value("${artemis.version-control.url}")
     private URL BITBUCKED_SERVER_URL;
 
-    @Value("${artemis.version-control.ssh-template-clone-url:}")
-    private String BITBUCKET_SSH_URL_TEMPLATE;
+    @Value("${artemis.version-control.ssh-template-clone-url:#{null}}")
+    private Optional<String> BITBUCKET_SSH_URL_TEMPLATE;
 
-    @Value("${artemis.version-control.ssh-keys-path:}")
-    private String BITBUCKET_SSH_KEYS_PATH;
+    @Value("${artemis.version-control.ssh-keys-url-path:#{null}}")
+    private Optional<String> BITBUCKET_SSH_KEYS_URL_PATH;
 
     @Override
     public void contribute(Info.Builder builder) {
 
-        final var sshKeysUrl = BITBUCKED_SERVER_URL + BITBUCKET_SSH_KEYS_PATH;
+        if (BITBUCKET_SSH_URL_TEMPLATE.isPresent()) {
+            builder.withDetail(Constants.INFO_SSH_CLONE_URL_DETAIL, BITBUCKET_SSH_URL_TEMPLATE);
+            if (BITBUCKET_SSH_KEYS_URL_PATH.isPresent()) {
+                final var sshKeysUrl = BITBUCKED_SERVER_URL + BITBUCKET_SSH_KEYS_URL_PATH.get();
+                builder.withDetail(Constants.INFO_SSH_KEYS_URL_DETAIL, sshKeysUrl);
+            }
+        }
 
-        builder.withDetail(Constants.INFO_SSH_CLONE_URL_DETAIL, BITBUCKET_SSH_URL_TEMPLATE);
-        builder.withDetail(Constants.INFO_SSH_KEYS_URL_DETAIL, sshKeysUrl);
     }
 }
