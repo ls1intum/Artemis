bd250327dfb467777d5fe35c4a97821d6e70f4df
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-06-19 00:37:47 +0200

    Fix edit button for exam programming exercise on detail page

--- src/main/webapp/app/exercises/programming/manage/programming-exercise-detail.component.ts
+++ src/main/webapp/app/exercises/programming/manage/programming-exercise-detail.component.ts
@@ -30,6 +30,7 @@ export class ProgrammingExerciseDetailComponent implements OnInit, OnDestroy {
     readonly PROGRAMMING = ExerciseType.PROGRAMMING;
 
     programmingExercise: ProgrammingExercise;
+    isExamExercise: boolean;
 
     loadingTemplateParticipationResults = true;
     loadingSolutionParticipationResults = true;
@@ -51,6 +52,7 @@ export class ProgrammingExerciseDetailComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ programmingExercise }) => {
             this.programmingExercise = programmingExercise;
+            this.isExamExercise = !!this.programmingExercise.exerciseGroup;
             this.gradingInstructions = this.artemisMarkdown.safeHtmlForMarkdown(this.programmingExercise.gradingInstructions);
             this.programmingExercise.isAtLeastTutor = this.accountService.isAtLeastTutorInCourse(programmingExercise.course);
             this.programmingExercise.isAtLeastInstructor = this.accountService.isAtLeastInstructorInCourse(programmingExercise.course);
@@ -93,6 +95,27 @@ export class ProgrammingExerciseDetailComponent implements OnInit, OnDestroy {
 
     previousState() {
         window.history.back();
+    }
+
+    /**
+     * Returns the route for editing the exercise. Exam and course exercises have different routes.
+     */
+    getEditRoute() {
+        if (this.isExamExercise) {
+            return [
+                '/course-management',
+                this.programmingExercise.exerciseGroup?.exam?.course?.id,
+                'exams',
+                this.programmingExercise.exerciseGroup?.exam?.id,
+                'exercise-groups',
+                this.programmingExercise.exerciseGroup?.id,
+                'programming-exercises',
+                this.programmingExercise.id,
+                'edit',
+            ];
+        } else {
+            return ['/course-management', this.programmingExercise.course?.id, 'programming-exercises', this.programmingExercise.id, 'edit'];
+        }
     }
 
     combineTemplateCommits() {
