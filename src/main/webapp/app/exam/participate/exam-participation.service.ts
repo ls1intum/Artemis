3e78cb3628967302f732f753bc6a4ae85a3e10d0
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-06-19 22:15:50 +0200

    add skeleton functions for service

--- src/main/webapp/app/exam/participate/exam-participation.service.ts
+++ src/main/webapp/app/exam/participate/exam-participation.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from 'app/app.constants';
+import { Observable, of } from 'rxjs';
+import { Submission } from 'app/entities/submission.model';
 
 @Injectable({ providedIn: 'root' })
 export class ExamParticipationService {
     public resourceUrl = SERVER_API_URL + 'api/courses';
 
     constructor() {}
+
+    getLatestSubmissionForParticipation(id: number): Observable<any> {
+        // TODO add service call
+        return of(false);
+    }
+
+    updateSubmission(submission: Submission, id: number) {
+        // TODO add service call
+    }
 }
