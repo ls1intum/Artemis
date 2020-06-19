70108c65cd8ad3a13dfacc8e4afd9ab9a103db8d
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-06-20 00:23:34 +0200

    Removed unused import

--- src/test/javascript/spec/component/programming-exercise/programming-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/programming-exercise-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
