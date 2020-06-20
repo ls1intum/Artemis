9024a6359ac04314805fca1567e7029006164cb0
Filip Gregurevic <filipgregurevic@gmail.com>
2020-06-20 17:24:11 +0200

    remove unused

--- src/main/webapp/app/entities/student-exam.model.ts
+++ src/main/webapp/app/entities/student-exam.model.ts
@@ -2,7 +2,6 @@ import { User } from 'app/core/user/user.model';
 import { Exam } from 'app/entities/exam.model';
 import { Exercise } from 'app/entities/exercise.model';
 import { BaseEntity } from 'app/shared/model/base-entity';
-import { Participation } from 'app/entities/participation/participation.model';
 
 export class StudentExam implements BaseEntity {
     public id: number;
