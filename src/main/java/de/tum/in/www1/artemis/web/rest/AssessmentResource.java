fbbbdfb81d304a3ce7953108ea8221b10a4f5eb4
Andi Turdiu <andi.turdiu@tum.de>
2020-06-20 17:03:07 +0200

    fix auth for exam exercises

--- src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
@@ -59,7 +59,7 @@ public abstract class AssessmentResource {
 
     void validateExercise(Exercise exercise) throws BadRequestAlertException {
         Course course = exercise.getCourse();
-        if (course == null) {
+        if (course == null && exercise.getExerciseGroup().getExam().getCourse() == null) {
             throw new BadRequestAlertException("The course belonging to this exercise does not exist", getEntityName(), "courseNotFound");
         }
     }
