6ea1b1d4e85396736d14ded697d92b256b2b43b4
Andi Turdiu <andi.turdiu@tum.de>
2020-06-21 12:12:52 +0200

    fix failing test by improving Exercise validation robustness

--- src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/AssessmentResource.java
@@ -59,7 +59,11 @@ public abstract class AssessmentResource {
 
     void validateExercise(Exercise exercise) throws BadRequestAlertException {
         Course course = exercise.getCourse();
-        if (course == null && exercise.getExerciseGroup().getExam().getCourse() == null) {
+        try {
+            if (course == null && exercise.getExerciseGroup().getExam().getCourse() == null) {
+                throw new BadRequestAlertException("The course belonging to this exercise does not exist", getEntityName(), "courseNotFound");
+            }
+        } catch (NullPointerException nullPointerException) {
             throw new BadRequestAlertException("The course belonging to this exercise does not exist", getEntityName(), "courseNotFound");
         }
     }
