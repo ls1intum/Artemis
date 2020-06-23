8a5c4909b1d39b2e95728096203a4bc9e3eea154
Stephan Krusche <s.krusche@tum.de>
2020-06-23 19:31:51 +0200

    update build scripts

--- src/main/resources/templates/bamboo/haskell/regularRuns/1_build_and_test_the_code.sh
+++ src/main/resources/templates/bamboo/haskell/regularRuns/1_build_and_test_the_code.sh
@@ -1,5 +1,4 @@
-stack config set system-ghc --global true
 # Build the libraries, delete the solution and tests (so that students cannot access it), and run the test executable.
-# Do not forget to set the right compilation flags (Prod and Solution).
+# Do not forget to set the right compilation flags (Prod).
 # Finally, return 0: as a convention, a failed haskell tasty test suite returns 1, but this stops the JUnit Parser from running.
-stack build --allow-different-user --flag test:Prod --flag test:Solution && rm -rf solution && rm -rf test && (stack exec test --allow-different-user || exit 0)
+stack build --allow-different-user --flag test:Prod && rm -rf solution && rm -rf test && (stack exec test --allow-different-user || exit 0)
