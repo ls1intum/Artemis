3cf36c425d9003bde0b04c03bf26d5db7cc4aa12
Sebastian Jagla <sebastianjagla@gmx.de>
2020-06-27 18:43:09 +0200

    page draft

--- src/main/webapp/app/exam/exam-scores/exam-scores.component.ts
+++ src/main/webapp/app/exam/exam-scores/exam-scores.component.ts
@@ -1,12 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute } from '@angular/router';
+import { ExamManagementService } from 'app/exam/manage/exam-management.service';
+import { Exam } from 'app/entities/exam.model';
+import { Result } from 'app/entities/result.model';
+import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+
+/**
+ * Filter properties for a result
+ */
+enum FilterProp {
+    ALL = 'all',
+    SUCCESSFUL = 'successful',
+    UNSUCCESSFUL = 'unsuccessful',
+}
 
 @Component({
     selector: 'jhi-exam-scores',
     templateUrl: './exam-scores.component.html',
     styles: [],
 })
-export class ExamScoresComponent implements OnInit {
-    constructor() {}
+export class ExamScoresComponent implements OnInit, OnDestroy {
+    // make constants available to html for comparison
+    readonly FilterProp = FilterProp;
 
-    ngOnInit(): void {}
+    isLoading: boolean;
+    paramSub: Subscription;
+    exam: Exam;
+    results: Result[];
+    filteredResultsSize: number;
+
+    resultCriteria: {
+        filterProp: FilterProp;
+    };
+
+    constructor(private route: ActivatedRoute, private examService: ExamManagementService) {
+        this.resultCriteria = {
+            filterProp: FilterProp.ALL,
+        };
+        this.results = [];
+        this.filteredResultsSize = 0;
+    }
+
+    ngOnInit(): void {
+        this.paramSub = this.route.params.subscribe((params) => {
+            this.examService.find(params['courseId'], params['examId']).subscribe((examResponse) => {
+                this.exam = examResponse.body!;
+            });
+        });
+    }
+
+    exportNames() {}
+
+    exportResults() {}
+
+    /**
+     * Updates the criteria by which to filter results
+     * @param newValue New filter prop value
+     */
+    updateResultFilter(newValue: FilterProp) {
+        this.isLoading = true;
+        setTimeout(() => {
+            this.resultCriteria.filterProp = newValue;
+            this.isLoading = false;
+        });
+    }
+
+    /**
+     * Predicate used to filter results by the current filter prop setting
+     * @param result Result for which to evaluate the predicate
+     */
+    filterResultByProp = (result: Result) => {
+        switch (this.resultCriteria.filterProp) {
+            case FilterProp.SUCCESSFUL:
+                return result.successful;
+            case FilterProp.UNSUCCESSFUL:
+                return !result.successful;
+            default:
+                return true;
+        }
+    };
+
+    /**
+     * Update the number of filtered results
+     * @param filteredResultsSize Total number of results after filters have been applied
+     */
+    handleResultsSizeChange = (filteredResultsSize: number) => {
+        this.filteredResultsSize = filteredResultsSize;
+    };
+
+    /**
+     * Formats the results in the autocomplete overlay.
+     * @param result
+     */
+    searchResultFormatter = (result: Result) => {
+        const participation = result.participation as StudentParticipation;
+        if (participation.student) {
+            const { login, name } = participation.student;
+            return `${login} (${name})`;
+        }
+    };
+
+    /**
+     * Converts a result object to a string that can be searched for. This is
+     * used by the autocomplete select inside the data table.
+     * @param result
+     */
+    searchTextFromResult = (result: Result): string => {
+        return (result.participation as StudentParticipation).participantIdentifier || '';
+    };
+
+    /**
+     * Triggers a re-fetch of the results from the server
+     */
+    refresh() {
+        this.isLoading = true;
+        this.results = [];
+        // this.getResults().subscribe(() => (this.isLoading = false));
+    }
+
+    /**
+     * Unsubscribes from all subscriptions
+     */
+    ngOnDestroy() {
+        this.paramSub.unsubscribe();
+    }
 }
--- src/main/webapp/app/exam/exam-scores/exam-scores.module.ts
+++ src/main/webapp/app/exam/exam-scores/exam-scores.module.ts
@@ -3,9 +3,12 @@ import { ExamScoresComponent } from './exam-scores.component';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { MomentModule } from 'ngx-moment';
 import { ArtemisExamScoresRoutingModule } from 'app/exam/exam-scores/exam-scores-routing.module';
+import { ArtemisDataTableModule } from 'app/shared/data-table/data-table.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ArtemisResultModule } from 'app/exercises/shared/result/result.module';
 
 @NgModule({
     declarations: [ExamScoresComponent],
-    imports: [ArtemisSharedModule, MomentModule, ArtemisExamScoresRoutingModule],
+    imports: [ArtemisSharedModule, MomentModule, ArtemisExamScoresRoutingModule, ArtemisDataTableModule, NgxDatatableModule, ArtemisResultModule],
 })
 export class ArtemisExamScoresModule {}
