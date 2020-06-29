3e53de4f2e4bd1c5ca6cf3e37ef82318dbc6bcdc
Stephan Krusche <s.krusche@tum.de>
2020-06-29 17:04:50 +0200

    add TODOs

--- src/main/webapp/app/shared/server-date.service.ts
+++ src/main/webapp/app/shared/server-date.service.ts
@@ -15,6 +15,10 @@ export class ArtemisServerDateService {
         const serverDate = moment(date);
         const clientDate = moment(new Date());
         this.offset = serverDate.diff(clientDate, 'ms');
+        // TODO: to improve the offset calculation, we should take the diff between request_date and response_date and divide it by 2
+        // TODO: to eliminate noise, we should store the 5 last offsets, remove the smallest and highest value and take the average of the remaining 3
+        // TODO: it might make sense to invoke one very fast explicit REST/websocket calls (e.g. "/time") without access control and database access.
+        // This would be faster and more reliable than using an interceptor with arbitrary REST calls
     }
 
     /**
