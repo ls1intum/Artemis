3c457b121a9600a3b7252fb93546142055877ea9
Christian Femers <christian.femers@tum.de>
2020-06-29 16:18:59 +0200

    Add max student exam working time database query to repository

--- src/main/java/de/tum/in/www1/artemis/repository/StudentExamRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/StudentExamRepository.java
@@ -32,4 +32,7 @@ public interface StudentExamRepository extends JpaRepository<StudentExam, Long> 
 
     @Query("select distinct se from StudentExam se left join se.exercises e where e.id = :#{#exerciseId} and se.user.id = :#{#userId}")
     Optional<StudentExam> findByExerciseIdAndUserId(@Param("exerciseId") Long exerciseId, @Param("userId") Long userId);
+
+    @Query("select max(se.workingTime) from StudentExam se where se.exam.id = :#{#examId}")
+    Optional<Integer> findMaxWorkingTimeByExamId(@Param("examId") Long examId);
 }
