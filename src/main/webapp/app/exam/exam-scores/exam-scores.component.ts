c9fdb9d6827491acfd7cc181aa703fc826d23d81
Sebastian Jagla <sebastianjagla@gmx.de>
2020-06-29 22:06:47 +0200

    page prepare

--- src/main/webapp/app/exam/exam-scores/exam-scores.component.ts
+++ src/main/webapp/app/exam/exam-scores/exam-scores.component.ts
@@ -1,25 +1,48 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Exam } from 'app/entities/exam.model';
+import { ExamManagementService } from 'app/exam/manage/exam-management.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { SortService } from 'app/shared/service/sort.service';
 
 @Component({
     selector: 'jhi-exam-scores',
     templateUrl: './exam-scores.component.html',
     styles: [],
 })
-export class ExamScoresComponent implements OnInit {
+export class ExamScoresComponent implements OnInit, OnDestroy {
     public examScoreDTO: any;
     public exerciseGroups: any[];
     public rows: any[];
 
+    paramSub: Subscription;
+    exam: Exam;
+    predicate: string;
+    reverse: boolean;
+
+    constructor(private route: ActivatedRoute, private examService: ExamManagementService, private sortService: SortService) {
+        this.predicate = 'id';
+        this.reverse = false;
+    }
+
     ngOnInit() {
+        this.paramSub = this.route.params.subscribe((params) => {
+            this.examService.find(params['courseId'], params['examId']).subscribe((examResponse) => {
+                this.exam = examResponse.body!;
+            });
+        });
+
         this.examScoreDTO = {
             exerciseGroups: [
                 {
                     id: 1,
                     title: 'Text Exercises',
+                    maxPoints: 15,
                 },
                 {
                     id: 2,
                     title: 'Modelling Exercises',
+                    maxPoints: 10,
                 },
             ],
             results: [
@@ -35,5 +58,29 @@ export class ExamScoresComponent implements OnInit {
         };
         this.rows = this.examScoreDTO.results;
         this.exerciseGroups = this.examScoreDTO.exerciseGroups;
+    }
+
+    calculateTotalAverage() {
+        let sum = 0;
+        for (let i = 0; i < this.rows.length; i++) {
+            sum += this.rows[i].overallPoints;
+        }
+
+        return sum / this.rows.length;
+    }
+
+    sortRows() {
+        this.sortService.sortByProperty(this.examScoreDTO.results, this.predicate, this.reverse);
+    }
+
+    exportResults() {
+        // TODO
+    }
+
+    /**
+     * Unsubscribes from all subscriptions
+     */
+    ngOnDestroy() {
+        this.paramSub.unsubscribe();
     }
 }
