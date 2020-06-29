66f70021f850c9601fd3b653d2ebda23f35b7440
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-06-29 19:27:25 +0200

    Update TextSimilarityClusteringServiceTest.java

--- src/test/java/de/tum/in/www1/artemis/service/connectors/TextSimilarityClusteringServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/connectors/TextSimilarityClusteringServiceTest.java
@@ -22,6 +22,7 @@ import de.tum.in.www1.artemis.exception.NetworkingError;
 public class TextSimilarityClusteringServiceTest {
 
     private static String EMBEDDING_ENDPOINT = "http://localhost:8001/embed";
+
     private static String CLUSTERING_ENDPOINT = "http://localhost:8002/cluster";
 
     @Test
