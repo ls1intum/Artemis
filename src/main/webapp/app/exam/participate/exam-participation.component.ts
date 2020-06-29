187198922677accfbbee7e1f4b23f06a4456f05a
Tobias Priesching <tobias.priesching@gmail.com>
2020-06-29 23:10:58 +0200

    remove circular dependency when serializing to JSON draw navigation bar without participation

--- src/main/webapp/app/exam/participate/exam-participation.component.ts
+++ src/main/webapp/app/exam/participate/exam-participation.component.ts
@@ -209,6 +209,8 @@ export class ExamParticipationComponent implements OnInit, OnDestroy {
             tap((createdParticipation: StudentParticipation) => {
                 // if the same participations is not yet present in the exercise -> add it
                 if (exercise.studentParticipations.findIndex((existingParticipation) => existingParticipation.id === createdParticipation.id) < 0) {
+                    // remove because of circular dependency when converting to JSON
+                    delete createdParticipation.exercise;
                     exercise.studentParticipations.push(createdParticipation);
                     if (createdParticipation.submissions && createdParticipation.submissions.length > 0) {
                         createdParticipation.submissions[0].isSynced = true;
