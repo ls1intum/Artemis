6c4272e926111760cc70d9e9718e0faabf95243f
Sascha Beele <sascha@beele.io>
2020-06-30 22:23:54 +0200

    Add max points to online editor for programming exercises

--- src/main/webapp/app/exercises/programming/shared/code-editor/layout/code-editor-grid.component.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/layout/code-editor-grid.component.ts
@@ -21,6 +21,8 @@ export class CodeEditorGridComponent implements AfterViewInit {
 
     @Input()
     exerciseTitle: string;
+    @Input()
+    exerciseMaxScore: number;
 
     interactResizableMain: Interactable;
     resizableMinHeightMain = 480;
