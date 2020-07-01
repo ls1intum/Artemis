d3997c50172f98c8d83fd48e9cdf3b40095c483a
Sascha Beele <sascha@beele.io>
2020-07-01 10:15:13 +0200

    Change ArtemisDurationFromSecondsPipe output format to mm:ss

--- src/main/webapp/app/shared/pipes/artemis-duration-from-seconds.pipe.ts
+++ src/main/webapp/app/shared/pipes/artemis-duration-from-seconds.pipe.ts
@@ -7,14 +7,9 @@ export class ArtemisDurationFromSecondsPipe implements PipeTransform {
      * @param seconds {number}
      */
     transform(seconds: number): string {
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds - hours * 3600) / 60);
-        seconds = seconds - hours * 3600 - minutes * 60;
+        const minutes = Math.floor(seconds / 60);
+        seconds = seconds - minutes * 60;
 
-        let hoursOut = hours.toString();
-        if (hours < 10) {
-            hoursOut = '0' + hours.toString();
-        }
         let minutesOut = minutes.toString();
         if (minutes < 10) {
             minutesOut = '0' + minutes.toString();
@@ -24,6 +19,6 @@ export class ArtemisDurationFromSecondsPipe implements PipeTransform {
             secondsOut = '0' + seconds.toString();
         }
 
-        return hoursOut + ':' + minutesOut + ':' + secondsOut;
+        return minutesOut + ':' + secondsOut;
     }
 }
