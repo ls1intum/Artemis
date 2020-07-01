20d5b9d11189b4cd7b0e73e885d3e793e2f8875a
Sascha Beele <sascha@beele.io>
2020-07-01 10:10:07 +0200

    First version of edit form

--- src/main/webapp/app/exam/manage/student-exams/student-exam-detail.component.ts
+++ src/main/webapp/app/exam/manage/student-exams/student-exam-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { StudentExamService } from 'app/exam/manage/student-exams/student-exam.service';
@@ -6,24 +7,41 @@ import { Course } from 'app/entities/course.model';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
 import { User } from 'app/core/user/user.model';
 import { Exercise, ExerciseType } from 'app/entities/exercise.model';
+import { ArtemisDurationFromSecondsPipe } from 'app/shared/pipes/artemis-duration-from-seconds.pipe';
 
 @Component({
     selector: 'jhi-student-exam-detail',
     templateUrl: './student-exam-detail.component.html',
+    providers: [ArtemisDurationFromSecondsPipe],
 })
 export class StudentExamDetailComponent implements OnInit {
     courseId: number;
     studentExam: StudentExam;
     course: Course;
     student: User;
+    workingTimeHours = 0;
+    workingTimeMinutes = 0;
+    workingTimeSeconds = 0;
+    isSavingWorkingTime = false;
+    workingTimeForm: FormGroup;
 
-    constructor(private route: ActivatedRoute, private studentExamService: StudentExamService, private courseService: CourseManagementService) {}
+    constructor(
+        private route: ActivatedRoute,
+        private studentExamService: StudentExamService,
+        private courseService: CourseManagementService,
+        private artemisDurationFromSecondsPipe: ArtemisDurationFromSecondsPipe,
+    ) {}
 
     /**
      * Initialize the courseId and studentExam
      */
     ngOnInit(): void {
         this.loadAll();
+        this.workingTimeForm = new FormGroup({
+            hours: new FormControl(this.workingTimeHours, [Validators.min(0), Validators.required]),
+            minutes: new FormControl(this.workingTimeMinutes, [Validators.min(0), Validators.max(59), Validators.required]),
+            seconds: new FormControl(this.workingTimeSeconds, [Validators.min(0), Validators.max(59), Validators.required]),
+        });
     }
 
     /**
@@ -31,7 +49,10 @@ export class StudentExamDetailComponent implements OnInit {
      */
     loadAll() {
         this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
-        this.route.data.subscribe(({ studentExam }) => (this.studentExam = studentExam));
+        this.route.data.subscribe(({ studentExam }) => {
+            this.studentExam = studentExam;
+            this.setWorkingTime();
+        });
 
         this.courseService.find(this.courseId).subscribe((courseResponse) => {
             this.course = courseResponse.body!;
@@ -63,5 +84,22 @@ export class StudentExamDetailComponent implements OnInit {
             default:
                 return 'font';
         }
+    }
+
+    /**
+     * Save the defined working time
+     */
+    saveWorkingTime() {
+        console.log(this.workingTimeSeconds);
+        const seconds = this.workingTimeHours * 3600 + this.workingTimeMinutes * 60 + this.workingTimeSeconds;
+        // TODO
+    }
+
+    private setWorkingTime() {
+        const workingTime = this.artemisDurationFromSecondsPipe.transform(this.studentExam.workingTime);
+        const workingTimeParts = workingTime.split('|');
+        this.workingTimeHours = parseInt(workingTimeParts[0] ? workingTimeParts[0] : '0', 10);
+        this.workingTimeSeconds = parseInt(workingTimeParts[1] ? workingTimeParts[1] : '0', 10);
+        this.workingTimeMinutes = parseInt(workingTimeParts[2] ? workingTimeParts[2] : '0', 10);
     }
 }
--- src/main/webapp/app/shared/pipes/artemis-duration-from-seconds.pipe.ts
+++ src/main/webapp/app/shared/pipes/artemis-duration-from-seconds.pipe.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
 
 @Pipe({ name: 'artemisDurationFromSeconds' })
 export class ArtemisDurationFromSecondsPipe implements PipeTransform {
@@ -8,6 +7,23 @@ export class ArtemisDurationFromSecondsPipe implements PipeTransform {
      * @param seconds {number}
      */
     transform(seconds: number): string {
-        return moment.utc(seconds * 1000).format('HH:mm:ss');
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds - hours * 3600) / 60);
+        seconds = seconds - hours * 3600 - minutes * 60;
+
+        let hoursOut = hours.toString();
+        if (hours < 10) {
+            hoursOut = '0' + hours.toString();
+        }
+        let minutesOut = minutes.toString();
+        if (minutes < 10) {
+            minutesOut = '0' + minutes.toString();
+        }
+        let secondsOut = seconds.toString();
+        if (seconds < 10) {
+            secondsOut = '0' + seconds.toString();
+        }
+
+        return hoursOut + ':' + minutesOut + ':' + secondsOut;
     }
 }
