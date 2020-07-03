63011721eeb7c5ca8b9ba7d46fe42bc9ab2c1159
Filip Gregurevic <filipgregurevic@gmail.com>
2020-07-03 10:50:19 +0200

    randomize order in quizzes

--- src/main/webapp/app/exam/participate/exercises/quiz/quiz-exam-submission.component.ts
+++ src/main/webapp/app/exam/participate/exercises/quiz/quiz-exam-submission.component.ts
@@ -15,6 +15,8 @@ import { ShortAnswerSubmittedAnswer } from 'app/entities/quiz/short-answer-submi
 import { QuizSubmission } from 'app/entities/quiz/quiz-submission.model';
 import { ExamSubmissionComponent } from 'app/exam/participate/exercises/exam-submission.component';
 import { cloneDeep } from 'lodash';
+import { MultipleChoiceQuestion } from 'app/entities/quiz/multiple-choice-question.model';
+import { DragAndDropQuestion } from 'app/entities/quiz/drag-and-drop-question.model';
 
 @Component({
     selector: 'jhi-quiz-submission-exam',
@@ -65,6 +67,8 @@ export class QuizExamSubmissionComponent extends ExamSubmissionComponent impleme
      * Initialize the selections / mappings for each question with an empty array
      */
     initQuiz() {
+        // randomize order
+        this.randomizeOrder(this.exercise);
         // prepare selection arrays for each question
         this.selectedAnswerOptions = new Map<number, AnswerOption[]>();
         this.dragAndDropMappings = new Map<number, DragAndDropMapping[]>();
@@ -235,5 +239,48 @@ export class QuizExamSubmissionComponent extends ExamSubmissionComponent impleme
             this.studentSubmission.submittedAnswers.push(shortAnswerSubmittedAnswer);
         }, this);
         this.studentSubmission.isSynced = false;
+    }
+
+    /**
+     * Randomize the order of the questions
+     * (and answerOptions or dragItems within each question)
+     * if randomizeOrder is true
+     *
+     * @param quizExercise {object} the quizExercise to randomize elements in
+     */
+    randomizeOrder(quizExercise: QuizExercise) {
+        if (quizExercise.quizQuestions) {
+            // shuffle questions
+            if (quizExercise.randomizeQuestionOrder) {
+                this.shuffle(quizExercise.quizQuestions);
+            }
+
+            // shuffle answerOptions / dragItems within questions
+            quizExercise.quizQuestions.forEach((question) => {
+                if (question.randomizeOrder) {
+                    if (question.type === QuizQuestionType.MULTIPLE_CHOICE) {
+                        this.shuffle((question as MultipleChoiceQuestion).answerOptions!);
+                    } else if (question.type === QuizQuestionType.DRAG_AND_DROP) {
+                        this.shuffle((question as DragAndDropQuestion).dragItems);
+                    } else if (question.type === QuizQuestionType.SHORT_ANSWER) {
+                    } else {
+                        console.log('Unknown question type: ' + question);
+                    }
+                }
+            }, this);
+        }
+    }
+
+    /**
+     * Shuffles array in place.
+     * @param {Array} items An array containing the items.
+     */
+    shuffle<T>(items: T[]) {
+        for (let i = items.length - 1; i > 0; i--) {
+            const pickedIndex = Math.floor(Math.random() * (i + 1));
+            const picked = items[pickedIndex];
+            items[pickedIndex] = items[i];
+            items[i] = picked;
+        }
     }
 }
