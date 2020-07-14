2646210f483392515aa953e279b7fa18fc570d0d
Sascha Beele <sascha@beele.io>
2020-07-14 19:44:06 +0200

    Hide participation stage

--- src/main/webapp/app/exam/participate/exam-participation.component.ts
+++ src/main/webapp/app/exam/participate/exam-participation.component.ts
@@ -282,7 +282,7 @@ export class ExamParticipationComponent implements OnInit, OnDestroy, ComponentC
      * check if exam is over
      */
     isOver(): boolean {
-        if (this.studentExam && this.studentExam.ended) {
+        if ((this.studentExam && this.studentExam.ended) || this.aborted) {
             // if this was calculated to true by the server, we can be sure the student exam has finished
             return true;
         }
