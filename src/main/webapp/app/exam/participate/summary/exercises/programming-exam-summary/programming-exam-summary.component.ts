c20e0652937f4a467cf8e025f45944776062348f
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-07-15 15:11:15 +0200

    Enable commit hash in summary

--- src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
+++ src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseStudentParticipation } from 'app/entities/participation/programming-exercise-student-participation.model';
+import { ProgrammingSubmission } from 'app/entities/programming-submission.model';
 
 @Component({
     selector: 'jhi-programming-exam-summary',
     templateUrl: './programming-exam-summary.component.html',
     styles: [],
 })
-export class ProgrammingExamSummaryComponent {
+export class ProgrammingExamSummaryComponent implements OnInit {
     @Input()
     exercise: ProgrammingExercise;
 
     @Input()
     participation: ProgrammingExerciseStudentParticipation;
 
+    submission: ProgrammingSubmission;
+
     constructor() {}
+
+    ngOnInit() {
+        this.submission = this.participation.submissions[0] as ProgrammingSubmission;
+    }
 }
