e7e974bad1fab3781fbead0707c19a0cc1ecbf72
Tobias Priesching <tobias.priesching@gmail.com>
2020-07-15 20:36:55 +0200

    add input to include angular check if submission changes

--- src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
+++ src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseStudentParticipation } from 'app/entities/participation/programming-exercise-student-participation.model';
 import { ProgrammingSubmission } from 'app/entities/programming-submission.model';
@@ -8,18 +8,15 @@ import { ProgrammingSubmission } from 'app/entities/programming-submission.model
     templateUrl: './programming-exam-summary.component.html',
     styles: [],
 })
-export class ProgrammingExamSummaryComponent implements OnInit {
+export class ProgrammingExamSummaryComponent {
     @Input()
     exercise: ProgrammingExercise;
 
     @Input()
     participation: ProgrammingExerciseStudentParticipation;
 
+    @Input()
     submission: ProgrammingSubmission;
 
     constructor() {}
-
-    ngOnInit() {
-        this.submission = this.participation.submissions[0] as ProgrammingSubmission;
-    }
 }
