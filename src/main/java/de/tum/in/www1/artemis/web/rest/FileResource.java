b3e8e3744654b85d2fab89858713627be34a60b8
Filip Gregurevic <filipgregurevic@gmail.com>
2020-07-15 15:42:07 +0200

    add endpoints

--- src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
@@ -155,10 +155,91 @@ public class FileResource {
      * @return The requested file, or 404 if the file doesn't exist
      */
     @GetMapping("/files/temp/{filename:.+}")
-    @PreAuthorize("permitAll()")
+    @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR', 'TA')")
     public ResponseEntity<byte[]> getTempFile(@PathVariable String filename) {
         log.debug("REST request to get file : {}", filename);
         return responseEntityForFilePath(FilePathService.getTempFilepath() + filename);
+    }
+
+    /**
+     * POST /markdownFileUpload : Upload a new file.
+     *
+     * @param file The file to save
+     * @param keepFileName specifies if original file name should be kept
+     * @return The path of the file
+     * @throws URISyntaxException if response path can't be converted into URI
+     */
+    @PostMapping("/markdownFileUpload")
+    @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR', 'TA', 'USER')")
+    public ResponseEntity<String> saveMarkdownFile(@RequestParam(value = "file") MultipartFile file, @RequestParam("keepFileName") Boolean keepFileName) throws URISyntaxException {
+        log.debug("REST request to upload file : {}", file.getOriginalFilename());
+
+        // NOTE: Maximum file size is set in resources/config/application.yml
+        // Currently set to 10 MB
+
+        // check for file type
+        String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+        if (fileExtension == null || this.allowedFileExtensions.stream().noneMatch(fileExtension::equalsIgnoreCase)) {
+            return ResponseEntity.badRequest().body("Unsupported file type! Allowed file types: " + String.join(", ", this.allowedFileExtensions));
+        }
+
+        try {
+            // create folder if necessary
+            File folder = new File(FilePathService.getMarkdownFilepath());
+            if (!folder.exists()) {
+                if (!folder.mkdirs()) {
+                    log.error("Could not create directory: {}", FilePathService.getMarkdownFilepath());
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                }
+            }
+
+            // create file (generate new filename, if file already exists)
+            boolean fileCreated;
+            File newFile;
+            String filename;
+            do {
+                if (keepFileName) {
+                    filename = file.getOriginalFilename().replaceAll("\\s", "");
+                }
+                else {
+                    filename = "Markdown_" + ZonedDateTime.now().toString().substring(0, 23).replaceAll(":|\\.", "-") + "_" + UUID.randomUUID().toString().substring(0, 8) + "."
+                        + fileExtension;
+                }
+                String path = FilePathService.getMarkdownFilepath() + filename;
+
+                newFile = new File(path);
+                if (keepFileName && newFile.exists()) {
+                    newFile.delete();
+                }
+                fileCreated = newFile.createNewFile();
+            }
+            while (!fileCreated);
+            String responsePath = "/api/files/markdown/" + filename;
+
+            // copy contents of uploaded file into newly created file
+            Files.copy(file.getInputStream(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            // return path for getting the file
+            String responseBody = "{\"path\":\"" + responsePath + "\"}";
+            return ResponseEntity.created(new URI(responsePath)).body(responseBody);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    /**
+     * GET /files/markdown/:filename : Get the markdown file with the given filename
+     *
+     * @param filename The filename of the file to get
+     * @return The requested file, or 404 if the file doesn't exist
+     */
+    @GetMapping("/files/markdown/{filename:.+}")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<byte[]> getMarkdownFile(@PathVariable String filename) {
+        log.debug("REST request to get file : {}", filename);
+        return responseEntityForFilePath(FilePathService.getMarkdownFilepath() + filename);
     }
 
     /**
