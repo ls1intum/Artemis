99f20c0da27c281f69071fa5ca68647fe8a97ad0
Tobias Priesching <tobias.priesching@gmail.com>
2020-07-16 23:01:34 +0200

    display editor as when the student models the exercise -> should solve the errors

--- src/main/webapp/app/exam/participate/summary/exercises/modeling-exam-summary/modeling-exam-summary.component.ts
+++ src/main/webapp/app/exam/participate/summary/exercises/modeling-exam-summary/modeling-exam-summary.component.ts
@@ -16,23 +16,12 @@ export class ModelingExamSummaryComponent implements OnInit {
     submission: ModelingSubmission;
 
     umlModel: UMLModel;
-    maxWidth: number;
-    maxHeight: number;
 
     constructor() {}
 
     ngOnInit() {
         if (this.submission && this.submission.model) {
             this.umlModel = JSON.parse(this.submission.model);
-            // TODO: fix this when we have a more reliable solution (currently diagram is not centered)
-            // margin on all sides
-            const margin = 100;
-            this.maxWidth = this.umlModel.size.width += 2 * margin;
-            this.maxHeight = this.umlModel.size.height += 2 * margin;
-            this.umlModel.elements.forEach((element) => {
-                element.bounds.x += margin / 2;
-                element.bounds.y += margin / 2;
-            });
         }
     }
 }
