5a5bf4f70345042dcced300cc478ae83dd4f70b5
Andi Turdiu <andi.turdiu@tum.de>
2020-07-20 19:05:30 +0200

    remove unused import

--- src/main/webapp/app/exam/participate/exam-participation.service.ts
+++ src/main/webapp/app/exam/participate/exam-participation.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { SERVER_API_URL } from 'app/app.constants';
 import { Observable, Subject, throwError } from 'rxjs';
 import { StudentExam } from 'app/entities/student-exam.model';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { QuizSubmission } from 'app/entities/quiz/quiz-submission.model';
 import { catchError, map } from 'rxjs/operators';
