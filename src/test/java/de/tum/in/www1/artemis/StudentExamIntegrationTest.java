42080a938660c387585f3b79923d4d125733226a
Andi Turdiu <andi.turdiu@tum.de>
2020-07-25 17:10:09 +0200

    different user submit test coverage

--- src/test/java/de/tum/in/www1/artemis/StudentExamIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/StudentExamIntegrationTest.java
@@ -443,6 +443,17 @@ public class StudentExamIntegrationTest extends AbstractSpringIntegrationBambooB
 
     @Test
     @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
+    public void testSubmitExamOtherUser_forbidden() throws Exception {
+        List<StudentExam> studentExams = prepareStudentExamsForConduction();
+        database.changeUser(studentExams.get(0).getUser().getLogin());
+        var studentExamResponse = request.get("/api/courses/" + course2.getId() + "/exams/" + exam2.getId() + "/studentExams/conduction", HttpStatus.OK, StudentExam.class);
+        studentExamResponse.setExercises(null);
+        database.changeUser("student2");
+        request.post("/api/courses/" + course2.getId() + "/exams/" + exam2.getId() + "/studentExams/submit", studentExamResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
     public void testSubmitExamWithNoExercise_badRequest() throws Exception {
         List<StudentExam> studentExams = prepareStudentExamsForConduction();
         database.changeUser(studentExams.get(0).getUser().getLogin());
