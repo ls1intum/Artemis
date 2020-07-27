0bfe42066f13058007058dd01be712ad0fb141c9
Birtan <birtan.gultekin@hotmail.com>
2020-07-27 21:25:37 +0200

    text assessment conflict enumeration

---
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/TextAssessmentConflictType.java
@@ -0,0 +1,8 @@
+package de.tum.in.www1.artemis.domain.enumeration;
+
+/**
+ * The TextAssessmentConflictType enumeration.
+ */
+public enum TextAssessmentConflictType {
+    INCONSISTENT_COMMENT, INCONSISTENT_SCORE, INCONSISTENT_FEEDBACK
+}
