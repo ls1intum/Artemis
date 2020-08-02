aee894c26a854ba18831347d977b04e38cdf51c9
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-08-02 15:28:34 +0200

    Fix edge case in build plan command creation method

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/StaticCodeAnalysisTool.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/StaticCodeAnalysisTool.java
@@ -71,12 +71,14 @@ public enum StaticCodeAnalysisTool {
      */
     public static String createBuildPlanCommandForProgrammingLanguage(ProgrammingLanguage language) {
         StringBuilder commandBuilder = new StringBuilder();
+        String separator = "";
         for (var tool : StaticCodeAnalysisTool.values()) {
             if (tool.language == language) {
+                commandBuilder.append(separator);
                 commandBuilder.append(tool.command);
-                commandBuilder.append(" ");
             }
+            separator = " ";
         }
-        return commandBuilder.deleteCharAt(commandBuilder.length() - 1).toString();
+        return commandBuilder.toString();
     }
 }
