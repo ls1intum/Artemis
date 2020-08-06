37a74dea92e6ea4c8a77d493692eeb7a63a1f8b2
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-08-06 12:07:19 +0200

    Delete empty scss files

--- src/main/webapp/app/overview/course-exams/course-exams.component.ts
+++ src/main/webapp/app/overview/course-exams/course-exams.component.ts
@@ -11,7 +11,7 @@ import { ArtemisServerDateService } from 'app/shared/server-date.service';
 @Component({
     selector: 'jhi-course-exams',
     templateUrl: './course-exams.component.html',
-    styleUrls: ['./course-exams.scss'],
+    styles: [],
 })
 export class CourseExamsComponent implements OnInit, OnDestroy {
     courseId: number;
