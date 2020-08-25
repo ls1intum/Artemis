ad387a7cdec5ac56e890b18b4ea6ce879a787982
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-08-25 20:24:36 +0200

    Tests for result details

--- src/test/javascript/spec/component/shared/result-detail.component.spec.ts
+++ src/test/javascript/spec/component/shared/result-detail.component.spec.ts
@@ -14,6 +14,8 @@ import { ResultDetailComponent } from 'app/exercises/shared/result/result-detail
 import { ExerciseType } from 'app/entities/exercise.model';
 import { Result } from 'app/entities/result.model';
 import { BuildLogService } from 'app/exercises/programming/shared/service/build-log.service';
+import { ProgrammingSubmission } from 'app/entities/programming-submission.model';
+import { ModelingSubmission } from 'app/entities/modeling-submission.model';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -43,6 +45,12 @@ describe('ResultDetailComponent', () => {
         return tcFeedback;
     };
 
+    const generateProgrammingSubmission = (buildFailed: boolean) => {
+        const programmingSubmission = new ProgrammingSubmission();
+        programmingSubmission.buildFailed = buildFailed;
+        return programmingSubmission;
+    };
+
     beforeEach(async () => {
         return TestBed.configureTestingModule({
             imports: [ArtemisTestModule, ArtemisSharedModule, ArtemisResultModule],
@@ -53,6 +61,8 @@ describe('ResultDetailComponent', () => {
                 comp = fixture.componentInstance;
                 debugElement = fixture.debugElement;
 
+                comp.result = { id: 89, participation: { id: 55 } } as Result;
+
                 buildLogService = debugElement.injector.get(BuildLogService);
                 resultService = debugElement.injector.get(ResultService);
 
@@ -61,32 +71,27 @@ describe('ResultDetailComponent', () => {
             });
     });
 
-    it('should not try to retrieve the feedbacks from the server if provided result has results', () => {
+    it('should not try to retrieve the feedbacks from the server if provided result has feedbacks', () => {
         const feedbacks: Feedback[] = [generateSCAFeedback(), generateTestCaseFeedback()];
         comp.exerciseType = ExerciseType.PROGRAMMING;
-        comp.result = { id: 89, participation: { id: 55 }, feedbacks } as Result;
+        comp.result.feedbacks = feedbacks;
 
         comp.ngOnInit();
 
         expect(getFeedbackDetailsForResultStub).to.not.have.been.called;
-        expect(buildlogsStub).to.not.have.been.called;
         expect([...comp.feedbackList, ...comp.staticCodeAnalysisFeedbackList]).to.have.same.deep.members(feedbacks);
-        expect(comp.buildLogs).to.be.undefined;
         expect(comp.isLoading).to.be.false;
     });
 
-    it('should try to retrieve the feedbacks from the server if provided result does not have results', () => {
+    it('should try to retrieve the feedbacks from the server if provided result does not have feedbacks', () => {
         const feedbacks: Feedback[] = [generateSCAFeedback(), generateTestCaseFeedback()];
         comp.exerciseType = ExerciseType.PROGRAMMING;
-        comp.result = { id: 89, participation: { id: 55 } } as Result;
         getFeedbackDetailsForResultStub.returns(of({ body: feedbacks as Feedback[] } as HttpResponse<Feedback[]>));
 
         comp.ngOnInit();
 
         expect(getFeedbackDetailsForResultStub).to.have.been.calledOnceWithExactly(comp.result.id);
-        expect(buildlogsStub).to.not.have.been.called;
         expect([...comp.feedbackList, ...comp.staticCodeAnalysisFeedbackList]).to.have.same.deep.members(feedbacks);
-        expect(comp.buildLogs).to.be.undefined;
         expect(comp.isLoading).to.be.false;
     });
 
@@ -95,38 +100,54 @@ describe('ResultDetailComponent', () => {
         const testCaseFeedback = generateTestCaseFeedback();
         const feedbacks: Feedback[] = [scaFeedback, testCaseFeedback];
         comp.exerciseType = ExerciseType.PROGRAMMING;
-        comp.result = { id: 89, participation: { id: 55 }, feedbacks } as Result;
+        comp.result.feedbacks = feedbacks;
 
         comp.ngOnInit();
 
         expect(getFeedbackDetailsForResultStub).to.not.have.been.called;
-        expect(buildlogsStub).to.not.have.been.called;
         expect(comp.staticCodeAnalysisFeedbackList).to.have.same.deep.members([scaFeedback]);
         expect(comp.feedbackList).to.have.same.deep.members([testCaseFeedback]);
     });
 
-    it('should try to retrieve build logs if the exercise type is PROGRAMMING and no feedbacks are provided.', () => {
+    it('should try to retrieve build logs if the exercise type is PROGRAMMING and no submission was provided.', () => {
         comp.exerciseType = ExerciseType.PROGRAMMING;
-        comp.result = { id: 89, participation: { id: 55 } } as Result;
 
         comp.ngOnInit();
 
-        expect(getFeedbackDetailsForResultStub).to.have.been.calledOnceWithExactly(comp.result.id);
         expect(buildlogsStub).to.have.been.calledOnceWithExactly(comp.result.participation!.id);
-        expect(comp.feedbackList).to.be.undefined;
         expect(comp.buildLogs).to.deep.equal([]);
         expect(comp.isLoading).to.be.false;
     });
 
-    it('should not try to retrieve build logs if the exercise type is not PROGRAMMING and no feedbacks are provided.', () => {
-        comp.exerciseType = ExerciseType.MODELING;
-        comp.result = { id: 89, participation: { id: 55 } } as Result;
+    it('should try to retrieve build logs if the exercise type is PROGRAMMING and a submission was provided which was marked with build failed.', () => {
+        comp.exerciseType = ExerciseType.PROGRAMMING;
+        comp.result.submission = generateProgrammingSubmission(true);
 
         comp.ngOnInit();
 
-        expect(getFeedbackDetailsForResultStub).to.have.been.calledOnceWithExactly(comp.result.id);
+        expect(buildlogsStub).to.have.been.calledOnceWithExactly(comp.result.participation!.id);
+        expect(comp.buildLogs).to.deep.equal([]);
+        expect(comp.isLoading).to.be.false;
+    });
+
+    it('should not try to retrieve build logs if the exercise type is not PROGRAMMING', () => {
+        comp.exerciseType = ExerciseType.MODELING;
+        comp.result.submission = new ModelingSubmission();
+
+        comp.ngOnInit();
+
         expect(buildlogsStub).to.not.have.been.called;
         expect(comp.feedbackList).to.be.undefined;
+        expect(comp.isLoading).to.be.false;
+    });
+
+    it('should not try to retrieve build logs if submission was not marked with build failed', () => {
+        comp.exerciseType = ExerciseType.PROGRAMMING;
+        comp.result.submission = generateProgrammingSubmission(false);
+
+        comp.ngOnInit();
+
+        expect(buildlogsStub).to.not.have.been.called;
         expect(comp.buildLogs).to.be.undefined;
         expect(comp.isLoading).to.be.false;
     });
