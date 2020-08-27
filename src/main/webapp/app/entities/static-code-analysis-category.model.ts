acfa44e55bb1dca3b22bc70695839928bb473124
Kilian Schulte <schultek@in.tum.de>
2020-08-27 13:02:54 +0200

    add code analysis mapping

--- src/main/webapp/app/entities/static-code-analysis-category.model.ts
+++ src/main/webapp/app/entities/static-code-analysis-category.model.ts
@@ -1,13 +1,8 @@
-import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { BaseEntity } from 'app/shared/model/base-entity';
 
 export class StaticCodeAnalysisCategory implements BaseEntity {
     id: number;
-    tool: string;
     name: string;
     description: string;
-    perIssuePenalty: number;
-    maxPenalty: number;
-    active: boolean;
-    exercise: ProgrammingExercise;
+    checks: [{ tool: string; check: string }];
 }
