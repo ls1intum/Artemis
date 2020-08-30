85088e3e4b272a899a24d76e20a10afd63dce636
Kilian Schulte <schultek@in.tum.de>
2020-08-30 17:53:01 +0200

    update category model

--- src/main/webapp/app/entities/static-code-analysis-category.model.ts
+++ src/main/webapp/app/entities/static-code-analysis-category.model.ts
@@ -4,5 +4,14 @@ export class StaticCodeAnalysisCategory implements BaseEntity {
     id: number;
     name: string;
     description: string;
+    state: StaticCodeAnalysisCategoryState;
     checks: [{ tool: string; check: string }];
+    penalty: number;
+    maxPenalty: number;
+}
+
+export enum StaticCodeAnalysisCategoryState {
+    Inactive,
+    Feedback,
+    Graded,
 }
