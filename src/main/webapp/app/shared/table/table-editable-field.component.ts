2b0869ce6b87d5ac769691e29f17248aced71a49
Kilian Schulte <schultek@in.tum.de>
2020-08-31 16:05:45 +0200

    formatting

--- src/main/webapp/app/shared/table/table-editable-field.component.ts
+++ src/main/webapp/app/shared/table/table-editable-field.component.ts
@@ -1,16 +1,11 @@
-import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 /**
  * An inline editing field to use for tables.
  */
 @Component({
     selector: 'jhi-table-editable-field',
-    styles: [
-        '.table-editable-field {display: flex; align-items: center}',
-        '.table-editable-field__value {flex: 2 1 auto;}',
-        '.table-editable-field__input {flex: 2 1 auto;}',
-        '.table-editable-field__edit {flex: 0 1 auto; margin-left: auto;}',
-    ],
+    styles: ['.table-editable-field {display: flex; align-items: center}', '.table-editable-field__input {flex: 2 1 auto;}'],
     template: `
         <div class="table-editable-field">
             <input
