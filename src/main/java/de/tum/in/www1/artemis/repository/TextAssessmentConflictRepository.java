58a9a7fc30bdfa1c7023869a7ff087f129baaea8
Birtan <birtan.gultekin@hotmail.com>
2020-08-31 22:47:56 +0200

    query to retrieve all conflicting feedback for the given list of feedback

--- src/main/java/de/tum/in/www1/artemis/repository/TextAssessmentConflictRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/TextAssessmentConflictRepository.java
@@ -1,6 +1,10 @@
 package de.tum.in.www1.artemis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.TextAssessmentConflict;
@@ -10,4 +14,10 @@ import de.tum.in.www1.artemis.domain.TextAssessmentConflict;
  */
 @Repository
 public interface TextAssessmentConflictRepository extends JpaRepository<TextAssessmentConflict, Long> {
+
+    @Query("select distinct conflict from TextAssessmentConflict conflict "
+            + "left join fetch conflict.firstFeedback f1 left join fetch f1.result r1 left join fetch r1.submission "
+            + "left join fetch conflict.secondFeedback f2 left join fetch f2.result r2 left join fetch r2.submission "
+            + "where conflict.firstFeedback.id in (:feedbackIds) or conflict.secondFeedback.id in (:feedbackIds)")
+    List<TextAssessmentConflict> findAllByFeedback(@Param("feedbackIds") List<Long> feedbackIds);
 }
