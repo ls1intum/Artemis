86bf8721f7fb5471e0f5f33df91ac7e3662748d3
Andi Turdiu <andi.turdiu@tum.de>
2020-09-06 18:49:52 +0200

    create the configure test run modal

--- src/main/webapp/app/exam/manage/test-runs/create-test-run-modal.component.ts
+++ src/main/webapp/app/exam/manage/test-runs/create-test-run-modal.component.ts
@@ -1,32 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { Exam } from 'app/entities/exam.model';
 import { Exercise } from 'app/entities/exercise.model';
+import { ExerciseGroup } from 'app/entities/exercise-group.model';
 
 @Component({
     selector: 'jhi-create-test-run-modal',
     templateUrl: './create-test-run-modal.component.html',
+    styles: ['.table tr.active td { background-color:#3e8acc; color: white; }'],
 })
-export class CreateTestRunModal implements OnInit {
+export class CreateTestRunModal {
     exam: Exam;
     testRunConfiguration: { [id: number]: Exercise } = {};
+
     constructor(private activeModal: NgbActiveModal) {}
 
     /**
-     * Closes the modal and returns the configured testRun
-     * @param testRun The test run configured by the instructor
+     * Creates a test run student exam based on the test run configuration, {@link testRunConfiguration}.
+     * Closes the modal and returns the configured testRun.
      */
-    createTestRun(testRun: StudentExam) {
-        this.activeModal.close(testRun);
+    createTestRun() {
+        if (this.testRunConfigured) {
+            let testRun = new StudentExam();
+            testRun.exam = this.exam;
+            testRun.exercises = Object.values(this.testRunConfiguration);
+            this.activeModal.close(testRun);
+        }
     }
 
     /**
-     * Sets the selected exercise for an exercise group in the {@link testRunConfiguration}
-     * @param exercise Exercise selected for an exercise Group
+     * Sets the selected exercise for an exercise group in the testRunConfiguration dictionary, {@link testRunConfiguration}.
+     * There, the exerciseGroups' id is used as a key to track the selected exercises for this test run.
+     * @param exercise The selected exercise
+     * @param exerciseGroup The exercise group for which the user selected an exercise
      */
-    onSelectExercise(exercise: Exercise) {
-        this.testRunConfiguration[exercise.exerciseGroup!.id] = exercise;
+    onSelectExercise(exercise: Exercise, exerciseGroup: ExerciseGroup) {
+        this.testRunConfiguration[exerciseGroup.id] = exercise;
+    }
+
+    /**
+     * Track the items by id on the exercise Tables
+     * @param index {number}
+     * @param item {Exercise}
+     */
+    trackId(index: number, item: Exercise) {
+        return item.id;
     }
 
     /**
@@ -36,7 +55,10 @@ export class CreateTestRunModal implements OnInit {
         return Object.keys(this.testRunConfiguration).length === this.exam.exerciseGroups?.length;
     }
 
-    ngOnInit(): void {
-        console.log(this.exam.exerciseGroups?.length);
+    /**
+     * Closes the modal by dismissing it
+     */
+    cancel() {
+        this.activeModal.dismiss('cancel');
     }
 }
