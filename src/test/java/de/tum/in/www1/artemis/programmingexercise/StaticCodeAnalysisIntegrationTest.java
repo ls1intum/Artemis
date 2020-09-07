171c5102c02ab0a1d2692ec097861fc19ce62931
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-09-07 13:17:56 +0200

    Remove unused var

--- src/test/java/de/tum/in/www1/artemis/programmingexercise/StaticCodeAnalysisIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/programmingexercise/StaticCodeAnalysisIntegrationTest.java
@@ -30,9 +30,6 @@ class StaticCodeAnalysisIntegrationTest extends AbstractSpringIntegrationBambooB
     RequestUtilService request;
 
     @Autowired
-    private StaticCodeAnalysisCategoryRepository scaCategoryRepository;
-
-    @Autowired
     private ProgrammingExerciseRepository programmingExerciseRepository;
 
     private ProgrammingExercise programmingExerciseSCAEnabled;
