8210c3b75ac4ddda84f70776dfe12c5424f5ed73
Fabian Sauter <sauter.fabian@mailbox.org>
2020-09-08 16:10:40 +0200

    Fixed c template scripts

--- src/main/resources/templates/bamboo/c/regularRuns/2_setup_makefile.sh
+++ src/main/resources/templates/bamboo/c/regularRuns/2_setup_makefile.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+# ------------------------------
+# Task Description:
+# Setup makefile
+# ------------------------------
+
 shadowFilePath="../tests/testUtils/c/shadow_exec.c"
 
 foundIncludeDirs=`grep -m 1 'INCLUDEDIRS\s*=' assignment/Makefile`
 echo "Include: $foundIncludeDirs"
 
 foundSource=`grep -m 1 'SOURCE\s*=' assignment/Makefile`
-foundSource="$foundSource $shadowFilePath"
+# foundSource="$foundSource $shadowFilePath"
 echo "Source: $foundSource"
 
 rm -f assignment/GNUmakefile
 rm -f assignment/makefile
 
 cp -f tests/Makefile assignment/Makefile || exit 2
-sed -i "s~INCLUDEDIRS\s*=.*~${foundIncludeDirs}~; s~SOURCE\s*=.*~${foundSource}~" assignment/Makefile
+sed -i "s~\bINCLUDEDIRS\s*=.*~${foundIncludeDirs}~; s~\bSOURCE\s*=.*~${foundSource}~" assignment/Makefile
