c6123e6899b512b2a5d7e853f7d2c2822a2a084d
Filip Gregurevic <filipgregurevic@gmail.com>
2020-09-09 09:01:43 +0200

    load course

--- src/main/webapp/app/course/course-questions/course-questions.component.ts
+++ src/main/webapp/app/course/course-questions/course-questions.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Course } from 'app/entities/course.model';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute } from '@angular/router';
+import { CourseManagementService } from 'app/course/manage/course-management.service';
 
 @Component({
     selector: 'jhi-course-questions',
     templateUrl: './course-questions.component.html',
 })
-export class CourseQuestionsComponent {}
+export class CourseQuestionsComponent implements OnInit, OnDestroy {
+    course: Course;
+
+    paramSub: Subscription;
+
+    constructor(private route: ActivatedRoute, private courseService: CourseManagementService) {}
+
+    /**
+     * On init fetch the course
+     */
+    ngOnInit() {
+        this.paramSub = this.route.params.subscribe((params) => {
+            this.courseService.findOneForDashboard(params['courseId']).subscribe((res) => {
+                this.course = res.body!;
+                console.log(this.course);
+            });
+        });
+    }
+
+    /**
+     * On destroy unsubscribe.
+     */
+    ngOnDestroy() {
+        this.paramSub.unsubscribe();
+    }
+}
