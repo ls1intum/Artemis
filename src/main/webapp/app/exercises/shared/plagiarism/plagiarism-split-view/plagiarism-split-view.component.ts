d91fc179f3ec46d1ca7ba461703f4cd5bfb9a75c
Philipp Bauch <ga53xex@mytum.de>
2020-09-15 12:30:20 +0200

    Add split.js to the project

--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Directive, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import Split from 'split.js';
+
+@Directive({ selector: '[jhiPane]' })
+export class SplitPaneDirective {
+    constructor(public el: ElementRef) {}
+}
 
 @Component({
     selector: 'jhi-plagiarism-split-view',
     styleUrls: ['./plagiarism-split-view.component.scss'],
     templateUrl: './plagiarism-split-view.component.html',
 })
-export class PlagiarismSplitViewComponent {}
+export class PlagiarismSplitViewComponent implements AfterViewInit {
+    @ViewChildren(SplitPaneDirective) panes!: QueryList<SplitPaneDirective>;
+
+    private split: any;
+
+    /**
+     * Initialize third party libs inside this lifecycle hook.
+     */
+    ngAfterViewInit(): void {
+        const paneElements = this.panes.map((pane: SplitPaneDirective) => pane.el.nativeElement);
+
+        this.split = Split(paneElements, {
+            minSize: 100,
+            sizes: [50, 50],
+        });
+    }
+}
--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { PlagiarismInspectorComponent } from 'app/exercises/shared/plagiarism/plagiarism-inspector/plagiarism-inspector.component';
 import { PlagiarismHeaderComponent } from 'app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component';
-import { PlagiarismSplitViewComponent } from 'app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component';
+import { PlagiarismSplitViewComponent, SplitPaneDirective } from 'app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component';
 
 @NgModule({
     imports: [ArtemisSharedModule],
-    declarations: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent],
+    declarations: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent, SplitPaneDirective],
     exports: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent],
 })
 export class ArtemisPlagiarismModule {}
