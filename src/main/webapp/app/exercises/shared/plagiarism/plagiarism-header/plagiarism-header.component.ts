ec39e1aa61a9192db7b948d9e3ec456850d846ce
Philipp Bauch <ga53xex@mytum.de>
2020-09-15 09:15:46 +0200

    Add plagiarism-header and plagiarism-split-view components

---
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-plagiarism-header',
+    styleUrls: ['./plagiarism-header.component.scss'],
+    templateUrl: './plagiarism-header.component.html',
+})
+export class PlagiarismHeaderComponent {}
--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { PlagiarismInspectorComponent } from 'app/exercises/shared/plagiarism/plagiarism-inspector/plagiarism-inspector.component';
+import { PlagiarismHeaderComponent } from 'app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component';
+import { PlagiarismSplitViewComponent } from 'app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component';
 
 @NgModule({
     imports: [ArtemisSharedModule],
-    declarations: [PlagiarismInspectorComponent],
-    exports: [PlagiarismInspectorComponent],
+    declarations: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent],
+    exports: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent],
 })
 export class ArtemisPlagiarismModule {}
