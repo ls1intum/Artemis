90eac6dc343231b381c5d11de1e544b498ea9ed9
Birtan <birtan.gultekin@hotmail.com>
2020-09-21 22:30:55 +0200

    all new queries

--- src/main/java/de/tum/in/www1/artemis/repository/TextAssessmentConflictRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/TextAssessmentConflictRepository.java
@@ -16,8 +16,21 @@ import de.tum.in.www1.artemis.domain.TextAssessmentConflict;
 public interface TextAssessmentConflictRepository extends JpaRepository<TextAssessmentConflict, Long> {
 
     @Query("select distinct conflict from TextAssessmentConflict conflict "
-            + "left join fetch conflict.firstFeedback f1 left join fetch f1.result r1 left join fetch r1.submission "
-            + "left join fetch conflict.secondFeedback f2 left join fetch f2.result r2 left join fetch r2.submission "
-            + "where conflict.firstFeedback.id in (:feedbackIds) or conflict.secondFeedback.id in (:feedbackIds)")
-    List<TextAssessmentConflict> findAllByFeedback(@Param("feedbackIds") List<Long> feedbackIds);
+            + "left join fetch conflict.firstFeedback f1 left join fetch f1.result r1 left join fetch r1.submission left join fetch r1.feedbacks left join fetch r1.assessor "
+            + "left join fetch conflict.secondFeedback f2 left join fetch f2.result r2 left join fetch r2.submission left join fetch r2.feedbacks left join fetch r2.assessor "
+            + "where conflict.conflict = true and (conflict.firstFeedback.id = :feedbackId or conflict.secondFeedback.id = :feedbackId)")
+    List<TextAssessmentConflict> findAllByFeedback(@Param("feedbackId") Long feedbackId);
+
+    @Query("select distinct conflict from TextAssessmentConflict conflict where conflict.conflict = true and (conflict.firstFeedback.id in (:feedbackIds) or conflict.secondFeedback.id in (:feedbackIds))")
+    List<TextAssessmentConflict> findAllByFeedbackList(@Param("feedbackIds") List<Long> feedbackIds);
+
+    List<TextAssessmentConflict> findByFirstFeedbackIdAndConflict(Long id, Boolean conflict);
+
+    List<TextAssessmentConflict> findBySecondFeedbackIdAndConflict(Long id, Boolean conflict);
+
+    @Query("select distinct conflict from TextAssessmentConflict conflict where conflict.conflict = true and "
+            + "((conflict.firstFeedback.id = :firstFeedbackId and conflict.secondFeedback.id = :secondFeedbackId) or "
+            + "(conflict.secondFeedback.id = :firstFeedbackId and conflict.firstFeedback.id = :secondFeedbackId))")
+    List<TextAssessmentConflict> findByFirstAndSecondFeedback(@Param("firstFeedbackId") Long firstFeedbackId, @Param("secondFeedbackId") Long secondFeedbackId);
+
 }
