f44040351c26e3fb709fd9e2af4e45f0ecac75f1
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-09-23 18:40:43 +0200

    Remove unused import

--- src/main/webapp/app/exercises/programming/manage/services/programming-exercise-grading.service.ts
+++ src/main/webapp/app/exercises/programming/manage/services/programming-exercise-grading.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { SERVER_API_URL } from 'app/app.constants';
