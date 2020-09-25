f1193b9a239e3ce94dfd3a692dc3ef66b0fd0335
kilianschulte <kilian@4xschulte.de>
2020-09-25 14:32:50 +0200

    remove unused category fields

--- src/main/webapp/app/entities/static-code-analysis-category.model.ts
+++ src/main/webapp/app/entities/static-code-analysis-category.model.ts
@@ -3,9 +3,7 @@ import { BaseEntity } from 'app/shared/model/base-entity';
 export class StaticCodeAnalysisCategory implements BaseEntity {
     id: number;
     name: string;
-    description: string;
     state: StaticCodeAnalysisCategoryState;
-    checks: { tool: string; check: string }[];
     penalty: number;
     maxPenalty: number;
 }
