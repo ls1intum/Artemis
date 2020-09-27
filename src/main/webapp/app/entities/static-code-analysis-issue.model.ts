9afc6eb609670d53de6317ed4e3d8bd59f93d882
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-09-27 21:32:03 +0200

    Make properties optional

--- src/main/webapp/app/entities/static-code-analysis-issue.model.ts
+++ src/main/webapp/app/entities/static-code-analysis-issue.model.ts
@@ -3,7 +3,7 @@ export class StaticCodeAnalysisIssue {
     public startLine: number;
     public endLine: number;
     public startColumn?: number;
-    public endColumn: number | null;
+    public endColumn?: number;
     public rule: string;
     public category: string;
     public message: string;
