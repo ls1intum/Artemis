0655281d8f94c348d618c7aeefc8fa833c2bf929
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-09-29 01:50:15 +0200

    Revert "Revert "Merge develop""
    This reverts commit fb5e2eaf5a44662241ea1834af78129137e8ae86, reversing
    changes made to cf95d44dc783378ac3e5854dce89525dc7054f4f.

--- src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
+++ src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
@@ -611,6 +611,7 @@ public class ModelFactory {
         job.setId(42);
         job.setFailedTests(failedTests);
         job.setSuccessfulTests(successfulTests);
+        job.setLogs(List.of());
 
         summary.setTotalCount(successfulTestNames.size() + failedTestNames.size());
         summary.setSuccessfulCount(successfulTestNames.size());
