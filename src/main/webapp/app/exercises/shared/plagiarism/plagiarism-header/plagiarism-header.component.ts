25c0f32e590faf8775694a6393bfb0c7997bbd51
Philipp Bauch <ga53xex@mytum.de>
2020-09-29 16:10:13 +0200

    Rename EventEmitters in PlagiarismHeaderComponent

--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component.ts
@@ -7,10 +7,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class PlagiarismHeaderComponent {
     @Input() comparisonIdx: number;
-    @Output() split = new EventEmitter<string>();
-    @Output() tag = new EventEmitter<boolean>();
+    @Output() splitViewChange = new EventEmitter<string>();
+    @Output() plagiarismConfirmation = new EventEmitter<boolean>();
 
     tagPlagiarism(confirmed: boolean) {
-        this.tag.emit(confirmed);
+        this.plagiarismConfirmation.emit(confirmed);
     }
 }
