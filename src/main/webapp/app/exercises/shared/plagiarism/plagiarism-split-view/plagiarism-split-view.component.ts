cf43c9974f3cf7fc7f41ea269ba2344362ca04aa
Stephan Krusche <s.krusche@tum.de>
2020-09-29 20:39:23 +0200

    fix an issue with Split import

--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, Directive, ElementRef, Input, OnChanges, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
 // @ts-ignore
-import Split from 'split.js';
+import { Split } from 'split.js';
 import { Observable } from 'rxjs';
 import { ModelingSubmissionComparisonDTO } from 'app/exercises/modeling/manage/modeling-exercise.service';
 import { ModelingSubmissionService } from 'app/exercises/modeling/participate/modeling-submission.service';
