3dce31f4fb693b59cefd5f0a1e3849f61ad9134a
Jonas Petry <ga27der@mytum.de>
2020-09-30 16:20:04 +0200

    Improve test coverage for rating-list.component.ts

--- src/test/javascript/spec/component/rating/rating-list.component.spec.ts
+++ src/test/javascript/spec/component/rating/rating-list.component.spec.ts
@@ -6,15 +6,20 @@ import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { ArtemisTestModule } from '../../test.module';
 import { MockActivatedRoute } from '../../helpers/mocks/activated-route/mock-activated-route';
 import { RatingService } from 'app/exercises/shared/rating/rating.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DifferencePipe } from 'ngx-moment';
 import { RatingModule as StarRatingComponent } from 'ng-starrating';
 
 import { of } from 'rxjs';
+import { Participation } from 'app/entities/participation/participation.model';
+import { Result } from 'app/entities/result.model';
+import { Rating } from 'app/entities/rating.model';
+import { Exercise, ExerciseType } from 'app/entities/exercise.model';
 
 describe('RatingListComponent', () => {
     let component: RatingListComponent;
     let fixture: ComponentFixture<RatingListComponent>;
+    let router: Router;
 
     const ratings = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
@@ -43,6 +48,7 @@ describe('RatingListComponent', () => {
             .then(() => {
                 fixture = TestBed.createComponent(RatingListComponent);
                 component = fixture.componentInstance;
+                router = TestBed.inject(Router);
 
                 component.ngOnInit();
             });
@@ -50,5 +56,26 @@ describe('RatingListComponent', () => {
 
     it('should initialize ratings', () => {
         expect(component.ratings.length).toEqual(3);
+    });
+
+    it('should not open exercise du to missing participation', () => {
+        const rating = { id: 1, result: { id: 1 } as Result } as Rating;
+        const routerNavigateSpy = jest.spyOn(router, 'navigate').mockImplementation(() => of(true).toPromise());
+        component.openResult(rating);
+        expect(routerNavigateSpy).not.toHaveBeenCalled();
+    });
+
+    it('should not open exercise du to missing exercise', () => {
+        const rating = { id: 1, result: { id: 1, participation: { id: 1 } as Participation } as Result } as Rating;
+        const routerNavigateSpy = jest.spyOn(router, 'navigate').mockImplementation(() => of(true).toPromise());
+        component.openResult(rating);
+        expect(routerNavigateSpy).not.toHaveBeenCalled();
+    });
+
+    it('should open exercise', () => {
+        const rating = { id: 1, result: { id: 1, participation: { id: 1, exercise: { id: 1, type: ExerciseType.TEXT } as Exercise } as Participation } as Result } as Rating;
+        const routerNavigateSpy = jest.spyOn(router, 'navigate').mockImplementation(() => of(true).toPromise());
+        component.openResult(rating);
+        expect(routerNavigateSpy).toHaveBeenCalled();
     });
 });
