542b93b28085c93600e13f60b15530b831dba55e
Andi Turdiu <andi.turdiu@tum.de>
2020-10-01 20:46:59 +0200

    add feature structure and translation

--- src/main/webapp/app/informative-marketing/instructors/informative-marketing-instructors.component.ts
+++ src/main/webapp/app/informative-marketing/instructors/informative-marketing-instructors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'jhi-informative-marketing',
@@ -6,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./../informative-marketing.scss'],
 })
 export class InformativeMarketingInstructorsComponent implements OnInit {
-    features: string[];
+    features: Feature[];
+
+    constructor(private translateService: TranslateService) {}
 
     ngOnInit(): void {
-        this.features = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+        const multipleExerciseTypes = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+        );
+        this.features = [
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+            multipleExerciseTypes,
+        ];
+    }
+
+    translateFeatureTitle(feature: Feature) {
+        return this.translateService.instant(feature.title);
+    }
+
+    translateFeatureShortDescription(feature: Feature) {
+        return this.translateService.instant(feature.shortDescription);
+    }
+
+    translateFeatureDescription(feature: Feature) {
+        return this.translateService.instant(feature.longDescription);
+    }
+}
+
+class Feature {
+    title: string;
+    shortDescription: string;
+    longDescription: string;
+
+    constructor(title: string, shortDescription: string, longDescription: string) {
+        this.title = title;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
     }
 }
