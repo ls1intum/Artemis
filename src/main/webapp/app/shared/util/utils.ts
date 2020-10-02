cc7205852c9c0b69b359cb8c59e50bf905d6a4e0
Kilian Schulte <schultek@in.tum.de>
2020-10-02 21:01:28 +0200

    fix empty results array error

--- src/main/webapp/app/shared/util/utils.ts
+++ src/main/webapp/app/shared/util/utils.ts
@@ -81,5 +81,5 @@ export const round = (value: any, exp: number) => {
  * @param results
  */
 export const findLatestResult = (results?: Result[]) => {
-    return results?.reduce((current, result) => (current.id! > result.id! ? current : result));
+    return results && results.length > 0 ? results.reduce((current, result) => (current.id! > result.id! ? current : result)) : undefined;
 };
