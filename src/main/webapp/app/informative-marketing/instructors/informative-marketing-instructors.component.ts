58c1ef16439d1d11f467f55294f6438a60d27136
Andi Turdiu <andi.turdiu@tum.de>
2020-10-02 16:09:28 +0200

    add different sections, add scrolling feature

--- src/main/webapp/app/informative-marketing/instructors/informative-marketing-instructors.component.ts
+++ src/main/webapp/app/informative-marketing/instructors/informative-marketing-instructors.component.ts
@@ -12,21 +12,65 @@ export class InformativeMarketingInstructorsComponent implements OnInit {
     constructor(private translateService: TranslateService) {}
 
     ngOnInit(): void {
-        const multipleExerciseTypes = new Feature(
+        const featureOne = new Feature(
             'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
             'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
             'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
         );
-        this.features = [
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-            multipleExerciseTypes,
-        ];
+        const featureTwo = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureThree = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureFour = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureFive = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureSix = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureSeven = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        const featureEight = new Feature(
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.title',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.shortDescription',
+            'informativeMarketing.instructor.feature.multipleExerciseTypes.fullDescription',
+            'fa fa-code',
+        );
+        this.features = [featureOne, featureTwo, featureThree, featureFour, featureFive, featureSix, featureSeven, featureEight];
+    }
+
+    navigateToFeature(featureId: string): void {
+        // get html element for feature
+        const element = document.getElementById('feature' + featureId);
+        if (element) {
+            // scroll to correct y
+            const y = element.getBoundingClientRect().top + window.pageYOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
     }
 
     translateFeatureTitle(feature: Feature) {
@@ -46,10 +90,23 @@ class Feature {
     title: string;
     shortDescription: string;
     longDescription: string;
+    icon: string;
+    id: string;
 
-    constructor(title: string, shortDescription: string, longDescription: string) {
+    constructor(title: string, shortDescription: string, longDescription: string, icon: string) {
         this.title = title;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+        this.icon = icon;
+        this.id = this.setId();
+    }
+
+    /**
+     * Math.random should be unique because of its seeding algorithm.
+     * Convert it to base 36 (numbers + letters), and grab the first 9 characters after the decimal.
+     * @private
+     */
+    setId(): string {
+        return ':' + Math.random().toString(36).substr(2, 9);
     }
 }
