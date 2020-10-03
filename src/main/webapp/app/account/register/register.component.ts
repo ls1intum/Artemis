b3a64b26a7361bd4e4b0a2b66fa8f49587eba7b4
Stephan Krusche <s.krusche@tum.de>
2020-10-03 18:02:29 +0200

    add some testing code

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -44,6 +44,14 @@ export class RegisterComponent implements OnInit, AfterViewInit {
         }
     }
 
+    // TEST CODE BEGIN
+    updateAllowedEmailPattern() {
+        if (this.allowedEmailPattern) {
+            this.registerForm.get('email')!.setValidators([Validators.required, Validators.minLength(4), Validators.maxLength(50), Validators.pattern(this.allowedEmailPattern)]);
+        }
+    }
+    // TEST CODE END
+
     ngOnInit() {
         this.profileService.getProfileInfo().subscribe((profileInfo) => {
             if (profileInfo) {
