74a1a1b59fb01170a7b9fc131bbe50c4e0693771
Balazs Czopf <balazs.czopf@tum.de>
2020-10-06 01:42:44 +0200

    add import

--- src/main/java/de/tum/in/www1/artemis/service/AchievementService.java
+++ src/main/java/de/tum/in/www1/artemis/service/AchievementService.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.Achievement;
