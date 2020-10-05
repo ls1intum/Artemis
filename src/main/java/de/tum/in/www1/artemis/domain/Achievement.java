a616036218e566ee1e309966c4cd89c4f8b5b7f0
Balazs Czopf <63286106+balazs-czopf@users.noreply.github.com>
2020-10-05 16:22:01 +0200

    remove getter and setter for id
    Co-authored-by: Christian Femers <c.femers@tum.de>

--- src/main/java/de/tum/in/www1/artemis/domain/Achievement.java
+++ src/main/java/de/tum/in/www1/artemis/domain/Achievement.java
@@ -62,14 +62,6 @@ public class Achievement extends DomainObject {
         this.exercise = exercise;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
