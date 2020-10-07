3fd50530f132bb5f680e5722975fbc808f9f4a66
Stefan Waldhauser <hello@stefanwaldhauser.me>
2020-10-07 10:58:18 +0200

    Created entity skeleton

---
+++ src/main/java/de/tum/in/www1/artemis/domain/LearningGoal.java
@@ -0,0 +1,7 @@
+package de.tum.in.www1.artemis.domain;
+
+/**
+ * Entity that represents a learning goal students should try to achieve in a course
+ */
+public class LearningGoal extends DomainObject {
+}
