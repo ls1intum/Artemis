8697724d209554e1f4f7d75ab6108765cf042b20
Xin Wang <winfo_xin.wang@tum.de>
2020-10-07 17:08:09 +0200

    resolve the coverage issue by remove JSON.stringify the object.

--- src/test/javascript/spec/service/exam-participation.service.spec.ts
+++ src/test/javascript/spec/service/exam-participation.service.spec.ts
@@ -1,6 +1,6 @@
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { map, take } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import * as moment from 'moment';
 import { ExamParticipationService } from 'app/exam/participate/exam-participation.service';
 import { Exam } from 'app/entities/exam.model';
@@ -54,7 +54,7 @@ describe('Exam Participation Service', () => {
                 .subscribe((resp) => expect(resp).toMatchObject({ body: studentExam }));
 
             const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('should load a StudentExam with exercises for summary', async () => {
             const returnedFromService = Object.assign({}, studentExam);
@@ -63,7 +63,7 @@ describe('Exam Participation Service', () => {
                 .pipe(take(1))
                 .subscribe((resp) => expect(resp).toMatchObject({ body: studentExam }));
             const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('should load a StudentExam', async () => {
             const sendExam = Object.assign(
@@ -89,7 +89,7 @@ describe('Exam Participation Service', () => {
                 .subscribe((resp) => expect(resp).toMatchObject({ body: studentExam }));
 
             const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('should load a StudentExam in the version of server', async () => {
             /*configure exercises of this student Exam*/
@@ -122,7 +122,7 @@ describe('Exam Participation Service', () => {
                 .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
 
             const req = httpMock.expectOne({ method: 'POST' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('should update a QuizSubmission', async () => {
             const returnedFromService = Object.assign({}, quizSubmission);
@@ -133,7 +133,7 @@ describe('Exam Participation Service', () => {
                 .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
 
             const req = httpMock.expectOne({ method: 'PUT' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('should load testRun with exercises for conduction', async () => {
             const returnedFromService = Object.assign({}, studentExam);
@@ -144,7 +144,7 @@ describe('Exam Participation Service', () => {
                 .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
 
             const req = httpMock.expectOne({ method: 'GET' });
-            req.flush(JSON.stringify(returnedFromService));
+            req.flush(returnedFromService);
         });
         it('save examSessionToken to sessionStorage', async () => {
             service.saveExamSessionTokenToSessionStorage('token1');
@@ -157,7 +157,7 @@ describe('Exam Participation Service', () => {
             const expected = Object.assign({}, sendToService);
             service.saveStudentExamToLocalStorage(1, 1, sendToService);
             spyOn(localStorage, 'setItem').and.callFake(() => {
-                expect(localStorage['artemis_student_exam_1_1'].toBe(JSON.stringify(expected)));
+                expect(localStorage['artemis_student_exam_1_1'].toBe(expected));
             });
         });
     });
