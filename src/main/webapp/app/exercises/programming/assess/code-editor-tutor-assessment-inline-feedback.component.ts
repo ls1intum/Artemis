496beedbd05f0560733b9c8c0659276cc1ad204a
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2020-10-13 17:28:06 +0200

    add drag and drop to inline feedback

--- src/main/webapp/app/exercises/programming/assess/code-editor-tutor-assessment-inline-feedback.component.ts
+++ src/main/webapp/app/exercises/programming/assess/code-editor-tutor-assessment-inline-feedback.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Feedback, FeedbackType } from 'app/entities/feedback.model';
 import { cloneDeep } from 'lodash';
 import { TranslateService } from '@ngx-translate/core';
+import { StructuredGradingCriterionService } from 'app/exercises/shared/structured-grading-criterion/structured-grading-criterion.service';
 
 @Component({
     selector: 'jhi-code-editor-tutor-assessment-inline-feedback',
@@ -17,6 +18,11 @@ export class CodeEditorTutorAssessmentInlineFeedbackComponent {
         this._feedback = feedback || new Feedback();
         this.oldFeedback = cloneDeep(this.feedback);
         this.editOnly = feedback ? true : false;
+        if (this._feedback.gradingInstruction && this._feedback.gradingInstruction.usageCount !== 0) {
+            this.disableEditScore = true;
+        } else {
+            this.disableEditScore = false;
+        }
     }
     private _feedback: Feedback;
     @Input()
@@ -36,7 +42,8 @@ export class CodeEditorTutorAssessmentInlineFeedbackComponent {
 
     editOnly: boolean;
     oldFeedback: Feedback;
-    constructor(private translateService: TranslateService) {}
+    disableEditScore = false;
+    constructor(private translateService: TranslateService, public structuredGradingCriterionService: StructuredGradingCriterionService) {}
 
     /**
      * Updates the current feedback and sets props and emits the feedback to parent component
@@ -79,5 +86,15 @@ export class CodeEditorTutorAssessmentInlineFeedbackComponent {
     editFeedback(line: number) {
         this.editOnly = false;
         this.onEditFeedback.emit(line);
+    }
+
+    updateFeedbackOnDrop(event: Event) {
+        this.structuredGradingCriterionService.updateFeedbackWithStructuredGradingInstructionEvent(this.feedback, event);
+        if (this.feedback.gradingInstruction && this.feedback.gradingInstruction.usageCount !== 0) {
+            this.disableEditScore = true;
+        } else {
+            this.disableEditScore = false;
+        }
+        this.onUpdateFeedback.emit(this.feedback);
     }
 }
