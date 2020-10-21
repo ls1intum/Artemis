0c304227986d08cdd6f133efbf58032735278ed3
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-10-22 01:24:42 +0200

    Fix category typos

--- src/main/java/de/tum/in/www1/artemis/config/StaticCodeAnalysisConfigurer.java
+++ src/main/java/de/tum/in/www1/artemis/config/StaticCodeAnalysisConfigurer.java
@@ -42,11 +42,11 @@ public class StaticCodeAnalysisConfigurer {
     private List<StaticCodeAnalysisDefaultCategory> createDefaultCategoriesForJava() {
         return List.of(
                 new StaticCodeAnalysisDefaultCategory("Bad Practice", 0.5D, 5D, CategoryState.FEEDBACK,
-                        List.of(createMapping(StaticCodeAnalysisTool.SPOTBUGS, "BAD_PRACTICE"), createMapping(StaticCodeAnalysisTool.PMD, "Best-Practices"))),
+                        List.of(createMapping(StaticCodeAnalysisTool.SPOTBUGS, "BAD_PRACTICE"), createMapping(StaticCodeAnalysisTool.PMD, "Best Practices"))),
                 new StaticCodeAnalysisDefaultCategory("Code Style", 0.2D, 2D, CategoryState.FEEDBACK,
                         List.of(createMapping(StaticCodeAnalysisTool.SPOTBUGS, "STYLE"), createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "blocks"),
                                 createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "coding"), createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "modifier"),
-                                createMapping(StaticCodeAnalysisTool.PMD, "Code-Style"))),
+                                createMapping(StaticCodeAnalysisTool.PMD, "Code Style"))),
                 new StaticCodeAnalysisDefaultCategory("Potential Bugs", 0.5D, 5D, CategoryState.FEEDBACK,
                         List.of(createMapping(StaticCodeAnalysisTool.SPOTBUGS, "CORRECTNESS"), createMapping(StaticCodeAnalysisTool.SPOTBUGS, "MT_CORRECTNESS"),
                                 createMapping(StaticCodeAnalysisTool.PMD, "Error Prone"), createMapping(StaticCodeAnalysisTool.PMD, "Multithreading"))),
@@ -64,7 +64,7 @@ public class StaticCodeAnalysisConfigurer {
                                 createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "annotation"), createMapping(StaticCodeAnalysisTool.PMD, "Documentation"))),
                 new StaticCodeAnalysisDefaultCategory("Naming & Formatting", 0D, 0D, CategoryState.INACTIVE,
                         List.of(createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "imports"), createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "indentation"),
-                                createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "naming"), createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "whitespaces"))),
+                                createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "naming"), createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "whitespace"))),
                 new StaticCodeAnalysisDefaultCategory("Miscellaneous", 0D, 0D, CategoryState.INACTIVE, List.of(createMapping(StaticCodeAnalysisTool.CHECKSTYLE, "miscellaneous"))));
     }
 
