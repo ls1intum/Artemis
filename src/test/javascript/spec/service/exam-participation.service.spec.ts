42fd7bd0448a0e69eb010cb0b30be4e5d91a08e3
Stephan Krusche <s.krusche@tum.de>
2020-10-24 21:33:41 +0200

    try to fix jasmin test error

--- src/test/javascript/spec/service/exam-participation.service.spec.ts
+++ src/test/javascript/spec/service/exam-participation.service.spec.ts
@@ -11,12 +11,15 @@ import { MockSyncStorage } from '../helpers/mocks/service/mock-sync-storage.serv
 import { MockTranslateService } from '../helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ArtemisTestModule } from '../test.module';
-import { expect } from '../helpers/jasmine.jest.fix';
 import { TextExercise } from 'app/entities/text-exercise.model';
 import { Course } from 'app/entities/course.model';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
 import { TextSubmission } from 'app/entities/text-submission.model';
 import { Result } from 'app/entities/result.model';
+
+// Note: for some reason the normal "import { expect } from '../helpers/jasmine.jest.fix';" does not work here and leads to an exception
+type JestExpect = <T>(actual: T) => jest.Matchers<T>;
+export declare const expect: JestExpect;
 
 describe('Exam Participation Service', () => {
     let injector: TestBed;
