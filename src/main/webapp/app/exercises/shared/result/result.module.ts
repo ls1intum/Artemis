3c320197a312033b4035e0a59d99cc5e59acf550
Kilian Schulte <schultek@in.tum.de>
2020-10-25 11:01:17 +0100

    move manual-result-button from programming assessment module into result module

--- src/main/webapp/app/exercises/shared/result/result.module.ts
+++ src/main/webapp/app/exercises/shared/result/result.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { MomentModule } from 'ngx-moment';
 import { ResultHistoryComponent } from 'app/overview/result-history/result-history.component';
@@ -8,10 +7,12 @@ import { SubmissionResultStatusComponent } from 'app/overview/submission-result-
 import { UpdatingResultComponent } from 'app/exercises/shared/result/updating-result.component';
 import { ResultComponent } from 'app/exercises/shared/result/result.component';
 import { ResultDetailComponent } from 'app/exercises/shared/result/result-detail.component';
+import { ManualResultButtonComponent } from 'app/exercises/shared/result/manual-result-button.component';
+import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingExerciseActionsModule],
-    declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
-    exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
+    imports: [ArtemisSharedModule, MomentModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule],
+    declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent, ManualResultButtonComponent],
+    exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent, ManualResultButtonComponent],
 })
 export class ArtemisResultModule {}
