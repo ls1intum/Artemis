602e8ed9740904db94b7313bdeda7807985ac773
Simon Leiß <git@simon-leiss.de>
2020-10-26 18:04:37 +0100

    Change order of eclipse and maven.

--- src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsProgrammingLanguageFeatureService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsProgrammingLanguageFeatureService.java
@@ -17,7 +17,7 @@ public class JenkinsProgrammingLanguageFeatureService extends ProgrammingLanguag
 
     public JenkinsProgrammingLanguageFeatureService() {
         // Must be extended once a new programming language is added
-        programmingLanguageFeatures.put(JAVA, new ProgrammingLanguageFeature(JAVA, false, true, true, true, false, List.of(MAVEN, ECLIPSE)));
+        programmingLanguageFeatures.put(JAVA, new ProgrammingLanguageFeature(JAVA, false, true, true, true, false, List.of(ECLIPSE, MAVEN)));
         programmingLanguageFeatures.put(PYTHON, new ProgrammingLanguageFeature(PYTHON, true, false, true, false, false, List.of()));
         // TODO: Should be re-enabled once Jenkins Pipelines are used
         // programmingLanguageFeatures.put(C, new ProgrammingLanguageFeature(C, false, false, true, false, false));
