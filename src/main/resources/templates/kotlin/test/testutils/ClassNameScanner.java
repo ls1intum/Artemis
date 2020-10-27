64da25c19db83616bae0e8e8b433988c5b6a4643
Christian Femers <christian.femers@tum.de>
2020-10-27 17:38:52 +0100

    Fix scanning of Kotlin files

--- src/main/resources/templates/kotlin/test/testutils/ClassNameScanner.java
+++ src/main/resources/templates/kotlin/test/testutils/ClassNameScanner.java
@@ -107,7 +107,7 @@ public class ClassNameScanner {
                 boolean hasWrongCase = observedClassName.equalsIgnoreCase(expectedClassName);
                 boolean hasTypos = FuzzySearch.ratio(observedClassName, expectedClassName) > 90;
                 //The previous implementation
-//				boolean hasTypos = DiffUtils.levEditDistance(observedClassName, expectedClassName, 1) < Math.ceil(expectedClassName.length() / 4);
+//              boolean hasTypos = DiffUtils.levEditDistance(observedClassName, expectedClassName, 1) < Math.ceil(expectedClassName.length() / 4);
 
                 foundObservedClassName = observedClassName;
                 foundObservedPackageName = observedPackageNames.toString();
@@ -224,25 +224,25 @@ public class ClassNameScanner {
      * each type it finds e.g. filenames ending in .java to the passed JSON object.
      * @param assignmentFolderName: The root folder where the method starts walking the project structure.
      * @param node: The current node the method is visiting.
-     * @param types: The JSON object where the type names and packages get appended.
+     * @param foundTypes: The JSON object where the type names and packages get appended.
      */
     private void walkProjectFileStructure(String assignmentFolderName, File node, Map<String, List<String>> foundTypes) {
         String fileName = node.getName();
 
-        if(fileName.contains(".java")) {
+        if(fileName.contains(".java") || fileName.contains(".kt")) {
             String[] fileNameComponents = fileName.split("\\.");
 
             String className = fileNameComponents[fileNameComponents.length - 2];
-            String packageName = node.getPath().substring(0, node.getPath().indexOf(".java"));
+            int until = node.getPath().indexOf(".java");
+            if (until == -1) {
+                until = node.getPath().indexOf(".kt");
+            }
+            String packageName = node.getPath().substring(0, until);
             packageName = packageName.substring(
                 packageName.indexOf(assignmentFolderName) + assignmentFolderName.length() + 1,
                 packageName.lastIndexOf(File.separator + className));
             packageName = packageName.replace(File.separatorChar, '.');
 
-            if (packageName.charAt(0) == '.') {
-                packageName = packageName.substring(1);
-            }
-            
             if(foundTypes.containsKey(className)) {
                 foundTypes.get(className).add(packageName);
             }
