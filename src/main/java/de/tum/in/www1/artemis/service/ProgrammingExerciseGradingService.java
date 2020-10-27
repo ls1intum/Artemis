1a4a239797612155f35c23743083808826432305
Christian Femers <christian.femers@tum.de>
2020-10-27 19:02:45 +0100

    Handle zero point exercises by calculating with placeholder point amount

--- src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseGradingService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ProgrammingExerciseGradingService.java
@@ -28,6 +28,11 @@ import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
 @Service
 public class ProgrammingExerciseGradingService {
 
+    /**
+     * Placeholder point value for the score calculation of zero-point exercises to avoid the score always being 0.
+     */
+    private static final double PLACEHOLDER_POINTS_FOR_ZERO_POINT_EXERCISES = 1.0;
+
     private final Logger log = LoggerFactory.getLogger(ProgrammingExerciseGradingService.class);
 
     private final Optional<ContinuousIntegrationService> continuousIntegrationService;
@@ -340,12 +345,13 @@ public class ProgrammingExerciseGradingService {
             List<Feedback> staticCodeAnalysisFeedback, ProgrammingExercise programmingExercise) {
         if (successfulTestCases.size() > 0) {
 
+            double maxScoreRespectingZeroPointExercises = getMaxScoreRespectingZeroPointExercises(programmingExercise);
             double weightSum = allTests.stream().mapToDouble(ProgrammingExerciseTestCase::getWeight).sum();
 
             // calculate the achieved points from the passed test cases
             double successfulTestPoints = successfulTestCases.stream().mapToDouble(test -> {
                 double testWeight = test.getWeight() * test.getBonusMultiplier();
-                double testPoints = testWeight / weightSum * programmingExercise.getMaxScore();
+                double testPoints = testWeight / weightSum * maxScoreRespectingZeroPointExercises;
                 double testPointsWithBonus = testPoints + test.getBonusPoints();
                 // update credits of related feedback
                 result.getFeedbacks().stream().filter(fb -> fb.getType() == FeedbackType.AUTOMATIC && fb.getText().equals(test.getTestName())).findFirst()
@@ -360,7 +366,7 @@ public class ProgrammingExerciseGradingService {
              * receive the full 20 points, if the points are not capped before the penalty is subtracted. With the implemented order in place
              * successfulTestPoints will be capped to 20 points first, then the penalty is subtracted resulting in 10 points.
              */
-            double maxPoints = programmingExercise.getMaxScore() + Optional.ofNullable(programmingExercise.getBonusPoints()).orElse(0.0);
+            double maxPoints = maxScoreRespectingZeroPointExercises + Optional.ofNullable(programmingExercise.getBonusPoints()).orElse(0.0);
             if (successfulTestPoints > maxPoints) {
                 successfulTestPoints = maxPoints;
             }
@@ -376,7 +382,7 @@ public class ProgrammingExerciseGradingService {
             }
 
             // The score is calculated as a percentage of the maximum points
-            long score = (long) (successfulTestPoints / programmingExercise.getMaxScore() * 100.0);
+            long score = (long) (successfulTestPoints / maxScoreRespectingZeroPointExercises * 100.0);
 
             result.setScore(score);
         }
@@ -429,12 +435,27 @@ public class ProgrammingExerciseGradingService {
          * points due to static code analysis issues.
          */
         final var maxExercisePenaltyPoints = (double) Optional.ofNullable(programmingExercise.getMaxStaticCodeAnalysisPenalty()).orElse(100) / 100.0
-                * programmingExercise.getMaxScore();
+                * getMaxScoreRespectingZeroPointExercises(programmingExercise);
         if (codeAnalysisPenaltyPoints > maxExercisePenaltyPoints) {
             codeAnalysisPenaltyPoints = maxExercisePenaltyPoints;
         }
 
         return codeAnalysisPenaltyPoints;
+    }
+
+    /**
+     * Returns the maximum amount of regular points for the given exercise or a replacement point amount if the exercise has zero points (neither regular nor bonus points).
+     * <p>
+     * <b>Must only be used for the exercise-local score calculation and display messages and never for the actual score of a student in a course.</b>  
+     * @param programmingExercise the exercise to the the maxScore for
+     * @return {@link Exercise#getMaxScore()} or {@link #PLACEHOLDER_POINTS_FOR_ZERO_POINT_EXERCISES}
+     */
+    private static double getMaxScoreRespectingZeroPointExercises(ProgrammingExercise programmingExercise) {
+        boolean hasNormalPoints = Objects.requireNonNullElse(programmingExercise.getMaxScore(), 0.0) > 0.0;
+        boolean hasBonusPoints = Objects.requireNonNullElse(programmingExercise.getMaxScore(), 0.0) > 0.0;
+        if (hasNormalPoints || hasBonusPoints)
+            return programmingExercise.getMaxScore();
+        return PLACEHOLDER_POINTS_FOR_ZERO_POINT_EXERCISES;
     }
 
     /**
