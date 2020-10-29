66ba13d6af835fe09d62e06a60b13ec9dabbf7d3
Christian Femers <christian.femers@tum.de>
2020-10-29 18:24:29 +0100

    Unify error message handling for Bamboo and Jenkins, remove stack traces

--- src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooService.java
@@ -1,6 +1,8 @@
 package de.tum.in.www1.artemis.service.connectors.bamboo;
 
-import static de.tum.in.www1.artemis.config.Constants.*;
+import static de.tum.in.www1.artemis.config.Constants.ASSIGNMENT_REPO_NAME;
+import static de.tum.in.www1.artemis.config.Constants.SETUP_COMMIT_MESSAGE;
+import static de.tum.in.www1.artemis.config.Constants.TEST_REPO_NAME;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -706,12 +708,12 @@ public class BambooService implements ContinuousIntegrationService {
 
                 // 1) add feedback for failed test cases
                 for (final var failedTest : job.getFailedTests()) {
-                    result.addFeedback(feedbackService.createFeedbackFromTestCase(failedTest.getName(), failedTest.getErrors(), false, programmingLanguage, true));
+                    result.addFeedback(feedbackService.createFeedbackFromTestCase(failedTest.getName(), failedTest.getErrors(), false, programmingLanguage));
                 }
 
                 // 2) add feedback for passed test cases
                 for (final var successfulTest : job.getSuccessfulTests()) {
-                    result.addFeedback(feedbackService.createFeedbackFromTestCase(successfulTest.getName(), successfulTest.getErrors(), true, programmingLanguage, true));
+                    result.addFeedback(feedbackService.createFeedbackFromTestCase(successfulTest.getName(), successfulTest.getErrors(), true, programmingLanguage));
                 }
 
                 // 3) process static code analysis feedback
