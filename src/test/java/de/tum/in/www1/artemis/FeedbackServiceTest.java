57d8d6ffaec45e4934b9295ca20ebe0bfd300124
Christian Femers <christian.femers@tum.de>
2020-10-29 18:25:57 +0100

    Add FeedbackServiceTest of #2224 again including more test cases

---
+++ src/test/java/de/tum/in/www1/artemis/FeedbackServiceTest.java
@@ -0,0 +1,151 @@
+package de.tum.in.www1.artemis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import de.tum.in.www1.artemis.domain.enumeration.ProgrammingLanguage;
+import de.tum.in.www1.artemis.service.FeedbackService;
+
+class FeedbackServiceTest {
+
+    private FeedbackService f = new FeedbackService(null);
+
+    @Test
+    void createFeedbackFromTestCaseCombineMultiple() {
+        String msg1 = """
+                java.lang.AssertionError: expected:
+                    4
+                but was:
+                    5""";
+        String msg2 = """
+                org.opentest4j.AssertionFailedError: Message2
+                with additions""";
+        String msg3 = """
+                org.opentest4j.AssertionFailedError:\s
+                message is only here on the next line:
+                  expected:
+                    []
+                  to contain:
+                    ["Java"]""";
+
+        assertThat("""
+                expected:
+                    4
+                but was:
+                    5
+
+                Message2
+                with additions
+
+                message is only here on the next line:
+                  expected:
+                    []
+                  to contain:
+                    ["Java"]""").isEqualTo(f.createFeedbackFromTestCase("test1", List.of(msg1, msg2, msg3), false, ProgrammingLanguage.JAVA).getDetailText());
+
+    }
+
+    @Test
+    void createFeedbackFromTestCaseMatchMultiple() {
+        String msgMatchMultiple = """
+                java.lang.AssertionError: expected:
+                    4
+                but was:
+                    5
+                org.opentest4j.AssertionFailedError: expected:
+                    something else""";
+        assertThat("""
+                expected:
+                    4
+                but was:
+                    5
+                expected:
+                    something else""").isEqualTo(f.createFeedbackFromTestCase("test2", List.of(msgMatchMultiple), false, ProgrammingLanguage.KOTLIN).getDetailText());
+
+    }
+
+    @Test
+    void createFeedbackFromTestCaseUnchanged() {
+        String msgUnchanged = "Should not be changed";
+        assertThat(msgUnchanged).isEqualTo(f.createFeedbackFromTestCase("test3", List.of(msgUnchanged), false, ProgrammingLanguage.JAVA).getDetailText());
+
+    }
+
+    @Test
+    void createFeedbackFromTestCaseWithStackTrace() {
+        String msgWithStackTrace = """
+                org.opentest4j.AssertionFailedError: the expected method 'getDates' of the class 'Context' with no parameters was not found or is named wrongly.
+                \tat test.MethodTest.checkMethods(MethodTest.java:129)
+                \tat test.MethodTest.testMethods(MethodTest.java:72)
+                \tat test.MethodTest.lambda$0(MethodTest.java:52)""";
+        assertThat("the expected method 'getDates' of the class 'Context' with no parameters was not found or is named wrongly.")
+                .isEqualTo(f.createFeedbackFromTestCase("test1", List.of(msgWithStackTrace), false, ProgrammingLanguage.JAVA).getDetailText());
+    }
+
+    @Test
+    void createFeedbackFromTestCaseWithStackTraceAndCause() {
+        String msgWithStackTrace = """
+                org.springframework.orm.jpa.JpaSystemException: org.springframework.orm.jpa.JpaSystemException: null index column for collection: de.tum.in.www1.artemis.domain.exam.Exam.exerciseGroups
+                \tat org.springframework.orm.jpa.vendor.HibernateJpaDialect.convertHibernateAccessException(HibernateJpaDialect.java:353)
+                \tat org.springframework.orm.jpa.vendor.HibernateJpaDialect.translateExceptionIfPossible(HibernateJpaDialect.java:255)
+                \tat org.springframework.orm.jpa.AbstractEntityManagerFactoryBean.translateExceptionIfPossible(AbstractEntityManagerFactoryBean.java:528)
+                \tat org.springframework.dao.support.ChainedPersistenceExceptionTranslator.translateExceptionIfPossible(ChainedPersistenceExceptionTranslator.java:61)
+                \tat org.springframework.dao.support.DataAccessUtils.translateIfNecessary(DataAccessUtils.java:242)
+                Caused by: org.hibernate.HibernateException: null index column for collection: de.tum.in.www1.artemis.domain.exam.Exam.exerciseGroups
+                \tat org.hibernate.persister.collection.AbstractCollectionPersister.readIndex(AbstractCollectionPersister.java:874)
+                \tat org.hibernate.collection.internal.PersistentList.readFrom(PersistentList.java:401)
+                \tat org.hibernate.loader.plan.exec.process.internal.CollectionReferenceInitializerImpl.finishUpRow(CollectionReferenceInitializerImpl.java:76)
+                \tat org.hibernate.loader.plan.exec.process.internal.AbstractRowReader.readRow(AbstractRowReader.java:125)
+                \tat org.hibernate.loader.plan.exec.process.internal.ResultSetProcessorImpl.extractRows(ResultSetProcessorImpl.java:157)
+                \tat org.hibernate.loader.plan.exec.process.internal.ResultSetProcessorImpl.extractResults(ResultSetProcessorImpl.java:94)""";
+        assertThat(
+                "org.springframework.orm.jpa.JpaSystemException: org.springframework.orm.jpa.JpaSystemException: null index column for collection: de.tum.in.www1.artemis.domain.exam.Exam.exerciseGroups")
+                        .isEqualTo(f.createFeedbackFromTestCase("test1", List.of(msgWithStackTrace), false, ProgrammingLanguage.JAVA).getDetailText());
+    }
+
+    @Test
+    void createFeedbackFromTestCaseOfAssertJ() {
+        String msgWithStackTrace = """
+                org.opentest4j.AssertionFailedError:\s
+                Expecting:
+                 <"expected:
+                    4
+                but was:XXXXXXXXXXXXX
+                    5
+                expected:
+                    something else">
+                to be equal to:
+                 <"expected:
+                    4
+                but was:
+                    5
+                expected:
+                    something else">
+                but was not.
+                \tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
+                \tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:64)
+                \tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
+                \tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)
+                \tat de.tum.in.www1.artemis.FeedbackServiceTest.createFeedbackFromTestCaseMatchMultiple(FeedbackServiceTest.java:66)
+                """;
+        assertThat("""
+                Expecting:
+                 <"expected:
+                    4
+                but was:XXXXXXXXXXXXX
+                    5
+                expected:
+                    something else">
+                to be equal to:
+                 <"expected:
+                    4
+                but was:
+                    5
+                expected:
+                    something else">
+                but was not.""").isEqualTo(f.createFeedbackFromTestCase("test1", List.of(msgWithStackTrace), false, ProgrammingLanguage.JAVA).getDetailText());
+    }
+}
