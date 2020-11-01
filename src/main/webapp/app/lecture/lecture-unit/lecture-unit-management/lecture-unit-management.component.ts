5d06e23b821d78c8a8bdf06123573296cfbb8908
Stefan Waldhauser <hello@stefanwaldhauser.me>
2020-11-02 00:31:56 +0100

    YouTube API fix

--- src/main/webapp/app/lecture/lecture-unit/lecture-unit-management/lecture-unit-management.component.ts
+++ src/main/webapp/app/lecture/lecture-unit/lecture-unit-management/lecture-unit-management.component.ts
@@ -53,7 +53,9 @@ export class LectureUnitManagementComponent implements OnInit, OnDestroy {
         const tag = document.createElement('script');
         tag.src = 'https://www.youtube.com/iframe_api';
         const firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode!.insertBefore(tag, firstScriptTag);
+        if (firstScriptTag) {
+            firstScriptTag.parentNode!.insertBefore(tag, firstScriptTag);
+        }
     }
 
     ngOnInit(): void {
--- src/main/webapp/app/overview/course-lectures/course-lecture-details.component.ts
+++ src/main/webapp/app/overview/course-lectures/course-lecture-details.component.ts
@@ -56,7 +56,9 @@ export class CourseLectureDetailsComponent implements OnInit, OnDestroy {
         const tag = document.createElement('script');
         tag.src = 'https://www.youtube.com/iframe_api';
         const firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode!.insertBefore(tag, firstScriptTag);
+        if (firstScriptTag) {
+            firstScriptTag.parentNode!.insertBefore(tag, firstScriptTag);
+        }
     }
 
     ngOnInit(): void {
