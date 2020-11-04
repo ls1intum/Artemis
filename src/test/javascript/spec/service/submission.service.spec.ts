a9f80111b3a9eb93b323f6360cf759331826e61d
DominikJonasFuchs <d0minik.fuchs@web.de>
2020-11-04 12:01:16 +0100

    new tests, getParticipiation

--- src/test/javascript/spec/service/submission.service.spec.ts
+++ src/test/javascript/spec/service/submission.service.spec.ts
@@ -1,20 +1,20 @@
-import * as moment from 'moment';
-import * as chai from 'chai';
 import { SubmissionService } from 'app/exercises/shared/submission/submission.service';
 import { getTestBed, TestBed } from '@angular/core/testing';
-import { Result } from 'app/entities/result.model';
 import { expect } from '../helpers/jest.fix';
+import { map, take } from 'rxjs/operators';
 import { ArtemisTestModule } from '../test.module';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { MockSyncStorage } from '../helpers/mocks/service/mock-sync-storage.service';
 import { TranslateService } from '@ngx-translate/core';
 import { MockTranslateService } from '../helpers/mocks/service/mock-translate.service';
+import { Submission, SubmissionExerciseType } from 'app/entities/submission.model';
 
 describe('Submission Service', () => {
     let injector: TestBed;
     let service: SubmissionService;
     let httpMock: HttpTestingController;
+    let elemDefault: Submission;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -28,6 +28,9 @@ describe('Submission Service', () => {
         injector = getTestBed();
         service = injector.get(SubmissionService);
         httpMock = injector.get(HttpTestingController);
+
+        // @ts-ignore
+        elemDefault = new Submission(SubmissionExerciseType.TEXT);
     });
 
     it('should delete an existing submission', async () => {
@@ -37,15 +40,32 @@ describe('Submission Service', () => {
     });
 
     it('should find all submissions of a given participation', async () => {
-        service.findAllSubmissionsOfParticipation(187).subscribe((resp) => expect(resp.ok));
+        const returnedFromService = Object.assign({}, elemDefault);
+        service
+            .findAllSubmissionsOfParticipation(187)
+            .pipe(
+                take(1),
+                map((resp) => resp.body),
+            )
+            .subscribe((body) => expect(body).toMatchObject({ body: elemDefault }));
         const req = httpMock.expectOne({ method: 'GET' });
-        req.flush({ status: 200 });
+        req.flush(JSON.stringify([returnedFromService]));
+        // httpMock.verify();
     });
 
     it('should get test run submission for a given exercise', async () => {
-        service.getTestRunSubmissionsForExercise(123).subscribe((resp) => expect(resp.ok));
+        // service.getTestRunSubmissionsForExercise(123).subscribe(val => console.log(val));
+
+        const returnedFromService = Object.assign({}, elemDefault);
+        service
+            .getTestRunSubmissionsForExercise(123)
+            .pipe(
+                take(1),
+                map((resp) => resp.body),
+            )
+            .subscribe((body) => expect(body).toMatchObject({ body: elemDefault }));
         const req = httpMock.expectOne({ method: 'GET' });
-        req.flush({ status: 200 });
+        req.flush(returnedFromService);
     });
 
     afterEach(() => {
