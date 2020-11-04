5808893bab111fe03650d58743cc98338e996ed0
René Lalla <r.lalla@gmx.de>
2020-11-04 09:17:11 +0100

    reduce to only one bamboo script

--- src/main/resources/templates/bamboo/swift/regularRuns/1_build_and_test_the_code.sh
+++ src/main/resources/templates/bamboo/swift/regularRuns/1_build_and_test_the_code.sh
@@ -6,3 +6,7 @@ swift build
 
 echo "---------- swift test ----------"
 swift test
+
+# The used docker container is calling 'swift build' which creates files as root (e.g. tests.xml),
+# so we need to allow bamboo to access these files
+chmod -R 777 ${bamboo.working.directory}
--- src/main/resources/templates/bamboo/swift/sequentialRuns/2_give_bamboo_access_rights.sh
+++
@@ -1,3 +0,0 @@
-# As swift build creates files as root, we need to allow bamboo
-# to access these files
-chmod -R 777 ${bamboo.working.directory}
