595bb7c53cbb1a4e7975fc699b5ee072b64aead7
René Lalla <r.lalla@gmx.de>
2020-11-06 07:58:46 +0100

    adapt swift package name regex

--- src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.component.ts
@@ -57,9 +57,10 @@ export class ProgrammingExerciseUpdateComponent implements OnInit {
     // with the restriction to a-z,A-Z,_ as "Java letter" and 0-9 as digits due to JavaScript/Browser Unicode character class limitations
     packageNamePatternForJavaKotlin =
         '^(?!.*(?:\\.|^)(?:abstract|continue|for|new|switch|assert|default|if|package|synchronized|boolean|do|goto|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|_|true|false|null)(?:\\.|$))[A-Z_a-z][0-9A-Z_a-z]*(?:\\.[A-Z_a-z][0-9A-Z_a-z]*)*$';
-    // Swift package names should not include any separators like '.'
+    // Swift package name Regex derived from (https://docs.swift.org/swift-book/ReferenceManual/LexicalStructure.html#ID412),
+    // with the restriction to a-z,A-Z as "Swift letter" and 0-9 as digits where no separators are allowed
     packageNamePatternForSwift =
-        '^(?!.*(?:\\.|^)(?:abstract|continue|for|new|switch|assert|default|if|package|synchronized|boolean|do|goto|private|this|break|double|implements|protected|throw|byte|else|import|public|throws|case|enum|instanceof|return|transient|catch|extends|int|short|try|char|final|interface|static|void|class|finally|long|strictfp|volatile|const|float|native|super|while|_|true|false|null)(?:\\.|$))[A-Za-z][0-9A-Za-z]*$';
+        '^(?!.*(?:associatedtype|class|deinit|enum|extension|fileprivate|func|import|init|inout|internal|let|open|operator|private|protocol|public|rethrows|static|struct|subscript|typealias|var|break|case|continue|default|defer|do|else|fallthrough|for|guard|if|in|repeat|return|switch|where|while|as|Any|catch|false|is|nil|super|self|Self|throw|throws|true|try|_)(?:\\.|$))[A-Za-z][0-9A-Za-z]*$';
     packageNamePattern = '';
 
     shortNamePattern = '^[a-zA-Z][a-zA-Z0-9]*'; // must start with a letter and cannot contain special characters
