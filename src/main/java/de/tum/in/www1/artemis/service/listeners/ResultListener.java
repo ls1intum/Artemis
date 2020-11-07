e83ecc333e75e21883f205e55749d04a9d285eb0
Balazs Czopf <balazs.czopf@tum.de>
2020-11-07 12:24:35 +0100

    try repo access in listener

--- src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
+++ src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import de.tum.in.www1.artemis.domain.Result;
+import de.tum.in.www1.artemis.domain.enumeration.AchievementType;
+import de.tum.in.www1.artemis.repository.AchievementRepository;
 import de.tum.in.www1.artemis.service.AchievementService;
 
 @Component
@@ -16,16 +18,23 @@ public class ResultListener {
 
     private static AchievementService achievementService;
 
+    private static AchievementRepository achievementRepository;
+
     private final Logger log = LoggerFactory.getLogger(ResultListener.class);
 
     @Autowired
-    public void setAchievementService(AchievementService achievementService) {
+    public void init(AchievementService achievementService, AchievementRepository achievementRepository) {
         this.achievementService = achievementService;
+        this.achievementRepository = achievementRepository;
     }
 
     @PreUpdate
     @PrePersist
     public void checkForAchievements(Result result) {
+        var course = result.getParticipation().getExercise().getCourseViaExerciseGroupOrCourseMember();
+
+        var pointBasedAchievements = achievementRepository.findAllForRewardedTypeInCourse(course.getId(), AchievementType.POINT);
+        log.debug("achievements found in resultListener : {}", pointBasedAchievements.size());
         log.debug("ResultListener was triggered with result : {}", result);
         if (result.getScore() != null && result.isRated() != null && result.isRated()) {
             achievementService.checkForAchievements(result);
