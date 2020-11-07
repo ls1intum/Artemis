584c7910fb2d0e2a4dddf2c657f0a60d2d4b54bc
DominikJonasFuchs <d0minik.fuchs@web.de>
2020-11-07 16:42:56 +0100

    reworked the tests for better attribute check

--- src/test/javascript/spec/service/submission.service.spec.ts
+++ src/test/javascript/spec/service/submission.service.spec.ts
@@ -1,6 +1,6 @@
 import { SubmissionService } from 'app/exercises/shared/submission/submission.service';
 import { getTestBed, TestBed } from '@angular/core/testing';
-import { expect } from '../helpers/jest.fix';
+import * as chai from 'chai';
 import { take } from 'rxjs/operators';
 import { ArtemisTestModule } from '../test.module';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
@@ -12,11 +12,15 @@ import { TextSubmission } from 'app/entities/text-submission.model';
 import { Result } from 'app/entities/result.model';
 import { Feedback } from 'app/entities/feedback.model';
 import { SERVER_API_URL } from 'app/app.constants';
+import { HttpResponse } from '@angular/common/http';
+import { Submission } from 'app/entities/submission.model';
+const expect = chai.expect;
 
 describe('Submission Service', () => {
     let injector: TestBed;
     let service: SubmissionService;
     let httpMock: HttpTestingController;
+    let expectedResult: any;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -30,16 +34,18 @@ describe('Submission Service', () => {
         injector = getTestBed();
         service = injector.get(SubmissionService);
         httpMock = injector.get(HttpTestingController);
+        expectedResult = {} as HttpResponse<Submission[]>;
     });
 
     it('should delete an existing submission', async () => {
-        service.delete(187).subscribe((resp) => expect(resp.ok));
+        service.delete(187).subscribe((resp) => (expectedResult = resp.ok));
         const req = httpMock.expectOne({ method: 'DELETE' });
         req.flush({ status: 200 });
+        expect(expectedResult).to.be.true;
     });
 
     it('should find all submissions of a given participation', async () => {
-        const participationId = 187;
+        const participationId = 1;
         const submission = ({
             id: 1,
             submitted: true,
@@ -61,17 +67,19 @@ describe('Submission Service', () => {
                 credits: 1,
             } as Feedback,
         ];
-        const returnedFromService = Object.assign({}, [submission]);
+        const returnedFromService = [...[submission]];
+        const expected = [...[submission]];
         service
-            .findAllSubmissionsOfParticipation(187)
+            .findAllSubmissionsOfParticipation(participationId)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).toMatchObject({ body: [submission] }));
+            .subscribe((resp) => (expectedResult = resp));
         const req = httpMock.expectOne({ url: `${SERVER_API_URL}api/participations/${participationId}/submissions`, method: 'GET' });
-        req.flush(JSON.stringify([returnedFromService]));
+        req.flush(returnedFromService);
+        expect(expectedResult.body).to.deep.equal(expected);
     });
 
     it('should get test run submission for a given exercise', async () => {
-        const exerciseId = 187;
+        const exerciseId = 1;
         const submission = ({
             id: 1,
             submitted: true,
@@ -93,13 +101,15 @@ describe('Submission Service', () => {
                 credits: 1,
             } as Feedback,
         ];
-        const returnedFromService = Object.assign({}, [submission]);
+        const returnedFromService = [...[submission]];
+        const expected = [...[submission]];
         service
             .getTestRunSubmissionsForExercise(exerciseId)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).toMatchObject({ body: [submission] }));
+            .subscribe((resp) => (expectedResult = resp));
         const req = httpMock.expectOne({ url: `api/exercises/${exerciseId}/test-run-submissions`, method: 'GET' });
-        req.flush(JSON.stringify(returnedFromService));
+        req.flush(returnedFromService);
+        expect(expectedResult.body).to.deep.equal(expected);
     });
 
     afterEach(() => {
