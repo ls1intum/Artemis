713e5cb75d3fb24099843110752d9cb922002f3c
Balazs Czopf <balazs.czopf@tum.de>
2020-11-07 14:22:33 +0100

    remove transactional

--- src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
+++ src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
@@ -5,7 +5,6 @@ import javax.persistence.PreUpdate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.Result;
 import de.tum.in.www1.artemis.service.AchievementService;
@@ -22,7 +21,6 @@ public class ResultListener {
 
     @PreUpdate
     @PrePersist
-    @Transactional
     public void checkForAchievements(Result result) {
         if (result.getScore() != null && result.isRated() != null && result.isRated()) {
             achievementService.checkForAchievements(result);
