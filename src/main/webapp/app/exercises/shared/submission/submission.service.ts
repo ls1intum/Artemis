499ffb2a9fae97c3e23295b02019f757b6f4269b
DominikJonasFuchs <d0minik.fuchs@web.de>
2020-11-07 17:49:39 +0100

    removed not needed import

--- src/main/webapp/app/exercises/shared/submission/submission.service.ts
+++ src/main/webapp/app/exercises/shared/submission/submission.service.ts
@@ -5,7 +5,6 @@ import { SERVER_API_URL } from 'app/app.constants';
 import * as moment from 'moment';
 import { createRequestOption } from 'app/shared/util/request-util';
 import { Result } from 'app/entities/result.model';
-import { Participation } from 'app/entities/participation/participation.model';
 import { Submission } from 'app/entities/submission.model';
 import { filter, map, tap } from 'rxjs/operators';
 import { TextSubmission } from 'app/entities/text-submission.model';
