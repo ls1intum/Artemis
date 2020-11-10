fcf8f6be2410172d1b72fcea6bc898444e1c14d3
Balazs Czopf <balazs.czopf@tum.de>
2020-11-10 08:23:50 +0100

    another try

--- src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
+++ src/main/java/de/tum/in/www1/artemis/service/listeners/ResultListener.java
@@ -3,27 +3,37 @@ package de.tum.in.www1.artemis.service.listeners;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import de.tum.in.www1.artemis.domain.Result;
 import de.tum.in.www1.artemis.service.AchievementService;
 
-@Component
+@Configurable
 public class ResultListener {
 
-    private static AchievementService achievementService;
+    private @Nullable ObjectFactory<AchievementService> achievementService;
 
     @Autowired
-    public void init(AchievementService achievementService) {
+    public void setAchievementService(ObjectFactory<AchievementService> achievementService) {
+        Assert.notNull(achievementService, "AuditingHandler must not be null!");
         this.achievementService = achievementService;
     }
 
     @PreUpdate
     @PrePersist
     public void checkForAchievements(Result result) {
-        if (result.getScore() != null && result.isRated() != null && result.isRated()) {
-            achievementService.checkForAchievements(result);
+        Assert.notNull(result, "Entity must not be null!");
+
+        if (achievementService != null) {
+            if (achievementService.getObject() != null) {
+                if (result.getScore() != null && result.isRated() != null && result.isRated()) {
+                    achievementService.getObject().checkForAchievements(result);
+                }
+            }
         }
     }
 }
