d8f0c42b0806f740317b5bed34d4bf3f75829f51
Stephan Krusche <s.krusche@tum.de>
2020-11-10 21:59:09 +0100

    Fix #2344 the cause of an exception in the Server Administration -> Audits page

--- src/main/java/de/tum/in/www1/artemis/domain/PersistentAuditEvent.java
+++ src/main/java/de/tum/in/www1/artemis/domain/PersistentAuditEvent.java
@@ -1,16 +1,13 @@
 package de.tum.in.www1.artemis.domain;
 
+import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * Persist AuditEvent managed by the Spring Boot actuator.
@@ -19,9 +16,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
  */
 @Entity
 @Table(name = "jhi_persistent_audit_event")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class PersistentAuditEvent extends DomainObject {
+public class PersistentAuditEvent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
 
     @NotNull
     @Column(nullable = false)
@@ -38,6 +40,14 @@ public class PersistentAuditEvent extends DomainObject {
     @Column(name = "value")
     @CollectionTable(name = "jhi_persistent_audit_evt_data", joinColumns = @JoinColumn(name = "event_id"))
     private Map<String, String> data = new HashMap<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getPrincipal() {
         return principal;
@@ -69,6 +79,27 @@ public class PersistentAuditEvent extends DomainObject {
 
     public void setData(Map<String, String> data) {
         this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PersistentAuditEvent persistentAuditEvent = (PersistentAuditEvent) o;
+        if (persistentAuditEvent.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), persistentAuditEvent.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 
     @Override
