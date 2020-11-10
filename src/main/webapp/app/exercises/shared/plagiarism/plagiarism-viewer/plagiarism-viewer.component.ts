c7fc7442a1ee96c4aace4c31c427431944a0b0d4
Philipp Bauch <ga53xex@mytum.de>
2020-11-10 14:28:13 +0100

    Restructure page layout of plagiarism inspector

---
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism-viewer/plagiarism-viewer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-plagiarism-viewer',
+    styleUrls: ['./plagiarism-viewer.component.scss'],
+    templateUrl: './plagiarism-viewer.component.html',
+})
+export class PlagiarismViewerComponent {}
--- src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
+++ src/main/webapp/app/exercises/shared/plagiarism/plagiarism.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { PlagiarismInspectorComponent } from 'app/exercises/shared/plagiarism/plagiarism-inspector/plagiarism-inspector.component';
 import { PlagiarismHeaderComponent } from 'app/exercises/shared/plagiarism/plagiarism-header/plagiarism-header.component';
+import { PlagiarismInspectorComponent } from 'app/exercises/shared/plagiarism/plagiarism-inspector/plagiarism-inspector.component';
 import { PlagiarismSplitViewComponent, SplitPaneDirective } from 'app/exercises/shared/plagiarism/plagiarism-split-view/plagiarism-split-view.component';
+import { PlagiarismViewerComponent } from 'app/exercises/shared/plagiarism/plagiarism-viewer/plagiarism-viewer.component';
 import { ArtemisModelingEditorModule } from 'app/exercises/modeling/shared/modeling-editor.module';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     imports: [ArtemisSharedModule, ArtemisModelingEditorModule, RouterModule, TranslateModule],
-    declarations: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent, SplitPaneDirective],
+    declarations: [PlagiarismHeaderComponent, PlagiarismInspectorComponent, PlagiarismSplitViewComponent, PlagiarismViewerComponent, SplitPaneDirective],
     exports: [PlagiarismInspectorComponent],
 })
 export class ArtemisPlagiarismModule {}
