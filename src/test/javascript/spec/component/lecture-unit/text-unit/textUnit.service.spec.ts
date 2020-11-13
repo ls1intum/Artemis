5d106299255a44761101990ffd15e6e06e2185f5
Stefan Waldhauser <hello@stefanwaldhauser.me>
2020-11-13 13:09:24 +0100

    Created Test for Text-Unit-Service

---
+++ src/test/javascript/spec/component/lecture-unit/text-unit/textUnit.service.spec.ts
@@ -0,0 +1,87 @@
+import { HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { LectureUnitService } from 'app/lecture/lecture-unit/lecture-unit-management/lectureUnit.service';
+import { MockProvider } from 'ng-mocks';
+import { TextUnitService } from 'app/lecture/lecture-unit/lecture-unit-management/textUnit.service';
+import { TextUnit } from 'app/entities/lecture-unit/textUnit.model';
+import { take } from 'rxjs/operators';
+import { LectureUnit } from 'app/entities/lecture-unit/lectureUnit.model';
+import * as moment from 'moment';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('TextUnitService', () => {
+    let injector: TestBed;
+    let service: TextUnitService;
+    let httpMock: HttpTestingController;
+    let elemDefault: TextUnit;
+    let expectedResult: any;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                MockProvider(LectureUnitService, {
+                    convertDateFromServerResponse<T extends LectureUnit>(res: HttpResponse<T>): HttpResponse<T> {
+                        if (res.body) {
+                            res.body.releaseDate = res.body.releaseDate ? moment(res.body.releaseDate) : undefined;
+                        }
+                        return res;
+                    },
+                }),
+            ],
+        });
+        expectedResult = {} as HttpResponse<TextUnit>;
+        injector = getTestBed();
+        service = injector.get(TextUnitService);
+        httpMock = injector.get(HttpTestingController);
+
+        elemDefault = new TextUnit();
+        elemDefault.id = 0;
+        elemDefault.content = 'Lorem Ipsum';
+        elemDefault.releaseDate = undefined;
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should find a TextUnit', async () => {
+        const returnedFromService = { ...elemDefault };
+        service
+            .findById(1, 1)
+            .pipe(take(1))
+            .subscribe((resp) => (expectedResult = resp));
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush(returnedFromService);
+        expect(expectedResult.body).to.deep.equal(elemDefault);
+    });
+
+    it('should create a TextUnit', async () => {
+        const returnedFromService = { ...elemDefault, id: 0 };
+        const expected = { ...returnedFromService };
+        service
+            .create(new TextUnit(), 1)
+            .pipe(take(1))
+            .subscribe((resp) => (expectedResult = resp));
+        const req = httpMock.expectOne({ method: 'POST' });
+        req.flush(returnedFromService);
+        expect(expectedResult.body).to.deep.equal(expected);
+    });
+
+    it('should update a TextUnit', async () => {
+        const returnedFromService = { ...elemDefault, name: 'Test' };
+        const expected = { ...returnedFromService };
+        service
+            .update(expected, 1)
+            .pipe(take(1))
+            .subscribe((resp) => (expectedResult = resp));
+        const req = httpMock.expectOne({ method: 'PUT' });
+        req.flush(returnedFromService);
+        expect(expectedResult.body).to.deep.equal(expected);
+    });
+});
