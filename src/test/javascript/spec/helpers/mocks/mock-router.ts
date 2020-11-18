4f6ce04640dc3b31aaca607424e21be67ffc870a
Stefan Waldhauser <hello@stefanwaldhauser.me>
2020-11-19 00:54:02 +0100

    Wrote more frontend tests

--- src/test/javascript/spec/helpers/mocks/mock-router.ts
+++ src/test/javascript/spec/helpers/mocks/mock-router.ts
@@ -1,6 +1,9 @@
+import { Observable } from 'rxjs';
+
 export class MockRouter {
     url: string;
     setUrl = (url: string) => (this.url = url);
     navigateByUrl = (url: string) => true;
     navigate = (commands: any[]) => true;
+    events: Observable<Event> = Observable.empty();
 }
