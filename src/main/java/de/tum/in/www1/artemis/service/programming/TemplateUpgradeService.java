56de967b0e015ae91e0f7099e488f419178fe777
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-11-20 18:02:30 +0100

    Add documentation

--- src/main/java/de/tum/in/www1/artemis/service/programming/TemplateUpgradeService.java
+++ src/main/java/de/tum/in/www1/artemis/service/programming/TemplateUpgradeService.java
@@ -4,5 +4,13 @@ import de.tum.in.www1.artemis.domain.ProgrammingExercise;
 
 public abstract class TemplateUpgradeService {
 
+    /**
+     * Upgrades the template files provided by Artemis for the given exercise.
+     * Usually this is done by checking out the repositories associated with the template/solution
+     * participation and the test repository. The latest Artemis templates serve as a reference.
+     * The specific strategy for the manipulation of files is defined in programming language specific services.
+     *
+     * @param exercise Exercise for which the template files should be upgraded
+     */
     public abstract void upgradeTemplate(ProgrammingExercise exercise);
 }
