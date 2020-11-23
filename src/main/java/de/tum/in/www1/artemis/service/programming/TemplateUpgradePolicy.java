6f0096fab51843a1e709c3af28f2b5f65c58fe80
Stefan Klöss-Schuster <16407766+kloessst@users.noreply.github.com>
2020-11-23 22:45:34 +0100

    Add an option to update the test repository when importing programming exercises (#2414)

---
+++ src/main/java/de/tum/in/www1/artemis/service/programming/TemplateUpgradePolicy.java
@@ -0,0 +1,34 @@
+package de.tum.in.www1.artemis.service.programming;
+
+import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.artemis.domain.enumeration.ProgrammingLanguage;
+
+/**
+ * The policy for choosing the appropriate service for upgrading of template files
+ */
+@Service
+public class TemplateUpgradePolicy {
+
+    private final JavaTemplateUpgradeService javaRepositoryUpgradeService;
+
+    private final DefaultTemplateUpgradeService defaultRepositoryUpgradeService;
+
+    public TemplateUpgradePolicy(JavaTemplateUpgradeService javaRepositoryUpgradeService, DefaultTemplateUpgradeService defaultRepositoryUpgradeService) {
+        this.javaRepositoryUpgradeService = javaRepositoryUpgradeService;
+        this.defaultRepositoryUpgradeService = defaultRepositoryUpgradeService;
+    }
+
+    /**
+     * Returns a programming language specific service which upgrades the template files in repositories.
+     *
+     * @param programmingLanguage The programming language of the programming exercise to be upgraded
+     * @return The upgrade service for the programming language
+     */
+    public TemplateUpgradeService getUpgradeService(ProgrammingLanguage programmingLanguage) {
+        return switch (programmingLanguage) {
+            case JAVA -> javaRepositoryUpgradeService;
+            case KOTLIN, PYTHON, C, HASKELL, VHDL, ASSEMBLER, SWIFT -> defaultRepositoryUpgradeService;
+        };
+    }
+}
