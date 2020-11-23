e060a61a4235cd277b8f201ce165dc9c1ca4b54c
Dominik Fuchs <d0minik.fuchs@web.de>
2020-11-23 11:00:01 +0100

    start chart method

--- src/main/webapp/app/admin/statistics/statistics.component.ts
+++ src/main/webapp/app/admin/statistics/statistics.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { StatisticsService } from 'app/admin/statistics/statistics.service';
 import { SPAN_PATTERN } from 'app/app.constants';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { DataSet } from 'app/exercises/quiz/manage/statistics/quiz-statistic/quiz-statistic.component';
 
 @Component({
     selector: 'jhi-statistics',
     templateUrl: './statistics.component.html',
 })
 export class JhiStatisticsComponent implements OnInit, OnChanges {
-    activities: any[] = [];
     spanPattern = SPAN_PATTERN;
     userSpan = 7;
     activeUserSpan = 7;
@@ -29,24 +31,16 @@ export class JhiStatisticsComponent implements OnInit, OnChanges {
     examRegistrations = 0;
     resultFeedbacks = 0;
 
-    constructor(private service: StatisticsService) {}
+    // Histogram related properties
+    public binWidth = 4;
+    public histogramData: number[] = Array(this.binWidth).fill(0);
+    public barChartOptions: ChartOptions = {};
+    public barChartLabels: Label[] = [];
+    public barChartType: ChartType = 'bar';
+    public barChartLegend = true;
+    public barChartData: ChartDataSets[] = [];
 
-    showActivity(activity: any) {
-        let existingActivity = false;
-        for (let index = 0; index < this.activities.length; index++) {
-            if (this.activities[index].sessionId === activity.sessionId) {
-                existingActivity = true;
-                if (activity.page === 'logout') {
-                    this.activities.splice(index, 1);
-                } else {
-                    this.activities[index] = activity;
-                }
-            }
-        }
-        if (!existingActivity && activity.page !== 'logout') {
-            this.activities.push(activity);
-        }
-    }
+    constructor(private service: StatisticsService) {}
 
     ngOnInit() {
         this.onChangedUserSpan();
@@ -122,4 +116,81 @@ export class JhiStatisticsComponent implements OnInit, OnChanges {
             this.resultFeedbacks = res;
         });
     }
+
+    /*private createChart() {
+        const labels: string[] = [];
+        let i;
+        for (i = 0; i < this.histogramData.length; i++) {
+            labels[i] = `[${i},${(i + 1)}`;
+            labels[i] += i === this.histogramData.length - 1 ? ']' : ')';
+        }
+        this.barChartLabels = labels;
+
+        const component = this;
+
+        this.barChartOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                align: 'start',
+                position: 'bottom',
+            },
+            scales: {
+                yAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: this.translateService.instant('artemisApp.examScores.yAxes'),
+                        },
+                        ticks: {
+                            maxTicksLimit: 11,
+                            beginAtZero: true,
+                            precision: 0,
+                            min: 0,
+                            max: this.calculateTickMax(),
+                        } as LinearTickOptions,
+                    },
+                ],
+                xAxes: [
+                    {
+                        scaleLabel: {
+                            display: true,
+                            labelString: this.translateService.instant('artemisApp.examScores.xAxes'),
+                        },
+                    },
+                ],
+            },
+            hover: {
+                animationDuration: 0,
+            },
+            animation: {
+                duration: 1,
+                onComplete() {
+                    const chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+
+                    this.data.datasets.forEach(function (dataset: DataSet, j: number) {
+                        const meta = chartInstance.controller.getDatasetMeta(j);
+                        meta.data.forEach(function (bar: any, index: number) {
+                            const data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x, bar._model.y - 20);
+                            ctx.fillText(`(${component.roundAndPerformLocalConversion((data * 100) / component.noOfExamsFiltered, 2, 2)}%)`, bar._model.x, bar._model.y - 5);
+                        });
+                    });
+                },
+            },
+        };
+
+        this.barChartData = [
+            {
+                label: '# of students',
+                data: this.histogramData,
+                backgroundColor: 'rgba(0,0,0,0.5)',
+            },
+        ];
+    }*/
 }
