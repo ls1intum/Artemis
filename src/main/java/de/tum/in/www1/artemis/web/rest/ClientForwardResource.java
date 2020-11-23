b793a2bf6913f541c78d951258080dd57fdd7523
Jan Philip Bernius <janphilip.bernius@tum.de>
2020-11-24 00:01:11 +0100

    Style and Docs

--- src/main/java/de/tum/in/www1/artemis/web/rest/ClientForwardResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ClientForwardResource.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientForwardResource {
+
     /**
      * Forwards any unmapped paths (except those containing a period) to the client index.html
+     * @return Forward Instruction for Browser
      */
     @GetMapping(value = "/**/{path:[^\\.]*}")
     public String forward() {
