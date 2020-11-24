a61f611a0bd0489896637cd627d28637eb371f87
Stefan Waldhauser <hello@stefanwaldhauser.me>
2020-11-24 11:08:41 +0100

    Changed implementation for text units so in modern browsers the separate page does not open in a popup but rather in a new tab

--- src/main/webapp/app/overview/course-lectures/text-unit/text-unit.component.ts
+++ src/main/webapp/app/overview/course-lectures/text-unit/text-unit.component.ts
@@ -32,9 +32,7 @@ export class TextUnitComponent implements OnInit {
     openPopup($event: any) {
         $event.stopPropagation();
 
-        const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        const win = window.open('', ' ', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' + `width=${width},height=${height}`);
+        const win = window.open('about:blank', '_blank');
         win!.document.write(`<html><head><title>${this.textUnit.name}</title>`);
         win!.document.write(`<link rel="stylesheet" href="${SERVER_API_URL}public/content/github-markdown.css">`);
         win!.document.write('</head><body class="markdown-body">');
