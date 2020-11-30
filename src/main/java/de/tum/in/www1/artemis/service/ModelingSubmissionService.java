7983370872bef7783a60d22012ad41662a5da65a
Andi Turdiu <andi.turdiu@tum.de>
2020-11-30 17:01:37 +0100

    fix test

--- src/main/java/de/tum/in/www1/artemis/service/ModelingSubmissionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ModelingSubmissionService.java
@@ -219,9 +219,9 @@ public class ModelingSubmissionService extends SubmissionService {
         long exerciseId = studentParticipation.getExercise().getId();
         Result automaticResult = compassService.getAutomaticResultForSubmission(modelingSubmission.getId(), exerciseId);
         if (automaticResult != null) {
+            automaticResult.setSubmission(null);
             automaticResult.setParticipation(modelingSubmission.getParticipation());
             automaticResult = resultRepository.save(automaticResult);
-            modelingSubmission.getParticipation().addResult(automaticResult);
 
             automaticResult.setSubmission(modelingSubmission);
             modelingSubmission.setResult(automaticResult);
