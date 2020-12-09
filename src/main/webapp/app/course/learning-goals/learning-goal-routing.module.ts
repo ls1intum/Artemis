d3ccb829410d053de5f770d4b487eed4c8799180
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2020-12-09 16:51:37 +0100

    Use child routes

--- src/main/webapp/app/course/learning-goals/learning-goal-routing.module.ts
+++ src/main/webapp/app/course/learning-goals/learning-goal-routing.module.ts
@@ -4,34 +4,49 @@ import { CreateLearningGoalComponent } from 'app/course/learning-goals/create-le
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 import { LearningGoalManagementComponent } from 'app/course/learning-goals/learning-goal-management/learning-goal-management.component';
 import { EditLearningGoalComponent } from 'app/course/learning-goals/edit-learning-goal/edit-learning-goal.component';
+import { CourseResolve } from 'app/course/manage/course-management.route';
 
 const routes: Routes = [
     {
-        path: ':courseId/goals/create',
-        component: CreateLearningGoalComponent,
-        data: {
-            authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
-            pageTitle: 'artemisApp.learningGoal.createLearningGoal.title',
+        path: ':courseId',
+        resolve: {
+            course: CourseResolve,
         },
-        canActivate: [UserRouteAccessService],
-    },
-    {
-        path: ':courseId/goals/:learningGoalId/edit',
-        component: EditLearningGoalComponent,
         data: {
-            authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
-            pageTitle: 'artemisApp.learningGoal.editLearningGoal.title',
+            breadcrumbLabelVariable: 'course.title',
         },
-        canActivate: [UserRouteAccessService],
-    },
-    {
-        path: ':courseId/goal-management',
-        component: LearningGoalManagementComponent,
-        data: {
-            authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
-            pageTitle: 'artemisApp.learningGoal.manageLearningGoals.title',
-        },
-        canActivate: [UserRouteAccessService],
+        children: [
+            {
+                path: 'goals/create',
+                component: CreateLearningGoalComponent,
+                data: {
+                    authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
+                    breadcrumbLabelVariable: '',
+                    pageTitle: 'artemisApp.learningGoal.createLearningGoal.title',
+                },
+                canActivate: [UserRouteAccessService],
+            },
+            {
+                path: 'goals/:learningGoalId/edit',
+                component: EditLearningGoalComponent,
+                data: {
+                    authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
+                    breadcrumbLabelVariable: '',
+                    pageTitle: 'artemisApp.learningGoal.editLearningGoal.title',
+                },
+                canActivate: [UserRouteAccessService],
+            },
+            {
+                path: 'goal-management',
+                component: LearningGoalManagementComponent,
+                data: {
+                    authorities: ['ROLE_ADMIN', 'ROLE_INSTRUCTOR'],
+                    breadcrumbLabelVariable: '',
+                    pageTitle: 'artemisApp.learningGoal.manageLearningGoals.title',
+                },
+                canActivate: [UserRouteAccessService],
+            },
+        ],
     },
 ];
 
