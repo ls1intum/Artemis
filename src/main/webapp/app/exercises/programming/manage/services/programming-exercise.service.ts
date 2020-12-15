9fd9a36d6162c1a9c9b35e01e7540f6d79f506ff
Philipp Bauch <ga53xex@mytum.de>
2020-12-16 00:52:18 +0100

    Add possibility to generate jplag report to client

--- src/main/webapp/app/exercises/programming/manage/services/programming-exercise.service.ts
+++ src/main/webapp/app/exercises/programming/manage/services/programming-exercise.service.ts
@@ -63,6 +63,17 @@ export class ProgrammingExerciseService {
     }
 
     /**
+     * Check for plagiarism
+     * @param exerciseId of the programming exercise
+     */
+    checkPlagiarismJPlagResponse(exerciseId: number): Observable<HttpResponse<Blob>> {
+        return this.http.get(`${this.resourceUrl}/${exerciseId}/check-plagiarism-jplag-report`, {
+            observe: 'response',
+            responseType: 'blob',
+        });
+    }
+
+    /**
      * Combines all commits of the template repository to one
      * @param exerciseId of the particular programming exercise
      */
