0d64d4e4672d2ba7b89e647f4274855482568159
Ivan Chimeno <chimeno.ivan@gmail.com>
2020-12-16 11:28:28 +0100

    exchange RestTemplate in JenkinsAuthorizationInterceptor with JenkinsHttpClient

--- src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsAuthorizationInterceptor.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsAuthorizationInterceptor.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -23,6 +24,8 @@ import com.offbytwo.jenkins.model.Crumb;
 @Component
 public class JenkinsAuthorizationInterceptor implements ClientHttpRequestInterceptor {
 
+    private static final Logger log = LoggerFactory.getLogger(JenkinsAuthorizationInterceptor.class);
+
     @Value("${artemis.continuous-integration.user}")
     private String username;
 
@@ -35,38 +38,26 @@ public class JenkinsAuthorizationInterceptor implements ClientHttpRequestInterce
     @Value("${jenkins.use-crumb:#{true}}")
     private boolean useCrumb;
 
-    private JenkinsHttpClient jenkinsClient;
-
     @NotNull
     @Override
     public ClientHttpResponse intercept(HttpRequest request, @NotNull byte[] body, @NotNull ClientHttpRequestExecution execution) throws IOException {
         request.getHeaders().setBasicAuth(username, password);
-        if (!useCrumb) {
+        if (useCrumb) {
             setCrumb(request.getHeaders());
         }
         return execution.execute(request, body);
     }
 
     private void setCrumb(final HttpHeaders headersToAuthenticate) {
-        /*
-         * final var headers = new HttpHeaders(); headers.setBasicAuth(username, password); final var entity = new HttpEntity<>(headers);
-         */
         try {
             JenkinsHttpClient jenkinsHttpClient = new JenkinsHttpClient(jenkinsURL.toURI(), username, password);
             Crumb crumb = jenkinsHttpClient.get("/crumbIssuer", Crumb.class);
             if (crumb != null) {
                 headersToAuthenticate.add(crumb.getCrumbRequestField(), crumb.getCrumb());
-
             }
         }
         catch (IOException | URISyntaxException e) {
-            LoggerFactory.getLogger(JenkinsAuthorizationInterceptor.class).warn("Cannot get crumb from Jenkins crumbIssuer: ", e);
+            log.warn("Cannot get crumb from Jenkins's crumb issuer: ", e);
         }
-
-        /*
-         * final var response = new RestTemplate().exchange(jenkinsURL.toString() + "/crumbIssuer/api/json", HttpMethod.GET, entity, JsonNode.class); final var sessionId =
-         * response.getHeaders().get("Set-Cookie").get(0); headersToAuthenticate.add("Jenkins-Crumb", response.getBody().get("crumb").asText()); headersToAuthenticate.add("Cookie",
-         * sessionId);
-         */
     }
 }
