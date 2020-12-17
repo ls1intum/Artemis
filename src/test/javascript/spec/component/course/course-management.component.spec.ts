3a5943dcb98449811655acddf548717e25bd2ec0
Dominik Fuchs <d0minik.fuchs@web.de>
2020-12-17 20:07:44 +0100

    course management tests

--- src/test/javascript/spec/component/course/course-management.component.spec.ts
+++ src/test/javascript/spec/component/course/course-management.component.spec.ts
@@ -5,18 +5,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseManagementComponent } from 'app/course/manage/course-management.component';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
 import { ArtemisTestModule } from '../../test.module';
-import { NotificationService } from 'app/shared/notification/notification.service';
-import { MockNotificationService } from '../../helpers/mocks/service/mock-notification.service';
-import { AccountService } from 'app/core/auth/account.service';
-import { MockAccountService } from '../../helpers/mocks/service/mock-account.service';
-import { UserService } from 'app/core/user/user.service';
-import { MockUserService } from '../../helpers/mocks/service/mock-user.service';
-import { MockAlertService } from '../../helpers/mocks/service/mock-alert.service';
 import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
-import { MockEventManager } from '../../helpers/mocks/service/mock-event-manager.service';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
 import { LocalStorageService } from 'ngx-webstorage';
 import { SessionStorageService } from 'ngx-webstorage';
+import { of } from 'rxjs/internal/observable/of';
+import { HttpResponse } from '@angular/common/http';
+import { Course } from 'app/entities/course.model';
+import { GuidedTourService } from 'app/guided-tour/guided-tour.service';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -25,6 +21,7 @@ describe('CourseManagementComponent', () => {
     let fixture: ComponentFixture<CourseManagementComponent>;
     let component: CourseManagementComponent;
     let service: CourseManagementService;
+    let guidedTourService: GuidedTourService;
     let alertService: JhiAlertService;
     let eventManager: JhiEventManager;
 
@@ -43,6 +40,7 @@ describe('CourseManagementComponent', () => {
                 fixture = TestBed.createComponent(CourseManagementComponent);
                 component = fixture.componentInstance;
                 service = TestBed.inject(CourseManagementService);
+                guidedTourService = TestBed.inject(GuidedTourService);
                 alertService = TestBed.inject(JhiAlertService);
                 eventManager = TestBed.inject(JhiEventManager);
             });
@@ -53,7 +51,65 @@ describe('CourseManagementComponent', () => {
     });
 
     it('should initialize', async () => {
+        const courseSS = new Course();
+        courseSS.id = 187;
+        courseSS.testCourse = false;
+        courseSS.semester = 'SS19';
+        const courseWS = new Course();
+        courseWS.id = 188;
+        courseWS.testCourse = false;
+        courseWS.semester = 'WS19/20';
+
+        const resp = [courseSS, courseWS];
+        spyOn(service, 'getWithUserStats').and.returnValue(of(new HttpResponse({ body: resp })));
+        spyOn(guidedTourService, 'enableTourForCourseOverview').and.returnValue(courseSS);
+
         fixture.detectChanges();
+        expect(component).to.be.ok;
+        expect(component.showOnlyActive).to.be.true;
+        component.courses[0].semester = '';
+        component.toggleShowOnlyActive();
+        expect(component.showOnlyActive).to.be.false;
+        component.courses[0].semester = 'SS16';
+        fixture.detectChanges();
+        expect(component).to.be.ok;
+    });
+
+    it('should track id', async () => {
+        const course = new Course();
+        course.id = 187;
+        course.testCourse = false;
+        course.semester = 'SS19';
+
+        expect(component.trackId(0, course)).to.equal(187);
+    });
+
+    it('should delete course', async () => {
+        const course = new Course();
+        course.id = 187;
+        spyOn(service, 'delete').and.returnValue(of(new HttpResponse({ body: null })));
+
+        component.deleteCourse(187);
+        expect(component).to.be.ok;
+    });
+
+    it('should give the current date', async () => {
+        expect(component.today).to.be.a('Date');
+    });
+
+    it('should sort rows', async () => {
+        const courseSS = new Course();
+        courseSS.id = 187;
+        courseSS.testCourse = false;
+        courseSS.semester = 'SS19';
+        const courseWS = new Course();
+        courseWS.id = 188;
+        courseWS.testCourse = false;
+        courseWS.semester = 'WS19/20';
+        component.courses = [courseWS, courseSS];
+        component.predicate = 'id';
+        component.reverse = false;
+        component.sortRows();
         expect(component).to.be.ok;
     });
 });
