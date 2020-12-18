8bb0659865e65765a244be6cf0b0e18535b58b2c
Dominik Fuchs <44401560+FuchsDominik@users.noreply.github.com>
2020-12-18 19:43:11 +0100

    Add more global user statistics charts (#2568)

--- src/main/java/de/tum/in/www1/artemis/repository/StatisticsRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/StatisticsRepository.java
@@ -1,5 +1,6 @@
 package de.tum.in.www1.artemis.repository;
 
+import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
@@ -42,4 +43,73 @@ public interface StatisticsRepository extends JpaRepository<User, Long> {
             order by e.releaseDate asc
             """)
     List<Map<String, Object>> getReleasedExercises(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select e.dueDate as day, count(e.id) as amount
+            from Exercise e
+            where e.dueDate >= :#{#startDate} and e.dueDate <= :#{#endDate}
+            group by e.dueDate
+            order by e.dueDate asc
+            """)
+    List<Map<String, Object>> getExercisesDue(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select p.auditEventDate as day, u.login as username
+            from User u, PersistentAuditEvent p
+            where u.login = p.principal and p.auditEventType = 'AUTHENTICATION_SUCCESS' and u.login not like '%test%' and p.auditEventDate >= :#{#startDate} and p.auditEventDate <= :#{#endDate}
+            order by p.auditEventDate asc
+            """)
+    List<Map<String, Object>> getLoggedInUsers(@Param("startDate") Instant startDate, @Param("endDate") Instant endDate);
+
+    @Query("""
+            select e.endDate as day, count(e.id) as amount
+            from Exam e
+            where e.endDate >= :#{#startDate} and e.endDate <= :#{#endDate}
+            group by e.endDate
+            order by e.endDate asc
+            """)
+    List<Map<String, Object>> getConductedExams(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select e.endDate as day, count(se.id) as amount
+            from StudentExam se, Exam e
+            where se.submitted = true and se.exam = e and e.endDate >= :#{#startDate} and e.endDate <= :#{#endDate}
+            group by e.endDate
+            order by e.endDate asc
+            """)
+    List<Map<String, Object>> getExamParticipations(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select e.endDate as day, sum(size(e.registeredUsers)) as amount
+            from Exam e
+            where e.endDate >= :#{#startDate} and e.endDate <= :#{#endDate}
+            group by e.endDate
+            order by e.endDate asc
+            """)
+    List<Map<String, Object>> getExamRegistrations(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select r.completionDate as day, r.assessor.login as username
+            from Result r
+            where (r.assessmentType = 'MANUAL' or r.assessmentType = 'SEMI-AUTOMATIC') and r.completionDate >= :#{#startDate} and r.completionDate <= :#{#endDate} and r.assessor.login not like '%test%'
+            """)
+    List<Map<String, Object>> getActiveTutors(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select r.completionDate as day, count(r.id) as amount
+            from Result r
+            where r.completionDate >= :#{#startDate} and r.completionDate <= :#{#endDate}
+            group by r.completionDate
+            order by r.completionDate
+            """)
+    List<Map<String, Object>> getCreatedResults(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
+
+    @Query("""
+            select r.completionDate as day, sum(size(r.feedbacks)) as amount
+            from Result r
+            where r.completionDate >= :#{#startDate} and r.completionDate <= :#{#endDate}
+            group by r.completionDate
+            order by r.completionDate
+            """)
+    List<Map<String, Object>> getResultFeedbacks(@Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
 }
