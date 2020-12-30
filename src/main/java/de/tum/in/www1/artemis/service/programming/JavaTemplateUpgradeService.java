e7fc365dfc10a1a2e7ae0ba886cec641d3d16b89
Stefan Klöss-Schuster <stefan.kloess-Schuster@tum.de>
2020-12-30 05:25:57 +0100

    Fix file deletion

--- src/main/java/de/tum/in/www1/artemis/service/programming/JavaTemplateUpgradeService.java
+++ src/main/java/de/tum/in/www1/artemis/service/programming/JavaTemplateUpgradeService.java
@@ -260,9 +260,9 @@ public class JavaTemplateUpgradeService implements TemplateUpgradeService {
     }
 
     private void deleteFileIfPresent(Repository repository, String fileName) throws IOException {
-        Optional<File> testUtilsFolder = gitService.listFiles(repository).stream().filter(file -> fileName.equals(file.getName())).findFirst();
-        if (testUtilsFolder.isPresent()) {
-            repositoryService.deleteFile(repository, testUtilsFolder.toString());
+        Optional<File> optionalFile = gitService.listFiles(repository).stream().filter(file -> fileName.equals(file.getName())).findFirst();
+        if (optionalFile.isPresent()) {
+            repositoryService.deleteFile(repository, optionalFile.get().toString());
         }
     }
 }
