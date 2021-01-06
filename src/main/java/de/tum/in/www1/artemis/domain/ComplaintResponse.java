acca9065ac91e5e835d914cbbefdbf9c77af9faf
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-01-06 13:57:44 +0100

    Added comment to clarify meaning of createdDate

--- src/main/java/de/tum/in/www1/artemis/domain/ComplaintResponse.java
+++ src/main/java/de/tum/in/www1/artemis/domain/ComplaintResponse.java
@@ -17,6 +17,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * A ComplaintResponse.
+ *
+ *  The createdDate in {@link AbstractAuditingEntity#createdDate} has a special meaning in this entity as it is used to calculate the lock status. See also
+ *  {@link ComplaintResponse#isCurrentlyLocked()}
  */
 @Entity
 @Table(name = "complaint_response")
