1ec2ee9580b840fa40fe34cc5ccb2836fd408eb1
René Lalla <r.lalla@gmx.de>
2021-01-08 17:03:14 +0100

    test buildlogs

--- src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/jenkins/JenkinsService.java
@@ -536,18 +536,13 @@ public class JenkinsService implements ContinuousIntegrationService {
             testentry.setLog("buildLog.size(): " + buildLog.size());
             prunedBuildLogs.add(testentry);
 
-            if (logHtml.childNodes().stream().noneMatch(child -> child.attr("class").contains("pipeline"))) {
-                BuildLogEntry testentry2 = new BuildLogEntry();
-                testentry2.setLog("Log is not pipeline log");
-                prunedBuildLogs.add(testentry2);
-            }
-
             BuildLogEntry testentry3 = new BuildLogEntry();
             testentry3.setLog("build: " + build.details().getConsoleOutputHtml());
             prunedBuildLogs.add(testentry3);
 
             for (BuildLogEntry entry : buildLog) {
                 String logString = entry.getLog();
+                prunedBuildLogs.add(entry);
                 if (logString.contains("Compilation failure")) {
                     break;
                 }
