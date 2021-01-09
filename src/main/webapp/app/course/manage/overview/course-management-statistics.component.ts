5b485f3fb97ca7269ecea166f2793425271e6ab6
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2021-01-09 11:32:12 +0100

    Add a stub CourseManagementStatisticsComponent

---
+++ src/main/webapp/app/course/manage/overview/course-management-statistics.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-course-management-statistics',
+    templateUrl: './course-management-statistics.component.html',
+})
+export class CourseManagementStatisticsComponent {
+    constructor() {}
+}
