6caa6114d303979babe3e2a3a533634f33ce07bb
Dominik Fuchs <d0minik.fuchs@web.de>
2021-01-09 11:54:41 +0100

    removed ngOnInit

--- src/test/javascript/spec/component/statistics/statistics-graph.component.spec.ts
+++ src/test/javascript/spec/component/statistics/statistics-graph.component.spec.ts
@@ -81,11 +81,12 @@ describe('StatisticsGraphComponent', () => {
                 graphData[i] = i + 1;
             }
             spy.and.returnValue(of(graphData));
-
-            component.ngOnInit();
+            const changes = { currentSpan: { currentValue: span } as SimpleChange };
+            component.ngOnChanges(changes);
 
             expect(component.dataForSpanType).to.equal(graphData);
             expect(component.chartData[0].data).to.equal(graphData);
+            expect(component.currentSpan).to.equal(span);
         }
     });
 
