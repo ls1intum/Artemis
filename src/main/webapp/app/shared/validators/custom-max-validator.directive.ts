1238c42b0d3a1e408efcbe65529d121d8cc3c446
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-01-10 18:45:51 +0100

    Made it invalid to set points to 0 in exercises

---
+++ src/main/webapp/app/shared/validators/custom-max-validator.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input } from '@angular/core';
+import { FormControl, NG_VALIDATORS, Validator } from '@angular/forms';
+
+/**
+ * Custom max validator as angular offers no such validator for template driven forms
+ * See: https://github.com/angular/angular/issues/16352
+ */
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[customMax][formControlName],[customMax][formControl],[customMax][ngModel]',
+    providers: [{ provide: NG_VALIDATORS, useExisting: CustomMaxDirective, multi: true }],
+})
+export class CustomMaxDirective implements Validator {
+    @Input()
+    customMax: number;
+
+    validate(c: FormControl): { [key: string]: any } | null {
+        const v = c.value;
+        if (v === undefined && v === null) {
+            return null;
+        }
+        return +v > this.customMax ? { customMax: true } : null;
+    }
+}
---
+++ src/main/webapp/app/shared/validators/custom-min-validator.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input } from '@angular/core';
+import { FormControl, NG_VALIDATORS, Validator } from '@angular/forms';
+
+/**
+ * Custom min validator as angular offers no such validator for template driven forms
+ * See: https://github.com/angular/angular/issues/16352
+ */
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: '[customMin][formControlName],[customMin][formControl],[customMin][ngModel]',
+    providers: [{ provide: NG_VALIDATORS, useExisting: CustomMinDirective, multi: true }],
+})
+export class CustomMinDirective implements Validator {
+    @Input()
+    customMin: number;
+
+    validate(c: FormControl): { [key: string]: any } | null {
+        const v = c.value;
+        if (v === undefined && v === null) {
+            return null;
+        }
+        return +v < this.customMin ? { customMin: true } : null;
+    }
+}
