edd7341d083ecc659ac01e8d41b739c341fc36eb
Philipp Bauch <ga53xex@mytum.de>
2021-01-12 09:26:20 +0100

    Add client tests for ExerciseGroupsComponent

--- src/test/javascript/spec/component/exam/manage/exercise-groups.component.spec.ts
+++ src/test/javascript/spec/component/exam/manage/exercise-groups.component.spec.ts
@@ -1,14 +1,12 @@
 import { HttpResponse } from '@angular/common/http';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { ActivatedRoute, convertToParamMap, RouterModule } from '@angular/router';
-import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, convertToParamMap, Router, RouterModule } from '@angular/router';
+import { NgbModal, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { TranslatePipe } from '@ngx-translate/core';
-import { JhiAlertService } from 'ng-jhipster';
 import { MockComponent, MockDirective, MockPipe } from 'ng-mocks';
 import { of } from 'rxjs';
 import { Course } from 'app/entities/course.model';
 import { Exam } from 'app/entities/exam.model';
-import { ExerciseGroupService } from 'app/exam/manage/exercise-groups/exercise-group.service';
 import { AlertErrorComponent } from 'app/shared/alert/alert-error.component';
 import { ExerciseGroupsComponent } from 'app/exam/manage/exercise-groups/exercise-groups.component';
 import { ExamManagementService } from 'app/exam/manage/exam-management.service';
@@ -21,6 +19,10 @@ import { DeleteButtonDirective } from 'app/shared/delete-dialog/delete-button.di
 import { BuildPlanLinkDirective } from 'app/exercises/programming/shared/utils/build-plan-link.directive';
 import { ArtemisTestModule } from '../../../test.module';
 import { TranslateTestingModule } from '../../../helpers/mocks/service/mock-translate.service';
+import { ExerciseType } from 'app/entities/exercise.model';
+import { MockNgbModalService } from '../../../helpers/mocks/service/mock-ngb-modal.service';
+import { MockRouter } from '../../../helpers/mocks/service/mock-route.service';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 
 describe('Exercise Groups Component', () => {
     const course = new Course();
@@ -30,13 +32,23 @@ describe('Exercise Groups Component', () => {
     exam.course = course;
     exam.id = 123;
 
-    const groups: ExerciseGroup[] = [{ id: 0 } as ExerciseGroup, { id: 1 } as ExerciseGroup, { id: 2 } as ExerciseGroup];
+    const groups: ExerciseGroup[] = [
+        {
+            id: 0,
+            exercises: [
+                { id: 3, type: ExerciseType.TEXT },
+                { id: 4, type: ExerciseType.PROGRAMMING },
+            ],
+        } as ExerciseGroup,
+        { id: 1 } as ExerciseGroup,
+        { id: 2 } as ExerciseGroup,
+    ];
 
     let comp: ExerciseGroupsComponent;
     let fixture: ComponentFixture<ExerciseGroupsComponent>;
-    let exerciseGroupService: ExerciseGroupService;
     let examManagementService: ExamManagementService;
-    let alertService: JhiAlertService;
+    let modalService: NgbModal;
+    let router: Router;
 
     const data = of({ exam });
     const route = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id, examId: exam.id }) }, data } as any) as ActivatedRoute;
@@ -56,14 +68,19 @@ describe('Exercise Groups Component', () => {
                 MockDirective(NgbTooltip),
                 MockPipe(TranslatePipe),
             ],
-            providers: [{ provide: ActivatedRoute, useValue: route }],
+            providers: [
+                { provide: ActivatedRoute, useValue: route },
+                { provide: Router, useClass: MockRouter },
+                { provide: NgbModal, useClass: MockNgbModalService },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ExerciseGroupsComponent);
         comp = fixture.componentInstance;
-        exerciseGroupService = TestBed.inject(ExerciseGroupService);
+
         examManagementService = TestBed.inject(ExamManagementService);
-        alertService = TestBed.inject(JhiAlertService);
+        modalService = TestBed.inject(NgbModal);
+        router = TestBed.inject(Router);
     });
 
     // Always initialized and bind before tests
@@ -72,7 +89,7 @@ describe('Exercise Groups Component', () => {
         tick();
     }));
 
-    it('first test', fakeAsync(() => {
+    it('loads the exercise groups', fakeAsync(() => {
         const mockResponse = new HttpResponse<Exam>({ body: exam });
 
         spyOn(examManagementService, 'find').and.returnValue(of(mockResponse));
@@ -85,12 +102,49 @@ describe('Exercise Groups Component', () => {
         tick();
     }));
 
+    it('opens the import modal', fakeAsync(() => {
+        const mockReturnValue = { result: Promise.resolve({ id: 1 } as ProgrammingExercise) };
+        spyOn(modalService, 'open').and.returnValue(mockReturnValue);
+
+        comp.openImportModal(groups[0], ExerciseType.PROGRAMMING);
+        tick();
+
+        expect(modalService.open).toHaveBeenCalled();
+        expect(router.navigate).toHaveBeenCalled();
+    }));
+
     it('moves up an exercise group', () => {
-        comp.exerciseGroups = groups;
+        comp.exerciseGroups = groups.slice();
+        const from = 1;
+        const to = 0;
 
-        comp.moveUp(1);
+        comp.moveUp(from);
 
-        expect(comp.exerciseGroups[0].id).toEqual(1);
-        expect(comp.exerciseGroups[1].id).toEqual(0);
+        expect(comp.exerciseGroups[to].id).toEqual(groups[from].id);
+        expect(comp.exerciseGroups[from].id).toEqual(groups[to].id);
+    });
+
+    it('moves down an exercise group', () => {
+        comp.exerciseGroups = groups.slice();
+        const from = 0;
+        const to = 1;
+
+        comp.moveDown(from);
+
+        expect(comp.exerciseGroups[to].id).toEqual(groups[from].id);
+        expect(comp.exerciseGroups[from].id).toEqual(groups[to].id);
+    });
+
+    it('maps exercise types to exercise groups', () => {
+        comp.exerciseGroups = groups.slice();
+        const firstGroupId = groups[0].id!;
+        const expectedResult = [ExerciseType.TEXT, ExerciseType.PROGRAMMING];
+
+        comp.setupExerciseGroupToExerciseTypesDict();
+        const map = comp.exerciseGroupToExerciseTypesDict;
+
+        expect(map).toBeDefined();
+        expect(map.size).toEqual(groups.length);
+        expect(map.get(firstGroupId)).toEqual(expectedResult);
     });
 });
