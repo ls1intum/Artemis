c35fb83fe1a8b4ccdf2732aaff3f88ca0e9a7b05
Stephan Krusche <s.krusche@tum.de>
2021-01-14 21:46:22 +0100

    allow all frame-src

--- src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
@@ -130,7 +130,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src * data:; font-src 'self' data:")
+            .contentSecurityPolicy("default-src 'self'; frame-src * data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src * data:; font-src 'self' data:")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
