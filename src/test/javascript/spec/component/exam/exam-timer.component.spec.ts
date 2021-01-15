bfad5824108ee612ed96afa41952c1c7341d118e
Dominik Fuchs <d0minik.fuchs@web.de>
2021-01-15 10:55:34 +0100

    removed unused imports

--- src/test/javascript/spec/component/exam/exam-timer.component.spec.ts
+++ src/test/javascript/spec/component/exam/exam-timer.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ArtemisTestModule } from '../../test.module';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
