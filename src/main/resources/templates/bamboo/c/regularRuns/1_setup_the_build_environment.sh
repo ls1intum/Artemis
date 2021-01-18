cce3639dbf7aba495b36fb6072bcb6b1320bc2ea
René Lalla <r.lalla@gmx.de>
2021-01-18 22:55:04 +0100

    remove debug log infos

--- src/main/resources/templates/bamboo/c/regularRuns/1_setup_the_build_environment.sh
+++ src/main/resources/templates/bamboo/c/regularRuns/1_setup_the_build_environment.sh
@@ -5,29 +5,13 @@
 # Build and run all tests
 # ------------------------------
 
-echo "--------------------setup-------------------"
-echo "User:"
-whoami
-echo "Updating assignment and test-reports ownership..."
+# Updating assignment and test-reports ownership...
 sudo chown artemis_user:artemis_user assignment/ -R
 sudo rm -rf test-reports
 sudo mkdir test-reports
 sudo chown artemis_user:artemis_user test-reports/ -R
-echo "--------------------setup-------------------"
-echo "--------------------info--------------------"
-python3 --version
-pip3 --version
-gcc --version
-# Generic debug infos:
-# tree
-echo "--------------------info--------------------"
-echo "--------------------tests-------------------"
-ls -la tests
-echo "--------------------tests-------------------"
-echo "--------------------assignment--------------"
-ls -la assignment
-echo "--------------------assignment--------------"
 
+# assignment
 cd tests
 REQ_FILE=requirements.txt
 if [ -f "$REQ_FILE" ]; then
