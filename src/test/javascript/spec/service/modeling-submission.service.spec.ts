7e45f2600b9d3494a3013eb4b03d7d2b77fb16e6
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-01-27 11:51:25 +0100

    Start writing test for modeling submission service

--- src/test/javascript/spec/service/modeling-submission.service.spec.ts
+++ src/test/javascript/spec/service/modeling-submission.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { take } from 'rxjs/operators';
 import { ModelingSubmissionService } from 'app/exercises/modeling/participate/modeling-submission.service';
 import { ModelingSubmission } from 'app/entities/modeling-submission.model';
+import { HttpRequest } from '@angular/common/http';
 
 describe('ModelingSubmission Service', () => {
     let injector: TestBed;
@@ -63,6 +64,44 @@ describe('ModelingSubmission Service', () => {
             .pipe(take(1))
             .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
         const req = httpMock.expectOne({ method: 'PUT' });
+        req.flush(JSON.stringify(returnedFromService));
+    });
+
+    it('should getModelingSubmissionsForExerciseByCorrectionRound without correction round', async () => {
+        const returnedFromService = Object.assign(
+            {
+                id: 5,
+            },
+            elemDefault,
+        );
+        const expected = Object.assign({}, returnedFromService);
+        const requestOption = { test: 'Test' };
+        service
+            .getModelingSubmissionsForExerciseByCorrectionRound(5, requestOption)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+        const req = httpMock.expectOne({ method: 'GET', url: `${service.resourceUrl}/exercises/${5}/modeling-submissions?test=Test` });
+        expect(req.request.params.get('test')).toBe('Test');
+        req.flush(JSON.stringify(returnedFromService));
+    });
+
+    it('should getModelingSubmissionsForExerciseByCorrectionRound without correction round', async () => {
+        const returnedFromService = Object.assign(
+            {
+                id: 5,
+            },
+            elemDefault,
+        );
+        const expected = Object.assign({}, returnedFromService);
+        const requestOption = { test: 'Test' };
+        const correctionRound = 6;
+        service
+            .getModelingSubmissionsForExerciseByCorrectionRound(5, requestOption, correctionRound)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+        const req = httpMock.expectOne({ method: 'GET', url: `${service.resourceUrl}/exercises/${5}/modeling-submissions?test=Test&correction-round=${correctionRound}` });
+        expect(req.request.params.get('test')).toBe('Test');
+        expect(req.request.params.get('correction-round')).toBe(`${correctionRound}`);
         req.flush(JSON.stringify(returnedFromService));
     });
 
