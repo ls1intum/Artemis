2e7c11b92556a782a98bd35cbd30a9234877c8b1
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-01-28 10:28:46 +0100

    Reverted exerciseRepository

--- src/main/java/de/tum/in/www1/artemis/repository/ExerciseRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ExerciseRepository.java
@@ -58,37 +58,36 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     /**
      * calculates the average score and the participation rate of students for each given individual course exercise
      * by using the last result (rated or not)
-     *
      * @param exerciseIds - exercise ids to count the statistics for
      * @return <code>Object[]</code> where each index corresponds to the column from the db (0 refers to exerciseId and so on)
      */
     @Query("""
             SELECT
-                e.id,
-                AVG(r.score),
-                    Count(Distinct p.student.id),
-                    (SELECT count(distinct u.id)
-                    FROM User u
-                    WHERE
-                    e.course.studentGroupName member of u.groups
-                    AND e.course.teachingAssistantGroupName not member of u.groups
-                    AND e.course.instructorGroupName not member of u.groups
-                    )
-                    FROM Exercise e JOIN e.studentParticipations p JOIN p.submissions s JOIN s.results r
-                    WHERE e.id IN :exerciseIds
-                    AND e.course.studentGroupName member of p.student.groups
-                    AND e.course.teachingAssistantGroupName not member of p.student.groups
-                    AND e.course.instructorGroupName not member of p.student.groups
-                    AND r.score IS NOT NULL
-                    AND
-                    s.id = (
-                        SELECT max(s2.id)
-                        FROM Submission s2 JOIN s2.results r2
-                        WHERE s2.participation.id = s.participation.id
-                        AND r2.score IS NOT NULL
-                        )
-                    GROUP BY e.id
-                    """)
+            e.id,
+            AVG(r.score),
+            Count(Distinct p.student.id),
+            (SELECT count(distinct u.id)
+            FROM User u
+            WHERE
+            e.course.studentGroupName member of u.groups
+            AND e.course.teachingAssistantGroupName not member of u.groups
+            AND e.course.instructorGroupName not member of u.groups
+            )
+            FROM Exercise e JOIN e.studentParticipations p JOIN p.submissions s JOIN s.results r
+            WHERE e.id IN :exerciseIds
+            AND e.course.studentGroupName member of p.student.groups
+            AND e.course.teachingAssistantGroupName not member of p.student.groups
+            AND e.course.instructorGroupName not member of p.student.groups
+            AND r.score IS NOT NULL
+            AND
+            s.id = (
+                SELECT max(s2.id)
+                FROM Submission s2 JOIN s2.results r2
+                WHERE s2.participation.id = s.participation.id
+                AND r2.score IS NOT NULL
+                )
+            GROUP BY e.id
+            """)
     List<Object[]> calculateExerciseStatisticsForIndividualCourseExercises(@Param("exerciseIds") List<Long> exerciseIds);
 
     /**
