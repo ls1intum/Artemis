2ce0e6d166afd597df5471b36aa0d9a8c824f5e0
Nicolas Ruscher <nicolas.ruscher@tum.de>
2021-01-28 15:12:53 +0100

    added tests for new component

---
+++ src/test/javascript/spec/component/tutor-dashboard/second-correction-enable-button.component.spec.ts
@@ -0,0 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { JhiLanguageHelper } from 'app/core/language/language.helper';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { ArtemisTestModule } from '../../test.module';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
+import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { MockRouter } from '../../helpers/mocks/mock-router';
+import { Router } from '@angular/router';
+import { SinonStub, stub } from 'sinon';
+
+import { SecondCorrectionEnableButtonComponent } from 'app/exercises/shared/dashboards/tutor/second-correction-button/second-correction-enable-button.component';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('SecondCorrectionEnableButtonComponent', () => {
+    let comp: SecondCorrectionEnableButtonComponent;
+    let fixture: ComponentFixture<SecondCorrectionEnableButtonComponent>;
+    const router = new MockRouter();
+
+    beforeEach(async () => {
+        return TestBed.configureTestingModule({
+            imports: [ArtemisTestModule, ArtemisSharedModule, TranslateModule.forRoot()],
+            declarations: [SecondCorrectionEnableButtonComponent],
+            providers: [
+                JhiLanguageHelper,
+                { provide: LocalStorageService, useClass: MockSyncStorage },
+                { provide: Router, useValue: router },
+                { provide: SessionStorageService, useClass: MockSyncStorage },
+            ],
+        })
+            .overrideModule(ArtemisTestModule, { set: { declarations: [], exports: [] } })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(SecondCorrectionEnableButtonComponent);
+                comp = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+    });
+
+    it('test call', () => {
+        let stubEmit = stub(comp.ngModelChange, 'emit');
+        comp.triggerSecondCorrectionButton();
+        expect(stubEmit).to.have.been.called;
+    });
+});
