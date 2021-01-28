c36827aac2eef520d522eb0877294380f0049ef1
SimonEntholzer <nom.ise@outlook.com>
2021-01-28 17:02:28 +0100

    removed unused variable

--- src/main/webapp/app/entities/exam.model.ts
+++ src/main/webapp/app/entities/exam.model.ts
@@ -37,7 +37,6 @@ export class Exam implements BaseEntity {
 
     public numberOfRegisteredUsers?: number; // transient
 
-    public numberOfCorrectionRoundsEnabled?: number;
     // helper attributes
     public visible?: boolean;
     public started?: boolean;
