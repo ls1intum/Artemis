34fa5c05e03112d64563b248fe466894742c5383
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2021-02-05 22:22:09 +0100

    small fixes and clean up file-upload-exam component

--- src/main/webapp/app/exam/participate/exercises/file-upload/file-upload-exam-submission.component.ts
+++ src/main/webapp/app/exam/participate/exercises/file-upload/file-upload-exam-submission.component.ts
@@ -2,7 +2,6 @@ import { Component, ElementRef, OnInit, ViewChild, Input } from '@angular/core';
 import { Location } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
 import { JhiAlertService } from 'ng-jhipster';
 import * as moment from 'moment';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
@@ -10,18 +9,14 @@ import { FileUploadSubmissionService } from 'app/exercises/file-upload/participa
 import { FileUploaderService } from 'app/shared/http/file-uploader.service';
 import { MAX_SUBMISSION_FILE_SIZE } from 'app/shared/constants/input.constants';
 import { FileUploadAssessmentsService } from 'app/exercises/file-upload/assess/file-upload-assessment.service';
-import { omit } from 'lodash';
 import { ParticipationWebsocketService } from 'app/overview/participation-websocket.service';
 import { FileUploadExercise } from 'app/entities/file-upload-exercise.model';
-import { ComponentCanDeactivate } from 'app/shared/guard/can-deactivate.model';
 import { FileService } from 'app/shared/http/file.service';
 import { ResultService } from 'app/exercises/shared/result/result.service';
 import { FileUploadSubmission } from 'app/entities/file-upload-submission.model';
-import { participationStatus } from 'app/exercises/shared/exercise/exercise-utils';
 import { ButtonType } from 'app/shared/components/button.component';
 import { Result } from 'app/entities/result.model';
 import { AccountService } from 'app/core/auth/account.service';
-import { getLatestSubmissionResult } from 'app/entities/submission.model';
 import { ExamSubmissionComponent } from 'app/exam/participate/exercises/exam-submission.component';
 import { IncludedInOverallScore } from 'app/entities/exercise.model';
 
@@ -32,7 +27,7 @@ import { IncludedInOverallScore } from 'app/entities/exercise.model';
     styleUrls: ['./file-upload-exam-submission.component.scss'],
     // change deactivation must be triggered manually
 })
-export class FileUploadExamSubmissionComponent implements OnInit, ComponentCanDeactivate {
+export class FileUploadExamSubmissionComponent implements OnInit {
     @ViewChild('fileInput', { static: false }) fileInput: ElementRef;
 
     @Input()
@@ -70,9 +65,6 @@ export class FileUploadExamSubmissionComponent implements OnInit, ComponentCanDe
         private location: Location,
         private translateService: TranslateService,
         private fileService: FileService,
-        private participationWebsocketService: ParticipationWebsocketService,
-        private fileUploadAssessmentService: FileUploadAssessmentsService,
-        private accountService: AccountService,
     ) {
         translateService.get('artemisApp.fileUploadSubmission.confirmSubmission').subscribe((text) => (this.submissionConfirmationText = text));
     }
@@ -81,100 +73,7 @@ export class FileUploadExamSubmissionComponent implements OnInit, ComponentCanDe
      * Initializes data for file upload editor
      */
     ngOnInit() {
-        const participationId = Number(this.route.snapshot.paramMap.get('participationId'));
-        if (Number.isNaN(participationId)) {
-            return this.jhiAlertService.error('artemisApp.fileUploadExercise.error');
-        }
-        this.fileUploadSubmissionService.getDataForFileUploadEditor(participationId).subscribe(
-            (submission: FileUploadSubmission) => {
-                // reconnect participation <--> result
-                const tmpResult = getLatestSubmissionResult(submission);
-                if (tmpResult) {
-                    submission.participation!.results = [tmpResult!];
-                }
-                this.participation = <StudentParticipation>submission.participation;
-
-                // reconnect participation <--> submission
-                this.participation.submissions = [<FileUploadSubmission>omit(submission, 'participation')];
-
-                this.submission = submission;
-                this.result = tmpResult!;
-                this.exercise = this.participation.exercise as FileUploadExercise;
-                this.examMode = !!this.exercise.exerciseGroup;
-                this.exercise.studentParticipations = [this.participation];
-                this.exercise.participationStatus = participationStatus(this.exercise);
-
-                // checks if the student started the exercise after the due date
-                this.isLate =
-                    this.exercise &&
-                    !!this.exercise.dueDate &&
-                    !!this.participation.initializationDate &&
-                    moment(this.participation.initializationDate).isAfter(this.exercise.dueDate);
-
-                this.acceptedFileExtensions = this.exercise
-                    .filePattern!.split(',')
-                    .map((extension) => `.${extension}`)
-                    .join(',');
-                this.isAfterAssessmentDueDate = !this.exercise.assessmentDueDate || moment().isAfter(this.exercise.assessmentDueDate);
-
-                if (this.submission.submitted) {
-                    this.setSubmittedFile();
-                }
-                if (this.submission.submitted && this.result && this.result.completionDate) {
-                    this.fileUploadAssessmentService.getAssessment(this.submission.id!).subscribe((assessmentResult: Result) => {
-                        this.result = assessmentResult;
-                    });
-                }
-                this.isOwnerOfParticipation = this.accountService.isOwnerOfParticipation(this.participation);
-            },
-            (error: HttpErrorResponse) => this.onError(error),
-        );
-    }
-    /**
-     * Uploads a submission file and submits File Upload Exercise
-     */
-    public submitExercise() {
-        if (this.isSaving) {
-            // don't execute the function if it is already currently executing
-            return;
-        }
-
-        const file = this.submissionFile;
-        if (!this.submission || !file) {
-            return;
-        }
-        this.isSaving = true;
-        this.fileUploadSubmissionService.update(this.submission!, this.exercise.id!, file).subscribe(
-            (response) => {
-                this.submission = response.body!;
-                this.participation = this.submission.participation as StudentParticipation;
-                // reconnect so that the submission status is displayed correctly in the result.component
-                this.submission.participation!.submissions = [this.submission];
-                this.participationWebsocketService.addParticipation(this.participation, this.exercise);
-                this.exercise.studentParticipations = [this.participation];
-                this.exercise.participationStatus = participationStatus(this.exercise);
-                this.result = getLatestSubmissionResult(this.submission)!;
-                this.setSubmittedFile();
-                if (this.isActive) {
-                    this.jhiAlertService.success('artemisApp.fileUploadExercise.submitSuccessful');
-                } else {
-                    this.jhiAlertService.warning('artemisApp.fileUploadExercise.submitDeadlineMissed');
-                }
-                this.isSaving = false;
-            },
-            (error: HttpErrorResponse) => {
-                this.submission!.submitted = false;
-                const serverError = error.headers.get('X-artemisApp-error');
-                if (serverError) {
-                    this.jhiAlertService.error(serverError, { fileName: file['name'] });
-                } else {
-                    this.jhiAlertService.error('artemisApp.fileUploadSubmission.fileUploadError', { fileName: file['name'] });
-                }
-                this.fileInput.nativeElement.value = '';
-                this.submissionFile = undefined;
-                this.isSaving = false;
-            },
-        );
+        console.log('blub');
     }
 
     /**
@@ -196,10 +95,6 @@ export class FileUploadExamSubmissionComponent implements OnInit, ComponentCanDe
         }
     }
 
-    private onError(error: HttpErrorResponse) {
-        this.jhiAlertService.error(error.message);
-    }
-
     private setSubmittedFile() {
         // clear submitted file so that it is not displayed in the input (this might be confusing)
         this.submissionFile = undefined;
@@ -214,34 +109,9 @@ export class FileUploadExamSubmissionComponent implements OnInit, ComponentCanDe
     }
 
     /**
-     * Returns false if user selected a file, but didn't submit the exercise, true otherwise.
-     */
-    canDeactivate(): boolean {
-        return !(this.submission && !this.submission.submitted && this.submissionFile);
-    }
-
-    /**
      * The exercise is still active if it's due date hasn't passed yet.
      */
     get isActive(): boolean {
-        return !this.examMode && this.exercise && (!this.exercise.dueDate || moment(this.exercise.dueDate).isSameOrAfter(moment()));
-    }
-
-    get submitButtonTooltip(): string {
-        if (!this.submissionFile) {
-            return 'artemisApp.fileUploadSubmission.selectFile';
-        }
-
-        if (!this.isLate) {
-            if (this.isActive && !this.exercise.dueDate) {
-                return 'entity.action.submitNoDeadlineTooltip';
-            } else if (this.isActive) {
-                return 'entity.action.submitTooltip';
-            } else {
-                return 'entity.action.deadlineMissedTooltip';
-            }
-        }
-
-        return 'entity.action.submitDeadlineMissedTooltip';
+        return this.exercise && (!this.exercise.dueDate || moment(this.exercise.dueDate).isSameOrAfter(moment()));
     }
 }
