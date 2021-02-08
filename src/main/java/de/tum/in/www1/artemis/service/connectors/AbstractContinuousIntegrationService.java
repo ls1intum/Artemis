f996b6418739486dda6538b8297efca0349de34f
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2021-02-08 23:51:30 +0100

    run prettier

--- src/main/java/de/tum/in/www1/artemis/service/connectors/AbstractContinuousIntegrationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/AbstractContinuousIntegrationService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
 
