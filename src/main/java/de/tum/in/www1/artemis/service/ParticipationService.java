352a56b4d69a0d5051529cdedbcb019e838f3ef6
Stephan Krusche <s.krusche@tum.de>
2021-02-09 20:22:04 +0100

    remove wrong and outdated comments

--- src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ParticipationService.java
@@ -607,12 +607,9 @@ public class ParticipationService {
     public ZonedDateTime getIndividualDueDate(Exercise exercise, StudentParticipation participation) {
         if (exercise.isExamExercise()) {
             var studentExam = findStudentExam(exercise, participation).orElse(null);
-            // this is the case for all non-exam exercises
             if (studentExam == null) {
                 return exercise.getDueDate();
             }
-            // TODO scale all exercise working times depending on the settings of the exercise, for now, this is just
-            // the individual end date of the exam
             return studentExam.getExam().getStartDate().plusSeconds(studentExam.getWorkingTime());
         }
         return exercise.getDueDate();
