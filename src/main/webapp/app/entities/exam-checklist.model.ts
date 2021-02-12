d4e83773db562e8e7948aa566b52bae284db5ab0
Nicolas Ruscher <nicolas.ruscher@tum.de>
2021-02-12 12:49:19 +0100

    mayor client refactorings new components

---
+++ src/main/webapp/app/entities/exam-checklist.model.ts
@@ -0,0 +1,18 @@
+export class ExamChecklist {
+    public numberOfGeneratedStudentExams?: number; // transient
+    public numberOfTestRuns?: number; // transient
+
+    public numberOfStudentsExamStarted?: number;
+
+    public numberOfStudentsExamSubmitted?: number;
+
+    public numberOfTotalExamParticipations?: number;
+
+    public numberOfTotalExamAssessmentsDone?: number;
+
+    public numberOfAllComplaints?: number;
+
+    public numberOfAllComplaintsDone?: number;
+
+    public allExamExercisesAllStudentsPrepared?: boolean;
+}
