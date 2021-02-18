a9ecdd8a4a590fd8328c0e3be492cc6d67c05ff9
Francisco De las Casas Young <f.de-las-casas-young@tum.de>
2021-02-18 20:54:32 +0100

    remove general feedback for file upload exercises

--- src/main/webapp/app/exercises/file-upload/assess/file-upload-assessment.component.ts
+++ src/main/webapp/app/exercises/file-upload/assess/file-upload-assessment.component.ts
@@ -40,8 +40,6 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
     submission: FileUploadSubmission;
     unassessedSubmission: FileUploadSubmission;
     result: Result;
-    generalFeedback: Feedback = new Feedback();
-    // TODO: rename this, because right now there is no reference
     unreferencedFeedback: Feedback[] = [];
     exercise: FileUploadExercise;
     exerciseId: number;
@@ -87,7 +85,7 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
     }
 
     get assessments(): Feedback[] {
-        return [this.generalFeedback, ...this.unreferencedFeedback];
+        return [...this.unreferencedFeedback];
     }
 
     public ngOnInit(): void {
@@ -179,7 +177,7 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
         this.submission.participation!.results = [this.result];
         this.result.participation = this.submission.participation;
         if (this.result.feedbacks) {
-            this.loadFeedbacks(this.result.feedbacks);
+            this.unreferencedFeedback = this.result.feedbacks;
         } else {
             this.result.feedbacks = [];
         }
@@ -216,7 +214,6 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
      * For the new submission to appear on the same page, the url has to be reloaded.
      */
     assessNext() {
-        this.generalFeedback = new Feedback();
         this.unreferencedFeedback = [];
         this.fileUploadSubmissionService.getFileUploadSubmissionForExerciseForCorrectionRoundWithoutAssessment(this.exercise.id!, false, this.correctionRound).subscribe(
             (response: FileUploadSubmission) => {
@@ -328,7 +325,7 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
 
     /**
      * Checks if the assessment is valid:
-     *   - There must be at least one referenced feedback or a general feedback.
+     *   - There must be at least one referenced feedback.
      *   - Each feedback must have either a score or a feedback text or both.
      *   - The score must be a valid number.
      *
@@ -338,7 +335,7 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
         this.assessmentsAreValid = true;
         this.invalidError = undefined;
 
-        if ((!this.generalFeedback.detailText || this.generalFeedback.detailText.length === 0) && this.unreferencedFeedback && this.unreferencedFeedback.length === 0) {
+        if (this.unreferencedFeedback && this.unreferencedFeedback.length === 0) {
             this.totalScore = 0;
             this.assessmentsAreValid = false;
             return;
@@ -440,17 +437,6 @@ export class FileUploadAssessmentComponent implements OnInit, OnDestroy {
                     }
                 },
             );
-    }
-
-    private loadFeedbacks(feedbacks: Feedback[]): void {
-        const generalFeedbackIndex = feedbacks.findIndex((feedback) => feedback.credits === 0);
-        if (generalFeedbackIndex !== -1) {
-            this.generalFeedback = feedbacks[generalFeedbackIndex];
-            feedbacks.splice(generalFeedbackIndex, 1);
-        } else {
-            this.generalFeedback = new Feedback();
-        }
-        this.unreferencedFeedback = feedbacks;
     }
 
     get readOnly(): boolean {
