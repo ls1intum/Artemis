67fa44f05778c64e5789b47ee72125cbc81ebfad
SimonEntholzer <nom.ise@outlook.com>
2021-03-02 21:01:01 +0100

    created guided tour utils spec file

---
+++ src/test/javascript/spec/component/utils/guided-tour-utils.spec.ts
@@ -0,0 +1,11 @@
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { clickOnElement } from 'app/guided-tour/guided-tour.utils';
+
+const expect = chai.expect;
+
+describe('GuidedTourUtilities', () => {
+    describe('clickOnElement', () => {
+        it;
+    });
+});
