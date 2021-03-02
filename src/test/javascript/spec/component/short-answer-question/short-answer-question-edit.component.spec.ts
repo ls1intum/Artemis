a5641d7f03efa62ef36e1430aac4951c855f690c
Dominik Fuchs <d0minik.fuchs@web.de>
2021-03-02 10:28:24 +0100

    another try

--- src/test/javascript/spec/component/short-answer-question/short-answer-question-edit.component.spec.ts
+++ src/test/javascript/spec/component/short-answer-question/short-answer-question-edit.component.spec.ts
@@ -23,8 +23,6 @@ import { stub } from 'sinon';
 import { ArtemisMarkdownService } from 'app/shared/markdown.service';
 import { ShortAnswerQuestionUtil } from 'app/exercises/quiz/shared/short-answer-question-util.service';
 
-/* tslint:disable:no-unused-variable */
-
 chai.use(sinonChai);
 const expect = chai.expect;
 
@@ -177,7 +175,7 @@ describe('ShortAnswerQuestionEditComponent', () => {
         const node = {} as Node;
 
         const returnValue = ({
-            // tslint:disable-next-line:no-unused-vars
+            // tslint:disable-next-line:no-unused-variable
             contains(_other: Node | null): boolean {
                 return true;
             },
