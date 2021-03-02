03ebf0506c0ae1f1c1794dcf40ca9087c2d60c79
Dominik Fuchs <d0minik.fuchs@web.de>
2021-03-02 11:35:29 +0100

    fix hasComplaint for programmingExercises

--- src/main/java/de/tum/in/www1/artemis/web/rest/ProgrammingAssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ProgrammingAssessmentResource.java
@@ -179,6 +179,7 @@ public class ProgrammingAssessmentResource extends AssessmentResource {
         // make sure that the submission cannot be manipulated on the client side
         var submission = (ProgrammingSubmission) existingManualResult.getSubmission();
         newManualResult.setSubmission(submission);
+        newManualResult.setHasComplaint(existingManualResult.hasComplaint());
         newManualResult = programmingAssessmentService.saveManualAssessment(newManualResult);
 
         if (submission.getParticipation() == null) {
