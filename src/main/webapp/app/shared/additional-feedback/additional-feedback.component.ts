7fe6429ff26cc6097c7f97417748a4dd6f49206d
Dominik Fuchs <d0minik.fuchs@web.de>
2021-03-03 12:23:11 +0100

    extract additional feedback into shared component for later usage

---
+++ src/main/webapp/app/shared/additional-feedback/additional-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Feedback } from 'app/entities/feedback.model';
+
+@Component({
+    selector: 'jhi-additional-feedback',
+    templateUrl: './additional-feedback.component.html',
+    styleUrls: ['./additional-feedback.component.scss'],
+})
+export class AdditionalFeedbackComponent {
+    @Input()
+    additionalFeedback: Feedback[];
+}
