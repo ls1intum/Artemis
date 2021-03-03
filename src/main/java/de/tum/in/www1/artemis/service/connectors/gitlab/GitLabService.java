1c06bd7dc703f3fcd27eccab4d5ae5b01c596292
Ivan Chimeno <chimeno.ivan@gmail.com>
2021-03-03 11:58:55 +0100

    print exception to see whats wrong

--- src/main/java/de/tum/in/www1/artemis/service/connectors/gitlab/GitLabService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/gitlab/GitLabService.java
@@ -111,7 +111,7 @@ public class GitLabService extends AbstractVersionControlService {
                 updateMemberPermissionInRepository(repositoryUrl, user.getLogin(), DEVELOPER);
             }
             else {
-                throw new GitLabException("Error while trying to add user " + user.getLogin() + " to repository: " + repositoryUrl, e);
+                throw new GitLabException("Error while trying to add user " + user.getLogin() + " to repository: " + repositoryUrl + " ex:" + e.getMessage() + e.getCause(), e);
             }
         }
     }
