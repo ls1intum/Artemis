e76f0d78597ed8a1186d8bce42d689f8fb0de430
SimonEntholzer <nom.ise@outlook.com>
2021-03-04 12:10:11 +0100

    increased font and put information in a separate container

--- src/main/webapp/app/exercises/modeling/assess/modeling-assessment-editor/modeling-assessment-dashboard.component.ts
+++ src/main/webapp/app/exercises/modeling/assess/modeling-assessment-editor/modeling-assessment-dashboard.component.ts
@@ -216,7 +216,7 @@ export class ModelingAssessmentDashboardComponent implements OnInit, OnDestroy {
                     this.busy = false;
                     if (optimal.length === 0) {
                         this.jhiAlertService.clear();
-                        this.jhiAlertService.info('assessmentDashboard.noSubmissionFound');
+                        this.jhiAlertService.info('artemisApp.assessmentDashboard.noSubmissionFound');
                     } else {
                         this.nextOptimalSubmissionIds = optimal;
                         this.navigateToNextRandomOptimalSubmission();
@@ -225,7 +225,7 @@ export class ModelingAssessmentDashboardComponent implements OnInit, OnDestroy {
                 () => {
                     this.busy = false;
                     this.jhiAlertService.clear();
-                    this.jhiAlertService.info('assessmentDashboard.noSubmissionFound');
+                    this.jhiAlertService.info('artemisApp.assessmentDashboard.noSubmissionFound');
                 },
             );
         } else {
