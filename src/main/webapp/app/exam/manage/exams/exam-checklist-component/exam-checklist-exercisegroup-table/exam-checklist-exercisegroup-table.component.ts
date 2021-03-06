319e466294a04ca76c71b1f30dd66d36c707d753
Nicolas Ruscher <nicolas.ruscher@tum.de>
2021-03-06 21:07:12 +0100

    new method reworks for new checklist table

--- src/main/webapp/app/exam/manage/exams/exam-checklist-component/exam-checklist-exercisegroup-table/exam-checklist-exercisegroup-table.component.ts
+++ src/main/webapp/app/exam/manage/exams/exam-checklist-component/exam-checklist-exercisegroup-table/exam-checklist-exercisegroup-table.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ExerciseGroup } from 'app/entities/exercise-group.model';
+import { Exercise } from 'app/entities/exercise.model';
 
 @Component({
     selector: 'jhi-exam-checklist-exercisegroup-table',
     templateUrl: './exam-checklist-exercisegroup-table.component.html',
 })
-export class ExamChecklistExerciseGroupTableComponent {
+export class ExamChecklistExerciseGroupTableComponent implements OnInit, OnChanges {
     @Input() exerciseGroups: ExerciseGroup[];
+
+    groupsWithoutExercises: ExerciseGroup[] = [];
+    allExercises: Exercise[] = [];
+
+    ngOnInit() {}
+
+    ngOnChanges() {
+        if (this.exerciseGroups) {
+            this.exerciseGroups.forEach((exerciseGroup) => {
+                if (exerciseGroup.exercises) {
+                    exerciseGroup.exercises!.forEach((exercise) => {
+                        exercise.exerciseGroup = exerciseGroup;
+                        this.allExercises.push(exercise);
+                    });
+                    const maxPoints = exerciseGroup.exercises?.[0].maxPoints;
+                    exerciseGroup.allPointsEqual = exerciseGroup.exercises?.some((exercise) => {
+                        return exercise.maxPoints !== maxPoints;
+                    });
+                } else {
+                    this.groupsWithoutExercises.push(exerciseGroup);
+                }
+            });
+        }
+        console.log('test');
+    }
 }
