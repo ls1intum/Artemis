1cd3f9892f03a5689ace24c04a04f0e456e46351
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2021-03-08 15:44:27 +0100

    Fix a rounding error due to review comment changes

--- src/main/webapp/app/overview/course-score-calculation.service.ts
+++ src/main/webapp/app/overview/course-score-calculation.service.ts
@@ -51,7 +51,7 @@ export class CourseScoreCalculationService {
                         if (score == undefined) {
                             score = 0;
                         }
-                        pointsAchievedByStudentInCourse += round(score * this.SCORE_NORMALIZATION_VALUE * maxPointsReachableInExercise);
+                        pointsAchievedByStudentInCourse += round(score * this.SCORE_NORMALIZATION_VALUE * maxPointsReachableInExercise, 1);
                     }
                     presentationScore += participation.presentationScore ? participation.presentationScore : 0;
                 }
