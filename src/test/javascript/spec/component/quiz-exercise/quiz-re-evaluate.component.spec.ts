8d8448b2ca5eb8bb87f592a5dc8285fbeffc28d0
René Lalla <r.lalla@gmx.de>
2021-03-09 14:33:18 +0100

    add test cases for re-evaluate quiz

---
+++ src/test/javascript/spec/component/quiz-exercise/quiz-re-evaluate.component.spec.ts
@@ -0,0 +1,112 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ArtemisTestModule } from '../../test.module';
+import { QuizExerciseService } from 'app/exercises/quiz/manage/quiz-exercise.service';
+import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { Course } from 'app/entities/course.model';
+import { QuizReEvaluateComponent } from 'app/exercises/quiz/manage/re-evaluate/quiz-re-evaluate.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MockRouter } from '../../helpers/mocks/mock-router';
+import { QuizExercisePopupService } from 'app/exercises/quiz/manage/quiz-exercise-popup.service';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { restore, SinonStub, stub } from 'sinon';
+import { ReEvaluateMultipleChoiceQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/multiple-choice-question/re-evaluate-multiple-choice-question.component';
+import { MockComponent, MockDirective, MockPipe } from 'ng-mocks';
+import { ReEvaluateDragAndDropQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/drag-and-drop-question/re-evaluate-drag-and-drop-question.component';
+import { ReEvaluateShortAnswerQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/short-answer-question/re-evaluate-short-answer-question.component';
+import { MockTranslateValuesDirective } from '../course/course-scores/course-scores.component.spec';
+import { NgModel } from '@angular/forms';
+import { JhiTranslateDirective } from 'ng-jhipster';
+import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
+import { QuizQuestion, QuizQuestionType } from 'app/entities/quiz/quiz-question.model';
+import { MultipleChoiceQuestion } from 'app/entities/quiz/multiple-choice-question.model';
+import { DragAndDropQuestion } from 'app/entities/quiz/drag-and-drop-question.model';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('QuizExercise Management Component', () => {
+    let comp: QuizReEvaluateComponent;
+    let fixture: ComponentFixture<QuizReEvaluateComponent>;
+    let quizService: QuizExerciseService;
+    let quizServiceFindSpy: SinonStub;
+
+    const course = { id: 123 } as Course;
+    const quizExercise = new QuizExercise(course, undefined);
+
+    const route = ({ params: of({ exerciseId: 123 }) } as any) as ActivatedRoute;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [
+                NgModel,
+                QuizReEvaluateComponent,
+                MockComponent(ReEvaluateMultipleChoiceQuestionComponent),
+                MockComponent(ReEvaluateDragAndDropQuestionComponent),
+                MockComponent(ReEvaluateShortAnswerQuestionComponent),
+                MockTranslateValuesDirective,
+                MockDirective(JhiTranslateDirective),
+                MockPipe(ArtemisDatePipe),
+            ],
+            providers: [
+                NgbModal,
+                QuizExercisePopupService,
+                { provide: ActivatedRoute, useValue: route },
+                { provide: TranslateService, useClass: MockTranslateService },
+                { provide: Router, useClass: MockRouter },
+            ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(QuizReEvaluateComponent);
+        comp = fixture.componentInstance;
+        quizService = fixture.debugElement.injector.get(QuizExerciseService);
+        quizExercise.id = 456;
+        const quizQuestion1 = new MultipleChoiceQuestion();
+        const quizQuestion2 = new DragAndDropQuestion();
+        quizExercise.quizQuestions = [quizQuestion1, quizQuestion2];
+        quizServiceFindSpy = stub(quizService, 'find').returns(of(new HttpResponse({ body: quizExercise })));
+    });
+
+    afterEach(() => {
+        restore();
+    });
+
+    it('Should initialize quiz exercise', () => {
+        comp.ngOnInit();
+        expect(comp.quizExercise).to.equal(quizExercise);
+        expect(quizServiceFindSpy).to.have.been.calledOnce;
+    });
+
+    it('Should delete quiz question', () => {
+        comp.ngOnInit();
+        expect(comp.quizExercise.quizQuestions!.length).to.equal(2);
+        comp.deleteQuestion(comp.quizExercise.quizQuestions![0]);
+        expect(comp.quizExercise.quizQuestions!.length).to.equal(1);
+    });
+
+    it('Should update quiz questions', () => {
+        comp.ngOnInit();
+        quizExercise.quizQuestions![0].points = 5;
+        comp.onQuestionUpdated();
+        expect(comp.quizExercise).to.equal(quizExercise);
+    });
+
+    it('Should update quiz questions', () => {
+        comp.ngOnInit();
+        quizExercise.quizQuestions![0].points = 5;
+        comp.onQuestionUpdated();
+        expect(comp.quizExercise).to.equal(quizExercise);
+    });
+
+    it('Should have pending changes', () => {
+        comp.ngOnInit();
+        comp.quizExercise.quizQuestions![0].points = 5;
+        expect(comp.pendingChanges()).to.be.true;
+    });
+});
