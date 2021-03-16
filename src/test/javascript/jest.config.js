cdfbbedbb9ffe47780459166c00781d190707d61
Stephan Krusche <s.krusche@tum.de>
2021-03-16 22:15:55 +0100

    update dependencies and test thresholds, try to fix flaky test with 20s timeout

--- src/test/javascript/jest.config.js
+++ src/test/javascript/jest.config.js
@@ -16,10 +16,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should be increase to at least 80%
-            statements: 72,
+            statements: 73,
             branches: 54,
-            functions: 61,
-            lines: 71,
+            functions: 62,
+            lines: 72,
         },
     },
     preset: 'jest-preset-angular',
