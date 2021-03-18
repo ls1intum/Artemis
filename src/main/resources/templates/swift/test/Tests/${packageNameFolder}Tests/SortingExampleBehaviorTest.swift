e489c028a593f76f4da25c7b3a527fd4d142ac1a
René Lalla <r.lalla@gmx.de>
2021-03-18 20:12:28 +0100

    adjust tests readme

--- src/main/resources/templates/swift/test/Tests/${packageNameFolder}Tests/SortingExampleBehaviorTest.swift
+++ src/main/resources/templates/swift/test/Tests/${packageNameFolder}Tests/SortingExampleBehaviorTest.swift
@@ -12,19 +12,19 @@ class SortingExampleBehaviorTest: XCTestCase {
     ]
 
     private func createOrderedDatesList(_ amount: Int) -> [Date] {
-		var list = [Date]()
-		let dateFormat = DateFormatter()
-		dateFormat.dateFormat = "dd.MM.yyyy"
-		dateFormat.timeZone = TimeZone(identifier: "UTC")
-		var orderedSeconds: Double = 0.00
-		for _ in 0 ..< amount {
-		    /// create random dates in an ascending order
-			orderedSeconds += Double.random(in: 1000000...1000000000)
-			let orderedDate: Date! = Date(timeIntervalSince1970: orderedSeconds)
-			list.append(orderedDate)
-		}
-		return list
-	}
+        var list = [Date]()
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "dd.MM.yyyy"
+        dateFormat.timeZone = TimeZone(identifier: "UTC")
+        var orderedSeconds: Double = 0.00
+        for _ in 0 ..< amount {
+            /// create random dates in an ascending order
+            orderedSeconds += Double.random(in: 1000000...1000000000)
+            let orderedDate: Date! = Date(timeIntervalSince1970: orderedSeconds)
+            list.append(orderedDate)
+        }
+        return list
+    }
 
     /// This is the setUp() instance method. It is called before each test method begins.
     override func setUp() {
