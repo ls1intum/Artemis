4087b72f3b080a7db8c63f00476c54954de5f695
René Lalla <r.lalla@gmx.de>
2021-03-21 13:55:47 +0100

    lock the correct and not illegal submission during assessment

--- src/main/java/de/tum/in/www1/artemis/service/programming/ProgrammingSubmissionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/programming/ProgrammingSubmissionService.java
@@ -729,10 +729,12 @@ public class ProgrammingSubmissionService extends SubmissionService {
         if (optionalExistingResult.isPresent()) {
             automaticFeedbacks = optionalExistingResult.get().getFeedbacks().stream().map(Feedback::copyFeedback).collect(Collectors.toList());
         }
-        // Create a new result (manual result) and try to reuse the existing submission with the latest commit hash
+        // Create a new result (manual result) and try to reuse the existing submission with the latest commit hash. In case the latest commit hash belongs to an ILLEGAL submission
+        // use the received submission.
         ProgrammingSubmission existingSubmission = getOrCreateSubmissionWithLastCommitHashForParticipation((ProgrammingExerciseStudentParticipation) submission.getParticipation(),
                 SubmissionType.MANUAL);
-        Result newResult = saveNewEmptyResult(existingSubmission);
+        Submission legalSubmission = SubmissionType.ILLEGAL.equals(existingSubmission.getType()) ? submission : existingSubmission;
+        Result newResult = saveNewEmptyResult(legalSubmission);
         newResult.setAssessor(userRepository.getUser());
         newResult.setAssessmentType(AssessmentType.SEMI_AUTOMATIC);
         // Copy automatic feedbacks into the manual result
@@ -750,7 +752,7 @@ public class ProgrammingSubmissionService extends SubmissionService {
         newResult.setAssessor(assessor);
         log.debug("Assessment locked with result id: " + newResult.getId() + " for assessor: " + newResult.getAssessor().getName());
         // Make sure that submission is set back after saving
-        newResult.setSubmission(existingSubmission);
+        newResult.setSubmission(legalSubmission);
         return newResult;
     }
 
