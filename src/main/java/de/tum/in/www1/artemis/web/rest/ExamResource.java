d7e7be7b41b0495874faa58e2039310762c476ce
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2021-03-23 22:08:04 +0100

    Fix the server style

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
@@ -9,7 +9,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpResponse;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
