9b0db818cb2b629f99fcd9d19762cabe1b1b2c8e
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2021-03-23 09:36:38 +0100

    Remove QuizStatus.java

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/QuizStatus.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.artemis.domain.enumeration;
-
-/**
- * Enum for defining the quiz status - analogue to the client quizStatus in the quizExercise model
- */
-public enum QuizStatus {
-    CLOSED, OPEN_FOR_PRACTICE, ACTIVE, VISIBLE, HIDDEN,
-}
