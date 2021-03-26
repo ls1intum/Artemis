18dea7d57eceaf47fe911ed6f7430465c257e35c
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-03-26 16:23:14 +0100

    Allow to configure the use of pseudonyms in Gitlab and Jenkins (#3071)

--- src/test/java/de/tum/in/www1/artemis/authentication/UserJenkinsGitlabIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/authentication/UserJenkinsGitlabIntegrationTest.java
@@ -6,17 +6,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationJenkinsGitlabTest;
+import de.tum.in.www1.artemis.service.connectors.gitlab.GitLabUserManagementService;
+import de.tum.in.www1.artemis.service.connectors.jenkins.JenkinsUserManagementService;
 import de.tum.in.www1.artemis.util.UserTestService;
 import de.tum.in.www1.artemis.web.rest.vm.ManagedUserVM;
 
 public class UserJenkinsGitlabIntegrationTest extends AbstractSpringIntegrationJenkinsGitlabTest {
 
+    @Value("${artemis.continuous-integration.user}")
+    private String jenkinsAdminUsername;
+
+    @Value("${jenkins.use-pseudonyms:#{false}}")
+    private boolean usePseudonymsJenkins;
+
+    @Value("${gitlab.use-pseudonyms:#{false}}")
+    private boolean usePseudonymsGitlab;
+
     @Autowired
     UserTestService userTestService;
+
+    @Autowired
+    JenkinsUserManagementService jenkinsUserManagementService;
+
+    @Autowired
+    GitLabUserManagementService gitLabUserManagementService;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -52,6 +71,41 @@ public class UserJenkinsGitlabIntegrationTest extends AbstractSpringIntegrationJ
     @WithMockUser(value = "tutor1", roles = "TA")
     public void updateUser_asTutor_forbidden() throws Exception {
         request.put("/api/users", new ManagedUserVM(userTestService.getStudent()), HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void createUserWithPseudonymsIsSuccessful() throws Exception {
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "usePseudonyms", true);
+        ReflectionTestUtils.setField(gitLabUserManagementService, "usePseudonyms", true);
+        userTestService.createUser_asAdmin_isSuccessful();
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "usePseudonyms", usePseudonymsJenkins);
+        ReflectionTestUtils.setField(gitLabUserManagementService, "usePseudonyms", usePseudonymsGitlab);
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void createAdminUserSkippedInJenkins() throws Exception {
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", "batman");
+        userTestService.createUser_asAdmin_isSuccessful();
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", jenkinsAdminUsername);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void deleteAdminUserSkippedInJenkins() throws Exception {
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", "student1");
+        userTestService.deleteUser_isSuccessful();
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", jenkinsAdminUsername);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void updateAdminUserSkippedInJenkins() throws Exception {
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", "student1");
+        userTestService.updateUser_asAdmin_isSuccessful();
+        ReflectionTestUtils.setField(jenkinsUserManagementService, "jenkinsAdminUsername", jenkinsAdminUsername);
     }
 
     @Test
