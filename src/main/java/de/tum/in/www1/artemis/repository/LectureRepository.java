218c1a7434ed50545cce930f7317bb0f553ed32a
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-03-28 18:34:26 +0200

    Fix an issue when deleting lectures, lecture units and fix flaky tests with null index column for collection errors (#3085)

--- src/main/java/de/tum/in/www1/artemis/repository/LectureRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/LectureRepository.java
@@ -3,12 +3,15 @@ package de.tum.in.www1.artemis.repository;
 import java.util.Optional;
 import java.util.Set;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.Lecture;
+import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
 
 /**
  * Spring Data repository for the Lecture entity.
@@ -56,4 +59,9 @@ public interface LectureRepository extends JpaRepository<Lecture, Long> {
             WHERE l.id = :lectureId
             """)
     String getLectureTitle(Long lectureId);
+
+    @NotNull
+    default Lecture findByIdWithStudentQuestionsAndLectureUnitsAndLearningGoalsElseThrow(Long lectureId) {
+        return findByIdWithStudentQuestionsAndLectureUnitsAndLearningGoals(lectureId).orElseThrow(() -> new EntityNotFoundException("Lecture", lectureId));
+    }
 }
