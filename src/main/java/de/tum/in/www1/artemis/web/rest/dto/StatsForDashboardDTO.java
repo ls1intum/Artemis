f4b5c6bda217e746f2f7ee8be5ef60a136a685bf
SimonEntholzer <nom.ise@outlook.com>
2021-03-29 12:30:45 +0200

    fixed displaying feedback/complaint information

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/StatsForDashboardDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/StatsForDashboardDTO.java
@@ -15,6 +15,10 @@ public class StatsForDashboardDTO {
 
     private Long totalNumberOfAssessmentLocks;
 
+    private Boolean complaintsEnabled;
+
+    private Boolean feedbackRequestEnabled;
+
     private DueDateStat[] numberOfAssessmentsOfCorrectionRounds;
 
     private DueDateStat[] numberOfLockedAssessmentByOtherTutorsOfCorrectionRound;
@@ -141,5 +145,21 @@ public class StatsForDashboardDTO {
 
     public void setNumberOfLockedAssessmentByOtherTutorsOfCorrectionRound(DueDateStat[] numberOfLockedAssessmentByOtherTutorsOfCorrectionRound) {
         this.numberOfLockedAssessmentByOtherTutorsOfCorrectionRound = numberOfLockedAssessmentByOtherTutorsOfCorrectionRound;
+    }
+
+    public Boolean getComplaintsEnabled() {
+        return complaintsEnabled;
+    }
+
+    public void setComplaintsEnabled(Boolean complaintsEnabled) {
+        this.complaintsEnabled = complaintsEnabled;
+    }
+
+    public Boolean getFeedbackRequestEnabled() {
+        return feedbackRequestEnabled;
+    }
+
+    public void setFeedbackRequestEnabled(Boolean feedbackRequestEnabled) {
+        this.feedbackRequestEnabled = feedbackRequestEnabled;
     }
 }
