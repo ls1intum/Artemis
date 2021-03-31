fed886a8d03d22a80c6b2771c89d9543b3ce5173
SimonEntholzer <nom.ise@outlook.com>
2021-03-31 19:15:22 +0200

    finished tests for guided tour utils

--- src/test/javascript/spec/component/utils/guided-tour.utils.spec.ts
+++ src/test/javascript/spec/component/utils/guided-tour.utils.spec.ts
@@ -1,21 +1,77 @@
 import * as chai from 'chai';
 import { Orientation } from 'app/guided-tour/guided-tour.constants';
-import { checkPageUrlEnding, clickOnElement, determineUrlMatching, getUrlParams } from 'app/guided-tour/guided-tour.utils';
+import {
+    clickOnElement,
+    calculateTopOffset,
+    calculateLeftOffset,
+    isElementInViewPortHorizontally,
+    checkPageUrlEnding,
+    determineUrlMatching,
+    getUrlParams,
+} from 'app/guided-tour/guided-tour.utils';
+import * as sinon from 'sinon';
+import { stub } from 'sinon';
 
 const expect = chai.expect;
 
 describe('GuidedTourUtils', () => {
     describe('clickOnElement', () => {
-        it('should', () => {});
-    });
-    describe('calculateTopOffset', () => {
-        it('should', () => {});
+        it('should clickOnElement', () => {
+            let querySelectorStub = stub(document, 'querySelector');
+            let mockElement = { click: () => {} } as HTMLElement;
+            querySelectorStub.returns(mockElement);
+            clickOnElement('ab');
+            sinon.assert.called(querySelectorStub);
+        });
     });
     describe('calculateLeftOffset', () => {
-        it('should', () => {});
+        it('should calculateLeftOffset', () => {
+            let offsetParent1 = { offsetLeft: 4 } as any;
+            let offsetParent2 = { offsetLeft: 2, offsetParent: offsetParent1 } as any;
+            let dummyElement2 = { offsetLeft: 4, offsetParent: offsetParent2 } as HTMLElement;
+            expect(calculateLeftOffset(dummyElement2)).to.be.equal(10);
+        });
+    });
+    describe('calculateTopOffset', () => {
+        it('should calculateTopOffset', () => {
+            let offsetParent1 = { offsetTop: 4 } as any;
+            let offsetParent2 = { offsetTop: 2, offsetParent: offsetParent1 } as any;
+            let dummyElement2 = { offsetTop: 4, offsetParent: offsetParent2 } as HTMLElement;
+            expect(calculateTopOffset(dummyElement2)).to.be.equal(10);
+        });
     });
     describe('isElementInViewPortHorizontally', () => {
-        it('should', () => {});
+        // window.width is 1024
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.TOPLEFT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 100)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 1000, 1000, 1000)).to.be.false;
+        });
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.BOTTOMLEFT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 100)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 1000, 1000, 1000)).to.be.false;
+        });
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.LEFT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 0, 50)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 100, 0, 150)).to.be.false;
+        });
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.TOPRIGHT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 100)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 300)).to.be.false;
+        });
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.BOTTOMRIGHT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 100)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 300)).to.be.false;
+        });
+        it('should isElementInViewPortHorizontally', () => {
+            let topleft = Orientation.RIGHT;
+            expect(isElementInViewPortHorizontally(topleft, 100, 100, 100)).to.be.true;
+            expect(isElementInViewPortHorizontally(topleft, 1000, 1000, 1000)).to.be.false;
+        });
     });
     describe('determineUrlMatching', () => {
         it('should match with empty pageUrl', () => {
@@ -40,8 +96,13 @@ describe('GuidedTourUtils', () => {
         });
         it('should match more', () => {
             const pageUrl = '/this/is/some/guided/tour/url?param=true';
-            const tourStepUrl = 'is*';
-            expect(determineUrlMatching(pageUrl, tourStepUrl)).to.deep.equal(['is']);
+            const tourStepUrl = 'some';
+            expect(determineUrlMatching(pageUrl, tourStepUrl)).to.deep.equal(['some']);
+        });
+        it('should match with params in tourStepUrl', () => {
+            const pageUrl = '/this/is/some/guided/tour/url?param=true';
+            const tourStepUrl = 'some?param=false';
+            expect(determineUrlMatching(pageUrl, tourStepUrl)).to.deep.equal(['some']);
         });
     });
     describe('getUrlParams', () => {
