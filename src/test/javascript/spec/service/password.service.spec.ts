780287691c1f87b6f63dccdaae3224c584e9d3cd
ivohashamov <ivohashumov17@gmail.com>
2021-03-31 23:52:30 +0300

    resolve merge conflict

---
+++ src/test/javascript/spec/service/password.service.spec.ts
@@ -0,0 +1,38 @@
+import { async } from '@angular/core/testing';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { SinonStub, stub } from 'sinon';
+import { MockHttpService } from '../helpers/mocks/service/mock-http.service';
+import { SERVER_API_URL } from 'app/app.constants';
+import { PasswordService } from 'app/account/password/password.service';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('ActivateService', () => {
+    let passwordService: PasswordService;
+    let httpService: MockHttpService;
+    let postStub: SinonStub;
+
+    const postURL = SERVER_API_URL + 'api/account/change-password';
+
+    beforeEach(async(() => {
+        httpService = new MockHttpService();
+        // @ts-ignore
+        passwordService = new PasswordService(httpService);
+        postStub = stub(httpService, 'post');
+    }));
+
+    afterEach(() => {
+        postStub.restore();
+    });
+
+    it('should set a new password for the current user', async () => {
+        const newPassword = 'newPassword';
+        const currentPassword = 'currentPassword';
+
+        await passwordService.save(newPassword, currentPassword);
+
+        expect(postStub).to.have.been.calledOnceWithExactly(postURL, { currentPassword, newPassword });
+    });
+});
