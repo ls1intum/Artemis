e9e86bdb155bf79ed71b055cf101267b7785b280
ivohashamov <ivohashumov17@gmail.com>
2021-04-03 12:53:30 +0300

    add additional test cases

--- src/test/javascript/spec/component/exam/manage/exam-exercise-row-buttons.component.spec.ts
+++ src/test/javascript/spec/component/exam/manage/exam-exercise-row-buttons.component.spec.ts
@@ -29,6 +29,11 @@ import { ProgrammingExerciseGroupCellComponent } from 'app/exam/manage/exercise-
 import { QuizExerciseGroupCellComponent } from 'app/exam/manage/exercise-groups/quiz-exercise-cell/quiz-exercise-group-cell.component';
 import { TextExerciseService } from 'app/exercises/text/manage/text-exercise/text-exercise.service';
 import { DueDateStat } from 'app/course/dashboards/instructor-course-dashboard/due-date-stat.model';
+import { ModelingExerciseService } from 'app/exercises/modeling/manage/modeling-exercise.service';
+import { FileUploadExerciseService } from 'app/exercises/file-upload/manage/file-upload-exercise.service';
+import { QuizExerciseService } from 'app/exercises/quiz/manage/quiz-exercise.service';
+import { ProgrammingExerciseService } from 'app/exercises/programming/manage/services/programming-exercise.service';
+import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -44,9 +49,41 @@ describe('Exam Exercise Row Buttons Component', () => {
 
     const dueDateStat = { inTime: 0, late: 0 } as DueDateStat;
 
-    const fileExercise = { id: 1, type: ExerciseType.FILE_UPLOAD, maxPoints: 100 };
-    const programmingExercise = { id: 2, type: ExerciseType.PROGRAMMING, maxPoints: 100 };
-    const modelingExercise = { id: 3, type: ExerciseType.MODELING, maxPoints: 100 };
+    const quizQuestions = [
+        {
+            id: 1,
+            text: 'text1',
+        },
+        {
+            id: 2,
+            text: 'text2',
+        },
+    ];
+
+    const fileExercise = {
+        id: 1,
+        type: ExerciseType.FILE_UPLOAD,
+        maxPoints: 100,
+        secondCorrectionEnabled: false,
+        studentAssignedTeamIdComputed: false,
+        numberOfAssessmentsOfCorrectionRounds: [dueDateStat],
+    };
+    const programmingExercise = {
+        id: 2,
+        type: ExerciseType.PROGRAMMING,
+        maxPoints: 100,
+        secondCorrectionEnabled: false,
+        studentAssignedTeamIdComputed: false,
+        numberOfAssessmentsOfCorrectionRounds: [dueDateStat],
+    };
+    const modelingExercise = {
+        id: 3,
+        type: ExerciseType.MODELING,
+        maxPoints: 100,
+        secondCorrectionEnabled: false,
+        studentAssignedTeamIdComputed: false,
+        numberOfAssessmentsOfCorrectionRounds: [dueDateStat],
+    };
     const textExercise = {
         id: 4,
         type: ExerciseType.TEXT,
@@ -55,6 +92,15 @@ describe('Exam Exercise Row Buttons Component', () => {
         studentAssignedTeamIdComputed: false,
         numberOfAssessmentsOfCorrectionRounds: [dueDateStat],
     };
+    const quizExercise = {
+        id: 5,
+        type: ExerciseType.QUIZ,
+        maxPoints: 100,
+        secondCorrectionEnabled: false,
+        studentAssignedTeamIdComputed: false,
+        numberOfAssessmentsOfCorrectionRounds: [dueDateStat],
+        quizQuestions,
+    };
 
     let comp: ExamExerciseRowButtonsComponent;
     let fixture: ComponentFixture<ExamExerciseRowButtonsComponent>;
@@ -62,6 +108,10 @@ describe('Exam Exercise Row Buttons Component', () => {
     let jhiEventManager: JhiEventManager;
     let router: Router;
     let textExerciseService: TextExerciseService;
+    let modelingExerciseService: ModelingExerciseService;
+    let fileUploadExerciseService: FileUploadExerciseService;
+    let quizExerciseService: QuizExerciseService;
+    let programmingExerciseService: ProgrammingExerciseService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -89,6 +139,10 @@ describe('Exam Exercise Row Buttons Component', () => {
 
         jhiEventManager = TestBed.inject(JhiEventManager);
         textExerciseService = TestBed.inject(TextExerciseService);
+        modelingExerciseService = TestBed.inject(ModelingExerciseService);
+        fileUploadExerciseService = TestBed.inject(FileUploadExerciseService);
+        quizExerciseService = TestBed.inject(QuizExerciseService);
+        programmingExerciseService = TestBed.inject(ProgrammingExerciseService);
 
         router = TestBed.inject(Router);
     });
@@ -104,37 +158,104 @@ describe('Exam Exercise Row Buttons Component', () => {
 
     describe('check exam is over', () => {
         it('should check exam is over is true', fakeAsync(() => {
-            // setup
             comp.latestIndividualEndDate = moment().subtract(1, 'days');
 
-            // call
             const isExamOver = comp.isExamOver();
 
-            // check
             expect(isExamOver).to.be.true;
         }));
 
         it('should check exam is over is false', fakeAsync(() => {
-            //setup
             comp.latestIndividualEndDate = moment().add(1, 'days');
+
             const isExamOver = comp.isExamOver();
+
             expect(isExamOver).to.be.false;
         }));
     });
 
-    it('should delete textExercise', fakeAsync(() => {
-        // setup
-        comp.exercise = textExercise;
-        const textExerciseServiceDeleteStub = sinon.stub(textExerciseService, 'delete').returns(
-            of(
-                new HttpResponse<{}>({ body: [] }),
-            ),
-        );
+    describe('check exercise deletion', () => {
+        it('should delete textExercise', fakeAsync(() => {
+            // setup
+            comp.exercise = textExercise;
+            const textExerciseServiceDeleteStub = sinon.stub(textExerciseService, 'delete').returns(
+                of(
+                    new HttpResponse<{}>({ body: [] }),
+                ),
+            );
 
-        // call
-        comp.deleteExercise();
+            comp.deleteExercise();
 
-        // check
-        expect(textExerciseServiceDeleteStub).to.have.been.calledOnceWith(textExercise.id);
-    }));
+            expect(textExerciseServiceDeleteStub).to.have.been.calledOnceWith(textExercise.id);
+        }));
+
+        it('should delete modelingExercise', fakeAsync(() => {
+            comp.exercise = modelingExercise;
+            const modelingExerciseServiceDeleteStub = sinon.stub(modelingExerciseService, 'delete').returns(
+                of(
+                    new HttpResponse<{}>({ body: [] }),
+                ),
+            );
+
+            comp.deleteExercise();
+
+            expect(modelingExerciseServiceDeleteStub).to.have.been.calledOnceWith(modelingExercise.id);
+        }));
+
+        it('should delete fileExercise', fakeAsync(() => {
+            comp.exercise = fileExercise;
+            const fileExerciseServiceDeleteStub = sinon.stub(fileUploadExerciseService, 'delete').returns(
+                of(
+                    new HttpResponse<{}>({ body: [] }),
+                ),
+            );
+
+            comp.deleteExercise();
+
+            expect(fileExerciseServiceDeleteStub).to.have.been.calledOnceWith(fileExercise.id);
+        }));
+
+        it('should delete quizExercise', fakeAsync(() => {
+            comp.exercise = quizExercise;
+            const quizExerciseServiceDeleteStub = sinon.stub(quizExerciseService, 'delete').returns(
+                of(
+                    new HttpResponse<{}>({ body: [] }),
+                ),
+            );
+
+            comp.deleteExercise();
+
+            expect(quizExerciseServiceDeleteStub).to.have.been.calledOnceWith(quizExercise.id);
+        }));
+
+        it('should delete programmingExercise', fakeAsync(() => {
+            comp.exercise = programmingExercise;
+            const programmingExerciseServiceDeleteStub = sinon.stub(programmingExerciseService, 'delete').returns(
+                of(
+                    new HttpResponse<{}>({ body: [] }),
+                ),
+            );
+
+            comp.deleteProgrammingExercise({ deleteStudentReposBuildPlans: true, deleteBaseReposBuildPlans: false });
+
+            expect(programmingExerciseServiceDeleteStub).to.have.been.calledOnceWith(programmingExercise.id, true, false);
+        }));
+    });
+
+    describe('check quiz is being exported', () => {
+        it('should export quiz exercise by id', fakeAsync(() => {
+            comp.exercise = quizExercise;
+            const quizExerciseServiceFindStub = sinon.stub(quizExerciseService, 'find').returns(
+                of(
+                    new HttpResponse<QuizExercise>({ body: quizExercise }),
+                ),
+            );
+            const quizExerciseServiceExportQuizStub = sinon.stub(quizExerciseService, 'exportQuiz');
+
+            comp.exportQuizById(true);
+
+            expect(quizExerciseServiceFindStub).to.have.been.calledOnceWith(quizExercise.id);
+            expect(quizExerciseServiceExportQuizStub).to.have.been.calledOnceWith(quizQuestions, true);
+        }));
+    });
 });
