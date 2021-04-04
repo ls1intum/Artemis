988d911fe4a74db6a374f812cc47bd3ef38bb8fa
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-04-05 01:05:39 +0200

    Add test for course exercise card

---
+++ src/test/javascript/spec/component/course/course-exercise-card.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+import { CourseExerciseCardComponent } from 'app/course/manage/course-exercise-card.component';
+import { CourseManagementService } from 'app/course/manage/course-management.service';
+import * as chai from 'chai';
+import { JhiTranslateDirective } from 'ng-jhipster';
+import { MockDirective, MockProvider } from 'ng-mocks';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import { ArtemisTestModule } from '../../test.module';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('Course Management Detail Component', () => {
+    let comp: CourseExerciseCardComponent;
+    let fixture: ComponentFixture<CourseExerciseCardComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [CourseExerciseCardComponent, MockDirective(NgbCollapse), MockDirective(JhiTranslateDirective)],
+            providers: [MockProvider(CourseManagementService)],
+        }).compileComponents();
+        fixture = TestBed.createComponent(CourseExerciseCardComponent);
+        comp = fixture.componentInstance;
+    });
+
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    it('should initialize', () => {
+        fixture.detectChanges();
+        expect(CourseExerciseCardComponent).to.be.ok;
+    });
+});
