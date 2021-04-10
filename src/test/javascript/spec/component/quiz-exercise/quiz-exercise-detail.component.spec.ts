4791a4b5620e04f017d83cc8ffc042cc70b5b3aa
Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>
2021-04-10 10:30:23 +0200

    Fix the test case style

--- src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/quiz-exercise/quiz-exercise-detail.component.spec.ts
@@ -239,7 +239,9 @@ describe('QuizExercise Management Detail Component', () => {
         });
 
         describe('with exam id', () => {
-            const testRoute = ({ snapshot: { paramMap: convertToParamMap({ courseId: course.id, exerciseId: quizExercise.id, examId: 1, exerciseGroupId: 2 }) } } as any) as ActivatedRoute;
+            const testRoute = ({
+                snapshot: { paramMap: convertToParamMap({ courseId: course.id, exerciseId: quizExercise.id, examId: 1, exerciseGroupId: 2 }) },
+            } as any) as ActivatedRoute;
             beforeEach(waitForAsync(() => configureTestBed(testRoute)));
             beforeEach(configureFixtureAndServices);
             it('should call exerciseGroupService.find', () => {
