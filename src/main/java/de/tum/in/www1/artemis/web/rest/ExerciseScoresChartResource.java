57e110f6adeb6186ee5b081d4bd1f4e837f9fa53
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-04-10 12:41:48 +0200

    Refactored ExerciseScoresChartResource

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseScoresChartResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseScoresChartResource.java
@@ -1,13 +1,9 @@
 package de.tum.in.www1.artemis.web.rest;
 
-import de.tum.in.www1.artemis.domain.Course;
-import de.tum.in.www1.artemis.domain.Exercise;
-import de.tum.in.www1.artemis.domain.User;
-import de.tum.in.www1.artemis.repository.CourseRepository;
-import de.tum.in.www1.artemis.repository.UserRepository;
-import de.tum.in.www1.artemis.service.AuthorizationCheckService;
-import de.tum.in.www1.artemis.service.ExerciseScoresChartService;
-import de.tum.in.www1.artemis.web.rest.dto.ExerciseScoresDTO;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +13,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import de.tum.in.www1.artemis.domain.Course;
+import de.tum.in.www1.artemis.domain.Exercise;
+import de.tum.in.www1.artemis.domain.User;
+import de.tum.in.www1.artemis.repository.CourseRepository;
+import de.tum.in.www1.artemis.repository.UserRepository;
+import de.tum.in.www1.artemis.security.Role;
+import de.tum.in.www1.artemis.service.AuthorizationCheckService;
+import de.tum.in.www1.artemis.service.ExerciseScoresChartService;
+import de.tum.in.www1.artemis.web.rest.dto.ExerciseScoresDTO;
 
-import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.forbidden;
-import static de.tum.in.www1.artemis.web.rest.util.ResponseUtil.notFound;
-
+/**
+ * Controller to provide the necessary data for the exercise-scores-chart.component.ts in the client
+ */
 @RestController
 @RequestMapping("/api")
 public class ExerciseScoresChartResource {
@@ -40,7 +41,7 @@ public class ExerciseScoresChartResource {
     private final AuthorizationCheckService authorizationCheckService;
 
     public ExerciseScoresChartResource(ExerciseScoresChartService exerciseScoresChartService, CourseRepository courseRepository, UserRepository userRepository,
-                                       AuthorizationCheckService authorizationCheckService) {
+            AuthorizationCheckService authorizationCheckService) {
         this.exerciseScoresChartService = exerciseScoresChartService;
         this.courseRepository = courseRepository;
         this.authorizationCheckService = authorizationCheckService;
@@ -53,29 +54,33 @@ public class ExerciseScoresChartResource {
      * This call returns the the information used for the exercise-scores-chart. It will get get the score of
      * the requesting user,the average score and the best score in course exercises
      * <p>
-     * Only released course exercises with assessment due date over are considered!
+     * Note: Only released course exercises with assessment due date over are considered!
      * <p>
      *
      * @return the ResponseEntity with status 200 (OK) and with the exercise scores in the body
      */
-    @GetMapping("/courses/{courseId}/charts/exercise-scores")
+    @GetMapping(Endpoints.COURSE_EXERCISE_SCORES)
     @PreAuthorize("hasAnyRole('USER', 'TA', 'INSTRUCTOR', 'ADMIN')")
     public ResponseEntity<List<ExerciseScoresDTO>> getCourseExerciseScores(@PathVariable Long courseId) {
         log.debug("REST request to get exercise scores for course with id: {}", courseId);
-        Optional<Course> courseOptional = Optional.ofNullable(courseRepository.findWithEagerExercisesById(courseId));
-        if (courseOptional.isEmpty()) {
-            return notFound();
-        }
-        Course course = courseOptional.get();
+        Course course = courseRepository.findByIdWithEagerExercisesElseThrow(courseId);
         User user = userRepository.getUserWithGroupsAndAuthorities();
-        if (!authorizationCheckService.isAtLeastStudentInCourse(course, user)) {
-            return forbidden();
-        }
+        authorizationCheckService.checkHasAtLeastRoleInCourseElseThrow(Role.STUDENT, course, user);
         // we only consider exercises in which the student had a chance to earn a score (released and due date over)
-        Set<Exercise> exercisesToConsider = course.getExercises().stream().filter(Exercise::isVisibleToStudents).filter(Exercise::isAssessmentDueDateOver)
-            .collect(Collectors.toSet());
-        List<ExerciseScoresDTO> exerciseScoresDTOList = exerciseScoresChartService.getExerciseScores(exercisesToConsider, user);
+        List<ExerciseScoresDTO> exerciseScoresDTOList = exerciseScoresChartService.getExerciseScores(removeNotVisibleAndUnfinishedExercise(course.getExercises()), user);
         return ResponseEntity.ok(exerciseScoresDTOList);
+    }
+
+    private Set<Exercise> removeNotVisibleAndUnfinishedExercise(Set<Exercise> exercises) {
+        return exercises.parallelStream().filter(Exercise::isVisibleToStudents).filter(Exercise::isAssessmentDueDateOver).collect(Collectors.toSet());
+    }
+
+    public static final class Endpoints {
+
+        public static final String COURSE_EXERCISE_SCORES = "/courses/{courseId}/charts/exercise-scores";
+
+        private Endpoints() {
+        }
     }
 
 }
--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/ExerciseScoresDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/ExerciseScoresDTO.java
@@ -5,10 +5,16 @@ import java.time.ZonedDateTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
- * This DTO contains the information used for the exercise-scores-chart
+ * This DTO contains the information used for the exercise-scores-chart.component.ts
+ * For every exercise we send the score of the requesting student, the average score achieved and the max score
+ * achieved
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ExerciseScoresDTO {
+
+    public ExerciseScoresDTO() {
+        // empty constructor for jackson
+    }
 
     public Long exerciseId;
 
