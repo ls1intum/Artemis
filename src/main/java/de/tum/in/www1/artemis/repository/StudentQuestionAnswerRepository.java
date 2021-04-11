f475e4033c20d6f643b7e7527d3396f8d761fcf0
Patrick Zintel <pat.zintel@tum.de>
2021-04-11 12:44:07 +0200

    resolved compilaton issues

--- src/main/java/de/tum/in/www1/artemis/repository/StudentQuestionAnswerRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/StudentQuestionAnswerRepository.java
@@ -1,5 +1,7 @@
 package de.tum.in.www1.artemis.repository;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
