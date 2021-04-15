e93dbd91abae7cc727edfe2c2d98a4fa5a9234b4
Stefan Waldhauser <hello@stefanwaldhauser.me>
2021-04-15 22:46:11 +0200

    Moved filtering logic into own method

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseScoresChartResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseScoresChartResource.java
@@ -1,5 +1,18 @@
 package de.tum.in.www1.artemis.web.rest;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.Exercise;
 import de.tum.in.www1.artemis.domain.User;
@@ -9,19 +22,6 @@ import de.tum.in.www1.artemis.security.Role;
 import de.tum.in.www1.artemis.service.AuthorizationCheckService;
 import de.tum.in.www1.artemis.service.ExerciseScoresChartService;
 import de.tum.in.www1.artemis.web.rest.dto.ExerciseScoresDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Controller to provides endpoints to query the necessary data for the exercise-scores-chart.component.ts in the client
@@ -67,16 +67,27 @@ public class ExerciseScoresChartResource {
         User user = userRepository.getUserWithGroupsAndAuthorities();
         authorizationCheckService.checkHasAtLeastRoleInCourseElseThrow(Role.STUDENT, course, user);
         // we only consider exercises in which the student had a chance to earn a score (released and due date over)
-        List<ExerciseScoresDTO> exerciseScoresDTOList = exerciseScoresChartService.getExerciseScores(removeNotVisibleAndUnfinishedExercise(course.getExercises()), user);
+        List<ExerciseScoresDTO> exerciseScoresDTOList = exerciseScoresChartService.getExerciseScores(filterExercises(course.getExercises()), user);
         return ResponseEntity.ok(exerciseScoresDTOList);
     }
 
-    private Set<Exercise> removeNotVisibleAndUnfinishedExercise(Set<Exercise> exercises) {
-        return exercises.parallelStream()
-            .filter(Exercise::isVisibleToStudents)
-            .filter(exercise -> Objects.isNull(exercise.getDueDate()) || ZonedDateTime.now().isAfter(exercise.getDueDate()))
-            .filter(Exercise::isAssessmentDueDateOver)
-            .collect(Collectors.toSet());
+    private Set<Exercise> filterExercises(Set<Exercise> exercises) {
+        return exercises.parallelStream().filter(exercise -> isExerciseFinished(exercise)).collect(Collectors.toSet());
+    }
+
+    private boolean isExerciseFinished(Exercise exercise) {
+        // Exercises not visible to the students are not finished either
+        if (!exercise.isVisibleToStudents()) {
+            return false;
+        }
+
+        // If an assessment due date is set, only count the exercise as finished if we passed that date
+        if (exercise.getAssessmentDueDate() != null) {
+            return ZonedDateTime.now().isAfter(exercise.getAssessmentDueDate());
+        }
+
+        // If no assessment due date is set, make sure to only count exercises which have a passed due date
+        return exercise.getDueDate() != null && ZonedDateTime.now().isAfter(exercise.getDueDate());
     }
 
     public static final class EndpointConstants {
