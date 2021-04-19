cb102697c5d80c5f5f4827e9e325c51c8065e54b
Nicolas Ruscher <nicolas.ruscher@tum.de>
2021-04-19 10:40:23 +0200

    fix merge problem

--- src/main/webapp/app/exercises/programming/assess/programming-assessment.route.ts
+++ src/main/webapp/app/exercises/programming/assess/programming-assessment.route.ts
@@ -5,60 +5,6 @@ import { CodeEditorTutorAssessmentContainerComponent } from 'app/exercises/progr
 import { Authority } from 'app/shared/constants/authority.constants';
 import { ProgrammingAssessmentDashboardComponent } from 'app/exercises/programming/assess/programming-assessment-dashboard/programming-assessment-dashboard.component';
 
-@Injectable({ providedIn: 'root' })
-export class NewStudentParticipationResolver implements Resolve<number | undefined> {
-    constructor(private programmingSubmissionService: ProgrammingSubmissionService, private jhiAlertService: JhiAlertService) {}
-
-    /**
-     * Resolves the needed studentParticipationId for the CodeEditorTutorAssessmentContainerComponent for submissions without assessment
-     * @param route
-     */
-    resolve(route: ActivatedRouteSnapshot) {
-        const exerciseId = Number(route.paramMap.get('exerciseId'));
-        const correctionRound = Number(route.queryParamMap.get('correction-round'));
-        if (exerciseId) {
-            const returnValue = this.programmingSubmissionService.getProgrammingSubmissionForExerciseForCorrectionRoundWithoutAssessment(exerciseId, true, correctionRound).pipe(
-                map((submission) => submission.participation!.id!),
-                catchError((error) => {
-                    if (error.error && error.error.errorKey === 'lockedSubmissionsLimitReached') {
-                        this.jhiAlertService.error('artemisApp.submission.lockedSubmissionsLimitReached');
-                    }
-                    return Observable.of(error);
-                }),
-            );
-            return returnValue;
-        }
-        return Observable.of(undefined);
-    }
-}
-
-@Injectable({ providedIn: 'root' })
-export class StudentParticipationResolver implements Resolve<number | undefined> {
-    constructor(private programmingSubmissionService: ProgrammingSubmissionService, private jhiAlertService: JhiAlertService) {}
-
-    /**
-     *
-     * Locks the latest submission of a programming exercises participation, if it is not already locked
-     * @param route
-     */
-    resolve(route: ActivatedRouteSnapshot) {
-        const participationId = Number(route.paramMap.get('participationId'));
-        const correctionRound = Number(route.queryParamMap.get('correction-round'));
-        if (participationId) {
-            return this.programmingSubmissionService.lockAndGetProgrammingSubmissionParticipation(participationId, correctionRound).pipe(
-                map((participation) => participation.id),
-                catchError((error) => {
-                    if (error.error && error.error.errorKey === 'lockedSubmissionsLimitReached') {
-                        this.jhiAlertService.error('artemisApp.submission.lockedSubmissionsLimitReached');
-                    }
-                    return Observable.of(error);
-                }),
-            );
-        }
-        return Observable.of(undefined);
-    }
-}
-
 export const routes: Routes = [
     {
         path: ':courseId/programming-exercises/:exerciseId/assessment',
