32ee7397a4d841c9272fce67410e89da6e425fe2
Robert Schauer <ga94bit@mytum.de>
2021-04-19 22:01:51 +0200

    Added a component for collapsing large feedback walls of text

---
+++ src/main/webapp/app/exercises/shared/result/feedback-collapse.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'jhi-feedback-collapse',
+    styleUrls: ['./result-detail.scss'],
+    templateUrl: './feedback-collapse.component.html',
+})
+export class FeedbackCollapseComponent {
+    @Input() text = '';
+    smallFeedbackCharacterLimit = 200;
+    isCollapsed = true;
+}
