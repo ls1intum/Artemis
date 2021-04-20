34ebc067c27f1db9708d1234a13532f15c6d404b
Robert Schauer <ga94bit@mytum.de>
2021-04-20 20:46:11 +0200

    renamed attribute and added comment documentation

--- src/main/webapp/app/exercises/shared/result/feedback-collapse.component.ts
+++ src/main/webapp/app/exercises/shared/result/feedback-collapse.component.ts
@@ -5,8 +5,13 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./result-detail.scss'],
     templateUrl: './feedback-collapse.component.html',
 })
+/**
+ * smallCharacterLimit can be adjusted make smaller or bigger items collapsable
+ * isCollapsed tracks whether an item is currently open or closed
+ * text is any string passed to the component
+ */
 export class FeedbackCollapseComponent {
     @Input() text: string;
-    smallFeedbackCharacterLimit = 200;
+    smallCharacterLimit = 200;
     isCollapsed = true;
 }
