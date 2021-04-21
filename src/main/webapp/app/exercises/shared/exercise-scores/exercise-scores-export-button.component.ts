047b534e3525c76cd4e87c48252a6390e3465ff4
Daniel Crazzolara <crazzolaradaniel@gmail.com>
2021-04-21 15:32:05 +0200

    Extracted and reused export results button and added tests

---
+++ src/main/webapp/app/exercises/shared/exercise-scores/exercise-scores-export-button.component.ts
@@ -0,0 +1,110 @@
+import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+import { round } from 'app/shared/util/utils';
+import { ProgrammingExerciseStudentParticipation } from 'app/entities/participation/programming-exercise-student-participation.model';
+import { Exercise, ExerciseType } from 'app/entities/exercise.model';
+import { Injectable, Component, Input } from '@angular/core';
+import { Result } from 'app/entities/result.model';
+import { ParticipationType } from 'app/entities/participation/participation.model';
+import { addUserIndependentRepositoryUrl } from 'app/overview/participation-utils';
+import { HttpResponse } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { ResultService } from 'app/exercises/shared/result/result.service';
+import { DifferencePipe } from 'ngx-moment';
+import { Moment } from 'moment';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+
+@Component({
+    selector: 'jhi-exercise-scores-export-button',
+    template: `
+        <button class="btn btn-info btn-sm mr-1" (click)="exportResults()">
+            <fa-icon [icon]="'download'"></fa-icon>
+            <span class="d-none d-md-inline" jhiTranslate="artemisApp.exercise.exportResults">Export Results</span>
+        </button>
+    `,
+})
+@Injectable({ providedIn: 'root' })
+export class ExerciseScoresExportButtonComponent {
+    @Input() exercises: Exercise[] = []; // Used to export multiple scores together
+    @Input() exercise: Exercise | ProgrammingExercise;
+
+    constructor(private resultService: ResultService, private momentDiff: DifferencePipe) {}
+
+    /**
+     * Fetches all results for an exercise and assigns them to the results in this component
+     */
+    getResults() {
+        return this.resultService
+            .getResultsForExercise(this.exercise.id!, {
+                withSubmissions: this.exercise.type === ExerciseType.MODELING,
+            })
+            .pipe(
+                tap((res: HttpResponse<Result[]>) => {
+                    return res.body!.map((result) => {
+                        result.participation!.results = [result];
+                        (result.participation! as StudentParticipation).exercise = this.exercise;
+                        if (result.participation!.type === ParticipationType.PROGRAMMING) {
+                            addUserIndependentRepositoryUrl(result.participation!);
+                        }
+                        result.durationInMinutes = this.durationInMinutes(
+                            result.completionDate!,
+                            result.participation!.initializationDate ? result.participation!.initializationDate : this.exercise.releaseDate!,
+                        );
+                        // Nest submission into participation so that it is available for the result component
+                        if (result.participation && result.submission) {
+                            result.participation.submissions = [result.submission];
+                        }
+                        return result;
+                    });
+                }),
+            );
+    }
+
+    /**
+     * Exports the exercise results as a csv file
+     */
+    exportResults() {
+        if (this.exercises.length === 0 && this.exercise !== undefined) {
+            this.exercises = this.exercises.concat(this.exercise);
+        }
+        this.exercises.forEach((exercise) => {
+            this.exercise = exercise;
+            this.getResults().subscribe((data) => {
+                const rows: string[] = [];
+                const results = data.body || [];
+                results.forEach((result, index) => {
+                    const studentParticipation = result.participation! as StudentParticipation;
+                    const { participantName, participantIdentifier } = studentParticipation;
+                    const score = round(result.score);
+
+                    if (index === 0) {
+                        const nameAndUserNameColumnHeaders = studentParticipation.team ? 'Team Name,Team Short Name' : 'Name,Username';
+                        const optionalStudentsColumnHeader = studentParticipation.team ? ',Students' : '';
+                        if (this.exercise.type !== ExerciseType.PROGRAMMING) {
+                            rows.push(`data:text/csv;charset=utf-8,${nameAndUserNameColumnHeaders},Score${optionalStudentsColumnHeader}`);
+                        } else {
+                            rows.push(`data:text/csv;charset=utf-8,${nameAndUserNameColumnHeaders},Score,Repo Link${optionalStudentsColumnHeader}`);
+                        }
+                    }
+                    const optionalStudentsColumnValue = studentParticipation.team ? `,"${studentParticipation.team?.students?.map((s) => s.name).join(', ')}"` : '';
+                    if (this.exercise.type !== ExerciseType.PROGRAMMING) {
+                        rows.push(`${participantName},${participantIdentifier},${score}${optionalStudentsColumnValue}`);
+                    } else {
+                        const repoLink = (studentParticipation as ProgrammingExerciseStudentParticipation).repositoryUrl;
+                        rows.push(`${participantName},${participantIdentifier},${score},${repoLink}${optionalStudentsColumnValue}`);
+                    }
+                });
+                const csvContent = rows.join('\n');
+                const encodedUri = encodeURI(csvContent);
+                const link = document.createElement('a');
+                link.setAttribute('href', encodedUri);
+                link.setAttribute('download', 'results-scores.csv');
+                document.body.appendChild(link); // Required for FF
+                link.click();
+            });
+        });
+    }
+
+    private durationInMinutes(completionDate: Moment, initializationDate: Moment) {
+        return this.momentDiff.transform(completionDate, initializationDate, 'minutes');
+    }
+}
---
+++ src/test/javascript/spec/component/shared/exercise-scores-export-button.component.spec.ts
@@ -0,0 +1,118 @@
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import { TranslateService } from '@ngx-translate/core';
+import { DifferencePipe } from 'ngx-moment';
+import { ExerciseScoresExportButtonComponent } from 'app/exercises/shared/exercise-scores/exercise-scores-export-button.component';
+import { ArtemisTestModule } from '../../test.module';
+import { of } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { ResultService } from 'app/exercises/shared/result/result.service';
+import { Result } from 'app/entities/result.model';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { Course } from 'app/entities/course.model';
+import { ExerciseGroup } from 'app/entities/exercise-group.model';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('ExerciseScoresExportButtonComponent', () => {
+    let component: ExerciseScoresExportButtonComponent;
+    let fixture: ComponentFixture<ExerciseScoresExportButtonComponent>;
+    let resultService: ResultService;
+
+    const course1 = new Course();
+    course1.id = 1;
+
+    const exerciseGroup1 = new ExerciseGroup();
+    exerciseGroup1.id = 1;
+
+    const exercise1 = new ProgrammingExercise(course1, exerciseGroup1);
+    exercise1.id = 1;
+
+    const exercise2 = new ProgrammingExercise(course1, exerciseGroup1);
+    exercise2.id = 2;
+
+    const participation1 = new StudentParticipation();
+    participation1.results = [];
+
+    const participation2 = new StudentParticipation();
+    participation2.results = [];
+
+    const result1 = new Result();
+    result1.id = 1;
+    result1.score = 1;
+    result1.participation = participation1;
+
+    const result2 = new Result();
+    result2.id = 2;
+    result2.score = 2;
+    result2.participation = participation2;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [ExerciseScoresExportButtonComponent],
+            providers: [DifferencePipe, { provide: TranslateService, useClass: MockTranslateService }],
+        })
+            .overrideTemplate(ExerciseScoresExportButtonComponent, '')
+            .compileComponents();
+
+        fixture = TestBed.createComponent(ExerciseScoresExportButtonComponent);
+        component = fixture.componentInstance;
+        resultService = TestBed.inject(ResultService);
+    });
+
+    afterEach(async () => {
+        jest.clearAllMocks();
+    });
+
+    it('should load results', fakeAsync(() => {
+        // GIVEN
+        component.exercise = exercise1;
+        spyOn(resultService, 'getResultsForExercise').and.returnValue(of(new HttpResponse({ body: [result1, result2] })));
+
+        // WHEN
+        let results = [];
+
+        component.getResults().subscribe((data) => {
+            results = data.body || [];
+        });
+
+        fixture.detectChanges();
+        tick();
+
+        // THEN
+        expect(component).to.be.ok;
+        expect(results.length).to.be.equal(2);
+    }));
+
+    it('should export results for one exercise', fakeAsync(() => {
+        // GIVEN
+        const getResultsStub = sinon.stub(component, 'getResults').returns(of(new HttpResponse({ body: [result1, result2] })));
+        component.exercise = exercise1;
+
+        // WHEN
+        component.exportResults();
+        fixture.detectChanges();
+
+        // THEN
+        expect(getResultsStub).to.have.been.called;
+    }));
+
+    it('should export results for multiple exercise', fakeAsync(() => {
+        // GIVEN
+        const getResultsStub = sinon.stub(component, 'getResults').returns(of(new HttpResponse({ body: [result1, result2] })));
+        component.exercises = [exercise1, exercise2];
+
+        // WHEN
+        component.exportResults();
+        fixture.detectChanges();
+
+        // THEN
+        expect(getResultsStub).to.have.been.calledTwice;
+    }));
+});
