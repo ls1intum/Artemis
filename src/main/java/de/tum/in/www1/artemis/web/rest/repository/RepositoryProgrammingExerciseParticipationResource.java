8ef4c757f80fa4f084d35a4b9f8b40b47a655c47
Florian Glombik <ge25bas@mytum.de>
2021-04-27 21:15:15 +0200

    Allowing editor to use "Edit in editor" for programming exercises in exams

--- src/main/java/de/tum/in/www1/artemis/web/rest/repository/RepositoryProgrammingExerciseParticipationResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/repository/RepositoryProgrammingExerciseParticipationResource.java
@@ -275,7 +275,7 @@ public class RepositoryProgrammingExerciseParticipationResource extends Reposito
         // Apply checks for exam (submission is in time & user's student exam has the exercise)
         // Checks only apply to students, tutors and editors, otherwise template, solution and assignment participation can't be edited using the code editor
         User user = userRepository.getUserWithGroupsAndAuthorities(principal.getName());
-        if (!authCheckService.isAtLeastInstructorForExercise(programmingExerciseParticipation.getProgrammingExercise())
+        if (!authCheckService.isAtLeastEditorForExercise(programmingExerciseParticipation.getProgrammingExercise())
                 && !examSubmissionService.isAllowedToSubmitDuringExam(programmingExerciseParticipation.getProgrammingExercise(), user, false)) {
             FileSubmissionError error = new FileSubmissionError(participationId, "notAllowedExam");
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, error.getMessage(), error);
