a5e0c92ecbc188e02880c96a3bb41ae328a90fa3
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-04-27 05:41:53 +0200

    Add tests for modeling exercise service

--- src/test/javascript/spec/service/modeling-exercise.service.spec.ts
+++ src/test/javascript/spec/service/modeling-exercise.service.spec.ts
@@ -1,4 +1,4 @@
-import { getTestBed, TestBed } from '@angular/core/testing';
+import { fakeAsync, getTestBed, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { take } from 'rxjs/operators';
 import { ModelingExerciseService } from 'app/exercises/modeling/manage/modeling-exercise.service';
@@ -10,13 +10,19 @@ import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { routes } from 'app/exercises/modeling/manage/modeling-exercise.route';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ArtemisModelingExerciseModule } from 'app/exercises/modeling/manage/modeling-exercise.module';
+import { ExerciseCategory } from 'app/entities/exercise-category.model';
+import * as moment from 'moment';
+import { ModelingPlagiarismResult } from 'app/exercises/shared/plagiarism/types/modeling/ModelingPlagiarismResult';
+import { PlagiarismOptions } from 'app/exercises/shared/plagiarism/types/PlagiarismOptions';
 
 describe('ModelingExercise Service', () => {
     let injector: TestBed;
     let service: ModelingExerciseService;
     let httpMock: HttpTestingController;
     let elemDefault: ModelingExercise;
-
+    let plagiarismResult: ModelingPlagiarismResult;
+    const category = { color: 'red', category: 'testCategory' } as ExerciseCategory;
+    const categories = [JSON.stringify(category) as ExerciseCategory];
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisModelingExerciseModule, HttpClientTestingModule, RouterTestingModule.withRoutes(routes)],
@@ -28,63 +34,152 @@ describe('ModelingExercise Service', () => {
         });
         injector = getTestBed();
         service = injector.get(ModelingExerciseService);
+        service.resourceUrl = 'resourceUrl';
         httpMock = injector.get(HttpTestingController);
 
         elemDefault = new ModelingExercise(UMLDiagramType.ComponentDiagram, undefined, undefined);
+        elemDefault.dueDate = moment();
+        elemDefault.releaseDate = moment();
+        elemDefault.assessmentDueDate = moment();
+        elemDefault.studentParticipations = [];
+        const plagiarismResult = new ModelingPlagiarismResult();
+        plagiarismResult.exercise = elemDefault;
+        plagiarismResult.comparisons = [];
+        plagiarismResult.duration = 43;
+        plagiarismResult.similarityDistribution = [3, 10];
     });
 
-    it('should find an element', async () => {
+    it('should find an element', fakeAsync(() => {
         const returnedFromService = Object.assign({}, elemDefault);
         service
             .find(123)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).toMatchObject({ body: elemDefault }));
-
+            .subscribe((resp) => expect(resp.body).toEqual(elemDefault));
         const req = httpMock.expectOne({ method: 'GET' });
-        req.flush(JSON.stringify(returnedFromService));
-    });
+        req.flush(returnedFromService);
+    }));
 
-    it('should create a ModelingExercise', async () => {
-        const returnedFromService = Object.assign(
-            {
-                id: 0,
-            },
-            elemDefault,
-        );
-        const expected = Object.assign({}, returnedFromService);
+    it('should create a ModelingExercise', fakeAsync(() => {
+        const returnedFromService = {
+            id: 0,
+            ...elemDefault,
+            categories,
+        };
+        const expected = { ...returnedFromService, categories: [category] };
         service
             .create(new ModelingExercise(UMLDiagramType.ComponentDiagram, undefined, undefined))
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+            .subscribe((resp) => {
+                expect(resp.body).toEqual(expected);
+            });
         const req = httpMock.expectOne({ method: 'POST' });
-        req.flush(JSON.stringify(returnedFromService));
-    });
+        req.flush(returnedFromService);
+        tick();
+    }));
 
-    it('should update a ModelingExercise', async () => {
-        const returnedFromService = Object.assign(
-            {
-                diagramType: 'BBBBBB',
-                sampleSolutionModel: 'BBBBBB',
-                sampleSolutionExplanation: 'BBBBBB',
-            },
-            elemDefault,
-        );
+    it('should update a ModelingExercise', fakeAsync(() => {
+        const returnedFromService = {
+            diagramType: UMLDiagramType.ClassDiagram,
+            sampleSolutionModel: 'BBBBBB',
+            sampleSolutionExplanation: 'BBBBBB',
+            ...elemDefault,
+            categories,
+        };
 
-        const expected = Object.assign({}, returnedFromService);
+        const expected = { ...returnedFromService, categories: [category] };
         service
             .update(expected)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+            .subscribe((resp) => expect(resp.body).toEqual(expected));
         const req = httpMock.expectOne({ method: 'PUT' });
-        req.flush(JSON.stringify(returnedFromService));
-    });
+        req.flush(returnedFromService);
+        tick();
+    }));
 
-    it('should delete a ModelingExercise', async () => {
+    it('should get statistics', fakeAsync(() => {
+        const returnedFromService = {
+            numberModels: 1,
+            numberConflicts: 2,
+            totalConfidence: 3,
+            totalCoverage: 4,
+            uniqueElements: [{ id1: { name: 'statisticsName', apollonId: 'statisticsApollonId', conflict: true } }],
+            models: [{ idModel: { confidence: 23, coverage: 12, conflicts: 3 } }],
+        };
+
+        const expected = { ...returnedFromService };
+        const id = 332;
+        service
+            .getStatistics(id)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp.body).toEqual(expected));
+        const req = httpMock.expectOne({ method: 'GET', url: `${service.resourceUrl}/${id}/statistics` });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should delete a ModelingExercise', fakeAsync(() => {
         service.delete(123).subscribe((resp) => expect(resp.ok));
 
         const req = httpMock.expectOne({ method: 'DELETE' });
         req.flush({ status: 200 });
-    });
+    }));
+
+    it('should import a modeling exercise', fakeAsync(() => {
+        const modelingExercise = { ...elemDefault };
+        modelingExercise.id = 445;
+        modelingExercise.categories = [category];
+        const returnedFromService = {
+            ...modelingExercise,
+            categories,
+        };
+
+        const expected = { ...returnedFromService, categories: [category] };
+        service
+            .import({ ...modelingExercise, categories })
+            .pipe(take(1))
+            .subscribe((resp) => {
+                expect(resp.body).toEqual(expected);
+            });
+        const req = httpMock.expectOne({ method: 'POST', url: `${service.resourceUrl}/import/${modelingExercise.id}` });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should check plagiarism result', fakeAsync(() => {
+        elemDefault.id = 756;
+
+        const returnedFromService = {
+            ...plagiarismResult,
+        };
+
+        const options = new PlagiarismOptions(9, 4, 6);
+
+        const expected = { ...returnedFromService };
+        service
+            .checkPlagiarism(elemDefault.id, options)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toEqual(expected));
+        const req = httpMock.expectOne({ method: 'GET', url: `${service.resourceUrl}/${elemDefault.id}/check-plagiarism?similarityThreshold=9&minimumScore=4&minimumSize=6` });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should get plagiarism result', fakeAsync(() => {
+        elemDefault.id = 756;
+
+        const returnedFromService = {
+            ...plagiarismResult,
+        };
+
+        const expected = { ...returnedFromService };
+        service
+            .getLatestPlagiarismResult(elemDefault.id)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toEqual(expected));
+        const req = httpMock.expectOne({ method: 'GET', url: `${service.resourceUrl}/${elemDefault.id}/plagiarism-result` });
+        req.flush(returnedFromService);
+        tick();
+    }));
 
     afterEach(() => {
         httpMock.verify();
