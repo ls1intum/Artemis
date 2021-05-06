5750312cb6e2f0d826ae98077e678b478cdc18b3
IAMsay <aleksandr.malyuk@googlemail.com>
2021-05-06 09:46:13 +0200

    variable extracted to reduce complexity

--- src/main/webapp/app/overview/course-card.component.ts
+++ src/main/webapp/app/overview/course-card.component.ts
@@ -60,13 +60,10 @@ export class CourseCardComponent implements OnChanges {
     ngOnChanges() {
         if (this.course.exercises && this.course.exercises.length > 0) {
             this.exerciseCount = this.course.exercises.length;
-            // sets the exercise with the earliest due date which is not yet successful (no 100%)
 
-            // debugger;
-            const nextExercises = this.exerciseService.getNextExercisesForDays(
-                this.course.exercises!.filter((exercise: Exercise) => !exercise.studentParticipations?.[0]?.submissions?.[0]?.results?.[0]?.successful),
-            );
-            console.log(nextExercises);
+            const nextExercisesWithAnyScore = this.exerciseService.getNextExercisesForDays(this.course.exercises!);
+            // filters out every already successful (100%) exercise, only exercises left that still need work
+            const nextExercises = nextExercisesWithAnyScore.filter((exercise: Exercise) => !exercise.studentParticipations?.[0]?.submissions?.[0]?.results?.[0]?.successful);
 
             if (nextExercises.length > 0 && nextExercises[0]) {
                 this.nextRelevantExercise = nextExercises[0];
