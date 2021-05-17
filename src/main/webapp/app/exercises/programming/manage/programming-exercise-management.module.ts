58534751a28ead357c03cf7db55d8e9be8cda661
Florian Glombik <florian.glombik@tum.de>
2021-05-17 22:02:20 +0200

    Template implementation for AddAuxiliaryRepository view

--- src/main/webapp/app/exercises/programming/manage/programming-exercise-management.module.ts
+++ src/main/webapp/app/exercises/programming/manage/programming-exercise-management.module.ts
@@ -21,7 +21,9 @@ import { OrionModule } from 'app/shared/orion/orion.module';
 import { ArtemisPlagiarismModule } from 'app/exercises/shared/plagiarism/plagiarism.module';
 import { ProgrammingExerciseInstructorRepoDownloadComponent } from '../shared/actions/programming-exercise-instructor-repo-download.component';
 import { ArtemisProgrammingExerciseLifecycleModule } from 'app/exercises/programming/shared/lifecycle/programming-exercise-lifecycle.module';
-import { ProgrammingExerciseInstructorExerciseDownloadComponent } from 'app/exercises/programming/shared/actions/programming-exercise-instructor-exercise-download.component';
+import { ProgrammingExerciseInstructorExerciseDownloadComponent } from '../shared/actions/programming-exercise-instructor-exercise-download.component';
+import { AuxiliaryRepositoryButtonComponent } from 'app/exercises/programming/manage/auxiliary-repository-button.component';
+import { AuxiliaryRepositoryDialogComponent } from 'app/exercises/programming/manage/auxiliary-repository-dialog.component';
 
 @NgModule({
     imports: [
@@ -50,6 +52,8 @@ import { ProgrammingExerciseInstructorExerciseDownloadComponent } from 'app/exer
         ProgrammingExerciseEditSelectedComponent,
         ProgrammingExerciseInstructorRepoDownloadComponent,
         ProgrammingExerciseInstructorExerciseDownloadComponent,
+        AuxiliaryRepositoryButtonComponent,
+        AuxiliaryRepositoryDialogComponent,
     ],
 })
 export class ArtemisProgrammingExerciseManagementModule {}
