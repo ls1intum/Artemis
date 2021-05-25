8f370bdae845b00a694d22143f8f6de83fc6d8b5
Ivan Chimeno <chimeno.ivan@gmail.com>
2021-05-25 15:11:51 +0200

    implement GitLab activation/deactivation

--- src/main/java/de/tum/in/www1/artemis/exception/CIAccountExistsException.java
+++
@@ -1,9 +0,0 @@
-package de.tum.in.www1.artemis.exception;
-
-public class CIAccountExistsException extends RuntimeException {
-
-    public CIAccountExistsException(String username) {
-        super("The account with username " + username + " is already used in the CIS!");
-    }
-
-}
--- src/main/java/de/tum/in/www1/artemis/exception/VCSAccountExistsException.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.artemis.exception;
-
-public class VCSAccountExistsException extends RuntimeException {
-
-    public VCSAccountExistsException(String username) {
-        super("The account with username" + username + " is already used in the VCS!");
-    }
-}
