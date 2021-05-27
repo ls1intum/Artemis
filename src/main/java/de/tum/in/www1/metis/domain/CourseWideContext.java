dd2138ee7cf0d8c605d2bc22cf6a72cef4e4bf5b
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-05-27 13:56:38 +0200

    revert recent commits

--- src/main/java/de/tum/in/www1/metis/domain/CourseWideContext.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.metis.domain;
-
-/**
- * The CourseWideContext enumeration for associating posts to contexts besides certain exercises or lectures.
- */
-public enum CourseWideContext {
-    TECH_SUPPORT, ORGANIZATION, RANDOM
-}
