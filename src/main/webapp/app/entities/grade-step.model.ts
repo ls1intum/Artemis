e0d36c8851cdc535dc05c46c9beba4633ec300ed
Ivo Hashamov <63168603+ivohashamov@users.noreply.github.com>
2021-05-28 19:52:46 +0300

    Show grade in student exam summary (#3482)

--- src/main/webapp/app/entities/grade-step.model.ts
+++ src/main/webapp/app/entities/grade-step.model.ts
@@ -1,4 +1,5 @@
 import { BaseEntity } from 'app/shared/model/base-entity';
+import { GradeType } from 'app/entities/grading-scale.model';
 
 export class GradeStep implements BaseEntity {
     public id?: number;
@@ -8,4 +9,10 @@ export class GradeStep implements BaseEntity {
     public lowerBoundInclusive = true;
     public upperBoundInclusive = false;
     public isPassingGrade = false;
+}
+
+export class GradeDTO {
+    public gradeName: string;
+    public isPassingGrade = false;
+    public gradeType: GradeType;
 }
