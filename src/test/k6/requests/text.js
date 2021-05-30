3cfd311543a91e8708c25c261f5968b42730c002
Robert Schauer <ga94bit@mytum.de>
2021-05-30 17:52:48 +0200

    move textexercise functionality into text.js

--- src/test/k6/requests/text.js
+++ src/test/k6/requests/text.js
@@ -1,6 +1,6 @@
 import { fail } from 'k6';
 import { nextAlphanumeric } from '../util/utils.js';
-import { SUBMIT_TEXT_EXAM } from './endpoints.js';
+import { SUBMIT_TEXT_EXAM, TEXT_EXERCISES } from './endpoints.js';
 
 export function submitRandomTextAnswerExam(artemis, exercise, submissionId) {
     const answer = {
@@ -20,4 +20,33 @@ export function submitRandomTextAnswerExam(artemis, exercise, submissionId) {
         fail('FAILTEST: Could not submit text (Exam) via REST (status: ' + res[0].status + ')! response: ' + res[0].body);
     }
     return answer;
+}
+
+export function newTextExercise(artemis, exerciseGroup, courseID) {
+    const textExercise = {
+        maxPoints: 1,
+        title: 'text' + nextAlphanumeric(5),
+        type: 'text',
+        mode: 'INDIVIDUAL',
+    };
+
+    if (courseID) {
+        textExercise.course = { id: courseId };
+    }
+
+    if (exerciseGroup) {
+        textExercise.exerciseGroup = exerciseGroup;
+    }
+
+    const res = artemis.post(TEXT_EXERCISES, textExercise);
+    if (res[0].status !== 201) {
+        console.log('ERROR when creating a new text exercise. Response headers:');
+        for (let [key, value] of Object.entries(res[0].headers)) {
+            console.log(`${key}: ${value}`);
+        }
+        fail('FAILTEST: Could not create text exercise (status: ' + res[0].status + ')! response: ' + res[0].body);
+    }
+    console.log('SUCCESS: Generated new text exercise');
+
+    return JSON.parse(res[0].body);
 }
