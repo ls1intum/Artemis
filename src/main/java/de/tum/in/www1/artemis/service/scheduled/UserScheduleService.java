967a85733581d979140c4f3d9e97d47ccbfc313a
Ivan Chimeno <chimeno.ivan@gmail.com>
2021-06-02 16:25:35 +0200

    set default removeNonActivatedUserDelayTime to 60 minutes

--- src/main/java/de/tum/in/www1/artemis/service/scheduled/UserScheduleService.java
+++ src/main/java/de/tum/in/www1/artemis/service/scheduled/UserScheduleService.java
@@ -20,7 +20,7 @@ import de.tum.in.www1.artemis.service.connectors.VcsUserManagementService;
 @Profile("scheduling")
 public class UserScheduleService {
 
-    @Value("${artemis.user-management.registration.cleanup-time-minutes}")
+    @Value("${artemis.user-management.registration.cleanup-time-minutes:60}")
     private Long removeNonActivatedUserDelayTime;
 
     private final Logger log = LoggerFactory.getLogger(UserScheduleService.class);
