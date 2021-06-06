c2b7c52054925ed93e3ff260ad0fb93942154c05
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-06-06 18:45:00 +0200

    update client models

--- src/main/webapp/app/entities/metis/post.model.ts
+++ src/main/webapp/app/entities/metis/post.model.ts
@@ -1,11 +1,9 @@
-import { Moment } from 'moment';
-import { BaseEntity } from 'app/shared/model/base-entity';
 import { Lecture } from 'app/entities/lecture.model';
 import { Exercise } from 'app/entities/exercise.model';
-import { User } from 'app/core/user/user.model';
 import { AnswerPost } from 'app/entities/metis/answer-post.model';
 import { Course } from 'app/entities/course.model';
 import { PostTag } from 'app/entities/metis/post-tag.model';
+import { Posting } from 'app/entities/metis/posting.model';
 
 export enum CourseWideContext {
     TECH_SUPPORT = 'TECH_SUPPORT',
@@ -13,23 +11,19 @@ export enum CourseWideContext {
     RANDOM = 'RANDOM',
 }
 
-export class Post implements BaseEntity {
-    public id?: number;
-    public author?: User;
-    public creationDate?: Moment;
-    public content?: string;
-    public tokenizedContent?: string;
+export class Post extends Posting {
     public title?: string;
     public visibleForStudents?: boolean;
     public votes?: number;
     public answers?: AnswerPost[];
-    public tags: PostTag[];
+    public tags?: PostTag[];
     public exercise?: Exercise;
     public lecture?: Lecture;
     public course?: Course;
     public courseWideContext?: CourseWideContext;
 
     constructor() {
+        super();
         this.visibleForStudents = true; // default value
         this.votes = 0; // default value
     }
