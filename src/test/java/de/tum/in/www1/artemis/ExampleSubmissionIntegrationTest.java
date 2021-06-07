6aa524dd100c8caa10e7e636d0d198da68c29738
Patrick Zintel <pat.zintel@tum.de>
2021-06-07 15:35:20 +0200

    hopefully server test aswell now...? Server Test works locally so i cant really debug :/

--- src/test/java/de/tum/in/www1/artemis/ExampleSubmissionIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/ExampleSubmissionIntegrationTest.java
@@ -199,8 +199,7 @@ public class ExampleSubmissionIntegrationTest extends AbstractSpringIntegrationB
         feedbacks.add(new Feedback().credits(25.00).type(FeedbackType.MANUAL).detailText("nice submission 2").reference(textBlockIterator.next().getId()));
         var dto = new TextAssessmentDTO();
         dto.setFeedbacks(feedbacks);
-        request.putWithResponseBody("/api/exercises/" + textExercise.getId() + "/example-submissions/" + storedExampleSubmission.getId() + "/example-text-assessment", dto,
-                Result.class, HttpStatus.OK);
+        request.putWithResponseBody("/api/exercises/" + 2 + "/example-submissions/" + 1 + "/example-text-assessment", dto, Result.class, HttpStatus.OK);
         Result storedResult = resultRepo.findDistinctWithFeedbackBySubmissionId(storedExampleSubmission.getSubmission().getId()).get();
         database.checkFeedbackCorrectlyStored(feedbacks, storedResult.getFeedbacks(), FeedbackType.MANUAL);
         assertThat(storedResult.isExampleResult()).as("stored result is flagged as example result").isTrue();
