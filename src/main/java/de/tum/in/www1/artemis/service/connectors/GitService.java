c3c25c7bef42ac3ff3482caff77a33fb4eb5b0d1
Florian Glombik <florian.glombik@tum.de>
2021-06-14 19:58:31 +0200

    git

--- src/main/java/de/tum/in/www1/artemis/service/connectors/GitService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/GitService.java
@@ -318,6 +318,8 @@ public class GitService {
         return repository;
     }
 
+    // test
+
     /**
      * Get the local repository for a given remote repository URL. If the local repo does not exist yet, it will be checked out.
      * Saves the repo in the default path
