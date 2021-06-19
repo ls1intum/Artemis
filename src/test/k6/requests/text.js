c4ccabb3972198528c552e1be5733518f7ed4db5
Robert Schauer <ga94bit@mytum.de>
2021-06-19 19:44:35 +0200

    use proper text assessment

--- src/test/k6/requests/text.js
+++ src/test/k6/requests/text.js
@@ -8,7 +8,7 @@ export function submitRandomTextAnswerExam(artemis, exercise, submissionId) {
         isSynced: false,
         submissionExerciseType: 'text',
         submitted: true,
-        text: 'SOME RANDOM ANSWER ' + nextAlphanumeric(100),
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
     };
 
     let res = artemis.put(SUBMIT_TEXT_EXAM(exercise.id), answer);
@@ -54,11 +54,66 @@ export function newTextExercise(artemis, exerciseGroup, courseID) {
 export function assessTextSubmission(artemis, exerciseId, resultId) {
     const assessment = [
         {
-            credits: 4,
-            reference: 'Package:35f037f7-0606-4798-b43a-9bc76d741421',
-            referenceId: '35f037f7-0606-4798-b43a-9bc76d741421',
-            referenceType: 'Package',
-            text: 'AssessmentText',
+            feedbacks: [
+                {
+                    credits: 1,
+                    reference: '674aa1b614606278282b1b0d80e34f609c641972',
+                    detailText: 'Good',
+                    type: 'MANUAL',
+                },
+                {
+                    credits: 2,
+                    reference: '314a5bbaeb4755e0e29ee69ec4fb0f9f3aa3d2d9',
+                    detailText: 'Good',
+                    type: 'MANUAL',
+                },
+                {
+                    credits: 0,
+                    reference: 'bb7b48643ec0fa3b8194e196bf97692ec6e4821f',
+                    detailText: 'Neutral',
+                    type: 'MANUAL',
+                },
+                {
+                    credits: -0.5,
+                    reference: '21e56278b55f2dfa5c1a9d646823d177b2ac2ef0',
+                    detailText: 'Negative',
+                    type: 'MANUAL',
+                },
+            ],
+            textBlocks: [
+                {
+                    id: '674aa1b614606278282b1b0d80e34f609c641972',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    startIndex: 0,
+                    endIndex: 123,
+                    numberOfAffectedSubmissions: 0,
+                    type: 'AUTOMATIC',
+                },
+                {
+                    id: '314a5bbaeb4755e0e29ee69ec4fb0f9f3aa3d2d9',
+                    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                    startIndex: 124,
+                    endIndex: 231,
+                    numberOfAffectedSubmissions: 0,
+                    type: 'AUTOMATIC',
+                },
+                {
+                    id: 'bb7b48643ec0fa3b8194e196bf97692ec6e4821f',
+                    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                    startIndex: 232,
+                    endIndex: 334,
+                    numberOfAffectedSubmissions: 0,
+                    type: 'AUTOMATIC',
+                },
+                {
+                    id: '21e56278b55f2dfa5c1a9d646823d177b2ac2ef0',
+                    text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    startIndex: 335,
+                    endIndex: 445,
+                    numberOfAffectedSubmissions: 0,
+                    type: 'AUTOMATIC',
+                },
+            ],
         },
     ];
     let res = artemis.put(ASSESS_TEXT_SUBMISSION(exerciseId, resultId), assessment);
