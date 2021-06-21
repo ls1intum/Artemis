1e6e245eb7f8b35e12a27b3d964090ea86829a06
Robert Schauer <56715566+rs-maker@users.noreply.github.com>
2021-06-21 09:26:42 +0200

    [Chore] k6 tests for text exercise api (#3518)

--- src/test/k6/requests/modeling.js
+++ src/test/k6/requests/modeling.js
@@ -1,14 +1,6 @@
-import { fail, sleep } from 'k6';
+import { fail } from 'k6';
 import { nextAlphanumeric } from '../util/utils.js';
-import {
-    ASSESS_MODELING_SUBMISSION,
-    MODELING_EXERCISE,
-    SUBMIT_MODELING_EXAM,
-    PARTICIPATIONS,
-    TUTOR_PARTICIPATIONS,
-    MODELING_SUBMISSION_WITHOUT_ASSESSMENT,
-    MODELING_EXERCISES,
-} from './endpoints.js';
+import { ASSESS_MODELING_SUBMISSION, SUBMIT_MODELING_EXAM, MODELING_EXERCISES } from './endpoints.js';
 
 export function submitRandomModelingAnswerExam(artemis, exercise, submissionId, participation) {
     const answer = {
@@ -82,67 +74,6 @@ export function updateModelingExerciseDueDate(artemis, exercise) {
     }
     console.log('SUCCESS: Generated new modeling exercise');
 
-    return JSON.parse(res[0].body);
-}
-
-export function startExercise(artemis, courseId, exerciseId) {
-    console.log('Try to start exercise for test user ' + __VU);
-    const res = artemis.post(PARTICIPATIONS(courseId, exerciseId), undefined, undefined);
-
-    if (res[0].status === 400) {
-        sleep(3000);
-        return;
-    }
-
-    if (res[0].status !== 201) {
-        fail('FAILTEST: error trying to start exercise for test user ' + __VU + ':\n #####ERROR (' + res[0].status + ')##### ' + res[0].body);
-    } else {
-        console.log('SUCCESSFULLY started exercise for test user ' + __VU);
-    }
-
-    return JSON.parse(res[0].body);
-}
-
-export function deleteModelingExercise(artemis, exerciseId) {
-    const res = artemis.delete(MODELING_EXERCISE(exerciseId));
-    if (res[0].status !== 200) {
-        fail('FAILTEST: Could not delete exercise (' + res[0].status + ')! Response was + ' + res[0].body);
-    }
-    console.log('DELETED modeling exercise, ID=' + exerciseId);
-}
-
-export function getExercise(artemis, exerciseId) {
-    const serverRespond = artemis.get(MODELING_EXERCISE(exerciseId));
-    console.log('Server response is ' + JSON.stringify(serverRespond));
-    const res = artemis.get(MODELING_EXERCISE(exerciseId));
-    if (res[0].status !== 200) {
-        console.log('ERROR when getting existing exercise. Response headers:');
-        for (let [key, value] of Object.entries(res[0].headers)) {
-            console.log(`${key}: ${value}`);
-        }
-        fail('FAILTEST: Could not get exercise (status: ' + res[0].status + ')! response: ' + res[0].body);
-    }
-    console.log('SUCCESS: Get existing exercise');
-
-    return JSON.parse(res[0].body);
-}
-
-export function startTutorParticipation(artemis, exerciseId) {
-    const res = artemis.post(TUTOR_PARTICIPATIONS(exerciseId), { status: 'NOT_PARTICIPATED' });
-    if (res[0].status !== 201) {
-        fail('FAILTEST: error trying to start tutor participation for test user ' + __VU + ':\n #####ERROR (' + res[0].status + ')##### ' + res[0].body);
-    } else {
-        console.log('SUCCESSFULLY started tutor participation for test user ' + __VU);
-    }
-
-    return JSON.parse(res[0].body);
-}
-
-export function getAndLockModelingSubmission(artemis, exerciseId) {
-    const res = artemis.get(MODELING_SUBMISSION_WITHOUT_ASSESSMENT(exerciseId));
-    if (res[0].status !== 200) {
-        fail('FAILTEST: Could not get submission without assessment (' + res[0].status + ')! Response was + ' + res[0].body);
-    }
     return JSON.parse(res[0].body);
 }
 
