6eec666840063f6b05ea73206ff34bdbe36c5c1d
Merlin Mehmed <merlinmehmed17@gmail.com>
2021-06-24 23:46:38 +0300

    Fix imports in ModelFactory

--- src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
+++ src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
@@ -1,12 +1,7 @@
 package de.tum.in.www1.artemis.util;
 
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
