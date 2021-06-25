2176e2d2baf21354a96c3692437b315a9dee252b
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2021-06-25 16:13:24 +0200

    Allow to configure the default branch directly in Artemis (#2844)

--- src/test/java/de/tum/in/www1/artemis/service/BitbucketServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/BitbucketServiceTest.java
@@ -2,12 +2,15 @@ package de.tum.in.www1.artemis.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -15,6 +18,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
+import de.tum.in.www1.artemis.domain.VcsRepositoryUrl;
 
 public class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
 
@@ -58,5 +62,13 @@ public class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbuck
         assertThat(health.getAdditionalInfo().get("url")).isEqualTo(bitbucketServerUrl);
         assertThat(health.isUp()).isEqualTo(false);
         assertThat(health.getException()).isNotNull();
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @ValueSource(strings = { "master", "main", "someOtherName" })
+    public void testGetDefaultBranch(String defaultBranch) throws IOException {
+        bitbucketRequestMockProvider.mockDefaultBranch(defaultBranch, "PROJECTNAME");
+        String actualDefaultBranch = versionControlService.getDefaultBranchOfRepository(new VcsRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git"));
+        assertThat(actualDefaultBranch).isEqualTo(defaultBranch);
     }
 }
