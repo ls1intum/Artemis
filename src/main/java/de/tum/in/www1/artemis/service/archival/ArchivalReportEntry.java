5026c47c245b13f44fccd3e136db6180788c5ab1
Martin Dunker <ga27kin@mytum.de>
2021-07-01 20:07:09 +0200

    add data for skipped entries

--- src/main/java/de/tum/in/www1/artemis/service/archival/ArchivalReportEntry.java
+++ src/main/java/de/tum/in/www1/artemis/service/archival/ArchivalReportEntry.java
@@ -12,26 +12,30 @@ public class ArchivalReportEntry {
 
     private final int successfulEntries;
 
+    private final int skippedEntries;
+
     private final int failedEntries;
 
     /**
      * Generates a new entry with the given data
      *
      * @param exerciseId the id or a negative number if no id is applicable
+     * @param skippedEntries Entries that did not get exported but did not fail either, e.g. because a participation did not have any submission
      */
-    public ArchivalReportEntry(long exerciseId, String exerciseName, int totalEntries, int successfulEntries, int failedEntries) {
+    public ArchivalReportEntry(long exerciseId, String exerciseName, int totalEntries, int successfulEntries, int skippedEntries, int failedEntries) {
         this.exerciseId = exerciseId >= 0 ? OptionalLong.of(exerciseId) : OptionalLong.empty();
         this.exerciseName = exerciseName;
         this.totalEntries = totalEntries;
         this.successfulEntries = successfulEntries;
+        this.skippedEntries = skippedEntries;
         this.failedEntries = failedEntries;
     }
 
     /**
      * Shortcut for {@link ArchivalReportEntry} but calculates the missing value
      */
-    public ArchivalReportEntry(long exerciseId, String exerciseName, int totalEntries, int successfulEntries) {
-        this(exerciseId, exerciseName, totalEntries, successfulEntries, totalEntries - successfulEntries);
+    public ArchivalReportEntry(long exerciseId, String exerciseName, int totalEntries, int successfulEntries, int skippedEntries) {
+        this(exerciseId, exerciseName, totalEntries, successfulEntries, skippedEntries, totalEntries - successfulEntries - skippedEntries);
     }
 
     /**
@@ -39,13 +43,14 @@ public class ArchivalReportEntry {
      */
     @Override
     public String toString() {
-        return (exerciseId.isPresent() ? exerciseId.getAsLong() : "") + "," + exerciseName + "," + totalEntries + "," + successfulEntries + "," + failedEntries;
+        return (exerciseId.isPresent() ? exerciseId.getAsLong() : "") + "," + exerciseName + "," + totalEntries + "," + successfulEntries + "," + skippedEntries + ","
+                + failedEntries;
     }
 
     /**
      * @return the headline of a csv file containing entries of this class
      */
     public static String getHeadline() {
-        return "exerciseId,exerciseName,totalEntries,successfulEntries,failedEntries";
+        return "exerciseId,exerciseName,totalEntries,successfulEntries,skippedEntries,failedEntries";
     }
 }
