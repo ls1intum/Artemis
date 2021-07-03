0586d17191e3e2b672e441a3c327cdfa5d9c887b
Patrick Zintel <pat.zintel@tum.de>
2021-07-03 19:00:23 +0200

    aligning path in navigation.utils with the text-submission-assessment.route

--- src/main/webapp/app/utils/navigation.utils.ts
+++ src/main/webapp/app/utils/navigation.utils.ts
@@ -121,7 +121,7 @@ export const getLinkToSubmissionAssessment = (
         return route;
     } else {
         if (exerciseType === ExerciseType.TEXT) {
-            return ['/course-management', courseId.toString(), 'participations', participationId.toString(), 'submissions', submissionId.toString(), 'for-text-assessment'];
+            return ['/participations', participationId.toString(), 'submissions', submissionId.toString(), 'for-text-assessment'];
         } else {
             return ['/course-management', courseId.toString(), exerciseType + '-exercises', exerciseId.toString(), 'submissions', submissionId.toString(), 'assessment'];
         }
