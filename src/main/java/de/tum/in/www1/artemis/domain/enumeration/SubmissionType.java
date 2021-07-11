ae4471168560bd6d935f9b3035361869022145b2
Florian Glombik <florian.glombik@tum.de>
2021-07-11 21:59:42 +0200

    Fixing javadoc

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/SubmissionType.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/SubmissionType.java
@@ -3,7 +3,7 @@ package de.tum.in.www1.artemis.domain.enumeration;
 /**
  * MANUAL: submission triggered by a student
  * EXTERNAL: submission that was added through the add external submission dialogue
- * ILLEGAL: submission of an exam programming exercise that is submitted after the individual exercise end date
+ * ILLEGAL: submission of an exam programming exercise that is submitted before the exercise start date or after the individual exercise end date
  */
 public enum SubmissionType {
     MANUAL, TIMEOUT, TEST, OTHER, INSTRUCTOR, EXTERNAL, ILLEGAL
