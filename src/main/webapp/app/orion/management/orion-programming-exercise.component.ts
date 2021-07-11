368c5ff4cda55a48aa21cf845b81d69a239b14ee
Martin Dunker <ga27kin@mytum.de>
2021-07-11 21:38:29 +0200

    decouple programming exercise component

---
+++ src/main/webapp/app/orion/management/orion-programming-exercise.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { OrionConnectorService } from 'app/shared/orion/orion-connector.service';
+import { ExerciseView, OrionState } from 'app/shared/orion/orion';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'jhi-orion-programming-exercise',
+    templateUrl: './orion-programming-exercise.component.html',
+})
+export class OrionProgrammingExerciseComponent implements OnInit {
+    @Input() programmingExercises: ProgrammingExercise[];
+
+    readonly ExerciseView = ExerciseView;
+
+    orionState: OrionState;
+
+    constructor(private orionConnectorService: OrionConnectorService, private router: Router,) {
+    }
+
+    ngOnInit() {
+        this.orionConnectorService.state().subscribe((state) => {
+            this.orionState = state;
+        });
+    }
+
+    editInIDE(programmingExercise: ProgrammingExercise) {
+        this.orionConnectorService.editExercise(programmingExercise);
+    }
+
+    openOrionEditor(exercise: ProgrammingExercise) {
+        try {
+            this.router.navigate(['code-editor', 'ide', exercise.id, 'admin', exercise.templateParticipation?.id]);
+        } catch (error) {
+            this.orionConnectorService.log(error);
+        }
+    }
+}
