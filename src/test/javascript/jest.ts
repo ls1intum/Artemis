f0a1e23aaee2e2163e74ded2bc6390977cbfa02e
Florian Glombik <florian.glombik@tum.de>
2021-07-12 20:41:38 +0200

    Declaring last() for tests

--- src/test/javascript/jest.ts
+++ src/test/javascript/jest.ts
@@ -15,3 +15,19 @@ Object.defineProperty(window, 'getComputedStyle', {
         },
     }),
 });
+
+// Same as in global.utils.ts
+declare global {
+    interface Array<T> {
+        last(): T | undefined;
+    }
+}
+
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        if (!this.length) {
+            return undefined;
+        }
+        return this[this.length - 1];
+    };
+}
