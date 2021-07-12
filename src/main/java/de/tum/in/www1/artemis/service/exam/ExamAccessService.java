57b5f4698e8de1bb2a19ca889e48defb7291a89b
Patrick Zintel <pat.zintel@tum.de>
2021-07-12 16:42:24 +0200

    addressed changes

--- src/main/java/de/tum/in/www1/artemis/service/exam/ExamAccessService.java
+++ src/main/java/de/tum/in/www1/artemis/service/exam/ExamAccessService.java
@@ -225,7 +225,7 @@ public class ExamAccessService {
      * @param role            The role of the callee
      * @param courseId        The id of the course
      * @param examId          The id of the exam
-     * @param exerciseGroup The id of the exercise group
+     * @param exerciseGroup   The exercise group
      * @param <X>             The type of the return type of the requesting route so that the
      *                        response can be returned there
      * @return an Optional with a typed ResponseEntity. If it is empty all checks passed
@@ -239,27 +239,11 @@ public class ExamAccessService {
         if (courseAndExamAccessFailure.isPresent()) {
             return courseAndExamAccessFailure;
         }
-        if (exerciseGroup.getExam() == null || !exerciseGroup.getExam().getId().equals(examId) || !exerciseGroup.getExam().getCourse().getId().equals(courseId)) {
+        Exam exam = exerciseGroup.getExam();
+        if (exam == null || !exam.getId().equals(examId) || !exam.getCourse().getId().equals(courseId)) {
             return Optional.of(badRequest());
         }
         return Optional.empty();
-    }
-
-    /**
-     * Checks if the current user is allowed to manage exams of the given course, that the exam exists,
-     * that the exam belongs to the given course and the exercise group belongs to the given exam.
-     *
-     * @param courseId        The id of the course
-     * @param examId          The id of the exam
-     * @param exerciseGroup The id of the exercise group
-     * @param <X>             The type of the return type of the requesting route so that the
-     *                        response can be returned there
-     * @return an Optional with a typed ResponseEntity. If it is empty all checks passed
-     */
-    // TODO: This method is replaced by checkCourseAndExamAndExerciseGroupAccess(), should be removed once all callees are refacored
-    @Deprecated
-    public <X> Optional<ResponseEntity<X>> checkCourseAndExamAndExerciseGroupAccess(Long courseId, Long examId, ExerciseGroup exerciseGroup) {
-        return checkCourseAndExamAndExerciseGroupAccess(Role.INSTRUCTOR, courseId, examId, exerciseGroup);
     }
 
     /**
