c2f87150c6e1c6aa9a1051139dbff04850bb00ea
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-07-13 16:29:48 +0200

    test changes F (changed CSS)

--- src/main/webapp/app/exercises/programming/shared/code-editor/container/code-editor-container.component.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/container/code-editor-container.component.ts
@@ -29,7 +29,7 @@ import { Feedback } from 'app/entities/feedback.model';
 @Component({
     selector: 'jhi-code-editor-container',
     templateUrl: './code-editor-container.component.html',
-    styleUrls: ['./code-editor-container.component.scss.css'],
+    styleUrls: ['./code-editor-container.component.scss'],
 })
 export class CodeEditorContainerComponent implements ComponentCanDeactivate {
     readonly CommitState = CommitState;
