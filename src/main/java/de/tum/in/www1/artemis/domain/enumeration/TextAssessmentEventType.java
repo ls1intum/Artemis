d253b778763aa5ea0974fa341aebf000e803e5c8
Ndricim Rrapi <ndricim.rrapi@tum.de>
2021-07-14 02:15:10 +0200

    Update src/main/java/de/tum/in/www1/artemis/domain/enumeration/TextAssessmentEventType.java
    Co-authored-by: Martin Dunker <61357727+MDK5@users.noreply.github.com>

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/TextAssessmentEventType.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/TextAssessmentEventType.java
@@ -3,7 +3,7 @@ package de.tum.in.www1.artemis.domain.enumeration;
 /**
  * The TextAssessmentEventType enumeration.
  * Enumerates the names of events that are tracked when users interact with the assessment
- * system in Text exercises
+ * system in text exercises
  */
 public enum TextAssessmentEventType {
     ADD_FEEDBACK_AUTOMATICALLY_SELECTED_BLOCK, ADD_FEEDBACK_MANUALLY_SELECTED_BLOCK, CLICK_TO_RESOLVE_CONFLICT, HOVER_OVER_IMPACT_WARNING, VIEW_AUTOMATIC_SUGGESTION_ORIGIN,
