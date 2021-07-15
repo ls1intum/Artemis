ceb25a7e733a281af68d4577b0fc52fa7b07231d
Lorena Schlesinger <lorena.schlesinger@online.de>
2021-07-15 11:17:17 +0200

    fix date

--- src/main/webapp/app/shared/metis/postings-header/postings-header.directive.ts
+++ src/main/webapp/app/shared/metis/postings-header/postings-header.directive.ts
@@ -2,6 +2,7 @@ import { Posting } from 'app/entities/metis/posting.model';
 import { PostingsService } from 'app/shared/metis/postings.service';
 import { Directive, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from 'app/core/user/user.model';
+import moment from 'moment';
 
 @Directive()
 export abstract class PostingsHeaderDirective<T extends Posting> implements OnInit {
@@ -12,11 +13,13 @@ export abstract class PostingsHeaderDirective<T extends Posting> implements OnIn
     @Output() onDelete: EventEmitter<T> = new EventEmitter<T>();
     @Output() onUpdate: EventEmitter<T> = new EventEmitter<T>();
     isAuthorOfPosting: boolean;
+    postingIsOfToday: boolean;
 
     protected constructor(protected postingService: PostingsService<T>) {}
 
     ngOnInit(): void {
         this.isAuthorOfPosting = this.user ? this.posting?.author!.id === this.user.id : false;
+        this.postingIsOfToday = moment().diff(this.posting.creationDate, 'd') === 0;
     }
 
     deletePosting(): void {
