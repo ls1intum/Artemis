71088ff5a52fa760f1d76a49443d08dbf1145adb
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-07-17 17:04:50 +0200

    finished notification.service.spec.ts ~90%

--- src/test/javascript/spec/service/notification.service.spec.ts
+++ src/test/javascript/spec/service/notification.service.spec.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { TranslateTestingModule } from '../helpers/mocks/service/mock-translate.service';
 import { Notification } from 'app/entities/notification.model';
-import { LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE } from 'app/shared/notification/notification.constants';
 import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import * as chai from 'chai';
@@ -16,12 +15,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import * as moment from 'moment';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
 import { MockCourseManagementService } from '../helpers/mocks/service/mock-course-management.service';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { AccountService } from 'app/core/auth/account.service';
 import { MockAccountService } from '../helpers/mocks/service/mock-account.service';
 import { JhiWebsocketService } from 'app/core/websocket/websocket.service';
 import { MockWebsocketService } from '../helpers/mocks/service/mock-websocket.service';
 import { SinonStub, stub } from 'sinon';
+import { Course } from 'app/entities/course.model';
+import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -35,32 +36,44 @@ describe('Logs Service', () => {
     let websocketService: JhiWebsocketService;
     let wsSubscribeStub: SinonStub;
     let wsUnsubscribeStub: SinonStub;
-    let wsReceiveStub: SinonStub;
+    let wsReceiveNotificationStub: SinonStub;
     let wsNotificationSubject: Subject<Notification | undefined>;
 
-    const generateQuizNotification = (id: number) => {
-        const generatedNotification = { id, title: 'Quiz started', text: 'Quiz "Proxy pattern" just started.' } as Notification;
-        generatedNotification.target = JSON.stringify({ mainPage: 'courses', course: 1, entity: 'exercise', id: 1 });
-        generatedNotification.notificationDate = moment();
-        return generatedNotification;
-    };
-    const quizNotification = generateQuizNotification(1);
+    let wsQuizExerciseSubject: Subject<QuizExercise | undefined>;
+    let wsReceiveQuizExerciseStub: SinonStub;
 
-    const generateExamExerciseUpdateNotification = () => {
-        const generatedNotification = { title: LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE, text: 'Fixed mistake' } as Notification;
-        generatedNotification.target = JSON.stringify({ mainPage: 'courses', course: 1, entity: 'exams', exam: 1, exercise: 7, problemStatement: 'Fixed Problem Statement' });
-        generatedNotification.notificationDate = moment().subtract(3, 'days');
+    let courseManagementService: CourseManagementService;
+    let cmGetCoursesForNotificationsStub: SinonStub;
+    let cmCoursesSubject: Subject<[Course] | undefined>;
+    const course: Course = new Course();
+    course.id = 42;
+
+    const quizExercise: QuizExercise = { course, title: 'test quiz', started: true, visibleToStudents: true, id: 27 } as QuizExercise;
+
+    const generateQuizNotification = () => {
+        const generatedNotification = {
+            title: 'Quiz started',
+            text: 'Quiz "' + quizExercise.title + '" just started.',
+            notificationDate: moment(),
+        } as Notification;
+        generatedNotification.target = JSON.stringify({ course: course.id, mainPage: 'courses', entity: 'exercises', id: quizExercise.id });
         return generatedNotification;
     };
-    const examExerciseUpdateNotification = generateExamExerciseUpdateNotification();
+    const quizNotification = generateQuizNotification();
 
     const generateSingleUserNotification = () => {
         const generatedNotification = { title: 'Single user notification', text: 'This is a notification for a single user' } as Notification;
-        generatedNotification.target = JSON.stringify({ mainPage: 'courses', course: 1, entity: 'exams', exam: 1, exercise: 7, problemStatement: 'Fixed Problem Statement' });
         generatedNotification.notificationDate = moment().subtract(3, 'days');
         return generatedNotification;
     };
     const singleUserNotification = generateSingleUserNotification();
+
+    const generateGroupNotification = () => {
+        const generatedNotification = { title: 'simple group notification', text: 'This is a  simple group notification' } as Notification;
+        generatedNotification.notificationDate = moment();
+        return generatedNotification;
+    };
+    const groupNotification = generateGroupNotification();
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -86,7 +99,14 @@ describe('Logs Service', () => {
                 wsSubscribeStub = stub(websocketService, 'subscribe');
                 wsUnsubscribeStub = stub(websocketService, 'unsubscribe');
                 wsNotificationSubject = new Subject<Notification | undefined>();
-                wsReceiveStub = stub(websocketService, 'receive').returns(wsNotificationSubject);
+                wsReceiveNotificationStub = stub(websocketService, 'receive').returns(wsNotificationSubject);
+
+                wsQuizExerciseSubject = new Subject<QuizExercise | undefined>();
+                //wsReceiveQuizExerciseStub = stub(websocketService, 'receive').returns(wsQuizExerciseSubject);
+
+                courseManagementService = TestBed.inject(CourseManagementService);
+                cmCoursesSubject = new Subject<[Course] | undefined>();
+                cmGetCoursesForNotificationsStub = stub(courseManagementService, 'getCoursesForNotifications').returns(cmCoursesSubject as BehaviorSubject<[Course] | undefined>);
             });
     });
 
@@ -114,7 +134,7 @@ describe('Logs Service', () => {
 
         it('should convert date array from server', fakeAsync(() => {
             //strange method, because notificationDate can only be of type Moment, I can not simulate an input with string for date
-            const notificationArray = [examExerciseUpdateNotification, quizNotification];
+            const notificationArray = [singleUserNotification, quizNotification];
             let serverResponse = notificationArray;
             const expectedResult = notificationArray.sort();
 
@@ -127,7 +147,7 @@ describe('Logs Service', () => {
             tick();
         }));
 
-        it('should subscribe to single user notification updates and receive new notification', fakeAsync(() => {
+        it('should subscribe to single user notification updates and receive new single user notification', fakeAsync(() => {
             notificationService.subscribeToNotificationUpdates().subscribe((notification) => {
                 expect(notification).to.equal(singleUserNotification);
             });
@@ -139,12 +159,70 @@ describe('Logs Service', () => {
             expect(wsSubscribeStub).to.have.been.calledOnceWithExactly(notificationTopic);
             //websocket correctly subscribed to the topic
 
-            expect(wsReceiveStub).to.have.been.calledOnce;
-            //websocket received "receive" call
+            expect(wsReceiveNotificationStub).to.have.been.calledOnce;
+            //websocket "receive" called
 
             //add new single user notification
             wsNotificationSubject.next(singleUserNotification);
             //calls addNotificationToObserver i.e. calls next on subscribeToNotificationUpdates' ReplaySubject
+        }));
+
+        it('should subscribe to group notification updates and receive new group notification', fakeAsync(() => {
+            notificationService.subscribeToNotificationUpdates().subscribe((notification) => {
+                expect(notification).to.equal(groupNotification);
+            });
+
+            tick(); // position of tick is very important here !
+
+            expect(cmGetCoursesForNotificationsStub).to.have.been.calledOnce;
+            //courseManagementService.getCoursesForNotifications had been successfully subscribed to
+
+            //push new courses
+            cmCoursesSubject.next([course]);
+
+            const notificationTopic = `/topic/course/${course.id}/TA`;
+            expect(wsSubscribeStub).to.have.been.calledWith(notificationTopic);
+            //websocket correctly subscribed to the topic
+
+            expect(wsReceiveNotificationStub).to.have.been.called;
+            //websocket "receive" called
+
+            //add new single user notification
+            wsNotificationSubject.next(groupNotification);
+            //calls addNotificationToObserver i.e. calls next on subscribeToNotificationUpdates' ReplaySubject
+        }));
+
+        it('should subscribe to quiz notification updates and receive a new quiz exercise and create a new quiz notification from it', fakeAsync(() => {
+            wsReceiveNotificationStub.restore();
+            wsReceiveQuizExerciseStub = stub(websocketService, 'receive').returns(wsQuizExerciseSubject);
+
+            notificationService.subscribeToNotificationUpdates().subscribe((notification) => {
+                //the quiz notification is created after a new quiz exercise has been detected, therefore the time will always be different
+                notification.notificationDate = undefined;
+                quizNotification.notificationDate = undefined;
+
+                expect(notification).to.be.deep.equal(quizNotification);
+            });
+
+            tick(); // position of tick is very important here !
+
+            expect(cmGetCoursesForNotificationsStub).to.have.been.calledOnce;
+            //courseManagementService.getCoursesForNotifications had been successfully subscribed to
+
+            //push new courses
+            cmCoursesSubject.next([course]);
+
+            const notificationTopic = `/topic/course/${course.id}/TA`;
+            expect(wsSubscribeStub).to.have.been.calledWith(notificationTopic);
+            //websocket correctly subscribed to the topic
+
+            expect(wsReceiveQuizExerciseStub).to.have.been.called;
+            //websocket "receive" called
+
+            //pushes new quizExercise
+            wsQuizExerciseSubject.next(quizExercise);
+            //calls addNotificationToObserver i.e. calls next on subscribeToNotificationUpdates' ReplaySubject
+            tick();
         }));
     });
 });
