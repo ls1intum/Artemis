5009d796360d2abed4083db7945d7855b623ad81
Merve Pinar <merveepinar@gmail.com>
2021-07-17 03:21:23 +0200

    fix for example solution repo in exercise assessment dashboard

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExerciseResource.java
@@ -66,6 +66,8 @@ public class ExerciseResource {
 
     private final ExampleSubmissionRepository exampleSubmissionRepository;
 
+    private final ProgrammingExerciseRepository programmingExerciseRepository;
+
     public ExerciseResource(ExerciseService exerciseService, ParticipationService participationService, UserRepository userRepository, ExamDateService examDateService,
             AuthorizationCheckService authCheckService, TutorParticipationService tutorParticipationService, ExampleSubmissionRepository exampleSubmissionRepository,
             ComplaintRepository complaintRepository, SubmissionRepository submissionRepository, TutorLeaderboardService tutorLeaderboardService,
@@ -81,6 +83,7 @@ public class ExerciseResource {
         this.gradingCriterionRepository = gradingCriterionRepository;
         this.examDateService = examDateService;
         this.exerciseRepository = exerciseRepository;
+        this.programmingExerciseRepository = programmingExerciseRepository;
     }
 
     /**
@@ -149,6 +152,10 @@ public class ExerciseResource {
         // Programming exercises with only automatic assessment should *NOT* be available on the assessment dashboard!
         if (exercise instanceof ProgrammingExercise && exercise.getAssessmentType().equals(AssessmentType.AUTOMATIC)) {
             return badRequest();
+        }
+
+        if (exercise instanceof ProgrammingExercise) {
+            exercise = programmingExerciseRepository.findByIdWithTemplateAndSolutionParticipationElseThrow(exerciseId);
         }
 
         Set<ExampleSubmission> exampleSubmissions = this.exampleSubmissionRepository.findAllWithResultByExerciseId(exerciseId);
