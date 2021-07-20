560ef3464b16a4a09dd6346142845c738c6a863b
Julian Hainzlschmid <julian.Hainzlschmid@gmail.com>
2021-07-20 14:42:58 +0200

    Move interception out of constructor and remove useless timeout

--- src/test/cypress/support/pageobjects/NavigationBar.ts
+++ src/test/cypress/support/pageobjects/NavigationBar.ts
@@ -2,17 +2,14 @@
  * A class which encapsulates UI selectors and actions for the navigation bar at the top.
  */
 export class NavigationBar {
-    constructor() {
-        cy.intercept('GET', '/api/courses/course-management-overview*').as('courseManagementQuery');
-    }
-
     /**
      * Opens the course management page via the menu at the top and waits until it is loaded.
      */
     openCourseManagement() {
         cy.log('Opening course-management page...');
+        cy.intercept('GET', '/api/courses/course-management-overview*').as('courseManagementQuery');
         cy.get('#course-admin-menu').should('be.visible').click();
         cy.wait('@courseManagementQuery', { timeout: 30000 });
-        cy.url({ timeout: 10000 }).should('include', '/course-management');
+        cy.url().should('include', '/course-management');
     }
 }
