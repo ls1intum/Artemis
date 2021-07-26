517621e578bc951d8b0e7e34551de043d390b4a4
Merve Pinar <merveepinar@gmail.com>
2021-07-26 21:16:48 +0200

    make prettier happy

--- src/test/javascript/spec/component/markdown-editor/gradingInstruction-command.component.spec.ts
+++ src/test/javascript/spec/component/markdown-editor/gradingInstruction-command.component.spec.ts
@@ -72,8 +72,6 @@ describe('GradingInstructionCommand', () => {
         comp.ngAfterViewInit();
 
         criterionCommand.execute();
-        expect(comp.aceEditorContainer.getEditor().getValue()).to.equal(
-            '\n' + '[criterion]' + GradingCriterionCommand.text + '\n' + gradingInstructionCommand.instructionText(),
-        );
+        expect(comp.aceEditorContainer.getEditor().getValue()).to.equal('\n' + '[criterion]' + GradingCriterionCommand.text + '\n' + gradingInstructionCommand.instructionText());
     });
 });
