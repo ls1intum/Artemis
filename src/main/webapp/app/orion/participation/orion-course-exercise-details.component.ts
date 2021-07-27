2066a41bb61c01de142f6b56cf93739c2ebcd72a
Martin Dunker <ga27kin@mytum.de>
2021-07-27 15:05:15 +0200

    inline html

--- src/main/webapp/app/orion/participation/orion-course-exercise-details.component.ts
+++ src/main/webapp/app/orion/participation/orion-course-exercise-details.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'jhi-orion-course-exercise-details',
-    templateUrl: './orion-course-exercise-details.component.html',
+    template: `
+        <jhi-course-exercise-details>
+            <ng-template #overrideStudentActions let-exercise="exercise" let-showResult="showResult" let-courseId="courseId">
+                <jhi-orion-exercise-details-student-actions [exercise]="exercise" [showResult]="showResult" [courseId]="courseId"></jhi-orion-exercise-details-student-actions>
+            </ng-template>
+        </jhi-course-exercise-details>
+    `,
 })
 export class OrionCourseExerciseDetailsComponent {
     // only replaces the student actions with Orion student actions in the overview
