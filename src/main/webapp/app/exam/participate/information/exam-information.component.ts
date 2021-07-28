2c0a44dba3ff819b68acbf5a39fa020a8157bf79
JoejoeSt <johannes.stoehr@tum.de>
2021-07-28 17:56:06 +0200

    Stephans feedback

--- src/main/webapp/app/exam/participate/information/exam-information.component.ts
+++ src/main/webapp/app/exam/participate/information/exam-information.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import * as moment from 'moment';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { Exam } from 'app/entities/exam.model';
+import * as examUtils from 'app/exam/participate/exam-utils';
 
 @Component({
     selector: 'jhi-exam-information',
@@ -11,53 +12,23 @@ export class ExamInformationComponent {
     @Input() exam: Exam;
     @Input() studentExam: StudentExam;
 
-    /**
-     * Calculates the end time depending on the individual working time.
-     */
     endTime() {
-        if (!this.exam || !this.exam.endDate) {
-            return undefined;
-        }
-        if (this.studentExam && this.studentExam.workingTime && this.exam.startDate) {
-            return moment(this.exam.startDate).add(this.studentExam.workingTime, 'seconds');
-        }
-        return this.exam.endDate;
+        return examUtils.endTime(this.exam, this.studentExam);
     }
 
     normalWorkingTime(): number | undefined {
-        if (!this.exam || !this.exam.endDate || !this.exam.startDate) {
-            return undefined;
-        }
-        return this.exam.endDate.diff(this.exam.startDate, 'seconds');
+        return examUtils.normalWorkingTime(this.exam);
     }
 
     hasAdditionalWorkingTime(): boolean | undefined {
-        if (!this.exam || !this.exam.endDate || !this.exam.startDate) {
-            return undefined;
-        }
-        if (this.studentExam && this.studentExam.workingTime) {
-            const personalEndDate = moment(this.exam.startDate).add(this.studentExam.workingTime, 'seconds');
-            return personalEndDate.isAfter(this.exam.endDate);
-        }
-        return false;
+        return examUtils.hasAdditionalWorkingTime(this.exam, this.studentExam);
     }
 
     getAdditionalWorkingTime(): number {
-        const personalEndDate = moment(this.exam.startDate).add(this.studentExam.workingTime, 'seconds');
-        return personalEndDate.diff(this.exam.endDate, 'seconds');
+        return examUtils.getAdditionalWorkingTime(this.exam, this.studentExam);
     }
 
     examOverMultipleDays(): boolean {
-        if (!this.exam || !this.exam.startDate || !this.exam.endDate) {
-            return false;
-        }
-        let endDate;
-        if (this.studentExam && this.studentExam.workingTime && this.exam.startDate) {
-            endDate = moment(this.exam.startDate).add(this.studentExam.workingTime, 'seconds');
-        } else {
-            endDate = this.exam.endDate;
-        }
-
-        return endDate.dayOfYear() !== this.exam.startDate.dayOfYear();
+        return examUtils.examOverMultipleDays(this.exam, this.studentExam);
     }
 }
