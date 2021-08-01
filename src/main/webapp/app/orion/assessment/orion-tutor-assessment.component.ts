3eb16417842817c5b086978306dfec00d2b8c7d3
Martin Dunker <ga27kin@mytum.de>
2021-08-01 16:03:50 +0200

    check attempt to initalize feedback in Orion

--- src/main/webapp/app/orion/assessment/orion-tutor-assessment.component.ts
+++ src/main/webapp/app/orion/assessment/orion-tutor-assessment.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, OnInit } from '@angular/core';
+import { CodeEditorTutorAssessmentContainerComponent } from 'app/exercises/programming/assess/code-editor-tutor-assessment-container.component';
 
 @Component({
     selector: 'jhi-orion-course-management-exercises',
     templateUrl: './orion-tutor-assessment.component.html',
 })
-export class OrionTutorAssessmentComponent {}
+export class OrionTutorAssessmentComponent implements AfterViewInit {
+    @ContentChild(CodeEditorTutorAssessmentContainerComponent) container: CodeEditorTutorAssessmentContainerComponent;
+
+    ngAfterViewInit() {
+        console.log(this.container.referencedFeedback);
+    }
+}
