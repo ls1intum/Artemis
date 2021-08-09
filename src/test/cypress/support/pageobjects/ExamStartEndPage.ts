62b576d8f3a681cb7ff9e5f8da7e60646a22af5e
Robert Schauer <ga94bit@mytum.de>
2021-08-09 20:19:23 +0200

    linting

--- src/test/cypress/support/pageobjects/ExamStartEndPage.ts
+++ src/test/cypress/support/pageobjects/ExamStartEndPage.ts
@@ -1,7 +1,6 @@
 import { artemis } from '../ArtemisTesting';
 
 export class ExamStartEndPage {
-
     enterFirstnameLastname() {
         artemis.users.getAccountInfo((account: any) => cy.get('#fullname').type((account.firstName ?? '') + ' ' + (account.lastName ?? '')));
     }
