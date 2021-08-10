2207d6db554f128facae2901fd2c0f849c58b43b
Argert Boja <argert.boja@tum.de>
2021-08-10 11:49:05 +0200

    Implement ModelAssessmentKnowledgeService

---
+++ src/main/java/de/tum/in/www1/artemis/service/ModelAssessmentKnowledgeService.java
@@ -0,0 +1,52 @@
+package de.tum.in.www1.artemis.service;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.artemis.domain.ModelAssessmentKnowledge;
+import de.tum.in.www1.artemis.domain.modeling.ModelingExercise;
+import de.tum.in.www1.artemis.repository.ModelAssesmentKnowledgeRepository;
+import de.tum.in.www1.artemis.repository.ModelingExerciseRepository;
+
+@Service
+public class ModelAssessmentKnowledgeService {
+
+    private final Logger log = LoggerFactory.getLogger(ModelAssessmentKnowledgeService.class);
+
+    private final AuthorizationCheckService authCheckService;
+
+    private final ModelAssesmentKnowledgeRepository modelAssesmentKnowledgeRepository;
+
+    private final ModelingExerciseRepository modelingExerciseRepository;
+
+    public ModelAssessmentKnowledgeService(ModelAssesmentKnowledgeRepository modelAssesmentKnowledgeRepository, AuthorizationCheckService authCheckService,
+            ModelingExerciseRepository modelingExerciseRepository) {
+        this.authCheckService = authCheckService;
+        this.modelAssesmentKnowledgeRepository = modelAssesmentKnowledgeRepository;
+        this.modelingExerciseRepository = modelingExerciseRepository;
+    }
+
+    // delete only when no exercises use the knowledge
+    public void deleteKnowledge(Long knowledgeId) {
+        Set<ModelingExercise> exerciseSet = modelingExerciseRepository.findAllByKnowledgeId(knowledgeId);
+        // If no other exercises use the same knowledge then remove knowledge
+        if (exerciseSet.isEmpty()) {
+            modelAssesmentKnowledgeRepository.deleteById(knowledgeId);
+        }
+    }
+
+    /**
+     * Create new knowledge if exercise is created from scratch
+     *
+     * @return The new created ModelAssessmentKnowledge
+     */
+    public ModelAssessmentKnowledge createNewKnowledge() {
+        ModelAssessmentKnowledge knowledge = new ModelAssessmentKnowledge();
+        modelAssesmentKnowledgeRepository.save(knowledge);
+        return knowledge;
+    }
+
+}
