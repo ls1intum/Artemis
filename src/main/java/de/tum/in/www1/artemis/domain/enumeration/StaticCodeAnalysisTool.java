2a0a3d377a94c2622fe7769ef02a16958d37c651
Alexander Stephan <alexander.stephan@tum.de>
2021-08-10 17:15:52 +0200

    Changed tool order

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/StaticCodeAnalysisTool.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/StaticCodeAnalysisTool.java
@@ -11,7 +11,7 @@ public enum StaticCodeAnalysisTool {
 
     SPOTBUGS(ProgrammingLanguage.JAVA, "spotbugs:spotbugs", "spotbugsXml.xml"), CHECKSTYLE(ProgrammingLanguage.JAVA, "checkstyle:checkstyle", "checkstyle-result.xml"),
     PMD(ProgrammingLanguage.JAVA, "pmd:pmd", "pmd.xml"), PMD_CPD(ProgrammingLanguage.JAVA, "pmd:cpd", "cpd.xml"), SWIFTLINT(ProgrammingLanguage.SWIFT, "", "swiftlint-result.xml"),
-    GCC(ProgrammingLanguage.C, "", "gcc.xml"), CPPCHECK(ProgrammingLanguage.C, "", "cppcheck.xml");
+    CPPCHECK(ProgrammingLanguage.C, "", "cppcheck.xml"), GCC(ProgrammingLanguage.C, "", "gcc.xml"),;
 
     private final ProgrammingLanguage language;
 
