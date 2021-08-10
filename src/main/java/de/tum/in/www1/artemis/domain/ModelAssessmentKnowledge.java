9a38751494a682ad7998b181f3c615737776cab0
Argert Boja <argert.boja@tum.de>
2021-08-10 11:31:59 +0200

    Create knowledge for modeling exercises

---
+++ src/main/java/de/tum/in/www1/artemis/domain/ModelAssessmentKnowledge.java
@@ -0,0 +1,52 @@
+package de.tum.in.www1.artemis.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import de.tum.in.www1.artemis.domain.modeling.ModelElement;
+import de.tum.in.www1.artemis.domain.modeling.ModelingExercise;
+
+@Entity
+@Table(name = "model_assessment_knowledge")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ModelAssessmentKnowledge extends DomainObject {
+
+    @OneToMany(mappedBy = "knowledge", fetch = FetchType.LAZY)
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @JsonIgnoreProperties("knowledge")
+    private Set<ModelingExercise> exercises = new HashSet<>();
+
+    @OneToMany(mappedBy = "knowledge", fetch = FetchType.LAZY)
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @JsonIgnoreProperties("knowledge")
+    private Set<ModelElement> modelElements = new HashSet<>();
+
+    public Set<ModelingExercise> getExercises() {
+        return exercises;
+    }
+
+    public ModelAssessmentKnowledge addExercises(ModelingExercise exercise) {
+        exercise.setKnowledge(this);
+        this.exercises.add(exercise);
+        return this;
+    }
+
+    public Set<ModelElement> getTextBlocks() {
+        return modelElements;
+    }
+
+    public ModelAssessmentKnowledge addModelElement(ModelElement modelElement) {
+        modelElement.setKnowledge(this);
+        this.modelElements.add(modelElement);
+        return this;
+    }
+}
