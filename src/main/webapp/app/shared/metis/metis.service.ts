45e2e95429a55ad371cbce0bd0036b42446b4fe4
Lorena Schlesinger <41366522+lschlesinger@users.noreply.github.com>
2021-08-12 15:19:54 +0200

    Update src/main/webapp/app/shared/metis/metis.service.ts
    Co-authored-by: Lukas Franke <72132281+FrankeLukas@users.noreply.github.com>

--- src/main/webapp/app/shared/metis/metis.service.ts
+++ src/main/webapp/app/shared/metis/metis.service.ts
@@ -282,22 +282,20 @@ export class MetisService {
      */
     static sortPosts(posts: Post[]): Post[] {
         return posts.sort(function (postA, postB) {
-            const postAPlusEmojiCount = postA.reactions?.filter((reaction) => reaction.emojiId === 'heavy_plus_sign').length
-                ? postA.reactions?.filter((reaction) => reaction.emojiId === 'heavy_plus_sign').length
+            const postAVoteEmojiCount = postA.reactions?.filter((reaction) => reaction.emojiId === VOTE_EMOJI_ID).length
+                ? postA.reactions?.filter((reaction) => reaction.emojiId === VOTE_EMOJI_ID).length
                 : 0;
-            const postBPlusEmojiCount = postB.reactions?.filter((reaction) => reaction.emojiId === 'heavy_plus_sign').length
-                ? postB.reactions?.filter((reaction) => reaction.emojiId === 'heavy_plus_sign').length
-                : 0;
+            const postBVoteEmojiCount = postB.reactions?.filter((reaction) => reaction.emojiId === VOTE_EMOJI_ID).length ?? 0;
             if (postA.pinned && !postB.pinned) {
                 return -1;
             }
             if (!postA.pinned && postB.pinned) {
                 return 1;
             }
-            if (postAPlusEmojiCount > postBPlusEmojiCount) {
+            if (postAVoteEmojiCount > postBVoteEmojiCount) {
                 return -1;
             }
-            if (postAPlusEmojiCount < postBPlusEmojiCount) {
+            if (postAVoteEmojiCount < postBVoteEmojiCount) {
                 return 1;
             }
             if (postA.archived && !postB.archived) {
