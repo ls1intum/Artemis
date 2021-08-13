98ad20e27773254c6a49dfac98df9bd6004023b9
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-08-13 14:05:09 +0200

    created first few new server tests for GroupNotificationFactoryTest.java

--- src/test/java/de/tum/in/www1/artemis/domain/notification/GroupNotificationFactoryTest.java
+++ src/test/java/de/tum/in/www1/artemis/domain/notification/GroupNotificationFactoryTest.java
@@ -2,18 +2,21 @@ package de.tum.in.www1.artemis.domain.notification;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import de.tum.in.www1.artemis.domain.*;
+import de.tum.in.www1.artemis.domain.enumeration.AttachmentType;
 import de.tum.in.www1.artemis.domain.enumeration.GroupNotificationType;
+import de.tum.in.www1.artemis.domain.enumeration.NotificationPriority;
 import de.tum.in.www1.artemis.domain.enumeration.NotificationType;
 
 public class GroupNotificationFactoryTest {
 
-    @Mock
+    // @Mock
+    // @Autowired
     private Attachment attachment;
 
     @Mock
@@ -28,30 +31,62 @@ public class GroupNotificationFactoryTest {
     @Mock
     private GroupNotificationType groupNotificationType;
 
+    @BeforeEach
+    public void initTestCase() {
+        attachment = new Attachment().attachmentType(AttachmentType.FILE).link("files/temp/example.txt").name("example");
+
+        course = new Course();
+        course.setId(1L);
+        lecture = new Lecture().title("test").description("test").course(course);
+        attachment.setLecture(lecture);
+    }
+
     @Test
-    public void createNotificationBasedOnAttachment() {
+    public void createNotificationBasedOnAttachment_withNotificationText() {
 
         GroupNotificationFactory groupNotificationFactory = mock(GroupNotificationFactory.class, CALLS_REAL_METHODS);
 
         // prepare method inputs
         NotificationType notificationType = NotificationType.ATTACHMENT_CHANGE;
         String notificationText = "notification text";
-        // attachment =
-        String attachmentName = "AttachmentName";
-
-        when(attachment.getAttachmentUnit()).thenReturn(null);
-        when(attachment.getLecture()).thenReturn(lecture);
-        when(attachment.getName()).thenReturn(attachmentName);
-
-        when(lecture.getCourse()).thenReturn(course);
 
         // create notification
         GroupNotification notificationResult = groupNotificationFactory.createNotification(attachment, user, groupNotificationType, notificationType, notificationText);
 
         // check if created notification is correct
+        String expectedTarget = "{\"message\":\"attachmentUpdated\",\"id\":null,\"entity\":\"lectures\",\"course\":1,\"mainPage\":\"courses\"}";
+
         assertThat(notificationResult.getTitle()).isEqualTo("Attachment updated");
         assertThat(notificationResult.getText()).isEqualTo(notificationText);
+        assertThat(notificationResult.getPriority()).isEqualTo(NotificationPriority.MEDIUM);
+        assertThat(notificationResult.getAuthor()).isEqualTo(user);
+        assertThat(notificationResult.getTarget()).isEqualTo(expectedTarget);
+    }
 
+    @Test
+    public void createNotificationBasedOnAttachment_withoutNotificationText() {
+
+        GroupNotificationFactory groupNotificationFactory = mock(GroupNotificationFactory.class, CALLS_REAL_METHODS);
+
+        // prepare method inputs
+        NotificationType notificationType = NotificationType.ATTACHMENT_CHANGE;
+
+        // create notification
+        GroupNotification notificationResult = groupNotificationFactory.createNotification(attachment, user, groupNotificationType, notificationType, null);
+
+        System.out.println("HELLO");
+        System.out.println(notificationResult.getText());
+        System.out.println("EEED");
+
+        // check if created notification is correct
+        String expectedTarget = "{\"message\":\"attachmentUpdated\",\"id\":null,\"entity\":\"lectures\",\"course\":1,\"mainPage\":\"courses\"}";
+        String expectedText = "Attachment \"" + attachment.getName() + "\" updated.";
+
+        assertThat(notificationResult.getTitle()).isEqualTo("Attachment updated");
+        assertThat(notificationResult.getText()).isEqualTo(expectedText);
+        assertThat(notificationResult.getPriority()).isEqualTo(NotificationPriority.MEDIUM);
+        assertThat(notificationResult.getAuthor()).isEqualTo(user);
+        assertThat(notificationResult.getTarget()).isEqualTo(expectedTarget);
     }
 
 }
