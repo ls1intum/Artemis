0111ed691875d10661dbb8558ba20da62a187193
Robert Schauer <ga94bit@mytum.de>
2021-08-15 15:31:31 +0200

    finish test

--- src/test/cypress/integration/exam/ExamAssessment.spec.ts
+++ src/test/cypress/integration/exam/ExamAssessment.spec.ts
@@ -9,6 +9,8 @@ const courseManagementRequests = artemis.requests.courseManagement;
 
 // page objects
 const examStartEnd = artemis.pageobjects.examStartEnd;
+const modelingEditor = artemis.pageobjects.modelingEditor;
+const modelingAssessment = artemis.pageobjects.modelingExerciseAssessmentEditor;
 
 // Common primitives
 const uid = generateUUID();
@@ -23,6 +25,7 @@ describe('Exam Assessment', () => {
         courseManagementRequests.createCourse(courseName, courseShortName).then((response) => {
             course = response.body;
             courseManagementRequests.addStudentToCourse(course.id, artemis.users.getStudentOne().username);
+            courseManagementRequests.addTutorToCourse(course, artemis.users.getTutor());
         });
     });
 
@@ -32,20 +35,23 @@ describe('Exam Assessment', () => {
     });
 
     after('Delete Course', () => {
-       courseManagementRequests.deleteCourse(course.id);
+        cy.login(artemis.users.getAdmin());
+        courseManagementRequests.deleteCourse(course.id);
     });
 
     describe('Exam Assessment', () => {
         let exam: any;
         const student = artemis.users.getStudentOne();
+        const tutor = artemis.users.getTutor();
         let exerciseGroup: any;
 
         beforeEach('Create Exam', () => {
             const examContent = new CypressExamBuilder(course)
                 .title(examTitle)
                 .visibleDate(dayjs().subtract(3, 'days'))
-                .startDate(dayjs().subtract(2, 'days'))
+                .startDate(dayjs().subtract(3, 'hours'))
                 .endDate(dayjs().add(15, 'seconds'))
+                .gracePeriod(1)
                 .build();
             courseManagementRequests.createExam(examContent).then((examResponse) => {
                 exam = examResponse.body;
@@ -69,6 +75,30 @@ describe('Exam Assessment', () => {
                 // TODO: in the future this might become redundant and should be replaced with requests handling the submission creation
                 cy.login(student, '/courses/' + course.id + '/exams/' + exam.id);
                 examStartEnd.startExam();
+                cy.contains('Cypress Modeling Exercise').should('be.visible').click();
+                modelingEditor.addComponentToModel(1);
+                modelingEditor.addComponentToModel(2);
+                modelingEditor.addComponentToModel(3);
+                cy.intercept('PUT', '/api/exercises/' + modelingExercise.id + '/modeling-submissions').as('createModelingSubmission');
+                cy.contains('Save').click();
+                cy.wait('@createModelingSubmission');
+                cy.contains('Hand in early').click();
+                examStartEnd.finishExam();
+                //
+                cy.login(tutor, '/course-management/' + course.id + '/exams');
+                cy.contains('Assessment Dashboard').click();
+                cy.get('.btn').contains('Exercise Dashboard').should('be.visible').click();
+                cy.contains('I have read and understood the instructions').click();
+                cy.get('.btn').contains('Start new assessment').should('be.visible').click();
+                cy.contains('You have the lock for this assessment').should('be.visible');
+                modelingAssessment.addNewFeedback(2, 'Noice');
+                modelingAssessment.openAssessmentForComponent(1);
+                modelingAssessment.assessComponent(1, 'Good');
+                modelingAssessment.openAssessmentForComponent(2);
+                modelingAssessment.assessComponent(0, 'Neutral');
+                modelingAssessment.openAssessmentForComponent(3);
+                modelingAssessment.assessComponent(-1, 'Wrong');
+                cy.contains('Submit').click();
             });
         });
     });
--- src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
+++ src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
@@ -4,12 +4,20 @@
 export class ModelingExerciseAssessmentEditor {
 
     openAssessmentForComponent(componentNumber: number) {
-        cy.getSettled(`.sc-furvIG >> :nth-child(${componentNumber})`).dblclick('top', { force: true });
+        cy.get('.apollon-row').getSettled(`.sc-furvIG >> :nth-child(${componentNumber})`).children().eq(0).dblclick('top', { force: true });
     }
 
     assessComponent(points: number, feedback: string) {
         cy.get('.sc-nVjpj > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(2)').type(`${points}`);
         cy.get('.sc-nVjpj > :nth-child(1) > :nth-child(3)').type(`${feedback}`);
+    }
+
+    addNewFeedback(points: number, feedback?: string) {
+        cy.get('.btn').contains('Add new Feedback').click();
+        cy.get('.col-lg-6 >>>> :nth-child(1) > :nth-child(2)').clear().type(points.toString());
+        if (feedback) {
+            cy.get('.col-lg-6 >>>> :nth-child(2) > :nth-child(2)').type(feedback);
+        }
     }
 
     saveExampleAssessment() {
