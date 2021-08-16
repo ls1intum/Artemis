875ce69c75fc754b2b082432a1746bda8a8fe942
Lorena Schlesinger <lorena.schlesinger@online.de>
2021-08-16 12:18:33 +0200

    harmonize doc strings

--- src/main/webapp/app/shared/metis/postings-reactions-bar/postings-reactions-bar.component.ts
+++ src/main/webapp/app/shared/metis/postings-reactions-bar/postings-reactions-bar.component.ts
@@ -10,7 +10,7 @@ const ARCHIVE_EMOJI_ID = 'open_file_folder';
 const ARCHIVE_EMOJI_UNICODE = '1F4C2';
 
 /*
-Event triggered by the emoji mart component, including EmojiData
+event triggered by the emoji mart component, including EmojiData
  */
 interface ReactionEvent {
     $event: Event;
@@ -18,8 +18,8 @@ interface ReactionEvent {
 }
 
 /*
-Calculated per emojiId on a post, counts the amount of users reacted with a certain emoji;
-hasReacted indicates if the currently logged in user is among those counted users, used for highlighting
+represents the amount of users that reacted
+hasReacted indicates if the currently logged in user is among those counted users
  */
 interface ReactionCount {
     count: number;
@@ -27,7 +27,7 @@ interface ReactionCount {
 }
 
 /*
-DataStructure used for displaying emoji reactions with counts on postings, Maps the ReactionCount to each emojiId
+data structure used for displaying emoji reactions with counts on postings
  */
 interface ReactionCountMap {
     [emojiId: string]: ReactionCount;
@@ -37,6 +37,9 @@ interface ReactionCountMap {
 export abstract class PostingsReactionsBarDirective<T extends Posting> implements OnInit, OnChanges {
     pinEmojiId: string = PIN_EMOJI_ID;
     archiveEmojiId: string = ARCHIVE_EMOJI_ID;
+    /*
+     * icons (as svg paths) to be used as category preview image in emoji mart selector
+     */
     categoriesIcons: { [key: string]: string } = {
         // category 'recent' (would show recently used emojis) is overwritten by a preselected set of emojis for that course,
         // therefore category icon is an asterisk (indicating customization) instead of a clock (indicating the "recently used"-use case)
@@ -48,13 +51,13 @@ export abstract class PostingsReactionsBarDirective<T extends Posting> implement
     currentUserIsAtLeastTutor: boolean;
 
     /*
-    Currently predefined fixed set of emojis that should be used within a course,
+    currently predefined fixed set of emojis that should be used within a course,
     they will be listed on first page of the emoji-mart selector
      */
     selectedCourseEmojis: string[];
 
     /*
-    A map that lists associated reaction (by emojiId) for the current posting together with its count
+    map that lists associated reaction (by emojiId) for the current posting together with its count
     and a flag that indicates if the current user has used this reaction
      */
     reactionCountMap: ReactionCountMap = {};
