910228f1b8b5f686293b2bf0390d1b0482368901
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-08-16 19:16:54 +0200

    first realisation of upper icon and full user name of user-settings.component

--- src/main/webapp/app/shared/user-settings/user-settings.component.ts
+++ src/main/webapp/app/shared/user-settings/user-settings.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'app/core/user/user.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'jhi-user-settings',
     templateUrl: 'user-settings.component.html',
     styleUrls: ['user-settings.component.scss'],
 })
-export class UserSettingsComponent {}
+export class UserSettingsComponent implements OnInit {
+    currAccount?: User;
+
+    private authStateSubscription: Subscription;
+
+    constructor(private accountService: AccountService) {}
+
+    ngOnInit() {
+        this.authStateSubscription = this.accountService
+            .getAuthenticationState()
+            .pipe(tap((user: User) => (this.currAccount = user)))
+            .subscribe();
+    }
+}
