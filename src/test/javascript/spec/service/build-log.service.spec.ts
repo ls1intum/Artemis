0049c28cf276bc522f1496f8a5a98b69ca6a161a
Christian Femers <christian.femers@tum.de>
2021-08-17 19:13:33 +0200

    Remove unused client test import

--- src/test/javascript/spec/service/build-log.service.spec.ts
+++ src/test/javascript/spec/service/build-log.service.spec.ts
@@ -2,7 +2,6 @@ import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { BuildLogService } from 'app/exercises/programming/shared/service/build-log.service';
 import { BuildLogEntry, BuildLogType } from 'app/entities/build-log.model';
 import { SERVER_API_URL } from 'app/app.constants';
-import { HttpParams } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('Build Log Service', () => {
