4c93c6be90ca481ec17841cc3aad6c3bea4037fd
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-08-18 18:04:47 +0200

    mostly finished with settings ui (maybe some allignment fixes are still needed)

--- src/main/webapp/app/shared/user-settings/notification-settings/notification-settings.component.ts
+++ src/main/webapp/app/shared/user-settings/notification-settings/notification-settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'jhi-notification-settings',
     templateUrl: './notification-settings.component.html',
-    styles: [],
+    styleUrls: ['../user-settings.component.scss'],
 })
 export class NotificationSettingsComponent implements OnInit {
     constructor() {}
