6420e940058eaeeaf18aebb2690b6aa9e9db5690
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-08-19 14:49:39 +0200

    adjusted save button

--- src/main/webapp/app/shared/user-settings/user-settings.route.ts
+++ src/main/webapp/app/shared/user-settings/user-settings.route.ts
@@ -4,25 +4,6 @@ import { AccountInformationComponent } from 'app/shared/user-settings/account-in
 import { NotificationSettingsComponent } from 'app/shared/user-settings/notification-settings/notification-settings.component';
 
 export const userSettingsState: Routes = [
-    /*
-    {
-        path: 'user-settings',
-        component: UserSettingsComponent,
-    },
-    {
-        path: 'account',
-        component: AccountInformationComponent,
-    },
-    {
-        path: 'notifications',
-        component: NotificationSettingsComponent,
-    },
-    {//maybe (not) needed? -> goal the accout information should be displayed (first) when going to user-settings
-        path: '',
-        redirectTo: 'account',
-        pathMatch: 'full',
-    }
-     */
     {
         path: 'user-settings',
         component: UserSettingsComponent,
