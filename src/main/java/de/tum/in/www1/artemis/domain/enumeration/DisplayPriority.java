58ab8c500173e03d33895f36898fe1c539c5367e
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-08-25 14:34:57 +0200

    fix enum doc

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/DisplayPriority.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/DisplayPriority.java
@@ -1,7 +1,7 @@
 package de.tum.in.www1.artemis.domain.enumeration;
 
 /**
- * The AssessmentType enumeration.
+ * The priority with which a post is shown in a list, PINNED represents a high priority, whereas ARCHIVED low priority.
  */
 public enum DisplayPriority {
     PINNED, ARCHIVED, NONE
