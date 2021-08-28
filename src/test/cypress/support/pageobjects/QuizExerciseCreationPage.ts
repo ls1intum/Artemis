628b5af1fb57d9a29ce5d839960135d2215dd5d8
Robert Schauer <ga94bit@mytum.de>
2021-08-28 17:40:28 +0200

    no force

--- src/test/cypress/support/pageobjects/QuizExerciseCreationPage.ts
+++ src/test/cypress/support/pageobjects/QuizExerciseCreationPage.ts
@@ -9,7 +9,7 @@ export class QuizExerciseCreationPage {
         cy.get('#quiz-add-mc-question').click();
         cy.get('#mc-question-title').type(title);
         cy.fixture('quiz_exercise_fixtures/MultipleChoiceQuiz.txt').then((fileContent) => {
-            cy.get('.ace_text-input').focus().clear({ force: true }).type(fileContent, { force: true });
+            cy.get('.ace_text-input').focus().clear().type(fileContent);
         });
     }
 
@@ -17,7 +17,7 @@ export class QuizExerciseCreationPage {
         cy.get('#quiz-add-short-answer-question').click();
         cy.get('#short-answer-question-title').type(title);
         cy.fixture('quiz_exercise_fixtures/ShortAnswerQuiz.txt').then((fileContent) => {
-            cy.get('.ace_text-input').focus().clear({ force: true }).type(fileContent, { force: true });
+            cy.get('.ace_text-input').focus().clear().type(fileContent);
             cy.get('[jhitranslate="artemisApp.shortAnswerQuestion.editor.visual"]').click();
         });
     }
