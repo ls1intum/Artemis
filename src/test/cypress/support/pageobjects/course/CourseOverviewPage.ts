3411daac707e56511e20d29bf1d11e3c8291da0b
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-08-28 23:41:03 +0200

    Improved selectors and strings in pageobject

--- src/test/cypress/support/pageobjects/course/CourseOverviewPage.ts
+++ src/test/cypress/support/pageobjects/course/CourseOverviewPage.ts
@@ -1,19 +1,20 @@
+import { POST, BASE_API, GET } from './../../constants';
 /**
  * A class which encapsulates UI selectors and actions for the course overview page (/courses/*).
  */
 export class CourseOverviewPage {
     private getExerciseCardRootElement(exerciseName: string) {
-        return cy.contains(exerciseName).parents('.course-exercise-row');
+        return cy.get('.course-body-container').contains(exerciseName).parents('.course-exercise-row');
     }
 
     startExercise(exerciseName: string) {
-        cy.intercept('POST', '/api/courses/*/exercises/*/participations').as('participateInExerciseQuery');
+        cy.intercept(POST, BASE_API + 'courses/*/exercises/*/participations').as('participateInExerciseQuery');
         this.getExerciseCardRootElement(exerciseName).find('.start-exercise').click();
         cy.wait('@participateInExerciseQuery');
     }
 
     openRunningProgrammingExercise(exerciseName: string) {
-        cy.intercept('GET', '/api/programming-exercise-participations/*/student-participation-with-latest-result-and-feedbacks').as('initialQuery');
+        cy.intercept(GET, BASE_API + 'programming-exercise-participations/*/student-participation-with-latest-result-and-feedbacks').as('initialQuery');
         this.getExerciseCardRootElement(exerciseName).find('[buttonicon="folder-open"]').click();
         cy.wait('@initialQuery').wait(2000);
     }
