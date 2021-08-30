96bb756b788b583b11797db7518acebc2960feeb
Alexander Stephan <alexander.stephan@tum.de>
2021-08-30 16:34:39 +0200

    Improve SCA scripts

--- src/main/resources/templates/bamboo/c/staticCodeAnalysisRuns/1_run_static_code_analysis.sh
+++ src/main/resources/templates/bamboo/c/staticCodeAnalysisRuns/1_run_static_code_analysis.sh
@@ -4,13 +4,11 @@
 # Task Description:
 # Build and run all tests if the compilation succeeds
 # ------------------------------
+
 # Artemis will look for SCA reports in target, specified in BambooBuildPlanService.java in Artemis
 mkdir target
-
-cp tests/Converter.py assignment || true
-
 # Go into students assignment and save the SCA results in gcc.xml
 cd assignment || exit
-chmod +x Converter.py
+chmod +x ../tests/Converter.py
 make staticAnalysis 2> gcc.txt
-python3 Converter.py -i gcc.txt -o ../target/gcc.xml
+python3 ../tests/Converter.py -i gcc.txt -o ../target/gcc.xml
