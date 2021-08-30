567aa80a896a91f1fc88230cbda3009f6c0ead94
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-08-30 19:04:03 +0200

    Added a fourth student account for cypress

--- src/test/cypress/support/users.ts
+++ src/test/cypress/support/users.ts
@@ -35,6 +35,13 @@ export class CypressUserManagement {
     }
 
     /**
+     * @returns the fourth testing account with student rights.
+     */
+    public getStudentFour(): CypressCredentials {
+        return this.getUserWithId('110');
+    }
+
+    /**
      * @returns an instructor account.
      */
     public getInstructor(): CypressCredentials {
