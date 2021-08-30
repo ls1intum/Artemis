6f5641e9b57be58363db277e603a2cae24402611
Ndricim Rrapi <ndricim.rrapi@tum.de>
2021-08-30 04:33:55 +0200

    Add numbers

--- src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.component.ts
+++ src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.component.ts
@@ -10,6 +10,9 @@ import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 })
 export class TutorEffortStatisticsComponent implements OnInit {
     tutorEfforts: TutorEffort[];
+    numberOfSubmissions: number;
+    totalTimeSpent: number;
+    averageTimeSpent: number;
     currentExerciseId: number;
     currentCourseId: number;
 
@@ -27,6 +30,12 @@ export class TutorEffortStatisticsComponent implements OnInit {
             (res: TutorEffort[]) => {
                 this.tutorEfforts = res!;
                 console.log('test', this.tutorEfforts);
+                this.numberOfSubmissions = this.tutorEfforts.reduce((n, { numberOfSubmissionsAssessed }) => n + numberOfSubmissionsAssessed, 0);
+                this.totalTimeSpent = this.tutorEfforts.reduce((n, { totalTimeSpentMinutes }) => n + totalTimeSpentMinutes, 0);
+                const avgTemp = this.numberOfSubmissions / this.totalTimeSpent;
+                if (avgTemp) {
+                    this.averageTimeSpent = Math.round((avgTemp + Number.EPSILON) * 100) / 100;
+                }
             },
             (error) => {
                 console.error('Error while retrieving tutor effort statistics:', error);
