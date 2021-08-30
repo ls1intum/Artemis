54fe9598d527e9b943aee1947650c7e9add68865
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-08-31 00:01:56 +0200

    Fixed SCA grading pageobject and added save action

--- src/test/cypress/support/pageobjects/exercises/programming/CodeAnalysisGradingPage.ts
+++ src/test/cypress/support/pageobjects/exercises/programming/CodeAnalysisGradingPage.ts
@@ -1,16 +1,18 @@
-import { COURSE_MANAGEMENT_BASE } from '../../../requests/CourseManagementRequests';
-
 /**
- * A class which encapsulates UI selectors and actions for the Online Editor Page.
+ * A class which encapsulates UI selectors and actions for static code analysis grading configuration page.
  */
 export class CodeAnalysisGradingPage {
     visit(courseId: number, exerciseId: number) {
-        cy.visit(`${COURSE_MANAGEMENT_BASE}${courseId}/programming-exercises/${exerciseId}/grading/code-analysis`);
+        cy.visit(`course-management/${courseId}/programming-exercises/${exerciseId}/grading/code-analysis`);
     }
 
     makeEveryScaCategoryInfluenceGrading() {
         cy.get('select').each((category) => {
-            cy.wrap(category).select('GRADING');
+            cy.wrap(category).select('GRADED');
         });
+    }
+
+    saveChanges() {
+        cy.get('#save-table-button').click();
     }
 }
