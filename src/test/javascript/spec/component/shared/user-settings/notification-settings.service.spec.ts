1af17942e238ddb6615974a05c7ba63d4d7d247d
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-09-02 10:01:41 +0200

    fixed client test

--- src/test/javascript/spec/component/shared/user-settings/notification-settings.service.spec.ts
+++ src/test/javascript/spec/component/shared/user-settings/notification-settings.service.spec.ts
@@ -9,13 +9,11 @@ import { NotificationType, OriginalNotificationType } from 'app/entities/notific
 import { OptionSpecifier } from 'app/shared/constants/user-settings.constants';
 
 export const notificationOptionCoreA: NotificationOptionCore = {
-    id: 1,
     optionSpecifier: OptionSpecifier.NOTIFICATION__EXERCISE_NOTIFICATION__EXERCISE_OPEN_FOR_PRACTICE,
     webapp: false,
     email: false,
 };
 export const notificationOptionCoreB: NotificationOptionCore = {
-    id: 2,
     optionSpecifier: OptionSpecifier.NOTIFICATION__INSTRUCTOR_EXCLUSIVE_NOTIFICATIONS__COURSE_AND_EXAM_ARCHIVING_STARTED,
     webapp: true,
     email: false,
