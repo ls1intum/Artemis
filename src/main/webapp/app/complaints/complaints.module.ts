50b58f278791f589b2c6083981693f292d793238
Julian Christl <julian.christl@tum.de>
2021-09-07 20:45:18 +0200

    fix module and client style

--- src/main/webapp/app/complaints/complaints.module.ts
+++ src/main/webapp/app/complaints/complaints.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import { ComplaintsFormComponent } from './form/complaints-form.component';
+import { ComplaintsFormComponent } from 'app/complaints/form/complaints-form.component';
 import { ClipboardModule } from 'ngx-clipboard';
 import { ComplaintService } from 'app/complaints/complaint.service';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { ComplaintStudentViewComponent } from './complaints-for-students/complaint-student-view.component';
-import { ComplaintRequestComponent } from './request/complaint-request.component';
+import { ComplaintStudentViewComponent } from 'app/complaints/complaints-for-students/complaint-student-view.component';
+import { ComplaintRequestComponent } from 'app/complaints/request/complaint-request.component';
 import { ComplaintResponseComponent } from 'app/complaints/response/complaint-response.component';
 
 @NgModule({
     imports: [ArtemisSharedModule, ClipboardModule],
     declarations: [ComplaintsFormComponent, ComplaintStudentViewComponent, ComplaintRequestComponent, ComplaintResponseComponent],
-    exports: [ComplaintsFormComponent, ComplaintStudentViewComponent, ComplaintRequestComponent, ComplaintResponseComponent],
+    exports: [ComplaintStudentViewComponent],
     providers: [ComplaintService],
 })
 export class ArtemisComplaintsModule {}
--- src/main/webapp/app/complaints/request/complaint-request.component.ts
+++ src/main/webapp/app/complaints/request/complaint-request.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Complaint, ComplaintType } from 'app/entities/complaint.model';
 
 @Component({
