6240c90aabaa71ab564b795f17e0f2d9dd48e5de
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-09-09 11:45:44 +0200

    started refactoring title/types

---
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/NotificationTitleTypeConstants.java
@@ -0,0 +1,6 @@
+package de.tum.in.www1.artemis.domain.notification;
+
+public class NotificationTitleTypeConstants {
+
+    public static final String LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE = "Live Exam Exercise Update";
+}
