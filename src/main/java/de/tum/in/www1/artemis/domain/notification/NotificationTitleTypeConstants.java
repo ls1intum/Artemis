a82e11c43771f565e8c49e44cc7a786a311ed471
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-09-09 12:46:46 +0200

    continue refactoring title/types

--- src/main/java/de/tum/in/www1/artemis/domain/notification/NotificationTitleTypeConstants.java
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/NotificationTitleTypeConstants.java
@@ -3,4 +3,38 @@ package de.tum.in.www1.artemis.domain.notification;
 public class NotificationTitleTypeConstants {
 
     public static final String LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE = "Live Exam Exercise Update";
+
+    public static final String ATTACHMENT_CHANGE_TITLE = "Attachment updated";
+
+    public static final String EXERCISE_CREATED_TITLE = "Exercise created";
+
+    public static final String EXERCISE_PRACTICE_TITLE = "Exercise open for practice";
+
+    public static final String QUIZ_EXERCISE_STARTED_TITLE = "Quiz started";
+
+    public static final String EXERCISE_UPDATED_TITLE = "Exercise updated";
+
+    public static final String DUPLICATE_TEST_CASE_TITLE = "Duplicate test case was found.";
+
+    public static final String ILLEGAL_SUBMISSION_TITLE = "Illegal submission of a student.";
+
+    public static final String NEW_POST_FOR_EXERCISE_TITLE = "New Post";
+
+    public static final String NEW_POST_FOR_LECTURE_TITLE = "New Post";
+
+    public static final String NEW_ANSWER_POST_FOR_EXERCISE_TITLE = "New Reply";
+
+    public static final String NEW_ANSWER_POST_FOR_LECTURE_TITLE = "New Reply";
+
+    public static final String COURSE_ARCHIVE_STARTED_TITLE = "Course archival started";
+
+    public static final String COURSE_ARCHIVE_FINISHED_TITLE = "Course archival finished";
+
+    public static final String COURSE_ARCHIVE_FAILED_TITLE = "Course archival failed";
+
+    public static final String EXAM_ARCHIVE_STARTED_TITLE = "Exam archival started";
+
+    public static final String EXAM_ARCHIVE_FINISHED_TITLE = "Exam archival finished";
+
+    public static final String EXAM_ARCHIVE_FAILED_TITLE = "Exam archival failed";
 }
