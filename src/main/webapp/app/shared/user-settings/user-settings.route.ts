4ea6f233464c537e713da679ea44abb156f53ee8
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-09-14 12:31:32 +0200

    testing routing

--- src/main/webapp/app/shared/user-settings/user-settings.route.ts
+++ src/main/webapp/app/shared/user-settings/user-settings.route.ts
@@ -3,12 +3,16 @@ import { UserSettingsContainerComponent } from 'app/shared/user-settings/user-se
 import { AccountInformationComponent } from 'app/shared/user-settings/account-information/account-information.component';
 import { NotificationSettingsComponent } from 'app/shared/user-settings/notification-settings/notification-settings.component';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { Authority } from 'app/shared/constants/authority.constants';
 
 export const userSettingsState: Routes = [
     {
         path: 'user-settings',
         component: UserSettingsContainerComponent,
         canActivate: [UserRouteAccessService],
+        data: {
+            authorities: [Authority.USER],
+        },
         children: [
             {
                 // redirects to account information when user-settings are opened
