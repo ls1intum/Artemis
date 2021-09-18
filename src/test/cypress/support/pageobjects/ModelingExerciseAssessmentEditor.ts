94b49a9749f52e7172884f186a877f1592a241f6
Robert Schauer <56715566+rs-maker@users.noreply.github.com>
2021-09-18 19:45:58 +0200

    Fix broken and unstable e2e cypress tests (#4015)

--- src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
+++ src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
@@ -1,14 +1,19 @@
+import { MODELING_SPACE } from './ModelingEditor';
+
+// TODO: find or create better selectors for this
+const FEEDBACK_CONTAINER = '.sc-lcuiOb';
+
 /**
  * A class which encapsulates UI selectors and actions for the Modeling Exercise Assessment editor
  */
 export class ModelingExerciseAssessmentEditor {
     openAssessmentForComponent(componentNumber: number) {
-        cy.get('.apollon-row').getSettled(`.sc-furvIG >> :nth-child(${componentNumber})`).children().eq(0).dblclick('top', { force: true });
+        cy.get('.apollon-row').getSettled(`${MODELING_SPACE} >> :nth-child(${componentNumber})`).children().eq(0).dblclick('top', { force: true });
     }
 
     assessComponent(points: number, feedback: string) {
-        cy.get('.sc-nVjpj > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(2)').type(`${points}`);
-        cy.get('.sc-nVjpj > :nth-child(1) > :nth-child(3)').type(`${feedback}`);
+        cy.get(`${FEEDBACK_CONTAINER} > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(2)`).type(`${points}`);
+        cy.get(`${FEEDBACK_CONTAINER} > :nth-child(1) > :nth-child(3)`).type(`${feedback}`);
     }
 
     addNewFeedback(points: number, feedback?: string) {
