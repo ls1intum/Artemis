e7a27ae0b5c52ebbc2503c4af789fea4f0e03273
Robert Schauer <ga94bit@mytum.de>
2021-09-18 11:40:49 +0200

    remove duplicate page object

--- src/test/cypress/support/pageobjects/MultipleChoiceQuiz.ts
+++ src/test/cypress/support/pageobjects/MultipleChoiceQuiz.ts
@@ -1,3 +1,5 @@
+import { BASE_API, POST } from '../constants';
+
 export class MultipleChoiceQuiz {
     getQuizBody() {
         return cy.get('.mc-question');
@@ -5,11 +7,15 @@ export class MultipleChoiceQuiz {
 
     tickAnswerOption(optionNumber: number) {
         this.getQuizBody()
-            .get('#answer-option-' + optionNumber + ' > .selection > .ng-fa-icon > .svg-inline--fa')
+            .get('#answer-option-' + optionNumber)
+            .find('.svg-inline--fa')
+            .first()
             .click();
     }
 
     submit() {
-        cy.get('.jhi-btn').contains('Submit').click();
+        cy.intercept(POST, BASE_API + 'exercises/*/submissions/live').as('createQuizExercise');
+        cy.get('.jhi-btn').should('have.text', 'Submit').click();
+        return cy.wait('@createQuizExercise');
     }
 }
