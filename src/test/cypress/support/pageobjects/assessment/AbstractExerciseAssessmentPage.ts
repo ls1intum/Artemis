18037cec8ca640ce8baa97df2863a65e4b64a662
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-09-20 18:15:43 +0200

    Improved feedback selectors

--- src/test/cypress/support/pageobjects/assessment/AbstractExerciseAssessmentPage.ts
+++ src/test/cypress/support/pageobjects/assessment/AbstractExerciseAssessmentPage.ts
@@ -3,15 +3,17 @@ import { PUT, BASE_API } from './../../constants';
  * Parent class for all exercise assessment pages.
  */
 export abstract class AbstractExerciseAssessmentPage {
+    readonly unreferencedFeedbackSelector = 'jhi-unreferenced-feedback';
+
     getInstructionsRootElement() {
         return cy.get('[jhitranslate="artemisApp.textAssessment.instructions"]').parents('.card');
     }
 
     addNewFeedback(points: number, feedback?: string) {
         cy.get('.btn-success').contains('Add new Feedback').click();
-        cy.get('.col-lg-6 >>>> :nth-child(1) > :nth-child(2)').clear().type(points.toString());
+        cy.get(this.unreferencedFeedbackSelector).find('input[type="number"]').clear().type(points.toString());
         if (feedback) {
-            cy.get('.col-lg-6 >>>> :nth-child(2) > :nth-child(2)').type(feedback);
+            cy.get(this.unreferencedFeedbackSelector).find('textarea').clear().type(feedback);
         }
     }
 
