cc7d7b8a9acba19e077e8febbd284e410b4cfc11
Robert Schauer <ga94bit@mytum.de>
2021-09-20 14:00:40 +0200

    add source to doc

--- src/test/cypress/support/index.ts
+++ src/test/cypress/support/index.ts
@@ -19,7 +19,7 @@ import './commands';
 import '@4tw/cypress-drag-drop';
 // Imports utility functions
 import './utils';
-// Imports cy.waitUntil
+// Imports cy.waitUntil https://github.com/NoriSte/cypress-wait-until
 import 'cypress-wait-until';
 
 // Alternatively you can use CommonJS syntax:
