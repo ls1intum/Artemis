55170828b6005afab785b76838f68ffc95a64a05
Alexander Stephan <alexander.stephan@tum.de>
2021-09-22 00:40:47 +0200

    Permission and dependency fix, remove SCA test case from template

--- src/main/resources/templates/bamboo/c/staticCodeAnalysisRuns/1_run_static_code_analysis.sh
+++ src/main/resources/templates/bamboo/c/staticCodeAnalysisRuns/1_run_static_code_analysis.sh
@@ -6,8 +6,11 @@
 # ------------------------------
 
 # Artemis will look for SCA reports in target, specified in BambooBuildPlanService.java in Artemis
+rm -rf target
 mkdir target
-# Go into students assignment and save the SCA results in gcc.xml
+sudo chown artemis_user:artemis_user target -R
+
+# Navigate into students assignment and save the SCA results in gcc.xml
 cd assignment || exit
 chmod +x ../tests/Converter.py
 make staticAnalysis 2> gcc.txt
