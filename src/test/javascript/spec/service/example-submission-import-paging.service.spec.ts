7490b794b26f01eef0e336e64d66c35917795023
Merve Pinar <merveepinar@gmail.com>
2021-09-24 11:26:47 +0200

    use only jest for assertions

--- src/test/javascript/spec/service/example-submission-import-paging.service.spec.ts
+++ src/test/javascript/spec/service/example-submission-import-paging.service.spec.ts
@@ -7,14 +7,8 @@ import { take } from 'rxjs/operators';
 import { MockSyncStorage } from '../helpers/mocks/service/mock-sync-storage.service';
 import { MockTranslateService } from '../helpers/mocks/service/mock-translate.service';
 import { ExampleSubmissionImportPagingService } from 'app/exercises/shared/example-submission/example-submission-import/example-submission-import-paging.service';
-import sinonChai from 'sinon-chai';
-import * as chai from 'chai';
-import * as sinon from 'sinon';
 import { Exercise } from 'app/entities/exercise.model';
 import { TextSubmission } from 'app/entities/text-submission.model';
-
-chai.use(sinonChai);
-const expect = chai.expect;
 
 describe('Example Submission Import Paging Service', () => {
     let injector: TestBed;
@@ -37,7 +31,6 @@ describe('Example Submission Import Paging Service', () => {
 
     afterEach(() => {
         httpMock.verify();
-        sinon.restore();
     });
 
     it('should find submission', fakeAsync(() => {
@@ -49,13 +42,13 @@ describe('Example Submission Import Paging Service', () => {
         service
             .searchForSubmissions(pageable, exercise.id!)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp).to.equal(searchResult));
+            .subscribe((resp) => expect(resp).toEqual(searchResult));
         const req = httpMock.expectOne({ method: 'GET' });
-        expect(req.request.params.get('pageSize')).to.equal('2');
-        expect(req.request.params.get('page')).to.equal('3');
-        expect(req.request.params.get('sortingOrder')).to.equal(SortingOrder.DESCENDING);
-        expect(req.request.params.get('searchTerm')).to.equal('testSearchTerm');
-        expect(req.request.params.get('sortedColumn')).to.equal('testSortedColumn');
+        expect(req.request.params.get('pageSize')).toEqual('2');
+        expect(req.request.params.get('page')).toEqual('3');
+        expect(req.request.params.get('sortingOrder')).toEqual(SortingOrder.DESCENDING);
+        expect(req.request.params.get('searchTerm')).toEqual('testSearchTerm');
+        expect(req.request.params.get('sortedColumn')).toEqual('testSortedColumn');
 
         req.flush(searchResult);
         tick();
