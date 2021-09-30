03166d5860565406305dcb99d3b2022617080f29
tim_cremer <tim.cremer@tum.de>
2021-09-30 12:59:54 +0200

    removed code clones and renamed the button

--- src/main/webapp/app/exam/manage/exam-management.module.ts
+++ src/main/webapp/app/exam/manage/exam-management.module.ts
@@ -23,8 +23,6 @@ import { ArtemisSharedComponentModule } from 'app/shared/components/shared-compo
 import { ArtemisMarkdownEditorModule } from 'app/shared/markdown-editor/markdown-editor.module';
 import { MomentModule } from 'ngx-moment';
 import { DurationPipe } from 'app/shared/pipes/artemis-duration.pipe';
-import { StudentsExamImportDialogComponent } from 'app/exam/manage/students/students-exam-import-dialog/students-exam-import-dialog.component';
-import { StudentsExamImportButtonComponent } from 'app/exam/manage/students/students-exam-import-dialog/students-exam-import-button.component';
 import { StudentExamStatusComponent } from 'app/exam/manage/student-exams/student-exam-status.component';
 import { StudentExamSummaryComponent } from 'app/exam/manage/student-exams/student-exam-summary.component';
 import { ArtemisParticipationSummaryModule } from 'app/exam/participate/summary/exam-participation-summary.module';
@@ -87,8 +85,6 @@ const ENTITY_STATES = [...examManagementState];
         StudentExamsComponent,
         TestRunManagementComponent,
         CreateTestRunModalComponent,
-        StudentsExamImportDialogComponent,
-        StudentsExamImportButtonComponent,
         StudentExamDetailComponent,
         DurationPipe,
         StudentExamSummaryComponent,
