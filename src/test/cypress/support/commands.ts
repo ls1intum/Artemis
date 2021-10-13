66461e3e52b810af99f410b15327a9882193adcf
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-10-13 19:12:59 +0200

    Disabled logging of the authentication request

--- src/test/cypress/support/commands.ts
+++ src/test/cypress/support/commands.ts
@@ -25,7 +25,7 @@ import { CypressCredentials } from './users';
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import { authTokenKey } from './constants';
+import { authTokenKey, BASE_API, POST } from './constants';
 
 export {};
 
@@ -65,22 +65,25 @@ Cypress.Commands.overwrite('request', (originalFn, options) => {
 Cypress.Commands.add('login', (credentials: CypressCredentials, url) => {
     const username = credentials.username;
     const password = credentials.password;
+    // IMPORTANT: The "log" and "failOnStatusCode" fields need to be set to false to prevent leakage of the credentials via the Cypress Dashboard!
+    // log = false does not prevent cypress to log the request if it failed, so failOnStatusCode also needs to be set to false, so that the request is never logged.
+    // We still want to the test to fail if the authentication is unsuccessful, so we expect the status code in the then block. This only logs the status code, so it is safe.
     cy.request({
-        url: '/api/authenticate',
-        method: 'POST',
+        url: BASE_API + 'authenticate',
+        method: POST,
         followRedirect: true,
-        retryOnStatusCodeFailure: true,
         body: {
             username,
             password,
             rememberMe: true,
         },
-    })
-        .its('body')
-        .then((res) => {
-            localStorage.setItem(authTokenKey, '"' + res.id_token + '"');
-            Cypress.env(authTokenKey, res.id_token);
-        });
+        log: false,
+        failOnStatusCode: false,
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        localStorage.setItem(authTokenKey, '"' + response.body.id_token + '"');
+        Cypress.env(authTokenKey, response.body.id_token);
+    });
     if (url) {
         cy.visit(url);
     }
@@ -104,8 +107,8 @@ Cypress.Commands.add('logout', () => {
  * */
 Cypress.Commands.add('loginWithGUI', (credentials) => {
     cy.visit('/');
-    cy.get('#username').type(credentials.username);
-    cy.get('#password').type(credentials.password).type('{enter}');
+    cy.get('#username').type(credentials.username, { log: false });
+    cy.get('#password').type(credentials.password, { log: false }).type('{enter}');
     Cypress.env(authTokenKey, localStorage.getItem(authTokenKey));
 });
 
