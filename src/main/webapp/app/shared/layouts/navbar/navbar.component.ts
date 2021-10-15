ed6d45d2a36ddfa8ecf63f5bce0141efa48ca491
Ndricim Rrapi <ndricim.rrapi@tum.de>
2021-10-15 03:23:12 +0200

    add missing comma

--- src/main/webapp/app/shared/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/shared/layouts/navbar/navbar.component.ts
@@ -201,7 +201,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
         grading_system: 'artemisApp.gradingSystem.title',
         exercise_statistics: 'exercise-statistics.title',
         tutor_effort_statistics: 'artemisApp.textExercise.tutorEffortStatistics.title',
-        user_settings: 'artemisApp.userSettings.title'
+        user_settings: 'artemisApp.userSettings.title',
     };
 
     /**
