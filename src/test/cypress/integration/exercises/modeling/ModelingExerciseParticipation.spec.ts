7df085c2dacff78d07af7208435d38f29c65a186
Robert Schauer <ga94bit@mytum.de>
2021-10-16 22:13:13 +0200

    Modeling Exercise Participation

---
+++ src/test/cypress/integration/exercises/modeling/ModelingExerciseParticipation.spec.ts
@@ -0,0 +1,49 @@
+import { BASE_API } from '../../../support/constants';
+import { artemis } from '../../../support/ArtemisTesting';
+
+// pageobjects
+const courseManagement = artemis.pageobjects.courseManagement;
+const modelingEditor = artemis.pageobjects.modelingExercise.editor;
+// requests
+const courseManagementRequests = artemis.requests.courseManagement;
+// Users
+const userManagement = artemis.users;
+const admin = userManagement.getAdmin();
+const student = userManagement.getStudentOne();
+let course: any;
+let modelingExercise: any;
+
+describe('Modeling Exercise Spec', () => {
+    before('Log in as admin and create a course', () => {
+        cy.login(admin);
+        courseManagementRequests.createCourse().then((courseResp: any) => {
+            course = courseResp.body;
+            cy.visit(`/course-management/${course.id}`).get('.row-md > :nth-child(2)').should('contain.text', course.title);
+            courseManagement.addStudentToCourse(student);
+            courseManagementRequests.createModelingExercise({ course }).then((resp: any) => {
+                modelingExercise = resp.body;
+            });
+        });
+    });
+
+    after('Delete the test course', () => {
+        cy.login(admin);
+        courseManagementRequests.deleteCourse(course.id);
+    });
+
+    it('Student can start and submit their model', () => {
+        cy.login(student);
+        cy.intercept(BASE_API + 'courses/*/exercises/*/participations').as('createModelingParticipation');
+        cy.login(student, `/courses/${course.id}`);
+        cy.get('.col-lg-8').contains(modelingExercise.title);
+        cy.get('.btn-sm').should('contain.text', 'Start exercise').click();
+        cy.wait('@createModelingParticipation');
+        cy.get('.course-exercise-row').find('.btn-primary').should('contain.text', 'Open modelling editor').click();
+        modelingEditor.addComponentToModel(1);
+        modelingEditor.addComponentToModel(2);
+        modelingEditor.addComponentToModel(3);
+        cy.get('.submission-button').click();
+        cy.get('.alerts').should('contain.text', 'Your submission was successful! You can change your submission or wait for your feedback.');
+        cy.contains('No graded result').should('be.visible');
+    });
+});
