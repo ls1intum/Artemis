6773f076ccf95dbbecb33309a8ec509eb9db9e5e
Lorena Schlesinger <lorena.schlesinger@online.de>
2021-10-18 18:53:59 +0200

    remove metis service dependencies form notification service and exclude migration

--- src/main/webapp/app/shared/notification/system-notification/system-notification.module.ts
+++ src/main/webapp/app/shared/notification/system-notification/system-notification.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { SystemNotificationService } from 'app/shared/notification/system-notification/system-notification.service';
-import { MetisService } from 'app/shared/metis/metis.service';
 
 @NgModule({
     imports: [ArtemisSharedModule],
-    providers: [SystemNotificationService, MetisService],
+    providers: [SystemNotificationService],
 })
 export class ArtemisSystemNotificationModule {}
