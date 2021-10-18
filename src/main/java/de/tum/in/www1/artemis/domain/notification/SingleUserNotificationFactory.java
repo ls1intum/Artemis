7dd64368d7f125082c64d6af2a3c0f468d290021
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-10-18 20:14:25 +0200

    added requested constants

--- src/main/java/de/tum/in/www1/artemis/domain/notification/SingleUserNotificationFactory.java
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/SingleUserNotificationFactory.java
@@ -10,6 +10,8 @@ public class SingleUserNotificationFactory {
 
     private static NotificationTargetService targetService = new NotificationTargetService();
 
+    private static final String POST_NOTIFICATION_TEXT = "Your post got replied.";
+
     /**
      * Creates an instance of SingleUserNotification.
      *
@@ -20,22 +22,21 @@ public class SingleUserNotificationFactory {
     public static SingleUserNotification createNotification(Post post, NotificationType notificationType, Course course) {
         User recipient = post.getAuthor();
         String title;
-        String text = "Your post got replied.";
         SingleUserNotification notification;
         switch (notificationType) {
             case NEW_REPLY_FOR_EXERCISE_POST -> {
                 title = NotificationTitleTypeConstants.NEW_REPLY_FOR_EXERCISE_POST_TITLE;
-                notification = new SingleUserNotification(recipient, title, text);
+                notification = new SingleUserNotification(recipient, title, POST_NOTIFICATION_TEXT);
                 notification.setTarget(targetService.getExercisePostTarget(post));
             }
             case NEW_REPLY_FOR_LECTURE_POST -> {
                 title = NotificationTitleTypeConstants.NEW_REPLY_FOR_LECTURE_POST_TITLE;
-                notification = new SingleUserNotification(recipient, title, text);
+                notification = new SingleUserNotification(recipient, title, POST_NOTIFICATION_TEXT);
                 notification.setTarget(targetService.getLecturePostTarget(post));
             }
             case NEW_REPLY_FOR_COURSE_POST -> {
                 title = NotificationTitleTypeConstants.NEW_REPLY_FOR_COURSE_POST_TITLE;
-                notification = new SingleUserNotification(recipient, title, text);
+                notification = new SingleUserNotification(recipient, title, POST_NOTIFICATION_TEXT);
                 notification.setTarget(targetService.getCoursePostTarget(post, course));
             }
             default -> throw new UnsupportedOperationException("Unsupported NotificationType: " + notificationType);
