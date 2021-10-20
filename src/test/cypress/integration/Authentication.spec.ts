43b0d3fa0c633c73695db29a268af6354ee9f26d
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-10-20 21:25:30 +0200

    Adjusted authentication test structure to stay consistent

--- src/test/cypress/integration/Authentication.spec.ts
+++ src/test/cypress/integration/Authentication.spec.ts
@@ -4,36 +4,38 @@ import { authTokenKey } from '../support/constants';
 const user = artemis.users.getStudentOne();
 const loginPage = artemis.pageobjects.login;
 
-it('Logs in via the UI', function () {
-    cy.visit('/');
-    loginPage.login(user);
-    cy.url()
-        .should('include', '/courses')
-        .then(() => {
-            expect(localStorage.getItem(authTokenKey)).to.not.be.null;
-        });
-});
+describe('Authentication tests', () => {
+    it('Logs in via the UI', () => {
+        cy.visit('/');
+        loginPage.login(user);
+        cy.url()
+            .should('include', '/courses')
+            .then(() => {
+                expect(localStorage.getItem(authTokenKey)).to.not.be.null;
+            });
+    });
 
-it('Logs in programmatically and logs out via the UI', function () {
-    cy.login(user, '/courses');
-    cy.url().should('include', '/courses');
-    cy.get('#account-menu').click().get('#logout').click();
-    cy.url()
-        .should('equal', Cypress.config().baseUrl + '/')
-        .then(() => {
-            expect(localStorage.getItem(authTokenKey)).to.be.null;
-        });
-});
+    it('Logs in programmatically and logs out via the UI', () => {
+        cy.login(user, '/courses');
+        cy.url().should('include', '/courses');
+        cy.get('#account-menu').click().get('#logout').click();
+        cy.url()
+            .should('equal', Cypress.config().baseUrl + '/')
+            .then(() => {
+                expect(localStorage.getItem(authTokenKey)).to.be.null;
+            });
+    });
 
-it('Displays error messages on wrong password', () => {
-    loginPage.login({ username: 'some_user_name', password: 'lorem-ipsum' });
-    cy.location('pathname').should('eq', '/');
-    cy.get('.alert').should('exist').and('have.text', 'Failed to sign in! Please check your username and password and try again.');
-    cy.get('.btn').click();
-    cy.get('.btn').click();
-});
+    it('Displays error messages on wrong password', () => {
+        loginPage.login({ username: 'some_user_name', password: 'lorem-ipsum' });
+        cy.location('pathname').should('eq', '/');
+        cy.get('.alert').should('exist').and('have.text', 'Failed to sign in! Please check your username and password and try again.');
+        cy.get('.btn').click();
+        cy.get('.btn').click();
+    });
 
-it('Fails to access protected resource without login', () => {
-    cy.visit('/course-management');
-    cy.location('pathname').should('eq', '/');
+    it('Fails to access protected resource without login', () => {
+        cy.visit('/course-management');
+        cy.location('pathname').should('eq', '/');
+    });
 });
