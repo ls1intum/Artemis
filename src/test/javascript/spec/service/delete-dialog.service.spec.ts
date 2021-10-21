728d9f38ad200dc5fc2727a6b249561e12407663
Lorena Schlesinger <lorena.schlesinger@online.de>
2021-10-21 19:40:10 +0200

    fix client test

--- src/test/javascript/spec/service/delete-dialog.service.spec.ts
+++ src/test/javascript/spec/service/delete-dialog.service.spec.ts
@@ -1,30 +1,33 @@
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { DeleteDialogService } from 'app/shared/delete-dialog/delete-dialog.service';
-import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ActionType, DeleteDialogData } from 'app/shared/delete-dialog/delete-dialog.model';
 import { EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { MockTranslateService } from '../helpers/mocks/service/mock-translate.service';
-import { ArtemisTestModule } from '../test.module';
 import { MockNgbModalService } from '../helpers/mocks/service/mock-ngb-modal.service';
 import { DeleteDialogComponent } from 'app/shared/delete-dialog/delete-dialog.component';
-import { AlertService } from 'app/core/util/alert.service';
-import { MockAlertService } from '../helpers/mocks/service/mock-alert.service';
-import { MockComponent, MockModule } from 'ng-mocks';
-import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ExerciseUpdateWarningService } from 'app/exercises/shared/exercise-update-warning/exercise-update-warning.service';
-import { MetisService } from 'app/shared/metis/metis.service';
-import { MockMetisService } from '../helpers/mocks/service/mock-metis-service.service';
+import { MockComponent, MockModule, MockPipe } from 'ng-mocks';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { AlertComponent } from 'app/shared/alert/alert.component';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { FormsModule } from '@angular/forms';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 describe('Delete Dialog Service', () => {
     let service: DeleteDialogService;
-    let modalService: MockNgbModalService;
+    let modalService: NgbModal;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [{ provide: TranslateService, useClass: MockTranslateService }],
+            imports: [MockModule(FormsModule)],
+            providers: [
+                { provide: DeleteDialogService, useClass: DeleteDialogService },
+                { provide: TranslateService, useClass: MockTranslateService },
+                { provide: NgbModal, useClass: MockNgbModalService },
+            ],
+            declarations: [DeleteDialogComponent, MockComponent(FaIconComponent), MockComponent(AlertComponent), MockComponent(TranslateDirective), MockPipe(ArtemisTranslatePipe)],
         });
 
         const injector = getTestBed();
@@ -43,9 +46,11 @@ describe('Delete Dialog Service', () => {
             delete: new EventEmitter<any>(),
             requireConfirmationOnlyForAdditionalChecks: false,
         };
-        const modalSpy = jest.spyOn(modalService, 'open');
+        const componentInstance = {};
+        const result = new Promise((resolve) => resolve({}));
+        const modalSpy = jest.spyOn(modalService, 'open').mockReturnValue(<NgbModalRef>{ componentInstance, result });
         service.openDeleteDialog(data);
         expect(modalSpy).toHaveBeenCalledTimes(1);
-        expect(modalSpy).toHaveBeenCalledWith('DeleteDialogComponent');
+        expect(modalSpy).toHaveBeenCalledWith(DeleteDialogComponent, { size: 'lg', backdrop: 'static' });
     });
 });
