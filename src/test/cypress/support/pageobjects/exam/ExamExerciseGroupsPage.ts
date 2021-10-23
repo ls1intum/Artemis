cdb2110fee78e7b7f615436c1ca5f16479d03c01
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-10-23 14:40:32 +0200

    Added exam exercise group pageobject

---
+++ src/test/cypress/support/pageobjects/exam/ExamExerciseGroupsPage.ts
@@ -0,0 +1,28 @@
+/**
+ * A class which encapsulates UI selectors and actions for the exam exercise groups page.
+ */
+export class ExamExerciseGroupsPage {
+    clickCreateNewExerciseGroup() {
+        cy.get('[jhitranslate="artemisApp.examManagement.exerciseGroup.create"]').click();
+    }
+
+    showsNumberOfExerciseGroups(numberOfGroups: number) {
+        cy.contains(`Number of exercise groups: ${numberOfGroups}`).should('be.visible');
+    }
+
+    clickAddExerciseGroup() {
+        cy.get('[jhitranslate="artemisApp.examManagement.exerciseGroup.create"]').click();
+    }
+
+    clickAddTextExercise() {
+        cy.contains('Add Text Exercise').click();
+    }
+
+    visitPageViaUrl(courseId: number, examId: number) {
+        cy.visit(`course-management/${courseId}/exams/${examId}/exercise-groups`);
+    }
+
+    shouldContainExerciseWithTitle(exerciseTitle: string) {
+        cy.get('.table').contains(exerciseTitle).scrollIntoView().should('be.visible');
+    }
+}
