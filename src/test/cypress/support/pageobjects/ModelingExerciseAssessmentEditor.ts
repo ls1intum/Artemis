00e8590d925c378e1f587fbfebe7b99026c420c1
Robert Schauer <ga94bit@mytum.de>
2021-10-25 22:51:03 +0200

    modify and use parent function

--- src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
+++ src/test/cypress/support/pageobjects/ModelingExerciseAssessmentEditor.ts
@@ -1,6 +1,6 @@
 import { AbstractExerciseAssessmentPage } from './assessment/AbstractExerciseAssessmentPage';
 import { MODELING_SPACE } from './ModelingEditor';
-import { BASE_API, PUT } from '../constants';
+import { CypressExerciseType } from '../requests/CourseManagementRequests';
 
 // TODO: find or create better selectors for this
 const FEEDBACK_CONTAINER = '.sc-lcuiOb';
@@ -19,21 +19,9 @@ export class ModelingExerciseAssessmentEditor extends AbstractExerciseAssessment
     }
 
     rejectComplaint(response: string) {
-        return this.handleModelingComplaint(response, false);
+        return super.rejectComplaint(response, CypressExerciseType.MODELING);
     }
-
     acceptComplaint(response: string) {
-        return this.handleModelingComplaint(response, true);
-    }
-
-    private handleModelingComplaint(response: string, accept: boolean) {
-        cy.get('#responseTextArea').type(response, { parseSpecialCharSequences: false });
-        cy.intercept(PUT, BASE_API + 'complaint-responses/complaint/*/resolve').as('complaintAnswer');
-        if (accept) {
-            cy.get('#acceptComplaintButton').click();
-        } else {
-            cy.get('#rejectComplaintButton').click();
-        }
-        return cy.wait('@complaintAnswer');
+        return super.acceptComplaint(response, CypressExerciseType.MODELING);
     }
 }
