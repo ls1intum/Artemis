477f8df03c79b1215831d296c2b2ed50bc700ce3
IAMsay <aleksandr.malyuk@googlemail.com>
2021-10-27 20:39:22 +0200

    fixed undefined bug

--- src/main/java/de/tum/in/www1/artemis/service/scheduled/NotificationScheduleService.java
+++ src/main/java/de/tum/in/www1/artemis/service/scheduled/NotificationScheduleService.java
@@ -89,9 +89,8 @@ public class NotificationScheduleService implements IExerciseScheduleService<Exe
                 if (!SecurityUtils.isAuthenticated()) {
                     SecurityUtils.setAuthorizationObject();
                 }
-                // only send a notification if ReleaseDate is null or not in the future (i.e. in the range [now-2 minutes, now]) (due to possible delays in scheduling)
-                if (exercise.getReleaseDate() == null
-                        || !exercise.getReleaseDate().isBefore(ZonedDateTime.now().minusMinutes(2)) && !exercise.getReleaseDate().isAfter(ZonedDateTime.now())) {
+                // only send a notification if ReleaseDate is defined and not in the future (i.e. in the range [now-2 minutes, now]) (due to possible delays in scheduling)
+                if (!exercise.getReleaseDate().isBefore(ZonedDateTime.now().minusMinutes(2)) && !exercise.getReleaseDate().isAfter(ZonedDateTime.now())) {
                     groupNotificationService.notifyAllGroupsAboutReleasedExercise(exercise);
                 }
             });
