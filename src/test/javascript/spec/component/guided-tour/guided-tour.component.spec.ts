41c8d849640cc525273605532afe67106f5d03b8
Julian Christl <julian.christl@tum.de>
2021-10-27 16:55:53 +0200

    optimised imports, improved naming, removed todo

--- src/test/javascript/spec/component/guided-tour/guided-tour.component.spec.ts
+++ src/test/javascript/spec/component/guided-tour/guided-tour.component.spec.ts
@@ -1,28 +1,18 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { CookieService } from 'ngx-cookie-service';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
-import { TranslateService } from '@ngx-translate/core';
 import { ArtemisTestModule } from '../../test.module';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
-import { MockCookieService } from '../../helpers/mocks/service/mock-cookie.service';
 import { TextTourStep } from 'app/guided-tour/guided-tour-step.model';
 import { GuidedTour } from 'app/guided-tour/guided-tour.model';
 import { GuidedTourComponent } from 'app/guided-tour/guided-tour.component';
 import { GuidedTourService } from 'app/guided-tour/guided-tour.service';
 import { Orientation, OverlayPosition, ResetParticipation } from 'app/guided-tour/guided-tour.constants';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { By } from '@angular/platform-browser';
-import { MockTranslateService, TranslatePipeMock } from '../../helpers/mocks/service/mock-translate.service';
+import { TranslatePipeMock } from '../../helpers/mocks/service/mock-translate.service';
 import { Authority } from 'app/shared/constants/authority.constants';
-import { MockRouter } from '../../helpers/mocks/mock-router';
-import { Router } from '@angular/router';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
-import { MockComponent, MockDirective, MockPipe } from 'ng-mocks';
+import { MockDirective, MockPipe } from 'ng-mocks';
 import { SafeResourceUrlPipe } from 'app/shared/pipes/safe-resource-url.pipe';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 describe('GuidedTourComponent', () => {
     const tourStep = new TextTourStep({
@@ -66,13 +56,12 @@ describe('GuidedTourComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
-            declarations: [GuidedTourComponent, MockDirective(TranslateDirective), TranslatePipeMock, MockPipe(SafeResourceUrlPipe), MockComponent(FaIconComponent)],
+            declarations: [GuidedTourComponent, MockDirective(TranslateDirective), TranslatePipeMock, MockPipe(SafeResourceUrlPipe)],
             providers: [
                 { provide: LocalStorageService, useClass: MockSyncStorage },
                 { provide: SessionStorageService, useClass: MockSyncStorage },
             ],
         })
-            .overrideModule(ArtemisTestModule, { set: { declarations: [], exports: [] } })
             .compileComponents()
             .then(() => {
                 guidedTourComponentFixture = TestBed.createComponent(GuidedTourComponent);
@@ -89,16 +78,16 @@ describe('GuidedTourComponent', () => {
         const currentStepSpy = jest.spyOn<any, any>(guidedTourComponent, 'subscribeToGuidedTourCurrentStepStream');
         const resizeEventSpy = jest.spyOn<any, any>(guidedTourComponent, 'subscribeToResizeEvent');
         const scrollEventSpy = jest.spyOn<any, any>(guidedTourComponent, 'subscribeToScrollEvent');
-        const dotNavigationSpy = jest.spyOn<any, any>(guidedTourComponent, 'subscribeToDotChanges').mockImplementation();
-        const guidedTourInitSpy = jest.spyOn(guidedTourService, 'init').mockImplementation();
+        const dotNavigationStub = jest.spyOn<any, any>(guidedTourComponent, 'subscribeToDotChanges').mockImplementation();
+        const guidedTourInitStub = jest.spyOn(guidedTourService, 'init').mockImplementation();
 
         guidedTourComponent.ngAfterViewInit();
 
         expect(currentStepSpy).toHaveBeenCalledTimes(1);
         expect(resizeEventSpy).toHaveBeenCalledTimes(1);
         expect(scrollEventSpy).toHaveBeenCalledTimes(1);
-        expect(dotNavigationSpy).toHaveBeenCalledTimes(1);
-        expect(guidedTourInitSpy).toHaveBeenCalledTimes(1);
+        expect(dotNavigationStub).toHaveBeenCalledTimes(1);
+        expect(guidedTourInitStub).toHaveBeenCalledTimes(1);
     });
 
     it('should handle user permissions', () => {
@@ -117,7 +106,7 @@ describe('GuidedTourComponent', () => {
             // Prepare guided tour service
             jest.spyOn(guidedTourService, 'init').mockImplementation();
             jest.spyOn(guidedTourService, 'getLastSeenTourStepIndex').mockReturnValue(0);
-            jest.spyOn<any, any>(guidedTourService, 'updateGuidedTourSettings');
+            // jest.spyOn<any, any>(guidedTourService, 'updateGuidedTourSettings');
             jest.spyOn<any, any>(guidedTourService, 'enableTour').mockImplementation(() => {
                 guidedTourService['availableTourForComponent'] = courseOverviewTour;
                 guidedTourService.currentTour = courseOverviewTour;
@@ -142,57 +131,56 @@ describe('GuidedTourComponent', () => {
         });
 
         it('should not trigger the guided tour with the right arrow key', () => {
-            guidedTourComponent.currentTourStep = undefined; // TODO
-            const nextStep = jest.spyOn(guidedTourService, 'nextStep');
+            guidedTourComponent.currentTourStep = undefined;
+            const nextStepSpy = jest.spyOn(guidedTourService, 'nextStep');
             const eventMock = new KeyboardEvent('keydown', { code: 'ArrowRight' });
             guidedTourComponent.handleKeyboardEvent(eventMock);
-            expect(nextStep).not.toHaveBeenCalled();
+            expect(nextStepSpy).not.toHaveBeenCalled();
         });
 
         it('should navigate next with the right arrow key', () => {
             guidedTourComponent['currentStepIndex'] = guidedTourService.currentTourStepIndex;
             guidedTourComponent['nextStepIndex'] = guidedTourService.currentTourStepIndex + 1;
-            const nextStep = jest.spyOn(guidedTourService, 'nextStep');
+            const nextStepSpy = jest.spyOn(guidedTourService, 'nextStep');
             const eventMock = new KeyboardEvent('keydown', { code: 'ArrowRight' });
             guidedTourComponent.handleKeyboardEvent(eventMock);
-            expect(nextStep).toHaveBeenCalledTimes(1);
+            expect(nextStepSpy).toHaveBeenCalledTimes(1);
         });
 
         it('should navigate back with the left arrow key', () => {
-            const backStep = jest.spyOn(guidedTourService, 'backStep').mockImplementation();
-            jest.spyOn(guidedTourService, 'nextStep');
+            const backStepStub = jest.spyOn(guidedTourService, 'backStep').mockImplementation();
             const eventMockRight = new KeyboardEvent('keydown', { code: 'ArrowRight' });
             const eventMockLeft = new KeyboardEvent('keydown', { code: 'ArrowLeft' });
 
             guidedTourComponent.handleKeyboardEvent(eventMockLeft);
-            expect(backStep).not.toHaveBeenCalled();
+            expect(backStepStub).not.toHaveBeenCalled();
 
             guidedTourComponent.handleKeyboardEvent(eventMockRight);
 
             guidedTourComponent.handleKeyboardEvent(eventMockLeft);
-            expect(backStep).toHaveBeenCalledTimes(1);
+            expect(backStepStub).toHaveBeenCalledTimes(1);
         });
 
         it('should skip the tour with the escape key', () => {
-            const skipTour = jest.spyOn(guidedTourService, 'skipTour').mockImplementation();
+            const skipTourStub = jest.spyOn(guidedTourService, 'skipTour').mockImplementation();
             jest.spyOn<any, any>(guidedTourService, 'isCurrentTour').mockReturnValue(false);
             const eventMock = new KeyboardEvent('keydown', { code: 'Escape' });
 
             guidedTourComponent.handleKeyboardEvent(eventMock);
-            expect(skipTour).toHaveBeenCalledTimes(1);
+            expect(skipTourStub).toHaveBeenCalledTimes(1);
 
             // Reset component
-            skipTour.mockReset();
+            skipTourStub.mockReset();
             guidedTourComponent.currentTourStep = undefined;
 
             // Skip tour with ESC key should not be possible when the component is not active
             guidedTourComponent.handleKeyboardEvent(eventMock);
-            expect(skipTour).not.toHaveBeenCalled();
+            expect(skipTourStub).not.toHaveBeenCalled();
         });
 
         it('should not skip but finish the cancel tour with the escape key', () => {
-            const skipTour = jest.spyOn(guidedTourService, 'skipTour').mockImplementation();
-            const finishTour = jest.spyOn(guidedTourService, 'finishGuidedTour').mockImplementation();
+            const skipTourStub = jest.spyOn(guidedTourService, 'skipTour').mockImplementation();
+            const finishTourStub = jest.spyOn(guidedTourService, 'finishGuidedTour').mockImplementation();
             jest.spyOn<any, any>(guidedTourService, 'isCurrentTour').mockReturnValue(true);
             const eventMock = new KeyboardEvent('keydown', { code: 'Escape' });
 
@@ -201,8 +189,8 @@ describe('GuidedTourComponent', () => {
             }
 
             guidedTourComponent.handleKeyboardEvent(eventMock);
-            expect(skipTour).not.toHaveBeenCalled();
-            expect(finishTour).toHaveBeenCalledTimes(1);
+            expect(skipTourStub).not.toHaveBeenCalled();
+            expect(finishTourStub).toHaveBeenCalledTimes(1);
         });
     });
 
@@ -324,13 +312,13 @@ describe('GuidedTourComponent', () => {
             window.scrollTo = () => {};
             jest.useFakeTimers();
             jest.spyOn<any, any>(guidedTourComponent, 'isTourOnScreen').mockReturnValue(false);
-            const flipOrientationSpy = jest.spyOn<any, any>(guidedTourComponent, 'flipOrientation').mockImplementation();
+            const flipOrientationStub = jest.spyOn<any, any>(guidedTourComponent, 'flipOrientation').mockImplementation();
             guidedTourComponent['scrollToAndSetElement']();
-            expect(flipOrientationSpy).not.toHaveBeenCalled();
+            expect(flipOrientationStub).not.toHaveBeenCalled();
 
             jest.advanceTimersByTime(300);
             guidedTourComponent['scrollToAndSetElement']();
-            expect(flipOrientationSpy).toHaveBeenCalledTimes(1);
+            expect(flipOrientationStub).toHaveBeenCalledTimes(1);
         });
 
         it('should flip orientation', () => {
