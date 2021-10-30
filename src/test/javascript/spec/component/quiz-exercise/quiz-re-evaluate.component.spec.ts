ce8ec261e69fc03c1c560cfa4301ba0b327dedb7
Georg Willer <therispense14@gmail.com>
2021-10-30 13:10:08 +0200

    improve test suits

--- src/test/javascript/spec/component/quiz-exercise/quiz-re-evaluate.component.spec.ts
+++ src/test/javascript/spec/component/quiz-exercise/quiz-re-evaluate.component.spec.ts
@@ -11,9 +11,8 @@ import { Course } from 'app/entities/course.model';
 import { QuizReEvaluateComponent } from 'app/exercises/quiz/manage/re-evaluate/quiz-re-evaluate.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MockRouter } from '../../helpers/mocks/mock-router';
-import { QuizExercisePopupService } from 'app/exercises/quiz/manage/quiz-exercise-popup.service';
 import { ReEvaluateMultipleChoiceQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/multiple-choice-question/re-evaluate-multiple-choice-question.component';
-import { MockComponent, MockDirective, MockPipe } from 'ng-mocks';
+import { MockComponent, MockDirective, MockPipe, MockProvider } from 'ng-mocks';
 import { ReEvaluateDragAndDropQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/drag-and-drop-question/re-evaluate-drag-and-drop-question.component';
 import { ReEvaluateShortAnswerQuestionComponent } from 'app/exercises/quiz/manage/re-evaluate/short-answer-question/re-evaluate-short-answer-question.component';
 import { NgModel } from '@angular/forms';
@@ -29,6 +28,9 @@ import { IncludedInOverallScore } from 'app/entities/exercise.model';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { MockTranslateValuesDirective } from '../../helpers/mocks/directive/mock-translate-values.directive';
 import { AnswerOption } from 'app/entities/quiz/answer-option.model';
+import { DragItem } from 'app/entities/quiz/drag-item.model';
+import { DropLocation } from 'app/entities/quiz/drop-location.model';
+import { DragAndDropMapping } from 'app/entities/quiz/drag-and-drop-mapping.model';
 
 describe('QuizExercise Re-evaluate Component', () => {
     let comp: QuizReEvaluateComponent;
@@ -42,6 +44,44 @@ describe('QuizExercise Re-evaluate Component', () => {
     quizExercise.title = 'MyQuiz';
 
     const route = { params: of({ exerciseId: 123 }) } as any as ActivatedRoute;
+
+    const createValidMCQuestion = () => {
+        const question = new MultipleChoiceQuestion();
+        question.title = 'test';
+        const answerOption1 = new AnswerOption();
+        answerOption1.text = 'wrong answer';
+        answerOption1.explanation = 'wrong explanation';
+        answerOption1.hint = 'wrong hint';
+        answerOption1.isCorrect = false;
+        const answerOption2 = new AnswerOption();
+        answerOption1.text = 'right answer';
+        answerOption1.explanation = 'right explanation';
+        answerOption1.isCorrect = true;
+        question.answerOptions = [answerOption1, answerOption2];
+        question.points = 10;
+        return { question, answerOption1, answerOption2 };
+    };
+
+    const createValidDnDQuestion = () => {
+        const question = new DragAndDropQuestion();
+        question.title = 'test';
+        const dragItem1 = new DragItem();
+        dragItem1.text = 'dragItem 1';
+        dragItem1.pictureFilePath = 'test';
+        const dragItem2 = new DragItem();
+        dragItem2.text = 'dragItem 1';
+        question.dragItems = [dragItem1, dragItem2];
+        const dropLocation = new DropLocation();
+        dropLocation.posX = 50;
+        dropLocation.posY = 60;
+        dropLocation.width = 70;
+        dropLocation.height = 80;
+        question.dropLocations = [dropLocation];
+        const correctDragAndDropMapping = new DragAndDropMapping(dragItem1, dropLocation);
+        question.correctMappings = [correctDragAndDropMapping];
+        question.points = 10;
+        return { question, dragItem1, dragItem2, dropLocation, correctDragAndDropMapping };
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -59,8 +99,7 @@ describe('QuizExercise Re-evaluate Component', () => {
                 MockPipe(ArtemisTranslatePipe),
             ],
             providers: [
-                NgbModal,
-                QuizExercisePopupService,
+                MockProvider(NgbModal),
                 { provide: ActivatedRoute, useValue: route },
                 { provide: TranslateService, useClass: MockTranslateService },
                 { provide: Router, useClass: MockRouter },
@@ -72,43 +111,39 @@ describe('QuizExercise Re-evaluate Component', () => {
         fixture = TestBed.createComponent(QuizReEvaluateComponent);
         comp = fixture.componentInstance;
         quizService = fixture.debugElement.injector.get(QuizExerciseService);
-        const quizQuestion1 = new MultipleChoiceQuestion();
-        // setup necessary in order to pass cacheValidation()
-        const answerOption = new AnswerOption();
-        answerOption.isCorrect = true;
-        quizQuestion1.answerOptions = [answerOption];
-        const quizQuestion2 = new DragAndDropQuestion();
+        const { question: quizQuestion1 } = createValidMCQuestion();
+        const { question: quizQuestion2 } = createValidDnDQuestion();
         quizExercise.quizQuestions = [quizQuestion1, quizQuestion2];
         quizServiceFindSpy = jest.spyOn(quizService, 'find').mockReturnValue(of(new HttpResponse({ body: quizExercise })));
     });
 
     afterEach(() => {
-        jest.clearAllMocks();
+        jest.restoreAllMocks();
     });
 
     it('Should initialize quiz exercise', () => {
         comp.ngOnInit();
-        expect(comp.validQuiz()).toBe(false);
+        expect(comp.validQuiz()).toBe(true);
         expect(comp.quizExercise).toEqual(quizExercise);
         expect(quizServiceFindSpy).toHaveBeenCalledTimes(1);
     });
 
     it('Should create correct duration strings', () => {
         comp.duration = new Duration(1, 0);
-        expect(comp.durationString()).toEqual('1:00');
+        expect(comp.durationString()).toBe('1:00');
 
         comp.duration = new Duration(1, 9);
-        expect(comp.durationString()).toEqual('1:09');
+        expect(comp.durationString()).toBe('1:09');
 
         comp.duration = new Duration(1, 10);
-        expect(comp.durationString()).toEqual('1:10');
+        expect(comp.durationString()).toBe('1:10');
     });
 
     it('Should delete quiz question', () => {
         comp.ngOnInit();
-        expect(comp.quizExercise.quizQuestions!.length).toEqual(2);
+        expect(comp.quizExercise.quizQuestions!.length).toBe(2);
         comp.deleteQuestion(comp.quizExercise.quizQuestions![0]);
-        expect(comp.quizExercise.quizQuestions!.length).toEqual(1);
+        expect(comp.quizExercise.quizQuestions!.length).toBe(1);
     });
 
     it('Should update and reset quiz questions', () => {
@@ -120,7 +155,7 @@ describe('QuizExercise Re-evaluate Component', () => {
         expect(comp.quizExercise).toEqual(quizExercise);
         // reset title
         comp.resetQuizTitle();
-        expect(comp.quizExercise.title).toEqual(comp.savedEntity.title);
+        expect(comp.quizExercise.title).toBe(comp.savedEntity.title);
         // reset all
         comp.resetAll();
         expect(comp.quizExercise).toEqual(comp.savedEntity);
@@ -153,11 +188,87 @@ describe('QuizExercise Re-evaluate Component', () => {
             quizExercise: { currentValue: quizExercise } as SimpleChange,
         });
 
-        expect(prepareEntitySpy).toHaveBeenCalled();
+        expect(prepareEntitySpy).toHaveBeenCalledTimes(2);
     });
 
-    it('Should save a quiz', () => {
-        comp.ngOnInit();
+    describe('Quiz question validation', () => {
+        beforeEach(() => {
+            comp.ngOnInit();
+        });
+        afterEach(() => {
+            comp.resetAll();
+            comp.onQuestionUpdated();
+
+            expect(comp.quizIsValid).toBe(true);
+        });
+
+        describe('Quiz mc question validation', () => {
+            let mcQuestion: MultipleChoiceQuestion;
+            let answerOption1: AnswerOption;
+            beforeEach(() => {
+                mcQuestion = comp.quizExercise.quizQuestions![0] as MultipleChoiceQuestion;
+                answerOption1 = mcQuestion.answerOptions![0];
+            });
+
+            it('Should be invalid if quiz hint is too long', () => {
+                mcQuestion.hint = new Array(256 + 1).join('x');
+                expect(mcQuestion.hint).toHaveLength(256);
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+
+            it('Should be invalid if quiz explanation is too long', () => {
+                mcQuestion.explanation = new Array(501 + 1).join('x');
+                expect(mcQuestion.explanation).toHaveLength(501);
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+
+            it('Should be invalid if answer option hint is too long', () => {
+                answerOption1.hint = new Array(501 + 1).join('x');
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+
+            it('Should be invalid if answer option explanation is too long', () => {
+                answerOption1.explanation = new Array(501 + 1).join('x');
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+        });
+
+        describe('Quiz dnd question validation', () => {
+            let dndQuestion: DragAndDropQuestion;
+            beforeEach(() => {
+                dndQuestion = comp.quizExercise.quizQuestions![1] as DragAndDropQuestion;
+            });
+
+            it('Should be invalid if question hint is invalid', () => {
+                dndQuestion.hint = new Array(256 + 1).join('x');
+                expect(dndQuestion.hint).toHaveLength(256);
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+
+            it('Should be invalid if question explanation is invalid', () => {
+                dndQuestion.explanation = new Array(501 + 1).join('x');
+                expect(dndQuestion.explanation).toHaveLength(501);
+
+                comp.onQuestionUpdated();
+
+                expect(comp.quizIsValid).toBe(false);
+            });
+        });
     });
 
     it('Should change score calculation type', () => {
