b99c80bbc6aefc3b43c90fe99a418bd16c5d78e8
rs-maker <robert.schauer@tum.de>
2021-11-01 21:57:50 +0100

    create first test

--- src/test/javascript/spec/service/quiz.service.spec.ts
+++ src/test/javascript/spec/service/quiz.service.spec.ts
@@ -1,16 +1,162 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { ArtemisQuizService } from 'app/shared/quiz/quiz.service';
-import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
+import { QuizQuestionType, ScoringType } from 'app/entities/quiz/quiz-question.model';
+import { ExerciseMode, ExerciseType, IncludedInOverallScore } from 'app/entities/exercise.model';
+import { ShortAnswerQuestion } from 'app/entities/quiz/short-answer-question.model';
+import { MultipleChoiceQuestion } from 'app/entities/quiz/multiple-choice-question.model';
 
 describe('Quiz Service', () => {
-    let quizService: ArtemisQuizService;
-    let quizExercise: QuizExercise;
-
+    let injector: TestBed;
+    let service: ArtemisQuizService;
+    const quiz = {
+        mode: ExerciseMode.INDIVIDUAL,
+        includedInOverallScore: IncludedInOverallScore.INCLUDED_COMPLETELY,
+        numberOfAssessmentsOfCorrectionRounds: [{ inTime: 0, late: 0, total: 0 }],
+        studentAssignedTeamIdComputed: false,
+        secondCorrectionEnabled: false,
+        type: ExerciseType.QUIZ,
+        bonusPoints: 0,
+        isAtLeastTutor: false,
+        isAtLeastEditor: false,
+        isAtLeastInstructor: false,
+        teamMode: false,
+        assessmentDueDateError: false,
+        dueDateError: false,
+        presentationScoreEnabled: false,
+        course: undefined,
+        randomizeQuestionOrder: true,
+        releaseDate: undefined,
+        isVisibleBeforeStart: false,
+        isOpenForPractice: false,
+        isPlannedToStart: false,
+        isActiveQuiz: false,
+        isPracticeModeAvailable: true,
+        title: 'Quiz title',
+        duration: 600,
+    };
+    const multipleChoice: MultipleChoiceQuestion = {
+        type: QuizQuestionType.MULTIPLE_CHOICE,
+        randomizeOrder: true,
+        invalid: false,
+        exportQuiz: false,
+        title: 'Multiple Choice Quiz',
+        text: 'A longer more detailed question',
+        hint: 'A general hint',
+        scoringType: ScoringType.PROPORTIONAL_WITHOUT_PENALTY,
+        points: 10,
+        answerOptions: [
+            {
+                isCorrect: true,
+                invalid: false,
+                text: 'Correct answer 1',
+                hint: 'A hint',
+                explanation: 'Explanation for why this is correct',
+            },
+            {
+                isCorrect: true,
+                invalid: false,
+                text: 'Correct answer 2',
+                hint: 'A hint',
+                explanation: 'Explanation for why this is correct',
+            },
+            {
+                isCorrect: false,
+                invalid: false,
+                text: 'Wrong Answer 1',
+                hint: 'A hint',
+                explanation: 'Explanation for why this is wrong',
+            },
+            {
+                isCorrect: false,
+                invalid: false,
+                text: 'Wrong Answer 2',
+                hint: 'A hint',
+                explanation: 'Explanation for why this is wrong',
+            },
+        ],
+    };
+    const shortAnswer: ShortAnswerQuestion = {
+        type: QuizQuestionType.SHORT_ANSWER,
+        randomizeOrder: true,
+        invalid: false,
+        exportQuiz: false,
+        matchLetterCase: false,
+        similarityValue: 85,
+        title: 'Short Answer Quiz',
+        text: 'Never gonna [-spot 1] you up\nNever gonna [-spot 2] you down\n',
+        scoringType: ScoringType.PROPORTIONAL_WITHOUT_PENALTY,
+        points: 1,
+        spots: [
+            {
+                tempID: 8562027009747859,
+                invalid: false,
+                width: 15,
+                spotNr: 1,
+            },
+            {
+                tempID: 1693282244205775,
+                invalid: false,
+                width: 15,
+                spotNr: 2,
+            },
+        ],
+        solutions: [
+            {
+                tempID: 7036040666954049,
+                invalid: false,
+                text: 'give',
+            },
+            {
+                tempID: 94143448556475,
+                invalid: false,
+                text: 'let',
+            },
+        ],
+        correctMappings: [
+            {
+                spot: {
+                    tempID: 8562027009747859,
+                    invalid: false,
+                    width: 15,
+                    spotNr: 1,
+                },
+                solution: {
+                    tempID: 7036040666954049,
+                    invalid: false,
+                    text: 'give',
+                },
+                invalid: false,
+            },
+            {
+                spot: {
+                    tempID: 1693282244205775,
+                    invalid: false,
+                    width: 15,
+                    spotNr: 2,
+                },
+                solution: {
+                    tempID: 94143448556475,
+                    invalid: false,
+                    text: 'let',
+                },
+                invalid: false,
+            },
+        ],
+    };
     beforeEach(() => {
-        quizService = new ArtemisQuizService();
-        quizExercise = new QuizExercise(undefined, undefined);
+        injector = getTestBed();
+        service = injector.get(ArtemisQuizService);
+    });
+
+    afterEach(() => {
+        jest.spyOn(global.Math, 'random').mockRestore();
     });
 
     it('randomizes order of Questions', () => {
+        const quizExercise = Object.assign(quiz, { quizQuestions: [shortAnswer, multipleChoice] });
+        const expected = Object.assign(quiz, { quizQuestions: [shortAnswer] });
+        jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
+        service.randomizeOrder(quizExercise);
+        expect(quizExercise).toStrictEqual(expected);
     });
 });
