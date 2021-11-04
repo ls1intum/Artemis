7b2172c64bc833eee693c13e06cde22f3b753796
Julian Christl <julian.christl@tum.de>
2021-11-04 15:27:59 +0100

    optimise client tests

--- src/test/javascript/spec/component/course/detail/course-detail-line-chart.component.spec.ts
+++ src/test/javascript/spec/component/course/detail/course-detail-line-chart.component.spec.ts
@@ -1,21 +1,15 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateService } from '@ngx-translate/core';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
-import * as chai from 'chai';
 import { MockModule, MockPipe } from 'ng-mocks';
-import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { of } from 'rxjs';
-import sinonChai from 'sinon-chai';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseDetailLineChartComponent } from 'app/course/manage/detail/course-detail-line-chart.component';
 import { CourseManagementService } from 'app/course/manage/course-management.service';
-import { MockSyncStorage } from '../../../helpers/mocks/service/mock-sync-storage.service';
 import { MockTranslateService } from '../../../helpers/mocks/service/mock-translate.service';
 import { ArtemisTestModule } from '../../../test.module';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
-chai.use(sinonChai);
-const expect = chai.expect;
+import { MockCourseManagementService } from '../../../helpers/mocks/service/mock-course-management.service';
 
 describe('CourseDetailLineChartComponent', () => {
     let fixture: ComponentFixture<CourseDetailLineChartComponent>;
@@ -26,13 +20,9 @@ describe('CourseDetailLineChartComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [ArtemisTestModule, RouterTestingModule.withRoutes([]), MockModule(NgxChartsModule)],
+            imports: [ArtemisTestModule, MockModule(NgxChartsModule)],
             declarations: [CourseDetailLineChartComponent, MockPipe(ArtemisTranslatePipe)],
-            providers: [
-                { provide: LocalStorageService, useClass: MockSyncStorage },
-                { provide: SessionStorageService, useClass: MockSyncStorage },
-                { provide: TranslateService, useClass: MockTranslateService },
-            ],
+            providers: [MockCourseManagementService, { provide: TranslateService, useClass: MockTranslateService }],
         })
             .compileComponents()
             .then(() => {
@@ -48,7 +38,7 @@ describe('CourseDetailLineChartComponent', () => {
     });
 
     afterEach(() => {
-        jest.clearAllMocks();
+        jest.restoreAllMocks();
     });
 
     it('should initialize', () => {
@@ -63,24 +53,24 @@ describe('CourseDetailLineChartComponent', () => {
 
         component.ngOnChanges();
 
+        expect(component.absoluteSeries).toHaveLength(initialStats.length);
         for (let i = 0; i < 16; i++) {
-            expect(component.absoluteSeries[i]['absoluteValue']).to.equal(initialStats[i]);
+            expect(component.absoluteSeries[i]['absoluteValue']).toBe(initialStats[i]);
         }
-        expect(component.absoluteSeries.length).to.equal(initialStats.length);
 
         component.switchTimeSpan(true);
 
+        expect(component.data[0].series).toHaveLength(initialStats.length);
         for (let i = 0; i < 16; i++) {
-            expect(component.absoluteSeries[i]['absoluteValue']).to.equal(graphData[i]);
+            expect(component.absoluteSeries[i]['absoluteValue']).toBe(graphData[i]);
         }
-        expect(component.data[0].series.length).to.equal(initialStats.length);
 
         component.numberOfStudentsInCourse = 0;
         component.switchTimeSpan(true);
 
-        expect(component.data[0].series.length).to.equal(initialStats.length);
+        expect(component.data[0].series).toHaveLength(initialStats.length);
         for (let i = 0; i < 16; i++) {
-            expect(component.absoluteSeries[i]['absoluteValue']).to.equal(0);
+            expect(component.absoluteSeries[i]['absoluteValue']).toBe(0);
         }
     });
 });
