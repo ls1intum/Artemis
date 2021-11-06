f5d8f31588692d6e69f317b7f6819e1c21c4e27e
Georg Willer <georg.willer@tum.de>
2021-11-06 11:32:39 +0100

    introduced additional tests

--- src/test/javascript/spec/component/programming-exercise/category-issues-chart.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/category-issues-chart.component.spec.ts
@@ -7,6 +7,7 @@ import { MockDirective } from 'ng-mocks';
 describe('CategoryIssuesChartComponent', () => {
     let fixture: ComponentFixture<CategoryIssuesChartComponent>;
     let comp: CategoryIssuesChartComponent;
+    let category: StaticCodeAnalysisCategory;
     beforeEach(async () => {
         TestBed.configureTestingModule({
             imports: [],
@@ -20,23 +21,39 @@ describe('CategoryIssuesChartComponent', () => {
             });
     });
 
-    it('should display the right tooltip', fakeAsync(() => {
-        const staticCodeAnalysisCategory = new StaticCodeAnalysisCategory();
-        staticCodeAnalysisCategory.penalty = 2;
-        staticCodeAnalysisCategory.maxPenalty = 4;
-        staticCodeAnalysisCategory.state = StaticCodeAnalysisCategoryState.Graded;
-        let issuesMap = {
+    const initComponent = (categories: StaticCodeAnalysisCategory, maxGradedIssues: number, totalStudents: number, maxNumberOfIssues: number) => {
+        comp.issuesMap = configureIssuesMap();
+        comp.category = category;
+        comp.maxGradedIssues = maxGradedIssues;
+        comp.totalStudents = totalStudents;
+        comp.maxNumberOfIssues = maxNumberOfIssues;
+
+        comp.ngOnChanges();
+        tick();
+    };
+
+    const configureCategory = () => {
+        category = {
+            id: 0,
+            name: '',
+            state: StaticCodeAnalysisCategoryState.Graded,
+            penalty: 2,
+            maxPenalty: 4,
+        };
+        return category;
+    };
+    const configureIssuesMap = () => {
+        const issuesMap = {
             '1': 2,
             '2': 3,
             '3': 4,
         };
+        return issuesMap;
+    };
 
-        comp.category = staticCodeAnalysisCategory;
-        comp.maxNumberOfIssues = 6;
-        comp.issuesMap = issuesMap;
-
-        comp.ngOnChanges();
-        tick();
+    it('should display the right tooltip content', fakeAsync(() => {
+        const staticCodeAnalysisCategory = configureCategory();
+        initComponent(staticCodeAnalysisCategory, 0, 0, 6);
 
         expect(comp.columns).toBeDefined();
         expect(comp.columns).toHaveLength(11);
@@ -44,25 +61,62 @@ describe('CategoryIssuesChartComponent', () => {
         expect(comp.columns[0].tooltip).toBe('2 students have 1 issue.');
         expect(comp.columns[1].tooltip).toBe('3 students have 2 issues.');
         expect(comp.columns[2].tooltip).toBe('4 students have 3 issues.');
-        expect(comp.columns[0].color).toBe('#ffc107');
+        expect(comp.columns[3].tooltip).toBe('0 students have 4 issues.');
+    }));
 
-        issuesMap = {
-            '1': 2,
-            '2': 3,
-            '3': 6,
-        };
-        comp.issuesMap = issuesMap;
-        comp.ngOnChanges();
-        tick();
+    it('should display the right tooltip color for feedback', fakeAsync(() => {
+        category = configureCategory();
+        category.state = StaticCodeAnalysisCategoryState.Feedback;
+        initComponent(category, 0, 0, 5);
 
-        expect(comp.columns[2].tooltip).toBe('6 students have 3 issues.');
-        expect(comp.columns[2].color).toBe('#dc3545');
-
-        staticCodeAnalysisCategory.state = StaticCodeAnalysisCategoryState.Feedback;
-
-        comp.ngOnChanges();
-        tick();
-
+        expect(comp.columns[0].color).toBe('#28a745');
         expect(comp.columns[1].color).toBe('#28a745');
+        expect(comp.columns[2].color).toBe('#28a745');
+        expect(comp.columns[3].color).toBe('#28a745');
+    }));
+
+    it('should display the right tooltip color for inactive', fakeAsync(() => {
+        category = configureCategory();
+        category.state = StaticCodeAnalysisCategoryState.Inactive;
+
+        initComponent(category, 0, 0, 5);
+
+        expect(comp.columns[0].color).toBe('#ddd');
+        expect(comp.columns[1].color).toBe('#ddd');
+        expect(comp.columns[2].color).toBe('#ddd');
+        expect(comp.columns[3].color).toBe('#ddd');
+    }));
+
+    it('should display the right tooltip color for more issues than max graded', fakeAsync(() => {
+        category = configureCategory();
+
+        initComponent(category, 1, 2, 5);
+
+        expect(comp.columns[0].color).toBe('#ffc107');
+        expect(comp.columns[1].color).toBe('#ffc107');
+        expect(comp.columns[2].color).toBe('#dc3545');
+        expect(comp.columns[3].color).toBe('#28a745');
+    }));
+
+    it('should have the right tooltip width', fakeAsync(() => {
+        category = configureCategory();
+
+        initComponent(category, 0, 0, 11);
+
+        expect(comp.columns[0].w).toBe('6.5%');
+        expect(comp.columns[1].w).toBe('6.5%');
+        expect(comp.columns[2].w).toBe('6.5%');
+        expect(comp.columns[3].w).toBe('6.5%');
+    }));
+
+    it('should have the right tooltip height', fakeAsync(() => {
+        category = configureCategory();
+
+        initComponent(category, 0, 10, 5);
+
+        expect(comp.columns[0].h).toBe('23%');
+        expect(comp.columns[1].h).toBe('32.5%');
+        expect(comp.columns[2].h).toBe('42%');
+        expect(comp.columns[3].h).toBe('4%');
     }));
 });
