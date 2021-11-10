793345cf5d242c7582c8a76dde502048f9227313
Argert Boja <argert.boja@tum.de>
2021-11-10 22:29:56 +0100

    refactoring

--- src/main/java/de/tum/in/www1/artemis/web/rest/TextAssessmentResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/TextAssessmentResource.java
@@ -561,9 +561,11 @@ public class TextAssessmentResource extends AssessmentResource {
             }).collect(toSet());
             textBlockService.saveAll(updatedTextBlocks);
             // Update the feedback_id for existing text blocks
-            final var blocksToUpdate = textSubmission.getBlocks();
-            blocksToUpdate.forEach(tb -> tb.setFeedback(feedbackMap.get(tb.getId())));
-            textBlockService.saveAll(blocksToUpdate);
+            if (!existingTextBlockIds.isEmpty()) {
+                final var blocksToUpdate = textSubmission.getBlocks();
+                blocksToUpdate.forEach(tb -> tb.setFeedback(feedbackMap.get(tb.getId())));
+                textBlockService.saveAll(blocksToUpdate);
+            }
         }
     }
 }
