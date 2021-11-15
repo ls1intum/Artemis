45e001a666fcbc250b8861eec12ab305f3e6005b
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-11-15 12:58:25 +0100

    fix some minor issues

--- src/main/webapp/app/shared/metis/postings-header/post-header/post-header.component.ts
+++ src/main/webapp/app/shared/metis/postings-header/post-header/post-header.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input, OnDestroy, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Post } from 'app/entities/metis/post.model';
 import { PostingsHeaderDirective } from 'app/shared/metis/postings-header/postings-header.directive';
 import { MetisService } from 'app/shared/metis/metis.service';
 import { PostCreateEditModalComponent } from 'app/shared/metis/postings-create-edit-modal/post-create-edit-modal/post-create-edit-modal.component';
+import { CourseWideContext } from '../../metis.util';
 
 @Component({
     selector: 'jhi-post-header',
     templateUrl: './post-header.component.html',
     styleUrls: ['../../metis.component.scss'],
 })
-export class PostHeaderComponent extends PostingsHeaderDirective<Post> implements OnDestroy {
+export class PostHeaderComponent extends PostingsHeaderDirective<Post> implements OnInit, OnDestroy {
     @Input() previewMode: boolean;
     @ViewChild(PostCreateEditModalComponent) postCreateEditModal?: PostCreateEditModalComponent;
+    isAtLeastInstructorInCourse: boolean;
+    readonly CourseWideContext = CourseWideContext;
 
     constructor(protected metisService: MetisService) {
         super(metisService);
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+        this.isAtLeastInstructorInCourse = this.metisService.metisUserIsAtLeastInstructorInCourse();
     }
 
     /**
