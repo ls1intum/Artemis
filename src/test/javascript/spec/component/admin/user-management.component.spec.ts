47eb3aff248d42bcf043ee9c98756163498a584f
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-11-19 12:34:05 +0100

    fix imports

--- src/test/javascript/spec/component/admin/user-management.component.spec.ts
+++ src/test/javascript/spec/component/admin/user-management.component.spec.ts
@@ -1,7 +1,5 @@
-import { ComponentFixture, TestBed, fakeAsync, tick, inject } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
-import { ArtemisTestModule } from '../../test.module';
-import { ArtemisAdminModule } from 'app/admin/admin.module';
+import { ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
 import { UserManagementComponent } from 'app/admin/user-management/user-management.component';
 import { UserService } from 'app/core/user/user.service';
 import { AccountService } from 'app/core/auth/account.service';
@@ -11,6 +9,28 @@ import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { User } from 'app/core/user/user.model';
 import { of } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { MockModule } from 'ng-mocks/cjs/lib/mock-module/mock-module';
+import { MockComponent } from 'ng-mocks/cjs/lib/mock-component/mock-component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { AlertComponent } from 'app/shared/alert/alert.component';
+import { AlertErrorComponent } from 'app/shared/alert/alert-error.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+import { ItemCountComponent } from 'app/shared/pagination/item-count.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
+import { MockDirective, MockPipe } from 'ng-mocks';
+import { DeleteButtonDirective } from 'app/shared/delete-dialog/delete-button.directive';
+import { SortByDirective } from 'app/shared/sort/sort-by.directive';
+import { SortDirective } from 'app/shared/sort/sort.directive';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+// tslint:disable-next-line:directive-selector
+@Directive({ selector: '[routerLink]' })
+export class MockRouterLinkDirective {
+    @Input('routerLink') data: any;
+}
 
 describe('UserManagementComponent', () => {
     let comp: UserManagementComponent;
@@ -22,15 +42,28 @@ describe('UserManagementComponent', () => {
         children: [],
     } as any as ActivatedRoute;
 
-    beforeEach(async () => {
+    beforeEach(() => {
         return TestBed.configureTestingModule({
-            imports: [TranslateModule.forRoot(), ArtemisTestModule, ArtemisAdminModule, RouterTestingModule],
+            imports: [MockModule(ReactiveFormsModule), MockModule(NgbModule), HttpClientTestingModule, MockModule(RouterTestingModule)],
             providers: [
                 {
                     provide: ActivatedRoute,
                     useValue: route,
                 },
                 { provide: AccountService, useClass: MockAccountService },
+                { provide: TranslateService, useClass: MockTranslateService },
+            ],
+            declarations: [
+                UserManagementComponent,
+                MockComponent(AlertErrorComponent),
+                MockComponent(AlertComponent),
+                MockComponent(FaIconComponent),
+                MockRouterLinkDirective,
+                MockComponent(ItemCountComponent),
+                MockPipe(ArtemisDatePipe),
+                MockDirective(DeleteButtonDirective),
+                MockDirective(SortByDirective),
+                MockDirective(SortDirective),
             ],
         })
             .compileComponents()
