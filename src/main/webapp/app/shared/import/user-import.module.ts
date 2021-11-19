4537731cadd5a42f394f475bcff87fb807bd6a1c
Tim Cremer <65229601+cremertim@users.noreply.github.com>
2021-11-19 10:31:10 +0100

    General: Allow instructors to import users via csv into course groups (#3973)

---
+++ src/main/webapp/app/shared/import/user-import.module.ts
@@ -0,0 +1,13 @@
+import { UsersImportDialogComponent } from 'app/shared/import/users-import-dialog.component';
+import { UsersImportButtonComponent } from 'app/shared/import/users-import-button.component';
+import { NgModule } from '@angular/core';
+import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
+import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
+
+@NgModule({
+    imports: [ArtemisSharedComponentModule, ArtemisSharedCommonModule],
+    entryComponents: [],
+    declarations: [UsersImportDialogComponent, UsersImportButtonComponent],
+    exports: [UsersImportDialogComponent, UsersImportButtonComponent],
+})
+export class UserImportModule {}
--- src/test/javascript/spec/component/exam/manage/student-exams/user-import-button.component.spec.ts
+++ src/test/javascript/spec/component/exam/manage/student-exams/user-import-button.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import * as chai from 'chai';
-import sinonChai from 'sinon-chai';
 import { MockComponent, MockProvider, MockModule } from 'ng-mocks';
 import { AlertService } from 'app/core/util/alert.service';
 import { TranslateModule } from '@ngx-translate/core';
@@ -8,33 +6,30 @@ import * as sinon from 'sinon';
 import { Exam } from 'app/entities/exam.model';
 import { By } from '@angular/platform-browser';
 import { NgbModal, NgbModule, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { StudentsExamImportButtonComponent } from 'app/exam/manage/students/students-exam-import-dialog/students-exam-import-button.component';
 import { ButtonComponent } from 'app/shared/components/button.component';
+import { UsersImportButtonComponent } from 'app/shared/import/users-import-button.component';
 
-chai.use(sinonChai);
-const expect = chai.expect;
-
-describe('StudentsExamImportButtonComponent', () => {
-    let fixture: ComponentFixture<StudentsExamImportButtonComponent>;
-    let comp: StudentsExamImportButtonComponent;
+describe('UsersImportButtonComponent', () => {
+    let fixture: ComponentFixture<UsersImportButtonComponent>;
+    let comp: UsersImportButtonComponent;
     let modalService: NgbModal;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [MockModule(NgbModule), TranslateModule.forRoot()],
-            declarations: [StudentsExamImportButtonComponent, MockComponent(ButtonComponent)],
+            declarations: [UsersImportButtonComponent, MockComponent(ButtonComponent)],
             providers: [MockProvider(AlertService)],
         })
             .compileComponents()
             .then(() => {
-                fixture = TestBed.createComponent(StudentsExamImportButtonComponent);
+                fixture = TestBed.createComponent(UsersImportButtonComponent);
                 comp = fixture.componentInstance;
                 modalService = TestBed.inject(NgbModal);
             });
     });
 
     afterEach(() => {
-        sinon.restore();
+        jest.restoreAllMocks();
     });
 
     it('should initialize', () => {
@@ -42,11 +37,9 @@ describe('StudentsExamImportButtonComponent', () => {
         const result = new Promise((resolve) => resolve(true));
         const modalServiceOpenStub = sinon.stub(modalService, 'open').returns(<NgbModalRef>{ componentInstance, result });
 
-        comp.openStudentsExamImportDialog(new MouseEvent('click'));
-
+        comp.openUsersImportDialog(new MouseEvent('click'));
         const openStudentsExamImportDialogButton = fixture.debugElement.query(By.css('jhi-button'));
-        expect(openStudentsExamImportDialogButton).to.exist;
-        expect(modalServiceOpenStub).to.have.been.called;
-        modalServiceOpenStub.restore();
+        expect(openStudentsExamImportDialogButton).not.toBe(null);
+        expect(modalServiceOpenStub).toHaveBeenCalledTimes(1);
     });
 });
