94289bad69dbb65f6f04eeca4e8f1b0236610cee
Merlin Mehmed <merlinmehmed17@gmail.com>
2021-11-20 08:45:10 +0200

    Architecture: Migrate to micro services: create JHipster gateway (#3954)

---
+++ gateway/src/main/java/de/tum/in/www1/artemis/gateway/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package de.tum.in.www1.artemis.gateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Gateway.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
---
+++ gateway/src/main/java/de/tum/in/www1/artemis/gateway/security/jwt/JWTRelayGatewayFilterFactory.java
@@ -0,0 +1,49 @@
+package de.tum.in.www1.artemis.gateway.security.jwt;
+
+import static de.tum.in.www1.artemis.gateway.security.jwt.JWTFilter.AUTHORIZATION_HEADER;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+public class JWTRelayGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private final TokenProvider tokenProvider;
+
+    public JWTRelayGatewayFilterFactory(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token = this.extractJWTToken(exchange.getRequest());
+            if (StringUtils.hasText(token) && this.tokenProvider.validateTokenForAuthority(token)) {
+                ServerHttpRequest request = exchange.getRequest().mutate().header(AUTHORIZATION_HEADER, "Bearer " + token).build();
+
+                return chain.filter(exchange.mutate().request(request).build());
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    /**
+     * Extract the JWT token from the request.
+     * Some requests (i.e. /time or public resources like logo) do not require authentication,
+     * therefore null is returned if the Authorization header is not set.
+     * Exception is thrown is the Authorization header is not set correctly.
+     */
+    private String extractJWTToken(ServerHttpRequest request) {
+        String bearerToken = request.getHeaders().getFirst(AUTHORIZATION_HEADER);
+        if (bearerToken == null) {
+            return null;
+        }
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        throw new IllegalArgumentException("Invalid token in Authorization header");
+    }
+}
