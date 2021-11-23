36f888ad1cc6a8b7ee671ab502e9368f757dda12
rs-maker <robert.schauer@tum.de>
2021-11-23 11:10:16 +0100

    fix other broken selectors

--- src/test/cypress/support/pageobjects/assessment/ModelingExerciseAssessmentEditor.ts
+++ src/test/cypress/support/pageobjects/assessment/ModelingExerciseAssessmentEditor.ts
@@ -3,19 +3,19 @@ import { MODELING_SPACE } from '../exercises/modeling/ModelingEditor';
 import { CypressExerciseType } from '../../requests/CourseManagementRequests';
 import { BASE_API, PUT } from '../../constants';
 // TODO: find or create better selectors for this
-const FEEDBACK_CONTAINER = '.sc-lcuiOb';
+const FEEDBACK_CONTAINER = '.sc-lcepkR';
 
 /**
  * A class which encapsulates UI selectors and actions for the Modeling Exercise Assessment editor
  */
 export class ModelingExerciseAssessmentEditor extends AbstractExerciseAssessmentPage {
     openAssessmentForComponent(componentNumber: number) {
-        cy.get('.apollon-row').getSettled(`${MODELING_SPACE} >> :nth-child(${componentNumber})`).children().eq(0).dblclick('top', { force: true });
+        cy.get('.apollon-row').getSettled(`${MODELING_SPACE} >>> :nth-child(${componentNumber})`).children().eq(0).dblclick('top', { force: true });
     }
 
     assessComponent(points: number, feedback: string) {
         cy.get(`${FEEDBACK_CONTAINER} > :nth-child(1) > :nth-child(2) > :nth-child(1) > :nth-child(2)`).type(`${points}`);
-        cy.get(`${FEEDBACK_CONTAINER} > :nth-child(1) > :nth-child(3)`).type(`${feedback}`);
+        cy.get(`${FEEDBACK_CONTAINER} > :nth-child(1) > :nth-child(4)`).type(`${feedback}`);
     }
 
     rejectComplaint(response: string) {
