b73feab2019b5aefc95ff7026a256b5431f9ff6b
Georg Willer <georg.willer@tum.de>
2021-11-23 22:26:32 +0100

    try reduce main size

--- src/main/webapp/app/exercises/shared/result/result.module.ts
+++ src/main/webapp/app/exercises/shared/result/result.module.ts
@@ -8,9 +8,10 @@ import { ResultComponent } from 'app/exercises/shared/result/result.component';
 import { ResultDetailComponent } from 'app/exercises/shared/result/result-detail.component';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { FeedbackCollapseComponent } from 'app/exercises/shared/result/feedback-collapse.component';
+import { Ng2ChartsModule } from 'app/shared/chart/ng2-charts.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule],
+    imports: [ArtemisSharedModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule, Ng2ChartsModule],
     declarations: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent, FeedbackCollapseComponent],
     exports: [ResultComponent, UpdatingResultComponent, ResultDetailComponent, ResultHistoryComponent, SubmissionResultStatusComponent],
 })
