0eb6ff9b6e066eb4c5a44c371509cd3a32872dd6
Lorena Schlesinger <lorena.schlesinger@tum.de>
2021-11-30 18:53:23 +0100

    rework pipe and test

--- src/main/webapp/app/shared/pipes/reacting-users-on-posting.pipe.ts
+++ src/main/webapp/app/shared/pipes/reacting-users-on-posting.pipe.ts
@@ -1,34 +1,31 @@
-import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
-import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
-import { map, Observable, Subscription, tap } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
 
 export const USER_COUNT_LIMIT = 10;
 export const PLACEHOLDER_USER_REACTED = 'REPLACE_WITH_TRANSLATED_YOU';
 
 /**
  * Pipe to show list of reacting users when hovering over emojis.
- * This pipe is stateful (pure = false) so that it can adapt to changes of the current locale.
  */
 @Pipe({
     name: 'reactingUsersOnPosting',
-    pure: false,
 })
 export class ReactingUsersOnPostingPipe implements PipeTransform {
-    private reactingUsersString: string;
-
     constructor(private translateService: TranslateService) {}
 
     /**
-     * Transforms a given name list of reacting users to a prosaic string
+     * Transforms a given name list of reacting users to a prosaic, translated string
      * @param {string[]} reactingUsers users that are reacting with a certain emoji on a posting
-     * @returns {string} concatenated (and shortened if required) list of reacting users
+     * @returns {Observable<string>} observable of concatenated, translated (and shortened if required) string of reacting users
      */
     transform(reactingUsers: string[]): Observable<string> {
-        return this.translateService.onLangChange.pipe(
-            map((event: LangChangeEvent) => {
-                return this.updateReactingUsersString(reactingUsers);
-            }),
-        );
+        return Observable.create((observer: any) => {
+            observer.next(this.updateReactingUsersString(reactingUsers));
+            this.translateService.onLangChange.subscribe(() => {
+                observer.next(this.updateReactingUsersString(reactingUsers));
+            });
+        });
     }
 
     /**
@@ -42,8 +39,7 @@ export class ReactingUsersOnPostingPipe implements PipeTransform {
         if (reactingUsers.includes(PLACEHOLDER_USER_REACTED)) {
             if (reactingUsers.length === 1) {
                 // set "you" as ready-to-use reacting users string
-                this.reactingUsersString = this.translateService.instant('artemisApp.metis.you');
-                return this.reactingUsersString;
+                return this.translateService.instant('artemisApp.metis.you');
             }
             // if more than the currently logged in user reacted,
             // remove placeholder and replace it with directly addressing currently logged in user
@@ -55,19 +51,18 @@ export class ReactingUsersOnPostingPipe implements PipeTransform {
         if (numberOfReactingUsers > USER_COUNT_LIMIT) {
             // prepare trimmed list
             reactingUsers = reactingUsers.slice(0, USER_COUNT_LIMIT);
-            this.reactingUsersString =
-                reactingUsers.join(', ') + this.translateService.instant('artemisApp.metis.reactedTooltipTrimmed', { number: numberOfReactingUsers - USER_COUNT_LIMIT });
+            return reactingUsers.join(', ') + this.translateService.instant('artemisApp.metis.reactedTooltipTrimmed', { number: numberOfReactingUsers - USER_COUNT_LIMIT });
         } else {
             // prepare list
-            this.reactingUsersString = reactingUsers.join(', ') + this.translateService.instant('artemisApp.metis.reactedTooltip');
+            let listOfReactingUsers = reactingUsers.join(', ') + this.translateService.instant('artemisApp.metis.reactedTooltip');
             // replace last comma by "and"
-            const lastCommaIndex = this.reactingUsersString.lastIndexOf(',');
+            const lastCommaIndex = listOfReactingUsers.lastIndexOf(',');
             if (lastCommaIndex > -1) {
-                const beforeLastComma = this.reactingUsersString.substring(0, lastCommaIndex);
-                const afterLastComma = this.reactingUsersString.substring(lastCommaIndex + 2, this.reactingUsersString.length);
-                this.reactingUsersString = beforeLastComma + this.translateService.instant('artemisApp.metis.and') + afterLastComma;
+                const beforeLastComma = listOfReactingUsers.substring(0, lastCommaIndex);
+                const afterLastComma = listOfReactingUsers.substring(lastCommaIndex + 2, listOfReactingUsers.length);
+                listOfReactingUsers = beforeLastComma + this.translateService.instant('artemisApp.metis.and') + afterLastComma;
             }
+            return listOfReactingUsers;
         }
-        return this.reactingUsersString;
     }
 }
--- src/test/javascript/spec/pipe/reacting-users-on-posting.pipe.spec.ts
+++ src/test/javascript/spec/pipe/reacting-users-on-posting.pipe.spec.ts
@@ -2,13 +2,14 @@ import { HtmlForPostingMarkdownPipe } from 'app/shared/pipes/html-for-posting-ma
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MockTranslateService } from '../helpers/mocks/service/mock-translate.service';
 import { PLACEHOLDER_USER_REACTED, ReactingUsersOnPostingPipe } from 'app/shared/pipes/reacting-users-on-posting.pipe';
-import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { metisTutor, metisUser1, metisUser2 } from '../helpers/sample/metis-sample-data';
 
 describe('ReactingUsersOnPostingsPipe', () => {
     let reactingUsersPipe: ReactingUsersOnPostingPipe;
     let translateService: TranslateService;
     let updateReactingUsersStringSpy: jest.SpyInstance;
+    let transformedStringWithReactingUsers: string;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -24,50 +25,58 @@ describe('ReactingUsersOnPostingsPipe', () => {
         jest.restoreAllMocks();
     });
 
-    it('Should return string for one user that is not "you"', () => {
+    it('Should return string for one user that is not "you"', fakeAsync(() => {
         const reactingUsers = [metisUser1.name!];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe(metisUser1.name + 'artemisApp.metis.reactedTooltip');
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
 
-    it('Should return string for one user that is "you"', () => {
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform([PLACEHOLDER_USER_REACTED]);
+    it('Should return string for one user that is "you"', fakeAsync(() => {
+        reactingUsersPipe.transform([PLACEHOLDER_USER_REACTED]).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe('artemisApp.metis.you');
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
 
-    it('Should return string for two users that do not include "you"', () => {
+    it('Should return string for two users that do not include "you"', fakeAsync(() => {
         const reactingUsers = [metisUser1.name!, metisUser2.name!];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe(metisUser1.name! + 'artemisApp.metis.and' + metisUser2.name! + 'artemisApp.metis.reactedTooltip');
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
 
-    it('Should return string for two users that do include "you"', () => {
+    it('Should return string for two users that do include "you"', fakeAsync(() => {
         const reactingUsers = [metisUser1.name!, PLACEHOLDER_USER_REACTED];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe('artemisApp.metis.you' + 'artemisApp.metis.and' + metisUser1.name! + 'artemisApp.metis.reactedTooltip');
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
 
-    it('Should return string for three users that do include "you" and separate the first two users with comma', () => {
-        const reactingUsers = [metisUser1.name!, metisUser2.name!, metisTutor.name!];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
-        expect(transformedStringWithReactingUsers).toBe(metisUser1.name! + ', ' + metisUser2.name! + 'artemisApp.metis.and' + metisTutor.name! + 'artemisApp.metis.reactedTooltip');
-        expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
-
-    it('Should return string for three users that do include "you" and separate the first two users with comma', () => {
+    it('Should return string for three users that do include "you" and separate the first two users with comma', fakeAsync(() => {
         const reactingUsers = [metisUser1.name!, PLACEHOLDER_USER_REACTED, metisTutor.name!];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe(
             'artemisApp.metis.you' + ', ' + metisUser1.name! + 'artemisApp.metis.and' + metisTutor.name! + 'artemisApp.metis.reactedTooltip',
         );
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
 
-    it('Should trim list of reacting users but always include "you', () => {
+    it('Should trim list of reacting users but always include "you', fakeAsync(() => {
         const reactingUsers = [
             metisUser1.name!,
             metisUser2.name!,
@@ -82,7 +91,10 @@ describe('ReactingUsersOnPostingsPipe', () => {
             'userH',
             PLACEHOLDER_USER_REACTED,
         ];
-        const transformedStringWithReactingUsers = reactingUsersPipe.transform(reactingUsers);
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
         expect(transformedStringWithReactingUsers).toBe(
             'artemisApp.metis.you' +
                 ', ' +
@@ -106,5 +118,16 @@ describe('ReactingUsersOnPostingsPipe', () => {
                 'artemisApp.metis.reactedTooltipTrimmed',
         );
         expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(1);
-    });
+    }));
+
+    it('Should trigger update of reacting users on language change', fakeAsync(() => {
+        const reactingUsers = [metisUser1.name!, PLACEHOLDER_USER_REACTED, metisTutor.name!];
+        reactingUsersPipe.transform(reactingUsers).subscribe((reactingUsers: string) => {
+            transformedStringWithReactingUsers = reactingUsers;
+        });
+        tick();
+        translateService.use('de');
+        tick();
+        expect(updateReactingUsersStringSpy).toHaveBeenCalledTimes(2);
+    }));
 });
