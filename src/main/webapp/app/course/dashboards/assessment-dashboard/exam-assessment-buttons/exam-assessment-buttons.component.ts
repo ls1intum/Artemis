9ed858045997165133f1faf19d253da8131ba695
rs-maker <robert.schauer@tum.de>
2021-12-01 15:33:46 +0100

    remove unused import

--- src/main/webapp/app/course/dashboards/assessment-dashboard/exam-assessment-buttons/exam-assessment-buttons.component.ts
+++ src/main/webapp/app/course/dashboards/assessment-dashboard/exam-assessment-buttons/exam-assessment-buttons.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { StudentExamService } from 'app/exam/manage/student-exams/student-exam.service';
 import { forkJoin } from 'rxjs';
 import { tap } from 'rxjs/operators';
