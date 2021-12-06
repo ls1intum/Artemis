3deb00c56b3556064dd54d07d51eea09a7f79123
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-12-06 20:16:34 +0100

    Added some ids for SCA

--- src/test/cypress/support/pageobjects/exercises/programming/ScaFeedbackModal.ts
+++ src/test/cypress/support/pageobjects/exercises/programming/ScaFeedbackModal.ts
@@ -2,10 +2,10 @@
  * A class which encapsulates UI selectors and actions for the static code analysis feedback modal in the online editor.
  */
 export class ScaFeedbackModal {
-    private readonly feedbackSelector = '.feedback-item, .alert-success';
+    private readonly feedbackSelector = '#feedback-message';
 
     shouldShowPointChart() {
-        cy.get('jhi-chart').find('canvas').should('be.visible');
+        cy.get('#chart').should('be.visible');
     }
 
     shouldShowFeedback(numberOfPassedTests: number, points: string) {
@@ -14,12 +14,12 @@ export class ScaFeedbackModal {
     }
 
     shouldShowCodeIssue(feedbackText: string, pointReduction: string) {
-        cy.get('.feedback-text')
+        cy.get('#feedback-text')
             .contains(feedbackText)
             .scrollIntoView()
             .should('be.visible')
-            .parents('.feedback-item')
-            .find('.feedback-points')
+            .parents('#feedback-message')
+            .find('#feedback-points')
             .scrollIntoView()
             .should('contain.text', `-${pointReduction}P`)
             .and('be.visible');
