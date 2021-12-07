be79dcb3b3504b933a2a0346eae0d9da07d2da52
Patrick Bassner <patrick@bassner.de>
2021-12-07 15:48:25 +0100

    remove unused ace-editor directive

--- src/main/webapp/app/shared/markdown-editor/ace-editor/ace-editor.module.ts
+++ src/main/webapp/app/shared/markdown-editor/ace-editor/ace-editor.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { AceEditorComponent } from 'app/shared/markdown-editor/ace-editor/ace-editor.component';
-import { AceEditorDirective } from 'app/shared/markdown-editor/ace-editor/ace-editor.directive';
 
 @NgModule({
-    declarations: [AceEditorComponent, AceEditorDirective],
-    exports: [AceEditorComponent, AceEditorDirective],
+    declarations: [AceEditorComponent],
+    exports: [AceEditorComponent],
 })
 export class AceEditorModule {}
