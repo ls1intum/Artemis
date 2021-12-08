516281a7c8e0ad80805259390234e1373fecf8c6
Stephan Krusche <krusche@tum.de>
2021-12-08 17:40:35 +0100

    Development: Improve client test quality for course component

--- jest.config.js
+++ jest.config.js
@@ -33,10 +33,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should be increase to at least 80%
-            statements: 77.1,
-            branches: 65.4,
-            functions: 68.7,
-            lines: 76.7,
+            statements: 77.2,
+            branches: 65.5,
+            functions: 68.8,
+            lines: 76.8,
         },
     },
     setupFilesAfterEnv: ['<rootDir>/src/test/javascript/spec/jest-test-setup.ts', 'jest-sinon', 'jest-extended/all'],
