89d3e79e6f73c1139b1a9094d0af4762200f3f06
Malyuk-A <malyuk.alexander1999@gmail.com>
2021-12-08 11:52:26 +0100

    fixed merge-conflict

--- src/main/java/de/tum/in/www1/artemis/domain/notification/GroupNotificationFactory.java
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/GroupNotificationFactory.java
@@ -173,7 +173,6 @@ public class GroupNotificationFactory {
                 text = "Lecture \"" + lecture.getTitle() + "\" got a new post.";
                 notification = new GroupNotification(course, title, text, author, groupNotificationType);
                 notification.setTransientAndStringTarget(NOTIFICATION_TARGET_PROVIDER.getLecturePostTarget(post, course));
-                notification.setTarget(targetProvider.getLecturePostTarget(post, course));
             }
             case NEW_COURSE_POST -> {
                 title = NEW_COURSE_POST_TITLE;
