575f956219404d337e07d5767810167514217561
Georg Willer <80622272+willergeorg@users.noreply.github.com>
2021-12-09 11:19:38 +0100

    Development: Modernize doughnut charts on course detail page, student overall score chart, student scores in exercise groups charts and programming feedback chart (#4321)

--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -4,9 +4,8 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
-    exports: [FormsModule, CommonModule, NgbModule, FontAwesomeModule, ReactiveFormsModule, TranslateModule, ChartsModule],
+    exports: [FormsModule, CommonModule, NgbModule, FontAwesomeModule, ReactiveFormsModule, TranslateModule],
 })
 export class ArtemisSharedLibsModule {}
