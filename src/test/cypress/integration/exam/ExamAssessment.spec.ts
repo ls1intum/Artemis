b7575d3f11eb2bcb49b6fb732b0317aaa928620d
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2021-12-11 17:19:10 +0100

    Wait for modeling editor to save changes

--- src/test/cypress/integration/exam/ExamAssessment.spec.ts
+++ src/test/cypress/integration/exam/ExamAssessment.spec.ts
@@ -126,6 +126,8 @@ describe('Exam assessment', () => {
                 modelingAssessment.assessComponent(0, 'Neutral');
                 modelingAssessment.clickNextAssessment();
                 modelingAssessment.assessComponent(-1, 'Wrong');
+                // Wait for the editor to save the changes
+                cy.wait(500);
                 examAssessment.submitModelingAssessment().then((assessmentResponse) => {
                     expect(assessmentResponse.response?.statusCode).to.equal(200);
                 });
