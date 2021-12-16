6bac81b5b65d07e246479167227c194e467b7838
Patrick Bassner <patrick@bassner.de>
2021-12-16 16:27:13 +0100

    next try

--- src/main/webapp/app/core/interceptor/artemis-version.interceptor.ts
+++ src/main/webapp/app/core/interceptor/artemis-version.interceptor.ts
@@ -15,12 +15,13 @@ export class ArtemisVersionInterceptor implements HttpInterceptor {
         // Allow the app to stabilize first, before starting
         // polling for updates with `interval()`.
         const appIsStable = appRef.isStable.pipe(first((isStable) => isStable === true));
+        appIsStable.subscribe(() => console.log('Application became stable'));
         const updateInterval = interval(10 * 1000); // every 60s
         const updateIntervalOnceAppIsStable$ = concat(appIsStable, updateInterval);
 
         updateIntervalOnceAppIsStable$.subscribe(() => {
             console.log('Interval triggered.');
-            this.checkForUpdates();
+            this.checkForUpdates(false);
         });
 
         updates.available.subscribe((event) => {
@@ -41,7 +42,7 @@ export class ArtemisVersionInterceptor implements HttpInterceptor {
                     const serverVersion = response.headers.get(ARTEMIS_VERSION_HEADER);
                     if (VERSION && serverVersion && VERSION !== serverVersion && !isTranslationStringsRequest) {
                         console.log('Version not equal!');
-                        this.checkForUpdates();
+                        this.checkForUpdates(true);
                     }
                     // only invoke the time call if the call was not already the time call to prevent recursion here
                     if (!request.url.includes('time')) {
@@ -52,25 +53,28 @@ export class ArtemisVersionInterceptor implements HttpInterceptor {
         );
     }
 
-    public checkForUpdates() {
+    private checkForUpdates(overrideCheckResult: boolean) {
         // first update the service worker
         console.log('Checking for updates now!');
         this.updates.checkForUpdate().then((updateAvailable: boolean) => {
             console.log('Check complete. Result: ' + updateAvailable);
-            console.log('Showing update alert now.');
 
-            this.alert?.close!();
+            if (updateAvailable || overrideCheckResult) {
+                console.log('Showing update alert now.');
 
-            this.alert = this.alertService.addAlert({
-                type: 'info',
-                message: 'artemisApp.outdatedAlert',
-                timeout: 30000,
-                action: {
-                    label: 'artemisApp.outdatedAction',
-                    callback: () => this.updates.activateUpdate().then(() => document.location.reload()),
-                },
-            });
-            console.log('Returned');
+                this.alert?.close!();
+
+                this.alert = this.alertService.addAlert({
+                    type: 'info',
+                    message: 'artemisApp.outdatedAlert',
+                    timeout: 30000,
+                    action: {
+                        label: 'artemisApp.outdatedAction',
+                        callback: () => this.updates.activateUpdate().then(() => document.location.reload()),
+                    },
+                });
+                console.log('Returned');
+            }
         });
     }
 }
