204491f86b017e05693941fe22585a83b1c25836
Patrick Bassner <patrick@bassner.de>
2021-12-16 17:10:45 +0100

    next try

--- src/main/webapp/app/core/interceptor/artemis-version.interceptor.ts
+++ src/main/webapp/app/core/interceptor/artemis-version.interceptor.ts
@@ -1,7 +1,7 @@
 import { ApplicationRef, Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { concat, interval, Observable } from 'rxjs';
-import { first, tap } from 'rxjs/operators';
+import { concat, EMPTY, interval, Observable } from 'rxjs';
+import { catchError, first, tap, timeout } from 'rxjs/operators';
 import { ARTEMIS_VERSION_HEADER, VERSION } from 'app/app.constants';
 import { ArtemisServerDateService } from 'app/shared/server-date.service';
 import { SwUpdate } from '@angular/service-worker';
@@ -9,12 +9,27 @@ import { Alert, AlertService } from 'app/core/util/alert.service';
 
 @Injectable()
 export class ArtemisVersionInterceptor implements HttpInterceptor {
+    // The currently displayed alert
     private alert: Alert;
+    // Set to true if we ever saw an outdated indication since last reload
+    // for some reason, SwUpdate.checkForUpdate returns false after a while even though we didn't actually update yet.
+    // We will show alerts in any case until the next reload, so we store this here.
+    // Note: We can't just display an alert one time, because it may get cleared in several occasions
+    private hasSeenOutdatedInThisSession = false;
 
     constructor(private appRef: ApplicationRef, private updates: SwUpdate, private serverDateService: ArtemisServerDateService, private alertService: AlertService) {
+        console.log('UMMMM');
         // Allow the app to stabilize first, before starting
         // polling for updates with `interval()`.
-        const appIsStable = appRef.isStable.pipe(first((isStable) => isStable === true));
+        const appIsStable = appRef.isStable.pipe(
+            first((isStable) => isStable === true),
+            // Sometimes, the application does not become stable apparently.
+            // This is a workaround. Using the same timeout as the service worker as well.
+            // TODO: Look for the cause why the app doesn't become stable
+            timeout(30000),
+            // Ignore error thrown by timeout
+            catchError(() => EMPTY),
+        );
         appIsStable.subscribe(() => console.log('Application became stable'));
         const updateInterval = interval(10 * 1000); // every 60s
         const updateIntervalOnceAppIsStable$ = concat(appIsStable, updateInterval);
@@ -44,6 +59,7 @@ export class ArtemisVersionInterceptor implements HttpInterceptor {
                         console.log('Version not equal!');
                         this.checkForUpdates(true);
                     }
+
                     // only invoke the time call if the call was not already the time call to prevent recursion here
                     if (!request.url.includes('time')) {
                         this.serverDateService.updateTime();
@@ -59,15 +75,17 @@ export class ArtemisVersionInterceptor implements HttpInterceptor {
         this.updates.checkForUpdate().then((updateAvailable: boolean) => {
             console.log('Check complete. Result: ' + updateAvailable);
 
-            if (updateAvailable || overrideCheckResult) {
+            if (this.hasSeenOutdatedInThisSession || updateAvailable || overrideCheckResult) {
                 console.log('Showing update alert now.');
+                this.hasSeenOutdatedInThisSession = true;
 
+                // Close previous alert to avoid duplicates
                 this.alert?.close!();
 
+                // Show fresh alert without timeout
                 this.alert = this.alertService.addAlert({
                     type: 'info',
                     message: 'artemisApp.outdatedAlert',
-                    timeout: 30000,
                     action: {
                         label: 'artemisApp.outdatedAction',
                         callback: () => this.updates.activateUpdate().then(() => document.location.reload()),
