dea2fedf0c40bbe37c589793d7c0db80bbf5e607
Georg Willer <80622272+willergeorg@users.noreply.github.com>
2021-12-20 23:13:27 +0100

    Development: Modernize appearance of plagiarism similarity distribution and tutor effort statistics  (#4509)

--- src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.module.ts
+++ src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.module.ts
@@ -3,11 +3,11 @@ import { RouterModule } from '@angular/router';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { TutorEffortStatisticsComponent } from 'app/exercises/text/manage/tutor-effort/tutor-effort-statistics.component';
 import { tutorEffortStatisticsRoute } from 'app/exercises/text/manage/tutor-effort/tutor-effort-statistics.route';
-import { ChartsModule } from 'ng2-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 const ENTITY_STATES = [...tutorEffortStatisticsRoute];
 @NgModule({
-    imports: [ArtemisSharedModule, RouterModule.forChild(ENTITY_STATES), ChartsModule],
+    imports: [ArtemisSharedModule, RouterModule.forChild(ENTITY_STATES), NgxChartsModule],
     declarations: [TutorEffortStatisticsComponent],
 })
 export class ArtemisTutorEffortStatisticsModule {}
