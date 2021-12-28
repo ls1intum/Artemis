6e9f9554add0e2bd3d6cae2f6b0c5e2a37df3758
Georg Willer <georg.willer@tum.de>
2021-12-28 14:02:43 +0100

    add test suite for StatisticsRepository

---
+++ src/test/java/de/tum/in/www1/artemis/repository/StatisticsRepositoryTest.java
@@ -0,0 +1,159 @@
+package de.tum.in.www1.artemis.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
+import de.tum.in.www1.artemis.domain.PersistentAuditEvent;
+import de.tum.in.www1.artemis.domain.enumeration.GraphType;
+import de.tum.in.www1.artemis.domain.enumeration.SpanType;
+import de.tum.in.www1.artemis.domain.enumeration.StatisticsView;
+import de.tum.in.www1.artemis.domain.statistics.StatisticsEntry;
+
+public class StatisticsRepositoryTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
+
+    @Autowired
+    private StatisticsRepository statisticsRepository;
+
+    @Autowired
+    private PersistenceAuditEventRepository persistenceAuditEventRepository;
+
+    @ParameterizedTest
+    @EnumSource(value = SpanType.class, names = { "WEEK", "QUARTER" })
+    @WithMockUser(username = "instructor1", roles = "INSTRUCTOR")
+    public void testFilterDuplicatedUsers_GraphType_LoggedInUsers(SpanType spanType) {
+        // start time and end time for the method call
+        var startDate = ZonedDateTime.parse("2007-12-03T10:15:30+01:00[Europe/Paris]");
+        var endDate = spanType == SpanType.WEEK ? startDate.plusDays(7) : startDate.plusYears(1);
+        // we need to add users in order to get non-empty results returned
+        database.addUsers(2, 0, 0, 0);
+        // the persistentEvents simulate a log in of a student
+        // here we simulate that student1 logged in on 03.12.07
+        var persistentEvent_student_1 = setupPersistentEvent("student1", startDate);
+        // here we simulate student1 logged in again on 07.12.07 for the weekly view and for the quarter view a login on 03.02.08
+        var persistentEvent_student_1_later = spanType == SpanType.WEEK ? setupPersistentEvent("student1", startDate.plusDays(4))
+                : setupPersistentEvent("student1", startDate.plusMonths(2));
+        // here we simulate that student2 logged in on 07.12.07
+        var persistentEvent_student_2 = setupPersistentEvent("student2", startDate.plusDays(4));
+        // we simulate the same case again in order to have duplication in the result of the query
+        var persistentEvent_student_2_duplicate = setupPersistentEvent("student2", startDate.plusDays(4).plusHours(2));
+        // save the events
+        persistenceAuditEventRepository
+                .saveAll(List.of(persistentEvent_student_1, persistentEvent_student_1_later, persistentEvent_student_2, persistentEvent_student_2_duplicate));
+        // this is the entry that should be returned by both span types
+        StatisticsEntry entry_03_12_07 = new StatisticsEntry();
+        entry_03_12_07.setDay(ZonedDateTime.parse("2007-12-03T00:00:00+01:00[Europe/Paris]"));
+
+        // needed as entry method due to private
+        List<StatisticsEntry> entryList = statisticsRepository.getNumberOfEntriesPerTimeSlot(spanType, startDate, endDate, GraphType.LOGGED_IN_USERS, StatisticsView.ARTEMIS, null);
+
+        if (spanType == SpanType.WEEK) {
+            StatisticsEntry entry_07_12_07 = new StatisticsEntry(ZonedDateTime.parse("2007-12-07T00:00:00+01:00[Europe/Paris]"), 2);
+            entry_03_12_07.setAmount(1);
+
+            assertThat(entryList).as("Result contains the entry for 07.12.07").anyMatch((entry) -> compareStatisticsEntries(entry, entry_07_12_07));
+        }
+        else {
+            entry_03_12_07.setAmount(2);
+            StatisticsEntry entry_29_01_08 = new StatisticsEntry(ZonedDateTime.parse("2008-01-29T00:00+01:00[Europe/Paris]"), 1);
+
+            assertThat(entryList).as("Result contains the entry for 29.01.08").anyMatch((entry) -> compareStatisticsEntries(entry, entry_29_01_08));
+        }
+
+        assertThat(entryList).as("Result has 2 entries for two time slots").hasSize(2);
+        assertThat(entryList).as("Result contains the entry for 03.12.07").anyMatch((entry) -> compareStatisticsEntries(entry, entry_03_12_07));
+
+        database.resetDatabase();
+        persistenceAuditEventRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetNumberOfEntriesPerTimeSlot_forInvalidView() {
+        var startDate = ZonedDateTime.parse("2007-12-03T10:15:30+01:00[Europe/Paris]");
+        var endDate = startDate.plusDays(7);
+
+        // the other graph types cover every possible view. The default cases for the view switch statements for these graph types are dead code as far as I can tell
+        GraphType[] graphTypes = new GraphType[] { GraphType.RELEASED_EXERCISES, GraphType.EXERCISES_DUE, GraphType.CONDUCTED_EXAMS, GraphType.EXAM_PARTICIPATIONS,
+                GraphType.EXAM_REGISTRATIONS, GraphType.POSTS, GraphType.RESOLVED_POSTS };
+
+        for (GraphType graphType : graphTypes) {
+            // depending on the graph type, we inject the view that is not supported for it
+            StatisticsView view = graphType == GraphType.POSTS || graphType == GraphType.RESOLVED_POSTS ? StatisticsView.ARTEMIS : StatisticsView.EXERCISE;
+            assertThrows(UnsupportedOperationException.class, () -> statisticsRepository.getNumberOfEntriesPerTimeSlot(SpanType.WEEK, startDate, endDate, graphType, view, null));
+        }
+    }
+
+    @Test
+    public void testMergeResultsIntoArrayForYear_differentYear_and_differentQuarter() {
+        List<StatisticsEntry> outcome = setupStatisticsEntryList();
+        // the start time is in a different year
+        ZonedDateTime startDate = ZonedDateTime.parse("2020-12-03T10:15:30+01:00[Europe/Paris]");
+        Integer[] resultYear = new Integer[] { 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0 };
+        Integer[] expectedResultYear = new Integer[] { 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 123 };
+        Integer[] returnedResultYear = statisticsRepository.mergeResultsIntoArrayForYear(outcome, resultYear, startDate);
+
+        assertThat(returnedResultYear).as("Bucket 11 now has value for the entry date (123)").isEqualTo(expectedResultYear);
+
+        Integer[] resultQuarter = new Integer[53];
+        Integer[] expectedResultQuarter = new Integer[53];
+        for (int i = 0; i < 53; i++) {
+            resultQuarter[i] = 0;
+            expectedResultQuarter[i] = i != 48 ? 0 : 123;
+        }
+
+        Integer[] returnedResultQuarter = statisticsRepository.mergeResultsIntoArrayForQuarter(outcome, resultQuarter, startDate);
+
+        assertThat(returnedResultQuarter).as("Bucket 48 now has value for the entry date (123)").isEqualTo(expectedResultQuarter);
+    }
+
+    /**
+     * A helper method in order to prevent code duplication for comparison of StatisticEntries
+     * @param entry1 the first entry to compare
+     * @param entry2 the second entry to compare
+     * @return true if both entries contain the same day and amount, false otherwise
+     */
+    private boolean compareStatisticsEntries(StatisticsEntry entry1, StatisticsEntry entry2) {
+        return entry1.getDay().toString().equals(entry2.getDay().toString()) && entry1.getAmount() == entry2.getAmount();
+    }
+
+    /**
+     * Setup method in order to prevent code duplication for initialisation of entry list
+     * @return entry list used as input for tests
+     */
+    private List<StatisticsEntry> setupStatisticsEntryList() {
+        StatisticsEntry entry = new StatisticsEntry();
+        ZonedDateTime date = ZonedDateTime.parse("2021-11-03T10:15:30+01:00[Europe/Paris]");
+        entry.setDay(date);
+        entry.setAmount(123);
+        var list = new ArrayList<StatisticsEntry>();
+        list.add(entry);
+
+        return list;
+    }
+
+    /**
+     * Creates persistent event for tests
+     * @param principal the student login that should be used
+     * @param date the timestamp the login should be simulated
+     * @return PersistentAuditEvent representing the login of the given user for at the given point in time
+     */
+    private PersistentAuditEvent setupPersistentEvent(String principal, ZonedDateTime date) {
+        PersistentAuditEvent persistentEvent = new PersistentAuditEvent();
+        persistentEvent.setPrincipal(principal);
+        persistentEvent.setAuditEventType("AUTHENTICATION_SUCCESS");
+        persistentEvent.setAuditEventDate(Instant.from(date));
+
+        return persistentEvent;
+    }
+}
