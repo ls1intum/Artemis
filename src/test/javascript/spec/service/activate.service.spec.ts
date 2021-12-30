93623a5d895a9847070cee6e2a3b035109b2f994
Julian Christl <julian.christl@tum.de>
2021-12-30 22:35:18 +0100

    convert tests

--- src/test/javascript/spec/service/activate.service.spec.ts
+++ src/test/javascript/spec/service/activate.service.spec.ts
@@ -1,18 +1,12 @@
 import { async } from '@angular/core/testing';
-import * as chai from 'chai';
-import sinonChai from 'sinon-chai';
-import { SinonStub, stub } from 'sinon';
 import { ActivateService } from 'app/account/activate/activate.service';
 import { MockHttpService } from '../helpers/mocks/service/mock-http.service';
 import { HttpParams } from '@angular/common/http';
 
-chai.use(sinonChai);
-const expect = chai.expect;
-
 describe('ActivateService', () => {
     let activateService: ActivateService;
     let httpService: MockHttpService;
-    let getStub: SinonStub;
+    let getStub: jest.SpyInstance;
 
     const getURL = SERVER_API_URL + 'api/activate';
 
@@ -20,11 +14,11 @@ describe('ActivateService', () => {
         httpService = new MockHttpService();
         // @ts-ignore
         activateService = new ActivateService(httpService);
-        getStub = stub(httpService, 'get');
+        getStub = jest.spyOn(httpService, 'get');
     }));
 
     afterEach(() => {
-        getStub.restore();
+        jest.restoreAllMocks();
     });
 
     it('should send a request to the server to activate the user', async () => {
@@ -32,7 +26,8 @@ describe('ActivateService', () => {
 
         await activateService.get(key);
 
-        expect(getStub).to.have.been.calledOnceWithExactly(getURL, {
+        expect(getStub).toHaveBeenCalledTimes(1);
+        expect(getStub).toHaveBeenCalledWith(getURL, {
             params: new HttpParams().set('key', key),
         });
     });
