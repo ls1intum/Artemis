cca0984655666b3750a5b4dc2559dc692516a07d
Georg Willer <georg.willer@tum.de>
2022-01-02 13:42:26 +0100

    increase test coverage

--- src/test/javascript/spec/component/statistics/statistics-average-score-graph.component.spec.ts
+++ src/test/javascript/spec/component/statistics/statistics-average-score-graph.component.spec.ts
@@ -6,10 +6,14 @@ import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { StatisticsAverageScoreGraphComponent } from 'app/shared/statistics-graph/statistics-average-score-graph.component';
 import { BarChartModule } from '@swimlane/ngx-charts';
 import { ExerciseType } from 'app/entities/exercise.model';
+import { GraphColors } from 'app/entities/statistics.model';
+import { MockRouter } from '../../helpers/mocks/mock-router';
+import { Router } from '@angular/router';
 
 describe('StatisticsAverageScoreGraphComponent', () => {
     let fixture: ComponentFixture<StatisticsAverageScoreGraphComponent>;
     let component: StatisticsAverageScoreGraphComponent;
+    let router: Router;
 
     const returnValue = [
         { exerciseId: 1, exerciseName: 'BridgePattern', averageScore: 20, exerciseType: ExerciseType.PROGRAMMING },
@@ -27,15 +31,19 @@ describe('StatisticsAverageScoreGraphComponent', () => {
 
     const courseAverageScore = 75;
 
+    const exerciseTypeStrings = ['programming', 'quiz', 'modeling', 'text', 'file-upload'];
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule, RouterTestingModule.withRoutes([]), MockModule(BarChartModule)],
             declarations: [StatisticsAverageScoreGraphComponent, MockPipe(ArtemisTranslatePipe)],
+            providers: [{ provide: Router, useClass: MockRouter }],
         })
             .compileComponents()
             .then(() => {
                 fixture = TestBed.createComponent(StatisticsAverageScoreGraphComponent);
                 component = fixture.componentInstance;
+                router = fixture.debugElement.injector.get(Router);
 
                 component.exerciseAverageScores = returnValue;
                 component.courseAverage = courseAverageScore;
@@ -49,6 +57,20 @@ describe('StatisticsAverageScoreGraphComponent', () => {
             averageExerciseScores.push(returnValue[i].averageScore);
         }
 
+        const exerciseTypes = [ExerciseType.PROGRAMMING, ExerciseType.QUIZ, ExerciseType.MODELING, ExerciseType.TEXT, ExerciseType.FILE_UPLOAD];
+        const expectedColors = [
+            GraphColors.RED,
+            GraphColors.DARK_BLUE,
+            GraphColors.DARK_BLUE,
+            GraphColors.DARK_BLUE,
+            GraphColors.DARK_BLUE,
+            GraphColors.DARK_BLUE,
+            GraphColors.DARK_BLUE,
+            GraphColors.GREEN,
+            GraphColors.RED,
+            GraphColors.RED,
+            GraphColors.DARK_BLUE,
+        ];
         // take first 10 exerciseTitles
         expect(component.barChartLabels).toEqual([
             'BridgePattern',
@@ -64,6 +86,32 @@ describe('StatisticsAverageScoreGraphComponent', () => {
         ]);
         for (let i = 0; i < averageExerciseScores.length; i++) {
             expect(component.ngxData[i].value).toBe(averageExerciseScores[i]);
+            expect(component.ngxData[i].exerciseType).toBe(exerciseTypes[i % 5]);
+            expect(component.ngxData[i].exerciseId).toBe(i + 1);
+            expect(component.ngxColor.domain[i]).toBe(expectedColors[i]);
+        }
+    });
+
+    it('should return the correct type stringification for the tooltips', () => {
+        for (let i = 0; i < 10; i++) {
+            expect(component.convertTypeForTooltip(returnValue[i].exerciseName, returnValue[i].averageScore)).toBe(exerciseTypeStrings[i % 5]);
+        }
+    });
+
+    it('should delegate the user to the correct pages', () => {
+        const routerMock = jest.spyOn(router, 'navigate');
+        component.courseId = 42;
+
+        let event: any;
+        let path: any[];
+        for (let i = 0; i < 10; i++) {
+            event = { name: returnValue[i].exerciseName, value: returnValue[i].averageScore };
+            path = ['course-management', 42, exerciseTypeStrings[i % 5] + '-exercises', returnValue[i].exerciseId, 'exercise-statistics'];
+            if (returnValue[i].exerciseType === ExerciseType.QUIZ) {
+                path[4] = 'quiz-point-statistic';
+            }
+            component.onSelect(event);
+            expect(routerMock).toHaveBeenCalledWith(path);
         }
     });
 
