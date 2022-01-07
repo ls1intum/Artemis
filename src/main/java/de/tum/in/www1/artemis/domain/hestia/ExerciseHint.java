ee5d35aabbe8c0ad3464c2cb6e063642263d5217
Ole Vester <ole.vester@t-online.de>
2022-01-07 10:22:32 +0100

    Codacy Style Issue

--- src/main/java/de/tum/in/www1/artemis/domain/hestia/ExerciseHint.java
+++ src/main/java/de/tum/in/www1/artemis/domain/hestia/ExerciseHint.java
@@ -21,7 +21,7 @@ import de.tum.in.www1.artemis.domain.Exercise;
 @Table(name = "exercise_hint")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "discriminator", discriminatorType = DiscriminatorType.STRING)
-@DiscriminatorValue(value = "H")
+@DiscriminatorValue("H")
 @DiscriminatorOptions(force = true)
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
