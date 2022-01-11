7120dd0f24a7b3ff6993973438135b52d06d80d6
Georg Willer <georg.willer@tum.de>
2022-01-11 13:28:15 +0100

    fix display of partial points

--- src/main/webapp/app/exercises/quiz/manage/statistics/quiz-statistic/quiz-statistic.component.ts
+++ src/main/webapp/app/exercises/quiz/manage/statistics/quiz-statistic/quiz-statistic.component.ts
@@ -12,6 +12,7 @@ import { Authority } from 'app/shared/constants/authority.constants';
 import { QuizStatisticsDirective } from 'app/exercises/quiz/manage/statistics/quiz-statistics.directive';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { calculateMaxScore } from 'app/exercises/quiz/manage/statistics/quiz-statistic/quiz-statistics.utils';
+import { round } from 'app/shared/util/utils';
 
 @Component({
     selector: 'jhi-quiz-statistic',
@@ -126,8 +127,8 @@ export class QuizStatisticComponent extends QuizStatisticsDirective implements O
 
         // add data for the last bar (Average)
         this.backgroundColor.push('#1e3368');
-        this.ratedData.push(this.ratedAverage / this.maxScore);
-        this.unratedData.push(this.unratedAverage / this.maxScore);
+        this.ratedData.push(round(this.ratedAverage / this.maxScore, 2));
+        this.unratedData.push(round(this.unratedAverage / this.maxScore, 2));
 
         // add Text for last label based on the language
         const lastLabel = this.translateService.instant('showStatistic.quizStatistic.average');
