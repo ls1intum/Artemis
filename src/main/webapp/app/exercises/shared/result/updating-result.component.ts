155eb034c250e331ec3d255c63982dceffcac197
Benedikt Fein <fein@fim.uni-passau.de>
2022-01-13 20:21:59 +0100

    add unit tests to make sure this bug doesn’t happen again

--- src/main/webapp/app/exercises/shared/result/updating-result.component.ts
+++ src/main/webapp/app/exercises/shared/result/updating-result.component.ts
@@ -131,6 +131,12 @@ export class UpdatingResultComponent implements OnChanges, OnDestroy {
         return MissingResultInfo.FAILED_PROGRAMMING_SUBMISSION_ONLINE_IDE;
     }
 
+    /**
+     * Checks if a status update should be shown for this submission.
+     *
+     * @param submission for which a status update should be shown.
+     * @private
+     */
     private shouldUpdateSubmissionState(submission?: Submission): boolean {
         // The updating result must ignore submissions that are ungraded if ungraded results should not be shown
         // (otherwise the building animation will be shown even though not relevant).
@@ -144,6 +150,12 @@ export class UpdatingResultComponent implements OnChanges, OnDestroy {
         );
     }
 
+    /**
+     * Updates the shown status based on the given state of a submission.
+     *
+     * @param submissionState the submission is currently in.
+     * @private
+     */
     private updateSubmissionState(submissionState: ProgrammingSubmissionState) {
         this.isBuilding = submissionState === ProgrammingSubmissionState.IS_BUILDING_PENDING_SUBMISSION;
 
