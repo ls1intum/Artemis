f6b741baf312b712e599177d3e2b199b8e78b2f2
Ole Vester <ole.vester@t-online.de>
2022-01-18 19:18:29 +0100

    Repository method for finding solution entries by code hint

--- src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseSolutionEntryRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseSolutionEntryRepository.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import de.tum.in.www1.artemis.domain.hestia.ProgrammingExerciseSolutionEntry;
 import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
@@ -18,4 +20,11 @@ public interface ProgrammingExerciseSolutionEntryRepository extends JpaRepositor
     default ProgrammingExerciseSolutionEntry findByIdElseThrow(long entryId) throws EntityNotFoundException {
         return findById(entryId).orElseThrow(() -> new EntityNotFoundException("Programming Exercise Solution Entry", entryId));
     }
+
+    @Query("""
+            SELECT h.solutionEntries
+            FROM CodeHint h
+            WHERE h.id = :codeHintId
+            """)
+    Set<ProgrammingExerciseSolutionEntry> findByCodeHintId(@Param("codeHintId") Long codeHintId);
 }
