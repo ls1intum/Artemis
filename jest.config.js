0edf76b0b3f1d2c4c78f5067d191bfb972f39cda
Stephan Krusche <krusche@tum.de>
2022-01-19 20:39:42 +0100

    Development: Bump version to 5.5.1

--- jest.config.js
+++ jest.config.js
@@ -52,10 +52,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should be increase to at least 80%
-            statements: 77.4,
-            branches: 65.1,
-            functions: 68.9,
-            lines: 77.0,
+            statements: 78.1,
+            branches: 65.7,
+            functions: 69.6,
+            lines: 77.7,
         },
     },
     setupFilesAfterEnv: ['<rootDir>/src/test/javascript/spec/jest-test-setup.ts', 'jest-extended/all'],
