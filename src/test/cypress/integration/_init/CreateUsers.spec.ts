eb73e61c75944ba39a0c48cf814f134b4a2df034
Julian Hainzlschmid <julian.hainzlschmid@gmail.com>
2022-01-20 18:46:46 +0100

    Enabled user creation with already existing users

--- src/test/cypress/integration/_init/CreateUsers.spec.ts
+++ src/test/cypress/integration/_init/CreateUsers.spec.ts
@@ -2,7 +2,7 @@ import { artemis } from 'src/test/cypress/support/ArtemisTesting';
 
 const users = artemis.users;
 
-it.skip('Creates the required users for cypress', () => {
+it('Creates the required users for cypress', () => {
     cy.login(users.getAdmin());
     // TODO: Find a way to fail the complete test suite immediately if this fails
     users.createRequiredUsers(artemis.requests.courseManagement);
