ac67844f4ad33e6bb93c4e61c57d68435e0b82b0
Georg Willer <georg.willer@tum.de>
2022-01-31 18:23:50 +0100

    move DISTINCT within database call

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
@@ -12,7 +12,6 @@ import java.nio.file.Path;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -406,8 +405,8 @@ public class ExamResource {
         log.debug("REST request to get all exams for Course : {}", courseId);
 
         examAccessService.checkCourseAccessForTeachingAssistantElseThrow(courseId);
-        // We need the exercise groups and exercises for the exam status now. As the database query does not remove duplicates, we have to
-        List<Exam> exams = examRepository.findByCourseIdWithExerciseGroupsAndExercises(courseId).stream().distinct().collect(Collectors.toList());
+        // We need the exercise groups and exercises for the exam status now
+        List<Exam> exams = examRepository.findByCourseIdWithExerciseGroupsAndExercises(courseId);
         examRepository.setNumberOfRegisteredUsersForExams(exams);
         return ResponseEntity.ok(exams);
     }
