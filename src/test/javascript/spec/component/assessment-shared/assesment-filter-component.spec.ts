7a7fee5e4977346702067f2d49d5d5dbcb0a2828
tim_cremer <tim.cremer@tum.de>
2022-02-01 20:49:57 +0100

    improve coverage

---
+++ src/test/javascript/spec/component/assessment-shared/assesment-filter-component.spec.ts
@@ -0,0 +1,56 @@
+import { ArtemisTestModule } from '../../test.module';
+import { AssessmentFiltersComponent } from 'app/assessment/assessment-filters/assessment-filters.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { MockDirective, MockPipe } from 'ng-mocks';
+import { NgModel } from '@angular/forms';
+import { TextSubmission } from 'app/entities/text-submission.model';
+import { SubmissionExerciseType } from 'app/entities/submission.model';
+import { Result } from 'app/entities/result.model';
+
+describe('AssessmentFiltersComponent', () => {
+    let component: AssessmentFiltersComponent;
+    let fixture: ComponentFixture<AssessmentFiltersComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [AssessmentFiltersComponent, MockPipe(ArtemisTranslatePipe), MockDirective(NgModel)],
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(AssessmentFiltersComponent);
+                component = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should update fitler when being called', () => {
+        const textSubmission1 = { id: 23, participation: { exercise: { id: 1 }, id: 2 }, results: {}, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+        const textSubmission2 = { id: 24, participation: { exercise: { id: 1 }, id: 2 }, results: {}, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+        component.submissions = [textSubmission1, textSubmission2];
+        const submissions = [textSubmission1, textSubmission2];
+        component.submissions = submissions;
+        component.updateFilter(component.FilterProp.ALL);
+        expect(component.filterProp).toBe(component.FilterProp.ALL);
+        expect(component.submissions).toBe(submissions);
+    });
+
+    it('emitting the changes should not change the underlying submission list', () => {
+        const textSubmission1 = { id: 23, participation: { exercise: { id: 1 }, id: 2 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+        const textSubmission2 = { id: 24, participation: { exercise: { id: 1 }, id: 2 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+        const submissions = [textSubmission1, textSubmission2];
+        textSubmission1.results = [];
+        textSubmission2.results = [];
+        textSubmission1.results = [new Result()];
+        component.submissions = submissions;
+        const emitSpy = jest.spyOn(component.filterChange, 'emit');
+        component.updateFilter(component.FilterProp.LOCKED);
+        expect(component.filterProp).toBe(component.FilterProp.LOCKED);
+        expect(component.submissions).toBe(submissions);
+        expect(emitSpy).toHaveBeenCalledTimes(1);
+    });
+});
