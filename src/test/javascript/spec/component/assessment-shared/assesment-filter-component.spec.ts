3a46ced70fcf1643010ae033fcee08332ee844c3
tim_cremer <tim.cremer@tum.de>
2022-02-02 15:44:14 +0100

    included the decision branches properly

--- src/test/javascript/spec/component/assessment-shared/assesment-filter-component.spec.ts
+++ src/test/javascript/spec/component/assessment-shared/assesment-filter-component.spec.ts
@@ -5,14 +5,17 @@ import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { MockDirective, MockPipe } from 'ng-mocks';
 import { NgModel } from '@angular/forms';
 import { TextSubmission } from 'app/entities/text-submission.model';
-import { SubmissionExerciseType } from 'app/entities/submission.model';
+import { Submission, SubmissionExerciseType } from 'app/entities/submission.model';
 import { Result } from 'app/entities/result.model';
+import dayjs from 'dayjs/esm';
 
 describe('AssessmentFiltersComponent', () => {
     let component: AssessmentFiltersComponent;
     let fixture: ComponentFixture<AssessmentFiltersComponent>;
-    let textSubmission1: TextSubmission;
-    let textSubmission2: TextSubmission;
+    let textSubmissionWithLock: TextSubmission;
+    let textSubmissionWithoutResult: TextSubmission;
+    let textSubmissionWithResult: TextSubmission;
+    let submissions: Submission[];
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -24,8 +27,10 @@ describe('AssessmentFiltersComponent', () => {
                 fixture = TestBed.createComponent(AssessmentFiltersComponent);
                 component = fixture.componentInstance;
                 fixture.detectChanges();
-                textSubmission1 = { id: 23, participation: { exercise: { id: 1 }, id: 2 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
-                textSubmission2 = { id: 24, participation: { exercise: { id: 1 }, id: 2 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+                textSubmissionWithLock = { id: 23, participation: { exercise: { id: 1 }, id: 2 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+                textSubmissionWithoutResult = { id: 24, participation: { exercise: { id: 1 }, id: 3 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+                textSubmissionWithResult = { id: 25, participation: { exercise: { id: 1 }, id: 4 }, submissionExerciseType: SubmissionExerciseType.TEXT } as TextSubmission;
+                submissions = [textSubmissionWithLock, textSubmissionWithoutResult, textSubmissionWithResult];
             });
     });
     afterEach(() => {
@@ -33,24 +38,30 @@ describe('AssessmentFiltersComponent', () => {
     });
 
     it('should set filter properly when being called', () => {
-        component.submissions = [textSubmission1, textSubmission2];
-        const submissions = [textSubmission1, textSubmission2];
         component.submissions = submissions;
+        const emitSpy = jest.spyOn(component.filterChange, 'emit');
         component.updateFilter(component.FilterProp.ALL);
         expect(component.filterProp).toBe(component.FilterProp.ALL);
         expect(component.submissions).toBe(submissions);
+        expect(emitSpy).toHaveBeenCalledTimes(1);
+        expect(emitSpy).toHaveBeenCalledWith(submissions);
     });
 
-    it('emitting the changes should not change the underlying submission list', () => {
-        const submissions = [textSubmission1, textSubmission2];
-        textSubmission1.results = [];
-        textSubmission2.results = [];
-        textSubmission1.results = [new Result()];
+    it('should filter for the right submissions', () => {
+        textSubmissionWithLock.results = [];
+        textSubmissionWithoutResult.results = [];
+        textSubmissionWithResult.results = [];
+        textSubmissionWithLock.results = [new Result()];
+        const result3 = new Result();
+        result3.completionDate = dayjs().add(5, 'minutes');
+        textSubmissionWithResult.results.push(result3);
         component.submissions = submissions;
+        const expectedResult = [textSubmissionWithLock];
         const emitSpy = jest.spyOn(component.filterChange, 'emit');
         component.updateFilter(component.FilterProp.LOCKED);
         expect(component.filterProp).toBe(component.FilterProp.LOCKED);
         expect(component.submissions).toBe(submissions);
         expect(emitSpy).toHaveBeenCalledTimes(1);
+        expect(emitSpy).toHaveBeenCalledWith(expectedResult);
     });
 });
