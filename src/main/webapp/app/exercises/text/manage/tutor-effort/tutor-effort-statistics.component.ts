d9a487b2baa687e05884905df21a2fd465a88c4e
Georg Willer <georg.willer@tum.de>
2022-02-11 12:29:23 +0100

    highlight distribution median; handle bar clicking; enhance bar tooltip

--- src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.component.ts
+++ src/main/webapp/app/exercises/text/manage/tutor-effort/tutor-effort-statistics.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { TutorEffort } from 'app/entities/tutor-effort.model';
 import { TextExerciseService } from 'app/exercises/text/manage/text-exercise/text-exercise.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TextAssessmentService } from 'app/exercises/text/assess/text-assessment.service';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { PlagiarismAndTutorEffortDirective } from 'app/exercises/shared/plagiarism/plagiarism-run-details/plagiarism-and-tutor-effort.directive';
 import { TranslateService } from '@ngx-translate/core';
 import { median } from 'simple-statistics';
 import { GraphColors } from 'app/entities/statistics.model';
+import { round } from 'app/shared/util/utils';
+
+interface TutorEffortRange {
+    minimumTimeSpent: number;
+    maximumTimeSpent?: number;
+}
 
 @Component({
     selector: 'jhi-text-exercise-tutor-effort-statistics',
@@ -26,7 +32,7 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
     yScaleMax = 10;
     medianValue: number;
 
-    showMedianLegend = true;
+    showMedianLegend = false;
 
     // Distance value representing step difference between chartLabel entries, i.e:. 1-10, 10-20
     readonly bucketSize = 10;
@@ -42,6 +48,7 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
         private textAssessmentService: TextAssessmentService,
         private route: ActivatedRoute,
         private translateService: TranslateService,
+        private router: Router,
     ) {
         super();
         this.translateService.onLangChange.subscribe(() => {
@@ -82,6 +89,7 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
         const avgTemp = this.totalTimeSpent === 0 ? 0 : this.numberOfSubmissions / this.totalTimeSpent;
         this.averageTimeSpent = avgTemp ? Math.round((avgTemp + Number.EPSILON) * 100) / 100 : 0;
         this.distributeEffortToSets();
+        this.ngxData = [];
         this.effortDistribution.forEach((effort, index) => {
             this.ngxData.push({ name: this.ngxChartLabels[index], value: effort });
         });
@@ -114,6 +122,26 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
     }
 
     /**
+     * Handles the click of a user on an arbitrary chart bar.
+     * Delegates the user to the assessment dashboard
+     */
+    onSelect() {
+        this.router.navigate(['/course-management', this.currentCourseId, 'assessment-dashboard', this.currentExerciseId]);
+    }
+
+    /**
+     * Auxiliary method used for tooltip generation
+     * @param label the label of the chart bar
+     * @returns the median assessed submissions for a bar
+     */
+    getMedianAmountOfAssessedSubmissions(label: string): number {
+        const index = this.ngxChartLabels.indexOf(label);
+        const range = this.identifyMinimumAndMaximumTimesSpent(index);
+
+        return this.computeMedianAmountOfAssessedSubmissions(range);
+    }
+
+    /**
      * Determines the upper limit for the y axis
      * @param data the data that should be displayed
      * @private
@@ -122,16 +150,32 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
         this.yScaleMax = Math.max(this.yScaleMax, ...data);
     }
 
+    /**
+     * Auxiliary method that ensures that the chart is instantly translation sensitive
+     * @private
+     */
     private translateLabels() {
         this.xAxisLabel = this.translateService.instant('artemisApp.textExercise.tutorEffortStatistics.minutes');
         this.yAxisLabel = this.translateService.instant('artemisApp.textExercise.tutorEffortStatistics.tutors');
     }
 
+    /**
+     * Auxiliary method that computes and sets the effort median
+     * @private
+     */
     private computeEffortMedian(): number {
+        if (this.tutorEfforts.length === 0) {
+            return 0;
+        }
         const timeSpent = this.tutorEfforts.map((effort) => effort.totalTimeSpentMinutes);
         return median(timeSpent);
     }
 
+    /**
+     * Auxiliary method that determines the Index a tutor effort should be inserted to for statistic calculation
+     * @param timeSpent the time the tutor spent
+     * @private
+     */
     private determineIndex(timeSpent: number): number {
         const BUCKET_LAST_INDEX = this.ngxChartLabels.length - 1;
         const BUCKET_POSITION = timeSpent / this.bucketSize;
@@ -140,12 +184,50 @@ export class TutorEffortStatisticsComponent extends PlagiarismAndTutorEffortDire
         return Math.min(Math.floor(BUCKET_POSITION), BUCKET_LAST_INDEX);
     }
 
+    /**
+     * Sets the color of the bar representing the median to a dark blue in order to highlight it
+     * @param medianValue the median amount of time spent for correcting this exercise
+     * @private
+     */
     private highlightMedian(medianValue: number) {
         const index = this.determineIndex(medianValue);
         if (this.ngxData[index].value > 0) {
             this.ngxColor.domain[index] = GraphColors.DARK_BLUE;
+            this.showMedianLegend = true;
         } else {
             this.showMedianLegend = false;
         }
+    }
+
+    /**
+     * Auxiliary method that computes the median assessed submissions for a bucket
+     * @param range TutorEffortRange representing the borders for the interval of occupation
+     * @returns the median rounded to two decimals
+     * @private
+     */
+    private computeMedianAmountOfAssessedSubmissions(range: TutorEffortRange): number {
+        let filterFunction;
+        if (range.maximumTimeSpent) {
+            filterFunction = (effort: TutorEffort) => effort.totalTimeSpentMinutes >= range.minimumTimeSpent && effort.totalTimeSpentMinutes < range.maximumTimeSpent!;
+        } else {
+            filterFunction = (effort: TutorEffort) => effort.totalTimeSpentMinutes >= range.minimumTimeSpent;
+        }
+        const filteredEfforts = this.tutorEfforts.filter(filterFunction);
+
+        return round(median(filteredEfforts.map((effort) => effort.numberOfSubmissionsAssessed)), 2);
+    }
+
+    /**
+     * Auxiliary method identifying the range of a tutor effort interval
+     * @param index computed index based on the position of the bar in the chart
+     * @returns TutorEffortRange representing the minimum and maximum border of the interval
+     * @private
+     */
+    private identifyMinimumAndMaximumTimesSpent(index: number): TutorEffortRange {
+        const minimumTimesSpentArray = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+        const minimumTimeSpent = minimumTimesSpentArray[index];
+        const maximumTimeSpent = minimumTimeSpent !== 120 ? minimumTimeSpent + 10 : undefined;
+
+        return { minimumTimeSpent, maximumTimeSpent };
     }
 }
