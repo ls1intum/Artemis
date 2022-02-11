a56c2f7189f40f23ea3681682fae5827f350e84f
Patrick Bassner <patrick@bassner.de>
2022-02-11 17:23:21 +0100

    Development: Increase client test coverage for user management (#4688)

--- src/test/javascript/spec/component/admin/user-management.component.spec.ts
+++ src/test/javascript/spec/component/admin/user-management.component.spec.ts
@@ -6,10 +6,10 @@ import { MockAccountService } from '../../helpers/mocks/service/mock-account.ser
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { User } from 'app/core/user/user.model';
-import { of } from 'rxjs';
+import { of, Subscription } from 'rxjs';
 import { AlertComponent } from 'app/shared/alert/alert.component';
 import { AlertErrorComponent } from 'app/shared/alert/alert-error.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, ReactiveFormsModule } from '@angular/forms';
 import { ItemCountComponent } from 'app/shared/pagination/item-count.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
@@ -19,11 +19,16 @@ import { SortDirective } from 'app/shared/sort/sort.directive';
 import { ArtemisTestModule } from '../../test.module';
 import { MockRouter } from '../../helpers/mocks/mock-router';
 import { MockRouterLinkDirective } from '../../helpers/mocks/directive/mock-router-link.directive';
+import { EventManager } from 'app/core/util/event-manager.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('UserManagementComponent', () => {
     let comp: UserManagementComponent;
     let fixture: ComponentFixture<UserManagementComponent>;
-    let service: UserService;
+    let userService: UserService;
+    let accountService: AccountService;
+    let eventManager: EventManager;
+    let httpMock: HttpTestingController;
 
     const route = {
         params: of({ courseId: 123, sort: 'id,desc' }),
@@ -32,7 +37,7 @@ describe('UserManagementComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [ArtemisTestModule, MockModule(ReactiveFormsModule), MockModule(NgbModule)],
+            imports: [ArtemisTestModule, MockModule(ReactiveFormsModule), MockModule(NgbModule), HttpClientTestingModule],
             declarations: [
                 UserManagementComponent,
                 MockComponent(AlertErrorComponent),
@@ -50,19 +55,41 @@ describe('UserManagementComponent', () => {
                 },
                 { provide: AccountService, useClass: MockAccountService },
                 { provide: Router, useClass: MockRouter },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        data: of({
+                            defaultSort: 'name,asc',
+                        }),
+                        queryParamMap: of(
+                            new Map([
+                                ['page', '1'],
+                                ['sort', 'id,asc'],
+                            ]),
+                        ),
+                    },
+                },
             ],
         })
             .compileComponents()
             .then(() => {
                 fixture = TestBed.createComponent(UserManagementComponent);
                 comp = fixture.componentInstance;
-                service = fixture.debugElement.injector.get(UserService);
+                userService = TestBed.inject(UserService);
+                accountService = TestBed.inject(AccountService);
+                eventManager = TestBed.inject(EventManager);
+                httpMock = TestBed.inject(HttpTestingController);
             });
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+        httpMock.verify();
     });
 
     it('should parse the user search result into the correct component state', fakeAsync(() => {
         const headers = new HttpHeaders().append('link', 'link;link').append('X-Total-Count', '1');
-        jest.spyOn(service, 'query').mockReturnValue(
+        jest.spyOn(userService, 'query').mockReturnValue(
             of(
                 new HttpResponse({
                     body: [new User(1)],
@@ -88,7 +115,7 @@ describe('UserManagementComponent', () => {
                 // GIVEN
                 const headers = new HttpHeaders().append('link', 'link;link');
                 const user = new User(123);
-                jest.spyOn(service, 'query').mockReturnValue(
+                jest.spyOn(userService, 'query').mockReturnValue(
                     of(
                         new HttpResponse({
                             body: [user],
@@ -96,17 +123,88 @@ describe('UserManagementComponent', () => {
                         }),
                     ),
                 );
-                jest.spyOn(service, 'update').mockReturnValue(of(new HttpResponse<User>({ status: 200 })));
+                jest.spyOn(userService, 'update').mockReturnValue(of(new HttpResponse<User>({ status: 200 })));
 
                 // WHEN
                 comp.setActive(user, true);
                 tick(1000); // simulate async
 
                 // THEN
-                expect(service.update).toHaveBeenCalledWith({ ...user, activated: true });
-                expect(service.query).toHaveBeenCalledTimes(1);
+                expect(userService.update).toHaveBeenCalledWith({ ...user, activated: true });
+                expect(userService.query).toHaveBeenCalledTimes(1);
                 expect(comp.users && comp.users[0]).toEqual(expect.objectContaining({ id: 123 }));
             }),
         ));
+    });
+
+    it('should searchForm + currentUser and handle navigation on init', fakeAsync(() => {
+        const identitySpy = jest.spyOn(accountService, 'identity');
+        const user = new User(123);
+        const querySpy = jest.spyOn(userService, 'query').mockReturnValue(
+            of(
+                new HttpResponse({
+                    body: [user],
+                }),
+            ),
+        );
+        comp.ngOnInit();
+        tick(1000);
+
+        expect(identitySpy).toHaveBeenCalledTimes(1);
+        expect(comp.currentAccount).toEqual({ id: 99 });
+
+        expect(comp.page).toBe(1);
+        expect(comp.predicate).toBe('id');
+        expect(comp.ascending).toBe(true);
+
+        expect(querySpy).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should destroy the user list subscription on destroy', () => {
+        const object = {};
+        comp.userListSubscription = object as Subscription;
+
+        const destroySpy = jest.spyOn(eventManager, 'destroy').mockImplementation(jest.fn());
+        comp.ngOnDestroy();
+        expect(destroySpy).toHaveBeenCalledTimes(1);
+        expect(destroySpy).toHaveBeenCalledWith(object);
+    });
+
+    it('should return the user id or -1 from trackIdentity', () => {
+        expect(comp.trackIdentity(0, { id: 1 } as User)).toBe(1);
+        expect(comp.trackIdentity(0, { id: undefined } as User)).toBe(-1);
+    });
+
+    it.each([
+        { status: 200, statusText: '' },
+        { status: 400, statusText: 'Delete Failure' },
+    ])('should broadcast after user deletion, or show error', ({ status, statusText }) => {
+        const deleteSpy = jest.spyOn(userService, 'delete');
+        const broadcastSpy = jest.spyOn(eventManager, 'broadcast');
+        let errorText: string | undefined = undefined;
+        comp.dialogError.subscribe((text) => (errorText = text));
+
+        comp.deleteUser('test');
+        expect(deleteSpy).toHaveBeenCalledTimes(1);
+        expect(deleteSpy).toHaveBeenCalledWith('test');
+        const reqD = httpMock.expectOne(SERVER_API_URL + 'api/users/test');
+        reqD.flush(null, { status, statusText });
+
+        if (status === 200) {
+            expect(broadcastSpy).toHaveBeenCalledTimes(1);
+            expect(broadcastSpy).toHaveBeenCalledWith({ name: 'userListModification', content: 'Deleted a user' });
+        } else {
+            expect(broadcastSpy).not.toHaveBeenCalled();
+        }
+        expect(errorText).toInclude(statusText);
+
+        jest.restoreAllMocks();
+    });
+
+    it('should validate user search correctly', () => {
+        expect(comp.validateUserSearch({ value: [] } as AbstractControl)).toBe(null);
+        expect(comp.validateUserSearch({ value: [0] } as AbstractControl)).toEqual({ searchControl: true });
+        expect(comp.validateUserSearch({ value: [0, 0] } as AbstractControl)).toEqual({ searchControl: true });
+        expect(comp.validateUserSearch({ value: [0, 0, 0] } as AbstractControl)).toBe(null);
     });
 });
