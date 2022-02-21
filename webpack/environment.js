3773a4d93d75a4ea641136ae96251bf69c51c8f2
Daniel Crazzolara <crazzolaradaniel@gmail.com>
2022-02-21 18:16:52 +0100

    Added automatic version inference

--- webpack/environment.js
+++ webpack/environment.js
@@ -1,6 +1,35 @@
 module.exports = {
     I18N_HASH: 'generated_hash',
     SERVER_API_URL: '',
-    __VERSION__: process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : 'DEV',
+    __VERSION__: process.env.hasOwnProperty('APP_VERSION') ? process.env.APP_VERSION : inferVersion(),
     __DEBUG_INFO_ENABLED__: false,
 };
+
+/*
+ * Needed for client compilations with docker-compose, where the 'APP_VERSION' property isn't injected by gradle.
+ *
+ * Returns the inferred APP_VERSION from 'build.gradle', or 'DEV' if this couldn't be retrieved
+ */
+function inferVersion() {
+    let version = 'DEV';
+
+    try {
+        const fs = require('fs');
+        const buildGradleFile = 'build.gradle';
+
+        let data = fs.readFileSync(buildGradleFile, 'UTF-8');
+
+        version = data.match(/\nversion\s=\s"(.*)"/);
+
+        if (version) {
+            version = version[1];
+        } else {
+            version = 'DEV';
+        }
+    } catch (error) {
+        console.log("Error while retrieving 'APP_VERSION' property");
+    }
+
+    console.log(version);
+    return version;
+}
