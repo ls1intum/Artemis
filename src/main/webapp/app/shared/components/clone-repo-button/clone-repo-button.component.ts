76551be4ff6f8949b216e914441d88f2f9d02e4d
Michael Allgaier <michael.allgaier@tum.de>
2022-02-27 15:36:53 +0100

    changed button to smaller size

--- src/main/webapp/app/shared/components/clone-repo-button/clone-repo-button.component.ts
+++ src/main/webapp/app/shared/components/clone-repo-button/clone-repo-button.component.ts
@@ -7,7 +7,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { User } from 'app/core/user/user.model';
 import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
 import { LocalStorageService } from 'ngx-webstorage';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'jhi-clone-repo-button',
@@ -39,6 +39,7 @@ export class CloneRepoButtonComponent implements OnInit {
 
     // Icons
     faDownload = faDownload;
+    faLink = faLink;
 
     constructor(
         private translateService: TranslateService,
