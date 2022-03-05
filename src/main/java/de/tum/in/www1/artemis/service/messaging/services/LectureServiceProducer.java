8633ad0e061e923591f765dfcced5f590f3baf9d
Merlin Mehmed <merlinmehmed17@gmail.com>
2022-03-05 18:20:03 +0200

    Add missing java doc. Update broken test

--- src/main/java/de/tum/in/www1/artemis/service/messaging/services/LectureServiceProducer.java
+++ src/main/java/de/tum/in/www1/artemis/service/messaging/services/LectureServiceProducer.java
@@ -38,11 +38,11 @@ public class LectureServiceProducer {
     }
 
     /**
-     * Send a message to Artemis get lecture exercises accessible for the given user and wait for response
+     * Send a message to the Lecture microservice to filter the active attachments for a given user and wait for response.
      *
      * @param lectures the lectures
-     * @param user the user
-     * @return the response from Artemis including set of exercises
+     * @param user     the user
+     * @return the response from the Lecture microservice including set of lectures
      */
     public Set<Lecture> filterActiveAttachments(Set<Lecture> lectures, User user) {
         if (CollectionUtils.isEmpty(lectures) || user == null) {
@@ -67,6 +67,12 @@ public class LectureServiceProducer {
         return filteredLectures;
     }
 
+    /**
+     * Send a message to the Lecture microservice to remove exercise unit from a lecture.
+     *
+     * @param exerciseId the id of the exercise to be removed
+     * @return true or false whether the exercise unit has been successfully removed
+     */
     public boolean removeExerciseUnits(long exerciseId) {
         LOGGER.info("Send message in queue {} with body {}", MessageBrokerConstants.LECTURE_QUEUE_REMOVE_EXERCISE_UNITS, exerciseId);
         String correlationId = Long.toString(exerciseId);
@@ -86,6 +92,12 @@ public class LectureServiceProducer {
         return result;
     }
 
+    /**
+     * Send a message to the Lecture microservice to delete lectures.
+     *
+     * @param course the course which lectures should be deleted
+     * @return true or false whether the exercise unit have been successfully deleted
+     */
     public boolean deleteLecturesOfCourse(Course course) {
         if (course == null || CollectionUtils.isEmpty(course.getLectures())) {
             return true;
