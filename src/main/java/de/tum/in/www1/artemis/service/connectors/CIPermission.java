6dffa0793f79858a08b1e5cafd0e37802d60f2b2
Michael Allgaier <michael.allgaier@tum.de>
2022-03-16 16:23:21 +0100

    Changed naming of CREATEREPOSITORY

--- src/main/java/de/tum/in/www1/artemis/service/connectors/CIPermission.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/CIPermission.java
@@ -3,5 +3,5 @@ package de.tum.in.www1.artemis.service.connectors;
 public enum CIPermission {
     READ, EDIT, CREATE, CREATEREPOSITORY, ADMIN
 
-    // CREATEREPOSITORY: New role necessary since Bamboo V8.1.3
+    // CREATEREPOSITORY: New permission necessary since Bamboo V8.1.3 to create repositories
 }
