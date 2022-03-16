c51612425901f5a312b3a98b034378ff3521f1cf
Michael Allgaier <michael.allgaier@tum.de>
2022-03-16 12:33:41 +0100

    Added CREATEREPOSITORY as CIPermission

--- src/main/java/de/tum/in/www1/artemis/service/connectors/CIPermission.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/CIPermission.java
@@ -1,5 +1,5 @@
 package de.tum.in.www1.artemis.service.connectors;
 
 public enum CIPermission {
-    READ, EDIT, CREATE, ADMIN
+    READ, EDIT, CREATE, CREATEREPRO, ADMIN
 }
--- src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooService.java
@@ -445,6 +445,7 @@ public class BambooService extends AbstractContinuousIntegrationService {
         return switch (permission) {
             case EDIT -> "WRITE";
             case CREATE -> "CREATE";
+            case CREATEREPRO -> "CREATEREPOSITORY";
             case READ -> "READ";
             case ADMIN -> "ADMINISTRATION";
         };
