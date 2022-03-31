dc998ef8ecdec1650103590dfff5679233eba752
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-03-31 10:02:33 +0200

    Revert "Try to activate cache for public resources"
    This reverts commit 6c6eb8f22b4b36d4fbac3c13a86c05a7c74260d5.

--- src/main/java/de/tum/in/www1/artemis/config/PublicResourcesConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/PublicResourcesConfiguration.java
@@ -1,23 +1,13 @@
 package de.tum.in.www1.artemis.config;
 
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import tech.jhipster.config.JHipsterProperties;
-
 @Configuration
 public class PublicResourcesConfiguration implements WebMvcConfigurer {
-
-    private final JHipsterProperties jhipsterProperties;
-
-    public PublicResourcesConfiguration(JHipsterProperties jHipsterProperties) {
-        this.jhipsterProperties = jHipsterProperties;
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -25,9 +15,6 @@ public class PublicResourcesConfiguration implements WebMvcConfigurer {
         // Allowed locations are under $artemisRunDir/public/** and resource/public/**
         final var userDir = System.getProperty("user.dir");
         final var publicFiles = Paths.get(userDir, "public");
-        final var maxAge = jhipsterProperties.getHttp().getCache().getTimeToLiveInDays();
-        registry.addResourceHandler("/public/**").addResourceLocations("file:" + publicFiles + "/", "classpath:public/")
-                .setCacheControl(CacheControl.maxAge(maxAge, TimeUnit.DAYS));
-        ;
+        registry.addResourceHandler("/public/**").addResourceLocations("file:" + publicFiles + "/", "classpath:public/");
     }
 }
