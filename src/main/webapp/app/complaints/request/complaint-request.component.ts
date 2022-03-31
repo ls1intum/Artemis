799ada455bf473424144fbb181946b301a3108e9
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-03-31 12:40:52 +0200

    add missing style url

--- src/main/webapp/app/complaints/request/complaint-request.component.ts
+++ src/main/webapp/app/complaints/request/complaint-request.component.ts
@@ -4,6 +4,7 @@ import { Complaint, ComplaintType } from 'app/entities/complaint.model';
 @Component({
     selector: 'jhi-complaint-request',
     templateUrl: './complaint-request.component.html',
+    styleUrls: ['../complaints.scss'],
 })
 export class ComplaintRequestComponent {
     @Input() complaint: Complaint;
