549ea162178ef823e6ed23f02dbefc3744ddcb9a
Patrick Bassner <patrick@bassner.de>
2022-04-02 19:38:51 +0200

    Fix and extent account service test

--- src/test/javascript/spec/component/exam/manage/student-exams/student-exam.service.spec.ts
+++ src/test/javascript/spec/component/exam/manage/student-exams/student-exam.service.spec.ts
@@ -1,19 +1,32 @@
 import { MockRouter } from '../../../../helpers/mocks/mock-router';
 import { StudentExamService } from 'app/exam/manage/student-exams/student-exam.service';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { AccountService } from 'app/core/auth/account.service';
+import { of } from 'rxjs';
+import { StudentExam } from 'app/entities/student-exam.model';
 
 describe('Student Exam Service', () => {
     let httpClient: any;
     let httpClientPutSpy: any;
     let service: StudentExamService;
+    let accountService: AccountService;
 
     beforeEach(() => {
         httpClient = {
             put: jest.fn(),
+            get: jest.fn(),
+            patch: jest.fn(),
         };
+        accountService = {
+            setAccessRightsForCourse: jest.fn(),
+        } as any as AccountService;
         httpClientPutSpy = jest.spyOn(httpClient, 'put');
-        service = new StudentExamService(new MockRouter() as any as Router, httpClient as HttpClient);
+        service = new StudentExamService(new MockRouter() as any as Router, httpClient as HttpClient, accountService);
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
     });
 
     it('should call correct url if toggling submitted state and unsubmit is false', () => {
@@ -28,5 +41,72 @@ describe('Student Exam Service', () => {
         expect(httpClientPutSpy).toHaveBeenCalledTimes(1);
         expect(httpClientPutSpy.mock.calls[0]).toHaveLength(3);
         expect(httpClientPutSpy.mock.calls[0][0]).toContain('toggle-to-unsubmitted');
+    });
+
+    it.each([
+        {
+            exam: {
+                course: {
+                    id: 1,
+                },
+            },
+        },
+        {
+            exam: {
+                course: undefined,
+            },
+        },
+        {
+            exam: undefined,
+        },
+        undefined,
+    ])('should fetch and process an exam correctly on find', (payload) => {
+        const response = new HttpResponse<StudentExam>({ body: payload });
+        const getSpy = jest.spyOn(httpClient, 'get').mockReturnValue(of(response));
+
+        let returnedExam;
+        service.find(1, 2, 3).subscribe((result) => (returnedExam = result));
+
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(getSpy).toHaveBeenCalledWith(`${SERVER_API_URL}api/courses/1/exams/2/student-exams/3`, { observe: 'response' });
+        expect(returnedExam).toBe(response);
+        expect(accountService.setAccessRightsForCourse).toHaveBeenCalledTimes(payload?.exam?.course ? 1 : 0);
+    });
+
+    it('should fetch and process exams correctly on findAllForExam', () => {
+        const payload = [
+            {
+                exam: {
+                    course: {
+                        id: 1,
+                    },
+                },
+            },
+            {
+                exam: {
+                    course: {
+                        id: 1,
+                    },
+                },
+            },
+            {
+                exam: {
+                    course: undefined,
+                },
+            },
+            {
+                exam: undefined,
+            },
+        ] as StudentExam[];
+        const response = new HttpResponse<StudentExam[]>({ body: payload });
+        const getSpy = jest.spyOn(httpClient, 'get').mockReturnValue(of(response));
+
+        let returnedExams;
+        service.findAllForExam(1, 2).subscribe((result) => (returnedExams = result));
+
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(getSpy).toHaveBeenCalledWith(`${SERVER_API_URL}api/courses/1/exams/2/student-exams`, { observe: 'response' });
+        expect(returnedExams).toBe(response);
+        expect(accountService.setAccessRightsForCourse).toHaveBeenCalledTimes(2);
     });
 });
