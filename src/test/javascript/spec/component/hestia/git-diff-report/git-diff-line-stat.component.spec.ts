b87faab82dd5e155f830379e88eac83dad03927a
Timor Morrien <timor.morrien@tum.de>
2022-04-06 01:52:31 +0200

    Improve line stat test

--- src/test/javascript/spec/component/hestia/git-diff-report/git-diff-line-stat.component.spec.ts
+++ src/test/javascript/spec/component/hestia/git-diff-report/git-diff-line-stat.component.spec.ts
@@ -18,55 +18,20 @@ describe('Git-Diff line-stat Component', () => {
         comp = fixture.componentInstance;
     });
 
-    function checkBoxesCount() {
-        comp.addedLineCount = 100;
-        comp.removedLineCount = 0;
-        comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(5);
-        expect(comp.removedSquareCount).toBe(0);
-    }
+    const boxesTestTable = [
+        [5, 0, 100, 0],
+        [4, 1, 8, 2],
+        [3, 2, 6, 4],
+        [2, 3, 4, 6],
+        [1, 4, 2, 8],
+        [0, 5, 0, 100],
+    ];
 
-    it('Should show 5-0 boxes', () => {
-        checkBoxesCount();
-    });
-
-    it('Should show 4-1 boxes', () => {
-        comp.addedLineCount = 8;
-        comp.removedLineCount = 2;
+    it.each(boxesTestTable)('Should show %s-%s boxes', (expectedAddedSquareCount, expectedRemovedSquareCount, addedLineCount, removedLineCount) => {
+        comp.addedLineCount = addedLineCount;
+        comp.removedLineCount = removedLineCount;
         comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(4);
-        expect(comp.removedSquareCount).toBe(1);
-    });
-
-    it('Should show 3-2 boxes', () => {
-        comp.addedLineCount = 6;
-        comp.removedLineCount = 4;
-        comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(3);
-        expect(comp.removedSquareCount).toBe(2);
-    });
-
-    it('Should show 2-3 boxes', () => {
-        comp.addedLineCount = 4;
-        comp.removedLineCount = 6;
-        comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(2);
-        expect(comp.removedSquareCount).toBe(3);
-    });
-
-    it('Should show 1-4 boxes', () => {
-        comp.addedLineCount = 2;
-        comp.removedLineCount = 8;
-        comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(1);
-        expect(comp.removedSquareCount).toBe(4);
-    });
-
-    it('Should show 0-5 boxes', () => {
-        comp.addedLineCount = 0;
-        comp.removedLineCount = 100;
-        comp.ngOnInit();
-        expect(comp.addedSquareCount).toBe(0);
-        expect(comp.removedSquareCount).toBe(5);
+        expect(comp.addedSquareCount).toBe(expectedAddedSquareCount);
+        expect(comp.removedSquareCount).toBe(expectedRemovedSquareCount);
     });
 });
