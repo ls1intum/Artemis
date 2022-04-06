e30327c1a21c727c758e7f0605fd680ea160b0b7
akesfeden <atakesfeden@gmail.com>
2022-04-06 17:16:11 +0200

    Add client tests for IntervalGradingSystemComponent.

---
+++ src/test/javascript/spec/component/grading-system/interval-grading-system.component.spec.ts
@@ -0,0 +1,302 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IntervalGradingSystemComponent } from 'app/grading-system/interval-grading-system/interval-grading-system.component';
+import { GradingSystemService } from 'app/grading-system/grading-system.service';
+import { ArtemisTestModule } from '../../test.module';
+import { GradeType, GradingScale } from 'app/entities/grading-scale.model';
+import { MockComponent, MockDirective, MockPipe, MockProvider } from 'ng-mocks';
+import { DeleteButtonDirective } from 'app/shared/delete-dialog/delete-button.directive';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { GradingSystemInfoModalComponent } from 'app/grading-system/grading-system-info-modal/grading-system-info-modal.component';
+import { NgModel, NgSelectOption } from '@angular/forms';
+import { GradeStep } from 'app/entities/grade-step.model';
+import { cloneDeep } from 'lodash-es';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Exam } from 'app/entities/exam.model';
+import { Course } from 'app/entities/course.model';
+import { ExamManagementService } from 'app/exam/manage/exam-management.service';
+import { CourseManagementService } from 'app/course/manage/course-management.service';
+import { MockCourseManagementService } from '../../helpers/mocks/service/mock-course-management.service';
+import { ModePickerComponent } from 'app/exercises/shared/mode-picker/mode-picker.component';
+
+const generateCsv = jest.fn();
+
+jest.mock('export-to-csv', () => ({
+    ExportToCsv: jest.fn().mockImplementation(() => ({
+        generateCsv,
+    })),
+}));
+
+describe('Interval Grading System Component', () => {
+    let comp: IntervalGradingSystemComponent;
+    let fixture: ComponentFixture<IntervalGradingSystemComponent>;
+    let gradingSystemService: GradingSystemService;
+    let translateService: TranslateService;
+    let translateStub: jest.SpyInstance;
+    let examService: ExamManagementService;
+
+    const route = { parent: { params: of({ courseId: 1, examId: 1 }) } } as any as ActivatedRoute;
+
+    const gradeStep1: GradeStep = {
+        gradeName: 'Fail',
+        lowerBoundPercentage: 0,
+        upperBoundPercentage: 40,
+        lowerBoundInclusive: true,
+        upperBoundInclusive: false,
+        isPassingGrade: false,
+    };
+    const gradeStep2: GradeStep = {
+        gradeName: 'Pass',
+        lowerBoundPercentage: 40,
+        upperBoundPercentage: 65,
+        lowerBoundInclusive: true,
+        upperBoundInclusive: false,
+        isPassingGrade: true,
+    };
+    const gradeStep3: GradeStep = {
+        gradeName: 'Excellent',
+        lowerBoundPercentage: 65,
+        upperBoundPercentage: 100,
+        lowerBoundInclusive: true,
+        upperBoundInclusive: true,
+        isPassingGrade: true,
+    };
+    const gradeSteps = [gradeStep1, gradeStep2, gradeStep3];
+
+    const exam = new Exam();
+    exam.maxPoints = 100;
+    const course = new Course();
+    course.maxPoints = 100;
+
+    function validateGradeStepBounds(actualGradeStepRow: any, percentageLowerBound: number, percentageUpperBound: number, maxPoints: number) {
+        expect(actualGradeStepRow.lowerBoundPercentage).toBe(percentageLowerBound);
+        expect(actualGradeStepRow.upperBoundPercentage).toBe(percentageUpperBound);
+
+        const multiplier = maxPoints / 100;
+        expect(actualGradeStepRow.lowerBoundPoints).toBe(percentageLowerBound * multiplier);
+        expect(actualGradeStepRow.upperBoundPoints).toBe(percentageUpperBound * multiplier);
+    }
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [
+                MockDirective(NgModel),
+                MockDirective(NgSelectOption),
+                IntervalGradingSystemComponent,
+                MockComponent(GradingSystemInfoModalComponent),
+                MockComponent(ModePickerComponent),
+                MockDirective(DeleteButtonDirective),
+                MockPipe(ArtemisTranslatePipe),
+            ],
+            providers: [
+                { provide: ActivatedRoute, useValue: route },
+                MockProvider(ExamManagementService),
+                { provide: CourseManagementService, useClass: MockCourseManagementService },
+            ],
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(IntervalGradingSystemComponent);
+                comp = fixture.componentInstance;
+
+                gradingSystemService = TestBed.inject(GradingSystemService);
+                examService = TestBed.inject(ExamManagementService);
+                translateService = TestBed.inject(TranslateService);
+
+                comp.gradingScale = new GradingScale();
+                comp.gradingScale.gradeSteps = cloneDeep(gradeSteps);
+                comp.courseId = 123;
+                comp.examId = 456;
+                comp.firstPassingGrade = 'Pass';
+                translateStub = jest.spyOn(translateService, 'instant');
+            });
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should generate default grading scale', () => {
+        comp.generateDefaultGradingScale();
+
+        expect(comp.gradingScale.gradeType).toStrictEqual(GradeType.GRADE);
+        expect(comp.firstPassingGrade).toStrictEqual('4.0');
+        expect(comp.lowerBoundInclusivity).toBe(true);
+        expect(comp.gradingScale.gradeSteps).toHaveLength(13);
+        comp.gradingScale.gradeSteps.forEach((gradeStep) => {
+            expect(gradeStep.id).toBe(undefined);
+            expect(gradeStep.gradeName).not.toBe(undefined);
+
+            // No need to test parts that are intrinsic to DetailedGradingSystemComponent.
+
+            expect(comp.getPercentageInterval(gradeStep)).toBe(gradeStep.upperBoundPercentage - gradeStep.lowerBoundPercentage);
+        });
+    });
+
+    it('should generate default grading scale with max points set', () => {
+        const maxPoints = 200;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.generateDefaultGradingScale();
+
+        comp.gradingScale.gradeSteps.forEach((gradeStep) => {
+            expect(comp.getPointsInterval(gradeStep)).toBe(gradeStep.upperBoundPoints! - gradeStep.lowerBoundPoints!);
+        });
+    });
+
+    it('should delete grade step', () => {
+        const maxPoints = 200;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.deleteGradeStep(1);
+
+        expect(comp.gradingScale.gradeSteps).toHaveLength(2);
+        expect(comp.gradingScale.gradeSteps).not.toContain(gradeStep2);
+
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[0], 0, 40, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[1], 40, 75, maxPoints);
+    });
+
+    it('should create grade step', () => {
+        comp.lowerBoundInclusivity = true;
+
+        comp.createGradeStep();
+
+        expect(comp.gradingScale.gradeSteps).toHaveLength(4);
+
+        const newGradeStep = comp.gradingScale.gradeSteps[3];
+        expect(newGradeStep.id).toBe(undefined);
+        expect(newGradeStep.gradeName).toStrictEqual('');
+        expect(newGradeStep.lowerBoundPercentage).toBe(100);
+        expect(newGradeStep.upperBoundPercentage).toBe(100);
+        expect(newGradeStep.isPassingGrade).toBe(true);
+        expect(newGradeStep.lowerBoundInclusive).toBe(true);
+        expect(newGradeStep.upperBoundInclusive).toBe(true);
+
+        // Previous gradeStep.upperBoundPercentage is already 100.
+        expect(comp.getPercentageInterval(newGradeStep)).toBe(0);
+    });
+
+    it('should delete grade names correctly', () => {
+        comp.deleteGradeNames();
+
+        comp.gradingScale.gradeSteps.forEach((gradeStep) => {
+            expect(gradeStep.gradeName).toStrictEqual('');
+        });
+    });
+
+    it('should set all grade step percentage intervals correctly', () => {
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[0])).toBe(40);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[1])).toBe(25);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[2])).toBe(35);
+    });
+
+    it('should set all grade step point intervals correctly', () => {
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(undefined);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(undefined);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(undefined);
+
+        const multiplier = 2;
+        const maxPoints = multiplier * 100;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(40 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(25 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(35 * multiplier);
+
+        comp.onChangeMaxPoints(-10);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(undefined);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(undefined);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(undefined);
+    });
+
+    it('should cascade percentage increase', () => {
+        const multiplier = 2;
+        const maxPoints = multiplier * 100;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.setPercentageInterval(1, 50);
+
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[0])).toBe(40);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[1])).toBe(50);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[2])).toBe(35);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(40 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(50 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(35 * multiplier);
+
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[0], 0, 40, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[1], 40, 90, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[2], 90, 125, maxPoints);
+    });
+
+    it('should cascade percentage decrease', () => {
+        const multiplier = 2;
+        const maxPoints = multiplier * 100;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.setPercentageInterval(1, 10);
+
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[0])).toBe(40);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[1])).toBe(10);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[2])).toBe(35);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(40 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(10 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(35 * multiplier);
+
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[0], 0, 40, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[1], 40, 50, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[2], 50, 85, maxPoints);
+    });
+
+    it('should cascade points increase', () => {
+        const multiplier = 2;
+        const maxPoints = multiplier * 100;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.setPointsInterval(1, 50 * multiplier);
+
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[0])).toBe(40);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[1])).toBe(50);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[2])).toBe(35);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(40 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(50 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(35 * multiplier);
+
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[0], 0, 40, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[1], 40, 90, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[2], 90, 125, maxPoints);
+    });
+
+    it('should cascade points decrease', () => {
+        const multiplier = 2;
+        const maxPoints = multiplier * 100;
+        comp.maxPoints = maxPoints;
+        comp.onChangeMaxPoints(maxPoints);
+
+        comp.setPointsInterval(1, 10 * multiplier);
+
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[0])).toBe(40);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[1])).toBe(10);
+        expect(comp.getPercentageInterval(comp.gradingScale.gradeSteps[2])).toBe(35);
+
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[0])).toBe(40 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[1])).toBe(10 * multiplier);
+        expect(comp.getPointsInterval(comp.gradingScale.gradeSteps[2])).toBe(35 * multiplier);
+
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[0], 0, 40, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[1], 40, 50, maxPoints);
+        validateGradeStepBounds(comp.gradingScale.gradeSteps[2], 50, 85, maxPoints);
+    });
+});
