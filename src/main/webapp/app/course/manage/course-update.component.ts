fe9f7aeafa35a2dc8bc2b210a0fb64ad8bfac39a
Dominik Weinzierl <60005702+Dominik-Weinzierl@users.noreply.github.com>
2022-04-07 21:44:00 +0200

    General: Allow instructors to configure variable complaint text limits per course (#4810)

--- src/main/webapp/app/course/manage/course-update.component.ts
+++ src/main/webapp/app/course/manage/course-update.component.ts
@@ -56,6 +56,11 @@ export class CourseUpdateComponent implements OnInit {
     faQuestionCircle = faQuestionCircle;
     faExclamationTriangle = faExclamationTriangle;
 
+    // NOTE: These constants are used to define the maximum length of complaints and complaint responses.
+    // This is the maximum value allowed in our database. These values must be the same as in Constants.java
+    readonly COMPLAINT_RESPONSE_TEXT_LIMIT = 5000;
+    readonly COMPLAINT_TEXT_LIMIT = 5000;
+
     constructor(
         private courseService: CourseManagementService,
         private activatedRoute: ActivatedRoute,
@@ -80,7 +85,11 @@ export class CourseUpdateComponent implements OnInit {
                 });
 
                 // complaints are only enabled when at least one complaint is allowed and the complaint duration is positive
-                this.complaintsEnabled = (this.course.maxComplaints! > 0 || this.course.maxTeamComplaints! > 0) && this.course.maxComplaintTimeDays! > 0;
+                this.complaintsEnabled =
+                    (this.course.maxComplaints! > 0 || this.course.maxTeamComplaints! > 0) &&
+                    this.course.maxComplaintTimeDays! > 0 &&
+                    this.course.maxComplaintTextLimit! > 0 &&
+                    this.course.maxComplaintResponseTextLimit! > 0;
                 this.requestMoreFeedbackEnabled = this.course.maxRequestMoreFeedbackTimeDays! > 0;
             }
         });
@@ -147,6 +156,12 @@ export class CourseUpdateComponent implements OnInit {
                 }),
                 maxComplaintTimeDays: new FormControl(this.course.maxComplaintTimeDays, {
                     validators: [Validators.required, Validators.min(0)],
+                }),
+                maxComplaintTextLimit: new FormControl(this.course.maxComplaintTextLimit, {
+                    validators: [Validators.required, Validators.min(0), Validators.max(this.COMPLAINT_TEXT_LIMIT)],
+                }),
+                maxComplaintResponseTextLimit: new FormControl(this.course.maxComplaintResponseTextLimit, {
+                    validators: [Validators.required, Validators.min(0), Validators.max(this.COMPLAINT_RESPONSE_TEXT_LIMIT)],
                 }),
                 maxRequestMoreFeedbackTimeDays: new FormControl(this.course.maxRequestMoreFeedbackTimeDays, {
                     validators: [Validators.required, Validators.min(0)],
@@ -341,11 +356,15 @@ export class CourseUpdateComponent implements OnInit {
             this.courseForm.controls['maxComplaints'].setValue(3);
             this.courseForm.controls['maxTeamComplaints'].setValue(3);
             this.courseForm.controls['maxComplaintTimeDays'].setValue(7);
+            this.courseForm.controls['maxComplaintTextLimit'].setValue(2000);
+            this.courseForm.controls['maxComplaintResponseTextLimit'].setValue(2000);
         } else {
             this.complaintsEnabled = false;
             this.courseForm.controls['maxComplaints'].setValue(0);
             this.courseForm.controls['maxTeamComplaints'].setValue(0);
             this.courseForm.controls['maxComplaintTimeDays'].setValue(0);
+            this.courseForm.controls['maxComplaintTextLimit'].setValue(0);
+            this.courseForm.controls['maxComplaintResponseTextLimit'].setValue(0);
         }
     }
 
