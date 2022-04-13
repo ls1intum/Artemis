9fdff536598b6bcbaf5a214926cb8976f5aba745
Timor Morrien <timor.morrien@tum.de>
2022-04-13 17:02:11 +0200

    Improve structural solution entry generation

--- src/main/java/de/tum/in/www1/artemis/service/hestia/structural/StructuralClass.java
+++ src/main/java/de/tum/in/www1/artemis/service/hestia/structural/StructuralClass.java
@@ -1,14 +1,18 @@
 package de.tum.in.www1.artemis.service.hestia.structural;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.thoughtworks.qdox.model.JavaClass;
 
 /**
  * Element of the test.json file representing the properties of a class
  * Used for the generation of solution entries for structural test cases
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-class StructuralClass {
+class StructuralClass implements StructuralElement {
 
     @JsonProperty(required = true)
     private String name;
@@ -18,7 +22,7 @@ class StructuralClass {
 
     private String superclass;
 
-    private String[] modifiers;
+    private List<String> modifiers = new ArrayList<>();
 
     @JsonProperty(defaultValue = "false")
     private boolean isInterface;
@@ -26,7 +30,47 @@ class StructuralClass {
     @JsonProperty(defaultValue = "false")
     private boolean isEnum;
 
-    private String[] interfaces;
+    private List<String> interfaces = new ArrayList<>();
+
+    private List<String> annotations = new ArrayList<>();
+
+    @Override
+    public String getSourceCode(StructuralClassElements structuralClassElements, JavaClass solutionClass) {
+        String classSolutionCode = "package " + this.getPackageName() + ";\n\n";
+
+        if (!this.getAnnotations().isEmpty()) {
+            classSolutionCode += getAnnotationsString(this.getAnnotations(), solutionClass != null ? solutionClass.getAnnotations() : null);
+        }
+
+        // Class header (modifiers, name & generics)
+        if (!this.getModifiers().isEmpty()) {
+            classSolutionCode += formatModifiers(this.getModifiers()) + " ";
+        }
+        classSolutionCode += (this.isInterface() ? "interface" : (this.isEnum() ? "enum" : "class")) + " ";
+        classSolutionCode += this.getName();
+        if (solutionClass != null && !solutionClass.getTypeParameters().isEmpty()) {
+            classSolutionCode += getGenericTypesString(solutionClass.getTypeParameters());
+        }
+        classSolutionCode += " ";
+
+        // Inheritance
+        if (this.getSuperclass() != null) {
+            classSolutionCode += "extends " + this.getSuperclass() + " ";
+        }
+        if (!this.getInterfaces().isEmpty()) {
+            classSolutionCode += "implements " + String.join(", ", this.getInterfaces()) + " ";
+        }
+
+        // Class Body
+        classSolutionCode += "{\n";
+        classSolutionCode += SINGLE_INDENTATION;
+        if (this.isEnum()) {
+            classSolutionCode += String.join(", ", structuralClassElements.getEnumValues());
+        }
+        classSolutionCode += "\n}";
+
+        return classSolutionCode;
+    }
 
     public String getName() {
         return name;
@@ -68,19 +112,27 @@ class StructuralClass {
         isEnum = anEnum;
     }
 
-    public String[] getInterfaces() {
+    public List<String> getInterfaces() {
         return interfaces;
     }
 
-    public void setInterfaces(String[] interfaces) {
+    public void setInterfaces(List<String> interfaces) {
         this.interfaces = interfaces;
     }
 
-    public String[] getModifiers() {
+    public List<String> getModifiers() {
         return modifiers;
     }
 
-    public void setModifiers(String[] modifiers) {
+    public void setModifiers(List<String> modifiers) {
         this.modifiers = modifiers;
+    }
+
+    public List<String> getAnnotations() {
+        return annotations;
+    }
+
+    public void setAnnotations(List<String> annotations) {
+        this.annotations = annotations;
     }
 }
--- src/main/java/de/tum/in/www1/artemis/service/hestia/structural/StructuralMethod.java
+++ src/main/java/de/tum/in/www1/artemis/service/hestia/structural/StructuralMethod.java
@@ -1,26 +1,101 @@
 package de.tum.in.www1.artemis.service.hestia.structural;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Lists;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaMethod;
+import com.thoughtworks.qdox.model.JavaParameter;
 
 /**
  * Element of the test.json file representing the properties of a method of a class
  * Used for the generation of solution entries for structural test cases
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-class StructuralMethod {
+class StructuralMethod implements StructuralElement {
 
     @JsonProperty(required = true)
     private String name;
 
-    private String[] modifiers;
+    private List<String> modifiers = new ArrayList<>();
 
-    private String[] parameters;
+    private List<String> parameters = new ArrayList<>();
 
-    private String[] annotations;
+    private List<String> annotations = new ArrayList<>();
 
     @JsonProperty(defaultValue = "void")
     private String returnType;
+
+    @Override
+    public String getSourceCode(StructuralClassElements structuralClassElements, JavaClass solutionClass) {
+        JavaMethod solutionMethod = getSolutionMethod(solutionClass);
+        String methodSolutionCode = "";
+
+        if (!this.getAnnotations().isEmpty()) {
+            methodSolutionCode += getAnnotationsString(this.getAnnotations(), solutionMethod != null ? solutionMethod.getAnnotations() : null);
+        }
+
+        var modifiers = Lists.newArrayList(this.getModifiers());
+        var isAbstract = modifiers.contains("abstract");
+        // Adjust modifiers for interfaces
+        if (structuralClassElements.getStructuralClass().isInterface()) {
+            if (isAbstract) {
+                modifiers.remove("abstract");
+            }
+            else {
+                modifiers.add(0, "default");
+            }
+        }
+        if (!modifiers.isEmpty()) {
+            methodSolutionCode += formatModifiers(modifiers) + " ";
+        }
+
+        List<JavaParameter> solutionParameters = solutionMethod == null ? Collections.emptyList() : solutionMethod.getParameters();
+        if (solutionMethod != null && !solutionMethod.getTypeParameters().isEmpty()) {
+            methodSolutionCode += getGenericTypesString(solutionMethod.getTypeParameters()) + " ";
+        }
+
+        if (solutionMethod != null) {
+            methodSolutionCode += solutionMethod.getReturnType().getGenericValue() + " ";
+        }
+        else {
+            methodSolutionCode += this.getReturnType() + " ";
+        }
+
+        methodSolutionCode += this.getName();
+
+        methodSolutionCode += generateParametersString(this.getParameters(), solutionParameters);
+        // Remove the method body if the method is abstract
+        if (isAbstract) {
+            methodSolutionCode += ";";
+        }
+        else {
+            methodSolutionCode += " {\n" + SINGLE_INDENTATION + "\n}";
+        }
+
+        return methodSolutionCode;
+    }
+
+    /**
+     * Finds the QDox method in a given class by its test.json specification
+     *
+     * @param solutionClass The QDox class instance
+     * @return The QDox method instance or null if not found
+     */
+    private JavaMethod getSolutionMethod(JavaClass solutionClass) {
+        if (solutionClass == null) {
+            return null;
+        }
+        return solutionClass.getMethods().stream().filter(javaMethod -> javaMethod.getName().equals(this.getName())).filter(javaMethod -> {
+            var genericTypes = new ArrayList<>(solutionClass.getTypeParameters());
+            genericTypes.addAll(javaMethod.getTypeParameters());
+            return doParametersMatch(this.getParameters(), javaMethod.getParameters(), genericTypes);
+        }).findFirst().orElse(null);
+    }
 
     public String getName() {
         return name;
@@ -30,27 +105,27 @@ class StructuralMethod {
         this.name = name;
     }
 
-    public String[] getModifiers() {
+    public List<String> getModifiers() {
         return modifiers;
     }
 
-    public void setModifiers(String[] modifiers) {
+    public void setModifiers(List<String> modifiers) {
         this.modifiers = modifiers;
     }
 
-    public String[] getParameters() {
+    public List<String> getParameters() {
         return parameters;
     }
 
-    public void setParameters(String[] parameters) {
+    public void setParameters(List<String> parameters) {
         this.parameters = parameters;
     }
 
-    public String[] getAnnotations() {
+    public List<String> getAnnotations() {
         return annotations;
     }
 
-    public void setAnnotations(String[] annotations) {
+    public void setAnnotations(List<String> annotations) {
         this.annotations = annotations;
     }
 
