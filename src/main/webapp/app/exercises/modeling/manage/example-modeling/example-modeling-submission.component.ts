2966dbace622d63bd5f10322dae16e0ed332cd17
akesfeden <atakesfeden@gmail.com>
2022-04-24 15:20:34 +0200

    Delete feedback for deleted references when UML elements and associations change in ExampleModelingSubmissionComponent.

--- src/main/webapp/app/exercises/modeling/manage/example-modeling/example-modeling-submission.component.ts
+++ src/main/webapp/app/exercises/modeling/manage/example-modeling/example-modeling-submission.component.ts
@@ -14,7 +14,7 @@ import { ModelingAssessmentService } from 'app/exercises/modeling/assess/modelin
 import { ModelingSubmission } from 'app/entities/modeling-submission.model';
 import { ModelingExercise } from 'app/entities/modeling-exercise.model';
 import { ModelingAssessmentComponent } from 'app/exercises/modeling/assess/modeling-assessment.component';
-import { concatMap, map, tap } from 'rxjs/operators';
+import { catchError, concatMap, map, tap } from 'rxjs/operators';
 import { getLatestSubmissionResult, setLatestSubmissionResult } from 'app/entities/submission.model';
 import { getPositiveAndCappedTotalScore } from 'app/exercises/shared/exercise/exercise.utils';
 import { onError } from 'app/shared/util/global.utils';
@@ -25,6 +25,7 @@ import { Course } from 'app/entities/course.model';
 import { faChalkboardTeacher, faCheck, faCircle, faCodeBranch, faExclamation, faExclamationTriangle, faInfoCircle, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ArtemisNavigationUtilService } from 'app/utils/navigation.utils';
 import { forkJoin } from 'rxjs';
+import { filterInvalidFeedback } from 'app/exercises/modeling/assess/modeling-assessment.util';
 
 @Component({
     selector: 'jhi-example-modeling-submission',
@@ -185,8 +186,7 @@ export class ExampleModelingSubmissionComponent implements OnInit, FeedbackMarke
         if (this.isNewSubmission) {
             this.createNewExampleModelingSubmission();
         } else {
-            this.updateExampleModelingSubmission();
-            this.updateAssessmentExplanationAndExampleAssessment();
+            this.updateExampleModelingSubmission().subscribe(() => this.updateAssessmentExplanationAndExampleAssessment());
         }
     }
 
@@ -230,11 +230,13 @@ export class ExampleModelingSubmissionComponent implements OnInit, FeedbackMarke
         if (!this.modelingSubmission) {
             this.createNewExampleModelingSubmission();
         }
-        this.modelingSubmission.model = JSON.stringify(this.modelingEditor.getCurrentModel());
+        const currentModel = this.modelingEditor.getCurrentModel();
+        this.modelingSubmission.model = JSON.stringify(currentModel);
 
         this.modelingSubmission.explanationText = this.explanationText;
         this.modelingSubmission.exampleSubmission = true;
         if (this.result) {
+            this.referencedFeedback = filterInvalidFeedback(this.referencedFeedback, currentModel);
             this.result.feedbacks = this.assessments;
             setLatestSubmissionResult(this.modelingSubmission, this.result);
             delete this.result.submission;
@@ -244,8 +246,8 @@ export class ExampleModelingSubmissionComponent implements OnInit, FeedbackMarke
         exampleSubmission.submission = this.modelingSubmission;
         exampleSubmission.exercise = this.exercise;
         exampleSubmission.usedForTutorial = this.usedForTutorial;
-        this.exampleSubmissionService.update(exampleSubmission, this.exerciseId).subscribe({
-            next: (exampleSubmissionResponse: HttpResponse<ExampleSubmission>) => {
+        return this.exampleSubmissionService.update(exampleSubmission, this.exerciseId).pipe(
+            tap((exampleSubmissionResponse: HttpResponse<ExampleSubmission>) => {
                 this.exampleSubmission = exampleSubmissionResponse.body!;
                 this.exampleSubmissionId = this.exampleSubmission.id!;
                 if (this.exampleSubmission.submission) {
@@ -260,11 +262,12 @@ export class ExampleModelingSubmissionComponent implements OnInit, FeedbackMarke
                 this.isNewSubmission = false;
 
                 this.alertService.success('artemisApp.modelingEditor.saveSuccessful');
-            },
-            error: (error: HttpErrorResponse) => {
+            }),
+            catchError((error: HttpErrorResponse) => {
                 onError(this.alertService, error);
-            },
-        });
+                throw error;
+            }),
+        );
     }
 
     onReferencedFeedbackChanged(referencedFeedback: Feedback[]) {
@@ -281,7 +284,7 @@ export class ExampleModelingSubmissionComponent implements OnInit, FeedbackMarke
 
     showAssessment() {
         if (this.modelChanged()) {
-            this.updateExampleModelingSubmission();
+            this.updateExampleModelingSubmission().subscribe();
         }
         this.assessmentMode = true;
     }
