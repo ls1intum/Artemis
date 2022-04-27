688e25fa464410ad8eebfbe6babad54c46e741d7
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-04-27 14:37:28 +0200

    update style

--- src/main/webapp/app/complaints/shared/complaints-textarea-counter.component.ts
+++ src/main/webapp/app/complaints/shared/complaints-textarea-counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { StringCountService } from 'app/exercises/text/participate/string-count.service';
 
 @Component({
     selector: 'jhi-complaints-textarea-counter',
@@ -9,4 +10,10 @@ export class ComplaintsTextAreaCounterComponent {
     @Input() maxLength: number;
     @Input() content?: string;
     @Input() visible?: boolean;
+
+    constructor(private stringCountService: StringCountService) {}
+
+    get characterCount(): number {
+        return this.stringCountService.countCharacters(this.content);
+    }
 }
