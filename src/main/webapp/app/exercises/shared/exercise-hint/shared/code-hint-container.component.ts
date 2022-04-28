aca5d45c64711fccf3514840079af61665b9018b
Ole Vester <ole.vester@tum.de>
2022-04-28 21:28:05 +0200

    Implement client side removal of solution entries

--- src/main/webapp/app/exercises/shared/exercise-hint/shared/code-hint-container.component.ts
+++ src/main/webapp/app/exercises/shared/exercise-hint/shared/code-hint-container.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 import { ProgrammingExerciseSolutionEntry } from 'app/entities/hestia/programming-exercise-solution-entry.model';
 import { CodeHint } from 'app/entities/hestia/code-hint-model';
+import { ProgrammingExerciseSolutionEntryService } from 'app/exercises/shared/exercise-hint/shared/programming-exercise-solution-entry.service';
 
 /**
  * Component containing the solution entries for a {@link CodeHint}.
@@ -12,16 +14,26 @@ import { CodeHint } from 'app/entities/hestia/code-hint-model';
     selector: 'jhi-code-hint-container',
     templateUrl: './code-hint-container.component.html',
 })
-export class CodeHintContainerComponent implements OnInit {
+export class CodeHintContainerComponent implements OnInit, OnDestroy {
     @Input()
     codeHint: CodeHint;
 
+    @Input()
+    enableEditing = false;
+
     sortedSolutionEntries: ProgrammingExerciseSolutionEntry[];
 
-    constructor(protected route: ActivatedRoute) {}
+    private dialogErrorSource = new Subject<string>();
+    dialogError$ = this.dialogErrorSource.asObservable();
+
+    constructor(protected route: ActivatedRoute, private solutionEntryService: ProgrammingExerciseSolutionEntryService) {}
 
     ngOnInit() {
         this.setSortedSolutionEntriesForCodeHint();
+    }
+
+    ngOnDestroy(): void {
+        this.dialogErrorSource.unsubscribe();
     }
 
     setSortedSolutionEntriesForCodeHint() {
@@ -29,5 +41,21 @@ export class CodeHintContainerComponent implements OnInit {
             this.codeHint.solutionEntries?.sort((a, b) => {
                 return a.filePath?.localeCompare(b.filePath!) || a.line! - b.line!;
             }) ?? [];
+    }
+
+    /**
+     * Delete a solution entry by its id
+     * @param solutionEntryId of the solution entry to be deleted
+     */
+    deleteEntry(solutionEntryId: number) {
+        this.solutionEntryService.deleteSolutionEntry(this.codeHint.exercise!.id!, this.codeHint.id!, solutionEntryId).subscribe({
+            next: () => {
+                this.sortedSolutionEntries = this.sortedSolutionEntries.filter((entry) => entry.id !== solutionEntryId);
+                this.dialogErrorSource.next('');
+            },
+            error: (error) => {
+                this.dialogErrorSource.next(error.message);
+            },
+        });
     }
 }
