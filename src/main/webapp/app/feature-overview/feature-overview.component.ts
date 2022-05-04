81803a4f9caf177fdd446336879006e98f92bb5a
akesfeden <atakesfeden@gmail.com>
2022-05-04 22:46:32 +0300

    Center feature checklist images and change order.

--- src/main/webapp/app/feature-overview/feature-overview.component.ts
+++ src/main/webapp/app/feature-overview/feature-overview.component.ts
@@ -331,9 +331,10 @@ export class FeatureOverviewComponent implements OnInit {
             'featureOverview.instructor.feature.checklist.descriptionTextOne',
             faClipboardCheck,
             undefined,
-            '/content/images/feature-overview/instructors/exam_checklist_overview.png',
             '/content/images/feature-overview/instructors/exam_checklist.png',
+            '/content/images/feature-overview/instructors/exam_checklist_overview.png',
         );
+        featureChecklist.centerTextAndImageOne();
 
         const featureGradeKey = new Feature(
             'featureOverview.instructor.feature.gradeKey.title',
