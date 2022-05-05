54e9725dacde0a5ebb8669cbc2331772ff04061b
JoejoeSt <johannes.stoehr@tum.de>
2022-05-05 22:04:02 +0200

    First redesign

--- src/main/webapp/app/exercises/shared/exercise-headers/header-exercise-page-with-details.component.ts
+++ src/main/webapp/app/exercises/shared/exercise-headers/header-exercise-page-with-details.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import dayjs from 'dayjs/esm';
-import { Exercise, ExerciseType, getIcon, IncludedInOverallScore } from 'app/entities/exercise.model';
+import { Exercise, ExerciseType, getCourseFromExercise, getIcon, getIconTooltip, IncludedInOverallScore } from 'app/entities/exercise.model';
 import { Exam } from 'app/entities/exam.model';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { ExerciseCategory } from 'app/entities/exercise-category.model';
@@ -8,13 +8,19 @@ import { SubmissionPolicy } from 'app/entities/submission-policy.model';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
 import { getExerciseDueDate, hasExerciseDueDatePassed } from 'app/exercises/shared/exercise/exercise.utils';
 import { faArrowLeft, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { Course } from 'app/entities/course.model';
+import { AssessmentType } from 'app/entities/assessment-type.model';
 
 @Component({
     selector: 'jhi-header-exercise-page-with-details',
     templateUrl: './header-exercise-page-with-details.component.html',
 })
-export class HeaderExercisePageWithDetailsComponent implements OnChanges {
+export class HeaderExercisePageWithDetailsComponent implements OnChanges, OnInit {
     readonly IncludedInOverallScore = IncludedInOverallScore;
+    readonly AssessmentType = AssessmentType;
+    readonly getIcon = getIcon;
+    readonly getIconTooltip = getIconTooltip;
 
     @Input() public exercise: Exercise;
     @Input() public studentParticipation?: StudentParticipation;
@@ -29,12 +35,21 @@ export class HeaderExercisePageWithDetailsComponent implements OnChanges {
     public exerciseCategories: ExerciseCategory[];
     public isExamMode = false;
     public dueDate?: dayjs.Dayjs;
+    public programmingExercise?: ProgrammingExercise;
+    public course: Course;
 
     icon: IconProp;
 
     // Icons
     faArrowLeft = faArrowLeft;
     faQuestionCircle = faQuestionCircle;
+
+    ngOnInit(): void {
+        if (this.exercise.type === ExerciseType.PROGRAMMING) {
+            this.programmingExercise = this.exercise as ProgrammingExercise;
+        }
+        this.course = getCourseFromExercise(this.exercise)!;
+    }
 
     /**
      * Sets the status badge and categories of the exercise on changes
