fb48fff3556497fdbb57c3dc7c3ef2d002ca9967
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-05-12 21:24:36 +0200

    Implement whitespace suggestion

--- src/main/webapp/app/shared/markdown-editor/commands/orderedListCommand.ts
+++ src/main/webapp/app/shared/markdown-editor/commands/orderedListCommand.ts
@@ -24,13 +24,14 @@ export class OrderedListCommand extends Command {
         const parseArray = selectedText.split('\n');
         let manipulatedText = '';
         let position = 1;
-        for (const line of parseArray) {
+
+        parseArray.forEach((line, index) => {
             // Empty line -> we start again
             if (line === '') {
                 // Special case: Single empty line
                 if (parseArray.length === 1) {
                     manipulatedText = '1. ';
-                    continue;
+                    return;
                 }
                 position = 1;
             } else {
@@ -38,8 +39,11 @@ export class OrderedListCommand extends Command {
                 manipulatedText += this.manipulateLine(line, position);
                 position++;
             }
-            manipulatedText += '\n';
-        }
+
+            if (index !== parseArray.length - 1) {
+                manipulatedText += '\n';
+            }
+        });
         this.replace(this.getRange(), manipulatedText);
     }
 
@@ -52,18 +56,21 @@ export class OrderedListCommand extends Command {
     manipulateLine(line: string, position: number): string {
         const index = line.indexOf('.');
 
+        // Calc leading whitespaces
+        const whitespaces = line.search(/\S|$/);
+
         // There is a dot in this line
         if (index !== -1) {
-            const elements = [line.slice(0, index), line.slice(index + 1)];
+            const elements = [line.slice(whitespaces, index), line.slice(index + 1)];
 
             // Check if this is really a number
             if (elements[0] !== '' && !isNaN(Number(elements[0]))) {
                 // Remove first whitespace
-                return elements[1].substring(1);
+                return ' '.repeat(whitespaces) + elements[1].substring(1);
             }
         }
 
         // Add the position of the list element
-        return `${position}. ${line}`;
+        return ' '.repeat(whitespaces) + `${position}. ${line.substring(whitespaces)}`;
     }
 }
