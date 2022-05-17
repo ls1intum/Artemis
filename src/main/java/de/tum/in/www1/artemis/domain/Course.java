15e02804d73838c0a9eb1714804243e2c43c8144
ge65cer <ge65cer@mytum.de>
2022-05-17 13:43:32 +0200

    Remove unused methods

--- src/main/java/de/tum/in/www1/artemis/domain/Course.java
+++ src/main/java/de/tum/in/www1/artemis/domain/Course.java
@@ -630,16 +630,6 @@ public class Course extends DomainObject {
         this.learningGoals = learningGoals;
     }
 
-    public void addLearningGoal(LearningGoal learningGoal) {
-        this.learningGoals.add(learningGoal);
-        learningGoal.setCourse(this);
-    }
-
-    public void removeLearningGoal(LearningGoal learningGoal) {
-        this.learningGoals.remove(learningGoal);
-        learningGoal.setCourse(null);
-    }
-
     public boolean hasCourseArchive() {
         return courseArchivePath != null && !courseArchivePath.isEmpty();
     }
