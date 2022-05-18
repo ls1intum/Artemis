ee9c3bd27fe46374c14b9be63c48d08e955d9074
Benjamin Schmitz <benjamin.schmitz@tum.de>
2022-05-18 10:06:06 +0200

    Add tests

--- src/test/java/de/tum/in/www1/artemis/service/GitLabCIServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/GitLabCIServiceTest.java
@@ -3,14 +3,19 @@ package de.tum.in.www1.artemis.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationGitLabCIGitLabTest;
 import de.tum.in.www1.artemis.ContinuousIntegrationTestService;
@@ -37,81 +42,81 @@ public class GitLabCIServiceTest extends AbstractSpringIntegrationGitLabCIGitLab
         continuousIntegrationTestService.tearDown();
     }
 
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testHealthOk() throws URISyntaxException, JsonProcessingException {
-    // gitlabRequestMockProvider.mockHealth("ok", HttpStatus.OK);
-    // var health = versionControlService.health();
-    // assertThat(health.getAdditionalInfo()).containsEntry("url", gitlabServerUrl);
-    // assertThat(health.isUp()).isTrue();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testHealthNotOk() throws URISyntaxException, JsonProcessingException {
-    // gitlabRequestMockProvider.mockHealth("notok", HttpStatus.OK);
-    // var health = versionControlService.health();
-    // assertThat(health.getAdditionalInfo()).containsEntry("url", gitlabServerUrl).containsEntry("status", "notok");
-    // assertThat(health.isUp()).isFalse();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testHealthException() throws URISyntaxException, JsonProcessingException {
-    // gitlabRequestMockProvider.mockHealth("ok", HttpStatus.INTERNAL_SERVER_ERROR);
-    // var health = versionControlService.health();
-    // assertThat(health.isUp()).isFalse();
-    // assertThat(health.getException()).isNotNull();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testAddFeedbackToResult() {
-    // var result = continuousIntegrationService.getWebHookUrl(null, null);
-    // assertThat(result).isEqualTo(Optional.empty());
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testCheckIfProjectExists() {
-    // var result = continuousIntegrationService.checkIfProjectExists(null, null);
-    // assertThat(result).isNull();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testRetrieveLatestArtifact() {
-    // var result = continuousIntegrationService.retrieveLatestArtifact(null);
-    // assertThat(result).isNull();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testGetLatestBuildLogs() {
-    // var result = continuousIntegrationService.getLatestBuildLogs(null);
-    // assertThat(result).isNull();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testCheckIfBuildPlanExists() {
-    // var result = continuousIntegrationService.checkIfBuildPlanExists(null, null);
-    // assertThat(result).isFalse();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testGetBuildStatus() {
-    // var result = continuousIntegrationService.getBuildStatus(null);
-    // assertThat(result).isNull();
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testConvertBuildResult() {
-    // var result = continuousIntegrationService.convertBuildResult(null);
-    // assertThat(result).isNull();
-    // }
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testHealthOk() throws URISyntaxException, JsonProcessingException {
+        gitlabRequestMockProvider.mockHealth("ok", HttpStatus.OK);
+        var health = versionControlService.health();
+        assertThat(health.getAdditionalInfo()).containsEntry("url", gitlabServerUrl);
+        assertThat(health.isUp()).isTrue();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testHealthNotOk() throws URISyntaxException, JsonProcessingException {
+        gitlabRequestMockProvider.mockHealth("notok", HttpStatus.OK);
+        var health = versionControlService.health();
+        assertThat(health.getAdditionalInfo()).containsEntry("url", gitlabServerUrl).containsEntry("status", "notok");
+        assertThat(health.isUp()).isFalse();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testHealthException() throws URISyntaxException, JsonProcessingException {
+        gitlabRequestMockProvider.mockHealth("ok", HttpStatus.INTERNAL_SERVER_ERROR);
+        var health = versionControlService.health();
+        assertThat(health.isUp()).isFalse();
+        assertThat(health.getException()).isNotNull();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testAddFeedbackToResult() {
+        var result = continuousIntegrationService.getWebHookUrl(null, null);
+        assertThat(result).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testCheckIfProjectExists() {
+        var result = continuousIntegrationService.checkIfProjectExists(null, null);
+        assertThat(result).isNull();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testRetrieveLatestArtifact() {
+        var result = continuousIntegrationService.retrieveLatestArtifact(null);
+        assertThat(result).isNull();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testGetLatestBuildLogs() {
+        var result = continuousIntegrationService.getLatestBuildLogs(null);
+        assertThat(result).isNull();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testCheckIfBuildPlanExists() {
+        var result = continuousIntegrationService.checkIfBuildPlanExists(null, null);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testGetBuildStatus() {
+        var result = continuousIntegrationService.getBuildStatus(null);
+        assertThat(result).isNull();
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testConvertBuildResult() {
+        var result = continuousIntegrationService.convertBuildResult(null);
+        assertThat(result).isNull();
+    }
 
     @Test
     @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
@@ -120,49 +125,49 @@ public class GitLabCIServiceTest extends AbstractSpringIntegrationGitLabCIGitLab
         assertThat(result).isNull();
     }
 
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testTriggerBuildSuccess() throws GitLabApiException {
-    // gitlabRequestMockProvider.mockCreateTrigger(false);
-    // gitlabRequestMockProvider.mockTriggerPipeline(false);
-    // gitlabRequestMockProvider.mockDeleteTrigger(false);
-    //
-    // ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
-    // participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
-    // continuousIntegrationService.triggerBuild(participation);
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testTriggerBuildFails() throws GitLabApiException {
-    // gitlabRequestMockProvider.mockCreateTrigger(true);
-    // gitlabRequestMockProvider.mockTriggerPipeline(true);
-    // gitlabRequestMockProvider.mockDeleteTrigger(true);
-    //
-    // ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
-    // participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
-    // assertThatThrownBy(() -> continuousIntegrationService.triggerBuild(participation)).isInstanceOf(GitLabCIException.class);
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testConfigureBuildPlanSuccess() throws GitLabApiException {
-    // gitlabRequestMockProvider.mockGetProject(false);
-    // gitlabRequestMockProvider.mockUpdateProject(false);
-    //
-    // ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
-    // participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
-    // continuousIntegrationService.configureBuildPlan(participation, "main");
-    // }
-    //
-    // @Test
-    // @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
-    // public void testConfigureBuildPlanFails() throws GitLabApiException {
-    // gitlabRequestMockProvider.mockGetProject(true);
-    // gitlabRequestMockProvider.mockUpdateProject(true);
-    //
-    // ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
-    // participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
-    // assertThatThrownBy(() -> continuousIntegrationService.configureBuildPlan(participation, "main")).isInstanceOf(GitLabCIException.class);
-    // }
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testTriggerBuildSuccess() throws GitLabApiException {
+        gitlabRequestMockProvider.mockCreateTrigger(false);
+        gitlabRequestMockProvider.mockTriggerPipeline(false);
+        gitlabRequestMockProvider.mockDeleteTrigger(false);
+
+        ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
+        participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
+        continuousIntegrationService.triggerBuild(participation);
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testTriggerBuildFails() throws GitLabApiException {
+        gitlabRequestMockProvider.mockCreateTrigger(true);
+        gitlabRequestMockProvider.mockTriggerPipeline(true);
+        gitlabRequestMockProvider.mockDeleteTrigger(true);
+
+        ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
+        participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
+        assertThatThrownBy(() -> continuousIntegrationService.triggerBuild(participation)).isInstanceOf(GitLabCIException.class);
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testConfigureBuildPlanSuccess() throws GitLabApiException {
+        gitlabRequestMockProvider.mockGetProject(false);
+        gitlabRequestMockProvider.mockUpdateProject(false);
+
+        ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
+        participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
+        continuousIntegrationService.configureBuildPlan(participation, "main");
+    }
+
+    @Test
+    @WithMockUser(roles = "INSTRUCTOR", username = "instructor1")
+    public void testConfigureBuildPlanFails() throws GitLabApiException {
+        gitlabRequestMockProvider.mockGetProject(true);
+        gitlabRequestMockProvider.mockUpdateProject(true);
+
+        ProgrammingExerciseStudentParticipation participation = new ProgrammingExerciseStudentParticipation();
+        participation.setRepositoryUrl("http://some.test.url/scm/PROJECTNAME/REPONAME-exercise.git");
+        assertThatThrownBy(() -> continuousIntegrationService.configureBuildPlan(participation, "main")).isInstanceOf(GitLabCIException.class);
+    }
 }
