f85499de446e47eb3f1119c1e6f119170fd1b0a1
Benedikt Fein <fein@fim.uni-passau.de>
2022-05-24 16:12:59 +0200

    reduce logspam for modeling submission update

--- src/main/java/de/tum/in/www1/artemis/web/rest/ModelingSubmissionResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ModelingSubmissionResource.java
@@ -113,12 +113,8 @@ public class ModelingSubmissionResource extends AbstractSubmissionResource {
     @PutMapping("/exercises/{exerciseId}/modeling-submissions")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<ModelingSubmission> updateModelingSubmission(@PathVariable long exerciseId, Principal principal, @RequestBody ModelingSubmission modelingSubmission) {
-        long start = System.currentTimeMillis();
         log.debug("REST request to update ModelingSubmission : {}", modelingSubmission.getModel());
-        ResponseEntity<ModelingSubmission> response = handleModelingSubmission(exerciseId, principal, modelingSubmission);
-        long end = System.currentTimeMillis();
-        log.info("updateModelingSubmission took {}ms for exercise {} and user {}", end - start, exerciseId, principal.getName());
-        return response;
+        return handleModelingSubmission(exerciseId, principal, modelingSubmission);
     }
 
     @NotNull
