fc62d27b052eadce392fde34dfc4603eef747ae1
Dominik Weinzierl <60005702+Dominik-Weinzierl@users.noreply.github.com>
2022-05-27 00:49:54 +0200

    General: Add start and end date validation for courses (#5143)

--- src/main/java/de/tum/in/www1/artemis/web/rest/CourseResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/CourseResource.java
@@ -166,6 +166,9 @@ public class CourseResource {
         course.validateComplaintsAndRequestMoreFeedbackConfig();
         course.validateOnlineCourseAndRegistrationEnabled();
         course.validateAccuracyOfScores();
+        if (!course.isValidStartAndEndDate()) {
+            throw new BadRequestAlertException("For Courses, the start date has to be before the end date", Course.ENTITY_NAME, "invalidCourseStartDate", true);
+        }
 
         courseService.createOrValidateGroups(course);
         Course result = courseRepository.save(course);
@@ -253,6 +256,9 @@ public class CourseResource {
         updatedCourse.validateOnlineCourseAndRegistrationEnabled();
         updatedCourse.validateShortName();
         updatedCourse.validateAccuracyOfScores();
+        if (!updatedCourse.isValidStartAndEndDate()) {
+            throw new BadRequestAlertException("For Courses, the start date has to be before the end date", Course.ENTITY_NAME, "invalidCourseStartDate", true);
+        }
 
         // Based on the old instructors, editors and TAs, we can update all exercises in the course in the VCS (if necessary)
         // We need the old instructors, editors and TAs, so that the VCS user management service can determine which
