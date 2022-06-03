83777cd59502438cba83e6955a148e303bc99d86
akesfeden <atakesfeden@gmail.com>
2022-06-03 14:40:36 +0300

    Update Javadoc.

--- src/main/java/de/tum/in/www1/artemis/service/exam/ExamService.java
+++ src/main/java/de/tum/in/www1/artemis/service/exam/ExamService.java
@@ -285,10 +285,10 @@ public class ExamService {
 
     /**
      * Calculates max points, max bonus points and achieved points per exercise if the given studentExam is assessed.
-     * Includes the corresponding grade type as well if a GradingScale is set for the relevant exam.
+     * Includes the corresponding grade and grade type as well if a GradingScale is set for the relevant exam.
      * @param studentExam a StudentExam instance that will have its points and grades calculated if it is assessed
      * @param participationsOfStudent StudentParticipation list for the given studentExam
-     * @return
+     * @return Student Exam results with exam grade calculated if applicable
      */
     @NotNull
     public StudentExamWithGradeDTO calculateStudentResultWithGradeAndPoints(StudentExam studentExam, List<StudentParticipation> participationsOfStudent) {
