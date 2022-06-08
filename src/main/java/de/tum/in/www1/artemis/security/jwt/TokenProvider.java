efad26550303953fdc76253e9d4aa038ab76e7a6
Lucas Alber <lucasd.alber@gmail.com>
2022-06-08 13:56:13 +0200

    JWT Token: Add method with custom duration

--- src/main/java/de/tum/in/www1/artemis/security/jwt/TokenProvider.java
+++ src/main/java/de/tum/in/www1/artemis/security/jwt/TokenProvider.java
@@ -81,17 +81,24 @@ public class TokenProvider {
      * @return JWT Token
      */
     public String createToken(Authentication authentication, boolean rememberMe) {
-        String authorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
-
-        long now = (new Date()).getTime();
-        Date validity;
         if (rememberMe) {
-            validity = new Date(now + this.tokenValidityInMillisecondsForRememberMe);
+            return createTokenWithCustomDuration(authentication, this.tokenValidityInMillisecondsForRememberMe);
         }
         else {
-            validity = new Date(now + this.tokenValidityInMilliseconds);
+            return createTokenWithCustomDuration(authentication, this.tokenValidityInMilliseconds);
         }
+    }
 
+    /**
+     * Create JWT Token a fully populated <code>Authentication</code> object.
+     * @param authentication Authentication Object
+     * @param durationValidityInMilliseconds The duration how long the access token should be valid
+     * @return JWT Token
+     */
+    public String createTokenWithCustomDuration(Authentication authentication, Long durationValidityInMilliseconds) {
+        String authorities = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + durationValidityInMilliseconds);
         return Jwts.builder().setSubject(authentication.getName()).claim(AUTHORITIES_KEY, authorities).signWith(key, SignatureAlgorithm.HS512).setExpiration(validity).compact();
     }
 
