81e40de6aaaf8d78bb7c6adcaac8829856735fe0
Benjamin Schmitz <benjamin.schmitz@tum.de>
2022-06-08 15:57:07 +0200

    Change build plan url

--- src/main/java/de/tum/in/www1/artemis/service/connectors/gitlabci/GitLabCIBuildPlanLinkInfoContributor.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/gitlabci/GitLabCIBuildPlanLinkInfoContributor.java
@@ -1,5 +1,8 @@
 package de.tum.in.www1.artemis.service.connectors.gitlabci;
 
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.context.annotation.Profile;
@@ -11,9 +14,12 @@ import de.tum.in.www1.artemis.config.Constants;
 @Profile("gitlabci")
 public class GitLabCIBuildPlanLinkInfoContributor implements InfoContributor {
 
+    @Value("${artemis.version-control.url}")
+    private URL gitlabServerUrl;
+
     @Override
     public void contribute(Info.Builder builder) {
-        // TODO: Template URL for build plan API
-        builder.withDetail(Constants.INFO_BUILD_PLAN_URL_DETAIL, GitLabCIService.CI_CONFIG_URL);
+        // TODO
+        builder.withDetail(Constants.INFO_BUILD_PLAN_URL_DETAIL, gitlabServerUrl);
     }
 }
