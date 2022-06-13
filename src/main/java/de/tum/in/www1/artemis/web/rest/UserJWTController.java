2cf8589a48ba555c9d8c5bce1a7e2921a9640100
Lucas Alber <lucasd.alber@gmail.com>
2022-06-13 09:53:19 +0200

    UserJWTController: Code Style

--- src/main/java/de/tum/in/www1/artemis/web/rest/UserJWTController.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/UserJWTController.java
@@ -149,9 +149,10 @@ public class UserJWTController {
         }
 
         long difference = this.personalAccessTokenMaxLifetimeMilliseconds - lifetimeMilliseconds;
-        if (lifetimeMilliseconds > this.personalAccessTokenMaxLifetimeMilliseconds)
+        if (lifetimeMilliseconds > this.personalAccessTokenMaxLifetimeMilliseconds) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "invalidPATLifetime",
                     "Requested token lifetime exceeds the maximum lifetime for personal access tokens by" + difference)).build();
+        }
 
         // Automatically returns 401 if not fully authorized
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
