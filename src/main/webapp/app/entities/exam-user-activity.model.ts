3ea7fc10824d386081d2d04d7d893df5ba744dfd
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-06-15 14:16:42 +0200

    fix merge issue

--- src/main/webapp/app/entities/exam-user-activity.model.ts
+++ src/main/webapp/app/entities/exam-user-activity.model.ts
@@ -19,6 +19,7 @@ export enum ExamActionType {
  */
 export class ExamActivity {
     public examActions: ExamAction[];
+    public id?: number;
 
     constructor() {
         this.examActions = [];
@@ -35,8 +36,8 @@ export class ExamActivity {
 export abstract class ExamAction implements BaseEntity {
     public id?: number;
     public studentExamId?: number;
-    public timestamp?: dayjs.Dayjs;
     public examActivityId?: number;
+    public timestamp?: dayjs.Dayjs;
     public ceiledTimestamp?: dayjs.Dayjs;
     public readonly type: ExamActionType;
 
@@ -102,13 +103,15 @@ export class SwitchedExerciseAction extends ExamAction {
 export class SavedExerciseAction extends ExamAction {
     public forced?: boolean;
     public submissionId?: number;
+    public exerciseId?: number;
     public failed?: boolean;
     public automatically?: boolean;
 
-    constructor(forced: boolean, submissionId: number | undefined, failed: boolean, automatically: boolean) {
+    constructor(forced: boolean, submissionId: number | undefined, exerciseId: number | undefined, failed: boolean, automatically: boolean) {
         super(ExamActionType.SAVED_EXERCISE);
         this.forced = forced;
         this.submissionId = submissionId;
+        this.exerciseId = exerciseId;
         this.failed = failed;
         this.automatically = automatically;
     }
