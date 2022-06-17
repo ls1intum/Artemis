a26ccebb5247f9f0edf7e02179ed68fdbeeca875
Johannes Stöhr <38322605+JohannesStoehr@users.noreply.github.com>
2022-06-17 09:52:02 +0200

    Update src/main/webapp/app/exercises/shared/result/result.component.ts
    Co-authored-by: Dominik Weinzierl <60005702+Dominik-Weinzierl@users.noreply.github.com>

--- src/main/webapp/app/exercises/shared/result/result.component.ts
+++ src/main/webapp/app/exercises/shared/result/result.component.ts
@@ -185,7 +185,7 @@ export class ResultComponent implements OnInit, OnChanges {
         this.evaluate();
 
         this.translate.onLangChange.subscribe(() => {
-            if (this.resultString !== '') {
+            if (!!this.resultString) {
                 this.resultString = this.resultService.getResultString(this.result!, this.exercise!);
             }
         });
