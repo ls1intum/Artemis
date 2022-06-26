5342a963d2322b5db3cb993b7696ca58498fb9c7
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-06-26 14:14:48 +0200

    minor refactoring

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/cache/PlagiarismCacheService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/cache/PlagiarismCacheService.java
@@ -1,40 +1,44 @@
 package de.tum.in.www1.artemis.service.plagiarism.cache;
 
+import static de.tum.in.www1.artemis.config.Constants.HAZELCAST_ACTIVE_PLAGIARISM_CHECKS_PER_COURSE_CACHE;
+
 import org.springframework.stereotype.Service;
 
+import com.hazelcast.collection.ISet;
 import com.hazelcast.core.HazelcastInstance;
 
 @Service
 public class PlagiarismCacheService {
 
-    private final PlagiarismCache plagiarismCache;
+    // Every course in this set is currently doing a plagiarism check
+    private final ISet<Long> activePlagiarismChecksPerCourse;
 
     public PlagiarismCacheService(HazelcastInstance hazelcastInstance) {
-        plagiarismCache = new PlagiarismCache(hazelcastInstance);
+        this.activePlagiarismChecksPerCourse = hazelcastInstance.getSet(HAZELCAST_ACTIVE_PLAGIARISM_CHECKS_PER_COURSE_CACHE);
     }
 
     /**
      * Returns the status of the course.
-     * @param courseId current course
+     * @param courseId courseId used to identify the table entry
      * @return true if there is an active plagiarism check
      */
     public boolean isActivePlagiarismCheck(Long courseId) {
-        return plagiarismCache.isActivePlagiarismCheck(courseId);
+        return activePlagiarismChecksPerCourse.contains(courseId);
     }
 
     /**
-     * There is an active plagiarism check in this course. The entry is set to true.
+     * There is an active plagiarism check in this course. The course id is added.
      * @param courseId current course
      */
     public void setActivePlagiarismCheck(Long courseId) {
-        plagiarismCache.setActivePlagiarismCheck(courseId);
+        activePlagiarismChecksPerCourse.add(courseId);
     }
 
     /**
-     * There is no active plagiarism check anymore. The entry is set to false.
+     * There is no active plagiarism check anymore. The course id is removed.
      * @param courseId current course
      */
     public void setInactivePlagiarismCheck(Long courseId) {
-        plagiarismCache.setInactivePlagiarismCheck(courseId);
+        activePlagiarismChecksPerCourse.remove(courseId);
     }
 }
