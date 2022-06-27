408cd8b4c30533e3590cb0dce556e54bc6f2fe2b
Lucas Welscher <ga53foy@mytum.de>
2022-06-27 14:39:14 +0200

    integrate review feedback

--- src/main/java/de/tum/in/www1/artemis/web/rest/errors/BadRequestAlertException.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/errors/BadRequestAlertException.java
@@ -15,6 +15,11 @@ public class BadRequestAlertException extends HttpStatusException {
         this(ErrorConstants.DEFAULT_TYPE, defaultMessage, entityName, errorKey, false);
     }
 
+    public BadRequestAlertException(String defaultMessage, String entityName, String errorKey, Map<String, Object> translationParameters) {
+        // translation params are expected to be a child of the "params" object
+        this(ErrorConstants.PARAMETERIZED_TYPE, defaultMessage, entityName, errorKey, Map.of("params", translationParameters));
+    }
+
     public BadRequestAlertException(URI type, String defaultMessage, String entityName, String errorKey, boolean skipAlert) {
         super(type, defaultMessage, Status.BAD_REQUEST, entityName, errorKey, getAlertParameters(entityName, errorKey, skipAlert));
     }
