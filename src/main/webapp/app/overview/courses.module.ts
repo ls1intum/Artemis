f0558ff937baa37b8ffb4942b84e162cd636c60a
Michael Allgaier <michael.allgaier@tum.de>
2022-07-14 23:01:08 +0200

    Resolve merge conflict - femoghalvfjerds

--- src/main/webapp/app/overview/courses.module.ts
+++ src/main/webapp/app/overview/courses.module.ts
@@ -18,8 +18,10 @@ import { ArtemisSharedPipesModule } from 'app/shared/pipes/shared-pipes.module';
 import { RatingModule } from 'app/exercises/shared/rating/rating.module';
 import { ArtemisExerciseButtonsModule } from 'app/overview/exercise-details/exercise-buttons.module';
 import { ArtemisCourseExerciseRowModule } from 'app/overview/course-exercises/course-exercise-row.module';
-import { PieChartModule } from '@swimlane/ngx-charts';
+import { NgxChartsModule, PieChartModule } from '@swimlane/ngx-charts';
 import { HeaderCourseComponent } from 'app/overview/header-course.component';
+import { ArtemisExamSharedModule } from 'app/exam/shared/exam-shared.module';
+import { CourseExamAttemptReviewDetailComponent } from 'app/overview/course-exams/course-exam-attempt-review-detail/course-exam-attempt-review-detail.component';
 
 @NgModule({
     imports: [
