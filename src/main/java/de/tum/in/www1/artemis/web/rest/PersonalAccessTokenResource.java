37bde03c789034842f7c44c12084f1e43842d88e
Lucas Alber <lucasd.alber@gmail.com>
2022-07-19 10:24:16 +0200

    PAT: PreAuthorize resource

--- src/main/java/de/tum/in/www1/artemis/web/rest/PersonalAccessTokenResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/PersonalAccessTokenResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -45,6 +46,7 @@ public class PersonalAccessTokenResource {
      * @param lifetimeSeconds the lifetime for the JWT token in seconds
      * @return a JWT Token
      */
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/personal-access-token")
     public ResponseEntity<UserJWTController.JWTToken> getPersonalAccessToken(@RequestBody Long lifetimeSeconds) {
         User user = userRepository.getUser();
