b00346b240d668c8c0a23639c859c4fe0f56b5ad
Ole Vester <ole.vester@tum.de>
2022-07-20 17:10:55 +0200

    Add a new knowledge source to drop git diff entries for removed blocks of code

---
+++ src/main/java/de/tum/in/www1/artemis/service/hestia/behavioral/knowledgesource/DropRemovedGitDiffEntries.java
@@ -0,0 +1,33 @@
+package de.tum.in.www1.artemis.service.hestia.behavioral.knowledgesource;
+
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+import de.tum.in.www1.artemis.service.hestia.behavioral.BehavioralBlackboard;
+import de.tum.in.www1.artemis.service.hestia.behavioral.BehavioralSolutionEntryGenerationException;
+
+/**
+ * Remove all {@link de.tum.in.www1.artemis.domain.hestia.ProgrammingExerciseGitDiffEntry} from the
+ * {@link de.tum.in.www1.artemis.domain.hestia.ProgrammingExerciseGitDiffReport} of the {@link BehavioralBlackboard}
+ * that represents removed consecutive blocks of code.
+ * Entries cannot be generated for removed code, therefore we have to drop them from the git diff report of the blackboard.
+ */
+public class DropRemovedGitDiffEntries extends BehavioralKnowledgeSource {
+
+    public DropRemovedGitDiffEntries(BehavioralBlackboard blackboard) {
+        super(blackboard);
+    }
+
+    @Override
+    public boolean executeCondition() {
+        return blackboard.getGitDiffReport() != null;
+    }
+
+    @Override
+    public boolean executeAction() throws BehavioralSolutionEntryGenerationException {
+        var nonRemovedEntries = blackboard.getGitDiffReport().getEntries().stream().filter(entry -> entry.getStartLine() != null && entry.getLineCount() != null)
+                .collect(Collectors.toCollection(HashSet::new));
+        blackboard.getGitDiffReport().setEntries(nonRemovedEntries);
+        return !nonRemovedEntries.isEmpty();
+    }
+}
