f14b4dbddcc9cf9d0b234e89d014f0ed882d3e3c
Evgeny Volynsky <evgeny.volynsky@tum.de>
2022-07-20 16:24:14 +0200

    * Remove double quotes from file extensions to increase the readabiity

--- src/main/webapp/app/shared/constants/file-extensions.constants.ts
+++ src/main/webapp/app/shared/constants/file-extensions.constants.ts
@@ -1,2 +1,1 @@
-export const FILE_EXTENSIONS =
-    '".png", ".jpg", ".jpeg", ".svg", ".pdf", ".zip", ".doc", ".docx", ".csv", ".xls", ".xlsx", ".ppt", ".pptx", ".pages", ".rtf", ".numbers", ".key", ".odt", ".ods", ".odp", ".odg", ".odf".';
+export const FILE_EXTENSIONS = '.png, .jpg, .jpeg, .svg, .pdf, .zip, .doc, .docx, .csv, .xls, .xlsx, .ppt, .pptx, .pages, .rtf, .numbers, .key, .odt, .ods, .odp, .odg, .odf.';
