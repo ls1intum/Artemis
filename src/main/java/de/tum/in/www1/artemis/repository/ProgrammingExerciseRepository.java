41f5b9436175b7391c6e8c85ba86ab8058482f95
Timor Morrien <timor.morrien@tum.de>
2022-07-26 21:19:19 +0200

    Programming exercises: Fix an issue that caused tasks to get duplicated during import (#5474)

--- src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseRepository.java
@@ -241,19 +241,16 @@ public interface ProgrammingExerciseRepository extends JpaRepository<Programming
 
     @Query("""
             SELECT p FROM ProgrammingExercise p
-            LEFT JOIN FETCH p.testCases
+            LEFT JOIN FETCH p.testCases tc
             LEFT JOIN FETCH p.staticCodeAnalysisCategories
             LEFT JOIN FETCH p.exerciseHints
             LEFT JOIN FETCH p.templateParticipation
             LEFT JOIN FETCH p.solutionParticipation
             LEFT JOIN FETCH p.auxiliaryRepositories
-            LEFT JOIN FETCH p.tasks t
-            LEFT JOIN FETCH t.testCases tc
             LEFT JOIN FETCH tc.solutionEntries
             WHERE p.id = :#{#exerciseId}
             """)
-    Optional<ProgrammingExercise> findByIdWithEagerTestCasesStaticCodeAnalysisCategoriesHintsAndTemplateAndSolutionParticipationsAndAuxReposAndTasksWithTestCases(
-            @Param("exerciseId") Long exerciseId);
+    Optional<ProgrammingExercise> findByIdWithEagerTestCasesStaticCodeAnalysisCategoriesHintsAndTemplateAndSolutionParticipationsAndAuxRepos(@Param("exerciseId") Long exerciseId);
 
     /**
      * Returns the programming exercises that have a buildAndTestStudentSubmissionsAfterDueDate higher than the provided date.
