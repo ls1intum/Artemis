a14def05bec82dbe1c06330226a27af9d8261ffd
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-08-01 23:04:12 +0200

    Created test for tutorial-group-service

---
+++ src/test/javascript/spec/component/tutorial-groups/tutorial-group.service.spec.ts
@@ -0,0 +1,83 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { map, take } from 'rxjs/operators';
+import { TutorialGroupsService } from 'app/course/tutorial-groups/tutorial-groups.service';
+import { TutorialGroup } from 'app/entities/tutorial-group.model';
+
+describe('TutorialGroupService', () => {
+    let service: TutorialGroupsService;
+    let httpMock: HttpTestingController;
+    let elemDefault: TutorialGroup;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
+        service = TestBed.inject(TutorialGroupsService);
+        httpMock = TestBed.inject(HttpTestingController);
+
+        elemDefault = new TutorialGroup();
+        elemDefault.id = 0;
+        elemDefault.title = 'Test';
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should find a TutorialGroup', fakeAsync(() => {
+        const returnedFromService = { ...elemDefault };
+        service
+            .findById(1, 1)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toMatchObject({ body: elemDefault }));
+
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should create a Tutorial Group', fakeAsync(() => {
+        const returnedFromService = { ...elemDefault, id: 0 };
+        const expected = { ...returnedFromService };
+        service
+            .create(new TutorialGroup(), 1)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+
+        const req = httpMock.expectOne({ method: 'POST' });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should update a TutorialGroup', fakeAsync(() => {
+        const returnedFromService = { ...elemDefault, title: 'Test' };
+        const expected = { ...returnedFromService };
+
+        service
+            .update(expected, 1)
+            .pipe(take(1))
+            .subscribe((resp) => expect(resp).toMatchObject({ body: expected }));
+
+        const req = httpMock.expectOne({ method: 'PUT' });
+        req.flush(returnedFromService);
+        tick();
+    }));
+
+    it('should return a list of Tutorial Groups', fakeAsync(() => {
+        const returnedFromService = { ...elemDefault, title: 'Test' };
+        const expected = { ...returnedFromService };
+
+        service
+            .getAllForCourse(1)
+            .pipe(
+                take(1),
+                map((resp) => resp.body),
+            )
+            .subscribe((body) => expect(body).toContainEqual(expected));
+
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush([returnedFromService]);
+        tick();
+    }));
+});
