d6b0a1e31a78b7dcb375d7f1e7eeff9e462c6ab0
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-08-01 22:52:26 +0200

    Added test for edit-tutorial-group.component

--- src/test/javascript/spec/component/tutorial-groups/create-tutorial-group.component.spec.ts
+++ src/test/javascript/spec/component/tutorial-groups/create-tutorial-group.component.spec.ts
@@ -53,7 +53,6 @@ describe('CreateTutorialGroupComponent', () => {
                     },
                 },
             ],
-            schemas: [],
         })
             .compileComponents()
             .then(() => {
