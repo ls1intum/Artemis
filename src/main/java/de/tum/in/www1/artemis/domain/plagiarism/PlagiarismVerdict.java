c331e02046d673fdfaf6059b0da755a8fcc8e933
JoejoeSt <johannes.stoehr@tum.de>
2022-08-05 09:53:46 +0200

    Improve verdict email

--- src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismVerdict.java
+++ src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismVerdict.java
@@ -1,5 +1,16 @@
 package de.tum.in.www1.artemis.domain.plagiarism;
 
 public enum PlagiarismVerdict {
-    PLAGIARISM, POINT_DEDUCTION, WARNING, NO_PLAGIARISM
+
+    PLAGIARISM, POINT_DEDUCTION, WARNING, NO_PLAGIARISM;
+
+    @SuppressWarnings("unused") // Used in the email template plagiarismVerdictEmail.html
+    public String getEmailMessage() {
+        return switch (this) {
+            case PLAGIARISM -> "Your case is considered as plagiarism!";
+            case POINT_DEDUCTION -> "Due to your plagiarism, we are deducting points from your score in this exercise!";
+            case WARNING -> "Due to the plagiarism attempt, you are warned. Any furrther plagiarism will lead to harsher consequences!";
+            case NO_PLAGIARISM -> "The case is no longer considered as plagiarism.";
+        };
+    }
 }
