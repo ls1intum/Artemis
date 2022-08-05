2b326002df4d6ffc7a2f8d989638f629f8b47de3
JoejoeSt <johannes.stoehr@tum.de>
2022-08-05 10:32:31 +0200

    Fix typo

--- src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismVerdict.java
+++ src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismVerdict.java
@@ -9,7 +9,7 @@ public enum PlagiarismVerdict {
         return switch (this) {
             case PLAGIARISM -> "Your case is considered as plagiarism!";
             case POINT_DEDUCTION -> "Due to your plagiarism, we are deducting points from your score in this exercise!";
-            case WARNING -> "Due to the plagiarism attempt, you are warned. Any furrther plagiarism will lead to harsher consequences!";
+            case WARNING -> "Due to the plagiarism attempt, you are warned. Any further plagiarism will lead to severe consequences!";
             case NO_PLAGIARISM -> "The case is no longer considered as plagiarism.";
         };
     }
