4804b3f9785d2cbb444e49abada19210f7fddd2d
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-08-08 23:13:39 +0200

    Removed tutor group from client

--- src/main/webapp/app/entities/tutorial-group/tutorial-group.model.ts
+++ src/main/webapp/app/entities/tutorial-group/tutorial-group.model.ts
@@ -1,11 +1,15 @@
 import { BaseEntity } from 'app/shared/model/base-entity';
-import { Course } from 'app/entities/course.model';
+import { Course, Language } from 'app/entities/course.model';
 import { User } from 'app/core/user/user.model';
-
 export class TutorialGroup implements BaseEntity {
     public id?: number;
     public title?: string;
     public course?: Course;
+    public capacity?: number;
+    public location?: string;
+    public language?: Language;
+    public additionalInformation?: string;
+    public isOnline?: boolean;
     public teachingAssistant?: User;
     public registeredStudents?: User[];
 
