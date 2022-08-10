3fa6b21cf16ce1b3dc18b90148756a22f1e60267
Benedikt Fein <fein@fim.uni-passau.de>
2022-08-10 17:37:31 +0200

    refactor configuration of CI Docker images

--- src/main/java/de/tum/in/www1/artemis/service/connectors/ProgrammingLanguageConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/ProgrammingLanguageConfiguration.java
@@ -9,44 +9,114 @@ import de.tum.in.www1.artemis.domain.enumeration.ProjectType;
 
 /**
  * This configuration file provides functions to get the configured Docker Images for {@link ProgrammingLanguage ProgrammingLanguages}.
- *
- * @author Dominik Fuchss
  */
 @ConfigurationProperties(prefix = "artemis.continuous-integration.build")
 public class ProgrammingLanguageConfiguration {
 
-    // Maps ProgrammingLanguage to Docker image and {ProgrammingLanguage_ProjectType} to Docker images as override
-    private Map<String, String> images = new HashMap<>();
+    private static final ProjectType DEFAULT_PROJECT_TYPE = ProjectType.PLAIN;
+
+    private static final ProjectType MAVEN_PROJECT_TYPE = ProjectType.MAVEN_MAVEN;
+
+    private static final ProjectType GRADLE_PROJECT_TYPE = ProjectType.GRADLE_GRADLE;
+
+    private Map<ProgrammingLanguage, Map<ProjectType, String>> images = new EnumMap<>(ProgrammingLanguage.class);
 
     /**
      * Set the map of languages to build images.
      *
      * @param buildImages the map of languages to build images
      */
-    public void setImages(Map<String, String> buildImages) {
-        // Get all language names ordered by its length (needed to be sure that check of project type works)
-        var allLanguageNames = Arrays.stream(ProgrammingLanguage.values()).map(Enum::name).sorted(Comparator.comparing(String::length).reversed()).toList();
-        var allProjectTypes = Arrays.stream(ProjectType.values()).map(Enum::name).toList();
+    public void setImages(final Map<String, Map<String, String>> buildImages) {
+        final var languageSpecificBuildImages = loadImages(buildImages);
+        checkImageForAllProgrammingLanguagesDefined(languageSpecificBuildImages);
+        images = languageSpecificBuildImages;
+    }
 
-        // Check that all programming languages are present
-        if (!allLanguageNames.stream().allMatch(buildImages::containsKey)) {
-            var missing = allLanguageNames.stream().filter(programmingLanguage -> !buildImages.containsKey(programmingLanguage)).toList();
-            throw new IllegalArgumentException("Not all Build Images for Programming Languages are set in configuration. Missing: " + missing);
+    /**
+     * Parses the configuration contents into a properly typed map.
+     *
+     * @param imageConfig the config as it was received on server startup.
+     * @return A map of programming languages to the CI build images that are defined for it.
+     */
+    private Map<ProgrammingLanguage, Map<ProjectType, String>> loadImages(final Map<String, Map<String, String>> imageConfig) {
+        final EnumMap<ProgrammingLanguage, Map<ProjectType, String>> buildImages = new EnumMap<>(ProgrammingLanguage.class);
+
+        for (var entry : imageConfig.entrySet()) {
+            final ProgrammingLanguage programmingLanguage = tryParseProgrammingLanguage(entry.getKey());
+            final Map<ProjectType, String> projectTypeImages = tryParseProjectTypeBuildImages(entry.getValue());
+
+            buildImages.put(programmingLanguage, projectTypeImages);
         }
-        // Check that all defined project types do exist. Schema: {Language}_{ProjectType}
-        for (var definedKey : buildImages.keySet()) {
-            if (allLanguageNames.contains(definedKey)) {
-                continue;
+
+        return buildImages;
+    }
+
+    /**
+     * Tries to parse the given programming language.
+     * <p>
+     * Aborts server startup by throwing an exception if the language cannot be parsed.
+     *
+     * @param language The name of the language that should be parsed.
+     * @return The given language, now parsed.
+     */
+    private ProgrammingLanguage tryParseProgrammingLanguage(final String language) {
+        try {
+            return ProgrammingLanguage.valueOf(language.toUpperCase(Locale.ROOT));
+        }
+        catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("Unknown programming language: " + language);
+        }
+    }
+
+    /**
+     * Tries to parse key-value pairs of project types and their build images.
+     * <p>
+     * Aborts server startup by throwing an exception if the project type cannot be parsed.
+     *
+     * @param buildImageConfig A map of project types to CI build images.
+     * @return A map of project types to CI build images.
+     */
+    private Map<ProjectType, String> tryParseProjectTypeBuildImages(final Map<String, String> buildImageConfig) {
+        final EnumMap<ProjectType, String> projectTypeImages = new EnumMap<>(ProjectType.class);
+
+        for (var entry : buildImageConfig.entrySet()) {
+            final String projectTypeRaw = entry.getKey();
+            final String image = entry.getValue();
+
+            final ProjectType projectType = switch (projectTypeRaw) {
+                case "default" -> DEFAULT_PROJECT_TYPE;
+                case "maven" -> MAVEN_PROJECT_TYPE;
+                case "gradle" -> GRADLE_PROJECT_TYPE;
+                case "gcc" -> ProjectType.GCC;
+                case "fact" -> ProjectType.FACT;
+                case "xcode" -> ProjectType.XCODE;
+                default -> throw new IllegalArgumentException("Unknown project type: " + projectTypeRaw);
+            };
+
+            projectTypeImages.put(projectType, image);
+        }
+
+        return projectTypeImages;
+    }
+
+    /**
+     * Makes sure that for each programming language at least a default image is defined.
+     * <p>
+     * Aborts server startup by throwing an exception if some definitions are missing.
+     *
+     * @param buildImages The build images as defined in the configuration.
+     */
+    private void checkImageForAllProgrammingLanguagesDefined(final Map<ProgrammingLanguage, Map<ProjectType, String>> buildImages) {
+        for (ProgrammingLanguage language : ProgrammingLanguage.values()) {
+            if (!buildImages.containsKey(language)) {
+                throw new IllegalArgumentException("For each programming language a CI build image must be defined. Missing: " + language);
             }
-            // Extract the longest key of a matching language
-            var matchedLanguage = allLanguageNames.stream().filter(definedKey::startsWith).findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("No matching language has been found for " + definedKey));
-            var projectType = definedKey.substring(matchedLanguage.length() + 1);
-            if (!allProjectTypes.contains(projectType)) {
-                throw new IllegalArgumentException("Could not find ProjectType: " + projectType + " in Definition: " + definedKey);
+
+            final Map<ProjectType, String> languageImages = buildImages.get(language);
+            if (!languageImages.containsKey(DEFAULT_PROJECT_TYPE)) {
+                throw new IllegalArgumentException("For each programming language a default image must be defined. Missing: " + language);
             }
         }
-        this.images = buildImages;
     }
 
     /**
@@ -59,13 +129,25 @@ public class ProgrammingLanguageConfiguration {
      * @return the docker image name
      */
     public String getImage(ProgrammingLanguage programmingLanguage, Optional<ProjectType> projectType) {
-        var image = this.images.get(programmingLanguage.name());
-        if (projectType.isPresent()) {
-            var languageWithType = programmingLanguage.name() + "_" + projectType.get().name();
-            if (images.containsKey(languageWithType)) {
-                image = images.get(languageWithType);
-            }
+        final Map<ProjectType, String> languageImages = images.get(programmingLanguage);
+        final ProjectType configuredProjectType = projectType.map(this::getConfiguredProjectType).orElse(DEFAULT_PROJECT_TYPE);
+
+        if (languageImages.containsKey(configuredProjectType)) {
+            return languageImages.get(configuredProjectType);
         }
-        return image;
+        else {
+            return languageImages.get(DEFAULT_PROJECT_TYPE);
+        }
+    }
+
+    private ProjectType getConfiguredProjectType(final ProjectType actualProjectType) {
+        return switch (actualProjectType) {
+            case MAVEN_MAVEN, PLAIN_MAVEN -> MAVEN_PROJECT_TYPE;
+            case PLAIN_GRADLE, GRADLE_GRADLE -> GRADLE_PROJECT_TYPE;
+            case PLAIN -> ProjectType.PLAIN;
+            case XCODE -> ProjectType.XCODE;
+            case FACT -> ProjectType.FACT;
+            case GCC -> ProjectType.GCC;
+        };
     }
 }
