abab597f8b93a4169cea7de685acb4dd4277e290
Manuel Manso <manel_manso@hotmail.com>
2022-08-18 07:50:40 +0100

    Update src/test/java/de/tum/in/www1/artemis/util/QueryCountAssert.java
    Co-authored-by: Benedikt Fein <fein@fim.uni-passau.de>

--- src/test/java/de/tum/in/www1/artemis/util/QueryCountAssert.java
+++ src/test/java/de/tum/in/www1/artemis/util/QueryCountAssert.java
@@ -19,6 +19,7 @@ public class QueryCountAssert<T, E extends Exception> extends AbstractAssert<Que
      * Asserts that the amount of database queries realized during a REST call exactly match the expected amount
      *
      * @param times the amount of queries expected on the database
+     * @return the result of the original call for which this assertion has been performed
      * @throws AssertionError if the expected calls do not match the actual queries on the database
      */
     public T hasBeenCalledTimes(long times) throws E {
