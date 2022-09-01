87728416eea29fc6b0276ffdbd57cd528d683f28
Julian Christl <julian.christl@tum.de>
2022-09-01 17:33:44 +0200

    add comment

--- src/main/java/de/tum/in/www1/artemis/service/FileService.java
+++ src/main/java/de/tum/in/www1/artemis/service/FileService.java
@@ -209,6 +209,7 @@ public class FileService implements DisposableBean {
         String newFilename = filename;
         do {
             if (!keepFileName) {
+                // append a timestamp and some randomness to the filename to avoid conflicts
                 newFilename = fileNameAddition + ZonedDateTime.now().toString().substring(0, 23).replaceAll(":|\\.", "-") + "_" + UUID.randomUUID().toString().substring(0, 8) + "."
                         + fileExtension;
             }
