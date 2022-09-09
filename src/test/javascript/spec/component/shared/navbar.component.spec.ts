7dbb8664119c8a7d638de9b613e854834878d4b7
Patrick Bassner <patrick@bassner.de>
2022-09-09 16:31:36 +0200

    Remove 'experimental' and new feature advertising from dark mode popover

--- src/test/javascript/spec/component/shared/navbar.component.spec.ts
+++ src/test/javascript/spec/component/shared/navbar.component.spec.ts
@@ -33,6 +33,7 @@ import { ExamParticipationService } from 'app/exam/participate/exam-participatio
 import dayjs from 'dayjs/esm';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { MockActivatedRoute } from '../../helpers/mocks/activated-route/mock-activated-route';
+import { SystemNotificationComponent } from 'app/shared/notification/system-notification/system-notification.component';
 
 class MockBreadcrumb {
     label: string;
@@ -87,6 +88,7 @@ describe('NavbarComponent', () => {
                 MockComponent(LoadingNotificationComponent),
                 MockComponent(JhiConnectionWarningComponent),
                 MockComponent(ThemeSwitchComponent),
+                MockComponent(SystemNotificationComponent),
             ],
             providers: [
                 MockProvider(UrlSerializer),
