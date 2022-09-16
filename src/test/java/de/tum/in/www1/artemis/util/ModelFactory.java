14df0c0ad1e15448dd01f0d9d39e04912cce4b43
Ata Keşfeden <atakesfeden@gmail.com>
2022-09-16 18:02:56 +0300

    Assessment: Map bonus to exam grades (#5222)

--- src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
+++ src/test/java/de/tum/in/www1/artemis/util/ModelFactory.java
@@ -57,6 +57,7 @@ public class ModelFactory {
 
     /**
      * Create a dummy attachment for testing
+     *
      * @param date The optional upload and release date to set on the attachment
      * @return Attachment that was created
      */
@@ -73,6 +74,7 @@ public class ModelFactory {
 
     /**
      * Create a dummy attachment for testing with a placeholder image file on disk
+     *
      * @param startDate The release date to set on the attachment
      * @return Attachment that was created with its link set to a testing file on disk
      */
@@ -291,10 +293,10 @@ public class ModelFactory {
     /**
      * Generate users that have registration numbers
      *
-     * @param loginPrefix prefix that will be added in front of every user's login
-     * @param groups groups that the users will be added
-     * @param authorities authorities that the users will have
-     * @param amount amount of users to generate
+     * @param loginPrefix              prefix that will be added in front of every user's login
+     * @param groups                   groups that the users will be added
+     * @param authorities              authorities that the users will have
+     * @param amount                   amount of users to generate
      * @param registrationNumberPrefix prefix that will be added in front of every user
      * @return users that were generated
      */
@@ -328,13 +330,13 @@ public class ModelFactory {
     /**
      * Generate a team
      *
-     * @param exercise exercise of the team
-     * @param name name of the team
-     * @param shortName short name of the team
-     * @param loginPrefix prefix that will be added in front of every user's login
-     * @param numberOfStudents amount of users to generate for team as students
-     * @param owner owner of the team generally a tutor
-     * @param creatorLogin login of user that creates the teams
+     * @param exercise           exercise of the team
+     * @param name               name of the team
+     * @param shortName          short name of the team
+     * @param loginPrefix        prefix that will be added in front of every user's login
+     * @param numberOfStudents   amount of users to generate for team as students
+     * @param owner              owner of the team generally a tutor
+     * @param creatorLogin       login of user that creates the teams
      * @param registrationPrefix prefix that will be added in front of every student's registration number
      * @return team that was generated
      */
@@ -361,11 +363,11 @@ public class ModelFactory {
     /**
      * Generate a team
      *
-     * @param exercise exercise of the team
-     * @param name name of the team
-     * @param shortName short name of the team
+     * @param exercise         exercise of the team
+     * @param name             name of the team
+     * @param shortName        short name of the team
      * @param numberOfStudents amount of users to generate for team as students
-     * @param owner owner of the team generally a tutor
+     * @param owner            owner of the team generally a tutor
      * @return team that was generated
      */
     public static Team generateTeamForExercise(Exercise exercise, String name, String shortName, int numberOfStudents, User owner) {
@@ -375,12 +377,12 @@ public class ModelFactory {
     /**
      * Generate teams
      *
-     * @param exercise exercise of the teams
+     * @param exercise        exercise of the teams
      * @param shortNamePrefix prefix that will be added in front of every team's short name
-     * @param loginPrefix prefix that will be added in front of every student's login
-     * @param numberOfTeams amount of teams to generate
-     * @param owner owner of the teams generally a tutor
-     * @param creatorLogin login of user that created the teams
+     * @param loginPrefix     prefix that will be added in front of every student's login
+     * @param numberOfTeams   amount of teams to generate
+     * @param owner           owner of the teams generally a tutor
+     * @param creatorLogin    login of user that created the teams
      * @return teams that were generated
      */
     public static List<Team> generateTeamsForExercise(Exercise exercise, String shortNamePrefix, String loginPrefix, int numberOfTeams, User owner, String creatorLogin) {
@@ -390,12 +392,12 @@ public class ModelFactory {
     /**
      * Generate teams
      *
-     * @param exercise exercise of the teams
-     * @param shortNamePrefix prefix that will be added in front of every team's short name
-     * @param loginPrefix prefix that will be added in front of every student's login
-     * @param numberOfTeams amount of teams to generate
-     * @param owner owner of the teams generally a tutor
-     * @param creatorLogin login of user that created the teams
+     * @param exercise           exercise of the teams
+     * @param shortNamePrefix    prefix that will be added in front of every team's short name
+     * @param loginPrefix        prefix that will be added in front of every student's login
+     * @param numberOfTeams      amount of teams to generate
+     * @param owner              owner of the teams generally a tutor
+     * @param creatorLogin       login of user that created the teams
      * @param registrationPrefix prefix that will be added in front of every student's registration number
      * @return teams that were generated
      */
@@ -412,14 +414,14 @@ public class ModelFactory {
     /**
      * Generate teams
      *
-     * @param exercise exercise of the teams
-     * @param shortNamePrefix prefix that will be added in front of every team's short name
-     * @param loginPrefix prefix that will be added in front of every student's login
-     * @param numberOfTeams amount of teams to generate
-     * @param owner owner of the teams generally a tutor
-     * @param creatorLogin login of user that created the teams
+     * @param exercise           exercise of the teams
+     * @param shortNamePrefix    prefix that will be added in front of every team's short name
+     * @param loginPrefix        prefix that will be added in front of every student's login
+     * @param numberOfTeams      amount of teams to generate
+     * @param owner              owner of the teams generally a tutor
+     * @param creatorLogin       login of user that created the teams
      * @param registrationPrefix prefix that will be added in front of every student's registration number
-     * @param teamSize size of each individual team
+     * @param teamSize           size of each individual team
      * @return teams that were generated
      */
     public static List<Team> generateTeamsForExerciseFixedTeamSize(Exercise exercise, String shortNamePrefix, String loginPrefix, int numberOfTeams, User owner,
@@ -565,14 +567,14 @@ public class ModelFactory {
     /**
      * Generates a TextAssessment event with the given parameters
      *
-     * @param eventType the type of the event
-     * @param feedbackType the type of the feedback
-     * @param segmentType the segment type of the event
-     * @param courseId the course id of the event
-     * @param userId the userid of the event
-     * @param exerciseId the exercise id of the event
+     * @param eventType       the type of the event
+     * @param feedbackType    the type of the feedback
+     * @param segmentType     the segment type of the event
+     * @param courseId        the course id of the event
+     * @param userId          the userid of the event
+     * @param exerciseId      the exercise id of the event
      * @param participationId the participation id of the event
-     * @param submissionId the submission id of the event
+     * @param submissionId    the submission id of the event
      * @return the TextAssessment event with all the properties applied
      */
     public static TextAssessmentEvent generateTextAssessmentEvent(TextAssessmentEventType eventType, FeedbackType feedbackType, TextBlockType segmentType, Long courseId,
@@ -593,11 +595,11 @@ public class ModelFactory {
     /**
      * Generates a list of different combinations of assessment events based on the given parameters
      *
-     * @param courseId the course id of the event
-     * @param userId the userid of the event
-     * @param exerciseId the exercise id of the event
+     * @param courseId        the course id of the event
+     * @param userId          the userid of the event
+     * @param exerciseId      the exercise id of the event
      * @param participationId the participation id of the event
-     * @param submissionId the submission id of the event
+     * @param submissionId    the submission id of the event
      * @return a list of TextAssessment events that are generated
      */
     public static List<TextAssessmentEvent> generateMultipleTextAssessmentEvents(Long courseId, Long userId, Long exerciseId, Long participationId, Long submissionId) {
@@ -664,7 +666,7 @@ public class ModelFactory {
     /**
      * Helper method to create an exam
      *
-     * @param course the associated course
+     * @param course   the associated course
      * @param testExam Boolean flag to determine whether it is a test exam
      * @return the created Exam
      */
@@ -918,7 +920,7 @@ public class ModelFactory {
      * Generates a minimal student participation without a specific user attached.
      *
      * @param initializationState the state of the participation
-     * @param exercise the referenced exercise of the participation
+     * @param exercise            the referenced exercise of the participation
      * @return the StudentParticipation created
      */
     public static StudentParticipation generateStudentParticipationWithoutUser(InitializationState initializationState, Exercise exercise) {
@@ -1038,10 +1040,10 @@ public class ModelFactory {
      *     <li>CustomFailedMessage: failed test with a message</li>
      * </ul>
      *
-     * @param repoName name of the repository
-     * @param successfulTestNames names of successful tests
-     * @param failedTestNames names of failed tests
-     * @param programmingLanguage programming language to use
+     * @param repoName                    name of the repository
+     * @param successfulTestNames         names of successful tests
+     * @param failedTestNames             names of failed tests
+     * @param programmingLanguage         programming language to use
      * @param enableStaticAnalysisReports should the notification include static analysis reports
      * @return TestResultDTO with dummy data
      */
@@ -1236,11 +1238,11 @@ public class ModelFactory {
     /**
      * Generate an example organization entity
      *
-     * @param name of organization
-     * @param shortName of organization
-     * @param url of organization
-     * @param description of organization
-     * @param logoUrl of organization
+     * @param name         of organization
+     * @param shortName    of organization
+     * @param url          of organization
+     * @param description  of organization
+     * @param logoUrl      of organization
      * @param emailPattern of organization
      * @return An organization entity
      */
@@ -1253,5 +1255,32 @@ public class ModelFactory {
         organization.setLogoUrl(logoUrl);
         organization.setEmailPattern(emailPattern);
         return organization;
+    }
+
+    /**
+     * Generates a Bonus instance with given arguments.
+     *
+     * @param bonusStrategy       of bonus
+     * @param weight              of bonus
+     * @param sourceGradingScaleId  of sourceGradingScale of bonus
+     * @param bonusToGradingScaleId of bonusToGradingScale bonus
+     * @return a new Bonus instance associated with the gradins scales corresonding to ids bonusToGradingScaleId and bonusToGradingScaleId.
+     */
+    public static Bonus generateBonus(BonusStrategy bonusStrategy, Double weight, long sourceGradingScaleId, long bonusToGradingScaleId) {
+        Bonus bonus = new Bonus();
+        bonus.setBonusStrategy(bonusStrategy);
+        bonus.setWeight(weight);
+        // New object is created to avoid circular dependency on json serialization.
+        var sourceGradingScale = new GradingScale();
+        sourceGradingScale.setId(sourceGradingScaleId);
+        bonus.setSourceGradingScale(sourceGradingScale);
+
+        // New object is created to avoid circular dependency on json serialization.
+        var bonusToGradingScale = new GradingScale();
+        bonusToGradingScale.setId(bonusToGradingScaleId);
+        bonus.setBonusToGradingScale(bonusToGradingScale);
+
+        return bonus;
+
     }
 }
