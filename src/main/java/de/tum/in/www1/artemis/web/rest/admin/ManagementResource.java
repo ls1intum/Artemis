432d1849758e6ec307c44f9dc83a660dc59d64a1
Julian Christl <julian.christl@tum.de>
2022-09-28 01:49:02 +0200

    move log management endpoint to admin section

--- src/main/java/de/tum/in/www1/artemis/web/rest/admin/ManagementResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/admin/ManagementResource.java
@@ -3,13 +3,18 @@ package de.tum.in.www1.artemis.web.rest.admin;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import de.tum.in.www1.artemis.security.annotations.EnforceAdmin;
 import de.tum.in.www1.artemis.service.feature.Feature;
 import de.tum.in.www1.artemis.service.feature.FeatureToggleService;
+import de.tum.in.www1.artemis.web.rest.vm.LoggerVM;
 
 @RestController
 @RequestMapping("api/admin/")
@@ -34,5 +39,30 @@ public class ManagementResource {
         featureToggleService.updateFeatureToggles(features);
 
         return new ResponseEntity<>(featureToggleService.enabledFeatures(), HttpStatus.OK);
+    }
+
+    /**
+     * GET management/logs -- Gets the current log levels.
+     * @return A list of all loggers with their log level
+     */
+    @GetMapping("management/logs")
+    @EnforceAdmin
+    public ResponseEntity<List<LoggerVM>> getList() {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        return ResponseEntity.ok(context.getLoggerList().stream().map(LoggerVM::new).toList());
+    }
+
+    /**
+     * PUT management/logs -- Changes the log level of a logger.
+     * @param jsonLogger The logger with the new log level
+     * @return The updated logger
+     */
+    @PutMapping("management/logs")
+    @EnforceAdmin
+    public ResponseEntity<LoggerVM> changeLevel(@RequestBody LoggerVM jsonLogger) {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger logger = context.getLogger(jsonLogger.getName());
+        logger.setLevel(Level.valueOf(jsonLogger.getLevel()));
+        return ResponseEntity.ok(new LoggerVM(logger));
     }
 }
