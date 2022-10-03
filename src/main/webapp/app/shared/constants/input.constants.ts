6fc3f49ccdc133f5e7f551f59b3065b9f803a3e4
ge65cer <ge65cer@mytum.de>
2022-10-03 18:30:13 +0200

    Prevent string submissions over max length

--- src/main/webapp/app/shared/constants/input.constants.ts
+++ src/main/webapp/app/shared/constants/input.constants.ts
@@ -7,7 +7,9 @@ export const DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 export const MAX_FILE_SIZE = 20 * 1024 * 1024;
 /** Maximum submission file size: 4 MB **/
 export const MAX_SUBMISSION_FILE_SIZE = 8 * 1024 * 1024;
-/** Maximum text exercise character length: 30.000 **/
+/** Maximum text exercise submission character length: 30.000 **/
 export const MAX_SUBMISSION_TEXT_LENGTH = 30 * 1000;
+/** Maximum quiz exercise short answer character length: 1.000 **/
+export const MAX_QUIZ_ANSWER_TEXT_LENGTH = 1000;
 /** Short names must start with a letter and cannot contain special characters **/
 export const SHORT_NAME_PATTERN = /^[a-zA-Z][a-zA-Z0-9]{2,}$/;
