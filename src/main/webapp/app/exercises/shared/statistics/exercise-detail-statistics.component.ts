85d1ede770edcbff0f3d0e2211eab018777e6367
JoejoeSt <johannes.stoehr@tum.de>
2022-10-03 09:47:04 +0200

    Remove statistics fix

--- src/main/webapp/app/exercises/shared/statistics/exercise-detail-statistics.component.ts
+++ src/main/webapp/app/exercises/shared/statistics/exercise-detail-statistics.component.ts
@@ -1,23 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ExerciseManagementStatisticsDto } from 'app/exercises/shared/statistics/exercise-management-statistics-dto';
 import { DoughnutChartType } from 'app/course/manage/detail/course-detail.component';
-import { Exercise, ExerciseType, getCourseFromExercise } from 'app/entities/exercise.model';
-import { Course } from 'app/entities/course.model';
+import { Exercise, ExerciseType } from 'app/entities/exercise.model';
 
 @Component({
     selector: 'jhi-exercise-detail-statistics',
     templateUrl: './exercise-detail-statistics.component.html',
 })
-export class ExerciseDetailStatisticsComponent implements OnInit {
+export class ExerciseDetailStatisticsComponent {
     @Input() exercise: Exercise;
     @Input() doughnutStats: ExerciseManagementStatisticsDto;
     @Input() exerciseType: ExerciseType;
 
-    course: Course;
-
     readonly DoughnutChartType = DoughnutChartType;
-
-    ngOnInit(): void {
-        this.course = getCourseFromExercise(this.exercise)!;
-    }
 }
