c166b4cba7bffc34d3b8834aff9779c956cabb4e
Stephan Krusche <krusche@in.tum.de>
2022-10-04 15:51:40 +0200

    Plagiarism checks: Update to JPlag 4.0 and support plagiarism checks for Swift and Kotlin (#5695)

--- src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
@@ -12,7 +12,7 @@ import java.util.*;
 import javax.activation.MimetypesFileTypeMap;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
