21b49d238195a9d65c33ddfa35ea6ab73cd6ae1b
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-10-07 17:58:49 +0200

    added canActivate for logout component

--- src/main/webapp/app/shared/layouts/navbar/navbar.route.ts
+++ src/main/webapp/app/shared/layouts/navbar/navbar.route.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 
 import { NavbarComponent } from './navbar.component';
 import { LogoutComponent } from 'app/shared/layouts/navbar/logout/logout.component';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
 export const navbarRoute: Route[] = [
     {
@@ -12,5 +13,6 @@ export const navbarRoute: Route[] = [
     {
         path: 'logout',
         component: LogoutComponent,
+        canActivate: [UserRouteAccessService],
     },
 ];
