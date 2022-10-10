e5375ee65f563eeabf60e76748bc708d7730c736
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-10-10 18:18:53 +0200

    Smaller fixes

--- src/main/java/de/tum/in/www1/artemis/domain/notification/TutorialGroupNotification.java
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/TutorialGroupNotification.java
@@ -15,7 +15,7 @@ import de.tum.in.www1.artemis.domain.enumeration.NotificationType;
 import de.tum.in.www1.artemis.domain.tutorialgroups.TutorialGroup;
 
 /**
- * A Notification concerning all students in a tutorial group.
+ * A Notification concerning all students and the assigned tutor of a tutorial group.
  */
 @Entity
 @DiscriminatorValue("T")
@@ -25,11 +25,14 @@ public class TutorialGroupNotification extends Notification {
     @JsonIgnore
     public final static Set<NotificationType> TUTORIAL_GROUP_NOTIFICATION_TYPES = Set.of(TUTORIAL_GROUP_DELETED, TUTORIAL_GROUP_UPDATED);
 
-    // ToDo: Reuse jhi_type_column to allow tutorial group notifications to be sent to only for example officially registered students and not self registered students
+    // ToDo: Idea: Reuse jhi_type_column to allow tutorial group notifications to be sent to only for example officially registered students and not self registered students
     @ManyToOne
     @JoinColumn(name = "tutorial_group_id")
     private TutorialGroup tutorialGroup;
 
+    /**
+     * Added here to make it more convenient to create tutorial group notifications without having to pass the type as an argument all the time
+     */
     @Transient
     @JsonIgnore
     public NotificationType notificationType;
@@ -55,15 +58,24 @@ public class TutorialGroupNotification extends Notification {
         this.tutorialGroup = tutorialGroup;
     }
 
+    /**
+     * Websocket notification channel for tutorial group notifications of a specific tutorial group
+     *
+     * @return the channel
+     */
     public String getTopic() {
         return "/topic/tutorial-group/" + tutorialGroup.getId() + "/notifications";
     }
 
-    @Override
-    public String toString() {
-        return "TutorialGroupNotification{" + "tutorialGroup=" + tutorialGroup + '}';
+    public NotificationType getNotificationType() {
+        return notificationType;
     }
 
+    /**
+     * Verifies that the given notification type is supported as a TutorialGroupNotification
+     *
+     * @param notificationType the notification type to verify
+     */
     public static void verifySupportedNotificationType(NotificationType notificationType) {
         if (!TUTORIAL_GROUP_NOTIFICATION_TYPES.contains(notificationType)) {
             throw new UnsupportedOperationException("Unsupported NotificationType for tutorial groups: " + notificationType);
--- src/main/java/de/tum/in/www1/artemis/repository/tutorialgroups/TutorialGroupNotificationRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/tutorialgroups/TutorialGroupNotificationRepository.java
@@ -1,16 +1,10 @@
 package de.tum.in.www1.artemis.repository.tutorialgroups;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.notification.Notification;
-import de.tum.in.www1.artemis.domain.notification.TutorialGroupNotification;
 
 @Repository
 public interface TutorialGroupNotificationRepository extends JpaRepository<Notification, Long> {
-
-    List<TutorialGroupNotification> findAllByTutorialGroupId(Long tutorialGroupId);
-
 }
