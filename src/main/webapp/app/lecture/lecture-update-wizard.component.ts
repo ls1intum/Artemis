8c5ea007e3730a8a6fd0a1ddddffab89f5ba2f1a
Sebastian Nikles <sebastian.nikles@tum.de>
2022-10-11 18:14:09 -0400

    Adds a new component for the wizard mode

---
+++ src/main/webapp/app/lecture/lecture-update-wizard.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AlertService } from 'app/core/util/alert.service';
+import { LectureService } from './lecture.service';
+import { CourseManagementService } from '../course/manage/course-management.service';
+import { Lecture } from 'app/entities/lecture.model';
+import { EditorMode } from 'app/shared/markdown-editor/markdown-editor.component';
+import { Course } from 'app/entities/course.model';
+import { KatexCommand } from 'app/shared/markdown-editor/commands/katex.command';
+import { onError } from 'app/shared/util/global.utils';
+import { ArtemisNavigationUtilService } from 'app/utils/navigation.utils';
+import { faBan, faSave, faHandshakeAngle } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'jhi-lecture-update-wizard',
+    templateUrl: './lecture-update-wizard.component.html',
+    styleUrls: ['./lecture-update-wizard.component.scss'],
+})
+export class LectureUpdateWizardComponent implements OnInit {
+    EditorMode = EditorMode;
+    lecture: Lecture;
+    isSaving: boolean;
+
+    courses: Course[];
+    startDate: string;
+    endDate: string;
+
+    domainCommandsDescription = [new KatexCommand()];
+
+    // Icons
+    faSave = faSave;
+    faBan = faBan;
+    faHandShakeAngle = faHandshakeAngle;
+
+    constructor(
+        protected alertService: AlertService,
+        protected lectureService: LectureService,
+        protected courseService: CourseManagementService,
+        protected activatedRoute: ActivatedRoute,
+        private navigationUtilService: ArtemisNavigationUtilService,
+        private router: Router,
+    ) {}
+
+    /**
+     * Life cycle hook called by Angular to indicate that Angular is done creating the component
+     */
+    ngOnInit() {
+        this.isSaving = false;
+        this.activatedRoute.parent!.data.subscribe((data) => {
+            this.lecture = new Lecture();
+
+            const course = data['course'];
+            if (course) {
+                this.lecture.course = course;
+            }
+        });
+    }
+}
