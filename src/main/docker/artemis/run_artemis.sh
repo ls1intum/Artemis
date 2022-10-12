94edb2dd4dee9aa4d6141a9b141ffa5ac6867007
Ludwig Pusl <ludwig.pusl@tum.de>
2022-10-12 20:44:28 +0200

    add possibility for remote debugging inside docker

--- src/main/docker/artemis/run_artemis.sh
+++ src/main/docker/artemis/run_artemis.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Entrypoint file for Docker Images of Artemis. The deployment of the application is set to /opt/artemis
+# TODO: maybe think about setting this not just after the environment as you mit want to also debug a prod environment?
+if [[ "$SPRING_PROFILES_ACTIVE" =~ .*"dev".* ]]; then
+    RemoteDebuggingOption="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+else
+    RemoteDebuggingOption=""
+fi
 
 echo "Starting application..."
 exec java \
+  ${RemoteDebuggingOption} \
   -Djdk.tls.ephemeralDHKeySize=2048 \
   -DLC_CTYPE=UTF-8 \
   -Dfile.encoding=UTF-8 \
