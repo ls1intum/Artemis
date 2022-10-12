0fe1ec9df7c5cace9b45b06088247a69c837fe69
JoejoeSt <johannes.stoehr@tum.de>
2022-10-12 16:55:01 +0200

    Fix cypress

--- src/test/cypress/support/commands.ts
+++ src/test/cypress/support/commands.ts
@@ -124,7 +124,7 @@ Cypress.Commands.add('getSettled', (selector, opts = {}) => {
  * Periodically refreshes the page until an element with the specified selector is found. The command fails if the time exceeds the timeout.
  */
 Cypress.Commands.add('reloadUntilFound', (selector: string, interval = 2000, timeout = 20000) => {
-    return cy.reloadUntilFound([selector], interval, timeout);
+    return cy.reloadUntilFoundSome([selector], interval, timeout);
 });
 
 /**
