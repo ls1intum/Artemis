4cf673d5692c5b3a987e4d1029b01f10de050935
JoejoeSt <johannes.stoehr@tum.de>
2022-10-13 15:04:07 +0200

    Fix assessment dashboard overview for good

--- src/main/java/de/tum/in/www1/artemis/service/AssessmentDashboardService.java
+++ src/main/java/de/tum/in/www1/artemis/service/AssessmentDashboardService.java
@@ -151,8 +151,8 @@ public class AssessmentDashboardService {
 
         // for all programming exercises and all non-programming-exercises we fetch the number of submissions here. The returned value comes in form of a list,
         // which has ExerciseMapEntries. With those for each individual exercise the number of submissions can be set.
-        programmingSubmissionsCounts = programmingExerciseRepository.countSubmissionsByExerciseIdsSubmitted(programmingExerciseIds);
-        submissionCounts = submissionRepository.countByExerciseIdsSubmittedBeforeDueDate(nonProgrammingExerciseIds);
+        programmingSubmissionsCounts = programmingExerciseRepository.countSubmissionsByExerciseIdsSubmittedIgnoreTestRun(programmingExerciseIds);
+        submissionCounts = submissionRepository.countByExerciseIdsSubmittedBeforeDueDateIgnoreTestRuns(nonProgrammingExerciseIds);
         if (examMode) {
             lateSubmissionCounts = new ArrayList<>();
         }
