8d4066fe36f44f756d4433664fb8d31cfcb0eaaa
Lucas Welscher <ga53foy@mytum.de>
2022-10-20 11:21:19 +0200

    fix imports

--- src/main/webapp/app/overview/course-exercises/course-exercise-row.component.ts
+++ src/main/webapp/app/overview/course-exercises/course-exercise-row.component.ts
@@ -4,7 +4,7 @@ import { ParticipationWebsocketService } from 'app/overview/participation-websoc
 import dayjs from 'dayjs/esm';
 import { Subscription } from 'rxjs';
 import { Course } from 'app/entities/course.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { AccountService } from 'app/core/auth/account.service';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
 import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
@@ -13,7 +13,6 @@ import { ExerciseService } from 'app/exercises/shared/exercise/exercise.service'
 import { getExerciseDueDate, participationStatus } from 'app/exercises/shared/exercise/exercise.utils';
 import { ExerciseCategory } from 'app/entities/exercise-category.model';
 import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
-import { HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-course-exercise-row',
