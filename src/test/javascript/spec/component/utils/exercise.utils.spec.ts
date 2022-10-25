ec90859afd80853aef5dcfcafbe390c4460dcd63
Benedikt Fein <fein@fim.uni-passau.de>
2022-10-25 07:08:14 +0200

    re-add removed test

--- src/test/javascript/spec/component/utils/exercise.utils.spec.ts
+++ src/test/javascript/spec/component/utils/exercise.utils.spec.ts
@@ -157,6 +157,13 @@ describe('ExerciseUtils', () => {
         expect(isResumeExerciseAvailable(exercise, participation)).toBeTrue();
     });
 
+    it('should allow students to resume the exercise between regular and individual due date', () => {
+        const exercise = exerciseWithDueDate(dayjs().subtract(1, 'hour'));
+        const participation = participationWithDueDate(dayjs().add(1, 'hour'));
+
+        expect(isResumeExerciseAvailable(exercise, participation)).toBeTrue();
+    });
+
     it('should not allow students to resume the exercise if the individual due date is in the past', () => {
         const exercise = exerciseWithDueDate(dayjs().add(1, 'hour'));
         const participation = participationWithDueDate(dayjs().subtract(1, 'hour'));
