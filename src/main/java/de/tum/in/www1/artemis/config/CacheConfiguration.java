c698ec80ff1c4577f97b384f31918c4f5aa38d7e
Simon Leiß <git@simon-leiss.de>
2022-10-31 13:06:39 +0100

    Use Hazelcast with multicast & auto join.

--- src/main/java/de/tum/in/www1/artemis/config/CacheConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/CacheConfiguration.java
@@ -14,9 +14,6 @@ import org.springframework.boot.info.GitProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,10 +40,6 @@ public class CacheConfiguration {
 
     private final ServerProperties serverProperties;
 
-    private final DiscoveryClient discoveryClient;
-
-    private Registration registration;
-
     private final ApplicationContext applicationContext;
 
     @Value("${spring.jpa.properties.hibernate.cache.hazelcast.instance_name}")
@@ -61,15 +54,9 @@ public class CacheConfiguration {
     @Value("${spring.hazelcast.localInstances:true}")
     private boolean hazelcastLocalInstances;
 
-    public CacheConfiguration(ServerProperties serverProperties, DiscoveryClient discoveryClient, ApplicationContext applicationContext) {
+    public CacheConfiguration(ServerProperties serverProperties, ApplicationContext applicationContext) {
         this.serverProperties = serverProperties;
-        this.discoveryClient = discoveryClient;
         this.applicationContext = applicationContext;
-    }
-
-    @Autowired(required = false) // ok
-    public void setRegistration(Registration registration) {
-        this.registration = registration;
     }
 
     @PreDestroy
@@ -99,61 +86,38 @@ public class CacheConfiguration {
         }
         Config config = new Config();
         config.setInstanceName(instanceName);
-        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(true);
 
-        config.getNetworkConfig().getJoin().getAutoDetectionConfig().setEnabled(false);
-
-        // Always enable TcpIp config: There has to be at least one join-config and we can not use multicast as this creates unwanted clusters
-        // If registration == null -> this will never connect to any instance as no other ip addresses are added
-        config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+        config.getNetworkConfig().getJoin().getAutoDetectionConfig().setEnabled(true);
 
         // Allows using @SpringAware and therefore Spring Services in distributed tasks
         config.setManagedContext(new SpringManagedContext(applicationContext));
         config.setClassLoader(applicationContext.getClassLoader());
-        if (registration == null) {
-            log.warn("No discovery service is set up, Hazelcast cannot create a cluster.");
-            hazelcastBindOnlyOnInterface("127.0.0.1", config);
+
+        // In the local setting (e.g. for development), everything goes through 127.0.0.1, with a different port
+        if (hazelcastLocalInstances) {
+            log.info("Application is running with the \"localInstances\" setting, Hazelcast cluster will only work with localhost instances");
+
+            // In the local configuration, the hazelcast port is the http-port + the hazelcastPort as offset
+            config.getNetworkConfig().setPort(serverProperties.getPort() + hazelcastPort); // Own port
+        }
+        else { // Production configuration, one host per instance all using the configured port
+            config.setClusterName("prod");
+            config.setInstanceName(instanceName);
+            config.getNetworkConfig().setPort(hazelcastPort); // Own port
+        }
+
+        // Bind to the interface specified in the config if the value is set
+        if (hazelcastInterface != null && !hazelcastInterface.isEmpty()) {
+            // We should not prefer IPv4, this will ensure that both IPv4 and IPv6 work as none is preferred
+            System.setProperty("hazelcast.prefer.ipv4.stack", "false");
+            hazelcastBindOnlyOnInterface(hazelcastInterface, config);
         }
         else {
-            // The serviceId is by default the application's name,
-            // see the "spring.application.name" standard Spring property
-            String serviceId = registration.getServiceId();
-            log.info("Configuring Hazelcast clustering for instanceId: {}", serviceId);
-
-            // Bind to the interface specified in the config if the value is set
-            if (hazelcastInterface != null && !hazelcastInterface.isEmpty()) {
-                // We should not prefer IPv4, this will ensure that both IPv4 and IPv6 work as none is preferred
-                System.setProperty("hazelcast.prefer.ipv4.stack", "false");
-                hazelcastBindOnlyOnInterface(hazelcastInterface, config);
-            }
-            else {
-                log.info("Binding Hazelcast to default interface");
-                hazelcastBindOnlyOnInterface("127.0.0.1", config);
-            }
-
-            // In the local setting (e.g. for development), everything goes through 127.0.0.1, with a different port
-            if (hazelcastLocalInstances) {
-                log.info("Application is running with the \"localInstances\" setting, Hazelcast cluster will only work with localhost instances");
-
-                // In the local configuration, the hazelcast port is the http-port + the hazelcastPort as offset
-                config.getNetworkConfig().setPort(serverProperties.getPort() + hazelcastPort); // Own port
-                for (ServiceInstance instance : discoveryClient.getInstances(serviceId)) {
-                    String clusterMember = instance.getHost() + ":" + (instance.getPort() + hazelcastPort); // Address where the other instance is expected
-                    log.info("Adding Hazelcast (dev) cluster member {}", clusterMember);
-                    config.getNetworkConfig().getJoin().getTcpIpConfig().addMember(clusterMember);
-                }
-            }
-            else { // Production configuration, one host per instance all using the configured port
-                config.setClusterName("prod");
-                config.setInstanceName(instanceName);
-                config.getNetworkConfig().setPort(hazelcastPort); // Own port
-                for (ServiceInstance instance : discoveryClient.getInstances(serviceId)) {
-                    String clusterMember = instance.getHost() + ":" + hazelcastPort; // Address where the other instance is expected
-                    log.info("Adding Hazelcast (prod) cluster member {}", clusterMember);
-                    config.getNetworkConfig().getJoin().getTcpIpConfig().addMember(clusterMember);
-                }
-            }
+            log.info("Binding Hazelcast to default interface");
+            hazelcastBindOnlyOnInterface("127.0.0.1", config);
         }
+
         config.getMapConfigs().put("default", initializeDefaultMapConfig(jHipsterProperties));
         config.getMapConfigs().put("de.tum.in.www1.artemis.domain.*", initializeDomainMapConfig(jHipsterProperties));
 
