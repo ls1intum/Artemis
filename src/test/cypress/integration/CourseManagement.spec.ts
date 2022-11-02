1d050256a3532c351b98f012bf2beb13a8a11d6d
Rizky Riyaldhi <rizky.riyaldhi@tum.de>
2022-11-02 12:36:06 +0000

    Remove all cypress integration tests

--- src/test/cypress/integration/CourseManagement.spec.ts
+++ src/test/cypress/integration/CourseManagement.spec.ts
@@ -36,93 +36,6 @@ describe('Course management', () => {
         cy.login(artemis.users.getAdmin(), '/');
     });
 
-    describe('Manual student selection', () => {
-        let course: Course;
-        let courseId: number;
-
-        beforeEach(() => {
-            artemisRequests.courseManagement.createCourse(false, courseName, courseShortName).then((response: Cypress.Response<Course>) => {
-                course = response.body;
-                courseId = response.body!.id!;
-            });
-        });
-
-        it('Adds a student manually to the course', () => {
-            const username = artemis.users.getStudentOne().username;
-            navigationBar.openCourseManagement();
-            courseManagementPage.openStudentOverviewOfCourse(courseId);
-            cy.intercept(GET, BASE_API + 'users/search*').as('getStudentQuery');
-            cy.intercept(POST, COURSE_BASE + '*/students/' + username).as('addStudentQuery');
-            cy.get('#typeahead-basic').type(username);
-            cy.wait('@getStudentQuery');
-            cy.get('#ngb-typeahead-0')
-                .contains(new RegExp('\\(' + username + '\\)'))
-                .should('be.visible')
-                .click();
-            cy.wait('@addStudentQuery');
-            cy.get('#registered-students').contains(username).should('be.visible');
-        });
-
-        it('Removes a student manually from the course', () => {
-            const user = artemis.users.getStudentOne();
-            const username = user.username;
-            artemisRequests.courseManagement.addStudentToCourse(course, user);
-            navigationBar.openCourseManagement();
-            courseManagementPage.openStudentOverviewOfCourse(courseId);
-            cy.get('#registered-students').contains(username).should('be.visible');
-            cy.get('#registered-students button[jhideletebutton]').should('be.visible').click();
-            cy.get('.modal #delete').click();
-            cy.get('#registered-students').contains(username).should('not.exist');
-        });
-
-        afterEach(() => {
-            if (!!courseId) {
-                artemisRequests.courseManagement.deleteCourse(courseId).its('status').should('eq', 200);
-            }
-        });
-    });
-
-    describe('Course creation', () => {
-        let courseId: number;
-
-        it('Creates a new course', () => {
-            navigationBar.openCourseManagement();
-            courseManagementPage.openCourseCreation();
-            cy.get('#field_title').type(courseName);
-            cy.get('#field_shortName').type(courseShortName);
-            cy.get('#field_testCourse').check();
-            cy.get('#field_customizeGroupNamesEnabled').uncheck();
-            cy.intercept(POST, BASE_API + 'courses').as('createCourseQuery');
-            cy.get('#save-entity').click();
-            cy.wait('@createCourseQuery').then((request: Interception) => {
-                courseId = request.response!.body.id!;
-                cy.get('#course-detail-info-bar').contains(courseName).should('be.visible');
-            });
-        });
-
-        afterEach(() => {
-            if (!!courseId) {
-                artemisRequests.courseManagement.deleteCourse(courseId).its('status').should('eq', 200);
-            }
-        });
-    });
-
-    describe('Course deletion', () => {
-        beforeEach(() => {
-            artemisRequests.courseManagement.createCourse(false, courseName, courseShortName).its('status').should('eq', 201);
-        });
-
-        it('Deletes an existing course', () => {
-            navigationBar.openCourseManagement();
-            courseManagementPage.openCourse(courseShortName);
-            cy.get('#delete-course').click();
-            cy.get(modalDeleteButton).should('be.disabled');
-            cy.get('#confirm-exercise-name').type(courseName);
-            cy.get(modalDeleteButton).should('not.be.disabled').click();
-            courseManagementPage.getCourseCard(courseShortName).should('not.exist');
-        });
-    });
-
     describe('Course icon deletion', () => {
         let courseId: number;
 
@@ -144,16 +57,6 @@ describe('Course management', () => {
                             });
                     });
                 });
-        });
-
-        it('Deletes not existing course icon', () => {
-            artemisRequests.courseManagement.createCourse(false, courseName, courseShortName).then((response) => {
-                courseId = response.body!.id!;
-                cy.visit(`/course-management/${courseId}/edit`).then(() => {
-                    cy.get('#delete-course-icon').should('not.exist');
-                    cy.get('.no-image').should('exist');
-                });
-            });
         });
 
         afterEach(() => {
