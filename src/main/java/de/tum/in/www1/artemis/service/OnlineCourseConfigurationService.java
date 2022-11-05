fde73e3b91f09c6fd5f0ad37fa98088f5f25027d
Manuel Manso <ge96sel@mytum.de>
2022-11-05 16:40:28 +0100

    Catch errors creating invalid ClientRegistration

--- src/main/java/de/tum/in/www1/artemis/service/OnlineCourseConfigurationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/OnlineCourseConfigurationService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -13,12 +15,15 @@ import org.springframework.stereotype.Service;
 import de.tum.in.www1.artemis.config.lti.CustomLti13Configurer;
 import de.tum.in.www1.artemis.domain.OnlineCourseConfiguration;
 import de.tum.in.www1.artemis.repository.OnlineCourseConfigurationRepository;
+import de.tum.in.www1.artemis.security.OAuth2JWKSService;
 
 /**
  * Service Implementation for OnlineCourseConfiguration.
  */
 @Service
 public class OnlineCourseConfigurationService implements ClientRegistrationRepository {
+
+    private final Logger log = LoggerFactory.getLogger(OAuth2JWKSService.class);
 
     private final OnlineCourseConfigurationRepository onlineCourseConfigurationRepository;
 
@@ -46,18 +51,23 @@ public class OnlineCourseConfigurationService implements ClientRegistrationRepos
      * @return the clientRegistration from the converted online course configuration
      */
     public ClientRegistration getClientRegistration(OnlineCourseConfiguration onlineCourseConfiguration) {
-        if (onlineCourseConfiguration == null || onlineCourseConfiguration.getRegistrationId() == null || onlineCourseConfiguration.getClientId() == null
-                || onlineCourseConfiguration.getAuthorizationUri() == null || onlineCourseConfiguration.getTokenUri() == null) {
+        if (onlineCourseConfiguration == null) {
             return null;
         }
-        return ClientRegistration.withRegistrationId(onlineCourseConfiguration.getRegistrationId()) // formatting
-                .clientId(onlineCourseConfiguration.getClientId()) //
-                .authorizationUri(onlineCourseConfiguration.getAuthorizationUri()) //
-                .jwkSetUri(onlineCourseConfiguration.getJwkSetUri()) //
-                .tokenUri(onlineCourseConfiguration.getTokenUri()) //
-                .redirectUri(artemisServerUrl + CustomLti13Configurer.LTI13_LOGIN_REDIRECT_PROXY_PATH) //
-                .scope("openid") //
-                .authorizationGrantType(AuthorizationGrantType.IMPLICIT) //
-                .build();
+        try {
+            return ClientRegistration.withRegistrationId(onlineCourseConfiguration.getRegistrationId()) // formatting
+                    .clientId(onlineCourseConfiguration.getClientId()) //
+                    .authorizationUri(onlineCourseConfiguration.getAuthorizationUri()) //
+                    .jwkSetUri(onlineCourseConfiguration.getJwkSetUri()) //
+                    .tokenUri(onlineCourseConfiguration.getTokenUri()) //
+                    .redirectUri(artemisServerUrl + CustomLti13Configurer.LTI13_LOGIN_REDIRECT_PROXY_PATH) //
+                    .scope("openid") //
+                    .authorizationGrantType(AuthorizationGrantType.IMPLICIT) //
+                    .build();
+        }
+        catch (IllegalArgumentException e) {
+            log.error("Could not build Client Registration from onlineCourseConfiguration");
+            return null;
+        }
     }
 }
