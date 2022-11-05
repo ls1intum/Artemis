5dbc65d99403a0711993494f958f82a170bc6300
Stephan Krusche <krusche@in.tum.de>
2022-11-05 17:29:04 +0100

    update coverage

--- jest.config.js
+++ jest.config.js
@@ -58,7 +58,7 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should be increase to at least 85%
-            statements: 84.6,
+            statements: 84.7,
             branches: 71.8,
             functions: 77.6,
             lines: 84.6,
