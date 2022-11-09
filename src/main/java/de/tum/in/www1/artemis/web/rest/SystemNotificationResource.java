862cf94ae8c7625951860b1efcf4f9a9ebdb5bab
Julian Christl <julian.christl@tum.de>
2022-11-09 21:20:57 +0100

    move system notification create, update, delete to admin endpoint

--- src/main/java/de/tum/in/www1/artemis/web/rest/SystemNotificationResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/SystemNotificationResource.java
@@ -1,13 +1,10 @@
 package de.tum.in.www1.artemis.web.rest;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -17,13 +14,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import de.tum.in.www1.artemis.domain.notification.Notification;
 import de.tum.in.www1.artemis.domain.notification.SystemNotification;
 import de.tum.in.www1.artemis.repository.SystemNotificationRepository;
-import de.tum.in.www1.artemis.security.annotations.EnforceAdmin;
 import de.tum.in.www1.artemis.service.SystemNotificationService;
-import de.tum.in.www1.artemis.web.rest.errors.BadRequestAlertException;
-import de.tum.in.www1.artemis.web.rest.util.HeaderUtil;
 import io.swagger.annotations.ApiParam;
 import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
@@ -35,11 +28,6 @@ public class SystemNotificationResource {
 
     private final Logger log = LoggerFactory.getLogger(SystemNotificationResource.class);
 
-    private static final String ENTITY_NAME = "systemNotification";
-
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
-
     private final SystemNotificationRepository systemNotificationRepository;
 
     private final SystemNotificationService systemNotificationService;
@@ -47,62 +35,6 @@ public class SystemNotificationResource {
     public SystemNotificationResource(SystemNotificationRepository systemNotificationRepository, SystemNotificationService systemNotificationService) {
         this.systemNotificationRepository = systemNotificationRepository;
         this.systemNotificationService = systemNotificationService;
-    }
-
-    /**
-     * POST /system-notifications : Create a new system notification.
-     *
-     * @param systemNotification the system notification to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new system notification, or with status 400 (Bad Request) if the system notification has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("system-notifications")
-    @EnforceAdmin
-    // TODO /admin
-    public ResponseEntity<Notification> createSystemNotification(@RequestBody SystemNotification systemNotification) throws URISyntaxException {
-        log.debug("REST request to save SystemNotification : {}", systemNotification);
-        if (systemNotification.getId() != null) {
-            throw new BadRequestAlertException("A new system notification cannot already have an ID", ENTITY_NAME, "idExists");
-        }
-        validateDatesOrThrow(systemNotification);
-        SystemNotification result = systemNotificationRepository.save(systemNotification);
-        systemNotificationService.distributeActiveAndFutureNotificationsToClients();
-        return ResponseEntity.created(new URI("/api/notifications/" + result.getId()))
-                .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString())).body(result);
-    }
-
-    /**
-     * PUT /system-notifications : Updates an existing system notification.
-     *
-     * @param systemNotification the system notification to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated notification, or with status 400 (Bad Request) if the system notification is not valid, or with
-     *         status 500 (Internal Server Error) if the system notification couldn't be updated
-     */
-    @PutMapping("system-notifications")
-    @EnforceAdmin
-    // TODO /admin
-    public ResponseEntity<SystemNotification> updateSystemNotification(@RequestBody SystemNotification systemNotification) {
-        log.debug("REST request to update SystemNotification : {}", systemNotification);
-        if (systemNotification.getId() == null) {
-            throw new BadRequestAlertException("ID must not be null", ENTITY_NAME, "idnull");
-        }
-        validateDatesOrThrow(systemNotification);
-        if (!systemNotificationRepository.existsById(systemNotification.getId())) {
-            throw new BadRequestAlertException("No system notification with this ID found", ENTITY_NAME, "idNull");
-        }
-        SystemNotification result = systemNotificationRepository.save(systemNotification);
-        systemNotificationService.distributeActiveAndFutureNotificationsToClients();
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, systemNotification.getId().toString())).body(result);
-    }
-
-    private void validateDatesOrThrow(SystemNotification systemNotification) {
-        if (systemNotification.getNotificationDate() == null || systemNotification.getExpireDate() == null) {
-            throw new BadRequestAlertException("System notification needs both a notification and expiration date.", ENTITY_NAME, "systemNotificationNeedsBothDates");
-        }
-        if (systemNotification.getNotificationDate().isAfter(systemNotification.getExpireDate())
-                || systemNotification.getNotificationDate().equals(systemNotification.getExpireDate())) {
-            throw new BadRequestAlertException("The notification date must be before the expiration date.", ENTITY_NAME, "systemNotificationNeedsNotificationBeforeExpiration");
-        }
     }
 
     /**
@@ -132,22 +64,6 @@ public class SystemNotificationResource {
         log.debug("REST request to get SystemNotification : {}", id);
         Optional<SystemNotification> systemNotification = systemNotificationRepository.findById(id);
         return ResponseUtil.wrapOrNotFound(systemNotification);
-    }
-
-    /**
-     * DELETE /system-notifications/:id : delete the "id" system notification.
-     *
-     * @param id the id of the system notification to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @DeleteMapping("system-notifications/{id}")
-    @EnforceAdmin
-    // TODO /admin
-    public ResponseEntity<Void> deleteSystemNotification(@PathVariable Long id) {
-        log.debug("REST request to delete SystemNotification : {}", id);
-        systemNotificationRepository.deleteById(id);
-        systemNotificationService.distributeActiveAndFutureNotificationsToClients();
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
     }
 
     /**
