2daffd536722afa29c3b8b9116947753cc366c9f
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-11-10 20:45:02 +0100

    Added javadoc

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/TutorialGroupSessionStatus.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/TutorialGroupSessionStatus.java
@@ -1,5 +1,15 @@
 package de.tum.in.www1.artemis.domain.enumeration;
 
+/**
+ * Describes the status of a {@link de.tum.in.www1.artemis.domain.tutorialgroups.TutorialGroupSession}
+ */
 public enum TutorialGroupSessionStatus {
-    ACTIVE, CANCELLED
+    /**
+     * The session is planned to occur
+     */
+    ACTIVE,
+    /**
+     * The session was originally planned to occur, but was cancelled
+     */
+    CANCELLED
 }
