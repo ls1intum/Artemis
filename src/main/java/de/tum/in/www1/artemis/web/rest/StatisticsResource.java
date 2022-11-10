406eafebd4284a6c131a337b69aee2bd280f508a
Julian Christl <julian.christl@tum.de>
2022-11-10 01:07:11 +0100

    move statistics endpoint to admin endpoint

--- src/main/java/de/tum/in/www1/artemis/web/rest/StatisticsResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/StatisticsResource.java
@@ -2,8 +2,6 @@ package de.tum.in.www1.artemis.web.rest;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +17,6 @@ import de.tum.in.www1.artemis.domain.enumeration.StatisticsView;
 import de.tum.in.www1.artemis.repository.CourseRepository;
 import de.tum.in.www1.artemis.repository.ExerciseRepository;
 import de.tum.in.www1.artemis.security.Role;
-import de.tum.in.www1.artemis.security.annotations.EnforceAdmin;
 import de.tum.in.www1.artemis.service.AuthorizationCheckService;
 import de.tum.in.www1.artemis.service.StatisticsService;
 import de.tum.in.www1.artemis.web.rest.dto.CourseManagementStatisticsDTO;
@@ -31,8 +28,6 @@ import de.tum.in.www1.artemis.web.rest.dto.ExerciseManagementStatisticsDTO;
 @RestController
 @RequestMapping("api/")
 public class StatisticsResource {
-
-    private final Logger log = LoggerFactory.getLogger(StatisticsResource.class);
 
     private final StatisticsService statisticsService;
 
@@ -48,22 +43,6 @@ public class StatisticsResource {
         this.authorizationCheckService = authorizationCheckService;
         this.courseRepository = courseRepository;
         this.exerciseRepository = exerciseRepository;
-    }
-
-    /**
-     * GET management/statistics/data : get the graph data in the last "span" days in the given period.
-     *
-     * @param span        the spanTime of which the amount should be calculated
-     * @param periodIndex an index indicating which time period, 0 is current week, -1 is one week in the past, -2 is two weeks in the past ...
-     * @param graphType   the type of graph the data should be fetched
-     * @return the ResponseEntity with status 200 (OK) and the data in body, or status 404 (Not Found)
-     */
-    @GetMapping("management/statistics/data")
-    @EnforceAdmin
-    // TODO /admin
-    public ResponseEntity<List<Integer>> getChartData(@RequestParam SpanType span, @RequestParam Integer periodIndex, @RequestParam GraphType graphType) {
-        log.debug("REST request to get graph data");
-        return ResponseEntity.ok(this.statisticsService.getChartData(span, periodIndex, graphType, StatisticsView.ARTEMIS, null));
     }
 
     /**
