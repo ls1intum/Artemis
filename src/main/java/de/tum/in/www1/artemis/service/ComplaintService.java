8b1374bb9daa1b78d339c79ca30811aa1c13dc49
Benedikt Fein <fein@fim.uni-passau.de>
2022-11-11 11:11:57 +0100

    make ExerciseDateService::getDueDate static

--- src/main/java/de/tum/in/www1/artemis/service/ComplaintService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ComplaintService.java
@@ -277,18 +277,7 @@ public class ComplaintService {
                     + " for an unrated result with no complaints on automatic assessment.", ENTITY_NAME, "complaintOrRequestMoreFeedbackNotGraded");
         }
 
-        ZonedDateTime relevantDueDate = studentParticipation != null && studentParticipation.getIndividualDueDate() != null ? studentParticipation.getIndividualDueDate()
-                : exercise.getDueDate();
-        List<ZonedDateTime> possibleComplaintStartDates = new ArrayList<>();
-        possibleComplaintStartDates.add(result.getCompletionDate());
-        if (relevantDueDate != null) {
-            possibleComplaintStartDates.add(relevantDueDate);
-        }
-        if (exercise.getAssessmentDueDate() != null) {
-            possibleComplaintStartDates.add(exercise.getAssessmentDueDate());
-        }
-        // At least result.getCompletionDate is present, since it was checked earlier
-        ZonedDateTime complaintStartDate = possibleComplaintStartDates.stream().max(Comparator.naturalOrder()).get();
+        final ZonedDateTime complaintStartDate = getComplaintStartDate(exercise, studentParticipation, result);
         boolean isTimeValid = ZonedDateTime.now().isBefore(complaintStartDate.plusDays(maxDays));
 
         if (!isTimeValid) {
@@ -301,6 +290,21 @@ public class ComplaintService {
                     + timeForComplaint + ".";
             throw new BadRequestAlertException(message, ENTITY_NAME, "complaintOrRequestMoreFeedbackTimeInvalid");
         }
+    }
+
+    private static ZonedDateTime getComplaintStartDate(final Exercise exercise, final StudentParticipation studentParticipation, final Result result) {
+        final List<ZonedDateTime> possibleComplaintStartDates = new ArrayList<>();
+        possibleComplaintStartDates.add(result.getCompletionDate());
+
+        final Optional<ZonedDateTime> relevantDueDate = Optional.ofNullable(studentParticipation).flatMap(ExerciseDateService::getDueDate);
+        relevantDueDate.ifPresent(possibleComplaintStartDates::add);
+
+        if (exercise.getAssessmentDueDate() != null) {
+            possibleComplaintStartDates.add(exercise.getAssessmentDueDate());
+        }
+
+        // At least result.getCompletionDate is present, since it was checked earlier
+        return possibleComplaintStartDates.stream().max(Comparator.naturalOrder()).orElseThrow();
     }
 
     /**
