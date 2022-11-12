7cb3ca45f14397332622eda11c623c26094bbd7a
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-11-12 14:47:42 +0100

    Added second trigger for detail dialog

--- src/main/webapp/app/overview/course-conversations/layout/conversation-header/conversation-header.component.ts
+++ src/main/webapp/app/overview/course-conversations/layout/conversation-header/conversation-header.component.ts
@@ -4,7 +4,10 @@ import { ConversationDto } from 'app/entities/metis/conversation/conversation.mo
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Course } from 'app/entities/course.model';
 import { ConversationAddUsersDialogComponent } from 'app/overview/course-conversations/dialogs/conversation-add-users-dialog/conversation-add-users-dialog.component';
-import { ConversationDetailDialogComponent } from 'app/overview/course-conversations/dialogs/conversation-detail-dialog/conversation-detail-dialog.component';
+import {
+    ConversationDetailDialogComponent,
+    ConversationDetailTabs,
+} from 'app/overview/course-conversations/dialogs/conversation-detail-dialog/conversation-detail-dialog.component';
 import { getAsChannelDto } from 'app/entities/metis/conversation/channel.model';
 import { MetisConversationService } from 'app/shared/metis/metis-conversation.service';
 import { from } from 'rxjs';
@@ -16,6 +19,9 @@ import { ConversationService } from 'app/shared/metis/conversations/conversation
     styleUrls: ['./conversation-header.component.scss'],
 })
 export class ConversationHeaderComponent implements OnInit {
+    INFO = ConversationDetailTabs.INFO;
+    MEMBERS = ConversationDetailTabs.MEMBERS;
+
     course: Course;
     activeConversation?: ConversationDto;
 
@@ -56,11 +62,12 @@ export class ConversationHeaderComponent implements OnInit {
         });
     }
 
-    openConversationDetailDialog(event: MouseEvent) {
+    openConversationDetailDialog(event: MouseEvent, tab: ConversationDetailTabs) {
         event.stopPropagation();
         const modalRef: NgbModalRef = this.modalService.open(ConversationDetailDialogComponent, { size: 'lg', scrollable: false, backdrop: 'static' });
         modalRef.componentInstance.course = this.course;
         modalRef.componentInstance.activeConversation = this.activeConversation;
+        modalRef.componentInstance.selectedTab = tab;
         modalRef.componentInstance.initialize();
         from(modalRef.result).subscribe(() => {
             this.metisConversationService.forceRefresh().subscribe();
