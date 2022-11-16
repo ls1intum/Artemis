23c532087dbbae84d52482132c06b70fcdde06e6
akesfeden <atakesfeden@gmail.com>
2022-11-17 01:56:43 +0300

    Add client test for ExampleSolutionComponent.

---
+++ src/test/javascript/spec/component/shared/example-solution.component.spec.ts
@@ -0,0 +1,58 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { ArtemisTestModule } from '../../test.module';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { MockComponent, MockPipe, MockProvider } from 'ng-mocks';
+import { MockActivatedRoute } from '../../helpers/mocks/activated-route/mock-activated-route';
+import { HttpResponse } from '@angular/common/http';
+import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
+import { ExampleSolutionComponent } from 'app/exercises/shared/example-solution/example-solution.component';
+import { ExampleSolutionInfo, ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
+import { ArtemisMarkdownService } from 'app/shared/markdown.service';
+import { TextExercise } from 'app/entities/text-exercise.model';
+import { Exercise } from 'app/entities/exercise.model';
+import { HeaderExercisePageWithDetailsComponent } from 'app/exercises/shared/exercise-headers/header-exercise-page-with-details.component';
+
+describe('Example Solution Component', () => {
+    let comp: ExampleSolutionComponent;
+    let fixture: ComponentFixture<ExampleSolutionComponent>;
+    let exerciseService: ExerciseService;
+    let artemisMarkdownService: ArtemisMarkdownService;
+
+    const exercise = {
+        id: 10,
+        exampleSolution: 'Example Solution',
+    } as TextExercise;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ArtemisTestModule],
+            declarations: [ExampleSolutionComponent, MockComponent(HeaderExercisePageWithDetailsComponent), MockPipe(ArtemisTranslatePipe), MockPipe(ArtemisDatePipe)],
+            providers: [{ provide: ActivatedRoute, useValue: new MockActivatedRoute({ exerciseId: 10 }) }, MockProvider(ExerciseService), MockProvider(ArtemisMarkdownService)],
+        }).compileComponents();
+        fixture = TestBed.createComponent(ExampleSolutionComponent);
+        comp = fixture.componentInstance;
+        exerciseService = fixture.debugElement.injector.get(ExerciseService);
+        artemisMarkdownService = fixture.debugElement.injector.get(ArtemisMarkdownService);
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should initialize', () => {
+        const exerciseServiceSpy = jest.spyOn(exerciseService, 'getExerciseForExampleSolution').mockReturnValue(of({ body: exercise } as HttpResponse<Exercise>));
+        const exampleSolutionInfo = { isProgrammingExerciseExampleSolutionPublished: true } as ExampleSolutionInfo;
+
+        const extractExampleSolutionInfoSpy = jest.spyOn(exerciseService, 'extractExampleSolutionInfo').mockReturnValue(exampleSolutionInfo);
+        fixture.detectChanges();
+        expect(exerciseServiceSpy).toHaveBeenCalledOnce();
+        expect(exerciseServiceSpy).toHaveBeenCalledWith(exercise.id);
+
+        expect(extractExampleSolutionInfoSpy).toHaveBeenCalledOnce();
+        expect(extractExampleSolutionInfoSpy).toHaveBeenCalledWith(exercise, artemisMarkdownService);
+
+        expect(comp.exampleSolutionInfo).toEqual(exampleSolutionInfo);
+    });
+});
