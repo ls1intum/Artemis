fe9a11511a1203018a063d0d9749b12cfb31ef5a
Sebastian Nikles <sebastian.nikles@tum.de>
2022-11-20 14:28:30 -0500

    Fixes tests

--- src/test/javascript/spec/component/programming-exercise/wizard-mode/programming-exercise-wizard-problem.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/wizard-mode/programming-exercise-wizard-problem.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { MockPipe } from 'ng-mocks';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
