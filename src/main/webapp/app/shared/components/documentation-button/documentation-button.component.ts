a9526d5337205c47ecc7f282c8ce8f925a4f54a3
Sebastian Nikles <sebastian.nikles@tum.de>
2022-11-19 19:27:54 -0500

    Adds tooltips to the buttons

--- src/main/webapp/app/shared/components/documentation-button/documentation-button.component.ts
+++ src/main/webapp/app/shared/components/documentation-button/documentation-button.component.ts
@@ -3,29 +3,34 @@ import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 // The routes here are used to build the link to the documentation.
 // Therefore, it's important that they exactly match the url to the subpage of the documentation.
+// Additionally, the case names must match the keys in documentationLinks.json for the tooltip.
 export enum DocumentationType {
-    Course = 'courses/customizable/',
-    Lecture = 'lectures/',
-    Exercise = 'exercises/',
-    Quiz = 'exercises/quiz/',
-    Model = 'exercises/modeling/',
-    Programming = 'exercises/programming/',
-    Text = 'exercises/textual/',
-    FileUpload = 'exercises/file-upload/',
-    Notifications = 'notifications/',
-    LearningGoals = 'lectures/#learning-goals',
-    Communications = 'communication/',
-    Exams = 'exam_mode/',
-    PlagiarismChecks = 'plagiarism-check/',
-    Grading = 'grading/',
-    Units = 'lectures/#lecture-units',
-    Assessment = 'exercises/assessment/',
+    Course = <any>'courses/customizable/',
+    Lecture = <any>'lectures/',
+    Exercise = <any>'exercises/',
+    Quiz = <any>'exercises/quiz/',
+    Model = <any>'exercises/modeling/',
+    Programming = <any>'exercises/programming/',
+    Text = <any>'exercises/textual/',
+    FileUpload = <any>'exercises/file-upload/',
+    Notifications = <any>'notifications/',
+    LearningGoals = <any>'lectures/#learning-goals',
+    Communications = <any>'communication/',
+    Exams = <any>'exam_mode/',
+    PlagiarismChecks = <any>'plagiarism-check/',
+    Grading = <any>'grading/',
+    Units = <any>'lectures/#lecture-units',
+    Assessment = <any>'exercises/assessment/',
 }
 
 @Component({
     selector: 'jhi-documentation-button',
     styleUrls: ['./documentation-button.component.scss'],
-    template: ` <button type="button" class="text-primary documentation-button" (click)="openDocumentation()"><fa-icon [icon]="faCircleInfo"></fa-icon></button> `,
+    template: `
+        <button type="button" class="text-primary documentation-button" (click)="openDocumentation()">
+            <fa-icon [icon]="faCircleInfo" ngbTooltip="{{ getTooltipForType() | artemisTranslate }}"></fa-icon>
+        </button>
+    `,
 })
 export class DocumentationButtonComponent {
     baseUrl = 'https://docs.artemis.ase.in.tum.de/user/';
@@ -36,5 +41,9 @@ export class DocumentationButtonComponent {
 
     openDocumentation() {
         window.open(this.baseUrl + this.type, '_blank');
+    }
+
+    getTooltipForType() {
+        return 'artemisApp.documentationLinks.' + DocumentationType[this.type].toLowerCase();
     }
 }
