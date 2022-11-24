fc8f1fd06a7195b7fa07f9d7f57372bb29e04202
Ludwig Pusl <ludwig.pusl@tum.de>
2022-11-24 11:52:57 +0100

    BitbucketServiceTest: user prefixes

--- src/test/java/de/tum/in/www1/artemis/service/BitbucketServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/BitbucketServiceTest.java
@@ -26,6 +26,8 @@ import de.tum.in.www1.artemis.repository.ProgrammingExerciseRepository;
 
 class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
 
+    private static final String TEST_PREFIX = "bitbucketservicetest";
+
     @Autowired
     private ProgrammingExerciseRepository programmingExerciseRepository;
 
@@ -34,6 +36,7 @@ class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraT
 
     @BeforeEach
     void initTestCase() {
+        database.addUsers(TEST_PREFIX, 1, 0, 0, 0);
         bitbucketRequestMockProvider.enableMockingOfRequests();
     }
 
@@ -44,7 +47,7 @@ class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraT
     }
 
     @Test
-    @WithMockUser(username = "student1")
+    @WithMockUser(username = TEST_PREFIX + "student1")
     void testHealthRunning() throws URISyntaxException, JsonProcessingException {
         bitbucketRequestMockProvider.mockHealth("RUNNING", HttpStatus.OK);
         var health = versionControlService.health();
@@ -53,7 +56,7 @@ class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraT
     }
 
     @Test
-    @WithMockUser(username = "student1")
+    @WithMockUser(username = TEST_PREFIX + "student1")
     void testHealthNotRunning() throws URISyntaxException, JsonProcessingException {
         bitbucketRequestMockProvider.mockHealth("PAUSED", HttpStatus.OK);
         var health = versionControlService.health();
@@ -62,7 +65,7 @@ class BitbucketServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraT
     }
 
     @Test
-    @WithMockUser(username = "student1")
+    @WithMockUser(username = TEST_PREFIX + "student1")
     void testHealthException() throws URISyntaxException, JsonProcessingException {
         bitbucketRequestMockProvider.mockHealth("RUNNING", HttpStatus.INTERNAL_SERVER_ERROR);
         var health = versionControlService.health();
