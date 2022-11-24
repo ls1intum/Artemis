9c6e27de9098d34b2c261e9e6ca1b7d89f89448d
Rizky Riyaldhi <rizky.riyaldhi@tum.de>
2022-11-24 14:28:21 +0000

    Add Test Cases

--- src/test/javascript/spec/component/short-answer-question/short-answer-question.component.spec.ts
+++ src/test/javascript/spec/component/short-answer-question/short-answer-question.component.spec.ts
@@ -5,11 +5,12 @@ import { NgbPopover, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { ShortAnswerQuestionComponent } from 'app/exercises/quiz/shared/questions/short-answer-question/short-answer-question.component';
 import { QuizScoringInfoStudentModalComponent } from 'app/exercises/quiz/shared/questions/quiz-scoring-infostudent-modal/quiz-scoring-info-student-modal.component';
 import { ShortAnswerQuestion } from 'app/entities/quiz/short-answer-question.model';
-import { ShortAnswerSpot } from 'app/entities/quiz/short-answer-spot.model';
+import { ShortAnswerSpot, SpotType } from 'app/entities/quiz/short-answer-spot.model';
 import { ShortAnswerMapping } from 'app/entities/quiz/short-answer-mapping.model';
 import { ShortAnswerSolution } from 'app/entities/quiz/short-answer-solution.model';
 import { ShortAnswerSubmittedText } from 'app/entities/quiz/short-answer-submitted-text.model';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { By } from '@angular/platform-browser';
 
 const question = new ShortAnswerQuestion();
 question.id = 1;
@@ -70,26 +71,39 @@ describe('ShortAnswerQuestionComponent', () => {
     it('should set submitted texts', () => {
         const alternativeQuestion = new ShortAnswerQuestion();
         alternativeQuestion.id = 10;
-        const text = 'Please explain this question about [-spot 1]';
+        const text = 'Please explain this question about [-spot 1]. There are [-spot-number 2] or [-spot-number 3] questions.';
         alternativeQuestion.text = text;
         alternativeQuestion.hint = 'new Hint!';
         alternativeQuestion.explanation = 'This is a very good explanation!';
-        const spot = new ShortAnswerSpot();
-        spot.spotNr = 1;
-        alternativeQuestion.spots = [spot];
+        const spot1 = new ShortAnswerSpot();
+        spot1.spotNr = 1;
+        const spot2 = new ShortAnswerSpot();
+        spot2.type = SpotType.NUMBER;
+        spot2.spotNr = 2;
+        const spot3 = new ShortAnswerSpot();
+        spot3.type = SpotType.NUMBER;
+        spot3.spotNr = 3;
+        alternativeQuestion.spots = [spot1, spot2, spot3];
         component.fnOnSubmittedTextUpdate = function () {
             return true;
         };
-        const returnValue = { value: text } as unknown as HTMLElement;
-        const getNavigationStub = jest.spyOn(document, 'getElementById').mockReturnValue(returnValue);
+        const getNavigationStub = jest
+            .spyOn(document, 'getElementById')
+            .mockReturnValueOnce({ value: text } as unknown as HTMLElement)
+            .mockReturnValueOnce({ value: '10' } as unknown as HTMLElement)
+            .mockReturnValueOnce({ value: 'test' } as unknown as HTMLElement);
 
         component.question = alternativeQuestion;
         component.setSubmittedText();
 
-        expect(getNavigationStub).toHaveBeenCalledOnce();
-        expect(component.submittedTexts).toHaveLength(1);
+        expect(getNavigationStub).toHaveBeenCalledTimes(3);
+        expect(component.submittedTexts).toHaveLength(3);
         expect(component.submittedTexts[0].text).toStrictEqual(text);
-        expect(component.submittedTexts[0].spot).toStrictEqual(spot);
+        expect(component.submittedTexts[0].spot).toStrictEqual(spot1);
+        expect(component.submittedTexts[1].text).toStrictEqual('10');
+        expect(component.submittedTexts[1].spot).toStrictEqual(spot2);
+        expect(component.submittedTexts[2].text).toStrictEqual('');
+        expect(component.submittedTexts[2].spot).toStrictEqual(spot3);
     });
 
     it('should show sample solution', () => {
@@ -188,5 +202,69 @@ describe('ShortAnswerQuestionComponent', () => {
         spot.invalid = false;
         alternativeQuestion.invalid = true;
         expect(component.classifyInputField(tag)).toBe('invalid');
+    });
+
+    it('should get spot type for spot', () => {
+        const shortAnswerQuestion = new ShortAnswerQuestion();
+        const spot1 = new ShortAnswerSpot();
+        spot1.spotNr = 1;
+        const spot2 = new ShortAnswerSpot();
+        spot2.type = SpotType.TEXT;
+        spot2.spotNr = 2;
+        const spot3 = new ShortAnswerSpot();
+        spot3.type = SpotType.NUMBER;
+        spot3.spotNr = 3;
+        shortAnswerQuestion.spots = [spot1, spot2, spot3];
+        component.shortAnswerQuestion = shortAnswerQuestion;
+        expect(component.getSpotType('[-spot 1]')).toEqual(SpotType.TEXT);
+        expect(component.getSpotType('[-spot 2]')).toEqual(SpotType.TEXT);
+        expect(component.getSpotType('[-spot-number 3]')).toEqual(SpotType.NUMBER);
+    });
+
+    it('should get spot input type for spot', () => {
+        const shortAnswerQuestion = new ShortAnswerQuestion();
+        const spot1 = new ShortAnswerSpot();
+        spot1.spotNr = 1;
+        const spot2 = new ShortAnswerSpot();
+        spot2.type = SpotType.TEXT;
+        spot2.spotNr = 2;
+        const spot3 = new ShortAnswerSpot();
+        spot3.type = SpotType.NUMBER;
+        spot3.spotNr = 3;
+        shortAnswerQuestion.spots = [spot1, spot2, spot3];
+        component.shortAnswerQuestion = shortAnswerQuestion;
+        expect(component.getSpotInputType('[-spot 1]')).toEqual('text');
+        expect(component.getSpotInputType('[-spot 2]')).toEqual('text');
+        expect(component.getSpotInputType('[-spot-number 3]')).toEqual('number');
+    });
+
+    it('should remove input if spot type is number and input is invalid', () => {
+        const shortAnswerQuestion = new ShortAnswerQuestion();
+        const spot1 = new ShortAnswerSpot();
+        spot1.spotNr = 1;
+        const spot2 = new ShortAnswerSpot();
+        spot2.type = SpotType.NUMBER;
+        spot2.spotNr = 2;
+        const spot3 = new ShortAnswerSpot();
+        spot3.type = SpotType.NUMBER;
+        spot3.spotNr = 3;
+        shortAnswerQuestion.id = 0;
+        shortAnswerQuestion.spots = [spot1, spot2, spot3];
+        component.shortAnswerQuestion = shortAnswerQuestion;
+        component.textParts = [['[-spot 1]', '[-spot-number 2]', '[-spot-number 3]']];
+        component.showResult = false;
+        fixture.detectChanges();
+        const input1 = fixture.debugElement.query(By.css('#solution-0-0-0')).nativeElement;
+        input1.value = 'text';
+        input1.dispatchEvent(new FocusEvent('blur'));
+        expect(input1.value).toEqual('text');
+        const input2 = fixture.debugElement.query(By.css('#solution-0-1-0')).nativeElement;
+        input2.value = 'text';
+        input2.dispatchEvent(new FocusEvent('blur'));
+        expect(input2.value).toEqual('');
+        const input3 = fixture.debugElement.query(By.css('#solution-0-2-0')).nativeElement;
+        input3.value = '-1.2345';
+        input3.dispatchEvent(new FocusEvent('blur'));
+        expect(input3.value).toEqual('-1.2345');
     });
 });
