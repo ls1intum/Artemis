e3b488700ae3822531a48e263589c75b6295013f
Lucas Welscher <micky.321@web.de>
2022-11-26 14:17:23 +0100

    Update ConversationService.java

--- src/main/java/de/tum/in/www1/artemis/service/metis/ConversationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/metis/ConversationService.java
@@ -76,7 +76,7 @@ public class ConversationService {
         List<Conversation> existingConversations = conversationRepository.findConversationsOfUserWithConversationParticipants(course.getId(), user.getId());
         Optional<Conversation> existingConversation = existingConversations.stream().filter((conversation1) -> conversation1.getConversationParticipants().stream()
                 .anyMatch(conversationParticipant -> conversationParticipant.getUser().getId().equals(
-                        conversation.getConversationParticipants()..iterator().next().getUser().getId())))
+                        conversation.getConversationParticipants().iterator().next().getUser().getId())))
                 .findAny();
 
         if (!existingConversation.isPresent()) {
