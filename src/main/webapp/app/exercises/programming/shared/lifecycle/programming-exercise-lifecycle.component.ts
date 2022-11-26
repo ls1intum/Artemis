6c9aeafb9ef18e14e92828f5821af5279b5335f9
JoejoeSt <johannes.stoehr@tum.de>
2022-11-26 11:13:22 +0100

    Improve timeline

--- src/main/webapp/app/exercises/programming/shared/lifecycle/programming-exercise-lifecycle.component.ts
+++ src/main/webapp/app/exercises/programming/shared/lifecycle/programming-exercise-lifecycle.component.ts
@@ -3,7 +3,7 @@ import dayjs from 'dayjs/esm';
 import { TranslateService } from '@ngx-translate/core';
 import { AssessmentType } from 'app/entities/assessment-type.model';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
-import { faCogs, faHandshake, faHandshakeSlash, faLink, faLinkSlash, faUserCheck, faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { faCogs, faUserCheck, faUserSlash } from '@fortawesome/free-solid-svg-icons';
 import { ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
 import { IncludedInOverallScore } from 'app/entities/exercise.model';
 
@@ -24,10 +24,6 @@ export class ProgrammingExerciseLifecycleComponent implements OnInit, OnChanges 
     faCogs = faCogs;
     faUserCheck = faUserCheck;
     faUserSlash = faUserSlash;
-    faHandshake = faHandshake;
-    faHandshakeSlash = faHandshakeSlash;
-    faLink = faLink;
-    faLinkSlash = faLinkSlash;
 
     constructor(private translator: TranslateService, private exerciseService: ExerciseService) {}
 
@@ -71,9 +67,11 @@ export class ProgrammingExerciseLifecycleComponent implements OnInit, OnChanges 
         if (this.exercise.assessmentType === AssessmentType.SEMI_AUTOMATIC) {
             this.exercise.assessmentType = AssessmentType.AUTOMATIC;
             this.exercise.assessmentDueDate = undefined;
+            this.exercise.allowComplaintsForAutomaticAssessments = false;
         } else {
             this.exercise.assessmentType = AssessmentType.SEMI_AUTOMATIC;
             this.exercise.allowComplaintsForAutomaticAssessments = false;
+            this.exercise.allowManualFeedbackRequests = false;
         }
     }
 
@@ -164,6 +162,9 @@ export class ProgrammingExerciseLifecycleComponent implements OnInit, OnChanges 
                     : 'artemisApp.programmingExercise.timeline.alertNewExampleSolutionPublicationDateAsReleaseDate';
             alert(this.translator.instant(message));
             this.exercise.exampleSolutionPublicationDate = newReleaseOrDueDate;
+            if (!newReleaseOrDueDate) {
+                this.exercise.releaseTestsWithExampleSolution = false;
+            }
         }
     }
 }
