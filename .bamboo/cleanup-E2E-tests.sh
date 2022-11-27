2c712a85d2fe72cf85cabc16ef4a8fae278e5c98
Ludwig Pusl <ludwig.pusl@tum.de>
2022-11-27 19:02:59 +0100

    better cleanup

--- .bamboo/cleanup-E2E-tests.sh
+++ .bamboo/cleanup-E2E-tests.sh
@@ -1,4 +1,3 @@
 cd src/main/docker/cypress
 
-docker-compose --env-file ./bamboo-E2E-testing.env stop
-docker-compose --env-file ./bamboo-E2E-testing.env rm -f
+docker-compose --env-file ./bamboo-E2E-testing.env down -v
