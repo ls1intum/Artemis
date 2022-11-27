280d8017a8b745395099b8c5f694df5f36370202
JoejoeSt <johannes.stoehr@tum.de>
2022-11-27 22:41:48 +0100

    Fix client tests

--- src/test/javascript/spec/component/shared/exercise-update-warning.component.spec.ts
+++ src/test/javascript/spec/component/shared/exercise-update-warning.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { ArtemisTestModule } from '../../test.module';
 import { ExerciseUpdateWarningComponent } from 'app/exercises/shared/exercise-update-warning/exercise-update-warning.component';
 import { MockDirective, MockPipe, MockProvider } from 'ng-mocks';
