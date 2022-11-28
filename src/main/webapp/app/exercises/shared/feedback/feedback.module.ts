68f0146a15d84dd307dcabfbf0b0ea7e5cf6942c
Simon Huang <simon.huang@tum.de>
2022-11-28 12:09:52 +0100

    remove WIP component

--- src/main/webapp/app/exercises/shared/feedback/feedback.module.ts
+++ src/main/webapp/app/exercises/shared/feedback/feedback.module.ts
@@ -3,13 +3,12 @@ import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { ArtemisProgrammingExerciseActionsModule } from 'app/exercises/programming/shared/actions/programming-exercise-actions.module';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { FeedbackCollapseComponent } from 'app/exercises/shared/feedback/feedback-collapse.component';
-import { FeedbackListProgrammingComponent } from 'app/exercises/shared/feedback/feedback-list-programming.component';
 import { BarChartModule } from '@swimlane/ngx-charts';
 import { FeedbackListComponent } from 'app/exercises/shared/feedback/feedback-list.component';
 
 @NgModule({
     imports: [ArtemisSharedModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedComponentModule, BarChartModule],
-    declarations: [FeedbackCollapseComponent, FeedbackListComponent, FeedbackListProgrammingComponent],
-    exports: [FeedbackCollapseComponent, FeedbackListComponent, FeedbackListProgrammingComponent],
+    declarations: [FeedbackCollapseComponent, FeedbackListComponent],
+    exports: [FeedbackCollapseComponent, FeedbackListComponent],
 })
 export class ArtemisFeedbackModule {}
