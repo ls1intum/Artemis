14f1d4f8cea9fd977f1bd23db62efbb662bd6da3
Johannes Stöhr <38322605+JohannesStoehr@users.noreply.github.com>
2022-12-02 14:52:58 +0100

    General: Check missing release date when creating or updating exercise (#5895)

--- src/main/webapp/app/exercises/shared/exercise-update-warning/exercise-update-warning.service.ts
+++ src/main/webapp/app/exercises/shared/exercise-update-warning/exercise-update-warning.service.ts
@@ -3,6 +3,7 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Exercise } from 'app/entities/exercise.model';
 import { GradingInstruction } from 'app/exercises/shared/structured-grading-criterion/grading-instruction.model';
 import { ExerciseUpdateWarningComponent } from 'app/exercises/shared/exercise-update-warning/exercise-update-warning.component';
+import dayjs from 'dayjs/esm';
 
 @Injectable({ providedIn: 'root' })
 export class ExerciseUpdateWarningService {
@@ -11,7 +12,10 @@ export class ExerciseUpdateWarningService {
     instructionDeleted: boolean;
     creditChanged: boolean;
     usageCountChanged: boolean;
+    immediateReleaseWarning: string;
     isSaving: boolean;
+
+    isExamMode: boolean;
 
     constructor(private modalService: NgbModal) {}
 
@@ -24,28 +28,43 @@ export class ExerciseUpdateWarningService {
         modalRef.componentInstance.instructionDeleted = this.instructionDeleted;
         modalRef.componentInstance.creditChanged = this.creditChanged;
         modalRef.componentInstance.usageCountChanged = this.usageCountChanged;
+        modalRef.componentInstance.immediateReleaseWarning = this.immediateReleaseWarning;
 
         return modalRef;
     }
 
     /**
-     * check if the changes affect the existing results
-     *  if it affects the results, then open warning modal before updating the exercise
+     * check if there might be unwanted changes and inform the instructor by opening warning modal before updating the exercise
      *
      * @param exercise the exercise for which the modal should be shown
      * @param backupExercise the copy of exercise for which the modal should be shown
+     * @param isExamMode flag that indicates if the exercise is part of an exam
      */
-    checkExerciseBeforeUpdate(exercise: Exercise, backupExercise: Exercise): Promise<NgbModalRef> {
-        this.instructionDeleted = false;
-        this.creditChanged = false;
-        this.usageCountChanged = false;
+    checkExerciseBeforeUpdate(exercise: Exercise, backupExercise: Exercise, isExamMode: boolean): Promise<NgbModalRef> {
+        if (exercise.course?.testCourse) {
+            return new Promise<NgbModalRef>((resolve) => resolve(this.ngbModalRef));
+        }
+
+        this.initializeVariables(isExamMode);
         this.loadExercise(exercise, backupExercise);
+        this.checkImmediateRelease(exercise, backupExercise);
         return new Promise<NgbModalRef>((resolve) => {
-            if (this.creditChanged || this.instructionDeleted || this.usageCountChanged) {
+            if (this.creditChanged || this.instructionDeleted || this.usageCountChanged || this.immediateReleaseWarning) {
                 this.ngbModalRef = this.open(ExerciseUpdateWarningComponent as Component);
             }
             resolve(this.ngbModalRef);
         });
+    }
+
+    /**
+     * Resets all possible warnings and checks if the exercise is part of an exam
+     */
+    initializeVariables(isExamMode: boolean) {
+        this.instructionDeleted = false;
+        this.creditChanged = false;
+        this.usageCountChanged = false;
+        this.immediateReleaseWarning = '';
+        this.isExamMode = isExamMode;
     }
 
     /**
@@ -61,7 +80,7 @@ export class ExerciseUpdateWarningService {
      */
     loadExercise(exercise: Exercise, backupExercise: Exercise): void {
         // check each grading criterion
-        backupExercise.gradingCriteria!.forEach((backupCriterion) => {
+        backupExercise.gradingCriteria?.forEach((backupCriterion) => {
             // find same grading criterion in backup exercise (necessary if the order has been changed)
             const updatedCriterion = exercise.gradingCriteria?.find((criterion) => criterion.id === backupCriterion.id);
 
@@ -86,16 +105,26 @@ export class ExerciseUpdateWarningService {
      * 1. compare backupInstruction and instruction
      * 2. set flags based on detected changes
      *
-     * @param instruction changed instruction
-     * @param backupInstruction original not changed instruction
+     * @param gradingInstruction changed instruction
+     * @param backupGradingInstruction original not changed instruction
      */
     checkGradingInstruction(gradingInstruction: GradingInstruction, backupGradingInstruction: GradingInstruction): void {
         // checking whether structured grading instruction credits or usageCount changed
-        if (gradingInstruction.credits !== backupGradingInstruction.credits) {
-            this.creditChanged = true;
-        }
-        if (gradingInstruction.usageCount !== backupGradingInstruction.usageCount) {
-            this.usageCountChanged = true;
+        this.creditChanged = gradingInstruction.credits !== backupGradingInstruction.credits;
+        this.usageCountChanged = gradingInstruction.usageCount !== backupGradingInstruction.usageCount;
+    }
+
+    /**
+     * Checks if the exercise will be released immediately to students and if students did not see the exercise before
+     *
+     * @param exercise the updated exercise
+     * @param backupExercise the optional exercise before the update that might already be released
+     */
+    checkImmediateRelease(exercise: Exercise, backupExercise: Exercise) {
+        const noReleaseDate = !exercise.releaseDate || !dayjs(exercise.releaseDate).isValid();
+        const creationOrReleaseDateBefore = !exercise.id || (backupExercise.releaseDate && dayjs(backupExercise.releaseDate).isValid());
+        if (noReleaseDate && !this.isExamMode && creationOrReleaseDateBefore) {
+            this.immediateReleaseWarning = exercise.startDate ? 'artemisApp.exercise.noReleaseDateWarning' : 'artemisApp.exercise.noReleaseAndStartDateWarning';
         }
     }
 }
