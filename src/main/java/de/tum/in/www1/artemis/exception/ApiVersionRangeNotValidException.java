8e653a054a4d1636ae0bb447938d65f2ba7ae241
Julian Christl <julian.christl@tum.de>
2022-12-08 13:18:08 +0100

    fix code review

--- src/main/java/de/tum/in/www1/artemis/exception/ApiVersionRangeNotValidException.java
+++ src/main/java/de/tum/in/www1/artemis/exception/ApiVersionRangeNotValidException.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class ApiVersionRangeNotValidException extends RuntimeException {
 
-    @Override
-    public String toString() {
-        return super.toString() + "\n The version range must contain one or two items. None or more were found.";
+    public ApiVersionRangeNotValidException() {
+        super("The version range must contain one or two items. None or more were found.");
     }
 }
