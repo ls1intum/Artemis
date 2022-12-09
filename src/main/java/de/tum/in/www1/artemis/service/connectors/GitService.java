044cfb134996a423a6a6ca617424482b85d07c67
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-12-09 17:23:51 +0100

    Update src/main/java/de/tum/in/www1/artemis/service/connectors/GitService.java
    Co-authored-by: Benedikt Fein <fein@fim.uni-passau.de>

--- src/main/java/de/tum/in/www1/artemis/service/connectors/GitService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/GitService.java
@@ -450,7 +450,7 @@ public class GitService {
                 // cleanup the folder to avoid problems in the future.
                 // 'deleteQuietly' is the same as 'deleteDirectory' but is not throwing an exception, thus we avoid another try-catch block.
                 if (!FileUtils.deleteQuietly(localPath.toFile())) {
-                    log.error("Could not delete directory during clone: {}", localPath.toAbsolutePath());
+                    log.error("Could not delete directory after failed clone: {}", localPath.toAbsolutePath());
                 }
                 throw new GitException(e);
             }
