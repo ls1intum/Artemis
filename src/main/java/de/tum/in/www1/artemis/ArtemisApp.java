091025ccbfbac296e328406a5effea37d9d20212
Ludwig Pusl <ludwig.pusl@tum.de>
2022-12-09 18:12:11 +0100

    reverted all configuration changes as they were moved to separate branch

--- src/main/java/de/tum/in/www1/artemis/ArtemisApp.java
+++ src/main/java/de/tum/in/www1/artemis/ArtemisApp.java
@@ -72,7 +72,6 @@ public class ArtemisApp {
         if (env.getProperty("server.ssl.key-store") != null) {
             protocol = "https";
         }
-        // TODO: recheck if the line can be deleted (port config value is never used anywhere else)
         String serverPort = env.getProperty("server.port");
         String version = buildProperties.getVersion();
         String gitCommitId = gitProperties.getShortCommitId();
