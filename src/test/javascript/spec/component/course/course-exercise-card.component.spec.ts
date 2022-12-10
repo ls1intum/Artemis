89f1c5b18e9af345168e5f6557e76c708c820b01
Stephan Krusche <krusche@in.tum.de>
2022-12-10 15:32:00 +0100

    Development: Update client dependencies and fix console log errors in client tests

--- src/test/javascript/spec/component/course/course-exercise-card.component.spec.ts
+++ src/test/javascript/spec/component/course/course-exercise-card.component.spec.ts
@@ -4,13 +4,14 @@ import { CourseManagementService } from 'app/course/manage/course-management.ser
 import { MockDirective, MockProvider } from 'ng-mocks';
 import { ArtemisTestModule } from '../../test.module';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 describe('Course Exercise Card Component', () => {
     let fixture: ComponentFixture<CourseExerciseCardComponent>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [ArtemisTestModule],
+            imports: [ArtemisTestModule, MockDirective(NgbCollapse)],
             declarations: [CourseExerciseCardComponent, MockDirective(TranslateDirective)],
             providers: [MockProvider(CourseManagementService)],
         }).compileComponents();
@@ -20,5 +21,6 @@ describe('Course Exercise Card Component', () => {
     it('should initialize', () => {
         fixture.detectChanges();
         expect(CourseExerciseCardComponent).toBeDefined();
+        // TODO: implement some proper client tests
     });
 });
