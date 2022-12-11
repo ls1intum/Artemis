197f5e24bef1c85e62cbd0dc2c8043c6233593cc
Stefan Waldhauser <hello@stefanwaldhauser.me>
2022-12-11 15:37:15 +0100

    Delete empty files

--- src/main/webapp/app/overview/course-conversations/other/group-chat-icon/group-chat-icon.component.ts
+++ src/main/webapp/app/overview/course-conversations/other/group-chat-icon/group-chat-icon.component.ts
@@ -4,7 +4,6 @@ import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
 @Component({
     selector: 'jhi-group-chat-icon',
     templateUrl: './group-chat-icon.component.html',
-    styleUrls: ['./group-chat-icon.component.scss'],
 })
 export class GroupChatIconComponent {
     // icons
