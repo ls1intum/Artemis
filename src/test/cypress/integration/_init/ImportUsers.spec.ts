a663fa1a2566cd28576d468d871867fd1d7b96a9
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-12-11 13:02:37 +0100

    make flakes more likely

--- src/test/cypress/integration/_init/ImportUsers.spec.ts
+++ src/test/cypress/integration/_init/ImportUsers.spec.ts
@@ -9,7 +9,8 @@ it('Logs in once with all required users', () => {
     cy.login(users.getStudentOne());
     cy.login(users.getStudentTwo());
     cy.login(users.getStudentThree());
-    if (Math.random() > 0.5) {
-        throw new Error('This is a random error to test the error logging');
+    if (Math.random() > 0.3) {
+        console.error('This is a random error to test the retry logic');
+        throw new Error('This is a random error to test the retry logic');
     }
 });
