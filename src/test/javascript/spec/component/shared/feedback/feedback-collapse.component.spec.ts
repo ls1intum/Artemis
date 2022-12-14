e58b52d090ce000c3e434f615de15317c50b4a51
Simon Huang <simon.huang@tum.de>
2022-12-14 14:18:25 +0100

    rename variables

--- src/test/javascript/spec/component/shared/feedback/feedback-collapse.component.spec.ts
+++ src/test/javascript/spec/component/shared/feedback/feedback-collapse.component.spec.ts
@@ -5,43 +5,43 @@ describe('FeedbackCollapseComponent', () => {
      * Same value as in feedback-collapse.component.ts
      */
     const FEEDBACK_PREVIEW_CHARACTER_LIMIT = 300;
-    let comp: FeedbackCollapseComponent;
+    let component: FeedbackCollapseComponent;
 
     beforeEach(() => {
-        comp = new FeedbackCollapseComponent();
+        component = new FeedbackCollapseComponent();
     });
 
     it('should not truncate if not necessary', () => {
-        comp.text = 'a'.repeat(FEEDBACK_PREVIEW_CHARACTER_LIMIT - 1);
-        comp.ngOnInit();
+        component.text = 'a'.repeat(FEEDBACK_PREVIEW_CHARACTER_LIMIT - 1);
+        component.ngOnInit();
 
-        expect(comp.previewText).toBeUndefined();
+        expect(component.previewText).toBeUndefined();
     });
 
     it('should truncate if necessary', () => {
         const text = '0123456789'.repeat(FEEDBACK_PREVIEW_CHARACTER_LIMIT);
-        comp.text = text;
-        comp.ngOnInit();
+        component.text = text;
+        component.ngOnInit();
 
         const expected = text.slice(0, FEEDBACK_PREVIEW_CHARACTER_LIMIT);
 
-        expect(comp.previewText).toBe(expected);
+        expect(component.previewText).toBe(expected);
     });
 
     it('should only show first line if truncated', () => {
         const text = '0123456789\n'.repeat(FEEDBACK_PREVIEW_CHARACTER_LIMIT);
-        comp.text = text;
-        comp.ngOnInit();
+        component.text = text;
+        component.ngOnInit();
 
         const expected = text.slice(0, text.indexOf('\n'));
 
-        expect(comp.previewText).toBe(expected);
+        expect(component.previewText).toBe(expected);
     });
 
     it('should only show the first line of feedback if truncating necessary', () => {
-        comp.text = 'Multi\nLine\nText' + 'a'.repeat(300);
-        comp.ngOnInit();
+        component.text = 'Multi\nLine\nText' + 'a'.repeat(300);
+        component.ngOnInit();
 
-        expect(comp.previewText).toBe('Multi');
+        expect(component.previewText).toBe('Multi');
     });
 });
