b29ec6f0ae6772b96758b47920ac08bf0f363141
Julian Christl <julian.christl@tum.de>
2022-12-16 14:59:17 +0100

    remove debuggin functionality

--- src/main/webapp/app/exercises/modeling/manage/modeling-exercise.service.ts
+++ src/main/webapp/app/exercises/modeling/manage/modeling-exercise.service.ts
@@ -80,8 +80,7 @@ export class ModelingExerciseService implements ExerciseServicable<ModelingExerc
     convertToPdf(model: string, filename: string): Observable<HttpResponse<Blob>> {
         return this.http
             .post(`${SERVER_API_URL}api/apollon-convert/pdf`, { model }, { observe: 'response', responseType: 'blob' })
-            .pipe(tap((response: HttpResponse<Blob>) => downloadStream(response.body, 'application/pdf', filename)))
-            .pipe(tap((foo) => foo));
+            .pipe(tap((response: HttpResponse<Blob>) => downloadStream(response.body, 'application/pdf', filename)));
     }
 
     /**
