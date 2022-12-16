d68037b64d65ec9f58416e0f225f206c6a60e8a5
Matthias Linhuber <github@linhuber.org>
2022-12-16 17:14:14 +0100

    Implement Feedback

--- src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooBuildPlanService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/bamboo/BambooBuildPlanService.java
@@ -58,6 +58,7 @@ import de.tum.in.www1.artemis.exception.ContinuousIntegrationBuildPlanException;
 import de.tum.in.www1.artemis.service.ResourceLoaderService;
 import de.tum.in.www1.artemis.service.connectors.ContinuousIntegrationService.RepositoryCheckoutPath;
 import de.tum.in.www1.artemis.service.connectors.VersionControlService;
+import tech.jhipster.config.JHipsterConstants;
 
 @Service
 @Profile("bamboo")
@@ -171,8 +172,9 @@ public class BambooBuildPlanService {
          */
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
 
-        // Xcode has no dockerfile, it only runs on macOS agents
-        if (!ProjectType.XCODE.equals(projectType)) {
+        // Do not run the builds in extra docker containers if the dev-profile is active
+        // Xcode has no dockerfile, it only runs on agents (e.g. sb2-agent-0050562fddde)
+        if (!activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && !ProjectType.XCODE.equals(projectType)) {
             defaultJob.dockerConfiguration(dockerConfigurationFor(programmingLanguage, Optional.ofNullable(projectType)));
         }
         switch (programmingLanguage) {
