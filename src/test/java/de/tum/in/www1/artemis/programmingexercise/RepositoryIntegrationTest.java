35e12cc189596267d349cb2ead5b07a2b77590b4
Simon Leiß <git@simon-leiss.de>
2022-12-22 08:43:18 +0100

    Fix RepositoryIntegrationTest.

--- src/test/java/de/tum/in/www1/artemis/programmingexercise/RepositoryIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/programmingexercise/RepositoryIntegrationTest.java
@@ -128,7 +128,6 @@ class RepositoryIntegrationTest extends AbstractSpringIntegrationBambooBitbucket
         programmingExercise = database.getFirstExerciseWithType(course, ProgrammingExercise.class);
         programmingExercise = programmingExerciseRepository.findWithEagerStudentParticipationsById(programmingExercise.getId()).get();
 
-        programmingExercise = programmingExerciseRepository.findAllWithEagerParticipations().get(0);
         programmingExercise.setReleaseDate(ZonedDateTime.now().minusHours(1));
         programmingExerciseRepository.save(programmingExercise);
 
