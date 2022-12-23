cd64aa2686dcaba52b9ca5cc0379db16c9f90833
Ludwig Pusl <ludwig.pusl@tum.de>
2022-12-23 15:57:29 +0100

    refactored cypress docker compose setup

--- .bamboo/cleanup-E2E-tests.sh
+++ .bamboo/cleanup-E2E-tests.sh
@@ -1,3 +1,5 @@
+cd src/main/docker/cypress
+
 # HOST_HOSTNAME not really necessary for shutdown but otherwise docker-compose complains
 export HOST_HOSTNAME=$(hostname)
-docker-compose --env-file .src/main/docker/cypress/bamboo-E2E-testing.env down -v
+docker-compose -f ./src/main/docker/cypress-E2E-tests.yml down -v
