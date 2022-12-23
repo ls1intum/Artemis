b5cce5a698bcc6111b1c131fb678dbe65c113ca9
Ludwig Pusl <ludwig.pusl@tum.de>
2022-12-23 16:14:40 +0100

    fix path and compose version workaround

--- .bamboo/cleanup-E2E-tests.sh
+++ .bamboo/cleanup-E2E-tests.sh
@@ -1,5 +1,3 @@
-cd src/main/docker/cypress
-
 # HOST_HOSTNAME not really necessary for shutdown but otherwise docker-compose complains
 export HOST_HOSTNAME=$(hostname)
 docker-compose -f ./src/main/docker/cypress-E2E-tests.yml down -v
