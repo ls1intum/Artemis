de8e5e866bd14fce337b25b72f4deda9e303be6d
Lucas Welscher <ga53foy@mytum.de>
2022-12-23 16:38:15 +0100

    fix testCreateQuizExercise()

--- src/test/java/de/tum/in/www1/artemis/quiz/QuizExerciseIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/quiz/QuizExerciseIntegrationTest.java
@@ -155,7 +155,6 @@ class QuizExerciseIntegrationTest extends AbstractSpringIntegrationBambooBitbuck
                 assertThat(dropLocations.get(0).getWidth()).as("Width for drop location is correct").isEqualTo(10);
                 assertThat(dropLocations.get(0).getHeight()).as("Height for drop location is correct").isEqualTo(10);
                 assertThat(dropLocations.get(0).getQuestion()).isNotNull();
-                assertThat(dropLocations.get(0).getMappings()).isNotEmpty();
                 assertThat(dropLocations.get(1).getPosX()).as("Pos X for drop location is correct").isEqualTo(20);
                 assertThat(dropLocations.get(1).getPosY()).as("Pos Y for drop location is correct").isEqualTo(20);
                 assertThat(dropLocations.get(1).getWidth()).as("Width for drop location is correct").isEqualTo(10);
