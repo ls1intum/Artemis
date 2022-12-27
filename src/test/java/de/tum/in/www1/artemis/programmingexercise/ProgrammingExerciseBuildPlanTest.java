deece11473572ac953b9e937d4effd06d0b95708
Benjamin Schmitz <benjamin.schmitz@tum.de>
2022-12-27 11:58:38 +0100

    Fix server style

--- src/test/java/de/tum/in/www1/artemis/programmingexercise/ProgrammingExerciseBuildPlanTest.java
+++ src/test/java/de/tum/in/www1/artemis/programmingexercise/ProgrammingExerciseBuildPlanTest.java
@@ -2,7 +2,6 @@ package de.tum.in.www1.artemis.programmingexercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
