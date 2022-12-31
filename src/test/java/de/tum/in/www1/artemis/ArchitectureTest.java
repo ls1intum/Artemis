2919df8d80459186f8be5f9b724f2e5937a2fa8e
Lucas Welscher <ga53foy@mytum.de>
2022-12-31 16:49:38 +0100

    add checks for test classes

--- src/test/java/de/tum/in/www1/artemis/ArchitectureTest.java
+++ src/test/java/de/tum/in/www1/artemis/ArchitectureTest.java
@@ -1,5 +1,8 @@
 package de.tum.in.www1.artemis;
 
+import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.simpleNameContaining;
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
 import org.junit.jupiter.api.*;
@@ -19,8 +22,12 @@ class ArchitectureTest {
         ArchRule noJUnit4Imports = noClasses().should().dependOnClassesThat().resideInAPackage("org.junit");
         ArchRule noPublicTests = noMethods().that().areAnnotatedWith(Test.class).or().areAnnotatedWith(ParameterizedTest.class).or().areAnnotatedWith(BeforeEach.class).or()
                 .areAnnotatedWith(BeforeAll.class).or().areAnnotatedWith(AfterEach.class).or().areAnnotatedWith(AfterAll.class).should().bePublic();
+        ArchRule classNames = methods().that().areAnnotatedWith(Test.class).should().beDeclaredInClassesThat().haveNameMatching(".*Test");
+        ArchRule noPublicTestClasses = noClasses().that().haveNameMatching(".*Test").should().bePublic();
 
         noJUnit4Imports.check(testClasses);
         noPublicTests.check(testClasses);
+        classNames.check(testClasses);
+        noPublicTestClasses.check(testClasses.that(are(not(simpleNameContaining("Abstract")))));
     }
 }
