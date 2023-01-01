c209a699b1b439f276b2f8c46e7e96e36cbbb9be
Pellumb Baboci <pellumbbaboci@gmail.com>
2023-01-01 17:32:25 +0100

    from java class to record dto

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/LectureUnitsDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/LectureUnitsDTO.java
@@ -5,44 +5,5 @@ import org.springframework.web.multipart.MultipartFile;
 import de.tum.in.www1.artemis.domain.Attachment;
 import de.tum.in.www1.artemis.domain.lecture.AttachmentUnit;
 
-public class LectureUnitsDTO {
-
-    private AttachmentUnit attachmentUnit;
-
-    private Attachment attachment;
-
-    private MultipartFile file;
-
-    public LectureUnitsDTO() {
-    }
-
-    public LectureUnitsDTO(AttachmentUnit attachmentUnit, Attachment attachment, MultipartFile file) {
-        this.attachmentUnit = attachmentUnit;
-        this.attachment = attachment;
-        this.file = file;
-    }
-
-    public AttachmentUnit getAttachmentUnit() {
-        return attachmentUnit;
-    }
-
-    public void setAttachmentUnit(AttachmentUnit attachmentUnit) {
-        this.attachmentUnit = attachmentUnit;
-    }
-
-    public Attachment getAttachment() {
-        return attachment;
-    }
-
-    public void setAttachment(Attachment attachment) {
-        this.attachment = attachment;
-    }
-
-    public MultipartFile getFile() {
-        return file;
-    }
-
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }
+public record LectureUnitsDTO(AttachmentUnit attachmentUnit, Attachment attachment, MultipartFile file) {
 }
