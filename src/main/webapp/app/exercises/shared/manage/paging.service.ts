bcc15143eb937146b30b7ddc56d981d682a1a8f6
Lucas Welscher <ga53foy@mytum.de>
2023-01-04 12:55:43 +0100

    refactor other paging services

---
+++ src/main/webapp/app/exercises/shared/manage/paging.service.ts
@@ -0,0 +1,15 @@
+import { HttpParams } from '@angular/common/http';
+import { PageableSearch } from 'app/shared/table/pageable-table';
+
+export abstract class PagingService {
+    protected constructor() {}
+
+    protected createParams(pageable: PageableSearch): HttpParams {
+        return new HttpParams()
+            .set('pageSize', String(pageable.pageSize))
+            .set('page', String(pageable.page))
+            .set('sortingOrder', pageable.sortingOrder)
+            .set('searchTerm', pageable.searchTerm)
+            .set('sortedColumn', pageable.sortedColumn);
+    }
+}
