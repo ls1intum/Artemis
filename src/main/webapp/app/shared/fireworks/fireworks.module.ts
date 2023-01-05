de1f9ce5074a27ad4c1dc7ece14ea44ffb6d183c
ge65cer <ge65cer@mytum.de>
2023-01-05 21:51:49 +0100

    Small fixes

--- src/main/webapp/app/shared/fireworks/fireworks.module.ts
+++ src/main/webapp/app/shared/fireworks/fireworks.module.ts
@@ -1,7 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FireworksComponent } from 'app/shared/fireworks/fireworks.component';
 
 @NgModule({
+    imports: [CommonModule],
     declarations: [FireworksComponent],
     exports: [FireworksComponent],
 })
