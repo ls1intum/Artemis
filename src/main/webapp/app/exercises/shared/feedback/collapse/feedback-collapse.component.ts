f326ae4396be898cca7bb80545669de8657d4269
Simon Huang <simon.huang@outlook.de>
2023-01-07 15:01:42 +0100

    Programming exercises: Improve feedback view (#5923)

---
+++ src/main/webapp/app/exercises/shared/feedback/collapse/feedback-collapse.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'jhi-feedback-collapse',
+    styleUrls: ['./feedback-collapse.scss'],
+    templateUrl: './feedback-collapse.component.html',
+})
+/**
+ * smallCharacterLimit can be adjusted make smaller or bigger items collapsable
+ * isCollapsed tracks whether an item is currently open or closed
+ * text is any string passed to the component
+ */
+export class FeedbackCollapseComponent implements OnInit {
+    /**
+     * amount of chars at which the text will be cut and the collapse functionality is enabled
+     */
+    readonly FEEDBACK_PREVIEW_CHARACTER_LIMIT = 300;
+
+    @Input() text: string; // this is typically feedback.detailText
+    previewText?: string;
+    isCollapsed = true;
+
+    // Icons
+    faAngleDown = faAngleDown;
+    faAngleRight = faAngleRight;
+
+    ngOnInit(): void {
+        this.previewText = this.computeFeedbackPreviewText(this.text);
+    }
+
+    /**
+     * Computes the feedback preview for feedback texts with multiple lines or feedback that is longer than {@link FEEDBACK_PREVIEW_CHARACTER_LIMIT} characters.
+     * @param text The feedback detail text.
+     * @return One line of text with at most {@link FEEDBACK_PREVIEW_CHARACTER_LIMIT} characters.
+     */
+    private computeFeedbackPreviewText = (text?: string): string | undefined => {
+        if (!text || text.length < this.FEEDBACK_PREVIEW_CHARACTER_LIMIT) {
+            return undefined;
+        }
+
+        if (text.includes('\n')) {
+            // if there are multiple lines, only use the first one
+            const firstLine = text.slice(0, text.indexOf('\n'));
+            return firstLine.slice(0, this.FEEDBACK_PREVIEW_CHARACTER_LIMIT);
+        }
+
+        return text.slice(0, this.FEEDBACK_PREVIEW_CHARACTER_LIMIT);
+    };
+}
