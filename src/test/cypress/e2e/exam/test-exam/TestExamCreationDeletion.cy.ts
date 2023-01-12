1480885a609ac52b011203e2309f116e8bd2edee
Florian Gareis <dev@zoker.me>
2023-01-12 13:36:13 +0100

    Refactor new test a bit

--- src/test/cypress/e2e/exam/test-exam/TestExamCreationDeletion.cy.ts
+++ src/test/cypress/e2e/exam/test-exam/TestExamCreationDeletion.cy.ts
@@ -3,7 +3,7 @@ import { Course } from 'app/entities/course.model';
 import { CypressExamBuilder, convertCourseAfterMultiPart } from '../../../support/requests/CourseManagementRequests';
 import dayjs from 'dayjs/esm';
 import { artemis } from '../../../support/ArtemisTesting';
-import { generateUUID } from '../../../support/utils';
+import { dayjsToString, generateUUID, trimDate } from '../../../support/utils';
 
 // Requests
 const courseManagementRequests = artemis.requests.courseManagement;
@@ -28,8 +28,6 @@ const examData = {
     confirmationStartText: 'Cypress exam confirmation start text',
     confirmationEndText: 'Cypress exam confirmation end text',
 };
-
-const dateFormat = 'YYYY-MM-DDTHH:mm:ss';
 
 describe('Test Exam creation/deletion', () => {
     let course: Course;
@@ -66,20 +64,21 @@ describe('Test Exam creation/deletion', () => {
         creationPage.setConfirmationEndText(examData.confirmationEndText);
 
         creationPage.submit().then((examResponse: Interception) => {
+            const examBody = examResponse.response!.body;
             examId = examResponse.response!.body.id;
             expect(examResponse.response!.statusCode).to.eq(201);
-            expect(examResponse.response!.body.title).to.eq(examData.title);
-            expect(examResponse.response!.body.testExam).to.be.true;
-            expect(trimDate(examResponse.response!.body.visibleDate)).to.eq(examData.visibleDate.utcOffset(0).format(dateFormat));
-            expect(trimDate(examResponse.response!.body.startDate)).to.eq(examData.startDate.utcOffset(0).format(dateFormat));
-            expect(trimDate(examResponse.response!.body.endDate)).to.eq(examData.endDate.utcOffset(0).format(dateFormat));
-            expect(examResponse.response!.body.workingTime).to.eq(examData.workingTime * 60);
-            expect(examResponse.response!.body.numberOfExercisesInExam).to.eq(examData.numberOfExercises);
-            expect(examResponse.response!.body.examMaxPoints).to.eq(examData.maxPoints);
-            expect(examResponse.response!.body.startText).to.eq(examData.startText);
-            expect(examResponse.response!.body.endText).to.eq(examData.endText);
-            expect(examResponse.response!.body.confirmationStartText).to.eq(examData.confirmationStartText);
-            expect(examResponse.response!.body.confirmationEndText).to.eq(examData.confirmationEndText);
+            expect(examBody.title).to.eq(examData.title);
+            expect(examBody.testExam).to.be.true;
+            expect(trimDate(examBody.visibleDate)).to.eq(trimDate(dayjsToString(examData.visibleDate)));
+            expect(trimDate(examBody.startDate)).to.eq(trimDate(dayjsToString(examData.startDate)));
+            expect(trimDate(examBody.endDate)).to.eq(trimDate(dayjsToString(examData.endDate)));
+            expect(examBody.workingTime).to.eq(examData.workingTime * 60);
+            expect(examBody.numberOfExercisesInExam).to.eq(examData.numberOfExercises);
+            expect(examBody.examMaxPoints).to.eq(examData.maxPoints);
+            expect(examBody.startText).to.eq(examData.startText);
+            expect(examBody.endText).to.eq(examData.endText);
+            expect(examBody.confirmationStartText).to.eq(examData.confirmationStartText);
+            expect(examBody.confirmationEndText).to.eq(examData.confirmationEndText);
             cy.url().should('contain', `/exams/${examId}`);
         });
         cy.get('#exam-detail-title').should('contain.text', examData.title);
@@ -109,8 +108,3 @@ describe('Test Exam creation/deletion', () => {
         }
     });
 });
-
-// This is necessary, to align the javascript time format with the java time format
-function trimDate(date: string) {
-    return date.slice(0, 19);
-}
