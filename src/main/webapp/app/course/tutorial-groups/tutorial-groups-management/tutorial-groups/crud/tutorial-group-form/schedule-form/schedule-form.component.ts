d066d29e9de40213e191917255626649c7480139
Stefan Waldhauser <hello@stefanwaldhauser.me>
2023-01-16 15:42:50 +0100

    Fix bug of schedule form not updating when online / offline switchedd

--- src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-groups/crud/tutorial-group-form/schedule-form/schedule-form.component.ts
+++ src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-groups/crud/tutorial-group-form/schedule-form/schedule-form.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import { NgbDateParserFormatter, NgbTimeAdapter } from '@ng-bootstrap/ng-bootstrap';
@@ -87,7 +87,7 @@ export class ScheduleFormComponent implements OnInit {
         return sessions;
     }
 
-    constructor(private fb: FormBuilder, public formatter: NgbDateParserFormatter) {}
+    constructor(private fb: FormBuilder, public formatter: NgbDateParserFormatter, public cdr: ChangeDetectorRef) {}
 
     ngOnInit(): void {
         if (this.course.tutorialGroupsConfiguration) {
@@ -108,6 +108,10 @@ export class ScheduleFormComponent implements OnInit {
         );
 
         this.parentFormGroup.addControl('schedule', this.formGroup);
+
+        this.parentIsOnlineControl?.valueChanges.subscribe(() => {
+            this.cdr.detectChanges();
+        });
     }
 
     get isPeriodInvalid() {
