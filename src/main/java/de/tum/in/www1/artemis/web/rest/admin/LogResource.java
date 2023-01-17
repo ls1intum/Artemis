b9e079d8030b9fb685930911db30635dbf44571a
Julian Christl <julian.christl@tum.de>
2023-01-17 11:37:50 +0100

    unwrap management path endpoints

--- src/main/java/de/tum/in/www1/artemis/web/rest/admin/LogResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/admin/LogResource.java
@@ -1,10 +1,8 @@
 package de.tum.in.www1.artemis.web.rest.admin;
 
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,40 +10,20 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import de.tum.in.www1.artemis.security.annotations.EnforceAdmin;
-import de.tum.in.www1.artemis.service.feature.Feature;
-import de.tum.in.www1.artemis.service.feature.FeatureToggleService;
 import de.tum.in.www1.artemis.web.rest.vm.LoggerVM;
 
+/**
+ * Controller for view and managing Log Level at runtime.
+ */
 @RestController
 @RequestMapping("admin/")
-public class ManagementResource {
-
-    private final FeatureToggleService featureToggleService;
-
-    public ManagementResource(FeatureToggleService featureToggleService) {
-        this.featureToggleService = featureToggleService;
-    }
+public class LogResource {
 
     /**
-     * PUT management/feature-toggle -- Updates all given features by enabling/disabling them. (Map of feature -> shouldBeEnabled)
-     *
-     * @see FeatureToggleService
-     * @param features A map of features (feature -> shouldBeActivated)
-     * @return A list of all enabled features
-     */
-    @PutMapping("management/feature-toggle")
-    @EnforceAdmin
-    public ResponseEntity<List<Feature>> toggleFeatures(@RequestBody Map<Feature, Boolean> features) {
-        featureToggleService.updateFeatureToggles(features);
-
-        return new ResponseEntity<>(featureToggleService.enabledFeatures(), HttpStatus.OK);
-    }
-
-    /**
-     * GET management/logs -- Gets the current log levels.
+     * GET logs -- Gets the current log levels.
      * @return A list of all loggers with their log level
      */
-    @GetMapping("management/logs")
+    @GetMapping("logs")
     @EnforceAdmin
     public ResponseEntity<List<LoggerVM>> getList() {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
@@ -53,11 +31,11 @@ public class ManagementResource {
     }
 
     /**
-     * PUT management/logs -- Changes the log level of a logger.
+     * PUT logs -- Changes the log level of a logger.
      * @param jsonLogger The logger with the new log level
      * @return The updated logger
      */
-    @PutMapping("management/logs")
+    @PutMapping("logs")
     @EnforceAdmin
     public ResponseEntity<LoggerVM> changeLevel(@RequestBody LoggerVM jsonLogger) {
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
