63eedfe4ebda8e21e23dd6cdfe592d1c5b01fb88
Simon Leiß <git@simon-leiss.de>
2023-01-20 22:10:04 +0100

    Remove transactional.

--- src/main/java/de/tum/in/www1/artemis/repository/BuildLogEntryRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/BuildLogEntryRepository.java
@@ -1,9 +1,7 @@
 package de.tum.in.www1.artemis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.BuildLogEntry;
 
@@ -13,8 +11,6 @@ import de.tum.in.www1.artemis.domain.BuildLogEntry;
 @Repository
 public interface BuildLogEntryRepository extends JpaRepository<BuildLogEntry, Long> {
 
-    @Transactional // ok because of delete
-    @Modifying
     void deleteByProgrammingSubmissionId(long programmingSubmissionId);
 
 }
