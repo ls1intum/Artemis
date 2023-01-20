fa451001c3a87c1bac09be490c0da9449ab0cc35
Simon Huang <simon.huang@tum.de>
2023-01-20 12:30:44 +0100

    change usage to new result badge component

--- src/main/webapp/app/overview/submission-result-status.module.ts
+++ src/main/webapp/app/overview/submission-result-status.module.ts
@@ -4,9 +4,10 @@ import { SubmissionResultStatusComponent } from 'app/overview/submission-result-
 import { UpdatingResultComponent } from 'app/exercises/shared/result/updating-result.component';
 import { ArtemisProgrammingExerciseActionsModule } from 'app/exercises/programming/shared/actions/programming-exercise-actions.module';
 import { ResultComponent } from 'app/exercises/shared/result/result.component';
+import { ArtemisResultBadgeModule } from 'app/exercises/shared/result-badges/result-badges.module';
 
 @NgModule({
-    imports: [ArtemisSharedModule, ArtemisProgrammingExerciseActionsModule],
+    imports: [ArtemisSharedModule, ArtemisProgrammingExerciseActionsModule, ArtemisResultBadgeModule],
     declarations: [SubmissionResultStatusComponent, UpdatingResultComponent, ResultComponent],
     exports: [SubmissionResultStatusComponent, UpdatingResultComponent, ResultComponent],
 })
