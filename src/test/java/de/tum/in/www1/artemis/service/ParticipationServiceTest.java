36e05f0c1d5f9ddea518f00e3a8f99bba61771d4
Simon Leiß <git@simon-leiss.de>
2023-01-20 09:54:13 +0100

    Remove annotation.

--- src/test/java/de/tum/in/www1/artemis/service/ParticipationServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/ParticipationServiceTest.java
@@ -174,7 +174,6 @@ class ParticipationServiceTest extends AbstractSpringIntegrationJenkinsGitlabTes
     }
 
     @Test
-    @WithMockUser(username = TEST_PREFIX + "student1", roles = "USER")
     void testDeleteParticipation_removesBuildLogEntries() {
         var course = database.addCourseWithOneProgrammingExerciseAndTestCases();
         var programmingExercise = database.getFirstExerciseWithType(course, ProgrammingExercise.class);
