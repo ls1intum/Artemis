2ee3baa0679b3af3d6b2fa785b7397d26fd1ee53
Simon Huang <simon.huang@tum.de>
2023-01-20 12:22:35 +0100

    add preliminary status to badge

--- src/main/webapp/app/exercises/shared/result-badges/result-badge.component.ts
+++ src/main/webapp/app/exercises/shared/result-badges/result-badge.component.ts
@@ -2,13 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Result } from 'app/entities/result.model';
 import { Participation, ParticipationType } from 'app/entities/participation/participation.model';
 import { StudentParticipation } from 'app/entities/participation/student-participation.model';
+import { resultIsPreliminary } from 'app/exercises/shared/result/result.utils';
+import { Exercise } from 'app/entities/exercise.model';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { AssessmentType } from 'app/entities/assessment-type.model';
 
 interface Badge {
     class: string;
     text: string;
     tooltip: string;
 }
-type Status = 'graded' | 'ungraded' | 'testRun';
+type Status = 'graded' | 'ungraded' | 'testRun' | 'preliminary';
 
 @Component({
     selector: 'jhi-result-badge',
@@ -17,15 +21,21 @@ type Status = 'graded' | 'ungraded' | 'testRun';
 export class ResultBadgeComponent implements OnInit {
     @Input() result: Result;
     @Input() participation: Participation;
+    exercise: Exercise;
 
     badge: Badge;
 
     ngOnInit(): void {
         const status = this.evaluateStatus(this.result, this.participation);
         this.badge = this.getBadge(status);
+        this.exercise = this.participation.exercise!;
     }
 
     private evaluateStatus(result: Result, participation: Participation) {
+        if (resultIsPreliminary(result)) {
+            return 'preliminary';
+        }
+
         if (participation.type === ParticipationType.STUDENT || participation.type === ParticipationType.PROGRAMMING) {
             const studentParticipation = participation as StudentParticipation;
             if (studentParticipation.testRun) {
@@ -37,6 +47,16 @@ export class ResultBadgeComponent implements OnInit {
     }
 
     private getBadge(status: Status): Badge {
+        if (status === 'preliminary') {
+            const programmingExercise = this.exercise as ProgrammingExercise;
+            return {
+                class: 'bg-secondary',
+                text: 'artemisApp.result.preliminary',
+                tooltip:
+                    programmingExercise?.assessmentType !== AssessmentType.AUTOMATIC ? 'artemisApp.result.preliminaryTooltipSemiAutomatic' : 'artemisApp.result.preliminaryTooltip',
+            };
+        }
+
         return {
             graded: { class: 'bg-success', text: 'artemisApp.result.graded', tooltip: 'artemisApp.result.gradedTooltip' },
             ungraded: { class: 'bg-info', text: 'artemisApp.result.notGraded', tooltip: 'artemisApp.result.notGradedTooltip' },
