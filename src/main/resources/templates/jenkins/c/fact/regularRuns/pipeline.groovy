a1ce91fdd9dfde2ed3c786b8a94e139057a61c33
Benedikt Fein <fein@fim.uni-passau.de>
2023-01-24 14:11:43 +0100

    convert pipeline scripts for non-Java exercises

---
+++ src/main/resources/templates/jenkins/c/fact/regularRuns/pipeline.groovy
@@ -0,0 +1,92 @@
+/*
+ * This file configures the actual build steps for the automatic grading.
+ *
+ * !!!
+ * For regular exercises, there is no need to make changes to this file.
+ * Only this base configuration is actively supported by the Artemis maintainers
+ * and/or your Artemis instance administrators.
+ * !!!
+ */
+
+dockerImage = "#dockerImage"
+dockerFlags = ""
+
+/**
+ * Main function called by Jenkins.
+ */
+void testRunner() {
+    docker.image(dockerImage).inside(dockerFlags) { c ->
+        runTestSteps()
+    }
+}
+
+private void runTestSteps() {
+    test()
+}
+
+/**
+ * Run unit tests
+ */
+private void test() {
+    stage('Build') {
+        sh '''
+        cd $WORKSPACE
+        echo "--------------------setup-------------------"
+        echo "User:"
+        whoami
+        echo "--------------------setup-------------------"
+        echo "--------------------info--------------------"
+        python3 --version
+        pip3 --version
+        pip3 freeze | grep fact
+        gcc --version
+        echo "--------------------tests-------------------"
+        ls -la tests
+        echo "--------------------tests-------------------"
+        echo "--------------------assignment--------------"
+        ls -la assignment
+        echo "--------------------assignment--------------"
+
+        exit 0
+        '''
+
+        sh '''
+        rm -f assignment/GNUmakefile
+        rm -f assignment/Makefile
+
+        cp -f tests/Makefile assignment/Makefile || exit 2
+        '''
+
+        sh '''
+        #!/bin/bash
+
+        cd tests
+        python3 Tests.py
+        rm Tests.py
+        rm -rf ./tests
+        exit 0
+        '''
+    }
+}
+
+/**
+ * Script of the post build tasks aggregating all JUnit files in $WORKSPACE/results.
+ *
+ * Called by Jenkins.
+ */
+void postBuildTasks() {
+    sh '''
+    if [ -e test-reports/tests-results.xml ]
+    then
+        sed -i 's/[^[:print:]]//g' test-reports/tests-results.xml
+        sed -i 's/<skipped/<error/g' test-reports/tests-results.xml
+        sed -i 's/<\\/skipped>/<\\/error>/g' test-reports/tests-results.xml
+    fi
+    rm -rf results
+    mv test-reports results
+    '''
+}
+
+// very important, do not remove
+// required so that Jenkins finds the methods defined in this script
+return this
---
+++ src/main/resources/templates/jenkins/swift/regularRuns/pipeline.groovy
@@ -0,0 +1,84 @@
+/*
+ * This file configures the actual build steps for the automatic grading.
+ *
+ * !!!
+ * For regular exercises, there is no need to make changes to this file.
+ * Only this base configuration is actively supported by the Artemis maintainers
+ * and/or your Artemis instance administrators.
+ * !!!
+ */
+
+dockerImage = "#dockerImage"
+dockerFlags = ""
+
+/**
+ * Main function called by Jenkins.
+ */
+void testRunner() {
+    docker.image(dockerImage).inside(dockerFlags) { c ->
+        runTestSteps()
+    }
+}
+
+private void runTestSteps() {
+    test()
+}
+
+/**
+ * Run unit tests
+ */
+private void test() {
+    stage('Build') {
+        sh '''
+        rm -rf Sources
+        mv assignment/Sources .
+        rm -rf assignment
+        mkdir assignment
+        cp -R Sources assignment
+        cp -R Tests assignment
+        cp Package.swift assignment
+
+        # swift build
+        cd assignment
+        swift build
+
+        # swift test
+        swift test || true
+        '''
+    }
+}
+
+private void staticCodeAnalysis() {
+    stage('Static Code Analysis') {
+        sh '''
+        #staticCodeAnalysisScript
+        '''
+    }
+}
+
+/**
+ * Script of the post build tasks aggregating all JUnit files in $WORKSPACE/results.
+ *
+ * Called by Jenkins.
+ */
+void postBuildTasks() {
+    if (#staticCodeAnalysisEnabled) {
+        catchError {
+            staticCodeAnalysis()
+        }
+    }
+
+    sh '''
+    rm -rf results
+    mkdir results
+    if [ -e assignment/tests.xml ]
+    then
+        sed -i 's/<testsuites>//g ; s/<\\/testsuites>//g' assignment/tests.xml
+        cp assignment/tests.xml $WORKSPACE/results/ || true
+    fi
+    '''
+}
+
+// very important, do not remove
+// required so that Jenkins finds the methods defined in this script
+return this
