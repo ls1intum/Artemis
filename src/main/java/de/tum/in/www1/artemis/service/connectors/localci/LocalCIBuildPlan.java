6e2bc81347023b3c5baf94ff71a56bfc5f24fd87
Markus <markus.loehde@gmx.de>
2023-01-27 16:31:14 +0100

    re-define LocalCiBuildPlan object

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIBuildPlan.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIBuildPlan.java
@@ -3,9 +3,7 @@ package de.tum.in.www1.artemis.service.connectors.localci;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import de.tum.in.www1.artemis.domain.VcsRepositoryUrl;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record LocalCIBuildPlan(String key, VcsRepositoryUrl submissionRepositoryUrl, VcsRepositoryUrl testRepositoryUrl) {
+public record LocalCIBuildPlan(String projectKey, String Key, String submissionRepositoryUrl, String testRepositoryUrl) {
 }
