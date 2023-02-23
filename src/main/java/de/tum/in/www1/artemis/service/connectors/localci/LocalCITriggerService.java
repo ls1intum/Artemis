539ba8435de6c8b2e6de0d50d8e932f271d68110
kutsch <frederik.kutsch@yahoo.com>
2023-02-23 10:50:07 +0100

    fix checking valid repository URL

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCITriggerService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCITriggerService.java
@@ -2,7 +2,7 @@ package de.tum.in.www1.artemis.service.connectors.localci;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import de.tum.in.www1.artemis.domain.ProgrammingExercise;
 import de.tum.in.www1.artemis.domain.Result;
+import de.tum.in.www1.artemis.domain.VcsRepositoryUrl;
 import de.tum.in.www1.artemis.domain.enumeration.ProgrammingLanguage;
 import de.tum.in.www1.artemis.domain.participation.Participation;
 import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseParticipation;
@@ -24,11 +25,12 @@ import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseStudentPar
 import de.tum.in.www1.artemis.domain.participation.StudentParticipation;
 import de.tum.in.www1.artemis.exception.LocalCIException;
 import de.tum.in.www1.artemis.security.SecurityUtils;
+import de.tum.in.www1.artemis.service.UrlService;
 import de.tum.in.www1.artemis.service.WebsocketMessagingService;
 import de.tum.in.www1.artemis.service.connectors.LtiNewResultService;
 import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildResultNotificationDTO;
-import de.tum.in.www1.artemis.service.connectors.localvc.LocalVCRepositoryUrl;
 import de.tum.in.www1.artemis.service.programming.ProgrammingExerciseGradingService;
+import de.tum.in.www1.artemis.service.util.TimeLogUtil;
 
 /**
  * Service for triggering builds on the local CI server.
@@ -41,20 +43,20 @@ public class LocalCITriggerService {
 
     private final Logger log = LoggerFactory.getLogger(LocalCITriggerService.class);
 
+    private final UrlService urlService;
+
     private final ProgrammingExerciseGradingService programmingExerciseGradingService;
 
     private final WebsocketMessagingService messagingService;
 
     private final LtiNewResultService ltiNewResultService;
 
-    @Value("${artemis.version-control.url}")
-    private URL localVCServerUrl;
-
     @Value("${artemis.version-control.local-vcs-repo-path}")
-    private String localVCPath;
+    private String localVCBasePath;
 
-    public LocalCITriggerService(ProgrammingExerciseGradingService programmingExerciseGradingService, WebsocketMessagingService messagingService,
+    public LocalCITriggerService(UrlService urlService, ProgrammingExerciseGradingService programmingExerciseGradingService, WebsocketMessagingService messagingService,
             LtiNewResultService ltiNewResultService) {
+        this.urlService = urlService;
         this.programmingExerciseGradingService = programmingExerciseGradingService;
         this.messagingService = messagingService;
         this.ltiNewResultService = ltiNewResultService;
@@ -62,24 +64,38 @@ public class LocalCITriggerService {
 
     /**
      * Triggers a build for given participation.
-     * Note: not used right now, can be used to prevent a circular dependency LocalCIService -> ProgrammingExercisesGradingService -> LocalCIService.
+     * Needs to be outside ContinuousIntegrationService (LocalCIService) to prevent a circular dependency LocalCIService -> ProgrammingExercisesGradingService -> LocalCIService.
      *
      * @param participation the participation with the id of the build plan that should be triggered.
      */
     @Async
     public void triggerBuild(ProgrammingExerciseParticipation participation) throws LocalCIException {
+
+        long timeNanoStart = System.nanoTime();
+
         // Create a new build job and run it. TODO: outsource execution to an ExecutorService.
         String assignmentRepositoryUrlString = participation.getRepositoryUrl();
-        LocalVCRepositoryUrl assignmentRepositoryUrl = new LocalVCRepositoryUrl(localVCServerUrl, assignmentRepositoryUrlString);
-        Path assignmentRepositoryPath = assignmentRepositoryUrl.getLocalPath(localVCPath).toAbsolutePath();
+        VcsRepositoryUrl assignmentRepositoryUrl;
+        try {
+            assignmentRepositoryUrl = new VcsRepositoryUrl(assignmentRepositoryUrlString);
+        }
+        catch (URISyntaxException e) {
+            throw new LocalCIException("Could not parse assignment repository url: " + assignmentRepositoryUrlString);
+        }
+        Path assignmentRepositoryPath = urlService.getLocalVCPathFromRepositoryUrl(assignmentRepositoryUrl, localVCBasePath).toAbsolutePath();
 
         ProgrammingExercise programmingExercise = participation.getProgrammingExercise();
         String testRepositoryUrlString = programmingExercise.getTestRepositoryUrl();
-        LocalVCRepositoryUrl testRepositoryUrl = new LocalVCRepositoryUrl(localVCServerUrl, testRepositoryUrlString);
-        Path testRepositoryPath = testRepositoryUrl.getLocalPath(localVCPath).toAbsolutePath();
+        VcsRepositoryUrl testRepositoryUrl;
+        try {
+            testRepositoryUrl = new VcsRepositoryUrl(testRepositoryUrlString);
+        }
+        catch (URISyntaxException e) {
+            throw new LocalCIException("Could not parse test repository url: " + testRepositoryUrlString);
+        }
+        Path testRepositoryPath = urlService.getLocalVCPathFromRepositoryUrl(testRepositoryUrl, localVCBasePath).toAbsolutePath();
 
         ProgrammingLanguage programmingLanguage = programmingExercise.getProgrammingLanguage();
-
         if (programmingLanguage != ProgrammingLanguage.JAVA) {
             throw new LocalCIException("Programming language " + programmingLanguage + " is not supported by local CI.");
         }
@@ -125,5 +141,7 @@ public class LocalCITriggerService {
         catch (IllegalArgumentException | IOException e) {
             throw new LocalCIException("Error while creating and running build job: " + e.getMessage());
         }
+
+        log.info("Building and testing submission for repository {} took {}", assignmentRepositoryUrlString, TimeLogUtil.formatDurationFrom(timeNanoStart));
     }
 }
