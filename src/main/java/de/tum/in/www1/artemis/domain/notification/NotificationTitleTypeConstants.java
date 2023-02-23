5b15fcbb5e8abd1469e4647f3d47af479db531c1
Daniel Bode <daniel.bode@tum.de>
2023-02-23 16:48:05 +0100

    update notification titles

--- src/main/java/de/tum/in/www1/artemis/domain/notification/NotificationTitleTypeConstants.java
+++ src/main/java/de/tum/in/www1/artemis/domain/notification/NotificationTitleTypeConstants.java
@@ -9,77 +9,77 @@ import de.tum.in.www1.artemis.domain.enumeration.NotificationType;
 
 public class NotificationTitleTypeConstants {
 
-    public static final String LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE = "Live Exam Exercise Update";
+    public static final String LIVE_EXAM_EXERCISE_UPDATE_NOTIFICATION_TITLE = "notification.title.liveExamExerciseUpdate";
 
-    public static final String EXERCISE_SUBMISSION_ASSESSED_TITLE = "Exercise Submission Assessed";
+    public static final String EXERCISE_SUBMISSION_ASSESSED_TITLE = "notification.title.exerciseSubmissionAssessed";
 
-    public static final String ATTACHMENT_CHANGE_TITLE = "Attachment updated";
+    public static final String ATTACHMENT_CHANGE_TITLE = "notification.title.attachmentChange";
 
-    public static final String EXERCISE_RELEASED_TITLE = "Exercise released";
+    public static final String EXERCISE_RELEASED_TITLE = "notification.title.exerciseReleased";
 
-    public static final String EXERCISE_PRACTICE_TITLE = "Exercise open for practice";
+    public static final String EXERCISE_PRACTICE_TITLE = "notification.title.exercisePractice";
 
-    public static final String QUIZ_EXERCISE_STARTED_TITLE = "Quiz started";
+    public static final String QUIZ_EXERCISE_STARTED_TITLE = "notification.title.quizExerciseStarted";
 
-    public static final String EXERCISE_UPDATED_TITLE = "Exercise updated";
+    public static final String EXERCISE_UPDATED_TITLE = "notification.title.exerciseUpdated";
 
-    public static final String DUPLICATE_TEST_CASE_TITLE = "Duplicate test case was found.";
+    public static final String DUPLICATE_TEST_CASE_TITLE = "notification.title.duplicateTestCase";
 
-    public static final String ILLEGAL_SUBMISSION_TITLE = "Illegal submission of a student.";
+    public static final String ILLEGAL_SUBMISSION_TITLE = "notification.title.illegalSubmission";
 
-    public static final String NEW_EXERCISE_POST_TITLE = "New exercise post";
+    public static final String NEW_EXERCISE_POST_TITLE = "notification.title.newExercisePost";
 
-    public static final String NEW_LECTURE_POST_TITLE = "New lecture post";
+    public static final String NEW_LECTURE_POST_TITLE = "notification.title.newLecturePost";
 
-    public static final String NEW_COURSE_POST_TITLE = "New course-wide post";
+    public static final String NEW_COURSE_POST_TITLE = "notification.title.newCoursePost";
 
-    public static final String NEW_ANNOUNCEMENT_POST_TITLE = "New announcement";
+    public static final String NEW_ANNOUNCEMENT_POST_TITLE = "notification.title.newAnnouncementPost";
 
-    public static final String NEW_REPLY_FOR_EXERCISE_POST_TITLE = "New reply for exercise post";
+    public static final String NEW_REPLY_FOR_EXERCISE_POST_TITLE = "notification.title.newReplyForExercisePost";
 
-    public static final String NEW_REPLY_FOR_LECTURE_POST_TITLE = "New reply for lecture post";
+    public static final String NEW_REPLY_FOR_LECTURE_POST_TITLE = "notification.title.newReplyForLecturePost";
 
-    public static final String NEW_REPLY_FOR_COURSE_POST_TITLE = "New reply for course-wide post";
+    public static final String NEW_REPLY_FOR_COURSE_POST_TITLE = "notification.title.newReplyForCoursePost";
 
-    public static final String FILE_SUBMISSION_SUCCESSFUL_TITLE = "File submission successful";
+    public static final String FILE_SUBMISSION_SUCCESSFUL_TITLE = "notification.title.fileSubmissionSuccessful";
 
-    public static final String COURSE_ARCHIVE_STARTED_TITLE = "Course archival started";
+    public static final String COURSE_ARCHIVE_STARTED_TITLE = "notification.title.courseArchiveStarted";
 
-    public static final String COURSE_ARCHIVE_FINISHED_TITLE = "Course archival finished";
+    public static final String COURSE_ARCHIVE_FINISHED_TITLE = "notification.title.courseArchiveFinished";
 
-    public static final String COURSE_ARCHIVE_FAILED_TITLE = "Course archival failed";
+    public static final String COURSE_ARCHIVE_FAILED_TITLE = "notification.title.courseArchiveFailed";
 
-    public static final String EXAM_ARCHIVE_STARTED_TITLE = "Exam archival started";
+    public static final String EXAM_ARCHIVE_STARTED_TITLE = "notification.title.examArchiveStarted";
 
-    public static final String EXAM_ARCHIVE_FINISHED_TITLE = "Exam archival finished";
+    public static final String EXAM_ARCHIVE_FINISHED_TITLE = "notification.title.examArchiveFinished";
 
-    public static final String EXAM_ARCHIVE_FAILED_TITLE = "Exam archival failed";
+    public static final String EXAM_ARCHIVE_FAILED_TITLE = "notification.title.examArchiveFailed";
 
-    public static final String PROGRAMMING_TEST_CASES_CHANGED_TITLE = "Test cases for programming exercise changed";
+    public static final String PROGRAMMING_TEST_CASES_CHANGED_TITLE = "notification.title.programmingTestCasesChanged";
 
-    public static final String NEW_MANUAL_FEEDBACK_REQUEST_TITLE = "A new manual feedback request has been submitted";
+    public static final String NEW_MANUAL_FEEDBACK_REQUEST_TITLE = "notification.title.newManualFeedbackRequest";
 
-    public static final String NEW_PLAGIARISM_CASE_STUDENT_TITLE = "New plagiarism case";
+    public static final String NEW_PLAGIARISM_CASE_STUDENT_TITLE = "notification.title.newPlagiarismCaseStudent";
 
-    public static final String PLAGIARISM_CASE_VERDICT_STUDENT_TITLE = "Verdict for your plagiarism case";
+    public static final String PLAGIARISM_CASE_VERDICT_STUDENT_TITLE = "notification.title.plagiarismCaseVerdictStudent";
 
-    public static final String TUTORIAL_GROUP_REGISTRATION_STUDENT_TITLE = "You have been registered to a tutorial group";
+    public static final String TUTORIAL_GROUP_REGISTRATION_STUDENT_TITLE = "notification.title.tutorialGroupRegistrationStudent";
 
-    public static final String TUTORIAL_GROUP_DEREGISTRATION_STUDENT_TITLE = "You have been deregistered from a tutorial group";
+    public static final String TUTORIAL_GROUP_DEREGISTRATION_STUDENT_TITLE = "notification.title.tutorialGroupDeregistrationStudent";
 
-    public static final String TUTORIAL_GROUP_REGISTRATION_TUTOR_TITLE = "A student has been registered to your tutorial group";
+    public static final String TUTORIAL_GROUP_REGISTRATION_TUTOR_TITLE = "notification.title.tutorialGroupRegistrationTutor";
 
-    public static final String TUTORIAL_GROUP_DEREGISTRATION_TUTOR_TITLE = "A student has been deregistered from your tutorial group";
+    public static final String TUTORIAL_GROUP_DEREGISTRATION_TUTOR_TITLE = "notification.title.tutorialGroupDeregistrationTutor";
 
-    public static final String TUTORIAL_GROUP_REGISTRATION_MULTIPLE_TUTOR_TITLE = "Multiple students have been registered to your tutorial group";
+    public static final String TUTORIAL_GROUP_REGISTRATION_MULTIPLE_TUTOR_TITLE = "notification.title.tutorialGroupRegistrationMultipleTutor";
 
-    public static final String TUTORIAL_GROUP_DELETED_TITLE = "Tutorial Group deleted";
+    public static final String TUTORIAL_GROUP_DELETED_TITLE = "notification.title.tutorialGroupDeleted";
 
-    public static final String TUTORIAL_GROUP_UPDATED_TITLE = "Tutorial Group updated";
+    public static final String TUTORIAL_GROUP_UPDATED_TITLE = "notification.title.tutorialGroupUpdated";
 
-    public static final String TUTORIAL_GROUP_ASSIGNED_TITLE = "You have been assigned to lead a tutorial group";
+    public static final String TUTORIAL_GROUP_ASSIGNED_TITLE = "notification.title.tutorialGroupAssigned";
 
-    public static final String TUTORIAL_GROUP_UNASSIGNED_TITLE = "You have been unassigned from leading a tutorial group";
+    public static final String TUTORIAL_GROUP_UNASSIGNED_TITLE = "notification.title.tutorialGroupUnassigned";
 
     // bidirectional map
     private static final BiMap<NotificationType, String> NOTIFICATION_TYPE_AND_TITLE_MAP = new ImmutableBiMap.Builder<NotificationType, String>()
