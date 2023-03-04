be823338054f718e9c8a03451ef7a1f1c82f7a53
kutsch <frederik.kutsch@yahoo.com>
2023-03-04 18:14:24 +0100

    add user to repository URL

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localvc/LocalVCRepositoryUrl.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localvc/LocalVCRepositoryUrl.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import de.tum.in.www1.artemis.domain.VcsRepositoryUrl;
 import de.tum.in.www1.artemis.exception.localvc.LocalVCException;
@@ -13,20 +14,57 @@ import de.tum.in.www1.artemis.exception.localvc.LocalVCException;
  */
 public class LocalVCRepositoryUrl extends VcsRepositoryUrl {
 
+    private final String projectKey;
+
+    private final String repositorySlug;
+
+    private final String repositoryTypeOrUserName;
+
+    private final boolean isPracticeRepository;
+
     public LocalVCRepositoryUrl(String projectKey, String repositorySlug, URL localVCServerUrl) {
-        final String urlString = localVCServerUrl + buildRepositoryPath(projectKey, repositorySlug) + ".git";
+        final String urlString = localVCServerUrl + buildRepositoryPath(projectKey, repositorySlug);
         try {
             this.uri = new URI(urlString);
         }
         catch (URISyntaxException e) {
             throw new LocalVCException("Could not create local VC Repository URL", e);
         }
+
+        this.projectKey = projectKey;
+        this.repositorySlug = repositorySlug;
+        this.repositoryTypeOrUserName = getRepositoryTypeOrUserName(repositorySlug, projectKey);
+        this.isPracticeRepository = getIsPracticeRepository(repositorySlug, projectKey);
+    }
+
+    public LocalVCRepositoryUrl(String urlString, URL localVCBaseUrl) {
+        if (!urlString.startsWith(localVCBaseUrl.toString())) {
+            throw new LocalVCException("Invalid local VC Repository URL: " + urlString);
+        }
+
+        Path urlPath = Paths.get(urlString.replaceFirst(localVCBaseUrl.toString(), ""));
+
+        if (!urlPath.getName(0).toString().equals("git") || !urlPath.getName(2).toString().endsWith(".git")) {
+            throw new LocalVCException("Invalid local VC Repository URL: " + urlString);
+        }
+
+        try {
+            this.uri = new URI(urlString);
+        }
+        catch (URISyntaxException e) {
+            throw new LocalVCException("Could not create local VC Repository URL", e);
+        }
+
+        this.projectKey = urlPath.getName(1).toString();
+        this.repositorySlug = urlPath.getName(2).toString().replace(".git", "");
+        this.repositoryTypeOrUserName = getRepositoryTypeOrUserName(repositorySlug, projectKey);
+        this.isPracticeRepository = getIsPracticeRepository(repositorySlug, projectKey);
     }
 
     public LocalVCRepositoryUrl(Path repositoryPath, URL localVCServerUrl) {
         String projectKey = repositoryPath.getParent().getFileName().toString();
-        String repositorySlug = repositoryPath.getFileName().toString();
-        if (!repositorySlug.endsWith(".git") || !repositorySlug.toLowerCase().startsWith(projectKey.toLowerCase())) {
+        String repositorySlug = repositoryPath.getFileName().toString().replace(".git", "");
+        if (!repositorySlug.toLowerCase().startsWith(projectKey.toLowerCase())) {
             throw new LocalVCException("Invalid repository path: " + repositoryPath);
         }
 
@@ -37,9 +75,43 @@ public class LocalVCRepositoryUrl extends VcsRepositoryUrl {
         catch (URISyntaxException e) {
             throw new LocalVCException("Could not create local VC Repository URL", e);
         }
+
+        this.projectKey = projectKey;
+        this.repositorySlug = repositorySlug;
+        this.repositoryTypeOrUserName = getRepositoryTypeOrUserName(repositorySlug, projectKey);
+        this.isPracticeRepository = getIsPracticeRepository(repositorySlug, projectKey);
     }
 
     private String buildRepositoryPath(String projectKey, String repositorySlug) {
-        return "/git/" + projectKey + "/" + repositorySlug;
+        return "/git/" + projectKey + "/" + repositorySlug + ".git";
+    }
+
+    private String getRepositoryTypeOrUserName(String repositorySlug, String projectKey) {
+        String repositoryTypeOrUserNameWithPracticePrefix = repositorySlug.toLowerCase().replace(projectKey.toLowerCase() + "-", "");
+        return repositoryTypeOrUserNameWithPracticePrefix.replace("practice-", "");
+    }
+
+    private boolean getIsPracticeRepository(String repositorySlug, String projectKey) {
+        return repositorySlug.toLowerCase().startsWith(projectKey.toLowerCase() + "-practice-");
+    }
+
+    public String getProjectKey() {
+        return projectKey;
+    }
+
+    public String getRepositorySlug() {
+        return repositorySlug;
+    }
+
+    public String getRepositoryTypeOrUserName() {
+        return repositoryTypeOrUserName;
+    }
+
+    public boolean isPracticeRepository() {
+        return isPracticeRepository;
+    }
+
+    public Path getLocalRepositoryPath(String localVCBasePath) {
+        return Paths.get(localVCBasePath, projectKey, repositorySlug + ".git");
     }
 }
