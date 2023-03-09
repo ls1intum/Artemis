42a14ff527e9a40cbc6b1b2f7cf53f771a8e7477
Ludwig Pusl <ludwig.pusl@tum.de>
2023-03-09 15:27:24 +0100

    E2E Tests: specify which containers should be built/pulled

--- .bamboo/E2E-tests/execute.sh
+++ .bamboo/E2E-tests/execute.sh
@@ -3,8 +3,9 @@
 # pass current host's hostname to the docker container for server.url (see docker compose config file)
 export HOST_HOSTNAME=$(hostname)
 
-docker-compose -f ./src/main/docker/cypress/cypress-E2E-tests.yml pull
-docker-compose -f ./src/main/docker/cypress/cypress-E2E-tests.yml build --no-cache --pull
+# just pull everything else than artemis-app as we build it later either way
+docker-compose -f ./src/main/docker/cypress/cypress-E2E-tests.yml pull artemis-cypress mysql artemis-nginx
+docker-compose -f ./src/main/docker/cypress/cypress-E2E-tests.yml build artemis-app --no-cache --pull
 docker-compose -f ./src/main/docker/cypress/cypress-E2E-tests.yml up --exit-code-from artemis-cypress
 exitCode=$?
 echo "Cypress container exit code: $exitCode"
