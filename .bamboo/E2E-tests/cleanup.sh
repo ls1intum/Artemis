9a749b64896b1686d38887e7995bf6799a170ac4
Ludwig Pusl <ludwig.pusl@tum.de>
2023-03-09 17:43:28 +0100

    trying to force stop and remove all docker containers, named volumes and networks in bamboo

--- .bamboo/E2E-tests/cleanup.sh
+++ .bamboo/E2E-tests/cleanup.sh
@@ -6,3 +6,6 @@ cd src/main/docker/cypress
 export HOST_HOSTNAME=$(hostname)
 docker-compose -f cypress-E2E-tests.yml stop -t 60
 docker-compose -f cypress-E2E-tests.yml rm -v -f
+# The following docker compose command could usually replace the commands above. But as we don't know the internals
+# of the commands we use both to make this more robust and "really" stop and remove all containers, volumes and networks
+docker-compose -f cypress-E2E-tests.yml down -v -t 60
