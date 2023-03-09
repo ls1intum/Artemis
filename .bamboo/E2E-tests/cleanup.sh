dbf9b47826bfc593d016856906867a576860ad28
Ludwig Pusl <ludwig.pusl@tum.de>
2023-03-09 15:30:51 +0100

    E2E Tests: more robust cleanup script with old docker compose version maybe

--- .bamboo/E2E-tests/cleanup.sh
+++ .bamboo/E2E-tests/cleanup.sh
@@ -4,4 +4,5 @@ cd src/main/docker/cypress
 
 # HOST_HOSTNAME not really necessary for shutdown but otherwise docker-compose complains
 export HOST_HOSTNAME=$(hostname)
-docker-compose -f cypress-E2E-tests.yml down -v
+docker-compose -f cypress-E2E-tests.yml stop -t 60
+docker-compose -f cypress-E2E-tests.yml rm -v -f
