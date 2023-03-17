adbdd890279ebd1c1c1313c98f93311595ad38a1
Ludwig Pusl <ludwig.pusl@tum.de>
2023-03-17 09:40:51 +0100

    revert .bamboo cleanup scripts to origin/develop

--- .bamboo/E2E-tests/cleanup.sh
+++ .bamboo/E2E-tests/cleanup.sh
@@ -4,8 +4,4 @@ cd src/main/docker/cypress
 
 # HOST_HOSTNAME not really necessary for shutdown but otherwise docker-compose complains
 export HOST_HOSTNAME=$(hostname)
-docker-compose -f cypress-E2E-tests.yml stop -t 60
-docker-compose -f cypress-E2E-tests.yml rm -v -f
-# The following docker compose command could usually replace the commands above. But as we don't know the internals
-# of the commands we use both to make this more robust and "really" stop and remove all containers, volumes and networks
-docker-compose -f cypress-E2E-tests.yml down -v -t 60
+docker compose -f cypress-E2E-tests.yml down -v
