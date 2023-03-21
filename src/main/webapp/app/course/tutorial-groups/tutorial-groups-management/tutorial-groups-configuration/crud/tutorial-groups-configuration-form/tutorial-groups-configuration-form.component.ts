2221796790ccfcf090bc0249e838ff5107a0c7d5
Stefan Waldhauser <stefan.waldhauser@proton.me>
2023-03-21 22:23:20 +0100

    Fix position of range picker

--- src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-groups-configuration/crud/tutorial-groups-configuration-form/tutorial-groups-configuration-form.component.ts
+++ src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-groups-configuration/crud/tutorial-groups-configuration-form/tutorial-groups-configuration-form.component.ts
@@ -12,6 +12,7 @@ export interface TutorialGroupsConfigurationFormData {
 @Component({
     selector: 'jhi-tutorial-groups-configuration-form',
     templateUrl: './tutorial-groups-configuration-form.component.html',
+    styleUrls: ['./tutorial-groups-configuration-form.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TutorialGroupsConfigurationFormComponent implements OnInit, OnChanges {
