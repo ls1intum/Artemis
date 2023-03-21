1f5d561112cc0ce672ff74425eaadf0ea94a6f78
Lucas Welscher <ga53foy@mytum.de>
2023-03-21 17:34:16 +0100

    Add comments to SQL migration

--- src/main/java/de/tum/in/www1/artemis/repository/FeedbackRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/FeedbackRepository.java
@@ -1,7 +1,5 @@
 package de.tum.in.www1.artemis.repository;
 
-import static de.tum.in.www1.artemis.config.Constants.FEEDBACK_DETAIL_TEXT_MAX_CHARACTERS;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -10,13 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import de.tum.in.www1.artemis.config.Constants;
 import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.enumeration.*;
-import de.tum.in.www1.artemis.service.dto.StaticCodeAnalysisReportDTO;
 
 /**
  * Spring Data JPA repository for the Feedback entity.
@@ -24,8 +17,6 @@ import de.tum.in.www1.artemis.service.dto.StaticCodeAnalysisReportDTO;
 @SuppressWarnings("unused")
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
-    String DEFAULT_FILEPATH = "notAvailable";
 
     List<Feedback> findByResult(Result result);
 
@@ -68,55 +59,6 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     }
 
     /**
-     * Transforms static code analysis reports to feedback objects.
-     * As we reuse the Feedback entity to store static code analysis findings, a mapping to those attributes
-     * has to be defined, violating the first normal form.
-     *
-     * Mapping:
-     * - text: STATIC_CODE_ANALYSIS_FEEDBACK_IDENTIFIER
-     * - reference: Tool
-     * - detailText: Issue object as JSON
-     *
-     * @param reports Static code analysis reports to be transformed
-     * @return Feedback objects representing the static code analysis findings
-     */
-    default List<Feedback> createFeedbackFromStaticCodeAnalysisReports(List<StaticCodeAnalysisReportDTO> reports) {
-        ObjectMapper mapper = new ObjectMapper();
-        List<Feedback> feedbackList = new ArrayList<>();
-        for (final var report : reports) {
-            StaticCodeAnalysisTool tool = report.getTool();
-
-            for (final var issue : report.getIssues()) {
-                // Remove CI specific path segments
-                issue.setFilePath(removeCIDirectoriesFromPath(issue.getFilePath()));
-
-                if (issue.getMessage() != null) {
-                    // Note: the feedback detail text is limited to 5.000 characters, so we limit the issue message to 4.500 characters to avoid issues
-                    // the remaining 500 characters are used for the json structure of the issue
-                    int maxLength = Math.min(issue.getMessage().length(), FEEDBACK_DETAIL_TEXT_MAX_CHARACTERS - 500);
-                    issue.setMessage(issue.getMessage().substring(0, maxLength));
-                }
-
-                Feedback feedback = new Feedback();
-                feedback.setText(Feedback.STATIC_CODE_ANALYSIS_FEEDBACK_IDENTIFIER);
-                feedback.setReference(tool.name());
-                feedback.setType(FeedbackType.AUTOMATIC);
-                feedback.setPositive(false);
-
-                // Store static code analysis in JSON format
-                try {
-                    feedback.setDetailText(mapper.writeValueAsString(issue));
-                }
-                catch (JsonProcessingException e) {
-                    continue;
-                }
-                feedbackList.add(feedback);
-            }
-        }
-        return feedbackList;
-    }
-
-    /**
      * Given the grading criteria, collects each sub grading instructions in a list.
      * Then, find all feedback that matches with the grading instructions ids
      *
@@ -127,22 +69,5 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
         List<Long> gradingInstructionsIds = gradingCriteria.stream().flatMap(gradingCriterion -> gradingCriterion.getStructuredGradingInstructions().stream())
                 .map(GradingInstruction::getId).toList();
         return findFeedbackByGradingInstructionIds(gradingInstructionsIds);
-    }
-
-    /**
-     * Removes CI specific path segments. Uses the assignment directory to decide where to cut the path.
-     *
-     * @param sourcePath Path to be shortened
-     * @return Shortened path if it contains an assignment directory, otherwise the full path
-     */
-    private String removeCIDirectoriesFromPath(String sourcePath) {
-        if (sourcePath == null || sourcePath.isEmpty()) {
-            return DEFAULT_FILEPATH;
-        }
-        int workingDirectoryStart = sourcePath.indexOf(Constants.ASSIGNMENT_DIRECTORY);
-        if (workingDirectoryStart == -1) {
-            return sourcePath;
-        }
-        return sourcePath.substring(workingDirectoryStart + Constants.ASSIGNMENT_DIRECTORY.length());
     }
 }
