ee93f17eff58b4e3cc693b6cfbf1fcecd018db49
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-03-21 13:25:00 +0100

    Added import to exercise-categories.module.ts
    Added the correct RouterModule this time

--- src/main/webapp/app/exercises/shared/exercise-categories/exercise-categories.module.ts
+++ src/main/webapp/app/exercises/shared/exercise-categories/exercise-categories.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { ExerciseCategoriesComponent } from 'app/exercises/shared/exercise-categories/exercise-categories.component';
 
 @NgModule({
-    imports: [ArtemisSharedModule],
+    imports: [ArtemisSharedModule, RouterModule],
     declarations: [ExerciseCategoriesComponent],
     exports: [ExerciseCategoriesComponent],
 })
