9b78cb71ad8f28cbb90ada04835f17f00597e5bc
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-03-22 19:23:41 +0100

    Implemented suggestions by Simon
    - Replaced negative margin and removed !important annotation
    - Implemented interface ShowTagsConfig for passing the configuration in a single object

--- src/main/webapp/app/shared/exercise-categories/exercise-categories.component.ts
+++ src/main/webapp/app/shared/exercise-categories/exercise-categories.component.ts
@@ -3,9 +3,18 @@ import { Exercise, IncludedInOverallScore } from 'app/entities/exercise.model';
 import dayjs from 'dayjs/esm';
 import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
 
+interface ShowTagsConfig {
+    notReleased?: boolean;
+    quizLive?: boolean;
+    difficulty?: boolean;
+    difficultyIfNoLevel?: boolean;
+    includedInScore?: boolean;
+}
+
 @Component({
     selector: 'jhi-exercise-categories',
     templateUrl: './exercise-categories.component.html',
+    styleUrls: ['./exercise-categories.component.scss'],
 })
 export class ExerciseCategoriesComponent {
     readonly IncludedInOverallScore = IncludedInOverallScore;
@@ -13,11 +22,14 @@ export class ExerciseCategoriesComponent {
 
     @Input() exercise: Exercise;
 
-    @Input() shouldShowNotReleasedTag = false;
-    @Input() shouldShowQuizLiveTag = false;
-    @Input() shouldShowDifficultyTag = false;
-    @Input() shouldShowDifficultyTagIfNoLevel = false;
-    @Input() shouldShowIncludedInScoreBadge = false;
+    @Input()
+    showTags: ShowTagsConfig = {
+        notReleased: false,
+        quizLive: false,
+        difficulty: false,
+        difficultyIfNoLevel: false,
+        includedInScore: false,
+    };
 
     asQuizExercise(exercise: Exercise): QuizExercise {
         return exercise as QuizExercise;
