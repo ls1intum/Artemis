2366a685f31e69923a1d9fb09053b8db82a8d6c9
Simon Leiß <git@simon-leiss.de>
2023-03-24 10:33:47 +0100

    Use ApplicationReadyEvent.

--- src/main/java/de/tum/in/www1/artemis/service/feature/ProfileToggleService.java
+++ src/main/java/de/tum/in/www1/artemis/service/feature/ProfileToggleService.java
@@ -1,11 +1,15 @@
 package de.tum.in.www1.artemis.service.feature;
 
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.hazelcast.cluster.InitialMembershipEvent;
@@ -13,6 +17,7 @@ import com.hazelcast.cluster.InitialMembershipListener;
 import com.hazelcast.cluster.Member;
 import com.hazelcast.cluster.MembershipEvent;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 
 import de.tum.in.www1.artemis.service.WebsocketMessagingService;
 
@@ -21,37 +26,62 @@ public class ProfileToggleService {
 
     private final Logger log = LoggerFactory.getLogger(ProfileToggleService.class);
 
+    private static final String INSTANCE_PROFILE_MAP_NAME = "instanceProfiles";
+
+    private static final String INSTANCE_READY_TOPIC_NAME = "instanceReady";
+
     private static final String TOPIC_PROFILE_TOGGLES = "/topic/management/profile-toggles";
 
     private final WebsocketMessagingService websocketMessagingService;
 
     private final HazelcastInstance hazelcastInstance;
 
-    public ProfileToggleService(WebsocketMessagingService websocketMessagingService, HazelcastInstance hazelcastInstance) {
+    private final Environment env;
+
+    private final IMap<UUID, String> instanceProfilesMap;
+
+    public ProfileToggleService(WebsocketMessagingService websocketMessagingService, HazelcastInstance hazelcastInstance, Environment env) {
         this.websocketMessagingService = websocketMessagingService;
         this.hazelcastInstance = hazelcastInstance;
+        this.env = env;
+
+        this.instanceProfilesMap = hazelcastInstance.getMap(INSTANCE_PROFILE_MAP_NAME);
 
         hazelcastInstance.getCluster().addMembershipListener(new InitialMembershipListener() {
 
             @Override
             public void init(InitialMembershipEvent event) {
-                sendUpdate(event.getMembers());
             }
 
             @Override
             public void memberAdded(MembershipEvent membershipEvent) {
-                sendUpdate(membershipEvent.getMembers());
             }
 
             @Override
             public void memberRemoved(MembershipEvent membershipEvent) {
+                log.debug("Instance removed event received");
+                instanceProfilesMap.remove(membershipEvent.getMember().getUuid());
                 sendUpdate(membershipEvent.getMembers());
             }
         });
+
+        hazelcastInstance.<String>getTopic(INSTANCE_READY_TOPIC_NAME).addMessageListener(instanceId -> {
+            log.debug("Instance startup event received");
+            sendUpdate(hazelcastInstance.getCluster().getMembers());
+        });
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void publishAvailableProfiles() {
+        // Update available profiles for this instance
+        instanceProfilesMap.put(hazelcastInstance.getLocalEndpoint().getUuid(), String.join(",", env.getActiveProfiles()));
+
+        // Inform other instances about readyness
+        hazelcastInstance.getTopic(INSTANCE_READY_TOPIC_NAME).publish(hazelcastInstance.getLocalEndpoint().getUuid());
     }
 
     private void sendUpdate(Set<Member> members) {
-        log.debug("Sending membership update");
+        log.debug("Sending membership update " + enabledProfiles(members));
         websocketMessagingService.sendMessage(TOPIC_PROFILE_TOGGLES, enabledProfiles(members));
     }
 
@@ -61,7 +91,7 @@ public class ProfileToggleService {
      * @return A list of enabled profiles (on the whole system)
      */
     private Set<String> enabledProfiles(Set<Member> members) {
-        return members.stream().flatMap(member -> Stream.of(member.getAttributes().getOrDefault("profiles", "").split(","))).collect(Collectors.toSet());
+        return members.stream().flatMap(member -> Stream.of(instanceProfilesMap.getOrDefault(member.getUuid(), "").split(","))).collect(Collectors.toSet());
     }
 
     /**
