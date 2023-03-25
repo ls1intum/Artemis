3b54b0444efbfe1a62954c5601f13dcb7ba675df
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-03-25 17:01:51 +0400

    removed duplicate identifier after merge

--- src/test/javascript/spec/component/course/course-update.component.spec.ts
+++ src/test/javascript/spec/component/course/course-update.component.spec.ts
@@ -30,9 +30,9 @@ import dayjs from 'dayjs/esm';
 import { ImageCropperModule } from 'app/shared/image-cropper/image-cropper.module';
 import { ProgrammingLanguage } from 'app/entities/programming-exercise.model';
 import { CourseAdminService } from 'app/course/manage/course-admin.service';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { AccountService } from 'app/core/auth/account.service';
 import { MockAccountService } from '../../helpers/mocks/service/mock-account.service';
+import { By } from '@angular/platform-browser';
 
 @Component({ selector: 'jhi-markdown-editor', template: '' })
 class MarkdownEditorStubComponent {
