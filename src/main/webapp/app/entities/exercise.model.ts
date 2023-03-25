a4a9356980b2a72ef061a1dff43f4ec888d58275
kutsch <frederik.kutsch@yahoo.com>
2023-03-25 12:01:52 +0100

    remove dot

--- src/main/webapp/app/entities/exercise.model.ts
+++ src/main/webapp/app/entities/exercise.model.ts
@@ -30,7 +30,7 @@ export enum ExerciseMode {
     TEAM = 'TEAM',
 }
 
-// IMPORTANT NOTICE: The following strings have to be consistent with the ones defined in Exercise.java.
+// IMPORTANT NOTICE: The following strings have to be consistent with the ones defined in Exercise.java
 export enum ExerciseType {
     PROGRAMMING = 'programming',
     MODELING = 'modeling',
