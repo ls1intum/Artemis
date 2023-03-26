033ecc39364d10ed97897755766ecdbd8bac34ff
Tobias Lippert <t.lippert@tum.de>
2023-03-26 11:11:55 +0200

    fix import dialog for SCA and exams

--- src/main/webapp/app/exercises/shared/import/exercise-import-wrapper/exercise-import-wrapper.component.ts
+++ src/main/webapp/app/exercises/shared/import/exercise-import-wrapper/exercise-import-wrapper.component.ts
@@ -1,18 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ExerciseType } from 'app/entities/exercise.model';
+import { ProgrammingLanguage } from 'app/entities/programming-exercise.model';
 
 @Component({
     selector: 'jhi-exercise-import-wrapper',
     templateUrl: './exercise-import-wrapper.component.html',
 })
-export class ExerciseImportWrapperComponent {
+export class ExerciseImportWrapperComponent implements OnInit {
     readonly ExerciseType = ExerciseType;
 
     @Input()
     exerciseType: ExerciseType;
+    titleKey: string;
+    @Input()
+    programmingLanguage?: ProgrammingLanguage;
 
     constructor(private activeModal: NgbActiveModal) {}
+
+    ngOnInit(): void {
+        if (this.programmingLanguage) {
+            this.titleKey = 'artemisApp.programmingExercise.configureGrading.categories.importLabel';
+        } else {
+            this.titleKey =
+                this.exerciseType === ExerciseType.FILE_UPLOAD ? `artemisApp.fileUploadExercise.home.importLabel` : `artemisApp.${this.exerciseType}Exercise.home.importLabel`;
+        }
+    }
 
     clear() {
         this.activeModal.dismiss('cancel');
