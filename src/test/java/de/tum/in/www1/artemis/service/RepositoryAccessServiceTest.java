41d1f98dc5cf4ffcc9eba04b96855926aa9b08aa
kutsch <frederik.kutsch@yahoo.com>
2023-03-26 14:03:10 +0200

    add testShouldDenyAccessToTestRepository

--- src/test/java/de/tum/in/www1/artemis/service/RepositoryAccessServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/RepositoryAccessServiceTest.java
@@ -1,40 +1,66 @@
 package de.tum.in.www1.artemis.service;
 
-import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
 import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.ProgrammingExercise;
 import de.tum.in.www1.artemis.domain.User;
-import de.tum.in.www1.artemis.domain.enumeration.InitializationState;
-import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseParticipation;
-import de.tum.in.www1.artemis.util.ModelFactory;
+import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseStudentParticipation;
+import de.tum.in.www1.artemis.domain.submissionpolicy.LockRepositoryPolicy;
+import de.tum.in.www1.artemis.repository.UserRepository;
+import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException;
+import de.tum.in.www1.artemis.web.rest.repository.RepositoryActionType;
 
 public class RepositoryAccessServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
+
+    private static final String TEST_PREFIX = "rastest";
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private RepositoryAccessService repositoryAccessService;
 
     User user;
 
-    ProgrammingExercise programmingExercise;
+    Course course;
 
-    ProgrammingExerciseParticipation participation;
+    ProgrammingExercise programmingExercise;
 
     @BeforeEach
     void setup() {
-        user = ModelFactory.generateActivatedUser("test_student_1");
-        Course course = ModelFactory.generateCourse(1L, null, null, Collections.emptySet());
-        programmingExercise = ModelFactory.generateProgrammingExercise(null, null, course);
-        participation = ModelFactory.generateProgrammingExerciseStudentParticipation(InitializationState.INITIALIZED, programmingExercise, user);
+        database.addUsers(TEST_PREFIX, 1, 0, 0, 0);
+        user = userRepository.findOneByLogin(TEST_PREFIX + "student1").orElseThrow();
+        course = database.addCourseWithOneProgrammingExercise();
+        programmingExercise = (ProgrammingExercise) course.getExercises().stream().iterator().next();
     }
 
     @Test
     void testShouldEnforceLockRepositoryPolicy() {
-        // Prepare a ProgrammingExerciseParticipation
+        ProgrammingExerciseStudentParticipation participation = database.addStudentParticipationForProgrammingExercise(programmingExercise, user.getLogin());
+        database.createSubmissionAndResult(participation, 50, true);
+
+        // Add LockRepositoryPolicy to the programmingExercise.
+        LockRepositoryPolicy lockRepositoryPolicy = new LockRepositoryPolicy();
+        lockRepositoryPolicy.setActive(true);
+        lockRepositoryPolicy.setSubmissionLimit(1);
+        database.addSubmissionPolicyToExercise(lockRepositoryPolicy, programmingExercise);
+
+        // Should throw an AccessForbiddenException because the submission limit is already reached.
+        assertThrows(AccessForbiddenException.class,
+                () -> repositoryAccessService.checkAccessRepositoryElseThrow(participation, user, programmingExercise, RepositoryActionType.WRITE));
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @ValueSource(booleans = { true, false })
+    void testShouldDenyAccessToTestRepository(boolean atLeastEditor) {
+        assertThrows(AccessForbiddenException.class, () -> repositoryAccessService.checkAccessTestRepositoryElseThrow(atLeastEditor, programmingExercise, user));
     }
 }
