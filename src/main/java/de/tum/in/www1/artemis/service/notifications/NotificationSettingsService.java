5467bfd24171620d7d0e49d75f56dbf697d9ded8
Sven Andabaka <sven.andabaka@tum.de>
2023-03-27 11:29:18 +0200

    fix email support

--- src/main/java/de/tum/in/www1/artemis/service/notifications/NotificationSettingsService.java
+++ src/main/java/de/tum/in/www1/artemis/service/notifications/NotificationSettingsService.java
@@ -136,7 +136,8 @@ public class NotificationSettingsService {
             TUTORIAL_GROUP_DEREGISTRATION_TUTOR, TUTORIAL_GROUP_DELETED, TUTORIAL_GROUP_UPDATED, TUTORIAL_GROUP_ASSIGNED, TUTORIAL_GROUP_UNASSIGNED, NEW_EXERCISE_POST,
             NEW_LECTURE_POST, NEW_REPLY_FOR_LECTURE_POST, NEW_COURSE_POST, NEW_REPLY_FOR_COURSE_POST, QUIZ_EXERCISE_STARTED);
 
-    private static final Set<NotificationType> INSTANT_NOTIFICATION_TYPES_WITHOUT_EMAIL_SUPPORT = Set.of(QUIZ_EXERCISE_STARTED);
+    private static final Set<NotificationType> INSTANT_NOTIFICATION_TYPES_WITHOUT_EMAIL_SUPPORT = Set.of(QUIZ_EXERCISE_STARTED, NEW_EXERCISE_POST, NEW_LECTURE_POST,
+            NEW_REPLY_FOR_LECTURE_POST, NEW_COURSE_POST, NEW_REPLY_FOR_COURSE_POST);
 
     public NotificationSettingsService(NotificationSettingRepository notificationSettingRepository) {
         this.notificationSettingRepository = notificationSettingRepository;
