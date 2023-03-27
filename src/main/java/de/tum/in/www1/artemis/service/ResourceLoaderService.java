69f4005aae0a97670cf872eab8c8a65634900c55
Lucas Welscher <ga53foy@mytum.de>
2023-03-27 11:26:26 +0200

    Revert "Development: Move programming exercise repository actions into separate service (#6273)"
    This reverts commit 16b29e0d to fix local development

--- src/main/java/de/tum/in/www1/artemis/service/ResourceLoaderService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ResourceLoaderService.java
@@ -35,31 +35,12 @@ public class ResourceLoaderService {
     }
 
     /**
-     * Loads the resource from the specified path.
-     * <p>
-     * Only relative paths are allowed.
-     *
-     * @param path A relative path to a resource.
-     * @return The resource located by the specified path.
-     * @throws IllegalArgumentException Thrown in case an absolute path was given.
-     */
-    public Resource getResource(final Path path) throws IllegalArgumentException {
-        if (path.isAbsolute()) {
-            throw new IllegalArgumentException("Cannot load resources from absolute paths!");
-        }
-
-        return getResource(path.toString());
-    }
-
-    /**
      * Load the resource from the specified path. The path MUST NOT start with a '/', it is appended automatically if needed.
      * File will be loaded from the relative path, if it exists, from the classpath otherwise.
      *
      * @param path the path to load the file from. Must not start with a '/'.
      * @return the loaded resource, which might not exist ({@link Resource#exists()}.
-     * @deprecated Use {@link #getResource(Path)} instead.
      */
-    @Deprecated(forRemoval = true)
     public Resource getResource(String path) {
         Resource resource = null;
         // Try to load from filesystem if override is allowed for path
@@ -80,28 +61,9 @@ public class ResourceLoaderService {
      *
      * @param pathSegments the segments of the path (e.g. ["templates", "java", "pom.xml"]). Will automatically be joined with '/'.
      * @return the loaded resource, which might not exist ({@link Resource#exists()}.
-     * @deprecated Use {@link #getResource(Path)} instead.
      */
-    @Deprecated(forRemoval = true)
     public Resource getResource(String... pathSegments) {
         return getResource(StringUtils.join(pathSegments, File.separator));
-    }
-
-    /**
-     * Loads the resources from the specified path.
-     * <p>
-     * Only relative paths are allowed.
-     *
-     * @param path A relative path to a resource.
-     * @return The resources located by the specified path.
-     * @throws IllegalArgumentException Thrown in case an absolute path was given.
-     */
-    public Resource[] getResources(final Path path) throws IllegalArgumentException {
-        if (path.isAbsolute()) {
-            throw new IllegalArgumentException("Cannot load resources from absolute paths!");
-        }
-
-        return getResources(path.toString());
     }
 
     /**
@@ -110,9 +72,7 @@ public class ResourceLoaderService {
      *
      * @param path the path to load the file from. Must not start with a '/'.
      * @return the loaded resources, which might be an empty array
-     * @deprecated Use {@link #getResources(Path)} instead.
      */
-    @Deprecated(forRemoval = true)
     public Resource[] getResources(String path) {
         Resource[] resources = null;
         // Try to load from filesystem if override is allowed for path
@@ -142,9 +102,7 @@ public class ResourceLoaderService {
      *
      * @param pathSegments the segments of the path (e.g. ["templates", "java"]). Will automatically be joined with '/'.
      * @return the loaded resources, which might be an empty array
-     * @deprecated Use {@link #getResources(Path)} instead.
      */
-    @Deprecated(forRemoval = true)
     public Resource[] getResources(String... pathSegments) {
         return getResources(StringUtils.join(pathSegments, File.separator));
     }
