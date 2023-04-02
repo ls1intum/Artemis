057f66d01d23b6d8d57f4cadbf9b315f86510a57
Rizky Riyaldhi <rriyaldhi@gmail.com>
2023-04-02 13:40:05 +0200

    Remove unnecessary change

--- src/main/webapp/app/entities/exam-exercise-overview-item.model.ts
+++ src/main/webapp/app/entities/exam-exercise-overview-item.model.ts
@@ -1,5 +1,5 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { Exercise } from 'app/entities/exercise.model';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 export class ExamExerciseOverviewItem {
     public exercise: Exercise;
