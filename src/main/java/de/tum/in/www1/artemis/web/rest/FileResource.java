fc9b7776b00a3a494fefc88d6a4ffdf96c5349b5
Pellumb Baboci <pellumbbaboci@gmail.com>
2023-04-03 02:25:31 +0200

    add cache for slide images

--- src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/FileResource.java
@@ -417,7 +417,7 @@ public class FileResource {
         if (!checkAttachmentAuthorization(course, attachment)) {
             throw new AccessForbiddenException();
         }
-        return buildFileResponse(Path.of(FilePathService.getSlideImageFilePath(), String.valueOf(attachmentUnit.getId())).toString(), filename);
+        return buildFileResponse(Path.of(FilePathService.getSlideImageFilePath(), String.valueOf(attachmentUnit.getId())).toString(), filename, true);
     }
 
     /**
