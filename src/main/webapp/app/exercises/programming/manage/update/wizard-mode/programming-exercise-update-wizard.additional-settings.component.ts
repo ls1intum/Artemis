7e00aa2aecdad56d33a609c6a588f9dc016cfb69
Simon Huang <simon.huang@tum.de>
2023-04-04 21:31:12 +0200

    Add texts and translations

--- src/main/webapp/app/exercises/programming/manage/update/wizard-mode/programming-exercise-update-wizard.additional-settings.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/wizard-mode/programming-exercise-update-wizard.additional-settings.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'jhi-programming-exercise-update-wizard-additional-settings',
     template: `
-        <h1><span jhiTranslate="artemisApp.programmingExercise.wizardMode.detailedSteps.difficultyStepTitle">Additional settings</span></h1>
-        <p><span jhiTranslate="artemisApp.programmingExercise.wizardMode.detailedSteps.difficultyStepMessage">Set additional settings related to infrastructure etc</span></p>
+        <h1><span jhiTranslate="artemisApp.programmingExercise.wizardMode.detailedSteps.additionalStepTitle">Additional settings</span></h1>
+        <p><span jhiTranslate="artemisApp.programmingExercise.wizardMode.detailedSteps.additionalStepMessage">Set additional settings related to infrastructure etc</span></p>
         <jhi-programming-exercise-additional-settings></jhi-programming-exercise-additional-settings>
     `,
     styleUrls: ['./programming-exercise-update-wizard.component.scss'],
