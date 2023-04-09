eb4eeac586af48d0c1a24bfd8e548c9d21fb8894
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-04-09 15:29:22 +0400

    Create a shared abstract import component to reduce duplicate code

--- src/main/webapp/app/shared/course-group/course-group.module.ts
+++ src/main/webapp/app/shared/course-group/course-group.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CourseGroupComponent } from 'app/shared/course-group/course-group.component';
 import { ArtemisDataTableModule } from 'app/shared/data-table/data-table.module';
-import { UserImportModule } from 'app/shared/import/user-import.module';
+import { UserImportModule } from 'app/shared/user-import/user-import.module';
 import { NgxDatatableModule } from '@flaviosantoro92/ngx-datatable';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { RouterModule } from '@angular/router';
--- src/test/javascript/spec/component/learning-goals/learning-goal-import.component.spec.ts
+++ src/test/javascript/spec/component/learning-goals/learning-goal-import.component.spec.ts
@@ -8,10 +8,11 @@ import { of } from 'rxjs';
 import { ArtemisTestModule } from '../../test.module';
 import { SortByDirective } from 'app/shared/sort/sort-by.directive';
 import { SortDirective } from 'app/shared/sort/sort.directive';
-import { LearningGoalImportComponent, TableColumn } from 'app/course/learning-goals/learning-goal-management/learning-goal-import.component';
+import { LearningGoalImportComponent } from 'app/course/learning-goals/learning-goal-management/learning-goal-import.component';
 import { LearningGoalPagingService } from 'app/course/learning-goals/learning-goal-paging.service';
 import { LearningGoal } from 'app/entities/learningGoal.model';
 import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
+import { TableColumn } from 'app/shared/import/import-component';
 
 describe('LearningGoalImportComponent', () => {
     let fixture: ComponentFixture<LearningGoalImportComponent>;
