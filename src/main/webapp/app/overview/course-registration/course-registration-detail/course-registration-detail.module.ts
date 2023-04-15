0d1ae459cddc53c692419767b6025edcf442106b
Paul Schwind <pal03377@users.noreply.github.com>
2023-04-15 17:11:20 +0200

    General: Allow direct linking to self-registering courses (#6318)

--- src/main/webapp/app/overview/course-registration/course-registration-detail/course-registration-detail.module.ts
+++ src/main/webapp/app/overview/course-registration/course-registration-detail/course-registration-detail.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CourseRegistrationComponent } from 'app/overview/course-registration/course-registration.component';
 import { Authority } from 'app/shared/constants/authority.constants';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
-import { ArtemisLearningGoalsModule } from 'app/course/learning-goals/learning-goal.module';
-import { CourseRegistrationButtonModule } from 'app/overview/course-registration/course-registration-button/course-registration-button.module';
+import { CourseRegistrationDetailComponent } from 'app/overview/course-registration/course-registration-detail/course-registration-detail.component';
 import { CoursePrerequisitesButtonModule } from 'app/overview/course-registration/course-prerequisites-button/course-prerequisites-button.module';
+import { CourseRegistrationButtonModule } from 'app/overview/course-registration/course-registration-button/course-registration-button.module';
 
 const routes: Routes = [
     {
         path: '',
-        component: CourseRegistrationComponent,
+        component: CourseRegistrationDetailComponent,
         data: {
             authorities: [Authority.USER],
             pageTitle: 'artemisApp.studentDashboard.register.title',
@@ -22,14 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [
-        ArtemisSharedModule,
-        ArtemisSharedComponentModule,
-        RouterModule.forChild(routes),
-        ArtemisLearningGoalsModule,
-        CourseRegistrationButtonModule,
-        CoursePrerequisitesButtonModule,
-    ],
-    declarations: [CourseRegistrationComponent],
+    imports: [ArtemisSharedModule, ArtemisSharedComponentModule, RouterModule.forChild(routes), CoursePrerequisitesButtonModule, CourseRegistrationButtonModule],
+    declarations: [CourseRegistrationDetailComponent],
 })
-export class CourseRegistrationModule {}
+export class CourseRegistrationDetailModule {}
