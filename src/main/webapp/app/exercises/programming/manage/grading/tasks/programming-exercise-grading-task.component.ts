2b68d901b9650da91d60db8ccbba0a72d309c7bb
Simon Huang <simon.huang@tum.de>
2023-04-19 14:46:53 +0200

    Add basic sorting

--- src/main/webapp/app/exercises/programming/manage/grading/tasks/programming-exercise-grading-task.component.ts
+++ src/main/webapp/app/exercises/programming/manage/grading/tasks/programming-exercise-grading-task.component.ts
@@ -2,10 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ProgrammingExerciseTaskService } from 'app/exercises/programming/manage/grading/tasks/programming-exercise-task.service';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { Course } from 'app/entities/course.model';
-import { faAngleDown, faAngleRight, faAsterisk, faMedal, faQuestionCircle, faScaleUnbalanced } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleRight, faAsterisk, faFilterCircleXmark, faMedal, faQuestionCircle, faScaleUnbalanced } from '@fortawesome/free-solid-svg-icons';
 import { ProgrammingExerciseGradingStatistics } from 'app/entities/programming-exercise-test-case-statistics.model';
 import { ProgrammingExerciseTask } from './programming-exercise-task';
 import { Observable, Subject } from 'rxjs';
+
+type Sort = {
+    by: 'name' | 'weight' | 'multiplier' | 'bonusPoints' | 'visibility' | 'resulting' | 'type';
+    descending: boolean;
+};
 
 @Component({
     selector: 'jhi-programming-exercise-grading-task',
@@ -21,6 +26,7 @@ export class ProgrammingExerciseGradingTaskComponent implements OnInit {
     faAngleDown = faAngleDown;
     faAngleRight = faAngleRight;
     faQuestionCircle = faQuestionCircle;
+    faFilterCircleXmark = faFilterCircleXmark;
     faScaleUnbalanced = faScaleUnbalanced;
     faMedal = faMedal;
     faAsterisk = faAsterisk;
@@ -28,6 +34,7 @@ export class ProgrammingExerciseGradingTaskComponent implements OnInit {
     isSaving = false;
     tasks: ProgrammingExerciseTask[];
     allTasksExpandedSubject: Subject<boolean>;
+    currentSort: Sort | undefined;
 
     get ignoreInactive() {
         return this.taskService.ignoreInactive;
@@ -66,5 +73,58 @@ export class ProgrammingExerciseGradingTaskComponent implements OnInit {
 
     toggleAllTasksExpanded = (value: boolean) => {
         this.allTasksExpandedSubject.next(value);
+    };
+
+    changeSort = (by: Sort['by']) => {
+        if (this.currentSort?.by === by) {
+            this.currentSort.descending = !this.currentSort.descending;
+        } else {
+            this.currentSort = {
+                by: by,
+                descending: true,
+            };
+        }
+
+        this.sort();
+    };
+
+    resetSort = () => {
+        this.currentSort = undefined;
+        this.updateTasks();
+    };
+
+    private sort = () => {
+        const comparators = {
+            name: this.compareStringForAttribute('taskName'),
+            weight: this.compareNumForAttribute('weight'),
+            multiplier: this.compareNumForAttribute('bonusMultiplier'),
+            bonusPoints: this.compareNumForAttribute('bonusPoints'),
+            visibility: this.compareStringForAttribute('visibility'),
+            resulting: this.compareNumForAttribute('resultingPoints'),
+            type: this.compareStringForAttribute('type'),
+        };
+        const comparator = comparators[this.currentSort!['by']];
+        this.tasks = this.tasks.slice().sort(comparator);
+
+        if (!this.currentSort?.descending) {
+            this.tasks = this.tasks.reverse();
+        }
+    };
+
+    private compareNumForAttribute = (attributeKey: keyof ProgrammingExerciseTask) => {
+        return (a: ProgrammingExerciseTask, b: ProgrammingExerciseTask) => {
+            return ((a[attributeKey] as number) ?? 0) - ((b[attributeKey] as number) ?? 0);
+        };
+    };
+
+    private compareStringForAttribute = (attributeKey: keyof ProgrammingExerciseTask) => {
+        return (a: ProgrammingExerciseTask, b: ProgrammingExerciseTask) => {
+            const aType = a[attributeKey] ?? '';
+            const bType = b[attributeKey] ?? '';
+
+            if (aType < bType) return -1;
+            if (aType > bType) return 1;
+            return 0;
+        };
     };
 }
