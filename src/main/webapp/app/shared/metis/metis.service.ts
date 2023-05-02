0336c8739f063a2eecd5e36e89edaab65bb9e7e4
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-05-02 13:51:10 +0200

    Fix incorrect websocket channel subscription check for exercise context

--- src/main/webapp/app/shared/metis/metis.service.ts
+++ src/main/webapp/app/shared/metis/metis.service.ts
@@ -439,7 +439,7 @@ export class MetisService implements OnDestroy {
             if (
                 (postDTO.post.courseWideContext && this.currentPostContextFilter.courseWideContexts?.includes(postDTO.post.courseWideContext)) ||
                 (postDTO.post.lecture?.id !== undefined && this.currentPostContextFilter.lectureIds?.includes(postDTO.post.lecture.id)) ||
-                (postDTO.post.exercise?.id !== undefined && this.currentPostContextFilter.lectureIds?.includes(postDTO.post.exercise.id)) ||
+                (postDTO.post.exercise?.id !== undefined && this.currentPostContextFilter.exerciseIds?.includes(postDTO.post.exercise.id)) ||
                 (postDTO.post.conversation?.id !== undefined && postDTO.post.conversation.id === this.currentPostContextFilter.conversationId)
             )
                 switch (postDTO.action) {
