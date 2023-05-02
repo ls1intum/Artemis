8c915e0ee7e3be78d462d58fa836557208aac9ef
Stephan Krusche <krusche@in.tum.de>
2023-05-02 22:05:33 +0200

    try to improve initialization performance

---
+++ src/main/java/de/tum/in/www1/artemis/config/TomcatConfiguration.java
@@ -0,0 +1,24 @@
+package de.tum.in.www1.artemis.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.webresources.ExtractingRoot;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        return (TomcatServletWebServerFactory container) -> {
+            container.addContextCustomizers((Context context) -> {
+                // This configuration is used to improve initialization performance.
+
+                context.setResources(new ExtractingRoot());
+                context.setReloadable(false);
+            });
+        };
+    }
+}
