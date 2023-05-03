d59f61e4f8bceba98d6486a10e89236e8a2d29ca
Felix T.J. Dietrich <felixtj.dietrich@tum.de>
2023-05-03 11:13:06 +0200

    Remove unused import and update artefact warning text

--- src/main/webapp/app/exercises/shared/exercise/exercise.service.ts
+++ src/main/webapp/app/exercises/shared/exercise/exercise.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import dayjs from 'dayjs/esm';
 import { Exercise, ExerciseType, IncludedInOverallScore } from 'app/entities/exercise.model';
