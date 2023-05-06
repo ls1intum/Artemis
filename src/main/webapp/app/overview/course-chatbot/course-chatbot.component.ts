5b908f22a17034744d4f5e0d61e14a5b1ac5e8b6
Aybike Ece Eren <aybikeceren@gmail.com>
2023-05-06 20:46:34 +0200

    Chatbot module is created & chatbot button is added

---
+++ src/main/webapp/app/overview/course-chatbot/course-chatbot.component.ts
@@ -0,0 +1,56 @@
+import { Component, HostListener, Input, OnChanges } from '@angular/core';
+import { Course } from 'app/entities/course.model';
+import { ARTEMIS_DEFAULT_COLOR } from 'app/app.constants';
+import { CachingStrategy } from 'app/shared/image/secured-image.component';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'jhi-course-chatbot',
+    templateUrl: './course-chatbot.component.html',
+    styleUrls: ['./course-chatbot.component.scss'],
+})
+export class CourseChatbotComponent implements OnChanges {
+    readonly ARTEMIS_DEFAULT_COLOR = ARTEMIS_DEFAULT_COLOR;
+    readonly CachingStrategy = CachingStrategy;
+
+    @Input() public course: Course;
+
+    public courseDescription?: string;
+    public enableShowMore = false;
+    public longDescriptionShown = false;
+
+    // Icons
+    faCommentDots = faCommentDots;
+
+    ngOnChanges() {
+        this.adjustCourseDescription();
+    }
+
+    @HostListener('window:resize')
+    onResize() {
+        this.adjustCourseDescription();
+    }
+
+    /**
+     * Toggle between showing the long and abbreviated course description
+     */
+    toggleCourseDescription() {
+        this.longDescriptionShown = !this.longDescriptionShown;
+        this.adjustCourseDescription();
+    }
+
+    /**
+     * Adjusts the course description and shows toggle buttons (if it is too long) based on the current window width
+     */
+    adjustCourseDescription() {
+        const shortDescriptionLength = window.innerWidth / 5;
+        if (this.course && this.course.description) {
+            this.enableShowMore = this.course.description.length > shortDescriptionLength;
+            if (this.enableShowMore && !this.longDescriptionShown) {
+                this.courseDescription = this.course.description.slice(0, shortDescriptionLength) + '…';
+            } else {
+                this.courseDescription = this.course.description;
+            }
+        }
+    }
+}
