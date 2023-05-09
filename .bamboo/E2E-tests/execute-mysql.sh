919f260135c4a0031b818f7bc39bd2ab263a899d
Florian Gareis <dev@zoker.me>
2023-05-09 11:33:58 +0200

    Add cypress with postgres build plan

--- .bamboo/E2E-tests/execute-mysql.sh
+++ .bamboo/E2E-tests/execute-mysql.sh
@@ -9,9 +9,9 @@ export HOST_HOSTNAME=$(hostname)
 
 cd docker
 #just pull everything else than artemis-app as we build it later either way
-docker compose -f cypress-E2E-tests.yml pull artemis-cypress mysql nginx
-docker compose -f cypress-E2E-tests.yml build --build-arg WAR_FILE_STAGE=external_builder --no-cache --pull artemis-app
-docker compose -f cypress-E2E-tests.yml up --exit-code-from artemis-cypress
+docker compose -f cypress-E2E-tests-mysql.yml pull artemis-cypress mysql nginx
+docker compose -f cypress-E2E-tests-mysql.yml build --build-arg WAR_FILE_STAGE=external_builder --no-cache --pull artemis-app
+docker compose -f cypress-E2E-tests-mysql.yml up --exit-code-from artemis-cypress
 exitCode=$?
 cd ..
 echo "Cypress container exit code: $exitCode"
