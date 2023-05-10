e04dc96beb95a95ae971699728acce6a6be16e61
Dmytro Polityka <dmytro.polityka@tum.de>
2023-05-10 23:14:41 +0200

    PCIL23UX-17: Reintroduce subscribable actions

--- src/main/webapp/app/iris/message-store.service.ts
+++ src/main/webapp/app/iris/message-store.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { ActionType, MessageStoreAction, MessageStoreState } from 'app/iris/message-store.model';
 
+/**
+ * Provides a store to manage message-related state data and dispatch actions.
+ */
 @Injectable({
     providedIn: 'root',
 })
@@ -10,18 +14,51 @@ export class IrisMessageStore implements OnDestroy {
         messages: [],
     };
 
+    private readonly action = new Subject<MessageStoreAction>();
     private readonly state = new BehaviorSubject<MessageStoreState>(this.initialState);
+    private readonly subscription: Subscription;
+
+    constructor() {
+        this.subscription = this.action
+            .pipe(
+                tap((action: MessageStoreAction) => {
+                    const currentState = this.state.getValue();
+                    const newState = IrisMessageStore.storeReducer(currentState, action);
+                    this.state.next(newState);
+                    console.log('Action received:', action);
+                }),
+            )
+            .subscribe();
+    }
 
     ngOnDestroy() {
+        this.subscription.unsubscribe();
         this.state.complete();
+        this.action.complete();
     }
 
-    dispatch(action: MessageStoreAction) {
-        this.state.next(IrisMessageStore.storeReducer(this.state.getValue(), action));
+    /**
+     * Dispatches an action to update the message store state.
+     * @param action The action to dispatch.
+     */
+    dispatch(action: MessageStoreAction): void {
+        this.action.next(action);
     }
 
+    /**
+     * Returns an observable of the current message store state.
+     * @returns An observable of the current message store state.
+     */
     getState(): Observable<MessageStoreState> {
         return this.state.asObservable();
+    }
+
+    /**
+     * Returns an observable of the actions dispatched to the message store.
+     * @returns An observable of the actions dispatched to the message store.
+     */
+    getActionObservable(): Observable<MessageStoreAction> {
+        return this.action.asObservable();
     }
 
     private static exhaustiveCheck(action: never): void {
