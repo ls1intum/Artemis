056db53b281ace5ace0639ce7ee7d3b81a57d269
Asli Aykan <asliaykan97@gmail.com>
2023-05-15 23:11:41 +0200

    i18n added

--- src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.module.ts
+++ src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { ExerciseChatWidgetComponent } from 'app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
 
 @NgModule({
     declarations: [ChatbotPopupComponent, ExerciseChatWidgetComponent],
-    imports: [MatDialogModule, FormsModule, CommonModule, FontAwesomeModule],
+    imports: [MatDialogModule, FormsModule, CommonModule, FontAwesomeModule, ArtemisSharedModule],
     providers: [],
 })
 export class ExerciseChatbotModule {}
