df5da299ca5ddf2291597d5bc7b1999c2af3fce7
Asli Aykan <asliaykan97@gmail.com>
2023-05-16 14:28:22 +0200

    auto-close widget bug fixed

--- src/main/webapp/app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component.ts
+++ src/main/webapp/app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { faCompress } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -10,6 +10,7 @@ import { MatDialog } from '@angular/material/dialog';
     styleUrls: ['./exercise-chat-widget.component.scss'],
 })
 export class ExerciseChatWidgetComponent {
+    @ViewChild('chatWidget') chatWidget!: ElementRef;
     @ViewChild('chatBody') chatBody!: ElementRef;
     messages: string[] = [];
     irisMessages: string[] = ['Hey! How can I help you?'];
@@ -37,6 +38,11 @@ export class ExerciseChatWidgetComponent {
             const chatBodyElement: HTMLElement = this.chatBody.nativeElement;
             chatBodyElement.scrollTop = chatBodyElement.scrollHeight;
         });
+    }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent) {
+        this.chatWidgetClosed.emit();
     }
 
     closeChat() {
