e1bdef2636aad10bae02bc2c67e1405930e158a9
Asli Aykan <asliaykan97@gmail.com>
2023-05-17 15:52:13 +0200

    minor updates

--- src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
+++ src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ChatbotPopupComponent } from './chatbot-popup/chatbot-popup.component';
 import { ExerciseChatWidgetComponent } from 'app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
 
@@ -33,12 +33,15 @@ export class ExerciseChatbotComponent {
 
         dialogRef.afterClosed().subscribe((result) => {
             this.chatAccepted = result;
+            if (this.chatAccepted === 'true') {
+                this.openChat();
+            }
         });
     }
 
     openChat() {
         if (!this.buttonDisabled) {
-            const dialogRef = this.dialog.open(ExerciseChatWidgetComponent, { position: { bottom: '0px', right: '0px' } });
+            const dialogRef = this.dialog.open(ExerciseChatWidgetComponent, { hasBackdrop: false, position: { bottom: '0px', right: '0px' } });
             dialogRef.componentInstance.chatWidgetClosed.subscribe(() => {
                 this.buttonDisabled = false;
             });
