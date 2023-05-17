ed19416875c8edc403f64bb1818568ac9cefc072
Tobias Lippert <t.lippert@tum.de>
2023-05-17 18:08:30 +0200

    add javadoc

--- src/main/java/de/tum/in/www1/artemis/domain/enumeration/Language.java
+++ src/main/java/de/tum/in/www1/artemis/domain/enumeration/Language.java
@@ -28,6 +28,12 @@ public enum Language {
         throw new IllegalArgumentException("Language not supported");
     }
 
+    /**
+     * Checks if the given language short name is valid.
+     *
+     * @param languageShortName the short name of the language
+     * @return true if the short name is valid, false otherwise
+     */
     public static boolean isValidShortName(String languageShortName) {
         for (Language language : Language.values()) {
             if (language.getShortName().equals(languageShortName)) {
