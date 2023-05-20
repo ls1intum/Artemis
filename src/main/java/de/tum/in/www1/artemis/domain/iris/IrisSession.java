73db71c8876452d057e9813c736be213b5b9f6e3
Timor Morrien <timor.morrien@tum.de>
2023-05-20 22:45:25 +0200

    Fix toString tests

--- src/main/java/de/tum/in/www1/artemis/domain/iris/IrisSession.java
+++ src/main/java/de/tum/in/www1/artemis/domain/iris/IrisSession.java
@@ -60,6 +60,7 @@ public class IrisSession extends DomainObject {
 
     @Override
     public String toString() {
-        return "IrisSession{" + "exercise=" + (exercise == null ? "null" : exercise.getId()) + ", user=" + user.getName() + ", messages=" + messages + '}';
+        return "IrisSession{" + "exercise=" + (exercise == null ? "null" : exercise.getId()) + ", user=" + (user == null ? "null" : user.getName()) + ", messages=" + messages
+                + '}';
     }
 }
