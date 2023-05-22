08d4e879d2ad42d2107f9aaef94bf394829360f9
Pellumb Baboci <pellumbbaboci@gmail.com>
2023-05-22 15:32:20 +0200

    request body for the endpoint

--- src/main/java/de/tum/in/www1/artemis/web/rest/ogparser/LinkPreviewResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ogparser/LinkPreviewResource.java
@@ -4,11 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.tum.in.www1.artemis.service.linkpreview.LinkPreviewService;
 
+/**
+ * REST controller for Link Preview.
+ */
 @RestController
 @RequestMapping("/api")
 public class LinkPreviewResource {
@@ -23,7 +27,7 @@ public class LinkPreviewResource {
 
     @GetMapping("/link-preview")
     @PreAuthorize("hasRole('User')")
-    public String getLinkPreview(String url) {
+    public String getLinkPreview(@RequestBody String url) {
         log.debug("REST request to get link preview for url: {}", url);
         return linkPreviewService.getLinkPreview(url);
     }
