267698c90ab778914f188df4615dfc61b377fcd0
Simon Leiß <git@simon-leiss.de>
2023-05-22 13:33:37 +0200

    Fix test.

--- src/test/javascript/spec/service/lecture.service.spec.ts
+++ src/test/javascript/spec/service/lecture.service.spec.ts
@@ -149,7 +149,7 @@ describe('Lecture Service', () => {
                 .pipe(take(1))
                 .subscribe((resp) => (expectedResult = resp));
             const req = httpMock.expectOne({
-                url: `api/lectures/course/${courseId}?withLectureUnits=0`,
+                url: `api/courses/${courseId}/lectures?withLectureUnits=0`,
                 method: 'GET',
             });
             req.flush(returnedFromService);
