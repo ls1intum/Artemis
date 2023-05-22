9f3a3978d9eec3ff461ccbad510bf95feb79880c
Rizky Riyaldhi <rriyaldhi@gmail.com>
2023-05-22 14:20:43 +0200

    Fix conflict

--- src/main/java/de/tum/in/www1/artemis/domain/quiz/QuizSubmission.java
+++ src/main/java/de/tum/in/www1/artemis/domain/quiz/QuizSubmission.java
@@ -4,11 +4,6 @@ import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.fasterxml.jackson.annotation.JsonView;
-
-import de.tum.in.www1.artemis.domain.Submission;
-import de.tum.in.www1.artemis.domain.exam.Exam;
-import de.tum.in.www1.artemis.domain.view.QuizView;
 
 /**
  * A QuizSubmission.
@@ -23,10 +18,6 @@ public class QuizSubmission extends AbstractQuizSubmission {
     public String getSubmissionExerciseType() {
         return "quiz";
     }
-
-    @Column(name = "score_in_points")
-    @JsonView(QuizView.After.class)
-    private Double scoreInPoints;
 
     // The use of id here is on purpose because @ManyToOne relation cannot be lazily fetched and typically, QuizBatch is not needed when loading QuizSubmission
     @Column(name = "quiz_batch")
