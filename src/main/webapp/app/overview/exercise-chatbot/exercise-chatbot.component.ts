f3df6ed10d8eabcb04a334909163d500363f8b76
Ece Eren <aybikeceren@gmail.com>
2023-05-23 00:52:54 +0200

    Chatbot scroll strategy fix

--- src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
+++ src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
@@ -3,6 +3,7 @@ import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ChatbotPopupComponent } from './chatbot-popup/chatbot-popup.component';
 import { ExerciseChatWidgetComponent } from 'app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
+import { Overlay } from '@angular/cdk/overlay';
 
 @Component({
     selector: 'jhi-exercise-chatbot',
@@ -17,7 +18,7 @@ export class ExerciseChatbotComponent implements OnDestroy {
     // Icons
     faCommentDots = faCommentDots;
 
-    constructor(private dialog: MatDialog) {}
+    constructor(private dialog: MatDialog, private overlay: Overlay) {}
 
     ngOnDestroy() {
         if (this.dialogRef) {
@@ -52,6 +53,7 @@ export class ExerciseChatbotComponent implements OnDestroy {
             this.chatOpen = true;
             this.dialogRef = this.dialog.open(ExerciseChatWidgetComponent, {
                 hasBackdrop: false,
+                scrollStrategy: this.overlay.scrollStrategies.noop(),
                 position: { bottom: '0px', right: '0px' },
             });
             this.dialogRef.afterClosed().subscribe(() => {
