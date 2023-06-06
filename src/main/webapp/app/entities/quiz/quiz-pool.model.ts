afba56abe16061a493548167118311791b8629d9
Rizky Riyaldhi <rriyaldhi@gmail.com>
2023-06-06 16:10:39 +0200

    Make QuizPool and QuizGroup implements BaseEntity

--- src/main/webapp/app/entities/quiz/quiz-pool.model.ts
+++ src/main/webapp/app/entities/quiz/quiz-pool.model.ts
@@ -1,8 +1,9 @@
 import { QuizGroup } from 'app/entities/quiz/quiz-group.model';
 import { QuizQuestion } from 'app/entities/quiz/quiz-question.model';
 import { Exam } from 'app/entities/exam.model';
+import { BaseEntity } from 'app/shared/model/base-entity';
 
-export class QuizPool {
+export class QuizPool implements BaseEntity {
     public id?: number;
     public exam: Exam;
     public quizGroups: QuizGroup[] = [];
