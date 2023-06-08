6966b56eab67c4396591cb274404fc41ef738fec
Rizky Riyaldhi <rriyaldhi@gmail.com>
2023-06-08 10:44:03 +0200

    Fix Function.identity in QuizPoolService

--- src/main/java/de/tum/in/www1/artemis/service/QuizPoolService.java
+++ src/main/java/de/tum/in/www1/artemis/service/QuizPoolService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
