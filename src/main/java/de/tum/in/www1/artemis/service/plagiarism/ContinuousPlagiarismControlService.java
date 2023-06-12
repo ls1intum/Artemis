6fad287e5cd079b0a682f77372e590f3f69f7517
jakubriegel <jakubriegel112@gmail.com>
2023-06-12 12:31:43 +0200

    trigger checks every 5min

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
@@ -67,7 +67,7 @@ public class ContinuousPlagiarismControlService {
     /**
      * Daily triggers plagiarism checks as a part of continuous plagiarism control.
      */
-    @Scheduled(cron = "0 0 4 * * *") // execute this every night at 4:00:00 am
+    @Scheduled(fixedDelay = 300_000) // execute this every night at 4:00:00 am
     public void executeChecks() {
         log.info("Starting continuous plagiarism control...");
 
