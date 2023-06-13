c1da068c00e3c5496be64a7a1c83b97b2df6492e
Tobias Kotzian <solizer@web.de>
2023-06-13 19:26:43 +0200

    Added javadoc comments

--- src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisConnectorService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisConnectorService.java
@@ -20,6 +20,10 @@ import de.tum.in.www1.artemis.service.connectors.iris.dto.IrisMessageResponseDTO
 import de.tum.in.www1.artemis.service.connectors.iris.dto.IrisRequestDTO;
 import de.tum.in.www1.artemis.service.connectors.iris.dto.IrisTemplateDTO;
 
+/**
+ * This service connects to the Python implementation of Iris (called Pyris) responsible for connecting to different
+ * LLMs and handle messages with Microsoft Guidance
+ */
 @Service
 public class IrisConnectorService {
 
@@ -35,16 +39,39 @@ public class IrisConnectorService {
         this.objectMapper = springMvcJacksonConverter.getObjectMapper();
     }
 
+    /**
+     * Updates an existing template (used e.g., as initial system message)
+     *
+     * @param templateId The id of the existing template
+     * @param template   The string representation of the new template
+     * @return The id of the updated template
+     */
     @Async
     public CompletableFuture<Long> updateTemplate(long templateId, String template) {
         return saveTemplate(new IrisTemplateDTO(templateId, template)).thenApplyAsync(IrisTemplateDTO::templateId);
     }
 
+    /**
+     * Saves a new template for use by Pyris
+     *
+     * @param template The template to be saved for usage later
+     * @return The id of the newly saved template
+     */
     @Async
     public CompletableFuture<Long> saveNewTemplate(String template) {
         return saveTemplate(new IrisTemplateDTO(null, template)).thenApplyAsync(IrisTemplateDTO::templateId);
     }
 
+    /**
+     * Requests a response from an LLM
+     *
+     * @param templateId     The id of the request template to be used
+     * @param preferredModel The LLM model to be used (e.g., GPT3.5-turbo). Note: The used model might not be the preferred model (e.g., if an error occurs or the preferredModel is
+     *                           not reachable)
+     * @param parameters     A map of parameters to be included in the template through handlebars (if they are specified
+     *                           in the template)
+     * @return The message response to the request which includes the {@link de.tum.in.www1.artemis.domain.iris.IrisMessage} and the used {@link IrisModel}
+     */
     @Async
     public CompletableFuture<IrisMessageResponseDTO> sendRequest(long templateId, IrisModel preferredModel, Map<String, Object> parameters) {
         var request = new IrisRequestDTO(templateId, preferredModel, parameters);
