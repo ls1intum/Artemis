6b1f72605041221cfecd64c14ad7a03b0c23a0ef
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-13 18:31:43 +0200

    Iris: Client - MVP (#6640)

--- src/main/webapp/app/iris/exercise-chatbot/exercise-chatbot.component.ts
+++ src/main/webapp/app/iris/exercise-chatbot/exercise-chatbot.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ChatbotPopupComponent } from './chatbot-popup/chatbot-popup.component';
-import { ExerciseChatWidgetComponent } from 'app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
-import { Overlay } from '@angular/cdk/overlay';
+import { ExerciseChatWidgetComponent } from 'app/iris/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
+import { IrisWebsocketService } from 'app/iris/websocket.service';
+import { IrisStateStore } from 'app/iris/state-store.service';
+import { IrisSessionService } from 'app/iris/session.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'jhi-exercise-chatbot',
     templateUrl: './exercise-chatbot.component.html',
     styleUrls: ['./exercise-chatbot.component.scss'],
+    providers: [IrisStateStore, IrisWebsocketService, IrisSessionService],
 })
-export class ExerciseChatbotComponent implements OnDestroy {
+export class ExerciseChatbotComponent implements OnDestroy, OnInit {
     public chatAccepted = false;
     public buttonDisabled = false;
-    dialogRef: MatDialogRef<ExerciseChatWidgetComponent> | null = null;
-    chatOpen = false;
+    private dialogRef: MatDialogRef<ExerciseChatWidgetComponent> | null = null;
+    private chatOpen = false;
+    private exerciseId: number;
     // Icons
     faCommentDots = faCommentDots;
 
-    constructor(public dialog: MatDialog, private overlay: Overlay) {}
+    constructor(
+        private dialog: MatDialog,
+        private readonly sessionService: IrisSessionService,
+        private readonly stateStore: IrisStateStore,
+        private readonly websocketService: IrisWebsocketService,
+        private route: ActivatedRoute,
+    ) {}
+
+    ngOnInit() {
+        this.route.params.subscribe((params) => {
+            this.exerciseId = parseInt(params['exerciseId'], 10);
+            if (this.exerciseId != null) {
+                this.sessionService.getCurrentSessionOrCreate(this.exerciseId);
+            }
+        });
+    }
 
     ngOnDestroy() {
         if (this.dialogRef) {
@@ -41,7 +61,7 @@ export class ExerciseChatbotComponent implements OnDestroy {
         const dialogRef = this.dialog.open(ChatbotPopupComponent, {});
 
         dialogRef.afterClosed().subscribe((result) => {
-            this.chatAccepted = result == 'true';
+            this.chatAccepted = result;
             if (this.chatAccepted) {
                 this.openChat();
             }
@@ -53,8 +73,10 @@ export class ExerciseChatbotComponent implements OnDestroy {
             this.chatOpen = true;
             this.dialogRef = this.dialog.open(ExerciseChatWidgetComponent, {
                 hasBackdrop: false,
-                scrollStrategy: this.overlay.scrollStrategies.noop(),
                 position: { bottom: '0px', right: '0px' },
+                data: {
+                    stateStore: this.stateStore,
+                },
             });
             this.dialogRef.afterClosed().subscribe(() => {
                 this.buttonDisabled = false;
---
+++ src/main/webapp/app/iris/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component.ts
@@ -0,0 +1,100 @@
+import { Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { faCircle, faExpand, faPaperPlane, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { IrisStateStore } from 'app/iris/state-store.service';
+import { ConversationErrorOccurredAction, StudentMessageSentAction } from 'app/iris/state-store.model';
+import { IrisHttpMessageService } from 'app/iris/http-message.service';
+import { IrisClientMessage, IrisMessage, IrisSender } from 'app/entities/iris/iris-message.model';
+import { IrisMessageContent, IrisMessageContentType } from 'app/entities/iris/iris-content-type.model';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'jhi-exercise-chat-widget',
+    templateUrl: './exercise-chat-widget.component.html',
+    styleUrls: ['./exercise-chat-widget.component.scss'],
+})
+export class ExerciseChatWidgetComponent implements OnInit, OnDestroy {
+    @ViewChild('chatWidget') chatWidget!: ElementRef;
+    @ViewChild('chatBody') chatBody!: ElementRef;
+
+    readonly SENDER_USER = IrisSender.USER;
+    readonly SENDER_SERVER = IrisSender.LLM;
+    readonly stateStore: IrisStateStore;
+
+    stateSubscription: Subscription;
+    messages: IrisMessage[] = [];
+    newMessageTextContent = '';
+    isLoading: boolean;
+    sessionId: number;
+    error = '';
+    dots = 1;
+
+    constructor(private dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any, private httpMessageService: IrisHttpMessageService) {
+        this.stateStore = data.stateStore;
+    }
+
+    // Icons
+    faPaperPlane = faPaperPlane;
+    faCircle = faCircle;
+    faExpand = faExpand;
+    faXmark = faXmark;
+
+    ngOnInit() {
+        this.scrollToBottom('auto');
+        this.animateDots();
+        this.stateSubscription = this.stateStore.getState().subscribe((state) => {
+            this.messages = state.messages as IrisMessage[];
+            this.isLoading = state.isLoading;
+            this.error = state.error;
+            this.sessionId = Number(state.sessionId);
+        });
+    }
+
+    ngOnDestroy() {
+        this.stateSubscription.unsubscribe();
+    }
+
+    animateDots() {
+        setInterval(() => {
+            this.dots = this.dots < 3 ? (this.dots += 1) : (this.dots = 1);
+        }, 500);
+    }
+
+    onSend(): void {
+        if (this.newMessageTextContent) {
+            const message = this.newUserMessage(this.newMessageTextContent);
+            this.stateStore
+                .dispatchAndThen(new StudentMessageSentAction(message))
+                .then(() => this.httpMessageService.createMessage(<number>this.sessionId, message).toPromise())
+                .then(() => this.scrollToBottom('smooth'))
+                .catch(() => this.stateStore.dispatch(new ConversationErrorOccurredAction('Something went wrong. Please try again later!')));
+            this.newMessageTextContent = '';
+        }
+        this.scrollToBottom('smooth');
+    }
+
+    scrollToBottom(behavior: ScrollBehavior) {
+        setTimeout(() => {
+            const chatBodyElement: HTMLElement = this.chatBody.nativeElement;
+            chatBodyElement.scrollTo({
+                top: chatBodyElement.scrollHeight,
+                behavior: behavior,
+            });
+        });
+    }
+
+    closeChat() {
+        this.dialog.closeAll();
+    }
+
+    private newUserMessage(message: string): IrisClientMessage {
+        const content: IrisMessageContent = {
+            type: IrisMessageContentType.TEXT,
+            textContent: message,
+        };
+        return {
+            sender: this.SENDER_USER,
+            content: [content],
+        };
+    }
+}
