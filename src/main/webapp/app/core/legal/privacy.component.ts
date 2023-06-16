242ae9f027abc3ee6262482d06b0a0ceb46de453
Tobias Lippert <84102468+tobias-lippert@users.noreply.github.com>
2023-06-16 20:12:52 +0200

    General: Add data export for exercise participations and communication data (#6500)

--- src/main/webapp/app/core/legal/privacy.component.ts
+++ src/main/webapp/app/core/legal/privacy.component.ts
@@ -3,22 +3,33 @@ import { ActivatedRoute } from '@angular/router';
 import { LegalDocumentService } from 'app/shared/service/legal-document.service';
 import { Subscription } from 'rxjs';
 import { JhiLanguageHelper } from 'app/core/language/language.helper';
+import { AccountService } from 'app/core/auth/account.service';
 import { LegalDocumentLanguage } from 'app/entities/legal-document.model';
 
 @Component({
     selector: 'jhi-privacy',
-    template: ` <div [innerHTML]="privacyStatement | htmlForMarkdown"></div> `,
+    template: `
+        <div [innerHTML]="privacyStatement | htmlForMarkdown"></div>
+        <a *ngIf="isAdmin" jhiTranslate="artemisApp.dataExport.title" [routerLink]="['/privacy/data-export']"> </a>
+    `,
 })
 export class PrivacyComponent implements AfterViewInit, OnInit, OnDestroy {
     privacyStatement: string;
     private languageChangeSubscription?: Subscription;
+    isAdmin: boolean;
 
-    constructor(private route: ActivatedRoute, private privacyStatementService: LegalDocumentService, private languageHelper: JhiLanguageHelper) {}
+    constructor(
+        private route: ActivatedRoute,
+        private privacyStatementService: LegalDocumentService,
+        private languageHelper: JhiLanguageHelper,
+        private accountService: AccountService,
+    ) {}
 
     /**
      * On init get the privacy statement file from the Artemis server and set up a subscription to fetch the file again if the language was changed.
      */
     ngOnInit(): void {
+        this.isAdmin = this.accountService.isAdmin();
         // Update the view if the language was changed
         this.languageChangeSubscription = this.languageHelper.language.subscribe((lang) => {
             this.privacyStatementService.getPrivacyStatement(lang as LegalDocumentLanguage).subscribe((statement) => {
