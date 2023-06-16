f8de8b8a67880f8c758ab6ed710c227653382c36
Tobias Lippert <t.lippert@tum.de>
2023-06-16 15:22:03 +0200

    move authorization checks to resource

--- src/main/java/de/tum/in/www1/artemis/web/rest/DataExportResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/DataExportResource.java
@@ -3,6 +3,8 @@ package de.tum.in.www1.artemis.web.rest;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import javax.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
@@ -13,8 +15,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import de.tum.in.www1.artemis.domain.DataExport;
-import de.tum.in.www1.artemis.repository.UserRepository;
+import de.tum.in.www1.artemis.repository.DataExportRepository;
+import de.tum.in.www1.artemis.security.SecurityUtils;
 import de.tum.in.www1.artemis.service.DataExportService;
+import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException;
 import de.tum.in.www1.artemis.web.rest.errors.InternalServerErrorException;
 
 @RestController
@@ -23,10 +27,13 @@ public class DataExportResource {
 
     private final DataExportService dataExportService;
 
+    private final DataExportRepository dataExportRepository;
+
     private final Logger log = LoggerFactory.getLogger(DataExportResource.class);
 
-    public DataExportResource(DataExportService dataExportService, UserRepository userRepository) {
+    public DataExportResource(DataExportService dataExportService, DataExportRepository dataExportRepository) {
         this.dataExportService = dataExportService;
+        this.dataExportRepository = dataExportRepository;
     }
 
     /**
@@ -55,11 +62,16 @@ public class DataExportResource {
      *
      * @param dataExportId the id of the data export to download
      * @return A resource containing the data export zip file
+     * @throws de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException  if the data export could not be found
+     * @throws de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException if the user is not allowed to download the data export
      */
     @GetMapping("data-export/{dataExportId}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Resource> downloadDataExport(@PathVariable long dataExportId) {
-        var dataExportPath = dataExportService.downloadDataExport(dataExportId);
+        var dataExport = dataExportRepository.findByIdElseThrow(dataExportId);
+        currentlyLoggedInUserIsOwnerOfDataExportElseThrow(dataExport);
+        checkDataExportCanBeDownloaded(dataExport);
+        var dataExportPath = dataExportService.downloadDataExport(dataExport);
         var finalZipFile = dataExportPath.toFile();
         InputStreamResource resource;
         try {
@@ -70,7 +82,39 @@ public class DataExportResource {
             throw new InternalServerErrorException("Could not find data export file");
         }
         return ResponseEntity.ok().contentLength(finalZipFile.length()).contentType(MediaType.APPLICATION_OCTET_STREAM).header("filename", finalZipFile.getName()).body(resource);
+    }
 
+    private void checkDataExportCanBeDownloaded(DataExport dataExport) {
+        if (!dataExport.getDataExportState().isDownloadable()) {
+            throw new AccessForbiddenException("Data export has either not been created or already been deleted");
+        }
+    }
+
+    /**
+     * checks if the currently logged-in user is the owner of the given data export
+     *
+     * @param dataExport the data export that needs to be checked
+     * @throws AccessForbiddenException if logged-in user isn't the owner of the data export
+     */
+    public void currentlyLoggedInUserIsOwnerOfDataExportElseThrow(@NotNull DataExport dataExport) {
+        if (!currentlyLoggedInUserIsOwnerOfDataExport(dataExport)) {
+            throw new AccessForbiddenException("data export", dataExport.getId());
+        }
+    }
+
+    /**
+     * checks if the currently logged-in user is owner of the given data export
+     *
+     * @param dataExport the data export that needs to be checked
+     * @return true if the user is the owner of the data export, false otherwise
+     */
+    private boolean currentlyLoggedInUserIsOwnerOfDataExport(DataExport dataExport) {
+        if (dataExport.getUser() == null) {
+            return false;
+        }
+        else {
+            return dataExport.getUser().getLogin().equals(SecurityUtils.getCurrentUserLogin().get());
+        }
     }
 
 }
