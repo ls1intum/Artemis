757f0d2e7cdafe5e9c892ce325e5c0e5d6404707
Asli Aykan <asliaykan97@gmail.com>
2023-06-16 13:43:10 +0200

    animation added

--- src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
+++ src/main/webapp/app/overview/exercise-chatbot/exercise-chatbot.component.ts
@@ -3,17 +3,21 @@ import { faChevronDown, faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ExerciseChatWidgetComponent } from 'app/overview/exercise-chatbot/exercise-chatwidget/exercise-chat-widget.component';
 import { Overlay } from '@angular/cdk/overlay';
+import { shakeAnimation } from 'angular-animations';
 
 @Component({
     selector: 'jhi-exercise-chatbot',
     templateUrl: './exercise-chatbot.component.html',
     styleUrls: ['./exercise-chatbot.component.scss'],
+    animations: [shakeAnimation({ anchor: 'shake', direction: '=>', duration: 700 })],
 })
 export class ExerciseChatbotComponent implements OnDestroy {
     public chatAccepted = false;
     public buttonDisabled = false;
     dialogRef: MatDialogRef<ExerciseChatWidgetComponent> | null = null;
     chatOpen = false;
+    runAnimation = false;
+
     // Icons
     faCommentDots = faCommentDots;
     faChevronDown = faChevronDown;
@@ -29,9 +33,11 @@ export class ExerciseChatbotComponent implements OnDestroy {
     handleButtonClick() {
         if (this.chatOpen && this.dialogRef) {
             this.dialogRef!.close();
+            //this.runAnimation = false;
             this.chatOpen = false;
         } else {
             this.openChat();
+            //this.runAnimation = true;
         }
     }
 
