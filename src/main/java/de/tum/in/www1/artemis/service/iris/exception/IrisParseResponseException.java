ff2def91dbff7e602a0ae5ffb45cbf2ba0cb5039
Dmytro Polityka <33299157+undernagruzez@users.noreply.github.com>
2023-06-20 15:09:18 +0200

    Delete IrisParseResponseException.java

--- src/main/java/de/tum/in/www1/artemis/service/iris/exception/IrisParseResponseException.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.artemis.service.iris.exception;
-
-public class IrisParseResponseException extends IrisException {
-
-    public IrisParseResponseException(Throwable cause) {
-        super("Unable to parse response of model", cause, "artemisApp.iris.error.parseResponse");
-    }
-}
