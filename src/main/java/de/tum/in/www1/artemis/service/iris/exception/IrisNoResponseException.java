f58f6166b4e63a1691b42c82531580e48080d43a
Dmytro Polityka <33299157+undernagruzez@users.noreply.github.com>
2023-06-20 15:09:08 +0200

    Delete IrisNoResponseException.java

--- src/main/java/de/tum/in/www1/artemis/service/iris/exception/IrisNoResponseException.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.artemis.service.iris.exception;
-
-public class IrisNoResponseException extends IrisException {
-
-    public IrisNoResponseException() {
-        super("No response from Iris model", "artemisApp.iris.error.noResponse");
-    }
-}
