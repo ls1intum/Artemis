e42c34c32699907cd0502d4f55f38e3843622075
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-06-21 16:26:10 +0200

    Filter channels in modal

--- src/main/webapp/app/overview/course-conversations/dialogs/channels-overview-dialog/channels-overview-dialog.component.ts
+++ src/main/webapp/app/overview/course-conversations/dialogs/channels-overview-dialog/channels-overview-dialog.component.ts
@@ -139,7 +139,7 @@ export class ChannelsOverviewDialogComponent extends AbstractDialogComponent imp
             )
             .subscribe({
                 next: (channels: ChannelDTO[]) => {
-                    this.channels = channels ?? [];
+                    this.channels = channels ? channels.filter((channel) => channel.subType === this.channelSubType) : [];
                     this.noOfChannels = this.channels.length;
                 },
                 error: (errorResponse: HttpErrorResponse) => onError(this.alertService, errorResponse),
