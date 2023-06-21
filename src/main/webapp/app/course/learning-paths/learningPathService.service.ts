42ca8d579a153bfe35b08885e737632c8dd57deb
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-06-21 13:26:05 +0200

    Update learningPathService.service.ts

--- src/main/webapp/app/course/learning-paths/learningPathService.service.ts
+++ src/main/webapp/app/course/learning-paths/learningPathService.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Course } from 'app/entities/course.model';
 
 @Injectable({ providedIn: 'root' })
 export class LearningPathService {
     private resourceURL = 'api';
+
+    constructor(private httpClient: HttpClient) {}
+
+    enableLearningPaths(courseId: number): Observable<HttpResponse<Course>> {
+        // return this.httpClient.put(`${this.resourceURL}/courses/${courseId}/learning-paths/enable`, { observe: 'response' })
+        return new Observable();
+    }
 }
