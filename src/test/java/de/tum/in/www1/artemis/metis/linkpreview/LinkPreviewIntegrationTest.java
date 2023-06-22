c42aea42b68a40bc7e2d34effbb9631b3921b04c
Pellumb Baboci <pellumbbaboci@gmail.com>
2023-06-23 01:41:45 +0200

    server test

--- src/test/java/de/tum/in/www1/artemis/metis/linkpreview/LinkPreviewIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/metis/linkpreview/LinkPreviewIntegrationTest.java
@@ -19,8 +19,11 @@ class LinkPreviewIntegrationTest extends AbstractSpringIntegrationBambooBitbucke
 
     private static final String TEST_PREFIX = "linkpreviewintegrationtest";
 
+    // this link will return null for all fields because it does not include OG tags
+    private static final String googleUrl = "https://google.com";
+
     private static final String[] urls = { "https://github.com/ls1intum/Artemis/pull/6615",
-            "https://stackoverflow.com/questions/40965622/unit-testing-an-endpoint-with-requestbody", "https://github.com/" };
+            "https://stackoverflow.com/questions/40965622/unit-testing-an-endpoint-with-requestbody", "https://github.com/", googleUrl };
 
     @Autowired
     private UserUtilService userUtilService;
@@ -33,17 +36,25 @@ class LinkPreviewIntegrationTest extends AbstractSpringIntegrationBambooBitbucke
     @ParameterizedTest
     @MethodSource("provideUrls")
     @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
-    void testCreateConversationPost(String url) throws Exception {
+    void testLinkPreviewDataExtraction(String url) throws Exception {
 
         LinkPreviewDTO linkPreviewData = request.postWithPlainStringResponseBody("/api/link-preview", url, LinkPreviewDTO.class, HttpStatus.OK);
         assertThat(linkPreviewData).isNotNull();
 
-        assertThat(linkPreviewData.url()).isNotNull();
-        assertThat(linkPreviewData.description()).isNotNull();
-        assertThat(linkPreviewData.image()).isNotNull();
-        assertThat(linkPreviewData.title()).isNotNull();
+        if (url.equals(googleUrl)) {
+            assertThat(linkPreviewData.url()).isNull();
+            assertThat(linkPreviewData.description()).isNull();
+            assertThat(linkPreviewData.image()).isNull();
+            assertThat(linkPreviewData.title()).isNull();
+        }
+        else {
+            assertThat(linkPreviewData.url()).isNotNull();
+            assertThat(linkPreviewData.description()).isNotNull();
+            assertThat(linkPreviewData.image()).isNotNull();
+            assertThat(linkPreviewData.title()).isNotNull();
 
-        assertThat(linkPreviewData.url()).isEqualTo(url);
+            assertThat(linkPreviewData.url()).isEqualTo(url);
+        }
     }
 
     private static Stream<String> provideUrls() {
