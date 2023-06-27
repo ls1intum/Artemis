adc263e83da8a53d1de091ce0a399c77685659b7
Dmytro Polityka <dmytro.polityka@tum.de>
2023-06-27 18:05:45 +0200

    declare missing components

--- src/main/webapp/app/iris/iris.module.ts
+++ src/main/webapp/app/iris/iris.module.ts
@@ -11,9 +11,23 @@ import { AboutIrisComponent } from 'app/iris/about-iris/about-iris.component';
 import { AngularDraggableModule } from 'angular2-draggable';
 import { RouterModule } from '@angular/router';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
+import { IrisSettingsUpdateComponent } from 'app/iris/settings/iris-settings-update/iris-settings-update.component';
+import { IrisGlobalSettingsUpdateComponent } from 'app/iris/settings/iris-global-settings-update/iris-global-settings-update.component';
+import { IrisCourseSettingsUpdateComponent } from 'app/iris/settings/iris-course-settings-update/iris-course-settings-update.component';
+import { IrisProgrammingExerciseSettingsUpdateComponent } from 'app/iris/settings/iris-programming-exercise-settings-update/iris-programming-exercise-settings-update.component';
+import { IrisSubSettingsUpdateComponent } from 'app/iris/settings/iris-settings-update/iris-sub-settings-update/iris-sub-settings-update.component';
 
 @NgModule({
-    declarations: [ExerciseChatWidgetComponent, ExerciseChatbotComponent, AboutIrisComponent],
+    declarations: [
+        ExerciseChatWidgetComponent,
+        ExerciseChatbotComponent,
+        AboutIrisComponent,
+        IrisSettingsUpdateComponent,
+        IrisGlobalSettingsUpdateComponent,
+        IrisCourseSettingsUpdateComponent,
+        IrisProgrammingExerciseSettingsUpdateComponent,
+        IrisSubSettingsUpdateComponent,
+    ],
     imports: [
         CommonModule,
         MatDialogModule,
