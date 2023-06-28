f051b4bf539f2610dbb8ce56c111afc376208c15
lara <lara.dvorsek@gmail.com>
2023-06-28 10:16:59 +0200

    use individual imports

--- src/test/java/de/tum/in/www1/artemis/notification/GroupNotificationServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/notification/GroupNotificationServiceTest.java
@@ -11,7 +11,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
