9d8336cca90ef6e5ae9b995bcd622889f6664686
Florian Gareis <dev@zoker.me>
2023-07-01 13:13:38 +0200

    Add local user management

---
+++ src/test/cypress/support/requests/UserManagementRequest.ts
@@ -0,0 +1,29 @@
+import { BASE_API, POST } from '../constants';
+import { UserRole } from '../users';
+
+/**
+ * A class which encapsulates all cypress requests related to course management.
+ */
+export class UserManagementRequests {
+    /**
+     * Creates a new user
+     * @param username the username of the new user
+     * @param password the password of the new user
+     * @param role the role of the new user
+     */
+    createUser(username: string, password: string, role: UserRole) {
+        const user = {
+            login: username,
+            password,
+            firstName: username,
+            lastName: username,
+            email: username + '@local',
+            authorities: [role],
+        };
+        return cy.request({
+            url: BASE_API + 'admin/users',
+            method: POST,
+            body: user,
+        });
+    }
+}
