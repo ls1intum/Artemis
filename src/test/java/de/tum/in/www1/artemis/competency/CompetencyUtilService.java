757ad9c9db122aa40651291654443c587e5457a0
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-07-03 10:53:36 +0200

    improve documentation

--- src/test/java/de/tum/in/www1/artemis/competency/CompetencyUtilService.java
+++ src/test/java/de/tum/in/www1/artemis/competency/CompetencyUtilService.java
@@ -16,6 +16,13 @@ public class CompetencyUtilService {
     @Autowired
     private CompetencyRepository competencyRepo;
 
+    /**
+     * Creates competency and links it to the course. The title of the competency will hold the specified suffix.
+     *
+     * @param course course the competency will be linked to
+     * @param suffix the suffix that will be included in the title
+     * @return the persisted competency
+     */
     private Competency createCompetency(Course course, String suffix) {
         Competency competency = new Competency();
         competency.setTitle("Example Competency" + suffix);
@@ -24,10 +31,23 @@ public class CompetencyUtilService {
         return competencyRepo.save(competency);
     }
 
+    /**
+     * Creates competency and links it to the course.
+     *
+     * @param course course the competency will be linked to
+     * @return the persisted competency
+     */
     public Competency createCompetency(Course course) {
         return createCompetency(course, "");
     }
 
+    /**
+     * Creates multiple competencies and links them to the course
+     *
+     * @param course               course the competencies will be linked to
+     * @param numberOfCompetencies number of competencies to create
+     * @return array of the persisted competencies
+     */
     public Competency[] createCompetencies(Course course, int numberOfCompetencies) {
         Competency[] competencies = new Competency[numberOfCompetencies];
         for (int i = 0; i < competencies.length; i++) {
--- src/test/java/de/tum/in/www1/artemis/competency/LearningPathUtilService.java
+++ src/test/java/de/tum/in/www1/artemis/competency/LearningPathUtilService.java
@@ -7,6 +7,9 @@ import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.repository.CourseRepository;
 import de.tum.in.www1.artemis.service.LearningPathService;
 
+/**
+ * Service responsible for initializing the database with specific testdata related to learning paths for use in integration tests.
+ */
 @Service
 public class LearningPathUtilService {
 
@@ -16,6 +19,12 @@ public class LearningPathUtilService {
     @Autowired
     LearningPathService learningPathService;
 
+    /**
+     * Enable and generate learning paths for course.
+     *
+     * @param course the course for which the learning paths are generated
+     * @return the updated course
+     */
     public Course enableAndGenerateLearningPathsForCourse(Course course) {
         course = courseRepository.findWithEagerLearningPathsAndCompetenciesByIdElseThrow(course.getId());
         learningPathService.generateLearningPaths(course);
