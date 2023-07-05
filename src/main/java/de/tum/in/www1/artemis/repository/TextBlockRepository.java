f45737747f0606aae6791f23a2bc5681cd6e0b02
Paul Schwind <git@pschwind.de>
2023-07-05 20:04:56 +0200

    Remove unused method from TextBlockRepository

--- src/main/java/de/tum/in/www1/artemis/repository/TextBlockRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/TextBlockRepository.java
@@ -1,10 +1,7 @@
 package de.tum.in.www1.artemis.repository;
 
-import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.LOAD;
-
 import java.util.*;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
@@ -15,14 +12,10 @@ import de.tum.in.www1.artemis.domain.TextBlock;
 /**
  * Spring Data repository for the TextBlock entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface TextBlockRepository extends JpaRepository<TextBlock, String> {
 
     Set<TextBlock> findAllBySubmissionId(Long id);
-
-    @EntityGraph(type = LOAD, attributePaths = { "submission" })
-    Set<TextBlock> findAllBySubmissionIdIn(Set<Long> submissionIdList);
 
     @Transactional // ok because of delete
     @Modifying
