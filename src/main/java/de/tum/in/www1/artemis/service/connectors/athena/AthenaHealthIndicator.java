2efab856c3200ffea2148cb3c2dad64c5f8ee85b
Paul Schwind <git@pschwind.de>
2023-07-06 03:36:56 +0200

    Correctly indicate Athena health in admin dashboard

--- src/main/java/de/tum/in/www1/artemis/service/connectors/athena/AthenaHealthIndicator.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/athena/AthenaHealthIndicator.java
@@ -1,5 +1,6 @@
 package de.tum.in.www1.artemis.service.connectors.athena;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -27,22 +28,51 @@ public class AthenaHealthIndicator implements HealthIndicator {
         this.shortTimeoutRestTemplate = shortTimeoutRestTemplate;
     }
 
+    private record AthenaModuleHealth(String type, boolean healthy, String url) {
+    }
+
+    private static String moduleHealthToString(AthenaModuleHealth moduleHealth) {
+        var healthString = "";
+        if (moduleHealth.healthy) {
+            healthString += "\uD83D\uDFE2"; // green circle
+        }
+        else {
+            healthString += "\uD83D\uDD34"; // red circle
+        }
+        healthString += " " + moduleHealth.url + " (" + moduleHealth.type + ")";
+        return healthString;
+    }
+
     /**
-     * Ping Athena at /queueStatus and check if the service is available.
+     * Ping Athena at /health and check if the service is available.
      */
     @Override
     public Health health() {
         ConnectorHealth health;
         try {
-            final var status = shortTimeoutRestTemplate.getForObject(athenaUrl + "/queueStatus", JsonNode.class);
-            var isUp = status.get("total").isNumber();
+            final var response = shortTimeoutRestTemplate.getForObject(athenaUrl + "/health", JsonNode.class);
+            var isUp = response.get("status").asText().equals("ok");
             health = new ConnectorHealth(isUp);
+            var additionalInfo = new HashMap<String, Object>();
+            additionalInfo.put("url", athenaUrl);
+            additionalInfo.put("assessment module manager", response.get("status").asText());
+            // add health of all modules
+            if (response.has("modules")) {
+                JsonNode modules = response.get("modules");
+                // keys are module names, values are description maps
+                modules.fields().forEachRemaining(module -> {
+                    var moduleHealth = new AthenaModuleHealth(module.getValue().get("type").asText(), module.getValue().get("healthy").asBoolean(),
+                            module.getValue().get("url").asText());
+                    additionalInfo.put(module.getKey(), moduleHealthToString(moduleHealth));
+                });
+            }
+            health.setAdditionalInfo(additionalInfo);
         }
         catch (Exception emAll) {
             health = new ConnectorHealth(emAll);
+            health.setAdditionalInfo(Map.of("url", athenaUrl));
         }
 
-        health.setAdditionalInfo(Map.of("url", athenaUrl));
         return health.asActuatorHealth();
     }
 }
