0d8b61dc94db76e1105a8835982015b2399dddcc
jakubriegel <jakubriegel112@gmail.com>
2023-07-08 11:38:40 +0200

    read cron expression from application.yml

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
@@ -37,7 +37,7 @@ public class ContinuousPlagiarismControlService {
     /**
      * Daily triggers plagiarism checks as a part of continuous plagiarism control.
      */
-    @Scheduled(cron = "0 0 4 * * *") // execute this every night at 4:00:00 am
+    @Scheduled(cron = "${artemis.scheduling.continuous-plagiarism-control-trigger-time:0 0 4 * * *}")
     public void executeChecks() {
         log.info("Starting continuous plagiarism control...");
 
