5a996ab9583b54cdd39522e60d62cab03d9f7bed
Paul Schwind <git@pschwind.de>
2023-07-13 15:31:27 +0200

    Only request feedback suggestions if Athena is available

--- src/main/webapp/app/assessment/athena.service.ts
+++ src/main/webapp/app/assessment/athena.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 import { TextBlockRef } from 'app/entities/text-block-ref.model';
+import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
 
 type FeedbackSuggestionsResponseType = HttpResponse<TextBlockRef[]>;
 
@@ -9,7 +10,7 @@ type FeedbackSuggestionsResponseType = HttpResponse<TextBlockRef[]>;
 export class AthenaService {
     public resourceUrl = 'api/athena';
 
-    constructor(protected http: HttpClient) {}
+    constructor(protected http: HttpClient, private profileService: ProfileService) {}
 
     /**
      * Get feedback suggestions for the given submission from Athena
@@ -17,7 +18,14 @@ export class AthenaService {
      * @param exerciseId the id of the exercise
      * @param submissionId the id of the submission
      */
-    getFeedbackSuggestions(exerciseId: number, submissionId: number): Observable<FeedbackSuggestionsResponseType> {
-        return this.http.get<TextBlockRef[]>(`${this.resourceUrl}/exercises/${exerciseId}/submissions/${submissionId}/feedback-suggestions`, { observe: 'response' });
+    getFeedbackSuggestions(exerciseId: number, submissionId: number): Observable<FeedbackSuggestionsResponseType | TextBlockRef[]> {
+        return this.profileService.getProfileInfo().pipe(
+            switchMap((profileInfo) => {
+                if (!profileInfo.activeProfiles.includes('athena')) {
+                    return of([]);
+                }
+                return this.http.get<TextBlockRef[]>(`${this.resourceUrl}/exercises/${exerciseId}/submissions/${submissionId}/feedback-suggestions`, { observe: 'response' });
+            }),
+        );
     }
 }
