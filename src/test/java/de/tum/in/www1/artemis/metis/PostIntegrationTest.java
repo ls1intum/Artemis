3391ee075ee38ed00f4379ae75eb73d53e16479e
Lucas Welscher <ga53foy@mytum.de>
2023-07-13 12:16:01 +0200

    Development: Enforce consistent usage of nullness annotations (#6872)

--- src/test/java/de/tum/in/www1/artemis/metis/PostIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/metis/PostIntegrationTest.java
@@ -11,8 +11,8 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotNull;
 
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
