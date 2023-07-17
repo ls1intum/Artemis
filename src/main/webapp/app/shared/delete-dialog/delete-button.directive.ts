95d79b46972392344f95ba50528ccefab6d13d03
Lucas Welscher <ga53foy@mytum.de>
2023-07-17 17:33:00 +0200

    use delete button instead of browser alerts

--- src/main/webapp/app/shared/delete-dialog/delete-button.directive.ts
+++ src/main/webapp/app/shared/delete-dialog/delete-button.directive.ts
@@ -9,6 +9,7 @@ import { ButtonSize, ButtonType } from 'app/shared/components/button.component';
 export class DeleteButtonDirective implements OnInit {
     @Input() entityTitle: string;
     @Input() deleteQuestion: string;
+    @Input() translateValues: { [key: string]: unknown } = {};
     @Input() deleteConfirmationText: string;
     @Input() buttonSize: ButtonSize = ButtonSize.SMALL;
     @Input() additionalChecks?: { [key: string]: string };
@@ -64,6 +65,7 @@ export class DeleteButtonDirective implements OnInit {
         const deleteDialogData: DeleteDialogData = {
             entityTitle: this.entityTitle,
             deleteQuestion: this.deleteQuestion,
+            translateValues: this.translateValues,
             deleteConfirmationText: this.deleteConfirmationText,
             additionalChecks: this.additionalChecks,
             actionType: this.actionType,
