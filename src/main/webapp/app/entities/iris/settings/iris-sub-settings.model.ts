8a38b501443a5096b09224936f044152e79e7f7d
Timor Morrien <timor.morrien@tum.de>
2023-07-17 21:11:45 +0200

    Remove unused import

--- src/main/webapp/app/entities/iris/settings/iris-sub-settings.model.ts
+++ src/main/webapp/app/entities/iris/settings/iris-sub-settings.model.ts
@@ -1,6 +1,5 @@
 import { BaseEntity } from 'app/shared/model/base-entity';
 import { IrisTemplate } from 'app/entities/iris/settings/iris-template';
-import { IrisModel } from 'app/entities/iris/settings/iris-model';
 
 export class IrisSubSettings implements BaseEntity {
     id?: number;
