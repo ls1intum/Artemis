47f025318ac82aee0418f76aadfbb7a0526e5d76
Lucas Welscher <ga53foy@mytum.de>
2023-07-17 17:37:16 +0200

    fix client tests

--- src/test/javascript/spec/component/assessment-shared/assessment-detail.component.spec.ts
+++ src/test/javascript/spec/component/assessment-shared/assessment-detail.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateService } from '@ngx-translate/core';
+import { DeleteButtonDirective } from 'app/shared/delete-dialog/delete-button.directive';
 import { MockComponent, MockDirective, MockModule, MockPipe, MockProvider } from 'ng-mocks';
 import { Feedback } from 'app/entities/feedback.model';
 import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
@@ -28,6 +29,7 @@ describe('Assessment Detail Component', () => {
                 MockComponent(FaLayersComponent),
                 MockPipe(ArtemisTranslatePipe),
                 MockDirective(NgModel),
+                MockDirective(DeleteButtonDirective),
                 MockComponent(AssessmentCorrectionRoundBadgeComponent),
             ],
             providers: [{ provide: TranslateService, useClass: MockTranslateService }, MockProvider(StructuredGradingCriterionService)],
@@ -66,11 +68,9 @@ describe('Assessment Detail Component', () => {
             credits: 1.5,
         } as Feedback;
         const emitSpy = jest.spyOn(comp.deleteAssessment, 'emit');
-        const confirmStub = jest.spyOn(window, 'confirm').mockReturnValue(true);
         comp.delete();
         fixture.detectChanges();
 
         expect(emitSpy).toHaveBeenCalledOnce();
-        expect(confirmStub).toHaveBeenCalledOnce();
     });
 });
