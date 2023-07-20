9efbed32e8902cd83cd82cc192d533d110a0e386
Benedikt Fein <fein@fim.uni-passau.de>
2023-07-20 17:40:10 +0200

    fix mock alert service typing

--- src/test/javascript/spec/helpers/mocks/service/mock-alert.service.ts
+++ src/test/javascript/spec/helpers/mocks/service/mock-alert.service.ts
@@ -1,6 +1,6 @@
 import { Alert } from 'app/core/util/alert.service';
 
 export class MockAlertService {
-    success = () => ({} as Alert);
-    error = () => ({} as Alert);
+    success = (message: string) => ({} as Alert);
+    error = (message: string) => ({} as Alert);
 }
