d9c3c69b6f6e52e4ab8455930446f2918d95aebd
Paul Schwind <git@pschwind.de>
2023-07-21 00:41:53 +0200

    Remove previous attempt at fixing tests

--- src/test/java/de/tum/in/www1/artemis/text/TextAssessmentIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/text/TextAssessmentIntegrationTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.util.LinkedMultiValueMap;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
@@ -46,7 +45,6 @@ import de.tum.in.www1.artemis.user.UserUtilService;
 import de.tum.in.www1.artemis.web.rest.dto.TextAssessmentDTO;
 import de.tum.in.www1.artemis.web.rest.dto.TextAssessmentUpdateDTO;
 
-@DirtiesContext // allow attempted (failing) requests to Athena when accessing the text assessment resource, see https://stackoverflow.com/q/50239757/4306257
 class TextAssessmentIntegrationTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
 
     private static final String TEST_PREFIX = "textassessment";
