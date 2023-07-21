4743c6bea69e02330b4a653d1640a1c045ea35ba
Lucas Welscher <ga53foy@mytum.de>
2023-07-21 22:34:45 +0200

    Update CourseManagementRequests.ts

--- src/test/cypress/support/requests/CourseManagementRequests.ts
+++ src/test/cypress/support/requests/CourseManagementRequests.ts
@@ -45,9 +45,10 @@ export class CourseManagementRequests {
      * @returns <Chainable> request response
      */
     deleteCourse(course: Course, admin: CypressCredentials) {
-        // Sometimes the server fails with a ConstraintViolationError if we delete the course immediately after a login
         if (course) {
             cy.login(admin);
+            // Sometimes the server fails with a ConstraintViolationError if we delete the course immediately after a login
+            cy.wait(500);
             return cy.request({ method: DELETE, url: `${COURSE_ADMIN_BASE}/${course.id}`, retryOnStatusCodeFailure: true });
         }
     }
