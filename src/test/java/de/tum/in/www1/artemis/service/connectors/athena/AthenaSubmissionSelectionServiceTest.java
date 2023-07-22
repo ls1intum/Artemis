d55971113d327a085f1d0b8f2f23bf1815e6d0db
Paul Schwind <git@pschwind.de>
2023-07-22 18:47:24 +0200

    Adapt submission selection tests to last change

--- src/test/java/de/tum/in/www1/artemis/service/connectors/athena/AthenaSubmissionSelectionServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/connectors/athena/AthenaSubmissionSelectionServiceTest.java
@@ -2,27 +2,21 @@ package de.tum.in.www1.artemis.service.connectors.athena;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tum.in.www1.artemis.domain.TextExercise;
 import de.tum.in.www1.artemis.domain.TextSubmission;
 import de.tum.in.www1.artemis.domain.enumeration.AssessmentType;
-import de.tum.in.www1.artemis.repository.SubmissionRepository;
 
 class AthenaSubmissionSelectionServiceTest extends AthenaTest {
 
-    @Mock
-    private SubmissionRepository submissionRepository;
-
+    @Autowired
     private AthenaSubmissionSelectionService athenaSubmissionSelectionService;
 
     private TextExercise textExercise;
@@ -33,17 +27,11 @@ class AthenaSubmissionSelectionServiceTest extends AthenaTest {
 
     @BeforeEach
     void setUp() {
-        athenaSubmissionSelectionService = new AthenaSubmissionSelectionService(athenaRequestMockProvider.getRestTemplate(), submissionRepository);
-        ReflectionTestUtils.setField(athenaSubmissionSelectionService, "athenaUrl", athenaUrl);
-
         athenaRequestMockProvider.enableMockingOfRequests();
 
         textExercise = createTextExercise();
         textSubmission1 = new TextSubmission(1L);
         textSubmission2 = new TextSubmission(2L);
-
-        when(submissionRepository.findById(textSubmission1.getId())).thenReturn(Optional.of(textSubmission1));
-        when(submissionRepository.findById(textSubmission2.getId())).thenReturn(Optional.of(textSubmission2));
     }
 
     @Test
@@ -57,7 +45,7 @@ class AthenaSubmissionSelectionServiceTest extends AthenaTest {
     void testSubmissionSelectionFromOne() {
         athenaRequestMockProvider.mockSelectSubmissionsAndExpect(1, jsonPath("$.exercise.id").value(textExercise.getId()), jsonPath("$.submissionIds").isArray());
         var submission = athenaSubmissionSelectionService.getProposedSubmission(textExercise, List.of(textSubmission1.getId()));
-        assertThat(submission).contains(textSubmission1);
+        assertThat(submission).contains(textSubmission1.getId());
     }
 
     @Test
@@ -71,7 +59,7 @@ class AthenaSubmissionSelectionServiceTest extends AthenaTest {
     void testSubmissionSelectionFromTwo() {
         athenaRequestMockProvider.mockSelectSubmissionsAndExpect(1, jsonPath("$.exercise.id").value(textExercise.getId()), jsonPath("$.submissionIds").isArray());
         var submission = athenaSubmissionSelectionService.getProposedSubmission(textExercise, List.of(textSubmission1.getId(), textSubmission2.getId()));
-        assertThat(submission).contains(textSubmission1);
+        assertThat(submission).contains(textSubmission1.getId());
     }
 
     @Test
@@ -80,6 +68,4 @@ class AthenaSubmissionSelectionServiceTest extends AthenaTest {
         assertThatThrownBy(() -> athenaSubmissionSelectionService.getProposedSubmission(textExercise, List.of(textSubmission1.getId())))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
-    // TODO: Tests for 1. Athena returns wrong ID, 2. Athena returns non-existing ID, 3. Athena returns invalid response
 }
