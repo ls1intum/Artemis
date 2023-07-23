e3b431126a6f76e1dfccb1ad3162c175f1a05847
jakubriegel <jakubriegel112@gmail.com>
2023-07-23 21:13:57 +0200

    refactor and tests for TextPlagiarismResultConverter

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/TextPlagiarismResultConverter.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/TextPlagiarismResultConverter.java
@@ -1,6 +1,5 @@
 package de.tum.in.www1.artemis.service.plagiarism;
 
-import static java.util.Comparator.naturalOrder;
 import static java.util.stream.Collectors.toUnmodifiableSet;
 
 import java.io.File;
@@ -15,12 +14,10 @@ import org.springframework.stereotype.Component;
 import de.jplag.JPlagComparison;
 import de.jplag.JPlagResult;
 import de.jplag.Submission;
-import de.tum.in.www1.artemis.domain.DomainObject;
-import de.tum.in.www1.artemis.domain.Exercise;
+import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.plagiarism.*;
 import de.tum.in.www1.artemis.domain.plagiarism.text.TextPlagiarismResult;
 import de.tum.in.www1.artemis.domain.plagiarism.text.TextSubmissionElement;
-import de.tum.in.www1.artemis.repository.SubmissionRepository;
 
 /**
  * Contains logic that converts JPlagResult into a TextPlagiarismResult for text and programming exercises
@@ -30,10 +27,10 @@ class TextPlagiarismResultConverter {
 
     private static final Logger log = LoggerFactory.getLogger(TextPlagiarismResultConverter.class);
 
-    private final SubmissionRepository submissionRepository;
+    private final JPlagSubmissionDataExtractor jPlagSubmissionDataExtractor;
 
-    TextPlagiarismResultConverter(SubmissionRepository submissionRepository) {
-        this.submissionRepository = submissionRepository;
+    TextPlagiarismResultConverter(JPlagSubmissionDataExtractor jPlagSubmissionDataExtractor) {
+        this.jPlagSubmissionDataExtractor = jPlagSubmissionDataExtractor;
     }
 
     /**
@@ -43,6 +40,8 @@ class TextPlagiarismResultConverter {
      * @param exercise the exercise to which the result should belong, either Text or Programming
      */
     TextPlagiarismResult fromJplagResult(JPlagResult result, Exercise exercise) {
+        verifyAllowedExerciseType(exercise);
+
         var textPlagiarismResult = new TextPlagiarismResult();
 
         // sort and limit the number of comparisons to 500 to save memory and cpu power
@@ -56,6 +55,12 @@ class TextPlagiarismResultConverter {
         textPlagiarismResult.setExercise(exercise);
 
         return textPlagiarismResult;
+    }
+
+    private static void verifyAllowedExerciseType(Exercise exercise) {
+        if (!(exercise instanceof TextExercise || exercise instanceof ProgrammingExercise)) {
+            throw new IllegalStateException("TextPlagiarismResults are supported only for TextExercise and ProgrammingExercise.");
+        }
     }
 
     /**
@@ -88,14 +93,6 @@ class TextPlagiarismResultConverter {
      * @return a new PlagiarismSubmission instance
      */
     private PlagiarismSubmission<TextSubmissionElement> createSubmissionFromJPlagSubmission(Submission jplagSubmission, Exercise exercise, File submissionDirectory) {
-        return switch (exercise.getExerciseType()) {
-            case TEXT -> createTextSubmissionFromJPlagSubmission(jplagSubmission, exercise, submissionDirectory);
-            case PROGRAMMING -> createProgramingSubmissionFromJPlagSubmission(jplagSubmission, exercise, submissionDirectory);
-            default -> throw new IllegalStateException("Unexpected exercise type: " + exercise.getExerciseType());
-        };
-    }
-
-    private PlagiarismSubmission<TextSubmissionElement> createTextSubmissionFromJPlagSubmission(Submission jplagSubmission, Exercise exercise, File submissionDirectory) {
         var submission = new PlagiarismSubmission<TextSubmissionElement>();
 
         submission.setElements(jplagSubmission.getTokenList().stream().filter(Objects::nonNull)
@@ -103,67 +100,7 @@ class TextPlagiarismResultConverter {
         submission.setSize(jplagSubmission.getNumberOfTokens());
         submission.setScore(null); // TODO
 
-        var jplagSubmissionNameData = jplagSubmission.getName().split("[-.]");
-        long submissionId = 0;
-        String studentLogin = "unknown";
-        if (jplagSubmissionNameData.length >= 2) {
-            try {
-                submissionId = Long.parseLong(jplagSubmissionNameData[0]);
-            }
-            catch (NumberFormatException e) {
-                log.error("Invalid participationId: {}", e.getMessage());
-            }
-
-            studentLogin = jplagSubmissionNameData[1];
-        }
-
-        submission.setSubmissionId(submissionId);
-        submission.setStudentLogin(studentLogin);
-
+        jPlagSubmissionDataExtractor.getAndSetSubmissionIdAndStudentLogin(submission, jplagSubmission, exercise);
         return submission;
-    }
-
-    private PlagiarismSubmission<TextSubmissionElement> createProgramingSubmissionFromJPlagSubmission(Submission jplagSubmission, Exercise exercise, File submissionDirectory) {
-        var submission = new PlagiarismSubmission<TextSubmissionElement>();
-
-        submission.setElements(jplagSubmission.getTokenList().stream().filter(Objects::nonNull)
-                .map(token -> TextSubmissionElement.fromJPlagToken(token, submission, exercise, submissionDirectory)).collect(Collectors.toCollection(ArrayList::new)));
-        submission.setSize(jplagSubmission.getNumberOfTokens());
-        submission.setScore(null); // TODO
-
-        var jplagSubmissionNameData = jplagSubmission.getName().split("[-.]");
-        long submissionId = 0;
-        String studentLogin = "unknown";
-        if (hasParticipationIdAndSubmissionId(jplagSubmissionNameData)) {
-            try {
-                submissionId = Long.parseLong(jplagSubmissionNameData[1]);
-            }
-            catch (NumberFormatException e) {
-                log.error("Invalid submissionId: {}", e.getMessage());
-            }
-            studentLogin = jplagSubmissionNameData[2];
-        }
-        else if (jplagSubmissionNameData.length >= 2) {
-            // path for text exercises and legacy programing repositories where only participationId is stored in result name
-            // needs to be here to handle old submissions
-            try {
-                var participationId = Long.parseLong(jplagSubmissionNameData[0]);
-                submissionId = submissionRepository.findAllByParticipationId(participationId).stream().max(naturalOrder()).map(DomainObject::getId).orElseThrow();
-            }
-            catch (NumberFormatException e) {
-                log.error("Invalid participationId: {}", e.getMessage());
-            }
-
-            studentLogin = jplagSubmissionNameData[1];
-        }
-
-        submission.setSubmissionId(submissionId);
-        submission.setStudentLogin(studentLogin);
-
-        return submission;
-    }
-
-    private static boolean hasParticipationIdAndSubmissionId(String[] jplagSubmissionNameData) {
-        return jplagSubmissionNameData.length == 3;
     }
 }
