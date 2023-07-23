54e07a2e9c30cc61eb180b985e63d8faa35e835b
Tobias Kotzian <solizer@web.de>
2023-07-23 17:18:15 +0200

    Try again

--- src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisHealthIndicator.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisHealthIndicator.java
@@ -9,8 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.tum.in.www1.artemis.service.connectors.ConnectorHealth;
 import de.tum.in.www1.artemis.service.connectors.iris.dto.IrisStatusDTO;
@@ -21,11 +26,14 @@ public class IrisHealthIndicator implements HealthIndicator {
 
     private final RestTemplate shortTimeoutRestTemplate;
 
+    private final ObjectMapper objectMapper;
+
     @Value("${artemis.iris.url}")
     private URI irisUrl;
 
-    public IrisHealthIndicator(@Qualifier("shortTimeoutIrisRestTemplate") RestTemplate shortTimeoutRestTemplate) {
+    public IrisHealthIndicator(@Qualifier("shortTimeoutIrisRestTemplate") RestTemplate shortTimeoutRestTemplate, MappingJackson2HttpMessageConverter springMvcJacksonConverter) {
         this.shortTimeoutRestTemplate = shortTimeoutRestTemplate;
+        this.objectMapper = springMvcJacksonConverter.getObjectMapper();
     }
 
     /**
@@ -35,7 +43,12 @@ public class IrisHealthIndicator implements HealthIndicator {
     public Health health() {
         ConnectorHealth health;
         try {
-            final var status = shortTimeoutRestTemplate.getForObject(irisUrl.resolve("/api/v1/health"), IrisStatusDTO[].class);
+            var response = shortTimeoutRestTemplate.getForEntity(irisUrl + "/api/v1/health", JsonNode.class);
+            if (!response.getStatusCode().is2xxSuccessful() || !response.hasBody()) {
+                health = new ConnectorHealth(false);
+                return health.asActuatorHealth();
+            }
+            IrisStatusDTO[] status = (IrisStatusDTO[]) parseResponse(response.getBody(), IrisStatusDTO.class.arrayType());
             var isUp = status != null && Arrays.stream(status).anyMatch(s -> s.status() == IrisStatusDTO.ModelStatus.UP);
             Map<String, Object> additionalInfo = Map.of("url", irisUrl, "modelStatuses", status);
             health = new ConnectorHealth(isUp, additionalInfo);
@@ -47,5 +60,14 @@ public class IrisHealthIndicator implements HealthIndicator {
         }
 
         return health.asActuatorHealth();
+    }
+
+    private <T> T parseResponse(JsonNode response, Class<T> clazz) {
+        try {
+            return objectMapper.treeToValue(response, clazz);
+        }
+        catch (JsonProcessingException e) {
+            return null;
+        }
     }
 }
