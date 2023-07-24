ef3440ee0e3b8869d3cb70c0128ad3b41e9c0025
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-07-24 10:32:44 +0200

    Stop files from being compressed with folders

--- src/main/webapp/app/exercises/programming/shared/code-editor/file-browser/code-editor-file-browser.component.ts
+++ src/main/webapp/app/exercises/programming/shared/code-editor/file-browser/code-editor-file-browser.component.ts
@@ -379,7 +379,7 @@ export class CodeEditorFileBrowserComponent implements OnInit, OnChanges, AfterV
      * @param node Tree node
      */
     compressTree(node: FileTreeItem): FileTreeItem {
-        if (node.children && node.children.length === 1 && node.children[0].children) {
+        if (node.children && node.children.length === 1 && this.repositoryFiles[node.children[0].value] === FileType.FOLDER) {
             return this.compressTree({ ...node.children[0], text: node.text + '/' + node.children[0].text, folder: node.folder, file: node.file });
         } else if (node.children) {
             return { ...node, children: node.children.map(this.compressTree.bind(this)) };
