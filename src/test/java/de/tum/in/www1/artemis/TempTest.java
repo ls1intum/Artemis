86ae1f0d76e249cde8a07a17a7bf56eac22a523b
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-07-26 10:35:28 +0200

    Failure: Because of Failing Test

--- src/test/java/de/tum/in/www1/artemis/TempTest.java
+++ src/test/java/de/tum/in/www1/artemis/TempTest.java
@@ -1,12 +1,10 @@
 package de.tum.in.www1.artemis;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TempTest {
 
     @Test
-    @Disabled
     void failingTest() {
         throw new RuntimeException("This test should fail");
     }
