402bdae46f2ae90c0b3a592af0ab43fb90c40609
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-07-27 10:44:03 +0200

    Remove unused property

--- src/main/webapp/app/course/learning-paths/learning-path-graph/learning-path-graph-node.component.ts
+++ src/main/webapp/app/course/learning-paths/learning-path-graph/learning-path-graph-node.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faCheckCircle, faCircle, faPlayCircle, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { NgxLearningPathNode, NodeType } from 'app/entities/competency/learning-path.model';
-import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'jhi-learning-path-graph-node',
@@ -18,7 +17,4 @@ export class LearningPathGraphNodeComponent {
 
     faCircle = faCircle;
     protected readonly NodeType = NodeType;
-
-    @ViewChild('inspectPopover')
-    private inspectPopover: NgbPopover;
 }
