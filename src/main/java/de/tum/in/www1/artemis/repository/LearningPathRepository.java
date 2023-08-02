a8c0f1062fe499fd7d6ac28d184212eaecea0124
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-08-02 13:32:34 +0200

    remove unused methods from repository

--- src/main/java/de/tum/in/www1/artemis/repository/LearningPathRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/LearningPathRepository.java
@@ -3,9 +3,6 @@ package de.tum.in.www1.artemis.repository;
 import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.LOAD;
 
 import java.util.Optional;
-import java.util.Set;
-
-import javax.validation.constraints.NotNull;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,10 +20,6 @@ public interface LearningPathRepository extends JpaRepository<LearningPath, Long
 
     Optional<LearningPath> findByCourseIdAndUserId(long courseId, long userId);
 
-    default LearningPath findByCourseIdAndUserIdElseThrow(long courseId, long userId) {
-        return findByCourseIdAndUserId(courseId, userId).orElseThrow(() -> new EntityNotFoundException("LearningPath"));
-    }
-
     @EntityGraph(type = LOAD, attributePaths = { "competencies" })
     Optional<LearningPath> findWithEagerCompetenciesByCourseIdAndUserId(long courseId, long userId);
 
@@ -37,41 +30,9 @@ public interface LearningPathRepository extends JpaRepository<LearningPath, Long
     @Query("""
             SELECT lp
             FROM LearningPath lp
-            WHERE lp.course.id = :courseId
-            """)
-    Set<LearningPath> findAllForCourse(@Param("courseId") long courseId);
-
-    @Query("""
-            SELECT lp
-            FROM LearningPath lp
             WHERE (lp.course.id = :courseId) AND (lp.user.login LIKE %:searchTerm% OR CONCAT(lp.user.firstName, ' ', lp.user.lastName) LIKE %:searchTerm%)
             """)
     Page<LearningPath> findByLoginOrNameInCourse(@Param("searchTerm") String searchTerm, @Param("courseId") long courseId, Pageable pageable);
-
-    @EntityGraph(type = LOAD, attributePaths = { "competencies" })
-    Optional<LearningPath> findWithEagerCompetenciesById(long learningPathId);
-
-    @NotNull
-    default LearningPath findWithEagerCompetenciesByIdElseThrow(long learningPathId) {
-        return findWithEagerCompetenciesById(learningPathId).orElseThrow(() -> new EntityNotFoundException("LearningPath", learningPathId));
-    }
-
-    @EntityGraph(type = LOAD, attributePaths = { "competencies", "competencies.lectureUnits", "competencies.exercises" })
-    Optional<LearningPath> findWithEagerCompetenciesAndLearningObjectsById(long learningPathId);
-
-    @NotNull
-    default LearningPath findWithEagerCompetenciesAndLearningObjectsByIdElseThrow(long learningPathId) {
-        return findWithEagerCompetenciesAndLearningObjectsById(learningPathId).orElseThrow(() -> new EntityNotFoundException("LearningPath", learningPathId));
-    }
-
-    @EntityGraph(type = LOAD, attributePaths = { "competencies", "competencies.lectureUnits", "competencies.lectureUnits.completedUsers", "competencies.exercises",
-            "competencies.exercises.studentParticipations" })
-    Optional<LearningPath> findWithEagerCompetenciesAndLearningObjectsAndCompletedUsersById(long learningPathId);
-
-    @NotNull
-    default LearningPath findWithEagerCompetenciesAndLearningObjectsAndCompletedUsersByIdElseThrow(long learningPathId) {
-        return findWithEagerCompetenciesAndLearningObjectsAndCompletedUsersById(learningPathId).orElseThrow(() -> new EntityNotFoundException("LearningPath", learningPathId));
-    }
 
     @Query("""
             SELECT COUNT (learningPath)
