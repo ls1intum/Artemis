62d49815e0c9988c238bb03df91fe997466c52d0
Tobias Lippert <t.lippert@tum.de>
2023-08-04 15:54:39 +0200

    highlight same data

--- src/main/webapp/app/exam/manage/suspicious-behavior/suspicious-sessions/suspicious-sessions.component.ts
+++ src/main/webapp/app/exam/manage/suspicious-behavior/suspicious-sessions/suspicious-sessions.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { SuspiciousExamSessions } from 'app/entities/exam-session.model';
+import { Component, Input, OnInit } from '@angular/core';
+import { SuspiciousExamSessions, SuspiciousSessionReason } from 'app/entities/exam-session.model';
 import { StudentExam } from 'app/entities/student-exam.model';
 
 @Component({
     selector: 'jhi-suspicious-sessions',
     templateUrl: './suspicious-sessions.component.html',
+    styleUrls: ['./suspicious-sessions.component.scss'],
 })
-export class SuspiciousSessionsComponent {
+export class SuspiciousSessionsComponent implements OnInit {
     @Input() suspiciousSessions: SuspiciousExamSessions;
+    suspiciousFingerprint = false;
+    suspiciousIpAddress = false;
+    suspiciousUserAgent = false;
+    ngOnInit(): void {
+        this.suspiciousFingerprint = this.suspiciousSessions.examSessions.some((session) => session.suspiciousReasons.includes(SuspiciousSessionReason.SAME_BROWSER_FINGERPRINT));
+        this.suspiciousIpAddress = this.suspiciousSessions.examSessions.some((session) => session.suspiciousReasons.includes(SuspiciousSessionReason.SAME_IP_ADDRESS));
+        this.suspiciousUserAgent = this.suspiciousSessions.examSessions.some((session) => session.suspiciousReasons.includes(SuspiciousSessionReason.SAME_USER_AGENT));
+    }
 
     getStudentExamLink(studentExam: StudentExam) {
         const studentExamId = studentExam.id;
