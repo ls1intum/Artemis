2e34111bb43dbff7726aac428295b067cf0e6033
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-08-23 18:42:42 +0200

    Fix isImport for programming exercises

--- src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-information.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-information.component.ts
@@ -1,30 +1,17 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProgrammingExercise, ProjectType } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseCreationConfig } from 'app/exercises/programming/manage/update/programming-exercise-creation-config';
-import { isMessagingEnabled } from 'app/entities/course.model';
-import { getCourseFromExercise, requiresChannelName } from 'app/entities/exercise.model';
 
 @Component({
     selector: 'jhi-programming-exercise-info',
     templateUrl: './programming-exercise-information.component.html',
     styleUrls: ['../../programming-exercise-form.scss'],
 })
-export class ProgrammingExerciseInformationComponent implements OnChanges {
-    @Input() isImportFromExistingExercise: boolean;
+export class ProgrammingExerciseInformationComponent {
+    @Input() isImport: boolean;
     @Input() isExamMode: boolean;
-    @Input() isEdit: boolean;
     @Input() programmingExercise: ProgrammingExercise;
     @Input() programmingExerciseCreationConfig: ProgrammingExerciseCreationConfig;
 
-    hideChannelNameInput = false;
-
     protected readonly ProjectType = ProjectType;
-    protected readonly isMessagingEnabled = isMessagingEnabled;
-    protected readonly getCourseFromExercise = getCourseFromExercise;
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes.programmingExercise || changes.isEdit || changes.isExamMode) {
-            this.hideChannelNameInput = !requiresChannelName(this.programmingExercise, this.isExamMode, !this.isEdit);
-        }
-    }
 }
