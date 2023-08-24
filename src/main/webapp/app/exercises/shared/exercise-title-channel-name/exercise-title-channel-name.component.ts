8d8f3f1dfa6be9c5db697795913c486b8ad1adcd
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-08-24 11:05:22 +0200

    Add course change detection

--- src/main/webapp/app/exercises/shared/exercise-title-channel-name/exercise-title-channel-name.component.ts
+++ src/main/webapp/app/exercises/shared/exercise-title-channel-name/exercise-title-channel-name.component.ts
@@ -1,12 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Course } from 'app/entities/course.model';
 import { Exercise, requiresChannelName } from 'app/entities/exercise.model';
 
 @Component({
     selector: 'jhi-exercise-title-channel-name',
     templateUrl: './exercise-title-channel-name.component.html',
 })
-export class ExerciseTitleChannelNameComponent implements OnInit {
+export class ExerciseTitleChannelNameComponent implements OnChanges {
     @Input() exercise: Exercise;
+    @Input() course?: Course;
     @Input() titlePattern: string;
     @Input() minTitleLength: number;
     @Input() isExamMode: boolean;
@@ -17,8 +19,10 @@ export class ExerciseTitleChannelNameComponent implements OnInit {
     @Output() onChannelNameChange = new EventEmitter<string>();
 
     hideChannelNameInput = false;
-    ngOnInit() {
-        this.hideChannelNameInput = !requiresChannelName(this.exercise, this.isExamMode, this.isImport);
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.exercise || changes.course || changes.isExamMode || this.isImport) {
+            this.hideChannelNameInput = !requiresChannelName(this.exercise, this.isExamMode, this.isImport);
+        }
     }
 
     updateTitle(newTitle: string) {
