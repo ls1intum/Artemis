922eaef90c0fb50ee80dfc24570a43a3e65792d8
Lucas Welscher <ga53foy@mytum.de>
2023-08-29 14:24:05 +0200

    and let's try another regex

--- src/main/webapp/app/exercises/programming/shared/instructions-render/extensions/programming-exercise-task.extension.ts
+++ src/main/webapp/app/exercises/programming/shared/instructions-render/extensions/programming-exercise-task.extension.ts
@@ -14,9 +14,9 @@ import { ShowdownExtension } from 'showdown';
 @Injectable({ providedIn: 'root' })
 export class ProgrammingExerciseTaskExtensionWrapper implements ArtemisShowdownExtensionWrapper {
     // E.g. [task][Implement BubbleSort](testBubbleSort)
-    private readonly taskRegex = /\[task]\[[^[\]]+]\((?:,?\s*[^()\s]+(?:\([^()]*\))?)*\)/g;
+    private readonly taskRegex = /\[task]\[[^[\]]+]\((?:[^()]+(?:\([^()]*\))?)*\)/g;
     // E.g. Implement BubbleSort, testBubbleSort
-    private readonly innerTaskRegex = /\[task]\[([^[\]]+)]\(((?:,?\s*[^()\s]+(?:\([^()]*\))?)*)\)/;
+    private readonly innerTaskRegex = /\[task]\[([^[\]]+)]\(((?:[^()]+(?:\([^()]*\))?)*)\)/;
 
     // We don't have a provider for ViewContainerRef, so we pass it from ProgrammingExerciseInstructionComponent
     viewContainerRef: ViewContainerRef;
