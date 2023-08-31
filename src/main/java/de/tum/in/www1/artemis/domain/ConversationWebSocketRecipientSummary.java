867d466fdbd291e99be065a27e407914a0ae58cb
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-08-31 16:25:41 +0200

    Fix typo
    Co-authored-by: Nityananda Zbil <nityananda.zbil@icloud.com>

--- src/main/java/de/tum/in/www1/artemis/domain/ConversationWebSocketRecipientSummary.java
+++ src/main/java/de/tum/in/www1/artemis/domain/ConversationWebSocketRecipientSummary.java
@@ -2,7 +2,7 @@ package de.tum.in.www1.artemis.domain;
 
 /**
  * Stores the user of a conversation participant, who is supposed to receive a websocket message and stores whether
- * the according conversation is hidden by the user.
+ * the corresponding conversation is hidden by the user.
  *
  * @param user                   the user who is a member of the conversation
  * @param isConversationHidden   true if the user has hidden the conversation
