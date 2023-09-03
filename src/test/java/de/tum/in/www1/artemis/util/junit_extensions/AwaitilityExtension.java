aff0f9e6b5e5db96e3fea309e7445ee69088eae4
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-09-03 17:04:32 +0200

    Revert "Adapt DEFAULT_POLL_DELAY"
    This reverts commit c7f97c1868a29a20d58e9f39b89b86e565800ef0.

--- src/test/java/de/tum/in/www1/artemis/util/junit_extensions/AwaitilityExtension.java
+++ src/test/java/de/tum/in/www1/artemis/util/junit_extensions/AwaitilityExtension.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 /**
- * A JUnit 5 extension that configures {@link Awaitility} to use a different default poll delay.
+ * A JUnit 5 extension that configures {@link Awaitility} to use a different default poll delay and interval.
  * <p>
- * By default, {@link Awaitility} would use a poll delay of 100ms, which makes tests run slower.
+ * By default, {@link Awaitility} would use a poll delay and interval of 100ms, which makes tests run slower.
  */
 public class AwaitilityExtension implements BeforeAllCallback {
 
-    private static final Duration DEFAULT_POLL_DELAY = Duration.ofMillis(10);
+    private static final Duration DEFAULT_POLL_DELAY = Duration.ZERO;
+
+    private static final Duration POLL_INTERVAL_START = Duration.ofMillis(100);
+
+    private static final long POLL_INTERVAL_MULTIPLIER = 2;
 
     private static boolean configured;
 
@@ -22,6 +26,7 @@ public class AwaitilityExtension implements BeforeAllCallback {
         if (!configured) {
             configured = true;
             Awaitility.setDefaultPollDelay(DEFAULT_POLL_DELAY);
+            Awaitility.setDefaultPollInterval((pollCount, previousDuration) -> pollCount == 1 ? POLL_INTERVAL_START : previousDuration.multipliedBy(POLL_INTERVAL_MULTIPLIER));
         }
     }
 }
