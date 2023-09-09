003e297d4c51772e1b613110871dce3bfa60c4cb
Tobias Lippert <t.lippert@tum.de>
2023-09-09 20:20:45 +0200

    refactor exercise update notification and enable it for programming exercises again

---
+++ src/main/webapp/app/exercises/shared/exercise-update-notification/exercise-update-notification.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Exercise } from 'app/entities/exercise.model';
+
+@Component({
+    selector: 'jhi-exercise-update-notification',
+    templateUrl: './exercise-update-notification.component.html',
+    styleUrls: ['./exercise-update-notification.component.scss'],
+})
+export class ExerciseUpdateNotificationComponent {
+    @Input() exercise: Exercise;
+    @Input() isImport: boolean;
+    @Input() notificationText?: string;
+    @Output() notificationTextChange: EventEmitter<string> = new EventEmitter<string>();
+
+    onInputChanged() {
+        this.notificationTextChange.emit(this.notificationText);
+    }
+}
