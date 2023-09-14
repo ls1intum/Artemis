3944ee8afe7d35e45cbe32be56eb80adbdb46526
Nityananda Zbil <nityananda.zbil@tum.de>
2023-09-15 00:09:56 +0200

    Fix TypeError:
    Cannot read properties of undefined (reading '__ngMocksConfig')

--- src/test/javascript/spec/component/overview/course-conversations/course-conversations-code-of-conduct.component.spec.ts
+++ src/test/javascript/spec/component/overview/course-conversations/course-conversations-code-of-conduct.component.spec.ts
@@ -1,11 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockPipe, MockProvider } from 'ng-mocks';
+import { MockPipe } from 'ng-mocks';
 import { CourseConversationsCodeOfConductComponent } from 'app/overview/course-conversations/code-of-conduct/course-conversations-code-of-conduct.component';
 import { ArtemisTestModule } from '../../../test.module';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { HtmlForMarkdownPipe } from 'app/shared/pipes/html-for-markdown.pipe';
-import { CourseManagementService } from 'app/course/manage/course-management.service';
-import { AlertService } from 'app/core/util/alert.service';
 
 describe('Course Conversations Code Of Conduct Component', () => {
     let fixture: ComponentFixture<CourseConversationsCodeOfConductComponent>;
@@ -14,13 +12,7 @@ describe('Course Conversations Code Of Conduct Component', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
-            declarations: [
-                CourseConversationsCodeOfConductComponent,
-                MockPipe(ArtemisTranslatePipe),
-                MockPipe(HtmlForMarkdownPipe),
-                MockProvider(AlertService),
-                MockProvider(CourseManagementService),
-            ],
+            declarations: [CourseConversationsCodeOfConductComponent, MockPipe(ArtemisTranslatePipe), MockPipe(HtmlForMarkdownPipe)],
             providers: [],
         })
             .compileComponents()
