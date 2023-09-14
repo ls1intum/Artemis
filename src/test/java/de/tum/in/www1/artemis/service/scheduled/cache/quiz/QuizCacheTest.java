f5e5835cfd4691b49b439fe6e423696609dc06ca
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-09-14 15:04:06 +0200

    Isolated QuizCacheTest

--- src/test/java/de/tum/in/www1/artemis/service/scheduled/cache/quiz/QuizCacheTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/scheduled/cache/quiz/QuizCacheTest.java
@@ -8,6 +8,8 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,8 @@ import de.tum.in.www1.artemis.service.QuizBatchService;
 import de.tum.in.www1.artemis.service.QuizExerciseService;
 import de.tum.in.www1.artemis.user.UserUtilService;
 
+@Isolated
+@Order(Integer.MAX_VALUE)
 class QuizCacheTest extends AbstractSpringIntegrationTest {
 
     private static final String TEST_PREFIX = "quizcachetest";
