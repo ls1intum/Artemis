682963d502c04ac09816f5f0863cd386ef4845d2
Lucas Welscher <ga53foy@mytum.de>
2023-09-16 17:46:27 +0200

    JsonInclude

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/RatingDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/RatingDTO.java
@@ -1,7 +1,10 @@
 package de.tum.in.www1.artemis.web.rest.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import de.tum.in.www1.artemis.domain.Rating;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record RatingDTO(Integer rating, DomainObjectIdDTO result) {
 
     public RatingDTO(Rating rating) {
