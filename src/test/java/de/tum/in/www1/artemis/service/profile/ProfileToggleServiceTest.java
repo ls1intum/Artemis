057454be5714a827e3b3432bcd60795425292edd
Simon Leiß <git@simon-leiss.de>
2023-09-16 23:28:28 +0200

    Add additional test.

---
+++ src/test/java/de/tum/in/www1/artemis/service/profile/ProfileToggleServiceTest.java
@@ -0,0 +1,25 @@
+package de.tum.in.www1.artemis.service.profile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import de.tum.in.www1.artemis.AbstractSpringIntegrationBambooBitbucketJiraTest;
+
+public class ProfileToggleServiceTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
+
+    @Autowired
+    private ProfileToggleService profileToggleService;
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    void getEnabledProfilesReturnsCorrectProfiles() {
+        // On this single instance that is used during the test, the profiles of that instance are equal to all available profiles
+        assertThat(profileToggleService.enabledProfiles()).containsExactlyInAnyOrder(env.getActiveProfiles());
+    }
+
+}
