af3d302f6c84dd913aea95ecbd40e8d6c43bcbac
Markus Paulsen <39456125+MarkusPaulsen@users.noreply.github.com>
2023-09-19 17:45:11 +0200

    Update src/main/java/de/tum/in/www1/artemis/service/FileService.java
    Removed FileService. from FileService.DRAG_AND_DROP_BACKGROUND_SUBPATH
    
    Co-authored-by: Paul Schwind <pal03377@users.noreply.github.com>

--- src/main/java/de/tum/in/www1/artemis/service/FileService.java
+++ src/main/java/de/tum/in/www1/artemis/service/FileService.java
@@ -383,7 +383,7 @@ public class FileService implements DisposableBean {
         if (publicPath.contains("files/temp")) {
             return Path.of(FilePathService.getTempFilePath(), filename).toString();
         }
-        if (publicPath.contains(FileService.DRAG_AND_DROP_BACKGROUND_SUBPATH)) {
+        if (publicPath.contains(DRAG_AND_DROP_BACKGROUND_SUBPATH)) {
             return Path.of(FilePathService.getDragAndDropBackgroundFilePath(), filename).toString();
         }
         if (publicPath.contains(FileService.DRAG_AND_DROP_PICTURE_SUBPATH)) {
