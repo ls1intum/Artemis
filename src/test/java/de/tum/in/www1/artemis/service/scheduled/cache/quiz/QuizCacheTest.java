89259bf357fbe964776ed9c557c38af189bfc190
lara <lara.dvorsek@gmail.com>
2023-09-19 20:57:26 +0200

    add quiz cache tests

--- src/test/java/de/tum/in/www1/artemis/service/scheduled/cache/quiz/QuizCacheTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/scheduled/cache/quiz/QuizCacheTest.java
@@ -1,15 +1,19 @@
 package de.tum.in.www1.artemis.service.scheduled.cache.quiz;
 
 import static de.tum.in.www1.artemis.service.scheduled.cache.quiz.QuizCache.HAZELCAST_CACHED_EXERCISE_UPDATE_TOPIC;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
+import java.security.Principal;
 import java.time.ZonedDateTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Isolated;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,10 +30,13 @@ import de.tum.in.www1.artemis.domain.quiz.QuizExercise;
 import de.tum.in.www1.artemis.domain.quiz.QuizSubmission;
 import de.tum.in.www1.artemis.exercise.quizexercise.QuizExerciseFactory;
 import de.tum.in.www1.artemis.exercise.quizexercise.QuizExerciseUtilService;
+import de.tum.in.www1.artemis.participation.ParticipationUtilService;
 import de.tum.in.www1.artemis.repository.QuizExerciseRepository;
+import de.tum.in.www1.artemis.repository.QuizSubmissionRepository;
 import de.tum.in.www1.artemis.service.QuizBatchService;
 import de.tum.in.www1.artemis.service.QuizExerciseService;
 import de.tum.in.www1.artemis.user.UserUtilService;
+import de.tum.in.www1.artemis.web.websocket.QuizSubmissionWebsocketService;
 
 @Isolated
 class QuizCacheTest extends AbstractSpringIntegrationIndependentTest {
@@ -56,6 +63,15 @@ class QuizCacheTest extends AbstractSpringIntegrationIndependentTest {
 
     @Autowired
     private QuizExerciseRepository quizExerciseRepository;
+
+    @Autowired
+    QuizSubmissionWebsocketService quizSubmissionWebsocketService;
+
+    @Autowired
+    QuizSubmissionRepository submissionRepository;
+
+    @Autowired
+    ParticipationUtilService participationUtilService;
 
     @BeforeEach
     void init() {
@@ -98,5 +114,65 @@ class QuizCacheTest extends AbstractSpringIntegrationIndependentTest {
 
         assertThatDb(() -> request.postWithResponseBody("/api/exercises/" + exerciseId + "/submissions/live", quizSubmission, Result.class, HttpStatus.OK))
                 .hasBeenCalledTimes(quizMode == QuizMode.SYNCHRONIZED ? 0 : 1);
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @WithMockUser(username = TEST_PREFIX + "student1", roles = "USER")
+    @CsvSource({ "true,true", "true,false", "false,true", "false,false" })
+    void testProcessSubmission(boolean submitted, boolean deleted) {
+        QuizExercise quizExercise = quizExerciseUtilService.createQuiz(ZonedDateTime.now().minusMinutes(1), null, QuizMode.SYNCHRONIZED);
+        quizExercise.duration(240);
+        quizExerciseRepository.save(quizExercise);
+
+        QuizSubmission quizSubmission = QuizExerciseFactory.generateSubmissionForThreeQuestions(quizExercise, 1, submitted, null);
+        String username = TEST_PREFIX + "student1";
+        Principal principal = () -> username;
+
+        quizSubmissionWebsocketService.saveSubmission(quizExercise.getId(), quizSubmission, principal);
+        if (deleted) {
+            quizExerciseRepository.delete(quizExercise);
+        }
+        quizScheduleService.processCachedQuizSubmissions();
+        assertThat(submissionRepository.findByParticipation_Exercise_Id(quizExercise.getId())).hasSize(submitted && !deleted ? 1 : 0);
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @WithMockUser(username = TEST_PREFIX + "student1", roles = "USER")
+    @CsvSource({ "true,true", "true,false", "false,true", "false,false" })
+    void testQuizBatchEnded(boolean quizEnded, boolean batchEnded) {
+        QuizExercise quizExercise = quizExerciseUtilService.createQuiz(ZonedDateTime.now().minusMinutes(2), quizEnded ? ZonedDateTime.now().minusMinutes(1) : null,
+                QuizMode.BATCHED);
+        quizExercise.duration(batchEnded ? 5 : 3600);
+        quizExerciseRepository.save(quizExercise);
+
+        var batch = quizBatchService.save(QuizExerciseFactory.generateQuizBatch(quizExercise, ZonedDateTime.now().minusMinutes(1)));
+        quizExerciseUtilService.joinQuizBatch(quizExercise, batch, TEST_PREFIX + "student1");
+
+        quizScheduleService.processCachedQuizSubmissions();
+
+        assertThat(submissionRepository.findByParticipation_Exercise_Id(quizExercise.getId())).hasSize(batchEnded ? 1 : 0);
+    }
+
+    @Test
+    @WithMockUser(username = TEST_PREFIX + "student1", roles = "USER")
+    void testQuizEnded() {
+        QuizExercise quizExercise = quizExerciseUtilService.createQuiz(ZonedDateTime.now().minusMinutes(1), null, QuizMode.SYNCHRONIZED);
+        quizExercise.duration(5);
+        quizExerciseRepository.save(quizExercise);
+
+        QuizSubmission quizSubmission = QuizExerciseFactory.generateSubmissionForThreeQuestions(quizExercise, 1, true, null);
+        String username = TEST_PREFIX + "student1";
+        Principal principal = () -> username;
+
+        quizSubmissionWebsocketService.saveSubmission(quizExercise.getId(), quizSubmission, principal);
+
+        quizExercise.setDueDate(ZonedDateTime.now().minusMinutes(1));
+        quizExerciseService.save(quizExercise);
+
+        quizScheduleService.processCachedQuizSubmissions();
+        // verify(websocketMessagingService, timeout(3000)).sendMessageToUser(any(), any(), any());
+        // todo: hasNewSubmissions, hasNewParticipations, hasNewResults
+
+        // assertThat(submissionRepository.findByParticipation_Exercise_Id(quizExercise.getId())).hasSize(submitted? 1 : 0);
     }
 }
