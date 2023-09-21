c63bb50ab9d05c894b56624bca1b39e5f84f71b6
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-09-21 16:59:04 +0200

    Adaptive learning: Add graph details view to learning path management dashboard (#7027)

--- src/test/javascript/spec/service/learning-path.service.spec.ts
+++ src/test/javascript/spec/service/learning-path.service.spec.ts
@@ -45,4 +45,10 @@ describe('LearningPathService', () => {
         expect(getStub).toHaveBeenCalledOnce();
         expect(getStub).toHaveBeenCalledWith('api/courses/1/learning-path-health', { observe: 'response' });
     });
+
+    it('should send a request to the server to get ngx representation of learning path', () => {
+        learningPathService.getLearningPathNgxGraph(1).subscribe();
+        expect(getStub).toHaveBeenCalledOnce();
+        expect(getStub).toHaveBeenCalledWith('api/learning-path/1/graph', { observe: 'response' });
+    });
 });
