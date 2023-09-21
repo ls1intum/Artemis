1a7e7089c96d9cddb5cb328e82e3f0a5a25a500e
Nityananda Zbil <nityananda.zbil@tum.de>
2023-09-21 12:00:35 +0200

    Migration and fix CourseCodeOfConductAgreementId member cannot be final

--- src/main/java/de/tum/in/www1/artemis/domain/CourseCodeOfConductAgreementId.java
+++ src/main/java/de/tum/in/www1/artemis/domain/CourseCodeOfConductAgreementId.java
@@ -1,17 +1,54 @@
 package de.tum.in.www1.artemis.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * The primary key for CourseCodeOfConductAgreement
- *
- * @param course the course's id
- * @param user   the user's id
  */
-record CourseCodeOfConductAgreementId(Long course, Long user) implements Serializable {
+class CourseCodeOfConductAgreementId implements Serializable {
 
-    // Needed for JPA
+    private Long course;
+
+    private Long user;
+
+    CourseCodeOfConductAgreementId(Long course, Long user) {
+        this.course = course;
+        this.user = user;
+    }
+
     CourseCodeOfConductAgreementId() {
-        this(null, null);
+        // Needed for JPA
+    }
+
+    public Long getCourse() {
+        return course;
+    }
+
+    public void setCourse(Long course) {
+        this.course = course;
+    }
+
+    public Long getUser() {
+        return user;
+    }
+
+    public void setUser(Long user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        CourseCodeOfConductAgreementId that = (CourseCodeOfConductAgreementId) o;
+        return course.equals(that.course) && user.equals(that.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course, user);
     }
 }
