89c33e6a556eafa1b4538acaebc2e0c5b74cc6d0
Dominik Remo <47261058+DominikRemo@users.noreply.github.com>
2023-09-21 21:12:28 +0200

    Added comments to ComplaintUtilService and GradingScaleUtilService

--- src/test/java/de/tum/in/www1/artemis/assessment/ComplaintUtilService.java
+++ src/test/java/de/tum/in/www1/artemis/assessment/ComplaintUtilService.java
@@ -30,6 +30,17 @@ public class ComplaintUtilService {
     @Autowired
     private UserRepository userRepo;
 
+    /**
+     * Crates and saves a complaint and response for the given result and user.
+     *
+     * @param userPrefix               The tutor's login prefix.
+     * @param j                        The current index (this method is called in a loop).
+     * @param numberOfComplaints       The max number of complaints to generate.
+     * @param numberComplaintResponses The max number of complaint responses to generate.
+     * @param typeComplaint            Whether the complaint is a complaint (true) or a more feedback request (false).
+     * @param result                   The result to generate the complaint for.
+     * @param currentUser              The user that replies to more feedback requests.
+     */
     public void generateComplaintAndResponses(String userPrefix, int j, int numberOfComplaints, int numberComplaintResponses, boolean typeComplaint, Result result,
             User currentUser) {
         result = resultRepo.save(result);
@@ -48,6 +59,13 @@ public class ComplaintUtilService {
         }
     }
 
+    /**
+     * Creates and saves an empty complaint response for the given complaint and tutor.
+     *
+     * @param loginOfTutor The tutor's login.
+     * @param complaint    The complaint to create the response for.
+     * @return The saved complaint response.
+     */
     public ComplaintResponse createInitialEmptyResponse(String loginOfTutor, Complaint complaint) {
         ComplaintResponse complaintResponse = new ComplaintResponse();
         complaintResponse.setComplaint(complaint);
@@ -57,6 +75,14 @@ public class ComplaintUtilService {
         return complaintResponse;
     }
 
+    /**
+     * Creates and saves a given number of complaints for the given participation and student.
+     *
+     * @param studentLogin       The student's login.
+     * @param participation      The participation to create the complaints for.
+     * @param numberOfComplaints The number of complaints to create.
+     * @param complaintType      The type of the complaints to create.
+     */
     public void addComplaints(String studentLogin, Participation participation, int numberOfComplaints, ComplaintType complaintType) {
         for (int i = 0; i < numberOfComplaints; i++) {
             Result dummyResult = new Result().participation(participation);
@@ -66,6 +92,13 @@ public class ComplaintUtilService {
         }
     }
 
+    /**
+     * Creates and saves a complaint for the given submission and student.
+     *
+     * @param submission The submission to create the complaint for.
+     * @param userLogin  The student's login.
+     * @param type       The type of the complaint to create.
+     */
     public void addComplaintToSubmission(Submission submission, String userLogin, ComplaintType type) {
         Result result = submission.getLatestResult();
         if (result != null) {
@@ -76,6 +109,14 @@ public class ComplaintUtilService {
         complaintRepo.save(complaint);
     }
 
+    /**
+     * Creates and saves a given number of complaints for the given team and participation.
+     *
+     * @param team               The team to create the complaints for.
+     * @param participation      The participation to create the complaints for.
+     * @param numberOfComplaints The number of complaints to create.
+     * @param complaintType      The type of the complaints to create.
+     */
     public void addTeamComplaints(Team team, Participation participation, int numberOfComplaints, ComplaintType complaintType) {
         for (int i = 0; i < numberOfComplaints; i++) {
             Result dummyResult = new Result().participation(participation);
