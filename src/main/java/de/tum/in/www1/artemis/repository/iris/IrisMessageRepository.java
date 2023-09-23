df33e82211696670a99c684467ace98b5c16d5fb
Timor Morrien <timor.morrien@tum.de>
2023-09-23 15:34:16 +0200

    Add import for `@Param`

--- src/main/java/de/tum/in/www1/artemis/repository/iris/IrisMessageRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/iris/IrisMessageRepository.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import de.tum.in.www1.artemis.domain.iris.IrisMessage;
 import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
