db41d6131720f20c78ee6d715a3a886f772b5264
Timor Morrien <timor.morrien@tum.de>
2023-09-25 13:08:07 +0200

    Only allow changing enabled and preferred model if user is admin

--- src/main/java/de/tum/in/www1/artemis/service/iris/IrisSettingsService.java
+++ src/main/java/de/tum/in/www1/artemis/service/iris/IrisSettingsService.java
@@ -21,6 +21,7 @@ import de.tum.in.www1.artemis.domain.iris.settings.IrisSubSettings;
 import de.tum.in.www1.artemis.repository.CourseRepository;
 import de.tum.in.www1.artemis.repository.ProgrammingExerciseRepository;
 import de.tum.in.www1.artemis.repository.iris.IrisSettingsRepository;
+import de.tum.in.www1.artemis.service.AuthorizationCheckService;
 import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException;
 import de.tum.in.www1.artemis.web.rest.errors.BadRequestAlertException;
 
@@ -39,12 +40,15 @@ public class IrisSettingsService {
 
     private final ProgrammingExerciseRepository programmingExerciseRepository;
 
+    private final AuthorizationCheckService authCheckService;
+
     public IrisSettingsService(CourseRepository courseRepository, ApplicationContext applicationContext, IrisSettingsRepository irisSettingsRepository,
-            ProgrammingExerciseRepository programmingExerciseRepository) {
+            ProgrammingExerciseRepository programmingExerciseRepository, AuthorizationCheckService authCheckService) {
         this.courseRepository = courseRepository;
         this.applicationContext = applicationContext;
         this.irisSettingsRepository = irisSettingsRepository;
         this.programmingExerciseRepository = programmingExerciseRepository;
+        this.authCheckService = authCheckService;
     }
 
     /**
@@ -379,8 +383,10 @@ public class IrisSettingsService {
         if (target == null || source == null) {
             return source;
         }
-        target.setEnabled(source.isEnabled());
-        target.setPreferredModel(source.getPreferredModel());
+        if (authCheckService.isAdmin()) {
+            target.setEnabled(source.isEnabled());
+            target.setPreferredModel(source.getPreferredModel());
+        }
         if (!Objects.equals(source.getTemplate(), target.getTemplate())) {
             target.setTemplate(source.getTemplate());
         }
