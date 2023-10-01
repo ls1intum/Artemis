6267994515e4b7f7eb1f322a395235ac192f9cf9
Simon Leiß <git@simon-leiss.de>
2023-10-01 02:20:50 +0200

    Add test for QuizJMSListenerService.

--- src/main/java/de/tum/in/www1/artemis/web/websocket/jms/QuizJMSListenerService.java
+++ src/main/java/de/tum/in/www1/artemis/web/websocket/jms/QuizJMSListenerService.java
@@ -3,11 +3,11 @@ package de.tum.in.www1.artemis.web.websocket.jms;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+import javax.jms.BytesMessage;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Session;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQBytesMessage;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -35,6 +35,8 @@ public class QuizJMSListenerService {
 
     private static final Pattern EXERCISE_ID_TOPIC_EXTRACTION_PATTERN = Pattern.compile("/queue/quizExercise/(\\d*)/submission");
 
+    private static final String QUEUE_PREFIX = "queue://";
+
     private final ObjectMapper objectMapper;
 
     private final QuizSubmissionService quizSubmissionService;
@@ -50,15 +52,31 @@ public class QuizJMSListenerService {
      * - spring.artemis.user<br>
      * - spring.artemis.password<br>
      *
+     * Uses a wildcard address that will listen to all submissions - only supported with an actual ActiveMQ broker.
+     * For testing, use @see {@link QuizJMSListenerService#quizSubmissionMessageListener(ConnectionFactory, String)}
+     * to specify a destination for testing.
+     *
      * @param connectionFactory the connection factory provided by Spring
      * @return the SimpleMessageListenerContainer that will be used by Spring to register the topic
      */
     @Bean
     public SimpleMessageListenerContainer quizSubmissionMessageListener(final ConnectionFactory connectionFactory) {
+        return quizSubmissionMessageListener(connectionFactory, "/queue/quizExercise/*/submission");
+    }
+
+    /**
+     * Create a SimpleMessageListenerContainer that uses the specified connectionFactory to listen on the specified
+     * destination and forward messages to the MessageListenerAdapter.
+     *
+     * @param connectionFactory the ConnectionFactory that should be used for the consumers
+     * @param destination       the destination on which the consumers should listen
+     * @return a SimpleMessageListenerContainer that forwards received messages to the MessageListenerAdapter
+     */
+    public SimpleMessageListenerContainer quizSubmissionMessageListener(final ConnectionFactory connectionFactory, String destination) {
         final SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 
         // Set the name of the queue that should be processed by the message listener
-        container.setDestinationName("/queue/quizExercise/*/submission");
+        container.setDestinationName(destination);
         // Set the message listener that will be invoked when a new message is received
         container.setMessageListener(getQuizMessageListenerAdapter());
         container.setConnectionFactory(connectionFactory);
@@ -77,16 +95,16 @@ public class QuizJMSListenerService {
             @Override
             public void onMessage(javax.jms.@NotNull Message message, Session session) throws JMSException {
                 // Message is of type ActiveMQMessage because we use Apache ActiveMQ as broker
-                ActiveMQBytesMessage activeMQMessage = (ActiveMQBytesMessage) message;
+                BytesMessage bytesMessage = (BytesMessage) message;
 
-                var messageLength = activeMQMessage.getBodyLength();
+                var messageLength = bytesMessage.getBodyLength();
                 byte[] bytes = new byte[(int) messageLength];
                 // Read message into bytes[]-array
-                activeMQMessage.readBytes(bytes);
+                bytesMessage.readBytes(bytes);
 
                 try {
                     var quizSubmission = objectMapper.readValue(bytes, QuizSubmission.class);
-                    var address = activeMQMessage.getCoreMessage().getAddress();
+                    var address = message.getJMSDestination().toString();
                     var username = JMSListenerUtils.extractUsernameFromMessage(message);
                     var exerciseId = extractExerciseIdFromAddress(address);
                     quizSubmissionService.saveSubmissionForLiveMode(exerciseId, quizSubmission, username, false);
@@ -98,7 +116,10 @@ public class QuizJMSListenerService {
                     logger.warn(String.format("Could not process Quiz Submission: %s", e));
                 }
                 catch (IOException e) {
-                    logger.warn(String.format("Could not process read JMS message: %s", e));
+                    logger.warn(String.format("Could not read JMS message: %s", e));
+                }
+                catch (Exception e) {
+                    logger.warn(String.format("Unknown error while processing quiz message: %s", e));
                 }
             }
         });
@@ -112,7 +133,12 @@ public class QuizJMSListenerService {
      * @throws JMSException if the exercise id could not be extracted
      */
     public static Long extractExerciseIdFromAddress(String address) throws JMSException {
-        var matcher = EXERCISE_ID_TOPIC_EXTRACTION_PATTERN.matcher(address);
+        var addressWithoutPrefix = address;
+        if (address.startsWith(QUEUE_PREFIX)) {
+            addressWithoutPrefix = address.replaceAll(QUEUE_PREFIX, "");
+        }
+
+        var matcher = EXERCISE_ID_TOPIC_EXTRACTION_PATTERN.matcher(addressWithoutPrefix);
         if (!matcher.matches()) {
             throw new JMSException("Could not extract exercise id");
         }
