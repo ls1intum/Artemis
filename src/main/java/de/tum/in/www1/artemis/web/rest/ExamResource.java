f6c4d2b8a3a6161290b775e2d4809f6035ab8d93
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2023-10-01 17:31:28 +0200

    Apply suggestions from code review
    Co-authored-by: Lucas Welscher <ga53foy@mytum.de>

--- src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/ExamResource.java
@@ -232,7 +232,7 @@ public class ExamResource {
 
         // NOTE: if the end date was changed, we need to update student exams and re-schedule exercises
         if (comparator.compare(originalExam.getEndDate(), savedExam.getEndDate()) != 0) {
-            int workingTimeChange = savedExam.getDuration() - originalExam.getDuration();
+            int workingTimeChange = savedExam.getDuration() - originalExamDuration;
             updateStudentExamsAndRescheduleExercises(examWithExercises, originalExamDuration, workingTimeChange);
         }
 
