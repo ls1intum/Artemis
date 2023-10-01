8e5e95f7b0175d75c6b06a675d5ec9e312b53ca0
Simon Leiß <git@simon-leiss.de>
2023-10-01 12:09:24 +0200

    Add profiles to test.

--- src/test/java/de/tum/in/www1/artemis/AbstractSpringIntegrationIndependentTest.java
+++ src/test/java/de/tum/in/www1/artemis/AbstractSpringIntegrationIndependentTest.java
@@ -35,7 +35,7 @@ import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 @ResourceLock("AbstractSpringIntegrationIndependentTest")
 @AutoConfigureEmbeddedDatabase
 // NOTE: we use a common set of active profiles to reduce the number of application launches during testing. This significantly saves time and memory!
-@ActiveProfiles({ SPRING_PROFILE_TEST, "artemis", "scheduling", "lti" })
+@ActiveProfiles({ SPRING_PROFILE_TEST, "artemis", "scheduling", "lti", "decoupling", "quiz", "lecture" })
 @TestPropertySource(properties = { "artemis.user-management.use-external=false" })
 public abstract class AbstractSpringIntegrationIndependentTest extends AbstractArtemisIntegrationTest {
 
--- src/test/java/de/tum/in/www1/artemis/web/websocket/jms/QuizJMSListenerServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/web/websocket/jms/QuizJMSListenerServiceTest.java
@@ -1,16 +1,31 @@
 package de.tum.in.www1.artemis.web.websocket.jms;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+import java.time.ZonedDateTime;
+
 import javax.jms.JMSException;
 
+import org.apache.activemq.junit.EmbeddedActiveMQBroker;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.jms.listener.SimpleMessageListenerContainer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationIndependentTest;
+import de.tum.in.www1.artemis.domain.enumeration.QuizMode;
+import de.tum.in.www1.artemis.domain.quiz.QuizExercise;
+import de.tum.in.www1.artemis.domain.quiz.QuizSubmission;
 import de.tum.in.www1.artemis.exception.QuizSubmissionException;
+import de.tum.in.www1.artemis.exercise.quizexercise.QuizExerciseFactory;
 import de.tum.in.www1.artemis.exercise.quizexercise.QuizExerciseUtilService;
 import de.tum.in.www1.artemis.repository.QuizExerciseRepository;
 import de.tum.in.www1.artemis.service.QuizSubmissionService;
@@ -26,60 +41,54 @@ class QuizJMSListenerServiceTest extends AbstractSpringIntegrationIndependentTes
     @Autowired
     QuizExerciseRepository quizExerciseRepository;
 
+    @Autowired
+    QuizJMSListenerService quizJMSListenerService;
+
     @SpyBean
     @Autowired
     QuizSubmissionService quizSubmissionService;
 
-    // Not autowired to reduce number of server starts during tests, created in @BeforeEach
-    // QuizJMSListenerService quizJMSListenerService;
-
-    // static EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
+    static EmbeddedActiveMQBroker broker = new EmbeddedActiveMQBroker();
 
     @BeforeAll
     static void startBroker() {
-        // broker.start();
+        broker.start();
     }
 
     @AfterAll
     static void stopBroker() {
-        // broker.stop();
-    }
-
-    @BeforeEach
-    void setup() {
-        // Autowiring the QuizJMSListenerService would require an additional server start during the test
-        // (because of different profiles that are used), therefor we create it manually
-        // quizJMSListenerService = new QuizJMSListenerService(objectMapper, quizSubmissionService);
+        broker.stop();
     }
 
     @Test
     void testCallsQuizSubmission() throws JsonProcessingException, QuizSubmissionException, JMSException {
-        /*
-         * QuizExercise quizExercise = quizExerciseUtilService.createQuiz(ZonedDateTime.now().minusMinutes(1), null, QuizMode.SYNCHRONIZED);
-         * quizExercise.duration(240);
-         * quizExerciseRepository.save(quizExercise);
-         * var connectionFactory = broker.createConnectionFactory();
-         * SimpleMessageListenerContainer simpleMessageListenerContainer = quizJMSListenerService.quizSubmissionMessageListener(connectionFactory,
-         * "/queue/quizExercise/" + quizExercise.getId() + "/submission");
-         * simpleMessageListenerContainer.afterPropertiesSet();
-         * simpleMessageListenerContainer.start();
-         * QuizSubmission quizSubmission = QuizExerciseFactory.generateSubmissionForThreeQuestions(quizExercise, 1, false, null);
-         * var message = broker.createBytesMessage();
-         * message.writeBytes(objectMapper.writeValueAsBytes(quizSubmission));
-         * message.setStringProperty("user-name", "user-name1");
-         * broker.pushMessage("/queue/quizExercise/" + quizExercise.getId() + "/submission", message);
-         * verify(quizSubmissionService, timeout(1000)).saveSubmissionForLiveMode(eq(quizExercise.getId()), any(QuizSubmission.class), eq("user-name1"), eq(false));
-         */
+        QuizExercise quizExercise = quizExerciseUtilService.createQuiz(ZonedDateTime.now().minusMinutes(1), null, QuizMode.SYNCHRONIZED);
+        quizExercise.duration(240);
+        quizExerciseRepository.save(quizExercise);
+
+        var connectionFactory = broker.createConnectionFactory();
+        SimpleMessageListenerContainer simpleMessageListenerContainer = quizJMSListenerService.quizSubmissionMessageListener(connectionFactory,
+                "/queue/quizExercise/" + quizExercise.getId() + "/submission");
+        simpleMessageListenerContainer.start();
+
+        QuizSubmission quizSubmission = QuizExerciseFactory.generateSubmissionForThreeQuestions(quizExercise, 1, false, null);
+
+        var message = broker.createBytesMessage();
+        message.writeBytes(objectMapper.writeValueAsBytes(quizSubmission));
+        message.setStringProperty("user-name", "user-name1");
+
+        broker.pushMessage("/queue/quizExercise/" + quizExercise.getId() + "/submission", message);
+
+        verify(quizSubmissionService, timeout(1000)).saveSubmissionForLiveMode(eq(quizExercise.getId()), any(QuizSubmission.class), eq("user-name1"), eq(false));
     }
 
     @Test
     void testExtractQuizExerciseIdFromAddress() throws JMSException {
-        /*
-         * assertThat(QuizJMSListenerService.extractExerciseIdFromAddress("/queue/quizExercise/123/submission")).isEqualTo(123);
-         * assertThat(QuizJMSListenerService.extractExerciseIdFromAddress("queue:///queue/quizExercise/123/submission")).isEqualTo(123);
-         * assertThatExceptionOfType(JMSException.class).isThrownBy(() -> QuizJMSListenerService.extractExerciseIdFromAddress("/queue/quizExercise/abc/submission"));
-         * assertThatExceptionOfType(JMSException.class).isThrownBy(() -> QuizJMSListenerService.extractExerciseIdFromAddress("/queue/some/other/address"));
-         */
+        assertThat(QuizJMSListenerService.extractExerciseIdFromAddress("/queue/quizExercise/123/submission")).isEqualTo(123);
+        assertThat(QuizJMSListenerService.extractExerciseIdFromAddress("queue:///queue/quizExercise/123/submission")).isEqualTo(123);
+
+        assertThatExceptionOfType(JMSException.class).isThrownBy(() -> QuizJMSListenerService.extractExerciseIdFromAddress("/queue/quizExercise/abc/submission"));
+        assertThatExceptionOfType(JMSException.class).isThrownBy(() -> QuizJMSListenerService.extractExerciseIdFromAddress("/queue/some/other/address"));
     }
 
 }
