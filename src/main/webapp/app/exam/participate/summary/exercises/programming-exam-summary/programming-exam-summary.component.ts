c626d8cc0f22d5d57e96b844890c46df51e670ee
Florian Glombik <florian.glombik@tum.de>
2023-10-02 11:24:11 +0200

    Programming Exercise: move content to summary component

--- src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
+++ src/main/webapp/app/exam/participate/summary/exercises/programming-exam-summary/programming-exam-summary.component.ts
@@ -2,18 +2,26 @@ import { Component, Input } from '@angular/core';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseStudentParticipation } from 'app/entities/participation/programming-exercise-student-participation.model';
 import { ProgrammingSubmission } from 'app/entities/programming-submission.model';
+import { AssessmentType } from 'app/entities/assessment-type.model';
+import { Exam } from 'app/entities/exam.model';
 
 @Component({
     selector: 'jhi-programming-exam-summary',
     templateUrl: './programming-exam-summary.component.html',
 })
 export class ProgrammingExamSummaryComponent {
-    @Input()
-    exercise: ProgrammingExercise;
+    @Input() exercise: ProgrammingExercise;
 
-    @Input()
-    participation: ProgrammingExerciseStudentParticipation;
+    @Input() participation: ProgrammingExerciseStudentParticipation;
 
-    @Input()
-    submission: ProgrammingSubmission;
+    @Input() submission: ProgrammingSubmission;
+
+    @Input() isTestRun?: boolean = false;
+
+    @Input() exam: Exam;
+
+    @Input() isAfterStudentReviewStart?: boolean = false;
+
+    protected readonly AssessmentType = AssessmentType;
+    protected readonly ProgrammingExercise = ProgrammingExercise;
 }
