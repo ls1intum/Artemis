b72953094b63baec14fb9c93d83a040925297707
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-10-05 23:42:04 +0200

    Add caching

--- src/main/webapp/app/shared/markdown-editor/commands/courseArtifactReferenceCommands/channelMentionCommand.ts
+++ src/main/webapp/app/shared/markdown-editor/commands/courseArtifactReferenceCommands/channelMentionCommand.ts
@@ -1,13 +1,14 @@
 import { InteractiveSearchCommand } from 'app/shared/markdown-editor/commands/interactiveSearchCommand';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { MetisService } from 'app/shared/metis/metis.service';
 import { ChannelDTO } from 'app/entities/metis/conversation/channel.model';
 import { ChannelService } from 'app/shared/metis/conversations/channel.service';
 
 export class ChannelMentionCommand extends InteractiveSearchCommand {
     buttonIcon = faHashtag;
+    cachedResponse: HttpResponse<ChannelDTO[]>;
 
     constructor(
         private readonly channelService: ChannelService,
@@ -20,11 +21,25 @@ export class ChannelMentionCommand extends InteractiveSearchCommand {
         return '#';
     }
 
-    performSearch(): Observable<HttpResponse<ChannelDTO[]>> {
-        return this.channelService.getChannelsOfCourse(this.metisService.getCourse().id!);
+    performSearch(searchTerm: string): Observable<HttpResponse<ChannelDTO[]>> {
+        // all channels are returned within a response. Therefore, the command can cache it
+        if (this.cachedResponse) {
+            return of(this.filterCachedResponse(searchTerm));
+        }
+        return this.channelService.getChannelsOfCourse(this.metisService.getCourse().id!).pipe(
+            map((response) => {
+                this.cachedResponse = response;
+                return this.filterCachedResponse(searchTerm);
+            }),
+        );
     }
 
     protected selectionToText(selected: ChannelDTO): string {
         return `[channel]${selected['name'] ?? 'empty'}(${selected.id})[/channel]`;
+    }
+
+    private filterCachedResponse(searchTerm: string): HttpResponse<ChannelDTO[]> {
+        const channels = this.cachedResponse.body!.filter((dto) => dto.name?.includes(searchTerm));
+        return new HttpResponse({ body: channels });
     }
 }
