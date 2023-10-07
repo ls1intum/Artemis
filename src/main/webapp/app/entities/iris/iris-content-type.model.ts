29b46abfb6bf26d39b0b4dbc13c9866779d8dd00
RY997 <liuxinyao97@gmail.com>
2023-10-07 20:10:10 +0200

    update Iris message content model

--- src/main/webapp/app/entities/iris/iris-content-type.model.ts
+++ src/main/webapp/app/entities/iris/iris-content-type.model.ts
@@ -1,8 +1,29 @@
+import { IrisExercisePlanComponent } from 'app/entities/iris/iris-exercise-plan-component.model';
+
 export enum IrisMessageContentType {
     TEXT = 'text',
+    PLAN = 'plan',
 }
 
-export class IrisMessageContent {
-    type: IrisMessageContentType.TEXT;
+export abstract class IrisMessageContent {
+    id?: number;
+    type?: IrisMessageContentType;
+    protected constructor(type: IrisMessageContentType) {
+        this.type = type;
+    }
+}
+
+export class IrisMessageTextContent extends IrisMessageContent {
     textContent: string;
+    constructor() {
+        super(IrisMessageContentType.TEXT);
+    }
+}
+
+export class IrisMessagePlanContent extends IrisMessageContent {
+    components: IrisExercisePlanComponent[];
+    currentComponentIndex: number;
+    constructor() {
+        super(IrisMessageContentType.PLAN);
+    }
 }
