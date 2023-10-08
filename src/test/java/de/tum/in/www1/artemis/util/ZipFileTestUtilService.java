2165ed6b6f62bffdc4cba122c103f6b99d1df0c6
Lucas Welscher <ga53foy@mytum.de>
2023-10-08 15:57:40 +0200

    shorten method using FileUtils

--- src/test/java/de/tum/in/www1/artemis/util/ZipFileTestUtilService.java
+++ src/test/java/de/tum/in/www1/artemis/util/ZipFileTestUtilService.java
@@ -1,14 +1,12 @@
 package de.tum.in.www1.artemis.util;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -40,25 +38,9 @@ public class ZipFileTestUtilService {
                 ZipEntry entry = zipFileEntries.nextElement();
                 String currentEntry = entry.getName();
                 File destFile = new File(newPath, currentEntry);
-                File destinationParent = destFile.getParentFile();
-
-                // create the parent directory structure if needed
-                destinationParent.mkdirs();
 
                 if (!entry.isDirectory()) {
-                    int currentByte;
-                    // establish buffer for writing file
-                    byte[] data = new byte[BUFFER];
-                    try (BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
-                            FileOutputStream fos = new FileOutputStream(destFile);
-                            BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER)) {
-
-                        // write the current file to disk
-                        // read and write until last byte is encountered
-                        while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
-                            dest.write(data, 0, currentByte);
-                        }
-                    }
+                    FileUtils.copyInputStreamToFile(zip.getInputStream(entry), destFile);
                 }
 
                 if (currentEntry.endsWith(".zip")) {
