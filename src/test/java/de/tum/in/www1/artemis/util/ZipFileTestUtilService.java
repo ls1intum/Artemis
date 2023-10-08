9bad66268fb23c8a1d702e797426eb7cb000d12a
Lucas Welscher <ga53foy@mytum.de>
2023-10-08 15:51:37 +0200

    `Development`: Fix running server tests on windows

--- src/test/java/de/tum/in/www1/artemis/util/ZipFileTestUtilService.java
+++ src/test/java/de/tum/in/www1/artemis/util/ZipFileTestUtilService.java
@@ -28,45 +28,43 @@ public class ZipFileTestUtilService {
         int BUFFER = 2048;
         File file = new File(zipFile);
 
-        ZipFile zip = new ZipFile(file);
-        String newPath = zipFile.substring(0, zipFile.length() - 4);
+        try (ZipFile zip = new ZipFile(file)) {
+            String newPath = zipFile.substring(0, zipFile.length() - 4);
 
-        new File(newPath).mkdir();
-        Enumeration<? extends ZipEntry> zipFileEntries = zip.entries();
+            new File(newPath).mkdir();
+            Enumeration<? extends ZipEntry> zipFileEntries = zip.entries();
 
-        // Process each entry
-        while (zipFileEntries.hasMoreElements()) {
-            // grab a zip file entry
-            ZipEntry entry = zipFileEntries.nextElement();
-            String currentEntry = entry.getName();
-            File destFile = new File(newPath, currentEntry);
-            File destinationParent = destFile.getParentFile();
+            // Process each entry
+            while (zipFileEntries.hasMoreElements()) {
+                // grab a zip file entry
+                ZipEntry entry = zipFileEntries.nextElement();
+                String currentEntry = entry.getName();
+                File destFile = new File(newPath, currentEntry);
+                File destinationParent = destFile.getParentFile();
 
-            // create the parent directory structure if needed
-            destinationParent.mkdirs();
+                // create the parent directory structure if needed
+                destinationParent.mkdirs();
 
-            if (!entry.isDirectory()) {
-                BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
-                int currentByte;
-                // establish buffer for writing file
-                byte[] data = new byte[BUFFER];
+                if (!entry.isDirectory()) {
+                    int currentByte;
+                    // establish buffer for writing file
+                    byte[] data = new byte[BUFFER];
+                    try (BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry));
+                            FileOutputStream fos = new FileOutputStream(destFile);
+                            BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER)) {
 
-                // write the current file to disk
-                FileOutputStream fos = new FileOutputStream(destFile);
-                BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
-
-                // read and write until last byte is encountered
-                while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
-                    dest.write(data, 0, currentByte);
+                        // write the current file to disk
+                        // read and write until last byte is encountered
+                        while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
+                            dest.write(data, 0, currentByte);
+                        }
+                    }
                 }
-                dest.flush();
-                dest.close();
-                is.close();
-            }
 
-            if (currentEntry.endsWith(".zip")) {
-                // found a zip file, try to open
-                extractZipFileRecursively(destFile.getAbsolutePath());
+                if (currentEntry.endsWith(".zip")) {
+                    // found a zip file, try to open
+                    extractZipFileRecursively(destFile.getAbsolutePath());
+                }
             }
         }
     }
