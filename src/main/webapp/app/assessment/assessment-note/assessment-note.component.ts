4e95448ad1c54478dc3e0579fb254cf6b2486ffb
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2023-10-10 15:23:39 +0200

    write existing note into textarea when loading the assessment page

--- src/main/webapp/app/assessment/assessment-note/assessment-note.component.ts
+++ src/main/webapp/app/assessment/assessment-note/assessment-note.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AssessmentNote } from 'app/entities/assessment-note.model';
 
 @Component({
@@ -6,23 +6,31 @@ import { AssessmentNote } from 'app/entities/assessment-note.model';
     templateUrl: './assessment-note.component.html',
 })
 export class AssessmentNoteComponent {
-    assessmentNote: AssessmentNote = new AssessmentNote();
+    private _assessmentNote: AssessmentNote;
     @Output() onAssessmentNoteChange = new EventEmitter<AssessmentNote>();
+
+    @Input() set assessmentNote(assessmentNote: AssessmentNote | undefined) {
+        if (assessmentNote === undefined) {
+            this._assessmentNote = new AssessmentNote();
+        } else {
+            this._assessmentNote = assessmentNote;
+        }
+    }
 
     /**
      * Called whenever an input is made on the internal tutor note text box.
      */
     onAssessmentNoteInput(event: any) {
-        this.assessmentNote.note = event.target.value;
-        this.onAssessmentNoteChange.emit(this.assessmentNote);
+        this._assessmentNote.note = event.target.value;
+        this.onAssessmentNoteChange.emit(this._assessmentNote);
     }
 
     /**
      * Return an empty string if the assessment note or its note field is undefined, or otherwise the text of the note.
      */
     getAssessmentNoteText() {
-        if (this.assessmentNote !== undefined && this.assessmentNote.note !== undefined) {
-            return this.assessmentNote.note;
+        if (this._assessmentNote !== undefined && this._assessmentNote.note !== undefined) {
+            return this._assessmentNote.note;
         } else {
             return '';
         }
