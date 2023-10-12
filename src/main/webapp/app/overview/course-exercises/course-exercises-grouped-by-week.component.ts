95b0da5abe3a38a4c0d658fe19f4345a1bd23986
Florian Glombik <florian.glombik@tum.de>
2023-10-12 21:30:27 +0200

    Fix collapsing and uncollapsing

--- src/main/webapp/app/overview/course-exercises/course-exercises-grouped-by-week.component.ts
+++ src/main/webapp/app/overview/course-exercises/course-exercises-grouped-by-week.component.ts
@@ -1,28 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Exercise } from 'app/entities/exercise.model';
-import { ExerciseWithDueDate } from 'app/overview/course-exercises/course-exercises.component';
+import { ExerciseFilter, ExerciseWithDueDate } from 'app/overview/course-exercises/course-exercises.component';
 import { Course } from 'app/entities/course.model';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { QuizExercise } from 'app/entities/quiz/quiz-exercise.model';
 
 @Component({
     selector: 'jhi-course-exercises-grouped-by-week',
     templateUrl: './course-exercises-grouped-by-week.component.html',
     styleUrls: ['../course-overview.scss'],
 })
-export class CourseExercisesGroupedByWeekComponent {
+export class CourseExercisesGroupedByWeekComponent implements OnInit, OnChanges {
     @Input() nextRelevantExercise?: ExerciseWithDueDate;
-    @Input() isVisibleToStudents: (exercise: Exercise) => boolean | undefined;
     @Input() course: Course;
     @Input() exerciseForGuidedTour?: Exercise;
     @Input() weeklyIndexKeys: string[];
-    @Input() weeklyExercisesGrouped: object;
+    @Input() immutableWeeklyExercisesGrouped: object;
+    @Input() activeFilters: Set<ExerciseFilter>;
+
+    weeklyExercisesGrouped: object;
 
     faAngleUp = faAngleUp;
     faAngleDown = faAngleDown;
 
-    toggleCollapsed(weekKey: string) {
-        if (this.weeklyExercisesGrouped[weekKey]) {
-            this.weeklyExercisesGrouped[weekKey].isCollapsed = !this.weeklyExercisesGrouped[weekKey].isCollapsed;
-        }
+    ngOnInit() {
+        this.weeklyExercisesGrouped = this.getAsMutableObject(this.immutableWeeklyExercisesGrouped);
+    }
+
+    ngOnChanges() {
+        this.weeklyExercisesGrouped = this.getAsMutableObject(this.immutableWeeklyExercisesGrouped);
+    }
+
+    /**
+     * Checks whether an exercise is visible to students or not
+     * @param exercise The exercise which should be checked
+     */
+    isVisibleToStudents(exercise: Exercise): boolean | undefined {
+        return !this.activeFilters.has(ExerciseFilter.UNRELEASED) || (exercise as QuizExercise)?.visibleToStudents;
+    }
+
+    private getAsMutableObject(object: any) {
+        return { ...object };
     }
 }
