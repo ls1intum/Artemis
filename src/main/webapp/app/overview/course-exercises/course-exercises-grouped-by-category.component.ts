aafaf72b13f8da500bf1bb662b3999188b6000d0
Florian Glombik <florian.glombik@tum.de>
2023-10-12 22:41:44 +0200

    Group exercises

--- src/main/webapp/app/overview/course-exercises/course-exercises-grouped-by-category.component.ts
+++ src/main/webapp/app/overview/course-exercises/course-exercises-grouped-by-category.component.ts
@@ -1,11 +1,68 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Exercise } from 'app/entities/exercise.model';
+import { Course } from 'app/entities/course.model';
+import dayjs from 'dayjs/esm/';
+
+type ExerciseGroup = 'current' | 'future' | 'previous' | 'noDueDate';
+
+type ExerciseGroups = Record<ExerciseGroup, Exercise[]>;
 
 @Component({
     selector: 'jhi-course-exercises-grouped-by-category',
     templateUrl: './course-exercises-grouped-by-category.component.html',
     styleUrls: ['../course-overview.scss'],
 })
-export class CourseExercisesGroupedByCategoryComponent {
+export class CourseExercisesGroupedByCategoryComponent implements OnChanges {
     @Input() filteredExercises?: Exercise[];
+    @Input() course?: Course;
+    @Input() exerciseForGuidedTour?: Exercise;
+
+    exerciseGroups: ExerciseGroups;
+
+    ngOnChanges() {
+        this.exerciseGroups = this.groupExercisesByDueDate();
+    }
+
+    private groupExercisesByDueDate(): ExerciseGroups {
+        const updatedExerciseGroups: ExerciseGroups = {
+            current: [],
+            future: [],
+            previous: [],
+            noDueDate: [],
+        };
+
+        if (!this.filteredExercises) {
+            return updatedExerciseGroups;
+        }
+
+        for (const exercise of this.filteredExercises) {
+            const exerciseGroup = this.getExerciseGroup(exercise);
+            updatedExerciseGroups[exerciseGroup].push(exercise);
+        }
+
+        return updatedExerciseGroups;
+    }
+
+    private getExerciseGroup(exercise: Exercise): ExerciseGroup {
+        if (!exercise.dueDate) {
+            return 'noDueDate';
+        }
+
+        const dueDate = dayjs(exercise.dueDate);
+        const now = dayjs();
+
+        const dueDateIsInThePast = dueDate.isBefore(now);
+        if (dueDateIsInThePast) {
+            return 'previous';
+        }
+
+        const dueDateIsWithinNextWeek = dueDate.isBefore(now.add(1, 'week'));
+        if (dueDateIsWithinNextWeek) {
+            return 'current';
+        }
+
+        return 'future';
+    }
+
+    protected readonly Object = Object;
 }
