b2bf9b8fd986202ae608b10ffd16f0aad8538030
jakubriegel <jakubriegel112@gmail.com>
2023-10-15 13:26:34 +0200

    add brackets to if

--- src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismDetectionConfig.java
+++ src/main/java/de/tum/in/www1/artemis/domain/plagiarism/PlagiarismDetectionConfig.java
@@ -94,12 +94,15 @@ public class PlagiarismDetectionConfig extends DomainObject {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
-        if (!super.equals(o))
+        }
+        if (!super.equals(o)) {
             return false;
+        }
         PlagiarismDetectionConfig that = (PlagiarismDetectionConfig) o;
         return continuousPlagiarismControlEnabled == that.continuousPlagiarismControlEnabled
                 && continuousPlagiarismControlPostDueDateChecksEnabled == that.continuousPlagiarismControlPostDueDateChecksEnabled
