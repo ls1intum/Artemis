72ed6a13f4544db2b8c95d83498f6797d0a3e368
jakubriegel <jakubriegel112@gmail.com>
2023-10-17 22:17:46 +0200

    cpc every 90s

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
@@ -70,7 +70,7 @@ public class ContinuousPlagiarismControlService {
     /**
      * Daily triggers plagiarism checks as a part of continuous plagiarism control.
      */
-    @Scheduled(initialDelay = 30_000, fixedDelay = 60_000)
+    @Scheduled(initialDelay = 30_000, fixedDelay = 90_000)
     public void executeChecks() {
         log.info("Starting continuous plagiarism control...");
 
