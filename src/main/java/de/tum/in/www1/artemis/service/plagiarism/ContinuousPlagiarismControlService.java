118a9e7391e437f79d2d301f796fb5a50f5e0e45
jakubriegel <jakubriegel112@gmail.com>
2023-10-20 00:26:43 +0200

    Catch mail exceptions to so that notification for the second student will be delivered

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/ContinuousPlagiarismControlService.java
@@ -25,6 +25,7 @@ import de.tum.in.www1.artemis.domain.plagiarism.PlagiarismComparison;
 import de.tum.in.www1.artemis.domain.plagiarism.PlagiarismResult;
 import de.tum.in.www1.artemis.domain.plagiarism.PlagiarismStatus;
 import de.tum.in.www1.artemis.domain.plagiarism.PlagiarismSubmissionElement;
+import de.tum.in.www1.artemis.exception.ArtemisMailException;
 import de.tum.in.www1.artemis.repository.ExerciseRepository;
 import de.tum.in.www1.artemis.repository.plagiarism.PlagiarismCaseRepository;
 import de.tum.in.www1.artemis.repository.plagiarism.PlagiarismComparisonRepository;
@@ -126,8 +127,15 @@ public class ContinuousPlagiarismControlService {
         var plagiarismCases = Set.of(plagiarismCaseService.createOrAddToPlagiarismCaseForStudent(comparison, comparison.getSubmissionA(), true),
                 plagiarismCaseService.createOrAddToPlagiarismCaseForStudent(comparison, comparison.getSubmissionB(), true));
 
-        plagiarismCases.stream().filter(plagiarismCase -> plagiarismCase.getPost() == null).map(ContinuousPlagiarismControlService::buildCpcPost)
-                .forEach(postService::createContinuousPlagiarismControlPlagiarismCasePost);
+        plagiarismCases.stream().filter(plagiarismCase -> plagiarismCase.getPost() == null).map(ContinuousPlagiarismControlService::buildCpcPost).forEach(post -> {
+            try {
+                postService.createContinuousPlagiarismControlPlagiarismCasePost(post);
+            }
+            catch (ArtemisMailException e) {
+                // Catch mail exceptions to so that notification for the second student will be delivered
+                log.error("Cannot send a cpc email: postId={}, plagiarismCaseId={}.", post.getId(), post.getPlagiarismCase().getId());
+            }
+        });
     }
 
     private static Post buildCpcPost(PlagiarismCase plagiarismCase) {
