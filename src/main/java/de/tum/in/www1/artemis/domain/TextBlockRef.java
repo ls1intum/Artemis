23990534713fd7e5ac3db50cc2c8dc90b74bdc31
Paul Schwind <git@pschwind.de>
2023-10-25 23:08:30 +0200

    Remove unused file

--- src/main/java/de/tum/in/www1/artemis/domain/TextBlockRef.java
+++
@@ -1,7 +0,0 @@
-package de.tum.in.www1.artemis.domain;
-
-/**
- * A TextBlockRef. It is a combining class for TextBlock and Feedback.
- */
-public record TextBlockRef(TextBlock block, Feedback feedback) {
-}
