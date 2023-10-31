71531faa72877c387b3b8b94b21ef841baa44cc8
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-10-31 10:49:58 +0100

    add test and comment

--- src/test/java/de/tum/in/www1/artemis/lecture/CompetencyIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/lecture/CompetencyIntegrationTest.java
@@ -20,6 +20,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.util.LinkedMultiValueMap;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationLocalCILocalVCTest;
+import de.tum.in.www1.artemis.competency.CompetencyProgressUtilService;
 import de.tum.in.www1.artemis.competency.CompetencyUtilService;
 import de.tum.in.www1.artemis.course.CourseUtilService;
 import de.tum.in.www1.artemis.domain.*;
@@ -103,6 +104,12 @@ class CompetencyIntegrationTest extends AbstractSpringIntegrationLocalCILocalVCT
     @Autowired
     private PageableSearchUtilService pageableSearchUtilService;
 
+    @Autowired
+    private CompetencyProgressUtilService competencyProgressUtilService;
+
+    @Autowired
+    private LectureUtilService lectureUtilService;
+
     private Course course;
 
     private Course course2;
@@ -141,6 +148,16 @@ class CompetencyIntegrationTest extends AbstractSpringIntegrationLocalCILocalVCT
         teamTextExercise = createTextExercise(pastTimestamp, pastTimestamp, pastTimestamp, Set.of(competency), true);
 
         creatingLectureUnitsOfLecture(competency);
+
+        User student1 = userUtilService.getUserByLogin(TEST_PREFIX + "student1");
+        competencyProgressUtilService.createCompetencyProgress(competency, student1, 0, 0);
+
+        User student2 = userUtilService.getUserByLogin(TEST_PREFIX + "student2");
+        competencyProgressUtilService.createCompetencyProgress(competency, student2, 0, 0);
+
+        final var textUnit = textUnitRepository.findById(idOfTextUnitOfLectureOne).get();
+        lectureUtilService.completeLectureUnitForUser(textUnit, student2);
+
     }
 
     private Competency createCompetency() {
@@ -282,6 +299,20 @@ class CompetencyIntegrationTest extends AbstractSpringIntegrationLocalCILocalVCT
     void getCompetency_asStudent_shouldReturnCompetency() throws Exception {
         Competency competency = request.get("/api/courses/" + course.getId() + "/competencies/" + this.competency.getId(), HttpStatus.OK, Competency.class);
         assertThat(competency.getId()).isEqualTo(this.competency.getId());
+    }
+
+    @Test
+    @WithMockUser(username = TEST_PREFIX + "student1", roles = "USER")
+    void getCompetency_shouldOnlySendUserSpecificData() throws Exception {
+
+        Competency competency = request.get("/api/courses/" + course.getId() + "/competencies/" + this.competency.getId(), HttpStatus.OK, Competency.class);
+        assertThat(competency.getId()).isEqualTo(this.competency.getId());
+
+        // only progress of student1 is fetched
+        assertThat(competency.getUserProgress()).hasSize(1);
+
+        // only student2 has completed the textUnit
+        assertThat(competency.getLectureUnits().stream().findFirst().get().getCompletedUsers()).isEmpty();
     }
 
     @Test
