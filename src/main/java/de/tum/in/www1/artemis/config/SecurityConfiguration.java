eb0eb06dff2d2c785cab21437fe1dd3f66e2af4e
Julian Christl <julian.christl@tum.de>
2023-11-03 18:19:59 +0100

    fix SecurityConfiguration

--- src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
+++ src/main/java/de/tum/in/www1/artemis/config/SecurityConfiguration.java
@@ -1,14 +1,13 @@
 package de.tum.in.www1.artemis.config;
 
-import static de.tum.in.www1.artemis.config.Constants.*;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -24,11 +23,13 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CorsFilter;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import de.tum.in.www1.artemis.config.lti.CustomLti13Configurer;
 import de.tum.in.www1.artemis.security.Role;
@@ -41,6 +42,7 @@ import jakarta.annotation.PostConstruct;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
+@Import(SecurityProblemSupport.class)
 public class SecurityConfiguration {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
@@ -53,16 +55,26 @@ public class SecurityConfiguration {
 
     private final Optional<AuthenticationProvider> remoteUserAuthenticationProvider;
 
+    private final CorsFilter corsFilter;
+
+    private final SecurityProblemSupport problemSupport;
+
+    private final Environment env;
+
     @Value("#{'${spring.prometheus.monitoringIp:127.0.0.1}'.split(',')}")
     private List<String> monitoringIpAddresses;
 
     public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService, TokenProvider tokenProvider,
-            PasswordService passwordService, Optional<AuthenticationProvider> remoteUserAuthenticationProvider) {
+            PasswordService passwordService, Optional<AuthenticationProvider> remoteUserAuthenticationProvider, CorsFilter corsFilter, SecurityProblemSupport problemSupport,
+            Environment env) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.userDetailsService = userDetailsService;
         this.tokenProvider = tokenProvider;
         this.passwordService = passwordService;
         this.remoteUserAuthenticationProvider = remoteUserAuthenticationProvider;
+        this.corsFilter = corsFilter;
+        this.problemSupport = problemSupport;
+        this.env = env;
     }
 
     /**
@@ -103,74 +115,41 @@ public class SecurityConfiguration {
         return roleHierarchy;
     }
 
-    /**
-     * Only allow the configured IP addresses to access the prometheus endpoint
-     *
-     * @return an access check like "hasIpAddress('127.0.0.1') or hasIpAddress('::1')" that can be used as argument for
-     *         {@link org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl#access(String)}}
-     */
-    private String getMonitoringAccessDefinition() {
-        return monitoringIpAddresses.stream().map(ip -> String.format("hasIpAddress(\"%s\")", ip)).collect(Collectors.joining(" or "));
-    }
-
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // @formatter:off
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(handler -> handler.authenticationEntryPoint(problemSupport).accessDeniedHandler(problemSupport))
             .addFilterAfter(new SpaWebFilter(), BasicAuthenticationFilter.class)
             .headers(headers -> headers
                 .contentSecurityPolicy(csp -> csp.policyDirectives("script-src 'self' 'unsafe-inline' 'unsafe-eval'"))
-                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
                 .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN))
                 .httpStrictTransportSecurity((HeadersConfigurer.HstsConfig::disable)) // this is already configured using nginx
                 .permissionsPolicy(permissions -> permissions.policy("camera=(), fullscreen=(*), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()")))
-            .authorizeHttpRequests(auth -> auth
-                // options: TODO: do we really need this?
-                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                // api
-                .requestMatchers("/api/admin/**").hasAuthority(Role.ADMIN.getAuthority())
-                .requestMatchers("/api/public/**").permitAll()
-                // TODO: Remove the following three lines in June 2024 together with LegacyResource
-                .requestMatchers(HttpMethod.POST, "/api/programming-exercises/new-result").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/programming-submissions/*").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/programming-exercises/test-cases-changed/*").permitAll()
-                .requestMatchers("/api/**").authenticated()
-                .requestMatchers("/api/public/**").permitAll()
-                // websockets
-                .requestMatchers("/websocket/**").permitAll()
-                // management
-                .requestMatchers("/management/health").permitAll()
-                .requestMatchers("/management/info").permitAll()
-                // Only allow the configured IP address to access the prometheus endpoint, or allow 127.0.0.1 if none is specified
-                .requestMatchers("/management/prometheus/**").access((authentication, context) ->
-                    new AuthorizationDecision(context.getRequest().getRemoteAddr().equals(getMonitoringAccessDefinition())))
-                .requestMatchers("/management/**").hasAuthority(Role.ADMIN.getAuthority())
-                .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
-                // others
-                .requestMatchers("/time").permitAll()
-                .requestMatchers("/", "/index.html", "/*.js", "/*.map", "/*.css").permitAll()
-                .requestMatchers("/*.ico", "/*.png", "/*.svg", "/*.webapp").permitAll()
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("/logo/*.svg", "/logo/*.png", "/logo/*.ico", "/logo/browserconfig.xml").permitAll()    // for favicons and logos
-                .requestMatchers("/ngsw.json").permitAll()    // for the service worker
-                .requestMatchers("/app/**").permitAll() //TODO: this might not work for us, because we don't use the app prefix
-                .requestMatchers("/i18n/**").permitAll()
-                .requestMatchers("/content/**").permitAll()
-                .requestMatchers("/api.html").permitAll()
-                .requestMatchers("/api-docs/**").permitAll()
-                .requestMatchers(CustomLti13Configurer.LTI13_LOGIN_PATH).permitAll())
-            //        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)    //TODO: we use JWTFilter, etc. at the moment, but we should switch to this solution
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .exceptionHandling(exceptions -> exceptions
-                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers(new AntPathRequestMatcher("/api/admin/**")).hasAuthority(Role.ADMIN.getAuthority())
+                .requestMatchers(new AntPathRequestMatcher("/api/public/**")).permitAll()
+                // TODO: Remove the following three lines in June 2024 together with LegacyResource
+                .requestMatchers(new AntPathRequestMatcher(HttpMethod.POST.name(), "/api/programming-exercises/new-result")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher(HttpMethod.POST.name(), "/api/programming-submissions/*")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher(HttpMethod.POST.name(), "/api/programming-exercises/test-cases-changed/*")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/websocket/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/.well-known/jwks.json")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/management/prometheus/**")).access((authentication, context) ->
+                    new AuthorizationDecision(monitoringIpAddresses.contains(context.getRequest().getRemoteAddr())))
+                .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
             )
             .apply(securityConfigurerAdapter());
-
-        // TODO: currently disabled because it is not fully working yet
-        //        http.apply(new CustomLti13Configurer());
         // @formatter:on
+
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains("lti")) {
+            http.apply(new CustomLti13Configurer());
+        }
 
         return http.build();
     }
