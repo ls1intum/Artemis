495682aa6ce68a89a4897d13236c7663ad4f0c41
Lucas Welscher <ga53foy@mytum.de>
2023-11-04 16:46:55 +0100

    use style classes

--- src/main/webapp/app/shared/components/documentation-button/documentation-button.component.ts
+++ src/main/webapp/app/shared/components/documentation-button/documentation-button.component.ts
@@ -32,7 +32,7 @@ export type DocumentationType = keyof typeof DocumentationLinks;
     selector: 'jhi-documentation-button',
     styleUrls: ['./documentation-button.component.scss'],
     template: `
-        <a class="text-primary documentation-button" href="{{ baseUrl + DocumentationLinks[this.type] }}">
+        <a class="text-primary documentation-button ms-1 mb-1" href="{{ baseUrl + DocumentationLinks[this.type] }}">
             <fa-icon [icon]="faCircleInfo" ngbTooltip="{{ getTooltipForType() }}"></fa-icon>
         </a>
     `,
