f4d13e3e1202b6fe039674fe8ce9cf6017e1a4de
Lucas Welscher <ga53foy@mytum.de>
2023-11-06 07:57:09 +0100

    Development: Refactor documentation buttons (#7502)

--- src/main/webapp/app/course/competencies/competency-management/competency-management.component.ts
+++ src/main/webapp/app/course/competencies/competency-management/competency-management.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { CompetencyService } from 'app/course/competencies/competency.service';
 import { AlertService } from 'app/core/util/alert.service';
 import { Competency, CompetencyRelation, CompetencyRelationError, CourseCompetencyProgress, getIcon, getIconTooltip } from 'app/entities/competency.model';
@@ -11,9 +11,8 @@ import { faPencilAlt, faPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PrerequisiteImportComponent } from 'app/course/competencies/competency-management/prerequisite-import.component';
 import { ClusterNode, Edge, Node } from '@swimlane/ngx-graph';
-import { AccountService } from 'app/core/auth/account.service';
-import { DocumentationType } from 'app/shared/components/documentation-button/documentation-button.component';
 import { CompetencyImportComponent } from 'app/course/competencies/competency-management/competency-import.component';
+import { DocumentationType } from 'app/shared/components/documentation-button/documentation-button.component';
 
 @Component({
     selector: 'jhi-competency-management',
@@ -38,12 +37,11 @@ export class CompetencyManagementComponent implements OnInit, OnDestroy {
     private dialogErrorSource = new Subject<string>();
     dialogError$ = this.dialogErrorSource.asObservable();
 
-    documentationType = DocumentationType.Competencies;
-
     update$: Subject<boolean> = new Subject<boolean>();
 
-    getIcon = getIcon;
-    getIconTooltip = getIconTooltip;
+    readonly getIcon = getIcon;
+    readonly getIconTooltip = getIconTooltip;
+    readonly documentationType: DocumentationType = 'Competencies';
 
     // Icons
     faPlus = faPlus;
@@ -52,8 +50,6 @@ export class CompetencyManagementComponent implements OnInit, OnDestroy {
 
     constructor(
         private activatedRoute: ActivatedRoute,
-        private router: Router,
-        private accountService: AccountService,
         private competencyService: CompetencyService,
         private alertService: AlertService,
         private modalService: NgbModal,
