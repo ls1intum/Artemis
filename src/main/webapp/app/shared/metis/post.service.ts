082ca3e18acd48ddd3d54576f062a72aaa7a4353
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-11-07 13:31:25 +0100

    Add comment

--- src/main/webapp/app/shared/metis/post.service.ts
+++ src/main/webapp/app/shared/metis/post.service.ts
@@ -166,6 +166,15 @@ export class PostService extends PostingService<Post> {
         return res;
     }
 
+    /**
+     * Determines whether to use the /messages or /posts API endpoints based on the presence of certain properties in the postContextFilter or post objects.
+     * If any properties related to conversations are present (conversation, conversationId, or courseWideChannelIds), it returns /messages.
+     * Otherwise, it defaults to /posts.
+     *
+     * @param postContextFilter current post context filter in use
+     * @param post new or updated post
+     * @return '/messages' or '/posts'
+     */
     private static getResourceEndpoint(postContextFilter?: PostContextFilter, post?: Post): string {
         if (post?.conversation || postContextFilter?.conversationId || postContextFilter?.courseWideChannelIds) {
             return '/messages';
