831496590f91cf1faf08b3a78545696bd77b391d
Lucas Welscher <ga53foy@mytum.de>
2023-11-08 19:51:29 +0100

    `Programming exercises`: Fix missing test names during manual assessment

--- src/main/java/de/tum/in/www1/artemis/web/rest/LongFeedbackTextResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/LongFeedbackTextResource.java
@@ -2,6 +2,7 @@ package de.tum.in.www1.artemis.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,13 +38,13 @@ public class LongFeedbackTextResource {
      */
     @GetMapping("results/{resultId}/feedbacks/{feedbackId}/long-feedback")
     @EnforceAtLeastStudent
-    public ResponseEntity<LongFeedbackText> getLongFeedback(@PathVariable Long resultId, @PathVariable Long feedbackId) {
+    public ResponseEntity<String> getLongFeedback(@PathVariable Long resultId, @PathVariable Long feedbackId) {
         log.debug("REST request to get long feedback: {} (result: {})", feedbackId, resultId);
 
         final LongFeedbackText longFeedbackText = longFeedbackTextRepository.findByFeedbackIdWithFeedbackAndResultAndParticipationElseThrow(feedbackId);
         checkCanAccessResultElseThrow(resultId, longFeedbackText);
 
-        return ResponseEntity.ok(longFeedbackText);
+        return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).body(longFeedbackText.getText());
     }
 
     private void checkCanAccessResultElseThrow(final Long resultId, final LongFeedbackText longFeedbackText) {
