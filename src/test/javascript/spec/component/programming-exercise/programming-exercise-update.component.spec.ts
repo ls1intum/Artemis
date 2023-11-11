5e5ab4ec1d033ca4369dce5f7a90d14eea838537
Jonathan Ostertag <jo.ostertag@tum.de>
2023-11-11 09:05:36 +0100

    chore: adjust tests

--- src/test/javascript/spec/component/programming-exercise/programming-exercise-update.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/programming-exercise-update.component.spec.ts
@@ -415,11 +415,7 @@ describe('ProgrammingExercise Management Update Component', () => {
             expect(comp.isImportFromFile).toBeFalse();
             expect(comp.isImportFromExistingExercise).toBeTrue();
 
-            verifyImport();
-
-            // name and short name should also be imported
-            expect(comp.programmingExercise.title).toEqual(programmingExercise.title);
-            expect(comp.programmingExercise.shortName).toEqual(programmingExercise.shortName);
+            verifyImport(programmingExercise);
         }));
 
         it.each([
@@ -504,11 +500,7 @@ describe('ProgrammingExercise Management Update Component', () => {
             expect(comp.isImportFromFile).toBeTrue();
             expect(comp.isImportFromExistingExercise).toBeFalse();
 
-            verifyImport();
-
-            // name and short name should be imported
-            expect(comp.programmingExercise.title).toBe('title');
-            expect(comp.programmingExercise.shortName).toBe('shortName');
+            verifyImport(programmingExercise);
         }));
 
         it('should call import-from-file from service on import for entity from file', fakeAsync(() => {
@@ -913,7 +905,7 @@ describe('ProgrammingExercise Management Update Component', () => {
         expect(comp.exerciseCategories).toBe(categories);
     }));
 
-    function verifyImport() {
+    function verifyImport(importedProgrammingExercise: ProgrammingExercise) {
         expect(comp.programmingExercise.projectKey).toBeUndefined();
         expect(comp.programmingExercise.id).toBeUndefined();
         expect(comp.programmingExercise.dueDate).toBeUndefined();
@@ -932,6 +924,9 @@ describe('ProgrammingExercise Management Update Component', () => {
         // allow manual feedback requests and complaints for automatic assessments should be set to false because we reset all dates and hence they can only be false
         expect(comp.programmingExercise.allowManualFeedbackRequests).toBeFalse();
         expect(comp.programmingExercise.allowComplaintsForAutomaticAssessments).toBeFalse();
+        // name and short name should also be imported
+        expect(comp.programmingExercise.title).toEqual(importedProgrammingExercise.title);
+        expect(comp.programmingExercise.shortName).toEqual(importedProgrammingExercise.shortName);
     }
 });
 
