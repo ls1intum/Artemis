036c9d208543d75ed6524692ca3799dbdee846d3
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2023-11-15 15:20:04 +0100

    load assessment notes for scores overview

--- src/main/java/de/tum/in/www1/artemis/repository/StudentParticipationRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/StudentParticipationRepository.java
@@ -194,6 +194,7 @@ public interface StudentParticipationRepository extends JpaRepository<StudentPar
                 LEFT JOIN FETCH p.results r
                 LEFT JOIN FETCH r.submission s
                 LEFT JOIN FETCH p.submissions
+                LEFT JOIN FETCH r.assessmentNote
             WHERE p.exercise.id = :exerciseId
                 AND (r.id = (SELECT max(p_r.id) FROM p.results p_r)
                     OR r.assessmentType <> 'AUTOMATIC'
@@ -207,6 +208,7 @@ public interface StudentParticipationRepository extends JpaRepository<StudentPar
                 LEFT JOIN FETCH p.results r
                 LEFT JOIN FETCH r.submission s
                 LEFT JOIN FETCH p.submissions
+                LEFT JOIN FETCH r.assessmentNote
             WHERE p.exercise.id = :exerciseId
                 AND (r.id = (SELECT max(p_r.id) FROM p.results p_r WHERE p_r.rated = true)
                     OR r.assessmentType <> 'AUTOMATIC'
