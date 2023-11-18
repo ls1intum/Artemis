e10b67fa4bf4f90e0837df7f4f966e0ea39120e5
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2023-11-18 12:39:02 +0100

    allow custom build scripts

--- src/main/java/de/tum/in/www1/artemis/service/connectors/aeolus/Windfile.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/aeolus/Windfile.java
@@ -1,5 +1,6 @@
 package de.tum.in.www1.artemis.service.connectors.aeolus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -49,5 +50,15 @@ public class Windfile {
             this.metadata = new WindfileMetadata();
         }
         this.metadata.setId(id);
+    }
+
+    public List<ScriptAction> getScriptActions() {
+        List<ScriptAction> scriptActions = new ArrayList<>();
+        for (Action action : actions) {
+            if (action instanceof ScriptAction) {
+                scriptActions.add((ScriptAction) action);
+            }
+        }
+        return scriptActions;
     }
 }
