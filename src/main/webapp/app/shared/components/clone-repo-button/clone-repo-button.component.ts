d1b9c5aa15a574e424c8048888dd18b683bf0c54
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2023-11-18 13:57:20 +0100

    update code doc

--- src/main/webapp/app/shared/components/clone-repo-button/clone-repo-button.component.ts
+++ src/main/webapp/app/shared/components/clone-repo-button/clone-repo-button.component.ts
@@ -105,10 +105,10 @@ export class CloneRepoButtonComponent implements OnInit, OnChanges {
     /**
      * Called when the user clicks on the clone repo button. Sometimes the VCS access token is not present in the user
      * object, so the resulting repo URL is wrong. This method tries to retrieve the token from the server by requesting
-     * the user object again. This should usually be enough, since this scenario only occurs when starting an exercise
-     * directly after the first time a new user registers via SAML2. In this case Artemis has validated that the
-     * authentication is correct and lets the user log in, but further actions happen in the background that populate
-     * the missing data.
+     * the user object again.
+     * This case only happens when a user is created before VCS access tokens are enabled. For those users, a token is
+     * created the first time they start participating in a programming exercise. In that case, however, they do not
+     * have an access token yet, because during login, there was no token available.
      *
      * In case the token could not be retrieved, this method is not called again and the student
      * is prompted to reload the page. Otherwise, the error would be displayed every time the button is clicked,
