24755dcdc6f3f25af9735402f9e97a6dbb030c9e
Andreas Resch <andreas@resch.io>
2023-11-21 15:46:43 +0100

    fix tests

--- src/test/java/de/tum/in/www1/artemis/connectors/AeolusTemplateResourceTest.java
+++ src/test/java/de/tum/in/www1/artemis/connectors/AeolusTemplateResourceTest.java
@@ -2,11 +2,17 @@ package de.tum.in.www1.artemis.connectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationIndependentTest;
 import de.tum.in.www1.artemis.service.connectors.aeolus.*;
@@ -31,15 +37,33 @@ class AeolusTemplateResourceTest extends AbstractSpringIntegrationIndependentTes
     @Test
     @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
     void testGetAeolusTemplateFile() throws Exception {
-        String javaDefaultAeolusTemplate = request.get("/api/aeolus/templates/JAVA/PLAIN_GRADLE", HttpStatus.OK, String.class);
-        assertThat(javaDefaultAeolusTemplate).isNotEmpty();
-        String javaSequentialAeolusTemplate = request.get("/api/aeolus/templates/JAVA/PLAIN_GRADLE?sequentialRuns=true", HttpStatus.OK, String.class);
-        assertThat(javaSequentialAeolusTemplate).isNotEmpty();
-        String javaNormalAeolusTemplate = request.get("/api/aeolus/templates/JAVA/PLAIN_MAVEN", HttpStatus.OK, String.class);
-        assertThat(javaNormalAeolusTemplate).isNotEmpty();
-        javaSequentialAeolusTemplate = request.get("/api/aeolus/templates/JAVA/PLAIN_MAVEN?sequentialRuns=true", HttpStatus.OK, String.class);
-        assertThat(javaSequentialAeolusTemplate).isNotEmpty();
-        String pythonTemplate = request.get("/api/aeolus/templates/PYTHON/", HttpStatus.OK, String.class);
-        assertThat(pythonTemplate).isNotEmpty();
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Action.class, new ActionDeserializer());
+        Gson gson = builder.create();
+        Map<String, Integer> templatesWithExpectedScriptActions = new HashMap<>();
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_GRADLE", 2);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_GRADLE?sequentialRuns=true", 3);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_GRADLE?staticAnalysis=true", 3);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_GRADLE?staticAnalysis=true&testCoverage=true", 3);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_MAVEN", 1);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_MAVEN?sequentialRuns=true", 2);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_MAVEN?testCoverage=true", 2);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_MAVEN?staticAnalysis=true", 2);
+        templatesWithExpectedScriptActions.put("JAVA/PLAIN_MAVEN?staticAnalysis=true&testCoverage=true", 3);
+        templatesWithExpectedScriptActions.put("PYTHON", 1);
+        templatesWithExpectedScriptActions.put("PYTHON?sequentialRuns=true", 3);
+        for (Map.Entry<String, Integer> entry : templatesWithExpectedScriptActions.entrySet()) {
+            String template = request.get("/api/aeolus/templates/" + entry.getKey(), HttpStatus.OK, String.class);
+            assertThat(template).isNotEmpty();
+            Windfile windfile = gson.fromJson(template, Windfile.class);
+            this.assertWindfileIsCorrect(windfile, entry.getValue());
+        }
+    }
+
+    void assertWindfileIsCorrect(Windfile windfile, long expectedScriptActions) {
+        assertThat(windfile.getApi()).isEqualTo("v0.0.1");
+        assertThat(windfile.getMetadata().getGitCredentials()).isNull();
+        assertThat(windfile.getMetadata().getDocker()).isNotNull();
+        assertThat(windfile.getActions().stream().filter(action -> action instanceof ScriptAction).count()).isEqualTo(expectedScriptActions);
     }
 }
