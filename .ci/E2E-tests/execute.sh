2b1c8cb112fa948fdae897048a386fe36a483d60
Murad Talibov <muradtalibov2@gmail.com>
2023-11-22 21:48:56 +0100

    Adds initial ci/cd scripts for playwright

--- .ci/E2E-tests/execute.sh
+++ .ci/E2E-tests/execute.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 DB=$1
+TEST_FRAMEWORK=$2
 
-if [ "$DB" = "mysql" ]; then
+if [ "$DB" = "mysql" && "$TEST_FRAMEWORK" = "playwright" ]; then
+  COMPOSE_FILE="playwright-E2E-tests-mysql.yml"
+elif [ "$DB" = "mysql" ]; then
   COMPOSE_FILE="cypress-E2E-tests-mysql.yml"
 elif [ "$DB" = "postgres" ]; then
   COMPOSE_FILE="cypress-E2E-tests-postgres.yml"
@@ -20,9 +23,17 @@ export HOST_HOSTNAME=$(hostname)
 
 cd docker
 #just pull everything else than artemis-app as we build it later either way
-docker compose -f $COMPOSE_FILE pull artemis-cypress $DB nginx
-docker compose -f $COMPOSE_FILE build --build-arg WAR_FILE_STAGE=external_builder --no-cache --pull artemis-app
-docker compose -f $COMPOSE_FILE up --exit-code-from artemis-cypress
+if [ "$TEST_FRAMEWORK" = "playwright" ]; then
+  echo "Building for playwright"
+  docker compose -f $COMPOSE_FILE pull artemis-playwright $DB nginx
+  docker compose -f $COMPOSE_FILE build --build-arg WAR_FILE_STAGE=external_builder --no-cache --pull artemis-app
+  docker compose -f $COMPOSE_FILE up --exit-code-from artemis-playwright
+else
+  echo "Building for cypress"
+  docker compose -f $COMPOSE_FILE pull artemis-cypress $DB nginx
+  docker compose -f $COMPOSE_FILE build --build-arg WAR_FILE_STAGE=external_builder --no-cache --pull artemis-app
+  docker compose -f $COMPOSE_FILE up --exit-code-from artemis-cypress
+fi
 exitCode=$?
 cd ..
 echo "Cypress container exit code: $exitCode"
