68adde1cd3d248a26a8c8753fb125a7aaaea03f0
Stephan Krusche <krusche@in.tum.de>
2023-11-25 11:02:17 +0100

    fix directory for browser files

--- src/main/java/de/tum/in/www1/artemis/config/WebConfigurer.java
+++ src/main/java/de/tum/in/www1/artemis/config/WebConfigurer.java
@@ -85,9 +85,9 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
 
     private void setLocationForStaticAssets(WebServerFactory server) {
         if (server instanceof ConfigurableServletWebServerFactory servletWebServer) {
-            File root;
             String prefixPath = resolvePathPrefix();
-            root = new File(prefixPath + "build/resources/main/static/");
+            // Note: since Angular 17, the path apparently changed
+            File root = new File(prefixPath + "build/resources/main/static/browser");
             if (root.exists() && root.isDirectory()) {
                 servletWebServer.setDocumentRoot(root);
             }
