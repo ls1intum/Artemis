23cb545ec10aa617459e2d4f38db71043580ff11
Florian Glombik <florian.glombik@selectcode.de>
2023-11-26 05:07:31 +0100

    Revert changes in utils

--- src/main/webapp/app/exercises/shared/result/result.utils.ts
+++ src/main/webapp/app/exercises/shared/result/result.utils.ts
@@ -200,7 +200,7 @@ export const isOnlyCompilationTested = (result: Result | undefined, templateStat
  * @return {string} the css class
  */
 export const getTextColorClass = (result: Result | undefined, templateStatus: ResultTemplateStatus) => {
-    if (result === undefined) {
+    if (!result) {
         return 'text-secondary';
     }
 
