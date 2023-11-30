dbaacf998f020b66a80680cffd2d7c0ab82480d9
Murad Talibov <muradtalibov2@gmail.com>
2023-11-30 03:51:02 +0100

    Update config baseURL and reporter

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['junit', { outputFile: './test-reports/results.xml' }]],
+    reporter: process.env.CI ? [['junit', { outputFile: './test-reports/results.xml' }]] : [['list'], ['html']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:9000',
+        baseURL: process.env.CI ? 'https://artemis-nginx' : 'http://localhost:9000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
