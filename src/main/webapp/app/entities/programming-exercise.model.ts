61cd4451fb8be10bcbf692f833b694e33b285b69
Andreas Resch <andreas@resch.io>
2023-11-30 22:04:48 +0100

    add results

--- src/main/webapp/app/entities/programming-exercise.model.ts
+++ src/main/webapp/app/entities/programming-exercise.model.ts
@@ -13,19 +13,24 @@ import { BuildLogStatisticsDTO } from 'app/exercises/programming/manage/build-lo
 export class BuildAction {
     name: string;
     runAlways: boolean;
-    class: string;
     workdir: string;
+    results?: AeolusResult[];
+    parameters: Map<string, string | boolean | number>;
+}
+
+export class AeolusResult {
+    name: string;
+    path: string;
+    ignore: string;
+    type?: string;
 }
 
 export class ScriptAction extends BuildAction {
     script: string;
-    class: string = 'script-action';
 }
 
 export class PlatformAction extends BuildAction {
     type: string;
-    class: string = 'platform-action';
-    parameters: Map<string, string | boolean | number>;
     kind: string;
 }
 
