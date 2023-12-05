3e60ccbbaf07d098b14ff942b92bfeb9e673a39e
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2023-12-05 19:15:13 +0100

    use gitservice for clone and checkout

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIBuildJobExecutionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIBuildJobExecutionService.java
@@ -1,12 +1,11 @@
 package de.tum.in.www1.artemis.service.connectors.localci;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +17,8 @@ import javax.xml.stream.XMLStreamReader;
 
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,15 +30,14 @@ import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.exception.NotFoundException;
 
 import de.tum.in.www1.artemis.config.localvcci.LocalCIConfiguration;
-import de.tum.in.www1.artemis.domain.AuxiliaryRepository;
-import de.tum.in.www1.artemis.domain.BuildLogEntry;
-import de.tum.in.www1.artemis.domain.ProgrammingExercise;
+import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.enumeration.ProgrammingLanguage;
 import de.tum.in.www1.artemis.domain.enumeration.ProjectType;
 import de.tum.in.www1.artemis.domain.participation.ProgrammingExerciseParticipation;
 import de.tum.in.www1.artemis.exception.LocalCIException;
 import de.tum.in.www1.artemis.exception.localvc.LocalVCInternalException;
 import de.tum.in.www1.artemis.repository.AuxiliaryRepositoryRepository;
+import de.tum.in.www1.artemis.service.connectors.GitService;
 import de.tum.in.www1.artemis.service.connectors.ci.ContinuousIntegrationService;
 import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildResult;
 import de.tum.in.www1.artemis.service.connectors.localvc.LocalVCRepositoryUrl;
@@ -75,6 +68,8 @@ public class LocalCIBuildJobExecutionService {
      */
     private final XMLInputFactory localCIXMLInputFactory;
 
+    private final GitService gitService;
+
     @Value("${artemis.version-control.url}")
     private URL localVCBaseUrl;
 
@@ -82,12 +77,14 @@ public class LocalCIBuildJobExecutionService {
     private String repoClonePath;
 
     public LocalCIBuildJobExecutionService(LocalCIBuildPlanService localCIBuildPlanService, Optional<VersionControlService> versionControlService,
-            LocalCIContainerService localCIContainerService, AuxiliaryRepositoryRepository auxiliaryRepositoryRepository, XMLInputFactory localCIXMLInputFactory) {
+            LocalCIContainerService localCIContainerService, AuxiliaryRepositoryRepository auxiliaryRepositoryRepository, XMLInputFactory localCIXMLInputFactory,
+            GitService gitService) {
         this.localCIBuildPlanService = localCIBuildPlanService;
         this.versionControlService = versionControlService;
         this.localCIContainerService = localCIContainerService;
         this.auxiliaryRepositoryRepository = auxiliaryRepositoryRepository;
         this.localCIXMLInputFactory = localCIXMLInputFactory;
+        this.gitService = gitService;
     }
 
     public enum LocalCIBuildJobRepositoryType {
@@ -180,8 +177,8 @@ public class LocalCIBuildJobExecutionService {
         }
 
         if (commitHash != null) {
-            String cloneRepoPath = createCloneToCheckoutCommit(assignmentRepositoryPath.toString(), commitHash, branch);
-            assignmentRepositoryPath = Path.of(cloneRepoPath);
+            // Clone the assignment repository into a temporary directory with the name of the commit hash and then checkout the commit hash.
+            assignmentRepositoryPath = cloneAndCheckoutRepository(participation, commitHash);
         }
 
         // Create the container from the "ls1tum/artemis-maven-template" image with the local paths to the Git repositories and the shell script bound to it. Also give the
@@ -267,7 +264,7 @@ public class LocalCIBuildJobExecutionService {
 
             // Delete cloned repository
             if (commitHash != null) {
-                deleteCloneRepo(commitHash);
+                deleteCloneRepo(participation, commitHash);
             }
         }
 
@@ -288,7 +285,6 @@ public class LocalCIBuildJobExecutionService {
         // Set the build status to "INACTIVE" to indicate that the build is not running anymore.
         localCIBuildPlanService.updateBuildPlanStatus(participation, ContinuousIntegrationService.BuildStatus.INACTIVE);
 
-        // TODO: why is commitHash null here?
         log.info("Building and testing submission for repository {} and commit hash {} took {}", participation.getRepositoryUrl(), commitHash,
                 TimeLogUtil.formatDurationFrom(timeNanoStart));
 
@@ -483,52 +479,28 @@ public class LocalCIBuildJobExecutionService {
         return new LocalCIBuildResult(assignmentRepoBranchName, assignmentRepoCommitHash, testsRepoCommitHash, isBuildSuccessful, buildRunDate, List.of(job));
     }
 
-    private String createCloneToCheckoutCommit(String sourceRepoPath, String commitHash, String branch) {
-        Path cloneRepoBasePath = Path.of("checked-out-repos");
-
-        if (!Files.exists(cloneRepoBasePath)) {
-            try {
-                Files.createDirectory(cloneRepoBasePath);
-            }
-            catch (IOException e) {
-                throw new LocalCIException("Failed to create directory for temporary cloned repositories", e);
-            }
-        }
-
-        String cloneRepoPath = cloneRepoBasePath.resolve(commitHash).toString();
-
-        // Clone the repository to a temporary folder
+    private Path cloneAndCheckoutRepository(ProgrammingExerciseParticipation participation, String commitHash) {
         try {
-            CloneCommand cloneCommand = Git.cloneRepository().setURI(sourceRepoPath).setDirectory(new File(cloneRepoPath)).setBranch(branch).setCloneAllBranches(false);
-            try (Git git = cloneCommand.call()) {
-                log.info("Cloned repository {} to {}", sourceRepoPath, cloneRepoPath);
-            }
+            // Clone the assignment repository into a temporary directory with the name of the commit hash and then checkout the commit hash.
+            Repository repository = gitService.getOrCheckoutRepository(participation.getVcsRepositoryUrl(), Paths.get("checked-out-repos", commitHash), false);
+            gitService.checkoutRepositoryAtCommit(repository, commitHash);
+            return repository.getLocalPath();
         }
         catch (GitAPIException e) {
-            log.error("Error while cloning repository {} to {}", sourceRepoPath, cloneRepoPath, e);
+            throw new LocalCIException("Error while cloning repository", e);
         }
-
-        try (Repository clonedRepository = new FileRepositoryBuilder().setGitDir(new File(cloneRepoPath + "/.git")).build(); Git git = new Git(clonedRepository)) {
-
-            git.checkout().setName(commitHash).call();
-            log.info("Checked out commit {} of repository {}", commitHash, cloneRepoPath);
-        }
-        catch (IOException | GitAPIException e) {
-            log.error("Error while checking out commit {} of repository {}", commitHash, cloneRepoPath, e);
-        }
-
-        return cloneRepoPath;
     }
 
-    private void deleteCloneRepo(String commitHash) {
-
-        Path cloneRepoBasePath = Path.of("checked-out-repos");
-        Path cloneRepoPath = cloneRepoBasePath.resolve(commitHash).toAbsolutePath();
+    private void deleteCloneRepo(ProgrammingExerciseParticipation participation, String commitHash) {
         try {
-            FileUtils.deleteDirectory(cloneRepoPath.toFile());
+            Repository repository = gitService.getOrCheckoutRepository(participation.getVcsRepositoryUrl(), Paths.get("checked-out-repos", commitHash), false);
+            gitService.deleteLocalRepository(repository);
+        }
+        catch (GitAPIException e) {
+            throw new LocalCIException("Error while checking out repository", e);
         }
         catch (IOException e) {
-            throw new LocalCIException("Failed to delete cloned Repo", e);
+            throw new RuntimeException("Error while deleting repository", e);
         }
     }
 }
