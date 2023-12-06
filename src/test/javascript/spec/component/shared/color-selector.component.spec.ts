106946f4a809adf7f5e80f496701ac2856fb9ef4
Jonathan Ostertag <jo.ostertag@tum.de>
2023-12-06 18:00:15 +0100

    chore: add test

--- src/test/javascript/spec/component/shared/color-selector.component.spec.ts
+++ src/test/javascript/spec/component/shared/color-selector.component.spec.ts
@@ -34,6 +34,15 @@ describe('ColorSelectorComponent', () => {
         expect(addEventListenerSpy).toHaveBeenCalledOnce();
     });
 
+    it('should stop event propagation on openSelector', () => {
+        const event = { stopPropagation: jest.fn(), target: { closest: jest.fn() } };
+        const eventStopPropagationSpy = jest.spyOn(event, 'stopPropagation');
+        component.colorSelectorPosition = { left: 0, top: 0 };
+        component.openColorSelector(event as unknown as MouseEvent, 0, 0);
+
+        expect(eventStopPropagationSpy).toHaveBeenCalledOnce();
+    });
+
     it('should position the color selector after opening correctly', () => {
         const target = document.createElement('div');
         const event = {
