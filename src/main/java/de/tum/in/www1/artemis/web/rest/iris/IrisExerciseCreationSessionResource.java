3db8834cc43da8b677e6e18c9059aed0d2ea7695
Michael Dyer <michaelowendyer@gmail.com>
2023-12-07 15:11:45 +0100

    Update endpoints

--- src/main/java/de/tum/in/www1/artemis/web/rest/iris/IrisExerciseCreationSessionResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/iris/IrisExerciseCreationSessionResource.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import de.tum.in.www1.artemis.domain.Course;
-import de.tum.in.www1.artemis.domain.iris.session.IrisChatSession;
 import de.tum.in.www1.artemis.domain.iris.session.IrisExerciseCreationSession;
 import de.tum.in.www1.artemis.repository.CourseRepository;
 import de.tum.in.www1.artemis.repository.UserRepository;
@@ -27,7 +26,7 @@ import de.tum.in.www1.artemis.service.iris.settings.IrisSettingsService;
 import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenAlertException;
 
 /**
- * REST controller for managing {@link IrisChatSession}.
+ * REST controller for managing {@link IrisExerciseCreationSession}.
  */
 @RestController
 @Profile("iris")
@@ -61,12 +60,13 @@ public class IrisExerciseCreationSessionResource {
     }
 
     /**
-     * GET courses/{courseId}/sessions/current: Retrieve the current iris session for the course.
+     * GET courses/{courseId}/sessions/current: Retrieve the current iris exercise creation session for the course.
      *
      * @param courseId of the course
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the current iris session for the course or {@code 404 (Not Found)} if no session exists
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the current iris exercise creation session for the course or {@code 404 (Not Found)} if no
+     *         session exists
      */
-    @GetMapping("courses/{courseId}/sessions/exercise-creations-current")
+    @GetMapping("courses/{courseId}/exercise-creation-sessions/current")
     @EnforceAtLeastEditor
     public ResponseEntity<IrisExerciseCreationSession> getCurrentSession(@PathVariable Long courseId) {
         Course course = courseRepository.findByIdElseThrow(courseId);
@@ -82,12 +82,12 @@ public class IrisExerciseCreationSessionResource {
     }
 
     /**
-     * GET courses/{courseId}/sessions: Retrieve all Iris Sessions for the programming exercise
+     * GET courses/{courseId}/sessions: Retrieve all IrisExerciseCreationSessions for the course.
      *
-     * @param courseId of the exercise
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body a list of the iris sessions for the exercise or {@code 404 (Not Found)} if no session exists
+     * @param courseId of the course
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body a list of the iris sessions for the course or {@code 404 (Not Found)} if no session exists
      */
-    @GetMapping("courses/{courseId}/exercise-creations-sessions")
+    @GetMapping("courses/{courseId}/exercise-creation-sessions")
     @EnforceAtLeastEditor
     public ResponseEntity<List<IrisExerciseCreationSession>> getAllSessions(@PathVariable Long courseId) {
         Course course = courseRepository.findByIdElseThrow(courseId);
@@ -103,14 +103,14 @@ public class IrisExerciseCreationSessionResource {
     }
 
     /**
-     * POST programming-exercises/{exerciseId}/session: Create a new iris session for an exercise and user.
-     * If there already exists an iris session for the exercise and user, a new one is created.
+     * POST courses/{courseId}/exercise-creation-sessions: Create a new exercise creation session for a course and user.
+     * If there already exists an iris session for the course and user, a new one is created.
      * Note: The old session including messages is not deleted and can still be retrieved
      *
-     * @param courseId of the exercise
-     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the new iris session for the exercise
+     * @param courseId of the course
+     * @return the {@link ResponseEntity} with status {@code 200 (Ok)} and with body the new iris session for the course
      */
-    @PostMapping("courses/{courseId}/exercise-creations-sessions")
+    @PostMapping("courses/{courseId}/exercise-creation-sessions")
     @EnforceAtLeastEditor
     public ResponseEntity<IrisExerciseCreationSession> createSessionForCourse(@PathVariable Long courseId) throws URISyntaxException {
         Course course = courseRepository.findByIdElseThrow(courseId);
@@ -127,7 +127,7 @@ public class IrisExerciseCreationSessionResource {
     }
 
     /**
-     * GET iris/sessions/{sessionId}/active: Retrieve if Iris is active for a session
+     * GET exercise-creations-sessions/{sessionId}/active: Retrieve if Iris is active for a session
      * This checks if the used model is healthy.
      *
      * @param sessionId id of the session
