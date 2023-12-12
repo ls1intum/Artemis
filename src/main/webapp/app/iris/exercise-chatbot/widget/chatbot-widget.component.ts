d313bee0f48f4bebc94fa9a00e1ef3a76e45c4ac
Kaan Çaylı <y.k.cayli@gmail.com>
2023-12-12 18:56:30 +0100

    Check if paramsMap is iterable

--- src/main/webapp/app/iris/exercise-chatbot/widget/chatbot-widget.component.ts
+++ src/main/webapp/app/iris/exercise-chatbot/widget/chatbot-widget.component.ts
@@ -879,7 +879,11 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
 
     getConvertedErrorMap() {
         if (this.error?.paramsMap) {
-            return Object.fromEntries(this.error.paramsMap);
+            // Check if paramsMap is iterable.
+            if (typeof this.error?.paramsMap[Symbol.iterator] === 'function') {
+                return Object.fromEntries(this.error.paramsMap);
+            }
+            return this.error.paramsMap;
         }
         return null;
     }
