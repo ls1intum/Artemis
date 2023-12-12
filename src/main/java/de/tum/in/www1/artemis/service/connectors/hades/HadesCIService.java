81fb580e72898507c76689043dbb827f22d0f1e7
Matthias Linhuber <matthias@linhuber.org>
2023-12-12 17:24:13 +0100

    Add debug info to HadesCIService

--- src/main/java/de/tum/in/www1/artemis/service/connectors/hades/HadesCIService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/hades/HadesCIService.java
@@ -31,19 +31,15 @@ public class HadesCIService extends AbstractContinuousIntegrationService {
     public HadesCIService(ProgrammingSubmissionRepository programmingSubmissionRepository, FeedbackRepository feedbackRepository, BuildLogEntryService buildLogService,
             BuildLogStatisticsEntryRepository buildLogStatisticsEntryRepository, TestwiseCoverageService testwiseCoverageService) {
         super(programmingSubmissionRepository, feedbackRepository, buildLogService, buildLogStatisticsEntryRepository, testwiseCoverageService);
-
-        log.debug("HadesCIService created");
     }
 
     @Override
     public void createBuildPlanForExercise(ProgrammingExercise exercise, String planKey, VcsRepositoryUrl repositoryURL, VcsRepositoryUrl testRepositoryURL,
             VcsRepositoryUrl solutionRepositoryURL) {
-
     }
 
     @Override
     public void recreateBuildPlansForExercise(ProgrammingExercise exercise) {
-
     }
 
     @Override
@@ -54,26 +50,25 @@ public class HadesCIService extends AbstractContinuousIntegrationService {
 
     @Override
     public void configureBuildPlan(ProgrammingExerciseParticipation participation, String branch) {
-
     }
 
     @Override
     public void deleteProject(String projectKey) {
-
     }
 
     @Override
     public void deleteBuildPlan(String projectKey, String buildPlanId) {
-
     }
 
     @Override
     public String getPlanKey(Object requestBody) throws ContinuousIntegrationException {
+        log.warn("Hades does not support plan key retrieval");
         return null;
     }
 
     @Override
     public BuildStatus getBuildStatus(ProgrammingExerciseParticipation participation) {
+        log.warn("Hades does not support build status retrieval");
         return null;
     }
 
@@ -84,48 +79,50 @@ public class HadesCIService extends AbstractContinuousIntegrationService {
 
     @Override
     public ResponseEntity<byte[]> retrieveLatestArtifact(ProgrammingExerciseParticipation participation) {
+        log.debug("Hades does not support artifact retrieval");
         return null;
     }
 
     @Override
     public String checkIfProjectExists(String projectKey, String projectName) {
+        log.debug("Hades does not support project existence checks");
         return null;
     }
 
     @Override
     public void enablePlan(String projectKey, String planKey) {
-
     }
 
     @Override
     public void updatePlanRepository(String buildProjectKey, String buildPlanKey, String ciRepoName, String repoProjectKey, String newRepoUrl, String existingRepoUrl,
             String newBranch) {
-
+        log.debug("Hades does not support plan repository updates");
     }
 
     @Override
     public void giveProjectPermissions(String projectKey, List<String> groups, List<CIPermission> permissions) {
-
+        log.debug("Hades does not support project permissions");
     }
 
     @Override
     public void givePlanPermissions(ProgrammingExercise programmingExercise, String planName) {
-
+        log.debug("Hades does not support plan permissions");
     }
 
     @Override
     public void removeAllDefaultProjectPermissions(String projectKey) {
-
+        log.debug("Hades does not support project permissions");
     }
 
     @Override
     public ConnectorHealth health() {
+        // TODO: implement Health check
         return null;
     }
 
     @Override
     public void createProjectForExercise(ProgrammingExercise programmingExercise) throws ContinuousIntegrationException {
-
+        log.debug("Hades does not support project creation");
     }
 
     @Override
