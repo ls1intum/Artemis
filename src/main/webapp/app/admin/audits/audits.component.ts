6e1ae96d1a26b99fb1bfcda89bf851b3bee50dfb
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2023-12-13 12:36:36 +0000

    Cleanup control flow in some files

--- src/main/webapp/app/admin/audits/audits.component.ts
+++ src/main/webapp/app/admin/audits/audits.component.ts
@@ -43,12 +43,12 @@ export class AuditsComponent implements OnInit {
         this.handleNavigation();
     }
 
-    canLoad(): boolean {
+    get canLoad(): boolean {
         return this.fromDate !== '' && this.toDate !== '';
     }
 
     transition(): void {
-        if (this.canLoad()) {
+        if (this.canLoad) {
             this.router.navigate(['/admin/audits'], {
                 queryParams: {
                     page: this.page,
