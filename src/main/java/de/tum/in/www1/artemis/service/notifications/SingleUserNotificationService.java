fe4a11c48394b47748cf80c47b676f595ac6a8a5
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2023-12-14 13:56:18 +0100

    Add missing return in java doc

--- src/main/java/de/tum/in/www1/artemis/service/notifications/SingleUserNotificationService.java
+++ src/main/java/de/tum/in/www1/artemis/service/notifications/SingleUserNotificationService.java
@@ -369,6 +369,7 @@ public class SingleUserNotificationService {
      * @param answerMessage the answerMessage of the user involved
      * @param author        the author of the message reply
      * @param conversation  conversation the message of the reply belongs to
+     * @return notification
      */
     public SingleUserNotification createNotificationAboutNewMessageReply(AnswerPost answerMessage, User author, Conversation conversation) {
         User authorWithHiddenData = new User(author.getId(), null, author.getFirstName(), author.getLastName(), null, null);
