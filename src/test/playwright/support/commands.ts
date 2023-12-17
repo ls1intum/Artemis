ef064e2b8319f004860e642527ba5a77f2063d57
Murad Talibov <muradtalibov2@gmail.com>
2023-12-17 17:44:32 +0100

    Switch to http requests through playwright

--- src/test/playwright/support/commands.ts
+++ src/test/playwright/support/commands.ts
@@ -1,8 +1,6 @@
 import { UserCredentials } from './users';
 import { BASE_API } from '../../cypress/support/constants';
 import { Page, expect } from '@playwright/test';
-import * as https from 'https';
-import fs from 'fs';
 
 export class Commands {
     static login = async (page: Page, credentials: UserCredentials, url?: string): Promise<void> => {
@@ -13,93 +11,121 @@ export class Commands {
             .cookies()
             .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
         if (!jwtCookie) {
-            await new Promise((resolve, reject) => {
-                const fullUrl = new URL(`${process.env.baseURL}/${BASE_API}public/authenticate`);
-                console.log('Login URL:', fullUrl);
-
-                const reqData = JSON.stringify({
+            const response = await page.request.post(BASE_API + 'public/authenticate', {
+                data: {
                     username,
                     password,
                     rememberMe: true,
-                });
-
-                console.log('Request Data:', reqData);
-                const req = https.request(
-                    {
-                        hostname: fullUrl.hostname,
-                        port: fullUrl.port.length != 0 ? fullUrl.port : null,
-                        path: `/${BASE_API}public/authenticate`,
-                        method: 'POST',
-                        agent: new https.Agent({
-                            ca: fs.readFileSync('./certs/rootCA.pem'),
-                            cert: fs.readFileSync('./certs/artemis-nginx+4.pem'),
-                            key: fs.readFileSync('./certs/artemis-nginx+4-key.pem'),
-                        }),
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'User-Agent': 'Playwright',
-                        },
-                    },
-                    (res) => {
-                        let data = '',
-                            jwtCookie = '';
-                        res.on('data', (chunk) => (data += chunk));
-                        res.on('end', async () => {
-                            console.log('Response:', data);
-                            expect(res.statusCode).toBe(200);
-                            console.log('Status code' + res.statusCode);
-                            const setCookieHeader = res.headers['set-cookie'];
-                            if (setCookieHeader) {
-                                setCookieHeader.forEach((cookie) => {
-                                    if (cookie.startsWith('jwt=')) {
-                                        jwtCookie = cookie.split(';')[0];
-                                    }
-                                });
-                            }
-
-                            if (jwtCookie) {
-                                console.log('Manually setting cookie: ' + jwtCookie);
-                                const [name, value] = jwtCookie.split('=');
-                                console.log('Manually setting cookie: ' + name + ' ' + value);
-                                await page.context().addCookies([
-                                    {
-                                        name,
-                                        value,
-                                        url: `${process.env.baseURL}/`,
-                                    },
-                                ]);
-                            }
-                            resolve(data);
-                        });
-                    },
-                );
-
-                req.on('error', (e) => {
-                    console.error(`Request error: ${e.message}`);
-                    reject(e);
-                });
-                req.write(reqData);
-                req.end();
+                },
+                failOnStatusCode: false,
             });
 
-            console.log('Checking cookies...');
+            expect(response.status()).toBe(200);
 
             const newJwtCookie = await page
                 .context()
                 .cookies()
                 .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
-
-            console.log('Cookie: ' + newJwtCookie);
-            console.log('Token is defined');
-            if (!newJwtCookie) {
-                throw new Error('Login failed: JWT cookie not found after login');
-            }
+            expect(newJwtCookie).not.toBeNull();
         }
-
-        console.log('Navigating to page...');
 
         if (url) {
             await page.goto(url);
         }
+        // const { username, password } = credentials;
+        //
+        // const jwtCookie = await page
+        //     .context()
+        //     .cookies()
+        //     .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
+        // if (!jwtCookie) {
+        //     await new Promise((resolve, reject) => {
+        //         const fullUrl = new URL(`${process.env.baseURL}/${BASE_API}public/authenticate`);
+        //         console.log('Login URL:', fullUrl);
+        //
+        //         const reqData = JSON.stringify({
+        //             username,
+        //             password,
+        //             rememberMe: true,
+        //         });
+        //
+        //         console.log('Request Data:', reqData);
+        //         const req = https.request(
+        //             {
+        //                 hostname: fullUrl.hostname,
+        //                 port: fullUrl.port.length != 0 ? fullUrl.port : null,
+        //                 path: `/${BASE_API}public/authenticate`,
+        //                 method: 'POST',
+        //                 agent: new https.Agent({
+        //                     ca: fs.readFileSync('./certs/rootCA.pem'),
+        //                     cert: fs.readFileSync('./certs/artemis-nginx+4.pem'),
+        //                     key: fs.readFileSync('./certs/artemis-nginx+4-key.pem'),
+        //                 }),
+        //                 headers: {
+        //                     'Content-Type': 'application/json',
+        //                     'User-Agent': 'Playwright',
+        //                 },
+        //             },
+        //             (res) => {
+        //                 let data = '',
+        //                     jwtCookie = '';
+        //                 res.on('data', (chunk) => (data += chunk));
+        //                 res.on('end', async () => {
+        //                     console.log('Response:', data);
+        //                     expect(res.statusCode).toBe(200);
+        //                     console.log('Status code' + res.statusCode);
+        //                     const setCookieHeader = res.headers['set-cookie'];
+        //                     if (setCookieHeader) {
+        //                         setCookieHeader.forEach((cookie) => {
+        //                             if (cookie.startsWith('jwt=')) {
+        //                                 jwtCookie = cookie.split(';')[0];
+        //                             }
+        //                         });
+        //                     }
+        //
+        //                     if (jwtCookie) {
+        //                         console.log('Manually setting cookie: ' + jwtCookie);
+        //                         const [name, value] = jwtCookie.split('=');
+        //                         console.log('Manually setting cookie: ' + name + ' ' + value);
+        //                         await page.context().addCookies([
+        //                             {
+        //                                 name,
+        //                                 value,
+        //                                 url: `${process.env.baseURL}/`,
+        //                             },
+        //                         ]);
+        //                     }
+        //                     resolve(data);
+        //                 });
+        //             },
+        //         );
+        //
+        //         req.on('error', (e) => {
+        //             console.error(`Request error: ${e.message}`);
+        //             reject(e);
+        //         });
+        //         req.write(reqData);
+        //         req.end();
+        //     });
+        //
+        //     console.log('Checking cookies...');
+        //
+        //     const newJwtCookie = await page
+        //         .context()
+        //         .cookies()
+        //         .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
+        //
+        //     console.log('Cookie: ' + newJwtCookie);
+        //     console.log('Token is defined');
+        //     if (!newJwtCookie) {
+        //         throw new Error('Login failed: JWT cookie not found after login');
+        //     }
+        // }
+        //
+        // console.log('Navigating to page...');
+        //
+        // if (url) {
+        //     await page.goto(url);
+        // }
     };
 }
