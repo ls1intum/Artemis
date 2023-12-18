9ae4d5723c5f6082d1aff3fb948746a479882a1d
Murad Talibov <muradtalibov2@gmail.com>
2023-12-18 04:19:30 +0100

    Turns off parallel execution + more logs

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -14,7 +14,7 @@ dotenv.config({ path: `${__dirname}/playwright.env` });
 export default defineConfig({
     testDir: './',
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
