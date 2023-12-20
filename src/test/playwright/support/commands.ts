cd8a757fc8887e2fcd7a6ea6ca2dd9b0ace353be
Murad Talibov <muradtalibov2@gmail.com>
2023-12-20 19:04:40 +0100

    Use axios + https agent for requests with certs

--- src/test/playwright/support/commands.ts
+++ src/test/playwright/support/commands.ts
@@ -1,37 +1,96 @@
 import { UserCredentials } from './users';
 import { BASE_API } from '../../cypress/support/constants';
 import { Page, expect } from '@playwright/test';
+import axiosBase, { AxiosResponse } from 'axios';
+import * as https from 'https';
+import * as fs from 'fs';
+
+function parseCookie(setCookieString: string) {
+    const parts = setCookieString.split(';').map((p) => p.trim());
+    const pair = parts[0].split('=');
+    return {
+        name: pair[0],
+        value: pair[1],
+        url: `${process.env.baseURL}/`,
+    };
+}
 
 export class Commands {
+    private static axios = axiosBase.create({
+        httpsAgent: new https.Agent({
+            ca: fs.readFileSync('./certs/rootCA.pem'),
+            cert: fs.readFileSync('./certs/artemis-nginx+4.pem'),
+            key: fs.readFileSync('./certs/artemis-nginx+4-key.pem'),
+            // rejectUnauthorized: false
+        }),
+    });
+
     static login = async (page: Page, credentials: UserCredentials, url?: string): Promise<void> => {
+        // axios implementation
         const { username, password } = credentials;
 
+        // Check for existing JWT cookie
         const jwtCookie = await page
             .context()
             .cookies()
             .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
         if (!jwtCookie) {
-            const response = await page.request.post(BASE_API + 'public/authenticate', {
-                data: {
-                    username,
-                    password,
-                    rememberMe: true,
-                },
-                failOnStatusCode: false,
+            // Making a POST request using Axios
+            const response: AxiosResponse = await this.axios.post(`${process.env.baseURL}/${BASE_API}public/authenticate`, {
+                username,
+                password,
+                rememberMe: true,
             });
 
-            expect(response.status()).toBe(200);
+            // Asserting that the response status is 200
+            expect(response.status).toBe(200);
 
-            const newJwtCookie = await page
-                .context()
-                .cookies()
-                .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
-            expect(newJwtCookie).not.toBeNull();
+            // Extracting set-cookie header and asserting its presence
+            const setCookieHeader = response.headers['set-cookie'];
+            expect(setCookieHeader).toBeDefined();
+
+            // Parsing and adding cookies to Playwright context
+            if (setCookieHeader) {
+                const cookies = setCookieHeader.map(parseCookie);
+                await page.context().addCookies(cookies);
+            }
         }
 
         if (url) {
             await page.goto(url);
         }
+
+        // plawright apirequestcontext implementation
+        // const { username, password } = credentials;
+        //
+        // const jwtCookie = await page
+        //     .context()
+        //     .cookies()
+        //     .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
+        // if (!jwtCookie) {
+        //     const response = await page.request.post(BASE_API + 'public/authenticate', {
+        //         data: {
+        //             username,
+        //             password,
+        //             rememberMe: true,
+        //         },
+        //         failOnStatusCode: false,
+        //     });
+        //
+        //     expect(response.status()).toBe(200);
+        //
+        //     const newJwtCookie = await page
+        //         .context()
+        //         .cookies()
+        //         .then((cookies) => cookies.find((cookie) => cookie.name === 'jwt'));
+        //     expect(newJwtCookie).not.toBeNull();
+        // }
+        //
+        // if (url) {
+        //     await page.goto(url);
+        // }
+
+        // node.js https implementation
         // const { username, password } = credentials;
         //
         // const jwtCookie = await page
