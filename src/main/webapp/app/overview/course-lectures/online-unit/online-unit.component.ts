ac80920f6dc83688e23dad181bcc735270bbef82
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-12-24 14:03:18 +0100

    log online unit

--- src/main/webapp/app/overview/course-lectures/online-unit/online-unit.component.ts
+++ src/main/webapp/app/overview/course-lectures/online-unit/online-unit.component.ts
@@ -3,13 +3,16 @@ import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 import { faLink, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { OnlineUnit } from 'app/entities/lecture-unit/onlineUnit.model';
 import { LectureUnitCompletionEvent } from 'app/overview/course-lectures/course-lecture-details.component';
+import { AbstractScienceComponent } from 'app/shared/science/science.component';
+import { ScienceService } from 'app/shared/science/science.service';
+import { ScienceEventType } from 'app/shared/science/science.model';
 
 @Component({
     selector: 'jhi-online-unit',
     templateUrl: './online-unit.component.html',
     styleUrls: ['../lecture-unit.component.scss'],
 })
-export class OnlineUnitComponent {
+export class OnlineUnitComponent extends AbstractScienceComponent {
     @Input() onlineUnit: OnlineUnit;
     @Input() isPresentationMode = false;
     @Output() onCompletion: EventEmitter<LectureUnitCompletionEvent> = new EventEmitter();
@@ -22,7 +25,9 @@ export class OnlineUnitComponent {
     faSquare = faSquare;
     faSquareCheck = faSquareCheck;
 
-    constructor() {}
+    constructor(scienceService: ScienceService) {
+        super(scienceService, ScienceEventType.LECTURE__ONLINE_UNIT_OPEN);
+    }
 
     handleCollapse(event: Event) {
         event.stopPropagation();
@@ -36,6 +41,13 @@ export class OnlineUnitComponent {
 
     openLink(event: Event) {
         event.stopPropagation();
+
+        // log event
+        if (this.onlineUnit?.id) {
+            this.setResourceId(this.onlineUnit.id);
+        }
+        this.logEvent();
+
         if (this.onlineUnit?.source) {
             window.open(this.onlineUnit.source, '_blank');
             this.onCompletion.emit({ lectureUnit: this.onlineUnit, completed: true });
