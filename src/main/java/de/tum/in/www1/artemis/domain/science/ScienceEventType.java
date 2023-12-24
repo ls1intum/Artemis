3742e35cd6138994c2bdb44b1f552ef8e1c52cb9
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2023-12-24 13:46:01 +0100

    fix file unit enum name

--- src/main/java/de/tum/in/www1/artemis/domain/science/ScienceEventType.java
+++ src/main/java/de/tum/in/www1/artemis/domain/science/ScienceEventType.java
@@ -6,5 +6,5 @@ package de.tum.in.www1.artemis.domain.science;
  * Important: Please follow the naming convention <category>__<detailed event name>
  */
 public enum ScienceEventType {
-    LECTURE__OPEN, LECTURE__TEXT_UNIT_OPEN, LECTURE__VIDEO_UNIT_OPEN, LECTURE__FILE_UNIT_OPEN, LECTURE__ONLINE_UNIT_OPEN
+    LECTURE__OPEN, LECTURE__TEXT_UNIT_OPEN, LECTURE__VIDEO_UNIT_OPEN, LECTURE__FILE_UNIT_DOWNLOAD, LECTURE__ONLINE_UNIT_OPEN
 }
