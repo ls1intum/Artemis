a19c59a0e49619c222862166ae637ce142bfe241
Mateus Messias Mendes <ge59rib@mytum.de>
2023-12-30 15:25:14 +0100

    add websocket for build queue representation

---
+++ src/main/java/de/tum/in/www1/artemis/web/websocket/localci/LocalCIBuildQueueWebsocketService.java
@@ -0,0 +1,69 @@
+package de.tum.in.www1.artemis.web.websocket.localci;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import de.tum.in.www1.artemis.security.Role;
+import de.tum.in.www1.artemis.service.WebsocketMessagingService;
+import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildAgentInformation;
+import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildJobQueueItem;
+
+@Service
+@Profile("localci")
+public class LocalCIBuildQueueWebsocketService {
+
+    private final Logger log = LoggerFactory.getLogger(LocalCIBuildQueueWebsocketService.class);
+
+    private final WebsocketMessagingService websocketMessagingService;
+
+    public LocalCIBuildQueueWebsocketService(WebsocketMessagingService websocketMessagingService) {
+        this.websocketMessagingService = websocketMessagingService;
+    }
+
+    public void sendQueuedBuildJobsForCourse(long courseId, List<LocalCIBuildJobQueueItem> buildJobQueue) {
+        String channel = "/topic/build-job-queue/queued/" + courseId;
+        log.debug("Sending message on topic {}: {}", channel, buildJobQueue);
+        websocketMessagingService.sendMessage(channel, buildJobQueue);
+    }
+
+    public void sendRunningBuildJobsForCourse(long courseId, List<LocalCIBuildJobQueueItem> buildJobsRunning) {
+        String channel = "/topic/build-job-queue/running/" + courseId;
+        log.debug("Sending message on topic {}: {}", channel, buildJobsRunning);
+        websocketMessagingService.sendMessage(channel, buildJobsRunning);
+    }
+
+    public void sendQueuedBuildJobs(List<LocalCIBuildJobQueueItem> buildJobQueue) {
+        if (isAdmin()) {
+            String channel = "/topic/admin/build-job-queue/queued";
+            log.debug("Sending message on topic {}: {}", channel, buildJobQueue);
+            websocketMessagingService.sendMessage(channel, buildJobQueue);
+        }
+    }
+
+    public void sendRunningBuildJobs(List<LocalCIBuildJobQueueItem> buildJobQueue) {
+        if (isAdmin()) {
+            String channel = "/topic/admin/build-job-queue/running";
+            log.debug("Sending message on topic {}: {}", channel, buildJobQueue);
+            websocketMessagingService.sendMessage(channel, buildJobQueue);
+        }
+    }
+
+    public void sendBuildAgentInformation(List<LocalCIBuildAgentInformation> buildAgentInfo) {
+        if (isAdmin()) {
+            String channel = "/topic/admin/build-agents";
+            log.debug("Sending message on topic {}: {}", channel, buildAgentInfo);
+            websocketMessagingService.sendMessage(channel, buildAgentInfo);
+        }
+    }
+
+    private boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ADMIN.getAuthority()));
+    }
+}
--- src/main/webapp/app/localci/build-queue/build-queue.component.ts
+++ src/main/webapp/app/localci/build-queue/build-queue.component.ts
@@ -1,32 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AccountService } from 'app/core/auth/account.service';
-import { BuildQueueService } from 'app/localci/build-queue/build-queue.service';
 import { BuildJob } from 'app/entities/build-job.model';
-import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import { JhiWebsocketService } from 'app/core/websocket/websocket.service';
+import { BuildQueueService } from 'app/localci/build-queue/build-queue.service';
 
 @Component({
     selector: 'jhi-build-queue',
     templateUrl: './build-queue.component.html',
     styleUrl: './build-queue.component.scss',
 })
-export class BuildQueueComponent implements OnInit {
+export class BuildQueueComponent implements OnInit, OnDestroy {
     queuedBuildJobs: BuildJob[];
     runningBuildJobs: BuildJob[];
-
-    //icons
-    faRefresh = faRefresh;
+    courseChannels: string[] = [];
 
     constructor(
         private route: ActivatedRoute,
-        private accountService: AccountService,
+        private websocketService: JhiWebsocketService,
         private buildQueueService: BuildQueueService,
     ) {}
 
     ngOnInit() {
         this.load();
+        this.initWebsocketSubscription();
     }
 
+    /**
+     * This method is used to unsubscribe from the websocket channels when the component is destroyed.
+     */
+    ngOnDestroy() {
+        this.websocketService.unsubscribe(`/topic/build-job-queue/queued`);
+        this.websocketService.unsubscribe(`/topic/build-job-queue/running`);
+        this.courseChannels.forEach((channel) => {
+            this.websocketService.unsubscribe(channel);
+        });
+    }
+
+    /**
+     * This method is used to initialize the websocket subscription for the build jobs. It subscribes to the channels for the queued and running build jobs.
+     */
+    initWebsocketSubscription() {
+        this.route.paramMap.subscribe((params) => {
+            const courseId = Number(params.get('courseId'));
+            if (courseId) {
+                this.websocketService.subscribe(`/topic/build-job-queue/queued/${courseId}`);
+                this.websocketService.subscribe(`/topic/build-job-queue/running/${courseId}`);
+                this.websocketService.receive(`/topic/build-job-queue/queued/${courseId}`).subscribe((queuedBuildJobs) => {
+                    this.queuedBuildJobs = queuedBuildJobs;
+                });
+                this.websocketService.receive(`/topic/build-job-queue/running/${courseId}`).subscribe((runningBuildJobs) => {
+                    this.runningBuildJobs = runningBuildJobs;
+                });
+                this.courseChannels.push(`/topic/build-job-queue/queued/${courseId}`);
+                this.courseChannels.push(`/topic/build-job-queue/running/${courseId}`);
+            } else {
+                this.websocketService.subscribe(`/topic/admin/build-job-queue/queued`);
+                this.websocketService.subscribe(`/topic/admin/build-job-queue/running`);
+                this.websocketService.receive(`/topic/admin/build-job-queue/queued`).subscribe((queuedBuildJobs) => {
+                    this.queuedBuildJobs = queuedBuildJobs;
+                });
+                this.websocketService.receive(`/topic/admin/build-job-queue/running`).subscribe((runningBuildJobs) => {
+                    this.runningBuildJobs = runningBuildJobs;
+                });
+            }
+        });
+    }
+
+    /**
+     * This method is used to load the build jobs from the backend when the component is initialized.
+     * This ensures that the table is filled with data when the page is loaded or refreshed otherwise the user needs to
+     * wait until the websocket subscription receives the data.
+     */
     load() {
         this.route.paramMap.subscribe((params) => {
             const courseId = Number(params.get('courseId'));
