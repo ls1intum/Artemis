11b5de19b34141356fade056716294e63a7f6de8
Murad Talibov <muradtalibov2@gmail.com>
2024-01-05 10:58:09 +0100

    Import fs

--- src/test/playwright/support/commands.ts
+++ src/test/playwright/support/commands.ts
@@ -1,6 +1,7 @@
 import { UserCredentials } from './users';
 import { BASE_API } from '../../cypress/support/constants';
 import { Page, expect } from '@playwright/test';
+import * as fs from 'fs';
 
 export class Commands {
     static login = async (page: Page, credentials: UserCredentials, url?: string): Promise<void> => {
