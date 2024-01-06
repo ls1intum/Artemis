32155c993dbbca116a762b0fbd251e86be033a66
Murad Talibov <muradtalibov2@gmail.com>
2024-01-06 08:09:07 +0100

    Ignore https errors temporarily

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
 
-        ignoreHTTPSErrors: false,
+        ignoreHTTPSErrors: true,
     },
 
     /* Configure projects for major browsers */
