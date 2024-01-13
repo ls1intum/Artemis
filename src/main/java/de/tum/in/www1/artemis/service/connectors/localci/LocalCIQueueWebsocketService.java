6a35878869397017b5f0c4f16ee4e97bc0a1f68d
Mateus Messias Mendes <ge59rib@mytum.de>
2024-01-13 11:38:07 +0100

    test 7 to fix issues on ts3 by removing scheduling profile

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIQueueWebsocketService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCIQueueWebsocketService.java
@@ -27,7 +27,7 @@ import de.tum.in.www1.artemis.web.websocket.localci.LocalCIWebsocketMessagingSer
  * same information multiple times and thus also avoids unnecessary load on the server.
  */
 @Service
-@Profile("localci & scheduling")
+@Profile("localci")
 public class LocalCIQueueWebsocketService {
 
     private final Logger log = LoggerFactory.getLogger(LocalCIQueueWebsocketService.class);
