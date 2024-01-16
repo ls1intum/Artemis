36d801b0d2e073bce17ad0f0dd5235ca7441dc85
Raphael Stief <raphael.stief@gmail.com>
2024-01-16 20:00:28 +0100

    changed GradingScale PageUtil to conform to naming convention

--- src/main/java/de/tum/in/www1/artemis/web/rest/util/PageUtil.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/util/PageUtil.java
@@ -5,9 +5,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
-import de.tum.in.www1.artemis.domain.Exercise;
-import de.tum.in.www1.artemis.domain.GradingScale;
-import de.tum.in.www1.artemis.domain.Lecture;
+import de.tum.in.www1.artemis.domain.*;
 import de.tum.in.www1.artemis.domain.competency.Competency;
 import de.tum.in.www1.artemis.domain.competency.LearningPath;
 import de.tum.in.www1.artemis.domain.enumeration.SortingOrder;
@@ -52,7 +50,7 @@ public class PageUtil {
     }
 
     @NotNull
-    public static PageRequest createGradingScaleRequest(PageableSearchDTO<String> search) {
+    public static PageRequest createGradingScalePageRequest(PageableSearchDTO<String> search) {
         var sortOptions = Sort.by(GradingScale.GradingScaleSearchColumn.valueOf(search.getSortedColumn()).getMappedColumnName());
         sortOptions = search.getSortingOrder() == SortingOrder.ASCENDING ? sortOptions.ascending() : sortOptions.descending();
         return PageRequest.of(search.getPage() - 1, search.getPageSize(), sortOptions);
