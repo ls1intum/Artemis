1f641f26fd4d8f21ef1ec36d025c0f5f47df9083
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2024-01-16 19:16:39 +0100

    fix tests

--- src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIResourceIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIResourceIntegrationTest.java
@@ -14,6 +14,7 @@ import com.hazelcast.collection.IQueue;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 
+import de.tum.in.www1.artemis.domain.enumeration.RepositoryType;
 import de.tum.in.www1.artemis.service.connectors.localci.LocalCISharedBuildJobQueueService;
 import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildAgentInformation;
 import de.tum.in.www1.artemis.service.connectors.localci.dto.LocalCIBuildJobQueueItem;
@@ -43,9 +44,10 @@ class LocalCIResourceIntegrationTest extends AbstractLocalCILocalVCIntegrationTe
         // temporarily remove listener to avoid triggering build job processing
         localCISharedBuildJobQueueService.removeListener();
 
-        job1 = new LocalCIBuildJobQueueItem(1, "job1", "address1", 1, "test", "commit1", ZonedDateTime.now(), 1, ZonedDateTime.now(), 1, course.getId(), false);
-        job2 = new LocalCIBuildJobQueueItem(2, "job2", "address2", 1, "test", "commit2", ZonedDateTime.now(), 1, ZonedDateTime.now(), 1, course.getId(), false);
-
+        job1 = new LocalCIBuildJobQueueItem(1, "job1", "address1", 1, "test", RepositoryType.USER, "commit1", ZonedDateTime.now(), 1, ZonedDateTime.now(),
+                ZonedDateTime.now().plusMinutes(1), 1, course.getId(), RepositoryType.USER, "image");
+        job1 = new LocalCIBuildJobQueueItem(2, "job2", "address1", 2, "test", RepositoryType.USER, "commit2", ZonedDateTime.now(), 1, ZonedDateTime.now(),
+                ZonedDateTime.now().plusMinutes(1), 1, course.getId(), RepositoryType.USER, "image");
         String memberAddress = hazelcastInstance.getCluster().getLocalMember().getAddress().toString();
         agent1 = new LocalCIBuildAgentInformation(memberAddress, 1, 0, null);
 
