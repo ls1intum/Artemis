ba3a4bacba7655be77de1f0c52ea35493de7151d
Julian Christl <julian.christl@tum.de>
2024-01-22 21:37:31 +0100

    fix exercise group repo

--- src/main/java/de/tum/in/www1/artemis/repository/ExerciseGroupRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ExerciseGroupRepository.java
@@ -23,7 +23,11 @@ import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
 public interface ExerciseGroupRepository extends JpaRepository<ExerciseGroup, Long> {
 
     @EntityGraph(type = LOAD, attributePaths = { "exercises" })
-    @Query("SELECT e FROM ExerciseGroup e WHERE e.id = :exerciseGroupId")
+    @Query("""
+            SELECT e
+            FROM ExerciseGroup e
+            WHERE e.id = :exerciseGroupId
+            """)
     Optional<ExerciseGroup> findWithExercisesById(@Param("exerciseGroupId") Long exerciseGroupId);
 
     @Query("""
