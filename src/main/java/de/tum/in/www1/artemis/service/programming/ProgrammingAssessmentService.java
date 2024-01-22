b9ee338bfb63f0acb769a27b87c4d4b02d0e80dc
Maximilian Soelch <maximilian.soelch@tum.de>
2024-01-22 16:48:19 +0100

    Fix merge issues

--- src/main/java/de/tum/in/www1/artemis/service/programming/ProgrammingAssessmentService.java
+++ src/main/java/de/tum/in/www1/artemis/service/programming/ProgrammingAssessmentService.java
@@ -117,7 +117,7 @@ public class ProgrammingAssessmentService extends AssessmentService {
      * Send feedback to Athena (if enabled for both the Artemis instance and the exercise).
      */
     private void sendFeedbackToAthena(final ProgrammingExercise exercise, final ProgrammingSubmission programmingSubmission, final List<Feedback> feedbacks) {
-        if (athenaFeedbackSendingService.isPresent() && exercise.getFeedbackSuggestionsEnabled()) {
+        if (athenaFeedbackSendingService.isPresent() && exercise.isFeedbackSuggestionsEnabled()) {
             athenaFeedbackSendingService.get().sendFeedback(exercise, programmingSubmission, feedbacks);
         }
     }
