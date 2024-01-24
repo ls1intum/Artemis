b6f8f3a11473619ec50fb8d5a1c0a8448227bb4d
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2024-01-24 17:37:21 +0100

    refactor triggerBuild()

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCITriggerService.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/LocalCITriggerService.java
@@ -90,7 +90,7 @@ public class LocalCITriggerService implements ContinuousIntegrationTriggerServic
     }
 
     /**
-     * Add a new build job for a specific commit to the queue managed by the ExecutorService and process the returned result.
+     * Add a new build job item containing all relevant information necessary for the execution to the distributed build job queue.
      *
      * @param participation     the participation of the repository which should be built and tested
      * @param commitHash        the commit hash of the commit that triggers the build. If it is null, the latest commit of the default branch will be built.
@@ -100,6 +100,105 @@ public class LocalCITriggerService implements ContinuousIntegrationTriggerServic
     @Override
     public void triggerBuild(ProgrammingExerciseParticipation participation, String commitHash, RepositoryType triggeredByPushTo) throws LocalCIException {
 
+        ProgrammingExercise programmingExercise = participation.getProgrammingExercise();
+
+        long courseId = programmingExercise.getCourseViaExerciseGroupOrCourseMember().getId();
+
+        // Exam exercises have a higher priority than normal exercises
+        int priority = programmingExercise.isExamExercise() ? 1 : 2;
+
+        ZonedDateTime submissionDate = ZonedDateTime.now();
+
+        String buildJobId = String.valueOf(participation.getId()) + submissionDate.toInstant().toEpochMilli();
+
+        JobTimingInfo jobTimingInfo = new JobTimingInfo(submissionDate, null, null);
+
+        RepositoryInfo repositoryInfo = getRepositoryInfo(participation, triggeredByPushTo);
+
+        BuildConfig buildConfig = getBuildConfig(participation, commitHash);
+
+        LocalCIBuildJobQueueItem buildJobQueueItem = new LocalCIBuildJobQueueItem(buildJobId, participation.getBuildPlanId(), null, participation.getId(), courseId,
+                programmingExercise.getId(), 0, priority, repositoryInfo, jobTimingInfo, buildConfig);
+
+        localCIBuildConfigurationService.createBuildScript(participation, buildJobId);
+
+        queue.add(buildJobQueueItem);
+    }
+
+    // -------Helper methods for triggerBuild()-------
+
+    private List<String> getTestResultPaths(Windfile windfile) throws IllegalArgumentException {
+        List<String> testResultPaths = new ArrayList<>();
+        for (AeolusResult testResultPath : windfile.getResults()) {
+            testResultPaths.add(WORKING_DIRECTORY + "/testing-dir/" + testResultPath.getPath());
+        }
+        return testResultPaths;
+    }
+
+    /**
+     * Collects all necessary information regarding the repositories involved in the build job processing.
+     *
+     * @param participation     the participation for which to get the repository information
+     * @param triggeredByPushTo type of the repository that was pushed to and triggered the build job
+     * @return the repository information for the given participation
+     */
+    private RepositoryInfo getRepositoryInfo(ProgrammingExerciseParticipation participation, RepositoryType triggeredByPushTo) {
+
+        ProgrammingExercise programmingExercise = participation.getProgrammingExercise();
+
+        List<AuxiliaryRepository> auxiliaryRepositories;
+
+        // If the auxiliary repositories are not initialized, we need to fetch them from the database.
+        if (Hibernate.isInitialized(participation.getProgrammingExercise().getAuxiliaryRepositories())) {
+            auxiliaryRepositories = programmingExercise.getAuxiliaryRepositories();
+        }
+        else {
+            auxiliaryRepositories = auxiliaryRepositoryRepository.findByExerciseId(participation.getProgrammingExercise().getId());
+        }
+
+        String assignmentRepositoryUri = participation.getRepositoryUri();
+        String testRepositoryUri = programmingExercise.getTestRepositoryUri();
+        String solutionRepositoryUri = null;
+        String[] auxiliaryRepositoryUris = auxiliaryRepositories.stream().map(AuxiliaryRepository::getRepositoryUri).toArray(String[]::new);
+        String[] auxiliaryRepositoryCheckoutDirectories1 = auxiliaryRepositories.stream().map(AuxiliaryRepository::getCheckoutDirectory).toArray(String[]::new);
+
+        if (programmingExercise.getCheckoutSolutionRepository()) {
+            ProgrammingLanguageFeature programmingLanguageFeature = programmingLanguageFeatureService.getProgrammingLanguageFeatures(programmingExercise.getProgrammingLanguage());
+            if (programmingLanguageFeature.checkoutSolutionRepositoryAllowed()) {
+                var solutionParticipation = solutionProgrammingExerciseParticipationRepository.findByProgrammingExerciseId(participation.getProgrammingExercise().getId());
+                if (solutionParticipation.isPresent()) {
+                    solutionRepositoryUri = solutionParticipation.get().getRepositoryUri();
+                }
+            }
+        }
+
+        String repositoryTypeOrUserName = participation.getVcsRepositoryUri().repositoryNameWithoutProjectKey();
+
+        String repositoryName = participation.getVcsRepositoryUri().repositorySlug();
+
+        RepositoryType repositoryType;
+        // Only template, solution and user repositories are build
+        if (repositoryTypeOrUserName.equals("exercise")) {
+            repositoryType = RepositoryType.TEMPLATE;
+        }
+        else if (repositoryTypeOrUserName.equals("solution")) {
+            repositoryType = RepositoryType.SOLUTION;
+        }
+        else {
+            repositoryType = RepositoryType.USER;
+        }
+
+        // if the build is not triggered by a push to the test or an auxiliary repository, it was triggered by a push to its own repository
+        if (triggeredByPushTo == null) {
+            triggeredByPushTo = repositoryType;
+        }
+
+        return new RepositoryInfo(repositoryName, repositoryType, triggeredByPushTo, assignmentRepositoryUri, testRepositoryUri, solutionRepositoryUri, auxiliaryRepositoryUris,
+                auxiliaryRepositoryCheckoutDirectories1);
+
+    }
+
+    private BuildConfig getBuildConfig(ProgrammingExerciseParticipation participation, String commitHash) {
         String branch;
         try {
             branch = versionControlService.orElseThrow().getOrRetrieveBranchOfParticipation(participation);
@@ -129,83 +228,7 @@ public class LocalCITriggerService implements ContinuousIntegrationTriggerServic
 
         List<String> resultPaths = getTestResultPaths(windfile);
 
-        List<AuxiliaryRepository> auxiliaryRepositories;
-
-        // If the auxiliary repositories are not initialized, we need to fetch them from the database.
-        if (Hibernate.isInitialized(participation.getProgrammingExercise().getAuxiliaryRepositories())) {
-            auxiliaryRepositories = participation.getProgrammingExercise().getAuxiliaryRepositories();
-        }
-        else {
-            auxiliaryRepositories = auxiliaryRepositoryRepository.findByExerciseId(participation.getProgrammingExercise().getId());
-        }
-
-        String assignmentRepositoryUri = participation.getRepositoryUri();
-        String testRepositoryUri = programmingExercise.getTestRepositoryUri();
-        String solutionRepositoryUri = null;
-        String[] auxiliaryRepositoryUris = auxiliaryRepositories.stream().map(AuxiliaryRepository::getRepositoryUri).toArray(String[]::new);
-        String[] auxiliaryRepositoryCheckoutDirectories1 = auxiliaryRepositories.stream().map(AuxiliaryRepository::getCheckoutDirectory).toArray(String[]::new);
-
-        if (programmingExercise.getCheckoutSolutionRepository()) {
-            ProgrammingLanguageFeature programmingLanguageFeature = programmingLanguageFeatureService.getProgrammingLanguageFeatures(programmingLanguage);
-            if (programmingLanguageFeature.checkoutSolutionRepositoryAllowed()) {
-                var solutionParticipation = solutionProgrammingExerciseParticipationRepository.findByProgrammingExerciseId(participation.getProgrammingExercise().getId());
-                if (solutionParticipation.isPresent()) {
-                    solutionRepositoryUri = solutionParticipation.get().getRepositoryUri();
-                }
-            }
-        }
-
-        long courseId = programmingExercise.getCourseViaExerciseGroupOrCourseMember().getId();
-
-        String repositoryTypeOrUserName = participation.getVcsRepositoryUri().repositoryNameWithoutProjectKey();
-
-        String repositoryName = participation.getVcsRepositoryUri().repositorySlug();
-
-        RepositoryType repositoryType;
-        // Only template, solution and user repositories are build
-        if (repositoryTypeOrUserName.equals("exercise")) {
-            repositoryType = RepositoryType.TEMPLATE;
-        }
-        else if (repositoryTypeOrUserName.equals("solution")) {
-            repositoryType = RepositoryType.SOLUTION;
-        }
-        else {
-            repositoryType = RepositoryType.USER;
-        }
-
-        // if the build is not triggered by a push to the test or an auxiliary repository, it was triggered by a push to its own repository
-        if (triggeredByPushTo == null) {
-            triggeredByPushTo = repositoryType;
-        }
-
-        // Exam exercises have a higher priority than normal exercises
-        int priority = programmingExercise.isExamExercise() ? 1 : 2;
-
-        ZonedDateTime submissionDate = ZonedDateTime.now();
-
-        RepositoryInfo repositoryInfo = new RepositoryInfo(repositoryName, repositoryType, triggeredByPushTo, assignmentRepositoryUri, testRepositoryUri, solutionRepositoryUri,
-                auxiliaryRepositoryUris, auxiliaryRepositoryCheckoutDirectories1);
-
-        BuildConfig buildConfig = new BuildConfig(dockerImage, commitHash, branch, programmingLanguage, projectType, staticCodeAnalysisEnabled, sequentialTestRunsEnabled,
-                testwiseCoverageEnabled, resultPaths);
-
-        JobTimingInfo jobTimingInfo = new JobTimingInfo(submissionDate, null, null);
-
-        String buildJobId = String.valueOf(participation.getId()) + submissionDate.toInstant().toEpochMilli();
-
-        LocalCIBuildJobQueueItem buildJobQueueItem = new LocalCIBuildJobQueueItem(buildJobId, repositoryName, null, participation.getId(), courseId, programmingExercise.getId(), 0,
-                priority, repositoryInfo, jobTimingInfo, buildConfig);
-
-        localCIBuildConfigurationService.createBuildScript(participation, buildJobId);
-
-        queue.add(buildJobQueueItem);
-    }
-
-    private List<String> getTestResultPaths(Windfile windfile) throws IllegalArgumentException {
-        List<String> testResultPaths = new ArrayList<>();
-        for (AeolusResult testResultPath : windfile.getResults()) {
-            testResultPaths.add(WORKING_DIRECTORY + "/testing-dir/" + testResultPath.getPath());
-        }
-        return testResultPaths;
+        return new BuildConfig(dockerImage, commitHash, branch, programmingLanguage, projectType, staticCodeAnalysisEnabled, sequentialTestRunsEnabled, testwiseCoverageEnabled,
+                resultPaths);
     }
 }
