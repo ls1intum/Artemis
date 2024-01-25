dfabf12e9e65194aabca730037b2560f2f05d748
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2024-01-25 13:11:20 +0100

    fix client even more!!

--- src/test/javascript/spec/component/localci/build-agents/build-agents.component.spec.ts
+++ src/test/javascript/spec/component/localci/build-agents/build-agents.component.spec.ts
@@ -42,7 +42,6 @@ describe('BuildAgentsComponent', () => {
     const jobTimingInfo: JobTimingInfo = {
         submissionDate: dayjs('2023-01-01'),
         buildStartDate: dayjs('2023-01-01'),
-        buildCompletionDate: 0,
     };
 
     const buildConfig: BuildConfig = {
