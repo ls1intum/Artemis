71dda57c77e72a94a26efca7587f68924f4c4d72
Basak Akan <basak.akan@tum.de>
2024-01-28 11:05:41 +0100

    fix build

--- src/test/java/de/tum/in/www1/artemis/exercise/programmingexercise/CourseGitlabJenkinsIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/exercise/programmingexercise/CourseGitlabJenkinsIntegrationTest.java
@@ -1072,10 +1072,4 @@ class CourseGitlabJenkinsIntegrationTest extends AbstractSpringIntegrationJenkin
     void testFindAllOnlineCoursesForLtiDashboard() throws Exception {
         courseTestService.testFindAllOnlineCoursesForLtiDashboard();
     }
-
-    @Test
-    @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
-    void testFindAllOnlineCoursesForLtiDashboard() throws Exception {
-        courseTestService.testFindAllOnlineCoursesForLtiDashboard();
-    }
 }
