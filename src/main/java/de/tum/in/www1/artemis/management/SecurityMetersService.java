1125264dd3d94999ae2e0a711b8b017659d04e1e
Laurenz Blumentritt <laurenz.blumentritt9@gmail.com>
2024-02-04 16:12:03 +0100

    add core profile to management classes

--- src/main/java/de/tum/in/www1/artemis/management/SecurityMetersService.java
+++ src/main/java/de/tum/in/www1/artemis/management/SecurityMetersService.java
@@ -1,11 +1,13 @@
 package de.tum.in.www1.artemis.management;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 
 @Service
+@Profile("core")
 public class SecurityMetersService {
 
     public static final String INVALID_TOKENS_METER_NAME = "security.authentication.invalid-tokens";
