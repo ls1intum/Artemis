b023f44dcca0dc99a62c2ae5345ce22f0e6a0102
Raphael Stief <raphael.stief@gmail.com>
2024-02-05 20:21:39 +0100

    improved UI

--- src/main/webapp/app/course/competencies/import-competencies/competency-search.component.ts
+++ src/main/webapp/app/course/competencies/import-competencies/competency-search.component.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+export class CompetencySearch {
+    courseTitleSearch: string;
+    courseSemesterSearch: string;
+    competencyDescriptionSearch: string;
+
+    constructor(courseTitleSearch = '', courseSemesterSearch = '', competencyDescriptionSearch = '') {
+        this.courseTitleSearch = courseTitleSearch;
+        this.courseSemesterSearch = courseSemesterSearch;
+        this.competencyDescriptionSearch = competencyDescriptionSearch;
+    }
+}
 
 @Component({
     selector: 'jhi-competency-search',
     templateUrl: './competency-search.component.html',
 })
 export class CompetencySearchComponent {
+    search: CompetencySearch = new CompetencySearch();
+
+    @Output()
+    searchEvent: EventEmitter<void>;
+
+    isCollapsed = true;
+
+    toggleCollapse() {
+        this.isCollapsed = !this.isCollapsed;
+    }
+
     constructor() {}
 
-    reset() {}
-
-    sendUpdate() {}
-
-    courseTitleSearch: string;
-    courseSemesterSearch: string;
-    competencyDescriptionSearch: string;
+    /**
+     * Resets all searches to default values
+     */
+    reset() {
+        this.search = new CompetencySearch();
+    }
+    /**
+     * Sends an updated filter through the event emitter
+     * Triggered every time the user manually presses Enter or the search button
+     */
+    performSearch() {
+        //TODO: emit with result
+        this.searchEvent.emit();
+    }
 }
