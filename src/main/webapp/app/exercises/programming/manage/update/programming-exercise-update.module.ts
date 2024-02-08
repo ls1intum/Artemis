1c91e0b6899f61a9a73c8270adce9d1a64c91abc
Jonathan Ostertag <jo.ostertag@tum.de>
2024-02-08 16:25:59 +0100

    chore: remove wizard components from modules

--- src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.module.ts
+++ src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.module.ts
@@ -18,9 +18,6 @@ import { ArtemisTableModule } from 'app/shared/table/table.module';
 import { RemoveAuxiliaryRepositoryButtonComponent } from 'app/exercises/programming/manage/update/remove-auxiliary-repository-button.component';
 import { SubmissionPolicyUpdateModule } from 'app/exercises/shared/submission-policy/submission-policy-update.module';
 import { ArtemisModePickerModule } from 'app/exercises/shared/mode-picker/mode-picker.module';
-import { ProgrammingExerciseUpdateWizardComponent } from 'app/exercises/programming/manage/update/wizard-mode/programming-exercise-update-wizard.component';
-import { ProgrammingExerciseUpdateWizardStepComponent } from 'app/exercises/programming/manage/update/wizard-mode/programming-exercise-update-wizard-step.component';
-import { ProgrammingExerciseUpdateWizardBottomBarComponent } from 'app/exercises/programming/manage/update/wizard-mode/programming-exercise-update-wizard-bottom-bar.component';
 import { ProgrammingExerciseInformationComponent } from 'app/exercises/programming/manage/update/update-components/programming-exercise-information.component';
 import { ProgrammingExerciseDifficultyComponent } from 'app/exercises/programming/manage/update/update-components/programming-exercise-difficulty.component';
 import { ProgrammingExerciseLanguageComponent } from 'app/exercises/programming/manage/update/update-components/programming-exercise-language.component';
@@ -60,9 +57,6 @@ import { FormsModule } from 'app/forms/forms.module';
     ],
     declarations: [
         ProgrammingExerciseUpdateComponent,
-        ProgrammingExerciseUpdateWizardComponent,
-        ProgrammingExerciseUpdateWizardStepComponent,
-        ProgrammingExerciseUpdateWizardBottomBarComponent,
         ProgrammingExerciseInformationComponent,
         ProgrammingExerciseDifficultyComponent,
         ProgrammingExerciseCustomAeolusBuildPlanComponent,
@@ -75,6 +69,6 @@ import { FormsModule } from 'app/forms/forms.module';
         AddAuxiliaryRepositoryButtonComponent,
         RemoveAuxiliaryRepositoryButtonComponent,
     ],
-    exports: [ProgrammingExerciseUpdateComponent, ProgrammingExerciseUpdateWizardComponent, ProgrammingExercisePlansAndRepositoriesPreviewComponent],
+    exports: [ProgrammingExerciseUpdateComponent, ProgrammingExercisePlansAndRepositoriesPreviewComponent],
 })
 export class ArtemisProgrammingExerciseUpdateModule {}
