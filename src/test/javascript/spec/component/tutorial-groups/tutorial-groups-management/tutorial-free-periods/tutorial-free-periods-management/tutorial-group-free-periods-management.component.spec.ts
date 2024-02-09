b7462ff309935cb8adcde046049bc54976b34aa9
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-02-09 15:28:06 +0100

    fixed bug with existing tests

--- src/test/javascript/spec/component/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-management.component.spec.ts
+++ src/test/javascript/spec/component/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-management.component.spec.ts
@@ -24,6 +24,7 @@ import { mockedActivatedRoute } from '../../../../../helpers/mocks/activated-rou
 import { Course } from 'app/entities/course.model';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { CreateTutorialGroupFreePeriodComponent } from 'app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/crud/create-tutorial-group-free-period/create-tutorial-group-free-period.component';
+import { TutorialGroupFreePeriodsTableComponent } from 'app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-table/tutorial-group-free-periods-table.component';
 
 @Component({ selector: 'jhi-tutorial-group-free-period-row-buttons', template: '' })
 class TutorialGroupRowButtonsStubComponent {
@@ -59,6 +60,7 @@ describe('TutorialGroupFreePeriodsManagementComponent', () => {
                 MockPipe(ArtemisDatePipe),
                 MockComponent(FaIconComponent),
                 MockRouterLinkDirective,
+                TutorialGroupFreePeriodsTableComponent,
             ],
             providers: [
                 MockProvider(TutorialGroupsConfigurationService),
