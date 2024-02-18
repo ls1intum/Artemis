0f52e9c9bdb1274021f31eea84fe6c05d4108bf2
Laurenz Blumentritt <38919977+laurenzfb@users.noreply.github.com>
2024-02-18 22:05:20 +0100

    remove empty line

--- src/main/java/de/tum/in/www1/artemis/repository/StudentScoreRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/StudentScoreRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
