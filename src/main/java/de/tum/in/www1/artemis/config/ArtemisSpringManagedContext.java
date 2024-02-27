bc55aae5c130ed9b1f09f6aa8550a92749916b78
Stephan Krusche <krusche@in.tum.de>
2024-02-27 08:37:04 +0100

    improve Hazelcast logging for slow operations

---
+++ src/main/java/de/tum/in/www1/artemis/config/ArtemisSpringManagedContext.java
@@ -0,0 +1,25 @@
+package de.tum.in.www1.artemis.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import com.hazelcast.spring.context.SpringManagedContext;
+
+/**
+ * This class only exists to improve logging in case of slow Hazelcast operations
+ */
+public class ArtemisSpringManagedContext extends SpringManagedContext {
+
+    private static final Logger log = LoggerFactory.getLogger(ArtemisSpringManagedContext.class);
+
+    public ArtemisSpringManagedContext(ApplicationContext applicationContext) {
+        super(applicationContext);
+    }
+
+    @Override
+    public Object initialize(Object obj) {
+        log.info("Initialize obj {} of type {}", obj, obj.getClass().getName());
+        return super.initialize(obj);
+    }
+}
