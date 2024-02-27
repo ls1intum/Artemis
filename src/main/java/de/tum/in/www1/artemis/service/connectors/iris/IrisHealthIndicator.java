7ff003f88edcb8180b33f19f2f88be0503061104
Timor Morrien <timor.morrien@tum.de>
2024-02-27 22:24:33 +0100

    Disable Pyris health check

--- src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisHealthIndicator.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/iris/IrisHealthIndicator.java
@@ -1,8 +1,6 @@
 package de.tum.in.www1.artemis.service.connectors.iris;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import de.tum.in.www1.artemis.service.connectors.ConnectorHealth;
-import de.tum.in.www1.artemis.service.connectors.iris.dto.IrisStatusDTO;
 
 @Component
 @Profile("iris")
@@ -33,19 +30,7 @@ public class IrisHealthIndicator implements HealthIndicator {
      */
     @Override
     public Health health() {
-        ConnectorHealth health;
-        try {
-            IrisStatusDTO[] status = restTemplate.getForObject(irisUrl + "/api/v1/health", IrisStatusDTO[].class);
-            var isUp = status != null && Arrays.stream(status).anyMatch(s -> s.status() == IrisStatusDTO.ModelStatus.UP);
-            Map<String, Object> additionalInfo = Map.of("url", irisUrl, "modelStatuses", status);
-            health = new ConnectorHealth(isUp, additionalInfo);
-        }
-        catch (Exception emAll) {
-            health = new ConnectorHealth(emAll);
-            health.setUp(false);
-            health.setAdditionalInfo(Map.of("url", irisUrl, "exception", emAll.getLocalizedMessage()));
-        }
-
-        return health.asActuatorHealth();
+        // Disabled until PyrisV2 reimplements this
+        return new ConnectorHealth(true).asActuatorHealth();
     }
 }
