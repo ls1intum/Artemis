1a2007942e4bd34d12dae1ca35acaf12ac8d0a41
Timor Morrien <timor.morrien@tum.de>
2024-02-28 16:26:44 +0100

    Remove IrisHealthIndicator.java completely

--- src/main/java/de/tum/in/www1/artemis/web/rest/iris/IrisExerciseChatBasedSessionResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/iris/IrisExerciseChatBasedSessionResource.java
@@ -16,7 +16,6 @@ import de.tum.in.www1.artemis.domain.iris.settings.IrisSubSettingsType;
 import de.tum.in.www1.artemis.repository.UserRepository;
 import de.tum.in.www1.artemis.security.Role;
 import de.tum.in.www1.artemis.service.AuthorizationCheckService;
-import de.tum.in.www1.artemis.service.connectors.iris.IrisHealthIndicator;
 import de.tum.in.www1.artemis.service.dto.iris.IrisCombinedSettingsDTO;
 import de.tum.in.www1.artemis.service.dto.iris.IrisCombinedSubSettingsInterface;
 import de.tum.in.www1.artemis.service.iris.IrisRateLimitService;
@@ -37,19 +36,16 @@ public abstract class IrisExerciseChatBasedSessionResource<E extends Exercise, S
 
     protected final IrisSettingsService irisSettingsService;
 
-    protected final IrisHealthIndicator irisHealthIndicator;
-
     protected final IrisRateLimitService irisRateLimitService;
 
     protected final Function<Long, E> exerciseByIdFunction;
 
     protected IrisExerciseChatBasedSessionResource(AuthorizationCheckService authCheckService, UserRepository userRepository, IrisSessionService irisSessionService,
-            IrisSettingsService irisSettingsService, IrisHealthIndicator irisHealthIndicator, IrisRateLimitService irisRateLimitService, Function<Long, E> exerciseByIdFunction) {
+            IrisSettingsService irisSettingsService, IrisRateLimitService irisRateLimitService, Function<Long, E> exerciseByIdFunction) {
         this.authCheckService = authCheckService;
         this.userRepository = userRepository;
         this.irisSessionService = irisSessionService;
         this.irisSettingsService = irisSettingsService;
-        this.irisHealthIndicator = irisHealthIndicator;
         this.irisRateLimitService = irisRateLimitService;
         this.exerciseByIdFunction = exerciseByIdFunction;
     }
