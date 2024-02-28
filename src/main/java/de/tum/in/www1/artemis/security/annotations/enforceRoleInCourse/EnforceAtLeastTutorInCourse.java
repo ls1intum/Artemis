dc1cfc9da989faf3f0b68fd3759409550007a8f3
Timor Morrien <timor.morrien@tum.de>
2024-02-28 14:45:44 +0100

    Make it work

--- src/main/java/de/tum/in/www1/artemis/security/annotations/enforceRoleInCourse/EnforceAtLeastTutorInCourse.java
+++ src/main/java/de/tum/in/www1/artemis/security/annotations/enforceRoleInCourse/EnforceAtLeastTutorInCourse.java
@@ -1,9 +1,13 @@
 package de.tum.in.www1.artemis.security.annotations.enforceRoleInCourse;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import de.tum.in.www1.artemis.security.Role;
 
+@Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("hasRole('TA')")
 @EnforceRoleInCourse(Role.TEACHING_ASSISTANT)
 public @interface EnforceAtLeastTutorInCourse {
--- src/main/java/de/tum/in/www1/artemis/security/annotations/enforceRoleInCourse/EnforceRoleInCourseAspect.java
+++ src/main/java/de/tum/in/www1/artemis/security/annotations/enforceRoleInCourse/EnforceRoleInCourseAspect.java
@@ -1,5 +1,6 @@
 package de.tum.in.www1.artemis.security.annotations.enforceRoleInCourse;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -24,28 +25,52 @@ public class EnforceRoleInCourseAspect {
 
     /**
      * Pointcut around all methods or classes annotated with {@link EnforceRoleInCourse}.
-     *
-     * @param enforceRoleInCourse The annotation containing the relevant role
      */
-    @Pointcut("@within(enforceRoleInCourse) || @annotation(enforceRoleInCourse)")
-    public void callAt(EnforceRoleInCourse enforceRoleInCourse) {
+    @Pointcut("@within(EnforceRoleInCourse) || @annotation(EnforceRoleInCourse) || execution(@(@EnforceRoleInCourse *) * *(..))")
+    public void callAt() {
     }
 
     /**
      * Aspect around all methods for which a role in course has been activated. Will check if the user has the required role in the course and only execute the underlying method if
      * the user has the required role. Will otherwise return forbidden (as response entity)
      *
-     * @param joinPoint           Proceeding join point of the aspect
-     * @param enforceRoleInCourse The annotation containing the required role
+     * @param joinPoint Proceeding join point of the aspect
      * @return The original return value of the called method, if all features are enabled, a forbidden response entity otherwise
      * @throws Throwable If there was any error during method execution (both the aspect or the actual called method)
      */
-    @Around(value = "callAt(enforceRoleInCourse)", argNames = "joinPoint,enforceRoleInCourse")
-    public Object around(ProceedingJoinPoint joinPoint, EnforceRoleInCourse enforceRoleInCourse) throws Throwable {
-        final var courseId = getCourseId(joinPoint, enforceRoleInCourse)
+    @Around(value = "callAt()", argNames = "joinPoint")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        var annotation = getAnnotation(joinPoint);
+
+        final var courseId = getCourseId(joinPoint, annotation)
                 .orElseThrow(() -> new IllegalArgumentException("Method annotated with @EnforceRoleInCourse must have a parameter named 'courseId'"));
-        authorizationCheckService.checkIsAtLeastRoleInCourseElseThrow(enforceRoleInCourse.value(), courseId);
+        authorizationCheckService.checkIsAtLeastRoleInCourseElseThrow(annotation.value(), courseId);
         return joinPoint.proceed();
+    }
+
+    private EnforceRoleInCourse getAnnotation(ProceedingJoinPoint joinPoint) {
+        var method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+        EnforceRoleInCourse annotation = method.getAnnotation(EnforceRoleInCourse.class);
+        if (annotation == null) {
+            annotation = method.getDeclaringClass().getAnnotation(EnforceRoleInCourse.class);
+        }
+        if (annotation == null) {
+            for (Annotation a : method.getDeclaredAnnotations()) {
+                annotation = a.annotationType().getAnnotation(EnforceRoleInCourse.class);
+                if (annotation != null) {
+                    break;
+                }
+            }
+        }
+        if (annotation == null) {
+            for (Annotation a : method.getDeclaringClass().getDeclaredAnnotations()) {
+                annotation = a.annotationType().getAnnotation(EnforceRoleInCourse.class);
+                if (annotation != null) {
+                    break;
+                }
+            }
+        }
+        return annotation;
     }
 
     /**
