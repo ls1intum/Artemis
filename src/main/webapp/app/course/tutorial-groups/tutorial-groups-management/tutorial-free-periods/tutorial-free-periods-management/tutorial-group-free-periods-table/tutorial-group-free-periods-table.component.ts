dc8e67c64d4ed18fc309fbd108532e2d37b71fc2
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-03-01 20:34:33 +0100

    Replaced start and end-time by "-" when displaying freeDays and freePeriods

--- src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-table/tutorial-group-free-periods-table.component.ts
+++ src/main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-table/tutorial-group-free-periods-table.component.ts
@@ -3,6 +3,7 @@ import { TutorialGroupFreePeriod } from 'app/entities/tutorial-group/tutorial-gr
 import { Course } from 'app/entities/course.model';
 import { TutorialGroupsConfiguration } from 'app/entities/tutorial-group/tutorial-groups-configuration.model';
 import dayjs from 'dayjs/esm';
+import { TutorialGroupFreePeriodsManagementComponent } from 'app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/tutorial-free-periods-management/tutorial-group-free-periods-management.component';
 
 @Component({
     selector: 'jhi-tutorial-group-free-periods-table',
@@ -16,6 +17,8 @@ export class TutorialGroupFreePeriodsTableComponent {
     @Input() tutorialGroupFreePeriods: TutorialGroupFreePeriod[];
     @Input() labelText: string;
     @Input() loadAll: () => void;
+
+    protected readonly TutorialGroupFreePeriodsManagementComponent = TutorialGroupFreePeriodsManagementComponent;
 
     public isInThePast(tutorialGroupFreeDay: TutorialGroupFreePeriod): boolean {
         return tutorialGroupFreeDay.end!.isBefore(dayjs());
