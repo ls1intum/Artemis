2d5293e44814f02cab515dc659a5c665b2617d4d
Julian Christl <julian.christl@tum.de>
2024-03-01 20:59:41 +0100

    remove extracted lti 1.0 classes

--- src/main/java/de/tum/in/www1/artemis/service/connectors/lti/LtiVerificationException.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.www1.artemis.service.connectors.lti;
-
-public class LtiVerificationException extends Exception {
-
-    public LtiVerificationException(String message, Exception exception) {
-        super(message, exception);
-    }
-}
