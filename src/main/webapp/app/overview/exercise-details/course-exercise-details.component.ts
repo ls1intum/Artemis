bb36aec71c3fa8329078dabc41d61ae5b3d7777a
Michael Allgaier <michael.allgaier@tum.de>
2024-03-04 12:54:34 +0100

    Add teams button to shortcut button row in the overview page

--- src/main/webapp/app/overview/exercise-details/course-exercise-details.component.ts
+++ src/main/webapp/app/overview/exercise-details/course-exercise-details.component.ts
@@ -34,7 +34,7 @@ import { Complaint } from 'app/entities/complaint.model';
 import { SubmissionPolicyService } from 'app/exercises/programming/manage/services/submission-policy.service';
 import { SubmissionPolicy } from 'app/entities/submission-policy.model';
 import { ArtemisMarkdownService } from 'app/shared/markdown.service';
-import { faAngleDown, faAngleUp, faBook, faEye, faFileSignature, faListAlt, faSignal, faTable, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faBook, faEye, faFileSignature, faListAlt, faSignal, faTable, faUsers, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { PlagiarismCasesService } from 'app/course/plagiarism-cases/shared/plagiarism-cases.service';
 import { ExerciseHintService } from 'app/exercises/shared/exercise-hint/shared/exercise-hint.service';
 import { ExerciseHint } from 'app/entities/hestia/exercise-hint.model';
@@ -124,6 +124,7 @@ export class CourseExerciseDetailsComponent extends AbstractScienceComponent imp
     faFileSignature = faFileSignature;
     faAngleDown = faAngleDown;
     faAngleUp = faAngleUp;
+    faUsers = faUsers;
 
     constructor(
         private exerciseService: ExerciseService,
