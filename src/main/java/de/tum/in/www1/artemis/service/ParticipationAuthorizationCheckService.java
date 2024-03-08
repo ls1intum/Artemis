ad6b2275079ebafc9148c6d5cb3658499371fda8
Mateus Messias Mendes <ge59rib@mytum.de>
2024-03-08 14:30:16 +0100

    implement feedback and fix issue

--- src/main/java/de/tum/in/www1/artemis/service/ParticipationAuthorizationCheckService.java
+++ src/main/java/de/tum/in/www1/artemis/service/ParticipationAuthorizationCheckService.java
@@ -2,8 +2,6 @@ package de.tum.in.www1.artemis.service;
 
 import static de.tum.in.www1.artemis.config.Constants.PROFILE_CORE;
 
-import java.time.ZonedDateTime;
-
 import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
@@ -103,8 +101,7 @@ public class ParticipationAuthorizationCheckService {
      */
     private boolean canAccessProgrammingParticipation(final ProgrammingExerciseParticipation participation, final User user) {
         // If the current user is owner of the participation, they are allowed to access it
-        boolean canParticipate = participation.getExercise().getParticipationStartDate().isBefore(ZonedDateTime.now());
-        if (participation instanceof ProgrammingExerciseStudentParticipation studentParticipation && studentParticipation.isOwnedBy(user) && canParticipate) {
+        if (participation instanceof ProgrammingExerciseStudentParticipation studentParticipation && studentParticipation.isOwnedBy(user)) {
             return true;
         }
 
