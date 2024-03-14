45da5a5a9fe031252a4b0d9045b14d51968c1ead
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2024-03-14 13:49:14 +0100

    Update LtiQuizIntegrationTest.java

--- src/test/java/de/tum/in/www1/artemis/LtiQuizIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/LtiQuizIntegrationTest.java
@@ -32,7 +32,9 @@ import de.tum.in.www1.artemis.course.CourseUtilService;
 import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.enumeration.AssessmentType;
 import de.tum.in.www1.artemis.domain.enumeration.QuizMode;
-import de.tum.in.www1.artemis.domain.quiz.*;
+import de.tum.in.www1.artemis.domain.quiz.DragAndDropQuestion;
+import de.tum.in.www1.artemis.domain.quiz.QuizExercise;
+import de.tum.in.www1.artemis.domain.quiz.QuizSubmission;
 import de.tum.in.www1.artemis.exercise.quizexercise.QuizExerciseFactory;
 import de.tum.in.www1.artemis.participation.ParticipationUtilService;
 import de.tum.in.www1.artemis.repository.CourseRepository;
@@ -44,7 +46,7 @@ import de.tum.in.www1.artemis.user.UserUtilService;
 import de.tum.in.www1.artemis.web.websocket.QuizSubmissionWebsocketService;
 
 @Isolated
-class LtiQuizIntegrationTest extends AbstractSpringIntegrationBambooBitbucketJiraTest {
+class LtiQuizIntegrationTest extends AbstractSpringIntegrationIndependentTest {
 
     private static final String TEST_PREFIX = "ltiquizsubmissiontest";
 
