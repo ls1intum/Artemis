307c1a5642c961da9a0605c2bfb8469750fa5cdf
Andreas Resch <andreas@resch.io>
2024-03-15 16:10:32 +0100

    Development: Clean up build script templates (#8166)

--- src/main/resources/templates/aeolus/haskell/default_sequential.sh
+++ src/main/resources/templates/aeolus/haskell/default_sequential.sh
@@ -19,26 +19,12 @@ run_behavior_tests () {
 
 }
 
-junit () {
-  echo '⚙️ executing junit'
-  #empty script action, just for the results
-}
-
-final_aeolus_post_action () {
-  set +e # from now on, we don't exit on errors
-  echo '⚙️ executing final_aeolus_post_action'
-  cd "${AEOLUS_INITIAL_DIRECTORY}"
-  junit
-}
-
 main () {
   if [[ "${1}" == "aeolus_sourcing" ]]; then
     return 0 # just source to use the methods in the subshell, no execution
   fi
   local _script_name
   _script_name=${BASH_SOURCE[0]:-$0}
-  trap final_aeolus_post_action EXIT
-
   cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; run_structural_tests"
   cd "${AEOLUS_INITIAL_DIRECTORY}"
