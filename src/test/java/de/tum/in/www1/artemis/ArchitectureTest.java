cc741729afd936051c6bdcf7f5cc970f35c09022
Raphael Stief <raphael.stief@gmail.com>
2024-03-15 14:40:30 +0100

    Fix ArchitectureTest

--- src/test/java/de/tum/in/www1/artemis/ArchitectureTest.java
+++ src/test/java/de/tum/in/www1/artemis/ArchitectureTest.java
@@ -41,6 +41,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.slf4j.Logger;
