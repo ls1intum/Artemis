15225406518c46ae7b5ffbb29b5ec1fcd0732b8d
Stephan Krusche <krusche@in.tum.de>
2024-03-17 17:02:13 +0100

    update more server dependencies

--- src/main/java/de/tum/in/www1/artemis/config/Jersey3TransportClientFactoriesConfig.java
+++ src/main/java/de/tum/in/www1/artemis/config/Jersey3TransportClientFactoriesConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
 
+/**
+ * This class is only necessary since Spring Boot 3. If it is not available, the application server would not start in a configuration for a multi-node setup
+ * The app would crash during startup when trying to connect to the registry, if this configuration and bean is not available
+ */
 @Configuration
 public class Jersey3TransportClientFactoriesConfig {
 
