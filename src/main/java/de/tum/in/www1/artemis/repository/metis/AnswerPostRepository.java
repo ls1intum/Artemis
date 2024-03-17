77951ad27fee3da6ea40645732eddcca71c21750
Patrik Zander <patrik.zander@tum.de>
2024-03-17 15:46:23 +0100

    (hack) Load post when loading answerPost (repository accessed getPost().getConversation()) findById(AnswerMessageService).

--- src/main/java/de/tum/in/www1/artemis/repository/metis/AnswerPostRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/metis/AnswerPostRepository.java
@@ -3,11 +3,14 @@ package de.tum.in.www1.artemis.repository.metis;
 import static de.tum.in.www1.artemis.config.Constants.PROFILE_CORE;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.constraints.NotNull;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.metis.AnswerPost;
@@ -22,20 +25,29 @@ public interface AnswerPostRepository extends JpaRepository<AnswerPost, Long> {
 
     List<AnswerPost> findAnswerPostsByAuthorId(long authorId);
 
+    @Query("""
+                SELECT answerPost
+                FROM AnswerPost answerPost
+                    LEFT JOIN FETCH answerPost.post post
+                    LEFT JOIN FETCH post.conversation
+                WHERE answerPost.id = :answerPostId
+            """)
+    Optional<AnswerPost> findWithPostById(@Param("answerPostId") Long answerPostId);
+
     @NotNull
     default AnswerPost findAnswerPostByIdElseThrow(Long answerPostId) {
-        return findById(answerPostId).filter(answerPost -> answerPost.getPost().getConversation() == null)
+        return findWithPostById(answerPostId).filter(answerPost -> answerPost.getPost().getConversation() == null)
                 .orElseThrow(() -> new EntityNotFoundException("Answer Post", answerPostId));
     }
 
     @NotNull
     default AnswerPost findAnswerMessageByIdElseThrow(Long answerPostId) {
-        return findById(answerPostId).filter(answerPost -> answerPost.getPost().getConversation() != null)
+        return findWithPostById(answerPostId).filter(answerPost -> answerPost.getPost().getConversation() != null)
                 .orElseThrow(() -> new EntityNotFoundException("Answer Post", answerPostId));
     }
 
     @NotNull
     default AnswerPost findAnswerPostOrAnswerMessageByIdElseThrow(Long answerPostId) {
-        return findById(answerPostId).orElseThrow(() -> new EntityNotFoundException("Answer Post", answerPostId));
+        return findWithPostById(answerPostId).orElseThrow(() -> new EntityNotFoundException("Answer Post", answerPostId));
     }
 }
