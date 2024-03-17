751fa328eab84e936a464fd062dcc08e7f774df5
Patrik Zander <patrik.zander@tum.de>
2024-03-17 16:05:50 +0100

    (hack) Fetch relations when loading reaction. deleteReactionById(ReactionService)

--- src/main/java/de/tum/in/www1/artemis/repository/metis/ReactionRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/metis/ReactionRepository.java
@@ -3,9 +3,12 @@ package de.tum.in.www1.artemis.repository.metis;
 import static de.tum.in.www1.artemis.config.Constants.PROFILE_CORE;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.metis.Reaction;
@@ -24,7 +27,18 @@ public interface ReactionRepository extends JpaRepository<Reaction, Long> {
 
     List<Reaction> findReactionsByAnswerPostId(Long answerPostId);
 
+    @Query("""
+                SELECT reaction
+                FROM Reaction reaction
+                    LEFT JOIN FETCH reaction.post
+                    LEFT JOIN FETCH reaction.answerPost answerPost
+                    LEFT JOIN FETCH answerPost.post post
+                    LEFT JOIN FETCH post.conversation
+                WHERE reaction.id = :reactionId
+            """)
+    Optional<Reaction> findWithAnswerPostOrPostById(@Param("reactionId") Long reactionId);
+
     default Reaction findByIdElseThrow(Long reactionId) throws EntityNotFoundException {
-        return findById(reactionId).orElseThrow(() -> new EntityNotFoundException("Reaction", reactionId));
+        return findWithAnswerPostOrPostById(reactionId).orElseThrow(() -> new EntityNotFoundException("Reaction", reactionId));
     }
 }
