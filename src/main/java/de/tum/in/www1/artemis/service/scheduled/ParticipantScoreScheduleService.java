4669f6337df8129118aa06382a304bd7ba066400
Stephan Krusche <krusche@in.tum.de>
2024-03-19 20:46:08 +0100

    add architecture tests for services and fix issues with wrong imports
    remove code that is related to a separate feature
    
    revert unrelated changes

--- src/main/java/de/tum/in/www1/artemis/service/scheduled/ParticipantScoreScheduleService.java
+++ src/main/java/de/tum/in/www1/artemis/service/scheduled/ParticipantScoreScheduleService.java
@@ -143,7 +143,7 @@ public class ParticipantScoreScheduleService {
      * Additionally, we schedule all participant scores that are outdated/invalid.
      */
     @Scheduled(cron = "0 * * * * *")
-    protected void scheduleTasks() {
+    public void scheduleTasks() {
         log.debug("Schedule tasks to process...");
         SecurityUtils.setAuthorizationObject();
         if (isRunning.get()) {
