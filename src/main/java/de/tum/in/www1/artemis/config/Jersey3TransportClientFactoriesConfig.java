bf1cb73651d0c886c04720fd2bc810af79f592dd
Stephan Krusche <krusche@in.tum.de>
2024-03-19 13:08:37 +0100

    add missing profile to new config

--- src/main/java/de/tum/in/www1/artemis/config/Jersey3TransportClientFactoriesConfig.java
+++ src/main/java/de/tum/in/www1/artemis/config/Jersey3TransportClientFactoriesConfig.java
@@ -1,7 +1,11 @@
 package de.tum.in.www1.artemis.config;
 
+import static de.tum.in.www1.artemis.config.Constants.PROFILE_BUILDAGENT;
+import static de.tum.in.www1.artemis.config.Constants.PROFILE_CORE;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
 
@@ -9,6 +13,7 @@ import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFact
  * This class is only necessary since Spring Boot 3. If it is not available, the application server would not start in a configuration for a multi-node setup
  * The app would crash during startup when trying to connect to the registry, if this configuration and bean is not available
  */
+@Profile({ PROFILE_CORE, PROFILE_BUILDAGENT })
 @Configuration
 public class Jersey3TransportClientFactoriesConfig {
 
