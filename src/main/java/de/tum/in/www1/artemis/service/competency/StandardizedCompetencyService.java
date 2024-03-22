254fd50b575df32f3dc4f9c8ba48b4a5a27af45e
Raphael Stief <raphael.stief@gmail.com>
2024-03-22 13:20:43 +0100

    Add update and delete endpoint

--- src/main/java/de/tum/in/www1/artemis/service/competency/StandardizedCompetencyService.java
+++ src/main/java/de/tum/in/www1/artemis/service/competency/StandardizedCompetencyService.java
@@ -13,6 +13,7 @@ import de.tum.in.www1.artemis.domain.competency.StandardizedCompetency;
 import de.tum.in.www1.artemis.repository.SourceRepository;
 import de.tum.in.www1.artemis.repository.competency.KnowledgeAreaRepository;
 import de.tum.in.www1.artemis.repository.competency.StandardizedCompetencyRepository;
+import de.tum.in.www1.artemis.web.rest.errors.EntityNotFoundException;
 
 /**
  * Service for managing {@link StandardizedCompetency} entities.
@@ -63,6 +64,55 @@ public class StandardizedCompetencyService {
         // set the reference to the first version of this standardized competency (itself)
         competencyToCreate.setFirstVersion(competencyToCreate);
         return standardizedCompetencyRepository.save(competencyToCreate);
+    }
+
+    /**
+     * Updates an existing standardized competency with the provided competency data
+     *
+     * @param competency the new competency values
+     * @return the updated standardized competency
+     */
+    public StandardizedCompetency updateStandardizedCompetency(StandardizedCompetency competency) {
+        var existingCompetency = standardizedCompetencyRepository.findByIdElseThrow(competency.getId());
+
+        if (competency.getVersion() != null && !competency.getVersion().equals(existingCompetency.getVersion())) {
+            throw new BadRequestException("You cannot change the version of a standardized competency");
+        }
+        if (competency.getFirstVersion() != null && !competency.getFirstVersion().equals(existingCompetency.getFirstVersion())) {
+            throw new BadRequestException("You cannot change the first version of a standardized competency");
+        }
+
+        existingCompetency.setTitle(competency.getTitle());
+        existingCompetency.setDescription(competency.getDescription());
+        existingCompetency.setTaxonomy(competency.getTaxonomy());
+        if (competency.getSource() == null) {
+            existingCompetency.setSource(null);
+        }
+        else if (!competency.getSource().equals(existingCompetency.getSource())) {
+            var source = sourceRepository.findByIdElseThrow(competency.getSource().getId());
+            existingCompetency.setSource(source);
+        }
+        if (competency.getKnowledgeArea() == null) {
+            existingCompetency.setKnowledgeArea(null);
+        }
+        else if (!competency.getKnowledgeArea().equals(existingCompetency.getKnowledgeArea())) {
+            var knowledgeArea = knowledgeAreaRepository.findByIdElseThrow(competency.getKnowledgeArea().getId());
+            existingCompetency.setKnowledgeArea(knowledgeArea);
+        }
+
+        return standardizedCompetencyRepository.save(existingCompetency);
+    }
+
+    /**
+     * Deletes an existing standardized competency with the given id or throws an EntityNotFoundException
+     *
+     * @param competencyId the id of the competency to delete
+     */
+    public void deleteStandardizedCompetencyElseThrow(long competencyId) throws EntityNotFoundException {
+        if (!standardizedCompetencyRepository.existsById(competencyId)) {
+            throw new EntityNotFoundException("StandardizedCompetency", competencyId);
+        }
+        standardizedCompetencyRepository.deleteById(competencyId);
     }
 
     /**
