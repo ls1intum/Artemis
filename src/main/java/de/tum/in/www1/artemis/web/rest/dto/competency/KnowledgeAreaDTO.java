13b08280b3e855cbc178ffbb10b36165dc802e99
Raphael Stief <raphael.stief@gmail.com>
2024-03-29 20:26:03 +0100

    Add german translation, add javadoc

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/KnowledgeAreaDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/KnowledgeAreaDTO.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import de.tum.in.www1.artemis.domain.competency.KnowledgeArea;
+
+/**
+ * DTO containing {@link KnowledgeArea} data. It only contains the id of its parent.
+ */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record KnowledgeAreaDTO(long id, String title, String description, Long parentId, List<KnowledgeAreaDTO> children, List<StandardizedCompetencyDTO> competencies) {
 }
--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/StandardizedCompetencyDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/StandardizedCompetencyDTO.java
@@ -3,7 +3,11 @@ package de.tum.in.www1.artemis.web.rest.dto.competency;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.in.www1.artemis.domain.competency.CompetencyTaxonomy;
+import de.tum.in.www1.artemis.domain.competency.StandardizedCompetency;
 
+/**
+ * DTO containing {@link StandardizedCompetency} data. It only contains the id of the knowledge area, source.
+ */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record StandardizedCompetencyDTO(long id, String title, String description, CompetencyTaxonomy taxonomy, String version, Long knowledgeAreaId, Long sourceId) {
 
