2f3ceb40a1bfd314c8309fd9e6a932acd06408fb
Stephan Krusche <krusche@tum.de>
2024-04-01 18:35:35 +0200

    Development: Prefer non empty json include annotation (#8224)

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/QuizBatchJoinDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/QuizBatchJoinDTO.java
@@ -4,6 +4,6 @@ import jakarta.annotation.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record QuizBatchJoinDTO(@Nullable String password) {
 }
