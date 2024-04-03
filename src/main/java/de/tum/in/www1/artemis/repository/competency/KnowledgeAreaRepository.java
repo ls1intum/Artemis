71060f1669cec21df7389b7273b9dc5a17603f0c
Raphael Stief <raphael.stief@gmail.com>
2024-04-03 13:37:05 +0200

    first version of query

--- src/main/java/de/tum/in/www1/artemis/repository/competency/KnowledgeAreaRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/competency/KnowledgeAreaRepository.java
@@ -11,6 +11,8 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.tum.in.www1.artemis.domain.competency.KnowledgeArea;
@@ -38,4 +40,26 @@ public interface KnowledgeAreaRepository extends JpaRepository<KnowledgeArea, Lo
     default KnowledgeArea findByIdElseThrow(long knowledgeAreaId) throws EntityNotFoundException {
         return findById(knowledgeAreaId).orElseThrow(() -> new EntityNotFoundException("KnowledgeArea", knowledgeAreaId));
     }
+
+    @Query(nativeQuery = true, value = """
+                WITH RECURSIVE recurse(id) AS (
+                SELECT id, parent, children
+                FROM knowledge_areas
+                WHERE id = :id1
+                UNION
+                SELECT ka.id, ka.parent, ka.children
+                FROM knowledge_areas ka
+                INNER JOIN recurse r ON ka.id = r.parent
+            )
+            SELECT CASE
+                       WHEN EXISTS (
+                           SELECT 1
+                           FROM recurse
+                           WHERE id = :id2
+                       ) THEN 'Yes'
+                       ELSE 'No'
+                   END AS IsDescendant
+            WHERE id = :id1
+            """)
+    String isDescendant(@Param("id1") long id1, @Param("id2") long id2);
 }
