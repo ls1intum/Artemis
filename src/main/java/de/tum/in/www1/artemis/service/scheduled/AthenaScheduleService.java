c9cc81461a90e7f07a4b166378c973fd015d4821
Timor Morrien <timor.morrien@tum.de>
2024-04-03 20:44:53 +0200

    Integrated code lifecycle: Export metrics for monitoring (#8292)

--- src/main/java/de/tum/in/www1/artemis/service/scheduled/AthenaScheduleService.java
+++ src/main/java/de/tum/in/www1/artemis/service/scheduled/AthenaScheduleService.java
@@ -49,7 +49,7 @@ public class AthenaScheduleService {
      */
     @PostConstruct
     public void scheduleRunningExercisesOnStartup() {
-        if (profileService.isDev()) {
+        if (profileService.isDevActive()) {
             // only execute this on production server, i.e. when the prod profile is active
             // NOTE: if you want to test this locally, please comment it out, but do not commit the changes
             return;
