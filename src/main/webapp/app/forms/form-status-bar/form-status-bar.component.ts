01387af1dfdfaf89748bc00af5fd830f111f80f7
Jonathan Ostertag <jo.ostertag@tum.de>
2024-04-05 14:20:01 +0200

    chore: fix sticky bars top

--- src/main/webapp/app/forms/form-status-bar/form-status-bar.component.ts
+++ src/main/webapp/app/forms/form-status-bar/form-status-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Input } from '@angular/core';
 
 export type FormSectionStatus = {
     title: string;
@@ -11,9 +11,20 @@ export type FormSectionStatus = {
     templateUrl: './form-status-bar.component.html',
     styleUrl: './form-status-bar.component.scss',
 })
-export class FormStatusBarComponent {
+export class FormStatusBarComponent implements AfterViewInit {
     @Input()
     formStatusSections: FormSectionStatus[];
+
+    headerHeight? = 0;
+
+    @HostListener('window:resize')
+    onResize() {
+        setTimeout(() => (this.headerHeight = (document.querySelector('jhi-navbar') as HTMLElement).offsetHeight));
+    }
+
+    ngAfterViewInit() {
+        this.onResize();
+    }
 
     scrollToHeadline(id: string) {
         const element = document.getElementById(id);
--- src/main/webapp/app/shared/detail-overview-navigation-bar/detail-overview-navigation-bar.component.ts
+++ src/main/webapp/app/shared/detail-overview-navigation-bar/detail-overview-navigation-bar.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Input } from '@angular/core';
 
 @Component({
     selector: 'jhi-detail-overview-navigation-bar',
     templateUrl: './detail-overview-navigation-bar.component.html',
     styleUrls: ['./detail-overview-navigation-bar.scss'],
 })
-export class DetailOverviewNavigationBarComponent {
+export class DetailOverviewNavigationBarComponent implements AfterViewInit {
     @Input()
     sectionHeadlines: { id: string; translationKey: string }[];
+
+    headerHeight = 0;
+
+    @HostListener('window:resize')
+    onResize() {
+        setTimeout(() => (this.headerHeight = (document.querySelector('jhi-navbar') as HTMLElement).offsetHeight));
+    }
+
+    ngAfterViewInit() {
+        this.onResize();
+    }
 
     scrollToView(id: string) {
         document.getElementById(id)?.scrollIntoView();
