06a30fbbf11616efc92b05b969d6be1bc798ea17
Stephan Krusche <krusche@in.tum.de>
2024-04-10 23:57:15 +0200

    remove one more gson

--- src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIServiceTest.java
@@ -12,8 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import com.google.gson.Gson;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.tum.in.www1.artemis.AbstractSpringIntegrationLocalCILocalVCTest;
 import de.tum.in.www1.artemis.domain.Course;
@@ -71,10 +70,10 @@ class LocalCIServiceTest extends AbstractSpringIntegrationLocalCILocalVCTest {
         exercise.setBuildScript(script);
         exercise.setBuildPlanConfiguration(null);
         continuousIntegrationService.recreateBuildPlansForExercise(exercise);
-        script = buildScriptProviderService.getScriptFor(exercise.getProgrammingLanguage(), Optional.of(exercise.getProjectType()), exercise.isStaticCodeAnalysisEnabled(),
+        script = buildScriptProviderService.getScriptFor(exercise.getProgrammingLanguage(), Optional.ofNullable(exercise.getProjectType()), exercise.isStaticCodeAnalysisEnabled(),
                 exercise.hasSequentialTestRuns(), exercise.isTestwiseCoverageEnabled());
         Windfile windfile = aeolusTemplateService.getDefaultWindfileFor(exercise);
-        assertThat(exercise.getBuildPlanConfiguration()).isEqualTo(new Gson().toJson(windfile));
+        assertThat(exercise.getBuildPlanConfiguration()).isEqualTo(new ObjectMapper().writeValueAsString(windfile));
         assertThat(exercise.getBuildScript()).isEqualTo(script);
         // test that the method does not throw an exception when the exercise is null
         continuousIntegrationService.recreateBuildPlansForExercise(null);
