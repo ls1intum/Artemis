10c3d91db2a6ed21e8cd6c220ec99ae36de309eb
Raphael Stief <raphael.stief@gmail.com>
2024-04-24 13:25:48 +0200

    Extract filter functionality, further improve import component

--- src/main/webapp/app/course/competencies/import-standardized-competencies/course-import-standardized-competencies.component.ts
+++ src/main/webapp/app/course/competencies/import-standardized-competencies/course-import-standardized-competencies.component.ts
@@ -1,9 +1,9 @@
 import { getIcon } from 'app/entities/competency.model';
-import { ButtonType } from 'app/shared/components/button.component';
+import { ButtonSize, ButtonType } from 'app/shared/components/button.component';
 import { KnowledgeAreaForTree, StandardizedCompetencyDTO, convertToKnowledgeAreaForTree } from 'app/entities/competency/standardized-competency.model';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { NestedTreeControl } from '@angular/cdk/tree';
-import { faBan, faFileImport } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faDownLeftAndUpRightToCenter, faFileImport, faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { onError } from 'app/shared/util/global.utils';
@@ -11,39 +11,13 @@ import { StandardizedCompetencyService } from 'app/admin/standardized-competenci
 import { map } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AlertService } from 'app/core/util/alert.service';
+import { FilterableKnowledgeAreaTreeComponent } from 'app/shared/standardized-competencies/filterable-knowledge-area-tree.component';
 
 @Component({
     selector: 'jhi-course-import-standardized-competencies',
     templateUrl: './course-import-standardized-competencies.component.html',
 })
-export class CourseImportStandardizedCompetenciesComponent implements OnInit {
-    constructor(
-        private router: Router,
-        private activatedRoute: ActivatedRoute,
-        private standardizedCompetencyService: StandardizedCompetencyService,
-        private alertService: AlertService,
-    ) {}
-
-    ngOnInit(): void {
-        this.isLoading = true;
-        this.standardizedCompetencyService
-            .getAllForTreeView()
-            .pipe(map((response) => response.body!))
-            .subscribe({
-                next: (knowledgeAreas) => {
-                    const knowledgeAreasForTree = knowledgeAreas.map((knowledgeArea) => convertToKnowledgeAreaForTree(knowledgeArea));
-                    this.dataSource.data = knowledgeAreasForTree;
-                    this.treeControl.dataNodes = knowledgeAreasForTree;
-                    /** TODO: re-add with filter.
-                    knowledgeAreasForTree.forEach((knowledgeArea) => {
-                        this.addSelfAndDescendantsToSelectArray(knowledgeArea);
-                    }); */
-                    this.isLoading = false;
-                },
-                error: (errorResponse: HttpErrorResponse) => onError(this.alertService, errorResponse),
-            });
-    }
-
+export class CourseImportStandardizedCompetenciesComponent extends FilterableKnowledgeAreaTreeComponent implements OnInit {
     protected competenciesToImport: StandardizedCompetencyDTO[] = [];
     protected isLoading = false;
 
@@ -56,6 +30,39 @@ export class CourseImportStandardizedCompetenciesComponent implements OnInit {
     // icons
     protected readonly faBan = faBan;
     protected readonly faFileImport = faFileImport;
+    protected readonly faMinimize = faDownLeftAndUpRightToCenter;
+    protected readonly faMaximize = faUpRightAndDownLeftFromCenter;
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private standardizedCompetencyService: StandardizedCompetencyService,
+        private alertService: AlertService,
+    ) {
+        super();
+    }
+
+    //TODO: rename and move kaForTree -> Filterable KA tree?
+
+    ngOnInit(): void {
+        this.isLoading = true;
+        this.standardizedCompetencyService
+            .getAllForTreeView()
+            .pipe(map((response) => response.body!))
+            .subscribe({
+                next: (knowledgeAreas) => {
+                    const knowledgeAreasForTree = knowledgeAreas.map((knowledgeArea) => convertToKnowledgeAreaForTree(knowledgeArea));
+                    this.dataSource.data = knowledgeAreasForTree;
+                    this.treeControl.dataNodes = knowledgeAreasForTree;
+                    knowledgeAreasForTree.forEach((knowledgeArea) => {
+                        this.addSelfAndDescendantsToMap(knowledgeArea);
+                        this.addSelfAndDescendantsToSelectArray(knowledgeArea);
+                    });
+                    this.isLoading = false;
+                },
+                error: (errorResponse: HttpErrorResponse) => onError(this.alertService, errorResponse),
+            });
+    }
 
     protected importCompetencies() {
         //TODO: import
@@ -65,4 +72,6 @@ export class CourseImportStandardizedCompetenciesComponent implements OnInit {
     protected cancel() {
         this.router.navigate(['../'], { relativeTo: this.activatedRoute });
     }
+
+    protected readonly ButtonSize = ButtonSize;
 }
--- src/main/webapp/app/shared/standardized-competencies/standardized-competency.module.ts
+++ src/main/webapp/app/shared/standardized-competencies/standardized-competency.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatTreeModule } from '@angular/material/tree';
 import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
 import { KnowledgeAreaTreeComponent } from 'app/shared/standardized-competencies/knowledge-area-tree.component';
+import { StandardizedCompetencyFilterComponent } from 'app/shared/standardized-competencies/standardized-competency-filter.component';
 
 @NgModule({
     imports: [ArtemisSharedCommonModule, MatTreeModule],
-    declarations: [KnowledgeAreaTreeComponent],
-    exports: [KnowledgeAreaTreeComponent],
+    declarations: [KnowledgeAreaTreeComponent, StandardizedCompetencyFilterComponent],
+    exports: [KnowledgeAreaTreeComponent, StandardizedCompetencyFilterComponent],
 })
 export class ArtemisStandardizedCompetencyModule {}
