afe001ac5e77d8706a015d5d7b58f7669a54e498
Florian Glombik <florian.glombik@tum.de>
2024-04-26 12:18:20 +0200

    Reducing Maximum Timeout amount

--- src/test/cypress/support/requests/ExerciseAPIRequests.ts
+++ src/test/cypress/support/requests/ExerciseAPIRequests.ts
@@ -51,8 +51,8 @@ type PatchProgrammingExerciseTestVisibilityDto = {
     visibility: Visibility;
 }[];
 
-const MAX_RETRIES: number = 15;
-const RETRY_DELAY: number = 5000;
+const MAX_RETRIES: number = 10;
+const RETRY_DELAY: number = 3000;
 
 /**
  * A class which encapsulates all API requests related to exercises.
--- src/test/playwright/support/requests/ExerciseAPIRequests.ts
+++ src/test/playwright/support/requests/ExerciseAPIRequests.ts
@@ -48,8 +48,8 @@ type PatchProgrammingExerciseTestVisibilityDto = {
     visibility: Visibility;
 }[];
 
-const MAX_RETRIES: number = 15;
-const RETRY_DELAY: number = 5000;
+const MAX_RETRIES: number = 10;
+const RETRY_DELAY: number = 3000;
 
 export class ExerciseAPIRequests {
     private readonly page: Page;
