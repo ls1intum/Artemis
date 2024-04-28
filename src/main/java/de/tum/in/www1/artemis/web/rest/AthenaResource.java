fdedb706ca8fe4d374dbd3dc5addad5d68ed95f0
Matthias Lehner <ga59bip@tum.de>
2024-04-28 14:22:40 +0200

    Clean up imports

--- src/main/java/de/tum/in/www1/artemis/web/rest/AthenaResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/AthenaResource.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import de.tum.in.www1.artemis.domain.Course;
 import de.tum.in.www1.artemis.domain.Exercise;
 import de.tum.in.www1.artemis.domain.Submission;
-
 import de.tum.in.www1.artemis.domain.enumeration.ExerciseType;
 import de.tum.in.www1.artemis.domain.enumeration.RepositoryType;
 import de.tum.in.www1.artemis.exception.NetworkingException;
