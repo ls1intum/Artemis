3a7f26d80062672c27391e4f30cef90d31deb10b
Murad Talibov <muradtalibov2@gmail.com>
2024-04-29 23:23:46 +0200

    Use selectors instead of IDs

--- src/test/playwright/e2e/SystemHealth.spec.ts
+++ src/test/playwright/e2e/SystemHealth.spec.ts
@@ -4,15 +4,15 @@ import { Page, expect } from '@playwright/test';
 import { Commands } from '../support/commands';
 
 const healthChecks = [
-    { id: 'continuousIntegrationServer', name: 'continuous integration server', expectedStatus: 'UP' },
-    { id: 'versionControlServer', name: 'version control server', expectedStatus: 'UP' },
-    { id: 'userManagement', name: 'user management', expectedStatus: 'UP' },
-    { id: 'db', name: 'db', expectedStatus: 'UP' },
-    { id: 'hazelcast', name: 'hazelcast', expectedStatus: 'UP' },
-    { id: 'ping', name: 'ping', expectedStatus: 'UP' },
-    { id: 'readinessState', name: 'readiness state', expectedStatus: 'UP' },
-    { id: 'websocketBroker', name: 'websocket broker', expectedStatus: 'UP' },
-    { id: 'websocketConnection', name: 'websocket connection', expectedStatus: 'Connected' },
+    { selector: '#continuousIntegrationServer', name: 'continuous integration server', expectedStatus: 'UP' },
+    { selector: '#versionControlServer', name: 'version control server', expectedStatus: 'UP' },
+    { selector: '#userManagement', name: 'user management', expectedStatus: 'UP' },
+    { selector: '#db', name: 'db', expectedStatus: 'UP' },
+    { selector: '#hazelcast', name: 'hazelcast', expectedStatus: 'UP' },
+    { selector: '#ping', name: 'ping', expectedStatus: 'UP' },
+    { selector: '#readinessState', name: 'readiness state', expectedStatus: 'UP' },
+    { selector: '#websocketBroker', name: 'websocket broker', expectedStatus: 'UP' },
+    { selector: '#websocketConnection', name: 'websocket connection', expectedStatus: 'Connected' },
 ];
 
 test.describe('Check artemis system health', () => {
@@ -27,43 +27,7 @@ test.describe('Check artemis system health', () => {
 
     for (const healthCheck of healthChecks) {
         test(`Checks ${healthCheck.name} health`, async () => {
-            await expect(page.locator(`#healthCheck #${healthCheck.id} .status`)).toHaveText(healthCheck.expectedStatus);
+            await expect(page.locator(`#healthCheck ${healthCheck.selector} .status`)).toHaveText(healthCheck.expectedStatus);
         });
     }
-
-    // test('Checks continuous integration health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #continuousIntegrationServer .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks version control health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #versionControlServer .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks user management health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #userManagement .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks database health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #db .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks hazelcast health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #hazelcast .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks ping health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #ping .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks readiness health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #readinessState .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks websocket broker health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #websocketBroker .status')).toHaveText('UP');
-    // });
-    //
-    // test('Checks websocket connection health', async ({ page }) => {
-    //     await expect(page.locator('#healthCheck #websocketConnection .status')).toHaveText('Connected');
-    // });
 });
