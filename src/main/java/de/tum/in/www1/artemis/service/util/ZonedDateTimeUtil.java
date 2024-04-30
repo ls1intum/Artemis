3125972f3dab578e25a01b4321871d2d6355145e
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2024-04-30 15:28:59 +0200

    remove unused

--- src/main/java/de/tum/in/www1/artemis/service/util/ZonedDateTimeUtil.java
+++ src/main/java/de/tum/in/www1/artemis/service/util/ZonedDateTimeUtil.java
@@ -1,12 +1,8 @@
 package de.tum.in.www1.artemis.service.util;
 
-import java.time.Instant;
-import java.time.ZonedDateTime;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.stream.LongStream;
+import jakarta.validation.constraints.NotNull;
 
-import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
 
 public class ZonedDateTimeUtil {
 
@@ -15,30 +11,6 @@ public class ZonedDateTimeUtil {
      */
     private ZonedDateTimeUtil() {
         throw new IllegalStateException("Utility class");
-    }
-
-    public static <T extends ZonedDateTime> LongStream getEpochSecondStream(@NotNull Collection<T> zonedDateTimes) {
-        return zonedDateTimes.stream().mapToLong(ZonedDateTime::toEpochSecond);
-    }
-
-    /**
-     * Get the average ZonedDateTime of a collection of ZonedDateTime objects.
-     *
-     * @param zonedDateTimes the collection of ZonedDateTime objects
-     * @return optional of the average ZonedDateTime object, empty if the collection is empty
-     */
-    public static Optional<ZonedDateTime> getZonedDateTimeAverage(@NotNull Collection<ZonedDateTime> zonedDateTimes) {
-        if (zonedDateTimes == null || zonedDateTimes.isEmpty()) {
-            return Optional.empty();
-        }
-        final var avg = getEpochSecondStream(zonedDateTimes).average();
-        if (avg.isPresent()) {
-            final var epochSecond = Math.round(avg.getAsDouble());
-            final var instant = Instant.ofEpochSecond(epochSecond);
-            final var zoneId = zonedDateTimes.iterator().next().getZone();
-            return Optional.of(ZonedDateTime.ofInstant(instant, zoneId));
-        }
-        return Optional.empty();
     }
 
     /**
