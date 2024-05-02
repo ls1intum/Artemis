6c07137c6fe1df7ec3ea1a17166390a245afecb6
Kaan Çaylı <38523756+kaancayli@users.noreply.github.com>
2024-05-03 01:15:55 +0200

    Extract common functionality to a base class

--- src/main/webapp/app/iris/exercise-chatbot/widget/chatbot-widget.component.ts
+++ src/main/webapp/app/iris/exercise-chatbot/widget/chatbot-widget.component.ts
@@ -15,17 +15,10 @@ import {
 import { NavigationStart, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ButtonType } from 'app/shared/components/button.component';
-import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { IrisStateStore } from 'app/iris/state-store.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import {
-    ActiveConversationMessageLoadedAction,
-    ConversationErrorOccurredAction,
-    NumNewMessagesResetAction,
-    RateMessageSuccessAction,
-    StudentMessageSentAction,
-} from 'app/iris/state-store.model';
+import { ActiveConversationMessageLoadedAction, ConversationErrorOccurredAction, NumNewMessagesResetAction, StudentMessageSentAction } from 'app/iris/state-store.model';
 import {
     IrisArtemisClientMessage,
     IrisMessage,
@@ -35,11 +28,10 @@ import {
     isServerSentMessage,
     isStudentSentMessage,
 } from 'app/entities/iris/iris-message.model';
-import { IrisMessageContent, IrisMessageContentType, IrisTextMessageContent, getTextContent, isTextContent } from 'app/entities/iris/iris-content-type.model';
-import { Subscription } from 'rxjs';
+import { IrisMessageContentType, IrisTextMessageContent, getTextContent, isTextContent } from 'app/entities/iris/iris-content-type.model';
 import { SharedService } from 'app/iris/shared.service';
 import { IrisSessionService } from 'app/iris/session.service';
-import { IrisErrorMessageKey, IrisErrorType } from 'app/entities/iris/iris-errors.model';
+import { IrisErrorMessageKey } from 'app/entities/iris/iris-errors.model';
 import dayjs from 'dayjs/esm';
 import { AnimationEvent, animate, state, style, transition, trigger } from '@angular/animations';
 import { UserService } from 'app/core/user/user.service';
@@ -48,6 +40,7 @@ import interact from 'interactjs';
 import { DOCUMENT } from '@angular/common';
 import { IrisChatSessionService } from 'app/iris/chat-session.service';
 import { TranslateService } from '@ngx-translate/core';
+import { IrisBaseChatbotComponent } from 'app/iris/iris-base-chatbot.component';
 
 @Component({
     selector: 'jhi-chatbot-widget',
@@ -71,7 +64,7 @@ import { TranslateService } from '@ngx-translate/core';
         ]),
     ],
 })
-export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewInit {
+export class IrisChatbotWidgetComponent extends IrisBaseChatbotComponent implements OnInit, OnDestroy, AfterViewInit {
     // Icons
     faTrash = faTrash;
     faCircle = faCircle;
@@ -86,23 +79,11 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
     faThumbsDown = faThumbsDown;
     faRedo = faRedo;
 
-    // ViewChilds
-    @ViewChild('chatBody') chatBody!: ElementRef;
-    @ViewChild('scrollArrow') scrollArrow!: ElementRef;
-    @ViewChild('messageTextarea', { static: false }) messageTextarea: ElementRef<HTMLTextAreaElement>;
-    @ViewChild('unreadMessage', { static: false }) unreadMessage!: ElementRef;
-
     // State variables
     stateStore: IrisStateStore;
-    stateSubscription: Subscription;
     messages: IrisMessage[] = [];
-    content: IrisMessageContent;
     newMessageTextContent = '';
-    isLoading: boolean;
-    sessionId: number;
     numNewMessages = 0;
-    unreadMessageIndex: number;
-    error: IrisErrorType | null;
     dots = 1;
     resendAnimationActive = false;
     shakeErrorField = false;
@@ -112,13 +93,9 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
     exerciseId: number;
     sessionService: IrisSessionService;
     shouldShowEmptyMessageError = false;
-    currentMessageCount: number;
-    rateLimit: number;
-    rateLimitTimeframeHours: number;
     importExerciseUrl: string;
 
     // User preferences
-    userAccepted: boolean;
     isScrolledToBottom = true;
     rows = 1;
     initialWidth = 330;
@@ -128,19 +105,19 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
     fullSize = false;
     public ButtonType = ButtonType;
     readonly IrisLogoSize = IrisLogoSize;
-    private navigationSubscription: Subscription;
     private readonly MAX_INT_JAVA = 2147483647;
 
     constructor(
         private dialog: MatDialog,
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private userService: UserService,
         private router: Router,
         private sharedService: SharedService,
-        private modalService: NgbModal,
         @Inject(DOCUMENT) private document: Document,
         private translateService: TranslateService,
+        userService: UserService,
+        modalService: NgbModal,
     ) {
+        super(userService, modalService);
         this.stateStore = data.stateStore;
         this.courseId = data.courseId;
         this.exerciseId = data.exerciseId;
@@ -154,33 +131,12 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
     }
 
     ngOnInit() {
-        this.userService.getIrisAcceptedAt().subscribe((res) => {
-            this.userAccepted = !!res;
-            if (this.userAccepted) {
-                this.loadFirstMessage();
-            }
-        });
+        this.checkIfUserAcceptedIris();
 
         this.animateDots();
 
         // Subscribe to state changes
-        this.stateSubscription = this.stateStore.getState().subscribe((state) => {
-            this.messages = state.messages as IrisMessage[];
-            this.isLoading = state.isLoading;
-            this.error = state.error;
-            this.sessionId = Number(state.sessionId);
-            this.numNewMessages = state.numNewMessages;
-            if (state.error?.key == IrisErrorMessageKey.EMPTY_MESSAGE) {
-                this.shouldShowEmptyMessageError = true;
-                this.fadeState = 'start';
-            }
-            if (this.error) {
-                this.getConvertedErrorMap();
-            }
-            this.currentMessageCount = state.currentMessageCount;
-            this.rateLimit = state.rateLimit;
-            this.rateLimitTimeframeHours = state.rateLimitTimeframeHours;
-        });
+        this.subscribeToStateChanges();
 
         // Focus on message textarea
         setTimeout(() => {
@@ -384,34 +340,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
             });
     }
 
-    private handleIrisError(error: HttpErrorResponse) {
-        if (error.status === 403) {
-            this.stateStore.dispatch(new ConversationErrorOccurredAction(IrisErrorMessageKey.IRIS_DISABLED));
-        } else if (error.status === 429) {
-            const map = new Map<string, any>();
-            map.set('hours', this.rateLimitTimeframeHours);
-            this.stateStore.dispatch(new ConversationErrorOccurredAction(IrisErrorMessageKey.RATE_LIMIT_EXCEEDED, map));
-        } else {
-            this.stateStore.dispatch(new ConversationErrorOccurredAction(IrisErrorMessageKey.SEND_MESSAGE_FAILED));
-        }
-    }
-
-    /**
-     * Rates a message as helpful or unhelpful.
-     * @param messageId - The ID of the message to rate.
-     * @param index - The index of the message in the messages array.
-     * @param helpful - A boolean indicating if the message is helpful or not.
-     */
-    rateMessage(messageId: number, index: number, helpful: boolean) {
-        this.sessionService
-            .rateMessage(this.sessionId, messageId, helpful)
-            .then(() => this.stateStore.dispatch(new RateMessageSuccessAction(index, helpful)))
-            .catch(() => {
-                this.stateStore.dispatch(new ConversationErrorOccurredAction(IrisErrorMessageKey.RATE_MESSAGE_FAILED));
-                this.scrollToBottom('smooth');
-            });
-    }
-
     /**
      * Closes the chat widget.
      */
@@ -419,41 +347,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
         this.stateStore.dispatch(new NumNewMessagesResetAction());
         this.sharedService.changeChatOpenStatus(false);
         this.dialog.closeAll();
-    }
-
-    /**
-     * Animates the dots while loading each Iris message in the chat widget.
-     */
-    animateDots() {
-        setInterval(() => {
-            this.dots = this.dots < 3 ? (this.dots += 1) : (this.dots = 1);
-        }, 500);
-    }
-
-    /**
-     * Scrolls to the unread message.
-     */
-    scrollToUnread() {
-        setTimeout(() => {
-            const unreadMessageElement: HTMLElement = this.unreadMessage?.nativeElement;
-            if (unreadMessageElement) {
-                unreadMessageElement.scrollIntoView({ behavior: 'auto' });
-            }
-        });
-    }
-
-    /**
-     * Scrolls the chat body to the bottom.
-     * @param behavior - The scroll behavior.
-     */
-    scrollToBottom(behavior: ScrollBehavior) {
-        setTimeout(() => {
-            const chatBodyElement: HTMLElement = this.chatBody.nativeElement;
-            chatBodyElement.scrollTo({
-                top: chatBodyElement.scrollHeight,
-                behavior: behavior,
-            });
-        });
     }
 
     /**
@@ -465,28 +358,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
         const scrollTop = chatBody.scrollTop;
         const clientHeight = chatBody.clientHeight;
         this.isScrolledToBottom = scrollHeight - scrollTop - clientHeight < 50;
-    }
-
-    /**
-     * Clear session and start a new conversation.
-     */
-    onClearSession(content: any) {
-        this.modalService.open(content).result.then((result: string) => {
-            if (result === 'confirm') {
-                this.isLoading = false;
-                this.createNewSession();
-            }
-        });
-    }
-
-    /**
-     * Accepts the permission to use the chat widget.
-     */
-    acceptPermission() {
-        this.userService.acceptIris().subscribe(() => {
-            this.userAccepted = true;
-        });
-        this.loadFirstMessage();
     }
 
     /**
@@ -505,120 +376,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
         this.setPositionAndScale();
     }
 
-    /**
-     * Handles the key events in the message textarea.
-     * @param event - The keyboard event.
-     */
-    handleKey(event: KeyboardEvent): void {
-        if (event.key === 'Enter') {
-            if (!this.deactivateSubmitButton()) {
-                if (!event.shiftKey) {
-                    event.preventDefault();
-                    this.onSend();
-                } else {
-                    const textArea = event.target as HTMLTextAreaElement;
-                    const { selectionStart, selectionEnd } = textArea;
-                    const value = textArea.value;
-                    textArea.value = value.slice(0, selectionStart) + value.slice(selectionEnd);
-                    textArea.selectionStart = textArea.selectionEnd = selectionStart + 1;
-                }
-            } else {
-                event.preventDefault();
-            }
-        }
-    }
-
-    /**
-     * Handles the input event in the message textarea.
-     */
-    onInput() {
-        this.adjustTextareaRows();
-    }
-
-    /**
-     * Handles the paste event in the message textarea.
-     */
-    onPaste() {
-        setTimeout(() => {
-            this.adjustTextareaRows();
-        }, 0);
-    }
-
-    /**
-     * Adjusts the height of the message textarea based on its content.
-     */
-    adjustTextareaRows() {
-        const textarea: HTMLTextAreaElement = this.messageTextarea.nativeElement;
-        textarea.style.height = 'auto'; // Reset the height to auto
-        const lineHeight = parseInt(getComputedStyle(textarea).lineHeight, 10);
-        const maxRows = 3;
-        const maxHeight = lineHeight * maxRows;
-
-        textarea.style.height = `${Math.min(textarea.scrollHeight, maxHeight)}px`;
-
-        this.adjustChatBodyHeight(Math.min(textarea.scrollHeight, maxHeight) / lineHeight);
-    }
-
-    /**
-     * Handles the row change event in the message textarea.
-     */
-    onRowChange() {
-        const textarea: HTMLTextAreaElement = this.messageTextarea.nativeElement;
-        const newRows = textarea.value.split('\n').length;
-        if (newRows != this.rows) {
-            if (newRows <= 3) {
-                textarea.rows = newRows;
-                this.adjustChatBodyHeight(newRows);
-                this.rows = newRows;
-            }
-        }
-    }
-
-    /**
-     * Adjusts the height of the chat body based on the number of rows in the message textarea.
-     * @param newRows - The new number of rows.
-     */
-    adjustChatBodyHeight(newRows: number) {
-        const textarea: HTMLTextAreaElement = this.messageTextarea.nativeElement;
-        const chatBody: HTMLElement = this.chatBody.nativeElement;
-        const scrollArrow: HTMLElement = this.scrollArrow.nativeElement;
-        const lineHeight = parseInt(window.getComputedStyle(textarea).lineHeight);
-        const rowHeight = lineHeight * newRows;
-        setTimeout(() => {
-            scrollArrow.style.bottom = `calc(11% + ${rowHeight}px)`;
-        }, 10);
-        setTimeout(() => {
-            chatBody.style.height = `calc(100% - ${rowHeight}px - 64px)`;
-        }, 10);
-    }
-
-    /**
-     * Resets the height of the chat body.
-     */
-    resetChatBodyHeight() {
-        const chatBody: HTMLElement = this.chatBody.nativeElement;
-        const textarea: HTMLTextAreaElement = this.messageTextarea.nativeElement;
-        const scrollArrow: HTMLElement = this.scrollArrow.nativeElement;
-        textarea.rows = 1;
-        textarea.style.height = '';
-        scrollArrow.style.bottom = '';
-        chatBody.style.height = '';
-        this.stateStore.dispatch(new NumNewMessagesResetAction());
-    }
-
-    /**
-     * Creates a new user message.
-     * @param message - The content of the message.
-     * @returns A new IrisClientMessage object representing the user message.
-     */
-    newUserMessage(message: string): IrisUserMessage {
-        const content = new IrisTextMessageContent(message);
-        return {
-            sender: IrisSender.USER,
-            content: [content],
-        };
-    }
-
     isSendMessageFailedError(): boolean {
         return this.error?.key == IrisErrorMessageKey.SEND_MESSAGE_FAILED || this.error?.key == IrisErrorMessageKey.IRIS_SERVER_RESPONSE_TIMEOUT;
     }
@@ -629,10 +386,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
         } else {
             document.body.classList.remove('cdk-global-scroll');
         }
-    }
-
-    deactivateSubmitButton(): boolean {
-        return !this.userAccepted || this.isLoading || (!!this.error && this.error.fatal);
     }
 
     isEmptyMessageError(): boolean {
@@ -646,17 +399,6 @@ export class IrisChatbotWidgetComponent implements OnInit, OnDestroy, AfterViewI
         if (event.fromState === 'start' && event.toState === 'end') {
             this.shouldShowEmptyMessageError = false;
         }
-    }
-
-    getConvertedErrorMap() {
-        if (this.error?.paramsMap) {
-            // Check if paramsMap is iterable.
-            if (typeof this.error?.paramsMap[Symbol.iterator] === 'function') {
-                return Object.fromEntries(this.error.paramsMap);
-            }
-            return this.error.paramsMap;
-        }
-        return null;
     }
 
     isClearChatButtonEnabled(): boolean {
