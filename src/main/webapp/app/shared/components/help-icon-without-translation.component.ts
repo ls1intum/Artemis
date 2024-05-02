617b5bc325ab4be0e9c885ab51fb0fcb1ed3fbef
Florian Glombik <florian.glombik@tum.de>
2024-05-02 16:29:16 +0200

    Fixing aux Repo tooltip

--- src/main/webapp/app/shared/components/help-icon-without-translation.component.ts
+++ src/main/webapp/app/shared/components/help-icon-without-translation.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
+const DEFAULT_PLACEMENT = 'top';
+
 @Component({
     selector: 'jhi-help-icon-without-translation',
     template: ` <fa-icon [icon]="faQuestionCircle" class="text-secondary" [placement]="placement" ngbTooltip="{{ text }}" /> `,
 })
 export class HelpIconComponentWithoutTranslationComponent {
-    @Input() placement: string;
+    @Input() placement: string = DEFAULT_PLACEMENT;
     @Input() text: string;
 
     // Icons
