4b73e45ac026e8fa30df0ba672da8ee334e88696
Johannes Stöhr <johannes.stoehr@tum.de>
2024-05-03 15:56:59 +0200

    Add ZonedDateTimeUtilTest

--- src/test/java/de/tum/in/www1/artemis/service/util/HttpRequestUtilsTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/util/HttpRequestUtilsTest.java
@@ -1,4 +1,4 @@
-package de.tum.in.www1.artemis.service;
+package de.tum.in.www1.artemis.service.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.anyString;
@@ -8,8 +8,6 @@ import static org.mockito.Mockito.when;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.junit.jupiter.api.Test;
-
-import de.tum.in.www1.artemis.service.util.HttpRequestUtils;
 
 class HttpRequestUtilsTest {
 
