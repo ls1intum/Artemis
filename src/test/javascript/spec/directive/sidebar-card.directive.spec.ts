6aff507a098e3b09c2496f95926c66fc27b992d1
Ege Kurt <egekurt14@gmail.com>
2024-05-08 00:03:04 +0200

    try new testing approach

--- src/test/javascript/spec/directive/sidebar-card.directive.spec.ts
+++ src/test/javascript/spec/directive/sidebar-card.directive.spec.ts
@@ -1,11 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, ViewChild } from '@angular/core';
 import { MockComponent } from 'ng-mocks';
+import { ActivatedRoute, Params, Router, convertToParamMap } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { SidebarCardDirective } from 'app/shared/sidebar/sidebar-card.directive';
-import { ConversationSelectionSidebarComponent } from 'app/overview/course-conversations/layout/conversation-selection-sidebar/conversation-selection-sidebar.component';
-import { ConversationHeaderComponent } from 'app/overview/course-conversations/layout/conversation-header/conversation-header.component';
-import { DocumentationButtonComponent } from 'app/shared/components/documentation-button/documentation-button.component';
 import { SidebarCardElement, SidebarTypes } from 'app/types/sidebar';
+import { MockRouter } from '../helpers/mocks/mock-router';
+import { SidebarCardSmallComponent } from 'app/shared/sidebar/sidebar-card-small/sidebar-card-small.component';
+import { SidebarCardMediumComponent } from 'app/shared/sidebar/sidebar-card-medium/sidebar-card-medium.component';
+import { SidebarCardLargeComponent } from 'app/shared/sidebar/sidebar-card-large/sidebar-card-large.component';
 
 @Component({
     template: `<div jhiSidebarCard [size]="size" [itemSelected]="false"></div>`,
@@ -21,26 +24,60 @@ class TestHostComponent {
 describe('SidebarCardDirective', () => {
     let component: TestHostComponent;
     let fixture: ComponentFixture<TestHostComponent>;
-    //   let hostElement: DebugElement;
+    let queryParamsSubject: BehaviorSubject<Params>;
+    const router = new MockRouter();
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [
                 TestHostComponent,
-                MockComponent(ConversationSelectionSidebarComponent),
-                MockComponent(ConversationHeaderComponent),
-                MockComponent(DocumentationButtonComponent),
                 SidebarCardDirective,
+                MockComponent(SidebarCardSmallComponent),
+                MockComponent(SidebarCardMediumComponent),
+                MockComponent(SidebarCardLargeComponent),
+            ],
+            providers: [
+                { provide: Router, useValue: router },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        parent: {
+                            parent: {
+                                paramMap: new BehaviorSubject(
+                                    convertToParamMap({
+                                        courseId: 1,
+                                    }),
+                                ),
+                            },
+                        },
+                        queryParams: queryParamsSubject,
+                    },
+                },
             ],
         }).compileComponents();
 
+        jest.spyOn(console, 'warn').mockImplementation(() => {});
+
         fixture = TestBed.createComponent(TestHostComponent);
         component = fixture.componentInstance;
-        //        hostElement = fixture.debugElement.query(By.directive(SidebarCardDirective));
+        TestBed.inject(ActivatedRoute);
         fixture.detectChanges();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
     });
 
     it('should create an instance', () => {
         expect(component.directive).toBeDefined();
+    });
+
+    it('should create SidebarCardSmallComponent when size is "S"', () => {
+        const createComponentSpy = jest.spyOn(component.directive.viewContainerRef, 'createComponent');
+
+        component.size = 'S';
+        fixture.detectChanges();
+
+        expect(createComponentSpy).toHaveBeenCalledWith(SidebarCardSmallComponent);
     });
 });
