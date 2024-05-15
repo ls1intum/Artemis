d7c00e0a857fd9c2d677dd16d0e089ff674c1800
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-05-15 18:37:04 +0200

    fix code style issues

--- supporting_scripts/analysis-of-endpoint-connections/src/main/java/analysisOfEndpointConnections/AnalysisOfEndpointConnections.java
+++ supporting_scripts/analysis-of-endpoint-connections/src/main/java/analysisOfEndpointConnections/AnalysisOfEndpointConnections.java
@@ -2,14 +2,6 @@ package analysisOfEndpointConnections;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-
-import com.thoughtworks.qdox.JavaProjectBuilder;
-import com.thoughtworks.qdox.model.JavaAnnotation;
-import com.thoughtworks.qdox.model.JavaClass;
-import com.thoughtworks.qdox.model.JavaMethod;
 
 public class AnalysisOfEndpointConnections {
 
@@ -22,10 +14,6 @@ public class AnalysisOfEndpointConnections {
         String[] serverFiles = Arrays.stream(args)
             .filter(filePath -> new File(filePath).exists() && filePath.endsWith(".java"))
             .toArray(String[]::new);
-        analyzeServerEndpoints(serverFiles);
     }
 
-    private static void analyzeServerEndpoints(String[] filePaths) {
-
-    }
 }
