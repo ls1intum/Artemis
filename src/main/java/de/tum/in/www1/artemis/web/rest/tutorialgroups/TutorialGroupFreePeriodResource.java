bef4e58fa157bf35b3cb80b634140fa914b562b1
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-05-16 10:18:09 +0200

    added test changes

--- src/main/java/de/tum/in/www1/artemis/web/rest/tutorialgroups/TutorialGroupFreePeriodResource.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/tutorialgroups/TutorialGroupFreePeriodResource.java
@@ -41,6 +41,9 @@ import de.tum.in.www1.artemis.web.rest.errors.BadRequestAlertException;
 @RequestMapping("api/")
 public class TutorialGroupFreePeriodResource {
 
+    // testChange
+    int i = 0;
+
     private static final String ENTITY_NAME = "tutorialGroupFreePeriod";
 
     private static final Logger log = LoggerFactory.getLogger(TutorialGroupFreePeriodResource.class);
