3780d147403f6f7e27099199027f027564d1b959
Florian Glombik <florian.glombik@tum.de>
2024-05-19 05:13:22 +0200

    Make client style happy

--- src/main/webapp/app/exercises/programming/manage/update/programming-exercise-plans-and-repositories-preview.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/programming-exercise-plans-and-repositories-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { getCourseFromExercise } from 'app/entities/exercise.model';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseService } from 'app/exercises/programming/manage/services/programming-exercise.service';
@@ -12,7 +12,7 @@ import { RepositoriesCheckoutDirectoriesDTO } from 'app/exercises/programming/ma
     templateUrl: './programming-exercise-plans-and-repositories-preview.component.html',
     styleUrls: ['../programming-exercise-form.scss'],
 })
-export class ProgrammingExercisePlansAndRepositoriesPreviewComponent {
+export class ProgrammingExercisePlansAndRepositoriesPreviewComponent implements OnInit, OnChanges, OnDestroy {
     @Input() programmingExercise: ProgrammingExercise | null;
     @Input() isLocal: boolean;
     @Input() programmingExerciseCreationConfig: ProgrammingExerciseCreationConfig;
