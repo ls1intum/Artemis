5c2c9370d2f608ca7bb7f6e28d52220faeff9ff2
EgeDoguKaya <egedk321@outlook.com>
2024-05-19 16:07:59 +0200

    Load exam summary directly after test exam

--- src/main/webapp/app/exam/participate/exam-participation.component.ts
+++ src/main/webapp/app/exam/participate/exam-participation.component.ts
@@ -359,13 +359,16 @@ export class ExamParticipationComponent implements OnInit, OnDestroy, ComponentC
                     if (this.testRunId) {
                         this.router.navigate(['course-management', this.courseId, 'exams', this.examId, 'test-runs', this.testRunId, 'summary']);
                     }
-
                     this.examSummaryButtonTimer = setInterval(() => {
                         this.examSummaryButtonSecondsLeft -= 1;
                         if (this.examSummaryButtonSecondsLeft === 0) {
                             clearInterval(this.examSummaryButtonTimer);
                         }
                     }, 1000);
+
+                    if (this.testExam) {
+                        this.router.navigate(['courses', this.courseId, 'exams', this.examId]);
+                    }
                 },
                 error: (error: Error) => {
                     // Explicitly check whether the error was caused by the submission not being in-time or already present, in this case, set hand in not possible
