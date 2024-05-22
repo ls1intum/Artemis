6636179cfe453ca8acd7cc104368d92484ba49e2
Timor Morrien <timor.morrien@tum.de>
2024-05-22 21:53:24 +0200

    Fix package

--- src/main/java/de/tum/in/www1/artemis/config/migration/DatabaseMigration.java
+++ src/main/java/de/tum/in/www1/artemis/config/migration/DatabaseMigration.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
