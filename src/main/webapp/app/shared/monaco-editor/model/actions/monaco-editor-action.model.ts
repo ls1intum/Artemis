03967b5e9bc4bc87b407e9e9003a55a412deb8d8
Patrik Zander <patrik.zander@tum.de>
2024-05-23 23:41:14 +0200

    Remove replace behavior; add todo for quote

--- src/main/webapp/app/shared/monaco-editor/model/actions/monaco-editor-action.model.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/monaco-editor-action.model.ts
@@ -14,7 +14,12 @@ export abstract class MonacoEditorAction implements monaco.editor.IActionDescrip
         this.keybindings = keybindings;
     }
 
-    abstract run(editor: monaco.editor.ICodeEditor): void;
+    // TODO: might be required to support e.g. color picker
+    runWithArguments(editor: monaco.editor.ICodeEditor, args: any): void {
+        editor.trigger(this.id, this.id, args);
+    }
+
+    abstract run(editor: monaco.editor.ICodeEditor, args?: unknown): void;
 
     insertTextAtPosition(editor: monaco.editor.ICodeEditor, position: monaco.IPosition, text: string): void {
         this.replaceTextAtRange(editor, new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column), text);
@@ -24,8 +29,16 @@ export abstract class MonacoEditorAction implements monaco.editor.IActionDescrip
         editor.executeEdits(this.id, [{ range, text }]);
     }
 
+    deleteTextAtRange(editor: monaco.editor.ICodeEditor, range: monaco.IRange): void {
+        this.replaceTextAtRange(editor, range, '');
+    }
+
     getTextAtRange(editor: monaco.editor.ICodeEditor, range: monaco.IRange): string | undefined {
         // End of line preference is important here. Otherwise, Windows may use CRLF line endings.
         return editor.getModel()?.getValueInRange(range, monaco.editor.EndOfLinePreference.LF);
+    }
+
+    getLineText(editor: monaco.editor.ICodeEditor, lineNumber: number): string | undefined {
+        return editor.getModel()?.getLineContent(lineNumber);
     }
 }
---
+++ src/main/webapp/app/shared/monaco-editor/model/actions/monaco-unordered-list.action.ts
@@ -0,0 +1,25 @@
+import * as monaco from 'monaco-editor';
+import { MonacoEditorAction } from 'app/shared/monaco-editor/model/actions/monaco-editor-action.model';
+
+const LIST_BULLET = '- ';
+export class MonacoUnorderedListAction extends MonacoEditorAction {
+    constructor(label: string, translationKey: string) {
+        super('monaco-unordered-list.action', label, translationKey, undefined);
+    }
+
+    run(editor: monaco.editor.ICodeEditor) {
+        const selection = editor.getSelection();
+        if (selection) {
+            for (let lineNumber = selection.startLineNumber; lineNumber <= selection.endLineNumber; lineNumber++) {
+                const lineContent = this.getLineText(editor, lineNumber);
+                // TODO: it could make sense not to toggle each line, but to toggle the whole selection.
+                // e.g. if all lines are already bulleted, remove bullets from all lines. otherwise, add bullets to all lines.
+                if (lineContent?.startsWith(LIST_BULLET)) {
+                    this.deleteTextAtRange(editor, new monaco.Range(lineNumber, 1, lineNumber, 1 + LIST_BULLET.length));
+                } else {
+                    this.insertTextAtPosition(editor, new monaco.Position(lineNumber, 1), LIST_BULLET);
+                }
+            }
+        }
+    }
+}
