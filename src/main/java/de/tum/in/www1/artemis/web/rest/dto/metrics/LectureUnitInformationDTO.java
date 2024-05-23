b8b724c557983d70276d04d1cc169f03f0ec6edc
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-05-23 10:13:05 +0200

    add lecture id to dto

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/metrics/LectureUnitInformationDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/metrics/LectureUnitInformationDTO.java
@@ -8,9 +8,10 @@ import de.tum.in.www1.artemis.domain.lecture.LectureUnit;
  * DTO for lecture unit information.
  *
  * @param id          the id of the lecture unit
+ * @param lectureId   the id of the lecture
  * @param name        the name of the lecture unit
  * @param releaseDate the release date of the lecture unit
  * @param type        the type of the lecture unit
  */
-public record LectureUnitInformationDTO(long id, String name, ZonedDateTime releaseDate, Class<? extends LectureUnit> type) {
+public record LectureUnitInformationDTO(long id, long lectureId, String name, ZonedDateTime releaseDate, Class<? extends LectureUnit> type) {
 }
