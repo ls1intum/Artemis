0033a24a78b298a3fb1f9a4518bb2ed2cd97481d
Maximilian Anzinger <anzinger@in.tum.de>
2024-05-23 15:55:15 +0200

    anonymize collected events on user delete

--- src/main/java/de/tum/in/www1/artemis/repository/science/ScienceEventRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/science/ScienceEventRepository.java
@@ -6,7 +6,10 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.in.www1.artemis.domain.science.ScienceEvent;
 import de.tum.in.www1.artemis.domain.science.ScienceEventType;
@@ -19,4 +22,13 @@ import de.tum.in.www1.artemis.domain.science.ScienceEventType;
 public interface ScienceEventRepository extends JpaRepository<ScienceEvent, Long> {
 
     Set<ScienceEvent> findAllByType(ScienceEventType type);
+
+    @Transactional // ok because of modifying query
+    @Modifying
+    @Query("""
+            UPDATE ScienceEvent se
+            SET se.identity = :newIdentity
+            WHERE se.identity = :oldIdentity
+            """)
+    void renameIdentity(String oldIdentity, String newIdentity);
 }
