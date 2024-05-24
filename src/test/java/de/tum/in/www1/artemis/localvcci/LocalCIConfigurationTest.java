63308759293fc6e698c7e4229becea5eab33c22e
Simon <simon.entholzer@tum.de>
2024-05-24 16:17:35 +0200

    fixed test to adhere to architecture guidelines

--- src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIConfigurationTest.java
+++ src/test/java/de/tum/in/www1/artemis/localvcci/LocalCIConfigurationTest.java
@@ -6,27 +6,27 @@ import org.junit.jupiter.api.Test;
 
 import de.tum.in.www1.artemis.config.localvcci.LocalCIConfiguration;
 
-public class LocalCIConfigurationTest {
+class LocalCIConfigurationTest {
 
     @Test
-    public void testBytes() {
+    void testBytes() {
         assertThat("512 Bytes").isEqualTo(LocalCIConfiguration.formatMemory(512));
     }
 
     @Test
-    public void testKilobytes() {
+    void testKilobytes() {
         assertThat("1 KB").isEqualTo(LocalCIConfiguration.formatMemory(1024));
         assertThat("999 KB").isEqualTo(LocalCIConfiguration.formatMemory(1024 * 999));
     }
 
     @Test
-    public void testMegabytes() {
+    void testMegabytes() {
         assertThat("1 MB").isEqualTo(LocalCIConfiguration.formatMemory(1024 * 1024));
         assertThat("1023 MB").isEqualTo(LocalCIConfiguration.formatMemory(1024 * 1024 * 1023L));
     }
 
     @Test
-    public void testGigabytes() {
+    void testGigabytes() {
         assertThat("1.0 GB").isEqualTo(LocalCIConfiguration.formatMemory(1024 * 1024 * 1024L));
         assertThat("1.5 GB").isEqualTo(LocalCIConfiguration.formatMemory(1024 * 1024 * 1024 * 3L / 2));
     }
