7a159fb647ae0843a67500b0ecddeb55bc50d7d8
Patrik Zander <patrik.zander@tum.de>
2024-05-24 22:27:21 +0200

    Add preview (breaks layout for Edit in Editor)

--- src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-monaco.component.ts
+++ src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-monaco.component.ts
@@ -1,20 +1,21 @@
-import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild } from '@angular/core';
 import { MonacoEditorComponent } from 'app/shared/monaco-editor/monaco-editor.component';
 import { MarkdownEditorHeight } from 'app/shared/markdown-editor/markdown-editor.component';
+import { NgbNavChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'jhi-markdown-editor-monaco',
     templateUrl: './markdown-editor-monaco.component.html',
     styleUrl: './markdown-editor-monaco.component.scss',
 })
-export class MarkdownEditorMonacoComponent implements OnInit, OnDestroy {
-    @ViewChild(MonacoEditorComponent, { static: true }) monacoEditor: MonacoEditorComponent;
+export class MarkdownEditorMonacoComponent implements AfterViewInit, OnDestroy {
+    @ViewChild(MonacoEditorComponent, { static: false }) monacoEditor: MonacoEditorComponent;
     @ViewChild('wrapper', { static: true }) wrapper: ElementRef<HTMLDivElement>;
 
     @Input()
     set markdown(value: string | undefined) {
         this._markdown = value;
-        this.monacoEditor.setText(value ?? '');
+        this.monacoEditor?.setText(value ?? '');
     }
 
     _markdown?: string;
@@ -28,9 +29,13 @@ export class MarkdownEditorMonacoComponent implements OnInit, OnDestroy {
     @Output()
     markdownChange = new EventEmitter<string>();
 
+    @Output()
+    onPreviewSelect = new EventEmitter();
+
+    inPreviewMode = false;
     resizeObserver?: ResizeObserver;
 
-    ngOnInit(): void {
+    ngAfterViewInit(): void {
         this.monacoEditor.changeModel('markdown-content.custom-md', this._markdown, 'custom-md');
         this.monacoEditor.layoutWithFixedSize(100, 400);
         this.resizeObserver = new ResizeObserver(() => {
@@ -46,5 +51,15 @@ export class MarkdownEditorMonacoComponent implements OnInit, OnDestroy {
     onTextChanged(text: string): void {
         this.markdown = text;
         this.markdownChange.emit(text);
+    }
+
+    onNavChanged(event: NgbNavChangeEvent) {
+        this.inPreviewMode = event.nextId === 'editor_preview';
+        if (!this.inPreviewMode) {
+            this.monacoEditor.layoutWithFixedSize(100, 400);
+            this.monacoEditor.focus();
+        } else {
+            this.onPreviewSelect.emit();
+        }
     }
 }
