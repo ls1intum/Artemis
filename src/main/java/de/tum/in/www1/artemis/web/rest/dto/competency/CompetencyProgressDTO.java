b38d1619ac6c99f3de9cd938afa64597f45a53bc
Johannes Stöhr <johannes.stoehr@tum.de>
2024-05-27 12:15:39 +0200

    Make Max happy

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/CompetencyProgressDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/CompetencyProgressDTO.java
@@ -2,9 +2,12 @@ package de.tum.in.www1.artemis.web.rest.dto.competency;
 
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import de.tum.in.www1.artemis.domain.competency.CompetencyProgress;
 import de.tum.in.www1.artemis.service.competency.CompetencyProgressService;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CompetencyProgressDTO(long id, String title, ZonedDateTime softDueDate, Double progress, Double confidence, Double mastery) {
 
     public static CompetencyProgressDTO of(CompetencyProgress competencyProgress) {
