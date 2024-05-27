19304f5e452250999ccd6aa4b954aef9246d18db
Maximilian Anzinger <anzinger@in.tum.de>
2024-05-27 13:38:31 +0200

    Update IrisCourseChatSessionService.java

--- src/main/java/de/tum/in/www1/artemis/service/iris/session/IrisCourseChatSessionService.java
+++ src/main/java/de/tum/in/www1/artemis/service/iris/session/IrisCourseChatSessionService.java
@@ -2,8 +2,6 @@ package de.tum.in.www1.artemis.service.iris.session;
 
 import java.util.Objects;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -31,8 +29,6 @@ import de.tum.in.www1.artemis.web.rest.errors.AccessForbiddenException;
 @Service
 @Profile("iris")
 public class IrisCourseChatSessionService implements IrisChatBasedFeatureInterface<IrisCourseChatSession>, IrisRateLimitedFeatureInterface {
-
-    private static final Logger log = LoggerFactory.getLogger(IrisCourseChatSessionService.class);
 
     private final IrisMessageService irisMessageService;
 
