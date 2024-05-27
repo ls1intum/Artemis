a091e5896af2bf27d8803cf264fd746a98a51ff7
Maximilian Anzinger <anzinger@in.tum.de>
2024-05-27 12:56:37 +0200

    Update IrisChatSession.java

--- src/main/java/de/tum/in/www1/artemis/domain/iris/session/IrisChatSession.java
+++ src/main/java/de/tum/in/www1/artemis/domain/iris/session/IrisChatSession.java
@@ -3,17 +3,12 @@ package de.tum.in.www1.artemis.domain.iris.session;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import de.tum.in.www1.artemis.domain.User;
 
 @MappedSuperclass
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public abstract class IrisChatSession extends IrisSession {
 
     @ManyToOne
-    @JsonIgnore
     private User user;
 
     public IrisChatSession(User user) {
