cf9d53936f2cee291d97fcfe4143704ae382d1dd
Florian Glombik <florian.glombik@tum.de>
2024-05-28 22:58:05 +0200

    Improve tooltip positioning

--- src/main/webapp/app/detail-overview-list/detail-overview-list.component.ts
+++ src/main/webapp/app/detail-overview-list/detail-overview-list.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular
 import { faArrowUpRightFromSquare, faCodeBranch, faCodeCompare, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { isEmpty } from 'lodash-es';
 import { FeatureToggle } from 'app/shared/feature-toggle/feature-toggle.service';
-import { ButtonSize, ButtonType } from 'app/shared/components/button.component';
+import { ButtonSize, ButtonType, TooltipPlacement } from 'app/shared/components/button.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GitDiffReportModalComponent } from 'app/exercises/programming/hestia/git-diff-report/git-diff-report-modal.component';
 import { ProgrammingExerciseGitDiffReport } from 'app/entities/hestia/programming-exercise-git-diff-report.model';
@@ -116,4 +116,6 @@ export class DetailOverviewListComponent implements OnInit, OnDestroy {
     ngOnDestroy() {
         this.profileSubscription?.unsubscribe();
     }
+
+    protected readonly TooltipPlacement = TooltipPlacement;
 }
--- src/main/webapp/app/shared/components/button.component.ts
+++ src/main/webapp/app/shared/components/button.component.ts
@@ -29,6 +29,13 @@ export enum ButtonSize {
     LARGE = 'btn-lg',
 }
 
+export enum TooltipPlacement {
+    TOP = 'top',
+    BOTTOM = 'bottom',
+    LEFT = 'left',
+    RIGHT = 'right',
+}
+
 /**
  * A generic button component that has a disabled & loading state.
  * The only event output is the click.
@@ -47,6 +54,7 @@ export class ButtonComponent {
     // Translation placeholders, will be translated in the component.
     @Input() title: string;
     @Input() tooltip: string;
+    @Input() tooltipPlacement: TooltipPlacement = TooltipPlacement.TOP;
 
     @Input() disabled = false;
     @Input() isLoading = false;
@@ -58,5 +66,5 @@ export class ButtonComponent {
     @Output() onClick = new EventEmitter<MouseEvent>();
 
     // Icons
-    faCircleNotch = faCircleNotch;
+    readonly faCircleNotch = faCircleNotch;
 }
