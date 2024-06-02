b5900f42aa0c81e69ce855578d7a94578577d435
EgeDoguKaya <egedk321@outlook.com>
2024-06-02 19:48:01 +0200

    Remove unused files

--- src/main/webapp/app/overview/course-exams/course-exams.module.ts
+++ src/main/webapp/app/overview/course-exams/course-exams.module.ts
@@ -1,7 +1,5 @@
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { CourseExamsComponent } from 'app/overview/course-exams/course-exams.component';
-import { CourseExamDetailComponent } from 'app/overview/course-exams/course-exam-detail/course-exam-detail.component';
-import { CourseExamAttemptReviewDetailComponent } from 'app/overview/course-exams/course-exam-attempt-review-detail/course-exam-attempt-review-detail.component';
 import { ArtemisExamSharedModule } from 'app/exam/shared/exam-shared.module';
 import { NgModule } from '@angular/core';
 import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
@@ -10,7 +8,7 @@ import { ArtemisSidebarModule } from 'app/shared/sidebar/sidebar.module';
 
 @NgModule({
     imports: [ArtemisSharedModule, ArtemisSharedCommonModule, ArtemisSharedComponentModule, ArtemisExamSharedModule, ArtemisSidebarModule],
-    declarations: [CourseExamsComponent, CourseExamDetailComponent, CourseExamAttemptReviewDetailComponent],
+    declarations: [CourseExamsComponent],
     exports: [CourseExamsComponent],
 })
 export class CourseExamsModule {}
