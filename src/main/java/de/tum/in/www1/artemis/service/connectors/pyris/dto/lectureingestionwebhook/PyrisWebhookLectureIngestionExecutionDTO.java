ebcf9223fac1ce688ada51a487ca3bf224cf52a0
Yassine Souissi <yassine.souissi@tum.de>
2024-06-03 16:43:06 +0200

    Revert "change NON_EMPTY TO NON_NULL"
    This reverts commit 23e353c50412d4ed47d3a094a00897234766adc2.

--- src/main/java/de/tum/in/www1/artemis/service/connectors/pyris/dto/lectureingestionwebhook/PyrisWebhookLectureIngestionExecutionDTO.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/pyris/dto/lectureingestionwebhook/PyrisWebhookLectureIngestionExecutionDTO.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.in.www1.artemis.service.connectors.pyris.dto.PyrisPipelineExecutionSettingsDTO;
 import de.tum.in.www1.artemis.service.connectors.pyris.dto.status.PyrisStageDTO;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PyrisWebhookLectureIngestionExecutionDTO(List<PyrisLectureUnitWebhookDTO> pyrisLectureUnitWebhookDTOS, PyrisPipelineExecutionSettingsDTO settings,
         List<PyrisStageDTO> initialStages) {
 }
