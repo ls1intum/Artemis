53913bb0a513c4a3bedc94fec2f24a695494e7c0
aniruddhzaveri <aniruddh.zaveri@tum.de>
2024-06-04 12:44:06 +0200

    export json modified

--- src/main/java/de/tum/in/www1/artemis/service/tutorialgroups/TutorialGroupService.java
+++ src/main/java/de/tum/in/www1/artemis/service/tutorialgroups/TutorialGroupService.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -728,7 +729,7 @@ public class TutorialGroupService {
 
         List<Map<String, Object>> exportData = new ArrayList<>();
         for (TutorialGroup tutorialGroup : tutorialGroups) {
-            Map<String, Object> groupData = new HashMap<>();
+            Map<String, Object> groupData = new LinkedHashMap<>();
             for (String field : selectedFields) {
                 switch (field) {
                     case "ID" -> groupData.put("ID", tutorialGroup.getId());
