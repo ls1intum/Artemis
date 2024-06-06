d8ee07a3af0633ab19b78fcb1188d71afd14dbe8
Dmytro Polityka <dmytro.polityka@tum.de>
2024-06-06 21:02:45 +0200

    coderabbit

--- src/main/webapp/app/entities/self-learning-feedback-request.model.ts
+++ src/main/webapp/app/entities/self-learning-feedback-request.model.ts
@@ -1,8 +1,8 @@
-import { BaseEntity } from 'app/shared/model/base-entity';
-import dayjs from 'dayjs/esm';
-import { Participation } from 'app/entities/participation/participation.model';
-import { Submission } from 'app/entities/submission.model';
-import { Result } from 'app/entities/result.model';
+import type { BaseEntity } from 'app/shared/model/base-entity';
+import type dayjs from 'dayjs/esm';
+import type { Participation } from 'app/entities/participation/participation.model';
+import type { Submission } from 'app/entities/submission.model';
+import type { Result } from 'app/entities/result.model';
 
 export class SelfLearningFeedbackRequest implements BaseEntity {
     public id?: number;
