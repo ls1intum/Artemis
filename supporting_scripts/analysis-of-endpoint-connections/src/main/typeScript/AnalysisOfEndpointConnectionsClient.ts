3c37828f5799de5e96da85635f302b1924d1692a
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-06-09 21:58:37 +0200

    check if httpClient is of type httpClient

--- supporting_scripts/analysis-of-endpoint-connections/src/main/typeScript/AnalysisOfEndpointConnectionsClient.ts
+++ supporting_scripts/analysis-of-endpoint-connections/src/main/typeScript/AnalysisOfEndpointConnectionsClient.ts
@@ -5,7 +5,11 @@ import {
     isStringLiteral,
     ScriptTarget,
     SourceFile,
-    Node, createProgram
+    Node, createProgram,
+    isClassDeclaration,
+    isConstructorDeclaration,
+    isParameter,
+    isTypeReferenceNode, SyntaxKind,
 } from 'typescript';
 import { readFileSync } from 'node:fs';
 
@@ -20,26 +24,47 @@ for (const fileName of fileNames.filter(fileName => fileName.endsWith('.ts')))  
 
     // Store class property definitions
     const classProperties: { [key: string]: string } = {};
+    const classMethods: { [key: string]: string } = {};
+
+    // Store parameter types
+    const parameterTypes: { [key: string]: string } = {};
 
     // Start traversing the AST from the root
-    visit(sourceFile, classProperties, sourceFile, fileName);
+    visit(sourceFile, classProperties, parameterTypes, sourceFile, fileName);
 };
 
 // This function will be called for each node in the AST
-function visit(node: Node, classProperties: { [key: string]: string }, sourceFile: SourceFile, fileName: string) {
-    if (isPropertyDeclaration(node) && node.initializer && isStringLiteral(node.initializer)) {
-        const key = node.name.getText();
-        const value = node.initializer.getText().slice(1, -1); // Remove the quotes
-        classProperties[key] = value;
+function visit(node: Node, classProperties: { [key: string]: string }, parameterTypes: { [key: string]: string }, sourceFile: SourceFile, fileName: string) {
+    if (isClassDeclaration(node)) {
+        for (const member of node.members) {
+            if (isConstructorDeclaration(member)) {
+                for (const param of member.parameters) {
+                    if (isParameter(param) && param.type && isTypeReferenceNode(param.type)) {
+                        const paramName = param.name.getText();
+                        const paramType = param.type.typeName.getText();
+                        parameterTypes[paramName] = paramType;
+                    }
+                }
+            }
+
+            if (isPropertyDeclaration(member) && member.initializer && isStringLiteral(member.initializer)) {
+                const key = member.name.getText();
+                const value = member.initializer.getText().slice(1, -1); // Remove the quotes
+                classProperties[key] = value;
+
+            }
+        }
     }
+
 
     if (isCallExpression(node)) {
         const expression = node.expression;
         // Check if the expression is a property access expression (e.g. httpClient.get)
         if (isPropertyAccessExpression(expression)) {
             const methodName = expression.name.getText();
+            const objectName = expression.expression.getText().replace(/^this\./, ''); // Remove 'this.' prefix if present;
             // Check if the property name is one of the httpClient methods
-            if (HTTP_METHODS.includes(methodName)) {
+            if (HTTP_METHODS.includes(methodName) && parameterTypes[objectName] === 'HttpClient') {
                 console.log(`Found REST call: ${methodName}`);
                 if (node.arguments.length > 0) {
                     let url = node.arguments[0].getText();
@@ -64,5 +89,5 @@ function visit(node: Node, classProperties: { [key: string]: string }, sourceFil
     }
 
     // Continue traversing the AST
-    forEachChild(node, (childNode) => visit(childNode, classProperties, sourceFile, fileName));
+    forEachChild(node, (childNode) => visit(childNode, classProperties, parameterTypes, sourceFile, fileName));
 }
