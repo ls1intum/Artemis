2b63811d9bde9c64031517143f15abf958069bfe
Johannes Wiest <Johannes.wiest@outlook.com>
2024-06-11 14:56:44 +0200

    add translations

--- src/main/webapp/app/course/learning-paths/components/competency-graph-modal/competency-graph-modal.component.ts
+++ src/main/webapp/app/course/learning-paths/components/competency-graph-modal/competency-graph-modal.component.ts
@@ -3,11 +3,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CompetencyGraphComponent } from 'app/course/learning-paths/components/competency-graph/competency-graph.component';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
 
 @Component({
     selector: 'jhi-competency-graph-modal',
     standalone: true,
-    imports: [FontAwesomeModule, CompetencyGraphComponent],
+    imports: [FontAwesomeModule, CompetencyGraphComponent, ArtemisSharedModule],
     templateUrl: './competency-graph-modal.component.html',
     styleUrl: './competency-graph-modal.component.scss',
 })
