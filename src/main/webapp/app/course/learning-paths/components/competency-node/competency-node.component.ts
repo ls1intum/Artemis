f90fe793c87bfa5efc4d18e3e953f466129fb6c9
Johannes Wiest <Johannes.wiest@outlook.com>
2024-06-12 15:12:54 +0200

    adjust coloring of nodes based on mastery

--- src/main/webapp/app/course/learning-paths/components/competency-node/competency-node.component.ts
+++ src/main/webapp/app/course/learning-paths/components/competency-node/competency-node.component.ts
@@ -22,7 +22,7 @@ export class CompetencyNodeComponent implements AfterViewInit {
     private readonly nodeHeight = 45.59;
 
     readonly competencyNode = input.required<CompetencyGraphNodeDTO>();
-    readonly mastery = computed(() => Math.round(this.competencyNode().mastery));
+    readonly mastery = computed(() => Math.floor(this.competencyNode().mastery));
     private readonly element: ElementRef = inject(ElementRef);
 
     readonly onSizeSet = output<SizeUpdate>();
@@ -32,11 +32,11 @@ export class CompetencyNodeComponent implements AfterViewInit {
     }
 
     isMastered(): boolean {
-        return this.mastery() >= 80;
+        return this.mastery() === 100;
     }
 
     isStarted(): boolean {
-        return this.mastery() > 0 && this.mastery() < 80;
+        return this.mastery() > 0 && this.mastery() < 100;
     }
 
     isNotStarted(): boolean {
