827f0e65ac9e69a890f1249ebfe7ac45246216d3
Michael Baumgartner <Michael-Baumgartner@posteo.de>
2024-06-15 09:17:35 +0200

    Add query method to get entries with Repo Uri
    From the BitBucket to LocalVC migration
    https://github.com/ls1intum/Artemis/compare/develop...chore/bitbucket-localvc-migration-release#diff-be584884b73f86ff8325679c4bbf4e9952f1b1de33216c234a683cb5240211fd

--- src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseStudentParticipationRepository.java
+++ src/main/java/de/tum/in/www1/artemis/repository/ProgrammingExerciseStudentParticipationRepository.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 import jakarta.validation.constraints.NotNull;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -194,6 +196,14 @@ public interface ProgrammingExerciseStudentParticipationRepository extends JpaRe
             WHERE p.id = :participationId
             """)
     void updateLockedById(@Param("participationId") long participationId, @Param("locked") boolean locked);
+
+    @Query("""
+            SELECT DISTINCT p
+            FROM ProgrammingExerciseStudentParticipation p
+            WHERE p.repositoryUri IS NOT NULL
+            ORDER BY p.id DESC
+            """)
+    Page<ProgrammingExerciseStudentParticipation> findAllWithRepositoryUri(Pageable pageable);
 
     /**
      * Remove the build plan id from all participations of the given exercise.
