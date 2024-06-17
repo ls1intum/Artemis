5843036903af6fd15d3860fb6461a724ca04e7b3
Michael Baumgartner <Michael-Baumgartner@posteo.de>
2024-06-17 19:57:33 +0200

    Check necessary profiles for the migration

--- src/main/java/de/tum/in/www1/artemis/config/migration/setups/localvc/MigrationServiceGitLabJenkinsToLocalVC.java
+++ src/main/java/de/tum/in/www1/artemis/config/migration/setups/localvc/MigrationServiceGitLabJenkinsToLocalVC.java
@@ -2,9 +2,15 @@ package de.tum.in.www1.artemis.config.migration.setups.localvc;
 
 import static de.tum.in.www1.artemis.config.Constants.PROFILE_MIGRATE_GITLAB_JENKINS_TO_LOCALVC;
 
+import java.util.HashSet;
+import java.util.List;
+
 import jakarta.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import de.tum.in.www1.artemis.config.migration.setups.localvc.gitlab.MigrationEntryGitLabToLocalVC;
@@ -13,14 +19,24 @@ import de.tum.in.www1.artemis.config.migration.setups.localvc.gitlab.MigrationEn
 @Profile(PROFILE_MIGRATE_GITLAB_JENKINS_TO_LOCALVC)
 public class MigrationServiceGitLabJenkinsToLocalVC {
 
+    private final static Logger log = LoggerFactory.getLogger(MigrationServiceGitLabJenkinsToLocalVC.class);
+
     private final MigrationEntryGitLabToLocalVC migrationEntryGitLabToLocalVC;
 
-    public MigrationServiceGitLabJenkinsToLocalVC(MigrationEntryGitLabToLocalVC migrationEntryGitLabToLocalVC) {
+    private final Environment environment;
+
+    public MigrationServiceGitLabJenkinsToLocalVC(MigrationEntryGitLabToLocalVC migrationEntryGitLabToLocalVC, Environment environment) {
         this.migrationEntryGitLabToLocalVC = migrationEntryGitLabToLocalVC;
+        this.environment = environment;
     }
 
     @PostConstruct
     public void execute() {
+        List<String> activeProfiles = List.of(environment.getActiveProfiles());
+        if (!new HashSet<>(activeProfiles).containsAll(List.of("gitlab", "jenkins", "localvc"))) {
+            log.error("Migration not possible, because the system does not support GitLab, Jenkins and LocalVC: {}", activeProfiles);
+            return;
+        }
         // TODO Add call to migration entry for Jenkins
         migrationEntryGitLabToLocalVC.execute();
     }
