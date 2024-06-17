cee1649727446121dcf02453c6bf590559b7866c
Julian Christl <julian.christl@tum.de>
2024-06-17 11:32:38 +0200

    Update src/main/java/de/tum/in/www1/artemis/service/plagiarism/PlagiarismCaseService.java
    Co-authored-by: Lucas Welscher <ga53foy@mytum.de>

--- src/main/java/de/tum/in/www1/artemis/service/plagiarism/PlagiarismCaseService.java
+++ src/main/java/de/tum/in/www1/artemis/service/plagiarism/PlagiarismCaseService.java
@@ -129,8 +129,8 @@ public class PlagiarismCaseService {
                 // the student was notified if the plagiarism case is available (due to the nature of the query above)
                 // the following line is already checked in the SQL statement, but we want to ensure it 100%
                 .filter((plagiarismCase) -> plagiarismCase.getPost() != null).map((plagiarismCase) -> {
-                    // Note: we only return the ID and verdict to tell the client there is a confirmed plagiarism case with student notification (post) and to support navigating to
-                    // the detail page
+                    // Note: we only return the ID and verdict to tell the client there is a confirmed plagiarism case with student notification (post)
+                    // and to support navigating to the detail page
                     // all other information might be irrelevant or sensitive and could lead to longer loading times
                     return new PlagiarismCaseInfoDTO(plagiarismCase.getId(), plagiarismCase.getVerdict(), plagiarismCase.isCreatedByContinuousPlagiarismControl());
                 });
