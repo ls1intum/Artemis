582bb5fdf7e9fd83cb0037d28d477b628da40b9b
Kaan Çaylı <38523756+kaancayli@users.noreply.github.com>
2024-06-17 02:23:33 +0200

    Restore FeatureToggleServiceTest.java to 'develop' version

--- src/test/java/de/tum/in/www1/artemis/service/FeatureToggleServiceTest.java
+++ src/test/java/de/tum/in/www1/artemis/service/FeatureToggleServiceTest.java
@@ -33,7 +33,6 @@ class FeatureToggleServiceTest extends AbstractSpringIntegrationIndependentTest 
         assertThat(featureToggleService.isFeatureEnabled(Feature.StandardizedCompetencies)).isTrue();
         assertThat(featureToggleService.isFeatureEnabled(Feature.StudentCourseAnalyticsDashboard)).isTrue();
         assertThat(featureToggleService.isFeatureEnabled(Feature.Science)).isFalse();
-        assertThat(featureToggleService.isFeatureEnabled(Feature.StudentCourseAnalyticsDashboard)).isFalse();
     }
 
     @Test
