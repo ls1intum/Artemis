07ee1262be649ccf52dd6dd53b350dd1cf19c4d8
Johannes Stöhr <johannes.stoehr@tum.de>
2024-06-18 16:25:36 +0200

    Fix ArchTest

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/CompetencyNameDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/competency/CompetencyNameDTO.java
@@ -1,7 +1,10 @@
 package de.tum.in.www1.artemis.web.rest.dto.competency;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import de.tum.in.www1.artemis.domain.competency.Competency;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CompetencyNameDTO(long id, String title) {
 
     public static CompetencyNameDTO of(Competency competency) {
