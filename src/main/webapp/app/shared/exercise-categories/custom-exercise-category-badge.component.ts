a2ad57df06144a0f1953927d39dbe85094409383
Florian Glombik <florian.glombik@tum.de>
2024-06-20 16:17:36 +0200

    Displaying remove button for the chip

--- src/main/webapp/app/shared/exercise-categories/custom-exercise-category-badge.component.ts
+++ src/main/webapp/app/shared/exercise-categories/custom-exercise-category-badge.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import type { ExerciseCategory } from 'app/entities/exercise-category.model';
 import { CommonModule } from '@angular/common';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
     selector: 'jhi-custom-exercise-category-badge',
     templateUrl: './custom-exercise-category-badge.component.html',
     styleUrls: ['./exercise-categories.component.scss'],
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FontAwesomeModule],
 })
 export class CustomExerciseCategoryBadgeComponent {
     @Input() category: ExerciseCategory;
+    @Input() displayRemoveButton = false;
+
+    readonly faTimes = faTimes;
 }
