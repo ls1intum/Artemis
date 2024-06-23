fcaec466326ce199b01e227dcb7e84f385648126
Florian Glombik <florian.glombik@tum.de>
2024-06-23 23:43:33 +0200

    Adding todo

--- src/main/webapp/app/entities/exercise-category.model.ts
+++ src/main/webapp/app/entities/exercise-category.model.ts
@@ -1,5 +1,7 @@
 export class ExerciseCategory {
     public color?: string;
+
+    // TODO should be renamed to "name" -> accessing variable via "category.name" instead of "category.category" - requires database migration (stored as json in database, see the table "exercise_categories")
     public category?: string;
 
     constructor() {}
