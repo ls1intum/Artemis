8c6e8a71286c52af1531b624e7a475dac0a2d91d
Patrik Zander <patrik.zander@tum.de>
2024-06-23 12:51:29 +0200

    Fix formula

--- src/main/webapp/app/shared/monaco-editor/model/actions/monaco-formula.action.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/monaco-formula.action.ts
@@ -1,16 +1,17 @@
 import * as monaco from 'monaco-editor';
 import { MonacoEditorDomainAction } from 'app/shared/monaco-editor/model/actions/monaco-editor-domain-action.model';
 
-const FORMULA_DELIMITER = '$$';
-const DEFAULT_FORMULA = ' e^{\\frac{1}{4} y^2} ';
+const FORMULA_OPEN_DELIMITER = '$$ ';
+const FORMULA_CLOSE_DELIMITER = ' $$';
 export class MonacoFormulaAction extends MonacoEditorDomainAction {
     static readonly ID = 'monaco-formula.action';
+    static readonly DEFAULT_FORMULA = 'e^{\\frac{1}{4} y^2}';
     constructor() {
         super(MonacoFormulaAction.ID, 'artemisApp.markdownEditor.commands.katex', undefined, undefined);
     }
 
     run(editor: monaco.editor.ICodeEditor) {
-        this.toggleDelimiterAroundSelection(editor, FORMULA_DELIMITER, FORMULA_DELIMITER, DEFAULT_FORMULA);
+        this.toggleDelimiterAroundSelection(editor, FORMULA_OPEN_DELIMITER, FORMULA_CLOSE_DELIMITER, MonacoFormulaAction.DEFAULT_FORMULA);
         editor.focus();
     }
 }
