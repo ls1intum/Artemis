3358afb614a4ae7278eeadaf97cc7819489d916e
Patrik Zander <patrik.zander@tum.de>
2024-06-23 22:43:28 +0200

    Tests for embedding files

--- src/test/javascript/spec/component/markdown-editor/markdown-editor-monaco.spec.ts
+++ src/test/javascript/spec/component/markdown-editor/markdown-editor-monaco.spec.ts
@@ -3,7 +3,7 @@ import { AlertService } from 'app/core/util/alert.service';
 import { ArtemisTestModule } from '../../test.module';
 import { ColorSelectorComponent } from 'app/shared/color-selector/color-selector.component';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NgbNavModule, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { MockComponent, MockDirective, MockPipe, MockProvider } from 'ng-mocks';
@@ -13,10 +13,13 @@ import { MonacoColorAction } from 'app/shared/monaco-editor/model/actions/monaco
 import { MockResizeObserver } from '../../helpers/mocks/service/mock-resize-observer';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
+import { MonacoUrlAction } from 'app/shared/monaco-editor/model/actions/monaco-url.action';
+import { MonacoAttachmentAction } from 'app/shared/monaco-editor/model/actions/monaco-attachment.action';
 
 describe('MarkdownEditorMonacoComponent', () => {
     let fixture: ComponentFixture<MarkdownEditorMonacoComponent>;
     let comp: MarkdownEditorMonacoComponent;
+    let fileUploaderService: FileUploaderService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -36,11 +39,63 @@ describe('MarkdownEditorMonacoComponent', () => {
         fixture = TestBed.createComponent(MarkdownEditorMonacoComponent);
         comp = fixture.componentInstance;
         comp.initialEditorHeight = 'external';
+        fileUploaderService = fixture.debugElement.injector.get(FileUploaderService);
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
+
+    it('should embed manually uploaded files', () => {
+        const embedFilesStub = jest.spyOn(comp, 'embedFiles').mockImplementation();
+        fixture.detectChanges();
+        const files = [new File([''], 'test.png')];
+        comp.onFileUpload({ target: { files } });
+        expect(embedFilesStub).toHaveBeenCalledExactlyOnceWith(files);
+    });
+
+    it('should embed dropped files', () => {
+        const embedFilesStub = jest.spyOn(comp, 'embedFiles').mockImplementation();
+        fixture.detectChanges();
+        const files = [new File([''], 'test.png')];
+        const event = { dataTransfer: { files }, preventDefault: jest.fn() };
+        comp.onFileDrop(event as any);
+        expect(embedFilesStub).toHaveBeenCalledExactlyOnceWith(files);
+    });
+
+    it('should throw on upload if the actions are not available', () => {
+        comp.defaultActions = [];
+        const files = [new File([''], 'test.png')];
+        fixture.detectChanges();
+        expect(() => comp.embedFiles(files)).toThrow(Error);
+    });
+
+    it('should embed image and .pdf files', fakeAsync(() => {
+        const urlAction = new MonacoUrlAction();
+        const urlStub = jest.spyOn(urlAction, 'executeInCurrentEditor').mockImplementation();
+        const attachmentAction = new MonacoAttachmentAction();
+        const attachmentStub = jest.spyOn(attachmentAction, 'executeInCurrentEditor').mockImplementation();
+        const fileInformation = [
+            { file: new File([''], 'test.png'), url: 'https://test.invalid/generated42.png' },
+            { file: new File([''], 'test.pdf'), url: 'https://test.invalid/generated1234.pdf' },
+        ];
+        comp.defaultActions = [urlAction, attachmentAction];
+        const files = [new File([''], 'test.png'), new File([''], 'test.pdf')];
+        const uploadMarkdownFileStub = jest.spyOn(fileUploaderService, 'uploadMarkdownFile').mockImplementation((file: File) => {
+            const path = file.name.endsWith('.png') ? fileInformation[0].url : fileInformation[1].url;
+            return Promise.resolve({ path });
+        });
+        fixture.detectChanges();
+        comp.embedFiles(files);
+        flush();
+        // Each file should be uploaded.
+        expect(uploadMarkdownFileStub).toHaveBeenCalledTimes(2);
+        expect(uploadMarkdownFileStub).toHaveBeenNthCalledWith(1, files[0]);
+        expect(uploadMarkdownFileStub).toHaveBeenNthCalledWith(2, files[1]);
+        // Each file should be embedded. PDFs should be embedded as URLs.
+        expect(attachmentStub).toHaveBeenCalledExactlyOnceWith({ url: fileInformation[0].url, text: fileInformation[0].file.name });
+        expect(urlStub).toHaveBeenCalledExactlyOnceWith({ url: fileInformation[1].url, text: fileInformation[1].file.name });
+    }));
 
     it('should pass the correct color as argument to the color action', () => {
         comp.colorAction = new MonacoColorAction();
