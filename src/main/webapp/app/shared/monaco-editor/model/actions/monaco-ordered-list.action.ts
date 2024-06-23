5d224d2bdee2986b52341ab6d47e5454d4cbcfc6
Patrik Zander <patrik.zander@tum.de>
2024-06-23 02:34:10 +0200

    Remove TODOs

--- src/main/webapp/app/shared/monaco-editor/model/actions/monaco-ordered-list.action.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/monaco-ordered-list.action.ts
@@ -15,8 +15,6 @@ export class MonacoOrderedListAction extends MonacoEditorAction {
             for (let lineNumber = selection.startLineNumber; lineNumber <= selection.endLineNumber; lineNumber++) {
                 const bulletNumber = lineNumber - selection.startLineNumber + 1;
                 const lineContent = this.getLineText(editor, lineNumber);
-                // TODO: it could make sense not to toggle each line, but to toggle the whole selection.
-                // e.g. if all lines are already numbered, remove numbers from all lines. otherwise, add numbers to all lines and fix the numbering
                 if (lineContent && NUMBER_REGEX.test(lineContent)) {
                     const offset = 3; // Delete the dot and the space after the number as well. Columns in Monaco are 1-based.
                     this.deleteTextAtRange(editor, new monaco.Range(lineNumber, 1, lineNumber, lineContent.indexOf('. ') + offset));
--- src/main/webapp/app/shared/monaco-editor/model/actions/monaco-unordered-list.action.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/monaco-unordered-list.action.ts
@@ -14,8 +14,6 @@ export class MonacoUnorderedListAction extends MonacoEditorAction {
         if (selection) {
             for (let lineNumber = selection.startLineNumber; lineNumber <= selection.endLineNumber; lineNumber++) {
                 const lineContent = this.getLineText(editor, lineNumber);
-                // TODO: it could make sense not to toggle each line, but to toggle the whole selection.
-                // e.g. if all lines are already bulleted, remove bullets from all lines. otherwise, add bullets to all lines.
                 if (lineContent?.startsWith(LIST_BULLET)) {
                     this.deleteTextAtRange(editor, new monaco.Range(lineNumber, 1, lineNumber, 1 + LIST_BULLET.length));
                 } else {
