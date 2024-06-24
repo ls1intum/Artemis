7670c502b0fe71e0ca106063dc0fe3089f52e2ef
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-06-24 09:13:04 +0200

    shared module

---
+++ supporting_scripts/analysis-of-endpoint-connections/shared/src/main/java/de/tum/in/www1/artemis/EndpointAnalysis.java
@@ -0,0 +1,34 @@
+package de.tum.in.www1.artemis;
+
+import java.util.List;
+
+public class EndpointAnalysis {
+
+    private List<UsedEndpoints> usedEndpoints;
+
+    private List<EndpointInformation> unusedEndpoints;
+
+    public EndpointAnalysis(List<UsedEndpoints> usedEndpoints, List<EndpointInformation> unusedEndpoints) {
+        this.usedEndpoints = usedEndpoints;
+        this.unusedEndpoints = unusedEndpoints;
+    }
+
+    public EndpointAnalysis() {
+    }
+
+    public List<UsedEndpoints> getUsedEndpoints() {
+        return usedEndpoints;
+    }
+
+    public void setUsedEndpoints(List<UsedEndpoints> usedEndpoints) {
+        this.usedEndpoints = usedEndpoints;
+    }
+
+    public List<EndpointInformation> getUnusedEndpoints() {
+        return unusedEndpoints;
+    }
+
+    public void setUnusedEndpoints(List<EndpointInformation> unusedEndpoints) {
+        this.unusedEndpoints = unusedEndpoints;
+    }
+}
