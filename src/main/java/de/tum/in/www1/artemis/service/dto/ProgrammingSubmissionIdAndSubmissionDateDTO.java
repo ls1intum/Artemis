fe9b63fdade6f765579db58542d9e0f5cb68f42b
Dmytro Polityka <dmytro.polityka@tum.de>
2024-06-25 17:06:00 +0200

    add json properties

--- src/main/java/de/tum/in/www1/artemis/service/dto/ProgrammingSubmissionIdAndSubmissionDateDTO.java
+++ src/main/java/de/tum/in/www1/artemis/service/dto/ProgrammingSubmissionIdAndSubmissionDateDTO.java
@@ -2,5 +2,8 @@ package de.tum.in.www1.artemis.service.dto;
 
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ProgrammingSubmissionIdAndSubmissionDateDTO(long programmingSubmissionId, ZonedDateTime submissionDate) {
 }
