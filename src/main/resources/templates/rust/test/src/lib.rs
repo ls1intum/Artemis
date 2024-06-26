8ecd785fbce13a459164e3ff34eff2b74ad59ced
Marcel Gaupp <m.gaupp@tum.de>
2024-06-26 11:23:30 +0200

    Improve error reporting of example test

--- src/main/resources/templates/rust/test/src/lib.rs
+++ src/main/resources/templates/rust/test/src/lib.rs
@@ -4,7 +4,8 @@ mod tests {
 
     #[test]
     fn test_addition() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let actual = add(2, 2);
+        let expected = 4;
+        assert_eq!(expected, actual, "expected: {expected}, actual: {actual}");
     }
 }
