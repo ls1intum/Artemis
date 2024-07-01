016767691c11a6d286ee250c493e73b07620d7c7
Marcel Gaupp <m.gaupp@tum.de>
2024-07-01 16:17:05 +0200

    Clarify test descriptions
    Co-authored-by: Johannes Stöhr <johannes.stoehr@tum.de>

--- src/main/resources/templates/javascript/test/src/math.test.js
+++ src/main/resources/templates/javascript/test/src/math.test.js
@@ -1,11 +1,11 @@
 import { math } from 'artemis-exercise';
 
 describe('math', () => {
-    it('should add 1 and 2 to 3', () => {
+    it('should add 1 and 2 to get 3', () => {
         expect(math.add(1, 2)).toBe(3);
     });
 
-    it('should multiply 3 and 5 to 15', () => {
+    it('should multiply 3 and 5 to get 15', () => {
         expect(math.multiply(3, 5)).toBe(15);
     });
 });
