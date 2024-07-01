62137e2b666a91581812b8aa934e574872c6dfbb
Ege Kurt <egekurt14@gmail.com>
2024-07-01 19:06:46 +0200

    temporarily disable exam page playwright tests

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['junit', { outputFile: './test-reports/results.xml' }]],
     testIgnore: [
-        '**/exam/**', // This will ignore all tests in the excluded-folder directory
+        '**/exam/**', // This will temporarily disable exam page tests
     ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
