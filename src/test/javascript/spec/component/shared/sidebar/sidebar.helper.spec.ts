5f60333471adc0f67116bbc09c92b581cd04957b
Florian Glombik <florian.glombik@tum.de>
2024-07-04 22:49:57 +0200

    Adding tests for exercise type filter options

--- src/test/javascript/spec/component/shared/sidebar/sidebar.helper.spec.ts
+++ src/test/javascript/spec/component/shared/sidebar/sidebar.helper.spec.ts
@@ -1,7 +1,7 @@
-import { getExerciseCategoryFilterOptions } from 'app/shared/sidebar/sidebar.helper';
+import { getExerciseCategoryFilterOptions, getExerciseTypeFilterOptions } from 'app/shared/sidebar/sidebar.helper';
 import { SidebarCardElement, SidebarData } from 'app/types/sidebar';
 import { ExerciseCategory } from 'app/entities/exercise-category.model';
-import { Exercise } from 'app/entities/exercise.model';
+import { Exercise, ExerciseType, getIcon } from 'app/entities/exercise.model';
 
 const EXERCISE_1 = { categories: [new ExerciseCategory('#691b0b', 'category1'), new ExerciseCategory('#1b97ca', 'category2')] } as Exercise;
 const EXERCISE_2 = { categories: [new ExerciseCategory('#0d3cc2', 'category3'), new ExerciseCategory('#6ae8ac', 'category4')] } as Exercise;
@@ -10,9 +10,9 @@ const EXERCISE_3 = { categories: [new ExerciseCategory('#691b0b', 'category5')] 
 /** contains 1 duplicate exercise category with {@link EXERCISE_1} */
 const EXERCISE_4 = { categories: [new ExerciseCategory('#691b0b', 'category1'), new ExerciseCategory('#1b97ca', 'category8')] } as Exercise;
 
-const SIDEBAR_CARD_ELEMENT_1 = { exercise: EXERCISE_1 } as SidebarCardElement;
-const SIDEBAR_CARD_ELEMENT_2 = { exercise: EXERCISE_2 } as SidebarCardElement;
-const SIDEBAR_CARD_ELEMENT_3 = { exercise: EXERCISE_3 } as SidebarCardElement;
+const SIDEBAR_CARD_ELEMENT_1 = { exercise: EXERCISE_1, type: ExerciseType.TEXT } as SidebarCardElement;
+const SIDEBAR_CARD_ELEMENT_2 = { exercise: EXERCISE_2, type: ExerciseType.PROGRAMMING } as SidebarCardElement;
+const SIDEBAR_CARD_ELEMENT_3 = { exercise: EXERCISE_3, type: ExerciseType.QUIZ } as SidebarCardElement;
 const SIDEBAR_CARD_ELEMENT_4 = { exercise: EXERCISE_4 } as SidebarCardElement;
 
 describe('getExerciseCategoryFilterOptions', () => {
@@ -90,6 +90,46 @@ describe('getExerciseCategoryFilterOptions', () => {
         const exerciseCategories = getExerciseCategoryFilterOptions(
             {
                 categoryFilter: {
+                    isDisplayed: false,
+                    options: [],
+                },
+            },
+            sidebarData,
+        );
+        expect(exerciseCategories).toEqual({
+            isDisplayed: false,
+            options: [],
+        });
+    });
+});
+
+describe('getExerciseTypeFilterOptions', () => {
+    it('should return present difficulty levels and sort them properly (same order as instructor creation)', () => {
+        const sidebarData: SidebarData = {
+            groupByCategory: true,
+            ungroupedData: [SIDEBAR_CARD_ELEMENT_1, SIDEBAR_CARD_ELEMENT_2, SIDEBAR_CARD_ELEMENT_3, SIDEBAR_CARD_ELEMENT_4],
+        };
+
+        const exerciseTypes = getExerciseTypeFilterOptions(undefined, sidebarData);
+        expect(exerciseTypes).toEqual({
+            isDisplayed: true,
+            options: [
+                { name: 'artemisApp.courseStatistics.programming', value: ExerciseType.PROGRAMMING, checked: false, icon: getIcon(ExerciseType.PROGRAMMING) },
+                { name: 'artemisApp.courseStatistics.quiz', value: ExerciseType.QUIZ, checked: false, icon: getIcon(ExerciseType.QUIZ) },
+                { name: 'artemisApp.courseStatistics.text', value: ExerciseType.TEXT, checked: false, icon: getIcon(ExerciseType.TEXT) },
+            ],
+        });
+    });
+
+    it('should directly return if already initialized', () => {
+        const sidebarData: SidebarData = {
+            groupByCategory: true,
+            ungroupedData: [SIDEBAR_CARD_ELEMENT_1, SIDEBAR_CARD_ELEMENT_2],
+        };
+
+        const exerciseCategories = getExerciseTypeFilterOptions(
+            {
+                exerciseTypesFilter: {
                     isDisplayed: false,
                     options: [],
                 },
