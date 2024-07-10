d9ad1d2459689c3dab77a45e20c9f69cea29115b
Lucas Welscher <ga53foy@mytum.de>
2024-07-10 17:04:37 +0200

    Update src/main/resources/templates/c/gcc/test/testUtils/Utils.py
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/main/resources/templates/c/gcc/test/testUtils/Utils.py
+++ src/main/resources/templates/c/gcc/test/testUtils/Utils.py
@@ -156,14 +156,13 @@ def printProg(text: str, addToCache: bool = True):
     if addToCache:
         testerOutputCache.append(msg)
 
-def sanitizeXml(text: str):
+def sanitizeXml(text: str) -> str:
     """
     Removes all characters that are not allowed in XML
     """
     # remove ANSI escape sequences first to avoid spurious characters
     # https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
     return re.sub(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])', "", text)
-
 
 def shortenText(text: str, maxNumChars: int):
     """
