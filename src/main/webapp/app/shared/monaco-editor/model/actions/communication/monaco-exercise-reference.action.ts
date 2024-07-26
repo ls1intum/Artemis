a48598568ffb6d699558dca96a8425d5a35ae981
Patrik Zander <patrik.zander@tum.de>
2024-07-26 13:35:35 +0200

    Refactor: Use shared completion provider method for Exercise reference as well

--- src/main/webapp/app/shared/monaco-editor/model/actions/communication/monaco-exercise-reference.action.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/communication/monaco-exercise-reference.action.ts
@@ -1,7 +1,8 @@
 import { TranslateService } from '@ngx-translate/core';
 import * as monaco from 'monaco-editor';
 import { MetisService } from 'app/shared/metis/metis.service';
-import { DomainActionWithOptionsArguments, MonacoEditorDomainActionWithOptions } from 'app/shared/monaco-editor/model/actions/monaco-editor-domain-action-with-options.model';
+import { MonacoEditorDomainActionWithOptions } from 'app/shared/monaco-editor/model/actions/monaco-editor-domain-action-with-options.model';
+import { ValueItem } from 'app/shared/markdown-editor/command-constants';
 
 export class MonacoExerciseReferenceAction extends MonacoEditorDomainActionWithOptions {
     static readonly ID = 'monaco-exercise-reference.action';
@@ -15,7 +16,6 @@ export class MonacoExerciseReferenceAction extends MonacoEditorDomainActionWithO
 
     register(editor: monaco.editor.IStandaloneCodeEditor, translateService: TranslateService) {
         super.register(editor, translateService);
-        const model = editor.getModel();
         const exercises = this.metisService.getCourse().exercises ?? [];
         this.setValues(
             exercises.map((exercise) => ({
@@ -24,58 +24,23 @@ export class MonacoExerciseReferenceAction extends MonacoEditorDomainActionWithO
                 type: exercise.type,
             })),
         );
-        if (!model) {
-            throw new Error(`A model must be attached to the editor to use the ${this.id} action.`);
-        }
-        const languageId = model.getLanguageId();
-        const modelId = model.id;
-        this.disposableCompletionProvider = monaco.languages.registerCompletionItemProvider(languageId, {
-            triggerCharacters: ['/'],
-            provideCompletionItems: (model: monaco.editor.ITextModel, position: monaco.Position): monaco.languages.ProviderResult<monaco.languages.CompletionList> => {
-                if (model.id !== modelId) {
-                    return undefined;
-                }
-                const wordUntilPosition = model.getWordUntilPosition(position);
-                const range = {
-                    startLineNumber: position.lineNumber,
-                    startColumn: wordUntilPosition.startColumn - 1,
-                    endLineNumber: position.lineNumber,
-                    endColumn: wordUntilPosition.endColumn,
-                };
-                // Check if before the word, we have a #
-                const beforeWord = model.getValueInRange({
-                    startLineNumber: position.lineNumber,
-                    startColumn: wordUntilPosition.startColumn - 1,
-                    endLineNumber: position.lineNumber,
-                    endColumn: wordUntilPosition.startColumn,
-                });
-                if (wordUntilPosition.word !== '/') {
-                    if (beforeWord !== '/') {
-                        return undefined;
-                    }
-                }
-
-                return {
-                    suggestions: this.getValues().map((item) => ({
-                        label: `/exercise ${item.value}`,
-                        kind: monaco.languages.CompletionItemKind.Constant,
-                        insertText: `[${item.type}]${item.value}(${this.metisService.getLinkForExercise(item.id)})[/${item.type}]`,
-                        range,
-                        detail: item.type ?? 'Exercise',
-                    })),
-                };
-            },
-        });
+        this.disposableCompletionProvider = this.registerCompletionProviderForCurrentModel<ValueItem>(
+            editor,
+            () => Promise.resolve(this.getValues()),
+            (item: ValueItem, range: monaco.IRange) => ({
+                label: `/exercise ${item.value}`,
+                kind: monaco.languages.CompletionItemKind.Constant,
+                insertText: `[${item.type}]${item.value}(${this.metisService.getLinkForExercise(item.id)})[/${item.type}]`,
+                range,
+                detail: item.type ?? this.label,
+            }),
+            '/',
+        );
     }
 
-    run(editor: monaco.editor.ICodeEditor, args?: DomainActionWithOptionsArguments): void {
-        if (!args?.selectedItem) {
-            editor.trigger('keyboard', 'type', { text: MonacoExerciseReferenceAction.DEFAULT_INSERT_TEXT });
-            editor.focus();
-        } else {
-            const item = args.selectedItem;
-            this.replaceTextAtCurrentSelection(editor, `[${item.type}]${item.value}(${this.metisService.getLinkForExercise(item.id)})[/${item.type}]`);
-        }
+    run(editor: monaco.editor.ICodeEditor): void {
+        this.typeText(editor, MonacoExerciseReferenceAction.DEFAULT_INSERT_TEXT);
+        editor.focus();
     }
 
     dispose() {
