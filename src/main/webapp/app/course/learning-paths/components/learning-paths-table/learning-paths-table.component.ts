cfdcf8fcdf003e2993ea0139bd6ba7932f4969cc
Johannes Wiest <Johannes.wiest@outlook.com>
2024-07-30 17:07:45 +0200

    add competency graph modal to table rows

--- src/main/webapp/app/course/learning-paths/components/learning-paths-table/learning-paths-table.component.ts
+++ src/main/webapp/app/course/learning-paths/components/learning-paths-table/learning-paths-table.component.ts
@@ -5,7 +5,9 @@ import { AlertService } from 'app/core/util/alert.service';
 import { LearningPathInformationDTO } from 'app/entities/competency/learning-path.model';
 import { SearchResult, SearchTermPageableSearch, SortingOrder } from 'app/shared/table/pageable-table';
 import { onError } from 'app/shared/util/global.utils';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faMaximize, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CompetencyGraphModalComponent } from 'app/course/learning-paths/components/competency-graph-modal/competency-graph-modal.component';
 
 enum TableColumn {
     ID = 'ID',
@@ -23,9 +25,11 @@ enum TableColumn {
 })
 export class LearningPathsTableComponent {
     protected readonly faSpinner = faSpinner;
+    protected readonly faMaximize = faMaximize;
 
     private readonly learningPathApiService = inject(LearningPathApiService);
     private readonly alertService = inject(AlertService);
+    private readonly modalService = inject(NgbModal);
 
     readonly courseId = input.required<number>();
 
@@ -73,7 +77,16 @@ export class LearningPathsTableComponent {
         }
     }
 
-    onPageChange(pageNumber: number) {
+    onPageChange(pageNumber: number): void {
         this.page.set(pageNumber);
+    }
+
+    openCompetencyGraph(learningPathId: number): void {
+        const modalRef = this.modalService.open(CompetencyGraphModalComponent, {
+            size: 'xl',
+            backdrop: 'static',
+            windowClass: 'competency-graph-modal',
+        });
+        modalRef.componentInstance.learningPathId = signal<number>(learningPathId);
     }
 }
