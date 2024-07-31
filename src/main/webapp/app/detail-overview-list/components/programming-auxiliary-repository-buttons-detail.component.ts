e1810ea5c737b0551b6e9dffaded592bec968ce9
Florian Glombik <florian.glombik@tum.de>
2024-07-31 22:47:52 +0200

    Fixing import of fa-icon component

--- src/main/webapp/app/detail-overview-list/components/programming-auxiliary-repository-buttons-detail.component.ts
+++ src/main/webapp/app/detail-overview-list/components/programming-auxiliary-repository-buttons-detail.component.ts
@@ -5,12 +5,13 @@ import { ArtemisSharedComponentModule } from 'app/shared/components/shared-compo
 import { ArtemisProgrammingExerciseActionsModule } from 'app/exercises/programming/shared/actions/programming-exercise-actions.module';
 import { ProgrammingAuxiliaryRepositoryButtonsDetail } from 'app/detail-overview-list/detail.model';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
 
 @Component({
     selector: 'jhi-programming-auxiliary-repository-buttons-detail',
     templateUrl: 'programming-auxiliary-repository-buttons-detail.component.html',
     standalone: true,
-    imports: [NoDataComponent, RouterModule, ArtemisSharedComponentModule, ArtemisProgrammingExerciseActionsModule],
+    imports: [NoDataComponent, RouterModule, ArtemisSharedComponentModule, ArtemisProgrammingExerciseActionsModule, ArtemisSharedModule],
 })
 export class ProgrammingAuxiliaryRepositoryButtonsDetailComponent {
     @Input() detail: ProgrammingAuxiliaryRepositoryButtonsDetail;
