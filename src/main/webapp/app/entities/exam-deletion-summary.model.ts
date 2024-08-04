bd2579986da546ab86a604336e587604b22733db
Ole Vester <ole.vester+gitlab@check24.de>
2024-08-04 14:57:36 +0200

    Add service method and DTO for fetching exam deletion summary

---
+++ src/main/webapp/app/entities/exam-deletion-summary.model.ts
@@ -0,0 +1,9 @@
+export class ExamDeletionSummaryDTO {
+    public numberOfBuilds: number;
+    public numberOfCommunicationPosts: number;
+    public numberOfAnswerPosts: number;
+    public numberRegisteredStudents: number;
+    public numberNotStartedExams: number;
+    public numberStartedExams: number;
+    public numberSubmittedExams: number;
+}
