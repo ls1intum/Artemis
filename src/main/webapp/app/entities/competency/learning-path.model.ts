74da0b3d2cf4ec8541838e669366831ba12dd92d
Johannes Wiest <Johannes.wiest@outlook.com>
2024-08-04 08:59:26 +0200

    Learning paths: Add learning path completion button (#9099)

--- src/main/webapp/app/entities/competency/learning-path.model.ts
+++ src/main/webapp/app/entities/competency/learning-path.model.ts
@@ -42,7 +42,7 @@ export interface LearningPathNavigationObjectDTO {
 
 export interface LearningPathNavigationDTO {
     predecessorLearningObject?: LearningPathNavigationObjectDTO;
-    currentLearningObject: LearningPathNavigationObjectDTO;
+    currentLearningObject?: LearningPathNavigationObjectDTO;
     successorLearningObject?: LearningPathNavigationObjectDTO;
     progress: number;
 }
