ed2f25c440dcbcc31d4d306c5a433ccecfa94e24
Lucas Welscher <ga53foy@mytum.de>
2024-08-12 14:57:50 +0200

    add server test

--- src/main/java/de/tum/in/www1/artemis/service/connectors/localci/buildagent/TestResultXmlParser.java
+++ src/main/java/de/tum/in/www1/artemis/service/connectors/localci/buildagent/TestResultXmlParser.java
@@ -106,7 +106,15 @@ class TestResultXmlParser {
         private String detailedMessage;
 
         private String extractMessage() {
-            return message != null ? message : detailedMessage;
+            if (message != null) {
+                return message;
+            }
+            else if (detailedMessage != null) {
+                return detailedMessage;
+            }
+            // empty text nodes are deserialized as null instead of a string, see: https://github.com/FasterXML/jackson-dataformat-xml/issues/565
+            // note that this workaround does not fix the issue entirely, as strings of only whitespace become the empty string
+            return "";
         }
 
         @JacksonXmlProperty(isAttribute = true, localName = "message")
@@ -116,8 +124,6 @@ class TestResultXmlParser {
 
         @JacksonXmlText
         public void setDetailedMessage(String detailedMessage) {
-            // empty text nodes are deserialized as null instead of a string, see: https://github.com/FasterXML/jackson-dataformat-xml/issues/565
-            // note that this workaround does not fix the issue entirely, as strings of only whitespace become the empty string
             this.detailedMessage = detailedMessage != null ? detailedMessage : "";
         }
     }
