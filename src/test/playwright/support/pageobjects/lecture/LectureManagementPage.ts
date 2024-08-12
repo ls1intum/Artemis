270979329a862e8b92d4fbc5686803818f26ad94
Patrik Zander <patrik.zander@tum.de>
2024-08-12 08:00:43 +0200

    Update playwright selectors, part 2

--- src/test/playwright/support/pageobjects/lecture/LectureManagementPage.ts
+++ src/test/playwright/support/pageobjects/lecture/LectureManagementPage.ts
@@ -95,7 +95,7 @@ export class LectureManagementPage {
         await this.openCreateUnit(UnitType.TEXT);
         await this.page.fill('#name', name);
         await this.page.fill('#pick-releaseDate #date-input-field', releaseDate.toString());
-        const contentField = this.page.locator('.monaco-editor .view-lines');
+        const contentField = this.page.locator('.monaco-editor');
         await contentField.click();
         await contentField.pressSequentially(text);
         return this.submitUnit();
