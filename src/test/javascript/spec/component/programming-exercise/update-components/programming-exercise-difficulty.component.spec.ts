e49688df0c880f4f5d989c6ced4d205eae04ace4
Yannik Schmidt <yannik.schmidt@tum.de>
2024-08-12 11:51:35 +0200

    Hide checkbox for allowing online Ide when THEIA profile is not active

--- src/test/javascript/spec/component/programming-exercise/update-components/programming-exercise-difficulty.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/update-components/programming-exercise-difficulty.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { MockComponent, MockPipe } from 'ng-mocks';
+import { DebugElement } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
@@ -9,10 +10,16 @@ import { DifficultyPickerComponent } from 'app/exercises/shared/difficulty-picke
 import { TeamConfigFormGroupComponent } from 'app/exercises/shared/team-config-form-group/team-config-form-group.component';
 import { programmingExerciseCreationConfigMock } from './programming-exercise-creation-config-mock';
 import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { ProfileInfo } from 'app/shared/layouts/profiles/profile-info.model';
+import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
+import { PROFILE_THEIA } from 'app/app.constants';
 
 describe('ProgrammingExerciseDifficultyComponent', () => {
     let fixture: ComponentFixture<ProgrammingExerciseDifficultyComponent>;
     let comp: ProgrammingExerciseDifficultyComponent;
+    let debugElement: DebugElement;
+    let profileService: ProfileService;
+    let getProfileInfoSub: jest.SpyInstance;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -42,6 +49,11 @@ describe('ProgrammingExerciseDifficultyComponent', () => {
                 comp = fixture.componentInstance;
                 comp.programmingExercise = new ProgrammingExercise(undefined, undefined);
                 comp.programmingExerciseCreationConfig = programmingExerciseCreationConfigMock;
+
+                debugElement = fixture.debugElement;
+                profileService = debugElement.injector.get(ProfileService);
+                getProfileInfoSub = jest.spyOn(profileService, 'getProfileInfo');
+                getProfileInfoSub.mockReturnValue(of({ inProduction: false, sshCloneURLTemplate: 'ssh://git@testserver.com:1234/' } as ProfileInfo));
             });
     });
 
@@ -53,4 +65,12 @@ describe('ProgrammingExerciseDifficultyComponent', () => {
         fixture.detectChanges();
         expect(comp).not.toBeNull();
     }));
+
+    it('should initialize theiaEnabled', () => {
+        getProfileInfoSub = jest.spyOn(profileService, 'getProfileInfo');
+        getProfileInfoSub.mockReturnValue(of({ activeProfiles: [PROFILE_THEIA] } as ProfileInfo));
+
+        fixture.detectChanges();
+        expect(comp.theiaEnabled).toBeTrue();
+    });
 });
