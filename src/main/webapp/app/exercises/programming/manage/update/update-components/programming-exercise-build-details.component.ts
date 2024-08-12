8157ee6fd64f2b241e5c7a06c0c05b8509297373
Florian Glombik <florian.glombik@tum.de>
2024-08-12 15:02:04 +0200

    Moving steps from general and language to the build details step

--- src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-build-details.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-build-details.component.ts
@@ -1,22 +1,67 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
-import { ProgrammingExerciseCreationConfig } from 'app/exercises/programming/manage/update/programming-exercise-creation-config';
-import { Subject } from 'rxjs';
+import { AfterViewInit, Component, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Subject, Subscription } from 'rxjs';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
+import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
+import { ArtemisTableModule } from 'app/shared/table/table.module';
+import { ProgrammingExerciseRepositoryAndBuildPlanDetailsComponent } from 'app/exercises/programming/shared/build-details/programming-exercise-repository-and-build-plan-details.component';
+import { ProgrammingExerciseCreationConfig } from 'app/exercises/programming/manage/update/programming-exercise-creation-config';
+import { ProgrammingExercise } from 'app/entities/programming-exercise.model';
+import { TableEditableFieldComponent } from 'app/shared/table/table-editable-field.component';
+import { every } from 'lodash-es';
 
 @Component({
     selector: 'jhi-programming-exercise-build-details',
     templateUrl: './programming-exercise-build-details.component.html',
     styleUrls: ['../../programming-exercise-form.scss'],
     standalone: true,
-    imports: [ArtemisSharedModule],
+    imports: [ArtemisSharedModule, ArtemisSharedComponentModule, ArtemisTableModule, ProgrammingExerciseRepositoryAndBuildPlanDetailsComponent],
 })
-export class ProgrammingExerciseBuildDetailsComponent {
+export class ProgrammingExerciseBuildDetailsComponent implements AfterViewInit {
     formValid: boolean;
     formValidChanges = new Subject<boolean>();
 
+    inputFieldSubscriptions: (Subscription | undefined)[] = [];
+
     @Input() isLocal: boolean;
+    @Input() programmingExercise: ProgrammingExercise;
     @Input() programmingExerciseCreationConfig: ProgrammingExerciseCreationConfig;
-    @Output() exerciseChange = new EventEmitter<ProgrammingExercise>();
-    @Output() problemStatementChange = new EventEmitter<string>();
+
+    @ViewChild('shortName') shortNameField: NgModel;
+    @ViewChildren(TableEditableFieldComponent) tableEditableFields?: QueryList<TableEditableFieldComponent>;
+    @ViewChild('checkoutSolutionRepository') checkoutSolutionRepositoryField?: NgModel;
+
+    ngAfterViewInit() {
+        this.inputFieldSubscriptions.push(this.checkoutSolutionRepositoryField?.valueChanges?.subscribe(() => this.calculateFormValid()));
+        this.inputFieldSubscriptions.push(this.shortNameField.valueChanges?.subscribe(() => this.calculateFormValid()));
+        this.tableEditableFields?.changes.subscribe((fields: QueryList<TableEditableFieldComponent>) => {
+            fields.toArray().forEach((field) => this.inputFieldSubscriptions.push(field.editingInput.valueChanges?.subscribe(() => this.calculateFormValid())));
+        });
+    }
+
+    calculateFormValid() {
+        this.formValid = Boolean(!this.shortNameField.invalid && this.isCheckoutSolutionRepositoryValid() && this.areAuxiliaryRepositoriesValid());
+        this.formValidChanges.next(this.formValid);
+    }
+
+    private areAuxiliaryRepositoriesValid(): boolean {
+        return (
+            (every(
+                this.tableEditableFields?.map((field) => field.editingInput.valid),
+                Boolean,
+            ) &&
+                !this.programmingExerciseCreationConfig.auxiliaryRepositoryDuplicateDirectories &&
+                !this.programmingExerciseCreationConfig.auxiliaryRepositoryDuplicateNames) ||
+            !this.programmingExercise.auxiliaryRepositories?.length
+        );
+    }
+
+    private isCheckoutSolutionRepositoryValid(): boolean {
+        return Boolean(
+            this.checkoutSolutionRepositoryField?.valid ||
+                this.programmingExercise.id ||
+                !this.programmingExercise.programmingLanguage ||
+                !this.programmingExerciseCreationConfig.checkoutSolutionRepositoryAllowed,
+        );
+    }
 }
--- src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-information.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/update-components/programming-exercise-information.component.ts
@@ -1,11 +1,9 @@
-import { AfterViewInit, Component, Input, OnDestroy, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, ViewChild } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { ProgrammingExercise, ProjectType } from 'app/entities/programming-exercise.model';
 import { ProgrammingExerciseCreationConfig } from 'app/exercises/programming/manage/update/programming-exercise-creation-config';
 import { ExerciseTitleChannelNameComponent } from 'app/exercises/shared/exercise-title-channel-name/exercise-title-channel-name.component';
 import { Subject, Subscription } from 'rxjs';
-import { TableEditableFieldComponent } from 'app/shared/table/table-editable-field.component';
-import { every } from 'lodash-es';
 import { ImportOptions } from 'app/types/programming-exercises';
 
 @Component({
@@ -22,9 +20,6 @@ export class ProgrammingExerciseInformationComponent implements AfterViewInit, O
     @Input() importOptions: ImportOptions;
 
     @ViewChild(ExerciseTitleChannelNameComponent) exerciseTitleChannelComponent: ExerciseTitleChannelNameComponent;
-    @ViewChildren(TableEditableFieldComponent) tableEditableFields?: QueryList<TableEditableFieldComponent>;
-    @ViewChild('shortName') shortNameField: NgModel;
-    @ViewChild('checkoutSolutionRepository') checkoutSolutionRepositoryField?: NgModel;
     @ViewChild('recreateBuildPlans') recreateBuildPlansField?: NgModel;
     @ViewChild('updateTemplateFiles') updateTemplateFilesField?: NgModel;
 
@@ -37,13 +32,8 @@ export class ProgrammingExerciseInformationComponent implements AfterViewInit, O
 
     ngAfterViewInit() {
         this.inputFieldSubscriptions.push(this.exerciseTitleChannelComponent.titleChannelNameComponent?.formValidChanges.subscribe(() => this.calculateFormValid()));
-        this.inputFieldSubscriptions.push(this.shortNameField.valueChanges?.subscribe(() => this.calculateFormValid()));
-        this.inputFieldSubscriptions.push(this.checkoutSolutionRepositoryField?.valueChanges?.subscribe(() => this.calculateFormValid()));
         this.inputFieldSubscriptions.push(this.recreateBuildPlansField?.valueChanges?.subscribe(() => this.calculateFormValid()));
         this.inputFieldSubscriptions.push(this.updateTemplateFilesField?.valueChanges?.subscribe(() => this.calculateFormValid()));
-        this.tableEditableFields?.changes.subscribe((fields: QueryList<TableEditableFieldComponent>) => {
-            fields.toArray().forEach((field) => this.inputFieldSubscriptions.push(field.editingInput.valueChanges?.subscribe(() => this.calculateFormValid())));
-        });
     }
 
     ngOnDestroy(): void {
@@ -53,31 +43,12 @@ export class ProgrammingExerciseInformationComponent implements AfterViewInit, O
     }
 
     calculateFormValid() {
-        const isCheckoutSolutionRepositoryValid = this.isCheckoutSolutionRepositoryValid();
         const isRecreateBuildPlansValid = this.isRecreateBuildPlansValid();
         const isUpdateTemplateFilesValid = this.isUpdateTemplateFilesValid();
-        const areAuxiliaryRepositoriesValid = this.areAuxiliaryRepositoriesValid();
         this.formValid = Boolean(
-            this.exerciseTitleChannelComponent.titleChannelNameComponent?.formValid &&
-                !this.shortNameField.invalid &&
-                isCheckoutSolutionRepositoryValid &&
-                isRecreateBuildPlansValid &&
-                isUpdateTemplateFilesValid &&
-                areAuxiliaryRepositoriesValid,
+            this.exerciseTitleChannelComponent.titleChannelNameComponent?.formValid && isRecreateBuildPlansValid && isUpdateTemplateFilesValid && areAuxiliaryRepositoriesValid,
         );
         this.formValidChanges.next(this.formValid);
-    }
-
-    areAuxiliaryRepositoriesValid(): boolean {
-        return (
-            (every(
-                this.tableEditableFields?.map((field) => field.editingInput.valid),
-                Boolean,
-            ) &&
-                !this.programmingExerciseCreationConfig.auxiliaryRepositoryDuplicateDirectories &&
-                !this.programmingExerciseCreationConfig.auxiliaryRepositoryDuplicateNames) ||
-            !this.programmingExercise.auxiliaryRepositories?.length
-        );
     }
 
     isUpdateTemplateFilesValid(): boolean {
@@ -91,14 +62,5 @@ export class ProgrammingExerciseInformationComponent implements AfterViewInit, O
 
     isRecreateBuildPlansValid(): boolean {
         return this.recreateBuildPlansField?.valid || !this.programmingExerciseCreationConfig.isImportFromExistingExercise;
-    }
-
-    isCheckoutSolutionRepositoryValid(): boolean {
-        return Boolean(
-            this.checkoutSolutionRepositoryField?.valid ||
-                this.programmingExercise.id ||
-                !this.programmingExercise.programmingLanguage ||
-                !this.programmingExerciseCreationConfig.checkoutSolutionRepositoryAllowed,
-        );
     }
 }
