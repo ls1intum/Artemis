8495333c5f143a291851aa061081fe5cf59bf687
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-08-14 15:29:54 +0200

    undo more CodeRabbit changes

--- supporting_scripts/analysis-of-endpoint-connections/src/main/java/de/tum/cit/endpointanalysis/EndpointAnalysis.java
+++ supporting_scripts/analysis-of-endpoint-connections/src/main/java/de/tum/cit/endpointanalysis/EndpointAnalysis.java
@@ -4,20 +4,30 @@ import java.util.List;
 
 public class EndpointAnalysis {
 
-    private final List<UsedEndpoints> usedEndpoints;
+    private List<UsedEndpoints> usedEndpoints;
 
-    private final List<EndpointInformation> unusedEndpoints;
+    private List<EndpointInformation> unusedEndpoints;
 
     public EndpointAnalysis(List<UsedEndpoints> usedEndpoints, List<EndpointInformation> unusedEndpoints) {
         this.usedEndpoints = usedEndpoints;
         this.unusedEndpoints = unusedEndpoints;
     }
 
+    public EndpointAnalysis() {
+    }
+
     public List<UsedEndpoints> getUsedEndpoints() {
         return usedEndpoints;
+    }
+
+    public void setUsedEndpoints(List<UsedEndpoints> usedEndpoints) {
+        this.usedEndpoints = usedEndpoints;
     }
 
     public List<EndpointInformation> getUnusedEndpoints() {
         return unusedEndpoints;
     }
-}
+
+    public void setUnusedEndpoints(List<EndpointInformation> unusedEndpoints) {
+        this.unusedEndpoints = unusedEndpoints;
+    }
