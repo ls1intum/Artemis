4089bf4855aace8bc4d09b5671ce3b8473009cff
Marcel Gaupp <m.gaupp@tum.de>
2024-08-17 15:49:13 +0200

    Fix bubble sort

--- src/main/resources/templates/javascript/solution/src/bubblesort.js
+++ src/main/resources/templates/javascript/solution/src/bubblesort.js
@@ -7,7 +7,7 @@ export default class BubbleSort {
     performSort(input) {
         for (let i = input.length - 1; i >= 0; i--) {
             for (let j = 0; j < i; j++) {
-                if (input[j].valueOf() > input[i].valueOf()) {
+                if (input[j].valueOf() > input[j + 1].valueOf()) {
                     const temp = input[j];
                     input[j] = input[j + 1];
                     input[j + 1] = temp;
