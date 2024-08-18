04a927d2802d408f9ddf143e5c7523a5c23a9928
Johannes Wiest <Johannes.wiest@outlook.com>
2024-08-18 17:23:46 +0200

    move create CoruseCompetencyRelations to course-competency-api.service.ts

--- src/main/webapp/app/course/competencies/services/course-competency-api.service.ts
+++ src/main/webapp/app/course/competencies/services/course-competency-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseApiHttpService } from 'app/course/learning-paths/services/base-api-http.service';
-import { CompetencyWithTailRelationDTO, CourseCompetencyImportOptionsDTO } from 'app/entities/competency.model';
+import { CompetencyRelation, CompetencyRelationDTO, CompetencyWithTailRelationDTO, CourseCompetencyImportOptionsDTO } from 'app/entities/competency.model';
 
 @Injectable({ providedIn: 'root' })
 export class CourseCompetencyApiService extends BaseApiHttpService {
@@ -12,5 +12,9 @@ export class CourseCompetencyApiService extends BaseApiHttpService {
 
     importAllByCourseId(courseId: number, courseCompetencyImportOptions: CourseCompetencyImportOptionsDTO): Promise<CompetencyWithTailRelationDTO[]> {
         return this.post<CompetencyWithTailRelationDTO[]>(`${this.getPath(courseId)}/import-all`, courseCompetencyImportOptions);
+    }
+
+    createCourseCompetencyRelation(courseId: number, relation: CompetencyRelation): Promise<CompetencyRelationDTO> {
+        return this.post<CompetencyRelationDTO>(`${this.getPath(courseId)}/relations`, relation);
     }
 }
