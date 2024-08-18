c944bd4a95e1bbf56abbbc4891a042a2428d18ad
Mohamed Bilel Besrour <besrourbilel@gmail.com>
2024-08-18 21:55:40 +0200

    update python

--- src/main/resources/templates/python/test/structural/structural_test.py
+++ src/main/resources/templates/python/test/structural/structural_test.py
@@ -1,9 +1,9 @@
 import unittest
-from assignment import sorting_algorithms
-from assignment import sort_strategy
-from assignment import context
-from assignment import policy
-from structural import structural_helpers
+from ${studentParentWorkingDirectoryName} import sorting_algorithms
+from ${studentParentWorkingDirectoryName} import sort_strategy
+from ${studentParentWorkingDirectoryName} import context
+from ${studentParentWorkingDirectoryName} import policy
+from ${studentParentWorkingDirectoryName} import structural_helpers
 
 
 class TestSortingStructural(unittest.TestCase):
