10fc7143cf3978937228f359c990118f38465462
Patrik Zander <patrik.zander@tum.de>
2024-08-19 12:32:43 +0200

    Refactor markdown-editor-parsing.helper.ts: rename / update docs

--- src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-parsing.helper.ts
+++ src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-parsing.helper.ts
@@ -2,7 +2,12 @@ import { MonacoEditorDomainAction } from 'app/shared/monaco-editor/model/actions
 import { escapeStringForUseInRegex } from 'app/shared/util/global.utils';
 import { TextWithDomainAction } from './markdown-editor-monaco.component';
 
-// TODO update docs
+/**
+ * Searches for the domain actions in the given markdown text and returns the text split into parts, each part associated with the domain action it belongs to.
+ * Note that the text will be trimmed before being returned.
+ * @param markdown The markdown text to parse
+ * @param domainActions The domain actions to search for in the markdown
+ */
 export function parseMarkdownForDomainActions(markdown: string, domainActions: MonacoEditorDomainAction[]): TextWithDomainAction[] {
     let remainingText = markdown;
     const actionIdentifiersString = domainActions
@@ -11,45 +16,44 @@ export function parseMarkdownForDomainActions(markdown: string, domainActions: M
         .map(escapeStringForUseInRegex)
         .join('|');
     const actionTextsMappedToActionIdentifiers: TextWithDomainAction[] = [];
-    /** create a new regex expression which searches for the domainCommands identifiers
-     * (?=   If a command is found, add the command identifier to the result of the split
-     * \\[  look for the character '[' to determine the beginning of the command identifier
-     * (${commandIdentifiersString}) look if after the '[' one of the element of commandIdentifiersString is contained
-     * ] look for the character ']' to determine the end of the command identifier
-     * )  close the bracket
-     *  g: search in the whole string
-     *  i: case insensitive, neglecting capital letters
-     *  m: match the regex over multiple lines*/
+
+    /*
+     * The following regex is used to split the text into parts, each part associated with the domain action it belongs to. It is structured as follows:
+     * 1. (?=   If an action is found, add the action identifier to the result of the split
+     * 2. \\[  look for the character '[' to determine the beginning of the action identifier
+     * 3. (${actionIdentifiersString}) look if after the '[' one of the element of actionIdentifiersString is contained
+     * 4. ] look for the character ']' to determine the end of the action identifier
+     * 5. ) ends the group
+     * Flags:
+     * - g: search in the whole string
+     * - m: match the regex over multiple lines
+     * - i: case-insensitive matching
+     */
     const regex = new RegExp(`(?=\\[(${actionIdentifiersString})])`, 'gmi');
     while (remainingText.length) {
         const [textWithActionIdentifier] = remainingText.split(regex, 1);
         remainingText = remainingText.substring(textWithActionIdentifier.length);
-        const commandTextWithCommandIdentifier = parseLineForDomainCommand(textWithActionIdentifier.trim(), domainActions);
-        actionTextsMappedToActionIdentifiers.push(commandTextWithCommandIdentifier);
+        const textWithDomainAction = parseLineForDomainAction(textWithActionIdentifier.trim(), domainActions);
+        actionTextsMappedToActionIdentifiers.push(textWithDomainAction);
     }
 
     return actionTextsMappedToActionIdentifiers;
 }
 
 /**
- * @function parseLineForDomainCommand
- * @desc Couple each text with the domainCommandIdentifier to emit that to the parent component for the value assignment
- *       1. Check which domainCommand identifier is contained within the text
- *       2. Remove the domainCommand identifier from the text
- *       3. Create an array with first element text and second element the domainCommand TODO UPDATE DOCS (command -> action) identifier
- * @param text {string} from the parse function
- * @param domainActions The array of domain actions for which to extract the identifiers
- * @return array of the text with the domainCommand identifier
+ * Checks if the given line contains any of the domain action identifiers and returns the text without the identifier along with the domain action it belongs to.
+ * @param text The text to parse
+ * @param domainActions The domain actions to search for in the text
  */
-function parseLineForDomainCommand(text: string, domainActions: MonacoEditorDomainAction[]): TextWithDomainAction {
+function parseLineForDomainAction(text: string, domainActions: MonacoEditorDomainAction[]): TextWithDomainAction {
     for (const domainAction of domainActions) {
-        const possibleOpeningCommandIdentifier = [
+        const possibleOpeningIdentifiers = [
             domainAction.getOpeningIdentifier(),
             domainAction.getOpeningIdentifier().toLowerCase(),
             domainAction.getOpeningIdentifier().toUpperCase(),
         ];
-        if (possibleOpeningCommandIdentifier.some((identifier) => text.indexOf(identifier) !== -1)) {
-            const trimmedLineWithoutIdentifier = possibleOpeningCommandIdentifier.reduce((line, identifier) => line.replace(identifier, ''), text).trim();
+        if (possibleOpeningIdentifiers.some((identifier) => text.indexOf(identifier) !== -1)) {
+            const trimmedLineWithoutIdentifier = possibleOpeningIdentifiers.reduce((line, identifier) => line.replace(identifier, ''), text).trim();
             return [trimmedLineWithoutIdentifier, domainAction];
         }
     }
