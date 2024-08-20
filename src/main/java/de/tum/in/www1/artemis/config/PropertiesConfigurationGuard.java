04287ddb6e89949dc3cc1ed8e8c7f2e7109bd201
entholzer <nom.ise@outlook.com>
2024-08-20 16:29:43 +0200

    added javadoc

--- src/main/java/de/tum/in/www1/artemis/config/PropertiesConfigurationGuard.java
+++ src/main/java/de/tum/in/www1/artemis/config/PropertiesConfigurationGuard.java
@@ -18,11 +18,14 @@ public class PropertiesConfigurationGuard implements InitializingBean {
     @Value("${info.operatorName:#{null}}")
     private String operatorName;
 
+    /**
+     * Checks if the info.operatorName value is set in the configuration ymls, and exits the application if not.
+     */
     public void afterPropertiesSet() {
         if (this.operatorName == null || this.operatorName.isEmpty()) {
             log.error(
                     "The name of the operator (University) is not configured in the application-prod.yml! It is needed to be displayed in the /about page, and for the telemetry service.");
-            throw new IllegalArgumentException("The name of the operator (university) must be configured, but is not!"); // exits the application
+            throw new IllegalArgumentException("The name of the operator (university) must be configured, but is not!");
         }
     }
 }
