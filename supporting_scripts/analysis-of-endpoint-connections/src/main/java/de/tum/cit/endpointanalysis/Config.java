80589e050160cb4d7ff5bc8f3077e68cc16a5049
Jan-Thurner <107639007+Jan-Thurner@users.noreply.github.com>
2024-08-21 17:22:37 +0200

    fix typo

--- supporting_scripts/analysis-of-endpoint-connections/src/main/java/de/tum/cit/endpointanalysis/Config.java
+++ supporting_scripts/analysis-of-endpoint-connections/src/main/java/de/tum/cit/endpointanalysis/Config.java
@@ -2,10 +2,10 @@ package de.tum.cit.endpointanalysis;
 
 import java.util.List;
 
-public record Config(List<String> excludedEndpointFiles, List<String> excludedEndpoints, List<ExcludedEnpointOrRestCall> endpointsParsedIncorrectly,
-        List<String> excludedRestCallFiles, List<String> excludedRestCalls, List<ExcludedEnpointOrRestCall> restCallsParsedIncorrectly, String endpointParsingResultPath,
-        String restCallParsingResultPath, String endpointAnalysisResultPath, String restCallAnalysisResultPath, String clientDirPath) {
+public record Config(List<String> excludedEndpointFiles, List<String> excludedEndpoints, List<ExcludedEndpointOrRestCall> endpointsParsedIncorrectly,
+                     List<String> excludedRestCallFiles, List<String> excludedRestCalls, List<ExcludedEndpointOrRestCall> restCallsParsedIncorrectly, String endpointParsingResultPath,
+                     String restCallParsingResultPath, String endpointAnalysisResultPath, String restCallAnalysisResultPath, String clientDirPath) {
 
-    static record ExcludedEnpointOrRestCall(String file, int line) {
+    static record ExcludedEndpointOrRestCall(String file, int line) {
     }
 }
