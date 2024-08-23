175896e90b1405760d5a55c314072a93f1d23f7f
Florian Glombik <florian.glombik@tum.de>
2024-08-23 16:52:52 +0200

    Re-adding fix for Safari

--- src/main/webapp/app/forms/form-status-bar/form-status-bar.component.ts
+++ src/main/webapp/app/forms/form-status-bar/form-status-bar.component.ts
@@ -21,6 +21,13 @@ export class FormStatusBarComponent implements AfterViewInit {
 
     @HostListener('window:resize')
     onResize() {
+        const addingDistanceToNavbarWouldBreakStatusBar = this.isSafari();
+        if (addingDistanceToNavbarWouldBreakStatusBar) {
+            // on Safari the FormStatusBar disappears when the following code is executed, so we skip it
+            // the only negative consequence is that the FormStatusBar is a bit closer to the navbar than in Chrome/Firefox
+            return;
+        }
+
         setTimeout(function addDistanceFromStatusBarToNavbar() {
             const headerHeight = (document.querySelector('jhi-navbar') as HTMLElement).offsetHeight;
             document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
@@ -31,13 +38,16 @@ export class FormStatusBarComponent implements AfterViewInit {
         this.onResize();
     }
 
+    private isSafari() {
+        return SAFARI_USER_AGENT_REGEX.test(navigator.userAgent);
+    }
+
     scrollToHeadline(id: string) {
         const element = document.getElementById(id);
         if (element) {
             const headerHeight = (document.querySelector('jhi-navbar') as HTMLElement)?.offsetHeight;
 
-            const isSafari = SAFARI_USER_AGENT_REGEX.test(navigator.userAgent);
-            const offset = isSafari ? SAFARI_HEADLINE_OFFSET : CHROME_HEADLINE_OFFSET;
+            const offset = this.isSafari() ? SAFARI_HEADLINE_OFFSET : CHROME_HEADLINE_OFFSET;
 
             element.style.scrollMarginTop = `calc(${offset}rem + ${headerHeight}px)`;
             element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
