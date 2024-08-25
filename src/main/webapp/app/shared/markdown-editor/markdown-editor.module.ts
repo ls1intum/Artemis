57ede76149a4219e4fceed2146fc19afc6b34586
Patrik Zander <patrik.zander@tum.de>
2024-08-25 09:20:24 +0200

    Delete SelectWithSearch and MultiOptionCommand

--- src/main/webapp/app/shared/markdown-editor/markdown-editor.module.ts
+++ src/main/webapp/app/shared/markdown-editor/markdown-editor.module.ts
@@ -4,14 +4,13 @@ import { ArtemisColorSelectorModule } from 'app/shared/color-selector/color-sele
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-import { SelectWithSearchComponent } from './select-with-search/select-with-search.component';
 import { MarkdownEditorMonacoComponent } from 'app/shared/markdown-editor/monaco/markdown-editor-monaco.component';
 import { MonacoEditorModule } from 'app/shared/monaco-editor/monaco-editor.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
     imports: [ArtemisSharedModule, MonacoEditorModule, FormsModule, ArtemisColorSelectorModule, MatMenuModule, MatButtonModule, DragDropModule],
-    declarations: [MarkdownEditorMonacoComponent, SelectWithSearchComponent],
+    declarations: [MarkdownEditorMonacoComponent],
     exports: [MarkdownEditorMonacoComponent],
 })
 export class ArtemisMarkdownEditorModule {}
