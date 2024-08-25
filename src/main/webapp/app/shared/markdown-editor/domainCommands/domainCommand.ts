1a7d5165fd4fb665134fda4dc403621281d8660d
Patrik Zander <patrik.zander@tum.de>
2024-08-25 09:16:58 +0200

    Delete Domain command hierarchy

--- src/main/webapp/app/shared/markdown-editor/domainCommands/domainCommand.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from 'app/shared/markdown-editor/commands/command';
-
-/** abstract class for all domainCommands - customized commands for Artemis specific use cases
- * e.g. multiple choice questions, drag and drop questions
- * Each domain command has its own logic and a unique identifier**/
-export abstract class DomainCommand extends Command {
-    abstract getOpeningIdentifier(): string; // e.g. [exp]
-    abstract getClosingIdentifier(): string; // e.g. [/exp]
-}
