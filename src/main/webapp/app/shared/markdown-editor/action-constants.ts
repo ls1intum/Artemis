86521e1a83373613506ac9bdc94cce6839684aef
Patrik Zander <patrik.zander@tum.de>
2024-08-25 15:45:32 +0200

    Remove unused SlideItem

--- src/main/webapp/app/shared/markdown-editor/action-constants.ts
+++ src/main/webapp/app/shared/markdown-editor/action-constants.ts
@@ -1,17 +1,7 @@
-import { ReferenceType } from 'app/shared/metis/metis.util';
-
 export type ValueItem = {
     id: string;
     value: string;
     type?: string;
     elements?: ValueItem[];
     attachmentUnits?: ValueItem[];
-};
-
-// TODO: Remove and rename file
-export type SlideItem = {
-    id: string;
-    slideImagePath: string;
-    slideNumber: number;
-    courseArtifactType: ReferenceType;
 };
