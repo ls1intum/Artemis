85a1b1b881b7dc50aa77f58848ded06a220acdf2
Florian Glombik <florian.glombik@tum.de>
2024-08-30 16:28:48 +0200

    Removing not needed imports

--- src/main/webapp/app/detail-overview-list/components/programming-diff-report-detail/programming-diff-report-detail.component.ts
+++ src/main/webapp/app/detail-overview-list/components/programming-diff-report-detail/programming-diff-report-detail.component.ts
@@ -11,11 +11,6 @@ import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { SubmissionResultStatusModule } from 'app/overview/submission-result-status.module';
 import { ArtemisProgrammingExerciseStatusModule } from 'app/exercises/programming/manage/status/programming-exercise-status.module';
 import { ArtemisProgrammingExerciseActionsModule } from 'app/exercises/programming/shared/actions/programming-exercise-actions.module';
-import { ArtemisProgrammingExerciseInstructionsEditorModule } from 'app/exercises/programming/manage/instructions-editor/programming-exercise-instructions-editor.module';
-import { ArtemisProgrammingExerciseLifecycleModule } from 'app/exercises/programming/shared/lifecycle/programming-exercise-lifecycle.module';
-import { AssessmentInstructionsModule } from 'app/assessment/assessment-instructions/assessment-instructions.module';
-import { ArtemisModelingEditorModule } from 'app/exercises/modeling/shared/modeling-editor.module';
-import { ExerciseDetailDirective } from 'app/detail-overview-list/exercise-detail.directive';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -29,11 +24,6 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
         ArtemisProgrammingExerciseStatusModule,
         ArtemisProgrammingExerciseActionsModule,
         GitDiffReportModule,
-        ArtemisProgrammingExerciseInstructionsEditorModule,
-        ArtemisProgrammingExerciseLifecycleModule,
-        AssessmentInstructionsModule,
-        ArtemisModelingEditorModule,
-        ExerciseDetailDirective,
     ],
 })
 export class ProgrammingDiffReportDetailComponent {
