a66c2cf54be4f8df5a28b8b64f037e339dbbba5e
Florian Glombik <florian.glombik@tum.de>
2024-09-04 21:51:49 +0200

    Fixing translation for difficulty

--- src/main/webapp/app/exercises/programming/manage/update/update-components/difficulty/programming-exercise-difficulty.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/update-components/difficulty/programming-exercise-difficulty.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ProgrammingExercise } from 'app/entities/programming/programming-exercise.model';
 import { ArtemisDifficultyPickerModule } from 'app/exercises/shared/difficulty-picker/difficulty-picker.module';
+import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
 
 @Component({
     selector: 'jhi-programming-exercise-difficulty',
     standalone: true,
     templateUrl: './programming-exercise-difficulty.component.html',
     styleUrls: ['../../../programming-exercise-form.scss'],
-    imports: [ArtemisDifficultyPickerModule],
+    imports: [ArtemisDifficultyPickerModule, ArtemisSharedCommonModule],
 })
 export class ProgrammingExerciseDifficultyComponent {
     @Input({ required: true }) programmingExercise: ProgrammingExercise;
