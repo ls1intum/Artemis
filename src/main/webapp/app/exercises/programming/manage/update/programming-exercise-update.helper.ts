6706f6ce47e45e37378a47c31e5266481707b5f5
Florian Glombik <florian.glombik@tum.de>
2024-09-05 23:08:06 +0200

    Hiding channel name

--- src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.helper.ts
+++ src/main/webapp/app/exercises/programming/manage/update/programming-exercise-update.helper.ts
@@ -1,7 +1,12 @@
-export enum ProgrammingExerciseInputField {
-    title = 'title',
-    shortName = 'shortName',
-}
+// export enum ProgrammingExerciseInputField {
+//     // General section
+//     title = 'title',
+//     channelName = 'channelName',
+//     shortName = 'shortName',
+//     //
+// }
+
+export type ProgrammingExerciseInputField = 'title' | 'channelName' | 'shortName';
 
 export type EditMode = 'SIMPLE' | 'ADVANCED';
 
@@ -12,6 +17,9 @@ export type InputFieldEditModeMapping = Record<ProgrammingExerciseInputField, In
 export const INPUT_FIELD_EDIT_MODE_MAPPING: InputFieldEditModeMapping = {
     title: {
         editModesToBeDisplayed: ['SIMPLE', 'ADVANCED'],
+    },
+    channelName: {
+        editModesToBeDisplayed: ['ADVANCED'],
     },
     shortName: {
         editModesToBeDisplayed: ['ADVANCED'],
