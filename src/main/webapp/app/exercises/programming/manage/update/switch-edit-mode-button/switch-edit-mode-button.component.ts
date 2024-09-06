fb61e2620749961b58b4e6d9f2940c89b04bc02b
Florian Glombik <florian.glombik@tum.de>
2024-09-06 13:18:46 +0200

    Using the jhi-button component and adjusting button size in footer

--- src/main/webapp/app/exercises/programming/manage/update/switch-edit-mode-button/switch-edit-mode-button.component.ts
+++ src/main/webapp/app/exercises/programming/manage/update/switch-edit-mode-button/switch-edit-mode-button.component.ts
@@ -1,16 +1,20 @@
 import { Component, Input, input } from '@angular/core';
 import { faHandshakeAngle } from '@fortawesome/free-solid-svg-icons';
 import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
+import { ButtonSize, ButtonType } from 'app/shared/components/button.component';
+import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 
 @Component({
     selector: 'jhi-switch-edit-mode-button',
     standalone: true,
     templateUrl: './switch-edit-mode-button.component.html',
-    imports: [ArtemisSharedCommonModule],
+    imports: [ArtemisSharedCommonModule, ArtemisSharedComponentModule],
 })
 export class SwitchEditModeButtonComponent {
     protected readonly faHandShakeAngle = faHandshakeAngle;
 
-    isSimpleMode = input.required<boolean>();
     @Input() switchEditMode: () => void;
+    isSimpleMode = input.required<boolean>();
+    buttonSize = input<ButtonSize>(ButtonSize.MEDIUM);
+    protected readonly ButtonType = ButtonType;
 }
