532a8617b4bfd91582ba7d4a7f2401ab39f20b1a
Ole Vester <ole.vester+gitlab@check24.de>
2024-09-11 18:50:22 +0200

    Autowire UserTestRepository

--- src/test/java/de/tum/cit/aet/artemis/repository/UserTestRepository.java
+++ src/test/java/de/tum/cit/aet/artemis/repository/UserTestRepository.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 import de.tum.cit.aet.artemis.core.domain.User;
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserTestRepository extends ArtemisJpaRepository<User, Long> {
 
     Set<User> findAllByGroupsNotEmpty();
