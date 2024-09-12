b8eeb3242e3a1a5771836e5e91da7c0b90c7c275
Dmytro Polityka <dmytro.polityka@tum.de>
2024-09-12 02:16:00 +0200

    javadoc

--- src/main/java/de/tum/in/www1/artemis/web/rest/dto/CleanupServiceExecutionRecordDTO.java
+++ src/main/java/de/tum/in/www1/artemis/web/rest/dto/CleanupServiceExecutionRecordDTO.java
@@ -7,7 +7,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.in.www1.artemis.domain.cleanup.CleanupJobExecution;
 
 /**
- * todo
+ * DTO representing a record of a cleanup service execution.
+ * This DTO contains information about the execution date and the type of the job that was executed.
+ *
+ * The JSON serialization will include non-empty fields only.
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CleanupServiceExecutionRecordDTO(ZonedDateTime executionDate, String jobType) {
