8f71b0acf5945d7fb9fcde040de3c3d9274bad9b
Tim Cremer <tim.cremer@tum.de>
2024-09-12 14:33:51 +0200

    Removed unnecessary import statements

--- src/main/webapp/app/faq/faq.module.ts
+++ src/main/webapp/app/faq/faq.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { FAQComponent } from 'app/faq/faq.component';
 import { faqRoutes } from 'app/faq/faq.routes';
 import { FAQUpdateComponent } from 'app/faq/faq-update.component';
-import { ArtemisMarkdownEditorModule } from 'app/shared/markdown-editor/markdown-editor.module';
-import { ArtemisCategorySelectorModule } from 'app/shared/category-selector/category-selector.module';
-import { CustomExerciseCategoryBadgeComponent } from 'app/shared/exercise-categories/custom-exercise-category-badge/custom-exercise-category-badge.component';
 const ENTITY_STATES = [...faqRoutes];
 
 @NgModule({
-    imports: [
-        ArtemisSharedModule,
-        RouterModule.forChild(ENTITY_STATES),
-        ArtemisSharedComponentModule,
-        ArtemisMarkdownEditorModule,
-        ArtemisCategorySelectorModule,
-        CustomExerciseCategoryBadgeComponent,
-        FAQComponent,
-        FAQUpdateComponent,
-    ],
+    imports: [RouterModule.forChild(ENTITY_STATES), FAQComponent, FAQUpdateComponent],
     exports: [FAQComponent, FAQUpdateComponent],
 })
 export class ArtemisFAQModule {}
