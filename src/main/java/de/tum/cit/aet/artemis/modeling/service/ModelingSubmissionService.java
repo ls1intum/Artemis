c1a326301c208ffa041a3702668334cacc182469
leon <leon.wehrhahn@tum.de>
2024-09-13 14:09:57 +0700

    Update ModelingSubmissionService to create new submissions for empty Results

--- src/main/java/de/tum/cit/aet/artemis/modeling/service/ModelingSubmissionService.java
+++ src/main/java/de/tum/cit/aet/artemis/modeling/service/ModelingSubmissionService.java
@@ -124,14 +124,14 @@ public class ModelingSubmissionService extends SubmissionService {
             throw new AccessForbiddenException();
         }
 
-        // remove result from submission (in the unlikely case it is passed here), so that students cannot inject a result
-        modelingSubmission.setResults(new ArrayList<>());
-
         // if athena results are present than create new submission on submit
         if (!modelingSubmission.getResults().isEmpty()) {
             log.debug("Creating a new submission due to Athena results for user: {}", user.getLogin());
             modelingSubmission.setId(null);
         }
+
+        // remove result from submission (in the unlikely case it is passed here), so that students cannot inject a result
+        modelingSubmission.setResults(new ArrayList<>());
 
         // update submission properties
         // NOTE: from now on we always set submitted to true to prevent problems here! Except for late submissions of course exercises to prevent issues in auto-save
