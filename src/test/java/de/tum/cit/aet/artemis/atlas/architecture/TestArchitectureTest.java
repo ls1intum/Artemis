c889957a4902d6499c44472fa76c4318d51e5900
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-09-27 11:39:39 +0200

    add integration test to prevent autowire in integrationt test implementations

--- src/test/java/de/tum/cit/aet/artemis/atlas/architecture/TestArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/atlas/architecture/TestArchitectureTest.java
@@ -1,8 +1,10 @@
 package de.tum.cit.aet.artemis.atlas.architecture;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noMembers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import de.tum.cit.aet.artemis.atlas.AbstractAtlasIntegrationTest;
 import de.tum.cit.aet.artemis.shared.architecture.AbstractArchitectureTest;
@@ -13,5 +15,12 @@ class TestArchitectureTest extends AbstractArchitectureTest {
     void integrationTestsShouldExtendAbstractAtlasIntegrationTest() {
         classes().that().resideInAPackage(ARTEMIS_PACKAGE + ".atlas").and().haveSimpleNameEndingWith("IntegrationTest").should().beAssignableTo(AbstractAtlasIntegrationTest.class)
                 .because("All integration tests should extend AbstractAtlasIntegrationTest").check(testClasses);
+    }
+
+    @Test
+    void integrationTestsShouldNotAutowireMembers() {
+        noMembers().that().areAnnotatedWith(Autowired.class).should().beDeclaredInClassesThat().areAssignableTo(AbstractAtlasIntegrationTest.class).andShould()
+                .notBeDeclaredIn(AbstractAtlasIntegrationTest.class)
+                .because("Integration tests should not autowire members in any class that inherits from AbstractAtlasIntegrationTest").check(testClasses);
     }
 }
