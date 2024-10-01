d378c63f1b34dcf2803f656cb991503a860a8909
Patrik Zander <patrik.zander@tum.de>
2024-10-01 21:33:08 +0200

    Revert "WIP themes"
    This reverts commit da4ea63eb68e04fe90ead3fac287d9a09b0fc6f5.

--- src/main/webapp/app/shared/monaco-editor/monaco-editor.service.ts
+++ src/main/webapp/app/shared/monaco-editor/monaco-editor.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as monaco from 'monaco-editor';
 import { CUSTOM_MARKDOWN_CONFIG, CUSTOM_MARKDOWN_LANGUAGE, CUSTOM_MARKDOWN_LANGUAGE_ID } from 'app/shared/monaco-editor/model/languages/monaco-custom-markdown.language';
-import { CUSTOM_DARK } from 'app/shared/monaco-editor/model/themes/monaco-editor-theme.model';
 
 @Injectable({ providedIn: 'root' })
 export class MonacoEditorService {
@@ -9,7 +8,6 @@ export class MonacoEditorService {
         monaco.languages.register({ id: CUSTOM_MARKDOWN_LANGUAGE_ID });
         monaco.languages.setLanguageConfiguration(CUSTOM_MARKDOWN_LANGUAGE_ID, CUSTOM_MARKDOWN_CONFIG);
         monaco.languages.setMonarchTokensProvider(CUSTOM_MARKDOWN_LANGUAGE_ID, CUSTOM_MARKDOWN_LANGUAGE);
-        CUSTOM_DARK.register();
     }
 
     foo() {
