64f1b1c19e061a5b51bbb7fb7c988c39ac3afebb
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:52:03 +0200

    add tests to exercise

---
+++ src/test/java/de/tum/cit/aet/artemis/exercise/architecture/ExerciseResourceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.exercise.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleResourceArchitectureTest;
+
+public class ExerciseResourceArchitectureTest extends AbstractModuleResourceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".exercise";
+    }
+}
---
+++ src/test/java/de/tum/cit/aet/artemis/exercise/architecture/ExerciseServiceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.exercise.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleServiceArchitectureTest;
+
+public class ExerciseServiceArchitectureTest extends AbstractModuleServiceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".exercise";
+    }
+}
