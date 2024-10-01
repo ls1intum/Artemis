f0bae93a3b4ab49fe8f5f9e1bb462822d161910f
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 01:03:55 +0200

    add tests to text

---
+++ src/test/java/de/tum/cit/aet/artemis/text/architecture/TextResourceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.text.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleResourceArchitectureTest;
+
+public class TextResourceArchitectureTest extends AbstractModuleResourceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".text";
+    }
+}
