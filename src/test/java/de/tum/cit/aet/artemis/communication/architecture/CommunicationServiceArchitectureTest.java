4e6496d1327bae1cafde437025bf9ad2fa87a3aa
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:47:11 +0200

    add tests to communication

---
+++ src/test/java/de/tum/cit/aet/artemis/communication/architecture/CommunicationServiceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.communication.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleServiceArchitectureTest;
+
+public class CommunicationServiceArchitectureTest extends AbstractModuleServiceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".communication";
+    }
+}
