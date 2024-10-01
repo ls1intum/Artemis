45f39d44c8ef7cb9ecfafb046bdc9770a607197f
raphaelklein <raphael.klein@tum.de>
2024-10-01 19:06:08 +0200

    change fixes

--- src/test/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseTestRepository.java
+++ src/test/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseTestRepository.java
@@ -1,22 +1,20 @@
 package de.tum.cit.aet.artemis.exercise.repository;
 
-import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
-
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
 import de.tum.cit.aet.artemis.exercise.domain.Exercise;
 
 /**
  * Spring Data JPA repository for the Exercise entity for Tests.
  */
+@Primary
 @Repository
 public interface ExerciseTestRepository extends ExerciseRepository {
 
