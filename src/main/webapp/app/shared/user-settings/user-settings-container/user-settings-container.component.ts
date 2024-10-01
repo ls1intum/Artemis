ebcad8794ad195524eaa86137fae3caf0cd2d351
entholzer <nom.ise@outlook.com>
2024-10-01 15:06:23 +0200

    use ned inject and made to standalone

--- src/main/webapp/app/shared/user-settings/user-settings-container/user-settings-container.component.ts
+++ src/main/webapp/app/shared/user-settings/user-settings-container/user-settings-container.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
 import { PROFILE_LOCALVC } from 'app/app.constants';
 import { User } from 'app/core/user/user.model';
 import { AccountService } from 'app/core/auth/account.service';
 import { tap } from 'rxjs';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 /**
  * UserSettingsContainerComponent serves as the common ground for different settings
@@ -12,20 +15,20 @@ import { tap } from 'rxjs';
 @Component({
     selector: 'jhi-user-settings',
     templateUrl: 'user-settings-container.component.html',
+    standalone: true,
     styleUrls: ['user-settings-container.component.scss'],
+    imports: [TranslateDirective, RouterModule, FontAwesomeModule],
 })
 export class UserSettingsContainerComponent implements OnInit {
+    private readonly profileService = inject(ProfileService);
+    private readonly accountService = inject(AccountService);
+
     // Icons
     faUser = faUser;
 
     currentUser?: User;
     localVCEnabled = false;
     isAtLeastTutor = false;
-
-    constructor(
-        private profileService: ProfileService,
-        private accountService: AccountService,
-    ) {}
 
     ngOnInit() {
         this.profileService.getProfileInfo().subscribe((profileInfo) => {
@@ -38,7 +41,6 @@ export class UserSettingsContainerComponent implements OnInit {
                 tap((user: User) => {
                     this.currentUser = user;
                     this.authorizeTutor();
-                    console.log(this.currentUser.authorities);
                     return this.currentUser;
                 }),
             )
--- src/main/webapp/app/shared/user-settings/user-settings.module.ts
+++ src/main/webapp/app/shared/user-settings/user-settings.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { UserSettingsContainerComponent } from 'app/shared/user-settings/user-settings-container/user-settings-container.component';
 import { AccountInformationComponent } from 'app/shared/user-settings/account-information/account-information.component';
 import { NotificationSettingsComponent } from 'app/shared/user-settings/notification-settings/notification-settings.component';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
@@ -16,7 +15,6 @@ import { IdeSettingsComponent } from 'app/shared/user-settings/ide-preferences/i
 @NgModule({
     imports: [RouterModule.forChild(userSettingsState), ArtemisSharedModule, ArtemisSharedComponentModule, ClipboardModule, FormDateTimePickerModule],
     declarations: [
-        UserSettingsContainerComponent,
         AccountInformationComponent,
         NotificationSettingsComponent,
         ScienceSettingsComponent,
