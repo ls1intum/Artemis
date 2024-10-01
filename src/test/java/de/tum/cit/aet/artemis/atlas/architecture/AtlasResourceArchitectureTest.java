67ce49337b2af7144c07c9acdf51939f7c413a9d
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:33:21 +0200

    create module service and resource architecture tests

---
+++ src/test/java/de/tum/cit/aet/artemis/atlas/architecture/AtlasResourceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.atlas.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleResourceArchitectureTest;
+
+public class AtlasResourceArchitectureTest extends AbstractModuleResourceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".atlas";
+    }
+}
