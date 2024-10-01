8d60795a5991e052975485a1fcdeafa7ef658d95
dennis <dp.jandow@gmail.com>
2024-10-01 18:42:12 +0200

    maybe this logger

--- src/main/java/de/tum/cit/aet/artemis/core/security/filter/CsrfArtemisFilter.java
+++ src/main/java/de/tum/cit/aet/artemis/core/security/filter/CsrfArtemisFilter.java
@@ -7,13 +7,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CsrfArtemisFilter extends OncePerRequestFilter {
 
+    private static final Logger log = LoggerFactory.getLogger(CsrfArtemisFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.info("origin" + request.getHeader("origin"));
+        log.info("origin: {}", request.getHeader("origin"));
 
         // Check if the custom CSRF header is present in the request
         if (request.getHeader("X-ARTEMIS-CSRF") != null) {
@@ -21,7 +25,7 @@ public class CsrfArtemisFilter extends OncePerRequestFilter {
         }
         else {
             // Reject the request if the header is missing
-            logger.error("Missing CSRF protection header");
+            log.error("Missing CSRF protection header");
             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Missing CSRF protection header");
         }
     }
