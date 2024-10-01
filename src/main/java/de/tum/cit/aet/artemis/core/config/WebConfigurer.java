1ee4259408d74d4a71fbab0d0ff9ce5f1b8d09ed
dennis <dp.jandow@gmail.com>
2024-10-01 21:43:33 +0200

    let cors accept patterns

--- src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
@@ -117,7 +117,8 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = jHipsterProperties.getCors();
-        if (config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty()) {
+        if ((config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty())
+            || (config.getAllowedOriginPatterns() != null && !config.getAllowedOriginPatterns().isEmpty())) {
             log.debug("Registering CORS filter");
             source.registerCorsConfiguration("/api/**", config);
             source.registerCorsConfiguration("/management/**", config);
