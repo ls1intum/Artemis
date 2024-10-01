4c64eee7b1515391d69b156a7277db03b088da1e
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 01:01:19 +0200

    add tests to programming

---
+++ src/test/java/de/tum/cit/aet/artemis/programming/architecture/ProgrammingRepositoryArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.programming.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
+
+public class ProgrammingRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".programming";
+    }
+}
