c7bd2218c73e661d402384eb6687460e144048da
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:40:55 +0200

    add tests to assessment

---
+++ src/test/java/de/tum/cit/aet/artemis/assessment/architecture/AssessmentRepositoryArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.assessment.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
+
+public class AssessmentRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".assessment";
+    }
+}
---
+++ src/test/java/de/tum/cit/aet/artemis/assessment/architecture/AssessmentServiceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.assessment.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleServiceArchitectureTest;
+
+public class AssessmentServiceArchitectureTest extends AbstractModuleServiceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".assessment";
+    }
+}
