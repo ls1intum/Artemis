8e80ad51d0bb11754d75016d74461d43db6aba26
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:55:38 +0200

    add tests to lecture

---
+++ src/test/java/de/tum/cit/aet/artemis/lecture/architecture/LectureRepositoryArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.lecture.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
+
+public class LectureRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".lecture";
+    }
+}
