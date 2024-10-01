23ae6f76bb82da2bb0425ecf3a81e9953323310d
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 01:02:23 +0200

    add tests to quiz

---
+++ src/test/java/de/tum/cit/aet/artemis/quiz/architecture/QuizResourceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.quiz.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleResourceArchitectureTest;
+
+public class QuizResourceArchitectureTest extends AbstractModuleResourceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".quiz";
+    }
+}
