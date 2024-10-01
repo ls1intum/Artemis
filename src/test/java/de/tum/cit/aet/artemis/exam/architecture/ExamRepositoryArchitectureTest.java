c6ce956eef1aec46fc848c31f51a0b0125f6b369
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:50:52 +0200

    add tests to exam

---
+++ src/test/java/de/tum/cit/aet/artemis/exam/architecture/ExamRepositoryArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.exam.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
+
+public class ExamRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".exam";
+    }
+}
