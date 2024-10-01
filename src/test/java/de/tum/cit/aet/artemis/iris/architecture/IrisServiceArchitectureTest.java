39d3f61927220b093b9ef2f2b4bb0575f8581a12
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:54:18 +0200

    add tests to iris

---
+++ src/test/java/de/tum/cit/aet/artemis/iris/architecture/IrisServiceArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.iris.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleServiceArchitectureTest;
+
+public class IrisServiceArchitectureTest extends AbstractModuleServiceArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".iris";
+    }
+}
