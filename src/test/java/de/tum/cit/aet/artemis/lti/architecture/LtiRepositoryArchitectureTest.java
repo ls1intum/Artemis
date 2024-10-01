1424dfde735c9533ed1142ed57519dec0714babe
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 00:56:54 +0200

    add tests to lti

---
+++ src/test/java/de/tum/cit/aet/artemis/lti/architecture/LtiRepositoryArchitectureTest.java
@@ -0,0 +1,10 @@
+package de.tum.cit.aet.artemis.lti.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
+
+public class LtiRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
+
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".lti";
+    }
+}
