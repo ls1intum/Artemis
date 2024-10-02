836505f9714cc615c5ae980f333c745ba92a1b9a
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-02 18:42:50 +0200

    fix LtiTestArchitectureTest visibility

--- src/test/java/de/tum/cit/aet/artemis/lti/architecture/LtiTestArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/lti/architecture/LtiTestArchitectureTest.java
@@ -3,7 +3,7 @@ package de.tum.cit.aet.artemis.lti.architecture;
 import de.tum.cit.aet.artemis.lti.AbstractLtiIntegrationTest;
 import de.tum.cit.aet.artemis.shared.architecture.AbstractModuleTestArchitectureTest;
 
-public class LtiTestArchitectureTest extends AbstractModuleTestArchitectureTest<AbstractLtiIntegrationTest> {
+class LtiTestArchitectureTest extends AbstractModuleTestArchitectureTest<AbstractLtiIntegrationTest> {
 
     @Override
     protected String getModulePackageName() {
