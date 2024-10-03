c5cf70798de142c1476cd47ef9e06a9a38671819
Florian Glombik <florian.glombik@tum.de>
2024-10-03 15:45:07 +0200

    Client: Adding error message when title / short name is already used

--- src/main/webapp/app/shared/form/title-channel-name/title-channel-name.module.ts
+++ src/main/webapp/app/shared/form/title-channel-name/title-channel-name.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { TitleChannelNameComponent } from 'app/shared/form/title-channel-name/title-channel-name.component';
 import { ArtemisSharedComponentModule } from 'app/shared/components/shared-component.module';
 import { CustomNotIncludedInValidatorDirective } from 'app/shared/validators/custom-not-included-in-validator.directive';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 @NgModule({
-    imports: [FormsModule, CommonModule, ArtemisSharedComponentModule, CustomNotIncludedInValidatorDirective],
+    imports: [FormsModule, CommonModule, ArtemisSharedComponentModule, CustomNotIncludedInValidatorDirective, TranslateDirective],
     declarations: [TitleChannelNameComponent],
     exports: [TitleChannelNameComponent],
 })
