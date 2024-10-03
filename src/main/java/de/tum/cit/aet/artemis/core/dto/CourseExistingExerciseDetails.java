b8461e41e4820d0b89f1e4119f5cf48d7982811d
Florian Glombik <florian.glombik@tum.de>
2024-10-03 15:24:11 +0200

    Server: Adjusting endpoint, channel names are not needed

--- src/main/java/de/tum/cit/aet/artemis/core/dto/CourseExistingExerciseDetails.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/CourseExistingExerciseDetails.java
@@ -3,5 +3,5 @@ package de.tum.cit.aet.artemis.core.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record CourseExistingExerciseDetails(String[] exerciseTitles, String[] shortNames, String[] channelNames) {
+public record CourseExistingExerciseDetails(String[] exerciseTitles, String[] shortNames) {
 }
