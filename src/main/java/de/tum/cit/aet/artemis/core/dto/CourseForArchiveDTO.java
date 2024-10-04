817c873704e0c36f6257c986cb7a3cacb4f6a577
EgeDoguKaya <egedk321@outlook.com>
2024-10-05 00:15:52 +0200

    add javadoc for dto

--- src/main/java/de/tum/cit/aet/artemis/core/dto/CourseForArchiveDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/CourseForArchiveDTO.java
@@ -2,6 +2,15 @@ package de.tum.cit.aet.artemis.core.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+/**
+ * DTO for representing archived courses from previous semesters.
+ *
+ * @param id       The id of the course
+ * @param title    The title of the course
+ * @param semester The semester in which the course was offered
+ * @param color    The background color of the course
+ * @param icon     The icon of the course
+ */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CourseForArchiveDTO(Long id, String title, String semester, String color, String icon) {
 }
