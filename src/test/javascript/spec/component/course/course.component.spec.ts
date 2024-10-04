96c77bfc7191c9fbded6868bc82fe5e5dbfff06f
EgeDoguKaya <egedk321@outlook.com>
2024-10-04 16:35:57 +0200

    Add mocks to course test

--- src/test/javascript/spec/component/course/course.component.spec.ts
+++ src/test/javascript/spec/component/course/course.component.spec.ts
@@ -34,6 +34,8 @@ import { of } from 'rxjs';
 import dayjs from 'dayjs/esm';
 import { Exam } from 'app/entities/exam/exam.model';
 import { CourseAccessStorageService } from 'app/course/course-access-storage.service';
+import { SearchFilterPipe } from 'app/shared/pipes/search-filter.pipe';
+import { SearchFilterComponent } from 'app/shared/search-filter/search-filter.component';
 
 const endDate1 = dayjs().add(1, 'days');
 const visibleDate1 = dayjs().subtract(1, 'days');
@@ -101,10 +103,12 @@ describe('CoursesComponent', () => {
                 MockDirective(SortDirective),
                 MockDirective(SortByDirective),
                 MockPipe(ArtemisDatePipe),
+                MockPipe(SearchFilterPipe),
                 MockComponent(CourseExerciseRowComponent),
                 MockComponent(CourseExercisesComponent),
                 MockComponent(CourseRegistrationComponent),
                 MockComponent(CourseCardComponent),
+                MockComponent(SearchFilterComponent),
             ],
             providers: [
                 { provide: LocalStorageService, useClass: MockSyncStorage },
