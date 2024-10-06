9bc870f8893f792c6fa0adf43e0aed732b691727
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-10-06 05:57:23 +0200

    fix visibility of FileUploadTestArchitectureTest

--- src/test/java/de/tum/cit/aet/artemis/fileupload/architecture/FileUploadTestArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/fileupload/architecture/FileUploadTestArchitectureTest.java
@@ -3,7 +3,7 @@ package de.tum.cit.aet.artemis.fileupload.architecture;
 import de.tum.cit.aet.artemis.fileupload.AbstractFileUploadIntegrationTest;
 import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleTestArchitectureTest;
 
-public class FileUploadTestArchitectureTest extends AbstractModuleTestArchitectureTest<AbstractFileUploadIntegrationTest> {
+class FileUploadTestArchitectureTest extends AbstractModuleTestArchitectureTest<AbstractFileUploadIntegrationTest> {
 
     @Override
     public String getModulePackage() {
