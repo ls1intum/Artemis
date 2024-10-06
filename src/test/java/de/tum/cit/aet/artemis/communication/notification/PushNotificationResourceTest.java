c77c69b673a22e3cc06219631c2371895c20d475
Stephan Krusche <krusche@tum.de>
2024-10-06 10:59:46 +0200

    Development: Reduce flakiness of server tests

--- src/test/java/de/tum/cit/aet/artemis/communication/notification/PushNotificationResourceTest.java
+++ src/test/java/de/tum/cit/aet/artemis/communication/notification/PushNotificationResourceTest.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -24,7 +23,6 @@ import de.tum.cit.aet.artemis.core.test_repository.UserTestRepository;
 import de.tum.cit.aet.artemis.core.user.util.UserUtilService;
 import de.tum.cit.aet.artemis.shared.base.AbstractSpringIntegrationIndependentTest;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PushNotificationResourceTest extends AbstractSpringIntegrationIndependentTest {
 
     @Autowired
