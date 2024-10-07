dda89f2094558f22015faa8e60b0e3d88b7e40ea
Patrik Zander <patrik.zander@tum.de>
2024-10-07 23:39:23 +0200

    Use signals (GitDiffFilePanelComponent)

--- src/main/webapp/app/exercises/programming/hestia/git-diff-report/git-diff-file-panel.component.ts
+++ src/main/webapp/app/exercises/programming/hestia/git-diff-report/git-diff-file-panel.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, computed, input, output } from '@angular/core';
 import { ProgrammingExerciseGitDiffEntry } from 'app/entities/hestia/programming-exercise-git-diff-entry.model';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import { GitDiffFilePanelTitleComponent } from 'app/exercises/programming/hestia/git-diff-report/git-diff-file-panel-title.component';
@@ -15,51 +15,51 @@ import { ArtemisSharedModule } from 'app/shared/shared.module';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [GitDiffFilePanelTitleComponent, GitDiffLineStatComponent, GitDiffFileComponent, ArtemisSharedModule],
 })
-export class GitDiffFilePanelComponent implements OnInit {
-    @Input() diffEntries: ProgrammingExerciseGitDiffEntry[];
+export class GitDiffFilePanelComponent {
+    diffEntries = input.required<ProgrammingExerciseGitDiffEntry[]>();
+    originalFileContent = input<string>();
+    modifiedFileContent = input<string>();
+    diffForTemplateAndSolution = input<boolean>(true);
+    allowSplitView = input<boolean>(true);
+    onDiffReady = output<boolean>();
 
-    @Input() templateFileContent: string | undefined;
+    originalFilePath = computed(() =>
+        this.diffEntries()
+            .map((entry) => entry.previousFilePath)
+            .filter((filePath) => filePath)
+            .first(),
+    );
+    modifiedFilePath = computed(() =>
+        this.diffEntries()
+            .map((entry) => entry.filePath)
+            .filter((filePath) => filePath)
+            .first(),
+    );
 
-    @Input() solutionFileContent: string | undefined;
+    addedLineCount = computed(
+        () =>
+            this.diffEntries()
+                .filter((entry) => entry && entry.filePath && entry.startLine && entry.lineCount)
+                .flatMap((entry) => {
+                    return this.modifiedFileContent()
+                        ?.split('\n')
+                        .slice(entry.startLine! - 1, entry.startLine! + entry.lineCount! - 1);
+                })
+                .filter((line) => line && line.trim().length !== 0).length,
+    );
 
-    @Input() diffForTemplateAndSolution = true;
-
-    @Input() allowSplitView = true;
-
-    @Output() onDiffReady = new EventEmitter<boolean>();
-
-    previousFilePath: string | undefined;
-    filePath: string | undefined;
-
-    addedLineCount: number;
-    removedLineCount: number;
+    removedLineCount = computed(
+        () =>
+            this.diffEntries()
+                .filter((entry) => entry && entry.previousFilePath && entry.previousStartLine && entry.previousLineCount)
+                .flatMap((entry) => {
+                    return this.originalFileContent()
+                        ?.split('\n')
+                        .slice(entry.previousStartLine! - 1, entry.previousStartLine! + entry.previousLineCount! - 1);
+                })
+                .filter((line) => line && line.trim().length !== 0).length,
+    );
 
     faAngleUp = faAngleUp;
     faAngleDown = faAngleDown;
-
-    ngOnInit(): void {
-        this.filePath = this.diffEntries
-            .map((entry) => entry.filePath)
-            .filter((filePath) => filePath)
-            .first();
-
-        this.previousFilePath = this.diffEntries
-            .map((entry) => entry.previousFilePath)
-            .filter((filePath) => filePath)
-            .first();
-
-        this.addedLineCount = this.diffEntries
-            .filter((entry) => entry && entry.filePath && entry.startLine && entry.lineCount)
-            .flatMap((entry) => {
-                return this.solutionFileContent?.split('\n').slice(entry.startLine! - 1, entry.startLine! + entry.lineCount! - 1);
-            })
-            .filter((line) => line && line.trim().length !== 0).length;
-
-        this.removedLineCount = this.diffEntries
-            .filter((entry) => entry && entry.previousFilePath && entry.previousStartLine && entry.previousLineCount)
-            .flatMap((entry) => {
-                return this.templateFileContent?.split('\n').slice(entry.previousStartLine! - 1, entry.previousStartLine! + entry.previousLineCount! - 1);
-            })
-            .filter((line) => line && line.trim().length !== 0).length;
-    }
 }
