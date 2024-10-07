36f456a14b281c50cad4920b6b5a1b0b4f112f47
Patrik Zander <patrik.zander@tum.de>
2024-10-08 00:20:10 +0200

    Use signals (GitDiffReportComponent)

--- src/main/webapp/app/exercises/programming/hestia/git-diff-report/git-diff-report.component.ts
+++ src/main/webapp/app/exercises/programming/hestia/git-diff-report/git-diff-report.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, input, signal, untracked } from '@angular/core';
 import { ProgrammingExerciseGitDiffReport } from 'app/entities/hestia/programming-exercise-git-diff-report.model';
 import { ProgrammingExerciseGitDiffEntry } from 'app/entities/hestia/programming-exercise-git-diff-entry.model';
 import { faSpinner, faTableColumns } from '@fortawesome/free-solid-svg-icons';
@@ -23,48 +23,18 @@ interface DiffInformation {
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [GitDiffLineStatComponent, ArtemisSharedModule, ArtemisSharedComponentModule, GitDiffFilePanelComponent],
 })
-export class GitDiffReportComponent implements OnInit {
-    @Input() report: ProgrammingExerciseGitDiffReport;
+export class GitDiffReportComponent {
+    report = input.required<ProgrammingExerciseGitDiffReport>();
+    templateFileContentByPath = input.required<Map<string, string>>();
+    solutionFileContentByPath = input.required<Map<string, string>>();
+    diffForTemplateAndSolution = input<boolean>(true);
+    diffForTemplateAndEmptyRepository = input<boolean>(false);
+    isRepositoryView = input<boolean>(false);
 
-    @Input() templateFileContentByPath: Map<string, string>;
-
-    @Input() solutionFileContentByPath: Map<string, string>;
-
-    @Input() filePaths: string[];
-
-    @Input() diffForTemplateAndSolution = true;
-
-    @Input() diffForTemplateAndEmptyRepository = false;
-
-    @Input() isRepositoryView = false;
-
-    leftCommit: string | undefined;
-
-    rightCommit: string | undefined;
-
-    entries: ProgrammingExerciseGitDiffEntry[];
-    entriesByPath: Map<string, ProgrammingExerciseGitDiffEntry[]>;
-    addedLineCount: number;
-    removedLineCount: number;
-    diffInformationForPaths: DiffInformation[] = [];
-    allDiffsReady = false;
-    nothingToDisplay = false;
-    allowSplitView = true;
-    renamedFilePaths: { [before: string]: string | undefined } = {};
-
-    faSpinner = faSpinner;
-    faTableColumns = faTableColumns;
-
-    // Expose to template
-    protected readonly ButtonSize = ButtonSize;
-    protected readonly ButtonType = ButtonType;
-
-    constructor() {}
-
-    ngOnInit(): void {
-        // Sort the diff entries by file path and start lines
-        this.entries =
-            this.report.entries?.sort((a, b) => {
+    // TODO rename to sortedEntries
+    entries = computed(() => {
+        return (
+            this.report().entries?.sort((a, b) => {
                 const filePathA = a.filePath ?? a.previousFilePath ?? '';
                 const filePathB = b.filePath ?? b.previousFilePath ?? '';
                 if (filePathA < filePathB) {
@@ -74,65 +44,97 @@ export class GitDiffReportComponent implements OnInit {
                     return 1;
                 }
                 return (a.startLine ?? a.previousStartLine ?? 0) - (b.startLine ?? b.previousStartLine ?? 0);
-            }) ?? [];
-
-        this.addedLineCount = this.entries
+            }) ?? []
+        );
+    });
+    addedLineCount = computed(() => {
+        return this.entries()
             .flatMap((entry) => {
                 if (entry && entry.filePath && entry.startLine && entry.lineCount) {
-                    return this.solutionFileContentByPath
+                    return this.solutionFileContentByPath()
                         .get(entry.filePath)
                         ?.split('\n')
                         .slice(entry.startLine - 1, entry.startLine + entry.lineCount - 1);
                 }
             })
             .filter((line) => line && line.trim().length !== 0).length;
+    });
 
-        this.removedLineCount = this.entries
+    removedLineCount = computed(() => {
+        return this.entries()
             .flatMap((entry) => {
                 if (entry && entry.previousFilePath && entry.previousStartLine && entry.previousLineCount) {
-                    return this.templateFileContentByPath
+                    return this.templateFileContentByPath()
                         .get(entry.previousFilePath!)
                         ?.split('\n')
                         .slice(entry.previousStartLine - 1, entry.previousStartLine + entry.previousLineCount - 1);
                 }
             })
             .filter((line) => line && line.trim().length !== 0).length;
+    });
 
-        // Create a set of all file paths
-        this.filePaths = [...new Set([...this.templateFileContentByPath.keys(), ...this.solutionFileContentByPath.keys()])].sort();
-        // Track renamed files
-        this.entries.forEach((entry) => {
+    filePaths = computed(() => {
+        return [...new Set([...this.templateFileContentByPath().keys(), ...this.solutionFileContentByPath().keys()])].sort();
+    });
+
+    renamedFilePaths = computed(() => {
+        const renamedFilePaths: { [before: string]: string | undefined } = {};
+        this.entries().forEach((entry) => {
             // Accounts only for files that have existed in the original and the modified version, but under different names
             if (entry.filePath && entry.previousFilePath && entry.filePath !== entry.previousFilePath) {
-                this.renamedFilePaths[entry.filePath] = entry.previousFilePath;
+                renamedFilePaths[entry.filePath] = entry.previousFilePath;
             }
         });
-        // Group the diff entries by file path
-        this.entriesByPath = new Map<string, ProgrammingExerciseGitDiffEntry[]>();
-        [...this.templateFileContentByPath.keys()].forEach((filePath) => {
-            this.entriesByPath.set(
+        return renamedFilePaths;
+    });
+
+    entriesByPath = computed(() => {
+        const entriesByPath = new Map<string, ProgrammingExerciseGitDiffEntry[]>();
+        this.filePaths().forEach((filePath) => {
+            entriesByPath.set(
                 filePath,
-                this.entries.filter((entry) => entry.previousFilePath === filePath && !entry.filePath),
+                this.entries().filter((entry) => entry.previousFilePath === filePath && !entry.filePath),
             );
         });
-        [...this.solutionFileContentByPath.keys()].forEach((filePath) => {
-            this.entriesByPath.set(
+        this.filePaths().forEach((filePath) => {
+            entriesByPath.set(
                 filePath,
-                this.entries.filter((entry) => entry.filePath === filePath),
+                this.entries().filter((entry) => entry.filePath === filePath),
             );
         });
-        this.leftCommit = this.report.leftCommitHash?.substring(0, 10);
-        this.rightCommit = this.report.rightCommitHash?.substring(0, 10);
-        this.diffInformationForPaths = this.filePaths
-            .filter((path) => this.entriesByPath.get(path)?.length)
-            .map((path) => {
-                // entries is not undefined due to the filter
-                const entries = this.entriesByPath.get(path)!;
-                const templateFileContent = this.templateFileContentByPath.get(this.renamedFilePaths[path] ?? path);
-                const solutionFileContent = this.solutionFileContentByPath.get(path);
-                return { path, entries, templateFileContent, solutionFileContent, diffReady: false };
+        return entriesByPath;
+    });
+
+    leftCommit = computed(() => this.report().leftCommitHash?.substring(0, 10));
+    rightCommit = computed(() => this.report().rightCommitHash?.substring(0, 10));
+    diffInformationForPaths = signal<DiffInformation[]>([]);
+    nothingToDisplay = computed(() => this.diffInformationForPaths().length === 0);
+    allDiffsReady = computed(() => Object.values(this.diffInformationForPaths()).every((info) => info.diffReady));
+    allowSplitView = signal<boolean>(true);
+
+    faSpinner = faSpinner;
+    faTableColumns = faTableColumns;
+
+    // Expose to template
+    protected readonly ButtonSize = ButtonSize;
+    protected readonly ButtonType = ButtonType;
+
+    constructor() {
+        effect(() => {
+            untracked(() => {
+                this.diffInformationForPaths.set(
+                    this.filePaths()
+                        .filter((path) => this.entriesByPath().get(path)?.length)
+                        .map((path) => {
+                            // entries is not undefined due to the filter above
+                            const entries = this.entriesByPath().get(path)!;
+                            const templateFileContent = this.templateFileContentByPath().get(this.renamedFilePaths()[path] ?? path);
+                            const solutionFileContent = this.solutionFileContentByPath().get(path);
+                            return { path, entries, templateFileContent, solutionFileContent, diffReady: false };
+                        }),
+                );
             });
-        this.nothingToDisplay = this.diffInformationForPaths.length === 0;
+        });
     }
 
     /**
@@ -142,10 +144,11 @@ export class GitDiffReportComponent implements OnInit {
      * @param ready Whether the diff is ready to be displayed or not.
      */
     onDiffReady(path: string, ready: boolean) {
-        const index = this.diffInformationForPaths.findIndex((info) => info.path === path);
+        const diffInformation = [...this.diffInformationForPaths()];
+        const index = diffInformation.findIndex((info) => info.path === path);
         if (index !== -1) {
-            this.diffInformationForPaths[index].diffReady = ready;
-            this.allDiffsReady = Object.values(this.diffInformationForPaths).every((info) => info.diffReady);
+            diffInformation[index].diffReady = ready;
+            this.diffInformationForPaths.set(diffInformation);
         } else {
             console.error(`Received diff ready event for unknown path: ${path}`);
         }
