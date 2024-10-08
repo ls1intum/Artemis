6f99488b0cf4259a9e4fd60a730f4d307abb2da8
Dennis Jandow <95364200+janthoXO@users.noreply.github.com>
2024-10-08 14:23:47 +0200

    Update to use isEmpty for arrays
    Co-authored-by: Marcel Gaupp <m.gaupp@tum.de>

--- src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
@@ -117,8 +117,7 @@ public class WebConfigurer implements ServletContextInitializer, WebServerFactor
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = jHipsterProperties.getCors();
-        if ((config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty())
-            || (config.getAllowedOriginPatterns() != null && !config.getAllowedOriginPatterns().isEmpty())) {
+        if (!CollectionUtils.isEmpty(config.getAllowedOrigins()) || !CollectionUtils.isEmpty(config.getAllowedOriginPatterns())) {
             log.debug("Registering CORS filter");
             source.registerCorsConfiguration("/api/**", config);
             source.registerCorsConfiguration("/management/**", config);
