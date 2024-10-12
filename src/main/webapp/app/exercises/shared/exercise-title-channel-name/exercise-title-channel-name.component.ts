756e15e78f7f2b5d1e86ad0935c441b03d2ac970
Florian Glombik <florian.glombik@tum.de>
2024-10-12 12:47:16 +0200

    Add descriptive method name for effect

--- src/main/webapp/app/exercises/shared/exercise-title-channel-name/exercise-title-channel-name.component.ts
+++ src/main/webapp/app/exercises/shared/exercise-title-channel-name/exercise-title-channel-name.component.ts
@@ -3,7 +3,7 @@ import { Course, isCommunicationEnabled } from 'app/entities/course.model';
 import { Exercise } from 'app/entities/exercise.model';
 import { TitleChannelNameComponent } from 'app/shared/form/title-channel-name/title-channel-name.component';
 import { ProgrammingExerciseInputField } from 'app/exercises/programming/manage/update/programming-exercise-update.helper';
-import { ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
+import { CourseExistingExerciseDetailsType, ExerciseService } from 'app/exercises/shared/exercise/exercise.service';
 
 @Component({
     selector: 'jhi-exercise-title-channel-name',
@@ -32,14 +32,17 @@ export class ExerciseTitleChannelNameComponent implements OnChanges {
     hideChannelNameInput = false;
 
     constructor() {
-        effect(() => {
-            const courseId = this.courseId() ?? this.course()?.id;
-            if (courseId && this.exercise.type) {
-                this.exerciseService.getExistingExerciseDetailsInCourse(courseId, this.exercise.type).subscribe((exerciseDetails) => {
-                    this.alreadyUsedExerciseNames.set(exerciseDetails.exerciseTitles ?? []);
-                });
-            }
-        });
+        effect(
+            function fetchExistingExerciseNamesOnInit() {
+                const courseId = this.courseId() ?? this.course()?.id;
+                if (courseId && this.exercise.type) {
+                    this.exerciseService.getExistingExerciseDetailsInCourse(courseId, this.exercise.type).subscribe((exerciseDetails: CourseExistingExerciseDetailsType) => {
+                        this.alreadyUsedExerciseNames.set(exerciseDetails.exerciseTitles ?? []);
+                    });
+                }
+            }.bind(this),
+            { allowSignalWrites: true },
+        );
     }
 
     ngOnChanges(changes: SimpleChanges) {
