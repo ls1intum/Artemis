ad9e2a25353f0136e63ef71824a183c00ee60099
Florian Glombik <florian.glombik@tum.de>
2024-10-12 21:47:32 +0200

    Add client test to increase coverage

---
+++ src/test/javascript/spec/component/programming-exercise/update-components/switch-edit-mode-button.component.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { SwitchEditModeButtonComponent } from '../../../../../../main/webapp/app/exercises/programming/manage/update/switch-edit-mode-button/switch-edit-mode-button.component';
+import { ArtemisSharedCommonModule } from '../../../../../../main/webapp/app/shared/shared-common.module';
+import { ArtemisSharedComponentModule } from '../../../../../../main/webapp/app/shared/components/shared-component.module';
+import { MockTranslateService } from '../../../helpers/mocks/service/mock-translate.service';
+import { By } from '@angular/platform-browser';
+import { MockModule } from 'ng-mocks';
+
+describe('SwitchEditModeButtonComponent', () => {
+    let fixture: ComponentFixture<SwitchEditModeButtonComponent>;
+    let comp: SwitchEditModeButtonComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [SwitchEditModeButtonComponent],
+            declarations: [MockModule(ArtemisSharedCommonModule), MockModule(ArtemisSharedComponentModule)],
+            providers: [{ provide: TranslateService, useClass: MockTranslateService }],
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(SwitchEditModeButtonComponent);
+                comp = fixture.componentInstance;
+
+                fixture.componentRef.setInput('switchEditMode', () => {});
+                fixture.componentRef.setInput('isSimpleMode', false);
+            });
+    });
+
+    it('should initialize', fakeAsync(() => {
+        fixture.detectChanges();
+        expect(comp).not.toBeNull();
+    }));
+
+    it('should call passed method when button is clicked', () => {
+        const testMethod = jest.fn();
+        fixture.componentRef.setInput('switchEditMode', testMethod);
+
+        const button = fixture.debugElement.query(By.css('jhi-button'));
+        button.triggerEventHandler('onClick', null);
+
+        expect(testMethod).toHaveBeenCalled();
+    });
+});
