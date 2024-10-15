bf2dd869432718cf65c03612e6c3faac91680ddb
Ole Vester <ole.vester+gitlab@check24.de>
2024-10-15 15:59:17 +0200

    Temporarily use PROFILE_CORE to verify getOrThrow

--- src/main/java/de/tum/cit/aet/artemis/core/api/AbstractApi.java
+++ src/main/java/de/tum/cit/aet/artemis/core/api/AbstractApi.java
@@ -1,5 +1,7 @@
 package de.tum.cit.aet.artemis.core.api;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
+
 import java.util.Optional;
 
 import org.springframework.core.env.Environment;
@@ -18,13 +20,15 @@ public abstract class AbstractApi {
         this.profileName = profileName;
     }
 
-    public boolean isActive() {
+    public boolean isActive(String profileName) {
         return environment.acceptsProfiles(Profiles.of(profileName));
     }
 
     /** @noinspection OptionalUsedAsFieldOrParameterType */
     protected <T> T getOrThrow(Optional<T> instance) {
-        if (!isActive() || instance.isEmpty()) {
+        // noinspection UnnecessaryLocalVariable
+        String moduleProfileName = PROFILE_CORE; // in the future, we will switch to profileName
+        if (!isActive(moduleProfileName) || instance.isEmpty()) {
             throw new ModuleNotPresentException(profileName);
         }
         return instance.get();
