0658e9a3555fbefc832f0600246796c5c364d658
Michal Kawka <michal.kawka@gmx.com>
2024-10-15 23:33:54 +0200

    resolve merge conflicts

--- src/test/javascript/spec/service/exam-participation.service.spec.ts
+++ src/test/javascript/spec/service/exam-participation.service.spec.ts
@@ -19,7 +19,7 @@ import { Result } from 'app/entities/result.model';
 import { getLatestSubmissionResult } from 'app/entities/submission.model';
 import { StudentExamWithGradeDTO, StudentResult } from 'app/exam/exam-scores/exam-score-dtos.model';
 import { GradeType } from 'app/entities/grading-scale.model';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, provideHttpClient } from '@angular/common/http';
 
 describe('ExamParticipationService', () => {
     let service: ExamParticipationService;
