0e1a93ce60961ec11b0aa48e00ffe0c47bd28cf3
Johannes Wiest <Johannes.wiest@outlook.com>
2024-10-16 15:02:24 +0200

    introduce loading spinner for navigation to previous and successor learning object

--- src/main/webapp/app/course/learning-paths/components/learning-path-student-nav/learning-path-student-nav.component.ts
+++ src/main/webapp/app/course/learning-paths/components/learning-path-student-nav/learning-path-student-nav.component.ts
@@ -1,9 +1,9 @@
-import { Component, InputSignal, Signal, WritableSignal, computed, effect, inject, input, signal } from '@angular/core';
+import { Component, computed, effect, inject, input, signal, untracked } from '@angular/core';
 import { LearningPathNavigationObjectDTO } from 'app/entities/competency/learning-path.model';
 import { CommonModule } from '@angular/common';
 import { NgbAccordionModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconDefinition, faCheckCircle, faChevronDown, faFlag } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faChevronDown, faChevronLeft, faChevronRight, faFlag, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { LearningPathNavOverviewComponent } from 'app/course/learning-paths/components/learning-path-nav-overview/learning-path-nav-overview.component';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
 import { LearningPathNavigationService } from 'app/course/learning-paths/services/learning-path-navigation.service';
@@ -16,33 +16,43 @@ import { LearningPathNavigationService } from 'app/course/learning-paths/service
     styleUrl: './learning-path-student-nav.component.scss',
 })
 export class LearningPathNavComponent {
-    protected readonly faChevronDown: IconDefinition = faChevronDown;
-    protected readonly faCheckCircle: IconDefinition = faCheckCircle;
-    protected readonly faFlag: IconDefinition = faFlag;
+    protected readonly faChevronDown = faChevronDown;
+    protected readonly faCheckCircle = faCheckCircle;
+    protected readonly faFlag = faFlag;
+    protected readonly faSpinner = faSpinner;
+    protected readonly faChevronLeft = faChevronLeft;
+    protected readonly faChevronRight = faChevronRight;
 
-    private learningPathNavigationService: LearningPathNavigationService = inject(LearningPathNavigationService);
+    private learningPathNavigationService = inject(LearningPathNavigationService);
 
-    readonly learningPathId: InputSignal<number> = input.required<number>();
+    readonly learningPathId = input.required<number>();
 
-    readonly isLoading: WritableSignal<boolean> = this.learningPathNavigationService.isLoading;
+    readonly isLoading = this.learningPathNavigationService.isLoading;
+    readonly isLoadingPredecessor = signal<boolean>(false);
+    readonly isLoadingSuccessor = signal<boolean>(false);
 
-    readonly learningPathProgress: Signal<number> = computed(() => this.learningPathNavigationService.learningPathNavigation()?.progress ?? 0);
-    readonly predecessorLearningObject: Signal<LearningPathNavigationObjectDTO | undefined> = computed(
-        () => this.learningPathNavigationService.learningPathNavigation()?.predecessorLearningObject,
-    );
-    readonly currentLearningObject: Signal<LearningPathNavigationObjectDTO | undefined> = computed(() => this.learningPathNavigationService.currentLearningObject());
-    readonly successorLearningObject: Signal<LearningPathNavigationObjectDTO | undefined> = computed(
-        () => this.learningPathNavigationService.learningPathNavigation()?.successorLearningObject,
-    );
+    private readonly learningPathNavigation = this.learningPathNavigationService.learningPathNavigation;
+    readonly learningPathProgress = computed(() => this.learningPathNavigation()?.progress ?? 0);
+    readonly predecessorLearningObject = computed(() => this.learningPathNavigation()?.predecessorLearningObject);
+    readonly currentLearningObject = computed(() => this.learningPathNavigation()?.currentLearningObject);
+    readonly successorLearningObject = computed(() => this.learningPathNavigation()?.successorLearningObject);
 
-    readonly isDropdownOpen: WritableSignal<boolean> = signal(false);
+    readonly isDropdownOpen = signal<boolean>(false);
 
     constructor() {
-        effect(async () => await this.learningPathNavigationService.loadLearningPathNavigation(this.learningPathId()), { allowSignalWrites: true });
+        effect(
+            () => {
+                const learningPathId = this.learningPathId();
+                untracked(() => this.learningPathNavigationService.loadLearningPathNavigation(learningPathId));
+            },
+            { allowSignalWrites: true },
+        );
     }
 
-    async selectLearningObject(selectedLearningObject: LearningPathNavigationObjectDTO): Promise<void> {
+    async selectLearningObject(selectedLearningObject: LearningPathNavigationObjectDTO, isSuccessor: boolean): Promise<void> {
+        isSuccessor ? this.isLoadingSuccessor.set(true) : this.isLoadingPredecessor.set(true);
         await this.learningPathNavigationService.loadRelativeLearningPathNavigation(this.learningPathId(), selectedLearningObject);
+        isSuccessor ? this.isLoadingSuccessor.set(false) : this.isLoadingPredecessor.set(false);
     }
 
     completeLearningPath(): void {
