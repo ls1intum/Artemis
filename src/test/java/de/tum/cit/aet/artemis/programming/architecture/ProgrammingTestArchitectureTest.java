0c1c581ce073de8ff15a4ae03da24780cbca2bc7
Ole Vester <ole.vester+gitlab@check24.de>
2024-10-16 17:29:15 +0200

    Add ProgrammingTestArchitectureTest

---
+++ src/test/java/de/tum/cit/aet/artemis/programming/architecture/ProgrammingTestArchitectureTest.java
@@ -0,0 +1,31 @@
+package de.tum.cit.aet.artemis.programming.architecture;
+
+import java.util.Set;
+
+import de.tum.cit.aet.artemis.programming.AbstractProgrammingIntegrationGitlabCIGitlabSamlTest;
+import de.tum.cit.aet.artemis.programming.AbstractProgrammingIntegrationIndependentTest;
+import de.tum.cit.aet.artemis.programming.AbstractProgrammingIntegrationJenkinsGitlabTest;
+import de.tum.cit.aet.artemis.programming.AbstractProgrammingIntegrationLocalCILocalVCTest;
+import de.tum.cit.aet.artemis.programming.AbstractProgrammingIntegrationLocalCILocalVCTestBase;
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleTestArchitectureTest;
+
+public class ProgrammingTestArchitectureTest extends AbstractModuleTestArchitectureTest {
+
+    @Override
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".programming";
+    }
+
+    @Override
+    protected Set<Class<?>> getAbstractModuleIntegrationTestClasses() {
+        // @formatter:off
+        return Set.of(
+            AbstractProgrammingIntegrationGitlabCIGitlabSamlTest.class,
+            AbstractProgrammingIntegrationIndependentTest.class,
+            AbstractProgrammingIntegrationJenkinsGitlabTest.class,
+            AbstractProgrammingIntegrationLocalCILocalVCTest.class,
+            AbstractProgrammingIntegrationLocalCILocalVCTestBase.class
+        );
+        // @formatter:on
+    }
+}
