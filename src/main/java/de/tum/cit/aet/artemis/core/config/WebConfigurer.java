a2e9e848571c70824c5275935ae2f1ff71214c20
dennis <dp.jandow@gmail.com>
2024-10-16 17:50:16 +0200

    add import for collection utils

--- src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/WebConfigurer.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
