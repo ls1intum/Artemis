b0cd2c7e54afc29ede55387055fca887bb320c71
Tim Cremer <tim.cremer@tum.de>
2024-10-19 14:57:57 +0200

    Fixed Tests

--- src/test/javascript/spec/component/shared/sidebar/sidebar-accordion.component.spec.ts
+++ src/test/javascript/spec/component/shared/sidebar/sidebar-accordion.component.spec.ts
@@ -53,7 +53,7 @@ describe('SidebarAccordionComponent', () => {
         };
         component.routeParams = { exerciseId: 3 };
         component.collapseState = { current: false, dueSoon: false, past: false, future: true, noDate: true };
-        fixture.componentRef.setInput('sidebarItemAlwaysShow', { current: true, dueSoon: true, past: true, future: true, noDate: true });
+        fixture.componentRef.setInput('sidebarItemAlwaysShow', { current: false, dueSoon: false, past: false, future: false, noDate: false });
         fixture.detectChanges();
     });
 
