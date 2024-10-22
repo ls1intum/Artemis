eea56d900c3eda01ce4bf4ed461de2645dc44932
Florian Glombik <florian.glombik@tum.de>
2024-10-22 14:41:57 +0200

    Add tooltip when button is disabled

--- src/main/webapp/app/forms/forms.module.ts
+++ src/main/webapp/app/forms/forms.module.ts
@@ -5,9 +5,10 @@ import { ArtemisSharedComponentModule } from 'app/shared/components/shared-compo
 import { FormFooterComponent } from 'app/forms/form-footer/form-footer.component';
 import { ExerciseUpdateNotificationModule } from 'app/exercises/shared/exercise-update-notification/exercise-update-notification.module';
 import { SwitchEditModeButtonComponent } from 'app/exercises/programming/manage/update/switch-edit-mode-button/switch-edit-mode-button.component';
+import { NgbTooltipMocksModule } from '../../../../test/javascript/spec/helpers/mocks/directive/ngbTooltipMocks.module';
 
 @NgModule({
-    imports: [ArtemisSharedCommonModule, ArtemisSharedComponentModule, ExerciseUpdateNotificationModule, SwitchEditModeButtonComponent],
+    imports: [ArtemisSharedCommonModule, ArtemisSharedComponentModule, ExerciseUpdateNotificationModule, SwitchEditModeButtonComponent, NgbTooltipMocksModule],
     declarations: [FormStatusBarComponent, FormFooterComponent],
     exports: [FormStatusBarComponent, FormFooterComponent],
 })
