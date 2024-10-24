e081979996006335044c3c018ae8316c6bf33721
EgeDoguKaya <egedk321@outlook.com>
2024-10-24 03:15:26 +0200

    Add translation to the button, remove margins

--- src/main/webapp/app/exam/participate/exercises/exercise-save-button/exercise-save-button.component.ts
+++ src/main/webapp/app/exam/participate/exercises/exercise-save-button/exercise-save-button.component.ts
@@ -3,13 +3,14 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 import { facSaveSuccess } from '../../../../../content/icons/icons';
 import { Submission } from 'app/entities/submission.model';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 @Component({
     selector: 'jhi-exercise-save-button',
     templateUrl: './exercise-save-button.component.html',
     styleUrls: ['./exercise-save-button.component.scss'],
     standalone: true,
-    imports: [FaIconComponent],
+    imports: [FaIconComponent, TranslateDirective],
 })
 export class ExerciseSaveButtonComponent {
     protected readonly faFloppyDisk = faFloppyDisk;
