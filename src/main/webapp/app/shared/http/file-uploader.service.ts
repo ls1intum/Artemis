65e33ba26487969f7d9d8b67810ee136b4d6ec1c
Patrik Zander <patrik.zander@tum.de>
2024-10-24 13:09:13 +0200

    Remove some unused options

--- src/main/webapp/app/shared/http/file-uploader.service.ts
+++ src/main/webapp/app/shared/http/file-uploader.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { MAX_FILE_SIZE } from 'app/shared/constants/input.constants';
 import { lastValueFrom } from 'rxjs';
 import { UPLOAD_MARKDOWN_FILE_EXTENSIONS } from 'app/shared/constants/file-extensions.constants';
@@ -8,24 +8,18 @@ export interface FileUploadResponse {
     path?: string;
 }
 
-type Options = {
-    keepFileName: boolean;
-};
-
 @Injectable({ providedIn: 'root' })
 export class FileUploaderService {
+    private readonly http = inject(HttpClient);
     readonly acceptedMarkdownFileExtensions = UPLOAD_MARKDOWN_FILE_EXTENSIONS;
-    constructor(private http: HttpClient) {}
 
     /**
      * Uploads a file for the markdown editor to the server.
      * @param file The file to upload
-     * @param fileName The name of the file
-     * @param options The options dictionary (e.g, { keepFileName: true })
      * @return A promise with the response from the server or an error
      */
-    uploadMarkdownFile(file: Blob | File, fileName?: string, options?: Options): Promise<FileUploadResponse> {
-        const fileExtension = fileName ? fileName.split('.').pop()!.toLocaleLowerCase() : (file as File).name.split('.').pop()!.toLocaleLowerCase();
+    uploadMarkdownFile(file: File): Promise<FileUploadResponse> {
+        const fileExtension = file.name.split('.').pop()!.toLocaleLowerCase();
         if (!this.acceptedMarkdownFileExtensions.includes(fileExtension)) {
             return Promise.reject(
                 new Error(
@@ -38,9 +32,33 @@ export class FileUploaderService {
             return Promise.reject(new Error('File is too big! Maximum allowed file size: ' + MAX_FILE_SIZE / (1024 * 1024) + ' MB.'));
         }
 
-        const keepFileName = !!options?.keepFileName;
         const formData = new FormData();
-        formData.append('file', file, fileName);
-        return lastValueFrom(this.http.post<FileUploadResponse>(`/api/markdown-file-upload?keepFileName=${keepFileName}`, formData));
+        formData.append('file', file, file.name);
+        return lastValueFrom(this.http.post<FileUploadResponse>(`/api/markdown-file-upload?keepFileName=${false}`, formData));
+    }
+
+    uploadMarkdownFileInCurrentMetisConversation(file: File, courseId: number | undefined, conversationId: number | undefined): Promise<FileUploadResponse> {
+        if (!courseId || !conversationId) {
+            return Promise.reject(new Error(`No course or conversation available for the file upload.`));
+        }
+
+        // TODO refactor
+        const fileExtension = (file as File).name.split('.').pop()!.toLocaleLowerCase();
+        if (!this.acceptedMarkdownFileExtensions.includes(fileExtension)) {
+            return Promise.reject(
+                new Error(
+                    'Unsupported file type! Only the following file extensions are allowed: ' + this.acceptedMarkdownFileExtensions.map((extension) => `.${extension}`).join(', '),
+                ),
+            );
+        }
+
+        // TODO: adjust file size
+        if (file.size > MAX_FILE_SIZE) {
+            return Promise.reject(new Error('File is too big! Maximum allowed file size: ' + MAX_FILE_SIZE / (1024 * 1024) + ' MB.'));
+        }
+
+        const formData = new FormData();
+        formData.append('file', file, file.name);
+        return lastValueFrom(this.http.post<FileUploadResponse>(`/api/files/courses/${courseId}/conversations/${conversationId}`, formData));
     }
 }
