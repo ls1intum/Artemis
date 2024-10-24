918da9a1c43f6fd4de4067fdf4c000de56d6fb09
Stephan Krusche <krusche@tum.de>
2024-10-24 19:22:16 +0200

    Temporarily disable course archive feature

--- src/main/webapp/app/overview/courses-routing.module.ts
+++ src/main/webapp/app/overview/courses-routing.module.ts
@@ -12,7 +12,7 @@ import { CourseTutorialGroupDetailComponent } from './tutorial-group-details/cou
 import { ExamParticipationComponent } from 'app/exam/participate/exam-participation.component';
 import { PendingChangesGuard } from 'app/shared/guard/pending-changes.guard';
 import { CourseDashboardGuard } from 'app/overview/course-dashboard/course-dashboard-guard.service';
-import { CourseArchiveComponent } from './course-archive/course-archive.component';
+//import { CourseArchiveComponent } from './course-archive/course-archive.component';
 
 const routes: Routes = [
     {
@@ -28,15 +28,15 @@ const routes: Routes = [
         path: 'enroll',
         loadChildren: () => import('./course-registration/course-registration.module').then((m) => m.CourseRegistrationModule),
     },
-    {
-        path: 'archive',
-        component: CourseArchiveComponent,
-        data: {
-            authorities: [Authority.USER],
-            pageTitle: 'overview.archive',
-        },
-        canActivate: [UserRouteAccessService],
-    },
+    // {
+    //     path: 'archive',
+    //     component: CourseArchiveComponent,
+    //     data: {
+    //         authorities: [Authority.USER],
+    //         pageTitle: 'overview.archive',
+    //     },
+    //     canActivate: [UserRouteAccessService],
+    // },
     // /courses/:courseId/register is special,
     // because we won't have access to the course object before the user is registered,
     // so we need to load it outside the normal course routing
