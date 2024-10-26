202c1f80ff3809bbcd914f019b31412b358462bf
Mohamed Bilel Besrour <besrourbilel@gmail.com>
2024-10-26 11:56:11 +0200

    REVERT

--- src/main/webapp/app/exercises/text/assess/textblock-feedback-editor/textblock-feedback-editor.component.ts
+++ src/main/webapp/app/exercises/text/assess/textblock-feedback-editor/textblock-feedback-editor.component.ts
@@ -21,8 +21,8 @@ export class TextblockFeedbackEditorComponent implements AfterViewInit {
     @Input() textBlock: TextBlock = new TextBlock();
     @Input() feedback: Feedback = new Feedback();
     @Output() feedbackChange = new EventEmitter<Feedback>();
-    // eslint-disable-next-line @angular-eslint/no-output-native
-    @Output() close = new EventEmitter<void>();
+
+    @Output() closeEmitter = new EventEmitter<void>();
     @Output() onFocus = new EventEmitter<void>();
     @ViewChild('detailText') textareaRef: ElementRef;
     @ViewChild(ConfirmIconComponent) confirmIconComponent: ConfirmIconComponent;
