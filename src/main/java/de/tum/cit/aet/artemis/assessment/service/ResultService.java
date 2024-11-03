cbafc5bd8ae548afc17f0f846b1408b8fde105a1
aniruddhzaveri <aniruddh.zaveri@tum.de>
2024-11-03 09:49:20 +0100

    inline comment added from b.fein review

--- src/main/java/de/tum/cit/aet/artemis/assessment/service/ResultService.java
+++ src/main/java/de/tum/cit/aet/artemis/assessment/service/ResultService.java
@@ -639,6 +639,10 @@ public class ResultService {
      * overridden or updated. The deletion is performed only for feedback items with a non-null ID and an associated long feedback text.
      * <p>
      * This approach reduces the need for individual deletion calls and performs batch deletion in a single database operation.
+     * <p>
+     * **Note:** This method should only be used for manually assessed submissions, not for fully automatic assessments, due to its dependency on the
+     * {@link Result#updateAllFeedbackItems} method, which is designed for manual feedback management. Using this method with automatic assessments could
+     * lead to unintended behavior or data inconsistencies.
      *
      * @param feedbackList The list of {@link Feedback} objects for which the long feedback texts are to be deleted. Only feedback items that have long feedback texts and a
      *                         non-null ID will be processed.
