e2fe5421d7f6ac5cc53fb1e693cfd47cb8754727
Marcel Gaupp <m.gaupp@tum.de>
2024-11-06 23:35:53 +0100

    WIP: Checkout Paths

--- src/main/java/de/tum/cit/aet/artemis/core/config/ProgrammingLanguageConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/ProgrammingLanguageConfiguration.java
@@ -2,17 +2,21 @@ package de.tum.cit.aet.artemis.core.config;
 
 import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
-import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 import de.tum.cit.aet.artemis.programming.domain.ProgrammingLanguage;
 import de.tum.cit.aet.artemis.programming.domain.ProjectType;
@@ -24,6 +28,23 @@ import de.tum.cit.aet.artemis.programming.domain.ProjectType;
 @Profile(PROFILE_CORE)
 public class ProgrammingLanguageConfiguration {
 
+    public record ConfigurationEntry(Map<ProjectType, String> images, CheckoutPaths checkout_paths) {
+    }
+
+    public record CheckoutPaths(String assignment, String test, String solution) {
+    }
+
+    /**
+     * A key-value pair of a CLI flag and its value for a {@code docker run} command.
+     * <p>
+     * E.g., the CLI option {@code --cpus 2} is represented as flag {@code --cpus} and value {@code 2}.
+     *
+     * @param flag  The option name.
+     * @param value The option value.
+     */
+    public record DockerFlag(String flag, String value) {
+    }
+
     private static final Logger log = LoggerFactory.getLogger(ProgrammingLanguageConfiguration.class);
 
     private static final ProjectType DEFAULT_PROJECT_TYPE = ProjectType.PLAIN;
@@ -32,12 +53,34 @@ public class ProgrammingLanguageConfiguration {
 
     private static final ProjectType GRADLE_PROJECT_TYPE = ProjectType.GRADLE_GRADLE;
 
-    private Map<ProgrammingLanguage, Map<ProjectType, String>> images = new EnumMap<>(ProgrammingLanguage.class);
+    /**
+     * Contains all the language specific configuration
+     */
+    private final Map<ProgrammingLanguage, ConfigurationEntry> configurationEntries;
 
     /**
      * Contains all the docker run arguments obtained from the spring properties
      */
-    private List<DockerFlag> defaultDockerFlags;
+    private final List<DockerFlag> defaultDockerFlags;
+
+    /**
+     * Sets the default docker run arguments based on the spring configuration
+     * Set the map of languages to build images.
+     * (Constructor implicitly called by spring with the yaml configs as parameter)
+     *
+     * @param defaultDockerFlags key value pairs of run arguments
+     * @param images             legacy map of languages to build images
+     */
+    public ProgrammingLanguageConfiguration(final List<DockerFlag> defaultDockerFlags, final Map<ProgrammingLanguage, ConfigurationEntry> programmingLanguages,
+            final Map<ProgrammingLanguage, Map<ProjectType, String>> images) {
+        this.defaultDockerFlags = Objects.requireNonNullElseGet(defaultDockerFlags, List::of);
+        log.debug("Set Docker flags to {}", defaultDockerFlags);
+
+        this.configurationEntries = Objects.requireNonNullElseGet(programmingLanguages, HashMap::new);
+        addImages(images);
+        checkImageForAllProgrammingLanguagesDefined();
+        log.debug("Loaded programming language configuration: {}", this.configurationEntries);
+    }
 
     /**
      * Set the map of languages to build images.
@@ -45,11 +88,18 @@ public class ProgrammingLanguageConfiguration {
      *
      * @param buildImages the map of languages to build images
      */
-    public void setImages(final Map<String, Map<String, String>> buildImages) {
-        final var languageSpecificBuildImages = loadImages(buildImages);
-        checkImageForAllProgrammingLanguagesDefined(languageSpecificBuildImages);
-        images = languageSpecificBuildImages;
-        log.debug("Loaded Docker image configuration: {}", images);
+    private void addImages(final Map<ProgrammingLanguage, Map<ProjectType, String>> buildImages) {
+        // checkImageForAllProgrammingLanguagesDefined(languageSpecificBuildImages);
+
+        for (var entry : buildImages.entrySet()) {
+            ConfigurationEntry configurationEntry = configurationEntries.get(entry.getKey());
+            if (configurationEntry != null) {
+                configurationEntry.images().putAll(entry.getValue());
+            }
+            else {
+                configurationEntries.put(entry.getKey(), new ConfigurationEntry(entry.getValue(), null));
+            }
+        }
     }
 
     /**
@@ -71,97 +121,18 @@ public class ProgrammingLanguageConfiguration {
     }
 
     /**
-     * Sets the default docker run arguments based on the spring configuration
-     * (Method implicitly called by spring with the yaml configs as parameter)
-     *
-     * @param dockerFlags key value pairs of run arguments
-     */
-    public void setDefaultDockerFlags(final List<DockerFlag> dockerFlags) {
-        log.debug("Set Docker flags to {}", dockerFlags);
-        this.defaultDockerFlags = dockerFlags;
-    }
-
-    /**
-     * Parses the configuration contents into a properly typed map.
-     *
-     * @param imageConfig the config as it was received on server startup.
-     * @return A map of programming languages to the CI build images that are defined for it.
-     */
-    private Map<ProgrammingLanguage, Map<ProjectType, String>> loadImages(final Map<String, Map<String, String>> imageConfig) {
-        final Map<ProgrammingLanguage, Map<ProjectType, String>> buildImages = new EnumMap<>(ProgrammingLanguage.class);
-
-        for (var entry : imageConfig.entrySet()) {
-            final ProgrammingLanguage programmingLanguage = tryParseProgrammingLanguage(entry.getKey());
-            final Map<ProjectType, String> projectTypeImages = tryParseProjectTypeBuildImages(programmingLanguage, entry.getValue());
-
-            buildImages.put(programmingLanguage, projectTypeImages);
-        }
-
-        return buildImages;
-    }
-
-    /**
-     * Tries to parse the given programming language.
-     * <p>
-     * Aborts server startup by throwing an exception if the language cannot be parsed.
-     *
-     * @param language The name of the language that should be parsed.
-     * @return The given language, now parsed.
-     */
-    private ProgrammingLanguage tryParseProgrammingLanguage(final String language) {
-        try {
-            return ProgrammingLanguage.valueOf(language.toUpperCase(Locale.ROOT));
-        }
-        catch (IllegalArgumentException ex) {
-            throw new IllegalArgumentException("Unknown programming language: " + language, ex);
-        }
-    }
-
-    /**
-     * Tries to parse key-value pairs of project types and their build images.
-     * <p>
-     * Aborts server startup by throwing an exception if the project type cannot be parsed.
-     *
-     * @param buildImageConfig A map of project types to CI build images.
-     * @return A map of project types to CI build images.
-     */
-    private Map<ProjectType, String> tryParseProjectTypeBuildImages(final ProgrammingLanguage language, final Map<String, String> buildImageConfig) {
-        final Map<ProjectType, String> projectTypeImages = new EnumMap<>(ProjectType.class);
-
-        for (var entry : buildImageConfig.entrySet()) {
-            final ProjectType projectType = tryParseProjectType(language, entry.getKey());
-            final String image = entry.getValue();
-
-            projectTypeImages.put(projectType, image);
-        }
-
-        return projectTypeImages;
-    }
-
-    private ProjectType tryParseProjectType(final ProgrammingLanguage language, final String projectType) {
-        try {
-            return ProjectType.tryFromString(projectType);
-        }
-        catch (IllegalArgumentException ex) {
-            throw new IllegalArgumentException("Unknown project type for " + language + ": " + projectType);
-        }
-    }
-
-    /**
      * Makes sure that for each programming language at least a default image is defined.
      * <p>
      * Aborts server startup by throwing an exception if some definitions are missing.
-     *
-     * @param buildImages The build images as defined in the configuration.
      */
-    private void checkImageForAllProgrammingLanguagesDefined(final Map<ProgrammingLanguage, Map<ProjectType, String>> buildImages) {
+    private void checkImageForAllProgrammingLanguagesDefined() {
         for (ProgrammingLanguage language : ProgrammingLanguage.getEnabledLanguages()) {
-            if (!buildImages.containsKey(language)) {
+            if (!configurationEntries.containsKey(language)) {
                 throw new IllegalArgumentException("For each programming language a CI build image must be defined. Missing: " + language);
             }
 
-            final Map<ProjectType, String> languageImages = buildImages.get(language);
-            if (!languageImages.containsKey(DEFAULT_PROJECT_TYPE)) {
+            final ConfigurationEntry configurationEntry = configurationEntries.get(language);
+            if (!configurationEntry.images().containsKey(DEFAULT_PROJECT_TYPE)) {
                 throw new IllegalArgumentException("For each programming language a default image must be defined. Missing: " + language);
             }
         }
@@ -177,7 +148,7 @@ public class ProgrammingLanguageConfiguration {
      * @return the docker image name
      */
     public String getImage(ProgrammingLanguage programmingLanguage, Optional<ProjectType> projectType) {
-        final Map<ProjectType, String> languageImages = images.get(programmingLanguage);
+        final Map<ProjectType, String> languageImages = configurationEntries.get(programmingLanguage).images();
         final ProjectType configuredProjectType = projectType.map(this::getConfiguredProjectType).orElse(DEFAULT_PROJECT_TYPE);
 
         if (languageImages.containsKey(configuredProjectType)) {
@@ -200,14 +171,65 @@ public class ProgrammingLanguageConfiguration {
         };
     }
 
-    /**
-     * A key-value pair of a CLI flag and its value for a {@code docker run} command.
-     * <p>
-     * E.g., the CLI option {@code --cpus 2} is represented as flag {@code --cpus} and value {@code 2}.
-     *
-     * @param flag  The option name.
-     * @param value The option value.
-     */
-    public record DockerFlag(String flag, String value) {
+    @Component
+    @ConfigurationPropertiesBinding
+    public static class ProjectTypeConverter implements Converter<String, ProjectType> {
+
+        /**
+         * Tries to parse the given project type string.
+         * <p>
+         * Ignores the casing of the input string.
+         * <p>
+         * In addition to the representation of values as obtained by calling
+         * {@link ProjectType#toString()}, a few shorthand aliases can be parsed:
+         * <ul>
+         * <li>{@code default} for {@link ProjectType#PLAIN}</li>
+         * <li>{@code maven} for {@link ProjectType#MAVEN_MAVEN}</li>
+         * <li>{@code gradle} for {@link ProjectType#GRADLE_GRADLE}</li>
+         * </ul>
+         *
+         * @param projectType The project type string that should be parsed.
+         * @return The project type represented by the given input.
+         * @throws IllegalArgumentException Thrown if the input cannot be parsed as a project type.
+         */
+        @Override
+        public ProjectType convert(String projectType) {
+            final String input = projectType.toUpperCase(Locale.ROOT);
+
+            try {
+                return ProjectType.valueOf(input);
+            }
+            catch (IllegalArgumentException ex) {
+                return switch (input) {
+                    case "DEFAULT" -> ProjectType.PLAIN;
+                    case "MAVEN" -> ProjectType.MAVEN_MAVEN;
+                    case "GRADLE" -> ProjectType.GRADLE_GRADLE;
+                    default -> throw new IllegalArgumentException("Unknown project type: " + projectType);
+                };
+            }
+        }
+    }
+
+    @Component
+    @ConfigurationPropertiesBinding
+    public static class ProgrammingLanguageConverter implements Converter<String, ProgrammingLanguage> {
+
+        /**
+         * Tries to parse the given programming language.
+         * <p>
+         * Aborts server startup by throwing an exception if the language cannot be parsed.
+         *
+         * @param language The name of the language that should be parsed.
+         * @return The given language, now parsed.
+         */
+        @Override
+        public ProgrammingLanguage convert(String language) {
+            try {
+                return ProgrammingLanguage.valueOf(language.toUpperCase(Locale.ROOT));
+            }
+            catch (IllegalArgumentException ex) {
+                throw new IllegalArgumentException("Unknown programming language: " + language, ex);
+            }
+        }
     }
 }
--- src/test/java/de/tum/cit/aet/artemis/core/config/ProgrammingLanguageConfigurationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/core/config/ProgrammingLanguageConfigurationTest.java
@@ -1,135 +1,163 @@
 package de.tum.cit.aet.artemis.core.config;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Predicate;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import de.tum.cit.aet.artemis.programming.domain.ProgrammingLanguage;
-import de.tum.cit.aet.artemis.programming.domain.ProjectType;
 
 class ProgrammingLanguageConfigurationTest {
 
     private static final String OVERRIDDEN_IMAGE_NAME = "overridden_image";
 
-    private Map<String, Map<String, String>> defaultConfig;
+    private static final Map<String, String> defaultConfig = getDefaultImagesProperties();
 
-    @BeforeEach
-    void setup() {
-        defaultConfig = getDefaultImages();
-    }
+    @Nested
+    @SpringBootTest
+    @ExtendWith(SpringExtension.class)
+    @ContextConfiguration(classes = { ProgrammingLanguageConfiguration.ProgrammingLanguageConverter.class,
+            ProgrammingLanguageConfiguration.ProjectTypeConverter.class }, locations = {})
+    @EnableConfigurationProperties({ ProgrammingLanguageConfiguration.class })
+    @ActiveProfiles({ PROFILE_CORE })
+    @TestPropertySource(properties = "artemis.continuous-integration.build.programming-languages.swift.images.plain=SWIFT")
+    class WithoutSwift {
 
-    @Test
-    void testLanguageMissing() {
-        defaultConfig.remove("swift");
-        var config = new ProgrammingLanguageConfiguration();
+        @Autowired
+        private ApplicationContext applicationContext;
 
-        assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Missing: SWIFT");
-    }
+        // @DynamicPropertySource
+        // static void addProperties(DynamicPropertyRegistry registry) {
+        // var config = new HashMap<>(defaultConfig);
+        // // config.remove("artemis.continuous-integration.build.swift.images.default");
+        //
+        // for (var entry : config.entrySet()) {
+        // registry.add(entry.getKey(), entry::getValue);
+        // }
+        // }
 
-    @Test
-    void testDefaultImageMissing() {
-        defaultConfig.put("kotlin", Map.of());
-        var config = new ProgrammingLanguageConfiguration();
-
-        assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Missing: KOTLIN");
-    }
-
-    @Test
-    void testUnknownProgrammingLanguageConfigured() {
-        defaultConfig.put("whitespace", Map.of("default", OVERRIDDEN_IMAGE_NAME));
-        var config = new ProgrammingLanguageConfiguration();
-
-        assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Unknown programming language: whitespace");
-    }
-
-    @Test
-    void testUnknownProjectTypeConfigured() {
-        defaultConfig.get("java").put("sbt", OVERRIDDEN_IMAGE_NAME);
-        var config = new ProgrammingLanguageConfiguration();
-
-        assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Unknown project type for JAVA: sbt");
-    }
-
-    @Test
-    void testDefaultImageConfigurationOnly() {
-        var config = new ProgrammingLanguageConfiguration();
-        config.setImages(defaultConfig);
-
-        assertThat(config.getImage(ProgrammingLanguage.JAVA, Optional.of(ProjectType.PLAIN_MAVEN))).isEqualTo("JAVA");
-    }
-
-    @Test
-    void testMultipleOverridesSameLanguage() {
-        defaultConfig.put("c", Map.of("default", "default_image", "gcc", "gcc_image", "fact", "fact_image"));
-        var config = new ProgrammingLanguageConfiguration();
-        config.setImages(defaultConfig);
-
-        assertThat(config.getImage(ProgrammingLanguage.C, Optional.of(ProjectType.GCC))).isEqualTo("gcc_image");
-        assertThat(config.getImage(ProgrammingLanguage.C, Optional.of(ProjectType.FACT))).isEqualTo("fact_image");
-    }
-
-    @Test
-    void testOverriddenImageConfigurationMaven() {
-        defaultConfig.get("java").put("maven", OVERRIDDEN_IMAGE_NAME);
-        testOverriddenImageConfiguration(ProgrammingLanguage.JAVA, List.of(ProjectType.MAVEN_MAVEN, ProjectType.PLAIN_MAVEN));
-    }
-
-    @Test
-    void testOverriddenImageConfigurationGradle() {
-        defaultConfig.get("java").put("gradle", OVERRIDDEN_IMAGE_NAME);
-        testOverriddenImageConfiguration(ProgrammingLanguage.JAVA, List.of(ProjectType.GRADLE_GRADLE, ProjectType.PLAIN_GRADLE));
-    }
-
-    @Test
-    void testOverriddenImageConfigurationSwift() {
-        defaultConfig.get("swift").put("xcode", OVERRIDDEN_IMAGE_NAME);
-        testOverriddenImageConfiguration(ProgrammingLanguage.SWIFT, List.of(ProjectType.XCODE));
-    }
-
-    @Test
-    void testDockerRunFlags() {
-        final var config = new ProgrammingLanguageConfiguration();
-        config.setDefaultDockerFlags(getDockerFlags());
-
-        final List<String> expectedFlags = List.of("-v", "\"$(pwd):/working_dir\"", "--cpus", "\"2\"", "--env", "\"VAR='value with spaces'\"", "--env", "\"SECOND_VAR=42\"");
-        assertThat(config.getDefaultDockerFlags()).containsExactlyElementsOf(expectedFlags);
-    }
-
-    void testOverriddenImageConfiguration(final ProgrammingLanguage language, final List<ProjectType> overriddenProjectTypes) {
-        var config = new ProgrammingLanguageConfiguration();
-        config.setImages(defaultConfig);
-
-        for (ProjectType projectType : overriddenProjectTypes) {
-            assertThat(config.getImage(language, Optional.of(projectType))).isEqualTo(OVERRIDDEN_IMAGE_NAME);
+        @Test
+        void test() {
+            ProgrammingLanguageConfiguration programmingLanguageConfiguration = applicationContext.getBean(ProgrammingLanguageConfiguration.class);
+            assertThat(programmingLanguageConfiguration).isNotNull();
         }
-
-        final List<ProjectType> nonOverriddenProjectTypes = Arrays.stream(ProjectType.values()).filter(Predicate.not(overriddenProjectTypes::contains)).toList();
-        for (ProjectType projectType : nonOverriddenProjectTypes) {
-            assertThat(config.getImage(language, Optional.of(projectType))).isEqualTo(language.toString());
-        }
-
-        assertThat(config.getImage(language, Optional.empty())).isEqualTo(language.toString());
     }
 
-    private Map<String, Map<String, String>> getDefaultImages() {
-        final Map<String, Map<String, String>> images = new HashMap<>();
+    // @Test
+    // void testLanguageMissing() {
+    // defaultConfig.remove("swift");
+    // var config = new ProgrammingLanguageConfiguration();
+    //
+    // assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Missing: SWIFT");
+    // }
+    //
+    // @Test
+    // void testDefaultImageMissing() {
+    // defaultConfig.put("kotlin", Map.of());
+    // var config = new ProgrammingLanguageConfiguration();
+    //
+    // assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Missing: KOTLIN");
+    // }
+    //
+    // @Test
+    // void testUnknownProgrammingLanguageConfigured() {
+    // defaultConfig.put("whitespace", Map.of("default", OVERRIDDEN_IMAGE_NAME));
+    // var config = new ProgrammingLanguageConfiguration();
+    //
+    // assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Unknown programming language: whitespace");
+    // }
+    //
+    // @Test
+    // void testUnknownProjectTypeConfigured() {
+    // defaultConfig.get("java").put("sbt", OVERRIDDEN_IMAGE_NAME);
+    // var config = new ProgrammingLanguageConfiguration();
+    //
+    // assertThatIllegalArgumentException().isThrownBy(() -> config.setImages(defaultConfig)).withMessageContaining("Unknown project type for JAVA: sbt");
+    // }
+    //
+    // @Test
+    // void testDefaultImageConfigurationOnly() {
+    // var config = new ProgrammingLanguageConfiguration();
+    // config.setImages(defaultConfig);
+    //
+    // assertThat(config.getImage(ProgrammingLanguage.JAVA, Optional.of(ProjectType.PLAIN_MAVEN))).isEqualTo("JAVA");
+    // }
+    //
+    // @Test
+    // void testMultipleOverridesSameLanguage() {
+    // defaultConfig.put("c", Map.of("default", "default_image", "gcc", "gcc_image", "fact", "fact_image"));
+    // var config = new ProgrammingLanguageConfiguration();
+    // config.setImages(defaultConfig);
+    //
+    // assertThat(config.getImage(ProgrammingLanguage.C, Optional.of(ProjectType.GCC))).isEqualTo("gcc_image");
+    // assertThat(config.getImage(ProgrammingLanguage.C, Optional.of(ProjectType.FACT))).isEqualTo("fact_image");
+    // }
+    //
+    // @Test
+    // void testOverriddenImageConfigurationMaven() {
+    // defaultConfig.get("java").put("maven", OVERRIDDEN_IMAGE_NAME);
+    // testOverriddenImageConfiguration(ProgrammingLanguage.JAVA, List.of(ProjectType.MAVEN_MAVEN, ProjectType.PLAIN_MAVEN));
+    // }
+    //
+    // @Test
+    // void testOverriddenImageConfigurationGradle() {
+    // defaultConfig.get("java").put("gradle", OVERRIDDEN_IMAGE_NAME);
+    // testOverriddenImageConfiguration(ProgrammingLanguage.JAVA, List.of(ProjectType.GRADLE_GRADLE, ProjectType.PLAIN_GRADLE));
+    // }
+    //
+    // @Test
+    // void testOverriddenImageConfigurationSwift() {
+    // defaultConfig.get("swift").put("xcode", OVERRIDDEN_IMAGE_NAME);
+    // testOverriddenImageConfiguration(ProgrammingLanguage.SWIFT, List.of(ProjectType.XCODE));
+    // }
+    //
+    // @Test
+    // void testDockerRunFlags() {
+    // final var config = new ProgrammingLanguageConfiguration();
+    // config.setDefaultDockerFlags(getDockerFlags());
+    //
+    // final List<String> expectedFlags = List.of("-v", "\"$(pwd):/working_dir\"", "--cpus", "\"2\"", "--env", "\"VAR='value with spaces'\"", "--env", "\"SECOND_VAR=42\"");
+    // assertThat(config.getDefaultDockerFlags()).containsExactlyElementsOf(expectedFlags);
+    // }
+    //
+    // void testOverriddenImageConfiguration(final ProgrammingLanguage language, final List<ProjectType> overriddenProjectTypes) {
+    // var config = new ProgrammingLanguageConfiguration();
+    // config.setImages(defaultConfig);
+    //
+    // for (ProjectType projectType : overriddenProjectTypes) {
+    // assertThat(config.getImage(language, Optional.of(projectType))).isEqualTo(OVERRIDDEN_IMAGE_NAME);
+    // }
+    //
+    // final List<ProjectType> nonOverriddenProjectTypes = Arrays.stream(ProjectType.values()).filter(Predicate.not(overriddenProjectTypes::contains)).toList();
+    // for (ProjectType projectType : nonOverriddenProjectTypes) {
+    // assertThat(config.getImage(language, Optional.of(projectType))).isEqualTo(language.toString());
+    // }
+    //
+    // assertThat(config.getImage(language, Optional.empty())).isEqualTo(language.toString());
+    // }
+
+    private static Map<String, String> getDefaultImagesProperties() {
+        final Map<String, String> properties = new HashMap<>();
 
         for (ProgrammingLanguage language : ProgrammingLanguage.getEnabledLanguages()) {
-            final Map<String, String> languageImages = new HashMap<>();
-            languageImages.put("default", language.name());
-            images.put(language.toString().toLowerCase(), languageImages);
+            properties.put("artemis.continuous-integration.build.programming-languages." + language.toString().toLowerCase() + ".images.default", language.name());
         }
 
-        return images;
+        return properties;
     }
 
     private List<ProgrammingLanguageConfiguration.DockerFlag> getDockerFlags() {
