e697bb5650225c577cd1ba98b3a46b3048ee51e1
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2024-11-08 15:05:54 +0100

    fix modules?

--- src/main/webapp/app/core/theme/theme-switch.component.ts
+++ src/main/webapp/app/core/theme/theme-switch.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit, computed, inject, input, viewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtemisSharedModule } from 'app/shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule, NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { PlacementArray } from '@ng-bootstrap/ng-bootstrap/util/positioning';
 import { Theme, ThemeService } from 'app/core/theme/theme.service';
 import { fromEvent } from 'rxjs';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 /**
  * Displays a sun or a moon in the navbar, depending on the current theme.
@@ -16,7 +18,7 @@ import { TranslateDirective } from 'app/shared/language/translate.directive';
     selector: 'jhi-theme-switch',
     templateUrl: './theme-switch.component.html',
     styleUrls: ['theme-switch.component.scss'],
-    imports: [TranslateDirective, NgbModule, FontAwesomeModule],
+    imports: [TranslateModule, CommonModule, ArtemisSharedModule, NgbModule, FontAwesomeModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
 })
