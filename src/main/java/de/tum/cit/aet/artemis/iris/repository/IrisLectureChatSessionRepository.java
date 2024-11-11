3b127784c0504651597a3a03929c544c923c25d6
isabella <isabella.gessl@tum.de>
2024-11-11 17:42:44 +0100

    fix resource and repository annotiation

--- src/main/java/de/tum/cit/aet/artemis/iris/repository/IrisLectureChatSessionRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/repository/IrisLectureChatSessionRepository.java
@@ -1,5 +1,6 @@
 package de.tum.cit.aet.artemis.iris.repository;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_IRIS;
 import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.LOAD;
 
 import java.util.Collections;
@@ -7,16 +8,20 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.domain.DomainObject;
 import de.tum.cit.aet.artemis.core.exception.EntityNotFoundException;
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
 import de.tum.cit.aet.artemis.iris.domain.session.IrisLectureChatSession;
 
+@Repository
+@Profile(PROFILE_IRIS)
 public interface IrisLectureChatSessionRepository extends ArtemisJpaRepository<IrisLectureChatSession, Long> {
 
     @Query("""
