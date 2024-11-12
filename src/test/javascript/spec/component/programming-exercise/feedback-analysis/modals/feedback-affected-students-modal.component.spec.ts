72b482e74a094e072c10f28aa0abbdb33df7be83
aniruddhzaveri <aniruddh.zaveri@tum.de>
2024-11-12 16:14:50 +0100

    client test

--- src/test/javascript/spec/component/programming-exercise/feedback-analysis/modals/feedback-affected-students-modal.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/feedback-analysis/modals/feedback-affected-students-modal.component.spec.ts
@@ -61,11 +61,13 @@ describe('AffectedStudentsModalComponent', () => {
 
     it('should handle error when loadAffected fails', async () => {
         jest.spyOn(feedbackService, 'getParticipationForFeedbackIds').mockReturnValueOnce(Promise.reject(new Error('Error loading data')));
+        const alertServiceSpy = jest.spyOn(component.alertService, 'error');
         const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
 
         await component['loadAffected']();
 
         expect(component.participation().content).toEqual([]);
+        expect(alertServiceSpy).toHaveBeenCalled();
         expect(consoleErrorSpy).toHaveBeenCalled();
 
         consoleErrorSpy.mockRestore();
