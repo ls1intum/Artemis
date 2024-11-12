042437d9819a9fce2c5ca67476a063dd7996ce02
Mohamed Bilel Besrour <besrourbilel@gmail.com>
2024-11-12 11:14:21 +0100

    remove unused code

--- src/main/java/de/tum/cit/aet/artemis/buildagent/dto/DockerRunConfig.java
+++ src/main/java/de/tum/cit/aet/artemis/buildagent/dto/DockerRunConfig.java
@@ -1,36 +1,7 @@
 package de.tum.cit.aet.artemis.buildagent.dto;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public record DockerRunConfig(boolean isNetworkDisabled, List<String> env) implements Serializable {
-
-    public enum AllowedDockerFlags {
-
-        NETWORK("network"), ENV("env");
-
-        private final String flag;
-
-        AllowedDockerFlags(String flag) {
-            this.flag = flag;
-        }
-
-        public String flag() {
-            return flag;
-        }
-
-        private static final Set<String> ALLOWED_FLAGS = new HashSet<>();
-
-        static {
-            for (AllowedDockerFlags value : values()) {
-                ALLOWED_FLAGS.add(value.flag());
-            }
-        }
-
-        public static boolean isAllowed(String flag) {
-            return ALLOWED_FLAGS.contains(flag);
-        }
-    }
 }
