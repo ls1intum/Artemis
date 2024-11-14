34a8a743f6d2451b05202c3663087b6d6fab4dfb
Florian Glombik <florian.glombik@tum.de>
2024-11-15 00:49:47 +0100

    Introduce signals in detail overview list component

--- src/main/webapp/app/detail-overview-list/detail-overview-list.component.ts
+++ src/main/webapp/app/detail-overview-list/detail-overview-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation, effect, input, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation, inject, input } from '@angular/core';
 import { isEmpty } from 'lodash-es';
 import { FeatureToggle } from 'app/shared/feature-toggle/feature-toggle.service';
 import { ButtonSize, TooltipPlacement } from 'app/shared/components/button.component';
@@ -11,7 +11,6 @@ import { UMLModel } from '@ls1intum/apollon';
 import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
 import { Subscription } from 'rxjs';
 import { PROFILE_LOCALVC } from 'app/app.constants';
-import { isEqual } from 'lodash-es';
 
 export interface DetailOverviewSection {
     headline: string;
@@ -51,14 +50,13 @@ export class DetailOverviewListComponent implements OnInit, OnDestroy {
     protected readonly FeatureToggle = FeatureToggle;
     protected readonly ButtonSize = ButtonSize;
     protected readonly ProgrammingExerciseParticipationType = ProgrammingExerciseParticipationType;
+    protected readonly CHAT = IrisSubSettingsType.CHAT;
 
-    readonly CHAT = IrisSubSettingsType.CHAT;
+    private readonly modelingExerciseService = inject(ModelingExerciseService);
+    private readonly alertService = inject(AlertService);
+    private readonly profileService = inject(ProfileService);
 
     sections = input.required<DetailOverviewSection[]>();
-
-    displayedSections = signal<DetailOverviewSection[]>([]);
-
-    sectionsBeforeChanges = signal<DetailOverviewSection[]>([]);
 
     // headline list for navigation bar
     headlines: { id: string; translationKey: string }[];
@@ -67,38 +65,6 @@ export class DetailOverviewListComponent implements OnInit, OnDestroy {
 
     profileSubscription: Subscription;
     isLocalVC = false;
-
-    constructor(
-        private modelingExerciseService: ModelingExerciseService,
-        private alertService: AlertService,
-        private profileService: ProfileService,
-    ) {
-        effect(
-            () => {
-                if (!isEqual(this.sections(), this.sectionsBeforeChanges())) {
-                    console.log('Sections have changed');
-                    this.sections().forEach((section, sectionIndex) => {
-                        const previousSection = this.sectionsBeforeChanges()[sectionIndex];
-                        if (!isEqual(section, previousSection)) {
-                            console.log(`Section ${sectionIndex} has changed:`);
-                            section.details.forEach((detail, detailIndex) => {
-                                const previousDetail = previousSection.details[detailIndex];
-                                if (!isEqual(detail, previousDetail)) {
-                                    console.log(`  Detail ${detailIndex} has changed:`);
-                                    console.log('    Current:', detail);
-                                    console.log('    Previous:', previousDetail);
-                                }
-                            });
-                        }
-                    });
-                    this.displayedSections.set(this.sections());
-                } else {
-                    console.log('Sections are the same');
-                }
-            },
-            { allowSignalWrites: true },
-        );
-    }
 
     ngOnInit() {
         this.headlines = this.sections().map((section) => {
@@ -113,9 +79,6 @@ export class DetailOverviewListComponent implements OnInit, OnDestroy {
         this.headlinesRecord = this.headlines.reduce((previousValue, currentValue) => {
             return { ...previousValue, [currentValue.translationKey]: currentValue.id };
         }, {});
-
-        this.sectionsBeforeChanges.set(this.sections());
-        this.displayedSections.set(this.sections());
     }
 
     downloadApollonDiagramAsPDf(umlModel?: UMLModel, title?: string) {
