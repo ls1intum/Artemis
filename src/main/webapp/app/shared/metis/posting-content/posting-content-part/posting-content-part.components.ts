fe673fb670dfdb2ca1ad0a0c3cbe031f4f69a6c0
Julian Gassner <julian.gassner@tum.de>
2024-11-21 04:05:31 +0100

    Implement ngOnChanges lifecycle hook in PostingContentPartComponent

--- src/main/webapp/app/shared/metis/posting-content/posting-content-part/posting-content-part.components.ts
+++ src/main/webapp/app/shared/metis/posting-content/posting-content-part/posting-content-part.components.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { PostingContentPart, ReferenceType } from '../../metis.util';
 import { FileService } from 'app/shared/http/file.service';
 
@@ -26,7 +26,7 @@ import { AccountService } from 'app/core/auth/account.service';
     templateUrl: './posting-content-part.component.html',
     styleUrls: ['./../../metis.component.scss'],
 })
-export class PostingContentPartComponent implements OnInit {
+export class PostingContentPartComponent implements OnInit, OnChanges {
     @Input() postingContentPart: PostingContentPart;
     @Output() userReferenceClicked = new EventEmitter<string>();
     @Output() channelReferenceClicked = new EventEmitter<number>();
@@ -55,6 +55,10 @@ export class PostingContentPartComponent implements OnInit {
     ) {}
 
     ngOnInit() {
+        this.processContent();
+    }
+
+    ngOnChanges() {
         this.processContent();
     }
 
