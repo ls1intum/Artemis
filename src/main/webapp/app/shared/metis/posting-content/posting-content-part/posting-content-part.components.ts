f6f4c8dd276ca8052fa318eb6b3de9c25f5eee31
Julian Gassner <julian.gassner@tum.de>
2024-11-21 04:35:10 +0100

    Remove OnInit lifecycle hook as it is not needed in this context

--- src/main/webapp/app/shared/metis/posting-content/posting-content-part/posting-content-part.components.ts
+++ src/main/webapp/app/shared/metis/posting-content/posting-content-part/posting-content-part.components.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { PostingContentPart, ReferenceType } from '../../metis.util';
 import { FileService } from 'app/shared/http/file.service';
 
@@ -26,7 +26,7 @@ import { AccountService } from 'app/core/auth/account.service';
     templateUrl: './posting-content-part.component.html',
     styleUrls: ['./../../metis.component.scss'],
 })
-export class PostingContentPartComponent implements OnInit, OnChanges {
+export class PostingContentPartComponent implements OnChanges {
     @Input() postingContentPart: PostingContentPart;
     @Output() userReferenceClicked = new EventEmitter<string>();
     @Output() channelReferenceClicked = new EventEmitter<number>();
@@ -54,11 +54,7 @@ export class PostingContentPartComponent implements OnInit, OnChanges {
         private accountService: AccountService,
     ) {}
 
-    ngOnInit() {
-        this.processContent();
-    }
-
-    ngOnChanges() {
+    ngOnChanges(): void {
         this.processContent();
     }
 
