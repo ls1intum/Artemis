7d4693edfdbc4216352b112647ef2d9bd03ad228
Ole Vester <ole.vester+gitlab@check24.de>
2024-11-26 13:58:54 +0100

    Add JavaDoc for AbstractSimpleRepositoryService

--- src/main/java/de/tum/cit/aet/artemis/core/repository/base/AbstractSimpleRepositoryService.java
+++ src/main/java/de/tum/cit/aet/artemis/core/repository/base/AbstractSimpleRepositoryService.java
@@ -4,6 +4,10 @@ import java.util.Optional;
 
 import de.tum.cit.aet.artemis.core.exception.EntityNotFoundException;
 
+/**
+ * Abstract base for simple repository services. These are services on repository
+ * layer which contain the default methods of the repository layer/data access.
+ */
 public abstract class AbstractSimpleRepositoryService {
 
     protected <U> U getValueElseThrow(Optional<U> optional, String entityName) {
