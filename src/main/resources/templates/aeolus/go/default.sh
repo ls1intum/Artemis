4f9e4b323d4184397c3c37dcbbf18eaa5d972b62
Marcel Gaupp <m.gaupp@tum.de>
2024-11-27 02:21:10 +0100

    Improve reporting of build failures

--- src/main/resources/templates/aeolus/go/default.sh
+++ src/main/resources/templates/aeolus/go/default.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
+export AEOLUS_INITIAL_DIRECTORY=${PWD}
+build () {
+  echo '⚙️ executing build'
+  cd "${testWorkingDirectory}"
+  go test -c -o /dev/null ./...
+
+}
 
 test () {
   echo '⚙️ executing test'
@@ -9,7 +16,15 @@ test () {
 }
 
 main () {
-  test
+  if [[ "${1}" == "aeolus_sourcing" ]]; then
+    return 0 # just source to use the methods in the subshell, no execution
+  fi
+  local _script_name
+  _script_name=${BASH_SOURCE[0]:-$0}
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; build"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; test"
 }
 
 main "${@}"
