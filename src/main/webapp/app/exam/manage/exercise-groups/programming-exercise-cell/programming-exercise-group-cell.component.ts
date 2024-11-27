159069ddeb4a8f1925eca16ab72f85c4563d6def
Michal Kawka <michal.kawka@gmx.com>
2024-11-27 23:45:07 +0100

    migrate programming-exercise-group-cell.component

--- src/main/webapp/app/exam/manage/exercise-groups/programming-exercise-cell/programming-exercise-group-cell.component.ts
+++ src/main/webapp/app/exam/manage/exercise-groups/programming-exercise-cell/programming-exercise-group-cell.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, inject, input } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { ProgrammingExercise } from 'app/entities/programming/programming-exercise.model';
 import { ProgrammingExerciseParticipationType } from 'app/entities/programming/programming-exercise-participation.model';
-import { Exercise } from 'app/entities/exercise.model';
 import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
 import { createBuildPlanUrl } from 'app/exercises/programming/shared/utils/programming-exercise.utils';
 import { ProgrammingExerciseInstructorRepositoryType, ProgrammingExerciseService } from 'app/exercises/programming/manage/services/programming-exercise.service';
@@ -17,53 +16,38 @@ import { PROFILE_LOCALVC, PROFILE_THEIA } from 'app/app.constants';
     styles: [':host{display: contents}'],
 })
 export class ProgrammingExerciseGroupCellComponent implements OnInit {
-    participationType = ProgrammingExerciseParticipationType;
+    private profileService = inject(ProfileService);
+    private programmingExerciseService = inject(ProgrammingExerciseService);
+    private alertService = inject(AlertService);
 
-    programmingExercise: ProgrammingExercise;
+    participationType = ProgrammingExerciseParticipationType;
 
     localVCEnabled = false;
     onlineIdeEnabled = false;
 
-    @Input()
-    displayShortName = false;
-    @Input()
-    displayRepositoryUri = false;
-    @Input()
-    displayTemplateUrls = false;
-    @Input()
-    displayEditorModus = false;
-
-    @Input()
-    set exercise(exercise: Exercise) {
-        this.programmingExercise = exercise as ProgrammingExercise;
-    }
+    displayShortName = input(false);
+    displayRepositoryUri = input(false);
+    displayTemplateUrls = input(false);
+    displayEditorModus = input(false);
+    programmingExercise = input.required<ProgrammingExercise>();
 
     faDownload = faDownload;
-
-    constructor(
-        private profileService: ProfileService,
-        private programmingExerciseService: ProgrammingExerciseService,
-        private alertService: AlertService,
-    ) {}
 
     ngOnInit(): void {
         this.profileService.getProfileInfo().subscribe((profileInfo) => {
             this.localVCEnabled = profileInfo.activeProfiles.includes(PROFILE_LOCALVC);
             this.onlineIdeEnabled = profileInfo.activeProfiles.includes(PROFILE_THEIA);
-            if (this.programmingExercise.projectKey) {
-                if (this.programmingExercise.solutionParticipation?.buildPlanId) {
-                    this.programmingExercise.solutionParticipation!.buildPlanUrl = createBuildPlanUrl(
-                        profileInfo.buildPlanURLTemplate,
-                        this.programmingExercise.projectKey,
-                        this.programmingExercise.solutionParticipation.buildPlanId,
-                    );
+
+            const projectKey = this.programmingExercise()?.projectKey;
+            if (projectKey) {
+                const solutionParticipation = this.programmingExercise()?.solutionParticipation;
+                if (solutionParticipation?.buildPlanId) {
+                    solutionParticipation.buildPlanUrl = createBuildPlanUrl(profileInfo.buildPlanURLTemplate, projectKey, solutionParticipation.buildPlanId);
                 }
-                if (this.programmingExercise.templateParticipation?.buildPlanId) {
-                    this.programmingExercise.templateParticipation!.buildPlanUrl = createBuildPlanUrl(
-                        profileInfo.buildPlanURLTemplate,
-                        this.programmingExercise.projectKey,
-                        this.programmingExercise.templateParticipation.buildPlanId,
-                    );
+
+                const templateParticipation = this.programmingExercise()?.templateParticipation;
+                if (templateParticipation?.buildPlanId) {
+                    templateParticipation.buildPlanUrl = createBuildPlanUrl(profileInfo.buildPlanURLTemplate, projectKey, templateParticipation.buildPlanId);
                 }
             }
         });
@@ -76,10 +60,11 @@ export class ProgrammingExerciseGroupCellComponent implements OnInit {
      *
      * @param repositoryType
      */
-    downloadRepository(repositoryType: ProgrammingExerciseInstructorRepositoryType) {
-        if (this.programmingExercise.id) {
+    downloadRepository(repositoryType: ProgrammingExerciseInstructorRepositoryType): void {
+        const programmingExerciseId = this.programmingExercise()?.id;
+        if (programmingExerciseId) {
             // Repository type cannot be 'AUXILIARY' as auxiliary repositories are currently not supported for the local VCS.
-            this.programmingExerciseService.exportInstructorRepository(this.programmingExercise.id, repositoryType, undefined).subscribe((response: HttpResponse<Blob>) => {
+            this.programmingExerciseService.exportInstructorRepository(programmingExerciseId, repositoryType, undefined).subscribe((response: HttpResponse<Blob>) => {
                 downloadZipFileFromResponse(response);
                 this.alertService.success('artemisApp.programmingExercise.export.successMessageRepos');
             });
