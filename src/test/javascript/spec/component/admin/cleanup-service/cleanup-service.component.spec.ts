f74abe9e14dd49ad166ecd92b2801b817f3d6b79
Michal Kawka <michal.kawka@gmx.com>
2024-11-27 12:19:10 +0100

    adjust cleanup-service.component.spec.ts

--- src/test/javascript/spec/component/admin/cleanup-service/cleanup-service.component.spec.ts
+++ src/test/javascript/spec/component/admin/cleanup-service/cleanup-service.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of, throwError } from 'rxjs';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 import dayjs from 'dayjs/esm';
 
 import { ArtemisTestModule } from '../../../test.module';
@@ -17,24 +17,16 @@ describe('CleanupServiceComponent', () => {
     beforeEach(() => {
         const mockCleanupService = {
             getLastExecutions: jest.fn(),
-            deleteOrphans: jest.fn(),
-            deletePlagiarismComparisons: jest.fn(),
-            deleteNonRatedResults: jest.fn(),
-            deleteOldRatedResults: jest.fn(),
-            deleteOldSubmissionVersions: jest.fn(),
-            deleteOldFeedback: jest.fn(),
         };
 
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule, CleanupServiceComponent],
             providers: [{ provide: DataCleanupService, useValue: mockCleanupService }],
-        })
-            .compileComponents()
-            .then(() => {
-                fixture = TestBed.createComponent(CleanupServiceComponent);
-                comp = fixture.componentInstance;
-                cleanupService = TestBed.inject(DataCleanupService);
-            });
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(CleanupServiceComponent);
+        comp = fixture.componentInstance;
+        cleanupService = TestBed.inject(DataCleanupService);
     });
 
     it('should load last executions on init', () => {
@@ -49,42 +41,6 @@ describe('CleanupServiceComponent', () => {
 
         expect(cleanupService.getLastExecutions).toHaveBeenCalledOnce();
         expect(comp.cleanupOperations[0].lastExecuted).toEqual(dayjs(executionRecord[0].executionDate));
-    });
-
-    it('should execute a cleanup operation successfully', () => {
-        const operation = comp.cleanupOperations[0];
-        const response = new HttpResponse<CleanupServiceExecutionRecordDTO>({
-            body: { executionDate: dayjs(), jobType: 'deleteOrphans' },
-        });
-
-        jest.spyOn(cleanupService, 'deleteOrphans').mockReturnValue(of(response));
-
-        comp.executeCleanupOperation(operation);
-
-        expect(cleanupService.deleteOrphans).toHaveBeenCalledOnce();
-        expect(operation.lastExecuted).toEqual(dayjs(response.body!.executionDate));
-    });
-
-    it('should handle error when executing cleanup operation', () => {
-        const operation = comp.cleanupOperations[0];
-        const errorResponse = new HttpErrorResponse({
-            status: 500,
-            statusText: 'Internal Server Error',
-            error: 'Some error message',
-            url: 'https://artemis.ase.in.tum.de/api/admin/orphans', // Adding a mock URL
-        });
-
-        jest.spyOn(cleanupService, 'deleteOrphans').mockReturnValue(throwError(() => errorResponse));
-
-        let errorMessage: string | undefined;
-        comp.dialogError.subscribe((error) => {
-            errorMessage = error;
-        });
-
-        comp.executeCleanupOperation(operation);
-
-        expect(cleanupService.deleteOrphans).toHaveBeenCalledOnce();
-        expect(errorMessage).toBe('Http failure response for https://artemis.ase.in.tum.de/api/admin/orphans: 500 Internal Server Error');
     });
 
     it('should validate date ranges correctly', () => {
