cad2c19e9f6f408f816f88c65f2fb81a71ef3e25
Hassan Matout <has.matout@gmail.com>
2024-11-28 16:31:43 +0100

    Development: Migrate plagiarism-case-instructor-view.component.ts dependency injection from constructor to inject

--- src/main/webapp/app/course/plagiarism-cases/instructor-view/plagiarism-cases-instructor-view.component.ts
+++ src/main/webapp/app/course/plagiarism-cases/instructor-view/plagiarism-cases-instructor-view.component.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PlagiarismCasesService } from 'app/course/plagiarism-cases/shared/plagiarism-cases.service';
 import { PlagiarismCase } from 'app/exercises/shared/plagiarism/types/PlagiarismCase';
@@ -28,6 +28,9 @@ import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
     ],
 })
 export class PlagiarismCasesInstructorViewComponent implements OnInit {
+    private plagiarismCasesService = inject(PlagiarismCasesService);
+    private route = inject(ActivatedRoute);
+
     courseId: number;
     examId?: number;
     plagiarismCases: PlagiarismCase[] = [];
@@ -36,11 +39,6 @@ export class PlagiarismCasesInstructorViewComponent implements OnInit {
 
     readonly getIcon = getIcon;
     readonly documentationType: DocumentationType = 'PlagiarismChecks';
-
-    constructor(
-        private plagiarismCasesService: PlagiarismCasesService,
-        private route: ActivatedRoute,
-    ) {}
 
     ngOnInit(): void {
         this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
