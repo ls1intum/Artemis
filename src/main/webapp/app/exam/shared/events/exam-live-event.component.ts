8fd11f3f358a58baabebe4791d3702bd20c6dc0e
Michal Kawka <michal.kawka@gmx.com>
2024-11-30 00:58:43 +0100

    migrate exam-live-event.component

--- src/main/webapp/app/exam/shared/events/exam-live-event.component.ts
+++ src/main/webapp/app/exam/shared/events/exam-live-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 import { faCheck, faEye, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import {
     ExamAttendanceCheckEvent,
@@ -8,18 +8,24 @@ import {
     ProblemStatementUpdateEvent,
     WorkingTimeUpdateEvent,
 } from 'app/exam/participate/exam-participation-live-events.service';
+import { NgClass } from '@angular/common';
+import { TranslateDirective } from '../../../shared/language/translate.directive';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { WorkingTimeChangeComponent } from '../working-time-change/working-time-change.component';
+import { ArtemisSharedCommonModule } from '../../../shared/shared-common.module';
+import { ArtemisMarkdownModule } from '../../../shared/markdown.module';
 
 @Component({
     selector: 'jhi-exam-live-event',
     templateUrl: './exam-live-event.component.html',
     styleUrls: ['./exam-live-event.component.scss'],
+    standalone: true,
+    imports: [NgClass, TranslateDirective, FaIconComponent, WorkingTimeChangeComponent, ArtemisSharedCommonModule, ArtemisMarkdownModule],
 })
 export class ExamLiveEventComponent {
-    @Input()
-    event: ExamLiveEvent;
+    event = input.required<ExamLiveEvent>();
 
-    @Input()
-    showAcknowledge: boolean = false;
+    showAcknowledge = input<boolean>(false);
 
     @Output()
     onAcknowledge = new EventEmitter<ExamLiveEvent>();
@@ -35,26 +41,26 @@ export class ExamLiveEventComponent {
     faEye = faEye;
 
     get examWideAnnouncementEvent(): ExamWideAnnouncementEvent {
-        return this.event as ExamWideAnnouncementEvent;
+        return this.event() as ExamWideAnnouncementEvent;
     }
 
     get examAttendanceCheckEvent(): ExamAttendanceCheckEvent {
-        return this.event as ExamAttendanceCheckEvent;
+        return this.event() as ExamAttendanceCheckEvent;
     }
 
     get workingTimeUpdateEvent(): WorkingTimeUpdateEvent {
-        return this.event as WorkingTimeUpdateEvent;
+        return this.event() as WorkingTimeUpdateEvent;
     }
 
     get problemStatementUpdateEvent(): ProblemStatementUpdateEvent {
-        return this.event as ProblemStatementUpdateEvent;
+        return this.event() as ProblemStatementUpdateEvent;
     }
 
     acknowledgeEvent() {
-        this.onAcknowledge.emit(this.event);
+        this.onAcknowledge.emit(this.event());
     }
 
     navigateToExercise() {
-        this.onNavigate.emit(this.event);
+        this.onNavigate.emit(this.event());
     }
 }
--- src/test/javascript/spec/component/exam/shared/events/exam-live-event.component.spec.ts
+++ src/test/javascript/spec/component/exam/shared/events/exam-live-event.component.spec.ts
@@ -9,12 +9,8 @@ import {
     WorkingTimeUpdateEvent,
 } from 'app/exam/participate/exam-participation-live-events.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
-import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
 import { MockTranslateService } from '../../../../helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
-import { HtmlForMarkdownPipe } from 'app/shared/pipes/html-for-markdown.pipe';
-import { ArtemisDurationFromSecondsPipe } from 'app/shared/pipes/artemis-duration-from-seconds.pipe';
 import { WorkingTimeChangeComponent } from 'app/exam/shared/working-time-change/working-time-change.component';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { MockDirective } from 'ng-mocks';
@@ -25,15 +21,7 @@ describe('ExamLiveEventComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [
-                ExamLiveEventComponent,
-                WorkingTimeChangeComponent,
-                ArtemisTranslatePipe,
-                ArtemisDatePipe,
-                HtmlForMarkdownPipe,
-                ArtemisDurationFromSecondsPipe,
-                MockDirective(TranslateDirective),
-            ],
+            declarations: [ExamLiveEventComponent, WorkingTimeChangeComponent, MockDirective(TranslateDirective)],
             imports: [FontAwesomeModule],
             providers: [{ provide: TranslateService, useClass: MockTranslateService }],
         }).compileComponents();
@@ -43,10 +31,11 @@ describe('ExamLiveEventComponent', () => {
     });
 
     it('should display the correct event type and author', () => {
-        component.event = {
+        const event = {
             eventType: ExamLiveEventType.EXAM_WIDE_ANNOUNCEMENT,
             createdBy: 'John Doe',
         } as ExamLiveEvent;
+        fixture.componentRef.setInput('event', event);
 
         fixture.detectChanges();
 
@@ -58,9 +47,10 @@ describe('ExamLiveEventComponent', () => {
     });
 
     it('should display the attendance check event', () => {
-        component.event = {
+        const event = {
             eventType: ExamLiveEventType.EXAM_ATTENDANCE_CHECK,
         } as ExamLiveEvent;
+        fixture.componentRef.setInput('event', event);
 
         fixture.detectChanges();
 
@@ -69,10 +59,11 @@ describe('ExamLiveEventComponent', () => {
     });
 
     it('should display exam-wide announcement text when event is of type EXAM_WIDE_ANNOUNCEMENT', () => {
-        component.event = {
+        const event = {
             eventType: ExamLiveEventType.EXAM_WIDE_ANNOUNCEMENT,
             text: 'This is an announcement',
         } as ExamWideAnnouncementEvent;
+        fixture.componentRef.setInput('event', event);
 
         fixture.detectChanges();
 
@@ -81,12 +72,13 @@ describe('ExamLiveEventComponent', () => {
     });
 
     it('should display working time update when event is of type WORKING_TIME_UPDATE', () => {
-        component.event = {
+        const event = {
             eventType: ExamLiveEventType.WORKING_TIME_UPDATE,
             oldWorkingTime: 300,
             newWorkingTime: 600,
             courseWide: true,
         } as WorkingTimeUpdateEvent;
+        fixture.componentRef.setInput('event', event);
 
         fixture.detectChanges();
 
@@ -100,7 +92,7 @@ describe('ExamLiveEventComponent', () => {
     });
 
     it('should display problem statement update when event is of type PROBLEM_STATEMENT_UPDATE', () => {
-        component.event = {
+        const event = {
             eventType: ExamLiveEventType.PROBLEM_STATEMENT_UPDATE,
             text: 'Dear students, the problem statement of the exercise was changed',
             problemStatement: 'New problem statement',
@@ -108,6 +100,7 @@ describe('ExamLiveEventComponent', () => {
             exerciseName: 'Programming Exercise',
             createdBy: 'John Doe',
         } as ProblemStatementUpdateEvent;
+        fixture.componentRef.setInput('event', event);
 
         fixture.detectChanges();
 
@@ -126,8 +119,8 @@ describe('ExamLiveEventComponent', () => {
             eventType: ExamLiveEventType.EXAM_WIDE_ANNOUNCEMENT,
             createdBy: 'John Doe',
         } as any as ExamLiveEvent;
-        component.event = mockEvent;
-        component.showAcknowledge = true;
+        fixture.componentRef.setInput('event', mockEvent);
+        fixture.componentRef.setInput('showAcknowledge', true);
 
         fixture.detectChanges();
 
@@ -143,8 +136,8 @@ describe('ExamLiveEventComponent', () => {
             eventType: ExamLiveEventType.PROBLEM_STATEMENT_UPDATE,
             createdBy: 'John Doe',
         } as any as ExamLiveEvent;
-        component.event = mockEvent;
-        component.showAcknowledge = true;
+        fixture.componentRef.setInput('event', mockEvent);
+        fixture.componentRef.setInput('showAcknowledge', true);
 
         fixture.detectChanges();
 
