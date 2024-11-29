29249dfbbc595355a696efca3d466c8f2471e00a
raphaelklein <raphael.klein@tum.de>
2024-11-29 18:06:29 +0100

    inject dynamic reg comp

--- src/main/webapp/app/lti/lti13-dynamic-registration.component.ts
+++ src/main/webapp/app/lti/lti13-dynamic-registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { TranslateDirective } from '../shared/language/translate.directive';
@@ -10,14 +10,12 @@ import { TranslateDirective } from '../shared/language/translate.directive';
     imports: [TranslateDirective],
 })
 export class Lti13DynamicRegistrationComponent implements OnInit {
+    private route = inject(ActivatedRoute);
+    private http = inject(HttpClient);
+
     courseId: number;
     isRegistering = true;
     registeredSuccessfully: boolean;
-
-    constructor(
-        private route: ActivatedRoute,
-        private http: HttpClient,
-    ) {}
 
     /**
      * perform LTI 13 dynamic registration
