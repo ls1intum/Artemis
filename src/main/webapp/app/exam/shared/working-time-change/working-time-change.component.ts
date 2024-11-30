d64dc479e8bb2c6963295d2d2b02029d8032d439
Michal Kawka <michal.kawka@gmx.com>
2024-11-30 01:01:44 +0100

    migrate working-time-change.component

--- src/main/webapp/app/exam/shared/working-time-change/working-time-change.component.ts
+++ src/main/webapp/app/exam/shared/working-time-change/working-time-change.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
+import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
 
 @Component({
     selector: 'jhi-working-time-change',
     templateUrl: './working-time-change.component.html',
+    standalone: true,
+    imports: [TranslateDirective, ArtemisSharedCommonModule],
 })
 export class WorkingTimeChangeComponent {
-    @Input() oldWorkingTime: number;
-    @Input() newWorkingTime: number;
+    oldWorkingTime = input.required<number>();
+    newWorkingTime = input.required<number>();
 }
