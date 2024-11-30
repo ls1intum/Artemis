590bb26f328e6d132d286c4fbd3d82f3405fea61
Michal Kawka <michal.kawka@gmx.com>
2024-11-30 16:05:41 +0100

    migrate testexam-working-time.component.ts

--- src/main/webapp/app/exam/shared/testExam-workingTime/testexam-working-time.component.ts
+++ src/main/webapp/app/exam/shared/testExam-workingTime/testexam-working-time.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { round } from 'app/shared/util/utils';
 import dayjs from 'dayjs/esm';
+import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
 
 @Component({
     selector: 'jhi-testexam-working-time',
     templateUrl: './testexam-working-time.component.html',
     providers: [],
+    standalone: true,
+    imports: [ArtemisSharedCommonModule],
 })
 export class TestexamWorkingTimeComponent implements OnInit {
-    @Input() studentExam: StudentExam;
+    studentExam = input.required<StudentExam>();
 
     percentUsedWorkingTime = 0;
     usedWorkingTime = 0;
@@ -20,16 +23,16 @@ export class TestexamWorkingTimeComponent implements OnInit {
      */
     ngOnInit() {
         if (
-            this.studentExam.exam!.testExam &&
-            this.studentExam.started &&
-            this.studentExam.submitted &&
-            this.studentExam.workingTime &&
-            this.studentExam.startedDate &&
-            this.studentExam.submissionDate
+            this.studentExam().exam!.testExam &&
+            this.studentExam().started &&
+            this.studentExam().submitted &&
+            this.studentExam().workingTime &&
+            this.studentExam().startedDate &&
+            this.studentExam().submissionDate
         ) {
-            const regularExamDuration = this.studentExam.workingTime;
+            const regularExamDuration = this.studentExam().workingTime!;
             // As students may submit during the grace period, the workingTime is limited to the regular exam duration
-            this.usedWorkingTime = Math.min(regularExamDuration, dayjs(this.studentExam.submissionDate).diff(dayjs(this.studentExam.startedDate), 'seconds'));
+            this.usedWorkingTime = Math.min(regularExamDuration, dayjs(this.studentExam().submissionDate).diff(dayjs(this.studentExam().startedDate), 'seconds'));
             // As students may submit during the grace period, the percentage is limited to 100%
             this.percentUsedWorkingTime = Math.min(100, round((this.usedWorkingTime / regularExamDuration) * 100, 2));
         }
