23d7a1043ef43d1715d4374eefe35b03b7ab147a
Michal Kawka <michal.kawka@gmx.com>
2024-11-30 16:07:18 +0100

    migrate student-exam-working-time.component.ts

--- src/main/webapp/app/exam/shared/student-exam-working-time/student-exam-working-time.component.ts
+++ src/main/webapp/app/exam/shared/student-exam-working-time/student-exam-working-time.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { StudentExam } from 'app/entities/student-exam.model';
 import { getRelativeWorkingTimeExtension } from 'app/exam/participate/exam.utils';
+import { ArtemisSharedCommonModule } from 'app/shared/shared-common.module';
 
 @Component({
     selector: 'jhi-student-exam-working-time',
     templateUrl: './student-exam-working-time.component.html',
     providers: [],
+    standalone: true,
+    imports: [ArtemisSharedCommonModule],
 })
 export class StudentExamWorkingTimeComponent implements OnInit {
-    @Input() studentExam: StudentExam;
+    studentExam = input.required<StudentExam>();
 
     percentDifference = 0;
     isTestRun = false;
 
     ngOnInit() {
-        if (this.studentExam.exam && this.studentExam.workingTime) {
-            this.percentDifference = getRelativeWorkingTimeExtension(this.studentExam.exam, this.studentExam.workingTime);
+        if (this.studentExam().exam && this.studentExam().workingTime) {
+            this.percentDifference = getRelativeWorkingTimeExtension(this.studentExam().exam!, this.studentExam().workingTime!);
         }
-        this.isTestRun = this.studentExam.testRun ?? false;
+        this.isTestRun = this.studentExam().testRun ?? false;
     }
 }
