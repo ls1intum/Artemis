663684f90af5daa3089da8be97a5f021f7304338
Paul Rangger <48455539+PaRangger@users.noreply.github.com>
2024-12-03 21:44:37 +0100

    Communication: Improve push notifications for the iOS mobile app (#9787)

--- src/main/java/de/tum/cit/aet/artemis/core/config/Constants.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/Constants.java
@@ -379,6 +379,11 @@ public final class Constants {
     public static final int PUSH_NOTIFICATION_VERSION = 1;
 
     /**
+     * The value of the version field we send with each push notification to the native clients (Android & iOS).
+     */
+    public static final int PUSH_NOTIFICATION_MINOR_VERSION = 2;
+
+    /**
      * The directory in the docker container in which the build script is executed
      */
     public static final String LOCALCI_WORKING_DIRECTORY = "/var/tmp";
