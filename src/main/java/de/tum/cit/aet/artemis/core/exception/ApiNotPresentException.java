9e0a4d9da5a6766ed174ebcae2ef187852859995
Ole Vester <ole.vester+gitlab@check24.de>
2024-12-03 12:31:40 +0100

    Add JavaDoc for ApiNotPresentException

--- src/main/java/de/tum/cit/aet/artemis/core/exception/ApiNotPresentException.java
+++ src/main/java/de/tum/cit/aet/artemis/core/exception/ApiNotPresentException.java
@@ -1,7 +1,17 @@
 package de.tum.cit.aet.artemis.core.exception;
 
+/**
+ * Exception that an optionally autowired API is not present.
+ * This is caused by Spring profiles not being present.
+ */
 public class ApiNotPresentException extends RuntimeException {
 
+    /**
+     * Constructor.
+     *
+     * @param api     name of the api class
+     * @param profile name of the Spring profile that needs to be enabled.
+     */
     public ApiNotPresentException(String api, String profile) {
         super(String.format("Api %s is not enabled, because Spring profile %s is not enabled. Did you enable it?", api, profile));
     }
