f82a9b33e0e21e6c82c9d3b7ed19d3f61f441d67
Mohamed Bilel Besrour <besrourbilel@gmail.com>
2024-12-04 22:31:09 +0100

    logging

--- src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobExecutionService.java
+++ src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobExecutionService.java
@@ -300,7 +300,7 @@ public class BuildJobExecutionService {
 
         ZonedDateTime buildCompletedDate = ZonedDateTime.now();
 
-        msg = "~~~~~~~~~~~~~~~~~~~~ Moving test results to specified directory ~~~~~~~~~~~~~~~~~~~~";
+        msg = "~~~~~~~~~~~~~~~~~~~~ Moving test results to specified directory for build job " + buildJob.id() + " ~~~~~~~~~~~~~~~~~~~~";
         buildLogsMap.appendBuildLogEntry(buildJob.id(), msg);
         log.debug(msg);
 
@@ -308,7 +308,7 @@ public class BuildJobExecutionService {
 
         // Get an input stream of the test result files.
 
-        msg = "~~~~~~~~~~~~~~~~~~~~ Collecting test results from container ~~~~~~~~~~~~~~~~~~~~";
+        msg = "~~~~~~~~~~~~~~~~~~~~ Collecting test results from container " + containerName + " for build job " + buildJob.id() + " ~~~~~~~~~~~~~~~~~~~~";
         buildLogsMap.appendBuildLogEntry(buildJob.id(), msg);
         log.info(msg);
 
@@ -349,6 +349,10 @@ public class BuildJobExecutionService {
             }
         }
 
+        msg = "~~~~~~~~~~~~~~~~~~~~ Parsing test results for build job " + buildJob.id() + " ~~~~~~~~~~~~~~~~~~~~";
+        buildLogsMap.appendBuildLogEntry(buildJob.id(), msg);
+        log.info(msg);
+
         BuildResult buildResult;
         try {
             buildResult = parseTestResults(testResultsTarInputStream, buildJob.buildConfig().branch(), assignmentRepoCommitHash, testRepoCommitHash, buildCompletedDate,
@@ -362,7 +366,7 @@ public class BuildJobExecutionService {
         }
 
         msg = "Building and testing submission for repository " + assignmentRepositoryUri.repositorySlug() + " and commit hash " + assignmentRepoCommitHash + " took "
-                + TimeLogUtil.formatDurationFrom(timeNanoStart);
+                + TimeLogUtil.formatDurationFrom(timeNanoStart) + " for build job " + buildJob.id();
         buildLogsMap.appendBuildLogEntry(buildJob.id(), msg);
         log.info(msg);
 
