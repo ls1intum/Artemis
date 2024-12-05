56351391d3c38dc71ca09935d6015739de2afdeb
Murad Talibov <muradtalibov2@gmail.com>
2024-12-05 21:59:30 +0100

    Accept the connection to ssh host and add it to known_hosts automatically

--- src/test/playwright/support/pageobjects/exercises/programming/GitClient.ts
+++ src/test/playwright/support/pageobjects/exercises/programming/GitClient.ts
@@ -5,6 +5,8 @@ class GitClient {
     async cloneRepo(url: string, repoName: string) {
         const git = simpleGit();
         const repoPath = `./${process.env.EXERCISE_REPO_DIRECTORY}/${repoName}`;
+        // Disable host key checking to avoid interactive prompt. Adds server to known_hosts.
+        git.env({ GIT_SSH_COMMAND: 'ssh -o StrictHostKeyChecking=no' });
 
         if (!fs.existsSync(repoPath)) {
             fs.mkdirSync(repoPath, { recursive: true });
