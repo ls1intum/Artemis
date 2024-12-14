2861df75ae9c561e98030508669f93716a240d39
Florian Glombik <florian.glombik@tum.de>
2024-12-14 22:23:01 +0100

    Add another test not requiring opening the modal

--- src/test/javascript/spec/component/lecture/hasLectureUnsavedChanges.guard.spec.ts
+++ src/test/javascript/spec/component/lecture/hasLectureUnsavedChanges.guard.spec.ts
@@ -1,6 +1,10 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { hasLectureUnsavedChangesGuard } from '../../../../../main/webapp/app/lecture/hasLectureUnsavedChanges.guard';
 import { LectureUpdateComponent } from '../../../../../main/webapp/app/lecture/lecture-update.component';
+import { TestBed } from '@angular/core/testing';
+import { MockRouter } from '../../helpers/mocks/mock-router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MockNgbModalService } from '../../helpers/mocks/service/mock-ngb-modal.service';
 
 describe('hasLectureUnsavedChanges', () => {
     let component: LectureUpdateComponent;
@@ -9,16 +13,37 @@ describe('hasLectureUnsavedChanges', () => {
     let nextState: RouterStateSnapshot;
 
     beforeEach(() => {
-        component = {
-            shouldDisplayDismissWarning: true,
-            isShowingWizardMode: false,
-            isChangeMadeToTitleSection: jest.fn(),
-            isChangeMadeToPeriodSection: jest.fn(),
-        } as unknown as LectureUpdateComponent;
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [LectureUpdateComponent],
+            providers: [
+                { provide: Router, useClass: MockRouter },
+                { provide: NgbModal, useClass: MockNgbModalService },
+                {
+                    provide: LectureUpdateComponent,
+                    useValue: {
+                        shouldDisplayDismissWarning: true,
+                        isShowingWizardMode: false,
+                        isChangeMadeToTitleSection: jest.fn().mockReturnValue(true),
+                        isChangeMadeToPeriodSection: jest.fn().mockReturnValue(true),
+                        isChangeMadeToTitleOrPeriodSection: true,
+                    },
+                },
+            ],
+        }).compileComponents();
 
+        component = TestBed.inject(LectureUpdateComponent);
         currentRoute = {} as ActivatedRouteSnapshot;
         currentState = {} as RouterStateSnapshot;
         nextState = {} as RouterStateSnapshot;
+    });
+
+    it('should return true if warning is not bypassed by shouldDisplayDismissWarning variable but no changes were made', async () => {
+        component.shouldDisplayDismissWarning = true;
+        component.isChangeMadeToTitleOrPeriodSection = false;
+
+        const result = await hasLectureUnsavedChangesGuard(component, currentRoute, currentState, nextState).toPromise();
+        expect(result).toBeTrue();
     });
 
     it('should return true if dismiss warning shall not be displayed', async () => {
