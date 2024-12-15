2ec985365ea808938f03ffbbdbd1a398fcca24c6
Murad Talibov <muradtalibov2@gmail.com>
2024-12-15 01:09:22 +0100

    Revert "Type token separately without including in URL"
    This reverts commit cae951de4388383a72939d2e1ade344911e3301a.

--- src/test/playwright/support/pageobjects/exercises/programming/GitClient.ts
+++ src/test/playwright/support/pageobjects/exercises/programming/GitClient.ts
@@ -2,14 +2,11 @@ import { simpleGit } from 'simple-git';
 import * as fs from 'fs';
 
 class GitClient {
-    async cloneRepo(url: string, repoName: string, token?: string) {
+    async cloneRepo(url: string, repoName: string) {
         const git = simpleGit();
         const repoPath = `./${process.env.EXERCISE_REPO_DIRECTORY}/${repoName}`;
         // Disable host key checking to avoid interactive prompt. Adds server to known_hosts.
         git.env({ GIT_SSH_COMMAND: 'ssh -o StrictHostKeyChecking=no' });
-        if (token !== undefined) {
-            git.env({ GIT_ASKPASS: 'echo', GIT_PASSWORD: token });
-        }
 
         if (!fs.existsSync(repoPath)) {
             fs.mkdirSync(repoPath, { recursive: true });
