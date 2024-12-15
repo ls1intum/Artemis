91c235957a28821b299b0652d812834e1003e220
Murad Talibov <muradtalibov2@gmail.com>
2024-12-15 01:09:20 +0100

    Revert "Try replacing stars from URL"
    This reverts commit 2a054ea0b6c55a6dde901667c04ad5004962da70.

--- src/test/playwright/e2e/exercise/programming/ProgrammingExerciseParticipation.spec.ts
+++ src/test/playwright/e2e/exercise/programming/ProgrammingExerciseParticipation.spec.ts
@@ -293,8 +293,6 @@ async function makeGitExerciseSubmission(
     }
     if (cloneMethod == GitCloneMethod.https) {
         repoUrl = repoUrl.replace(student.username!, `${student.username!}:${student.password!}`);
-    }
-    if (cloneMethod == GitCloneMethod.https || cloneMethod == GitCloneMethod.httpsWithToken) {
         repoUrl = repoUrl.replace(`:**********`, ``);
     }
     console.log(`Cloning repository from ${repoUrl}`);
