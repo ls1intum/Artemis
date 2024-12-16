1cad26f79128b9a0b3d41fc1e13bbe08c18b4b46
Ole Vester <ole.vester+gitlab@check24.de>
2024-12-16 22:31:08 +0100

    add documentation to get_changed_modules.sh

--- supporting_scripts/get_changed_modules.sh
+++ supporting_scripts/get_changed_modules.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Determines the changed modules following the module-directory structure.
+# Based on git-diff between the local state and an input branch name.
+# Example: "./get_changed_modules.sh develop"
+
+# Check for the branch input argument.
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <branch_to_compare>"
+    exit 1
+fi
+
 BRANCH_TO_COMPARE="$1"
+if ! git show-ref --verify --quiet "refs/heads/$BRANCH_TO_COMPARE"; then
+    echo "Branch $BRANCH_TO_COMPARE does not exist."
+    exit 1
+fi
 
 MODULE_BASE_PATH="src/main/java/de/tum/cit/aet/artemis"
 MODULES=$(find "$MODULE_BASE_PATH" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
