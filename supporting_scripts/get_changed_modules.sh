631e229323cd9d71e4f9e78ebb612778963522b9
Ole Vester <ole.vester+gitlab@check24.de>
2024-12-17 20:58:55 +0100

    include tests into determination of changed modules

--- supporting_scripts/get_changed_modules.sh
+++ supporting_scripts/get_changed_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Determines the changed modules following the module-directory structure.
+# Determines the changed modules following the module-directory structure for both main/test.
 # Based on git-diff between the local state and an input branch name.
 # Returns a comma-separated list of changed modules.
 # Example: "./get_changed_modules.sh develop"
@@ -14,11 +14,15 @@ fi
 BRANCH_TO_COMPARE="$1"
 
 MODULE_BASE_PATH="src/main/java/de/tum/cit/aet/artemis"
+MODULE_BASE_TEST_PATH="src/test/java/de/tum/cit/aet/artemis"
+
 MODULES=$(find "$MODULE_BASE_PATH" -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 CHANGED_MODULES=()
 
 for MODULE in $MODULES; do
     if git diff "$BRANCH_TO_COMPARE" --name-only | grep -q "^$MODULE_BASE_PATH/$MODULE/"; then
+        CHANGED_MODULES+=("$MODULE")
+    elif git diff "$BRANCH_TO_COMPARE" --name-only | grep -q "^$MODULE_BASE_TEST_PATH/$MODULE/"; then
         CHANGED_MODULES+=("$MODULE")
     fi
 done
