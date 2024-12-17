b29036c1769d9554de81bac023196f69a00c39be
Michal Kawka <michal.kawka@gmx.com>
2024-12-17 19:22:57 +0100

    add annotation to DTOs

--- src/main/java/de/tum/cit/aet/artemis/core/dto/OrphanCleanupCountDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/OrphanCleanupCountDTO.java
@@ -1,5 +1,8 @@
 package de.tum.cit.aet.artemis.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record OrphanCleanupCountDTO(int orphanFeedback, int orphanLongFeedbackText, int orphanTextBlock, int orphanStudentScore, int orphanTeamScore,
         int orphanFeedbackForOrphanResults, int orphanLongFeedbackTextForOrphanResults, int orphanTextBlockForOrphanResults, int orphanRating,
         int orphanResultsWithoutParticipation) {
