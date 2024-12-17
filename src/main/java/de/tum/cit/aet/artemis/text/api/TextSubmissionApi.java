8264208370a50610ad15fe14ddefe16dc0d63caa
Ole Vester <ole.vester+gitlab@check24.de>
2024-12-17 15:31:11 +0100

    use TextSubmissionApi in ParticipationTeamWebsocketService

--- src/main/java/de/tum/cit/aet/artemis/text/api/TextSubmissionApi.java
+++ src/main/java/de/tum/cit/aet/artemis/text/api/TextSubmissionApi.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 
 import de.tum.cit.aet.artemis.assessment.domain.GradingInstruction;
+import de.tum.cit.aet.artemis.core.domain.User;
+import de.tum.cit.aet.artemis.text.domain.TextExercise;
 import de.tum.cit.aet.artemis.text.domain.TextSubmission;
 import de.tum.cit.aet.artemis.text.repository.TextSubmissionRepository;
 import de.tum.cit.aet.artemis.text.service.TextExerciseImportService;
+import de.tum.cit.aet.artemis.text.service.TextSubmissionService;
 
 @Controller
 @Profile(PROFILE_CORE)
@@ -21,9 +24,12 @@ public class TextSubmissionApi extends AbstractTextApi {
 
     private final TextExerciseImportService textExerciseImportService;
 
-    public TextSubmissionApi(TextSubmissionRepository textSubmissionRepository, TextExerciseImportService textExerciseImportService) {
+    private final TextSubmissionService textSubmissionService;
+
+    public TextSubmissionApi(TextSubmissionRepository textSubmissionRepository, TextExerciseImportService textExerciseImportService, TextSubmissionService textSubmissionService) {
         this.textSubmissionRepository = textSubmissionRepository;
         this.textExerciseImportService = textExerciseImportService;
+        this.textSubmissionService = textSubmissionService;
     }
 
     public TextSubmission importStudentSubmission(long submissionId, long exerciseId, Map<Long, GradingInstruction> gradingInstructionCopyTracker) {
@@ -37,6 +43,12 @@ public class TextSubmissionApi extends AbstractTextApi {
 
     public TextSubmission saveTextSubmission(TextSubmission textSubmission) {
         return textSubmissionRepository.save(textSubmission);
+    }
+
+    public TextSubmission handleTextSubmission(TextSubmission textSubmission, TextExercise exercise, User user) {
+        var submission = textSubmissionService.handleTextSubmission(textSubmission, exercise, user);
+        textSubmissionService.hideDetails(submission, user);
+        return submission;
     }
 
     private void checkGivenExerciseIdSameForSubmissionParticipation(long originalExerciseId, long exerciseIdInSubmission) {
