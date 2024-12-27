d91a2c6b34853933cf001fe4398cdc67be20882d
Stephan Krusche <krusche@tum.de>
2024-12-27 14:24:40 +0100

    exclude unnecessary Java server dependencies

--- src/main/java/de/tum/cit/aet/artemis/core/service/ZipFileService.java
+++ src/main/java/de/tum/cit/aet/artemis/core/service/ZipFileService.java
@@ -2,26 +2,27 @@ package de.tum.cit.aet.artemis.core.service;
 
 import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import jakarta.annotation.Nullable;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
-import net.lingala.zip4j.ZipFile;
 
 /**
  * A service class to create zip files
@@ -52,16 +53,38 @@ public class ZipFileService {
      * @throws IOException if an error occurred while zipping
      */
     public void createZipFile(Path zipFilePath, List<Path> paths) throws IOException {
-        try (ZipFile zipFile = new ZipFile(zipFilePath.toFile())) {
+        try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFilePath.toFile()))) {
             for (var path : paths) {
-                if (Files.isReadable(path) && !Files.isDirectory(path)) {
-                    zipFile.addFile(path.toFile());
-                }
-                else if (Files.isReadable(path) && Files.isDirectory(path)) {
-                    zipFile.addFolder(path.toFile());
+                if (Files.isReadable(path)) {
+                    if (Files.isDirectory(path)) {
+                        Files.walk(path).filter(p -> !Files.isDirectory(p)).forEach(p -> {
+                            try {
+                                addFileToZip(zipOut, p, path);
+                            }
+                            catch (IOException e) {
+                                throw new RuntimeException("Error adding file to ZIP", e);
+                            }
+                        });
+                    }
+                    else {
+                        addFileToZip(zipOut, path, path.getParent());
+                    }
                 }
             }
         }
+    }
+
+    private void addFileToZip(ZipOutputStream zipOut, Path file, Path basePath) throws IOException {
+        String zipEntryName = basePath.relativize(file).toString();
+        zipOut.putNextEntry(new ZipEntry(zipEntryName));
+        try (FileInputStream fis = new FileInputStream(file.toFile())) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) >= 0) {
+                zipOut.write(buffer, 0, length);
+            }
+        }
+        zipOut.closeEntry();
     }
 
     /**
@@ -99,19 +122,36 @@ public class ZipFileService {
      * @param zipPath path to the zip file
      * @throws IOException if an error occurred while extracting
      */
+
     public void extractZipFileRecursively(Path zipPath) throws IOException {
-        var dirToUnzip = Files.createDirectory(zipPath.toAbsolutePath().getParent().resolve(FilenameUtils.getBaseName(zipPath.toString())));
-        try (ZipFile zipFile = new ZipFile(zipPath.toFile())) {
-            zipFile.extractAll(dirToUnzip.toString());
-        }
-        List<Path> zipFilesInDirList;
-        try (var zipFilesInDir = Files.list(dirToUnzip).filter(path -> "zip".equalsIgnoreCase(FilenameUtils.getExtension(path.toString())))) {
-            zipFilesInDirList = zipFilesInDir.toList();
-        }
-        for (Path path : zipFilesInDirList) {
-            extractZipFileRecursively(path);
+        Path parentDir = zipPath.toAbsolutePath().getParent();
+        String baseName = zipPath.getFileName().toString().replaceFirst("\\.zip$", "");
+        Path dirToUnzip = Files.createDirectory(parentDir.resolve(baseName));
+
+        // Extract the zip file
+        try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zipPath))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                Path filePath = dirToUnzip.resolve(entry.getName());
+                if (entry.isDirectory()) {
+                    Files.createDirectories(filePath);
+                }
+                else {
+                    Files.createDirectories(filePath.getParent()); // Ensure parent directories exist
+                    Files.copy(zis, filePath, StandardCopyOption.REPLACE_EXISTING);
+                }
+                zis.closeEntry();
+            }
         }
 
+        // Check for nested zip files and extract them
+        try (var filesInDirStream = Files.list(dirToUnzip)) {
+            List<Path> zipFilesInDir = filesInDirStream.filter(path -> path.toString().toLowerCase().endsWith(".zip")).toList();
+
+            for (Path nestedZip : zipFilesInDir) {
+                extractZipFileRecursively(nestedZip);
+            }
+        }
     }
 
     private void createZipFileFromPathStream(Path zipFilePath, Stream<Path> paths, Path pathsRoot, @Nullable Predicate<Path> extraFilter) throws IOException {
