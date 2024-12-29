7190c7d370e5486288563e22d1bd6d333b421eab
Marcel Gaupp <m.gaupp@tum.de>
2024-12-29 17:16:57 +0100

    Add documentation

--- src/main/java/de/tum/cit/aet/artemis/programming/service/LicenseService.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/LicenseService.java
@@ -14,6 +14,9 @@ import de.tum.cit.aet.artemis.core.config.LicenseConfiguration;
 import de.tum.cit.aet.artemis.programming.domain.ProgrammingLanguage;
 import de.tum.cit.aet.artemis.programming.domain.ProjectType;
 
+/**
+ * Provides licensing information for proprietary software to build jobs via environment variables.
+ */
 @Profile(PROFILE_CORE)
 @Service
 public class LicenseService {
@@ -24,6 +27,14 @@ public class LicenseService {
         this.licenseConfiguration = licenseConfiguration;
     }
 
+    /**
+     * Checks whether a required license is configured for the specified exercise type.
+     * If no license is required this returns true.
+     *
+     * @param programmingLanguage the programming language of the exercise type
+     * @param projectType         the project type of the exercise type
+     * @return whether a required license is configured
+     */
     public boolean isLicensed(ProgrammingLanguage programmingLanguage, @Nullable ProjectType projectType) {
         if (programmingLanguage == ProgrammingLanguage.MATLAB && projectType == null) {
             return licenseConfiguration.getMatlabLicenseServer() != null;
@@ -32,6 +43,13 @@ public class LicenseService {
         return true;
     }
 
+    /**
+     * Returns environment variables required to run programming exercise tests.
+     *
+     * @param programmingLanguage the programming language of the exercise
+     * @param projectType         the project type of the exercise
+     * @return environment variables for the specified exercise type
+     */
     public Map<String, String> getEnvironment(ProgrammingLanguage programmingLanguage, @Nullable ProjectType projectType) {
         if (programmingLanguage == ProgrammingLanguage.MATLAB && projectType == null) {
             return Map.of("MLM_LICENSE_FILE", Objects.requireNonNull(licenseConfiguration.getMatlabLicenseServer()));
