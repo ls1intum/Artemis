1446b6a420dfd3e901e1cbd61f6cf747fb99842b
Marcel Gaupp <m.gaupp@tum.de>
2025-01-02 01:43:15 +0100

    Add example exercise

--- src/main/resources/templates/bash/test/test_helper/common-setup.bash
+++ src/main/resources/templates/bash/test/test_helper/common-setup.bash
@@ -1,10 +1,34 @@
 _common_setup() {
-    load "/usr/lib/bats/bats-support/load"
-    load "/usr/lib/bats/bats-assert/load"
-    # get the containing directory of this file
-    # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
-    # as those will point to the bats executable's location or the preprocessed file respectively
-    PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
-    # make executables in ${studentParentWorkingDirectoryName}/ visible to PATH
-    PATH="$PROJECT_ROOT/${studentParentWorkingDirectoryName}:$PATH"
+    bats_load_library "bats-support"
+    bats_load_library "bats-assert"
+    bats_load_library "bats-file"
+
+    PROJECT_ROOT="$( cd "$BATS_TEST_DIRNAME/.." >/dev/null 2>&1 && pwd )"
+    ASSIGNMENT_ROOT="$PROJECT_ROOT/${studentParentWorkingDirectoryName}"
+
+    PATH="$ASSIGNMENT_ROOT:$PATH"
 }
+
+# _assert_file_contents
+# ============
+#
+# Fail if the actual and expected file contents differ.
+#
+# Usage: _assert_file_contents <actual_path> <expected_path>
+#
+# IO:
+#   STDERR - unified diff, on failure
+# Options:
+#   <actual_path>      The file being compared.
+#   <expected_path>    The file to compare against.
+_assert_file_contents() {
+    if ! diff_output=$(diff -u --label="actual" --label="expected" "$1" "$2" 2>&1); then
+        echo "$diff_output" \
+        | batslib_decorate "$1: file contents differ" \
+        | fail
+    fi
+}
+
+# reduce output
+bats_print_stack_trace() { :; }
+bats_print_failed_command() { :; }
