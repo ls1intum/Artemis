d0905159bd401e5fc540e812c85e1d280feb7794
Michal Kawka <michal.kawka@gmx.com>
2025-01-03 14:56:12 +0100

    add missing auth checks

--- src/main/webapp/app/exam/exam-scores/exam-scores.route.ts
+++ src/main/webapp/app/exam/exam-scores/exam-scores.route.ts
@@ -1,5 +1,7 @@
 import { Route, Routes } from '@angular/router';
 import { ExamScoresComponent } from 'app/exam/exam-scores/exam-scores.component';
+import { Authority } from 'app/shared/constants/authority.constants';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
 
 export const examScoresRoute: Route[] = [
     {
@@ -14,5 +16,10 @@ export const examScoresState: Routes = [
     {
         path: '',
         children: EXAM_SCORES_ROUTES,
+        data: {
+            authorities: [Authority.ADMIN, Authority.INSTRUCTOR],
+            pageTitle: 'artemisApp.examScores.title',
+        },
+        canActivate: [UserRouteAccessService],
     },
 ];
