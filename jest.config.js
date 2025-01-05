294adf7cab365fbfd1678448b4a6d35ef2dca0bd
Stephan Krusche <krusche@tum.de>
2025-01-05 20:30:36 +0100

    Development: Update client dependencies

--- jest.config.js
+++ jest.config.js
@@ -105,10 +105,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should increase to at least 90%
-            statements: 87.71,
-            branches: 73.85,
-            functions: 82.28,
-            lines: 87.77,
+            statements: 87.73,
+            branches: 73.87,
+            functions: 82.44,
+            lines: 87.78,
         },
     },
     coverageReporters: ['clover', 'json', 'lcov', 'text-summary'],
