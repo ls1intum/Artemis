271044726aa4fe012b785ce7896040c5ab4bc7f8
Maximilian Sölch <maximiliansoelch@users.noreply.github.com>
2025-01-05 22:10:29 +0100

    Development: Migrate client code for the grading system (#10055)

--- src/main/webapp/app/exam/manage/exam-management.module.ts
+++ src/main/webapp/app/exam/manage/exam-management.module.ts
@@ -66,6 +66,8 @@ import { ArtemisProgrammingExerciseModule } from 'app/exercises/programming/shar
 import { DetailModule } from 'app/detail-overview-list/detail.module';
 import { ArtemisDurationFromSecondsPipe } from 'app/shared/pipes/artemis-duration-from-seconds.pipe';
 import { NoDataComponent } from 'app/shared/no-data-component';
+import { SafeHtmlPipe } from 'app/shared/pipes/safe-html.pipe';
+import { GradeStepBoundsPipe } from 'app/shared/pipes/grade-step-bounds.pipe';
 import { examScoresState } from 'app/exam/exam-scores/exam-scores.route';
 import { GitDiffLineStatComponent } from 'app/exercises/programming/git-diff-report/git-diff-line-stat.component';
 
@@ -73,7 +75,7 @@ const ENTITY_STATES = [...examManagementState, ...examScoresState];
 
 @NgModule({
     // TODO: For better modularization we could define an exercise module with the corresponding exam routes
-    providers: [ArtemisDurationFromSecondsPipe],
+    providers: [ArtemisDurationFromSecondsPipe, SafeHtmlPipe, GradeStepBoundsPipe],
     imports: [
         RouterModule.forChild(ENTITY_STATES),
         ArtemisTextExerciseModule,
@@ -108,6 +110,9 @@ const ENTITY_STATES = [...examManagementState, ...examScoresState];
         DetailModule,
         NoDataComponent,
         GitDiffLineStatComponent,
+        SafeHtmlPipe,
+        GradeStepBoundsPipe,
+        BonusComponent,
     ],
     declarations: [
         ExamManagementComponent,
@@ -135,7 +140,6 @@ const ENTITY_STATES = [...examManagementState, ...examScoresState];
         StudentExamDetailTableRowComponent,
         ExamImportComponent,
         ExamExerciseImportComponent,
-        BonusComponent,
         ExamEditWorkingTimeComponent,
         ExamEditWorkingTimeDialogComponent,
         ExamLiveAnnouncementCreateModalComponent,
