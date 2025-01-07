99f48c4bcda0664ac8b2bd232b4e2be97d68fdb5
Michal Kawka <michal.kawka@gmx.com>
2025-01-07 17:54:17 +0100

    fix modeling-exercise-group-cell.component.spec.ts

--- src/test/javascript/spec/component/exam/manage/exercise-groups/modeling-exercise-group-cell.component.spec.ts
+++ src/test/javascript/spec/component/exam/manage/exercise-groups/modeling-exercise-group-cell.component.spec.ts
@@ -3,13 +3,14 @@ import { ExerciseType } from 'app/entities/exercise.model';
 import { ModelingExercise } from 'app/entities/modeling-exercise.model';
 import { ModelingExerciseGroupCellComponent } from 'app/exam/manage/exercise-groups/modeling-exercise-cell/modeling-exercise-group-cell.component';
 import { UMLDiagramType } from '@ls1intum/apollon';
+import { ArtemisTestModule } from '../../../../test.module';
 
 describe('Modeling Exercise Group Cell Component', () => {
     let comp: ModelingExerciseGroupCellComponent;
     let fixture: ComponentFixture<ModelingExerciseGroupCellComponent>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({})
+        TestBed.configureTestingModule({ imports: [ArtemisTestModule] })
             .compileComponents()
             .then(() => {
                 fixture = TestBed.createComponent(ModelingExerciseGroupCellComponent);
