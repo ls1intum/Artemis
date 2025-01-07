3c359a65769ef0c6c7d8582f61a6259e4e6f3b12
Michal Kawka <michal.kawka@gmx.com>
2025-01-07 17:24:48 +0100

    fix file-upload-exercise-group-cell.component.spec.ts

--- src/test/javascript/spec/component/exam/manage/exercise-groups/file-upload-exercise-group-cell.component.spec.ts
+++ src/test/javascript/spec/component/exam/manage/exercise-groups/file-upload-exercise-group-cell.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ArtemisTestModule } from '../../../../test.module';
 import { ExerciseType } from 'app/entities/exercise.model';
-import { TranslatePipeMock } from '../../../../helpers/mocks/service/mock-translate.service';
 import { FileUploadExerciseGroupCellComponent } from 'app/exam/manage/exercise-groups/file-upload-exercise-cell/file-upload-exercise-group-cell.component';
 import { FileUploadExercise } from 'app/entities/file-upload-exercise.model';
 
@@ -12,8 +11,6 @@ describe('File Upload Exercise Group Cell Component', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ArtemisTestModule],
-            declarations: [FileUploadExerciseGroupCellComponent, TranslatePipeMock],
-            providers: [],
         })
             .compileComponents()
             .then(() => {
