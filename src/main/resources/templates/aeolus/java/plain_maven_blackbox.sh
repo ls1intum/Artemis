a3cb64bf5dcda8ae246ea285c25ff488a0249d63
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2025-01-08 19:42:35 +0100

    rewrite pipeline scripts

--- src/main/resources/templates/aeolus/java/plain_maven_blackbox.sh
+++ src/main/resources/templates/aeolus/java/plain_maven_blackbox.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -e
+export AEOLUS_INITIAL_DIRECTORY=${PWD}
 build () {
   echo '⚙️ executing build'
   mvn -B clean compile
 }
 
 main_method_checker () {
+  echo '⚙️ executing main_method_checker'
   main_checker_output=$(pipeline-helper main-method -s target/classes)
 
   line_count="$(echo "$main_checker_output" | wc -l)"
-  main_method="$(echo "$main_checker_output" | head -n1)"
+  main_class="$(echo "$main_checker_output" | head -n1)"
 
   if [ "${line_count}" -eq 2 ]; then
-    echo "${main_method}"
+    echo main method found in file: "${main_class}"
+    sed -i "s#MAIN_CLASS#${main_class}#" testsuite/config/default.exp
   else
     echo "no main method found. quitting the test run."
     exit 1
@@ -20,17 +23,31 @@ main_method_checker () {
 }
 
 custom_checkers () {
-  echo '⚙️ executing custom checkers'
+  echo '⚙️ executing custom_checkers'
   # all java files in the assignment folder should have maximal line length 80
   pipeline-helper line-length -l 80 -s ${studentParentWorkingDirectoryName}/ -e java
   # checks that the file exists and is not empty for non gui programs
   pipeline-helper file-exists ${studentParentWorkingDirectoryName}/Tests.txt
 }
 
-secret_tests () {
-  echo '⚙️ executing secret tests'
+replace_script_variables () {
+  echo '⚙️ executing replace_script_variables'
+  local JAVA_FLAGS="-Djdk.console=java.base"
+  local testfiles_base_path="./testsuite/testfiles"
+  local tool=$(find testsuite -name "*.tests" -type d -printf "%f" | sed 's#.tests$##')
 
-  export step="secret"
+  sed -i "s#JAVA_FLAGS#${JAVA_FLAGS}#;s#CLASSPATH#../target/classes#" testsuite/config/default.exp
+  sed -i "s#TESTFILES_DIRECTORY#../${testfiles_base_path}#" testsuite/${tool}.tests/*.exp
+}
+
+secret_tests () {
+  echo '⚙️ executing secret_tests'
+  if [ ! -d ./testsuite ]; then
+    echo "skipping secret tests because the testsuite folder does not exist."
+  fi
+
+  local step="secret"
+  local tool=$(find testsuite -name "*.tests" -type d -printf "%f" | sed 's#.tests$##')
   cd testsuite || exit
   rm ${tool}.log || true
   timeout 60s runtest --tool ${tool} ${step}.exp || true
@@ -47,9 +64,13 @@ secret_tests () {
 }
 
 public_tests () {
-  echo '⚙️ executing public tests'
+  echo '⚙️ executing public_tests'
+  if [ ! -d ./testsuite ]; then
+    echo "skipping public tests because the testsuite folder does not exist."
+  fi
 
-  export step="public"
+  local step="public"
+  local tool=$(find testsuite -name "*.tests" -type d -printf "%f" | sed 's#.tests$##')
   cd testsuite || exit
   rm ${tool}.log || true
   timeout 60s runtest --tool ${tool} ${step}.exp || true
@@ -58,9 +79,13 @@ public_tests () {
 }
 
 advanced_tests () {
-  echo '⚙️ executing advanced tests'
+  echo '⚙️ executing advanced_tests'
+  if [ ! -d ./testsuite ]; then
+    echo "skipping advanced tests because the testsuite folder does not exist."
+  fi
 
-  export step="advanced"
+  local step="advanced"
+  local tool=$(find testsuite -name "*.tests" -type d -printf "%f" | sed 's#.tests$##')
   cd testsuite || exit
   rm ${tool}.log || true
   timeout 60s runtest --tool ${tool} ${step}.exp || true
@@ -68,39 +93,26 @@ advanced_tests () {
   pipeline-helper -o customFeedbacks dejagnu -n "dejagnu[${step}]" -l testsuite/${tool}.log
 }
 
-replace_script_variables () {
-    JAVA_FLAGS="-Djdk.console=java.base"
-
-    sed -i "s#JAVA_FLAGS#${JAVA_FLAGS}#;s#CLASSPATH#../target/classes#" testsuite/config/default.exp
-    sed -i "s#MAIN_CLASS#${MAIN_CLASS}#" testsuite/config/default.exp
-    sed -i "s#TESTFILES_DIRECTORY#../${testfiles_base_path}#" testsuite/${tool}.tests/*.exp
-}
-
 main () {
   if [[ "${1}" == "aeolus_sourcing" ]]; then
     return 0 # just source to use the methods in the subshell, no execution
   fi
-
   local _script_name
   _script_name=${BASH_SOURCE[0]:-$0}
-
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; build"
-
-  MAIN_CLASS=$(bash -c "source ${_script_name} aeolus_sourcing; main_method_checker")
-  echo "main method found in class: ${MAIN_CLASS}"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; main_method_checker"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; custom_checkers"
-
-  if [ -d ./testsuite ]; then
-    export tool=$(find testsuite -name "*.tests" -type d -printf "%f" | sed 's#.tests$##')
-    export testfiles_base_path="./testsuite/testfiles"
-
-    replace_script_variables
-    bash -c "source ${_script_name} aeolus_sourcing; secret_tests"
-    bash -c "source ${_script_name} aeolus_sourcing; public_tests"
-    bash -c "source ${_script_name} aeolus_sourcing; advanced_tests"
-  else
-    echo "skipping dejagnu tests because the testsuite folder does not exist."
-  fi
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; replace_script_variables"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; secret_tests"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; public_tests"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; advanced_tests"
 }
 
 main "${@}"
