b1074d58756e3a6bcafc76fb50003d0ebbdb6356
entholzer <nom.ise@outlook.com>
2025-01-10 19:51:12 +0100

    fix EditAttachmentUnitComponent test

--- src/test/javascript/spec/component/lecture-unit/attachment-unit/edit-attachment-unit.component.spec.ts
+++ src/test/javascript/spec/component/lecture-unit/attachment-unit/edit-attachment-unit.component.spec.ts
@@ -1,12 +1,11 @@
 import dayjs from 'dayjs/esm';
 import { MockRouter } from '../../../helpers/mocks/mock-router';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockProvider } from 'ng-mocks';
 import { AlertService } from 'app/core/util/alert.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, convertToParamMap } from '@angular/router';
 import { of } from 'rxjs';
-import { AttachmentUnitFormData } from 'app/lecture/lecture-unit/lecture-unit-management/attachment-unit-form/attachment-unit-form.component';
+import { AttachmentUnitFormComponent, AttachmentUnitFormData } from 'app/lecture/lecture-unit/lecture-unit-management/attachment-unit-form/attachment-unit-form.component';
 import { AttachmentUnitService } from 'app/lecture/lecture-unit/lecture-unit-management/attachmentUnit.service';
 import { EditAttachmentUnitComponent } from 'app/lecture/lecture-unit/lecture-unit-management/edit-attachment-unit/edit-attachment-unit.component';
 import { Attachment, AttachmentType } from 'app/entities/attachment.model';
@@ -14,27 +13,11 @@ import { AttachmentUnit } from 'app/entities/lecture-unit/attachmentUnit.model';
 import { HttpResponse } from '@angular/common/http';
 import { By } from '@angular/platform-browser';
 import { objectToJsonBlob } from 'app/utils/blob-util';
-
-@Component({ selector: 'jhi-attachment-unit-form', template: '' })
-class AttachmentUnitFormStubComponent {
-    errorMessage: string;
-    @Input() isEditMode = false;
-    @Input() formData: AttachmentUnitFormData;
-    @Output() formSubmitted: EventEmitter<AttachmentUnitFormData> = new EventEmitter<AttachmentUnitFormData>();
-
-    setFileUploadError(errorMessage: string) {
-        this.errorMessage = errorMessage;
-    }
-}
-
-@Component({ selector: 'jhi-lecture-unit-layout', template: '<ng-content />' })
-class LectureUnitLayoutStubComponent {
-    @Input()
-    isLoading = false;
-}
+import { OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
+import { ArtemisTestModule } from '../../../test.module';
 
 describe('EditAttachmentUnitComponent', () => {
-    let editAttachmentUnitComponentFixture: ComponentFixture<EditAttachmentUnitComponent>;
+    let fixture: ComponentFixture<EditAttachmentUnitComponent>;
 
     let attachmentUnitService;
     let router: Router;
@@ -47,8 +30,7 @@ describe('EditAttachmentUnitComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [],
-            declarations: [AttachmentUnitFormStubComponent, LectureUnitLayoutStubComponent, EditAttachmentUnitComponent],
+            imports: [ArtemisTestModule, OwlNativeDateTimeModule],
             providers: [
                 MockProvider(AttachmentUnitService),
                 MockProvider(AlertService),
@@ -56,6 +38,9 @@ describe('EditAttachmentUnitComponent', () => {
                 {
                     provide: ActivatedRoute,
                     useValue: {
+                        snapshot: {
+                            paramMap: convertToParamMap({ courseId: 1 }),
+                        },
                         paramMap: of({
                             get: (key: string) => {
                                 switch (key) {
@@ -80,71 +65,64 @@ describe('EditAttachmentUnitComponent', () => {
                 },
             ],
             schemas: [],
-        })
-            .compileComponents()
-            .then(() => {
-                editAttachmentUnitComponentFixture = TestBed.createComponent(EditAttachmentUnitComponent);
-                router = TestBed.inject(Router);
-                attachmentUnitService = TestBed.inject(AttachmentUnitService);
+        }).compileComponents();
+        fixture = TestBed.createComponent(EditAttachmentUnitComponent);
+        router = TestBed.inject(Router);
+        attachmentUnitService = TestBed.inject(AttachmentUnitService);
 
-                attachment = new Attachment();
-                attachment.id = 1;
-                attachment.version = 1;
-                attachment.attachmentType = AttachmentType.FILE;
-                attachment.releaseDate = dayjs().year(2010).month(3).date(5);
-                attachment.uploadDate = dayjs().year(2010).month(3).date(5);
-                attachment.name = 'test';
-                attachment.link = '/path/to/file';
+        attachment = new Attachment();
+        attachment.id = 1;
+        attachment.version = 1;
+        attachment.attachmentType = AttachmentType.FILE;
+        attachment.releaseDate = dayjs().year(2010).month(3).date(5);
+        attachment.uploadDate = dayjs().year(2010).month(3).date(5);
+        attachment.name = 'test';
+        attachment.link = '/path/to/file';
 
-                attachmentUnit = new AttachmentUnit();
-                attachmentUnit.id = 1;
-                attachmentUnit.description = 'lorem ipsum';
-                attachmentUnit.attachment = attachment;
+        attachmentUnit = new AttachmentUnit();
+        attachmentUnit.id = 1;
+        attachmentUnit.description = 'lorem ipsum';
+        attachmentUnit.attachment = attachment;
 
-                fakeFile = new File([''], 'Test-File.pdf', { type: 'application/pdf' });
+        fakeFile = new File([''], 'Test-File.pdf', { type: 'application/pdf' });
 
-                baseFormData = new FormData();
-                baseFormData.append('file', fakeFile, 'updated file');
-                baseFormData.append('attachment', objectToJsonBlob(attachment));
-                baseFormData.append('attachmentUnit', objectToJsonBlob(attachmentUnit));
+        baseFormData = new FormData();
+        baseFormData.append('file', fakeFile, 'updated file');
+        baseFormData.append('attachment', objectToJsonBlob(attachment));
+        baseFormData.append('attachmentUnit', objectToJsonBlob(attachmentUnit));
 
-                jest.spyOn(attachmentUnitService, 'findById').mockReturnValue(
-                    of(
-                        new HttpResponse({
-                            body: attachmentUnit,
-                            status: 200,
-                        }),
-                    ),
-                );
-                updateAttachmentUnitSpy = jest.spyOn(attachmentUnitService, 'update');
-                navigateSpy = jest.spyOn(router, 'navigate');
-            });
+        jest.spyOn(attachmentUnitService, 'findById').mockReturnValue(
+            of(
+                new HttpResponse({
+                    body: attachmentUnit,
+                    status: 200,
+                }),
+            ),
+        );
+        updateAttachmentUnitSpy = jest.spyOn(attachmentUnitService, 'update');
+        navigateSpy = jest.spyOn(router, 'navigate');
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    it('should set form data correctly', () => {
-        editAttachmentUnitComponentFixture.detectChanges();
-        const attachmentUnitFormStubComponent: AttachmentUnitFormStubComponent = editAttachmentUnitComponentFixture.debugElement.query(
-            By.directive(AttachmentUnitFormStubComponent),
-        ).componentInstance;
+    it('should set form data correctly', async () => {
+        fixture.detectChanges();
+        const attachmentUnitFormStubComponent: AttachmentUnitFormComponent = fixture.debugElement.query(By.directive(AttachmentUnitFormComponent)).componentInstance;
 
-        expect(attachmentUnitFormStubComponent.formData.formProperties.name).toEqual(attachment.name);
-        expect(attachmentUnitFormStubComponent.formData.formProperties.releaseDate).toEqual(attachment.releaseDate);
-        expect(attachmentUnitFormStubComponent.formData.formProperties.updateNotificationText).toBeUndefined();
-        expect(attachmentUnitFormStubComponent.formData.formProperties.version).toBe(1);
-        expect(attachmentUnitFormStubComponent.formData.formProperties.description).toEqual(attachmentUnit.description);
-        expect(attachmentUnitFormStubComponent.formData.fileProperties.fileName).toEqual(attachment.link);
-        expect(attachmentUnitFormStubComponent.formData.fileProperties.file).toBeUndefined();
+        expect(attachmentUnitFormStubComponent.formData()?.formProperties.name).toEqual(attachment.name);
+        expect(attachmentUnitFormStubComponent.formData()?.formProperties.releaseDate).toEqual(attachment.releaseDate);
+        expect(attachmentUnitFormStubComponent.formData()?.formProperties.updateNotificationText).toBeUndefined();
+        expect(attachmentUnitFormStubComponent.formData()?.formProperties.version).toBe(1);
+        expect(attachmentUnitFormStubComponent.formData()?.formProperties.description).toEqual(attachmentUnit.description);
+        expect(attachmentUnitFormStubComponent.formData()?.fileProperties.fileName).toEqual(attachment.link);
+        expect(attachmentUnitFormStubComponent.formData()?.fileProperties.file).toBeUndefined();
     });
 
-    it('should update attachment unit with file change without notification', () => {
-        editAttachmentUnitComponentFixture.detectChanges();
-        const attachmentUnitFormStubComponent: AttachmentUnitFormStubComponent = editAttachmentUnitComponentFixture.debugElement.query(
-            By.directive(AttachmentUnitFormStubComponent),
-        ).componentInstance;
+    it('should update attachment unit with file change without notification', async () => {
+        fixture.detectChanges();
+        const attachmentUnitFormStubComponent: AttachmentUnitFormComponent = fixture.debugElement.query(By.directive(AttachmentUnitFormComponent)).componentInstance;
 
         const fileName = 'updated file';
 
@@ -164,17 +142,15 @@ describe('EditAttachmentUnitComponent', () => {
 
         updateAttachmentUnitSpy.mockReturnValue(of({ body: attachmentUnit, status: 200 }));
         attachmentUnitFormStubComponent.formSubmitted.emit(attachmentUnitFormData);
-        editAttachmentUnitComponentFixture.detectChanges();
+        fixture.detectChanges();
 
         expect(updateAttachmentUnitSpy).toHaveBeenCalledWith(1, 1, baseFormData, undefined);
         expect(navigateSpy).toHaveBeenCalledOnce();
     });
 
-    it('should update attachment unit with file change with notification', () => {
-        editAttachmentUnitComponentFixture.detectChanges();
-        const attachmentUnitFormStubComponent: AttachmentUnitFormStubComponent = editAttachmentUnitComponentFixture.debugElement.query(
-            By.directive(AttachmentUnitFormStubComponent),
-        ).componentInstance;
+    it('should update attachment unit with file change with notification', async () => {
+        fixture.detectChanges();
+        const attachmentUnitFormStubComponent: AttachmentUnitFormComponent = fixture.debugElement.query(By.directive(AttachmentUnitFormComponent)).componentInstance;
 
         const fileName = 'updated file';
 
@@ -196,17 +172,15 @@ describe('EditAttachmentUnitComponent', () => {
 
         updateAttachmentUnitSpy.mockReturnValue(of({ body: attachmentUnit, status: 200 }));
         attachmentUnitFormStubComponent.formSubmitted.emit(attachmentUnitFormData);
-        editAttachmentUnitComponentFixture.detectChanges();
+        fixture.detectChanges();
 
         expect(updateAttachmentUnitSpy).toHaveBeenCalledWith(1, 1, baseFormData, notification);
         expect(navigateSpy).toHaveBeenCalledOnce();
     });
 
-    it('should update attachment unit without file change without notification', () => {
-        editAttachmentUnitComponentFixture.detectChanges();
-        const attachmentUnitFormStubComponent: AttachmentUnitFormStubComponent = editAttachmentUnitComponentFixture.debugElement.query(
-            By.directive(AttachmentUnitFormStubComponent),
-        ).componentInstance;
+    it('should update attachment unit without file change without notification', async () => {
+        fixture.detectChanges();
+        const attachmentUnitFormStubComponent: AttachmentUnitFormComponent = fixture.debugElement.query(By.directive(AttachmentUnitFormComponent)).componentInstance;
 
         const attachmentUnitFormData: AttachmentUnitFormData = {
             formProperties: {
@@ -225,7 +199,7 @@ describe('EditAttachmentUnitComponent', () => {
 
         updateAttachmentUnitSpy.mockReturnValue(of({ body: attachmentUnit, status: 200 }));
         attachmentUnitFormStubComponent.formSubmitted.emit(attachmentUnitFormData);
-        editAttachmentUnitComponentFixture.detectChanges();
+        fixture.detectChanges();
 
         expect(updateAttachmentUnitSpy).toHaveBeenCalledWith(1, 1, formData, undefined);
         expect(navigateSpy).toHaveBeenCalledOnce();
