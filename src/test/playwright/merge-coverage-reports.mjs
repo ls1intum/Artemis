c15657c6de60e42e2f1a7b4f5949d15151a353a8
Murad Talibov <muradtalibov2@gmail.com>
2025-01-12 06:36:12 +0100

    Revert "Upload only the coverage of parallel tests as artifact to check if the problem is on coverage data itself on CI"
    This reverts commit 67c12a99318b5e0573546daa210658d115c3e4d9.

--- src/test/playwright/merge-coverage-reports.mjs
+++ src/test/playwright/merge-coverage-reports.mjs
@@ -2,36 +2,36 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import archiver from 'archiver';
-// import coverage from 'istanbul-lib-coverage';
-// import reports from 'istanbul-reports';
-// import libReport from 'istanbul-lib-report';
+import coverage from 'istanbul-lib-coverage';
+import reports from 'istanbul-reports';
+import libReport from 'istanbul-lib-report';
 import fs from 'fs';
 
-// const coverageJsonParallel = './test-reports/monocart-report-parallel/coverage/coverage-final.json';
-// const coverageJsonSequential = './test-reports/monocart-report-sequential/coverage/coverage-final.json';
-// const outputDir = './test-reports/monocart-report/coverage';
-//
-// console.log(`Merging coverage reports`);
-//
-// const coverageParallel = JSON.parse(fs.readFileSync(coverageJsonParallel, 'utf8'));
-// const coverageSequential = JSON.parse(fs.readFileSync(coverageJsonSequential, 'utf8'));
-//
-// const mapA = coverage.createCoverageMap(coverageParallel);
-// const mapB = coverage.createCoverageMap(coverageSequential);
-// mapA.merge(mapB);
-//
-// const context = libReport.createContext({
-//     dir: outputDir,
-//     coverageMap: mapA,
-// });
-//
-// const htmlReport = reports.create('html');
-// const lcovReport = reports.create('lcovonly', { file: 'lcov.info' });
-//
-// htmlReport.execute(context);
-// lcovReport.execute(context);
-//
-// console.log(`Merged coverage reports successfully`);
+const coverageJsonParallel = './test-reports/monocart-report-parallel/coverage/coverage-final.json';
+const coverageJsonSequential = './test-reports/monocart-report-sequential/coverage/coverage-final.json';
+const outputDir = './test-reports/monocart-report/coverage';
+
+console.log(`Merging coverage reports`);
+
+const coverageParallel = JSON.parse(fs.readFileSync(coverageJsonParallel, 'utf8'));
+const coverageSequential = JSON.parse(fs.readFileSync(coverageJsonSequential, 'utf8'));
+
+const mapA = coverage.createCoverageMap(coverageParallel);
+const mapB = coverage.createCoverageMap(coverageSequential);
+mapA.merge(mapB);
+
+const context = libReport.createContext({
+    dir: outputDir,
+    coverageMap: mapA,
+});
+
+const htmlReport = reports.create('html');
+const lcovReport = reports.create('lcovonly', { file: 'lcov.info' });
+
+htmlReport.execute(context);
+lcovReport.execute(context);
+
+console.log(`Merged coverage reports successfully`);
 
 // Archive the lcov coverage report
 const __filename = fileURLToPath(import.meta.url);
@@ -52,5 +52,5 @@ archive.on('error', (err) => {
 });
 
 archive.pipe(output);
-archive.directory(path.join(__dirname, 'test-reports/monocart-report-parallel/coverage/lcov-report'), false);
+archive.directory(path.join(__dirname, 'test-reports/monocart-report/coverage'), false);
 await archive.finalize();
