1f2d1e5a59b44ae12e7481f061878724863ed2e5
Konrad Gößmann <konrad.goessmann@tum.de>
2025-01-14 18:26:13 +0100

    Cleans lazy fetch test up

--- src/test/java/de/tum/cit/aet/artemis/atlas/profile/LearnerProfileIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/atlas/profile/LearnerProfileIntegrationTest.java
@@ -2,30 +2,16 @@ package de.tum.cit.aet.artemis.atlas.profile;
 
 import de.tum.cit.aet.artemis.atlas.AbstractAtlasIntegrationTest;
 import de.tum.cit.aet.artemis.core.domain.User;
-import de.tum.cit.aet.artemis.shared.TestRepositoryConfiguration;
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.hibernate.Hibernate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
-@Execution(ExecutionMode.CONCURRENT)
-@Import(TestRepositoryConfiguration.class)
-@AutoConfigureEmbeddedDatabase
-public class LearnerProfileArchitectureTest extends AbstractAtlasIntegrationTest {
 
-    private static final String TEST_PREFIX = "learningpathintegration";
+class LearnerProfileIntegrationTest extends AbstractAtlasIntegrationTest {
+
+    private static final String TEST_PREFIX = "learnerprofiledatabase";
 
     private static final int NUMBER_OF_STUDENTS = 1;
 
@@ -45,8 +31,8 @@ public class LearnerProfileArchitectureTest extends AbstractAtlasIntegrationTest
 
     @Test
     @WithMockUser(username = STUDENT1_OF_COURSE, roles = "USER")
-    void testAll_asStudent() {
+    void testLazyFetchLearnerProfile() {
         User user = userTestRepository.getUserWithGroupsAndAuthorities(STUDENT1_OF_COURSE);
-        assertFalse(Hibernate.isInitialized(user.getLearnerProfile()));
+        assertThat(Hibernate.isInitialized(user.getLearnerProfile())).isFalse();
     }
 }
