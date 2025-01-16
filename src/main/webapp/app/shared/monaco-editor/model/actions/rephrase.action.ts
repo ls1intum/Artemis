6875c4fcfc89527b4578d56bbfc8b7dedfa6ac51
Tim Cremer <tim.cremer@tum.de>
2025-01-16 17:25:24 +0100

    Added Felix Icon to resemble AI

--- src/main/webapp/app/shared/monaco-editor/model/actions/rephrase.action.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/rephrase.action.ts
@@ -1,12 +1,12 @@
 import { TextEditorAction } from 'app/shared/monaco-editor/model/actions/text-editor-action.model';
 
-import { faReceipt } from '@fortawesome/free-solid-svg-icons';
 import { TextEditor } from 'app/shared/monaco-editor/model/actions/adapter/text-editor.interface';
 import { RephraseService } from 'app/shared/monaco-editor/rephrase.service';
 import RephrasingVariant from 'app/shared/monaco-editor/model/rephrasing-variant';
+import { facArtemisIntelligence } from 'app/icons/icons';
 
 /**
- * Action to toggle fullscreen mode in the editor.
+ * Action to the rephrasing action the editor.
  */
 export class RephraseAction extends TextEditorAction {
     static readonly ID = 'rephrase.action';
@@ -18,11 +18,11 @@ export class RephraseAction extends TextEditorAction {
         private readonly rephrasingVariant: RephrasingVariant,
         private readonly courseId: number,
     ) {
-        super(RephraseAction.ID, 'artemisApp.markdownEditor.commands.rephrase', faReceipt);
+        super(RephraseAction.ID, 'artemisApp.markdownEditor.commands.rephrase', facArtemisIntelligence);
     }
 
     /**
-     * Toggles the fullscreen mode of the editor.
+     * Toggles the rephrasing of the markdown content the editor.
      * @param editor The editor in which to rephrase the markdown.
      * @param variant The variant of the rephrasing.
      */
