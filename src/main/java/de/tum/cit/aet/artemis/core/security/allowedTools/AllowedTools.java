0cc648128af7db2cf722ca57be5865b1a621e488
dennis <dp.jandow@gmail.com>
2025-01-16 19:16:05 +0100

    add doc comment vor @interface method

--- src/main/java/de/tum/cit/aet/artemis/core/security/allowedTools/AllowedTools.java
+++ src/main/java/de/tum/cit/aet/artemis/core/security/allowedTools/AllowedTools.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Annotation to specify the tools allowed for certain methods or classes.
  * Used to restrict access or customize behavior based on the provided {@link ToolTokenType}.
  */
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AllowedTools {
 
