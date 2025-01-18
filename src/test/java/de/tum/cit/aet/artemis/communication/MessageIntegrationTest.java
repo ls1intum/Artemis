66e2681dea9a622c28b3cb4f31af008c377cee56
Paul Rangger <parangger@gmail.com>
2025-01-18 11:40:26 +0100

    Same treatment for deletingMessage test case

--- src/test/java/de/tum/cit/aet/artemis/communication/MessageIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/communication/MessageIntegrationTest.java
@@ -14,6 +14,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import jakarta.validation.ConstraintViolation;
@@ -719,7 +720,7 @@ class MessageIntegrationTest extends AbstractSpringIntegrationIndependentTest {
 
         request.delete("/api/courses/" + courseId + "/messages/" + createdPost2.getId(), HttpStatus.OK);
         // After deleting the message in the second chat, the unread count in the first chat should stay the same, the unread count in the second chat will become 0
-        await().untilAsserted(() -> {
+        await().atMost(2, TimeUnit.SECONDS).pollInterval(100, TimeUnit.MILLISECONDS).untilAsserted(() -> {
             SecurityUtils.setAuthorizationObject();
             assertThat(getUnreadMessagesCount(oneToOneChat1, student2)).isEqualTo(1);
             assertThat(getUnreadMessagesCount(oneToOneChat2, student2)).isZero();
