b5600316ce9b999f374774fdf1e4014f8201ed21
Paul Rangger <parangger@gmail.com>
2025-01-18 13:47:06 +0100

    increase wait time and remove duplicate assertion

--- src/test/java/de/tum/cit/aet/artemis/communication/MessageIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/communication/MessageIntegrationTest.java
@@ -720,14 +720,14 @@ class MessageIntegrationTest extends AbstractSpringIntegrationIndependentTest {
 
         request.delete("/api/courses/" + courseId + "/messages/" + createdPost2.getId(), HttpStatus.OK);
         // After deleting the message in the second chat, the unread count in the first chat should stay the same, the unread count in the second chat will become 0
-        await().atMost(2, TimeUnit.SECONDS).pollInterval(50, TimeUnit.MILLISECONDS).pollDelay(300, TimeUnit.MILLISECONDS).untilAsserted(() -> {
+        await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
             SecurityUtils.setAuthorizationObject();
             assertThat(getUnreadMessagesCount(oneToOneChat1, student2)).isEqualTo(1);
             assertThat(getUnreadMessagesCount(oneToOneChat2, student2)).isZero();
 
             // no changes for student1
             assertThat(getUnreadMessagesCount(oneToOneChat1, student1)).isZero();
-            assertThat(getUnreadMessagesCount(oneToOneChat1, student1)).isZero();
+            assertThat(getUnreadMessagesCount(oneToOneChat2, student1)).isZero();
         });
     }
 
