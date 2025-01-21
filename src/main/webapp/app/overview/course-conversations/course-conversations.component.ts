c8c5b483c432f51dfcb44cabdf75c8cafca39222
Asli Aykan <asliaykan97@gmail.com>
2025-01-21 02:37:09 +0100

    update distinctuntilchanged

--- src/main/webapp/app/overview/course-conversations/course-conversations.component.ts
+++ src/main/webapp/app/overview/course-conversations/course-conversations.component.ts
@@ -260,7 +260,15 @@ export class CourseConversationsComponent implements OnInit, OnDestroy {
             this.channelActions$
                 .pipe(
                     debounceTime(500),
-                    distinctUntilChanged((prev, curr) => prev.action === curr.action && prev.channel.id === curr.channel.id && prev.channel.name === curr.channel.name),
+                    distinctUntilChanged((prev, curr) => {
+                        if (prev.action !== curr.action) {
+                            return false;
+                        }
+                        if (!curr.channel.id) {
+                            return false;
+                        }
+                        return prev.channel.id === curr.channel.id && prev.channel.name === curr.channel.name;
+                    }),
                     takeUntil(this.ngUnsubscribe),
                 )
                 .subscribe((channelAction) => {
