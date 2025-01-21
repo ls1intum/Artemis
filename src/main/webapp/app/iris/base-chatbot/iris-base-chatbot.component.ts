be907f98b0ee5637164693db0becd151eea3da8c
Patrick Bassner <patrick@bassner.de>
2025-01-21 20:37:01 +0100

    focus input field after message

--- src/main/webapp/app/iris/base-chatbot/iris-base-chatbot.component.ts
+++ src/main/webapp/app/iris/base-chatbot/iris-base-chatbot.component.ts
@@ -167,6 +167,7 @@ export class IrisBaseChatbotComponent implements OnInit, OnDestroy, AfterViewIni
         this.messagesSubscription = this.chatService.currentMessages().subscribe((messages) => {
             if (messages.length !== this.messages?.length) {
                 this.scrollToBottom('auto');
+                setTimeout(() => this.messageTextarea?.nativeElement?.focus(), 10);
             }
             this.messages = _.cloneDeep(messages).reverse();
             this.messages.forEach((message) => {
