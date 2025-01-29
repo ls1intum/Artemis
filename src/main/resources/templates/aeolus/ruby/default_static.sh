87ee8e27ed9011a8c9fe2665c25da9e8977a5493
Marcel Gaupp <m.gaupp@tum.de>
2025-01-29 16:16:36 +0100

    Update build scripts

--- src/main/resources/templates/aeolus/ruby/default_static.sh
+++ src/main/resources/templates/aeolus/ruby/default_static.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 export AEOLUS_INITIAL_DIRECTORY=${PWD}
+install_dependencies () {
+  echo '⚙️ executing install_dependencies'
+  cd "${testWorkingDirectory}"
+  bundler install
+}
+
 static_code_analysis () {
   echo '⚙️ executing static_code_analysis'
   cd "${testWorkingDirectory}"
@@ -19,6 +25,8 @@ main () {
   fi
   local _script_name
   _script_name=${BASH_SOURCE[0]:-$0}
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; install_dependencies"
   cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; static_code_analysis"
   cd "${AEOLUS_INITIAL_DIRECTORY}"
