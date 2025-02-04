a3fb3dcd08ec7ede9d37d0d050f07b0f933e38d4
Ole Vester <ole.vester+gitlab@check24.de>
2025-02-04 22:37:19 +0100

    add arch test to validate api architecture for tutorialgroup module

---
+++ src/test/java/de/tum/cit/aet/artemis/tutorialgroup/architecture/TutorialGroupApiArchitectureTest.java
@@ -0,0 +1,11 @@
+package de.tum.cit.aet.artemis.tutorialgroup.architecture;
+
+import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleAccessArchitectureTest;
+
+class TutorialGroupApiArchitectureTest extends AbstractModuleAccessArchitectureTest {
+
+    @Override
+    public String getModulePackage() {
+        return ARTEMIS_PACKAGE + ".tutorialgroup";
+    }
+}
