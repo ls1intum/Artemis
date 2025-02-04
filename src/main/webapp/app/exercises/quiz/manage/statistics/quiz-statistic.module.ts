c2a9df6f6d4ee66366de72bb3b56af91d88f9afb
Tobias Lippert <t.lippert@tum.de>
2025-02-04 21:05:47 +0100

    remove quiz question types module

--- src/main/webapp/app/exercises/quiz/manage/statistics/quiz-statistic.module.ts
+++ src/main/webapp/app/exercises/quiz/manage/statistics/quiz-statistic.module.ts
@@ -8,7 +8,6 @@ import { ShortAnswerQuestionStatisticComponent } from './short-answer-question-s
 import { QuizStatisticsFooterComponent } from 'app/exercises/quiz/manage/statistics/quiz-statistics-footer/quiz-statistics-footer.component';
 import { quizStatisticRoute } from 'app/exercises/quiz/manage/statistics/quiz-statistic.route';
 import { ArtemisSharedModule } from 'app/shared/shared.module';
-import { ArtemisQuizQuestionTypesModule } from 'app/exercises/quiz/shared/questions/artemis-quiz-question-types.module';
 import { ArtemisMarkdownModule } from 'app/shared/markdown.module';
 import { BarChartModule } from '@swimlane/ngx-charts';
 
@@ -18,7 +17,6 @@ const ENTITY_STATES = [...quizStatisticRoute];
     imports: [
         ArtemisSharedModule,
         RouterModule.forChild(ENTITY_STATES),
-        ArtemisQuizQuestionTypesModule,
         ArtemisMarkdownModule,
         BarChartModule,
         QuizStatisticComponent,
