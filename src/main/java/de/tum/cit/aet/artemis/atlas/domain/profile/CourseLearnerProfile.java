82a3a264e6b3b2e5f6ebc3c00d37bb56b4ea42e5
Konrad Gößmann <konrad.goessmann@tum.de>
2025-02-04 15:00:13 +0100

    Fixes min values of Course Learner Profile Attributes

--- src/main/java/de/tum/cit/aet/artemis/atlas/domain/profile/CourseLearnerProfile.java
+++ src/main/java/de/tum/cit/aet/artemis/atlas/domain/profile/CourseLearnerProfile.java
@@ -35,19 +35,19 @@ public class CourseLearnerProfile extends DomainObject {
     private Course course;
 
     @Column(name = "aim_for_grade_or_bonus")
-    @Min(0)
+    @Min(1)
     @Max(5)
-    private int aimForGradeOrBonus;
+    private int aimForGradeOrBonus = 1;
 
     @Column(name = "time_investment")
-    @Min(0)
+    @Min(1)
     @Max(5)
-    private int timeInvestment;
+    private int timeInvestment = 1;
 
     @Column(name = "repetition_intensity")
-    @Min(0)
+    @Min(1)
     @Max(5)
-    private int repetitionIntensity;
+    private int repetitionIntensity = 1;
 
     public void setLearnerProfile(LearnerProfile learnerProfile) {
         this.learnerProfile = learnerProfile;
