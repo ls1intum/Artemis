bd3c0765d6c8e73c8b1d752012ff9a370997816f
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-02-06 11:20:07 +0100

    got rid of overhead code

--- src/main/webapp/app/exercises/shared/feedback/long-feedback-text.service.ts
+++ src/main/webapp/app/exercises/shared/feedback/long-feedback-text.service.ts
@@ -1,33 +1,12 @@
-import { Injectable, OnDestroy, inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, Subject, Subscription, distinctUntilChanged, tap } from 'rxjs';
-import { AccountService } from 'app/core/auth/account.service';
+import { Observable } from 'rxjs';
 
 export type LongFeedbackResponse = HttpResponse<string>;
 
-const logoutSubject = new Subject<void>();
-
 @Injectable({ providedIn: 'root' })
-export class LongFeedbackTextService implements OnDestroy {
+export class LongFeedbackTextService {
     private http = inject(HttpClient);
-    private accountService = inject(AccountService);
-
-    private userChangeSubscription: Subscription;
-
-    init() {
-        this.userChangeSubscription = this.accountService
-            .getAuthenticationState()
-            .pipe(
-                // Fires on every event where the user has changed (login/logout).
-                distinctUntilChanged(),
-                tap(() => logoutSubject.next()),
-            )
-            .subscribe();
-    }
-
-    ngOnDestroy() {
-        this.userChangeSubscription?.unsubscribe();
-    }
 
     find(resultId: number, feedbackId: number): Observable<LongFeedbackResponse> {
         return this.http.get(`api/results/${resultId}/feedbacks/${feedbackId}/long-feedback`, { observe: 'response', responseType: 'text' });
