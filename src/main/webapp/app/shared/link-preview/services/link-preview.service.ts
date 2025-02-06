64de0d49953bee92e6e3ee28c8ca1ba14f797b02
Tobias Lippert <t.lippert@tum.de>
2025-02-06 20:29:42 +0100

    fix injections

--- src/main/webapp/app/shared/link-preview/services/link-preview.service.ts
+++ src/main/webapp/app/shared/link-preview/services/link-preview.service.ts
@@ -12,7 +12,7 @@ export interface LinkPreview {
     shouldPreviewBeShown?: boolean;
 }
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class LinkPreviewService {
     private http = inject(HttpClient);
 
