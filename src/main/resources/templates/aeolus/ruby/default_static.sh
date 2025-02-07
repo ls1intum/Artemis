c30947d8e79ef7f7b0a3efb0cdcfe1ca33b3f804
Marcel Gaupp <m.gaupp@tum.de>
2025-02-07 15:45:52 +0100

    Programming exercises: Add static code analysis for Ruby with integrated code lifecycle (#10204)

--- src/main/resources/templates/aeolus/ruby/default_static.sh
+++ src/main/resources/templates/aeolus/ruby/default_static.sh
@@ -7,6 +7,12 @@ install_dependencies () {
   bundler install
 }
 
+static_code_analysis () {
+  echo '⚙️ executing static_code_analysis'
+  cd "${testWorkingDirectory}"
+  bundler exec rake ci:rubocop
+}
+
 test () {
   echo '⚙️ executing test'
   cd "${testWorkingDirectory}"
@@ -21,6 +27,8 @@ main () {
   _script_name=${BASH_SOURCE[0]:-$0}
   cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; install_dependencies"
+  cd "${AEOLUS_INITIAL_DIRECTORY}"
+  bash -c "source ${_script_name} aeolus_sourcing; static_code_analysis"
   cd "${AEOLUS_INITIAL_DIRECTORY}"
   bash -c "source ${_script_name} aeolus_sourcing; test"
 }
