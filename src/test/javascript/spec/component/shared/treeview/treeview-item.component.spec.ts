996a2077aafe0c790cef557a4d4994a964e196f0
Michal Kawka <michal.kawka@gmx.com>
2025-02-07 09:45:52 +0100

    remove unused imports

--- src/test/javascript/spec/component/shared/treeview/treeview-item.component.spec.ts
+++ src/test/javascript/spec/component/shared/treeview/treeview-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, DebugElement, Input, TemplateRef } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
