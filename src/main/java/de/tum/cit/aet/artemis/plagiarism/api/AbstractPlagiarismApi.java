80fcaa45683feb508156cae782abee5ef756c718
Ole Vester <ole.vester@check24.de>
2025-02-08 20:11:36 +0100

    make AbstractPlagiarismApi actually an abstract class

--- src/main/java/de/tum/cit/aet/artemis/plagiarism/api/AbstractPlagiarismApi.java
+++ src/main/java/de/tum/cit/aet/artemis/plagiarism/api/AbstractPlagiarismApi.java
@@ -2,5 +2,5 @@ package de.tum.cit.aet.artemis.plagiarism.api;
 
 import de.tum.cit.aet.artemis.core.api.AbstractApi;
 
-public class AbstractPlagiarismApi implements AbstractApi {
+public abstract class AbstractPlagiarismApi implements AbstractApi {
 }
