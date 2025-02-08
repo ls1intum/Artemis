4b3538c0144c06387b5d2a784dbe6ea58679ef2b
Ole Vester <ole.vester@check24.de>
2025-02-08 20:10:13 +0100

    make ProgrammingLanguageNotSupportedForPlagiarismDetectionException public

--- src/main/java/de/tum/cit/aet/artemis/plagiarism/exceptions/ProgrammingLanguageNotSupportedForPlagiarismDetectionException.java
+++ src/main/java/de/tum/cit/aet/artemis/plagiarism/exceptions/ProgrammingLanguageNotSupportedForPlagiarismDetectionException.java
@@ -4,7 +4,7 @@ import de.tum.cit.aet.artemis.programming.domain.ProgrammingLanguage;
 
 public class ProgrammingLanguageNotSupportedForPlagiarismDetectionException extends Exception {
 
-    ProgrammingLanguageNotSupportedForPlagiarismDetectionException(ProgrammingLanguage language) {
+    public ProgrammingLanguageNotSupportedForPlagiarismDetectionException(ProgrammingLanguage language) {
         super("Artemis does not support plagiarism checks for the programming language " + language);
     }
 }
