20bb5b4d9150b05d6f98d28ff2280469c3b64c81
Ajayvir S. <ajayvirsingh11@hotmail.com>
2025-02-08 16:32:04 +0100

    add comment for config class

--- src/main/java/de/tum/cit/aet/artemis/core/config/BinaryFileExtensionConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/BinaryFileExtensionConfiguration.java
@@ -2,9 +2,10 @@ package de.tum.cit.aet.artemis.core.config;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+/**
+ * Provides a list of binary file extensions which can be used to filter returned files in the business logic of
+ * the application.
+ */
 public class BinaryFileExtensionConfiguration {
 
     private static final List<String> binaryFileExtensions = List.of(".exe", ".jar", ".dll", ".so", ".class", ".bin", ".msi", ".pyc", ".iso", ".o", ".app");
