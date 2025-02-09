ab7437d43f5f564191f55f41f6aa91d10e79acd5
Asli Aykan <asliaykan97@gmail.com>
2025-02-09 22:54:33 +0100

    add server test

--- src/main/java/de/tum/cit/aet/artemis/communication/dto/PostContextFilterDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/dto/PostContextFilterDTO.java
@@ -10,4 +10,11 @@ import de.tum.cit.aet.artemis.core.dto.SortingOrder;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PostContextFilterDTO(@NotBlank Long courseId, long[] courseWideChannelIds, Long plagiarismCaseId, Long conversationId, String searchText, Boolean filterToUnresolved,
         Boolean filterToOwn, Boolean filterToAnsweredOrReacted, PostSortCriterion postSortCriterion, SortingOrder sortingOrder, Boolean pinnedOnly) {
+
+    // Overloaded constructor to set pinnedOnly to false by default if not provided.
+    public PostContextFilterDTO(@NotBlank Long courseId, long[] courseWideChannelIds, Long plagiarismCaseId, Long conversationId, String searchText, Boolean filterToUnresolved,
+            Boolean filterToOwn, Boolean filterToAnsweredOrReacted, PostSortCriterion postSortCriterion, SortingOrder sortingOrder) {
+        this(courseId, courseWideChannelIds, plagiarismCaseId, conversationId, searchText, filterToUnresolved, filterToOwn, filterToAnsweredOrReacted, postSortCriterion,
+                sortingOrder, false);
+    }
 }
