7ace37ad086c701a568489789199a05510b1a6d3
Michal Kawka <michal.kawka@gmx.com>
2025-02-10 19:53:02 +0100

    remove guided-tour.module.ts

--- src/main/webapp/app/guided-tour/guided-tour.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { GuidedTourComponent } from './guided-tour.component';
-
-@NgModule({
-    imports: [GuidedTourComponent],
-    exports: [GuidedTourComponent],
-})
-export class GuidedTourModule {}
