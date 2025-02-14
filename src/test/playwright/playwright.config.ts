07ae4c0efd802fb303c5351798422bb99ea0e7b4
Murad Talibov <muradtalibov2@gmail.com>
2025-02-15 00:08:03 +0100

    Revert debugging additions

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -32,9 +32,6 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
         ignoreHTTPSErrors: true,
-        video: {
-            mode: 'on-first-retry',
-        },
     },
 
     /* Configure projects for fast and slow tests */
