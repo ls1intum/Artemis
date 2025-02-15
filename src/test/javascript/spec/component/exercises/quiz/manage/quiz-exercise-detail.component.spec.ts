87b51917e1629fb925d9b67e6e1bdd1c5cd16634
Tobias Lippert <t.lippert@tum.de>
2025-02-15 15:31:38 +0100

    fix quiz-exercise-detail.component.spec.ts

--- src/test/javascript/spec/component/exercises/quiz/manage/quiz-exercise-detail.component.spec.ts
+++ src/test/javascript/spec/component/exercises/quiz/manage/quiz-exercise-detail.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
-import { HttpResponse } from '@angular/common/http';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 import { TranslateService } from '@ngx-translate/core';
@@ -15,6 +15,7 @@ import { QuizExerciseDetailComponent } from 'app/exercises/quiz/manage/quiz-exer
 import dayjs, { Dayjs } from 'dayjs/esm';
 import { StatisticsService } from 'app/shared/statistics-graph/statistics.service';
 import { ExerciseManagementStatisticsDto } from 'app/exercises/shared/statistics/exercise-management-statistics-dto';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('QuizExercise Details Component', () => {
     let comp: QuizExerciseDetailComponent;
@@ -34,13 +35,14 @@ describe('QuizExercise Details Component', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [],
             providers: [
                 { provide: ActivatedRoute, useValue: route },
                 { provide: LocalStorageService, useClass: MockSyncStorage },
                 { provide: SessionStorageService, useClass: MockSyncStorage },
                 { provide: TranslateService, useClass: MockTranslateService },
                 { provide: AlertService, useClass: MockAlertService },
+                provideHttpClient(),
+                provideHttpClientTesting(),
             ],
         })
             .overrideTemplate(QuizExerciseDetailComponent, '')
