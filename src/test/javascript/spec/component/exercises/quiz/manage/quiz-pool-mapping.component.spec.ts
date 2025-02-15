d0a1d62265a6e6f51140f8d7cf88c86edabf0a35
Tobias Lippert <t.lippert@tum.de>
2025-02-15 21:28:23 +0100

    fix quiz-pool-mapping.component.spec.ts

--- src/test/javascript/spec/component/exercises/quiz/manage/quiz-pool-mapping.component.spec.ts
+++ src/test/javascript/spec/component/exercises/quiz/manage/quiz-pool-mapping.component.spec.ts
@@ -4,6 +4,8 @@ import { QuizPoolMappingComponent } from 'app/exercises/quiz/manage/quiz-pool-ma
 import { QuizGroup } from 'app/entities/quiz/quiz-group.model';
 import { MultipleChoiceQuestion } from 'app/entities/quiz/multiple-choice-question.model';
 import { provideHttpClient } from '@angular/common/http';
+import { MockTranslateService } from '../../../../helpers/mocks/service/mock-translate.service';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('QuizPoolMappingComponent', () => {
     let fixture: ComponentFixture<QuizPoolMappingComponent>;
@@ -11,8 +13,7 @@ describe('QuizPoolMappingComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [],
-            providers: [provideHttpClient(), provideHttpClientTesting()],
+            providers: [provideHttpClient(), provideHttpClientTesting(), { provide: TranslateService, useClass: MockTranslateService }],
         })
             .compileComponents()
             .then(() => {
