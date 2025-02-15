f2ac4f74942345dbdcc144c66dd8432e79f1c2e4
Tobias Lippert <t.lippert@tum.de>
2025-02-15 18:12:39 +0100

    fix participation.component.spec.ts

--- src/test/javascript/spec/component/participation/participation.component.spec.ts
+++ src/test/javascript/spec/component/participation/participation.component.spec.ts
@@ -1,5 +1,5 @@
 import { ActivatedRoute, Params } from '@angular/router';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse, provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ParticipationService } from 'app/exercises/shared/participation/participation.service';
 import { ParticipationComponent } from 'app/exercises/shared/participation/participation.component';
@@ -25,6 +25,10 @@ import { AlertService } from 'app/core/util/alert.service';
 import { MockAlertService } from '../../helpers/mocks/service/mock-alert.service';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
 import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { EventManager } from 'app/core/util/event-manager.service';
 
 describe('ParticipationComponent', () => {
     let component: ParticipationComponent;
@@ -45,7 +49,6 @@ describe('ParticipationComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [],
             providers: [
                 { provide: ActivatedRoute, useValue: route },
                 { provide: ProfileService, useClass: MockProfileService },
@@ -55,6 +58,10 @@ describe('ParticipationComponent', () => {
                 { provide: SessionStorageService, useClass: MockSyncStorage },
                 MockProvider(ExerciseService),
                 MockProvider(ParticipationService),
+                { provide: TranslateService, useClass: MockTranslateService },
+                MockProvider(EventManager),
+                provideHttpClient(),
+                provideHttpClientTesting(),
             ],
         })
             .compileComponents()
