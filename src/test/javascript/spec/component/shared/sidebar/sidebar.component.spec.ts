ea0b5979593791b26598b2a28bc6ed9bf0f2f42c
Tobias Lippert <t.lippert@tum.de>
2025-02-15 15:59:14 +0100

    fix sidebar.component.spec.ts

--- src/test/javascript/spec/component/shared/sidebar/sidebar.component.spec.ts
+++ src/test/javascript/spec/component/shared/sidebar/sidebar.component.spec.ts
@@ -10,7 +10,7 @@ import { By } from '@angular/platform-browser';
 import { MockComponent, MockDirective, MockModule, MockPipe, MockProvider } from 'ng-mocks';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MockRouterLinkDirective } from '../../../helpers/mocks/directive/mock-router-link.directive';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { SidebarCardElement, SidebarData } from 'app/types/sidebar';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
@@ -20,6 +20,11 @@ import { EventEmitter, input, runInInjectionContext } from '@angular/core';
 import { ExerciseCategory } from 'app/entities/exercise-category.model';
 import { ExerciseType } from 'app/entities/exercise.model';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { MockActivatedRoute } from '../../../helpers/mocks/activated-route/mock-activated-route';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ProfileService } from 'app/shared/layouts/profiles/profile.service';
+import { MockProfileService } from '../../../helpers/mocks/service/mock-profile.service';
 
 describe('SidebarComponent', () => {
     let component: SidebarComponent;
@@ -45,7 +50,13 @@ describe('SidebarComponent', () => {
                 MockPipe(ArtemisTranslatePipe),
                 MockRouterLinkDirective,
             ],
-            providers: [MockProvider(NgbModal)],
+            providers: [
+                MockProvider(NgbModal),
+                { provide: ActivatedRoute, useValue: new MockActivatedRoute() },
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                { provide: ProfileService, useClass: MockProfileService },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SidebarComponent);
