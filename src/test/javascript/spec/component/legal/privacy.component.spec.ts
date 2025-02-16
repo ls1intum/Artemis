20cc9c9e2acb93dd676e95790f5dead26c004024
Tobias Lippert <t.lippert@tum.de>
2025-02-16 16:51:04 +0100

    fix privacy.component.spec.ts

--- src/test/javascript/spec/component/legal/privacy.component.spec.ts
+++ src/test/javascript/spec/component/legal/privacy.component.spec.ts
@@ -1,7 +1,7 @@
 import { LegalDocumentService } from 'app/shared/service/legal-document.service';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { PrivacyComponent } from 'app/core/legal/privacy.component';
-import { MockDirective, MockPipe } from 'ng-mocks';
+import { MockDirective, MockPipe, MockProvider } from 'ng-mocks';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HtmlForMarkdownPipe } from 'app/shared/pipes/html-for-markdown.pipe';
 import { JhiLanguageHelper } from 'app/core/language/language.helper';
@@ -10,7 +10,10 @@ import { SessionStorageService } from 'ngx-webstorage';
 import { of } from 'rxjs';
 import { MockSyncStorage } from '../../helpers/mocks/service/mock-sync-storage.service';
 import { LegalDocumentLanguage } from 'app/entities/legal-document.model';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MockActivatedRoute } from '../../helpers/mocks/activated-route/mock-activated-route';
+import { AccountService } from 'app/core/auth/account.service';
+import { MockAccountService } from '../../helpers/mocks/service/mock-account.service';
 
 describe('PrivacyComponent', () => {
     let component: PrivacyComponent;
@@ -27,6 +30,9 @@ describe('PrivacyComponent', () => {
                     provide: SessionStorageService,
                     useClass: MockSyncStorage,
                 },
+                { provide: ActivatedRoute, useValue: new MockActivatedRoute() },
+                { provide: AccountService, useClass: MockAccountService },
+                MockProvider(LegalDocumentService),
             ],
         }).compileComponents();
         fixture = TestBed.createComponent(PrivacyComponent);
