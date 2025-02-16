4e46d862aecaebfaf04645e5e05937bb08a89c32
Tobias Lippert <t.lippert@tum.de>
2025-02-16 18:00:40 +0100

    fix create-tutorial-group-free-period.component.spec.ts

--- src/test/javascript/spec/component/tutorial-groups/tutorial-groups-management/tutorial-free-periods/crud/create-tutorial-group-free-period.component.spec.ts
+++ src/test/javascript/spec/component/tutorial-groups/tutorial-groups-management/tutorial-free-periods/crud/create-tutorial-group-free-period.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockProvider } from 'ng-mocks';
 import { AlertService } from 'app/core/util/alert.service';
 import { of } from 'rxjs';
-import { HttpResponse } from '@angular/common/http';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
 import { By } from '@angular/platform-browser';
 import { TutorialGroupSession } from 'app/entities/tutorial-group/tutorial-group-session.model';
 import { CreateTutorialGroupFreePeriodComponent } from 'app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/crud/create-tutorial-group-free-period/create-tutorial-group-free-period.component';
@@ -18,6 +18,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { TutorialGroupFreePeriodFormComponent } from '../../../../../../../../main/webapp/app/course/tutorial-groups/tutorial-groups-management/tutorial-free-periods/crud/tutorial-free-period-form/tutorial-group-free-period-form.component';
 import { MockTranslateService } from '../../../../../helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CreateTutorialGroupFreePeriodComponent', () => {
     let fixture: ComponentFixture<CreateTutorialGroupFreePeriodComponent>;
@@ -35,6 +36,8 @@ describe('CreateTutorialGroupFreePeriodComponent', () => {
                 MockProvider(AlertService),
                 MockProvider(NgbActiveModal),
                 { provide: TranslateService, useClass: MockTranslateService },
+                provideHttpClient(),
+                provideHttpClientTesting(),
             ],
         }).compileComponents();
         activeModal = TestBed.inject(NgbActiveModal);
