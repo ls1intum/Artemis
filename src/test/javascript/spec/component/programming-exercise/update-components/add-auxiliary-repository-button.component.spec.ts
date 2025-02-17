cc893762e33066b8b6c5b4d0f5b32f980ebe279c
Tobias Lippert <t.lippert@tum.de>
2025-02-17 13:11:46 +0100

    fix add-auxiliary-repository-button.component.spec.ts

--- src/test/javascript/spec/component/programming-exercise/update-components/add-auxiliary-repository-button.component.spec.ts
+++ src/test/javascript/spec/component/programming-exercise/update-components/add-auxiliary-repository-button.component.spec.ts
@@ -4,6 +4,8 @@ import { ButtonComponent } from 'app/shared/components/button.component';
 import { MockComponent } from 'ng-mocks';
 import { AddAuxiliaryRepositoryButtonComponent } from '../../../../../../main/webapp/app/exercises/programming/manage/update/add-auxiliary-repository-button.component';
 import { ProgrammingExercise } from '../../../../../../main/webapp/app/entities/programming/programming-exercise.model';
+import { FeatureToggleService } from 'app/shared/feature-toggle/feature-toggle.service';
+import { MockFeatureToggleService } from '../../../helpers/mocks/service/mock-feature-toggle.service';
 
 describe('AddAuxiliaryRepositoryButtonComponent', () => {
     let comp: AddAuxiliaryRepositoryButtonComponent;
@@ -12,7 +14,7 @@ describe('AddAuxiliaryRepositoryButtonComponent', () => {
     beforeEach(() => {
         return TestBed.configureTestingModule({
             imports: [TranslateModule.forRoot(), MockComponent(ButtonComponent)],
-            providers: [],
+            providers: [{ provide: FeatureToggleService, useClass: MockFeatureToggleService }],
         })
             .compileComponents()
             .then(() => {
