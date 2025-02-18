11af9e471c3d97408b9b3c578d342a4ce3c60132
Tobias Lippert <t.lippert@tum.de>
2025-02-18 19:50:38 +0100

    fix modeling-explanation-editor.component.spec.ts

--- src/test/javascript/spec/component/modeling-explanation-editor/modeling-explanation-editor.component.spec.ts
+++ src/test/javascript/spec/component/modeling-explanation-editor/modeling-explanation-editor.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ModelingExplanationEditorComponent } from 'app/exercises/modeling/shared/modeling-explanation-editor.component';
+import { MockTranslateService } from '../../helpers/mocks/service/mock-translate.service';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('ModelingExplanationEditorComponent', () => {
     let fixture: ComponentFixture<ModelingExplanationEditorComponent>;
@@ -8,7 +10,7 @@ describe('ModelingExplanationEditorComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [],
+            providers: [{ provide: TranslateService, useClass: MockTranslateService }],
         })
             .compileComponents()
             .then(() => {
