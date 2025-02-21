9d2cb2ea4c98e7ac9a7c55ea7e3ba4f729f03d12
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2025-02-21 12:35:48 +0100

    add javadoc comment

--- src/main/java/de/tum/cit/aet/artemis/exercise/domain/Exercise.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/domain/Exercise.java
@@ -906,6 +906,12 @@ public abstract class Exercise extends BaseExercise implements LearningObject {
                 .forEach(Collection::clear);
     }
 
+    /**
+     * Gets either the due date of this exercise, or the individual due date for the given participation, if it is set.
+     *
+     * @param participation The participation for which the individual due date is returned if it is set.
+     * @return The exercise due date, or the participation-specific due date, if it is set.
+     */
     public ZonedDateTime getDueDateForParticipation(StudentParticipation participation) {
         if (participation.getIndividualDueDate() != null) {
             return participation.getIndividualDueDate();
