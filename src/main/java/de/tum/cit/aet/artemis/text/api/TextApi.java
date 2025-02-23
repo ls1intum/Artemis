8b2134471a833976bc8dc312a706a2f50e0277f1
Ole Vester <ole.vester@check24.de>
2025-02-23 10:05:50 +0100

    remove unused field

--- src/main/java/de/tum/cit/aet/artemis/text/api/TextApi.java
+++ src/main/java/de/tum/cit/aet/artemis/text/api/TextApi.java
@@ -5,19 +5,15 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 
-import de.tum.cit.aet.artemis.text.repository.TextExerciseRepository;
 import de.tum.cit.aet.artemis.text.service.TextExerciseService;
 
 @Controller
 @Profile(PROFILE_CORE)
 public class TextApi extends AbstractTextApi {
 
-    private final TextExerciseRepository textExerciseRepository;
-
     private final TextExerciseService textExerciseService;
 
-    public TextApi(TextExerciseRepository textExerciseRepository, TextExerciseService textExerciseService) {
-        this.textExerciseRepository = textExerciseRepository;
+    public TextApi(TextExerciseService textExerciseService) {
         this.textExerciseService = textExerciseService;
     }
 
