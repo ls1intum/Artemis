20da65b8596aead0f7505cfd83f4d901ef42f863
jfr2102 <johannes@friedlein.eu>
2025-02-25 00:32:30 +0100

    remove low level RedisClient

--- src/main/java/de/tum/cit/aet/artemis/core/service/connectors/redis/RedisClientListResolver.java
+++ src/main/java/de/tum/cit/aet/artemis/core/service/connectors/redis/RedisClientListResolver.java
@@ -4,10 +4,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.redisson.client.RedisClient;
-import org.redisson.client.protocol.RedisCommands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.types.RedisClientInfo;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,25 +15,25 @@ public class RedisClientListResolver {
 
     private static final Logger log = LoggerFactory.getLogger(RedisClientListResolver.class);
 
-    // TODO: When https://github.com/redisson/redisson/issues/6108 is resolved, this is also possible with the following code
-    // redisConnectionFactory.getConnection().serverCommands().getClientList()
-    private final RedisClient redisClient;
+    private final RedisConnectionFactory connectionFactory;
 
-    public RedisClientListResolver(RedisClient redisClient) {
-        this.redisClient = redisClient;
+    public RedisClientListResolver(RedisConnectionFactory redisConnectionFactory) {
+        this.connectionFactory = redisConnectionFactory;
     }
 
     public Set<String> getUniqueClients() {
-
-        log.debug("Current redis client name: {}", redisClient.getConfig().getClientName());
-        List<String> clients = redisClient.connect().sync(RedisCommands.CLIENT_LIST);
+        List<RedisClientInfo> clients = connectionFactory.getConnection().serverCommands().getClientList();
 
         // Parse the Redis client list to extract names and filter duplicates
         Set<String> uniqueClients = new HashSet<>();
+        if (clients == null) {
+            log.error("Redis client list is null");
+            return uniqueClients;
+        }
 
-        for (String clientInfo : clients) {
-            String clientName = extractClientNameFromClientInfo(clientInfo);
-            if (clientName != null && clientName.toLowerCase().startsWith("artemis")) {
+        for (RedisClientInfo clientInfo : clients) {
+            String clientName = clientInfo.getName();
+            if (clientName.toLowerCase().startsWith("artemis")) {
                 // Optional: Apply more complex logic here to choose the most relevant connection
                 uniqueClients.add(clientName);
             }
@@ -43,16 +43,4 @@ public class RedisClientListResolver {
 
         return uniqueClients;
     }
-
-    // Helper method to extract the 'name' field from the Redis client info
-    private String extractClientNameFromClientInfo(String clientInfo) {
-        String[] parts = clientInfo.split(" ");
-        for (String part : parts) {
-            if (part.startsWith("name=")) {
-                return part.substring(5); // Extract the client name
-            }
-        }
-        return null; // Return null if no name is found
-    }
-
 }
