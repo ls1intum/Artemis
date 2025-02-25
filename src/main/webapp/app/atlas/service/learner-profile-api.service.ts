16a3316a4715e48bf65abfaf94cd3a3300472d90
Konrad Gößmann <konrad.goessmann@tum.de>
2025-02-25 17:07:55 +0100

    Fix code style in learner-profile-api service

--- src/main/webapp/app/atlas/service/learner-profile-api.service.ts
+++ src/main/webapp/app/atlas/service/learner-profile-api.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { BaseApiHttpService } from 'app/course/learning-paths/services/base-api-http.service';
 import { CourseLearnerProfileDTO } from 'app/entities/learner-profile.model';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class LearnerProfileApiService extends BaseApiHttpService {
     async getCourseLearnerProfilesForCurrentUser(): Promise<Record<number, CourseLearnerProfileDTO>> {
         return await this.get<Record<number, CourseLearnerProfileDTO>>('atlas/course-learner-profiles');
