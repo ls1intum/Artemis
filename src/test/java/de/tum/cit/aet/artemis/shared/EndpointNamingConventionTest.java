903a0e5723f94cf19b10e7a643d8644a9fbcb10e
Ole Vester <ole.vester@check24.de>
2025-02-27 17:36:12 +0100

    add an architecture test to validate the consistent endpoint prefix (module name)

---
+++ src/test/java/de/tum/cit/aet/artemis/shared/EndpointNamingConventionTest.java
@@ -0,0 +1,6 @@
+package de.tum.cit.aet.artemis.shared;
+
+import de.tum.cit.aet.artemis.shared.base.AbstractSpringIntegrationIndependentTest;
+
+public class EndpointNamingConventionTest extends AbstractSpringIntegrationIndependentTest {
+}
