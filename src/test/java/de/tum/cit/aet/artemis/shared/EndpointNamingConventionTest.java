22c6ab15edd71dda80e4d8abf569e6dd7f672c83
Ole Vester <ole.vester@check24.de>
2025-02-27 17:37:04 +0100

    delete unused class

--- src/test/java/de/tum/cit/aet/artemis/shared/EndpointNamingConventionTest.java
+++
@@ -1,6 +0,0 @@
-package de.tum.cit.aet.artemis.shared;
-
-import de.tum.cit.aet.artemis.shared.base.AbstractSpringIntegrationIndependentTest;
-
-public class EndpointNamingConventionTest extends AbstractSpringIntegrationIndependentTest {
-}
