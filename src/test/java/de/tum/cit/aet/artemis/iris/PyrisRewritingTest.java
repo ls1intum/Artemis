24fe12b522d5abbcd633b9b6fcf94c06408ab670
Tim Cremer <tim.cremer@tum.de>
2025-03-03 19:43:37 +0100

    Only add a single test

--- src/test/java/de/tum/cit/aet/artemis/iris/PyrisRewritingTest.java
+++ src/test/java/de/tum/cit/aet/artemis/iris/PyrisRewritingTest.java
@@ -5,12 +5,17 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_IRIS;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import de.tum.cit.aet.artemis.core.domain.Course;
 import de.tum.cit.aet.artemis.core.user.util.UserUtilService;
 import de.tum.cit.aet.artemis.core.util.CourseUtilService;
+import de.tum.cit.aet.artemis.iris.service.pyris.dto.data.PyrisRewriteTextRequestDTO;
+import de.tum.cit.aet.artemis.iris.service.pyris.dto.rewriting.RewritingVariant;
 import de.tum.cit.aet.artemis.programming.domain.ProgrammingExercise;
 
 @Profile(PROFILE_IRIS)
@@ -39,6 +44,15 @@ class PyrisRewritingTest extends AbstractIrisIntegrationTest {
         userUtilService.createAndSaveUser(TEST_PREFIX + "student42");
         userUtilService.createAndSaveUser(TEST_PREFIX + "tutor42");
         userUtilService.createAndSaveUser(TEST_PREFIX + "instructor42");
+    }
+
+    @Test
+    @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
+    void callRewritingPipeline() throws Exception {
+        irisRequestMockProvider.mockRunFaqRewritingResponse(dto -> {
+        });
+        PyrisRewriteTextRequestDTO requestDTO = new PyrisRewriteTextRequestDTO("test", RewritingVariant.FAQ);
+        request.postWithoutResponseBody("/api/courses/" + course1.getId() + "/rewrite-text", requestDTO, HttpStatus.OK);
     }
 
 }
