814b1dd5f1c0a82d18f02a10013e83a6e1381406
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-03-07 17:29:15 +0100

    revert

--- src/main/webapp/app/course/tutorial-groups/shared/tutorial-groups-table/tutorial-groups-table.component.ts
+++ src/main/webapp/app/course/tutorial-groups/shared/tutorial-groups-table/tutorial-groups-table.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, OnChanges, SimpleChanges, TemplateRef, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, Input, OnChanges, SimpleChanges, TemplateRef, inject } from '@angular/core';
 import { faQuestionCircle, faSort } from '@fortawesome/free-solid-svg-icons';
 import { TutorialGroup } from 'app/entities/tutorial-group/tutorial-group.model';
 import { SortService } from 'app/shared/service/sort.service';
@@ -27,15 +27,20 @@ export class TutorialGroupsTableComponent implements OnChanges {
 
     @ContentChild(TemplateRef, { static: true }) extraColumn: TemplateRef<any>;
 
-    showIdColumn = input(false);
+    @Input()
+    showIdColumn = false;
 
-    showChannelColumn = input(false);
+    @Input()
+    showChannelColumn = false;
 
-    tutorialGroups = input<TutorialGroup[]>([]);
+    @Input()
+    tutorialGroups: TutorialGroup[] = [];
 
-    course = input<Course>();
+    @Input()
+    course: Course;
 
-    timeZone = input<string | undefined>(undefined);
+    @Input()
+    timeZone?: string = undefined;
 
     timeZoneUsedForDisplay = dayjs.tz.guess();
 
@@ -64,11 +69,11 @@ export class TutorialGroupsTableComponent implements OnChanges {
 
     sortRows() {
         if (this.sortingPredicate === 'dayAndTime') {
-            this.sortService.sortByMultipleProperties(this.tutorialGroups(), ['tutorialGroupSchedule.dayOfWeek', 'tutorialGroupSchedule.startTime'], this.ascending);
+            this.sortService.sortByMultipleProperties(this.tutorialGroups, ['tutorialGroupSchedule.dayOfWeek', 'tutorialGroupSchedule.startTime'], this.ascending);
         } else if (this.sortingPredicate === 'capacityAndRegistrations') {
-            this.sortService.sortByMultipleProperties(this.tutorialGroups(), ['capacity', 'numberOfRegisteredUsers'], this.ascending);
+            this.sortService.sortByMultipleProperties(this.tutorialGroups, ['capacity', 'numberOfRegisteredUsers'], this.ascending);
         } else {
-            this.sortService.sortByProperty(this.tutorialGroups(), this.sortingPredicate, this.ascending);
+            this.sortService.sortByProperty(this.tutorialGroups, this.sortingPredicate, this.ascending);
         }
     }
 
@@ -87,13 +92,11 @@ export class TutorialGroupsTableComponent implements OnChanges {
                     case 'tutorialGroups':
                         {
                             if (change.currentValue && change.currentValue.length > 0) {
-                                this.tutorialGroupsSplitAcrossMultipleCampuses = this.tutorialGroups().some(
-                                    (tutorialGroup) => tutorialGroup.campus !== this.tutorialGroups()[0].campus,
+                                this.tutorialGroupsSplitAcrossMultipleCampuses = this.tutorialGroups.some(
+                                    (tutorialGroup) => tutorialGroup.campus !== this.tutorialGroups[0].campus,
                                 );
-                                this.mixOfOfflineAndOfflineTutorialGroups = this.tutorialGroups().some(
-                                    (tutorialGroup) => tutorialGroup.isOnline !== this.tutorialGroups()[0].isOnline,
-                                );
-                                this.mifOfDifferentLanguages = this.tutorialGroups().some((tutorialGroup) => tutorialGroup.language !== this.tutorialGroups()[0].language);
+                                this.mixOfOfflineAndOfflineTutorialGroups = this.tutorialGroups.some((tutorialGroup) => tutorialGroup.isOnline !== this.tutorialGroups[0].isOnline);
+                                this.mifOfDifferentLanguages = this.tutorialGroups.some((tutorialGroup) => tutorialGroup.language !== this.tutorialGroups[0].language);
                                 this.cdr.detectChanges();
                             }
                         }
