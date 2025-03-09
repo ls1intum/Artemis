13aa44c4e38f64ae8e37bb5137af58fbcb7b5124
Michal Kawka <73854755+coolchock@users.noreply.github.com>
2025-03-09 08:36:36 +0100

    Exam mode: Remove author from exam live events (#10422)

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/examevent/ProblemStatementUpdateEventDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/examevent/ProblemStatementUpdateEventDTO.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
  * A DTO for the {@link de.tum.cit.aet.artemis.exam.domain.event.ProblemStatementUpdateEvent} entity.
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ProblemStatementUpdateEventDTO(Long id, String createdBy, Instant createdDate, String text, String problemStatement, long exerciseId, String exerciseName)
+public record ProblemStatementUpdateEventDTO(Long id, Instant createdDate, String text, String problemStatement, long exerciseId, String exerciseName)
         implements ExamLiveEventBaseDTO {
 
 }
