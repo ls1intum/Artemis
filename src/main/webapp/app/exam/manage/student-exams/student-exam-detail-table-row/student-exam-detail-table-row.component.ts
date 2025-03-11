a1da950f59122833b6f6015c6c85a56bab17859c
Michal Kawka <michal.kawka@gmx.com>
2025-03-11 09:39:54 +0100

    extend the condition

--- src/main/webapp/app/exam/manage/student-exams/student-exam-detail-table-row/student-exam-detail-table-row.component.ts
+++ src/main/webapp/app/exam/manage/student-exams/student-exam-detail-table-row/student-exam-detail-table-row.component.ts
@@ -11,6 +11,7 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { RouterLink } from '@angular/router';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { AssessmentType } from 'app/entities/assessment-type.model';
 
 @Component({
     selector: '[jhi-student-exam-detail-table-row]',
@@ -118,4 +119,6 @@ export class StudentExamDetailTableRowComponent implements OnChanges {
                 return 0;
         }
     }
+
+    protected readonly AssessmentType = AssessmentType;
 }
