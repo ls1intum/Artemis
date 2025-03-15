7d8630c19d7aa30c6dd7ece5209f47f9628cb984
Florian Glombik <florian.glombik@tum.de>
2025-03-15 20:50:02 +0100

    Use computed signals instead of template calculations

--- src/main/webapp/app/detail-overview-list/components/programming-build-statistics/programming-build-statistics.component.ts
+++ src/main/webapp/app/detail-overview-list/components/programming-build-statistics/programming-build-statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { ProgrammingBuildStatisticsDetail } from 'app/detail-overview-list/detail.model';
 import { CommonModule } from '@angular/common';
@@ -10,4 +10,24 @@ import { CommonModule } from '@angular/common';
 })
 export class ProgrammingBuildStatisticsComponent {
     detail = input.required<ProgrammingBuildStatisticsDetail>();
+
+    agentSetupDuration = computed(() => {
+        const duration = this.detail().data.buildLogStatistics.agentSetupDuration;
+        return duration ? `${duration.toFixed(2)}s` : '-';
+    });
+
+    testDuration = computed(() => {
+        const duration = this.detail().data.buildLogStatistics.testDuration;
+        return duration ? `${duration.toFixed(2)}s` : '-';
+    });
+
+    scaDuration = computed(() => {
+        const duration = this.detail().data.buildLogStatistics.scaDuration;
+        return duration ? `${duration.toFixed(2)}s` : '-';
+    });
+
+    totalJobDuration = computed(() => {
+        const duration = this.detail().data.buildLogStatistics.totalJobDuration;
+        return duration ? `${duration.toFixed(2)}s` : '-';
+    });
 }
