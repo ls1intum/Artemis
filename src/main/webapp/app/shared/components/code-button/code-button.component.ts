a3b4f599e28ccbdd5cc49b9efb00259bb1fe3912
iyannsch <yannik.schmidt@tum.de>
2025-03-17 11:23:56 +0100

    dont double assign local variable

--- src/main/webapp/app/shared/components/code-button/code-button.component.ts
+++ src/main/webapp/app/shared/components/code-button/code-button.component.ts
@@ -441,18 +441,17 @@ export class CodeButtonComponent implements OnInit {
 
     private initTheia(profileInfo: ProfileInfo) {
         if (profileInfo.activeProfiles?.includes(PROFILE_THEIA) && this.exercise()) {
-            let exercise = this.exercise()!;
+            const exercise = this.exercise()!;
             // Theia requires the Build Config of the programming exercise to be set
             this.programmingExerciseService.getTheiaConfig(exercise.id!).subscribe((theiaConfig) => {
                 // Merge the theiaConfig (containing the theiaImage) into the buildConfig
                 this.exercise()!.buildConfig = { ...exercise.buildConfig, ...theiaConfig };
-                exercise = this.exercise()!;
 
                 // Set variables now, sanitize later on
                 this.theiaPortalURL = profileInfo.theiaPortalURL ?? '';
 
                 // Verify that all conditions are met
-                if (this.theiaPortalURL !== '' && exercise.allowOnlineIde && exercise.buildConfig?.theiaImage) {
+                if (this.theiaPortalURL !== '' && exercise.allowOnlineIde && theiaConfig.theiaImage) {
                     this.theiaEnabled = true;
                 }
             });
