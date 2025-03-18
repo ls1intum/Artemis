fd73539b36186b1083a45641c690f08c0eb94ade
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2025-03-18 12:32:29 +0100

    add missing documentation to enums

--- src/main/java/de/tum/cit/aet/artemis/exercise/domain/InitializationState.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/domain/InitializationState.java
@@ -1,15 +1,42 @@
 package de.tum.cit.aet.artemis.exercise.domain;
 
 /**
- * ToDo: Add javadoc for the remaining states, and explain the states better
- * The InitializationState enumeration.
- * UNINITIALIZED:
- * INITIALIZED: The participation is set up for submissions from the student
- * FINISHED: Text- / Modelling: At least one submission is done. Quiz: No further submissions should be possible
+ * Describes the state of a participation.
  */
 public enum InitializationState {
 
-    UNINITIALIZED(0), REPO_COPIED(1), REPO_CONFIGURED(2), INACTIVE(3), BUILD_PLAN_COPIED(4), BUILD_PLAN_CONFIGURED(5), INITIALIZED(6), FINISHED(7);
+    /**
+     * Student started the exercise, but repository/build plan are not yet set up.
+     */
+    UNINITIALIZED(0),
+    /**
+     * Template content was copied to the student repository.
+     */
+    REPO_COPIED(1),
+    /**
+     * Student repository is fully set up.
+     */
+    REPO_CONFIGURED(2),
+    /**
+     * Build plan in external CI system was cleaned up due to inactivity.
+     */
+    INACTIVE(3),
+    /**
+     * Template build plan was copied.
+     */
+    BUILD_PLAN_COPIED(4),
+    /**
+     * Build plan for the student participation is fully configured.
+     */
+    BUILD_PLAN_CONFIGURED(5),
+    /**
+     * Student can work on the repository and submit, i.e. push code.
+     */
+    INITIALIZED(6),
+    /**
+     * Student has submitted successfully to the exercise.
+     */
+    FINISHED(7);
 
     private final Integer stateNumber;
 
--- src/main/java/de/tum/cit/aet/artemis/exercise/domain/SubmissionType.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/domain/SubmissionType.java
@@ -1,12 +1,38 @@
 package de.tum.cit.aet.artemis.exercise.domain;
 
 /**
- * ToDo: Add javadoc for the other cases
- * MANUAL: submission triggered by a student
- * EXTERNAL: submission that was added through the add external submission dialogue
- * ILLEGAL: submission of an exam programming exercise that is submitted before the exercise start date or after the individual exercise end date
- * TEST: submission created for solution and template repositories after pushing to test repository
+ * Distinguishes between different kinds of submissions.
  */
 public enum SubmissionType {
-    MANUAL, TIMEOUT, TEST, OTHER, INSTRUCTOR, EXTERNAL, ILLEGAL
+    /**
+     * A submission triggered by a student.
+     */
+    MANUAL,
+    /**
+     * Quiz ended before the submission was manually sent to the server.
+     *
+     * <p>
+     * Automatic checkpoint of the last known state.
+     */
+    TIMEOUT,
+    /**
+     * Submission created for solution and template repositories after pushing to test repository.
+     */
+    TEST,
+    /**
+     * Submissions not covered by one of the more specific cases.
+     */
+    OTHER,
+    /**
+     * Commits to the solution, test, template repositories.
+     */
+    INSTRUCTOR,
+    /**
+     * Submission that was added through the add external submission dialogue.
+     */
+    EXTERNAL,
+    /**
+     * Submission of an exam programming exercise that is submitted before the exercise start date or after the individual exercise end date.
+     */
+    ILLEGAL
 }
