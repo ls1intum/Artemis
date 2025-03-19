916a9cd9cbbb270d85d604c7f82dd74438feebfc
raphaelklein <raphael.klein@tum.de>
2025-03-19 18:01:03 +0100

    Add MultiLaunch support for lti.service.ts

--- src/main/webapp/app/shared/service/lti.service.ts
+++ src/main/webapp/app/shared/service/lti.service.ts
@@ -8,7 +8,14 @@ export class LtiService {
     private shownViaLtiSubject = new BehaviorSubject<boolean>(false);
     isShownViaLti$ = this.shownViaLtiSubject.asObservable();
 
+    private multiLaunchSubject = new BehaviorSubject<boolean>(false);
+    isMultiLaunch$ = this.multiLaunchSubject.asObservable();
+
     setShownViaLti(shownViaLti: boolean) {
         this.shownViaLtiSubject.next(shownViaLti);
+    }
+
+    setMultiLaunch(isMultiLaunch: boolean) {
+        this.multiLaunchSubject.next(isMultiLaunch);
     }
 }
