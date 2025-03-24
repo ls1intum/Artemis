6303d30b97ecbad88e36143f59c32f09dedb280f
Asli Aykan <asliaykan97@gmail.com>
2025-03-24 22:30:01 +0100

    update postingdto

--- src/main/java/de/tum/cit/aet/artemis/communication/dto/PostingDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/dto/PostingDTO.java
@@ -12,12 +12,12 @@ import de.tum.cit.aet.artemis.communication.domain.UserRole;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PostingDTO(Long id, AuthorDTO author, UserRole role, ZonedDateTime creationDate, ZonedDateTime updatedDate, String content, boolean isSaved, short savedPostStatus,
-        List<ReactionDTO> reactions, PostingConversationDTO conversation, short postingType, Long referencePostId) {
+        List<ReactionDTO> reactions, PostingConversationDTO conversation, short postingType, Long referencePostId, boolean hasForwardedMessages) {
 
     public PostingDTO(Posting post, boolean isSaved, short savedPostStatus) {
         this(post.getId(), new AuthorDTO(post.getAuthor()), post.getAuthorRole(), post.getCreationDate(), post.getUpdatedDate(), post.getContent(), isSaved, savedPostStatus,
                 post.getReactions().stream().map(ReactionDTO::new).toList(), new PostingConversationDTO(post.getConversation()), getSavedPostType(post).getDatabaseKey(),
-                getReferencePostId(post));
+                getReferencePostId(post), post.getHasForwardedMessages());
     }
 
     static PostingType getSavedPostType(Posting posting) {
