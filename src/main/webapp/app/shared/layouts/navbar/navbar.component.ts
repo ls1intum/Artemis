1ad637c7f39bfe861e66eba3f6f9b8a5a94082b0
Simon Entholzer <33342534+SimonEntholzer@users.noreply.github.com>
2025-03-24 10:54:50 +0100

    Programming exercises: Fix repository breadcrumbs (#10383)

--- src/main/webapp/app/shared/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/shared/layouts/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnDestroy, OnInit, inject } from '@angular/core';
 import { AccountService } from 'app/core/auth/account.service';
+import { RepositoryType } from 'app/exercises/programming/shared/code-editor/model/code-editor.model';
 import { HasAnyAuthorityDirective } from 'app/shared/auth/has-any-authority.directive';
 import { Subscription } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
@@ -407,6 +408,12 @@ export class NavbarComponent implements OnInit, OnDestroy {
         prerequisites: 'artemisApp.prerequisite.title',
         import_standardized: 'artemisApp.standardizedCompetency.courseImport.title',
         cleanup_service: 'cleanupService.title',
+        user_repository: 'artemisApp.repository.userRepository.title',
+        template_repository: 'artemisApp.repository.templateRepository.title',
+        solution_repository: 'artemisApp.repository.solutionRepository.title',
+        tests_repository: 'artemisApp.repository.testsRepository.title',
+        auxiliary_repository: 'artemisApp.repository.auxiliaryRepository.title',
+        vcs_access_log: 'artemisApp.repository.vcsAccessLog.title',
     };
 
     studentPathBreadcrumbTranslations: { [key: string]: string } = {
@@ -511,7 +518,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
             // Displays the path segment as breadcrumb (no other title exists)
             case 'system-notification-management':
             case 'teams':
-            case 'repository':
             case 'code-editor':
                 this.addBreadcrumb(currentPath, segment, false);
                 break;
@@ -523,6 +529,12 @@ export class NavbarComponent implements OnInit, OnDestroy {
                 // Special case: A raw /course-management/XXX/exercises/XXX doesn't work, we need to add the exercise type
                 // For example /course-management/XXX/programming-exercises/XXX
                 this.addExerciseCrumb(Number(segment), currentPath);
+                break;
+            case 'USER':
+                this.addTranslationAsCrumb(currentPath, 'user_repository');
+                break;
+            case 'AUXILIARY':
+                this.addTranslationAsCrumb(currentPath, 'auxiliary_repository');
                 break;
             case 'text-exercises':
             case 'modeling-exercises':
@@ -622,6 +634,7 @@ export class NavbarComponent implements OnInit, OnDestroy {
             case 'reset':
             case 'groups':
             case 'code-editor':
+            case 'repository':
             case 'admin':
             case 'ide':
             case 'text-units':
@@ -673,6 +686,9 @@ export class NavbarComponent implements OnInit, OnDestroy {
                 } else if (this.lastRouteUrlSegment === 'exams' && segment === 'import') {
                     // This route is only used internally when opening the exam import modal and therefore shouldn't be displayed.
                     // When opening the exam-update.component, the id of the to be imported exam is appended (-> case `import`).
+                    break;
+                } else if (this.lastRouteUrlSegment === 'repository') {
+                    this.addRepositoryViewBreadcrumb(currentPath, segment);
                     break;
                 }
 
@@ -950,6 +966,29 @@ export class NavbarComponent implements OnInit, OnDestroy {
                 break;
             case EntityType.LECTURE:
                 this.lectureTitle = title;
+                break;
+        }
+    }
+
+    /**
+     * Sets the breadcrumb for the template, tests and solution repository view. User and Auxiliary repositories are not treated here
+     * as they have an ID after the repository type.
+     *
+     * @param currentPath    The path that should get used for the breadcrumb
+     * @param repositoryType The type of the repository
+     */
+    private addRepositoryViewBreadcrumb(currentPath: string, repositoryType: string) {
+        switch (repositoryType) {
+            case RepositoryType.TEMPLATE:
+                this.addTranslationAsCrumb(currentPath, 'template_repository');
+                break;
+            case RepositoryType.TESTS:
+                this.addTranslationAsCrumb(currentPath, 'tests_repository');
+                break;
+            case RepositoryType.SOLUTION:
+                this.addTranslationAsCrumb(currentPath, 'solution_repository');
+                break;
+            default:
                 break;
         }
     }
