ce1e39c28569228066bf1bcb014557074d3ba1d5
Christoph Knoedlseder <knoedlsede@fim.uni-passau.de>
2025-03-25 21:48:41 +0100

    fix import

--- src/main/webapp/app/programming/manage/update/update-components/version-control/programming-exercise-version-control.component.ts
+++ src/main/webapp/app/programming/manage/update/update-components/version-control/programming-exercise-version-control.component.ts
@@ -1,9 +1,9 @@
 import { Component, input } from '@angular/core';
-import { ProgrammingExercise } from 'app/entities/programming/programming-exercise.model';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HelpIconComponent } from 'app/shared/components/help-icon.component';
 import { InputFieldEditModeMapping } from 'app/programming/manage/update/programming-exercise-update.helper';
+import { ProgrammingExercise } from 'app/programming/shared/entities/programming-exercise.model';
 
 @Component({
     selector: 'jhi-programming-exercise-version-control',
