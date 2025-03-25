2f528d75a535fc42b75d5f7c3e4a913c06170dd7
Stephan Krusche <krusche@tum.de>
2025-03-25 16:43:37 +0100

    fix security config

--- src/main/java/de/tum/cit/aet/artemis/core/security/filter/SpaWebFilter.java
+++ src/main/java/de/tum/cit/aet/artemis/core/security/filter/SpaWebFilter.java
@@ -9,13 +9,24 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
+/**
+ * Filter to redirect all requests to the Single Page Application (SPA) index.html (we are not talking about wellness here 😉).
+ */
 public class SpaWebFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String path = request.getRequestURI();
-        if (!path.startsWith("/api") && !path.startsWith("/management") && !path.startsWith("/time") && !path.startsWith("/public") && !path.startsWith("/websocket")
-                && !path.startsWith("/git") && !path.contains(".") && path.matches("/(.*)")) {
+        // @formatter:off
+        if (!path.startsWith("/api") &&
+            !path.startsWith("/management") &&
+            !path.startsWith("/time") &&
+            !path.startsWith("/public") &&
+            !path.startsWith("/websocket") &&
+            !path.startsWith("/git") &&
+            !path.startsWith("/webauthn") && // exclude webauthn (passkey) from spa
+            !path.contains(".") &&
+            path.matches("/(.*)")) {
             request.getRequestDispatcher("/").forward(request, response);
             return;
         }
