7d60f265d85a6ced07ec922d8154d3360970068b
Florian Glombik <florian.glombik@tum.de>
2025-03-29 13:18:09 +0100

    Use enum in database

--- src/main/java/de/tum/cit/aet/artemis/core/domain/Transports.java
+++ src/main/java/de/tum/cit/aet/artemis/core/domain/Transports.java
@@ -2,6 +2,8 @@ package de.tum.cit.aet.artemis.core.domain;
 
 /**
  * Transport types of {@link PasskeyCredentials}
+ * <br>
+ * <b>Important: The enum types must match the database table definition passkey_credentials.transports <i>(case-sensitive)</i></b>
  */
 public enum Transports {
     USB, NFC, BLE, INTERNAL, HYBRID
