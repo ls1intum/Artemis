b0d1338c3fd856a18047b29eefd0124d2425c97d
Ole Vester <ole.vester@check24.de>
2025-04-01 16:39:19 +0200

    fix cclient compilation

--- src/test/javascript/spec/component/shared/competency-selection.component.spec.ts
+++ src/test/javascript/spec/component/shared/competency-selection.component.spec.ts
@@ -15,7 +15,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { MockAccountService } from '../../helpers/mocks/service/mock-account.service';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ProfileService } from '../../../../../main/webapp/app/core/layouts/profiles/shared/profile.service';
-import { ProfileInfo } from '../../../../../main/webapp/app/shared/layouts/profiles/profile-info.model';
+import { ProfileInfo } from 'app/core/layouts/profiles/profile-info.model';
 import { MockProfileService } from '../../helpers/mocks/service/mock-profile.service';
 import { MODULE_FEATURE_ATLAS } from '../../../../../main/webapp/app/app.constants';
 import { CompetencySelectionComponent } from 'app/atlas/shared/competency-selection/competency-selection.component';
