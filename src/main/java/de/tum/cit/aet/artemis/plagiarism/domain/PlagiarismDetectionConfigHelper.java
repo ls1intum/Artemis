d90fc39a744346d04f0ffb502bdb84f4d77cc753
Ole Vester <ole.vester@check24.de>
2025-04-01 20:50:22 +0200

    re-add private constructor and add exclusion to ObjectMethodTest

--- src/main/java/de/tum/cit/aet/artemis/plagiarism/domain/PlagiarismDetectionConfigHelper.java
+++ src/main/java/de/tum/cit/aet/artemis/plagiarism/domain/PlagiarismDetectionConfigHelper.java
@@ -17,6 +17,9 @@ public final class PlagiarismDetectionConfigHelper {
 
     private static final Logger log = LoggerFactory.getLogger(PlagiarismDetectionConfigHelper.class);
 
+    private PlagiarismDetectionConfigHelper() {
+    }
+
     /**
      * Ads missing plagiarism checks config for course exercises.
      *
