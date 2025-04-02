b2c153ce70afebd90585b989aa79322d3b287868
Ole Vester <ole.vester@check24.de>
2025-04-02 14:32:28 +0200

    add ExamApiNotPresentException to ignored class in ExamApiArchitectureTest

--- src/test/java/de/tum/cit/aet/artemis/exam/architecture/ExamApiArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/exam/architecture/ExamApiArchitectureTest.java
@@ -1,5 +1,8 @@
 package de.tum.cit.aet.artemis.exam.architecture;
 
+import java.util.Set;
+
+import de.tum.cit.aet.artemis.exam.config.ExamApiNotPresentException;
 import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleAccessArchitectureTest;
 
 class ExamApiArchitectureTest extends AbstractModuleAccessArchitectureTest {
@@ -7,5 +10,10 @@ class ExamApiArchitectureTest extends AbstractModuleAccessArchitectureTest {
     @Override
     public String getModulePackage() {
         return ARTEMIS_PACKAGE + ".exam";
+    }
+
+    @Override
+    protected Set<Class<?>> getIgnoredClasses() {
+        return Set.of(ExamApiNotPresentException.class);
     }
 }
