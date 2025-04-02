3da2e29e32c43b028a323acbfcfc9ba8bad79590
Ole Vester <ole.vester@check24.de>
2025-04-02 15:03:50 +0200

    javadoc

--- src/main/java/de/tum/cit/aet/artemis/exam/config/ExamApiNotPresentException.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/config/ExamApiNotPresentException.java
@@ -5,8 +5,14 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 import de.tum.cit.aet.artemis.core.api.AbstractApi;
 import de.tum.cit.aet.artemis.core.exception.ApiProfileNotPresentException;
 
+/**
+ * Exception to be thrown when the exam API is not present.
+ */
 public class ExamApiNotPresentException extends ApiProfileNotPresentException {
 
+    /**
+     * @param api the api class that should be present
+     */
     public ExamApiNotPresentException(Class<? extends AbstractApi> api) {
         super(api, PROFILE_CORE);
     }
