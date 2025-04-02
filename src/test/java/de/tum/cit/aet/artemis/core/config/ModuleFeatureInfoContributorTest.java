f2ce607eb3553e6a47235f63a00c2fe6a3ca0e63
Ole Vester <ole.vester@check24.de>
2025-04-03 00:11:00 +0200

    turn of formatter for property lists in tests

--- src/test/java/de/tum/cit/aet/artemis/core/config/ModuleFeatureInfoContributorTest.java
+++ src/test/java/de/tum/cit/aet/artemis/core/config/ModuleFeatureInfoContributorTest.java
@@ -11,15 +11,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.core.env.Environment;
 
+/**
+ * Test for {@link ModuleFeatureInfoContributor}.
+ */
 class ModuleFeatureInfoContributorTest {
 
     private final Environment mockEnv = mock(Environment.class);
 
-    private static final List<String> modulePropertyNames = List.of(Constants.ATLAS_ENABLED_PROPERTY_NAME);
+    // @formatter:off
+    private static final List<String> modulePropertyNames = List.of(
+        Constants.ATLAS_ENABLED_PROPERTY_NAME
+    );
+    // @formatter:on
+
+    // @formatter:off
+    private static final List<String> moduleFeatures = List.of(
+        Constants.MODULE_FEATURE_ATLAS
+    );
+    // @formatter:on
 
     @Test
     void testEnabledContribution() {
-        testContribution(true, List.of(Constants.MODULE_FEATURE_ATLAS));
+        testContribution(true, moduleFeatures);
     }
 
     @Test
