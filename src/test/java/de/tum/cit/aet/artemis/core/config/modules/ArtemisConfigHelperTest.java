cf50730e2c34a380ceffdd70639d8d31d02936af
Ole Vester <ole.vester@check24.de>
2025-04-04 14:50:06 +0200

    add test for fileupload property in ArtemisConfigHelperTest

--- src/test/java/de/tum/cit/aet/artemis/core/config/modules/ArtemisConfigHelperTest.java
+++ src/test/java/de/tum/cit/aet/artemis/core/config/modules/ArtemisConfigHelperTest.java
@@ -27,6 +27,11 @@ class ArtemisConfigHelperTest {
         testProperty(artemisConfigHelper::isAtlasEnabled, Constants.ATLAS_ENABLED_PROPERTY_NAME);
     }
 
+    @Test
+    void testFileuploadProperty() {
+        testProperty(artemisConfigHelper::isFileuploadExerciseEnabled, Constants.FILEUPLOAD_ENABLED_PROPERTY_NAME);
+    }
+
     private void testProperty(Function<Environment, Boolean> propertyTest, String propertyName) {
         mockProperty(propertyName, true);
         assertThat(propertyTest.apply(mockEnv)).isTrue();
