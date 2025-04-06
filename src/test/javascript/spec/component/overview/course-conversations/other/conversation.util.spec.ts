e5b0279977eca2aa5a2ccdf6e5ff52bb5c907070
Tobias Lippert <t.lippert@tum.de>
2025-04-06 17:08:53 +0200

    remove moved test files

--- src/test/javascript/spec/component/overview/course-conversations/other/conversation.util.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getUserLabel } from 'app/communication/course-conversations-components/other/conversation.util';
-
-describe('ConversationUtil', () => {
-    it('should return the correct user label', () => {
-        const user = { firstName: 'John', lastName: 'Doe', login: 'johndoe' };
-        expect(getUserLabel(user)).toBe('John Doe (johndoe)');
-    });
-});
