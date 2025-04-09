23f5f6f310224151043e30b4192c4234721928e4
Florian Glombik <florian.glombik@tum.de>
2025-04-09 17:46:19 +0200

    Remove ConfigurationProperties annotation

--- src/main/java/de/tum/cit/aet/artemis/core/service/PasskeyInfoContributor.java
+++ src/main/java/de/tum/cit/aet/artemis/core/service/PasskeyInfoContributor.java
@@ -5,7 +5,6 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -15,7 +14,6 @@ import de.tum.cit.aet.artemis.core.config.Constants;
 @Profile(PROFILE_CORE)
 @Component
 @EnableConfigurationProperties
-@ConfigurationProperties("artemis.user-management.passkey")
 public class PasskeyInfoContributor implements InfoContributor {
 
     @Value("${artemis.user-management.passkey.enabled:false}")
