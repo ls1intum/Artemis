a5c87a007dd04e9c9c2b2ba9b7481d9979dc7dea
Florian Glombik <florian.glombik@tum.de>
2025-04-10 23:23:42 +0200

    Fix server style check that fails because of me

--- src/main/java/de/tum/cit/aet/artemis/core/dto/PasskeyDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/PasskeyDTO.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import jakarta.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import de.tum.cit.aet.artemis.core.dto.validator.Base64Url;
 
 /**
  * DTO for passkey information.
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PasskeyDTO(@NotNull @Base64Url String credentialId, String label, Instant created, Instant lastUsed) {
 }
