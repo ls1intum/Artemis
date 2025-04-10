3298460e72ed6b8ed61003904d690ab5d56d1c15
Florian Glombik <florian.glombik@tum.de>
2025-04-10 16:03:20 +0200

    Address review: return null instead of throwing exception

--- src/main/java/de/tum/cit/aet/artemis/core/domain/converter/AuthenticatorTransportConverter.java
+++ src/main/java/de/tum/cit/aet/artemis/core/domain/converter/AuthenticatorTransportConverter.java
@@ -1,16 +1,21 @@
 package de.tum.cit.aet.artemis.core.domain.converter;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.web.webauthn.api.AuthenticatorTransport;
 
 @Converter(autoApply = true)
 public class AuthenticatorTransportConverter implements AttributeConverter<Set<AuthenticatorTransport>, String> {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthenticatorTransportConverter.class);
 
     @Override
     public String convertToDatabaseColumn(Set<AuthenticatorTransport> attribute) {
@@ -25,6 +30,14 @@ public class AuthenticatorTransportConverter implements AttributeConverter<Set<A
         if (dbData == null || dbData.isBlank()) {
             return Set.of();
         }
-        return Arrays.stream(dbData.split(",")).map(AuthenticatorTransport::valueOf).collect(Collectors.toSet());
+        return Arrays.stream(dbData.split(",")).map(value -> {
+            try {
+                return AuthenticatorTransport.valueOf(value);
+            }
+            catch (IllegalArgumentException e) {
+                log.debug("Invalid AuthenticatorTransport value: {}", value);
+                return null;
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toSet());
     }
 }
