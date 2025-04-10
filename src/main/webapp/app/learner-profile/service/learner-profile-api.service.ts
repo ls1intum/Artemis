ed23fdbf01723a24dbd255aec201c03a12a85a65
ahmetsenturk <iahmetsenturk@yahoo.com>
2025-04-10 12:25:01 +0300

    add learner profile dto and service to webapp

---
+++ src/main/webapp/app/learner-profile/service/learner-profile-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { LearnerProfileDTO } from 'app/learner-profile/shared/entities/learner-profile.model';
+import { BaseApiHttpService } from 'app/shared/service/base-api-http.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class LearnerProfileApiService extends BaseApiHttpService {
+    private http = inject(HttpClient);
+    private resourceUrl = 'atlas/learner-profile';
+
+    getLearnerProfileForCurrentUser(): Observable<LearnerProfileDTO> {
+        return this.http.get<LearnerProfileDTO>(`${this.resourceUrl}`);
+    }
+
+    putUpdatedLearnerProfile(learnerProfile: LearnerProfileDTO): Observable<LearnerProfileDTO> {
+        return this.http.put<LearnerProfileDTO>(`${this.resourceUrl}`, learnerProfile);
+    }
+}
---
+++ src/main/webapp/app/learner-profile/shared/entities/learner-profile.model.ts
@@ -0,0 +1,6 @@
+export class LearnerProfileDTO {
+    public feedbackPracticalTheoretical: number;
+    public feedbackCreativeGuidance: number;
+    public feedbackFollowupSummary: number;
+    public feedbackBriefDetailed: number;
+}
