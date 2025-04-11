c1690d31beb903344da6354a427c7e3fa9ef2b40
Michael Breu <Michael.Breu@uibk.ac.at>
2025-04-11 10:09:51 +0200

    More Tests

--- src/test/java/de/tum/cit/aet/artemis/programming/service/sharing/SharingInfoDTOTest.java
+++ src/test/java/de/tum/cit/aet/artemis/programming/service/sharing/SharingInfoDTOTest.java
@@ -2,6 +2,10 @@ package de.tum.cit.aet.artemis.programming.service.sharing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import de.tum.cit.aet.artemis.core.dto.SharingInfoDTO;
@@ -44,4 +48,27 @@ public class SharingInfoDTOTest {
         si.setChecksum("Not relevant for equals");
         assertThat(si.equals(si2)).isTrue();
     }
+
+    @Test
+    void testVariousOnSharingZipFile() throws IOException {
+        final String testZipName = "testZip";
+        try (SharingMultipartZipFile sharingZip = new SharingMultipartZipFile(testZipName, this.getClass().getResource("./basket/sampleExercise.zip").openStream());) {
+            assertThat(sharingZip.getName()).isEqualTo(testZipName);
+            assertThat(sharingZip.getContentType()).isEqualTo("application/zip");
+            assertThat(sharingZip.getInputStream()).isNotNull();
+            assertThat(sharingZip.isEmpty()).isFalse();
+            assertThat(sharingZip.getSize()).isGreaterThan(0);
+            File tmpFile = File.createTempFile("zipTest", "zip");
+            sharingZip.transferTo(tmpFile);
+            assertThat(tmpFile.length()).isGreaterThan(0);
+            assertThat(sharingZip.getSize()).isEqualTo(0);
+        }
+    }
+
+    @Test
+    void testWeirdUseOfSharingZipFile() throws IOException {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SharingMultipartZipFile(null, this.getClass().getResource("./basket/sampleExercise.zip").openStream()));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SharingMultipartZipFile("no stream", null));
+    }
+
 }
