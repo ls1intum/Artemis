cdaeacace20b30155448b108a5e3095265626651
Ole Vester <ole.vester@check24.de>
2025-04-15 00:20:56 +0200

    remove unused method (merge aftermath)

--- src/main/webapp/app/core/layouts/profiles/shared/profile.service.ts
+++ src/main/webapp/app/core/layouts/profiles/shared/profile.service.ts
@@ -46,12 +46,4 @@ export class ProfileService {
     public isTestServer(): boolean {
         return this.profileInfo.testServer ?? false;
     }
-
-    /**
-     * Check if the given module feature is active.
-     * @param moduleFeatureName The module feature to check.
-     */
-    public isFeatureActive(moduleFeatureName: string): boolean {
-        return this.profileInfo?.value?.activeModuleFeatures?.includes(moduleFeatureName) ?? false;
-    }
 }
