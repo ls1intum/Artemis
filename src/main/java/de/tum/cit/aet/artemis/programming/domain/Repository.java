bc9a8c2e1b1fe81d6e6599953d3ad24f4f9fef10
Erik Stern <erik.stern@outlook.de>
2025-04-15 12:23:11 +0200

    Fix formatting.

--- src/main/java/de/tum/cit/aet/artemis/programming/domain/Repository.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/domain/Repository.java
@@ -9,11 +9,11 @@ import org.eclipse.jgit.lib.BaseRepositoryBuilder;
 import de.tum.cit.aet.artemis.programming.service.localvc.LocalVCRepositoryUri;
 
 /**
- * This class represents repositories cloned from the VC system to Artemis to then be used in the online editor. These
- * repositories are cloned from the remote VCS and are saved in the folder defined in the application properties at
- * artemis.repo-clone-path. Note: This class does not represent local VCS repositories. The local VCS is treated as a
- * remote VCS in code and is represented by the {@link LocalVCRepositoryUri} class. Its repositories are saved as bare
- * repositories in the folder defined in the application properties at artemis.version-control.local-vcs-repo-path.
+ * This class represents repositories cloned from the VC system to Artemis to then be used in the online editor.
+ * These repositories are cloned from the remote VCS and are saved in the folder defined in the application properties at artemis.repo-clone-path.
+ * Note: This class does not represent local VCS repositories. The local VCS is treated as a remote VCS in code and is represented by the
+ * {@link LocalVCRepositoryUri} class.
+ * Its repositories are saved as bare repositories in the folder defined in the application properties at artemis.version-control.local-vcs-repo-path.
  */
 public class Repository extends org.eclipse.jgit.internal.storage.file.FileRepository {
 
@@ -42,9 +42,8 @@ public class Repository extends org.eclipse.jgit.internal.storage.file.FileRepos
     }
 
     /**
-     * Check if the file is allowed in this repository. This checks if the path of the file is a subdirectory of the
-     * repository directory. Also checks that the ../ operator is not used to traverse up directories on the server.
-     * Also checks that the file is not using the ".git" folder
+     * Check if the file is allowed in this repository. This checks if the path of the file is a subdirectory of the repository directory. Also checks that the ../ operator is not
+     * used to traverse up directories on the server. Also checks that the file is not using the ".git" folder
      *
      * @param file for which to check if it is valid.
      * @return true if the file is valid.
