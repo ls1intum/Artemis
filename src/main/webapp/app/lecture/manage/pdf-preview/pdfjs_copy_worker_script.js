de1cb00572b6a354d3672c19d97d74ad64510bde
marlonnienaber <marlonnienaber@web.de>
2025-04-17 16:59:02 +0200

    avoid require

--- src/main/webapp/app/lecture/manage/pdf-preview/pdfjs_copy_worker_script.js
+++ src/main/webapp/app/lecture/manage/pdf-preview/pdfjs_copy_worker_script.js
@@ -4,12 +4,13 @@
  * This script is called in the postinstall lifecycle hook in package.json.
  */
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const source = path.resolve('node_modules/pdfjs-dist/build/pdf.worker.min.mjs');
 const destinationDir = path.resolve('src/main/webapp/content/scripts');
 const destination = path.join(destinationDir, 'pdf.worker.min.mjs');
 
 fs.mkdirSync(destinationDir, { recursive: true });
+
 fs.copyFileSync(source, destination);
