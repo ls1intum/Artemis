9e331acc95a95c0f4e7538c3dc43b1b70e95fa5d
marlonnienaber <marlonnienaber@web.de>
2025-04-17 16:53:25 +0200

    minor adaptations to script

--- src/main/webapp/app/lecture/manage/pdf-preview/pdfjs_copy_worker_script.js
+++ src/main/webapp/app/lecture/manage/pdf-preview/pdfjs_copy_worker_script.js
@@ -4,13 +4,12 @@
  * This script is called in the postinstall lifecycle hook in package.json.
  */
 
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const source = path.resolve('node_modules/pdfjs-dist/build/pdf.worker.min.mjs');
 const destinationDir = path.resolve('src/main/webapp/content/scripts');
 const destination = path.join(destinationDir, 'pdf.worker.min.mjs');
 
 fs.mkdirSync(destinationDir, { recursive: true });
-
 fs.copyFileSync(source, destination);
