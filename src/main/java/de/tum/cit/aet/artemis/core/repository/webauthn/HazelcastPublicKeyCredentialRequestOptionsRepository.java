105f571d0c9abeffd24a40ad009f0f3f2e4ce668
Florian Glombik <florian.glombik@tum.de>
2025-04-17 20:14:01 +0200

    Try to fix passkeys on testserver - improve logging

--- src/main/java/de/tum/cit/aet/artemis/core/repository/webauthn/HazelcastPublicKeyCredentialRequestOptionsRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/core/repository/webauthn/HazelcastPublicKeyCredentialRequestOptionsRepository.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.web.webauthn.api.PublicKeyCredentialRequestOptions;
 import org.springframework.security.web.webauthn.authentication.PublicKeyCredentialRequestOptionsRepository;
 
@@ -11,6 +13,8 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 
 public class HazelcastPublicKeyCredentialRequestOptionsRepository implements PublicKeyCredentialRequestOptionsRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(HazelcastPublicKeyCredentialRequestOptionsRepository.class);
 
     private static final String MAP_NAME = "PublicKeyCredentialRequestOptions";
 
@@ -26,26 +30,43 @@ public class HazelcastPublicKeyCredentialRequestOptionsRepository implements Pub
 
     @Override
     public void save(HttpServletRequest request, HttpServletResponse response, PublicKeyCredentialRequestOptions options) {
+        log.info("Saving PublicKeyCredentialRequestOptions to session with id {}", request.getSession().getId());
+
+        log.info("Logging all elements in the Hazelcast map:");
+        hazelcastMap.forEach((key, value) -> log.info("Key: {}, Value: {}", key, value));
+
         HttpSession session = request.getSession();
         session.setAttribute(this.attrName, options);
+
+        log.info("Hazelcast size before removing session info: {}", hazelcastMap.size());
         hazelcastMap.remove(session.getId());
+        log.info("Hazelcast size after removing session info: {}", hazelcastMap.size());
     }
 
     @Override
     public PublicKeyCredentialRequestOptions load(HttpServletRequest request) {
+        log.info("Loading PublicKeyCredentialRequestOptions");
         HttpSession session = request.getSession(false);
         if (session == null) {
             return null;
         }
+        log.info("Loading PublicKeyCredentialRequestOptions from session with id {}", request.getSession().getId());
 
         String sessionId = request.getSession().getId();
         // load from hazelcast, if it is stored there
         if (hazelcastMap.containsKey(sessionId)) {
+            log.info("Found PublicKeyCredentialRequestOptions in hazelcast for session with id {}", sessionId);
             return hazelcastMap.get(sessionId);
         }
         // not found in hazelcast => we have a new request => read from request options and store it in hazelcast
         PublicKeyCredentialRequestOptions options = (PublicKeyCredentialRequestOptions) session.getAttribute(this.attrName);
         hazelcastMap.put(sessionId, options);
+
+        log.info("Logging all elements in the Hazelcast map:");
+        hazelcastMap.forEach((key, value) -> log.info("Key: {}, Value: {}", key, value));
+
+        log.info("Stored PublicKeyCredentialRequestOptions in hazelcast for session with id {}", sessionId);
+        log.info("Hazelcast size: {}", hazelcastMap.size());
 
         return options;
     }
