3cf4260a0ca87fc7ea74c8a748ec1bf369a42941
marlonnienaber <marlonnienaber@web.de>
2025-04-17 16:09:20 +0200

    add comment to copy script

--- supporting_scripts/pdfjs_copy_worker.mjs
+++ supporting_scripts/pdfjs_copy_worker.mjs
@@ -1,3 +1,9 @@
+/**
+ * pdfjs-dist (used in pdf-preview-thumbnail-grid.component.ts) requires a URL pointing to a worker script to process PDFs.
+ * This script copies the worker script to a location where it can be referenced by pdfjs-dist.
+ * This script is called in the postinstall lifecycle hook in package.json.
+ */
+
 import fs from 'fs';
 import path from 'path';
 
