325940077e5e61b1d83162154500d7ca35e75ede
Tobias Lippert <t.lippert@tum.de>
2025-04-20 01:48:25 +0200

    make buttons conditional

--- src/main/webapp/app/exercise/exercise-create-buttons/exercise-create-buttons.component.ts
+++ src/main/webapp/app/exercise/exercise-create-buttons/exercise-create-buttons.component.ts
@@ -1,15 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Course } from 'app/core/course/shared/entities/course.model';
 import { ExerciseType } from 'app/exercise/shared/entities/exercise/exercise.model';
-import { RouterLink } from '@angular/router';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { ExerciseCreateButtonComponent } from 'app/exercise/exercise-create-buttons/exercise-create-button/exercise-create-button.component';
 import { ExerciseImportButtonComponent } from 'app/exercise/exercise-create-buttons/exercise-import-button/exercise-import-button.component';
 
 @Component({
     selector: 'jhi-exercise-create-buttons',
     templateUrl: './exercise-create-buttons.component.html',
-    imports: [RouterLink, FaIconComponent, ExerciseCreateButtonComponent, ExerciseImportButtonComponent],
+    imports: [ExerciseCreateButtonComponent, ExerciseImportButtonComponent],
 })
 export class ExerciseCreateButtonsComponent {
     course = input<Course | undefined>();
