d478778cb0475af7da630103ae8d44887d5396f2
Florian Glombik <florian.glombik@tum.de>
2025-04-19 15:26:22 +0200

    Rely on spring to catch exception

--- src/main/java/de/tum/cit/aet/artemis/core/repository/webauthn/ArtemisUserCredentialRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/core/repository/webauthn/ArtemisUserCredentialRepository.java
@@ -131,21 +131,14 @@ public class ArtemisUserCredentialRepository implements UserCredentialRepository
      * @param userId of the user for which the passkeys should be found
      */
     public List<PasskeyDTO> findPasskeyDtosByUserId(Bytes userId) {
-        try {
-            Optional<User> user = userRepository.findById(BytesConverter.bytesToLong(userId));
+        Optional<User> user = userRepository.findById(BytesConverter.bytesToLong(userId));
 
-            List<CredentialRecord> credentialRecords = user
-                    .map(passkeyUser -> passkeyCredentialsRepository.findByUser(passkeyUser.getId()).stream().map(PasskeyCredential::toCredentialRecord).toList())
-                    .orElseGet(List::of);
+        List<CredentialRecord> credentialRecords = user
+                .map(passkeyUser -> passkeyCredentialsRepository.findByUser(passkeyUser.getId()).stream().map(PasskeyCredential::toCredentialRecord).toList()).orElseGet(List::of);
 
-            return credentialRecords.stream()
-                    .map(credential -> new PasskeyDTO(credential.getCredentialId().toBase64UrlString(), credential.getLabel(), credential.getCreated(), credential.getLastUsed()))
-                    .toList();
-        }
-        catch (IllegalArgumentException e) {
-            log.warn("Invalid user ID provided: {}", e.getMessage());
-            return List.of();
-        }
+        return credentialRecords.stream()
+                .map(credential -> new PasskeyDTO(credential.getCredentialId().toBase64UrlString(), credential.getLabel(), credential.getCreated(), credential.getLastUsed()))
+                .toList();
     }
 
     public static PasskeyCredential toPasskeyCredential(PasskeyCredential credential, CredentialRecord credentialRecord, User user) {
