15800cafe2281ecd4d30fc41e5449c0161979d80
Tobias Lippert <t.lippert@tum.de>
2025-04-27 18:25:32 +0200

    improve scroll behavior

--- src/main/webapp/app/core/course/manage/course-management-container/course-management-container.component.ts
+++ src/main/webapp/app/core/course/manage/course-management-container/course-management-container.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, Subject, Subscription, of } from 'rxjs';
@@ -85,6 +85,7 @@ export class CourseManagementContainerComponent extends BaseCourseContainerCompo
     private courseSub?: Subscription;
     private urlSubscription?: Subscription;
     private learningPathsActive = signal(false);
+    @ViewChild('courseBodyContainer') courseBody: ElementRef<HTMLElement>;
     isSettingsPage = signal(false);
     studentViewLink = signal<string[]>([]);
 
@@ -252,6 +253,7 @@ export class CourseManagementContainerComponent extends BaseCourseContainerCompo
             const childRouteComponent = this.activatedComponentReference() as CourseConversationsComponent;
             this.isSidebarCollapsed.set(childRouteComponent?.isCollapsed ?? false);
         }
+        this.courseBody.nativeElement.scrollTop = 0;
     }
 
     handleToggleSidebar(): void {
