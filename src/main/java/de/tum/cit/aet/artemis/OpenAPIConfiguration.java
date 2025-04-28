a125e82d3b90b0552b50229a7d0b029942356cbe
Tobias Lippert <t.lippert@tum.de>
2025-04-28 13:34:58 +0200

    cleanup openapi

--- src/main/java/de/tum/cit/aet/artemis/OpenAPIConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/OpenAPIConfiguration.java
@@ -1,5 +1,6 @@
 package de.tum.cit.aet.artemis;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -24,63 +25,63 @@ public class OpenAPIConfiguration {
             var components = openApi.getComponents();
             openApi.info(new Info().title("Artemis Application Server API").version("8.0.0").contact(new Contact().email("krusche@tum.de").name("Stephan Krusche")));
 
-            // if (components != null && components.getSchemas() != null) {
-            // // Only include schemas with DTO suffix and remove the suffix
-            // var schemas = components.getSchemas().entrySet().stream().filter(entry -> entry.getKey().endsWith("DTO"))
-            // .collect(Collectors.toMap(entry -> entry.getKey().substring(0, entry.getKey().length() - 3), entry -> {
-            // var schema = entry.getValue();
-            // schema.setName(entry.getKey().substring(0, entry.getKey().length() - 3));
-            // return schema;
-            // }));
-            //
-            // // Remove DTO suffix from attribute names
-            // schemas.forEach((key, value) -> {
-            // @SuppressWarnings("unchecked")
-            // Map<String, Schema<?>> properties = value.getProperties();
-            // if (properties != null) {
-            // properties.forEach((propertyKey, propertyValue) -> {
-            // removeDTOSuffixesFromSchemaRecursively(propertyValue);
-            // });
-            // }
-            // });
-            //
-            // components.setSchemas(schemas);
-            // }
-            // else {
-            // logger.warn("Components or Schemas are null in OpenAPI configuration.");
-            // }
-            //
+            if (components != null && components.getSchemas() != null) {
+                // Only include schemas with DTO suffix and remove the suffix
+                var schemas = components.getSchemas().entrySet().stream().filter(entry -> entry.getKey().endsWith("DTO"))
+                        .collect(Collectors.toMap(entry -> entry.getKey().substring(0, entry.getKey().length() - 3), entry -> {
+                            var schema = entry.getValue();
+                            schema.setName(entry.getKey().substring(0, entry.getKey().length() - 3));
+                            return schema;
+                        }));
+
+                // Remove DTO suffix from attribute names
+                schemas.forEach((key, value) -> {
+                    @SuppressWarnings("unchecked")
+                    Map<String, Schema<?>> properties = value.getProperties();
+                    if (properties != null) {
+                        properties.forEach((propertyKey, propertyValue) -> {
+                            removeDTOSuffixesFromSchemaRecursively(propertyValue);
+                        });
+                    }
+                });
+
+                components.setSchemas(schemas);
+            }
+            else {
+                logger.warn("Components or Schemas are null in OpenAPI configuration.");
+            }
+
             var paths = openApi.getPaths();
             if (paths != null) {
                 paths.forEach((path, pathItem) -> {
                     logger.info("Processing path: {}", path);
                     pathItem.readOperations().forEach(operation -> {
                         // Remove DTO suffix from response schemas
-                        // var responses = operation.getResponses();
-                        // if (responses != null) {
-                        // responses.forEach((responseCode, response) -> {
-                        // var content = response.getContent();
-                        // if (content != null) {
-                        // content.forEach((contentType, mediaType) -> {
-                        // if (mediaType != null && mediaType.getSchema() != null) {
-                        // removeDTOSuffixesFromSchemaRecursively(mediaType.getSchema());
-                        // }
-                        // else {
-                        // logger.warn("MediaType or Schema is null for content type: {}", contentType);
-                        // }
-                        // });
-                        // }
-                        // else {
-                        // logger.warn("Response with code {} has no content.", responseCode);
-                        // }
-                        // });
-                        // }
-                        // if (operation.getRequestBody() != null) {
-                        // var requestBodyContent = operation.getRequestBody().getContent();
-                        // requestBodyContent.forEach((contentType, mediaType) -> {
-                        // removeDTOSuffixesFromSchemaRecursively(mediaType.getSchema());
-                        // });
-                        // }
+                        var responses = operation.getResponses();
+                        if (responses != null) {
+                            responses.forEach((responseCode, response) -> {
+                                var content = response.getContent();
+                                if (content != null) {
+                                    content.forEach((contentType, mediaType) -> {
+                                        if (mediaType != null && mediaType.getSchema() != null) {
+                                            removeDTOSuffixesFromSchemaRecursively(mediaType.getSchema());
+                                        }
+                                        else {
+                                            logger.warn("MediaType or Schema is null for content type: {}", contentType);
+                                        }
+                                    });
+                                }
+                                else {
+                                    logger.warn("Response with code {} has no content.", responseCode);
+                                }
+                            });
+                        }
+                        if (operation.getRequestBody() != null) {
+                            var requestBodyContent = operation.getRequestBody().getContent();
+                            requestBodyContent.forEach((contentType, mediaType) -> {
+                                removeDTOSuffixesFromSchemaRecursively(mediaType.getSchema());
+                            });
+                        }
 
                         // Remove -controller suffix from tags
                         if (operation.getTags() != null) {
