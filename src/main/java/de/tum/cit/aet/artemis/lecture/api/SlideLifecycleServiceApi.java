3700e1ec083764bf887a6f045a2d456a68cfab2c
Ece Eren <aybikeceren@gmail.com>
2025-04-29 00:04:05 +0200

    Fix architecture test

--- src/main/java/de/tum/cit/aet/artemis/lecture/api/SlideLifecycleServiceApi.java
+++ src/main/java/de/tum/cit/aet/artemis/lecture/api/SlideLifecycleServiceApi.java
@@ -3,13 +3,14 @@ package de.tum.cit.aet.artemis.lecture.api;
 import java.time.ZonedDateTime;
 import java.util.concurrent.ScheduledFuture;
 
+import de.tum.cit.aet.artemis.core.api.AbstractApi;
 import de.tum.cit.aet.artemis.lecture.domain.Slide;
 import de.tum.cit.aet.artemis.lecture.domain.SlideLifecycle;
 
 /**
  * Service API for scheduling tasks at specific points in the lifecycle of a slide.
  */
-public interface SlideLifecycleServiceApi {
+public interface SlideLifecycleServiceApi extends AbstractApi {
 
     /**
      * Schedule a {@code Runnable} task in the lifecycle of a slide.
