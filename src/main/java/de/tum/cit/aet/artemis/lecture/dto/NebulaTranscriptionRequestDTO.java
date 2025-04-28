05e5dd08ad2b10d9f1b621ccd04bb520dea0e3c6
Anish <anish.goyal@tum.de>
2025-04-28 22:57:10 +0200

    Adding @JsonInclude annotation

--- src/main/java/de/tum/cit/aet/artemis/lecture/dto/NebulaTranscriptionRequestDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/lecture/dto/NebulaTranscriptionRequestDTO.java
@@ -1,4 +1,7 @@
 package de.tum.cit.aet.artemis.lecture.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record NebulaTranscriptionRequestDTO(String videoUrl, Long lectureId, Long lectureUnitId) {
 }
