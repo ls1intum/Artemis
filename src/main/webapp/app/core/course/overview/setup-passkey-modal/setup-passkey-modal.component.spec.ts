11e7a90c5e2d0154bba571855b7594ec7dcb14c7
Florian Glombik <florian.glombik@tum.de>
2025-04-29 22:31:24 +0200

    Add tests for modal

--- src/main/webapp/app/core/course/overview/setup-passkey-modal/setup-passkey-modal.component.spec.ts
+++ src/main/webapp/app/core/course/overview/setup-passkey-modal/setup-passkey-modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockDirective, MockProvider } from 'ng-mocks';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 import { SetupPasskeyModalComponent } from './setup-passkey-modal.component';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
@@ -8,20 +9,38 @@ import { TranslateDirective } from 'app/shared/language/translate.directive';
 describe('SetupPasskeyModalComponent', () => {
     let component: SetupPasskeyModalComponent;
     let fixture: ComponentFixture<SetupPasskeyModalComponent>;
+    let router: Router;
+    let activeModal: NgbActiveModal;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [SetupPasskeyModalComponent],
             declarations: [MockDirective(TranslateDirective)],
-            providers: [MockProvider(NgbActiveModal)],
+            providers: [MockProvider(NgbActiveModal), { provide: Router, useValue: { navigateByUrl: jest.fn() } }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SetupPasskeyModalComponent);
         component = fixture.componentInstance;
+        router = TestBed.inject(Router);
+        activeModal = TestBed.inject(NgbActiveModal);
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should navigate to setup passkey and close modal', () => {
+        const navigateByUrlSpy = jest.spyOn(router, 'navigateByUrl');
+        const closeModalSpy = jest.spyOn(activeModal, 'close');
+
+        component.navigateToSetupPasskey();
+
+        expect(closeModalSpy).toHaveBeenCalled();
+        expect(navigateByUrlSpy).toHaveBeenCalledWith('/user-settings/passkeys');
+    });
+
+    it('should close the modal', () => {
+        const closeModalSpy = jest.spyOn(activeModal, 'close');
+
+        component.closeModal();
+
+        expect(closeModalSpy).toHaveBeenCalled();
     });
 });
