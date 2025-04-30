fc8c23db2015babb282461da692840a6aca72cbf
Florian Glombik <florian.glombik@tum.de>
2025-05-01 00:40:08 +0200

    Bump client coverage up

--- jest.config.js
+++ jest.config.js
@@ -91,10 +91,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should increase to at least 90%
-            statements: 88.95,
-            branches: 75.1,
-            functions: 82.9,
-            lines: 89.01,
+            statements: 88.98,
+            branches: 75.14,
+            functions: 82.91,
+            lines: 89.04,
         },
     },
     coverageReporters: ['clover', 'json', 'lcov', 'text-summary'],
