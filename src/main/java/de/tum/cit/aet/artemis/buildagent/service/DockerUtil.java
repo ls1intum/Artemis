53961526933a999fe4ad1f9cc3dce0dde0a588b3
jfr2102 <johannes@friedlein.eu>
2025-05-05 00:00:49 +0200

    log docker exception

--- src/main/java/de/tum/cit/aet/artemis/buildagent/service/DockerUtil.java
+++ src/main/java/de/tum/cit/aet/artemis/buildagent/service/DockerUtil.java
@@ -2,9 +2,15 @@ package de.tum.cit.aet.artemis.buildagent.service;
 
 import java.net.SocketException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class DockerUtil {
 
+    private static final Logger log = LoggerFactory.getLogger(DockerUtil.class);
+
     public static boolean isDockerNotAvailable(Exception ex) {
+        log.info("Checking if Docker is not available: {} {} \n {}", ex.getCause().getMessage(), ex.getCause().getClass().getCanonicalName(), ex.getCause());
         return ex.getCause() != null && ex.getCause() instanceof SocketException && ex.getCause().getMessage() != null
                 && ex.getCause().getMessage().contains("No such file or directory");
     }
