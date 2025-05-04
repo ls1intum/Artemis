b75afa0bde150b35cf5b09f5be6a42632fe2fd0f
Florian Glombik <florian.glombik@tum.de>
2025-05-04 12:13:24 +0200

    Add javadoc to credential

--- src/main/webapp/app/core/user/settings/passkey-settings/entities/malformed-bitwarden-credential.ts
+++ src/main/webapp/app/core/user/settings/passkey-settings/entities/malformed-bitwarden-credential.ts
@@ -5,14 +5,45 @@ export interface MalformedBitwardenCredential {
     authenticatorAttachment: string;
     response: {
         clientDataJSON: Record<string, number>;
-        attestationObject?: Record<string, number>; // only available on register, not on login
-        getAuthenticatorData?: () => Record<string, number>; // only available on register, not on login
-        authenticatorData: Record<string, number>; // only available on login, not on register
-        getPublicKey: () => Record<string, number>; // only available on register, not on login
-        getPublicKeyAlgorithm: () => number; // only available on register, not on login
-        getTransports: () => string[]; // only available on register, not on login
-        signature?: Record<string, number>; // only available on login, not on register
-        userHandle?: Record<string, number>; // only available on login, not on register
+        /**
+         * only available during registration.
+         */
+        attestationObject?: Record<string, number>;
+
+        /**
+         * only available during registration.
+         */
+        getAuthenticatorData?: () => Record<string, number>;
+
+        /**
+         * only available during login.
+         */
+        authenticatorData?: Record<string, number>;
+
+        /**
+         * only available during registration.
+         */
+        getPublicKey?: () => Record<string, number>;
+
+        /**
+         * only available during registration.
+         */
+        getPublicKeyAlgorithm?: () => number;
+
+        /**
+         * only available during registration.
+         */
+        getTransports?: () => string[];
+
+        /**
+         * only available during login.
+         */
+        signature?: Record<string, number>;
+
+        /**
+         * only available during login.
+         */
+        userHandle?: Record<string, number>;
     };
     getClientExtensionResults: () => unknown;
 }
