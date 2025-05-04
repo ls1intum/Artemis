08884f1640b250f8cdad219060071447d830b4d2
Florian Glombik <florian.glombik@tum.de>
2025-05-04 14:52:27 +0200

    Add comment for prototype

--- src/main/webapp/app/core/user/settings/passkey-settings/entities/invalid-credential-error.ts
+++ src/main/webapp/app/core/user/settings/passkey-settings/entities/invalid-credential-error.ts
@@ -2,6 +2,6 @@ export class InvalidCredentialError extends Error {
     constructor(message: string = 'Invalid credential') {
         super(message);
         this.name = 'InvalidCredentialError';
-        Object.setPrototypeOf(this, InvalidCredentialError.prototype);
+        Object.setPrototypeOf(this, InvalidCredentialError.prototype); // ensure instanceof checks work correctly
     }
 }
