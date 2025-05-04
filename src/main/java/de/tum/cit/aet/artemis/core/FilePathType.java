e9a81594b387044a1ce08fc2b5c8fca996028f70
Tobias Lippert <t.lippert@tum.de>
2025-05-04 21:11:43 +0200

    fix enum file formatting

--- src/main/java/de/tum/cit/aet/artemis/core/FilePathType.java
+++ src/main/java/de/tum/cit/aet/artemis/core/FilePathType.java
@@ -9,8 +9,11 @@ import de.tum.cit.aet.artemis.core.service.FilePathService;
  */
 public enum FilePathType {
     LECTURE_ATTACHMENT,
+    // @formatter:off
     /** Path for slide images when attachments are split into single slides **/
-    SLIDE, TEMPORARY, DRAG_AND_DROP_BACKGROUND, DRAG_ITEM, PROFILE_PICTURE, COURSE_ICON, EXAM_USER_SIGNATURE, EXAM_ATTENDANCE_CHECK_STUDENT_IMAGE,
+    SLIDE,
+    TEMPORARY, DRAG_AND_DROP_BACKGROUND, DRAG_ITEM, PROFILE_PICTURE, COURSE_ICON, EXAM_USER_SIGNATURE, EXAM_ATTENDANCE_CHECK_STUDENT_IMAGE,
     /** Path for the student version of slides. This is a version of the actual slides with hidden slides published to students **/
-    STUDENT_VERSION_SLIDES, FILE_UPLOAD_SUBMISSION, ATTACHMENT_UNIT
+    STUDENT_VERSION_SLIDES,
+    FILE_UPLOAD_SUBMISSION, ATTACHMENT_UNIT
 }
