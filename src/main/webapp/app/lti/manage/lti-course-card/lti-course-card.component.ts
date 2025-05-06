f7802909f9bd2df045a0717d6a530d2807273e69
RaphaelKlein <raphael.klein@tum.de>
2025-05-06 21:24:03 +0200

    Use artemis date

--- src/main/webapp/app/lti/manage/lti-course-card/lti-course-card.component.ts
+++ src/main/webapp/app/lti/manage/lti-course-card/lti-course-card.component.ts
@@ -6,7 +6,6 @@ import { RouterLink } from '@angular/router';
 import { NgStyle } from '@angular/common';
 import { getContrastingTextColor } from 'app/shared/util/color.utils';
 import { TranslateDirective } from '../../../shared/language/translate.directive';
-import { dayJs } from 'dayjs/esm';
 
 @Component({
     selector: 'jhi-overview-lti-course-card',
@@ -20,21 +19,12 @@ export class LtiCourseCardComponent {
     CachingStrategy = CachingStrategy;
     courseColor: string;
     contentColor: string;
-    startDate: string;
-    endDate: string;
 
     constructor() {
         effect(() => {
             const courseValue = this.course();
             this.courseColor = courseValue?.color || this.ARTEMIS_DEFAULT_COLOR;
             this.contentColor = getContrastingTextColor(this.courseColor);
-
-            if (courseValue.startDate) {
-                this.startDate = dayJs(courseValue.startDate).format('YYYY-MM-DD');
-            }
-            if (courseValue.endDate) {
-                this.endDate = dayJs(courseValue.endDate).format('YYYY-MM-DD');
-            }
         });
     }
 }
