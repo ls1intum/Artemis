dc3cbde8cb804de2842101cb3dd21ede85f69117
Florian Glombik <florian.glombik@tum.de>
2025-05-07 11:17:41 +0200

    Fix setup passkey client test

--- src/main/webapp/app/core/course/overview/setup-passkey-modal/setup-passkey-modal.component.spec.ts
+++ src/main/webapp/app/core/course/overview/setup-passkey-modal/setup-passkey-modal.component.spec.ts
@@ -2,9 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockDirective, MockProvider } from 'ng-mocks';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
-
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { EARLIEST_SETUP_PASSKEY_REMINDER_DATE_LOCAL_STORAGE_KEY, SetupPasskeyModalComponent } from './setup-passkey-modal.component';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
+import { AlertService } from 'app/shared/service/alert.service';
+import { AccountService } from 'app/core/auth/account.service';
 
 describe('SetupPasskeyModalComponent', () => {
     let component: SetupPasskeyModalComponent;
@@ -16,7 +19,18 @@ describe('SetupPasskeyModalComponent', () => {
         await TestBed.configureTestingModule({
             imports: [SetupPasskeyModalComponent],
             declarations: [MockDirective(TranslateDirective)],
-            providers: [MockProvider(NgbActiveModal), { provide: Router, useValue: { navigateByUrl: jest.fn() } }],
+            providers: [
+                MockProvider(NgbActiveModal),
+                {
+                    provide: Router,
+                    useValue: { navigateByUrl: jest.fn() },
+                },
+                MockProvider(AlertService),
+                MockProvider(AlertService),
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                MockProvider(AccountService),
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SetupPasskeyModalComponent);
