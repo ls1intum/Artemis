af3a82138f5b5ef4587917fc664b25e26ae4d52a
Maximilian Anzinger <anzinger@in.tum.de>
2025-05-09 14:54:37 +0200

    migrate competency node component

--- src/main/webapp/app/atlas/manage/competency-node/competency-node.component.spec.ts
+++ src/main/webapp/app/atlas/manage/competency-node/competency-node.component.spec.ts
@@ -1,11 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CompetencyNodeComponent } from 'app/atlas/manage/competency-node/competency-node.component';
+import { CompetencyNodeComponent, SizeUpdate } from 'app/atlas/manage/competency-node/competency-node.component';
 import { CompetencyGraphNodeDTO } from 'app/atlas/shared/entities/learning-path.model';
+import { Component } from '@angular/core';
+import { getComponentInstanceFromFixture } from 'test/helpers/utils/general-test.utils';
+
+@Component({
+    template: '<jhi-learning-path-competency-node [competencyNode]="competencyNode" (onSizeSet)="onSizeSet($event)" />',
+    imports: [CompetencyNodeComponent],
+})
+class WrapperComponent {
+    competencyNode: CompetencyGraphNodeDTO;
+
+    onSizeSet(event: SizeUpdate) {}
+}
 
 describe('CompetencyNodeComponent', () => {
-    let component: CompetencyNodeComponent;
-    let fixture: ComponentFixture<CompetencyNodeComponent>;
-    let sizeUpdateEmitSpy: jest.SpyInstance;
+    let component: WrapperComponent;
+    let fixture: ComponentFixture<WrapperComponent>;
+    let competencyNodeComponent: CompetencyNodeComponent;
+    let onSizeSetSpy: jest.SpyInstance;
 
     const competencyNode = <CompetencyGraphNodeDTO>{
         id: '1',
@@ -17,15 +30,17 @@ describe('CompetencyNodeComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [CompetencyNodeComponent],
+            imports: [WrapperComponent],
             providers: [],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(CompetencyNodeComponent);
+        fixture = TestBed.createComponent(WrapperComponent);
         component = fixture.componentInstance;
-        fixture.componentRef.setInput('competencyNode', competencyNode);
+        competencyNodeComponent = getComponentInstanceFromFixture(fixture, 'jhi-learning-path-competency-node') as CompetencyNodeComponent;
 
-        sizeUpdateEmitSpy = jest.spyOn(component.onSizeSet, 'emit');
+        component.competencyNode = competencyNode;
+
+        onSizeSetSpy = jest.spyOn(component, 'onSizeSet');
 
         fixture.detectChanges();
     });
@@ -36,26 +51,26 @@ describe('CompetencyNodeComponent', () => {
 
     it('should initialize and emit size update', async () => {
         expect(component).toBeTruthy();
-        expect(component.competencyNode()).toEqual(competencyNode);
-        expect(component.value()).toBe(71);
-        expect(component.valueType()).toBe(competencyNode.valueType);
-        expect(sizeUpdateEmitSpy).toHaveBeenCalled();
+        expect(competencyNodeComponent.competencyNode()).toEqual(competencyNode);
+        expect(competencyNodeComponent.value()).toBe(71);
+        expect(competencyNodeComponent.valueType()).toBe(competencyNode.valueType);
+        expect(onSizeSetSpy).toHaveBeenCalled();
     });
 
     it('should check if competency is green', () => {
-        expect(component.isGreen()).toBeFalse();
+        expect(competencyNodeComponent.isGreen()).toBeFalse();
     });
 
     it('should check if competency is yellow', () => {
-        expect(component.isYellow()).toBeTrue();
+        expect(competencyNodeComponent.isYellow()).toBeTrue();
     });
 
     it('should check if competency is gray', () => {
-        expect(component.isGray()).toBeFalse();
+        expect(competencyNodeComponent.isGray()).toBeFalse();
     });
 
     it('should set dimensions', () => {
-        component.setDimensions();
-        expect(sizeUpdateEmitSpy).toHaveBeenCalled();
+        competencyNodeComponent.setDimensions();
+        expect(onSizeSetSpy).toHaveBeenCalled();
     });
 });
