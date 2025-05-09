3c84912fcf529d7d3809f97da0f6eb23a5ab0e98
Paul Rangger <parangger@gmail.com>
2025-05-09 10:42:42 +0200

    Add small button

--- src/main/webapp/app/communication/course-notification/course-notification-preset-picker/course-notification-preset-picker.component.ts
+++ src/main/webapp/app/communication/course-notification/course-notification-preset-picker/course-notification-preset-picker.component.ts
@@ -1,4 +1,5 @@
 import { Component, effect, input, output } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
@@ -12,13 +13,14 @@ import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from
  */
 @Component({
     selector: 'jhi-course-notification-preset-picker',
-    imports: [TranslateDirective, FaIconComponent, NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle],
+    imports: [TranslateDirective, FaIconComponent, NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle, NgClass],
     templateUrl: './course-notification-preset-picker.component.html',
     styleUrls: ['./course-notification-preset-picker.component.scss'],
 })
 export class CourseNotificationPresetPickerComponent {
     readonly availableCourseSettingPresets = input.required<CourseNotificationSettingPreset[]>();
     readonly selectedCourseSettingPreset = input.required<CourseNotificationSettingPreset | undefined>();
+    readonly isSmallButton = input<boolean>(false);
 
     readonly onPresetSelected = output<number>();
 
