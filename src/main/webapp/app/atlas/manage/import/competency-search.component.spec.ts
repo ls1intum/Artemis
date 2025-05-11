8ae64e726e866c032edf6b6bf6c13ae69fa57763
Maximilian Anzinger <anzinger@in.tum.de>
2025-05-11 21:41:57 +0200

    migrate most for competency search component

--- src/main/webapp/app/atlas/manage/import/competency-search.component.spec.ts
+++ src/main/webapp/app/atlas/manage/import/competency-search.component.spec.ts
@@ -3,31 +3,49 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockPipe } from 'ng-mocks';
 import { CompetencySearchComponent } from 'app/atlas/manage/import/competency-search.component';
 import { ButtonComponent } from 'app/shared/components/button/button.component';
-import { CourseCompetencyFilter } from 'app/shared/table/pageable-table';
+import { CourseCompetencyFilter, PageableSearch } from 'app/shared/table/pageable-table';
 import { MockTranslateService } from 'test/helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { Component } from '@angular/core';
+import { getComponentInstanceFromFixture } from 'test/helpers/utils/general-test.utils';
+
+@Component({
+    template: '<jhi-competency-search [(search)]="search" (searchChange)="searchChange($event)"/>',
+    imports: [CompetencySearchComponent],
+})
+class WrapperComponent {
+    search: CourseCompetencyFilter;
+
+    searchChange(search: PageableSearch) {}
+}
 
 describe('CompetencySearchComponent', () => {
-    let componentFixture: ComponentFixture<CompetencySearchComponent>;
-    let component: CompetencySearchComponent;
+    let fixture: ComponentFixture<WrapperComponent>;
+    let component: WrapperComponent;
+    let competencySearchComponent: CompetencySearchComponent;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [WrapperComponent],
             declarations: [CompetencySearchComponent, MockPipe(ArtemisTranslatePipe), ButtonComponent],
             providers: [{ provide: TranslateService, useClass: MockTranslateService }, provideHttpClient(), provideHttpClientTesting()],
         })
             .compileComponents()
             .then(() => {
-                componentFixture = TestBed.createComponent(CompetencySearchComponent);
-                component = componentFixture.componentInstance;
+                fixture = TestBed.createComponent(WrapperComponent);
+                component = fixture.componentInstance;
+                competencySearchComponent = getComponentInstanceFromFixture(fixture, 'jhi-competency-search') as CompetencySearchComponent;
+
                 component.search = {
                     title: '',
                     semester: '',
                     courseTitle: '',
                     description: '',
                 };
+
+                fixture.detectChanges();
             });
     });
 
@@ -36,56 +54,52 @@ describe('CompetencySearchComponent', () => {
     });
 
     it('should initialize', () => {
-        componentFixture.detectChanges();
         expect(component).toBeDefined();
     });
 
     it('should reset', () => {
-        componentFixture.detectChanges();
         initializeSearch();
 
-        componentFixture.debugElement.nativeElement.querySelector('#resetFilterButton > .jhi-btn').click();
+        fixture.debugElement.nativeElement.querySelector('#resetFilterButton > .jhi-btn').click();
 
         for (const key in component.search) {
-            expect(component.search[key as keyof CourseCompetencyFilter]).toBe('');
+            expect(competencySearchComponent.search()[key as keyof CourseCompetencyFilter]).toBe('');
         }
     });
 
     it('should submit with only title', () => {
-        componentFixture.detectChanges();
-        const searchChangeEmitSpy = jest.spyOn(component.searchChange, 'emit');
+        const searchChangeSpy = jest.spyOn(component, 'searchChange');
 
         initializeSearch();
 
-        componentFixture.debugElement.nativeElement.querySelector('#submitFilterButton > .jhi-btn').click();
-        expect(searchChangeEmitSpy).toHaveBeenCalledWith({ title: 'any value', description: '', courseTitle: '', semester: '' });
+        fixture.debugElement.nativeElement.querySelector('#submitFilterButton > .jhi-btn').click();
+        expect(searchChangeSpy).toHaveBeenCalledWith({ title: 'any value', description: '', courseTitle: '', semester: '' });
     });
 
     it('should submit with advanced search', () => {
-        componentFixture.detectChanges();
-        const searchChangeEmitSpy = jest.spyOn(component.searchChange, 'emit');
+        const searchChangeSpy = jest.spyOn(component, 'searchChange');
 
         initializeSearch();
-        component.advancedSearchEnabled = true;
+        competencySearchComponent.advancedSearchEnabled = true;
 
-        componentFixture.debugElement.nativeElement.querySelector('#submitFilterButton > .jhi-btn').click();
-        expect(searchChangeEmitSpy).toHaveBeenCalledWith({ title: 'any value', description: 'any value', courseTitle: 'any value', semester: 'any value' });
+        fixture.debugElement.nativeElement.querySelector('#submitFilterButton > .jhi-btn').click();
+        expect(searchChangeSpy).toHaveBeenCalledWith({ title: 'any value', description: 'any value', courseTitle: 'any value', semester: 'any value' });
     });
 
     it('should toggle advanced search', () => {
-        componentFixture.detectChanges();
-        const advancedSearchToggle = componentFixture.debugElement.nativeElement.querySelector('#toggleAdvancedSearch');
+        const advancedSearchToggle = fixture.debugElement.nativeElement.querySelector('#toggleAdvancedSearch');
 
         advancedSearchToggle.click();
-        expect(component.advancedSearchEnabled).toBeTrue();
+        expect(competencySearchComponent.advancedSearchEnabled).toBeTrue();
 
         advancedSearchToggle.click();
-        expect(component.advancedSearchEnabled).toBeFalse();
+        expect(competencySearchComponent.advancedSearchEnabled).toBeFalse();
     });
 
     function initializeSearch(): void {
         for (const key in component.search) {
             component.search[key as keyof CourseCompetencyFilter] = 'any value';
         }
+        fixture.detectChanges();
     }
 });
