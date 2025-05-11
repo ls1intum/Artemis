f4d6664f201f084c59886d3ebf7567dba5afb6b8
marlonnienaber <marlonnienaber@web.de>
2025-05-11 15:05:49 +0200

    re-trigger pipeline

--- src/main/java/de/tum/cit/aet/artemis/text/web/TextExerciseResource.java
+++ src/main/java/de/tum/cit/aet/artemis/text/web/TextExerciseResource.java
@@ -480,7 +480,7 @@ public class TextExerciseResource {
             }
         }
 
-        // if all submissions were deleted, add a new one since the client relies on the existence of at least one
+        // if all submissions were deleted, add a new one since the client relies on the existence of at least one submission
         if (submissions.isEmpty()) {
             TextSubmission textSubmission = new TextSubmission();
             textSubmission.setParticipation(participation);
