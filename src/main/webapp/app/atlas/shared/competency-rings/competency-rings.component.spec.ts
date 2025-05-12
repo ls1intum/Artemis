2b430fa9f211bfce15e515a82f928cbdbc8ad3e7
Maximilian Anzinger <anzinger@in.tum.de>
2025-05-12 10:15:52 +0200

    migrate competency rings

--- src/main/webapp/app/atlas/shared/competency-rings/competency-rings.component.spec.ts
+++ src/main/webapp/app/atlas/shared/competency-rings/competency-rings.component.spec.ts
@@ -4,20 +4,38 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { CompetencyRingsComponent } from 'app/atlas/shared/competency-rings/competency-rings.component';
 import { MockModule, MockPipe } from 'ng-mocks';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
+import { CompetencyGraphComponent } from 'app/atlas/manage/competency-graph/competency-graph.component';
+import { CompetencyGraphDTO } from 'app/atlas/shared/entities/learning-path.model';
+import { Component } from '@angular/core';
+import { getComponentInstanceFromFixture } from 'test/helpers/utils/general-test.utils';
+
+@Component({
+    template: '<jhi-competency-rings [progress]="progress" [mastery]="mastery" [playAnimation]="playAnimation" [hideTooltip]="hideTooltip" [hideProgress]="hideProgress"/>',
+    imports: [CompetencyRingsComponent],
+})
+class WrapperComponent {
+    progress?: number;
+    mastery?: number;
+    playAnimation?: boolean;
+    hideTooltip?: boolean;
+    hideProgress?: boolean;
+}
 
 describe('CompetencyRings', () => {
-    let fixture: ComponentFixture<CompetencyRingsComponent>;
-    let component: CompetencyRingsComponent;
+    let fixture: ComponentFixture<WrapperComponent>;
+    let component: WrapperComponent;
+    let competencyRingsComponent: CompetencyRingsComponent;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [MockModule(NgbTooltipModule)],
+            imports: [WrapperComponent, MockModule(NgbTooltipModule)],
             declarations: [CompetencyRingsComponent, MockPipe(ArtemisTranslatePipe)],
         })
             .compileComponents()
             .then(() => {
-                fixture = TestBed.createComponent(CompetencyRingsComponent);
+                fixture = TestBed.createComponent(WrapperComponent);
                 component = fixture.componentInstance;
+                competencyRingsComponent = getComponentInstanceFromFixture(fixture, 'jhi-competency-rings') as CompetencyRingsComponent;
 
                 component.progress = 110;
                 component.mastery = -10;
@@ -31,14 +49,14 @@ describe('CompetencyRings', () => {
     it('should calculate percentage values', () => {
         fixture.detectChanges();
 
-        expect(component.progressPercentage).toBe(100);
-        expect(component.masteryPercentage).toBe(0);
+        expect(competencyRingsComponent.progressPercentage).toBe(100);
+        expect(competencyRingsComponent.masteryPercentage).toBe(0);
     });
 
     it('should restrict number to percentage range', () => {
-        expect(component.percentageRange(110)).toBe(100);
-        expect(component.percentageRange(50)).toBe(50);
-        expect(component.percentageRange(-10)).toBe(0);
+        expect(competencyRingsComponent.percentageRange(110)).toBe(100);
+        expect(competencyRingsComponent.percentageRange(50)).toBe(50);
+        expect(competencyRingsComponent.percentageRange(-10)).toBe(0);
     });
 
     it('should visualize using progress bars', () => {
