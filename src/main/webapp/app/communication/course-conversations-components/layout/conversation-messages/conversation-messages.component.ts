6d7339d42d5092a2ec0d8e83fcdab02e87f4cde7
Florian Glombik <florian.glombik@tum.de>
2025-05-12 11:24:39 +0200

    Revert "Remove unused imports"
    This reverts commit 929ddb9bc68bf2a024d1052420c2e08435b45a66.

--- src/main/webapp/app/communication/course-conversations-components/layout/conversation-messages/conversation-messages.component.ts
+++ src/main/webapp/app/communication/course-conversations-components/layout/conversation-messages/conversation-messages.component.ts
@@ -42,6 +42,7 @@ import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 import { NgClass } from '@angular/common';
 import { PostCreateEditModalComponent } from 'app/communication/posting-create-edit-modal/post-create-edit-modal/post-create-edit-modal.component';
 import { MessageInlineInputComponent } from 'app/communication/message/message-inline-input/message-inline-input.component';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { ForwardedMessageDTO } from 'app/communication/shared/entities/forwarded-message.model';
 import { AnswerPost } from 'app/communication/shared/entities/answer-post.model';
 import { Posting, PostingType } from 'app/communication/shared/entities/posting.model';
@@ -57,13 +58,18 @@ interface PostGroup {
     templateUrl: './conversation-messages.component.html',
     styleUrls: ['./conversation-messages.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    imports: [FaIconComponent, TranslateDirective, InfiniteScrollDirective, NgClass, PostingThreadComponent, PostCreateEditModalComponent, MessageInlineInputComponent],
+    imports: [
+        FaIconComponent,
+        TranslateDirective,
+        InfiniteScrollDirective,
+        NgClass,
+        PostingThreadComponent,
+        PostCreateEditModalComponent,
+        MessageInlineInputComponent,
+        ArtemisTranslatePipe,
+    ],
 })
 export class ConversationMessagesComponent implements OnInit, AfterViewInit, OnDestroy, OnChanges {
-    protected readonly faTimes = faTimes;
-    protected readonly faEnvelope = faEnvelope;
-    protected readonly faCircleNotch = faCircleNotch;
-
     metisService = inject(MetisService);
     metisConversationService = inject(MetisConversationService);
     cdr = inject(ChangeDetectorRef);
@@ -114,6 +120,10 @@ export class ConversationMessagesComponent implements OnInit, AfterViewInit, OnD
     totalNumberOfPosts = 0;
     page = 1;
     public isFetchingPosts = true;
+    // Icons
+    faTimes = faTimes;
+    faEnvelope = faEnvelope;
+    faCircleNotch = faCircleNotch;
     isMobile = false;
     isHiddenInputWithCallToAction = false;
     isHiddenInputFull = false;
