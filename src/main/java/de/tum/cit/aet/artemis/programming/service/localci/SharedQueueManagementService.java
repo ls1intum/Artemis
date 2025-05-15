d5a2b501e02736d5b7c82dddbee9cca7deeb201e
jfr2102 <johannes@friedlein.eu>
2025-05-15 17:43:02 +0200

    reuse existing listener

--- src/main/java/de/tum/cit/aet/artemis/programming/service/localci/SharedQueueManagementService.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/localci/SharedQueueManagementService.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.function.Function;
@@ -33,9 +34,12 @@ import com.hazelcast.map.listener.EntryUpdatedListener;
 import de.tum.cit.aet.artemis.buildagent.dto.BuildAgentInformation;
 import de.tum.cit.aet.artemis.buildagent.dto.BuildJobQueueItem;
 import de.tum.cit.aet.artemis.buildagent.dto.DockerImageBuild;
+import de.tum.cit.aet.artemis.communication.service.notifications.MailService;
+import de.tum.cit.aet.artemis.core.domain.User;
 import de.tum.cit.aet.artemis.core.dto.SortingOrder;
 import de.tum.cit.aet.artemis.core.dto.pageablesearch.FinishedBuildJobPageableSearchDTO;
 import de.tum.cit.aet.artemis.core.service.ProfileService;
+import de.tum.cit.aet.artemis.core.service.user.UserService;
 import de.tum.cit.aet.artemis.programming.domain.build.BuildJob;
 import de.tum.cit.aet.artemis.programming.domain.build.BuildStatus;
 import de.tum.cit.aet.artemis.programming.repository.BuildJobRepository;
@@ -55,14 +59,21 @@ public class SharedQueueManagementService {
 
     private final ProfileService profileService;
 
+    private final MailService mailService;
+
+    private final UserService userService;
+
     private int buildAgentsCapacity;
 
     private int runningBuildJobCount;
 
-    public SharedQueueManagementService(BuildJobRepository buildJobRepository, ProfileService profileService, DistributedDataAccessService distributedDataAccessService) {
+    public SharedQueueManagementService(BuildJobRepository buildJobRepository, ProfileService profileService, DistributedDataAccessService distributedDataAccessService,
+            MailService mailService, UserService userService) {
         this.buildJobRepository = buildJobRepository;
         this.profileService = profileService;
         this.distributedDataAccessService = distributedDataAccessService;
+        this.mailService = mailService;
+        this.userService = userService;
     }
 
     /**
@@ -378,6 +389,20 @@ public class SharedQueueManagementService {
         public void entryUpdated(com.hazelcast.core.EntryEvent<String, BuildAgentInformation> event) {
             log.debug("Build agent updated: {}", event.getValue());
             updateBuildAgentCapacity();
+
+            BuildAgentInformation oldValue = event.getOldValue();
+            BuildAgentInformation newValue = event.getValue();
+
+            if (oldValue != null && newValue != null && oldValue.status() != BuildAgentInformation.BuildAgentStatus.SELF_PAUSED
+                    && newValue.status() == BuildAgentInformation.BuildAgentStatus.SELF_PAUSED) {
+
+                Optional<User> admin = userService.findInternalAdminUser();
+                if (admin.isEmpty()) {
+                    log.warn("No internal admin user found. Cannot send email to admin about successful creation of data exports.");
+                    return;
+                }
+                mailService.sendBuildAgentSelfPausedEmailToAdmin(admin.get(), newValue.buildAgent().name());
+            }
         }
     }
 
