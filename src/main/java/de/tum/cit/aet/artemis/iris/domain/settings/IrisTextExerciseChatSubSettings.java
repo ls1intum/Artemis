81a843ae79b9a2736c8cd55f0dff6138e855c939
Patrick Bassner <patrick@bassner.de>
2025-05-19 15:48:48 +0200

    Update src/main/java/de/tum/cit/aet/artemis/iris/domain/settings/IrisTextExerciseChatSubSettings.java
    Co-authored-by: Alexander Joham <73483450+alexjoham@users.noreply.github.com>

--- src/main/java/de/tum/cit/aet/artemis/iris/domain/settings/IrisTextExerciseChatSubSettings.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/domain/settings/IrisTextExerciseChatSubSettings.java
@@ -33,7 +33,7 @@ public class IrisTextExerciseChatSubSettings extends IrisSubSettings {
     private SortedSet<String> enabledForCategories = new TreeSet<>();
 
     @Nullable
-    @Column(name = "custom_instructions", length = 2048)
+    @Column(name = "custom_instructions", length = IRIS_CUSTOM_INSTRUCTIONS_MAX_LENGTH)
     private String customInstructions;
 
     @Nullable
