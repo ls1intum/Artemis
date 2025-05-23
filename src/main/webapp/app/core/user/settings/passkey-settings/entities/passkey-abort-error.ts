410be516519cefeaf81db310a0aa33dd8ecd4124
Florian Glombik <florian.glombik@tum.de>
2025-05-23 10:58:59 +0200

    Fix typo

--- src/main/webapp/app/core/user/settings/passkey-settings/entities/passkey-abort-error.ts
+++ src/main/webapp/app/core/user/settings/passkey-settings/entities/passkey-abort-error.ts
@@ -2,6 +2,6 @@ export class PasskeyAbortError extends Error {
     constructor(message: string = 'Cannot initiate a new credential request while a pending request is active. The previous request has been aborted to proceed.') {
         super(message);
         this.name = 'AbortError';
-        Object.setPrototypeOf(this, PasskeyAbortError.prototype); // ensure instanceof checks work correctly
+        Object.setPrototypeOf(this, PasskeyAbortError.prototype); // ensure instanceof checks works correctly
     }
 }
