13ca2ae61d9db2199e40fcf8dfe6caba54253c40
Eylül Naz Can <eylulnc@gmail.com>
2025-05-25 12:52:43 +0200

    add missing javadoc

--- src/main/java/de/tum/cit/aet/artemis/communication/service/EmailNotificationSettingService.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/service/EmailNotificationSettingService.java
@@ -25,14 +25,36 @@ public class EmailNotificationSettingService {
         this.emailNotificationSettingRepository = emailNotificationSettingRepository;
     }
 
+    /**
+     * Retrieves all email notification settings for a given user.
+     *
+     * @param userId the ID of the user
+     * @return a list of email notification settings
+     */
     public List<EmailNotificationSetting> getUserSettings(Long userId) {
         return emailNotificationSettingRepository.findByUserId(userId);
     }
 
+    /**
+     * Retrieves a specific email notification setting for a user and notification type.
+     *
+     * @param userId           the ID of the user
+     * @param notificationType the type of notification
+     * @return an optional containing the setting if it exists
+     */
     public Optional<EmailNotificationSetting> getUserSetting(Long userId, EmailNotificationType notificationType) {
         return emailNotificationSettingRepository.findByUserIdAndNotificationType(userId, notificationType);
     }
 
+    /**
+     * Creates or updates a user's email notification setting.
+     * If the setting exists, it will be updated; otherwise, a new one will be created.
+     *
+     * @param user             the user entity
+     * @param notificationType the type of notification
+     * @param enabled          whether the notification is enabled
+     * @return the saved email notification setting
+     */
     public EmailNotificationSetting createOrUpdateSetting(User user, EmailNotificationType notificationType, boolean enabled) {
         Optional<EmailNotificationSetting> existingSetting = emailNotificationSettingRepository.findByUserIdAndNotificationType(user.getId(), notificationType);
 
@@ -51,6 +73,14 @@ public class EmailNotificationSettingService {
         return emailNotificationSettingRepository.save(setting);
     }
 
+    /**
+     * Checks whether a specific notification is enabled for a given user.
+     * Defaults to true if no explicit setting exists.
+     *
+     * @param userId           the ID of the user
+     * @param notificationType the type of notification
+     * @return true if the notification is enabled or no setting exists, false otherwise
+     */
     public boolean isNotificationEnabled(Long userId, EmailNotificationType notificationType) {
         // Default to true if no setting exists
         return emailNotificationSettingRepository.findByUserIdAndNotificationType(userId, notificationType).map(EmailNotificationSetting::getEnabled).orElse(true);
