3a8e827fcaabafcd4c327f9bbf5c011e30279279
Eylül Naz Can <eylulnc@gmail.com>
2025-05-25 19:19:02 +0200

    add missing annotation

--- src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateEmailNotificationSettingDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateEmailNotificationSettingDTO.java
@@ -1,9 +1,12 @@
 package de.tum.cit.aet.artemis.communication.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * DTO for updating the enabled state of an email notification setting.
  *
  * @param enabled whether the email notification type is enabled
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record UpdateEmailNotificationSettingDTO(Boolean enabled) {
 }
