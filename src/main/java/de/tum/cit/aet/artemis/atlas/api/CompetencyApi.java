fc386d2698cfffcf32539fc66ad81dd257902c87
Tobias Lippert <84102468+tobias-lippert@users.noreply.github.com>
2025-05-29 08:25:38 +0200

    Development: Polish PyrisPipelineService (#10932)

--- src/main/java/de/tum/cit/aet/artemis/atlas/api/CompetencyApi.java
+++ src/main/java/de/tum/cit/aet/artemis/atlas/api/CompetencyApi.java
@@ -1,13 +1,9 @@
 package de.tum.cit.aet.artemis.atlas.api;
 
-import java.util.Set;
-
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 
 import de.tum.cit.aet.artemis.atlas.config.AtlasEnabled;
-import de.tum.cit.aet.artemis.atlas.domain.competency.Competency;
-import de.tum.cit.aet.artemis.atlas.repository.CompetencyRepository;
 import de.tum.cit.aet.artemis.atlas.service.competency.CompetencyService;
 import de.tum.cit.aet.artemis.lecture.domain.Lecture;
 
@@ -17,18 +13,11 @@ public class CompetencyApi extends AbstractAtlasApi {
 
     private final CompetencyService competencyService;
 
-    private final CompetencyRepository competencyRepository;
-
-    public CompetencyApi(CompetencyService competencyService, CompetencyRepository competencyRepository) {
+    public CompetencyApi(CompetencyService competencyService) {
         this.competencyService = competencyService;
-        this.competencyRepository = competencyRepository;
     }
 
     public void addCompetencyLinksToExerciseUnits(Lecture lecture) {
         competencyService.addCompetencyLinksToExerciseUnits(lecture);
-    }
-
-    public Set<Competency> findAllByCourseId(long courseId) {
-        return competencyRepository.findAllByCourseId(courseId);
     }
 }
