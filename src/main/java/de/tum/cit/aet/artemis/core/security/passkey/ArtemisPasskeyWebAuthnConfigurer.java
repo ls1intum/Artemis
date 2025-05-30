993ddc03ab95ba42ba5b25bf9b556c82bfa9a061
Tobias Lippert <t.lippert@tum.de>
2025-05-30 12:26:22 +0200

    make passkey conditional

--- src/main/java/de/tum/cit/aet/artemis/core/security/passkey/ArtemisPasskeyWebAuthnConfigurer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/security/passkey/ArtemisPasskeyWebAuthnConfigurer.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Component;
 
 import de.tum.cit.aet.artemis.communication.service.notifications.MailSendingService;
 import de.tum.cit.aet.artemis.core.config.Constants;
+import de.tum.cit.aet.artemis.core.config.PasskeyEnabled;
 import de.tum.cit.aet.artemis.core.repository.UserRepository;
 import de.tum.cit.aet.artemis.core.security.jwt.JWTCookieService;
 import de.tum.cit.aet.artemis.core.service.AndroidFingerprintService;
@@ -36,7 +37,7 @@ import de.tum.cit.aet.artemis.core.util.AndroidApkKeyHashUtil;
  * Configurer for WebAuthn passkey authentication in Artemis.
  */
 @Component
-@Profile(Constants.PROFILE_CORE)
+@Conditional(PasskeyEnabled.class)
 public class ArtemisPasskeyWebAuthnConfigurer {
 
     private static final Logger log = LoggerFactory.getLogger(ArtemisPasskeyWebAuthnConfigurer.class);
