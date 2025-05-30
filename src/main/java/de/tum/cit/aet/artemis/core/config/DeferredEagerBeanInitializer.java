051fbccebd69f5b4244c5ec4c3b82ecc3226833e
Tobias Lippert <t.lippert@tum.de>
2025-05-30 20:33:27 +0200

    use parallel stream instead of ThreadPoolExecutor to minimize overhead

--- src/main/java/de/tum/cit/aet/artemis/core/config/DeferredEagerBeanInitializer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/DeferredEagerBeanInitializer.java
@@ -3,6 +3,8 @@ package de.tum.cit.aet.artemis.core.config;
 import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_BUILDAGENT;
 import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -10,7 +12,6 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 /**
@@ -27,34 +28,26 @@ public class DeferredEagerBeanInitializer {
 
     private final ConfigurableApplicationContext context;
 
-    private final ThreadPoolTaskExecutor executor;
-
-    public DeferredEagerBeanInitializer(ConfigurableApplicationContext context, ThreadPoolTaskExecutor deferredEagerInitializationTaskExecutor) {
+    public DeferredEagerBeanInitializer(ConfigurableApplicationContext context) {
         this.context = context;
-        this.executor = deferredEagerInitializationTaskExecutor;
     }
 
     public void initializeDeferredEagerBeans() {
         DefaultListableBeanFactory bf = (DefaultListableBeanFactory) context.getBeanFactory();
-
-        for (String name : bf.getBeanDefinitionNames()) {
+        Arrays.stream(bf.getBeanDefinitionNames()).parallel().filter(name -> {
             BeanDefinition def = bf.getBeanDefinition(name);
-            if (def.getRole() == BeanDefinition.ROLE_APPLICATION && def.isSingleton() && def.isLazyInit()) {
-
-                executor.submit(() -> {
-                    try {
-                        // accessing a bean will trigger its initialization
-                        context.getBean(name);
-                        log.debug("Deferred eager initialization of bean {} completed", name);
-                    }
-                    catch (Throwable ex) {
-                        log.warn("Deferred eager initialization of bean {} failed", name, ex);
-                    }
-                });
+            return def.isSingleton() && def.isLazyInit();
+        }).forEach(name -> {
+            try {
+                // trigger initialization
+                context.getBean(name);
+                log.debug("Deferred eager initialization of bean {} completed", name);
             }
-        }
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.shutdown();
-        log.info("DeferredBeanPreloader executor shutdown after submitting all tasks");
+            catch (Throwable ex) {
+                log.warn("Deferred eager initialization of bean {} failed", name, ex);
+            }
+        });
+
+        log.info("Deferred eager initialization of all beans completed");
     }
 }
