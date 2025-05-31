ed19d9a57aacfcb74a28b37adbd23f0e10d9cc90
Florian Glombik <florian.glombik@tum.de>
2025-05-31 02:12:42 +0200

    Adjust todo

--- src/main/java/de/tum/cit/aet/artemis/core/web/open/PublicUserJwtResource.java
+++ src/main/java/de/tum/cit/aet/artemis/core/web/open/PublicUserJwtResource.java
@@ -98,7 +98,7 @@ public class PublicUserJwtResource {
             ResponseCookie responseCookie = jwtCookieService.buildLoginCookie(rememberMe, tool);
             response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
 
-            // TODO: move this to the actual login implementations
+            // TODO add a long delay to verify it is asny or needs to be marked async
             artemisSuccessfulLoginService.sendLoginEmail(username, AuthenticationMethod.PASSWORD, HttpRequestUtils.getClientEnvironment(request));
 
             return ResponseEntity.ok(Map.of("access_token", responseCookie.getValue()));
