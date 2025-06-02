9b6a9346afb0005951fc9922f4499fce832b0caa
jfr2102 <johannes@friedlein.eu>
2025-06-02 23:11:27 +0200

    add DistributedMap and DistributedTopic javadoc

--- src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributedData/api/DistributedTopic.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributedData/api/DistributedTopic.java
@@ -5,7 +5,18 @@ import java.util.function.Consumer;
 
 public interface DistributedTopic<T> {
 
+    /**
+     * Publishes a message to the topic.
+     *
+     * @param message the message to publish
+     */
     void publish(T message);
 
+    /**
+     * Adds a message listener to the topic.
+     *
+     * @param messageConsumer the consumer that will handle incoming messages
+     * @return a unique identifier for the listener, which can be used to remove it later
+     */
     UUID addMessageListener(Consumer<T> messageConsumer);
 }
