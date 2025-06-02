ed1afb251754f5132fd66aa7ad50bc0f96f6c63d
jfr2102 <johannes@friedlein.eu>
2025-06-02 23:03:58 +0200

    add DistributedQueue javadoc

--- src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributedData/api/queue/DistributedQueue.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributedData/api/queue/DistributedQueue.java
@@ -8,25 +8,81 @@ import de.tum.cit.aet.artemis.programming.service.localci.distributedData.api.qu
 
 public interface DistributedQueue<T> {
 
+    /**
+     * Adds an item to the queue.
+     *
+     * @param item the item to add
+     * @return {@code true} if the item was added successfully, {@code false} otherwise
+     */
     boolean add(T item);
 
+    /**
+     * Retrieves and removes the head of the queue.
+     *
+     * @return the head of the queue, or {@code null} if the queue is empty
+     */
     T poll();
 
+    /**
+     * Retrieves, but does not remove, the head of the queue.
+     *
+     * @return the head of the queue, or {@code null} if the queue is empty
+     */
     T peek();
 
+    /**
+     * Clears the queue, removing all items.
+     */
     void clear();
 
+    /**
+     * Adds all items from the specified collection to the queue.
+     *
+     * @param items the collection of items to add
+     * @return {@code true} if the queue was modified as a result of the call, {@code false} otherwise
+     */
     boolean addAll(Collection<T> items);
 
+    /**
+     * Removes all specified items from the queue.
+     *
+     * @param items the collection of items to remove
+     */
     void removeAll(Collection<T> items);
 
+    /**
+     * Retrieves all items currently in the queue.
+     *
+     * @return a list of all items in the queue
+     */
     List<T> getAll();
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return {@code true} if the queue contains no items, {@code false} otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * Returns the number of items in the queue.
+     *
+     * @return the size of the queue
+     */
     int size();
 
+    /**
+     * Adds a listener that will be notified when items are added or removed from the queue.
+     *
+     * @param listener the listener to add
+     * @return a unique identifier for the registration, which can be used to remove the listener later
+     */
     UUID addItemListener(QueueItemListener<T> listener);
 
+    /**
+     * Removes a previously registered item listener using its unique identifier.
+     *
+     * @param registrationId the unique identifier of the listener to remove
+     */
     void removeItemListener(UUID registrationId);
 }
