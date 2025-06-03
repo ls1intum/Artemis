bd2c3ffc5ad2b227e1f4ce4c0480e4b450cd8f8c
Tobias Lippert <t.lippert@tum.de>
2025-06-03 12:57:00 +0200

    remove more unnecessary files

--- src/main/webapp/app/openapi/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './tutorialGroupApi.service';
-import { TutorialGroupApiService } from './tutorialGroupApi.service';
-export * from './tutorialGroupFreePeriodApi.service';
-import { TutorialGroupFreePeriodApiService } from './tutorialGroupFreePeriodApi.service';
-export * from './tutorialGroupSessionApi.service';
-import { TutorialGroupSessionApiService } from './tutorialGroupSessionApi.service';
-export const APIS = [TutorialGroupApiService, TutorialGroupFreePeriodApiService, TutorialGroupSessionApiService];
