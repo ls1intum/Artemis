ebc07bb1a79f107cbc9a8df3cdc8bdec28dcec5a
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-06-05 00:16:32 +0200

    Add isStandalone property to component

--- src/main/webapp/app/core/course/overview/exercise-details/problem-statement/problem-statement.component.ts
+++ src/main/webapp/app/core/course/overview/exercise-details/problem-statement/problem-statement.component.ts
@@ -1,4 +1,5 @@
 import { HttpResponse } from '@angular/common/http';
+import { NgClass } from '@angular/common';
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Exercise, ExerciseType } from 'app/exercise/shared/entities/exercise/exercise.model';
@@ -13,7 +14,7 @@ import { HtmlForMarkdownPipe } from 'app/shared/pipes/html-for-markdown.pipe';
     selector: 'jhi-problem-statement',
     templateUrl: './problem-statement.component.html',
     styleUrls: ['../../course-overview/course-overview.scss'],
-    imports: [ProgrammingExerciseInstructionComponent, TranslateDirective, HtmlForMarkdownPipe],
+    imports: [ProgrammingExerciseInstructionComponent, TranslateDirective, HtmlForMarkdownPipe, NgClass],
 })
 export class ProblemStatementComponent implements OnInit {
     private route = inject(ActivatedRoute);
@@ -22,6 +23,7 @@ export class ProblemStatementComponent implements OnInit {
 
     @Input() public exercise?: Exercise;
     @Input() participation?: StudentParticipation;
+    isStandalone: boolean;
 
     ngOnInit() {
         this.route.params.subscribe((params) => {
@@ -41,6 +43,10 @@ export class ProblemStatementComponent implements OnInit {
                     this.participation = participationResponse.body!;
                 });
             }
+        });
+        // Check whether problem statement is displayed standalone (mobile apps)
+        this.route.url.subscribe((segments) => {
+            this.isStandalone = segments.some(segment => segment.path == "problem-statement");
         });
     }
 
