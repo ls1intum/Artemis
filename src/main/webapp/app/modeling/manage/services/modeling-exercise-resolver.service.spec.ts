c62e6999a6aa48733c0c4ad08954413107c63bf9
Florian Glombik <florian.glombik@tum.de>
2025-06-10 13:10:15 +0200

    Add test

--- src/main/webapp/app/modeling/manage/services/modeling-exercise-resolver.service.spec.ts
+++ src/main/webapp/app/modeling/manage/services/modeling-exercise-resolver.service.spec.ts
@@ -2,12 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { ModelingExerciseResolver } from 'app/modeling/manage/services/modeling-exercise-resolver.service';
 import { ModelingExerciseService } from 'app/modeling/manage/services/modeling-exercise.service';
 import { MockProvider } from 'ng-mocks';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { CourseManagementService } from 'app/core/course/manage/services/course-management.service';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { ModelingExercise } from 'app/modeling/shared/entities/modeling-exercise.model';
+import { UMLDiagramType } from '@ls1intum/apollon';
+import { of } from 'rxjs';
 
 describe('ModelingExerciseResolver', () => {
     let component: ModelingExerciseResolver;
+    let currentRoute: ActivatedRouteSnapshot;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -15,9 +20,25 @@ describe('ModelingExerciseResolver', () => {
         });
 
         component = TestBed.inject(ModelingExerciseResolver);
+
+        currentRoute = {} as ActivatedRouteSnapshot;
     });
 
     it('should be created', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should resolve a ModelingExercise when exerciseId is provided', () => {
+        const dummyExercise = new ModelingExercise(UMLDiagramType.ClassDiagram, undefined, undefined);
+        const modelingExerciseService = TestBed.inject(ModelingExerciseService);
+        jest.spyOn(modelingExerciseService, 'find').mockReturnValue(of(new HttpResponse({ body: dummyExercise })));
+
+        currentRoute.params = { exerciseId: '123' };
+
+        component.resolve(currentRoute).subscribe((result) => {
+            expect(result).toEqual(dummyExercise);
+        });
+
+        expect(modelingExerciseService.find).toHaveBeenCalledWith('123');
     });
 });
