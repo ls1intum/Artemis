2cce3fa067ddefe0488db7336cd124a6927b3cc4
Moritz Spengler <moco_spengler@gmx.de>
2025-06-11 22:35:40 +0200

    Fix Mistake on last commit

--- src/main/webapp/app/core/course/overview/course-practice/course-practice.component.ts
+++ src/main/webapp/app/core/course/overview/course-practice/course-practice.component.ts
@@ -2,7 +2,7 @@ import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonComponent } from 'app/shared/components/buttons/button/button.component';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { throwError } from 'rxjs';
+import { EMPTY } from 'rxjs';
 
 @Component({
     selector: 'jhi-course-practice',
@@ -13,8 +13,7 @@ export class CoursePracticeComponent {
     private router = inject(Router);
     private route = inject(ActivatedRoute);
 
-    paramsSignal = toSignal(this.route.parent?.params ?? throwError(() => new Error('parent.params fehlt')));
-    //paramsSignal = toSignal(this.route.parent?.params);
+    paramsSignal = toSignal(this.route.parent?.params ?? EMPTY);
     courseId = computed(() => this.paramsSignal()?.['courseId']);
 
     public navigateToPractice(): void {
