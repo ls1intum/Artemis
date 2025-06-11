a28a3d518ddd1f817f391e8911bfbcbf4e0a08b7
Moritz Spengler <moco_spengler@gmx.de>
2025-06-11 21:52:08 +0200

    Adjust route.parent

--- src/main/webapp/app/core/course/overview/course-practice/course-practice.component.ts
+++ src/main/webapp/app/core/course/overview/course-practice/course-practice.component.ts
@@ -2,7 +2,6 @@ import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonComponent } from 'app/shared/components/buttons/button/button.component';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { EMPTY } from 'rxjs';
 
 @Component({
     selector: 'jhi-course-practice',
@@ -13,7 +12,7 @@ export class CoursePracticeComponent {
     private router = inject(Router);
     private route = inject(ActivatedRoute);
 
-    paramsSignal = toSignal(this.route.parent?.params ?? EMPTY);
+    paramsSignal = toSignal(this.route.parent?.params!);
     courseId = computed(() => this.paramsSignal()?.['courseId']);
 
     public navigateToPractice(): void {
