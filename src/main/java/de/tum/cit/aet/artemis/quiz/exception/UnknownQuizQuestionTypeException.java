c01c46f5a891293ea9a60bbbc0bfde3afe7ba468
Samuel <samu.roettgermann@t-online.de>
2025-06-12 03:55:42 +0200

    exceptions now extend an Artemis exception type

--- src/main/java/de/tum/cit/aet/artemis/quiz/exception/UnknownQuizQuestionTypeException.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/exception/UnknownQuizQuestionTypeException.java
@@ -1,10 +1,12 @@
 package de.tum.cit.aet.artemis.quiz.exception;
 
+import de.tum.cit.aet.artemis.core.exception.EntityNotFoundException;
 import de.tum.cit.aet.artemis.quiz.domain.QuizQuestion;
 
-public class UnknownQuizQuestionTypeException extends RuntimeException {
+public class UnknownQuizQuestionTypeException extends EntityNotFoundException {
 
     public UnknownQuizQuestionTypeException(QuizQuestion question) {
-        super("Artemis doesn't recognize \"" + question.getExercise().getType() + "\" as a valid quiz question type");
+        super("Artemis doesn't recognize \"" + ((question != null && question.getExercise() != null) ? question.getExercise().getType() : "unknown")
+                + "\" as a valid quiz question type");
     }
 }
