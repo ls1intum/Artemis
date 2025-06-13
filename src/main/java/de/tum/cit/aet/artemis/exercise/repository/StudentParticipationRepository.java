6d31932eacaa9d36194d63060ad9ff138d92b6a5
jfr2102 <johannes@friedlein.eu>
2025-06-13 19:22:47 +0200

    remove useless condition in filterParticipationsWithRelevantResults

--- src/main/java/de/tum/cit/aet/artemis/exercise/repository/StudentParticipationRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/repository/StudentParticipationRepository.java
@@ -921,7 +921,7 @@ public interface StudentParticipationRepository extends ArtemisJpaRepository<Stu
         var participations = findByExamIdWithEagerLegalSubmissionsRatedResults(examId); // without test run participations
         // filter out the participations of test runs which can only be made by instructors
         participations = participations.stream().filter(studentParticipation -> !studentParticipation.isTestRun()).toList();
-        return filterParticipationsWithRelevantResults(participations, true);
+        return filterParticipationsWithRelevantResults(participations);
     }
 
     /**
@@ -933,7 +933,7 @@ public interface StudentParticipationRepository extends ArtemisJpaRepository<Stu
      */
     default List<StudentParticipation> findByCourseIdAndStudentIdWithRelevantResult(long courseId, long studentId) {
         List<StudentParticipation> participations = findByCourseIdAndStudentIdWithEagerRatedResults(courseId, studentId);
-        return filterParticipationsWithRelevantResults(participations, false);
+        return filterParticipationsWithRelevantResults(participations);
     }
 
     /**
@@ -944,17 +944,16 @@ public interface StudentParticipationRepository extends ArtemisJpaRepository<Stu
      */
     default List<StudentParticipation> findByCourseIdWithRelevantResult(long courseId) {
         List<StudentParticipation> participations = findByCourseIdWithEagerRatedResults(courseId);
-        return filterParticipationsWithRelevantResults(participations, false);
+        return filterParticipationsWithRelevantResults(participations);
     }
 
     /**
      * filters the relevant results by removing all irrelevant ones
      *
-     * @param participations     the participations to get filtered
-     * @param resultInSubmission flag to indicate if the results are represented in the submission or participation
+     * @param participations the participations to get filtered
      * @return an unmodifiable list of filtered participations
      */
-    private List<StudentParticipation> filterParticipationsWithRelevantResults(List<StudentParticipation> participations, boolean resultInSubmission) {
+    private List<StudentParticipation> filterParticipationsWithRelevantResults(List<StudentParticipation> participations) {
         return participations.stream()
                 // Filter out participations without Students
                 // These participations are used e.g. to store template and solution build plans in programming exercises
@@ -964,15 +963,9 @@ public interface StudentParticipationRepository extends ArtemisJpaRepository<Stu
                 .peek(participation -> {
                     List<Result> relevantResults = new ArrayList<>();
 
-                    // Get the results over the participation or over submissions
-                    Set<Result> resultsOfParticipation;
-                    if (resultInSubmission) {
-                        resultsOfParticipation = participation.getSubmissions().stream().map(Submission::getLatestResult).collect(Collectors.toSet());
-                    }
-                    else {
-                        resultsOfParticipation = participation.getSubmissions().stream()
-                                .flatMap(submission -> Stream.ofNullable(submission.getResults()).flatMap(Collection::stream)).collect(Collectors.toSet());
-                    }
+                    Set<Result> resultsOfParticipation = participation.getSubmissions().stream().map(Submission::getLatestResult).collect(Collectors.toSet());
+                    ;
+
                     // search for the relevant result by filtering out irrelevant results using the continue keyword
                     // this for loop is optimized for performance and thus not very easy to understand ;)
                     for (Result result : resultsOfParticipation) {
