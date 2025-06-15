f1c09119a8c21314bb91f7169dfe07561ffca585
Florian Glombik <florian.glombik@tum.de>
2025-06-15 17:43:40 +0200

    Fix format

--- supporting_scripts/code-coverage/module-coverage-client/check-client-module-coverage.mjs
+++ supporting_scripts/code-coverage/module-coverage-client/check-client-module-coverage.mjs
@@ -149,7 +149,9 @@ const evaluateAndPrintMetrics = (module, aggregatedMetrics, thresholds) => {
         const threshold = thresholds[metric];
         const pass = roundedPercentage >= threshold;
         const higherThanExpected = roundedPercentage > threshold && threshold < AIMED_FOR_COVERAGE;
-        console.log(`  ${pass ? '✅' : '❌'} ${higherThanExpected ? '⬆️' : '   '} ${metric.padEnd(12)} : ${percentage.toFixed(2)}%  (need ≥ ${threshold}%)`);
+
+        const status = `${pass ? '✅' : '❌'}${higherThanExpected ? ' ⬆️' : ''}`.padEnd(8);
+        console.log(`  ${status} ${metric.padEnd(12)}: ${percentage.toFixed(2)}%  (need ≥ ${threshold}%)`);
         if (!pass) failed = true;
     }
     return failed;
