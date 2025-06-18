547aed2fcb69cc7675a6745abdf67929e9d4a2d5
Florian Glombik <florian.glombik@tum.de>
2025-06-18 17:39:59 +0200

    Fix client test setup

--- src/main/webapp/app/core/user/settings/iris-settings/iris-settings.component.spec.ts
+++ src/main/webapp/app/core/user/settings/iris-settings/iris-settings.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IrisSettingsComponent } from './iris-settings.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { IrisChatService } from 'app/iris/overview/services/iris-chat.service';
+import { MockDirective, MockProvider } from 'ng-mocks';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 describe('IrisSettingsComponent', () => {
     let component: IrisSettingsComponent;
@@ -9,6 +15,8 @@ describe('IrisSettingsComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [IrisSettingsComponent],
+            declarations: [MockDirective(TranslateDirective)],
+            providers: [MockProvider(IrisChatService), MockProvider(TranslateService), provideHttpClient(), provideHttpClientTesting()],
         }).compileComponents();
 
         fixture = TestBed.createComponent(IrisSettingsComponent);
