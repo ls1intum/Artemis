895d7d8c3a82b47c55c8c0be90eb741deec4d8e7
Florian Glombik <florian.glombik@tum.de>
2025-06-18 19:07:46 +0200

    Format date with datepipe

--- src/main/webapp/app/core/user/settings/iris-settings/iris-settings.component.ts
+++ src/main/webapp/app/core/user/settings/iris-settings/iris-settings.component.ts
@@ -3,10 +3,11 @@ import { TranslateDirective } from 'app/shared/language/translate.directive';
 import { IrisChatService } from 'app/iris/overview/services/iris-chat.service';
 import { AccountService } from 'app/core/auth/account.service';
 import dayjs from 'dayjs/esm';
+import { ArtemisDatePipe } from 'app/shared/pipes/artemis-date.pipe';
 
 @Component({
     selector: 'jhi-iris-settings',
-    imports: [TranslateDirective],
+    imports: [TranslateDirective, ArtemisDatePipe],
     templateUrl: './iris-settings.component.html',
 })
 export class IrisSettingsComponent implements OnInit {
