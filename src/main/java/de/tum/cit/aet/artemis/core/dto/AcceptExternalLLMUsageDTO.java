ed7224a7c8237cd670e2e302c190150cf6abe314
Florian Glombik <florian.glombik@tum.de>
2025-06-18 19:24:48 +0200

    Primitive cannot be null

--- src/main/java/de/tum/cit/aet/artemis/core/dto/AcceptExternalLLMUsageDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/AcceptExternalLLMUsageDTO.java
@@ -1,9 +1,7 @@
 package de.tum.cit.aet.artemis.core.dto;
 
-import jakarta.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record AcceptExternalLLMUsageDTO(@NotNull boolean accepted) {
+public record AcceptExternalLLMUsageDTO(boolean accepted) {
 }
