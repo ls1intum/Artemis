c258bd4b9f9475d5e1effdb18a6e736c386d3279
Stephan Krusche <krusche@tum.de>
2025-06-22 19:19:41 +0200

    Development: Remove unsupported modeling plagiarism results from the database (#11048)

--- src/main/webapp/app/text/manage/text-exercise/service/text-exercise.service.spec.ts
+++ src/main/webapp/app/text/manage/text-exercise/service/text-exercise.service.spec.ts
@@ -15,7 +15,7 @@ import { PlagiarismOptions } from 'app/plagiarism/shared/entities/PlagiarismOpti
 import dayjs from 'dayjs/esm';
 import { StudentParticipation } from 'app/exercise/shared/entities/participation/student-participation.model';
 import { TutorEffort } from 'app/assessment/shared/entities/tutor-effort.model';
-import { TextPlagiarismResult } from 'app/plagiarism/shared/entities/text/TextPlagiarismResult';
+import { PlagiarismResult } from 'app/plagiarism/shared/entities/PlagiarismResult';
 
 describe('TextExercise Service', () => {
     let service: TextExerciseService;
@@ -44,7 +44,7 @@ describe('TextExercise Service', () => {
         elemDefault.dueDate = dayjs();
         elemDefault.releaseDate = dayjs();
         elemDefault.studentParticipations = new Array<StudentParticipation>();
-        plagiarismResults = new TextPlagiarismResult();
+        plagiarismResults = new PlagiarismResult();
         plagiarismResults.exercise = elemDefault;
         plagiarismResults.comparisons = [];
         plagiarismResults.duration = 40;
