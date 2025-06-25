0927430be817ff9f57fbbf52bf534b36dfc79f54
isabella <isabella.gessl@tum.de>
2025-06-25 15:33:26 +0200

    add javadoc

--- src/main/java/de/tum/cit/aet/artemis/iris/domain/session/IrisChatMode.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/domain/session/IrisChatMode.java
@@ -1,5 +1,8 @@
 package de.tum.cit.aet.artemis.iris.domain.session;
 
+/**
+ * This enum represents the different types of Iris chats.
+ */
 public enum IrisChatMode {
 
     TEXT_EXERCISE("text-exercise-chat"), PROGRAMMING_EXERCISE("programming-exercise-chat"), COURSE("course-chat"), LECTURE("lecture-chat"), TUTOR_SUGGESTION("tutor-suggestion");
