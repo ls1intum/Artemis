7eba84edec04754a00dbeeb8db8ed808de790a68
Stephan Krusche <krusche@tum.de>
2025-06-27 20:14:24 +0200

    try out logging bean instantiation on production

--- src/main/java/de/tum/cit/aet/artemis/core/config/BeanInstantiationLogger.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/BeanInstantiationLogger.java
@@ -1,19 +1,17 @@
 package de.tum.cit.aet.artemis.core.config;
 
-import static tech.jhipster.config.JHipsterConstants.SPRING_PROFILE_DEVELOPMENT;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import de.tum.cit.aet.artemis.ArtemisApp;
 import de.tum.cit.aet.artemis.core.util.TimeLogUtil;
 
-@Profile(SPRING_PROFILE_DEVELOPMENT)
+// TODO: activate the following line after debugging
+// @Profile(SPRING_PROFILE_DEVELOPMENT)
 @Component
 @Lazy
 public class BeanInstantiationLogger implements BeanPostProcessor {
