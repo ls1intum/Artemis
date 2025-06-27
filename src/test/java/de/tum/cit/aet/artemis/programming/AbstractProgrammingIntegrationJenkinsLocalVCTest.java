3aed11afe8f885cc254276af52b9c58a6d4d5986
Konrad Gößmann <konrad.goessmann@tum.de>
2025-06-27 23:25:28 +0200

    Reintegrate model an i18n and remove tests

--- src/test/java/de/tum/cit/aet/artemis/programming/AbstractProgrammingIntegrationJenkinsLocalVCTest.java
+++ src/test/java/de/tum/cit/aet/artemis/programming/AbstractProgrammingIntegrationJenkinsLocalVCTest.java
@@ -25,7 +25,6 @@ import de.tum.cit.aet.artemis.modeling.util.ModelingExerciseUtilService;
 import de.tum.cit.aet.artemis.plagiarism.repository.PlagiarismCaseRepository;
 import de.tum.cit.aet.artemis.programming.repository.BuildPlanRepository;
 import de.tum.cit.aet.artemis.programming.repository.ProgrammingExerciseBuildConfigRepository;
-import de.tum.cit.aet.artemis.programming.repository.SolutionProgrammingExerciseParticipationRepository;
 import de.tum.cit.aet.artemis.programming.service.ConsistencyCheckTestService;
 import de.tum.cit.aet.artemis.programming.service.ProgrammingExerciseService;
 import de.tum.cit.aet.artemis.programming.service.ProgrammingExerciseValidationService;
@@ -78,9 +77,6 @@ public abstract class AbstractProgrammingIntegrationJenkinsLocalVCTest extends A
 
     @Autowired
     protected ProgrammingSubmissionTestRepository submissionRepository;
-
-    @Autowired
-    protected SolutionProgrammingExerciseParticipationRepository solutionProgrammingExerciseParticipationRepository;
 
     // External Repositories
     @Autowired
