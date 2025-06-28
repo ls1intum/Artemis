b27515099157717914f3e772bea210c65ec92266
Tobias Lippert <t.lippert@tum.de>
2025-06-28 12:59:21 +0200

    Revert "make findLatestSubmission type-safe"
    This reverts commit e84fa4f9be32d4f80bd88e6633ef2aec5300bbd0.

--- src/main/java/de/tum/cit/aet/artemis/exercise/domain/participation/Participation.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/domain/participation/Participation.java
@@ -235,19 +235,20 @@ public abstract class Participation extends DomainObject implements Participatio
 
     /**
      * Finds the latest legal submission for the participation. Checks if the participation has any submissions. If there are no submissions, return null.
-     * Otherwise, sort the submissions by submission date and return the first. WARNING: The submissions of the participation might not be loaded because of Hibernate and
+     * Otherwise sort the submissions by submission date and return the first. WARNING: The submissions of the participation might not be loaded because of Hibernate and
      * therefore, the function might return null, although the participation has submissions. This might not be high-performance, so use it at your own risk.
      *
      * @param <T> submission type
      * @return the latest submission or null
      */
+    @SuppressWarnings("unchecked")
     @Override
     public <T extends Submission> Optional<T> findLatestSubmission() {
         Set<Submission> submissions = this.submissions;
         if (submissions == null || submissions.isEmpty()) {
             return Optional.empty();
         }
-        return submissions.stream().filter(submission -> submission.getClass().isAssignableFrom(Submission.class)).map(submission -> (T) submission).max(Comparator.naturalOrder());
+        return (Optional<T>) submissions.stream().max(Comparator.naturalOrder());
     }
 
     /**
