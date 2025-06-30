fbac306114db2d7bd0fc98353cddbe4f74636062
ekayandan <ekayandan16@ku.edu.tr>
2025-06-30 14:14:00 +0200

    Development: Enhance database migration approval check to ignore additional merge patterns

--- supporting_scripts/database_approval_check/main.py
+++ supporting_scripts/database_approval_check/main.py
@@ -36,8 +36,10 @@ def is_db_maintainer(user: NamedUser):
 
 last_commit_with_db_changes = None
 for commit in pr.get_commits().reversed:
-    # Ignore merges from the develop branch
-    if commit.commit.message.startswith("Merge branch 'develop' into"):
+    # Ignore merges from the develop branch (local and remote-tracking)
+    if (commit.commit.message.startswith("Merge branch 'develop' into") or 
+        commit.commit.message.startswith("Merge branch 'origin/develop' into") or
+        commit.commit.message.startswith("Merge remote-tracking branch 'origin/develop' into")):
         continue
     if has_db_changes(commit):
         last_commit_with_db_changes = commit
