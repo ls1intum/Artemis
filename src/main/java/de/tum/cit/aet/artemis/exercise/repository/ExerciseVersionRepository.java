a5c261d5c79b8c08f8175f89a6a2545037150cca
Ailipaer Abuduaini <alpharpledez@outlook.com>
2025-07-01 01:12:42 +0200

    Development: annotate ExerciseVersionRepository with @Lazy and @Profile

--- src/main/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseVersionRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseVersionRepository.java
@@ -1,5 +1,9 @@
 package de.tum.cit.aet.artemis.exercise.repository;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
@@ -8,6 +12,8 @@ import de.tum.cit.aet.artemis.exercise.domain.ExerciseVersion;
 /**
  * Spring Data JPA repository for the ExerciseVersion entity.
  */
+@Profile(PROFILE_CORE)
+@Lazy
 @Repository
 public interface ExerciseVersionRepository extends ArtemisJpaRepository<ExerciseVersion, Long> {
     // Basic CRUD operations are inherited from ArtemisJpaRepository
