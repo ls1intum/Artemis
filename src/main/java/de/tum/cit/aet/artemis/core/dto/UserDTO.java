c76e69ded71f53c89a31bab3a2f11f42019d6d0c
Florian Glombik <florian.glombik@tum.de>
2025-07-03 17:08:13 +0200

    Address Coderabbit findings

--- src/main/java/de/tum/cit/aet/artemis/core/dto/UserDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/UserDTO.java
@@ -29,6 +29,8 @@ import de.tum.cit.aet.artemis.core.domain.User;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class UserDTO extends AuditingEntityDTO {
 
+    private static final boolean DEFAULT_IS_LOGGED_IN_WITH_PASSKEY = false;
+
     private Long id;
 
     @NotBlank
@@ -81,7 +83,7 @@ public class UserDTO extends AuditingEntityDTO {
      */
     private boolean askToSetupPasskey = false;
 
-    private boolean isLoggedInWithPasskey = false;
+    private boolean isLoggedInWithPasskey = DEFAULT_IS_LOGGED_IN_WITH_PASSKEY;
 
     private ZonedDateTime externalLLMUsageAccepted;
 
@@ -91,13 +93,13 @@ public class UserDTO extends AuditingEntityDTO {
 
     public UserDTO(User user) {
         this(user.getId(), user.getLogin(), user.getName(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getVisibleRegistrationNumber(), user.getActivated(),
-                user.getImageUrl(), user.getLangKey(), user.isInternal(), user.getCreatedBy(), user.getCreatedDate(), user.getLastModifiedBy(), user.getLastModifiedDate(),
-                user.getAuthorities(), user.getGroups(), user.getOrganizations(), user.getExternalLLMUsageAcceptedTimestamp());
+                user.getImageUrl(), user.getLangKey(), user.isInternal(), DEFAULT_IS_LOGGED_IN_WITH_PASSKEY, user.getCreatedBy(), user.getCreatedDate(), user.getLastModifiedBy(),
+                user.getLastModifiedDate(), user.getAuthorities(), user.getGroups(), user.getOrganizations(), user.getExternalLLMUsageAcceptedTimestamp());
     }
 
     public UserDTO(Long id, String login, String name, String firstName, String lastName, String email, String visibleRegistrationNumber, boolean activated, String imageUrl,
-            String langKey, boolean internal, String createdBy, Instant createdDate, String lastModifiedBy, Instant lastModifiedDate, Set<Authority> authorities,
-            Set<String> groups, Set<Organization> organizations, ZonedDateTime externalLLMUsageAccepted) {
+            String langKey, boolean internal, boolean isLoggedInWithPasskey, String createdBy, Instant createdDate, String lastModifiedBy, Instant lastModifiedDate,
+            Set<Authority> authorities, Set<String> groups, Set<Organization> organizations, ZonedDateTime externalLLMUsageAccepted) {
 
         this.id = id;
         this.login = login;
@@ -120,6 +122,7 @@ public class UserDTO extends AuditingEntityDTO {
         this.groups = groups;
         this.organizations = organizations;
         this.externalLLMUsageAccepted = externalLLMUsageAccepted;
+        this.isLoggedInWithPasskey = isLoggedInWithPasskey;
     }
 
     public Long getId() {
@@ -267,7 +270,8 @@ public class UserDTO extends AuditingEntityDTO {
     public String toString() {
         return "UserDTO{" + "login='" + login + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", imageUrl='"
                 + imageUrl + '\'' + ", activated=" + activated + ", langKey='" + langKey + '\'' + ", createdBy=" + getCreatedBy() + ", createdDate=" + getCreatedDate()
-                + ", lastModifiedBy='" + getLastModifiedBy() + '\'' + ", lastModifiedDate=" + getLastModifiedDate() + ", authorities=" + authorities + "}";
+                + ", lastModifiedBy='" + getLastModifiedBy() + '\'' + ", lastModifiedDate=" + getLastModifiedDate() + ", isLoggedInWithPasskey=" + isLoggedInWithPasskey
+                + ", authorities=" + authorities + "}";
     }
 
     public boolean isInternal() {
