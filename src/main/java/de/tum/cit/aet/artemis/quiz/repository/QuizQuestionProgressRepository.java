95ef8a5b607e2007378ee006537b1847c1f25706
Moritz Spengler <moco_spengler@gmx.de>
2025-07-04 17:18:44 +0200

    fix architecture

--- src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
@@ -2,11 +2,13 @@ package de.tum.cit.aet.artemis.quiz.repository;
 
 import java.util.Optional;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
 import de.tum.cit.aet.artemis.quiz.domain.QuizQuestionProgress;
 
+@Lazy
 @Repository
 public interface QuizQuestionProgressRepository extends ArtemisJpaRepository<QuizQuestionProgress, Long> {
 
