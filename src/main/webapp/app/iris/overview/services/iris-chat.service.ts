07022f2a65713b919f01f3f89b724a9962f47c9c
Florian Glombik <florian.glombik@tum.de>
2025-07-04 15:07:31 +0200

    let courseId throw if no courseId is defined

--- src/main/webapp/app/iris/overview/services/iris-chat.service.ts
+++ src/main/webapp/app/iris/overview/services/iris-chat.service.ts
@@ -430,9 +430,12 @@ export class IrisChatService implements OnDestroy {
      * <p>Required in edge cases where a route requiring the {@link courseId} (e.g., a lecture from the student view)
      * is loaded directly by accessing the link or by reloading the page.</p>
      */
-    public getCourseId(): number | undefined {
+    public getCourseId(): number {
         if (!this.courseId) {
             this.updateCourseId();
+        }
+        if (!this.courseId) {
+            throw new Error('Course ID is not set');
         }
 
         return this.courseId;
