2b7849e49bbbb1b8e7dfcb3f4f1197539e40aaa2
Moritz Spengler <moco_spengler@gmx.de>
2025-07-04 17:52:52 +0200

    Add Profile annotation

--- src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
@@ -1,13 +1,17 @@
 package de.tum.cit.aet.artemis.quiz.repository;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
+
 import java.util.Optional;
 
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
 import de.tum.cit.aet.artemis.quiz.domain.QuizQuestionProgress;
 
+@Profile(PROFILE_CORE)
 @Lazy
 @Repository
 public interface QuizQuestionProgressRepository extends ArtemisJpaRepository<QuizQuestionProgress, Long> {
