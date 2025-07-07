fa8356d63d2a7b9a3ce8e5df22fe345a0db9e0ff
Samuel <samu.roettgermann@t-online.de>
2025-07-07 11:39:26 +0200

    init setup of the exam room repository

---
+++ src/main/java/de/tum/cit/aet/artemis/exam/repository/ExamRoomRepository.java
@@ -0,0 +1,19 @@
+package de.tum.cit.aet.artemis.exam.repository;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
+import de.tum.cit.aet.artemis.exam.config.ExamEnabled;
+import de.tum.cit.aet.artemis.exam.domain.room.ExamRoom;
+
+/**
+ * Spring Data JPA repository for the {@link de.tum.cit.aet.artemis.exam.domain.room.ExamRoom} entity.
+ */
+@Conditional(ExamEnabled.class)
+@Lazy
+@Repository
+public interface ExamRoomRepository extends ArtemisJpaRepository<ExamRoom, Long> {
+
+}
