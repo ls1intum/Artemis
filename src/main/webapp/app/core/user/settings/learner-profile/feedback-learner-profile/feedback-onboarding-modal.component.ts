2a2d7e48e435d13c3f9822a6a5ed2d8fea99d726
ahmetsenturk <iahmetsenturk@yahoo.com>
2025-07-09 19:35:39 +0300

    add the navigation logic to the modal

--- src/main/webapp/app/core/user/settings/learner-profile/feedback-learner-profile/feedback-onboarding-modal.component.ts
+++ src/main/webapp/app/core/user/settings/learner-profile/feedback-learner-profile/feedback-onboarding-modal.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextResultComponent } from 'app/text/overview/text-result/text-result.component';
 import { FEEDBACK_EXAMPLES } from './feedback-examples';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LearnerProfileApiService } from '../learner-profile-api.service';
+import { LearnerProfileDTO } from '../dto/learner-profile-dto.model';
+import { AlertService, AlertType } from 'app/shared/service/alert.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-feedback-onboarding-modal',
@@ -16,6 +21,9 @@ export class FeedbackOnboardingModalComponent {
     selected: (number | null)[] = [null, null, null];
     feedbackExamples = FEEDBACK_EXAMPLES;
 
+    private activeModal = inject(NgbActiveModal);
+    private learnerProfileApiService = inject(LearnerProfileApiService);
+    private alertService = inject(AlertService);
     next() {
         if (this.step < this.totalSteps - 1) {
             this.step++;
@@ -27,12 +35,53 @@ export class FeedbackOnboardingModalComponent {
         }
     }
     select(step: number, choice: number) {
-        this.selected[step] = choice;
+        if (this.selected[step] === choice) {
+            this.selected[step] = null;
+        } else {
+            this.selected[step] = choice;
+        }
     }
     close() {
-        // To be implemented: emit close event or call modal service
+        this.activeModal.close();
     }
-    finish() {
-        // To be implemented: save preferences and close
+    async finish() {
+        try {
+            const profile = await this.learnerProfileApiService.getLearnerProfileForCurrentUser();
+            const mapValue = (val: number | null) => (val === null ? 2 : val === 0 ? 1 : 3);
+            const updatedProfile = new LearnerProfileDTO({
+                id: profile.id,
+                feedbackAlternativeStandard: mapValue(this.selected[0]),
+                feedbackFollowupSummary: mapValue(this.selected[1]),
+                feedbackBriefDetailed: mapValue(this.selected[2]),
+            });
+            await this.learnerProfileApiService.putUpdatedLearnerProfile(updatedProfile);
+            this.alertService.closeAll();
+            this.alertService.addAlert({
+                type: AlertType.SUCCESS,
+                message: 'artemisApp.learnerProfile.feedbackLearnerProfile.profileSaved',
+            });
+            this.activeModal.close();
+        } catch (error) {
+            this.handleError(error);
+            this.activeModal.close();
+        }
+    }
+
+    /**
+     * Handles errors that occur during API calls.
+     * @param error - The error that occurred
+     */
+    private handleError(error: unknown): void {
+        if (error instanceof HttpErrorResponse) {
+            this.alertService.addAlert({
+                type: AlertType.DANGER,
+                message: error.message,
+            });
+        } else {
+            this.alertService.addAlert({
+                type: AlertType.DANGER,
+                message: 'artemisApp.learnerProfile.feedbackLearnerProfile.error',
+            });
+        }
     }
 }
