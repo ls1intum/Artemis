7aa6919a49e38bfacf85c2f21c3d8e50b94039e2
Marlon Nienaber <109298356+marlonnienaber@users.noreply.github.com>
2025-07-10 21:22:54 +0200

    Update src/main/java/de/tum/cit/aet/artemis/iris/dao/IrisChatSessionDAO.java
    Co-authored-by: Florian Glombik <63976129+florian-glombik@users.noreply.github.com>

--- src/main/java/de/tum/cit/aet/artemis/iris/dao/IrisChatSessionDAO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/dao/IrisChatSessionDAO.java
@@ -8,6 +8,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.artemis.iris.domain.session.IrisChatSession;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record IrisChatSessionDAO(@NotNull IrisChatSession session, @NotNull long entityId, @Nullable String entityName) {
+public record IrisChatSessionDAO(@NotNull IrisChatSession session, long entityId, @Nullable String entityName) {
 
 }
