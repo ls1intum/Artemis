0b00acb1b1a85ab6079f35dca9f0814080679a89
Marlon Nienaber <109298356+marlonnienaber@users.noreply.github.com>
2025-07-10 21:23:06 +0200

    Update src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisChatSessionDTO.java
    Co-authored-by: Florian Glombik <63976129+florian-glombik@users.noreply.github.com>

--- src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisChatSessionDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisChatSessionDTO.java
@@ -10,6 +10,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.artemis.iris.domain.session.IrisChatMode;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record IrisChatSessionDTO(@NotNull long id, @NotNull long entityId, @Nullable String entityName, ZonedDateTime creationDate, IrisChatMode chatMode) {
+public record IrisChatSessionDTO(long id, long entityId, @Nullable String entityName, ZonedDateTime creationDate, IrisChatMode chatMode) {
 
 }
