85fd7c78cfcb1a31d388ba70e80425fd7e44047a
marlonnienaber <marlonnienaber@web.de>
2025-07-10 12:54:26 +0200

    adapt DAO and DTO signatures on server

--- src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisChatSessionDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisChatSessionDTO.java
@@ -2,11 +2,14 @@ package de.tum.cit.aet.artemis.iris.dto;
 
 import java.time.ZonedDateTime;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.iris.domain.session.IrisChatMode;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record IrisChatSessionDTO(Long id, Long entityId, String entityName, ZonedDateTime creationDate, IrisChatMode chatMode) {
+public record IrisChatSessionDTO(@NotNull long id, @NotNull long entityId, @Nullable String entityName, ZonedDateTime creationDate, IrisChatMode chatMode) {
 
 }
