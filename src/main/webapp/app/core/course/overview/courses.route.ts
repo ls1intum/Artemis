e21ac5f86f5317b1c4e743431824ae2b578f9c08
Moritz Spengler <moco_spengler@gmx.de>
2025-07-17 12:48:48 +0200

    redo last changes

--- src/main/webapp/app/core/course/overview/courses.route.ts
+++ src/main/webapp/app/core/course/overview/courses.route.ts
@@ -340,7 +340,6 @@ export const routes: Routes = [
                     hasSidebar: false,
                     showRefreshButton: true,
                 },
-                canActivate: [UserRouteAccessService, CourseOverviewGuard],
             },
             {
                 path: CourseOverviewRoutePath.TRAINING_QUIZ,
@@ -349,7 +348,6 @@ export const routes: Routes = [
                     authorities: [Authority.USER],
                     pageTitle: 'overview.training',
                 },
-                canActivate: [UserRouteAccessService, CourseOverviewGuard],
             },
             {
                 path: '',
