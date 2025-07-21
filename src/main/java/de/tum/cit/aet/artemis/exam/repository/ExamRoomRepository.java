a05b723f23f8c98aa62e8a9841116aafe92da61f
Samuel <samu.roettgermann@t-online.de>
2025-07-21 04:18:58 +0200

    added a query to find all outdated exam rooms

--- src/main/java/de/tum/cit/aet/artemis/exam/repository/ExamRoomRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/repository/ExamRoomRepository.java
@@ -1,10 +1,10 @@
 package de.tum.cit.aet.artemis.exam.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.lang.NonNull;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
@@ -19,5 +19,22 @@ import de.tum.cit.aet.artemis.exam.domain.room.ExamRoom;
 @Repository
 public interface ExamRoomRepository extends ArtemisJpaRepository<ExamRoom, Long> {
 
-    Optional<ExamRoom> findById(@NonNull Long examRoomId);
+    @Query("""
+            WITH latestRooms AS (
+                SELECT roomNumber, alternativeRoomNumber, name, alternativeName, MAX(createdDate) AS createdDate
+                FROM ExamRoom
+                GROUP BY roomNumber, alternativeRoomNumber, name, alternativeName
+                HAVING COUNT(*) > 1
+            )
+            SELECT examRoom
+            FROM ExamRoom examRoom
+            JOIN latestRooms latestRoom
+                ON examRoom.roomNumber = latestRoom.roomNumber
+                AND examRoom.alternativeName = latestRoom.alternativeName
+                AND examRoom.name = latestRoom.name
+                AND examRoom.alternativeName = latestRoom.alternativeName
+                AND examRoom.createdDate < latestRoom.createdDate
+            ORDER BY examRoom.roomNumber, examRoom.alternativeRoomNumber, examRoom.name, examRoom.alternativeName, examRoom.createdDate DESC
+            """)
+    List<ExamRoom> findAllOutdatedExamRooms();
 }
