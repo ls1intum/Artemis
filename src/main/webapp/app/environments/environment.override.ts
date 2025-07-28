57c7e800ce1088dd5a5813aae11aa179f29ab03d
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-07-28 11:05:53 +0200

    Remove environment overrides

--- src/main/webapp/app/environments/environment.override.ts
+++
@@ -1,7 +0,0 @@
-// Don't change this file manually, it will be overwritten by the build process!
-export const __DEBUG_INFO_ENABLED__ = true;
-export const __VERSION__ = '7.1.2';
-// The root URL for API calls, ending with a '/' - for example: `"https://www.jhipster.tech:8081/myservice/"`.
-// If you use an API server, in `prod` mode, you will need to enable CORS
-// (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-export const I18N_HASH = '642f766cde0fc322554afebdee1e1b95';
