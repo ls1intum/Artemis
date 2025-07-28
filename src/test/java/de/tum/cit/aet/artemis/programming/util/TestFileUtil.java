65ea770b7bf07a489b21e4de81de0b2f990f93ed
Tobias Lippert <t.lippert@tum.de>
2025-07-28 10:51:12 +0200

    code rabbit

--- src/test/java/de/tum/cit/aet/artemis/programming/util/TestFileUtil.java
+++ src/test/java/de/tum/cit/aet/artemis/programming/util/TestFileUtil.java
@@ -1,14 +1,15 @@
 package de.tum.cit.aet.artemis.programming.util;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class TestFileUtil {
 
-    public static void writeEmptyJsonFileToPath(Path path) throws Exception {
+    public static void writeEmptyJsonFileToPath(Path path) throws IOException {
         var fileContent = "{}";
-        path.toFile().getParentFile().mkdirs();
+        Files.createDirectories(path.getParent());
         try (var writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
             writer.write(fileContent);
         }
