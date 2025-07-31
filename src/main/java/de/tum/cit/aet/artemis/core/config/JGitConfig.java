2caaaaaf19028a2faaa66a73f984717d9ebb8bd0
Tobias Lippert <t.lippert@tum.de>
2025-07-31 20:58:52 +0200

    apply jgit config

---
+++ src/main/java/de/tum/cit/aet/artemis/core/config/JGitConfig.java
@@ -0,0 +1,26 @@
+package de.tum.cit.aet.artemis.core.config;
+
+import jakarta.annotation.PostConstruct;
+
+import org.eclipse.jgit.storage.file.WindowCacheConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+// this needs to run as early as possible, so JGit is properly configured
+@Lazy(value = false)
+public class JGitConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(JGitConfig.class);
+
+    @PostConstruct
+    public void disablePackedGitMMap() {
+        log.debug("Applying JGit configuration: disable packed git mmap");
+        WindowCacheConfig cfg = new WindowCacheConfig();
+        // this should prevent issues when deleting repositories on NFS filesystems
+        cfg.setPackedGitMMAP(false);
+        cfg.install();
+    }
+}
