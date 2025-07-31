8fd67c656d14b258afd6af531ab6144900237f47
Tobias Lippert <t.lippert@tum.de>
2025-07-31 22:02:15 +0200

    add @Profile

--- src/main/java/de/tum/cit/aet/artemis/core/config/JGitConfig.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/JGitConfig.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 // this needs to run as early as possible, so JGit is properly configured
 @Lazy(value = false)
+@Profile({ Constants.PROFILE_CORE, Constants.PROFILE_BUILDAGENT })
 public class JGitConfig {
 
     private static final Logger log = LoggerFactory.getLogger(JGitConfig.class);
