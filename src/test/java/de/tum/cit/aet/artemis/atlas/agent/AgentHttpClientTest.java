8517f2547d6895d45639da4f048b379dcd54a8f4
Yassine Hmidi <ge84tef@tum.de>
2025-08-04 20:21:04 +0200

    backend artemis integration

--- src/test/java/de/tum/cit/aet/artemis/atlas/agent/AgentHttpClientTest.java
+++ src/test/java/de/tum/cit/aet/artemis/atlas/agent/AgentHttpClientTest.java
@@ -1,4 +1,4 @@
-package de.tum.cit.aet.artemis.atlas.service.agent;
+package de.tum.cit.aet.artemis.atlas.agent;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import de.tum.cit.aet.artemis.atlas.dto.AgentChatRequestDto;
 import de.tum.cit.aet.artemis.atlas.dto.AgentChatResponseDto;
+import de.tum.cit.aet.artemis.atlas.service.agent.AgentHttpClient;
 
 class AgentHttpClientTest {
 
