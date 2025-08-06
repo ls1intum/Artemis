87393a04968e2b44568f7ac9291e4a47ae2655f7
Timor Morrien <morrien@tum.de>
2025-08-07 00:06:07 +0200

    Fix tests

--- src/main/webapp/app/iris/shared/entities/memiris.model.ts
+++ src/main/webapp/app/iris/shared/entities/memiris.model.ts
@@ -1,19 +1,3 @@
-export class MemirisLearning {
-    id: string;
-    title: string;
-    content: string;
-    reference: string;
-    memories: string[]; // Array of memory IDs associated with this learning
-
-    constructor(id: string, title: string, content: string, reference: string, memories: string[]) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.reference = reference;
-        this.memories = memories;
-    }
-}
-
 export class MemirisMemory {
     id: string;
     title: string;
@@ -31,29 +15,5 @@ export class MemirisMemory {
         this.connections = connections;
         this.slept_on = slept_on;
         this.deleted = deleted;
-    }
-}
-
-export enum MemirisConnectionType {
-    RELATED = 'related',
-    CONTRADICTS = 'contradicts',
-    SAME_TOPIC = 'same_topic',
-    DUPLICATE = 'duplicate',
-    CREATED_FROM = 'created_from',
-}
-
-export class MemirisMemoryConnection {
-    id: string;
-    connection_type: MemirisConnectionType;
-    memories: string[]; // Array of memory objects associated with this connection
-    description: string;
-    weight: number;
-
-    constructor(id: string, connection_type: MemirisConnectionType, memories: string[], description: string, weight: number) {
-        this.id = id;
-        this.connection_type = connection_type;
-        this.memories = memories;
-        this.description = description;
-        this.weight = weight;
     }
 }
