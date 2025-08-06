75cdad8b9539ef30050bd5657703f6b24464c550
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-08-06 20:49:48 +0200

    Update comment

--- src/main/java/de/tum/cit/aet/artemis/communication/service/notifications/push_notifications/PushNotificationDataDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/service/notifications/push_notifications/PushNotificationDataDTO.java
@@ -4,9 +4,7 @@ import de.tum.cit.aet.artemis.communication.dto.CourseNotificationSerializedDTO;
 import de.tum.cit.aet.artemis.core.config.Constants;
 
 /**
- * This DTO represents the data that is encrypted and sent to the hermes service. All values except courseNotificationDTO
- * are part of the old notification api. As soon as iOS and Android are able to process the courseNotificationDTO by itself
- * we can remove the initial values in this record.
+ * This DTO represents the data that is encrypted and sent to the hermes service.
  */
 public record PushNotificationDataDTO(int version, CourseNotificationSerializedDTO courseNotificationDTO) {
 
