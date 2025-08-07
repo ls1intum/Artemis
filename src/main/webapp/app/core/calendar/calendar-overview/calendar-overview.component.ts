6900319b6a97c8b12956c26e11e810cea314a5b0
marlonnienaber <marlonnienaber@web.de>
2025-08-07 14:40:15 +0200

    improve conditional rendering in CalendarOverviewComponent

--- src/main/webapp/app/core/calendar/calendar-overview/calendar-overview.component.ts
+++ src/main/webapp/app/core/calendar/calendar-overview/calendar-overview.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
 import dayjs from 'dayjs/esm';
 import isoWeek from 'dayjs/esm/plugin/isoWeek';
 import isSameOrBefore from 'dayjs/esm/plugin/isSameOrBefore';
 import { CalendarDesktopOverviewComponent } from 'app/core/calendar/desktop/overview/calendar-desktop-overview.component';
 import { CalendarMobileOverviewComponent } from 'app/core/calendar/mobile/overview/calendar-mobile-overview';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 dayjs.extend(isoWeek);
 dayjs.extend(isSameOrBefore);
@@ -14,4 +17,8 @@ dayjs.extend(isSameOrBefore);
     templateUrl: './calendar-overview.component.html',
     styleUrl: './calendar-overview.component.scss',
 })
-export class CalendarOverviewComponent {}
+export class CalendarOverviewComponent {
+    private breakpointObserver = inject(BreakpointObserver);
+
+    readonly isMobile = toSignal(this.breakpointObserver.observe(['(max-width: 600px)']).pipe(map((result) => result.matches)), { initialValue: false });
+}
