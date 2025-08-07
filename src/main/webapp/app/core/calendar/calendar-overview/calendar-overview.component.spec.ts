89813d7ec17be78f4f4dc65a1090f44cf452067c
marlonnienaber <marlonnienaber@web.de>
2025-08-07 14:40:37 +0200

    test CalendarOverviewComponent

--- src/main/webapp/app/core/calendar/calendar-overview/calendar-overview.component.spec.ts
+++ src/main/webapp/app/core/calendar/calendar-overview/calendar-overview.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalendarOverviewComponent } from './calendar-overview.component';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { BehaviorSubject } from 'rxjs';
+import { MockComponent } from 'ng-mocks';
+import { CalendarDesktopOverviewComponent } from 'app/core/calendar/desktop/overview/calendar-desktop-overview.component';
+import { CalendarMobileOverviewComponent } from 'app/core/calendar/mobile/overview/calendar-mobile-overview';
 
 describe('CalendarOverview', () => {
     let component: CalendarOverviewComponent;
     let fixture: ComponentFixture<CalendarOverviewComponent>;
+    let breakpoint$: BehaviorSubject<BreakpointState>;
 
     beforeEach(async () => {
+        breakpoint$ = new BehaviorSubject<BreakpointState>({
+            matches: false,
+            breakpoints: { '(max-width: 600px)': false },
+        });
+
         await TestBed.configureTestingModule({
-            imports: [CalendarOverviewComponent],
+            imports: [CalendarOverviewComponent, MockComponent(CalendarDesktopOverviewComponent), MockComponent(CalendarMobileOverviewComponent)],
+            providers: [{ provide: BreakpointObserver, useValue: { observe: () => breakpoint$.asObservable() } }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(CalendarOverviewComponent);
@@ -18,5 +29,26 @@ describe('CalendarOverview', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should render desktop overview when not mobile', () => {
+        const desktop = fixture.nativeElement.querySelector('calendar-desktop-overview');
+        const mobile = fixture.nativeElement.querySelector('calendar-mobile-overview');
+        expect(desktop).toBeTruthy();
+        expect(mobile).toBeFalsy();
+    });
+
+    it('should render mobile overview when screen is small', () => {
+        breakpoint$.next({
+            matches: true,
+            breakpoints: { '(max-width: 600px)': true },
+        });
+
+        fixture.detectChanges();
+
+        const desktop = fixture.nativeElement.querySelector('calendar-desktop-overview');
+        const mobile = fixture.nativeElement.querySelector('calendar-mobile-overview');
+        expect(desktop).toBeFalsy();
+        expect(mobile).toBeTruthy();
     });
 });
