400b492a7a6ecd15d64551b1bf18c86a0703f48f
marlonnienaber <marlonnienaber@web.de>
2025-08-08 13:47:29 +0200

    automatically scroll to 7:30 after init of CalendarMobileDayPresentationComponent

--- src/main/webapp/app/core/calendar/mobile/day-presentation/calendar-mobile-day-presentation.component.ts
+++ src/main/webapp/app/core/calendar/mobile/day-presentation/calendar-mobile-day-presentation.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, signal, viewChild } from '@angular/core';
 import { CalendarDayBadgeComponent } from 'app/core/calendar/shared/calendar-day-badge/calendar-day-badge.component';
 import { Dayjs } from 'dayjs/esm';
 import * as utils from 'app/core/calendar/shared/util/calendar-util';
@@ -10,11 +10,19 @@ import { CalendarEventsPerDaySectionComponent } from 'app/core/calendar/shared/c
     templateUrl: './calendar-mobile-day-presentation.component.html',
     styleUrl: './calendar-mobile-day-presentation.component.scss',
 })
-export class CalendarMobileDayPresentationComponent {
+export class CalendarMobileDayPresentationComponent implements AfterViewInit {
     readonly utils = utils;
+    private scrollContainer = viewChild<ElementRef>('scrollContainer');
 
     selectedDay = input.required<Dayjs>();
     isEventSelected = signal<boolean>(false);
+
+    ngAfterViewInit(): void {
+        const container = this.scrollContainer();
+        if (container) {
+            container.nativeElement.scrollTop = 7.5 * CalendarEventsPerDaySectionComponent.HOUR_SEGMENT_HEIGHT_IN_PIXEL;
+        }
+    }
 
     isSelected(day: Dayjs): boolean {
         return day.isSame(this.selectedDay(), 'day');
