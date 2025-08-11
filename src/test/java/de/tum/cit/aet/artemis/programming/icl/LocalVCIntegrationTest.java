df29e0c6293d70f89925e2eb6f0711ca211ebf7d
Tobias Lippert <t.lippert@tum.de>
2025-08-11 20:43:30 +0200

    try without isolated

--- src/test/java/de/tum/cit/aet/artemis/programming/icl/LocalVCIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/programming/icl/LocalVCIntegrationTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Isolated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -41,7 +40,6 @@ import de.tum.cit.aet.artemis.programming.util.LocalRepository;
 /**
  * This class contains integration tests for edge cases pertaining to the local VC system.
  */
-@Isolated
 class LocalVCIntegrationTest extends AbstractProgrammingIntegrationLocalCILocalVCTestBase {
 
     private static final String TEST_PREFIX = "localvcint";
