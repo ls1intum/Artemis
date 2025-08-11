38c8f84991830659b55a92c4e900f9fd89fa7974
marlonnienaber <marlonnienaber@web.de>
2025-08-11 13:10:15 +0200

    apply Johannes suggestions

--- src/main/webapp/app/lti/manage/lti13-select-course/lti13-select-course.component.ts
+++ src/main/webapp/app/lti/manage/lti13-select-course/lti13-select-course.component.ts
@@ -35,6 +35,8 @@ export class LtiCoursesComponent implements OnInit {
                     });
                 },
             });
+        } else {
+            this.courses = [];
         }
     }
 }
