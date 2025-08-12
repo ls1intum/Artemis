b549253787550100179dd4027a3de87e2ad56b14
Tobias Lippert <t.lippert@tum.de>
2025-08-12 12:41:35 +0200

    remove logger

--- src/main/java/de/tum/cit/aet/artemis/programming/service/RepositoryAccessService.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/RepositoryAccessService.java
@@ -4,8 +4,6 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -30,8 +28,6 @@ import de.tum.cit.aet.artemis.programming.web.repository.RepositoryActionType;
 @Lazy
 @Service
 public class RepositoryAccessService {
-
-    private static final Logger log = LoggerFactory.getLogger(RepositoryAccessService.class);
 
     private final Optional<PlagiarismAccessApi> plagiarismAccessApi;
 
