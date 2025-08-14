e240cb9a37955f14e972199c7c31f82fb780f45d
Enver Can Kayandan <ge84dic@mytum.de>
2025-08-14 11:03:42 +0200

    added try catch for thread pool adjustment call

--- src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobManagementService.java
+++ src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobManagementService.java
@@ -197,7 +197,12 @@ public class BuildJobManagementService {
         return futureResult.whenComplete(((result, throwable) -> {
             runningFutures.remove(buildJobItem.id());
             runningFuturesWrapper.remove(buildJobItem.id());
-            buildAgentConfiguration.checkAndAdjustThreadPoolSize();
+            try {
+                buildAgentConfiguration.checkAndAdjustThreadPoolSize();
+            }
+            catch (Exception e) {
+                log.error("Error while adjusting thread pool size", e);
+            }
         }));
     }
 
