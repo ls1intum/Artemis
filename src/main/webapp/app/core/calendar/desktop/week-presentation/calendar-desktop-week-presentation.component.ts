a1eeb161bb14ac9456240c34344fd80add945fec
marlonnienaber <marlonnienaber@web.de>
2025-08-17 17:15:25 +0200

    test

--- src/main/webapp/app/core/calendar/desktop/week-presentation/calendar-desktop-week-presentation.component.ts
+++ src/main/webapp/app/core/calendar/desktop/week-presentation/calendar-desktop-week-presentation.component.ts
@@ -35,4 +35,3 @@ export class CalendarDesktopWeekPresentationComponent implements AfterViewInit {
         return Array.from({ length: 7 }, (_, index) => firstDayOfWeek.add(index, 'day'));
     }
 }
-// test
