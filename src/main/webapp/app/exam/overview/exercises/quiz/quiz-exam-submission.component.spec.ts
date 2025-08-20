ac28e8ecbd2c645cf54e5204389de4da6151a718
Florian Glombik <florian.glombik@tum.de>
2025-08-20 13:05:53 +0200

    Fix tests

--- src/main/webapp/app/exam/overview/exercises/quiz/quiz-exam-submission.component.spec.ts
+++ src/main/webapp/app/exam/overview/exercises/quiz/quiz-exam-submission.component.spec.ts
@@ -197,7 +197,7 @@ describe('QuizExamSubmissionComponent', () => {
             points: 1,
             randomizeOrder: false,
             invalid: false,
-            exportQuiz = false,
+            exportQuiz: false,
         };
         const quizConfiguration: QuizConfiguration = { quizQuestions: [mockQuestion] };
         fixture.componentRef.setInput('quizConfiguration', quizConfiguration);
