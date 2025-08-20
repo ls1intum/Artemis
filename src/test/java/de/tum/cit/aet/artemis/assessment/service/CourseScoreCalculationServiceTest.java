f3ad53b694828edb48df869ba1197c3b766abf38
Tobias Lippert <t.lippert@tum.de>
2025-08-20 21:26:49 +0200

    caching improves performance starting with the second request

--- src/test/java/de/tum/cit/aet/artemis/assessment/service/CourseScoreCalculationServiceTest.java
+++ src/test/java/de/tum/cit/aet/artemis/assessment/service/CourseScoreCalculationServiceTest.java
@@ -168,7 +168,7 @@ class CourseScoreCalculationServiceTest extends AbstractSpringIntegrationIndepen
 
         var courseScores = studentParticipationRepository.findGradeScoresForAllExercisesForCourseAndStudent(course.getId(), student.getId());
 
-        StudentScoresDTO studentScoresDTO = courseScoreCalculationService.calculateCourseScoreForStudent(course, null, student.getId(), courseScores,
+        StudentScoresDTO studentScoresDTO = courseScoreCalculationService.calculateCourseScoreForStudent(course, null, student.getId(), studentParticipations,
                 new MaxAndReachablePointsDTO(25.0, 5.0, 0.0), List.of());
         if (withDueDate) {
             assertThat(studentScoresDTO.absoluteScore()).isEqualTo(6.6);
