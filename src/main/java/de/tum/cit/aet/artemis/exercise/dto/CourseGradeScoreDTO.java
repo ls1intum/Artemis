38f72cd00c20d9df95f4155b4364e878d91acc5c
Tobias Lippert <t.lippert@tum.de>
2025-08-21 14:54:39 +0200

    annotate dtos

--- src/main/java/de/tum/cit/aet/artemis/exercise/dto/CourseGradeScoreDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/dto/CourseGradeScoreDTO.java
@@ -1,16 +1,13 @@
 package de.tum.cit.aet.artemis.exercise.dto;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.exercise.domain.ExerciseType;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record CourseGradeScoreDTO(long participationId, long userId, long exerciseId, double score, @Nullable Double presentationScore, @Nullable ExerciseType type) {
+public record CourseGradeScoreDTO(long participationId, long userId, long exerciseId, double score, @Nullable Double presentationScore, @NotNull ExerciseType type) {
 
-    // Constructor for queries that don't include ExerciseType
-    public CourseGradeScoreDTO(long participationId, long userId, long exerciseId, double score, @Nullable Double presentationScore) {
-        this(participationId, userId, exerciseId, score, presentationScore, null);
-    }
 }
