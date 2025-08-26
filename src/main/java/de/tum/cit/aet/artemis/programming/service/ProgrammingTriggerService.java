d0e7a01b58a776795f256e002cd62436ee02d467
Tobias Lippert <t.lippert@tum.de>
2025-08-26 15:08:52 +0200

    batch build triggers when triggering multiple participations

--- src/main/java/de/tum/cit/aet/artemis/programming/service/ProgrammingTriggerService.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/ProgrammingTriggerService.java
@@ -125,7 +125,6 @@ public class ProgrammingTriggerService {
         Set<ProgrammingExerciseStudentParticipation> participations = programmingExerciseStudentParticipationRepository.findWithLatestSubmissionByExerciseId(exerciseId);
         log.debug("Database call to fetch participations for trigger all took {} ms", (System.currentTimeMillis() - start));
         triggerBuildForParticipations(participations);
-        log.debug("Database call + trigger all builds for exercise {} took {} ms", exerciseId, (System.currentTimeMillis() - start));
 
         // When the instructor build was triggered for the programming exercise, it is not considered 'dirty' anymore.
         programmingExerciseTestCaseChangedService.setTestCasesChanged(programmingExercise, false);
