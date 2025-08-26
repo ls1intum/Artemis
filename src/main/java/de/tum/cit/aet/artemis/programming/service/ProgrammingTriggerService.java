e4595026e1969384b976bab8cebd64c98f1236ac
Tobias Lippert <t.lippert@tum.de>
2025-08-26 15:08:00 +0200

    batch build triggers when triggering multiple participations

--- src/main/java/de/tum/cit/aet/artemis/programming/service/ProgrammingTriggerService.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/ProgrammingTriggerService.java
@@ -142,7 +142,7 @@ public class ProgrammingTriggerService {
         var index = 0;
         for (var participation : participations) {
             // Execute requests in batches when using an external build system.
-            if (!profileService.isLocalCIActive() && index > 0 && index % externalSystemRequestBatchSize == 0) {
+            if (index > 0 && index % externalSystemRequestBatchSize == 0) {
                 try {
                     log.info("Sleep for {}s during triggerBuild", externalSystemRequestBatchWaitingTime / 1000);
                     Thread.sleep(externalSystemRequestBatchWaitingTime);
