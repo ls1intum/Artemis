ce490d8562b8a34f6c15e390d59b733a81bc96d8
Jonas Andersson <jonas@andersson.co>
2025-08-27 02:04:37 +0200

    reformated and restructured the course update component, improved wording

--- src/main/webapp/app/core/course/manage/update/course-update.component.ts
+++ src/main/webapp/app/core/course/manage/update/course-update.component.ts
@@ -42,6 +42,7 @@ import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { RemoveKeysPipe } from 'app/shared/pipes/remove-keys.pipe';
 import { FeatureOverlayComponent } from 'app/shared/components/feature-overlay/feature-overlay.component';
 import { FileService } from 'app/shared/service/file.service';
+import { DocumentationLinkComponent } from 'app/shared/components/documentation-link/documentation-link.component';
 
 const DEFAULT_CUSTOM_GROUP_NAME = 'artemis-dev';
 
@@ -71,6 +72,7 @@ const DEFAULT_CUSTOM_GROUP_NAME = 'artemis-dev';
         FeatureOverlayComponent,
         // NOTE: this is actually used in the html template, otherwise *jhiHasAnyAuthority would not work
         HasAnyAuthorityDirective,
+        DocumentationLinkComponent,
     ],
 })
 export class CourseUpdateComponent implements OnInit {
