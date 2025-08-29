3ffba9c16f3b5e2466f691157b6b1b6f5944e00e
Moritz Spengler <moco_spengler@gmx.de>
2025-08-29 15:34:29 +0200

    Add jsonInclude to dto

--- src/main/java/de/tum/cit/aet/artemis/quiz/dto/LeaderboardEntryDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/dto/LeaderboardEntryDTO.java
@@ -1,5 +1,8 @@
 package de.tum.cit.aet.artemis.quiz.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record LeaderboardEntryDTO(int rank, long league, long studentLeague, String student, int totalScore, int score, int answeredCorrectly, int answeredWrong,
         int totalQuestions) {
 }
