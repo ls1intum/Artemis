c28b0239ceeaa37b588107eed3c7bb0032015c27
marlonnienaber <marlonnienaber@web.de>
2025-08-31 16:17:10 +0200

    add import in TutorialGroupSession model

--- src/main/webapp/app/tutorialgroup/shared/entities/tutorial-group-session.model.ts
+++ src/main/webapp/app/tutorialgroup/shared/entities/tutorial-group-session.model.ts
@@ -1,5 +1,5 @@
 import { BaseEntity } from 'app/shared/model/base-entity';
-import dayjs from 'dayjs/esm';
+import { Dayjs } from 'dayjs/esm';
 import { TutorialGroup } from 'app/tutorialgroup/shared/entities/tutorial-group.model';
 import { TutorialGroupSchedule } from 'app/tutorialgroup/shared/entities/tutorial-group-schedule.model';
 import { TutorialGroupFreePeriod } from 'app/tutorialgroup/shared/entities/tutorial-group-free-day.model';
@@ -13,8 +13,8 @@ export class TutorialGroupSession implements BaseEntity {
     public id?: number;
     public tutorialGroupSchedule?: TutorialGroupSchedule;
     public tutorialGroup?: TutorialGroup;
-    public start?: dayjs.Dayjs;
-    public end?: dayjs.Dayjs;
+    public start?: Dayjs;
+    public end?: Dayjs;
     public status?: TutorialGroupSessionStatus;
     public statusExplanation?: string;
     public location?: string;
