5c00337d47a4433d3fb3cfdbad348cb472605e6e
Moritz Spengler <moco_spengler@gmx.de>
2025-09-05 17:23:51 +0200

    add nullable annotation to dto

--- src/main/java/de/tum/cit/aet/artemis/quiz/dto/LeaderboardEntryDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/dto/LeaderboardEntryDTO.java
@@ -2,9 +2,12 @@ package de.tum.cit.aet.artemis.quiz.dto;
 
 import java.time.ZonedDateTime;
 
+import jakarta.annotation.Nullable;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record LeaderboardEntryDTO(int rank, int league, int studentLeague, long userId, String userName, String imageURL, String leaderboardName, int score, int answeredCorrectly,
-        int answeredWrong, long totalQuestions, ZonedDateTime dueDate, int streak) {
+public record LeaderboardEntryDTO(@Nullable Integer rank, @Nullable Integer league, @Nullable Integer studentLeague, @Nullable Long userId, @Nullable String userName,
+        @Nullable String imageURL, @Nullable String leaderboardName, @Nullable Integer score, @Nullable Integer answeredCorrectly, @Nullable Integer answeredWrong,
+        @Nullable Long totalQuestions, @Nullable ZonedDateTime dueDate, @Nullable Integer streak) {
 }
