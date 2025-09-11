93cd540dee9c35f5f31b2a79cbbeefea8bd76bfc
jfr2102 <johannes@friedlein.eu>
2025-09-12 01:57:17 +0200

    missing Lazy

--- src/main/java/de/tum/cit/aet/artemis/config/DisableRedisAutoConfig.java
+++ src/main/java/de/tum/cit/aet/artemis/config/DisableRedisAutoConfig.java
@@ -3,7 +3,9 @@ package de.tum.cit.aet.artemis.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
+@Lazy
 @ConditionalOnExpression("'${artemis.continuous-integration.data-store:Hazelcast}' != 'Redis'")
 @Configuration
 @EnableAutoConfiguration(exclude = { org.redisson.spring.starter.RedissonAutoConfigurationV2.class, org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
