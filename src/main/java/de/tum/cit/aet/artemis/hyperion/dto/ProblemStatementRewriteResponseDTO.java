ee766547f2891cbef4af0001aa767ec25cc5ce0a
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-09-11 17:01:21 +0200

    update tests

--- src/main/java/de/tum/cit/aet/artemis/hyperion/dto/ProblemStatementRewriteResponseDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/hyperion/dto/ProblemStatementRewriteResponseDTO.java
@@ -2,8 +2,6 @@ package de.tum.cit.aet.artemis.hyperion.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-import org.springframework.lang.NonNull;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,5 +13,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Response containing rewritten problem statement")
 public record ProblemStatementRewriteResponseDTO(@NotNull @Schema(description = "Improved problem statement text") String rewrittenText,
 
-        @NonNull @Schema(description = "Whether the text was actually improved", example = "true") boolean improved) {
+        @NotNull @Schema(description = "Whether the text was actually improved", example = "true") Boolean improved) {
 }
