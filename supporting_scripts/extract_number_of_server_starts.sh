97ebb5a47dd712f5949a563ae4a2705431d406f2
jfr2102 <johannes@friedlein.eu>
2025-09-12 14:08:23 +0200

    fix print

--- supporting_scripts/extract_number_of_server_starts.sh
+++ supporting_scripts/extract_number_of_server_starts.sh
@@ -11,6 +11,6 @@ fi
 
 if [[ $numberOfStarts -gt 5 ]]
 then
-  echo "The number of Server Starts should be lower than/equals 4! Please adapt this check if the change is intended or try to fix the underlying issue causing a different number of server starts!"
+  echo "The number of Server Starts should be lower than/equals 5! Please adapt this check if the change is intended or try to fix the underlying issue causing a different number of server starts!"
   exit 1
 fi
