2e30450f06362b1f5994ec4dd62b7c683c1b8156
jfr2102 <johannes@friedlein.eu>
2025-09-14 00:46:21 +0200

    javadoc improve DistributedMap

--- src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributed/api/map/DistributedMap.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributed/api/map/DistributedMap.java
@@ -97,7 +97,7 @@ public interface DistributedMap<K, V> {
     void unlock(K key);
 
     /**
-     * Adds a listener that will be notified of changes to the map.
+     * Adds a listener that will be notified of changes to the map. The listener methods get the affected entries passed as parameters.
      *
      * @param listener the listener to add
      * @return a unique identifier for the listener, which can be used to remove it later
@@ -105,7 +105,7 @@ public interface DistributedMap<K, V> {
     UUID addEntryListener(MapEntryListener<K, V> listener);
 
     /**
-     * Adds a listener that will be notified of changes to the map.
+     * Adds a listener that will be notified of changes to the map. It is simplified version of listener that does not pass the specific entries.
      *
      * @param listener the listener to add
      * @return a unique identifier for the listener, which can be used to remove it later
