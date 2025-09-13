7572ecd254a66b470933118fc100e8d9a6ace8ca
Stephan Krusche <krusche@tum.de>
2025-09-13 13:42:42 +0300

    Development: Adapt coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -94,10 +94,10 @@ module.exports = {
     coverageThreshold: {
         global: {
             // TODO: in the future, the following values should increase to at least 90%
-            statements: 89.67,
-            branches: 75.91,
-            functions: 83.68,
-            lines: 89.74,
+            statements: 89.66,
+            branches: 75.86,
+            functions: 83.69,
+            lines: 89.73,
         },
     },
     // 'json-summary' reporter is used by supporting_scripts/code-coverage/module-coverage-client/check-client-module-coverage.mjs
