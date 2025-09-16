c10e9d1748196f085632e5b5b7f5d411f0998c75
marlonnienaber <marlonnienaber@web.de>
2025-09-16 17:52:16 +0200

    add javadoc

--- src/main/java/de/tum/cit/aet/artemis/tutorialgroup/dto/TutorialGroupDetailSessionDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/tutorialgroup/dto/TutorialGroupDetailSessionDTO.java
@@ -9,6 +9,9 @@ import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import de.tum.cit.aet.artemis.core.domain.Course;
+import de.tum.cit.aet.artemis.tutorialgroup.domain.TutorialGroupSchedule;
+import de.tum.cit.aet.artemis.tutorialgroup.domain.TutorialGroupSession;
 import de.tum.cit.aet.artemis.tutorialgroup.domain.TutorialGroupSessionStatus;
 import de.tum.cit.aet.artemis.tutorialgroup.util.TutorialGroupDetailSessionData;
 
@@ -16,9 +19,26 @@ import de.tum.cit.aet.artemis.tutorialgroup.util.TutorialGroupDetailSessionData;
 public record TutorialGroupDetailSessionDTO(@NotNull ZonedDateTime start, @NotNull ZonedDateTime end, @NotNull String location, boolean isCancelled, boolean locationChanged,
         boolean timeChanged, boolean dateChanged, @Nullable Integer attendanceCount) {
 
+    /**
+     * Builds a {@link TutorialGroupDetailSessionDTO} from:
+     * <ul>
+     * <li>data that were derived from a {@link TutorialGroupSession}</li>
+     * <li>several fields that represent information about the {@link TutorialGroupSchedule} of the TutorialGroup related to the TutorialGroupSession</li>
+     * <li>the time zone of the {@link Course} related to the TutorialGroup of the TutorialGroupSession</li>
+     * </ul>
+     *
+     * @param data              the actual session data
+     * @param scheduleDayOfWeek the schedule's day of week (1 = Monday, …, 7 = Sunday)
+     * @param scheduleStart     the schedule's start time
+     * @param scheduleEnd       the schedule's end time
+     * @param scheduleLocation  the schedule's location
+     * @param courseTimeZone    the course time zone
+     * @return a DTO with session details and flags for deviations
+     */
     public static TutorialGroupDetailSessionDTO from(TutorialGroupDetailSessionData data, int scheduleDayOfWeek, LocalTime scheduleStart, LocalTime scheduleEnd,
             String scheduleLocation, ZoneId courseTimeZone) {
         boolean isCancelled = data.status() == TutorialGroupSessionStatus.CANCELLED;
+        // if isCancelled is true, set sameLocation, sameTime and sameDay always to true
         boolean sameLocation = isCancelled || data.location().equals(scheduleLocation);
         ZonedDateTime sessionStart = data.start().withZoneSameInstant(courseTimeZone);
         ZonedDateTime sessionEnd = data.end().withZoneSameInstant(courseTimeZone);
