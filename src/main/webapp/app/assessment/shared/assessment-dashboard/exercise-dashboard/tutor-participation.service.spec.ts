c19c633fb49e0ed5926f1da1a9b5c5d3dc6d91a7
pac <ge63nax@mytum.de>
2025-09-18 17:44:53 +0200

    update for create function to send no request body

--- src/main/webapp/app/assessment/shared/assessment-dashboard/exercise-dashboard/tutor-participation.service.spec.ts
+++ src/main/webapp/app/assessment/shared/assessment-dashboard/exercise-dashboard/tutor-participation.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { take } from 'rxjs/operators';
-import { TutorParticipation } from 'app/exercise/shared/entities/participation/tutor-participation.model';
+import { TutorParticipationDTO } from 'app/exercise/shared/entities/participation/tutor-participation.model';
 import { ExampleSubmission } from 'app/assessment/shared/entities/example-submission.model';
 import { MockAccountService } from 'test/helpers/mocks/service/mock-account.service';
 import { AccountService } from 'app/core/auth/account.service';
@@ -22,24 +22,25 @@ describe('Tutor Participation Service', () => {
         httpMock = TestBed.inject(HttpTestingController);
     });
 
-    it('should create a TutorParticipation for an exercise', fakeAsync(() => {
-        const returnedFromService = new TutorParticipation();
+    it('should create a TutorParticipationDTO for an exercise', fakeAsync(() => {
+        const returnedFromService = new TutorParticipationDTO();
         service
             .create(exerciseId)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp.body).toBe(returnedFromService));
+            .subscribe((resp) => expect(resp.body).toEqual(returnedFromService));
 
         const req = httpMock.expectOne({ method: 'POST' });
+        expect(req.request.body).toBeNull();
         req.flush(returnedFromService);
         tick();
     }));
 
     it('should assess ExampleSubmission for an exercise', fakeAsync(() => {
-        const returnedFromService = new TutorParticipation();
+        const returnedFromService = new TutorParticipationDTO();
         service
             .assessExampleSubmission(new ExampleSubmission(), exerciseId)
             .pipe(take(1))
-            .subscribe((resp) => expect(resp.body).toBe(returnedFromService));
+            .subscribe((resp) => expect(resp.body).toEqual(returnedFromService));
 
         const req = httpMock.expectOne({ method: 'POST' });
         req.flush(returnedFromService);
