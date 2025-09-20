e8361c2b813a1e4f2b9c31401ddbca37f9a25519
Moritz Spengler <moco_spengler@gmx.de>
2025-09-20 14:18:51 +0200

    annotate questionIDs with Nullable

--- src/main/java/de/tum/cit/aet/artemis/quiz/dto/question/QuizQuestionTrainingDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/dto/question/QuizQuestionTrainingDTO.java
@@ -2,12 +2,13 @@ package de.tum.cit.aet.artemis.quiz.dto.question;
 
 import java.util.Set;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.ALWAYS)
-public record QuizQuestionTrainingDTO(@NotNull QuizQuestionWithSolutionDTO quizQuestionWithSolutionDTO, boolean isRated, Set<Long> questionIds) {
+public record QuizQuestionTrainingDTO(@NotNull QuizQuestionWithSolutionDTO quizQuestionWithSolutionDTO, boolean isRated, @Nullable Set<Long> questionIds) {
 
     public long getId() {
         return quizQuestionWithSolutionDTO().quizQuestionBaseDTO().id();
