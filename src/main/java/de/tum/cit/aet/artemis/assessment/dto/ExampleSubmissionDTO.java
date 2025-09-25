3f48e738e26e52d1af12d1b2ad0971b2e870a078
pac <ge63nax@mytum.de>
2025-09-25 17:26:00 +0200

    update Dto

--- src/main/java/de/tum/cit/aet/artemis/assessment/dto/ExampleSubmissionDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/assessment/dto/ExampleSubmissionDTO.java
@@ -2,14 +2,12 @@ package de.tum.cit.aet.artemis.assessment.dto;
 
 import java.util.Objects;
 
-import org.springframework.lang.Nullable;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.assessment.domain.ExampleSubmission;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ExampleSubmissionDTO(long id, boolean usedForTutorial, long submissionId, @Nullable String assessmentExplanation) {
+public record ExampleSubmissionDTO(long id, boolean usedForTutorial, long submissionId, String assessmentExplanation) {
 
     /**
      * Convert an ExampleSubmission entity to a ExampleSubmissionDTO.
