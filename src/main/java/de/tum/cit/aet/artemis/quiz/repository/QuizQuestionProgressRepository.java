7172a19a2958b1ef51fc241c39234bd5e41a5482
Moritz Spengler <146204394+MoritzSpengler@users.noreply.github.com>
2025-09-26 15:22:25 +0200

    Quiz exercises: Add unrated mode to quiz training (#11382)

--- src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
@@ -19,6 +19,6 @@ public interface QuizQuestionProgressRepository extends ArtemisJpaRepository<Qui
 
     Optional<QuizQuestionProgress> findByUserIdAndQuizQuestionId(long userId, long quizQuestionId);
 
-    Set<QuizQuestionProgress> findAllByUserIdAndQuizQuestionIdIn(long userId, Set<Long> quizQuestionIds);
+    Set<QuizQuestionProgress> findAllByUserIdAndCourseId(long userId, long courseId);
 
 }
