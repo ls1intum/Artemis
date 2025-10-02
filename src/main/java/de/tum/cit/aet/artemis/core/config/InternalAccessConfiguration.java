f8e87303cec652b5a7fc567244ee02121f4b34b9
Benjamin Schmitz <benjamin.schmitz@tum.de>
2025-10-02 10:04:37 +0000

    Add Lazy

--- src/main/java/de/tum/cit/aet/artemis/core/config/InternalAccessConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/InternalAccessConfiguration.java
@@ -1,9 +1,15 @@
 package de.tum.cit.aet.artemis.core.config;
 
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
+
 import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 
+@Profile(PROFILE_CORE)
+@Lazy
 @ConfigurationProperties(prefix = "artemis.security.internal")
 public class InternalAccessConfiguration {
 
