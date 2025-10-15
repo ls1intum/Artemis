7e1730af4bf653863f73087e06f109662c542239
Florian Glombik <florian.glombik@tum.de>
2025-10-15 17:32:04 +0200

    Fix build

--- docs/student/src/pages/index.tsx
+++ docs/student/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/guidelines/intro">
+                        to="/student/intro">
                         To the documentation →
                     </Link>
                 </div>
