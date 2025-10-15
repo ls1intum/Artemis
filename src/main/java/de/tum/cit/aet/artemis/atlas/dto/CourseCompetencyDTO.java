57ec86c5cab943263ba9f9c11cd717dc28c86195
ahmetsenturk <iahmetsenturk@yahoo.com>
2025-10-15 11:49:51 +0200

    update annotations for non-primitives

--- src/main/java/de/tum/cit/aet/artemis/atlas/dto/CourseCompetencyDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/atlas/dto/CourseCompetencyDTO.java
@@ -1,12 +1,15 @@
 package de.tum.cit.aet.artemis.atlas.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.atlas.domain.competency.CompetencyTaxonomy;
 import de.tum.cit.aet.artemis.atlas.domain.competency.CourseCompetency;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record CourseCompetencyDTO(long id, String title, String description, CompetencyTaxonomy taxonomy) {
+public record CourseCompetencyDTO(long id, @NotNull String title, @Nullable String description, @Nullable CompetencyTaxonomy taxonomy) {
 
     public static CourseCompetencyDTO of(CourseCompetency competency) {
         return new CourseCompetencyDTO(competency.getId(), competency.getTitle(), competency.getDescription(), competency.getTaxonomy());
