c78a3c781c53669e919cf59d14d83fbdfe760ed4
Samuel Röttgermann <38440557+SamuelRoettgermann@users.noreply.github.com>
2025-10-18 21:38:44 +0200

    Development: Add server functionality for simple exam seat distribution (#11378)

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomDTO.java
@@ -7,5 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ExamRoomDTO(@NotNull String roomNumber, @NotNull String name, @NotNull String building, int numberOfSeats, @NotNull Set<ExamRoomLayoutStrategyDTO> layoutStrategies) {
+public record ExamRoomDTO(long id, @NotNull String roomNumber, @NotNull String name, @NotNull String building, int numberOfSeats,
+        @NotNull Set<ExamRoomLayoutStrategyDTO> layoutStrategies) {
 }
