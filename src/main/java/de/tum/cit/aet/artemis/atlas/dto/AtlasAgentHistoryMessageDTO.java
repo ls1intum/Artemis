95173e050d7609ef0397721ccdbf3699f39ab2b0
Yassine Hmidi <ge84tef@tum.de>
2025-10-18 11:59:51 +0100

    dto for history

--- src/main/java/de/tum/cit/aet/artemis/atlas/dto/AtlasAgentHistoryMessageDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/atlas/dto/AtlasAgentHistoryMessageDTO.java
@@ -3,10 +3,12 @@ package de.tum.cit.aet.artemis.atlas.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
- * DTO for Atlas Agent conversation history messages.
- * Used to transfer chat history between backend and frontend.
+ * DTO for representing a single message in the conversation history.
+ *
+ * @param content The message content
  * @param isUser  Whether the message is from the user (true) or the agent (false)
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record AtlasAgentHistoryMessageDTO(String content, boolean isUser) {
+
 }
