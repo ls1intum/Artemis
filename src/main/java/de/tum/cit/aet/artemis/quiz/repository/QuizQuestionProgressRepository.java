8940eda02bb9d9e2a328d5d63448465b5d19b277
Moritz Spengler <moco_spengler@gmx.de>
2025-10-21 11:28:57 +0200

    Migrate due date into own table column

--- src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/repository/QuizQuestionProgressRepository.java
@@ -2,11 +2,13 @@ package de.tum.cit.aet.artemis.quiz.repository;
 
 import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
@@ -20,5 +22,14 @@ public interface QuizQuestionProgressRepository extends ArtemisJpaRepository<Qui
     Optional<QuizQuestionProgress> findByUserIdAndQuizQuestionId(long userId, long quizQuestionId);
 
     Set<QuizQuestionProgress> findAllByUserIdAndCourseId(long userId, long courseId);
+
+    @Query("""
+            SELECT quizQuestionProgress.quizQuestionId
+            FROM QuizQuestionProgress quizQuestionProgress
+            WHERE quizQuestionProgress.userId = :userId
+            AND quizQuestionProgress.courseId = :courseId
+            AND quizQuestionProgress.dueDate > :dueDate
+            """)
+    Set<Long> findNotDueQuizQuestions(long userId, long courseId, ZonedDateTime dueDate);
 
 }
