deab1d2688c0a4a7073a5988c4b5c829464bbf12
pac <ge63nax@mytum.de>
2025-10-28 23:17:36 +0800

    update comments

--- src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateFaqDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateFaqDTO.java
@@ -5,27 +5,14 @@ import java.util.Set;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import de.tum.cit.aet.artemis.communication.domain.Faq;
 import de.tum.cit.aet.artemis.communication.domain.FaqState;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record UpdateFaqDTO(long id, long courseId, @NotBlank String questionTitle, @Nullable String questionAnswer, @Nullable Set<String> categories, @NotNull FaqState faqState) {
+public record UpdateFaqDTO(@Positive long id, @Positive long courseId, @NotBlank String questionTitle, @Nullable String questionAnswer, @Nullable Set<String> categories,
+        @NotNull FaqState faqState) {
 
-    /**
-     * Converts this DTO to a Faq entity.
-     *
-     * @return a new Faq entity with the data from this DTO
-     */
-    public Faq toEntity() {
-        Faq faq = new Faq();
-        faq.setId(this.id);
-        faq.setFaqState(this.faqState);
-        faq.setCategories(this.categories);
-        faq.setQuestionTitle(this.questionTitle);
-        faq.setQuestionAnswer(this.questionAnswer);
-        return faq;
-    }
 }
