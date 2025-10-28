000cb1e34762cf2cf9de5264987f7dde6e5f3ff7
pac <ge63nax@mytum.de>
2025-10-28 22:32:49 +0800

    update comments

--- src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateFaqDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/dto/UpdateFaqDTO.java
@@ -6,12 +6,14 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.communication.domain.Faq;
 import de.tum.cit.aet.artemis.communication.domain.FaqState;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record UpdateFaqDTO(long id, long courseId, @NotBlank String questionTitle, @Nullable String questionAnswer, @Nullable Set<String> categories, @NotNull FaqState faqState) {
 
     /**
