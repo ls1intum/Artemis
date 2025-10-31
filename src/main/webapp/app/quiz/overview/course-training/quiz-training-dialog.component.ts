757169f0e018999977de5094792ed252bab8a538
Moritz Spengler <moco_spengler@gmx.de>
2025-10-31 12:16:16 +0100

    switch from primeicons to fontawesome

--- src/main/webapp/app/quiz/overview/course-training/quiz-training-dialog.component.ts
+++ src/main/webapp/app/quiz/overview/course-training/quiz-training-dialog.component.ts
@@ -7,14 +7,17 @@ import { TooltipModule } from 'primeng/tooltip';
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { LeagueIconComponent } from 'app/quiz/overview/course-training/course-training-quiz/leaderboard/league/league-icon.component';
 import { ButtonComponent } from 'app/shared/components/buttons/button/button.component';
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
     selector: 'jhi-quiz-training-dialog',
     styleUrl: 'course-training.component.scss',
-    imports: [DialogModule, FormsModule, TranslateDirective, ToggleSwitchModule, TooltipModule, ArtemisTranslatePipe, LeagueIconComponent, ButtonComponent],
+    imports: [DialogModule, FormsModule, TranslateDirective, ToggleSwitchModule, TooltipModule, ArtemisTranslatePipe, LeagueIconComponent, ButtonComponent, FontAwesomeModule],
     templateUrl: './quiz-training-dialog.component.html',
 })
 export class QuizTrainingDialogComponent {
+    protected readonly faQuestionCircle = faQuestionCircle;
     protected readonly leagues = [
         { league: 'Bronze', translationKey: 'artemisApp.quizTraining.bronze', range: '0 - 50' },
         { league: 'Silver', translationKey: 'artemisApp.quizTraining.silver', range: '50 - 150' },
