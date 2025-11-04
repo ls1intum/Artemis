0608f9e65b600ff04fe6232997b4a4404a0ac58f
Konstantin Starke <Konstantin.der.Starke@gmail.com>
2025-11-04 12:42:58 +0100

    Fix test error missing argument

--- src/main/webapp/app/shared/monaco-editor/model/actions/artemis-intelligence/artemis-intelligence.service.spec.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/artemis-intelligence/artemis-intelligence.service.spec.ts
@@ -31,6 +31,7 @@ describe('ArtemisIntelligenceService', () => {
     let service: ArtemisIntelligenceService;
     let websocketService: WebsocketService;
     let alertService: AlertService;
+    let translateService: TranslateService;
 
     const monacoEditorComponent = {
         addLineWidget: jest.fn(),
@@ -143,6 +144,7 @@ describe('ArtemisIntelligenceService', () => {
         service = TestBed.inject(ArtemisIntelligenceService);
         websocketService = TestBed.inject(WebsocketService);
         alertService = TestBed.inject(AlertService);
+        translateService = TestBed.inject(TranslateService);
     });
 
     afterEach(() => {
@@ -387,7 +389,7 @@ describe('ArtemisIntelligenceService', () => {
         });
 
         it('addCommentBoxes calls correct functions', () => {
-            addCommentBoxes(monacoEditorComponent, mockIssues, 'problem_statement.md', 'PROBLEM_STATEMENT');
+            addCommentBoxes(monacoEditorComponent, mockIssues, 'problem_statement.md', 'PROBLEM_STATEMENT', translateService);
             expect(monacoEditorComponent.addLineWidget).toHaveBeenCalledOnce();
         });
     });
