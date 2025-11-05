0ad387bb5e140ad8a6308f1294dda0cddb91e1b2
Maximilian Soelch <maximilian.soelch@tum.de>
2025-11-05 23:59:09 +0100

    Fix test in ExerciseAthenaConfigDeserializationTest

--- src/test/java/de/tum/cit/aet/artemis/exercise/domain/ExerciseAthenaConfigDeserializationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/exercise/domain/ExerciseAthenaConfigDeserializationTest.java
@@ -5,9 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import de.tum.cit.aet.artemis.modeling.domain.ModelingExercise;
 
@@ -19,18 +16,15 @@ class ExerciseAthenaConfigDeserializationTest {
                 {"type":"modeling","id":3,"title":"t","maxPoints":10,"bonusPoints":0,"assessmentType":"SEMI_AUTOMATIC","mode":"INDIVIDUAL",
                   "allowComplaintsForAutomaticAssessments":false,"allowManualFeedbackRequests":false,
                   "includedInOverallScore":"INCLUDED_COMPLETELY","problemStatement":"statement","presentationScoreEnabled":false,
-                  "secondCorrectionEnabled":false,
-                  "diagramType":"ClassDiagram","exerciseType":"MODELING","teamMode":false,
+                  "secondCorrectionEnabled":false,"diagramType":"ClassDiagram",
                   "athenaConfig":{"feedbackSuggestionModule":"module_modeling_llm","preliminaryFeedbackModule":"module_modeling_llm"}}
                 """;
 
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.registerModule(new Jdk8Module());
-        mapper.registerModule(new Hibernate6Module());
 
         ModelingExercise exercise = mapper.readValue(json, ModelingExercise.class);
         assertThat(exercise.getAthenaConfig()).isNotNull();
         assertThat(exercise.getAthenaConfig().getFeedbackSuggestionModule()).isEqualTo("module_modeling_llm");
+        assertThat(exercise.getAthenaConfig().getPreliminaryFeedbackModule()).isEqualTo("module_modeling_llm");
     }
 }
