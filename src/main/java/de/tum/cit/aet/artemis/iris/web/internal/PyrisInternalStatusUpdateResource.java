084e400dc90bb4c32919a828ddbb300c867881a0
Tim Cremer <tim.cremer@tum.de>
2025-11-07 13:02:58 +0100

    Remove rewriting job once more

--- src/main/java/de/tum/cit/aet/artemis/iris/web/internal/PyrisInternalStatusUpdateResource.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/web/internal/PyrisInternalStatusUpdateResource.java
@@ -27,7 +27,6 @@ import de.tum.cit.aet.artemis.iris.service.pyris.dto.chat.textexercise.PyrisText
 import de.tum.cit.aet.artemis.iris.service.pyris.dto.competency.PyrisCompetencyStatusUpdateDTO;
 import de.tum.cit.aet.artemis.iris.service.pyris.dto.faqingestionwebhook.PyrisFaqIngestionStatusUpdateDTO;
 import de.tum.cit.aet.artemis.iris.service.pyris.dto.lectureingestionwebhook.PyrisLectureIngestionStatusUpdateDTO;
-import de.tum.cit.aet.artemis.iris.service.pyris.dto.rewriting.PyrisRewritingStatusUpdateDTO;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.CompetencyExtractionJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.CourseChatJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.ExerciseChatJob;
@@ -35,7 +34,6 @@ import de.tum.cit.aet.artemis.iris.service.pyris.job.FaqIngestionWebhookJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.LectureChatJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.LectureIngestionWebhookJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.PyrisJob;
-import de.tum.cit.aet.artemis.iris.service.pyris.job.RewritingJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.TextExerciseChatJob;
 import de.tum.cit.aet.artemis.iris.service.pyris.job.TutorSuggestionJob;
 
@@ -173,31 +171,6 @@ public class PyrisInternalStatusUpdateResource {
     @Internal
     public ResponseEntity<Void> respondInLectureChat(@PathVariable String runId, @RequestBody PyrisLectureChatStatusUpdateDTO statusUpdateDTO, HttpServletRequest request) {
         var job = pyrisJobService.getAndAuthenticateJobFromHeaderElseThrow(request, LectureChatJob.class);
-        if (!Objects.equals(job.jobId(), runId)) {
-            throw new ConflictException("Run ID in URL does not match run ID in request body", "Job", "runIdMismatch");
-        }
-
-        pyrisStatusUpdateService.handleStatusUpdate(job, statusUpdateDTO);
-
-        return ResponseEntity.ok().build();
-    }
-
-    /**
-     * POST internal/pipelines/rewriting/runs/:runId/status : Send the rewritten text in a status update
-     * <p>
-     * Uses custom token based authentication.
-     *
-     * @param runId           the ID of the job
-     * @param statusUpdateDTO the status update
-     * @param request         the HTTP request
-     * @throws ConflictException        if the run ID in the URL does not match the run ID in the request body
-     * @throws AccessForbiddenException if the token is invalid
-     * @return a {@link ResponseEntity} with status {@code 200 (OK)}
-     */
-    @PostMapping("pipelines/rewriting/runs/{runId}/status")
-    @Internal
-    public ResponseEntity<Void> setRewritingJobStatus(@PathVariable String runId, @RequestBody PyrisRewritingStatusUpdateDTO statusUpdateDTO, HttpServletRequest request) {
-        var job = pyrisJobService.getAndAuthenticateJobFromHeaderElseThrow(request, RewritingJob.class);
         if (!Objects.equals(job.jobId(), runId)) {
             throw new ConflictException("Run ID in URL does not match run ID in request body", "Job", "runIdMismatch");
         }
