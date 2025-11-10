3fde3b0a9b3d11c2f131970f41119038355013bf
Predixx <liam.patrice.berger@gmail.com>
2025-11-10 16:12:33 +0100

    Tests: Suppress console.warn in AboutIrisComponent tests
    - Add jest.spyOn(console, 'warn').mockImplementation() in beforeEach
    - Prevents ng-mocks warnings from failing tests
    - Follows standard pattern used in other Artemis tests

--- src/main/webapp/app/iris/overview/about-iris/about-iris.component.spec.ts
+++ src/main/webapp/app/iris/overview/about-iris/about-iris.component.spec.ts
@@ -14,6 +14,7 @@ describe('AboutIrisComponent', () => {
     let compiled: DebugElement;
 
     beforeEach(() => {
+        jest.spyOn(console, 'warn').mockImplementation();
         TestBed.configureTestingModule({
             imports: [MockComponent(IrisLogoComponent), MockPipe(ArtemisTranslatePipe), MockDirective(TranslateDirective)],
             declarations: [AboutIrisComponent],
