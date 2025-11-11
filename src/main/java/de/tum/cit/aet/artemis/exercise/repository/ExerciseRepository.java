9fb2759d9890350916baf982a4384faaac3c5e40
Vivien Finley <vivien.finley@web.de>
2025-11-11 13:50:56 +0100

    fix data base query

--- src/main/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/repository/ExerciseRepository.java
@@ -463,13 +463,13 @@ public interface ExerciseRepository extends ArtemisJpaRepository<Exercise, Long>
      * Fetches the number of student participations in the given exercise
      *
      * @param exerciseId the id of the exercise to get the amount for
-     * @return The number of participations as <code>Long</code>
+     * @return The number of student participations as <code>Long</code>
      */
     @Query("""
             SELECT COUNT(DISTINCT p.student.id)
             FROM Exercise e
                 JOIN e.studentParticipations p
-            WHERE e.id = :exerciseId
+            WHERE e.id = :exerciseId AND e.course.studentGroupName MEMBER OF p.student.groups
             """)
     Long getStudentParticipationCountById(@Param("exerciseId") Long exerciseId);
 
