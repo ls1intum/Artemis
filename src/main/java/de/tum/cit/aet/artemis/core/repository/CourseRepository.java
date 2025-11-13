872c5073c15bbf6856484cac8d5579bdfc460cf5
marlonnienaber <marlonnienaber@web.de>
2025-11-13 11:58:21 +0100

    fix unsupported query

--- src/main/java/de/tum/cit/aet/artemis/core/repository/CourseRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/core/repository/CourseRepository.java
@@ -130,10 +130,10 @@ public interface CourseRepository extends ArtemisJpaRepository<Course, Long> {
     @Query("""
             SELECT course
             FROM Course course
-                LEFT JOIN FETCH course.lectures lecture WITH NOT lecture.isTutorialLecture
+                LEFT JOIN FETCH course.lectures lecture
                 LEFT JOIN FETCH lecture.lectureUnits
                 LEFT JOIN FETCH lecture.attachments
-            WHERE course.id = :courseId
+            WHERE course.id = :courseId AND (lecture IS NULL OR NOT lecture.isTutorialLecture)
             """)
     Optional<Course> findByIdWithNormalLecturesAndLectureUnitsAndAttachments(@Param("courseId") long courseId);
 
