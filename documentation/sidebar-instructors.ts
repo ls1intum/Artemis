8235a77fea0108167f8fcdb10c2b5441865a5339
Florian Glombik <florian.glombik@tum.de>
2025-11-21 13:45:06 +0100

    Fix sidebar sorting

---
+++ documentation/sidebar-instructors.ts
@@ -0,0 +1,11 @@
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+    default: [
+        'intro',
+        'programming',
+        'assessment',
+    ],
+};
+
+export default sidebars;
