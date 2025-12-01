174460f67d81983ed43bf67d6fba29eb645e7447
benno-tum <benno.brueck@tum.de>
2025-12-02 00:40:40 +0100

    test: add debug test

--- src/test/java/de/tum/cit/aet/artemis/hyperion/web/QuizGenerationResourceTest.java
+++ src/test/java/de/tum/cit/aet/artemis/hyperion/web/QuizGenerationResourceTest.java
@@ -13,8 +13,12 @@ import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
@@ -34,6 +38,14 @@ import de.tum.cit.aet.artemis.shared.base.AbstractSpringIntegrationIndependentTe
 class QuizGenerationResourceTest extends AbstractSpringIntegrationIndependentTest {
 
     private static final String TEST_PREFIX = "quizgeneration";
+
+    private static final Logger log = LoggerFactory.getLogger(QuizGenerationResourceTest.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private CourseUtilService courseUtilService;
@@ -94,6 +106,14 @@ class QuizGenerationResourceTest extends AbstractSpringIntegrationIndependentTes
 
         var response = new AiQuizGenerationResponseDTO(List.of(question), List.of());
         doReturn(response).when(aiQuizGenerationService).generate(anyLong(), any());
+    }
+
+    @Test
+    void debugHyperionContext() {
+        log.info("DEBUG hyperion: artemis.hyperion.enabled = {}", environment.getProperty("artemis.hyperion.enabled"));
+
+        boolean hasControllerBean = applicationContext.containsBean("quizGenerationResource");
+        log.info("DEBUG hyperion: has bean 'quizGenerationResource' = {}", hasControllerBean);
     }
 
     @Test
