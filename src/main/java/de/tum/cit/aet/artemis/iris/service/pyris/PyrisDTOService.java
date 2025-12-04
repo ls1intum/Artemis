1672b423524d345390ce835063d21c9cc84a58db
Predixx <liam.patrice.berger@gmail.com>
2025-12-04 21:40:14 +0100

    docs(pyris): Remove unnecessary parameter description for uncommitted files in PyrisDTOService

--- src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/PyrisDTOService.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/PyrisDTOService.java
@@ -73,7 +73,6 @@ public class PyrisDTOService {
     /**
      * Uncommitted files override the committed files if they have the same path.
      *
-     * @param submission       the student's submission
      * @param uncommittedFiles the uncommitted files from the client
      * @return the converted PyrisSubmissionDTO
      */
