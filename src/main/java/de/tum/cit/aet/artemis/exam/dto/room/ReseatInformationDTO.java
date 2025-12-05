8ff608609c5a476d5f6a05e53481a0a55838ed64
SamuelRoettgermann <samu.roettgermann@t-online.de>
2025-12-05 19:41:34 +0100

    fix failing architecture test

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ReseatInformationDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ReseatInformationDTO.java
@@ -1,11 +1,10 @@
 package de.tum.cit.aet.artemis.exam.dto.room;
 
-import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /* Purely received from the client */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ReseatInformationDTO(@NotNull Long examUserId, @NotNull String newRoom, @Nullable String newSeat) {
+public record ReseatInformationDTO(@NotNull Long examUserId, @NotNull String newRoom, String newSeat) {
 }
