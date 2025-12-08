d31102f8ad069b998dac87642eb1b9d71d1cb08c
Florian Glombik <florian.glombik@tum.de>
2025-12-08 21:40:33 +0100

    Simplify test

--- src/test/playwright/e2e/PasskeyReminderPersistence.spec.ts
+++ src/test/playwright/e2e/PasskeyReminderPersistence.spec.ts
@@ -1,21 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { test } from '../support/fixtures';
 
-test('Passkey reminder modal is not displayed on re-login after do not remind for 30 days was chosen', async ({ page }) => {
-    await page.goto('http://localhost:9000/');
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').click();
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').fill('artemis_admin');
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').press('Tab');
-    await page.getByLabel('Password').fill('artemis');
-    await page.getByLabel('Password').press('ControlOrMeta+a');
-    await page.getByLabel('Password').fill('artemis_admin');
-    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+test('Passkey reminder modal is not displayed on re-login after do not remind for 30 days was chosen', async ({ page, loginPage, navigationBar }) => {
+    const artemisAdmin = { username: 'artemis_admin', password: 'artemis_admin' };
+
+    // First login
+    await page.goto('/');
+    await loginPage.login(artemisAdmin);
+
+    // Hide Modal for 30 days
+    await expect(page.getByRole('button', { name: 'Remind Me in 30 Days' })).toBeVisible();
     await page.getByRole('button', { name: 'Remind Me in 30 Days' }).click();
-    await page.locator('#account-menu').click();
-    await page.locator('#logout').click();
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').click();
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').fill('artemis_admin');
-    await page.getByPlaceholder('e.g. go42tum / example@tum.de').press('Tab');
-    await page.getByLabel('Password').fill('artemis_admin');
-    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+    // Logout
+    await navigationBar.logout();
+
+    // Second login
+    await loginPage.login(artemisAdmin);
+
+    // Verify the reminder button is not shown
     await expect(page.getByRole('button', { name: 'Remind Me in 30 Days' })).not.toBeVisible();
 });
