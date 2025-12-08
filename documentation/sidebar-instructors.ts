747f1f76836a615a656b406c442af92302ef903d
Florian Glombik <florian.glombik@tum.de>
2025-12-09 00:09:06 +0100

    Migrate exercise pages

--- documentation/sidebar-instructors.ts
+++ documentation/sidebar-instructors.ts
@@ -3,6 +3,19 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
     default: [
         'intro',
+        {
+            type: 'category',
+            label: 'Exercises',
+            link: {
+                type: 'doc',
+                id: 'exercises/index'
+            },
+            items: [
+                'exercises/general-information',
+                'exercises/textual-exercise',
+                'exercises/file-upload-exercise',
+            ]
+        },
         'programming',
         'lecture-series',
         'assessment',
