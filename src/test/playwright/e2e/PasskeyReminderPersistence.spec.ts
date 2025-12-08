5807f8c94e7412b72b8f67f0858598eb924293b9
Florian Glombik <florian.glombik@tum.de>
2025-12-08 22:02:34 +0100

    adjust test name

--- src/test/playwright/e2e/PasskeyReminderPersistence.spec.ts
+++ src/test/playwright/e2e/PasskeyReminderPersistence.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '../support/fixtures';
 
-test('Passkey reminder modal is not displayed on re-login after do not remind for 30 days was chosen', async ({ page, loginPage, navigationBar }) => {
+test('Passkey reminder modal is not displayed on re-login after remind me in 30 days was chosen', async ({ page, loginPage, navigationBar }) => {
     const artemisAdmin = { username: 'artemis_admin', password: 'artemis_admin' };
 
     // First login
