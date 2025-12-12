43754f04b5bfcdb01d37557f66a66ed5acb9b72c
Benjamin Schmitz <benjamin.schmitz@tum.de>
2025-12-12 14:36:41 +0100

    Add RateLimit Feature Toggle to client

--- src/main/webapp/app/shared/feature-toggle/feature-toggle.service.ts
+++ src/main/webapp/app/shared/feature-toggle/feature-toggle.service.ts
@@ -22,6 +22,7 @@ export enum FeatureToggle {
     AtlasML = 'AtlasML',
     AtlasAgent = 'AtlasAgent',
     Memiris = 'Memiris',
+    RateLimit = 'RateLimit',
 }
 export type ActiveFeatureToggles = Array<FeatureToggle>;
 
