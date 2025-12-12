d2fafaeba5f8087b3c3ab5cf34e50513b184e1e5
Khiem Nguyen <ge48hid@tum.de>
2025-12-12 15:50:55 +0100

    fix server style issue

--- src/main/java/de/tum/cit/aet/artemis/hyperion/dto/ProblemStatementRefinementRequestDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/hyperion/dto/ProblemStatementRefinementRequestDTO.java
@@ -2,11 +2,12 @@ package de.tum.cit.aet.artemis.hyperion.dto;
 
 import java.util.List;
 
-import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import org.jspecify.annotations.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
