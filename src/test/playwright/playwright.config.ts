ad8f2c1f9e3ebf8de6dc72deb6b788b366650bd3
Marcos Oliva Kaczmarek <116270052+MarcosOlivaKaczmarek@users.noreply.github.com>
2025-12-15 15:57:07 +0100

    chore: revert unintended changes to playwright.config.ts

--- src/test/playwright/playwright.config.ts
+++ src/test/playwright/playwright.config.ts
@@ -11,7 +11,6 @@ import path from 'path';
  * https://github.com/motdotla/dotenv
  */
 dotenv.config({ path: path.join(__dirname, 'playwright.env') });
-//dotenv.config({ path: `./playwright.env` });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -61,7 +60,7 @@ export default defineConfig({
             name: 'fast-tests',
             grep: /@fast|^[^@]*$/,
             timeout: (parseNumber(process.env.FAST_TEST_TIMEOUT_SECONDS) ?? 45) * 1000,
-            use: { browserName: 'chromium', viewport: null },
+            use: { browserName: 'chromium', viewport: { width: 1920, height: 1080 } },
         },
         // Tests with @slow tag. These tests are expected to run longer
         // than faster tests and have higher timeout.
