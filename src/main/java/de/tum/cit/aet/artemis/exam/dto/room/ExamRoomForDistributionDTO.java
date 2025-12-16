c6f51bf281737e004fa499f5dfd050fc5d182b50
SamuelRoettgermann <ge38hev@mytum.de>
2025-12-16 14:50:47 +0100

    Konsti changes

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomForDistributionDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomForDistributionDTO.java
@@ -1,6 +1,7 @@
 package de.tum.cit.aet.artemis.exam.dto.room;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import org.jspecify.annotations.Nullable;
 
@@ -9,8 +10,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import de.tum.cit.aet.artemis.exam.domain.room.ExamRoom;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ExamRoomForDistributionDTO(long id, @NotBlank String roomNumber, @Nullable String alternativeRoomNumber, @NotBlank String name, @Nullable String alternativeName,
-        @NotBlank String building) {
+public record ExamRoomForDistributionDTO(@NotNull Long id, @NotBlank String roomNumber, @Nullable String alternativeRoomNumber, @NotBlank String name,
+        @Nullable String alternativeName, @NotBlank String building) {
 
     public static ExamRoomForDistributionDTO from(ExamRoom examRoom) {
         return new ExamRoomForDistributionDTO(examRoom.getId(), examRoom.getRoomNumber(), examRoom.getAlternativeRoomNumber(), examRoom.getName(), examRoom.getAlternativeName(),
