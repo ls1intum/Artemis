c352508cbca94ee94bb739d251df339a46d5781e
Erik Stern <erik.stern@outlook.de>
2025-12-17 00:51:16 +0100

    Fix architecture tests.

--- src/test/java/de/tum/cit/aet/artemis/programming/service/GitServiceTest.java
+++ src/test/java/de/tum/cit/aet/artemis/programming/service/GitServiceTest.java
@@ -1,16 +1,17 @@
 package de.tum.cit.aet.artemis.programming.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.URIish;
@@ -23,7 +24,7 @@ import de.tum.cit.aet.artemis.core.exception.GitException;
 import de.tum.cit.aet.artemis.programming.domain.Repository;
 import de.tum.cit.aet.artemis.programming.service.localvc.LocalVCRepositoryUri;
 
-public class GitServiceTest {
+class GitServiceTest {
 
     private GitService gitService;
 
@@ -45,24 +46,29 @@ public class GitServiceTest {
         return Git.init().setDirectory(localDir.toFile()).call();
     }
 
+    private void writeString(Path path, String content) throws IOException {
+        final File file = new File(path.toUri());
+        FileUtils.writeStringToFile(file, content, "ISO-8859-1");
+    }
+
     private Repository getRepo(Git git, boolean pushFirstCommit, boolean addAndCommitSecondLocalFile, boolean addThirdFile, boolean stageThirdFile)
             throws GitAPIException, IOException, URISyntaxException {
         Git.init().setBare(true).setDirectory(remoteDir.toFile()).setInitialBranch("main").call();
 
-        Files.writeString(localDir.resolve("file.txt"), "initial");
+        writeString(localDir.resolve("file.txt"), "initial");
         git.add().addFilepattern(".").call();
-        git.commit().setMessage("init").call();
+        GitService.commit(git).setMessage("init").call();
         git.remoteAdd().setName("origin").setUri(new URIish(remoteDir.toUri().toString())).call();
         if (pushFirstCommit) {
             git.push().setRemote("origin").call();
         }
         if (addAndCommitSecondLocalFile) {
-            Files.writeString(localDir.resolve("file2.txt"), "second");
+            writeString(localDir.resolve("file2.txt"), "second");
             git.add().addFilepattern(".").call();
-            git.commit().setMessage("file 2").call();
+            GitService.commit(git).setMessage("file 2").call();
         }
         if (addThirdFile) {
-            Files.writeString(localDir.resolve("staged.txt"), "staged");
+            writeString(localDir.resolve("staged.txt"), "staged");
         }
         if (stageThirdFile) {
             git.add().addFilepattern(".").call();
@@ -131,7 +137,7 @@ public class GitServiceTest {
             try (Repository repo = getRepo(git, true, true, true, true)) {
                 var repoSpy = Mockito.spy(repo);
                 when(repoSpy.findRef(anyString())).thenThrow(IOException.class);
-                assertThrows(GitException.class, () -> gitService.areLocalAndRemoteBranchesDivergent(repoSpy, git));
+                assertThatThrownBy(() -> gitService.areLocalAndRemoteBranchesDivergent(repoSpy, git)).isInstanceOf(GitException.class);
             }
         }
     }
