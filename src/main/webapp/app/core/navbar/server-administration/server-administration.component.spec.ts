962f39398f50562c738b8ab4e0dbc5a1d60f00c2
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-12-17 10:50:33 +0100

    resolve CI issues

--- src/main/webapp/app/core/navbar/server-administration/server-administration.component.spec.ts
+++ src/main/webapp/app/core/navbar/server-administration/server-administration.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ServerAdministrationComponent } from './server-administration.component';
