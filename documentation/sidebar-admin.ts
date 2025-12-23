c338aec0028c3733b9ac73a9d549b9cbdfc94a51
Florian Glombik <florian.glombik@tum.de>
2025-12-23 03:56:53 +0100

    Migrate access rights documentation

--- documentation/sidebar-admin.ts
+++ documentation/sidebar-admin.ts
@@ -3,6 +3,7 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
     default: [
         'intro',
+        'access-rights',
         'artemis-intelligence',
         {
             type: 'category',
