1a6a945192976b8c053bb671fb9e27ad237ab730
Florian Glombik <florian.glombik@tum.de>
2025-12-23 16:59:06 +0100

    Remove Nullable annotations

--- src/main/java/de/tum/cit/aet/artemis/core/dto/PasskeyAdminDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/PasskeyAdminDTO.java
@@ -4,8 +4,6 @@ import java.time.Instant;
 
 import jakarta.validation.constraints.NotNull;
 
-import org.jspecify.annotations.Nullable;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import de.tum.cit.aet.artemis.core.config.validator.Base64Url;
@@ -15,6 +13,6 @@ import de.tum.cit.aet.artemis.core.config.validator.Base64Url;
  * This extends the basic PasskeyDTO with additional user details.
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record PasskeyAdminDTO(@NotNull @Base64Url String credentialId, @Nullable String label, @NotNull Instant created, @Nullable Instant lastUsed, boolean isSuperAdminApproved,
-        @NotNull Long userId, @NotNull String userLogin, @NotNull String userName) {
+public record PasskeyAdminDTO(@NotNull @Base64Url String credentialId, String label, @NotNull Instant created, Instant lastUsed, boolean isSuperAdminApproved, @NotNull Long userId,
+        @NotNull String userLogin, @NotNull String userName) {
 }
