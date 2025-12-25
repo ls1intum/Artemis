7ad51507153a7fe5394c5a508b4254a0fa052dec
Mátyás Hoffer-Tóth <matyas.hoffer.toth@gmail.com>
2025-12-25 01:53:22 +0100

    Programming exercises: implement coderabbit suggestion

--- src/main/java/de/tum/cit/aet/artemis/programming/dto/ProgrammingExerciseDeletionSummaryDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/dto/ProgrammingExerciseDeletionSummaryDTO.java
@@ -2,6 +2,6 @@ package de.tum.cit.aet.artemis.programming.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ProgrammingExerciseDeletionSummaryDTO(long numberOfStudentParticipations, long numberOfBuilds, long numberOfCommunicationPosts, long numberOfAnswerPosts) {
 }
