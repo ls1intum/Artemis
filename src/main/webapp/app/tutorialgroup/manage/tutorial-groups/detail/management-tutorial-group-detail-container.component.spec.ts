27d4ada06d743f27977b205392368ec87b2ac2f4
Stephan Krusche <krusche@tum.de>
2026-01-02 23:10:32 +0100

    Development: Migrate the tutorial group client module to signals and vitest (#11878)

--- src/main/webapp/app/tutorialgroup/manage/tutorial-groups/detail/management-tutorial-group-detail-container.component.spec.ts
+++ src/main/webapp/app/tutorialgroup/manage/tutorial-groups/detail/management-tutorial-group-detail-container.component.spec.ts
@@ -1,4 +1,6 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 import { ManagementTutorialGroupDetailContainerComponent } from 'app/tutorialgroup/manage/tutorial-groups/detail/management-tutorial-group-detail-container.component';
 import { TutorialGroupsService } from 'app/tutorialgroup/shared/service/tutorial-groups.service';
 import { MockProvider } from 'ng-mocks';
@@ -7,20 +9,26 @@ import { Router } from '@angular/router';
 import { MockRouter } from 'test/helpers/mocks/mock-router';
 import { generateExampleTutorialGroup } from 'test/helpers/sample/tutorialgroup/tutorialGroupExampleModels';
 import { TutorialGroup } from 'app/tutorialgroup/shared/entities/tutorial-group.model';
-import { HttpResponse } from '@angular/common/http';
+import { HttpResponse, provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { of } from 'rxjs';
 import { mockedActivatedRoute } from 'test/helpers/mocks/activated-route/mock-activated-route-query-param-map';
 import { MockTranslateService } from 'test/helpers/mocks/service/mock-translate.service';
 import { TranslateService } from '@ngx-translate/core';
 import { DialogService } from 'primeng/dynamicdialog';
 import { MockDialogService } from 'test/helpers/mocks/service/mock-dialog.service';
+import { OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
+import '@angular/localize/init';
 
 describe('TutorialGroupManagementDetailComponent', () => {
+    setupTestBed({ zoneless: true });
+
     let fixture: ComponentFixture<ManagementTutorialGroupDetailContainerComponent>;
     let component: ManagementTutorialGroupDetailContainerComponent;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ManagementTutorialGroupDetailContainerComponent, OwlNativeDateTimeModule],
             providers: [
                 MockProvider(TutorialGroupsService),
                 MockProvider(AlertService),
@@ -40,17 +48,17 @@ describe('TutorialGroupManagementDetailComponent', () => {
                     },
                     {},
                 ),
+                provideHttpClient(),
+                provideHttpClientTesting(),
             ],
-        })
-            .compileComponents()
-            .then(() => {
-                fixture = TestBed.createComponent(ManagementTutorialGroupDetailContainerComponent);
-                component = fixture.componentInstance;
-            });
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(ManagementTutorialGroupDetailContainerComponent);
+        component = fixture.componentInstance;
     });
 
     afterEach(() => {
-        jest.restoreAllMocks();
+        vi.restoreAllMocks();
     });
 
     it('should initialize', () => {
@@ -68,7 +76,7 @@ describe('TutorialGroupManagementDetailComponent', () => {
             status: 200,
         });
 
-        const findByIdStub = jest.spyOn(tutorialGroupService, 'getOneOfCourse').mockReturnValue(of(response));
+        const findByIdStub = vi.spyOn(tutorialGroupService, 'getOneOfCourse').mockReturnValue(of(response));
         fixture.detectChanges();
         expect(component.tutorialGroup).toEqual(tutorialGroupOfResponse);
         expect(component.tutorialGroupId).toBe(1);
