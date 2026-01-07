526aeeebfd2e15e36e6715b8736c9d24245747d8
Florian Glombik <florian.glombik@tum.de>
2026-01-07 08:42:48 +0100

    Fix displaying of section

--- documentation/sidebar-staff.ts
+++ documentation/sidebar-staff.ts
@@ -1,11 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-    default: [
-        'intro',
-        'setup',
-        'spring-ai'
-    ],
+    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc'],
 };
 
 export default sidebars;
