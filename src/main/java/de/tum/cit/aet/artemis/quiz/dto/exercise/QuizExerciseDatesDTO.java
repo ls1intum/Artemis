731832e3b5a75e7ee1783a64312410be094aec7e
Konstantin Kissel <konstikissel@gmail.com>
2026-01-13 12:14:35 +0100

    fix tests

--- src/main/java/de/tum/cit/aet/artemis/quiz/dto/exercise/QuizExerciseDatesDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/dto/exercise/QuizExerciseDatesDTO.java
@@ -2,8 +2,11 @@ package de.tum.cit.aet.artemis.quiz.dto.exercise;
 
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import de.tum.cit.aet.artemis.quiz.domain.QuizExercise;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record QuizExerciseDatesDTO(ZonedDateTime releaseDate, ZonedDateTime startDate, ZonedDateTime dueDate) {
 
     public static QuizExerciseDatesDTO of(QuizExercise quizExercise) {
