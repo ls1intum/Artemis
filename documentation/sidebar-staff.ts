ffc13d47d972d33f0cadae84aa6ba3cb5484007f
Florian Glombik <florian.glombik@tum.de>
2026-01-14 01:47:08 +0100

    Add section to sidebar and improve copy pasting

--- documentation/sidebar-staff.ts
+++ documentation/sidebar-staff.ts
@@ -1,7 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus'],
+    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi'],
 };
 
 export default sidebars;
