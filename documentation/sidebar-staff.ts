9d735b4a47a370c2eaa5aca18be0faeed5d8d5df
Florian Glombik <florian.glombik@tum.de>
2026-01-14 01:54:08 +0100

    Add debugging section

--- documentation/sidebar-staff.ts
+++ documentation/sidebar-staff.ts
@@ -1,7 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi', 'docker-compose'],
+    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi', 'docker-compose', 'docker-debugging'],
 };
 
 export default sidebars;
