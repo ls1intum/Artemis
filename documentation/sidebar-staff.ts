d19366df3e24669f3bf6b4ec4678f2940a36d718
Florian Glombik <florian.glombik@tum.de>
2026-01-14 01:55:54 +0100

    Add local database section and fix image size

--- documentation/sidebar-staff.ts
+++ documentation/sidebar-staff.ts
@@ -1,7 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi', 'docker-compose', 'docker-debugging'],
+    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi', 'docker-compose', 'docker-debugging', 'local-database-tests'],
 };
 
 export default sidebars;
