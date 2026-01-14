fdbed2b6bc02d0419d75f2de13e7bc60d1536b92
matyasht <matyas.hoffer.toth@gmail.com>
2026-01-14 20:55:47 +0100

    Programming exercises: Fix architecture test

--- src/main/java/de/tum/cit/aet/artemis/programming/dto/ProgrammingExerciseDeletionSummaryDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/dto/ProgrammingExerciseDeletionSummaryDTO.java
@@ -2,6 +2,6 @@ package de.tum.cit.aet.artemis.programming.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ProgrammingExerciseDeletionSummaryDTO(long numberOfStudentParticipations, long numberOfBuilds, long numberOfCommunicationPosts, long numberOfAnswerPosts) {
 }
