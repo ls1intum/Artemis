5cf8650da98f5083bff70693568e81b738993a20
Anian Schleyer <anian.schleyer@tum.de>
2026-01-16 14:38:46 +0100

    add validation annotations

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/ActiveExamDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/ActiveExamDTO.java
@@ -2,16 +2,19 @@ package de.tum.cit.aet.artemis.exam.dto;
 
 import java.time.ZonedDateTime;
 
+import jakarta.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ActiveExamDTO(Long id, String title, ZonedDateTime startDate, ZonedDateTime endDate, boolean testExam, CourseForActiveExamDTO course) {
+public record ActiveExamDTO(long id, @NotNull String title, @NotNull ZonedDateTime startDate, @NotNull ZonedDateTime endDate, boolean testExam,
+        @NotNull CourseForActiveExamDTO course) {
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public record CourseForActiveExamDTO(Long id, String title) {
+    public record CourseForActiveExamDTO(long id, @NotNull String title) {
     }
 
-    public ActiveExamDTO(Long id, String title, ZonedDateTime startDate, ZonedDateTime endDate, boolean testExam, Long courseId, String courseTitle) {
+    public ActiveExamDTO(long id, String title, ZonedDateTime startDate, ZonedDateTime endDate, boolean testExam, Long courseId, String courseTitle) {
         this(id, title, startDate, endDate, testExam, new CourseForActiveExamDTO(courseId, courseTitle));
     }
 }
