c94f10c03d430cd3e69d7f3ac01c804e22e014ec
Florian Glombik <63976129+florian-glombik@users.noreply.github.com>
2026-01-17 12:48:36 +0100

    Development: Migrate setup guide documentation (#11879)

--- documentation/sidebar-staff.ts
+++ documentation/sidebar-staff.ts
@@ -1,11 +1,7 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-    default: [
-        'intro',
-        'setup',
-        'spring-ai'
-    ],
+    default: ['intro', 'setup', 'spring-ai', 'jenkins-localvc', 'aeolus', 'openapi', 'docker-compose', 'docker-debugging', 'local-database-tests'],
 };
 
 export default sidebars;
