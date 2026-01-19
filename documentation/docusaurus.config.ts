b06b0ec8780699f0a6b680df8709c1b2e5b4e8e0
Sascha O. Bacher <sascha.bacher@tum.de>
2026-01-19 19:22:25 +0100

    Fix Docusaurus config for custom pages URL

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -22,10 +22,10 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: 'https://ls1intum.github.io/',
+    url: 'https://docs.artemis.tum.de/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/Artemis/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
