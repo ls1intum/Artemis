3219133db5057ea6a5da4a6bae0d9f4d496edc4b
pacss <ge63nax@mytum.de>
2026-01-23 16:09:13 +0100

    comments change

--- src/main/webapp/app/quiz/manage/statistics/quiz-statistic/quiz-statistics.utils.ts
+++ src/main/webapp/app/quiz/manage/statistics/quiz-statistic/quiz-statistics.utils.ts
@@ -1,19 +1,17 @@
 import { QuizExercise } from 'app/quiz/shared/entities/quiz-exercise.model';
 
 /**
- * calculate the maximal  possible Score for the quiz
+ * calculate the maximal possible Score for the quiz
  * @param quizExercise the exercise the score should be computed
  * @return (int): sum over the Scores of all questions
  */
 export function calculateMaxScore(quizExercise: QuizExercise) {
+    const questions = quizExercise.quizQuestions ?? [];
     let result = 0;
 
-    if (quizExercise.quizQuestions) {
-        quizExercise.quizQuestions.forEach(function (question) {
-            result = result + question.points!;
-        });
-    } else {
-        result = quizExercise.maxPoints!;
-    }
+    questions.forEach((question) => {
+        result += question.points ?? 0;
+    });
+
     return result;
 }
