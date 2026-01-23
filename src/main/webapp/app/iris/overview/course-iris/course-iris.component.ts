a62383002a56ab6a51aef7e16d4e6878d84c87a0
Thy Nguyen <thy.nguyen@tum.de>
2026-01-23 23:09:15 +0100

    Added new location for iris on global sidebar

---
+++ src/main/webapp/app/iris/overview/course-iris/course-iris.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, computed, inject, viewChild } from '@angular/core';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { CourseChatbotComponent } from 'app/iris/overview/course-chatbot/course-chatbot.component';
+
+@Component({
+    selector: 'jhi-course-iris',
+    templateUrl: './course-iris.component.html',
+    styleUrls: ['./course-iris.component.scss'],
+    imports: [CourseChatbotComponent],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CourseIrisComponent {
+    private readonly route = inject(ActivatedRoute);
+    private readonly courseChatbot = viewChild(CourseChatbotComponent);
+
+    private readonly courseIdParam = toSignal((this.route.parent?.paramMap ?? of(convertToParamMap({}))).pipe(map((params) => params.get('courseId'))), { initialValue: null });
+
+    readonly courseId = computed(() => {
+        const value = this.courseIdParam();
+        if (!value) {
+            return undefined;
+        }
+        const parsed = Number.parseInt(value, 10);
+        return Number.isNaN(parsed) ? undefined : parsed;
+    });
+
+    isCollapsed = false;
+
+    toggleSidebar(): void {
+        this.courseChatbot()?.toggleChatHistory();
+        this.isCollapsed = !this.isCollapsed;
+    }
+}
