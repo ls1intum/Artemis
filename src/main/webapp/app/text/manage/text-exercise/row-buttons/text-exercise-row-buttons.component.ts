af61da042426c7d20233323256ba4940dca50212
matyasht <matyas.hoffer.toth@gmail.com>
2026-01-25 20:05:09 +0100

    Fix formatting and remove redundant parameter

--- src/main/webapp/app/text/manage/text-exercise/row-buttons/text-exercise-row-buttons.component.ts
+++ src/main/webapp/app/text/manage/text-exercise/row-buttons/text-exercise-row-buttons.component.ts
@@ -25,7 +25,6 @@ export class TextExerciseRowButtonsComponent {
     private textExerciseService = inject(TextExerciseService);
     private exerciseService = inject(ExerciseService);
 
-    courseId = input.required<number>();
     course = input.required<Course>();
     exercise = input.required<TextExercise>();
     private dialogErrorSource = new Subject<string>();
