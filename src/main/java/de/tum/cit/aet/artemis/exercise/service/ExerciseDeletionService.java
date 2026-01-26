898c20d9be6cdc5badd29954f79a7578b47126f0
matyasht <matyas.hoffer.toth@gmail.com>
2026-01-26 17:37:13 +0100

    Fix typo in vaiable name

--- src/main/java/de/tum/cit/aet/artemis/exercise/service/ExerciseDeletionService.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/service/ExerciseDeletionService.java
@@ -129,8 +129,8 @@ public class ExerciseDeletionService {
         final boolean hasBuilds = exercise instanceof ProgrammingExercise;
         final Long numberOfBuilds = hasBuilds ? buildJobRepository.countBuildJobsByExerciseIds(Set.of(exerciseId)) : null;
 
-        final boolean hasAssesments = !(exercise instanceof ProgrammingExercise || exercise instanceof QuizExercise);
-        final Long numberOfAssessments = hasAssesments ? resultRepository.countNumberOfFinishedAssessmentsForExercise(exerciseId) : null;
+        final boolean hasAssessments = !(exercise instanceof ProgrammingExercise || exercise instanceof QuizExercise);
+        final Long numberOfAssessments = hasAssessments ? resultRepository.countNumberOfFinishedAssessmentsForExercise(exerciseId) : null;
 
         long numberOfCommunicationPosts = 0;
         long numberOfAnswerPosts = 0;
