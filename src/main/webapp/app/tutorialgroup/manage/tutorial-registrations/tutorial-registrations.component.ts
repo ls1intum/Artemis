99863156d08fcbce81926f879a02392c87512ac1
marlonnienaber <marlonnienaber@web.de>
2026-01-27 12:07:57 +0100

    add basic components to registrations page

--- src/main/webapp/app/tutorialgroup/manage/tutorial-registrations/tutorial-registrations.component.ts
+++ src/main/webapp/app/tutorialgroup/manage/tutorial-registrations/tutorial-registrations.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CourseTutorialGroupDetailSessionStatusIndicatorComponent } from 'app/tutorialgroup/overview/course-tutorial-group-detail-session-status-indicator/course-tutorial-group-detail-session-status-indicator.component';
+import { TranslateDirective } from 'app/shared/language/translate.directive';
 
 @Component({
     selector: 'jhi-tutorial-registrations',
-    imports: [],
+    imports: [IconFieldModule, InputIconModule, InputTextModule, ButtonModule, CourseTutorialGroupDetailSessionStatusIndicatorComponent, TranslateDirective],
     templateUrl: './tutorial-registrations.component.html',
     styleUrl: './tutorial-registrations.component.scss',
 })
