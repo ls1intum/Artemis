fab9288161e0c280e3a0d18929362b809e9515d6
Marlon Nienaber <marlonnienaber@Maintainers-MacBook-Pro.local>
2026-02-04 15:05:04 +0100

    add simple registartion search panel content UI

--- src/main/webapp/app/tutorialgroup/manage/tutorial-registrations-register-modal/tutorial-registrations-register-modal.component.ts
+++ src/main/webapp/app/tutorialgroup/manage/tutorial-registrations-register-modal/tutorial-registrations-register-modal.component.ts
@@ -11,28 +11,30 @@ import { viewChild } from '@angular/core';
 import { Overlay, OverlayModule, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { ViewContainerRef } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { TutorialGroupRegisteredStudentDTO } from 'app/tutorialgroup/shared/entities/tutorial-group.model';
 
 @Component({
     selector: 'jhi-tutorial-registrations-register-modal',
-    imports: [Dialog, FormsModule, IconFieldModule, InputIconModule, InputTextModule, OverlayModule],
+    imports: [Dialog, FormsModule, IconFieldModule, InputIconModule, InputTextModule, OverlayModule, ScrollingModule],
     templateUrl: './tutorial-registrations-register-modal.component.html',
     styleUrl: './tutorial-registrations-register-modal.component.scss',
 })
 export class TutorialRegistrationsRegisterModalComponent implements OnDestroy {
     private translateService = inject(TranslateService);
     private currentLocale = getCurrentLocaleSignal(this.translateService);
-
-    isOpen = signal(false);
-    searchString = signal<string>('');
-    header = computed<string>(() => this.computeHeader());
-    searchBarPlaceholder = computed<string>(() => this.computeSearchBarPlaceholder());
-
-    searchInput = viewChild<ElementRef<HTMLInputElement>>('searchInput');
-    panelTemplate = viewChild<TemplateRef<unknown>>('panelTemplate');
-
     private overlay = inject(Overlay);
     private overlayRef: OverlayRef | undefined = undefined;
     private viewContainerRef = inject(ViewContainerRef);
+    private searchInput = viewChild<ElementRef<HTMLInputElement>>('searchInput');
+    private panelTemplate = viewChild<TemplateRef<unknown>>('panelTemplate');
+
+    isOpen = signal(false);
+    isLoading = signal(true);
+    searchString = signal<string>('');
+    header = computed<string>(() => this.computeHeader());
+    searchBarPlaceholder = computed<string>(() => this.computeSearchBarPlaceholder());
+    students = signal<TutorialGroupRegisteredStudentDTO[]>(mockStudents);
 
     ngOnDestroy(): void {
         this.overlayRef?.dispose();
@@ -82,6 +84,10 @@ export class TutorialRegistrationsRegisterModalComponent implements OnDestroy {
         this.overlayRef.attach(new TemplatePortal(panelTemplate, this.viewContainerRef));
     }
 
+    trackStudentById(index: number, student: TutorialGroupRegisteredStudentDTO): number {
+        return student.id;
+    }
+
     private computeHeader(): string {
         this.currentLocale();
         return this.translateService.instant('artemisApp.pages.tutorialGroupRegistrations.registerModal.header');
@@ -91,3 +97,11 @@ export class TutorialRegistrationsRegisterModalComponent implements OnDestroy {
         return 'Search by Login or Name';
     }
 }
+
+const mockStudents: TutorialGroupRegisteredStudentDTO[] = [
+    { id: 1, name: 'Jaohn Doe', login: 'jdoe' },
+    { id: 2, name: 'Jaice Smith', login: 'asmith' },
+    { id: 3, name: 'Jabruce Wayne', login: 'bwayne' },
+    { id: 4, name: 'Jalark Kent', login: 'ckent' },
+    { id: 5, name: 'Japeter Parker', login: 'pparker' },
+];
