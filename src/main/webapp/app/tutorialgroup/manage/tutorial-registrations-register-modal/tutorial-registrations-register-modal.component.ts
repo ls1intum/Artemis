63e6439f0ee319a19fc7105dbfd6444429df9cf1
Marlon Nienaber <marlonnienaber@Maintainers-MacBook-Pro.fritz.box>
2026-02-04 14:29:20 +0100

    create custome search panel for student registration

--- src/main/webapp/app/tutorialgroup/manage/tutorial-registrations-register-modal/tutorial-registrations-register-modal.component.ts
+++ src/main/webapp/app/tutorialgroup/manage/tutorial-registrations-register-modal/tutorial-registrations-register-modal.component.ts
@@ -6,14 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { ElementRef, OnDestroy, TemplateRef } from '@angular/core';
+import { viewChild } from '@angular/core';
+import { Overlay, OverlayModule, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { ViewContainerRef } from '@angular/core';
 
 @Component({
     selector: 'jhi-tutorial-registrations-register-modal',
-    imports: [Dialog, FormsModule, IconFieldModule, InputIconModule, InputTextModule],
+    imports: [Dialog, FormsModule, IconFieldModule, InputIconModule, InputTextModule, OverlayModule],
     templateUrl: './tutorial-registrations-register-modal.component.html',
     styleUrl: './tutorial-registrations-register-modal.component.scss',
 })
-export class TutorialRegistrationsRegisterModalComponent {
+export class TutorialRegistrationsRegisterModalComponent implements OnDestroy {
     private translateService = inject(TranslateService);
     private currentLocale = getCurrentLocaleSignal(this.translateService);
 
@@ -22,8 +27,59 @@ export class TutorialRegistrationsRegisterModalComponent {
     header = computed<string>(() => this.computeHeader());
     searchBarPlaceholder = computed<string>(() => this.computeSearchBarPlaceholder());
 
+    searchInput = viewChild<ElementRef<HTMLInputElement>>('searchInput');
+    panelTemplate = viewChild<TemplateRef<unknown>>('panelTemplate');
+
+    private overlay = inject(Overlay);
+    private overlayRef: OverlayRef | undefined = undefined;
+    private viewContainerRef = inject(ViewContainerRef);
+
+    ngOnDestroy(): void {
+        this.overlayRef?.dispose();
+        this.overlayRef = undefined;
+    }
+
     open() {
         this.isOpen.set(true);
+    }
+
+    openPanel(): void {
+        if (this.overlayRef) return;
+
+        const searchInput = this.searchInput()?.nativeElement;
+        const panelTemplate = this.panelTemplate();
+        if (!searchInput || !panelTemplate) return;
+
+        const positionStrategy = this.overlay
+            .position()
+            .flexibleConnectedTo(searchInput)
+            .withPositions([
+                {
+                    originX: 'start',
+                    originY: 'bottom',
+                    overlayX: 'start',
+                    overlayY: 'top',
+                },
+                {
+                    originX: 'start',
+                    originY: 'top',
+                    overlayX: 'start',
+                    overlayY: 'bottom',
+                },
+            ])
+            .withFlexibleDimensions(false)
+            .withPush(false);
+
+        this.overlayRef = this.overlay.create({
+            positionStrategy,
+            scrollStrategy: this.overlay.scrollStrategies.reposition(),
+        });
+
+        this.overlayRef.updateSize({
+            width: searchInput.offsetWidth,
+        });
+
+        this.overlayRef.attach(new TemplatePortal(panelTemplate, this.viewContainerRef));
     }
 
     private computeHeader(): string {
