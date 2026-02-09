96280d4594c63befd9ed282e1b95859d923f1e9e
Florian Glombik <florian.glombik@tum.de>
2026-02-09 19:39:50 +0100

    add environment variable

--- src/main/java/de/tum/cit/aet/artemis/core/config/weaviate/WeaviateConfigurationProperties.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/weaviate/WeaviateConfigurationProperties.java
@@ -17,15 +17,17 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
  * @param httpPort         the Weaviate HTTP port
  * @param grpcPort         the Weaviate gRPC port
  * @param scheme           the HTTP scheme (http/https) - determines secure connection type
- * @param collectionPrefix prefix prepended to all Weaviate collection names (useful for test isolation)
+ * @param collectionPrefix prefix prepended to all Weaviate collection names to avoid naming conflicts (defaults to "artemis")
  */
 @ConfigurationProperties(prefix = "artemis.weaviate")
 public record WeaviateConfigurationProperties(boolean enabled, String httpHost, @DefaultValue(DEFAULT_HTTP_PORT) int httpPort, @DefaultValue(DEFAULT_GRPC_PORT) int grpcPort,
-        String scheme, @DefaultValue("") String collectionPrefix) {
+        String scheme, @DefaultValue(DEFAULT_COLLECTION_PREFIX) String collectionPrefix) {
 
     public static final String DEFAULT_HTTP_PORT = "8001";
 
     public static final String DEFAULT_GRPC_PORT = "50051";
+
+    public static final String DEFAULT_COLLECTION_PREFIX = "";
 
     /**
      * Returns whether secure connections should be used based on the scheme.
