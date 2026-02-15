1ed34671ef2f1abf55c3463fe52c68b2f9e8cadf
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-15 05:27:25 +0100

    remove unused import

--- src/main/webapp/app/core/navbar/global-search/services/search-overlay.service.spec.ts
+++ src/main/webapp/app/core/navbar/global-search/services/search-overlay.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { setupTestBed } from '@analogjs/vitest-angular/setup-testbed';
 import { SearchOverlayService } from './search-overlay.service';
 
