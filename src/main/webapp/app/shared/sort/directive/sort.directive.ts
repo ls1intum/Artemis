fe6ff0ee8ecb861128fe4893d8ebc013a699cee0
Ajayvir S. <ajayvirsingh11@hotmail.com>
2026-02-15 22:58:45 +0100

    split model into output and input to emit only defined practices

--- src/main/webapp/app/shared/sort/directive/sort.directive.ts
+++ src/main/webapp/app/shared/sort/directive/sort.directive.ts
@@ -1,16 +1,18 @@
-import { Directive, model, output } from '@angular/core';
+import { Directive, input, output } from '@angular/core';
 
 @Directive({ selector: '[jhiSort]' })
 export class SortDirective<T> {
-    predicate = model<T | undefined>();
-    ascending = model<boolean>(false);
+    predicate = input<T | undefined>();
+    ascending = input<boolean>(false);
 
+    predicateChange = output<T>();
+    ascendingChange = output<boolean>();
     sortChange = output<{ predicate: T; ascending: boolean }>();
 
     sort(field: T): void {
         const newAscending = field !== this.predicate() ? true : !this.ascending();
-        this.ascending.set(newAscending);
-        this.predicate.set(field);
+        this.predicateChange.emit(field);
+        this.ascendingChange.emit(newAscending);
         this.sortChange.emit({ predicate: field, ascending: newAscending });
     }
 }
