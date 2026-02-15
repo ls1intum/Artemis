d70bca6edc6dbdace13431a8b82ca573c4d2c7df
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-15 13:23:26 +0100

    add tests to jest

--- jest.config.js
+++ jest.config.js
@@ -93,6 +93,7 @@ module.exports = {
         '!<rootDir>/src/main/webapp/app/buildagent/**',     // buildagent module uses Vitest (see vitest.config.ts)
         '!<rootDir>/src/main/webapp/app/core/account/**',   // core account module uses Vitest (see vitest.config.ts)
         '!<rootDir>/src/main/webapp/app/core/admin/**',     // core admin module uses Vitest (see vitest.config.ts)
+        '!<rootDir>/src/main/webapp/app/core/navbar/global-search/**', // global search uses Vitest (see vitest.config.ts)
         '!<rootDir>/src/main/webapp/app/fileupload/**',     // fileupload module uses Vitest (see vitest.config.ts)
         '!<rootDir>/src/main/webapp/app/lecture/**',        // lecture module uses Vitest (see vitest.config.ts)
         '!<rootDir>/src/main/webapp/app/modeling/**',       // modeling module uses Vitest (see vitest.config.ts)
@@ -111,6 +112,7 @@ module.exports = {
         '<rootDir>/src/main/webapp/app/buildagent/',        // buildagent module uses Vitest
         '<rootDir>/src/main/webapp/app/core/account/',      // core account module uses Vitest
         '<rootDir>/src/main/webapp/app/core/admin/',        // core admin module uses Vitest
+        '<rootDir>/src/main/webapp/app/core/navbar/global-search/', // global search uses Vitest
         '<rootDir>/src/main/webapp/app/fileupload/',        // fileupload module uses Vitest
         '<rootDir>/src/main/webapp/app/lecture/',           // lecture module uses Vitest
         '<rootDir>/src/main/webapp/app/modeling/',          // modeling module uses Vitest
@@ -157,6 +159,7 @@ module.exports = {
         '<rootDir>/src/main/webapp/app/buildagent/',    // buildagent module
         '<rootDir>/src/main/webapp/app/core/account/',  // core account module
         '<rootDir>/src/main/webapp/app/core/admin/',    // core admin module
+        '<rootDir>/src/main/webapp/app/core/navbar/global-search/', // global search
         '<rootDir>/src/main/webapp/app/fileupload/',    // fileupload module
         '<rootDir>/src/main/webapp/app/lecture/',       // lecture module
         '<rootDir>/src/main/webapp/app/modeling/',      // modeling module
