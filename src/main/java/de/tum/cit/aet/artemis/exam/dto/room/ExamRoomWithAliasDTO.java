418dc24c3e09bf411ff9b1ba72108184b1406267
SamuelRoettgermann <ge38hev@mytum.de>
2026-02-17 12:37:10 +0100

    re-added JsonInclude to satisfy architecture tests

--- src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomWithAliasDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exam/dto/room/ExamRoomWithAliasDTO.java
@@ -4,8 +4,11 @@ import jakarta.validation.constraints.NotNull;
 
 import org.jspecify.annotations.Nullable;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 // Constructed and returned in a repository query
 // Only used for server internals, never shared between server and client
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ExamRoomWithAliasDTO(@NotNull Long id, @NotNull String roomNumber, @Nullable String alternativeRoomNumber, @NotNull String name, @Nullable String alternativeName,
         @NotNull String building, @Nullable String alias) {
 }
