afff3f639d0bcd80a254e6398484065c164ec5b8
matyasht <matyas.hoffer.toth@gmail.com>
2026-02-17 21:08:49 +0100

    Exercises: Fix pipeline errors

--- src/main/java/de/tum/cit/aet/artemis/exercise/dto/ExerciseDeletionSummaryDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/dto/ExerciseDeletionSummaryDTO.java
@@ -1,5 +1,8 @@
 package de.tum.cit.aet.artemis.exercise.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude
 public record ExerciseDeletionSummaryDTO(long numberOfStudentParticipations, long numberOfBuilds, long numberOfAssessments, long numberOfCommunicationPosts,
         long numberOfAnswerPosts) {
 }
--- src/main/webapp/app/exercise/services/exercise.service.ts
+++ src/main/webapp/app/exercise/services/exercise.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import dayjs from 'dayjs/esm';
