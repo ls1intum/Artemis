474f2b946cdf26d2ddd0cdfbafe11221ffd4d920
Anian Schleyer <anian.schleyer@tum.de>
2026-02-17 18:58:38 +0100

    Fix tests

--- src/main/webapp/app/shared/monaco-editor/model/actions/url.action.spec.ts
+++ src/main/webapp/app/shared/monaco-editor/model/actions/url.action.spec.ts
@@ -2,18 +2,19 @@ import { UrlAction } from './url.action';
 import { TextEditor } from 'app/shared/monaco-editor/model/actions/adapter/text-editor.interface';
 import { TextEditorPosition } from 'app/shared/monaco-editor/model/actions/adapter/text-editor-position.model';
 import { TextEditorRange } from 'app/shared/monaco-editor/model/actions/adapter/text-editor-range.model';
+import { MonacoTextEditorAdapter } from 'app/shared/monaco-editor/model/actions/adapter/monaco-text-editor.adapter';
+import * as monaco from 'monaco-editor';
 
 describe('UrlAction', () => {
     const action = new UrlAction();
 
-    const mockEditor: jest.Mocked<TextEditor> = {
-        getSelection: jest.fn(),
-        getPosition: jest.fn(),
-        replaceTextAtRange: jest.fn(),
-        getTextAtRange: jest.fn(),
-        focus: jest.fn(),
-        setSelection: jest.fn(),
-    };
+    const editor: TextEditor = new MonacoTextEditorAdapter(monaco.editor.IStandaloneCodeEditor);
+
+    beforeEach(() => {
+        jest.spyOn(editor, 'replaceTextAtRange').mockReturnValue(null);
+        jest.spyOn(editor, 'focus').mockReturnValue(null);
+        jest.spyOn(editor, 'setSelection').mockReturnValue(null);
+    });
 
     afterEach(() => {
         jest.clearAllMocks();
@@ -21,32 +22,32 @@ describe('UrlAction', () => {
 
     it('should add a placeholder at cursor position if no text is selected', () => {
         const currentPosition = new TextEditorPosition(1, 50);
-        mockEditor.getSelection.mockReturnValue(null);
-        mockEditor.getPosition.mockReturnValue(currentPosition);
+        jest.spyOn(editor, 'getSelection').mockReturnValue(null);
+        jest.spyOn(editor, 'getPosition').mockReturnValue(currentPosition);
 
-        action.run(mockEditor);
+        action.run(editor);
 
         const expectedInsertRange = new TextEditorRange(currentPosition, currentPosition);
-        expect(mockEditor.replaceTextAtRange).toHaveBeenCalledWith(expectedInsertRange, UrlAction.DEFAULT_INSERT_TEXT);
+        expect(editor.replaceTextAtRange).toHaveBeenCalledWith(expectedInsertRange, UrlAction.DEFAULT_INSERT_TEXT);
     });
 
     it('should replace, then select placeholder if text is selected', () => {
         const currentPosition = new TextEditorPosition(1, 36);
         const selectionStart = new TextEditorPosition(1, 15);
         const mockText = 'something interesting';
-        mockEditor.getSelection.mockReturnValue(new TextEditorRange(selectionStart, currentPosition));
-        mockEditor.getPosition.mockReturnValue(currentPosition);
-        mockEditor.getTextAtRange.mockReturnValue(mockText);
+        jest.spyOn(editor, 'getSelection').mockReturnValue(new TextEditorRange(selectionStart, currentPosition));
+        jest.spyOn(editor, 'getPosition').mockReturnValue(currentPosition);
+        jest.spyOn(editor, 'getTextAtRange').mockReturnValue(mockText);
 
-        action.run(mockEditor);
+        action.run(editor);
 
         const expectedInsertRange = new TextEditorRange(selectionStart, currentPosition);
         const expectedText = `[${mockText}](${UrlAction.DEFAULT_LINK_PLACEHOLDER})`;
-        expect(mockEditor.replaceTextAtRange).toHaveBeenCalledWith(expectedInsertRange, expectedText);
+        expect(editor.replaceTextAtRange).toHaveBeenCalledWith(expectedInsertRange, expectedText);
 
         const expectedSelectionStart = new TextEditorPosition(1, 27);
         const expectedSelectionEnd = new TextEditorPosition(1, 35);
         const expectedSelectionRange = new TextEditorRange(expectedSelectionStart, expectedSelectionEnd);
-        expect(mockEditor.setSelection).toHaveBeenCalledWith(expectedSelectionRange);
+        expect(editor.setSelection).toHaveBeenCalledWith(expectedSelectionRange);
     });
 });
