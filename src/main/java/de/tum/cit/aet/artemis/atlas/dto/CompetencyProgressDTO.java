954648655e51d9d769c720937199e343037131ea
Maximilian Anzinger <anzinger@in.tum.de>
2026-02-25 11:51:36 +0100

    add more java doc

--- src/main/java/de/tum/cit/aet/artemis/atlas/dto/CompetencyProgressDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/atlas/dto/CompetencyProgressDTO.java
@@ -10,6 +10,12 @@ import de.tum.cit.aet.artemis.atlas.domain.competency.CompetencyProgress;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CompetencyProgressDTO(@Nullable Double progress, @Nullable Double confidence, @Nullable CompetencyProgressConfidenceReason confidenceReason) {
 
+    /**
+     * Maps competency progress to a DTO.
+     *
+     * @param progress the progress to map
+     * @return the DTO or null if the progress is null
+     */
     public static @Nullable CompetencyProgressDTO of(@Nullable CompetencyProgress progress) {
         if (progress == null) {
             return null;
