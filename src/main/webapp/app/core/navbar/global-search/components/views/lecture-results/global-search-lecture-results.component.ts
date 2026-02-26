468e37884393bdcfaee282a1dd73957daf1d45d8
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-27 00:41:19 +0100

    add lecture search model, service, and results component skeleton

---
+++ src/main/webapp/app/core/navbar/global-search/components/views/lecture-results/global-search-lecture-results.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-global-search-lecture-results',
+    standalone: true,
+    templateUrl: 'global-search-lecture-results.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [],
+})
+export class GlobalSearchLectureResultsComponent {}
