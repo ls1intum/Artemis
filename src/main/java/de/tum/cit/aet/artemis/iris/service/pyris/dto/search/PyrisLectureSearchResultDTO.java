1961a5aef0b05343a71d6aa3cd00fd2c102f526f
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-27 00:39:54 +0100

    add TODO for nested DTO refactor on lecture search result

--- src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/dto/search/PyrisLectureSearchResultDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/dto/search/PyrisLectureSearchResultDTO.java
@@ -2,6 +2,8 @@ package de.tum.cit.aet.artemis.iris.service.pyris.dto.search;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+// TODO: Refactor to nested structure once Pyris is updated to return nested JSON.
+// Planned shape: { course: { id, name }, lecture: { id, name }, lectureUnit: { id, name, link, pageNumber }, snippet, baseUrl }
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record PyrisLectureSearchResultDTO(long lectureUnitId, String lectureUnitName, String lectureUnitLink, long lectureId, String lectureName, long courseId, String courseName,
         int pageNumber, String snippet, String baseUrl) {
