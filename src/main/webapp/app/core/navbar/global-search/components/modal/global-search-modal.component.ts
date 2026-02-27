7b0b8dca8e644e2dd99e65f53040e6d9e7789d18
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-27 02:04:43 +0100

    wire search input to lecture search endpoint

--- src/main/webapp/app/core/navbar/global-search/components/modal/global-search-modal.component.ts
+++ src/main/webapp/app/core/navbar/global-search/components/modal/global-search-modal.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnDestroy, inject, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, OnDestroy, inject, signal, viewChild } from '@angular/core';
 import { SearchOverlayService } from '../../services/search-overlay.service';
 import { OsDetectorService } from '../../services/os-detector.service';
 import { AccountService } from 'app/core/auth/account.service';
@@ -7,12 +7,14 @@ import { faArrowDown, faArrowUp, faFileLines, faSearch } from '@fortawesome/free
 import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 import { DialogModule } from 'primeng/dialog';
 import { GlobalSearchActionItemComponent } from 'app/core/navbar/global-search/components/action-item/global-search-action-item.component';
+import { SearchView } from 'app/core/navbar/global-search/models/search-view.model';
+import { GlobalSearchLectureResultsComponent } from 'app/core/navbar/global-search/components/views/lecture-results/global-search-lecture-results.component';
 
 @Component({
     selector: 'jhi-global-search-modal',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [DialogModule, FaIconComponent, ArtemisTranslatePipe, GlobalSearchActionItemComponent],
+    imports: [DialogModule, FaIconComponent, ArtemisTranslatePipe, GlobalSearchActionItemComponent, GlobalSearchLectureResultsComponent],
     templateUrl: './global-search-modal.component.html',
     styleUrls: ['./global-search-modal.component.scss'],
 })
@@ -26,6 +28,9 @@ export class GlobalSearchModalComponent implements OnDestroy {
     protected readonly faArrowDown = faArrowDown;
     protected readonly faFileLines = faFileLines;
     protected searchInput = viewChild<ElementRef<HTMLInputElement>>('searchInput');
+    protected readonly currentView = signal(SearchView.Navigation);
+    protected readonly SearchView = SearchView;
+    protected readonly searchQuery = signal('');
 
     protected focusInput() {
         setTimeout(() => {
@@ -47,6 +52,10 @@ export class GlobalSearchModalComponent implements OnDestroy {
 
     private isToggleShortcut(event: KeyboardEvent): boolean {
         return event.key.toLowerCase() === 'k' && this.osDetector.isActionKey(event) && this.accountService.isAuthenticated() && !event.repeat;
+    }
+
+    protected navigateTo(view: SearchView) {
+        this.currentView.set(view);
     }
 
     ngOnDestroy() {
