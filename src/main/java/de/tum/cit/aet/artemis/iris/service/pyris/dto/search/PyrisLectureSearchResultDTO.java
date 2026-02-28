2670bc84a22783aa7aa104499a7a9eea25866c22
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-28 23:51:24 +0100

    fix architecture violations in lecture search resource and DTOs

--- src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/dto/search/PyrisLectureSearchResultDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/service/pyris/dto/search/PyrisLectureSearchResultDTO.java
@@ -3,17 +3,17 @@ package de.tum.cit.aet.artemis.iris.service.pyris.dto.search;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record PyrisLectureSearchResultDTO(Course course, Lecture lecture, LectureUnit lectureUnit, String snippet) {
+public record PyrisLectureSearchResultDTO(CourseDTO course, LectureDTO lecture, LectureUnitDTO lectureUnit, String snippet) {
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public record Course(long id, String name) {
+    public record CourseDTO(long id, String name) {
     }
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public record Lecture(long id, String name) {
+    public record LectureDTO(long id, String name) {
     }
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    public record LectureUnit(long id, String name, String link, int pageNumber) {
+    public record LectureUnitDTO(long id, String name, String link, int pageNumber) {
     }
 }
