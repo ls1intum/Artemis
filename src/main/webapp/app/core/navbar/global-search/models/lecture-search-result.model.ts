967fa62787be179f079509534269fbb64d293013
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-28 22:44:35 +0100

    refactor lecture search result DTO to nested structure

--- src/main/webapp/app/core/navbar/global-search/models/lecture-search-result.model.ts
+++ src/main/webapp/app/core/navbar/global-search/models/lecture-search-result.model.ts
@@ -1,11 +1,23 @@
-export interface LectureSearchResult {
-    lectureUnitId: number;
-    lectureUnitName: string;
-    lectureUnitLink: string;
-    lectureId: number;
-    lectureName: string;
-    courseId: number;
-    courseName: string;
+export interface CourseInfo {
+    id: number;
+    name: string;
+}
+
+export interface LectureInfo {
+    id: number;
+    name: string;
+}
+
+export interface LectureUnitInfo {
+    id: number;
+    name: string;
+    link: string;
     pageNumber: number;
+}
+
+export interface LectureSearchResult {
+    course: CourseInfo;
+    lecture: LectureInfo;
+    lectureUnit: LectureUnitInfo;
     snippet?: string;
 }
