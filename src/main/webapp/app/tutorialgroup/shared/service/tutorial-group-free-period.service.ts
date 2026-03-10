eb6bc294af584a78cb59e2f0ae28e90acd6134fb
pacss <ge63nax@mytum.de>
2026-03-10 12:27:39 +0100

    fix comments issues and merging conflicts

--- src/main/webapp/app/tutorialgroup/shared/service/tutorial-group-free-period.service.ts
+++ src/main/webapp/app/tutorialgroup/shared/service/tutorial-group-free-period.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TutorialGroupFreePeriodApiService } from 'app/openapi/api/tutorialGroupFreePeriodApi.service';
-import { TutorialGroupFreePeriodDTO } from 'app/tutorialgroup/shared/entities/tutorial-group-free-period-dto.model';
+import { TutorialGroupFreePeriodDTO, TutorialGroupFreePeriodRequestDTO } from 'app/tutorialgroup/shared/entities/tutorial-group-free-period-dto.model';
 import { TutorialGroupFreePeriod } from 'app/tutorialgroup/shared/entities/tutorial-group-free-day.model';
 import { convertDateFromServer } from 'app/shared/util/date.utils';
 
@@ -22,10 +22,10 @@ export class TutorialGroupFreePeriodService {
         );
     }
 
-    create(courseId: number, tutorialGroupConfigurationId: number, tutorialGroupFreePeriodDTO: TutorialGroupFreePeriodDTO): Observable<EntityResponseType> {
+    create(courseId: number, tutorialGroupConfigurationId: number, tutorialGroupFreePeriodRequestDTO: TutorialGroupFreePeriodRequestDTO): Observable<EntityResponseType> {
         return this.httpClient.post<TutorialGroupFreePeriodDTO>(
             `${this.resourceURL}/courses/${courseId}/tutorial-groups-configuration/${tutorialGroupConfigurationId}/tutorial-free-periods`,
-            tutorialGroupFreePeriodDTO,
+            tutorialGroupFreePeriodRequestDTO,
             {
                 observe: 'response',
             },
@@ -36,11 +36,11 @@ export class TutorialGroupFreePeriodService {
         courseId: number,
         tutorialGroupConfigurationId: number,
         tutorialGroupFreePeriodId: number,
-        tutorialGroupFreePeriodDTO: TutorialGroupFreePeriodDTO,
+        tutorialGroupFreePeriodRequestDTO: TutorialGroupFreePeriodRequestDTO,
     ): Observable<EntityResponseType> {
         return this.httpClient.put<TutorialGroupFreePeriodDTO>(
             `${this.resourceURL}/courses/${courseId}/tutorial-groups-configuration/${tutorialGroupConfigurationId}/tutorial-free-periods/${tutorialGroupFreePeriodId}`,
-            tutorialGroupFreePeriodDTO,
+            tutorialGroupFreePeriodRequestDTO,
             {
                 observe: 'response',
             },
