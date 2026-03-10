673c5166f50c60a222e896a21a4cd031d1922e5d
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2026-03-10 16:55:34 +0100

    change type

--- src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-monaco.component.ts
+++ src/main/webapp/app/shared/markdown-editor/monaco/markdown-editor-monaco.component.ts
@@ -673,7 +673,7 @@ export class MarkdownEditorMonacoComponent implements AfterContentInit, AfterVie
         this.markdownChange.emit(event.text);
     }
 
-    onSelectionChanged(selection: IRange | undefined): void {
+    onSelectionChanged(selection: { isEmpty: boolean } | undefined): void {
         if (!selection.isEmpty === this.showTextStyleActions() && selection.isEmpty === this.showNonTextStyleActions()) {
             return;
         }
