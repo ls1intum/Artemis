6d2ddadb20f444f68c6fa114ef2927f4f17a2e34
Marlon Nienaber <marlonnienaber@web.de>
2026-03-13 11:53:01 +0100

    add first dependency for TutorialRegistrationsRegisterModalComponent

---
+++ src/main/webapp/app/shared/loading-indicator-overlay/loading-indicator-overlay.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-loading-indicator-overlay',
+    imports: [],
+    templateUrl: './loading-indicator-overlay.component.html',
+})
+export class LoadingIndicatorOverlayComponent {}
