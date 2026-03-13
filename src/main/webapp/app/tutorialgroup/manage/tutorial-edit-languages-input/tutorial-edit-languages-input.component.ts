2f6196ec6c704a7b4130fc47a5a7f3ce60a63efe
Marlon Nienaber <marlonnienaber@web.de>
2026-03-13 16:27:40 +0100

    add dependency for TutorialEditLanguagesInputComponent

--- src/main/webapp/app/tutorialgroup/manage/tutorial-edit-languages-input/tutorial-edit-languages-input.component.ts
+++ src/main/webapp/app/tutorialgroup/manage/tutorial-edit-languages-input/tutorial-edit-languages-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, TemplateRef, ViewContainerRef, computed, inject, input, model, signal, viewChild } from '@angular/core';
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
-import { Validation, ValidationStatus } from 'app/tutorialgroup/manage/tutorial-create-or-edit/tutorial-create-or-edit.component';
+import { Validation, ValidationStatus } from 'app/shared/util/validation';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
