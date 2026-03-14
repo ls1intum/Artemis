b5d2dc89502a3cd6e71c769a8cfb7829867c7de3
Benjamin Schmitz <benjamin.schmitz@tum.de>
2026-03-14 11:37:13 +0100

    Non final serialization

--- src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributed/redisson/RedissonCodecConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/service/localci/distributed/redisson/RedissonCodecConfiguration.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import de.tum.cit.aet.artemis.core.config.RedisCondition;
@@ -22,6 +25,18 @@ public class RedissonCodecConfiguration {
     public RedissonAutoConfigurationCustomizer redissonAutoConfigurationCustomizer() {
         ObjectMapper redissonObjectMapper = new ObjectMapper();
         redissonObjectMapper.registerModule(new JavaTimeModule());
-        return (Config configuration) -> configuration.setCodec(new JsonJacksonCodec(redissonObjectMapper));
+
+        return (Config configuration) -> configuration.setCodec(new JsonJacksonCodec(redissonObjectMapper) {
+
+            @Override
+            protected void init(ObjectMapper objectMapper) {
+                objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+                // Use EVERYTHING instead of the default NON_FINAL to include Java records (which are final)
+                // in type information. Without this, records like BuildAgentInformation are serialized
+                // without @class and cannot be deserialized back from Object.class.
+                objectMapper.activateDefaultTyping(BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(), ObjectMapper.DefaultTyping.EVERYTHING,
+                        JsonTypeInfo.As.PROPERTY);
+            }
+        });
     }
 }
