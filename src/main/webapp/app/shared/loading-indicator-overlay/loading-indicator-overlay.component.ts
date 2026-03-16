8715a3447efcd633aea58b61c54839307d4912d7
Marlon Nienaber <marlonnienaber@web.de>
2026-03-16 13:14:14 +0100

    use prime ng loading spinner for LoadingIndicatorOverlayComponent

--- src/main/webapp/app/shared/loading-indicator-overlay/loading-indicator-overlay.component.ts
+++ src/main/webapp/app/shared/loading-indicator-overlay/loading-indicator-overlay.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ArtemisTranslatePipe } from 'app/shared/pipes/artemis-translate.pipe';
 
 @Component({
     selector: 'jhi-loading-indicator-overlay',
-    imports: [],
+    imports: [ProgressSpinnerModule, ArtemisTranslatePipe],
     templateUrl: './loading-indicator-overlay.component.html',
 })
 export class LoadingIndicatorOverlayComponent {}
