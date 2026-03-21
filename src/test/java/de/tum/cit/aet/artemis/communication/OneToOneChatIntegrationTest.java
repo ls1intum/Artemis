2007e2d0057a58c68aacb241125c7335c202016b
claudia service user <claudia@aetvm38.cit.tum.de>
2026-03-21 12:46:56 +0100

    Fix CI failures: use ConversationService for test tearDown to avoid Hibernate 6.6 TransientObjectException
    Written by Claudia Anthropica

--- src/test/java/de/tum/cit/aet/artemis/communication/OneToOneChatIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/communication/OneToOneChatIntegrationTest.java
@@ -13,18 +13,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import de.tum.cit.aet.artemis.communication.dto.MetisCrudAction;
 import de.tum.cit.aet.artemis.communication.dto.OneToOneChatDTO;
 import de.tum.cit.aet.artemis.communication.dto.PostDTO;
+import de.tum.cit.aet.artemis.communication.service.conversation.ConversationService;
 import de.tum.cit.aet.artemis.core.domain.CourseInformationSharingConfiguration;
 import de.tum.cit.aet.artemis.core.user.util.UserFactory;
 
 class OneToOneChatIntegrationTest extends AbstractConversationTest {
 
     private static final String TEST_PREFIX = "ootest";
+
+    @Autowired
+    private ConversationService conversationService;
 
     @BeforeEach
     @Override
@@ -38,8 +43,8 @@ class OneToOneChatIntegrationTest extends AbstractConversationTest {
 
     @AfterEach
     void tearDown() {
-        conversationMessageRepository.deleteAll();
-        conversationRepository.deleteAllByCourseId(exampleCourseId);
+        var conversations = conversationRepository.findAllByCourseId(exampleCourseId);
+        conversations.forEach(conversation -> conversationService.deleteConversation(conversation.getId()));
     }
 
     @Override
