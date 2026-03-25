8e0734086ba859f8f72d8b4c1378dea63d3f79b9
Marlon Nienaber <marlonnienaber@web.de>
2026-03-25 20:14:27 +0100

    add TODOs for remaining unimplemented client test

--- src/main/webapp/app/tutorialgroup/manage/service/tutorial-group-tutors.service.spec.ts
+++ src/main/webapp/app/tutorialgroup/manage/service/tutorial-group-tutors.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
-
-import { TutorialGroupTutorsService } from './tutorial-group-tutors.service';
-
-describe('TutorialGroupTutor', () => {
-    let service: TutorialGroupTutorsService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(TutorialGroupTutorsService);
-    });
-
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-});
+// TODO: implement
--- src/main/webapp/app/tutorialgroup/shared/service/tutorial-group-shared-state.service.spec.ts
+++ src/main/webapp/app/tutorialgroup/shared/service/tutorial-group-shared-state.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
-
-import { TutorialGroupSharedStateService } from './tutorial-group-shared-state.service';
-
-describe('TutorialGroupManagementSharedState', () => {
-    let service: TutorialGroupSharedStateService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(TutorialGroupSharedStateService);
-    });
-
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-});
+// TODO: implement
