e95b3770bb6753eca3d2f4e40dd300064302b5aa
Marlon Nienaber <marlonnienaber@web.de>
2026-03-30 16:12:01 +0200

    implement change requests

--- src/main/webapp/app/shared/route/getRouteData.ts
+++ src/main/webapp/app/shared/route/getRouteData.ts
@@ -1,5 +1,8 @@
 import { ActivatedRoute, Data } from '@angular/router';
 
+/**
+ * Returns the first matching route data entry found while traversing from the current route up to the root.
+ */
 export function getRouteData<T>(route: ActivatedRoute, dataKey: string): T | undefined {
     for (const currentRoute of [...route.snapshot.pathFromRoot].reverse()) {
         if (hasDataKey(currentRoute.data, dataKey)) {
