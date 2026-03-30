ac86ff90613b0fd52b18ace8a0d0bcc75418f578
Marlon Nienaber <marlonnienaber@web.de>
2026-03-30 16:12:01 +0200

    implement change requests

--- src/main/webapp/app/shared/route/getPathVariable.ts
+++ src/main/webapp/app/shared/route/getPathVariable.ts
@@ -3,6 +3,9 @@ import { Signal, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs/operators';
 
+/**
+ * Returns a signal for the first matching numeric path parameter found while traversing from the current route up to the root.
+ */
 export function getNumericPathVariableSignal(route: ActivatedRoute, variableName: string): Signal<number | undefined> {
     let currentRoute: ActivatedRoute | null = route;
 
