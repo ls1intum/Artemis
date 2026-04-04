8f4532096be7cab7b11b10a3d188632503a541a9
claudia service user <claudia@aetvm38.cit.tum.de>
2026-04-04 21:45:39 +0200

    Fix CI failures: exclude test utility @Transactional from architecture test
    Written by Claudia Anthropica

--- src/test/java/de/tum/cit/aet/artemis/assessment/architecture/AssessmentRepositoryArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/assessment/architecture/AssessmentRepositoryArchitectureTest.java
@@ -1,10 +1,19 @@
 package de.tum.cit.aet.artemis.assessment.architecture;
 
+import java.util.Set;
+
 import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
 
 class AssessmentRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
 
     public String getModulePackage() {
         return ARTEMIS_PACKAGE + ".assessment";
+    }
+
+    @Override
+    protected Set<String> testTransactionalExclusions() {
+        return Set.of(
+                // Test utility method that uses EntityManager to bulk-update lastModifiedDate for deterministic test results
+                "de.tum.cit.aet.artemis.assessment.util.StudentScoreUtilService.normalizeLastModifiedDates(de.tum.cit.aet.artemis.core.domain.User, java.time.Instant)");
     }
 }
