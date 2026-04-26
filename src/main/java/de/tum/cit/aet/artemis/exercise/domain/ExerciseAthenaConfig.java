c96742dd261262fd7beb942909a64360ecc6f321
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2026-04-26 12:51:42 +0200

    feat: add ExerciseAthenaConfig entity

---
+++ src/main/java/de/tum/cit/aet/artemis/exercise/domain/ExerciseAthenaConfig.java
@@ -0,0 +1,59 @@
+package de.tum.cit.aet.artemis.exercise.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import de.tum.cit.aet.artemis.core.domain.DomainObject;
+
+/**
+ * Stores Athena module configuration for an exercise, such as feedback module names.
+ */
+@Entity
+@Table(name = "exercise_athena_config")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExerciseAthenaConfig extends DomainObject {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id", nullable = false, unique = true)
+    private Exercise exercise;
+
+    @Column(name = "preliminary_feedback_module", length = 255)
+    private String preliminaryFeedbackModule;
+
+    @Column(name = "graded_feedback_module", length = 255)
+    private String gradedFeedbackModule;
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public String getPreliminaryFeedbackModule() {
+        return preliminaryFeedbackModule;
+    }
+
+    public void setPreliminaryFeedbackModule(String preliminaryFeedbackModule) {
+        this.preliminaryFeedbackModule = preliminaryFeedbackModule;
+    }
+
+    public String getGradedFeedbackModule() {
+        return gradedFeedbackModule;
+    }
+
+    public void setGradedFeedbackModule(String gradedFeedbackModule) {
+        this.gradedFeedbackModule = gradedFeedbackModule;
+    }
+}
