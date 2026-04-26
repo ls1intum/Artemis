f2f71d348bcf964c8a552f9eaff0cd6d55e78b6b
matyasht <matyas.hoffer.toth@gmail.com>
2026-04-26 23:56:20 +0200

    Try to fix the pw manager ignore

--- src/main/webapp/app/shared/form/password-manager-ignore.directive.ts
+++ src/main/webapp/app/shared/form/password-manager-ignore.directive.ts
@@ -7,11 +7,11 @@ import { Directive } from '@angular/core';
     selector: 'input[jhiPasswordManagerIgnore]',
     standalone: true,
     host: {
-        'attr.autocomplete': '"off"',
-        'attr.data-1p-ignore': '"true"', // 1Password
-        'attr.data-lpignore': '"true"', // LastPass
-        'attr.data-protonpass-ignore': '"true"', // ProtonPass
-        'attr.data-bwignore': '"1"', // Bitwarden
+        '[attr.autocomplete]': '"off"',
+        '[attr.data-1p-ignore]': '"true"', // 1Password
+        '[attr.data-lpignore]': '"true"', // LastPass
+        '[attr.data-protonpass-ignore]': '"true"', // ProtonPass
+        '[attr.data-bwignore]': '"1"', // Bitwarden
     },
 })
 export class PasswordManagerIgnoreDirective {}
