0f904dd7e9e5f722bf17fa75590b1d2faddc14ec
matyasht <matyas.hoffer.toth@gmail.com>
2026-04-27 19:13:54 +0200

    Fix builds and split them into compile and test

--- src/main/resources/templates/ocaml/test/run.sh
+++ src/main/resources/templates/ocaml/test/run.sh
@@ -16,84 +16,98 @@ BUILD_TIMEOUT="1m"
 TEST_TIMEOUT="10m"
 PROBE_TIMEOUT="10s"
 SAFE=false
+ONLY="all"
 BUILD_ROOT=$(realpath ..)
-
-# run hidden tests before this date, this should be somtimes before the release of the exercise
-HIDDEN_START=$(date -d 'TZ="Europe/Berlin" 2020-01-01 00:00' +%s)
-# run hidden tests after this date, this should be somtimes after the due date and before the run of the after due tests
-HIDDEN_END=$(date -d 'TZ="Europe/Berlin" 2030-01-01 00:00' +%s)
-NOW=$(date +%s)
 RUN_HIDDEN=false
 
-if [ "$HIDDEN_START" -gt "$NOW" ] || [ "$NOW" -gt "$HIDDEN_END" ]; then
-    RUN_HIDDEN=true
-fi
-
-# check passed flags
-while getopts s opt; do
-  case $opt in
-    s) SAFE=true;;
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -s)
+      SAFE=true
+      shift
+      ;;
+    --only)
+      ONLY="$2"
+      shift 2
+      ;;
+    --run-hidden)
+      RUN_HIDDEN=true
+      shift
+      ;;
     *)
+      shift
+      ;;
   esac
 done
+
+if [ "$ONLY" != "all" ] && [ "$ONLY" != "compile" ] && [ "$ONLY" != "test" ]; then
+    echo "Unknown value for --only: $ONLY" >&2
+    exit 1
+fi
 
 if $SAFE; then
   exec 3>/dev/null
 else
   exec 3>&1
-  RUN_HIDDEN=true
 fi
 
 # check for symlink is the submission
-find ../${studentParentWorkingDirectoryName}/ -type l | grep -q . && echo "Cannot build with symlinks in submission." && exit 0
-
-# include solution and assignment in the tests
-# this will only pick up *.ml files in the /src folders if other files are required for the tests this needs to be adjusted
-cp_code ${solutionWorkingDirectory} solution
-echo 'include Assignment' > ${solutionWorkingDirectory}/solution.ml
-cp_code ${studentParentWorkingDirectoryName} assignment
-
-# select if tests are run by generated source code as student toplevel code may run before the tests and be able to spoof a runtime signal
-echo "let runHidden = $RUN_HIDDEN" > test/runHidden.ml
+find ../${studentParentWorkingDirectoryName}/ -type l | grep -q . && echo "Cannot build with symlinks in submission." && exit 1
 
 # shellcheck disable=SC2046
 eval $(opam env)
 
-# build the limitation checker
-if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build checker >&3 2>&3; then
-    echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
-    exit 0
-fi
-# and run it
-# this will report syntax errors in the submission
-if ! timeout -s SIGTERM $BUILD_TIMEOUT checker/checker.exe; then
-    echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
-    exit 0
-fi
-# build the student submission
-# don't reference the tests or solution, so that we can show the build output to the student and not leak test / solution code
-if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build --force ${solutionWorkingDirectory}; then
-    echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
-    exit 0
-fi
-# If there are build failures, the compiler sometimes prints source code of tests to stderr by default, which is shown to the participant.
-# Therefore, drop stderr output. If the student submission builds and matches the interface, this should never fail
-if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build --force test >&3 2>&3; then
-    echo "Unable to build tests, please report this failure to an instructor" >&2
-    exit 0
-fi
+if [ "$ONLY" != "test" ] || $RUN_HIDDEN; then
+    # include solution and assignment in the tests
+    # this will only pick up *.ml files in the /src folders if other files are required for the tests this needs to be adjusted
+    cp_code ${solutionWorkingDirectory} solution
+    echo 'include Assignment' > ${solutionWorkingDirectory}/solution.ml
+    cp_code ${studentParentWorkingDirectoryName} assignment
 
-cd "$BUILD_ROOT" || exit
+    # select if tests are run by generated source code as student toplevel code may run before the tests and be able to spoof a runtime signal
+    echo "let runHidden = $RUN_HIDDEN" > test/runHidden.ml
 
-# copy the test executable into the project root
-mv -f ${testWorkingDirectory}/test/test.exe ./
+    # build the limitation checker
+    if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build checker >&3 2>&3; then
+        echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
+        exit 1
+    fi
+    # and run it
+    # this will report syntax errors in the submission
+    if ! timeout -s SIGTERM $BUILD_TIMEOUT checker/checker.exe; then
+        echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
+        exit 1
+    fi
+    # build the student submission
+    # don't reference the tests or solution, so that we can show the build output to the student and not leak test / solution code
+    if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build --force ${solutionWorkingDirectory}; then
+        echo "Unable to build submission, please ensure that your code builds and matches the provided interface" >&2
+        exit 1
+    fi
+    # If there are build failures, the compiler sometimes prints source code of tests to stderr by default, which is shown to the participant.
+    # Therefore, drop stderr output. If the student submission builds and matches the interface, this should never fail
+    if ! timeout -s SIGTERM $BUILD_TIMEOUT dune build --force test >&3 2>&3; then
+        echo "Unable to build tests, please report this failure to an instructor" >&2
+        exit 1
+    fi
+    cd "$BUILD_ROOT" || exit
 
-# to then delete all source code, to prevent access to it while running the code
-if $SAFE; then
-    rm -rf ${studentParentWorkingDirectoryName}
-    rm -rf ${solutionWorkingDirectory}
-    rm -rf ${testWorkingDirectory}
-fi;
+    # copy the test executable into the project root
+    mv -f ${testWorkingDirectory}/test/test.exe ./
+
+    if [ "$ONLY" = "compile" ]; then
+        exit 0
+    fi
+
+    # to then delete all source code, to prevent access to it while running the code
+    if $SAFE; then
+        rm -rf ${studentParentWorkingDirectoryName}
+        rm -rf ${solutionWorkingDirectory}
+        rm -rf ${testWorkingDirectory}
+    fi
+else
+    cd "$BUILD_ROOT" || exit
+fi
 
 # running the test executable without arguments to cause them to exit without actually running any tests
 # if it doesnt, there is most likely a infinite loop in the students top level code.
@@ -105,7 +119,7 @@ fi;
 timeout -s SIGTERM $PROBE_TIMEOUT ./test.exe
 if [ $? = 124 ]; then # timeout exits with 124 if it had to kill the tests.
     echo -e "Testing your submission resulted in a timeout." 1>&2
-    exit 0
+    exit 1
 fi
 
 # Run the test
@@ -114,10 +128,11 @@ timeout -s SIGTERM $TEST_TIMEOUT ./test.exe -output-junit-file "test-reports/res
 
 if [ $? = 124 ]; then # timeout exits with 124 if it had to kill the tests.
     echo -e "Testing your submission resulted in a timeout." 1>&2
+    exit 1
 # Warn the participant in case no output file could be generated
 elif [ ! -f "$OUTPUT_FILE" ]; then
     echo -e "Your submission could not be built.\nPlease check whether your submission compiles and whether all functions and constants have the type specified in the problem statement." 1>&2
+    exit 1
 fi
 
-# We always exit this script with error code 0, so that the following steps on Bamboo are performed.
 exit 0
--- src/main/resources/templates/typescript/staticCodeAnalysis/test/eslint.config.mjs
+++ src/main/resources/templates/typescript/staticCodeAnalysis/test/eslint.config.mjs
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {ignores: ["**/dist/**"]},
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
