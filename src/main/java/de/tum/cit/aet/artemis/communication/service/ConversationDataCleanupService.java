b8a3756739183b17ff450cb2dbf58fab7764acac
Florian Glombik <florian.glombik@tum.de>
2026-05-20 01:18:36 +0200

    Make service lazy

--- src/main/java/de/tum/cit/aet/artemis/communication/service/ConversationDataCleanupService.java
+++ src/main/java/de/tum/cit/aet/artemis/communication/service/ConversationDataCleanupService.java
@@ -4,6 +4,7 @@ import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_CORE;
 
 import java.util.Optional;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import de.tum.cit.aet.artemis.communication.repository.PostRepository;
 import de.tum.cit.aet.artemis.communication.repository.ReactionRepository;
 import de.tum.cit.aet.artemis.globalsearch.service.SearchableEntityWeaviateService;
 
+@Lazy
 @Service
 @Profile(PROFILE_CORE)
 public class ConversationDataCleanupService {
