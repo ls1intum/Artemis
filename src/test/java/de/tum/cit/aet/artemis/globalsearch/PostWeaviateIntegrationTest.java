b5a1edbd25301c627f04749784539d9e0332a5b7
Florian Glombik <florian.glombik@tum.de>
2026-05-21 00:05:44 +0200

    Add integration test

--- src/test/java/de/tum/cit/aet/artemis/globalsearch/PostWeaviateIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/globalsearch/PostWeaviateIntegrationTest.java
@@ -1,5 +1,7 @@
 package de.tum.cit.aet.artemis.globalsearch;
 
+import static de.tum.cit.aet.artemis.globalsearch.util.WeaviateTestUtil.assertAnswerPostExistsInWeaviate;
+import static de.tum.cit.aet.artemis.globalsearch.util.WeaviateTestUtil.assertAnswerPostNotInWeaviate;
 import static de.tum.cit.aet.artemis.globalsearch.util.WeaviateTestUtil.assertPostExistsInWeaviate;
 import static de.tum.cit.aet.artemis.globalsearch.util.WeaviateTestUtil.assertPostNotInWeaviate;
 import static de.tum.cit.aet.artemis.globalsearch.util.WeaviateTestUtil.queryPostProperties;
@@ -9,6 +11,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 
 import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,14 +22,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import de.tum.cit.aet.artemis.communication.domain.AnswerPost;
 import de.tum.cit.aet.artemis.communication.domain.Post;
 import de.tum.cit.aet.artemis.communication.domain.conversation.Channel;
+import de.tum.cit.aet.artemis.communication.repository.AnswerPostRepository;
 import de.tum.cit.aet.artemis.communication.service.conversation.ChannelService;
 import de.tum.cit.aet.artemis.communication.test_repository.PostTestRepository;
 import de.tum.cit.aet.artemis.communication.util.ConversationFactory;
 import de.tum.cit.aet.artemis.core.domain.Course;
 import de.tum.cit.aet.artemis.core.domain.User;
 import de.tum.cit.aet.artemis.globalsearch.config.schema.entityschemas.SearchableEntitySchema;
+import de.tum.cit.aet.artemis.globalsearch.dto.searchableentity.AnswerPostSearchableEntityDTO;
 import de.tum.cit.aet.artemis.globalsearch.dto.searchableentity.PostSearchableEntityDTO;
 import de.tum.cit.aet.artemis.globalsearch.service.SearchableEntityWeaviateService;
 import de.tum.cit.aet.artemis.globalsearch.service.WeaviateService;
@@ -57,6 +63,9 @@ class PostWeaviateIntegrationTest extends AbstractProgrammingIntegrationLocalCIL
 
     @Autowired
     private PostTestRepository postRepository;
+
+    @Autowired
+    private AnswerPostRepository answerPostRepository;
 
     @Autowired
     private ProgrammingExerciseUtilService programmingExerciseUtilService;
@@ -99,6 +108,15 @@ class PostWeaviateIntegrationTest extends AbstractProgrammingIntegrationLocalCIL
         Post post = ConversationFactory.createBasicPost(0, instructor);
         post.setConversation(channel);
         return postRepository.save(post);
+    }
+
+    private AnswerPost createAndSaveAnswerPost(Post post) {
+        AnswerPost answerPost = new AnswerPost();
+        answerPost.setContent("Reply to " + post.getContent());
+        answerPost.setAuthor(instructor);
+        answerPost.setPost(post);
+        answerPost.setCreationDate(ZonedDateTime.now());
+        return answerPostRepository.save(answerPost);
     }
 
     @Nested
@@ -233,6 +251,56 @@ class PostWeaviateIntegrationTest extends AbstractProgrammingIntegrationLocalCIL
             request.delete("/api/core/admin/courses/" + course.getId(), HttpStatus.OK);
 
             assertPostNotInWeaviate(weaviateService, postId);
+        }
+
+        @Test
+        @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
+        void testDeleteAllAnswerPostsForPost_removesAnswerPostsFromWeaviate() throws Exception {
+            Channel channel = createPublicChannel("delete-answers-test");
+            Post post = createAndSavePost(channel);
+            AnswerPost answer1 = createAndSaveAnswerPost(post);
+            AnswerPost answer2 = createAndSaveAnswerPost(post);
+
+            searchableEntityWeaviateService.upsertPostAsync(PostSearchableEntityDTO.fromPost(post, channel));
+            searchableEntityWeaviateService.upsertAnswerPostAsync(AnswerPostSearchableEntityDTO.fromAnswerPost(answer1, channel));
+            searchableEntityWeaviateService.upsertAnswerPostAsync(AnswerPostSearchableEntityDTO.fromAnswerPost(answer2, channel));
+            assertPostExistsInWeaviate(weaviateService, post.getId());
+            assertAnswerPostExistsInWeaviate(weaviateService, answer1.getId());
+            assertAnswerPostExistsInWeaviate(weaviateService, answer2.getId());
+
+            long answer1Id = answer1.getId();
+            long answer2Id = answer2.getId();
+            searchableEntityWeaviateService.deleteAllAnswerPostsForPostAsync(post.getId());
+
+            assertAnswerPostNotInWeaviate(weaviateService, answer1Id);
+            assertAnswerPostNotInWeaviate(weaviateService, answer2Id);
+            // The parent post itself should still exist
+            assertPostExistsInWeaviate(weaviateService, post.getId());
+        }
+
+        @Test
+        @WithMockUser(username = TEST_PREFIX + "instructor1", roles = "INSTRUCTOR")
+        void testDeleteMessage_removesAnswerPostsFromWeaviate() throws Exception {
+            Channel channel = createPublicChannel("delete-msg-answers-test");
+            Post post = createAndSavePost(channel);
+            AnswerPost answer1 = createAndSaveAnswerPost(post);
+            AnswerPost answer2 = createAndSaveAnswerPost(post);
+
+            searchableEntityWeaviateService.upsertPostAsync(PostSearchableEntityDTO.fromPost(post, channel));
+            searchableEntityWeaviateService.upsertAnswerPostAsync(AnswerPostSearchableEntityDTO.fromAnswerPost(answer1, channel));
+            searchableEntityWeaviateService.upsertAnswerPostAsync(AnswerPostSearchableEntityDTO.fromAnswerPost(answer2, channel));
+            assertPostExistsInWeaviate(weaviateService, post.getId());
+            assertAnswerPostExistsInWeaviate(weaviateService, answer1.getId());
+            assertAnswerPostExistsInWeaviate(weaviateService, answer2.getId());
+
+            long postId = post.getId();
+            long answer1Id = answer1.getId();
+            long answer2Id = answer2.getId();
+            request.delete("/api/communication/courses/" + course.getId() + "/messages/" + postId, HttpStatus.OK);
+
+            assertPostNotInWeaviate(weaviateService, postId);
+            assertAnswerPostNotInWeaviate(weaviateService, answer1Id);
+            assertAnswerPostNotInWeaviate(weaviateService, answer2Id);
         }
     }
 }
