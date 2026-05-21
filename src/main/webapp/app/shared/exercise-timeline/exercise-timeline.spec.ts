afe371058334e94fb8999207d1fffa520f7b754b
Marlon Nienaber <marlonnienaber@web.de>
2026-05-21 19:25:36 +0200

    patch failing test

--- src/main/webapp/app/shared/exercise-timeline/exercise-timeline.spec.ts
+++ src/main/webapp/app/shared/exercise-timeline/exercise-timeline.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExerciseTimeline } from './exercise-timeline';
+import { TranslateService } from '@ngx-translate/core';
+import { MockTranslateService } from 'test/helpers/mocks/service/mock-translate.service';
 
 describe('ExerciseTimeline', () => {
     let component: ExerciseTimeline;
@@ -9,6 +11,7 @@ describe('ExerciseTimeline', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [ExerciseTimeline],
+            providers: [{ provide: TranslateService, useClass: MockTranslateService }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ExerciseTimeline);
