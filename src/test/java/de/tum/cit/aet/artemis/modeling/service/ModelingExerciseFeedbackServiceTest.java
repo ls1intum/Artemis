96949cc6d848abe86a78814f68d091d5fb7700ff
claudia service user <claudia@aetvm38.cit.tum.de>
2026-05-22 23:56:01 +0200

    Fix CI failures: use UserTestRepository in feedback service tests
    ModelingExerciseFeedbackServiceTest and TextExerciseFeedbackServiceTest
    mocked the production UserRepository, which now has the UserTestRepository
    subclass. This violated the enforceUsageOfTestRepository architecture rule.
    Switch the mocked field to UserTestRepository to match the test-repository
    convention (as already done for ResultTestRepository in the same classes).
    
    Written by Claudia Anthropica

--- src/test/java/de/tum/cit/aet/artemis/modeling/service/ModelingExerciseFeedbackServiceTest.java
+++ src/test/java/de/tum/cit/aet/artemis/modeling/service/ModelingExerciseFeedbackServiceTest.java
@@ -26,7 +26,7 @@ import de.tum.cit.aet.artemis.assessment.test_repository.ResultTestRepository;
 import de.tum.cit.aet.artemis.assessment.web.ResultWebsocketService;
 import de.tum.cit.aet.artemis.athena.api.AthenaFeedbackApi;
 import de.tum.cit.aet.artemis.core.domain.User;
-import de.tum.cit.aet.artemis.core.repository.UserRepository;
+import de.tum.cit.aet.artemis.core.test_repository.UserTestRepository;
 import de.tum.cit.aet.artemis.exercise.domain.participation.StudentParticipation;
 import de.tum.cit.aet.artemis.exercise.service.ParticipationService;
 import de.tum.cit.aet.artemis.exercise.service.SubmissionService;
@@ -64,7 +64,7 @@ class ModelingExerciseFeedbackServiceTest {
     private ParticipationService participationService;
 
     @Mock
-    private UserRepository userRepository;
+    private UserTestRepository userRepository;
 
     private ModelingExercise modelingExercise;
 
