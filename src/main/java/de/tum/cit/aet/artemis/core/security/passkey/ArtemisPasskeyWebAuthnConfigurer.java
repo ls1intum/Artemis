3180c2aed7cf553afeb94ee9bbe083309f9ae080
Florian Glombik <florian.glombik@tum.de>
2026-05-22 09:01:03 +0200

    Try to fix E2E tests

--- src/main/java/de/tum/cit/aet/artemis/core/security/passkey/ArtemisPasskeyWebAuthnConfigurer.java
+++ src/main/java/de/tum/cit/aet/artemis/core/security/passkey/ArtemisPasskeyWebAuthnConfigurer.java
@@ -135,16 +135,12 @@ public class ArtemisPasskeyWebAuthnConfigurer {
 
         try {
             URL clientUrlToRegisterPasskey = new URI(serverUrl).toURL();
+            URL clientUrlToAuthenticateWithPasskey = new URI(serverUrl + ":" + port).toURL();
 
             relyingPartyId = clientUrlToRegisterPasskey.getHost();
 
             allowedOrigins.add(clientUrlToRegisterPasskey.toString());
-
-            // Only append the client port if the server URL does not already contain one
-            if (clientUrlToRegisterPasskey.getPort() == -1) {
-                URL clientUrlToAuthenticateWithPasskey = new URI(serverUrl + ":" + port).toURL();
-                allowedOrigins.add(clientUrlToAuthenticateWithPasskey.toString());
-            }
+            allowedOrigins.add(clientUrlToAuthenticateWithPasskey.toString());
 
             addAndroidApkKeyHashesToAllowedOrigins();
 
