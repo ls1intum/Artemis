4a8bf73ae0e20185f9c7da3649d4782fb9887679
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2026-05-27 16:02:37 +0200

    fix: update CourseTestService DTO constructor call for new Athena fields

--- src/test/java/de/tum/cit/aet/artemis/core/util/CourseTestService.java
+++ src/test/java/de/tum/cit/aet/artemis/core/util/CourseTestService.java
@@ -3382,7 +3382,9 @@ public class CourseTestService {
                 course.getMaxRequestMoreFeedbackTimeDays(), course.getMaxComplaintTextLimit(), course.getMaxComplaintResponseTextLimit(), course.getColor(),
                 course.isEnrollmentEnabled(), course.getEnrollmentConfirmationMessage(), course.isUnenrollmentEnabled(), course.getLearningPathsEnabled(),
                 course.getStudentCourseAnalyticsDashboardEnabled(), course.getPresentationScore(), course.getMaxPoints(), course.getAccuracyOfScores(),
-                course.getRestrictedAthenaModulesAccess(), course.getTimeZone(), course.getCourseInformationSharingConfiguration());
+                course.getRestrictedAthenaModulesAccess(), course.isAthenaTextGradingEnabled(), course.isAthenaTextPreliminaryEnabled(), course.isAthenaModelingGradingEnabled(),
+                course.isAthenaModelingPreliminaryEnabled(), course.isAthenaProgrammingGradingEnabled(), course.isAthenaProgrammingPreliminaryEnabled(), course.getTimeZone(),
+                course.getCourseInformationSharingConfiguration());
     }
 
     public MockMultipartHttpServletRequestBuilder buildUpdateCourse(long id, @NonNull Course course) throws JsonProcessingException {
