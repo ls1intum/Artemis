2b804cf8b86581e30bf5d298453ec249f37df256
Patrick Bassner <patrick@bassner.de>
2026-05-30 08:49:35 +0200

    migrate(quiz): convert quiz re-evaluate popup/warning dialogs to PrimeNG DialogService

--- src/main/webapp/app/quiz/manage/service/quiz-exercise-popup.service.ts
+++ src/main/webapp/app/quiz/manage/service/quiz-exercise-popup.service.ts
@@ -1,58 +1,60 @@
 import { Component, Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { QuizExercise } from 'app/quiz/shared/entities/quiz-exercise.model';
 
 @Injectable({ providedIn: 'root' })
 export class QuizExercisePopupService {
-    private modalService = inject(NgbModal);
+    private dialogService = inject(DialogService);
     private router = inject(Router);
 
-    private ngbModalRef: NgbModalRef | null;
+    private dialogRef: DynamicDialogRef | null;
 
     constructor() {
-        this.ngbModalRef = null;
+        this.dialogRef = null;
     }
 
     /**
-     * Open the modal with the given content for the given exercise.
+     * Open the dialog with the given content for the given exercise.
      * @param component the content that should be shown
-     * @param quizExercise the quiz exercise for which the modal should be shown
+     * @param quizExercise the quiz exercise for which the dialog should be shown
      * @param files required for the form data upload
      */
-    open(component: Component, quizExercise: QuizExercise, files: Map<string, File>): Promise<NgbModalRef> {
-        return new Promise<NgbModalRef>((resolve) => {
-            if (this.ngbModalRef == undefined) {
-                this.ngbModalRef = this.quizExerciseModalRef(component, quizExercise, files);
+    open(component: Component, quizExercise: QuizExercise, files: Map<string, File>): Promise<DynamicDialogRef> {
+        return new Promise<DynamicDialogRef>((resolve) => {
+            if (this.dialogRef == undefined) {
+                this.dialogRef = this.quizExerciseDialogRef(component, quizExercise, files);
             }
-            resolve(this.ngbModalRef);
+            resolve(this.dialogRef);
         });
     }
 
     /**
-     * Open the modal with the given content for the given exercise.
+     * Open the dialog with the given content for the given exercise.
      * @param component the content that should be shown
-     * @param quizExercise the quiz exercise for which the modal should be shown
+     * @param quizExercise the quiz exercise for which the dialog should be shown
      * @param files required for the form data upload
      */
-    quizExerciseModalRef(component: Component, quizExercise: QuizExercise, files: Map<string, File>): NgbModalRef {
-        const modalRef: NgbModalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static' });
-        modalRef.componentInstance.quizExercise = quizExercise;
-        modalRef.componentInstance.files = files;
-        modalRef.result.then(
-            (result) => {
-                if (result === 're-evaluate') {
-                    this.router.navigate(['/course-management/' + quizExercise.course!.id + '/quiz-exercises']);
-                } else {
-                    this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
-                    this.ngbModalRef = null;
-                }
-            },
-            () => {
+    quizExerciseDialogRef(component: Component, quizExercise: QuizExercise, files: Map<string, File>): DynamicDialogRef {
+        const ref = this.dialogService.open(component, {
+            width: '50rem',
+            modal: true,
+            closable: true,
+            closeOnEscape: true,
+            dismissableMask: false,
+            draggable: false,
+            resizable: false,
+            showHeader: false,
+            data: { quizExercise, files },
+        });
+        ref.onClose.subscribe((result) => {
+            if (result === 're-evaluate') {
+                this.router.navigate(['/course-management/' + quizExercise.course!.id + '/quiz-exercises']);
+            } else {
                 this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
-                this.ngbModalRef = null;
-            },
-        );
-        return modalRef;
+                this.dialogRef = null;
+            }
+        });
+        return ref;
     }
 }
