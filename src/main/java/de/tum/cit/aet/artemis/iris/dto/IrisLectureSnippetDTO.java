992698f6ef098e20785c63f11a93057cc23bbc49
Louis Heinrich <louis.heinrich@tum.de>
2026-06-02 12:41:09 +0200

    Add @JsonInclude to IrisLectureSnippetDTO

--- src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisLectureSnippetDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/iris/dto/IrisLectureSnippetDTO.java
@@ -1,5 +1,7 @@
 package de.tum.cit.aet.artemis.iris.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * A single lecture-content snippet returned by the Iris lecture-search API.
  *
@@ -7,5 +9,6 @@ package de.tum.cit.aet.artemis.iris.dto;
  * @param lectureUnitName human-readable name of the lecture unit the snippet was extracted from
  * @param snippet         the extracted text passage
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record IrisLectureSnippetDTO(String lectureName, String lectureUnitName, String snippet) {
 }
