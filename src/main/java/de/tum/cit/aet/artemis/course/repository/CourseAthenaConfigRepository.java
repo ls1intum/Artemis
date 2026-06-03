8656f2f21a5b2bdd796cee962dd15da9b25f26b7
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2026-06-03 16:40:01 +0200

    fix: add @Transactional @Modifying to CourseAthenaConfigRepository.deleteByCourseId

--- src/main/java/de/tum/cit/aet/artemis/course/repository/CourseAthenaConfigRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/course/repository/CourseAthenaConfigRepository.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
 import de.tum.cit.aet.artemis.course.domain.CourseAthenaConfig;
@@ -18,5 +20,7 @@ public interface CourseAthenaConfigRepository extends ArtemisJpaRepository<Cours
 
     Optional<CourseAthenaConfig> findByCourseId(Long courseId);
 
+    @Transactional // ok because of delete
+    @Modifying
     void deleteByCourseId(Long courseId);
 }
