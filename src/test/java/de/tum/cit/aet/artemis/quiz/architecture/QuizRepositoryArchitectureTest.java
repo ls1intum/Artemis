1dca6e2d9d23a4a3c1f3d4c771071e866367e4dc
Nihad Uddin <113698778+Nihad74@users.noreply.github.com>
2026-06-05 11:29:31 +0200

    Fix: Add @Transactional exclusions to QuizRepositoryArchitectureTest for lazy collection initialization methods

--- src/test/java/de/tum/cit/aet/artemis/quiz/architecture/QuizRepositoryArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/quiz/architecture/QuizRepositoryArchitectureTest.java
@@ -1,10 +1,22 @@
 package de.tum.cit.aet.artemis.quiz.architecture;
 
+import java.util.Set;
+
 import de.tum.cit.aet.artemis.shared.architecture.module.AbstractModuleRepositoryArchitectureTest;
 
 class QuizRepositoryArchitectureTest extends AbstractModuleRepositoryArchitectureTest {
 
     public String getModulePackage() {
         return ARTEMIS_PACKAGE + ".quiz";
+    }
+
+    @Override
+    protected Set<String> testTransactionalExclusions() {
+        return Set.of("de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.withInitializedQuestionChildCollections(java.util.Optional)",
+                "de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.findWithEagerQuestionsAndStatisticsById(java.lang.Long)",
+                "de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.findWithEagerQuestionsAndStatisticsAndCompetenciesAndBatchesAndGradingCriteriaById(java.lang.Long)",
+                "de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.findWithEagerQuestionsById(java.lang.Long)",
+                "de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.findWithEagerQuestionsAndCompetenciesById(java.lang.Long)",
+                "de.tum.cit.aet.artemis.quiz.repository.QuizExerciseRepository.findForVersioningById(java.lang.Long)");
     }
 }
