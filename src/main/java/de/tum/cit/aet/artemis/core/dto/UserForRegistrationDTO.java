3ab930055064b41f694174be02a283ab0edba6e6
Stephan Krusche <krusche@tum.de>
2026-06-06 22:40:53 +0200

    Development: Enforce non empty DTO fields again

--- src/main/java/de/tum/cit/aet/artemis/core/dto/UserForRegistrationDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/core/dto/UserForRegistrationDTO.java
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
  * Generic DTO for displaying a user in a registration modal (search + register flow).
  * Used wherever an instructor needs to search Artemis users and register them into some entity (exam, tutorial group, etc.).
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record UserForRegistrationDTO(Long id, String login, String name, String email, String registrationNumber, String profilePictureUrl, boolean isRegistered) {
 }
