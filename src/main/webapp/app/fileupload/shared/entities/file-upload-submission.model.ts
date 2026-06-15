9f28c8b8978fbea8aa9944d2ace4e8ff1f2a3648
Dogus Aytok <dogus.aytok@tum.de>
2026-06-15 16:34:35 +0200

    refactor(fileupload): adapt angular submission service and models to DTOs

--- src/main/webapp/app/fileupload/shared/entities/file-upload-submission.model.ts
+++ src/main/webapp/app/fileupload/shared/entities/file-upload-submission.model.ts
@@ -1,5 +1,27 @@
-import { Submission, SubmissionExerciseType } from 'app/exercise/shared/entities/submission/submission.model';
+import { Submission, SubmissionExerciseType, SubmissionType } from 'app/exercise/shared/entities/submission/submission.model';
 import { addPublicFilePrefix } from 'app/app.constants';
+import { StudentParticipation } from 'app/exercise/shared/entities/participation/student-participation.model';
+import { Result } from 'app/exercise/shared/entities/result/result.model';
+import dayjs from 'dayjs/esm';
+
+export interface FileUploadSubmissionInputDTO {
+    id?: number;
+    submitted: boolean;
+    exerciseId?: number;
+}
+
+export interface FileUploadSubmissionDTO {
+    id?: number;
+    submitted?: boolean;
+    submissionDate?: dayjs.Dayjs;
+    type?: SubmissionType;
+    exampleSubmission?: boolean;
+    submissionExerciseType?: SubmissionExerciseType;
+    durationInMinutes?: number;
+    filePath?: string;
+    participation?: StudentParticipation;
+    results?: Result[];
+}
 
 export class FileUploadSubmission extends Submission {
     public filePath?: string;
