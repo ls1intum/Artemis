86fb7ca78b2d692a8946a5b3c1a3d8c3f297f060
Nihad Uddin <nihad.uddin@tum.de>
2026-06-22 17:59:18 +0200

    reduce DTO violations

--- src/test/java/de/tum/cit/aet/artemis/exam/architecture/ExamEntityUsageArchitectureTest.java
+++ src/test/java/de/tum/cit/aet/artemis/exam/architecture/ExamEntityUsageArchitectureTest.java
@@ -18,7 +18,7 @@ class ExamEntityUsageArchitectureTest extends AbstractModuleEntityUsageArchitect
     // TODO: Reduce this to 0 by returning DTOs instead of entities
     @Override
     protected int getExpectedEntityReturnViolations() {
-        return 31;
+        return 28;
     }
 
     // TODO: Reduce this to 0 by accepting DTOs instead of entities in @RequestBody/@RequestPart
