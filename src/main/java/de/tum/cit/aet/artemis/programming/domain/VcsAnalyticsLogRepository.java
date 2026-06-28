8d76418dd31383db9683833e2a4ebde9d066e88a
Lyynnnx <go98mip@mytum.de>
2026-06-28 16:01:35 +0200

    add: VcsAnalyticsLogRepository

---
+++ src/main/java/de/tum/cit/aet/artemis/programming/domain/VcsAnalyticsLogRepository.java
@@ -0,0 +1,16 @@
+package de.tum.cit.aet.artemis.programming.domain;
+
+import static de.tum.cit.aet.artemis.core.config.Constants.PROFILE_LOCALVC;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import de.tum.cit.aet.artemis.core.repository.base.ArtemisJpaRepository;
+
+@Profile(PROFILE_LOCALVC)
+@Lazy
+@Repository
+public interface VcsAnalyticsLogRepository extends ArtemisJpaRepository<VcsAccessLog, Long> {
+
+}
