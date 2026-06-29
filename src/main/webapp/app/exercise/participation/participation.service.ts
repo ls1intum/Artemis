46afcfe32ecb5eb50907b2cd5efbaea9cccec20c
Dogus Aytok <dogus.aytok@tum.de>
2026-06-29 18:19:54 +0200

    refactor(client): adapt client models and services for student participation DTOs

--- src/main/webapp/app/exercise/participation/participation.service.ts
+++ src/main/webapp/app/exercise/participation/participation.service.ts
@@ -16,6 +16,7 @@ import { ExerciseService } from 'app/exercise/services/exercise.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { convertDateFromClient, convertDateFromServer } from 'app/foundation/util/date.utils';
 import dayjs from 'dayjs/esm';
+import { StudentParticipationDTO, fromStudentParticipationDTO } from 'app/exercise/shared/entities/participation/student-participation.dto';
 
 export type EntityResponseType = HttpResponse<StudentParticipation>;
 export type EntityArrayResponseType = HttpResponse<StudentParticipation[]>;
@@ -62,15 +63,15 @@ export class ParticipationService {
             presentationScore: participation.presentationScore,
         };
         return this.http
-            .put<StudentParticipation>(`api/exercise/exercises/${exercise.id}/participations`, dto, { observe: 'response' })
-            .pipe(map((res: EntityResponseType) => this.processParticipationEntityResponseType(res)));
+            .put<StudentParticipationDTO>(`api/exercise/exercises/${exercise.id}/participations`, dto, { observe: 'response' })
+            .pipe(map((res) => this.processParticipationDTOResponseType(res)));
     }
 
     updateIndividualDueDates(exercise: Exercise, participations: StudentParticipation[]): Observable<EntityArrayResponseType> {
         const dtos = participations.map((participation) => this.toDueDateUpdateDTO(participation, exercise.id!));
         return this.http
-            .put<StudentParticipation[]>(`api/exercise/exercises/${exercise.id}/participations/update-individual-due-date`, dtos, { observe: 'response' })
-            .pipe(map((res: EntityArrayResponseType) => this.processParticipationEntityArrayResponseType(res)));
+            .put<StudentParticipationDTO[]>(`api/exercise/exercises/${exercise.id}/participations/update-individual-due-date`, dtos, { observe: 'response' })
+            .pipe(map((res) => this.processParticipationDTOArrayResponseType(res)));
     }
 
     private toDueDateUpdateDTO(participation: StudentParticipation, exerciseId: number): ParticipationDueDateUpdateDTO {
@@ -83,8 +84,8 @@ export class ParticipationService {
 
     find(participationId: number): Observable<EntityResponseType> {
         return this.http
-            .get<StudentParticipation>(`${this.resourceUrl}/${participationId}`, { observe: 'response' })
-            .pipe(map((res: EntityResponseType) => this.processParticipationEntityResponseType(res)));
+            .get<StudentParticipationDTO>(`${this.resourceUrl}/${participationId}`, { observe: 'response' })
+            .pipe(map((res) => this.processParticipationDTOResponseType(res)));
     }
 
     /**
@@ -178,8 +179,8 @@ export class ParticipationService {
     cleanupBuildPlan(participation: StudentParticipation): Observable<EntityResponseType> {
         const copy = this.convertParticipationDatesFromClient(participation);
         return this.http
-            .put<StudentParticipation>(`${this.resourceUrl}/${participation.id}/cleanup-build-plan`, copy, { observe: 'response' })
-            .pipe(map((res: EntityResponseType) => this.convertParticipationResponseDatesFromServer(res)));
+            .put<StudentParticipationDTO>(`${this.resourceUrl}/${participation.id}/cleanup-build-plan`, copy, { observe: 'response' })
+            .pipe(map((res) => this.processParticipationDTOResponseType(res)));
     }
 
     shouldPreferPractice(exercise?: Exercise): boolean {
@@ -333,10 +334,10 @@ export class ParticipationService {
      * This method bundles recurring conversion steps for Participation EntityArrayResponses.
      * @param participationRes
      */
-    private processParticipationEntityArrayResponseType(participationRes: EntityArrayResponseType): EntityArrayResponseType {
-        this.convertParticipationResponseArrayDatesFromServer(participationRes);
-        this.setAccessRightsParticipationEntityArrayResponseType(participationRes);
-        return participationRes;
+    private processParticipationDTOArrayResponseType(participationRes: HttpResponse<StudentParticipationDTO[]>): EntityArrayResponseType {
+        const convertedResponse = participationRes.clone({ body: participationRes.body?.map(fromStudentParticipationDTO) ?? null });
+        this.setAccessRightsParticipationEntityArrayResponseType(convertedResponse);
+        return convertedResponse;
     }
 
     /**
@@ -347,6 +348,12 @@ export class ParticipationService {
         this.convertParticipationResponseDatesFromServer(participationRes);
         this.setAccessRightsParticipationEntityResponseType(participationRes);
         return participationRes;
+    }
+
+    private processParticipationDTOResponseType(participationRes: HttpResponse<StudentParticipationDTO>): EntityResponseType {
+        const convertedResponse = participationRes.clone({ body: participationRes.body ? fromStudentParticipationDTO(participationRes.body) : null });
+        this.setAccessRightsParticipationEntityResponseType(convertedResponse);
+        return convertedResponse;
     }
 
     private setAccessRightsParticipationEntityResponseType(res: EntityResponseType): EntityResponseType {
