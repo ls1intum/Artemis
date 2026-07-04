117fd10486231a1acc6ade5501f3cc1568090f0e
matyasht <matyas.hoffer.toth@gmail.com>
2026-07-04 20:54:28 +0200

    Fix findAllNonStaffByLoginOrNameOrEmailOrRegistrationNumber

--- src/main/java/de/tum/cit/aet/artemis/account/repository/UserRepository.java
+++ src/main/java/de/tum/cit/aet/artemis/account/repository/UserRepository.java
@@ -694,7 +694,7 @@ public interface UserRepository extends ArtemisJpaRepository<User, Long>, JpaSpe
                 )
                 AND NOT EXISTS (
                     SELECT 1 FROM User u JOIN u.authorities a
-                    WHERE u.id = user.id AND a.name IN (
+                    WHERE u.id = user.id AND a IN (
                         :#{T(de.tum.cit.aet.artemis.account.domain.Authority).ADMIN_AUTHORITY},
                         :#{T(de.tum.cit.aet.artemis.account.domain.Authority).SUPER_ADMIN_AUTHORITY}
                     )
