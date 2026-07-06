acf4876f4a2d20f34e2c1517d194360992ef70be
matyasht <matyas.hoffer.toth@gmail.com>
2026-07-06 22:50:39 +0200

    Fix server compilation

--- src/test/java/de/tum/cit/aet/artemis/text/TextAssessmentIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/text/TextAssessmentIntegrationTest.java
@@ -711,7 +711,7 @@ class TextAssessmentIntegrationTest extends AbstractSpringIntegrationIndependent
         assertThat(participation.exercise().examId()).as("exam id is exposed so the client can resolve the exam").isNotNull();
         assertThat(participation.exercise().exerciseGroup()).as("the nested exam shape is exposed so the client can detect exam mode").isNotNull();
         assertThat(participation.exercise().exerciseGroup().exam()).as("the nested exam reference is exposed").isNotNull();
-        assertThat(!participation.exercise().exerciseGroup().exam().examMode().isReal()).as("exam mode is exposed for the text editor").isTrue();
+        assertThat(participation.exercise().exerciseGroup().exam().testExam()).as("exam mode is exposed for the text editor").isTrue();
     }
 
     @Test
