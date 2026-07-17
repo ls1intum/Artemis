534a707981f3248d2c051fa83d692fdeee71a154
Liam <liam.patrice.berger@gmail.com>
2026-07-17 14:18:59 +0200

    Communication: Offer emoji shortcode completion in the posting editor

--- src/main/webapp/app/communication/posting-markdown-editor/posting-markdown-editor.component.ts
+++ src/main/webapp/app/communication/posting-markdown-editor/posting-markdown-editor.component.ts
@@ -35,6 +35,8 @@ import { UrlAction } from 'app/editor/monaco-editor/model/actions/url.action';
 import { AttachmentAction } from 'app/editor/monaco-editor/model/actions/attachment.action';
 import { ConversationDTO } from 'app/communication/shared/entities/conversation/conversation.model';
 import { EmojiAction } from 'app/editor/monaco-editor/model/actions/emoji.action';
+import { EmojiCompletionAction } from 'app/editor/monaco-editor/model/actions/communication/emoji-completion.action';
+import { EmojiSearch } from '@ctrl/ngx-emoji-mart';
 import { Overlay, OverlayPositionBuilder } from '@angular/cdk/overlay';
 import { BulletedListAction } from 'app/editor/monaco-editor/model/actions/bulleted-list.action';
 import { OrderedListAction } from 'app/editor/monaco-editor/model/actions/ordered-list.action';
@@ -66,6 +68,7 @@ export class PostingMarkdownEditorComponent implements OnInit, ControlValueAcces
     private courseManagementService = inject(CourseManagementService);
     private lectureService = inject(LectureService);
     private channelService = inject(ChannelService);
+    private emojiSearch = inject(EmojiSearch);
     viewContainerRef = inject(ViewContainerRef);
     private positionBuilder = inject(OverlayPositionBuilder);
 
@@ -113,6 +116,7 @@ export class PostingMarkdownEditorComponent implements OnInit, ControlValueAcces
             new UnderlineAction(),
             new StrikethroughAction(),
             new EmojiAction(this.viewContainerRef, this.overlay, this.positionBuilder),
+            new EmojiCompletionAction(this.emojiSearch),
             new BulletedListAction(),
             new OrderedListAction(),
             new QuoteAction(),
