1d09a166b2d319600cf032b12fdc38a8e599ac2a
Anian Schleyer <anian.schleyer@tum.de>
2026-07-19 18:30:39 +0200

    Undo JsonTypeInfo (not needed for client for now)

--- src/main/java/de/tum/cit/aet/artemis/quiz/dto/participation/StudentQuizParticipationDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/dto/participation/StudentQuizParticipationDTO.java
@@ -1,19 +1,8 @@
 package de.tum.cit.aet.artemis.quiz.dto.participation;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-import io.swagger.v3.oas.annotations.media.DiscriminatorMapping;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(discriminatorProperty = "type", discriminatorMapping = { @DiscriminatorMapping(value = "without-questions", schema = StudentQuizParticipationWithoutQuestionsDTO.class),
-        @DiscriminatorMapping(value = "with-questions", schema = StudentQuizParticipationWithQuestionsDTO.class),
-        @DiscriminatorMapping(value = "with-solutions", schema = StudentQuizParticipationWithSolutionsDTO.class) }, oneOf = { StudentQuizParticipationWithoutQuestionsDTO.class,
-                StudentQuizParticipationWithQuestionsDTO.class, StudentQuizParticipationWithSolutionsDTO.class })
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-@JsonSubTypes({ @JsonSubTypes.Type(value = StudentQuizParticipationWithoutQuestionsDTO.class, name = "without-questions"),
-        @JsonSubTypes.Type(value = StudentQuizParticipationWithQuestionsDTO.class, name = "with-questions"),
-        @JsonSubTypes.Type(value = StudentQuizParticipationWithSolutionsDTO.class, name = "with-solutions") })
+@Schema(oneOf = { StudentQuizParticipationWithSolutionsDTO.class, StudentQuizParticipationWithQuestionsDTO.class, StudentQuizParticipationWithoutQuestionsDTO.class })
 public sealed interface StudentQuizParticipationDTO
         permits StudentQuizParticipationWithoutQuestionsDTO, StudentQuizParticipationWithQuestionsDTO, StudentQuizParticipationWithSolutionsDTO {
 
