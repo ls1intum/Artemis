5bbdd4f48a300e97d3b44361ed0391c11f69986e
Hannes Harbeck <go29wix@mytum.de>
2026-07-27 17:44:24 +0200

    Programming exercises: drop build config column migration in favour of a separate PR
    Keep the app-layer build config size validation (buildPlanConfiguration
    <= 1 MB, dockerFlags <= 8 KB) and move the LONGTEXT to MEDIUMTEXT column
    migration, together with its changelog guard test, to a separate PR for
    dedicated discussion.

--- src/main/java/de/tum/cit/aet/artemis/programming/domain/ProgrammingExerciseBuildConfig.java
+++ src/main/java/de/tum/cit/aet/artemis/programming/domain/ProgrammingExerciseBuildConfig.java
@@ -34,7 +34,7 @@ public class ProgrammingExerciseBuildConfig extends DomainObject {
     @Column(name = "branch")
     private String branch;
 
-    @Column(name = "build_plan_configuration", columnDefinition = "mediumtext")
+    @Column(name = "build_plan_configuration", columnDefinition = "longtext")
     private String buildPlanConfiguration;
 
     private transient String buildScript;
@@ -58,7 +58,7 @@ public class ProgrammingExerciseBuildConfig extends DomainObject {
     @Column(name = "timeout_seconds")
     private int timeoutSeconds;
 
-    @Column(name = "docker_flags", columnDefinition = "mediumtext")
+    @Column(name = "docker_flags", columnDefinition = "longtext")
     private String dockerFlags;
 
     @OneToOne(mappedBy = "buildConfig")
