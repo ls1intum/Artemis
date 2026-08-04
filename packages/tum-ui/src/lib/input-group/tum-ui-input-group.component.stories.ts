45bcba707254de4ccee7bb4c83526fbdfa45c6fc
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-08-04 17:11:00 +0200

    Development: Move TUM UI into an Angular workspace package (#13323)

---
+++ packages/tum-ui/src/lib/input-group/tum-ui-input-group.component.stories.ts
@@ -0,0 +1,52 @@
+import { moduleMetadata } from '@storybook/angular-vite';
+import type { Meta, StoryObj } from '@storybook/angular-vite';
+
+import { formStoryDecorator } from '../../../.storybook/story-decorators';
+import { TumUiInputDirective } from '../input/tum-ui-input.directive';
+import { TumUiInputGroupAddonComponent } from './tum-ui-input-group-addon.component';
+import { TumUiInputGroupComponent } from './tum-ui-input-group.component';
+
+interface InputGroupStoryArgs {
+    label: string;
+    placeholder: string;
+    prefix: string;
+    suffix: string;
+}
+
+const meta = {
+    title: 'Forms/Input Group',
+    component: TumUiInputGroupComponent,
+    subcomponents: {
+        Addon: TumUiInputGroupAddonComponent,
+        Input: TumUiInputDirective,
+    },
+    args: {
+        label: 'Budget',
+        placeholder: '0',
+        prefix: '€',
+        suffix: 'EUR',
+    },
+    decorators: [
+        formStoryDecorator,
+        moduleMetadata({
+            imports: [TumUiInputDirective, TumUiInputGroupAddonComponent],
+        }),
+    ],
+    render: (args) => ({
+        props: args,
+        template: `
+            <label for="budget">{{ label }}</label>
+            <tum-ui-input-group>
+                <tum-ui-input-group-addon>{{ prefix }}</tum-ui-input-group-addon>
+                <input id="budget" tumUiInput inputmode="decimal" [placeholder]="placeholder" />
+                <tum-ui-input-group-addon>{{ suffix }}</tum-ui-input-group-addon>
+            </tum-ui-input-group>
+        `,
+    }),
+} satisfies Meta<InputGroupStoryArgs>;
+
+export default meta;
+
+type Story = StoryObj<InputGroupStoryArgs>;
+
+export const Default: Story = {};
