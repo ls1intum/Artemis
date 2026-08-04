32074628a174b3fec90b89ff3e2d6f3e1a275c83
matyasht <matyas.hoffer.toth@gmail.com>
2026-08-04 22:05:20 +0300

    Fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -60,6 +60,7 @@ export default tseslint.config(
             '.idea/',
             '.jhipster/',
             '.venv/',
+            'bin/',
             'build/',
             'coverage/',
             'docker/',
