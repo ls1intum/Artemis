a2be3090e98d464f6b27db59c9613a1a94f6e8a3
Nihad Uddin <nihad.uddin@tum.de>
2026-08-10 12:26:05 +0200

    Quiz exercises: preserve JSON counter dirty checking

--- src/main/java/de/tum/cit/aet/artemis/quiz/domain/PointCounter.java
+++ src/main/java/de/tum/cit/aet/artemis/quiz/domain/PointCounter.java
@@ -1,17 +1,21 @@
 package de.tum.cit.aet.artemis.quiz.domain;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import de.tum.cit.aet.artemis.core.domain.DomainObject;
 
 /**
  * Counts rated and unrated results for one integer point bucket of a {@link QuizPointStatistic}.
  * <p>
- * Point counters are stored as a JSON list in {@code quiz_statistic.counters}. Their ids are local to the owning statistic and remain stable while the bucket exists.
+ * Point counters are stored as a JSON list in {@code quiz_statistic.counters}. Their ids are local to the owning statistic and remain stable while the bucket exists. Equality
+ * includes every persisted value so that Hibernate detects in-place counter changes when dirty-checking the JSON column.
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class PointCounter extends DomainObject {
+public class PointCounter {
+
+    @JsonProperty("id")
+    private Long id;
 
     @JsonProperty("points")
     private double points;
@@ -21,6 +25,14 @@ public class PointCounter extends DomainObject {
 
     @JsonProperty("unRatedCounter")
     private int unRatedCounter;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public double getPoints() {
         return points;
@@ -44,6 +56,27 @@ public class PointCounter extends DomainObject {
 
     public void setUnRatedCounter(int unRatedCounter) {
         this.unRatedCounter = unRatedCounter;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        PointCounter pointCounter = (PointCounter) obj;
+        if (id == null || pointCounter.id == null) {
+            return false;
+        }
+        return Double.compare(points, pointCounter.points) == 0 && ratedCounter == pointCounter.ratedCounter && unRatedCounter == pointCounter.unRatedCounter
+                && Objects.equals(id, pointCounter.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, points, ratedCounter, unRatedCounter);
     }
 
     @Override
