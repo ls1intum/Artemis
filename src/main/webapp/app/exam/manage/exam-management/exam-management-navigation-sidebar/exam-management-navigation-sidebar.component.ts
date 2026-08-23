4b5b3b1768dd8335c1dcd390de65ab318c60bf9a
matyasht <matyas.hoffer.toth@gmail.com>
2026-08-23 17:44:12 +0200

    Improve the navigation

--- src/main/webapp/app/exam/manage/exam-management/exam-management-navigation-sidebar/exam-management-navigation-sidebar.component.ts
+++ src/main/webapp/app/exam/manage/exam-management/exam-management-navigation-sidebar/exam-management-navigation-sidebar.component.ts
@@ -4,7 +4,7 @@ import { CourseSidebarToggleButtonComponent } from 'app/course/shared/course-sid
 import { DocumentationButtonComponent, DocumentationType } from 'app/shared-ui/components/buttons/documentation-button/documentation-button.component';
 import { Exam } from 'app/exam/shared/entities/exam.model';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
-import { faChevronRight, faClipboard, faEye, faFlaskVial, faHeartBroken, faListAlt, faThList, faUser, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faClipboard, faEye, faFlaskVial, faHeartBroken, faInfoCircle, faListAlt, faThList, faUser, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { SidebarSubpageItem } from 'app/exam/manage/exam-management/exam-management-navigation-sidebar/sidebar-subpage-item/sidebar-subpage-item';
@@ -40,6 +40,7 @@ export class ExamManagementNavigationSidebarComponent {
     // Icons
     readonly faChevronRight = faChevronRight;
     readonly faListAlt = faListAlt;
+    readonly faInfoCircle = faInfoCircle;
     readonly faThList = faThList;
     readonly faWrench = faWrench;
     readonly faUser = faUser;
