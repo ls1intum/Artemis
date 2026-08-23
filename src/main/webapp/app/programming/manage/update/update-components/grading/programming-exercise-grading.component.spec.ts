5b7222dc31b16b32671eece057ac79de5f631ab9
Marlon Nienaber <marlonnienaber@web.de>
2026-08-23 12:25:21 +0200

    patch failing client tests

--- src/main/webapp/app/programming/manage/update/update-components/grading/programming-exercise-grading.component.spec.ts
+++ src/main/webapp/app/programming/manage/update/update-components/grading/programming-exercise-grading.component.spec.ts
@@ -104,7 +104,7 @@ describe('ProgrammingExerciseGradingComponent', () => {
         const editGroupDatesSpy = vi.spyOn(comp.editGroupDates, 'emit');
         fixture.detectChanges();
 
-        const timelineControls = fixture.debugElement.query(By.css('jhi-programming-exercise-timeline .assessment-layout'));
+        const timelineControls = fixture.debugElement.query(By.css('jhi-programming-exercise-timeline [data-testid="assessment-layout"]'));
         const notice = timelineControls.query(By.directive(ExerciseGroupDateNoticeComponent));
 
         expect(timelineControls.nativeElement.firstElementChild).toBe(notice.nativeElement);
