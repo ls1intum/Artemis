80619be591941360e280234342ffb89f2b3f34a6
Stephan Krusche <krusche@tum.de>
2026-08-27 01:20:56 +0200

    Exam mode: Report the first correction round in the exam score statistics (#13574)

--- src/main/java/de/tum/cit/aet/artemis/exercise/dto/CorrectionRoundResultDTO.java
+++ src/main/java/de/tum/cit/aet/artemis/exercise/dto/CorrectionRoundResultDTO.java
@@ -21,8 +21,9 @@ import de.tum.cit.aet.artemis.assessment.domain.AssessmentType;
  * @param assessmentType  how the result was produced
  * @param completionDate  when the assessment was finished, null while it is still a draft
  * @param hasComplaint    whether the student complained about this result
+ * @param score           the score of the result in percent, null while the assessment is still a draft
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record CorrectionRoundResultDTO(long submissionId, long resultId, @Nullable Integer correctionRound, @Nullable AssessmentType assessmentType,
-        @Nullable ZonedDateTime completionDate, @Nullable Boolean hasComplaint) {
+        @Nullable ZonedDateTime completionDate, @Nullable Boolean hasComplaint, @Nullable Double score) {
 }
