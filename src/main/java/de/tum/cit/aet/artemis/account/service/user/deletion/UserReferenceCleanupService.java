51a429cec0756a123e2bce24187af4f672c07b39
Josi Suckart <josef.suckart@tum.de>
2026-09-07 22:40:29 +0200

    Presentation: handle user deletion references

--- src/main/java/de/tum/cit/aet/artemis/account/service/user/deletion/UserReferenceCleanupService.java
+++ src/main/java/de/tum/cit/aet/artemis/account/service/user/deletion/UserReferenceCleanupService.java
@@ -82,6 +82,10 @@ public class UserReferenceCleanupService {
         bindings.put(UserDeletionReferencePolicy.RESULT_ASSESSOR, bind(assessment::countAssessedResults, assessment::detachAssessedResults));
         bindings.put(UserDeletionReferencePolicy.PARTICIPANT_SCORE, bind(assessment::countStudentScores, assessment::deleteStudentScores));
         bindings.put(UserDeletionReferencePolicy.TUTOR_PARTICIPATION, bind(assessment::countTutorParticipations, assessment::deleteTutorParticipations));
+        bindings.put(UserDeletionReferencePolicy.PRESENTATION_ASSESSMENT_STUDENT,
+                bind(assessment::countPresentationAssessmentStudents, assessment::deletePresentationAssessmentStudents));
+        bindings.put(UserDeletionReferencePolicy.PRESENTATION_ASSESSMENT_INSTANCE_STUDENT,
+                bind(assessment::countPresentationAssessmentInstanceStudents, assessment::deletePresentationAssessmentInstanceStudents));
 
         bindings.put(UserDeletionReferencePolicy.SUBMISSION_VERSION_AUTHOR, bind(exercise::countSubmissionVersions, exercise::deleteSubmissionVersions));
         bindings.put(UserDeletionReferencePolicy.EXERCISE_VERSION_AUTHOR, bind(exercise::countExerciseVersions, exercise::deleteExerciseVersions));
