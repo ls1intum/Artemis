f8d9cfaa845b17e72249311fe045e81073975e66
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-09-09 23:12:21 +0200

    Hyperion: Preserve session-bound authorization after the trunk update

--- src/main/java/de/tum/cit/aet/artemis/core/config/websocket/WebsocketConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/websocket/WebsocketConfiguration.java
@@ -388,7 +388,7 @@ public class WebsocketConfiguration extends DelegatingWebSocketMessageBrokerConf
 
             Optional<Long> hyperionExerciseId = getExerciseIdFromHyperionStateDestination(destination);
             if (hyperionExerciseId.isPresent()) {
-                return authorizationCheckService.isAtLeastEditorInExercise(principal.getName(), hyperionExerciseId.get());
+                return userRepository.isAtLeastEditorInExercise(principal.getName(), hyperionExerciseId.get()) || hasAdministratorAccess(principal);
             }
             if (destination.equals("/topic/user-registry") || destination.equals("/topic/unresolved-user") || destination.equals("/topic/hyperion")
                     || destination.startsWith("/topic/hyperion/")) {
