c7c5dcba59038d467bc3f82817c4c49567c225bf
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-09-09 21:09:53 +0200

    Hyperion: Link generation primitives from the component reference

--- documentation/src/components/StorybookRedirect/storybook-reference.generated.ts
+++ documentation/src/components/StorybookRedirect/storybook-reference.generated.ts
@@ -20,6 +20,8 @@ export function storybookStoryFromHash(hash: string): string {
             return 'data-display-card--docs';
         case 'data-display-chip':
             return 'data-display-chip--docs';
+        case 'data-display-empty':
+            return 'data-display-empty--docs';
         case 'data-display-list':
             return 'data-display-list--docs';
         case 'data-display-native-table':
@@ -28,6 +30,8 @@ export function storybookStoryFromHash(hash: string): string {
             return 'data-display-paginator--docs';
         case 'data-display-panel':
             return 'data-display-panel--docs';
+        case 'data-display-prose':
+            return 'data-display-prose--docs';
         case 'data-display-table':
             return 'data-display-table--docs';
         case 'data-display-tag':
@@ -46,6 +50,12 @@ export function storybookStoryFromHash(hash: string): string {
             return 'feedback-progress-bar--docs';
         case 'feedback-progress-spinner':
             return 'feedback-progress-spinner--docs';
+        case 'feedback-skeleton':
+            return 'feedback-skeleton--docs';
+        case 'feedback-status-dot':
+            return 'feedback-status-dot--docs';
+        case 'feedback-stepper':
+            return 'feedback-stepper--docs';
         case 'forms-autocomplete':
             return 'forms-autocomplete--docs';
         case 'forms-checkbox':
