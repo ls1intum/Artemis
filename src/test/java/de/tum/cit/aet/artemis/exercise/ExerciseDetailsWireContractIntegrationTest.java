4b509d003e9ef885db157f45f8fc4aa732aa41f0
woh <wolfgang.hobmaier@and-solution.com>
2026-09-15 18:17:20 +0200

    Exercise: Cover remaining participant and result fields in the details wire test

--- src/test/java/de/tum/cit/aet/artemis/exercise/ExerciseDetailsWireContractIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/exercise/ExerciseDetailsWireContractIntegrationTest.java
@@ -33,6 +33,7 @@ import de.tum.cit.aet.artemis.core.domain.Language;
 import de.tum.cit.aet.artemis.core.service.AuthorizationCheckService;
 import de.tum.cit.aet.artemis.course.domain.Course;
 import de.tum.cit.aet.artemis.course.repository.CourseAthenaConfigRepository;
+import de.tum.cit.aet.artemis.exam.util.ExamUtilService;
 import de.tum.cit.aet.artemis.exercise.domain.Exercise;
 import de.tum.cit.aet.artemis.exercise.domain.ExerciseMode;
 import de.tum.cit.aet.artemis.exercise.domain.Submission;
@@ -41,6 +42,7 @@ import de.tum.cit.aet.artemis.exercise.domain.Team;
 import de.tum.cit.aet.artemis.exercise.domain.participation.StudentParticipation;
 import de.tum.cit.aet.artemis.exercise.participation.util.ParticipationFactory;
 import de.tum.cit.aet.artemis.exercise.participation.util.ParticipationUtilService;
+import de.tum.cit.aet.artemis.exercise.repository.TeamRepository;
 import de.tum.cit.aet.artemis.exercise.service.ExerciseService;
 import de.tum.cit.aet.artemis.exercise.service.ParticipationService;
 import de.tum.cit.aet.artemis.exercise.team.TeamUtilService;
@@ -142,9 +144,21 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
     @Autowired
     private TextExerciseUtilService textExerciseUtilService;
 
+    @Autowired
+    private ExamUtilService examUtilService;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
     @BeforeEach
     void initTestCase() {
         userUtilService.addUsers(TEST_PREFIX, 2, 1, 0, 1);
+        User student = userUtilService.getUserByLogin(STUDENT);
+        student.setImageUrl("https://artemis.example/users/" + STUDENT + ".png");
+        userTestRepository.save(student);
+        User tutor = userUtilService.getUserByLogin(TUTOR);
+        tutor.setImageUrl("https://artemis.example/users/" + TUTOR + ".png");
+        userTestRepository.save(tutor);
     }
 
     @Test
@@ -192,6 +206,8 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
         textExercise.setMode(ExerciseMode.TEAM);
         textExercise = exerciseRepository.save(textExercise);
         Team textTeam = teamUtilService.createTeam(Set.of(student, userUtilService.getUserByLogin(TEST_PREFIX + "student2")), tutor, textExercise, TEST_PREFIX + "textteam");
+        textTeam.setImage("data:image/png;base64,iVBORw0KGgo=");
+        textTeam = teamRepository.save(textTeam);
         StudentParticipation textParticipation = participationUtilService.addTeamParticipationForExercise(textExercise, textTeam.getId());
         TextSubmission textSubmission = new TextSubmission();
         textSubmission.setText("team text");
@@ -203,8 +219,12 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
         programmingExercise.setReleaseDate(ZonedDateTime.now().minusDays(1));
         programmingExercise = exerciseRepository.save(programmingExercise);
         Team programmingTeam = teamUtilService.createTeam(Set.of(student), tutor, programmingExercise, TEST_PREFIX + "progteam");
-        ProgrammingExerciseStudentParticipation programmingParticipation = studentParticipationRepository
-                .save(ParticipationFactory.generateTeamProgrammingExerciseStudentParticipation(programmingExercise, programmingTeam));
+        programmingTeam.setImage("data:image/png;base64,iVBORw0KGgo=");
+        programmingTeam = teamRepository.save(programmingTeam);
+        ProgrammingExerciseStudentParticipation teamParticipationToSave = ParticipationFactory.generateTeamProgrammingExerciseStudentParticipation(programmingExercise,
+                programmingTeam);
+        teamParticipationToSave.setBranch("main");
+        ProgrammingExerciseStudentParticipation programmingParticipation = studentParticipationRepository.save(teamParticipationToSave);
         ProgrammingSubmission programmingSubmission = new ProgrammingSubmission();
         programmingSubmission.setCommitHash("abc123");
         addSubmissionWithResult(programmingParticipation, programmingSubmission, AssessmentType.AUTOMATIC);
@@ -299,8 +319,11 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
         exercise.setMode(ExerciseMode.TEAM);
         exercise = exerciseRepository.save(exercise);
         Team team = teamUtilService.createTeam(Set.of(userUtilService.getUserByLogin(STUDENT)), userUtilService.getUserByLogin(TUTOR), exercise, TEST_PREFIX + "substeam");
+        team.setImage("data:image/png;base64,iVBORw0KGgo=");
+        team = teamRepository.save(team);
         ProgrammingExerciseStudentParticipation participation = ParticipationFactory.generateTeamProgrammingExerciseStudentParticipation(exercise, team);
         participation.setRepositoryUri("http://localhost:8080/git/TST/tst-team.git");
+        participation.setBranch("main");
         participation = studentParticipationRepository.save(participation);
         ProgrammingSubmission submission = new ProgrammingSubmission();
         submission.setCommitHash("0123456789abcdef");
@@ -311,6 +334,19 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
         assertThat(response.size()).isEqualTo(2);
         assertThat(response.at("/0/participation/type").asString()).isEqualTo("programming");
         assertThat(response.at("/0/participation/team/shortName").asString()).isEqualTo(TEST_PREFIX + "substeam");
+    }
+
+    @Test
+    @WithMockUser(username = INSTRUCTOR, roles = "INSTRUCTOR")
+    void submissions_examExerciseParticipation_matchesTheEntityWire() throws Exception {
+        TextExercise examExercise = examUtilService.addEnrolledCourseExamExerciseGroupWithOneTextExercise(TEST_PREFIX);
+        StudentParticipation participation = participationUtilService.createAndSaveParticipationForExercise(examExercise, STUDENT);
+        TextSubmission submission = new TextSubmission();
+        submission.setText("exam text");
+        addSubmissionWithResult(participation, submission, AssessmentType.SEMI_AUTOMATIC);
+
+        JsonNode response = assertSubmissionsMatchEntityWire(participation.getId());
+        assertThat(response.at("/0/participation/exercise/type").asString()).isEqualTo("text");
     }
 
     @Test
@@ -400,6 +436,7 @@ class ExerciseDetailsWireContractIntegrationTest extends AbstractSpringIntegrati
         result.setRated(true);
         result.setSuccessful(true);
         result.setScore(80.0);
+        result.setCorrectionRound(1);
         return resultRepository.save(result);
     }
 
