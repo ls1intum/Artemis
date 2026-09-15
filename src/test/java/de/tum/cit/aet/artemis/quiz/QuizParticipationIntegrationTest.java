a90c398d161cb4d2370fd73a8711b2d631c680b2
Nihad Uddin <nihad.uddin@tum.de>
2026-09-15 08:13:29 +0200

    Quiz exercises: Collapse resource package

--- src/test/java/de/tum/cit/aet/artemis/quiz/QuizParticipationIntegrationTest.java
+++ src/test/java/de/tum/cit/aet/artemis/quiz/QuizParticipationIntegrationTest.java
@@ -28,7 +28,7 @@ import de.tum.cit.aet.artemis.quiz.dto.submission.QuizSubmissionFromStudentDTO;
 import de.tum.cit.aet.artemis.quiz.service.QuizExerciseService;
 import de.tum.cit.aet.artemis.quiz.util.QuizExerciseFactory;
 import de.tum.cit.aet.artemis.quiz.util.QuizExerciseUtilService;
-import de.tum.cit.aet.artemis.quiz.web.openapi.QuizParticipationResource;
+import de.tum.cit.aet.artemis.quiz.web.QuizParticipationResource;
 import de.tum.cit.aet.artemis.shared.base.AbstractSpringIntegrationIndependentTest;
 
 /**
