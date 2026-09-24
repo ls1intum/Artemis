2a9f327212ff0ecd17d6554a0b5d6882d489afa6
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2026-09-24 11:22:13 +0200

    Defer unused worker contracts to their consumer layers

--- src/main/java/de/tum/cit/aet/artemis/aiworker/domain/WorkerCommandType.java
+++
@@ -1,6 +0,0 @@
-package de.tum.cit.aet.artemis.aiworker.domain;
-
-/** Execution transport states, independent of workload interpretation. */
-public enum WorkerCommandType {
-    START, CANCEL, FINISH, RENEW
-}
